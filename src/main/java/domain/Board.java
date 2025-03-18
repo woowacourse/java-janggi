f16f044caeb3846ea08f73cb2c12573d32380833
민경윤 <ruddbs3308@naver.com>
2025-03-18 19:29:35 +0900

    feat: 장기판에 말들에 대한 초기 배치 상태 결정 기능 구현

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -11,16 +11,19 @@ public class Board {
         this.pieces = pieces;
     }
 
-    public static Board initialize() {
+    public static Board initialize(
+            final PiecesSetupRule greenSetupRule,
+            final PiecesSetupRule redSetupRule
+    ) {
         Map<Position, Piece> pieces = new HashMap<>();
 
         pieces.put(new Position(0, 0), new Chariot(Team.GREEN));
-        pieces.put(new Position(1, 0), new Elephant(Team.GREEN));
-        pieces.put(new Position(2, 0), new Horse(Team.GREEN));
+        pieces.put(greenSetupRule.getPiecePosition(Team.GREEN).get(PieceType.ELEPHANT).getFirst(), new Elephant(Team.GREEN));
+        pieces.put(greenSetupRule.getPiecePosition(Team.GREEN).get(PieceType.HORSE).getFirst(), new Horse(Team.GREEN));
         pieces.put(new Position(3, 0), new Guard(Team.GREEN));
         pieces.put(new Position(5, 0), new Guard(Team.GREEN));
-        pieces.put(new Position(6, 0), new Horse(Team.GREEN));
-        pieces.put(new Position(7, 0), new Elephant(Team.GREEN));
+        pieces.put(greenSetupRule.getPiecePosition(Team.GREEN).get(PieceType.HORSE).getLast(), new Horse(Team.GREEN));
+        pieces.put(greenSetupRule.getPiecePosition(Team.GREEN).get(PieceType.ELEPHANT).getLast(), new Elephant(Team.GREEN));
         pieces.put(new Position(8, 0), new Chariot(Team.GREEN));
         pieces.put(new Position(1, 2), new Cannon(Team.GREEN));
         pieces.put(new Position(7, 2), new Cannon(Team.GREEN));
@@ -32,12 +35,12 @@ public class Board {
         pieces.put(new Position(4, 1), new General(Team.GREEN));
 
         pieces.put(new Position(0, 9), new Chariot(Team.RED));
-        pieces.put(new Position(1, 9), new Elephant(Team.RED));
-        pieces.put(new Position(2, 9), new Horse(Team.RED));
+        pieces.put(redSetupRule.getPiecePosition(Team.RED).get(PieceType.ELEPHANT).getFirst(), new Elephant(Team.RED));
+        pieces.put(redSetupRule.getPiecePosition(Team.RED).get(PieceType.HORSE).getFirst(), new Horse(Team.RED));
         pieces.put(new Position(3, 9), new Guard(Team.RED));
         pieces.put(new Position(5, 9), new Guard(Team.RED));
-        pieces.put(new Position(6, 9), new Horse(Team.RED));
-        pieces.put(new Position(7, 9), new Elephant(Team.RED));
+        pieces.put(redSetupRule.getPiecePosition(Team.RED).get(PieceType.HORSE).getLast(), new Horse(Team.RED));
+        pieces.put(redSetupRule.getPiecePosition(Team.RED).get(PieceType.ELEPHANT).getLast(), new Elephant(Team.RED));
         pieces.put(new Position(8, 9), new Chariot(Team.RED));
         pieces.put(new Position(1, 7), new Cannon(Team.RED));
         pieces.put(new Position(7, 7), new Cannon(Team.RED));
---
+++ src/main/java/domain/PieceType.java
@@ -0,0 +1,11 @@
+package domain;
+
+public enum PieceType {
+    CANNON,
+    CHARIOT,
+    ELEPHANT,
+    GENERAL,
+    GUARD,
+    HORSE,
+    쭈;
+}
