fa043655892f0854827d68a6d168c33755cec8c2
taek2222 <oot309@daum.net>
2025-03-18 17:33:40 +0900

    test: 잘못된 입력인 경우 테스트 추가

--- src/test/java/domain/piece/PositionTest.java
+++ src/test/java/domain/piece/PositionTest.java
@@ -1,8 +1,9 @@
 package domain.piece;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class PositionTest {
 
@@ -10,5 +11,12 @@ class PositionTest {
     void 좌표를_생성한다() {
         assertThatCode(() -> new Position(1, 2))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 잘못된_좌표_입력인_경우_예외가_발생한다() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Position(-1, 5))
+                .withMessage("[ERROR] 좌표 입력은 9X10 보드 이내만 가능합니다.");
     }
 }
