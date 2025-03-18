5536b559fe2d2a3be8104107e45b93a3fcb166eb
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-18 14:14:39 +0900

    feat(green): 장기판의 크기는 9 * 10 이다

--- src/main/java/domain/JanggiBoard.java
+++ src/main/java/domain/JanggiBoard.java
@@ -2,8 +2,9 @@ package domain;
 
 public class JanggiBoard {
 
+    private final int[][] board = new int[9][10];
 
     public int[][] getBoard() {
-        return new int[9][9];
+        return board;
     }
 }
