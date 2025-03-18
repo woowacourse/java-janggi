cb3d751244919c3356de359c589988c9ce23db6a
ykmxxi <ykmxxi97@gmail.com>
2025-03-18 16:34:29 +0900

    feat: 팀 정보를 생성하는 기능 구현

---
+++ src/test/java/domain/TeamTest.java
@@ -0,0 +1,16 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class TeamTest {
+
+    @EnumSource(Team.class)
+    @ParameterizedTest
+    void 팀은_홍팀과_청팀이_있다(Team team) {
+        assertThat(team).isEqualByComparingTo(team);
+    }
+
+}
