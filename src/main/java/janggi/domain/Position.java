60124294828ee5e1da46f5ce1ee9b8bc9cc56176
CheChe903 <cheche903@naver.com>
2025-03-18 16:36:24 +0900

    feat(Position): 위치 동등성 로직 및 단위 테스트 구현

---
+++ src/main/java/janggi/domain/Position.java
@@ -0,0 +1,28 @@
+package janggi.domain;
+
+import java.util.Objects;
+
+public class Position {
+
+    private final int x;
+    private final int y;
+
+    public Position(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
---
+++ src/test/java/janggi/domain/PositionTest.java
@@ -0,0 +1,26 @@
+package janggi.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PositionTest {
+
+    @DisplayName("같은 위치면 true를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1, 1, 1, 1, true", " 1, 1, 2, 2, false"
+    })
+    void equalTest(final int firstX, final int firstY, final int secondX, final int secondY, final boolean expected) {
+
+        // given
+        Position firstPosition = new Position(firstX, firstY);
+        Position secondPosition = new Position(secondX, secondY);
+
+        // when & then
+        assertThat(firstPosition.equals(secondPosition)).isEqualTo(expected);
+    }
+
+}
