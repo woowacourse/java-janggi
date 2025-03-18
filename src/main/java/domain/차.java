416245a069c73b8e7cc8864a561ab7e5783e54d1
dltmddus <tmddus4671@gmail.com>
2025-03-18 15:42:43 +0900

    feat: 차 기물 도메인 추가

--- src/main/java/domain/차.java
+++ src/main/java/domain/차.java
@@ -1,4 +1,8 @@
 package domain;
 
 public class 차 extends Piece {
+
+    public boolean canMove(int beforeRow, int beforeColumn, int afterRow, int afterColumn) {
+        return beforeRow == afterRow || beforeColumn == afterColumn;
+    }
 }
---
+++ src/test/java/domain/차Test.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 차Test {
+
+    @ParameterizedTest
+    @CsvSource({"5, 1", "0, 9", "1, 1", "0, 2"})
+    void 상_하_좌_우로_여러_칸_이동할_수_있다(int afterRow, int afterColumn) {
+        // given
+        차 piece = new 차();
+        int beforeRow = 0;
+        int beforeColumn = 1;
+
+        // when & then
+        Assertions.assertThat(piece.canMove(beforeRow, beforeColumn, afterRow, afterColumn))
+                .isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"9, 2", "5, 8"})
+    void 상_하_좌_우가_아닌_경우_이동할_수_없다(int afterRow, int afterColumn) {
+        // given
+        차 piece = new 차();
+        int beforeRow = 0;
+        int beforeColumn = 1;
+
+        // when & then
+        Assertions.assertThat(piece.canMove(beforeRow, beforeColumn, afterRow, afterColumn))
+                .isFalse();
+    }
+}
