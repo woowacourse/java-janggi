1d48c3b5bb8a27104d5ea4f3a8601c0e043f558e
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 19:47:51 +0900

    RED) test(model): 사 이동 경로 테스트

--- src/test/java/SaTest.java
+++ src/test/java/SaTest.java
@@ -1,6 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import model.Jang;
+import model.Point;
 import model.Sa;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -32,6 +33,27 @@ public class SaTest {
         public void test3() {
             Sa sa = new Sa("red");
             assertThat(sa.canMove(0, 0, 2, 0)).isFalse();
+        }
+    }
+    @Nested
+    @DisplayName("장 이동 경로 계산 테스트")
+    class SaCalculatePathTest {
+        @Test
+        @DisplayName("수직")
+        public void test1() {
+            Sa sa = new Sa("red");
+            Point point =new Point(0,1);
+
+            assertThat(sa.calculatePath(0,0,0,1).contains(point)).isTrue();
+        }
+
+        @Test
+        @DisplayName("수평")
+        public void test2() {
+            Sa sa = new Sa("red");
+            Point point =new Point(1,0);
+
+            assertThat(sa.calculatePath(0,0,1,0).contains(point)).isTrue();
         }
     }
 }
