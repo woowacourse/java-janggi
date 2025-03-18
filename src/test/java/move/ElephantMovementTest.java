13d248588c7583c6c3911c50be26173d16c48372
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-18 17:22:28 +0900

    test: 상 이동 테스트 작성

---
+++ src/test/java/move/ElephantMovementTest.java
@@ -0,0 +1,144 @@
+package move;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ElephantMovementTest {
+
+    @Test
+    @DisplayName("위쪽 1칸, 왼쪽 대각선 2칸으로 이동할 수 있다.")
+    void test1() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(0, -1);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("위쪽 1칸, 오른쪽 대각선 2칸으로 이동할 수 있다.")
+    void test2() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(4, -1);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("왼쪽 1칸, 위쪽 대각선 2칸으로 이동할 수 있다.")
+    void test3() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(-1, 0);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("왼쪽 1칸, 아래쪽 대각선 2칸으로 이동할 수 있다.")
+    void test4() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(-1, 4);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("아래쪽 1칸, 왼쪽 대각선 2칸으로 이동할 수 있다.")
+    void test5() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(0, 5);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("아래쪽 1칸, 오른쪽 대각선 2칸으로 이동할 수 있다.")
+    void test6() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(4, 5);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("오른쪽 1칸, 위쪽 대각선 2칸으로 이동할 수 있다.")
+    void test7() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(5, 0);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("오른쪽 1칸, 아래쪽 대각선 2칸으로 이동할 수 있다.")
+    void test8() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(5, 4);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        Position result = elephantMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("직선 1칸, 대각선 2칸을 제외하고 움직일 수 없다.")
+    void test9() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(8, 3);
+        ElephantMovement elephantMovement = new ElephantMovement();
+
+        //when
+        //then
+        Assertions.assertThatThrownBy(() -> elephantMovement.move(from, to))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
