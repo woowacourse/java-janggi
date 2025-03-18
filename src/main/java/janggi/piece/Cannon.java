4972676ca6a5cfd35bbff588db5d68b5e5ea0cd7
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-18 20:32:21 +0900

    feat: Cannon 이동 검증 로직 추가

--- src/main/java/janggi/piece/Cannon.java
+++ src/main/java/janggi/piece/Cannon.java
@@ -13,11 +13,26 @@ public class Cannon extends Piece {
 
     @Override
     public Piece move(final Board board, final Position destination) {
+        validateMove(board, destination);
         return new Cannon(destination, team);
+    }
+
+    private void validateMove(final Board board, final Position destination) {
+        if (isAllyInDestination(board, destination) || isInvalidMove(board, destination)) {
+            throw new IllegalArgumentException("이동할 수 없는 지점입니다.");
+        }
+    }
+
+    private boolean isAllyInDestination(final Board board, final Position destination) {
+        return board.isExists(destination) && board.isAlly(destination, this.team);
     }
 
     @Override
     public Score die() {
         return Score.Cannon();
+    }
+
+    private boolean isInvalidMove(final Board board, final Position destination) {
+        return destination.row() != position.row() && destination.column() != position.column();
     }
 }
