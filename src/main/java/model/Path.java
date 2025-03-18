be03c7a322d687ceaedb7050525aaa9b98f569ad
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 19:23:32 +0900

    feat(model) : Path 생성

---
+++ src/main/java/model/Path.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Path {
+    private List<Point> path;
+
+    public Path() {
+        this.path = new ArrayList<>();
+    }
+
+}
