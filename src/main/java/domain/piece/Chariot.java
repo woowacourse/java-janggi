1dc05bde25c79c6d424e3a84d731cbf5571fee24
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-18 20:07:11 +0900

    feat(green): 차의 이동 경로를 계산할 수 있다.

--- src/main/java/domain/piece/Chariot.java
+++ src/main/java/domain/piece/Chariot.java
@@ -1,9 +1,12 @@
 package domain.piece;
 
 import domain.Team;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Chariot extends Piece{
+public class Chariot extends Piece {
+
+    private final List<Move> moves = List.of(Move.FRONT, Move.BACK, Move.RIGHT, Move.LEFT);
 
     public Chariot(Team team) {
         super(team);
@@ -11,6 +14,43 @@ public class Chariot extends Piece{
 
     @Override
     public List<Move> calculatePath(int startRow, int startColumn, int targetRow, int targetColumn) {
-        return List.of();
+
+        //1. 세로가 같고 가로가 다르다
+        List<Move> moves = new ArrayList<>();
+        if (startRow != targetRow && startColumn == targetColumn) {
+            int count = targetRow - startRow;
+            if (count < 0) {
+                for (int i = 0; i < Math.abs(count); i++) {
+                    moves.add(Move.FRONT);
+                }
+            }
+            if (count > 0) {
+                for (int i = 0; i < count; i++) {
+                    moves.add(Move.BACK);
+                }
+            }
+        }
+        //2. 세로가 다르고 가로가 같다.
+        if (startRow == targetRow && startColumn != targetColumn) {
+            int count = targetColumn - startColumn;
+            if (count < 0) {
+                for (int i = 0; i < Math.abs(count); i++) {
+                    moves.add(Move.LEFT);
+                }
+            }
+            if (count > 0) {
+                for (int i = 0; i < count; i++) {
+                    moves.add(Move.RIGHT);
+                }
+            }
+        }
+        //3. 세로가 같고, 가로가 같다
+        if (startRow == targetRow && startColumn == targetColumn) {
+            throw new IllegalArgumentException("말을 움직여 주세요");
+        }
+        if (startRow != targetRow && startColumn != targetColumn) {
+            throw new IllegalArgumentException("이 위치로는 움직일 수 없습니다.");
+        }
+        return moves;
     }
 }
--- src/test/java/ChariotTest.java
+++ src/test/java/ChariotTest.java
@@ -4,6 +4,7 @@ import domain.piece.Horse;
 import domain.piece.Move;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +13,20 @@ public class ChariotTest {
     @DisplayName("차의 이동 경로를 계산할 수 있다")
     @Test
     void test1() {
-        Chariot horse = new Chariot(Team.RED);
+        Chariot chariot = new Chariot(Team.RED);
 
-        List<Move> moves = horse.calculatePath(0, 0, 0, 6);
-        List<Move> expected = List.of(Move.FRONT,Move.FRONT,Move.FRONT,Move.FRONT,Move.FRONT,Move.FRONT);
+        List<Move> moves1 = chariot.calculatePath(0, 0, 0, 6);
+        List<Move> moves2 = chariot.calculatePath(0, 0, 7, 0);
+        List<Move> moves3 = chariot.calculatePath(6, 0, 1, 0);
+        List<Move> expected1 = List.of(Move.RIGHT,Move.RIGHT,Move.RIGHT,Move.RIGHT,Move.RIGHT,Move.RIGHT);
+        List<Move> expected2 = List.of(Move.BACK,Move.BACK,Move.BACK,Move.BACK,Move.BACK,Move.BACK,Move.BACK);
+        List<Move> expected3 = List.of(Move.FRONT,Move.FRONT,Move.FRONT,Move.FRONT,Move.FRONT);
 
-        Assertions.assertThat(moves).isEqualTo(expected);
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(moves1).isEqualTo(expected1);
+            softAssertions.assertThat(moves2).isEqualTo(expected2);
+            softAssertions.assertThat(moves3).isEqualTo(expected3);
+
+        });
     }
 }
