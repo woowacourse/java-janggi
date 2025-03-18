f49cabf7cd8da65e3a549c8b0be88110f1c0752c
MinJun Choi <mj043000@naver.com>
2025-03-18 16:52:45 +0900

    feat: 메뉴에 없는 입력을 했을 경우, 재입력을 받는다.
    Co-authored-by: yesjuhee <juhee0924@g.skku.edu>

---
+++ src/main/java/janggi/SetupOption.java
@@ -0,0 +1,24 @@
+package janggi;
+
+import java.util.Arrays;
+
+public enum SetupOption {
+
+    INNER_SETUP("1"),
+    OUTER_SETUP("2"),
+    RIGHT_SETUP("3"),
+    LEFT_SETUP("4");
+
+    private final String option;
+
+    SetupOption(final String option) {
+        this.option = option;
+    }
+
+    public static SetupOption of(final String input) {
+        return Arrays.stream(SetupOption.values())
+                .filter(setupOption -> setupOption.option.equals(input))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 1~4의 숫자만 입력할 수 있습니다."));
+    }
+}
