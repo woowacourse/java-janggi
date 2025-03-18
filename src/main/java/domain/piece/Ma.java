d34e3267f4a0b2232c6925f9dec11384c488ab33
Han <jumdo12@gmail.com>
2025-03-18 21:42:25 +0900

    feat(Ma): 마 행동 경로에 기물이 있다면 이동할 수 없도록 로직 추가

--- src/main/java/domain/piece/Ma.java
+++ src/main/java/domain/piece/Ma.java
@@ -1,19 +1,31 @@
 package domain.piece;
 
 import domain.JanggiCoordinate;
+import domain.board.JanggiBoard;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Ma implements Piece {
+    private static final List<JanggiCoordinate> OFFSET_ROUTE = List.of(
+            new JanggiCoordinate(1,0),
+            new JanggiCoordinate(0,1),
+            new JanggiCoordinate(-1,0),
+            new JanggiCoordinate(0,-1)
+    );
+
     private static final List<JanggiCoordinate> OFFSET_MOVEMENT = List.of(
-            new JanggiCoordinate(2, 1),
-            new JanggiCoordinate(2, -1),
-            new JanggiCoordinate(-2, 1),
-            new JanggiCoordinate(-2, -1),
-            new JanggiCoordinate(1, 2),
+            new JanggiCoordinate(-1, -2),
             new JanggiCoordinate(1, -2),
-            new JanggiCoordinate(-1, 2),
-            new JanggiCoordinate(-1, -2)
+
+            new JanggiCoordinate(2,-1),
+            new JanggiCoordinate(2,1),
+
+            new JanggiCoordinate(1,2),
+            new JanggiCoordinate(1,-2),
+
+            new JanggiCoordinate(-2,1),
+            new JanggiCoordinate(-2,-1)
     );
 
     private final JanggiCoordinate currCoordinate;
@@ -25,11 +37,58 @@ public class Ma implements Piece {
     }
 
     @Override
-    public List<JanggiCoordinate> availableMovePositions() {
+    public List<JanggiCoordinate> availableMovePositions(JanggiBoard janggiBoard) {
         List<JanggiCoordinate> availablePositions = new ArrayList<>();
-        for (JanggiCoordinate coordinate : OFFSET_MOVEMENT) {
-            availablePositions.add(movePosition(coordinate));
+        if(!janggiBoard.hasPiece(movePosition(OFFSET_ROUTE.get(0)))){
+            JanggiCoordinate next;
+
+            next = movePosition(OFFSET_MOVEMENT.get(0));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
+
+            next = movePosition(OFFSET_MOVEMENT.get(1));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
         }
+        if(!janggiBoard.hasPiece(movePosition(OFFSET_ROUTE.get(1)))){
+            JanggiCoordinate next;
+            next = movePosition(OFFSET_MOVEMENT.get(2));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
+
+            next = movePosition(OFFSET_MOVEMENT.get(3));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
+        }
+        if(!janggiBoard.hasPiece(movePosition(OFFSET_ROUTE.get(2)))){
+            JanggiCoordinate next;
+            next = movePosition(OFFSET_MOVEMENT.get(4));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
+
+            next = movePosition(OFFSET_MOVEMENT.get(5));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
+        }
+        if (!janggiBoard.hasPiece(movePosition(OFFSET_ROUTE.get(3)))) {
+            JanggiCoordinate next;
+            next = movePosition(OFFSET_MOVEMENT.get(6));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
+
+            next = movePosition(OFFSET_MOVEMENT.get(7));
+            if(!janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,next)) {
+                availablePositions.add(next);
+            }
+        }
+
         return availablePositions;
     }
 
