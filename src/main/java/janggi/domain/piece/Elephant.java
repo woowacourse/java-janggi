203482fd73a1001e3027ce66db63f538f9552ff6
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-18 17:09:57 +0900

    feat: Elephant 의 이동 가능 규칙 구현

--- src/main/java/janggi/domain/piece/Elephant.java
+++ src/main/java/janggi/domain/piece/Elephant.java
@@ -1,4 +1,30 @@
 package janggi.domain.piece;
 
-public class Elephant {
+import janggi.domain.Position;
+import janggi.domain.Side;
+
+public class Elephant extends Piece {
+
+    private static final int VERTICAL_BASE_X_MOVEABLE_DISTANCE = 2;
+    private static final int VERTICAL_BASE_Y_MOVEABLE_DISTANCE = 3;
+
+    private static final int HORIZONTAL_BASE_X_MOVEABLE_DISTANCE = 3;
+    private static final int HORIZONTAL_BASE_Y_MOVEABLE_DISTANCE = 2;
+
+    public Elephant(Side side, Position position) {
+        super(side, position);
+    }
+
+    @Override
+    protected boolean isMoveable(int x, int y) {
+        Position position = getPosition();
+
+        if (Math.abs(position.getX() - x) == VERTICAL_BASE_X_MOVEABLE_DISTANCE) {
+            return Math.abs(position.getY() - y) == VERTICAL_BASE_Y_MOVEABLE_DISTANCE;
+        }
+        if (Math.abs(position.getX() - x) == HORIZONTAL_BASE_X_MOVEABLE_DISTANCE) {
+            return Math.abs(position.getY() - y) == HORIZONTAL_BASE_Y_MOVEABLE_DISTANCE;
+        }
+        return false;
+    }
 }
---
+++ src/test/java/janggi/domain/piece/ElephantTest.java
@@ -0,0 +1,68 @@
+package janggi.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import janggi.domain.Position;
+import janggi.domain.Side;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ElephantTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 3, 4", "1, 2, -1, 4", "1, 2, 3, 0", "1, 2, -1, 0"})
+    void 이동하고자_하는_x와의_차이가_2인_경우_이동하고자_하는_y와의_차이가_3이_아니라면_움직일_수_없다(
+            int x,
+            int y,
+            int moveX,
+            int moveY
+    ) {
+
+        Elephant elephant = new Elephant(Side.CHO, new Position(x, y));
+
+        assertThat(elephant.isMoveable(moveX, moveY)).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 3, 5", "1, 2, -1, 5", "1, 2, 3, -1", "1, 2, -1, -1"})
+    void 이동하고자_하는_x와의_차이가_2인_경우_이동하고자_하는_y와의_차이가_3이라면_움직일_수_있다(
+            int x,
+            int y,
+            int moveX,
+            int moveY
+    ) {
+
+        Elephant elephant = new Elephant(Side.CHO, new Position(x, y));
+
+        assertThat(elephant.isMoveable(moveX, moveY)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 4, 3", "1, 2, -2, 3", "1, 2, 4, 1", "1, 2, -2, 1"})
+    void 이동하고자_하는_x와의_차이가_3인_경우_이동하고자_하는_y와의_차이가_2가_아니라면_움직일_수_없다(
+            int x,
+            int y,
+            int moveX,
+            int moveY
+    ) {
+
+        Elephant elephant = new Elephant(Side.CHO, new Position(x, y));
+
+        assertThat(elephant.isMoveable(moveX, moveY)).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 4, 4", "1, 2, -2, 4", "1, 2, 4, 0", "1, 2, -2, 0"})
+    void 이동하고자_하는_x와의_차이가_3인_경우_이동하고자_하는_y와의_차이가_2라면_움직일_수_있다(
+            int x,
+            int y,
+            int moveX,
+            int moveY
+    ) {
+
+        Elephant elephant = new Elephant(Side.CHO, new Position(x, y));
+
+        assertThat(elephant.isMoveable(moveX, moveY)).isTrue();
+    }
+}
