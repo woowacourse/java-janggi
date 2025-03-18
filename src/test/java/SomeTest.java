461c43d6df48e5b92cf0b5e4d55cc08d64038b50
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 16:26:18 +0900

    RED) test(model) : 장 이동 가능 여부 판별 테스트

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -1,16 +1,26 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import model.Jang;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SomeTest {
 
 
     @Test
-    public void UnitTest() {
-
+    @DisplayName("장 기물 생성 테스트")
+    public void test1() {
         String team = "red";
+
         Jang jang  = new Jang(team);
+
         assertThat(jang.getTeam()).isEqualTo(team);
+    }
+
+    @Test
+    @DisplayName("장 이동 가능 여부 판별 테스트")
+    public void test2() {
+        Jang jang  = new Jang("red");
+        assertThat(jang.canMove(0,0, 1, 0)).isTrue();
     }
 }
