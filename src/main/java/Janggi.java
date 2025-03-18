3caf96d45acbae53b8d627fed2caf2d5aae81d3f
junho <wnsgh732@gmail.com>
2025-03-18 17:13:36 +0900

    feat: 보드를 초기화하면 청차가 01, 09, 홍차가 11, 19에 배치된다

--- src/main/java/Janggi.java
+++ src/main/java/Janggi.java
@@ -7,10 +7,18 @@ public class Janggi {
 
     public void initializeBoard() {
         initializeGeneral();
+        initializeChariot();
     }
 
     private void initializeGeneral() {
-        board.putPiece(new Position(9, 5), new Piece(PieceType.GENERAL, TeamType.RED));
-        board.putPiece(new Position(2, 5), new Piece(PieceType.GENERAL, TeamType.BLUE));
+        board.putPiece(new Position(9, 5), new Piece(PieceType.GENERAL, TeamType.BLUE));
+        board.putPiece(new Position(2, 5), new Piece(PieceType.GENERAL, TeamType.RED));
+    }
+
+    private void initializeChariot() {
+        board.putPiece(new Position(0, 1), new Piece(PieceType.CHARIOT, TeamType.BLUE));
+        board.putPiece(new Position(0, 9), new Piece(PieceType.CHARIOT, TeamType.BLUE));
+        board.putPiece(new Position(1, 1), new Piece(PieceType.CHARIOT, TeamType.RED));
+        board.putPiece(new Position(1, 9), new Piece(PieceType.CHARIOT, TeamType.RED));
     }
 }
--- src/test/java/JanggiTest.java
+++ src/test/java/JanggiTest.java
@@ -12,7 +12,19 @@ class JanggiTest {
         Board board = new Board(new HashMap<>());
         Janggi janggi = new Janggi(board);
         janggi.initializeBoard();
-        assertThat(board.getPiece(new Position(9, 5))).isEqualTo(new Piece(PieceType.GENERAL, TeamType.RED));
-        assertThat(board.getPiece(new Position(2, 5))).isEqualTo(new Piece(PieceType.GENERAL, TeamType.BLUE));
+        assertThat(board.getPiece(new Position(9, 5))).isEqualTo(new Piece(PieceType.GENERAL, TeamType.BLUE));
+        assertThat(board.getPiece(new Position(2, 5))).isEqualTo(new Piece(PieceType.GENERAL, TeamType.RED));
+    }
+
+    @DisplayName("보드를 초기화하면 청차가 01, 09, 홍차가 11, 19에 배치된다.")
+    @Test
+    void initializeChariot() {
+        Board board = new Board(new HashMap<>());
+        Janggi janggi = new Janggi(board);
+        janggi.initializeBoard();
+        assertThat(board.getPiece(new Position(0, 1))).isEqualTo(new Piece(PieceType.CHARIOT, TeamType.BLUE));
+        assertThat(board.getPiece(new Position(0, 9))).isEqualTo(new Piece(PieceType.CHARIOT, TeamType.BLUE));
+        assertThat(board.getPiece(new Position(1, 1))).isEqualTo(new Piece(PieceType.CHARIOT, TeamType.RED));
+        assertThat(board.getPiece(new Position(1, 9))).isEqualTo(new Piece(PieceType.CHARIOT, TeamType.RED));
     }
 }
