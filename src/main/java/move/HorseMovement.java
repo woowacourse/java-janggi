ea5afe7a75947b6f99993f8b4e91a2153ee2040e
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-18 17:08:42 +0900

    feat: 말 이동 기능 구현

--- src/main/java/move/HorseMovement.java
+++ src/main/java/move/HorseMovement.java
@@ -1,8 +1,21 @@
 package move;
 
-public class HorseMovement implements MoveStrategy{
+public class HorseMovement implements MoveStrategy {
+
+    private static final int[][] MOVEMENT_RANGE_CASES = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2},
+            {1, 2}, {2, 1}, {2, -1}, {1, -2}};
+
     @Override
     public Position move(Position from, Position to) {
-        return to;
+        for (int i = 0; i < 8; i++) {
+            int nx = from.x() + MOVEMENT_RANGE_CASES[i][0];
+            int ny = from.y() + MOVEMENT_RANGE_CASES[i][1];
+
+            if (to.equals(new Position(nx, ny))) {
+                return to;
+            }
+        }
+
+        throw new IllegalArgumentException();
     }
 }
---
+++ src/main/java/piece/Horse.java
@@ -0,0 +1,11 @@
+package piece;
+
+import move.MoveStrategy;
+import move.Position;
+
+public class Horse extends Piece {
+
+    public Horse(MoveStrategy moveStrategy, Position position) {
+        super(moveStrategy, position);
+    }
+}
