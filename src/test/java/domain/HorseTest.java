670da6472877dddeb302d84a7d255ecf9295b657
ryan-dia <cjfdnjs158@naver.com>
2025-03-18 20:53:06 +0900

    test: Horse 이동 거리 확인 실패 테스트 작성
    Co-authored-by:saera-yook<yook.saera@gmail.com>

---
+++ src/test/java/domain/HorseTest.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class HorseTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,2,true",
+            "-1,2,true",
+            "1,-2,true",
+            "-1,-2,true",
+            "2,1,true",
+            "-2,1,true",
+            "2,-1,true",
+            "-2,-1,true",
+            "0,-1,false",
+            "0,0,false",
+            "8,9,false",
+    })
+    void 말이_움직일_수_있으면_true_아니면_false를_반환한다(final int x, final int y, final boolean expected) {
+
+        // given
+        Horse horse = PieceFactory.createRedTeam(Horse::new);
+
+        // when
+        Distance distance = new Distance(x, y);
+
+        // then
+        assertThat(horse.isMovable(distance)).isEqualTo(expected);
+    }
+}
