5986a13b9bca88f477cbb00c2465073a631ae253
dltmddus <tmddus4671@gmail.com>
2025-03-18 15:36:22 +0900

    feat: 궁 기물 도메인 추가

--- src/main/java/domain/궁.java
+++ src/main/java/domain/궁.java
@@ -4,4 +4,12 @@ public class 궁 extends Piece {
 
     public 궁() {
     }
+
+    public boolean canMove(int beforeRow, int beforeColumn, int afterRow, int afterColumn) {
+        if (beforeRow == afterRow && Math.abs(beforeColumn - afterColumn) == 1) {
+            return true;
+        }
+        return beforeColumn == afterColumn && (Math.abs(beforeRow - afterRow) == 1
+                || Math.abs(beforeRow - afterRow) == 9);
+    }
 }
--- src/test/java/domain/궁Test.java
+++ src/test/java/domain/궁Test.java
@@ -1,19 +1,36 @@
-//package domain;
-//
-//import org.junit.jupiter.api.Test;
-//
-//public class 궁Test {
-//    @Test
-//    void 상_하_좌_우로_한_칸_움직일_수_있다() {
-//        // given
-//        궁 piece = new 궁("d","5");
-//        String afterRow = "d";
-//        String afterColumn = "6";
-//
-//        // when
-//        piece.move(afterColumn, afterRow);
-//
-//        // then
-//        piece.
-//    }
-//}
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class 궁Test {
+
+    @ParameterizedTest
+    @CsvSource({"8, 5", "9, 4", "9, 6", "0, 5"})
+    void 상_하_좌_우로_한_칸_이동할_수_있다(int afterRow, int afterColumn) {
+        // given
+        궁 piece = new 궁();
+        int beforeRow = 9;
+        int beforeColumn = 5;
+
+        // when & then
+        Assertions.assertThat(piece.canMove(beforeRow, beforeColumn, afterRow, afterColumn))
+                .isTrue();
+    }
+
+    @Test
+    void 상_하_좌_우로_한_칸이_아닌_경우_이동할_수_없다() {
+        // given
+        궁 piece = new 궁();
+        int beforeRow = 9;
+        int beforeColumn = 5;
+        int afterRow = 8;
+        int afterColumn = 4;
+
+        // when & then
+        Assertions.assertThat(piece.canMove(beforeRow, beforeColumn, afterRow, afterColumn))
+                .isFalse();
+    }
+}
