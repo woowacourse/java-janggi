f82c967a4f436d2230ea92fc86c88a5076b0a226
rladmstn <oz33177351@gmail.com>
2025-03-18 19:34:12 +0900

    test: 기물 객체 생성 테스트 작성

---
+++ src/test/java/domain/piece/PieceTest.java
@@ -0,0 +1,26 @@
+package domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class PieceTest {
+
+    @Test
+    void 기물_객체를_생성할_수_있다() {
+        // given
+        final Position position = new Position(1, 2);
+
+        // when
+        final TestPiece piece = new TestPiece(1, 2);
+
+        // then
+        assertThat(piece.getPosition()).isEqualTo(position);
+    }
+
+    static class TestPiece extends Piece {
+        public TestPiece(int row, int column) {
+            super(row, column);
+        }
+    }
+}
