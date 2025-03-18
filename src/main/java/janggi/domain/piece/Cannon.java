67800efd88708e37f9376258c7b9108f79116c2d
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-18 19:47:17 +0900

    refactor: 잘못 부여된 Cannon 의 역할을 Rook 으로 변경

--- src/main/java/janggi/domain/piece/Cannon.java
+++ src/main/java/janggi/domain/piece/Cannon.java
@@ -1,19 +1,4 @@
 package janggi.domain.piece;
 
-import janggi.domain.Position;
-import janggi.domain.Side;
-
-public class Cannon extends Piece {
-
-    public Cannon(Side side, Position position) {
-        super(side, position);
-    }
-
-    @Override
-    protected boolean isMoveable(int x, int y) {
-        if (getPosition().hasSameX(x)) {
-            return !getPosition().hasSameY(y);
-        }
-        return getPosition().hasSameY(y);
-    }
+public class Cannon {
 }
--- src/main/java/janggi/domain/piece/Rook.java
+++ src/main/java/janggi/domain/piece/Rook.java
@@ -1,4 +1,19 @@
 package janggi.domain.piece;
 
-public class Rook {
+import janggi.domain.Position;
+import janggi.domain.Side;
+
+public class Rook extends Piece {
+
+    public Rook(Side side, Position position) {
+        super(side, position);
+    }
+
+    @Override
+    protected boolean isMoveable(int x, int y) {
+        if (getPosition().hasSameX(x)) {
+            return !getPosition().hasSameY(y);
+        }
+        return getPosition().hasSameY(y);
+    }
 }
