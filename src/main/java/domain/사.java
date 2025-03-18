95421aab0275469ea96187dfde4b8b65d0be933b
dltmddus <tmddus4671@gmail.com>
2025-03-18 15:53:25 +0900

    feat: 사 기물 도메인 추가

--- src/main/java/domain/사.java
+++ src/main/java/domain/사.java
@@ -1,4 +1,14 @@
 package domain;
 
 public class 사 extends Piece {
+    public 사() {
+    }
+
+    public boolean canMove(int beforeRow, int beforeColumn, int afterRow, int afterColumn) {
+        if (beforeRow == afterRow && Math.abs(beforeColumn - afterColumn) == 1) {
+            return true;
+        }
+        return beforeColumn == afterColumn && (Math.abs(beforeRow - afterRow) == 1
+                || Math.abs(beforeRow - afterRow) == 9);
+    }
 }
---
+++ src/test/java/domain/사Test.java
@@ -0,0 +1,35 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class 사Test {
+    @ParameterizedTest
+    @CsvSource({"8, 5", "9, 4", "9, 6", "0, 5"})
+    void 상_하_좌_우로_한_칸_이동할_수_있다(int afterRow, int afterColumn) {
+        // given
+        사 piece = new 사();
+        int beforeRow = 9;
+        int beforeColumn = 5;
+
+        // when & then
+        Assertions.assertThat(piece.canMove(beforeRow, beforeColumn, afterRow, afterColumn))
+                .isTrue();
+    }
+
+    @Test
+    void 상_하_좌_우로_한_칸이_아닌_경우_이동할_수_없다() {
+        // given
+        사 piece = new 사();
+        int beforeRow = 9;
+        int beforeColumn = 5;
+        int afterRow = 8;
+        int afterColumn = 4;
+
+        // when & then
+        Assertions.assertThat(piece.canMove(beforeRow, beforeColumn, afterRow, afterColumn))
+                .isFalse();
+    }
+}
