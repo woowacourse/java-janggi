19eed8a9aa924d3e68bc9c44e5a505f95599da53
rladmstn <oz33177351@gmail.com>
2025-03-18 17:24:43 +0900

    feat: 좌표 생성 구현

---
+++ src/main/java/domain/piece/Position.java
@@ -0,0 +1,12 @@
+package domain.piece;
+
+public class Position {
+
+    private final int x;
+    private final int y;
+
+    public Position(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
