c636cfe38a1c3fc68f11aea9091773e4d38c7354
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-18 17:12:40 +0900

    refactor: Canon 테스트 방식 변경
    - 이동 가능, 불가능 테스트를 분기.

--- src/test/java/janggi/domain/piece/CanonTest.java
+++ src/test/java/janggi/domain/piece/CanonTest.java
@@ -1,23 +1,29 @@
 package janggi.domain.piece;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import janggi.domain.Position;
 import janggi.domain.ReplaceUnderBar;
 import janggi.domain.Side;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-
 @ReplaceUnderBar
 public class CanonTest {
 
     @ParameterizedTest
     @CsvSource(value = {"1, 2, 3, 4", "1, 2, 4, 5"})
-    void x_y좌표가_모두_다른_위치로_움직일_수_없다(int x, int y, int moveX, int moveY) {
+    void 이동하고자_하는_x_y좌표가_현재_x_y좌표와_모두_다르면_움직일_수_없다(int x, int y, int moveX, int moveY) {
         Cannon cannon = new Cannon(Side.CHO, new Position(x, y));
 
-        assertThatIllegalArgumentException()
-            .isThrownBy(() -> cannon.move(moveX, moveY))
-            .withMessage("해당 위치로 이동할 수 없습니다.");
+        assertThat(cannon.isMoveable(moveX, moveY)).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 3, 2", "1, 2, 1, 4"})
+    void 이동하고자_하는_x_y좌표가_현재_x_y좌표와_하나만_다르면_움직일_수_있다(int x, int y, int moveX, int moveY) {
+        Cannon cannon = new Cannon(Side.CHO, new Position(x, y));
+
+        assertThat(cannon.isMoveable(moveX, moveY)).isTrue();
     }
 }
