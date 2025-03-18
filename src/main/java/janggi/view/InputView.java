3edca78ac40fe03c1286866039f3b8dd769f1325
MinJun Choi <mj043000@naver.com>
2025-03-18 21:12:45 +0900

    chore: 기물표기법 안내문구 추가
    Co-authored-by: yesjuhee <juhee0924@g.skku.edu>

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -7,6 +7,15 @@ public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
     public String readSetupOption() {
+        System.out.println("=== 기물표기법 ===\n"
+                + "  한 초\n"
+                + "궁 k K\n"
+                + "차 c C\n"
+                + "포 p P\n"
+                + "마 h H\n"
+                + "상 e E\n"
+                + "사 g G\n"
+                + "병 s S\n");
         System.out.println("상차림을 입력하세요.\n"
                 + "1. 안상\n"
                 + "2. 바깥상\n"
