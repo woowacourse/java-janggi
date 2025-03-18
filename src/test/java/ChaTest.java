3828320a180971b5630df41be7c4d67bf94ea9c3
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 20:59:32 +0900

    RED) test(model): 차 이동 경로 테스트

--- src/test/java/ChaTest.java
+++ src/test/java/ChaTest.java
@@ -1,6 +1,9 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import model.Cha;
+import model.Path;
+import model.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -31,6 +34,52 @@ public class ChaTest {
         public void test3() {
             Cha cha = new Cha("red");
             assertThat(cha.canMove(0, 0, 10, 10)).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("차 이동 경로 계산 테스트")
+    class ChaCalculatePathTest {
+        @Test
+        @DisplayName("수직 테스트")
+        public void test1() {
+            Cha cha = new Cha("red");
+            Point point1 = new Point(0, 1);
+            Point point2 = new Point(0, 2);
+            Point point3 = new Point(0, 3);
+            Point point4 = new Point(0, 4);
+            Point point5 = new Point(0, 7);
+
+            Path path = cha.calculatePath(0, 0, 0, 7);
+
+            assertAll(
+                    () -> assertThat(path.contains(point1)).isTrue(),
+                    () -> assertThat(path.contains(point2)).isTrue(),
+                    () -> assertThat(path.contains(point3)).isTrue(),
+                    () -> assertThat(path.contains(point4)).isTrue(),
+                    () -> assertThat(path.contains(point5)).isTrue()
+            );
+        }
+
+        @Test
+        @DisplayName("수평 테스트")
+        public void test2() {
+            Cha cha = new Cha("red");
+            Point point1 = new Point(1, 0);
+            Point point2 = new Point(2, 0);
+            Point point3 = new Point(3, 0);
+            Point point4 = new Point(6, 0);
+            Point point5 = new Point(7, 0);
+
+            Path path = cha.calculatePath(0, 0, 7, 0);
+
+            assertAll(
+                    () -> assertThat(path.contains(point1)).isTrue(),
+                    () -> assertThat(path.contains(point2)).isTrue(),
+                    () -> assertThat(path.contains(point3)).isTrue(),
+                    () -> assertThat(path.contains(point4)).isTrue(),
+                    () -> assertThat(path.contains(point5)).isTrue()
+            );
         }
     }
 }
