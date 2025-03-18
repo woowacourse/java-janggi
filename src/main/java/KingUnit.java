a6c04536313bcd2d59848e2ddea8c41c56818c42
ppparkta <pppppark2@gmail.com>
2025-03-18 17:54:22 +0900

    feat: 궁, 사 기물을 구현한다

---
+++ src/main/java/KingUnit.java
@@ -0,0 +1,13 @@
+public class KingUnit implements Unit {
+
+    private final Point point;
+
+    public KingUnit(Point point) {
+        this.point = point;
+    }
+
+    @Override
+    public Unit move(Point point) {
+        return null;
+    }
+}
---
+++ src/main/java/ScholarUnit.java
@@ -0,0 +1,13 @@
+public class ScholarUnit implements Unit {
+
+    private final Point point;
+
+    public ScholarUnit(Point point) {
+        this.point = point;
+    }
+
+    @Override
+    public Unit move(Point point) {
+        return null;
+    }
+}
