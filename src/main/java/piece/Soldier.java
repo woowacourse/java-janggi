dc090a72dc8f884d105f204b61671dfee7616432
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-18 17:51:55 +0900

    feat: 졸/병 이동 기능 구현

---
+++ src/main/java/piece/Soldier.java
@@ -0,0 +1,10 @@
+package piece;
+
+import move.MoveStrategy;
+import move.Position;
+
+public class Soldier extends Piece {
+    public Soldier(MoveStrategy moveStrategy, Position position) {
+        super(moveStrategy, position);
+    }
+}
