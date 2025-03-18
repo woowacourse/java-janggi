2c10ee043030d77ca4652b03fda1bf7268b701d1
taek2222 <oot309@daum.net>
2025-03-18 17:43:27 +0900

    refactor: 잘못된 좌표 입력 검증 개선

--- src/main/java/domain/piece/Position.java
+++ src/main/java/domain/piece/Position.java
@@ -2,14 +2,22 @@ package domain.piece;
 
 public class Position {
 
+    public static final int MIN_ROW = 1;
+    public static final int MIN_COLUMN = 1;
+    public static final int MAX_ROW = 9;
+    public static final int MAX_COLUMN = 10;
+
     private final int row;
     private final int column;
 
     public Position(final int row, final int column) {
+        validateRange(row, column);
         this.row = row;
         this.column = column;
+    }
 
-        if (row < 1 || column < 1 || row > 10 || column > 9) {
+    private void validateRange(final int row, final int column) {
+        if (row < MIN_ROW || column < MIN_COLUMN || row > MAX_ROW || column > MAX_COLUMN) {
             throw new IllegalArgumentException("[ERROR] 좌표 입력은 9X10 보드 이내만 가능합니다.");
         }
     }
