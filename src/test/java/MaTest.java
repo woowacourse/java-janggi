49fa95f76f01b728129618be39a2016dcc9ecd3d
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 19:51:17 +0900

    RED) test(model): 마 이동 경로 테스트
    - 중간 경유지 포함 여부
    - 종점 포함 여부

--- src/test/java/MaTest.java
+++ src/test/java/MaTest.java
@@ -1,6 +1,8 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import model.Jang;
 import model.Ma;
+import model.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -31,6 +33,28 @@ public class MaTest {
         public void test2() {
             Ma ma = new Ma("red");
             assertThat(ma.canMove(0, 0, 2, 2)).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("마 이동 경로 계산 테스트")
+    class MaCalculatePathTest {
+        @Test
+        @DisplayName("중간 경유지 포함 여부 테스트")
+        public void test1() {
+            Ma ma = new Ma("red");
+            Point point =new Point(0,1);    // 중간 경유지
+
+            assertThat(ma.calculatePath(0,0,1,2).contains(point)).isTrue();
+        }
+
+        @Test
+        @DisplayName("종점 포함 여부 테스트")
+        public void test2() {
+            Ma ma = new Ma("red");
+            Point point =new Point(1,2);
+
+            assertThat(ma.calculatePath(0,0,1,2).contains(point)).isTrue();
         }
     }
 
