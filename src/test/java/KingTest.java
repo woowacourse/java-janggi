4eda82d6b6ac0442af53258f71b383ee51742452
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-18 17:11:36 +0900

    feat: 킹 이동 경로 계산

--- src/test/java/KingTest.java
+++ src/test/java/KingTest.java
@@ -1,4 +1,26 @@
+import domain.JanggiBoard;
+import domain.Team;
+import domain.piece.King;
+import domain.piece.Move;
+import domain.piece.Piece;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class KingTest {
 
+    @DisplayName("킹은 오른쪽으로 이동할 수 있다.")
+    @Test
+    void test() {
 
+        //given
+        King king = new King(Team.RED);
+
+        //when
+        List<Move> move = king.calculatePath(4,1,4,2);
+
+        // then
+        Assertions.assertThat(move).isEqualTo(List.of(Move.RIGHT));
+    }
 }
