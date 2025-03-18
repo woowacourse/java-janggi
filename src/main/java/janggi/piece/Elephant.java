92b347da077c595bac0814a1d5d3918a358cd802
jhan0121 <jhan0121@gmail.com>
2025-03-18 20:23:16 +0900

    feat: Horse(마), Elephant(상) 추가

--- src/main/java/janggi/piece/Elephant.java
+++ src/main/java/janggi/piece/Elephant.java
@@ -13,7 +13,21 @@ public class Elephant extends Piece {
 
     @Override
     public Piece move(final Board board, final Position destination) {
+        if ((board.isExists(destination) && board.isAlly(destination, this.team)) || isInvalidMove(destination)) {
+            throw new IllegalArgumentException("이동할 수 없는 지점입니다.");
+        }
         return new Elephant(destination, team);
+    }
+
+    private boolean isInvalidMove(Position destination) {
+        int diffRow = destination.row() - position.row();
+        int diffColumn = destination.column() - position.column();
+
+        if ((Math.abs(diffRow) != 2 || Math.abs(diffColumn) != 3)
+                && (Math.abs(diffRow) != 3 || Math.abs(diffColumn) != 3)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
--- src/main/java/janggi/piece/Horse.java
+++ src/main/java/janggi/piece/Horse.java
@@ -13,7 +13,21 @@ public class Horse extends Piece {
 
     @Override
     public Piece move(final Board board, final Position destination) {
+        if ((board.isExists(destination) && board.isAlly(destination, this.team)) || isInvalidMove(destination)) {
+            throw new IllegalArgumentException("이동할 수 없는 지점입니다.");
+        }
         return new Horse(destination, team);
+    }
+
+    private boolean isInvalidMove(Position destination) {
+        int diffRow = destination.row() - position.row();
+        int diffColumn = destination.column() - position.column();
+
+        if ((Math.abs(diffRow) != 2 || Math.abs(diffColumn) != 1)
+                && (Math.abs(diffRow) != 1 || Math.abs(diffColumn) != 2)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
