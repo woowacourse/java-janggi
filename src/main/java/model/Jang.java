69a4bf70279157d486a7c794b3cb494fcb2f3faa
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 16:14:57 +0900

    Green) feat: 장 기물 객체 생성

---
+++ src/main/java/model/Jang.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Jang {
+    private String team;
+
+    public Jang(String team) {
+        this.team = team;
+    }
+
+    public String getTeam(){
+        return team;
+    }
+}
