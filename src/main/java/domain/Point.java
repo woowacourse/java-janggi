489f3ec6cca23aaff50beaeaf16ee7bdd105f71b
ryan-dia <cjfdnjs158@naver.com>
2025-03-18 19:26:46 +0900

    feat: Point 생성 기능 구현
    Co-authored-by:saera-yook<yook.saera@gmail.com>

---
+++ src/main/java/domain/Point.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class Point {
+    private static final int MAX_X = 8;
+    private static final int MAX_Y = 9;
+    private final int x;
+    private final int y;
+
+    private Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point of(final String x, final String y) {
+        final int parsedX = parseInt(x);
+        final int parsedY = parseInt(y);
+        validateRange(parsedX, MAX_X);
+        validateRange(parsedY, MAX_Y);
+        return new Point(parsedX, parsedY);
+    }
+
+    private static void validateRange(final int point, final int maxPoint) {
+        if (point < 0 || point > maxPoint) {
+            throw new IllegalArgumentException("X축은 0부터 8까지, Y축은 0부터 9까지 입력 가능합니다.");
+        }
+    }
+
+    private static int parseInt(final String x) {
+        try {
+            return Integer.parseInt(x);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+    }
+}
