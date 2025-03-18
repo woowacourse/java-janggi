f3af070a6b2a3b0a00e7b5b301ed2e7f0021c471
ppparkta <pppppark2@gmail.com>
2025-03-18 17:49:17 +0900

    test: 수직, 수평 테스트

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -20,4 +20,42 @@ class PointTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("");
     }
+
+    @ValueSource(strings = {"0,2", "2,0", "2,9", "8,2"})
+    @ParameterizedTest
+    @DisplayName("수평 혹은 수직 직선에 점이 위치해 있으면 참을 반환한다")
+    void test2(String str) {
+        // given
+        String[] split = str.split(",");
+        int x = Integer.parseInt(split[0]);
+        int y = Integer.parseInt(split[1]);
+        Point point = new Point(x, y);
+
+        Point opposite = new Point(2, 2);
+
+        // when
+        boolean b = point.isHorizontal(opposite);
+
+        // then
+        Assertions.assertThat(b).isTrue();
+    }
+
+    @ValueSource(strings = {"3,3", "1,1", "3,1", "1,3"})
+    @ParameterizedTest
+    @DisplayName("수평 혹은 수직 직선에 점이 위치해있지 않으면 거짓을 반환한다")
+    void test3(String str) {
+        // given
+        String[] split = str.split(",");
+        int x = Integer.parseInt(split[0]);
+        int y = Integer.parseInt(split[1]);
+        Point point = new Point(x, y);
+
+        Point opposite = new Point(2, 2);
+
+        // when
+        boolean b = point.isHorizontal(opposite);
+
+        // then
+        Assertions.assertThat(b).isFalse();
+    }
 }
