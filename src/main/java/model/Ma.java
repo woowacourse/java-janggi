76011d9cbd187dc084586d4a2e2448eaff4a0ece
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 20:08:12 +0900

    GREEN) feat(model) : 마 경로 계산 로직 구현

--- src/main/java/model/Ma.java
+++ src/main/java/model/Ma.java
@@ -17,4 +17,26 @@ public class Ma extends Piece {
         return IntStream.range(0, horizontal.size())
                 .anyMatch(i -> horizontal.get(i) + beforeX == afterX && vertical.get(i) + beforeY == afterY);
     }
+
+    @Override
+    public Path calculatePath(int beforeX, int beforeY, int afterX, int afterY) {
+        // 1. 이동 벡터를 구한다
+        // 2. 이동 단위 벡터를 구한다.
+        // 3. 단위 벡터 역벡터를 도착점에 더한다.
+        // 3.이랑 도착점을 Path에 담아서 리턴
+
+        int vectorX = afterX - beforeX;
+        int vectorY = afterY - beforeY;
+
+        int unitVectorX = vectorX / Math.abs(vectorX);
+        int unitVectorY = vectorY / Math.abs(vectorY);
+
+        Point middlePoint = new Point(afterX - unitVectorX, afterY - unitVectorY);
+        Point endPoint = new Point(afterX, afterY);
+
+        Path path = new Path();
+        path.addPoint(middlePoint);
+        path.addPoint(endPoint);
+        return path;
+    }
 }
