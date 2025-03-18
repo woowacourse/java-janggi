c49cc7ca6e4a1fd82fc6eee010a121dda8132858
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 20:59:46 +0900

    GREEN) feat(model) : 차 경로 계산 로직 구현

--- src/main/java/model/Cha.java
+++ src/main/java/model/Cha.java
@@ -8,6 +8,29 @@ public class Cha extends Piece {
 
     @Override
     public boolean canMove(int beforeX, int beforeY, int afterX, int afterY) {
-        return !((beforeX == afterX && beforeY == afterY) || (beforeX!=afterX && beforeY != afterY));
+        return !((beforeX == afterX && beforeY == afterY) || (beforeX != afterX && beforeY != afterY));
+    }
+
+    @Override
+    public Path calculatePath(int beforeX, int beforeY, int afterX, int afterY) {
+        int vectorX = afterX - beforeX;
+        int vectorY = afterY - beforeY;
+
+        Path path = new Path();
+
+        if (vectorX == 0) {
+            int unitVectorY = vectorY / Math.abs(vectorY);
+            for (int i = 0; i < Math.abs(vectorY); i++) {
+                path.addPoint(new Point(afterX, afterY - unitVectorY * i));
+            }
+        }
+
+        if (vectorY == 0) {
+            int unitVectorX = vectorX / Math.abs(vectorX);
+            for (int i = 0; i < Math.abs(vectorX); i++) {
+                path.addPoint(new Point(afterX - unitVectorX * i, afterY));
+            }
+        }
+        return path;
     }
 }
