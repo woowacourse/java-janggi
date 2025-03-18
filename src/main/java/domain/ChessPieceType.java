873e99d0f9c2705e6172a388fc51946909ab5cbd
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-18 18:41:54 +0900

    feat: 기물 타입 재정의

--- src/main/java/domain/ChessPieceType.java
+++ src/main/java/domain/ChessPieceType.java
@@ -1,23 +1,52 @@
 package domain;
 
+import java.util.List;
+
 public enum ChessPieceType {
 
-    /*
-    - 차 (Cha) → Chariot
-- 마 (Ma) → Horse
-- 상 (Sang) → Elephant
-- 포 (Po) → Cannon
-- 사 (Sa) → Guard/Advisor
-- 졸/병 (Jol/Byeong) → Soldier/Pawn
-- 왕/장 (Wang/Jang) → King/General
-     */
+    CHARIOT(
+            List.of(new ChessPosition(0, 0), new ChessPosition(0, 8)),
+            List.of(new ChessPosition(9, 0), new ChessPosition(9, 8))
+    ),
+    HORSE(
+            List.of(new ChessPosition(0, 1), new ChessPosition(0, 7)),
+            List.of(new ChessPosition(9, 1), new ChessPosition(9, 7))
+    ),
+    ELEPHANT(
+            List.of(new ChessPosition(0, 2), new ChessPosition(0, 6)),
+            List.of(new ChessPosition(9, 2), new ChessPosition(9, 6))
+    ),
+    CANNON(
+            List.of(new ChessPosition(2, 1), new ChessPosition(2, 7)),
+            List.of(new ChessPosition(7, 1), new ChessPosition(7, 7))
+    ),
+    GUARD(
+            List.of(new ChessPosition(0, 3), new ChessPosition(0, 5)),
+            List.of(new ChessPosition(9, 3), new ChessPosition(9, 5))
+    ),
+    PAWN(
+            List.of(new ChessPosition(3, 0), new ChessPosition(3, 2), new ChessPosition(3, 4), new ChessPosition(3, 6), new ChessPosition(3, 8)),
+            List.of(new ChessPosition(6, 0), new ChessPosition(6, 2), new ChessPosition(6, 4), new ChessPosition(6, 6), new ChessPosition(6, 8))
+    ),
+    KING(
+            List.of(new ChessPosition(1, 4)),
+            List.of(new ChessPosition(8, 4))
+    ),
+    NONE(List.of(), List.of());
 
-    CHARIOT,
-    HORSE,
-    ELEPHANT,
-    CANNON,
-    GUARD,
-    PAWN,
-    KING,
-    NONE;
+    private final List<ChessPosition> redPosition;
+    private final List<ChessPosition> bluePosition;
+
+    ChessPieceType(final List<ChessPosition> redPosition, final List<ChessPosition> bluePosition) {
+        this.redPosition = redPosition;
+        this.bluePosition = bluePosition;
+    }
+
+    public List<ChessPosition> getRedPosition() {
+        return redPosition;
+    }
+
+    public List<ChessPosition> getBluePosition() {
+        return bluePosition;
+    }
 }
