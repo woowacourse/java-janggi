353ca50e667f0b1c57cd009fe1f2183ae46e0144
dltmddus <tmddus4671@gmail.com>
2025-03-18 15:51:42 +0900

    feat: 마 기물 도메인 추가

--- src/main/java/domain/마.java
+++ src/main/java/domain/마.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class 마 extends Piece {
+    public 마() {
+    }
+
+    public boolean canMove(int beforeRow, int beforeColumn, int afterRow, int afterColumn) {
+        return (Math.abs(afterRow - beforeRow) == 2 && Math.abs(afterColumn - beforeColumn) == 1) || (
+                Math.abs(afterRow - beforeRow) == 1 && Math.abs(afterColumn - beforeColumn) == 2);
+    }
 }
