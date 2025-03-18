cec1102a5ef2c388515815d2582203f4947ca750
taek2222 <oot309@daum.net>
2025-03-18 17:37:37 +0900

    feat: 잘못된 입력 검증 추가

--- src/main/java/domain/piece/Position.java
+++ src/main/java/domain/piece/Position.java
@@ -2,11 +2,15 @@ package domain.piece;
 
 public class Position {
 
-    private final int x;
-    private final int y;
+    private final int row;
+    private final int column;
 
-    public Position(final int x, final int y) {
-        this.x = x;
-        this.y = y;
+    public Position(final int row, final int column) {
+        this.row = row;
+        this.column = column;
+
+        if (row < 1 || column < 1 || row > 10 || column > 9) {
+            throw new IllegalArgumentException("[ERROR] 좌표 입력은 9X10 보드 이내만 가능합니다.");
+        }
     }
 }
