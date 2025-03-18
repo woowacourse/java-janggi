dd21da4f171495a9a28cb9711ee0176e12309147
Firedrago95 <yonghwa95@naver.com>
2025-03-18 14:59:45 +0900

    feat: 위치는 가로 세로 2차원 좌표를 가진다

--- src/test/java/janggi/domain/PositionTest.java
+++ src/test/java/janggi/domain/PositionTest.java
@@ -1,5 +1,16 @@
 package janggi.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ReplaceUnderBar
 public class PositionTest {
+
+    @Test
+    void 가로_세로_2차원_좌표를_가진다() {
+        Position position = new Position(1, 2);
+        assertThat(position.getX()).isEqualTo(1);
+        assertThat(position.getY()).isEqualTo(2);
+    }
 }
