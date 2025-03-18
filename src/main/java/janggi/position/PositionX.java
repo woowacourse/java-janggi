7f2308564586cf0667ab6b141a51fd68a77c289f
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-18 17:37:05 +0900

    feat: PositionX, PositionY 생성 및 검증 구현

--- src/main/java/janggi/position/PositionX.java
+++ src/main/java/janggi/position/PositionX.java
@@ -1,6 +1,17 @@
 package janggi.position;
 
 public class PositionX {
+
+    private final int value;
+
     public PositionX(int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value < 1 || value > 9) {
+            throw new IllegalArgumentException("X 위치가 장기판을 벗어났습니다");
+        }
     }
 }
--- src/main/java/janggi/position/PositionY.java
+++ src/main/java/janggi/position/PositionY.java
@@ -1,6 +1,17 @@
 package janggi.position;
 
 public class PositionY {
+
+    private final int value;
+
     public PositionY(int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value < 0 || value > 9) {
+            throw new IllegalArgumentException("Y 위치가 장기판을 벗어났습니다");
+        }
     }
 }
