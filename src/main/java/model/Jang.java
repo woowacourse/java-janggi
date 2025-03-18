0356f0e35d72babdb573e6e7a6b4762ca60f5f4c
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-18 16:26:36 +0900

    GREEN) feat(model) : 장 이동 가능 여부 판별 구현

--- src/main/java/model/Jang.java
+++ src/main/java/model/Jang.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Jang {
     private String team;
 
@@ -9,5 +13,13 @@ public class Jang {
 
     public String getTeam(){
         return team;
+    }
+
+    public boolean canMove(int beforeX, int beforeY, int afterX, int afterY) {
+        List<Integer> horizontal = List.of(0, 0, -1, 1);
+        List<Integer> vertical = List.of(1, -1, 0, 0);
+
+        return IntStream.range(0, horizontal.size())
+                .anyMatch(i -> horizontal.get(i) + beforeX == afterX && vertical.get(i) + beforeY == afterY);
     }
 }
