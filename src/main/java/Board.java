a70ac742f5145d3deef36e3ef6c71e303c31d21a
junho <wnsgh732@gmail.com>
2025-03-18 16:52:57 +0900

    feat: RED팀의 궁을 95에 배치한다

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -4,21 +4,19 @@ import java.util.Map;
 public class Board {
     private final Map<Position, Piece> pieces;
 
-    private Board(Map<Position, Piece> pieces) {
-        this.pieces = pieces;
-    }
-
-    public static Board createEmpty() {
-        Map<Position, Piece> cells = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 9; j++) {
-                cells.put(new Position(i, j), Piece.createEmpty());
-            }
-        }
-        return new Board(cells);
+    public Board(Map<Position, Piece> pieces) {
+        this.pieces = new HashMap<>(pieces);
     }
 
     public Map<Position, Piece> getPieces() {
         return pieces;
+    }
+
+    public void putPiece(Position position, Piece piece) {
+        pieces.put(position, piece);
+    }
+
+    public Piece getPiece(Position position) {
+        return pieces.getOrDefault(position, Piece.createEmpty());
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -2,11 +2,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class BoardTest {
     @DisplayName("보드가 10*9의 Empty Cell을 가진다")
     @Test
     void boardCeil() {
-        Board board = Board.createEmpty();
+        Board board = new Board(new HashMap<>());
         int count = (int) board.getPieces()
                 .entrySet()
                 .stream()
@@ -14,6 +16,14 @@ public class BoardTest {
                         entry.getValue().equals(Piece.createEmpty()))
                 .count();
         Assertions.assertThat(count).isEqualTo(90);
+    }
 
+    @DisplayName("RED팀의 궁을 95에 배치한다")
+    @Test
+    void boardCeilRed() {
+        Board board = new Board(new HashMap<>());
+        Piece piece = new Piece(PieceType.GENERAL, TeamType.RED);
+        board.putPiece(new Position(9, 5), piece);
+        Assertions.assertThat(board.getPiece(new Position(9, 5))).isEqualTo(piece);
     }
 }
