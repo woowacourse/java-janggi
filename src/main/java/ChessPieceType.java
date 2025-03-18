56f7a91126a51d70315749e279893e699a4eb99c
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-18 16:13:48 +0900

    feat: 기물 타입 정의

---
+++ src/main/java/ChessPieceType.java
@@ -0,0 +1,21 @@
+public enum ChessPieceType {
+
+    /*
+    - 차 (Cha) → Chariot
+- 마 (Ma) → Horse
+- 상 (Sang) → Elephant
+- 포 (Po) → Cannon
+- 사 (Sa) → Guard/Advisor
+- 졸/병 (Jol/Byeong) → Soldier/Pawn
+- 왕/장 (Wang/Jang) → King/General
+     */
+
+    CHARIOT,
+    HORSE,
+    ELEPHANT,
+    CANNON,
+    GUARD,
+    PAWN,
+    KING,
+    NONE;
+}
