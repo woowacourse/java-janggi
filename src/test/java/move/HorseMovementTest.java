e0726d0aa5dbf2cf1a57aa447bcd3bbfbd12036e
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-18 16:51:20 +0900

    test: 말 이동 모든 경우의 수 테스트 작성

--- src/test/java/move/HorseMovementTest.java
+++ src/test/java/move/HorseMovementTest.java
@@ -20,4 +20,109 @@ class HorseMovementTest {
         //then
         assertThat(result).isEqualTo(to);
     }
+
+    @Test
+    @DisplayName("위쪽 1칸, 오른쪽 대각선 1칸으로 이동할 수 있다.")
+    void test2() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(3, 0);
+        HorseMovement horseMovement = new HorseMovement();
+
+        //when
+        Position result = horseMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("왼쪽 1칸, 위쪽 대각선 1칸으로 이동할 수 있다.")
+    void test3() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(0, 1);
+        HorseMovement horseMovement = new HorseMovement();
+
+        //when
+        Position result = horseMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("왼쪽 1칸, 아래쪽 대각선 1칸으로 이동할 수 있다.")
+    void test4() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(0, 3);
+        HorseMovement horseMovement = new HorseMovement();
+
+        //when
+        Position result = horseMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("아래쪽 1칸, 왼쪽 대각선 1칸으로 이동할 수 있다.")
+    void test5() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(1, 4);
+        HorseMovement horseMovement = new HorseMovement();
+
+        //when
+        Position result = horseMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("아래쪽 1칸, 오른쪽 대각선 1칸으로 이동할 수 있다.")
+    void test6() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(3, 4);
+        HorseMovement horseMovement = new HorseMovement();
+
+        //when
+        Position result = horseMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("오른쪽 1칸, 위쪽 대각선 1칸으로 이동할 수 있다.")
+    void test7() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(4, 1);
+        HorseMovement horseMovement = new HorseMovement();
+
+        //when
+        Position result = horseMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
+
+    @Test
+    @DisplayName("오른쪽 1칸, 아래쪽 대각선 1칸으로 이동할 수 있다.")
+    void test8() {
+        //given
+        Position from = new Position(2, 2);
+        Position to = new Position(4, 3);
+        HorseMovement horseMovement = new HorseMovement();
+
+        //when
+        Position result = horseMovement.move(from, to);
+
+        //then
+        assertThat(result).isEqualTo(to);
+    }
 }
