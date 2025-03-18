5fc4ee949eced0240e56ad79af356134d38fc9e8
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-18 20:19:04 +0900

    feat: 상의 이동경로를 계산하는 로직 구현

--- src/main/java/domain/piece/Elephant.java
+++ src/main/java/domain/piece/Elephant.java
@@ -5,12 +5,39 @@ import java.util.List;
 
 public class Elephant extends Piece{
 
+    private final List<List<Move>> moves = List.of(
+            List.of(Move.FRONT,Move.FRONT_LEFT, Move.FRONT_LEFT),
+            List.of(Move.FRONT,Move.FRONT_RIGHT, Move.FRONT_RIGHT),
+            List.of(Move.BACK,Move.BACK_LEFT, Move.BACK_LEFT),
+            List.of(Move.BACK,Move.BACK_RIGHT, Move.BACK_RIGHT),
+            List.of(Move.RIGHT,Move.FRONT_RIGHT, Move.FRONT_RIGHT),
+            List.of(Move.RIGHT,Move.BACK_RIGHT, Move.BACK_RIGHT),
+            List.of(Move.LEFT,Move.FRONT_LEFT, Move.FRONT_LEFT),
+            List.of(Move.LEFT,Move.BACK_LEFT, Move.BACK_LEFT)
+    );
+
     public Elephant(Team team) {
         super(team);
     }
 
     @Override
     public List<Move> calculatePath(int startRow, int startColumn, int targetRow, int targetColumn) {
-        return List.of();
+        for (List<Move> moveList : moves) {
+            boolean compareResult = comparePath(startRow, startColumn, targetRow, targetColumn, moveList);
+            if (compareResult) {
+                return moveList;
+            }
+        }
+        throw new IllegalArgumentException("이 위치로 이동할 수 없습니다.");
+    }
+
+    private boolean comparePath(int startRow, int startColumn, int targetRow, int targetColumn, List<Move> moveList) {
+        int newRow = startRow;
+        int newColumn = startColumn;
+        for (Move move : moveList) {
+            newRow = newRow + move.getDy();
+            newColumn = newColumn + move.getDx();
+        }
+        return newRow == targetRow && newColumn == targetColumn;
     }
 }
---
+++ src/test/java/domain/piece/ElephantTest.java
@@ -0,0 +1,34 @@
+package domain.piece;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import domain.Team;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ElephantTest {
+
+    @DisplayName("상의 이동 경로를 계산할 수 있다")
+    @Test
+    void test1() {
+        Elephant elephant = new Elephant(Team.RED);
+
+        List<Move> moves = elephant.calculatePath(4, 4, 2, 7);
+        List<Move> expected = List.of(Move.RIGHT, Move.FRONT_RIGHT, Move.FRONT_RIGHT);
+
+        Assertions.assertThat(moves).isEqualTo(expected);
+    }
+
+    @DisplayName("상으로 갈 수 없는 위치일 경우 예외를 발생시킨다")
+    @Test
+    void test2() {
+        Elephant elephant = new Elephant(Team.RED);
+
+        Assertions.assertThatThrownBy(() -> elephant.calculatePath(4, 4, 4, 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이 위치로 이동할 수 없습니다.");
+    }
+
+}
