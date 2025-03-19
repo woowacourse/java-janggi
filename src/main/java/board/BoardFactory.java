f4fa4f95040e3dde1b01557ab0c9cc9e1e9bd257
changuii <rhljh7410@gmail.com>
2025-03-19 11:53:23 +0900

    feat: BoardFactory에서 Board를 생성

---
+++ src/main/java/board/BoardFactory.java
@@ -0,0 +1,27 @@
+package board;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import piece.Piece;
+import piece.PieceType;
+import piece.TeamType;
+
+public class BoardFactory {
+
+    public Board generateBoard() {
+        final Map<Position, Piece> initMap = new HashMap<>();
+
+        for (final TeamType teamType : TeamType.values()) {
+            for (final PieceType pieceType : PieceType.values()) {
+                final List<Position> initPositions = pieceType.getInitPositions(teamType);
+                for (final Position position : initPositions) {
+                    initMap.put(position, new Piece(pieceType, teamType));
+                }
+            }
+        }
+
+        return new Board(initMap);
+    }
+}
