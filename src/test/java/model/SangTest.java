1fe1846018f062c33235766a60e7f8bd935a7ef2
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-19 21:10:19 +0900

    RED) test(model): 상 경로 방해 테스트

--- src/test/java/model/SangTest.java
+++ src/test/java/model/SangTest.java
@@ -3,6 +3,8 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -70,6 +72,63 @@ public class SangTest {
                     () -> assertThat(path.contains(point2)).isTrue(),
                     () -> assertThat(path.contains(point3)).isTrue()
             );
+        }
+    }
+
+    @Nested
+    @DisplayName("상 경로 방해 테스트")
+    class SangIsProhibitedPathTest {
+
+        @Test
+        @DisplayName("중간 아군")
+        public void test1() {
+            Sang sang = new Sang(Team.RED);
+            Map<Piece,Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.RED),false);
+
+            assertThat(sang.canMove(pieces)).isFalse();
+        }
+
+        @Test
+        @DisplayName("중간 아군 2명")
+        public void test5() {
+            Sang sang = new Sang(Team.RED);
+            Map<Piece,Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.RED),false);
+            pieces.put(new Cha(Team.RED),false);
+
+            assertThat(sang.canMove(pieces)).isFalse();
+        }
+
+        @Test
+        @DisplayName("종점 아군")
+        public void test2() {
+            Sang sang = new Sang(Team.RED);
+            Map<Piece,Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.RED),true);
+
+            assertThat(sang.canMove(pieces)).isFalse();
+        }
+
+
+        @Test
+        @DisplayName("중간 적군")
+        public void test3() {
+            Sang sang = new Sang(Team.RED);
+            Map<Piece,Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.BLUE),false);
+
+            assertThat(sang.canMove(pieces)).isFalse();
+        }
+
+        @Test
+        @DisplayName("종점 적군")
+        public void test4() {
+            Sang sang = new Sang(Team.RED);
+            Map<Piece,Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.BLUE),true);
+
+            assertThat(sang.canMove(pieces)).isTrue();
         }
     }
 }
