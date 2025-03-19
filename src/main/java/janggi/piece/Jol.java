c0ae064b8af7f8625169e2f810bf5ae690d69da7
KJungW <sagakkb0707@naver.com>
2025-03-19 20:45:58 +0900

    feat(jol): 이동 기능 구현

--- src/main/java/janggi/piece/Jol.java
+++ src/main/java/janggi/piece/Jol.java
@@ -12,27 +12,56 @@ public class Jol implements Piece {
 
     private final PieceType pieceType;
     private final Position position;
+    private final CampType campType;
 
-    private Jol(final Position position) {
+    private Jol(final Position position, final CampType campType) {
         this.pieceType = PieceType.JOL;
         this.position = position;
+        this.campType = campType;
+    }
+
+    public static Jol from(final Position position, final CampType campType) {
+        return new Jol(position, campType);
     }
 
     public static List<Jol> generateInitialJols(final CampType campType) {
         int yPosition = Math.abs(campType.getStartYPosition() - height);
         return xPositions.stream()
-                .map(xPosition -> new Jol(new Position(xPosition, yPosition)))
+                .map(xPosition -> new Jol(new Position(xPosition, yPosition), campType))
                 .toList();
     }
 
     @Override
-    public Jol move(Position destination, List<Piece> enemy, List<Piece> allies) {
-        return new Jol(destination);
+    public Jol move(final Position destination, final List<Piece> enemy, final List<Piece> allies) {
+        boolean isAble = ableToMove(destination, enemy, allies);
+        if (!isAble) {
+            throw new IllegalArgumentException("[ERROR] 이동이 불가능합니다.");
+        }
+        return new Jol(destination, campType);
     }
 
     @Override
     public boolean ableToMove(Position destination, List<Piece> enemy, List<Piece> allies) {
-        return true;
+        if (!isRuleOfMove(destination)) {
+            return false;
+        }
+        return isNotHurdle(destination, allies);
+    }
+
+    private boolean isRuleOfMove(Position destination) {
+        if (campType == CampType.CHO) {
+            return destination.equals(new Position(position.getX() - 1, position.getY()))
+                    || destination.equals(new Position(position.getX() + 1, position.getY()))
+                    || destination.equals(new Position(position.getX(), position.getY() - 1));
+        }
+        return destination.equals(new Position(position.getX() - 1, position.getY()))
+                || destination.equals(new Position(position.getX() + 1, position.getY()))
+                || destination.equals(new Position(position.getX(), position.getY() + 1));
+    }
+
+    private static boolean isNotHurdle(Position destination, List<Piece> allies) {
+        return allies.stream()
+                .noneMatch(piece -> piece.getPosition().equals(destination));
     }
 
     @Override
