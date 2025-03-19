9e2c3241cdac31b5ffd2a5eb1e2933b9fd2bc7a7
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-19 19:52:24 +0900

    RED) test(model): 장 경로 방해 여부 테스트

--- src/test/java/model/JangTest.java
+++ src/test/java/model/JangTest.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -25,16 +27,17 @@ public class JangTest {
         @DisplayName("장 이동 가능 테스트")
         public void test2() {
             Jang jang = new Jang(Team.RED);
-            assertThat(jang.isValidPoint(Point.of(0,0),Point.of(1,0))).isTrue();
+            assertThat(jang.isValidPoint(Point.of(0, 0), Point.of(1, 0))).isTrue();
         }
 
         @Test
         @DisplayName("장 이동 불가능 테스트")
         public void test3() {
             Jang jang = new Jang(Team.RED);
-            assertThat(jang.isValidPoint(Point.of(0,0),Point.of(2,0))).isFalse();
+            assertThat(jang.isValidPoint(Point.of(0, 0), Point.of(2, 0))).isFalse();
         }
     }
+
     @Nested
     @DisplayName("장 이동 경로 계산 테스트")
     class JangCalculatePathTest {
@@ -42,18 +45,43 @@ public class JangTest {
         @DisplayName("수직")
         public void test1() {
             Jang jang = new Jang(Team.RED);
-            Point point =new Point(0,1);
+            Point point = new Point(0, 1);
 
-            assertThat(jang.calculatePath(Point.of(0,0),Point.of(0,1)).contains(point)).isTrue();
+            assertThat(jang.calculatePath(Point.of(0, 0), Point.of(0, 1)).contains(point)).isTrue();
         }
 
         @Test
         @DisplayName("수평")
         public void test2() {
             Jang jang = new Jang(Team.RED);
-            Point point =new Point(1,0);
+            Point point = new Point(1, 0);
 
-            assertThat(jang.calculatePath(Point.of(0,0),Point.of(1,0)).contains(point)).isTrue();
+            assertThat(jang.calculatePath(Point.of(0, 0), Point.of(1, 0)).contains(point)).isTrue();
+        }
+    }
+
+    @Nested
+    @DisplayName("장 경로 방해 테스트")
+    class JangIsProhibitedPathTest {
+
+        @Test
+        @DisplayName("아군")
+        public void test3() {
+            Jang jang = new Jang(Team.RED);
+            List<Piece> pieces = new ArrayList<>();
+            pieces.add(new Cha(Team.RED));
+
+            assertThat(jang.isProhibitedPath(pieces)).isFalse();
+        }
+
+        @Test
+        @DisplayName("적군")
+        public void test4() {
+            Jang jang = new Jang(Team.RED);
+            List<Piece> pieces = new ArrayList<>();
+            pieces.add(new Cha(Team.BLUE));
+
+            assertThat(jang.isProhibitedPath(pieces)).isTrue();
         }
     }
 
