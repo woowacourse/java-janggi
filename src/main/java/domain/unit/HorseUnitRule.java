1e03a1397bd2408268258db619885f6598e1bd99
ppparkta <pppppark2@gmail.com>
2025-03-19 20:30:47 +0900

    feat: "마"의 이동 가능한 모든 경로 구현

--- src/main/java/domain/unit/HorseUnitRule.java
+++ src/main/java/domain/unit/HorseUnitRule.java
@@ -1,12 +1,39 @@
 package domain.unit;
 
 import domain.Route;
+import java.util.ArrayList;
 import java.util.List;
 
-public class HorseUnitRule implements UnitRule{
+public class HorseUnitRule implements UnitRule {
     @Override
     public List<Route> calculateAllRoute(Point start) {
-        return List.of();
+        List<Route> routes = new ArrayList<>();
+
+        List<Integer> dx = List.of(0, 1, 0, -1);
+        List<Integer> dy = List.of(1, 0, -1, 0);
+
+        int x = start.getX();
+        int y = start.getY();
+        for (int i = 0; i < dx.size(); i++) {
+            Point pivot = new Point(x + dx.get(i), y + dy.get(i));
+            if (i == 0) {
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() + 1, pivot.getY() + 1))));
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() - 1, pivot.getY() + 1))));
+            }
+            if (i == 1) {
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() + 1, pivot.getY() + 1))));
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() + 1, pivot.getY() - 1))));
+            }
+            if (i == 2) {
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() + 1, pivot.getY() - 1))));
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() - 1, pivot.getY() - 1))));
+            }
+            if (i == 3) {
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() - 1, pivot.getY() + 1))));
+                routes.add(Route.of(List.of(pivot, new Point(pivot.getX() - 1, pivot.getY() - 1))));
+            }
+        }
+        return routes;
     }
 
     @Override
