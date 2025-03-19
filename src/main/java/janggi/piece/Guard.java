8969eec8d432b1489394dfe95fd8687e43d1516f
sung-june27 <kmymy27@gmail.com>
2025-03-19 15:56:08 +0900

    feat(Guard): Guard의 이동 경로 반환 구현

--- src/main/java/janggi/piece/Guard.java
+++ src/main/java/janggi/piece/Guard.java
@@ -1,10 +1,28 @@
 package janggi.piece;
 
+import janggi.Position;
 import janggi.Side;
+import java.util.List;
 
 public class Guard extends Piece {
 
     public Guard(Side side) {
         super(side);
+    }
+
+    @Override
+    public List<Position> calculatePath(Position start, Position end) {
+        int differenceX = end.x() - start.x();
+        int differenceY = end.y() - start.y();
+        validateMovingRule(differenceX, differenceY);
+        return List.of();
+    }
+
+    private void validateMovingRule(int differenceX, int differenceY) {
+        if ((Math.abs(differenceX) == 1 && Math.abs(differenceY) == 0)
+        || (Math.abs(differenceX) == 0 && Math.abs(differenceY) == 1)) {
+            return;
+        }
+        throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
     }
 }
