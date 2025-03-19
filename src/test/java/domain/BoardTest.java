94edf1dd1e63e3b28739f4d73c10ddcdaa63e45a
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-03-19 16:15:03 +0900

    feat : 한나라는 마, 상은 입력받은 위치로 생성 기능 구현

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import fixture.BoardFixture;
 import java.util.List;
 import java.util.Map;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,16 +15,47 @@ public class BoardTest {
     @DisplayName("궁 1개, 차 2개, 포 2개, 마 2개, 상 2개, 사 2개, 졸 5개를 갖는다")
     void test() {
         // given & when
-        HanBoard hanBoard = HanBoard.createWithPieces();
-        Map<Location, Piece> hanPieces = (Map<Location, Piece>)BoardFixture.createHanPieces();
+        Location horseLocation1 = new Location(2, 1);
+        Location horseLocation2 = new Location(8,1);
+        Location elephantLocation1 = new Location(3,1);
+        Location elephantLocation2 = new Location(7,1);
+        List<Location> horseLocations = List.of(horseLocation1, horseLocation2);
+        List<Location> elephantLocations = List.of(elephantLocation1, elephantLocation2);
+        HanBoard hanBoard = HanBoard.createWithPieces(horseLocations, elephantLocations);
+        Map<Location, Piece> hanPieces = BoardFixture.createHanPieces(horseLocations, elephantLocations);
 
         //when
-        Map<Location, Piece> pieces = (Map<Location, Piece>)hanBoard.getPieces();
+        Map<Location, Piece> pieces = hanBoard.getPieces();
 
         // then
         assertThat(pieces).containsExactlyInAnyOrderEntriesOf(hanPieces);
     }
 
-    List<Location> horseLocations = List.of(new Location(2,1), new Location(8,1));
-    List<Location> elephantLocations = List.of(new Location(3,1), new Location(7,1));
+    @Test
+    @DisplayName("마와 상의 위치는 입력값으로 정한다")
+    void test2() {
+        // given & when
+        Location horseLocation1 = new Location(2, 1);
+        Location horseLocation2 = new Location(8,1);
+        Location elephantLocation1 = new Location(3,1);
+        Location elephantLocation2 = new Location(7,1);
+        List<Location> horseLocations = List.of(horseLocation1, horseLocation2);
+        List<Location> elephantLocations = List.of(elephantLocation1, elephantLocation2);
+
+        //when
+        Map<Location, Piece> hanPieces = BoardFixture.createHanPieces(horseLocations, elephantLocations);
+
+        // then
+        Piece horsePiece1 = hanPieces.get(horseLocation1);
+        Piece horsePiece2 = hanPieces.get(horseLocation2);
+        Piece elephantPiece1 = hanPieces.get(elephantLocation1);
+        Piece elephantPiece2 = hanPieces.get(elephantLocation2);
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat((Horse)horsePiece1).isInstanceOf(Horse.class);
+            softly.assertThat((Horse)horsePiece2).isInstanceOf(Horse.class);
+            softly.assertThat((Elephant)elephantPiece1).isInstanceOf(Elephant.class);
+            softly.assertThat((Elephant)elephantPiece2).isInstanceOf(Elephant.class);
+        });
+    }
 }
--- src/test/java/fixture/BoardFixture.java
+++ src/test/java/fixture/BoardFixture.java
@@ -10,19 +10,18 @@ import domain.Piece;
 import domain.Scholar;
 import domain.Soldier;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BoardFixture {
 
-    public static Map<Location, ? super Piece> createHanPieces() {
-        Map<Location, ? super Piece> pieces = new HashMap<>();
+    public static Map<Location, Piece> createHanPieces(List<Location> horseLocations, List<Location> elephantLocations) {
+        Map<Location, Piece> pieces = new HashMap<>();
+        horseLocations.forEach(horseLocation -> pieces.put(horseLocation, new Horse()));
+        elephantLocations.forEach(elephantLocation -> pieces.put(elephantLocation, new Elephant()));
         pieces.put(new Location(1, 1), new Chariot());
-        pieces.put(new Location(2, 1), new Horse());
-        pieces.put(new Location(3, 1), new Elephant());
         pieces.put(new Location(4, 1), new Scholar());
         pieces.put(new Location(6, 1), new Scholar());
-        pieces.put(new Location(7, 1), new Elephant());
-        pieces.put(new Location(8, 1), new Horse());
         pieces.put(new Location(9, 1), new Chariot());
         pieces.put(new Location(5, 2), new King());
         pieces.put(new Location(2, 3), new Cannon());
