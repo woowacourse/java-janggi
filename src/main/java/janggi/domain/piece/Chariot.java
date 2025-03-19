489a86a893fb4f304b87ac17b05f1b937b53934b
goohong <goohong.chung@gmail.com>
2025-03-19 20:21:12 +0900

    feat: 차의 이동 기능 구현

--- src/main/java/janggi/domain/piece/Chariot.java
+++ src/main/java/janggi/domain/piece/Chariot.java
@@ -2,6 +2,7 @@ package janggi.domain.piece;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Chariot extends Piece {
     private static final List<Position> INITIAL_POSITIONS_BLUE = List.of(
@@ -11,7 +12,7 @@ public class Chariot extends Piece {
             new Position(1, 1),
             new Position(1, 9));
 
-    private Chariot(final Position position, final TeamType teamType) {
+    public Chariot(final Position position, final TeamType teamType) {
         super("차", position, teamType);
     }
 
@@ -25,5 +26,36 @@ public class Chariot extends Piece {
         INITIAL_POSITIONS_RED.forEach(position ->
                 chariots.add(new Chariot(position, teamType)));
         return chariots;
+    }
+
+    public Chariot move(final Map<Position, Piece> pieces, final Position positionToMove) {
+        validateIsPositionMovable(positionToMove);
+        validateIsSameTeamNotInPositionToMove(pieces, positionToMove);
+        validateNothingBetweenPositionToMove(pieces, positionToMove);
+        return new Chariot(positionToMove, teamType);
+    }
+
+    private void validateIsPositionMovable(final Position value) {
+        if (checkIsPositionNotDiagonal(value)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkIsPositionNotDiagonal(final Position value) {
+        return Math.abs(value.x() - getPosition().x()) != 0 && Math.abs(value.y() - getPosition().y()) != 0;
+    }
+
+    private void validateIsSameTeamNotInPositionToMove(Map<Position, Piece> pieces, Position positionToMove) {
+        if (pieces.containsKey(positionToMove)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNothingBetweenPositionToMove(Map<Position, Piece> pieces, Position positionToMove) {
+        if (pieces.entrySet().stream()
+                .filter(entry -> !entry.getKey().equals(this.getPosition()) && !entry.getKey().equals(positionToMove))
+                .anyMatch(entry -> !(entry.getValue() instanceof None))) {
+            throw new IllegalArgumentException();
+        }
     }
 }
---
+++ src/test/java/janggi/domain/piece/ChariotTest.java
@@ -0,0 +1,58 @@
+package janggi.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ChariotTest {
+    @DisplayName("이동 위치 값을 입력 받아 이동한다.")
+    @Test
+    void move() {
+        Chariot chariot = new Chariot(new Position(5, 5), TeamType.BLUE);
+        Position positionToMove = new Position(5, 10);
+        Map<Position, Piece> pieces = new HashMap<>();
+        Chariot movedChariot = chariot.move(pieces, positionToMove);
+        assertThat(movedChariot.getPosition()).isEqualTo(positionToMove);
+    }
+
+    @DisplayName("차의 이동 위치 값이 불가능한 값인 경우 예외를 던진다.")
+    @CsvSource(value = {"7,7", "4,4"})
+    @ParameterizedTest
+    void move2(int x, int y) {
+        Chariot chariot = new Chariot(new Position(5, 5), TeamType.BLUE);
+        Position positionToMove = new Position(x, y);
+        Map<Position, Piece> pieces = new HashMap<>();
+        assertThatThrownBy(() -> chariot.move(pieces, positionToMove))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("차의 초기 위치와 이동 위치 사이에 말이 존재하는 경우 예외를 던진다.")
+    @Test
+    void move3() {
+        Chariot chariot = new Chariot(new Position(5, 5), TeamType.BLUE);
+        Map<Position, Piece> pieces = new HashMap<>();
+        Soldier otherSoldier = new Soldier(new Position(3, 5), TeamType.BLUE);
+        pieces.put(otherSoldier.getPosition(), otherSoldier);
+        assertThatThrownBy(() ->
+                chariot.move(pieces, new Position(1, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("차의 이동 위치에 같은 편 말이 있으면 예외를 던진다")
+    @Test
+    void move4() {
+        Chariot chariot = new Chariot(new Position(5, 5), TeamType.BLUE);
+        Map<Position, Piece> pieces = new HashMap<>();
+        Soldier otherSoldier = new Soldier(new Position(4, 5), TeamType.BLUE);
+        pieces.put(otherSoldier.getPosition(), otherSoldier);
+        assertThatThrownBy(() ->
+                chariot.move(pieces, otherSoldier.getPosition()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
