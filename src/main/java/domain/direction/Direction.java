59718ba4364e77561ea27e35c62905cc0f95bd14
taek2222 <oot309@daum.net>
2025-03-19 16:16:10 +0900

    feat: 이동 거리 반환 기능 추가

--- src/main/java/domain/direction/Direction.java
+++ src/main/java/domain/direction/Direction.java
@@ -1,6 +1,8 @@
 package domain.direction;
 
 import domain.piece.Position;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Direction {
@@ -17,5 +19,16 @@ public class Direction {
             result = result.merge(dir);
         }
         return result.equals(target);
+    }
+
+    public List<Position> createPath(final Position start) {
+        List<Position> paths = new ArrayList<>();
+
+        Position path = start;
+        for (Position dir : direction) {
+            paths.add(path);
+            path = path.merge(dir);
+        }
+        return paths.subList(1, direction.size());
     }
 }
