42595c00bb5922c0895ea716f666af78cc859e21
Yehyeok Bang <qkddpgur318@gmail.com>
2025-03-19 21:23:47 +0900

    feat: 상 고유 기능 구현
    - 상은 다른 기물을 넘어 이동할 수 없습니다.
    
    Co-authored-by: YeonhuLee <yeonnhuu@gmail.com>
    Co-authored-by: eunseongu <rndmstjs75@gmail.com>

--- src/main/java/janggi/piece/Elephant.java
+++ src/main/java/janggi/piece/Elephant.java
@@ -4,6 +4,8 @@ import janggi.Camp;
 import janggi.PieceSymbol;
 import janggi.Point;
 import janggi.board.Board;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class Elephant extends Piece {
 
@@ -17,6 +19,49 @@ public final class Elephant extends Piece {
                 (fromPoint.calculateXDistance(toPoint) == 3 && fromPoint.calculateYDistance(toPoint) == 2))) {
             throw new IllegalArgumentException("상은 직선으로 한 칸, 대각선으로 두 칸 움직여야 합니다.");
         }
+
+        Set<Piece> pieces = getBoard().getPiecesByPoint(findRoute(fromPoint, toPoint));
+        if (!pieces.isEmpty()) {
+            throw new IllegalArgumentException("상은 기물을 넘어서 이동할 수 없습니다.");
+        }
+    }
+
+    private Set<Point> findRoute(Point fromPoint, Point toPoint) {
+        Set<Point> route = new HashSet<>();
+        if (isHorizontal(fromPoint, toPoint)) {
+            if (fromPoint.getX() < toPoint.getX()) {
+                Point firstPoint = new Point(fromPoint.getX() + 1, fromPoint.getY());
+                route.add(firstPoint);
+                Point secondPoint = new Point((firstPoint.getX() + toPoint.getX()) / 2,
+                        (firstPoint.getY() + toPoint.getY()) / 2);
+                route.add(secondPoint);
+                return route;
+            }
+            Point firstPoint = new Point(fromPoint.getX() - 1, fromPoint.getY());
+            route.add(firstPoint);
+            Point secondPoint = new Point((firstPoint.getX() + toPoint.getX()) / 2,
+                    (firstPoint.getY() + toPoint.getY()) / 2);
+            route.add(secondPoint);
+            return route;
+        }
+        if (fromPoint.getY() < toPoint.getY()) {
+            Point firstPoint = new Point(fromPoint.getX(), fromPoint.getY() + 1);
+            route.add(firstPoint);
+            Point secondPoint = new Point((firstPoint.getX() + toPoint.getX()) / 2,
+                    (firstPoint.getY() + toPoint.getY()) / 2);
+            route.add(secondPoint);
+            return route;
+        }
+        Point firstPoint = new Point(fromPoint.getX(), fromPoint.getY() - 1);
+        route.add(firstPoint);
+        Point secondPoint = new Point((firstPoint.getX() + toPoint.getX()) / 2,
+                (firstPoint.getY() + toPoint.getY()) / 2);
+        route.add(secondPoint);
+        return route;
+    }
+
+    private boolean isHorizontal(Point fromPoint, Point toPoint) {
+        return fromPoint.calculateXDistance(toPoint) == 3;
     }
 
     @Override
--- src/test/java/janggi/piece/ElephantTest.java
+++ src/test/java/janggi/piece/ElephantTest.java
@@ -57,4 +57,58 @@ class ElephantTest {
         assertThatCode(() -> elephant.validateMove(fromPoint, toPoint))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("상은 직선으로 한 칸, 대각선으로 두 칸 움직일 때 직선 이동중 기물에 막힌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "HAN,7,8",
+            "HAN,7,2",
+            "HAN,8,3",
+            "HAN,3,2",
+    })
+    void shouldThrowException_WhenLinearBlocked(Camp camp, int toX, int toY) {
+        // given
+        Board board = new Board();
+        Elephant elephant = new Elephant(camp, board);
+        Point fromPoint = new Point(5, 5);
+        Point toPoint = new Point(toX, toY);
+        board.placePiece(new Point(5, 6), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(6, 5), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(5, 4), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(4, 5), new Soldier(Camp.CHU, board));
+
+        // when & then
+        assertThatCode(() -> elephant.validateMove(fromPoint, toPoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상은 기물을 넘어서 이동할 수 없습니다.");
+    }
+
+    @DisplayName("상은 직선으로 한 칸, 대각선으로 두 칸 움직일 때 대각선으로 이동중 기물에 막힌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "HAN,7,8",
+            "HAN,7,2",
+            "HAN,8,3",
+            "HAN,3,2",
+    })
+    void shouldThrowException_WhenDiagonalBlocked(Camp camp, int toX, int toY) {
+        // given
+        Board board = new Board();
+        Elephant elephant = new Elephant(camp, board);
+        Point fromPoint = new Point(5, 5);
+        Point toPoint = new Point(toX, toY);
+        board.placePiece(new Point(6, 7), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(7, 6), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(6, 3), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(3, 6), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(4, 3), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(3, 4), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(7, 4), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(4, 7), new Soldier(Camp.CHU, board));
+
+        // when & then
+        assertThatCode(() -> elephant.validateMove(fromPoint, toPoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상은 기물을 넘어서 이동할 수 없습니다.");
+    }
 }
