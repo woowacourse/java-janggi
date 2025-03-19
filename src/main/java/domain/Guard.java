6364666bcbdf4fcb66606b38e184d7aef56370de
ykmxxi <ykmxxi97@gmail.com>
2025-03-19 17:36:50 +0900

    feat: 궁과 사의 움직임 로직 구현

---
+++ src/main/java/domain/Guard.java
@@ -0,0 +1,24 @@
+package domain;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Guard extends Piece {
+
+    public Guard(final Position position, final Team team, final Board board) {
+        super(position, team, board);
+    }
+
+    @Override
+    protected Set<Position> getMovablePositions() {
+        return Direction.getStraightDirection().stream()
+                .map(direction -> position.nextPosition(direction))
+                .filter(this::isMovable)
+                .collect(Collectors.toSet());
+    }
+
+    private boolean isMovable(final Position position) {
+        return !board.isExists(position) || !board.isSameTeam(this, position);
+    }
+
+}
