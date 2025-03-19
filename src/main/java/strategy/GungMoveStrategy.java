fa179882d985247b90bb961e27d7b53c9f6c8c0e
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-19 18:00:22 +0900

    test: 사,궁 테스트 추가

--- src/main/java/strategy/GungMoveStrategy.java
+++ src/main/java/strategy/GungMoveStrategy.java
@@ -7,13 +7,15 @@ import piece.Team;
 
 public class GungMoveStrategy implements MoveStrategy {
 
+    private static final String NOT_IMPLEMENTED_FEATURE = "아직 구현되지 않은 기능입니다.";
+
     @Override
-    public Route getLegalRoute(piece.Position startPosition, piece.Position endPosition) {
-        return null;
+    public Route getLegalRoute(Position startPosition, Position endPosition) {
+        throw new IllegalStateException(NOT_IMPLEMENTED_FEATURE);
     }
 
     @Override
-    public piece.Position move(Position destination, Pieces onRoutePieces, Team moveTeam) {
-        return null;
+    public Position move(Position destination, Pieces onRoutePieces, Team moveTeam) {
+        throw new IllegalStateException(NOT_IMPLEMENTED_FEATURE);
     }
 }
--- src/main/java/strategy/SaMoveStrategy.java
+++ src/main/java/strategy/SaMoveStrategy.java
@@ -6,14 +6,16 @@ import piece.Route;
 import piece.Team;
 
 public class SaMoveStrategy implements MoveStrategy {
+
+    private static final String NOT_IMPLEMENTED_FEATURE = "아직 구현되지 않은 기능입니다.";
     
     @Override
     public Route getLegalRoute(Position startPosition, Position endPosition) {
-        return null;
+        throw new IllegalStateException(NOT_IMPLEMENTED_FEATURE);
     }
 
     @Override
     public Position move(Position destination, Pieces onRoutePieces, Team moveTeam) {
-        return null;
+        throw new IllegalStateException(NOT_IMPLEMENTED_FEATURE);
     }
 }
