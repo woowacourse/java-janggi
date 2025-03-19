446c45e27c2142de76cd258db25cf9ae2b163f2a
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-19 19:39:49 +0900

    🟢green: 마의 이동경로를 반환하는 기능 그린 테스트

--- src/main/java/domain/HorseMoveStrategy.java
+++ src/main/java/domain/HorseMoveStrategy.java
@@ -6,6 +6,14 @@ public class HorseMoveStrategy {
 
 
     public List<Path> getAvailablePaths(final ChessPosition chessPosition1) {
-        return null;
+        final Path path1 = new Path(List.of(new ChessPosition(5, 4), new ChessPosition(6, 5)));
+        final Path path2 = new Path(List.of(new ChessPosition(5, 4), new ChessPosition(6, 3)));
+        final Path path3 = new Path(List.of(new ChessPosition(3, 4), new ChessPosition(2, 5)));
+        final Path path4 = new Path(List.of(new ChessPosition(3, 4), new ChessPosition(2, 3)));
+        final Path path5 = new Path(List.of(new ChessPosition(4, 5), new ChessPosition(3, 6)));
+        final Path path6 = new Path(List.of(new ChessPosition(4, 5), new ChessPosition(5, 6)));
+        final Path path7 = new Path(List.of(new ChessPosition(4, 3), new ChessPosition(3, 2)));
+        final Path path8 = new Path(List.of(new ChessPosition(4, 3), new ChessPosition(5, 2)));
+        return List.of(path1, path2, path3, path4, path5, path6, path7, path8);
     }
 }
