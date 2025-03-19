65c6d35e7a299ccbee1a7db0ad36f310cb87c600
ryan-dia <cjfdnjs158@naver.com>
2025-03-19 21:08:51 +0900

    test: 상의 이동 경로에 말 존재 여부 확인 기능 실패 테스트 작성
    Co-authored-by:saera-yook<yook.saera@gmail.com>

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -3,6 +3,7 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
@@ -44,5 +45,20 @@ class BoardTest {
         assertThat(position).isEqualTo(expectedPosition);
     }
 
+    @Test
+    void 상의_이동_경로에_말이_있으면_true_없으면_false_반환() {
 
+        // given
+        final Board board = Board.initialize();
+
+        // when
+        final Position treuPosition = board.findPositionBy(Point.of(1, 0));
+        final Position falsePosition = board.findPositionBy(Point.of(6, 0));
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            assertThat(board.hasPieceOnPath(treuPosition, Point.of(3, 3))).isTrue();
+            assertThat(board.hasPieceOnPath(falsePosition, Point.of(8, 3))).isFalse();
+        });
+    }
 }
