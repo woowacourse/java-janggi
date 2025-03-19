3a2cc8cafa41faf9dc8a10c97bdf2aff2e99f2c2
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-03-19 12:00:25 +0900

    feat : 보드판은 세로 10, 가로 9의 좌표로 이루어져있도록 구현

---
+++ src/main/java/Location.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+
+public class Location {
+
+    int x;
+    int y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Location location)) {
+            return false;
+        }
+        return x == location.x && y == location.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public boolean isUnder(Location location) {
+        return this.x < location.x || this.y < location.y;
+    }
+
+    public boolean isOver(Location location) {
+        return this.x > location.x || this.y > location.y;
+    }
+}
---
+++ src/test/java/BoardRangeTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BoardRangeTest {
+
+    @DisplayName("보드의 범위는 가로9, 세로 10 범위 밖에있는지 검증한다")
+    @ParameterizedTest
+    @CsvSource({
+            "0,0",
+            "1,0",
+            "0,1",
+            "10,9",
+            "10,10"
+    })
+    void test() {
+        // given
+        Location location = new Location(0, 0);
+
+        // when & then
+        assertThatThrownBy(() ->{
+            BoardRange.validateBoardRange(location);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
