cf487e604817be66197bb0ee63f3ba37fa2dcb5b
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-19 21:15:30 +0900

    RED) test(model): 차 경로 방해 테스트

--- src/test/java/model/ChaTest.java
+++ src/test/java/model/ChaTest.java
@@ -3,6 +3,8 @@ package model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -26,14 +28,14 @@ public class ChaTest {
         @DisplayName("차 이동 가능 여부 판별 테스트")
         public void test2() {
             Cha cha = new Cha(Team.RED);
-            assertThat(cha.isValidPoint(Point.of(0,0),Point.of(100,0))).isTrue();
+            assertThat(cha.isValidPoint(Point.of(0, 0), Point.of(100, 0))).isTrue();
         }
 
         @Test
         @DisplayName("차 이동 불가능 여부 판별 테스트")
         public void test3() {
             Cha cha = new Cha(Team.RED);
-            assertThat(cha.isValidPoint(Point.of(0,0), Point.of(10,10))).isFalse();
+            assertThat(cha.isValidPoint(Point.of(0, 0), Point.of(10, 10))).isFalse();
         }
     }
 
@@ -50,7 +52,7 @@ public class ChaTest {
             Point point4 = new Point(0, 4);
             Point point5 = new Point(0, 7);
 
-            Path path = cha.calculatePath(Point.of(0,0), Point.of(0,7));
+            Path path = cha.calculatePath(Point.of(0, 0), Point.of(0, 7));
 
             assertAll(
                     () -> assertThat(path.contains(point1)).isTrue(),
@@ -71,7 +73,7 @@ public class ChaTest {
             Point point4 = new Point(6, 0);
             Point point5 = new Point(7, 0);
 
-            Path path = cha.calculatePath(Point.of(0,0), Point.of(7,0));
+            Path path = cha.calculatePath(Point.of(0, 0), Point.of(7, 0));
 
             assertAll(
                     () -> assertThat(path.contains(point1)).isTrue(),
@@ -80,6 +82,63 @@ public class ChaTest {
                     () -> assertThat(path.contains(point4)).isTrue(),
                     () -> assertThat(path.contains(point5)).isTrue()
             );
+        }
+    }
+
+    @Nested
+    @DisplayName("차 경로 방해 테스트")
+    class ChaIsProhibitedPathTest {
+
+        @Test
+        @DisplayName("중간 아군")
+        public void test1() {
+            Cha cha = new Cha(Team.RED);
+            Map<Piece, Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.RED), false);
+
+            assertThat(cha.canMove(pieces)).isFalse();
+        }
+
+        @Test
+        @DisplayName("중간 아군 2명")
+        public void test5() {
+            Cha cha = new Cha(Team.RED);
+            Map<Piece, Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.RED), false);
+            pieces.put(new Cha(Team.RED), false);
+
+            assertThat(cha.canMove(pieces)).isFalse();
+        }
+
+        @Test
+        @DisplayName("종점 아군")
+        public void test2() {
+            Cha cha = new Cha(Team.RED);
+            Map<Piece, Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.RED), true);
+
+            assertThat(cha.canMove(pieces)).isFalse();
+        }
+
+
+        @Test
+        @DisplayName("중간 적군")
+        public void test3() {
+            Cha cha = new Cha(Team.RED);
+            Map<Piece, Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.BLUE), false);
+
+            assertThat(cha.canMove(pieces)).isFalse();
+        }
+
+        @Test
+        @DisplayName("종점 적군")
+        public void test4() {
+            Cha cha = new Cha(Team.RED);
+            Map<Piece, Boolean> pieces = new HashMap<>();
+            pieces.put(new Cha(Team.BLUE), true);
+
+            assertThat(cha.canMove(pieces)).isTrue();
         }
     }
 }
