af1892b8ec997c4eb357e3b222fea976948fbdd3
CheChe903 <cheche903@naver.com>
2025-03-19 15:18:34 +0900

    feat(Route): 루트 동등성 체크 로직 및 단위 테스트 구현

--- src/main/java/janggi/domain/Route.java
+++ src/main/java/janggi/domain/Route.java
@@ -2,6 +2,7 @@ package janggi.domain;
 
 import janggi.domain.piece.Position;
 import java.util.List;
+import java.util.Objects;
 
 public class Route {
 
@@ -15,5 +16,19 @@ public class Route {
 
     public boolean hasNotPosition(Position position) {
         return !route.contains(position);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Route route1 = (Route) o;
+        return Objects.equals(route, route1.route) && Objects.equals(destination, route1.destination);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(route, destination);
     }
 }
--- src/test/java/janggi/domain/RouteTest.java
+++ src/test/java/janggi/domain/RouteTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import janggi.domain.piece.Position;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -26,5 +27,33 @@ class RouteTest {
         assertThat(route.hasNotPosition(new Position(targetX, targetY))).isEqualTo(expected);
     }
 
+    @DisplayName("두 루트가 동일한지 확인한다.")
+    @Test
+    void routeEqualTest() {
 
+        // given
+        Route route = new Route(List.of(new Position(1, 1), new Position(2, 2)),
+                new Position(3, 3));
+
+        Route otherRoute = new Route(List.of(new Position(1, 1), new Position(2, 2)),
+                new Position(3, 3));
+
+        // when & then
+        assertThat(route.equals(otherRoute)).isTrue();
+    }
+
+    @DisplayName("두 루트가 다른지 확인한다.")
+    @Test
+    void routeNotEqualTest() {
+
+        // given
+        Route route = new Route(List.of(new Position(1, 1), new Position(2, 2)),
+                new Position(3, 3));
+
+        Route otherRoute = new Route(List.of(new Position(1, 1), new Position(2, 2)),
+                new Position(3, 2));
+
+        // when & then
+        assertThat(route.equals(otherRoute)).isFalse();
+    }
 }
