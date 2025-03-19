92511d98b0f5badf64bb8d2ca3151dc61db2cdc2
ppparkta <pppppark2@gmail.com>
2025-03-19 21:25:53 +0900

    feat: 엔드포인트에 턴과 동일한 팀의 기물이 있으면 경로 제거

--- src/main/java/domain/unit/Team.java
+++ src/main/java/domain/unit/Team.java
@@ -4,4 +4,11 @@ public enum Team {
     HAN,
     CHO,
     ;
+
+    public boolean isFront(Point before, Point after) {
+        if (this == HAN) {
+            return before.getY() < after.getY();
+        }
+        return before.getY() > after.getY();
+    }
 }
