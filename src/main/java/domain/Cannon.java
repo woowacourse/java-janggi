cff50e27a571c80e2c956c06493530d684457d4b
ryan-dia <cjfdnjs158@naver.com>
2025-03-19 21:54:25 +0900

    feat: 포의 이동 가능 경로 반환 기능 구현
    Co-authored-by:saera-yook<yook.saera@gmail.com>

--- src/main/java/domain/Cannon.java
+++ src/main/java/domain/Cannon.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cannon extends AbstractPiece {
@@ -8,9 +9,56 @@ public class Cannon extends AbstractPiece {
         super(team);
     }
 
+    /*
+    포는 말 1개를 건너 뛰어야만 이동 가능하므로 target은 prev에서 한 칸 옮긴 위치에서 시작한다.
+     */
     @Override
-    public List<Point> getPossiblePoint(final Point prev, final Point newPoint) {
-        return List.of();
+    public List<Point> getPossiblePoint(Point prev, Point newPoint) {
+        List<Point> possiblePoint = new ArrayList<>();
+
+        int x = prev.calculateSubtractionX(newPoint);
+        int y = prev.calculateSubtractionY(newPoint);
+        if (x > 0) {
+            Point target = prev.right();
+            for (int i = 0; i < prev.distanceToMaxX(); i++) {
+                target = target.right();
+                if (target.equals(newPoint)) {
+                    break;
+                }
+                possiblePoint.add(target);
+            }
+        }
+        if (x < 0) {
+            Point target = prev.left();
+            for (int i = 0; i < prev.distanceToMinX(); i++) {
+                target = target.left();
+                if (target.equals(newPoint)) {
+                    break;
+                }
+                possiblePoint.add(target);
+            }
+        }
+        if (y > 0) {
+            Point target = prev.up();
+            for (int i = 0; i < prev.distanceToMaxY(); i++) {
+                target = target.up();
+                if (target.equals(newPoint)) {
+                    break;
+                }
+                possiblePoint.add(target);
+            }
+        }
+        if (y < 0) {
+            Point target = prev.down();
+            for (int i = 0; i < prev.distanceToMinY(); i++) {
+                target = target.down();
+                if (target.equals(newPoint)) {
+                    break;
+                }
+                possiblePoint.add(target);
+            }
+        }
+        return possiblePoint;
     }
 
     @Override
