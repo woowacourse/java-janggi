d85120521f627a14e762a2498276334c8b3f98f4
taek2222 <oot309@daum.net>
2025-03-19 16:22:02 +0900

    test: 반복 이동이 있는 방향성 경로 반환 테스트 추가

--- src/test/java/domain/direction/DirectionTest.java
+++ src/test/java/domain/direction/DirectionTest.java
@@ -12,21 +12,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DirectionTest {
 
     @Test
-    void 이동_경로를_반환한다() {
+    void 반복_이동이_없는_방향성에_대해_경로를_반환한다() {
         // given
         Position start = Position.of(5, 5);
-        Position target = Position.of(6, 3);
         List<Position> expected = List.of(Position.of(5, 4));
 
         List<Position> positions = List.of(Position.ofDirection(0, -1), Position.ofDirection(1, -1));
         Direction direction = new Direction(positions);
 
         // when
+        List<Position> result = direction.createPath(start);
+
+        // then
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void 반복_이동이_있는_방향성에_대해_경로를_반환한다() {
+        // given
+        Position start = Position.of(5, 5);
+        Position target = Position.of(5, 9);
+        List<Position> expected = List.of(Position.of(5, 6), Position.of(5, 7), Position.of(5, 8));
+
+        List<Position> positions = List.of(Position.ofDirection(0, 1));
+        boolean repeat = true;
+        Direction direction = new Direction(positions, repeat);
+
+        // when
         List<Position> result = direction.createPath(start, target);
 
         // then
-        assertThat(result)
-                .containsAll(expected);
+        assertThat(result).containsAll(expected);
     }
 
     @ParameterizedTest
