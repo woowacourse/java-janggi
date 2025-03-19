6ff81886bf2ab1c3f80b19ed80e2fe96c0cc3507
wogus <ind07152@naver.com>
2025-03-19 16:22:47 +0900

    feat: 중간 이동 지점 반환 기능 추가

--- src/main/java/domain/Movement.java
+++ src/main/java/domain/Movement.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Movement {
@@ -12,7 +13,7 @@ public class Movement {
     public boolean isValidMove(Position startPosition, Position endPosition) {
         Position curPosition = startPosition;
         for (Direction direction : movement) {
-            if(isInvalidMove(curPosition, direction)){
+            if (isInvalidMove(curPosition, direction)) {
                 return false;
             }
             curPosition = curPosition.movePosition(direction.getDeltaRow(), direction.getDeltaColumn());
@@ -20,7 +21,21 @@ public class Movement {
         return curPosition.equals(endPosition);
     }
 
-    private static boolean isInvalidMove(Position curPosition, Direction direction) {
+    public List<Position> findIntermediatePositions(Position startPosition, Position endPosition) {
+        if (isValidMove(startPosition, endPosition)) {
+            List<Position> positions = new ArrayList<>();
+            Position curPosition = startPosition;
+            for (Direction direction : movement) {
+                curPosition = curPosition.movePosition(direction.getDeltaRow(), direction.getDeltaColumn());
+                positions.add(curPosition);
+            }
+            positions.removeLast();
+            return positions;
+        }
+        throw new IllegalArgumentException("지정한 포지션으로 이동할 수 없습니다.");
+    }
+
+    private boolean isInvalidMove(Position curPosition, Direction direction) {
         return !curPosition.canMovePosition(direction.getDeltaRow(), direction.getDeltaColumn());
     }
 }
