89efcb4248386837d30f5f466dde78c25c022103
junhaa <2171326@hansung.ac.kr>
2025-03-19 16:38:20 +0900

    feat: 이동 경로 지점을 반환하는 기능 구현

--- src/main/java/domain/Movements.java
+++ src/main/java/domain/Movements.java
@@ -14,4 +14,12 @@ public class Movements {
         return movements.stream()
                 .anyMatch(movement -> movement.isValidMove(startPosition, endPosition));
     }
+
+    public List<Position> findIntermediatePositions(Position startPosition, Position endPosition) {
+        return movements.stream()
+                .filter(movement -> movement.isValidMove(startPosition, endPosition))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("지정한 포지션으로 이동할 수 없습니다."))
+                .findIntermediatePositions(startPosition, endPosition);
+    }
 }
--- src/test/java/domain/MovementsTest.java
+++ src/test/java/domain/MovementsTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -39,4 +40,38 @@ class MovementsTest {
 
         assertThat(result).isFalse();
     }
+
+    @Test
+    @DisplayName("이동 경로의 포지션들을 반환한다")
+    void findIntermediatePositionsTest() {
+        List<Movement> movementList = List.of(
+                new Movement(List.of(Direction.UP, Direction.RIGHT_UP, Direction.RIGHT)),
+                new Movement(List.of(Direction.LEFT, Direction.LEFT_DOWN, Direction.DOWN))
+        );
+        Movements movements = new Movements(movementList);
+
+        Position startPosition = Position.of(2, 2);
+        Position endPosition = Position.of(0, 0);
+
+        assertThat(movements.findIntermediatePositions(startPosition, endPosition))
+                .containsExactly(Position.of(2, 1), Position.of(1, 0));
+    }
+
+    @Test
+    @DisplayName("이동 경로로 이동할 수 없으면 예외가 발생한다")
+    void findIntermediatePositionsException() {
+        List<Movement> movementList = List.of(
+                new Movement(List.of(Direction.UP, Direction.RIGHT_UP, Direction.RIGHT)),
+                new Movement(List.of(Direction.LEFT, Direction.LEFT_DOWN, Direction.DOWN))
+        );
+        Movements movements = new Movements(movementList);
+
+        Position startPosition = Position.of(2, 2);
+        Position endPosition = Position.of(8, 8);
+
+        assertThatThrownBy(() -> movements.findIntermediatePositions(startPosition, endPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지정한 포지션으로 이동할 수 없습니다.");
+    }
+
 }
