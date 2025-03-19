fc0d07cdabf9f4b303cf4dfb66bd813e51136f81
ryan-dia <cjfdnjs158@naver.com>
2025-03-19 15:19:58 +0900

    test: 해당 좌표에 기물 유무 확인하는 기능 실패하는 테스트 작성
    Co-authored-by:saera-yook<yook.saera@gmail.com>

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -3,6 +3,7 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
@@ -27,5 +28,21 @@ class BoardTest {
 
         // then
         assertThat(pieceCount).isEqualTo(32);
+    }
+
+    @Test
+    void 해당_위치에_말_존재_유무_확인() {
+
+        // given
+        final Point truePoint = Point.of(0, 0);
+        final Point falsePoint = Point.of(5, 5);
+
+        final Board board = Board.initialize();
+
+        // when
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(board.hasPieceOn(truePoint)).isTrue();
+            softly.assertThat(board.hasPieceOn(falsePoint)).isFalse();
+        });
     }
 }
