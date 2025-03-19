527eadf6de59ef52bbc6f3b1de27b750a774a69e
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-19 14:03:09 +0900

    feat(green): 포의 이동 경로를 계산할 수 있다

--- src/main/java/domain/piece/Cannon.java
+++ src/main/java/domain/piece/Cannon.java
@@ -2,9 +2,10 @@ package domain.piece;
 
 import domain.Position;
 import domain.Team;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Cannon extends Piece{
+public class Cannon extends Piece {
 
     public Cannon(Team team) {
         super(team);
@@ -12,7 +13,37 @@ public class Cannon extends Piece{
 
     @Override
     public List<Move> calculatePath(Position startPosition, Position targetPosition) {
-        return List.of();
+        //1. 세로가 다르고 가로가 같다.
+        List<Move> moves = new ArrayList<>();
+        if (startPosition.compareRow(targetPosition) != 0 && startPosition.compareColumn(targetPosition) == 0) {
+            int count = startPosition.compareRow(targetPosition);
+            if (count < 0) {
+                for (int i = 0; i < Math.abs(count); i++) {
+                    moves.add(Move.BACK);
+                }
+            }
+            if (count > 0) {
+                for (int i = 0; i < count; i++) {
+                    moves.add(Move.FRONT);
+                }
+            }
+        }
+        //2. 세로가 같고 가로가 다르다
+        if (startPosition.compareRow(targetPosition) == 0 && startPosition.compareColumn(targetPosition) != 0) {
+            int count = startPosition.compareColumn(targetPosition);
+            if (count < 0) {
+                for (int i = 0; i < Math.abs(count); i++) {
+                    moves.add(Move.RIGHT);
+                }
+            }
+            if (count > 0) {
+                for (int i = 0; i < count; i++) {
+                    moves.add(Move.LEFT);
+                }
+            }
+        }
+        
+        return moves;
     }
 
     @Override
