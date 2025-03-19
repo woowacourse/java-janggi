352c8b660b9169b539d1d6d6a3b889e44f0cf29e
taek2222 <oot309@daum.net>
2025-03-19 17:37:18 +0900

    test: 반복인 경우 방향 올바르게 찾는 테스트 검증 추가

--- src/test/java/domain/direction/DirectionsTest.java
+++ src/test/java/domain/direction/DirectionsTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DirectionsTest {
 
     @Test
-    void 타겟_위치_방향을_찾아_이동_경로를_반환한다() {
+    void 반복이_아닌_타겟_위치_방향을_찾아_이동_경로를_반환한다() {
         // given
         Position start = Position.of(5, 5);
         Position target = Position.of(6, 3);
@@ -18,6 +18,24 @@ class DirectionsTest {
 
         List<Position> positions = List.of(Position.ofDirection(0, -1), Position.ofDirection(1, -1));
         List<Direction> directionElements = List.of(new Direction(positions, false));
+        Directions directions = new Directions(directionElements);
+
+        // when
+        List<Position> result = directions.getPath(start, target);
+
+        // then
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void 반복인_타겟_위치_방향을_찾아_이동_경로를_반환한다() {
+        // given
+        Position start = Position.of(5, 5);
+        Position target = Position.of(9, 5);
+        List<Position> expected = List.of(Position.of(6, 5), Position.of(7, 5), Position.of(8, 5));
+
+        List<Position> positions = List.of(Position.ofDirection(1, 0));
+        List<Direction> directionElements = List.of(new Direction(positions, true));
         Directions directions = new Directions(directionElements);
 
         // when
