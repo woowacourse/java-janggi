bc55529dc0d958189fa19789d4dc8a9a719ce203
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-19 16:46:02 +0900

    feat: 캐논 이동 가능 경로 판단 기능 구현

--- src/main/java/janggi/domain/piece/Cannon.java
+++ src/main/java/janggi/domain/piece/Cannon.java
@@ -2,6 +2,7 @@ package janggi.domain.piece;
 
 import janggi.domain.Position;
 import janggi.domain.Side;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cannon extends Piece {
@@ -20,6 +21,71 @@ public class Cannon extends Piece {
 
     @Override
     protected boolean isMoveablePath(List<Piece> existingPieces, Position destination) {
+
+        List<Piece> 이동하고자_하는_위치_내_기물들 = 이동하고자_하는_위치_내에_몇_가지_기물들(existingPieces, destination);
+
+        if (이동하고자_하는_위치_내_기물들.size() == 1 && 이동하고자_하는_위치_내_기물들.stream()
+                .noneMatch(piece -> piece.getClass().equals(this.getClass()))) {
+
+            if (!배열_내에_특정_포지션을_갖는_말이_있는지(existingPieces, destination)) {
+                return true;
+            }
+            Piece piece = 배열_내에_특정_포지션을_갖는_말(existingPieces, destination);
+
+            return piece.getSide() != getSide() && !piece.getClass().equals(this.getClass());
+        }
         return false;
+    }
+
+    private boolean 배열_내에_특정_포지션을_갖는_말이_있는지(List<Piece> existingPieces, Position position) {
+        return existingPieces.stream()
+                .anyMatch(existingPiece -> existingPiece.isSamePosition(position));
+    }
+
+
+    private Piece 배열_내에_특정_포지션을_갖는_말(List<Piece> existingPieces, Position position) {
+        return existingPieces.stream()
+                .filter(existingPiece -> existingPiece.isSamePosition(position))
+                .findAny()
+                .get();
+    }
+
+    private List<Piece> 이동하고자_하는_위치_내에_몇_가지_기물들(List<Piece> existingPieces, Position destination) {
+        List<Piece> result = new ArrayList<>();
+        int currentX = getXPosition();
+        int currentY = getYPosition();
+        int destinationX = destination.getX();
+        int destinationY = destination.getY();
+
+        if (getPosition().hasSameX(destination)) {
+            if (currentY < destinationY) {
+                for (int y = currentY + 1; y < destinationY; y++) {
+                    if (배열_내에_특정_포지션을_갖는_말이_있는지(existingPieces, new Position(currentX, y))) {
+                        result.add(배열_내에_특정_포지션을_갖는_말(existingPieces, new Position(currentX, y)));
+                    }
+                }
+                return result;
+            }
+            for (int y = destinationY + 1; y < currentY; y++) {
+                if (배열_내에_특정_포지션을_갖는_말이_있는지(existingPieces, new Position(currentX, y))) {
+                    result.add(배열_내에_특정_포지션을_갖는_말(existingPieces, new Position(currentX, y)));
+                }
+            }
+            return result;
+        }
+        if (currentX < destinationX) {
+            for (int x = currentX + 1; x < destinationX; x++) {
+                if (배열_내에_특정_포지션을_갖는_말이_있는지(existingPieces, new Position(x, currentY))) {
+                    result.add(배열_내에_특정_포지션을_갖는_말(existingPieces, new Position(x, currentY)));
+                }
+            }
+            return result;
+        }
+        for (int x = destinationX + 1; x < currentX; x++) {
+            if (배열_내에_특정_포지션을_갖는_말이_있는지(existingPieces, new Position(x, currentY))) {
+                result.add(배열_내에_특정_포지션을_갖는_말(existingPieces, new Position(x, currentY)));
+            }
+        }
+        return result;
     }
 }
--- src/test/java/janggi/domain/piece/CannonTest.java
+++ src/test/java/janggi/domain/piece/CannonTest.java
@@ -5,11 +5,313 @@ import static org.assertj.core.api.Assertions.assertThat;
 import janggi.domain.Position;
 import janggi.domain.ReplaceUnderBar;
 import janggi.domain.Side;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @ReplaceUnderBar
 class CannonTest {
+
+    private static Stream<Arguments> 이동하고자_하는_위치_내에_두_가지_이상의_기물이_있다면_이동할_수_없다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(5, 7)), new Rook(Side.HAN, new Position(5, 8))),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(5, 3)), new Rook(Side.HAN, new Position(5, 1))),
+                        new Position(5, 0)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(3, 5)), new Rook(Side.HAN, new Position(1, 5))),
+                        new Position(0, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(6, 5)), new Rook(Side.HAN, new Position(7, 5))),
+                        new Position(8, 5)
+                )
+        );
+    }
+
+    private static Stream<Arguments> 이동하고자_하는_위치_내에_기물이_없다면_이동할_수_없다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        new Position(7, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        new Position(2, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        new Position(5, 7)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        new Position(5, 3)
+                )
+        );
+    }
+
+    private static Stream<Arguments> 이동하고자_하는_위치_내에_Canon_이_아닌_한_가지_기물이_있다면_이동할_수_있다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.HAN, new Position(7, 5))),
+                        new Position(8, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(7, 5))),
+                        new Position(8, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.HAN, new Position(5, 7))),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(5, 7))),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.HAN, new Position(3, 5))),
+                        new Position(0, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(3, 5))),
+                        new Position(0, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.HAN, new Position(5, 3))),
+                        new Position(5, 1)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Rook(Side.CHO, new Position(5, 3))),
+                        new Position(5, 1)
+                )
+        );
+    }
+
+    private static Stream<Arguments> 이동하고자_하는_위치_내에_Canon_인_한_가지_기물이_있다면_이동할_수_없다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.HAN, new Position(7, 5))),
+                        new Position(8, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.CHO, new Position(7, 5))),
+                        new Position(8, 5)
+                ),
+
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.HAN, new Position(5, 7))),
+                        new Position(5, 8)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.CHO, new Position(5, 7))),
+                        new Position(5, 8)
+                ),
+
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.HAN, new Position(3, 5))),
+                        new Position(0, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.CHO, new Position(3, 5))),
+                        new Position(0, 5)
+                ),
+
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.HAN, new Position(5, 3))),
+                        new Position(5, 0)
+                ),
+                Arguments.of(
+                        new Cannon(Side.CHO, new Position(5, 5)),
+                        List.of(new Cannon(Side.CHO, new Position(5, 3))),
+                        new Position(5, 0)
+                )
+        );
+    }
+
+    private static Stream<Arguments> 이동하고자_하는_위치에_아군_기물이_있다면_이동할_수_없다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(6, 5)),
+                                new Rook(Side.HAN, new Position(8, 5))
+                        ),
+                        new Position(8, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(3, 5)),
+                                new Rook(Side.HAN, new Position(1, 5))
+                        ),
+                        new Position(1, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(5, 7)),
+                                new Rook(Side.HAN, new Position(5, 9))
+                        ),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(5, 7)),
+                                new Rook(Side.HAN, new Position(5, 9))
+                        ),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(5, 3)),
+                                new Rook(Side.HAN, new Position(5, 1))
+                        ),
+                        new Position(5, 1)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(5, 3)),
+                                new Rook(Side.HAN, new Position(5, 1))
+                        ),
+                        new Position(5, 1)
+                )
+        );
+    }
+
+    private static Stream<Arguments> 이동하고자_하는_위치에_Cannon_이_아닌_적_기물이_있다면_이동할_수_있다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(6, 5)),
+                                new Rook(Side.CHO, new Position(8, 5))
+                        ),
+                        new Position(8, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(3, 5)),
+                                new Rook(Side.CHO, new Position(1, 5))
+                        ),
+                        new Position(1, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(5, 7)),
+                                new Rook(Side.CHO, new Position(5, 9))
+                        ),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(5, 7)),
+                                new Rook(Side.CHO, new Position(5, 9))
+                        ),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(5, 3)),
+                                new Rook(Side.CHO, new Position(5, 1))
+                        ),
+                        new Position(5, 1)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(5, 3)),
+                                new Rook(Side.CHO, new Position(5, 1))
+                        ),
+                        new Position(5, 1)
+                )
+        );
+    }
+
+    private static Stream<Arguments> 이동하고자_하는_위치에_Cannon_인_적_기물이_있다면_이동할_수_없다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(6, 5)),
+                                new Cannon(Side.CHO, new Position(8, 5))
+                        ),
+                        new Position(8, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(3, 5)),
+                                new Cannon(Side.CHO, new Position(1, 5))
+                        ),
+                        new Position(1, 5)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(5, 7)),
+                                new Cannon(Side.CHO, new Position(5, 9))
+                        ),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(5, 7)),
+                                new Cannon(Side.CHO, new Position(5, 9))
+                        ),
+                        new Position(5, 9)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.HAN, new Position(5, 3)),
+                                new Cannon(Side.CHO, new Position(5, 1))
+                        ),
+                        new Position(5, 1)
+                ),
+                Arguments.of(
+                        new Cannon(Side.HAN, new Position(5, 5)),
+                        List.of(
+                                new Rook(Side.CHO, new Position(5, 3)),
+                                new Cannon(Side.CHO, new Position(5, 1))
+                        ),
+                        new Position(5, 1)
+                )
+        );
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"1, 2, 3, 4", "1, 2, 4, 5"})
@@ -25,5 +327,67 @@ class CannonTest {
         Cannon cannon = new Cannon(Side.CHO, new Position(x, y));
 
         assertThat(cannon.isMoveablePosition(new Position(moveX, moveY))).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("이동하고자_하는_위치_내에_두_가지_이상의_기물이_있다면_이동할_수_없다_테스트_케이스")
+    void 이동하고자_하는_위치_내에_두_가지_이상의_기물이_있다면_이동할_수_없다(Cannon cannon, List<Piece> existingPieces, Position destination) {
+        assertThat(cannon.isMoveablePath(existingPieces, destination)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("이동하고자_하는_위치_내에_기물이_없다면_이동할_수_없다_테스트_케이스")
+    void 이동하고자_하는_위치_내에_기물이_없다면_이동할_수_없다(Cannon cannon, Position destination) {
+        assertThat(cannon.isMoveablePath(List.of(), destination)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("이동하고자_하는_위치_내에_Canon_이_아닌_한_가지_기물이_있다면_이동할_수_있다_테스트_케이스")
+    void 이동하고자_하는_위치_내에_Canon_이_아닌_한_가지_기물이_있다면_이동할_수_있다(
+            Cannon cannon,
+            List<Piece> existingPieces,
+            Position destination
+    ) {
+        assertThat(cannon.isMoveablePath(existingPieces, destination)).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("이동하고자_하는_위치_내에_Canon_인_한_가지_기물이_있다면_이동할_수_없다_테스트_케이스")
+    void 이동하고자_하는_위치_내에_Canon_인_한_가지_기물이_있다면_이동할_수_없다(
+            Cannon cannon,
+            List<Piece> existingPieces,
+            Position destination
+    ) {
+        assertThat(cannon.isMoveablePath(existingPieces, destination)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("이동하고자_하는_위치에_아군_기물이_있다면_이동할_수_없다_테스트_케이스")
+    void 이동하고자_하는_위치에_아군_기물이_있다면_이동할_수_없다(
+            Cannon cannon,
+            List<Piece> existingPieces,
+            Position destination
+    ) {
+        assertThat(cannon.isMoveablePath(existingPieces, destination)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("이동하고자_하는_위치에_Cannon_이_아닌_적_기물이_있다면_이동할_수_있다_테스트_케이스")
+    void 이동하고자_하는_위치에_Cannon_이_아닌_적_기물이_있다면_이동할_수_있다(
+            Cannon cannon,
+            List<Piece> existingPieces,
+            Position destination
+    ) {
+        assertThat(cannon.isMoveablePath(existingPieces, destination)).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("이동하고자_하는_위치에_Cannon_인_적_기물이_있다면_이동할_수_없다_테스트_케이스")
+    void 이동하고자_하는_위치에_Cannon_인_적_기물이_있다면_이동할_수_없다(
+            Cannon cannon,
+            List<Piece> existingPieces,
+            Position destination
+    ) {
+        assertThat(cannon.isMoveablePath(existingPieces, destination)).isFalse();
     }
 }
