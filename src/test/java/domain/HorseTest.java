ae798b82cd9f6521ed9392caab64cb90cd226fb8
ryan-dia <cjfdnjs158@naver.com>
2025-03-19 21:19:08 +0900

    test: 말의 이동 가능 경로 반환 기능 실패 테스트 작성
    Co-authored-by:saera-yook<yook.saera@gmail.com>

--- src/test/java/domain/HorseTest.java
+++ src/test/java/domain/HorseTest.java
@@ -2,6 +2,9 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -31,5 +34,21 @@ class HorseTest {
 
         // then
         assertThat(horse.isMovable(distance)).isEqualTo(expected);
+    }
+
+    @Test
+    void 말의_이동_가능_경로_모두_반환() {
+
+        // given
+        Horse horse = PieceFactory.createGreenTeam(Horse::new);
+
+        // when
+        List<Point> possiblePoint = horse.getPossiblePoint(Point.of(2, 0), Point.of(3, 2));
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(possiblePoint.size()).isEqualTo(1);
+            softly.assertThat(possiblePoint.getFirst()).isEqualTo(Point.of(2, 1));
+        });
     }
 }
