df1d8640203417033b3b7988bff0481c21f204f9
ryan-dia <cjfdnjs158@naver.com>
2025-03-19 17:39:05 +0900

    feat: 그린팀 확인 기능 추가
    Co-authored-by:saera-yook<yook.saera@gmail.com>

--- src/main/java/domain/AbstractPiece.java
+++ src/main/java/domain/AbstractPiece.java
@@ -7,4 +7,8 @@ public abstract class AbstractPiece implements Piece {
     public AbstractPiece(final Team team) {
         this.team = team;
     }
+
+    public boolean isGreenTeam() {
+        return Team.GREEN == team;
+    }
 }
