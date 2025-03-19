364cc847d35b263f5819401c17bcdc1bc7347719
soeun2537 <soeun2537@gmail.com>
2025-03-19 19:54:15 +0900

    feat: 사 객체 구현

---
+++ src/main/java/domain/piece/Sa.java
@@ -0,0 +1,33 @@
+package domain.piece;
+
+import domain.Board;
+import domain.Coordinate;
+import domain.Team;
+import java.util.Set;
+
+public class Sa extends Piece {
+
+    public Sa(Team team) {
+        super(team);
+    }
+
+    @Override
+    protected Set<Coordinate> findMovableCandidates(Coordinate departure) {
+        return Set.of(
+                departure.change(1, 0),
+                departure.change(-1, 0),
+                departure.change(0, 1),
+                departure.change(0, -1)
+        );
+    }
+
+    @Override
+    protected boolean canMoveConsideringObstacles(Board board, Coordinate departure, Coordinate arrival) {
+        return true;
+    }
+
+    @Override
+    protected Set<Coordinate> findPaths(Coordinate departure, Coordinate arrival) {
+        return Set.of();
+    }
+}
