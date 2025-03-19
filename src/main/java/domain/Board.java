bdbf32951cda42498ac947ffa1b7fca1c10f5fe5
taek2222 <oot309@daum.net>
2025-03-19 22:41:52 +0900

    feat: 움직이는 기능 추가 기능 개발 단계 정의

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -1,7 +1,10 @@
 package domain;
 
+import domain.piece.Piece;
 import domain.piece.Pieces;
+import domain.piece.Position;
 
+import java.util.List;
 import java.util.Map;
 
 public class Board {
@@ -10,6 +13,44 @@ public class Board {
 
     public Board(final Map<Player, Pieces> board) {
         this.board = board;
+    }
+
+    private void move(Player player, Position startPosition, Position targetPosition) {
+        // 해당 위치의 타겟 가져오기
+        Piece piece = board.get(player).findByPosition(startPosition);
+
+        // 지나가는 경로 좌표들 가져오기
+        List<Position> path = piece.getPath(targetPosition);
+
+        /* 검사 했을 때 이동할 수 없는 조건일 경우
+        // 포 : 중간 기물 개수가 1개가 아닐 경우 (0개 또는 1개 초과)
+        나머지 : 중간 기물이 1개 이상일 경우
+         */
+
+        if (!canMove(path)) {
+            throw new IllegalArgumentException();
+        }
+
+        // 우리팀 도착지 검증
+//        if (board.get(player).findByPosition(targetPosition) != null) {
+//
+//        }
+//
+        //타겟 위치로 기물 이동시키기
+//        movePiece(piece, targetPosition);
+//
+//        // 해당 타겟 위치에 상대편 기물이 존재하면
+//        if (existsOppositePiece(piece)) {
+//            // 해당 위치의 상대 기물 잡기
+//            catchPiece(targetPosition);
+//        }
+    }
+
+    private boolean canMove(final List<Position> path) {
+        int containedPiecesCount = board.values().stream()
+                .mapToInt(pieces -> pieces.countPiecesInPositions(path))
+                .sum();
+        return containedPiecesCount > 0;
     }
 
     public Map<Player, Pieces> getBoard() {
