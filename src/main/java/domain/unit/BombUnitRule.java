cbe9296ac566db1cd01b41ee725feb1580f6c7ae
ppparkta <pppppark2@gmail.com>
2025-03-19 20:31:00 +0900

    feat: "포"의 이동 가능한 모든 경로 구현

--- src/main/java/domain/unit/BombUnitRule.java
+++ src/main/java/domain/unit/BombUnitRule.java
@@ -1,12 +1,62 @@
 package domain.unit;
 
 import domain.Route;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class BombUnitRule implements UnitRule {
     @Override
     public List<Route> calculateAllRoute(Point start) {
-        return List.of();
+        List<Route> routes = new ArrayList<>();
+        List<Point> points = calculateEndPoints(start);
+        for (Point end : points) {
+            Route route = calculateRoute(start, end);
+            if (route.getPoints().size() == 1) {
+                continue;
+            }
+            routes.add(route);
+        }
+        return routes;
+    }
+
+    public List<Point> calculateEndPoints(Point start) {
+        int x = start.getX();
+        int y = start.getY();
+        List<Point> xPoints = IntStream.range(0, Point.X_MAX + 1)
+                .filter(element -> element != x)
+                .mapToObj(element -> new Point(element, y))
+                .toList();
+        List<Point> yPoints = IntStream.range(0, Point.Y_MAX + 1)
+                .filter(element -> element != y)
+                .mapToObj(element -> new Point(x, element))
+                .toList();
+        return Stream.concat(xPoints.stream(), yPoints.stream())
+                .toList();
+    }
+
+    public Route calculateRoute(Point start, Point end) {
+        int startX = start.getX();
+        int startY = start.getY();
+
+        int endX = end.getX();
+        int endY = end.getY();
+
+        if (startX == endX) {
+            int maxY = Integer.max(startY, endY);
+            int minY = Integer.min(startY, endY);
+            return Route.of(IntStream.range(minY, maxY + 1)
+                    .filter(y -> startY != y)
+                    .mapToObj(y -> new Point(startX, y))
+                    .toList());
+        }
+        int maxX = Integer.max(startX, endX);
+        int minX = Integer.min(startX, endX);
+        return Route.of(IntStream.range(minX, maxX + 1)
+                .filter(x -> startX != x)
+                .mapToObj(x -> new Point(x, startY))
+                .toList());
     }
 
     @Override
