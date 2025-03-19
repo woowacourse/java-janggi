8162682849b6aa56207133834e6121abcf5ea94c
Yehyeok Bang <qkddpgur318@gmail.com>
2025-03-19 20:57:53 +0900

    feat: 마 고유 기능 구현
    - 마는 다른 기물을 넘어 이동할 수 없습니다.
    
    Co-authored-by: YeonhuLee <yeonnhuu@gmail.com>
    Co-authored-by: eunseongu <rndmstjs75@gmail.com>

--- src/main/java/janggi/piece/Horse.java
+++ src/main/java/janggi/piece/Horse.java
@@ -4,6 +4,7 @@ import janggi.Camp;
 import janggi.PieceSymbol;
 import janggi.Point;
 import janggi.board.Board;
+import java.util.Set;
 
 public final class Horse extends Piece {
 
@@ -17,6 +18,28 @@ public final class Horse extends Piece {
                 (fromPoint.calculateXDistance(toPoint) == 1 && fromPoint.calculateYDistance(toPoint) == 2))) {
             throw new IllegalArgumentException("마는 직선으로 한 칸, 대각선으로 한 칸 움직여야 합니다.");
         }
+
+        Set<Piece> pieces = getBoard().getPiecesByPoint(Set.of(findRoute(fromPoint, toPoint)));
+        if (!pieces.isEmpty()) {
+            throw new IllegalArgumentException("마는 기물을 넘어서 이동할 수 없습니다.");
+        }
+    }
+
+    private Point findRoute(Point fromPoint, Point toPoint) {
+        if (isHorizontal(fromPoint, toPoint)) {
+            if (fromPoint.getX() < toPoint.getX()) {
+                return new Point(fromPoint.getX() + 1, fromPoint.getY());
+            }
+            return new Point(fromPoint.getX() - 1, fromPoint.getY());
+        }
+        if (fromPoint.getY() < toPoint.getY()) {
+            return new Point(fromPoint.getX(), fromPoint.getY() + 1);
+        }
+        return new Point(fromPoint.getX(), fromPoint.getY() - 1);
+    }
+
+    private boolean isHorizontal(Point fromPoint, Point toPoint) {
+        return fromPoint.calculateXDistance(toPoint) == 2;
     }
 
     @Override
--- src/test/java/janggi/piece/HorseTest.java
+++ src/test/java/janggi/piece/HorseTest.java
@@ -57,4 +57,29 @@ class HorseTest {
         assertThatCode(() -> horse.validateMove(fromPoint, toPoint))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("마는 직선으로 한 칸, 대각선으로 한 칸 움직일 때 기물에 막힌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "HAN,6,7",
+            "HAN,7,4",
+            "HAN,6,3",
+            "HAN,3,4",
+    })
+    void shouldThrowException_WhenBlocked(Camp camp, int toX, int toY) {
+        // given
+        Board board = new Board();
+        Horse horse = new Horse(camp, board);
+        Point fromPoint = new Point(5, 5);
+        Point toPoint = new Point(toX, toY);
+        board.placePiece(new Point(5, 6), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(6, 5), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(4, 5), new Soldier(Camp.CHU, board));
+        board.placePiece(new Point(5, 4), new Soldier(Camp.CHU, board));
+
+        // when & then
+        assertThatCode(() -> horse.validateMove(fromPoint, toPoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("마는 기물을 넘어서 이동할 수 없습니다.");
+    }
 }
