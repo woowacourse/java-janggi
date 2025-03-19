a6253893d8f967a627358939daebb4d9b5468c78
changuii <rhljh7410@gmail.com>
2025-03-19 11:10:08 +0900

    refactor: Piece에서 Position을 제거하라

--- src/main/java/piece/Piece.java
+++ src/main/java/piece/Piece.java
@@ -5,21 +5,15 @@ import board.Position;
 public class Piece {
 
     private final PieceType pieceType;
-    private final Position position;
     private final TeamType teamType;
 
-    public Piece(final PieceType pieceType, final Position position, final TeamType teamType) {
+    public Piece(final PieceType pieceType, final TeamType teamType) {
         this.pieceType = pieceType;
-        this.position = position;
         this.teamType = teamType;
     }
 
     public PieceType getPieceType() {
         return pieceType;
-    }
-
-    public Position getPosition() {
-        return position;
     }
 
     public TeamType getTeamType() {
--- src/test/java/piece/PieceTest.java
+++ src/test/java/piece/PieceTest.java
@@ -2,7 +2,6 @@ package piece;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import board.Position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -20,16 +19,14 @@ public class PieceTest {
         void construct1() {
             // given
             final PieceType pieceType = PieceType.CANNON;
-            final Position position = new Position(1, 1);
             final TeamType teamType = TeamType.RED;
 
             // when
-            final Piece piece = new Piece(pieceType, position, teamType);
+            final Piece piece = new Piece(pieceType, teamType);
 
             // then
             assertAll(
                     () -> Assertions.assertThat(piece.getPieceType()).isEqualTo(pieceType),
-                    () -> Assertions.assertThat(piece.getPosition()).isEqualTo(position),
                     () -> Assertions.assertThat(piece.getTeamType()).isEqualTo(teamType)
             );
         }
