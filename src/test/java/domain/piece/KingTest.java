64fc4c210cb3a36d3af732095cbeae93c1b7e4d4
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-19 15:08:13 +0900

    test: King 테스트 변경
    - 상하좌우 이동 테스트

--- src/test/java/domain/piece/KingTest.java
+++ src/test/java/domain/piece/KingTest.java
@@ -6,21 +6,31 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class KingTest {
 
-    @DisplayName("킹은 오른쪽으로 이동할 수 있다.")
-    @Test
-    void test() {
+    @DisplayName("사는 상하좌우로 이동할 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+            "5,4,BACK",
+            "3,4,FRONT",
+            "4,3,LEFT",
+            "4,5,RIGHT"
+    })
+    void test(int row, int column, Move move) {
 
         //given
-        King king = new King(Team.RED);
+        King king = new King(Team.BLUE);
+        Position startPosition = new Position(4, 4);
+        Position targetPosition = new Position(row, column);
 
         //when
-        List<Move> move = king.calculatePath(new Position(4,1),new Position(4,2));
+        List<Move> resultMove = king.calculatePath(startPosition, targetPosition);
 
         // then
-        Assertions.assertThat(move).isEqualTo(List.of(Move.RIGHT));
+        Assertions.assertThat(resultMove).isEqualTo(List.of(move));
     }
 
     @DisplayName("킹이 이동할 수 없는 위치라면 예외가 발생한다.")
@@ -30,9 +40,8 @@ public class KingTest {
         //given
         King king = new King(Team.RED);
 
-
         // when & then
-        Assertions.assertThatThrownBy(() -> king.calculatePath(new Position(4,1),new Position(4,3)))
+        Assertions.assertThatThrownBy(() -> king.calculatePath(new Position(4, 1), new Position(4, 3)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이 위치로 이동할 수 없습니다.");
     }
