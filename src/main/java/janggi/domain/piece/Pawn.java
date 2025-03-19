2f591a34146e419e8e6c5bc17f14582515c84489
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-19 22:18:02 +0900

    refactor: 폰 이동 로직 함수 분리

--- src/main/java/janggi/domain/piece/Pawn.java
+++ src/main/java/janggi/domain/piece/Pawn.java
@@ -2,7 +2,6 @@ package janggi.domain.piece;
 
 import janggi.domain.Position;
 import janggi.domain.Side;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,36 +14,29 @@ public class Pawn extends Piece {
 
     @Override
     protected boolean isMoveablePosition(Position destination) {
-        Set<Position> moveablePositions = new HashSet<>();
-        moveablePositions.add(new Position(getXPosition() - 1, getYPosition()));
-        moveablePositions.add(new Position(getXPosition() + 1, getYPosition()));
-
-        if (getSide() == Side.HAN) {
-            moveablePositions.add(new Position(getXPosition(), getYPosition() + 1));
-            return moveablePositions.contains(destination);
-        }
-        moveablePositions.add(new Position(getXPosition(), getYPosition() - 1));
-        return moveablePositions.contains(destination);
+        return getMovablePositions().contains(destination);
     }
 
     @Override
     protected boolean isMoveablePath(List<Piece> existingPieces, Position destination) {
-        Set<Position> moveablePositions = new HashSet<>();
-        if (getSide() == Side.HAN) {
-            moveablePositions.add(new Position(getXPosition() - 1, getYPosition()));
-            moveablePositions.add(new Position(getXPosition() + 1, getYPosition()));
-            moveablePositions.add(new Position(getXPosition(), getYPosition() + 1));
-
-            return existingPieces.stream()
-                .anyMatch(piece -> moveablePositions.contains(piece.getPosition())
-                    && piece.getSide() != getSide());
-        }
-        moveablePositions.add(new Position(getXPosition() - 1, getYPosition()));
-        moveablePositions.add(new Position(getXPosition() + 1, getYPosition()));
-        moveablePositions.add(new Position(getXPosition(), getYPosition() - 1));
+        Set<Position> moveablePositions = getMovablePositions();
 
         return existingPieces.stream()
-            .anyMatch(piece -> moveablePositions.contains(piece.getPosition())
-            && piece.getSide() != getSide());
+                .filter(existingPiece -> existingPiece.isSamePosition(destination))
+                .filter(existingPiece -> moveablePositions.contains(existingPiece.getPosition()))
+                .anyMatch(existingPiece -> existingPiece.getSide() != getSide());
+    }
+
+    private Set<Position> getMovablePositions() {
+        Set<Position> moveablePositions = new HashSet<>();
+        moveablePositions.add(new Position(getXPosition() - 1, getYPosition()));
+        moveablePositions.add(new Position(getXPosition() + 1, getYPosition()));
+
+        if (getSide() == Side.HAN) {
+            moveablePositions.add(new Position(getXPosition(), getYPosition() + 1));
+            return moveablePositions;
+        }
+        moveablePositions.add(new Position(getXPosition(), getYPosition() - 1));
+        return moveablePositions;
     }
 }
