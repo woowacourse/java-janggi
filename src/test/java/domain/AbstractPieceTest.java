4b53830ffb6bf6675116c13479c61d5f7e183ce3
ryan-dia <cjfdnjs158@naver.com>
2025-03-19 17:38:09 +0900

    test: 그린팀 확인 기능 실패하는 테스트 작성
    Co-authored-by:saera-yook<yook.saera@gmail.com>

---
+++ src/test/java/domain/AbstractPieceTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+class AbstractPieceTest {
+
+    class FakePiece extends AbstractPiece {
+
+        public FakePiece(final Team team) {
+            super(team);
+        }
+
+        @Override
+        public boolean isMovable(final Distance distance) {
+            return false;
+        }
+    }
+
+    @Test
+    void 그린팀이면_true_아니면_false_반환() {
+
+        // given
+        final FakePiece greenTeam = PieceFactory.createGreenTeam(FakePiece::new);
+        final FakePiece redTeam = PieceFactory.createRedTeam(FakePiece::new);
+
+        // when
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(greenTeam.isGreenTeam()).isTrue();
+            softly.assertThat(redTeam.isGreenTeam()).isFalse();
+        });
+    }
+}
