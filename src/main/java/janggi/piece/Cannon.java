c9aa118fe1605808cc4ab9fae2072f9041b03698
sung-june27 <kmymy27@gmail.com>
2025-03-19 16:55:27 +0900

    feat(Cannon): Cannon의 이동 경로 반환 구현

--- src/main/java/janggi/piece/Cannon.java
+++ src/main/java/janggi/piece/Cannon.java
@@ -1,10 +1,53 @@
 package janggi.piece;
 
+import janggi.Position;
 import janggi.Side;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Cannon extends Piece {
 
     public Cannon(Side side) {
         super(side);
+    }
+
+    @Override
+    public List<Position> calculatePath(Position start, Position end) {
+        int differenceX = end.x() - start.x();
+        int differenceY = end.y() - start.y();
+        validateMovingRule(differenceX, differenceY);
+        return findPath(start, differenceX, differenceY);
+    }
+
+    private List<Position> findPath(Position start, int differenceX, int differenceY) {
+        int differenceXAmount = Math.abs(differenceX);
+        if (differenceX > 0) {
+            return IntStream.range(1, differenceXAmount)
+                    .mapToObj(start::right)
+                    .toList();
+        }
+        if (differenceX < 0) {
+            return IntStream.range(1, differenceXAmount)
+                    .mapToObj(start::left)
+                    .toList();
+        }
+
+        int differenceYAmount = Math.abs(differenceY);
+        if (differenceY > 0) {
+            return IntStream.range(1, differenceYAmount)
+                    .mapToObj(start::up)
+                    .toList();
+        }
+        return IntStream.range(1, differenceYAmount)
+                .mapToObj(start::down)
+                .toList();
+    }
+
+    private void validateMovingRule(int differenceX, int differenceY) {
+        if ((differenceX == 0 && differenceY != 0)
+                || (differenceX != 0 && differenceY == 0)) {
+            return;
+        }
+        throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
     }
 }
