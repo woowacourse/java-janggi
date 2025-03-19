adce4009b7d1c6ef9748303c8ef1e82ebfb219fe
minSsan <msp1125@hanyang.ac.kr>
2025-03-19 11:25:35 +0900

    🛠️refactor: 위치 검증 로직 리팩터링

--- src/main/java/domain/ChessPosition.java
+++ src/main/java/domain/ChessPosition.java
@@ -4,9 +4,18 @@ public record ChessPosition(
         int row,
         int column
 ) {
+    private static final int MIN_ROW = 0;
+    private static final int MAX_ROW = 9;
+    private static final int MIN_COL = 0;
+    private static final int MAX_COL = 8;
+
     public ChessPosition {
-        if (row < 0 || row > 9 || column < 0 || column > 8) {
+        if (!isValid(row, column)) {
             throw new IllegalArgumentException("위치는 (0, 0) ~ (9, 8) 값만 가능합니다.");
         }
+    }
+
+    private static boolean isValid(final int row, final int col) {
+        return row >= MIN_ROW && row <= MAX_ROW && col >= MIN_COL && col <= MAX_COL;
     }
 }
