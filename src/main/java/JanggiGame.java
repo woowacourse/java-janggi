9b2460b2909fb1624d49b6091cbfc907e78bfcda
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-03-19 22:37:30 +0900

    fix: 한나라, 초나라 기물 배치 오류 수정
    - 재할당 로직 제거후 putAll로 대체

--- src/main/java/JanggiGame.java
+++ src/main/java/JanggiGame.java
@@ -5,14 +5,21 @@ public class JanggiGame {
     private Map<Dot, Piece> pieces = new HashMap<>();
 
     public void arrangeHanPieces(ArrangementStrategy strategy) {
-        Map<Dot, Piece> tempPieces = strategy.arrange(Dynasty.HAN);
+        Map<Dot, Piece> pieces = strategy.arrange(Dynasty.HAN);
+        Map<Dot, Piece> reversePieces = new HashMap<>();
         pieces.keySet()
-                .forEach(dot -> {
-                    pieces.put(dot.reverse(), tempPieces.get(dot));
-                });
+                .forEach(dot -> reversePieces.put(dot.reverse(), pieces.get(dot))
+                );
+
+        this.pieces.putAll(reversePieces);
+
     }
 
     public void arrangeChoPieces(ArrangementStrategy strategy) {
-        pieces = strategy.arrange(Dynasty.CHO);
+        pieces.putAll(strategy.arrange(Dynasty.CHO));
+    }
+
+    public Map<Dot, Piece> getPieces() {
+        return pieces; //todo 방어적 복사로 바꾸기
     }
 }
