f1fee7c3bc928e15d1653d2924a93e365860fbc4
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-03-19 19:24:25 +0900

    refactor : 보드 범위 상수 선언 위치 수정

---
+++ src/main/java/domain/BoardLocation.java
@@ -0,0 +1,56 @@
+package domain;
+
+import java.util.Objects;
+
+public class BoardLocation {
+    private static  final int START_X = 1;
+    private static  final int START_Y = 1;
+    private static  final int END_X = 9;
+    private static  final int END_Y = 10;
+
+    private final int x;
+    private final int y;
+
+    public BoardLocation(int x, int y) {
+        validateXRange(x);
+        validateYRange(y);
+        this.x = x;
+        this.y = y;
+    }
+
+    private void validateYRange(int y) {
+        if (y < START_Y || y > END_Y) {
+            throw new IllegalArgumentException("[ERROR] 보드판 y값이 범위 밖에 위치합니다. y :" + y);
+        }
+    }
+
+    private void validateXRange(int x) {
+        if (x < START_X || x > END_X) {
+            throw new IllegalArgumentException("[ERROR] 보드판 x값이 범위 밖에 위치합니다. x :" + x);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof BoardLocation boardLocation)) {
+            return false;
+        }
+        return x == boardLocation.x && y == boardLocation.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public int minusX(BoardLocation target) {
+        return Math.abs(x - target.x);
+    }
+
+    public int minusY(BoardLocation target) {
+        return Math.abs(y - target.y);
+    }
+}
--- src/test/java/domain/BoardLocationTest.java
+++ src/test/java/domain/BoardLocationTest.java
@@ -1,12 +1,13 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class BoardRangeTest {
+class BoardLocationTest {
 
     @DisplayName("보드의 범위는 가로9, 세로 10 범위 밖에있는지 검증한다")
     @ParameterizedTest
@@ -17,13 +18,10 @@ public class BoardRangeTest {
             "10,9",
             "10,10"
     })
-    void test() {
-        // given
-        Location location = new Location(0, 0);
-
-        // when & then
+    void test(int x, int y) {
+        // given when & then
         assertThatThrownBy(() ->{
-            BoardRange.validateBoardRange(location);
+            new BoardLocation(x, y);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
