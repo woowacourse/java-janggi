799ca72ef2e533198e0732e11daa2a867e36ec57
taek2222 <oot309@daum.net>
2025-03-19 16:10:37 +0900

    test: 이동 경로 반환 테스트 작성

--- src/test/java/domain/direction/DirectionTest.java
+++ src/test/java/domain/direction/DirectionTest.java
@@ -1,31 +1,32 @@
 package domain.direction;
 
 import domain.piece.Position;
-import java.util.List;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class DirectionTest {
 
-    @Disabled
     @Test
     void 이동_경로를_반환한다() {
         // given
-        // 마의 이동
-//        Position start = Position.ofDirection(5, 5);
-//        Position target = Position.ofDirection((6, 3);
-//
-//        List<Position> positions = List.of(Position.ofDirection((0, -1), Position.ofDirection((1, -1));
-////        Direction direction = new Direction(positions);
+        Position start = Position.of(5, 5);
+        Position target = Position.of(6, 3);
+        List<Position> expected = List.of(Position.of(5, 4));
+
+        List<Position> positions = List.of(Position.ofDirection(0, -1), Position.ofDirection(1, -1));
+        Direction direction = new Direction(positions);
 
         // when
-//        direction.
+        List<Position> result = direction.createPath(start, target);
 
         // then
-
+        assertThat(result)
+                .containsAll(expected);
     }
 
     @ParameterizedTest
@@ -35,8 +36,8 @@ class DirectionTest {
     })
     void 해당_위치로_도달할_수_있는지_판단한다(int row, int column, boolean expectedResult) {
         // given
-        Position start = Position.ofDirection(5, 5);
-        Position target = Position.ofDirection(row, column);
+        Position start = Position.of(5, 5);
+        Position target = Position.of(row, column);
 
         List<Position> positions = List.of(Position.ofDirection(0, -1), Position.ofDirection(1, -1));
         Direction direction = new Direction(positions);
@@ -45,6 +46,6 @@ class DirectionTest {
         boolean result = direction.canReach(start, target);
 
         // then
-        Assertions.assertThat(result).isEqualTo(expectedResult);
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
