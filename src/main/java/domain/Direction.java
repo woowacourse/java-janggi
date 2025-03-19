72068ac9cb6d854903a7cd4264e9a4f41d18d16e
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-19 21:19:25 +0900

    feat: 방향 enum 정의

---
+++ src/main/java/domain/Direction.java
@@ -0,0 +1,21 @@
+package domain;
+
+public enum Direction {
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    LEFT_UP(-1, -1),
+    RIGHT_UP(-1, 1),
+    LEFT_DOWN(1, -1),
+    RIGHT_DOWN(1, 1),
+    ;
+
+    public final int dr;
+    public final int dc;
+
+    Direction(int dr, int dc) {
+        this.dr = dr;
+        this.dc = dc;
+    }
+}
