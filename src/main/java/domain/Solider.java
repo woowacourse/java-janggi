6f844237c7b68e644c4c9f825a649762a689b9bc
ykmxxi <ykmxxi97@gmail.com>
2025-03-19 17:31:17 +0900

    feat: 졸의 움직임 로직 구현

---
+++ src/main/java/domain/Solider.java
@@ -0,0 +1,32 @@
+package domain;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Solider extends Piece {
+
+    public Solider(final Position position, final Team team, final Board board) {
+        super(position, team, board);
+    }
+
+    @Override
+    protected Set<Position> getMovablePositions() {
+        return Direction.getStraightDirection().stream()
+                .filter(direction -> getUnmovableDirection() != direction)
+                .map(direction -> position.nextPosition(direction))
+                .filter(this::isMovable)
+                .collect(Collectors.toSet());
+    }
+
+    private Direction getUnmovableDirection() {
+        if (team == Team.BLUE) {
+            return Direction.BOTTOM;
+        }
+        return Direction.TOP;
+    }
+
+    private boolean isMovable(final Position position) {
+        return !board.isExists(position) || !board.isSameTeam(this, position);
+    }
+
+}
---
+++ src/test/java/domain/SoliderTest.java
@@ -0,0 +1,22 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class SoliderTest {
+
+    private final Position initPosition = new Position(4, 3);
+
+    @Test
+    void 졸의_이동할_수_있는_위치를_계산한다() {
+        Board board = new Board(List.of());
+        Piece piece = new Solider(initPosition, Team.RED, board);
+        board.putPiece(new Solider(new Position(4,4), Team.BLUE, board)); // 1
+        board.putPiece(new Solider(new Position(3,3), Team.RED, board));
+
+        assertThat(piece.getMovablePositions()).hasSize(2);
+    }
+}
