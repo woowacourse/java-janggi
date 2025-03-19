638a7cbeae9524c38d0709040ad7368957b66164
BWY <quantumjump10@gmail.com>
2025-03-19 16:48:11 +0900

    feat: 차 이동 여부 판단 기능 추가

---
+++ src/main/java/janggi/piece/Chariot.java
@@ -0,0 +1,76 @@
+package janggi.piece;
+
+import janggi.board.Position;
+
+import java.util.Map;
+
+public class Chariot extends Piece {
+
+    protected static final String NAME = "차";
+    private static final int[][] dRows = {
+            //상
+            {1}, {1, 1}, {1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1, 1},
+            //하
+            {-1}, {-1, -1}, {-1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1},
+            {-1, -1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+            //좌
+            {0}, {0, 0}, {0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //우
+            {0}, {0, 0}, {0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    private static final int[][] dColumns = {
+            //상
+            {0}, {0, 0}, {0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //하
+            {0}, {0, 0}, {0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //좌
+            {-1}, {-1, -1}, {-1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1},
+            {-1, -1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+            //우
+            {1}, {1, 1}, {1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+    public Chariot(String team) {
+        super(team);
+    }
+
+    @Override
+    protected void validatePath(Map<Position, Piece> board, Position start, int pathIndex) {
+        validateNonPieceOnPath(board, start, pathIndex);
+    }
+
+    @Override
+    protected int[] getPathRows(int pathIndex) {
+        return dRows[pathIndex];
+    }
+
+    @Override
+    protected int[] getPathColumns(int pathIndex) {
+        return dColumns[pathIndex];
+    }
+
+    @Override
+    protected int[][] getAllPathRows() {
+        return dRows;
+    }
+
+    @Override
+    protected int[][] getAllPathColumns() {
+        return dColumns;
+    }
+
+    @Override
+    protected String getName() {
+        return NAME;
+    }
+
+    @Override
+    protected boolean isSameType(Piece other) {
+        return other instanceof Chariot;
+    }
+}
