79997473d46008770769e77408913eba9845dde7
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-19 11:59:42 +0900

    RED) test(model): 교차점 테스트

---
+++ src/test/java/model/DotTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DotTest {
+    @Test
+    @DisplayName("교차점 위에 기물이 존재하는지 여부 반환")
+    void test1() {
+        Dot dot = new Dot();
+        Piece jang = new Jang(Team.RED);
+        dot.place(jang);
+
+        assertThat(dot.isPlaced()).isTrue();
+    }
+}
