7b1d2fcac877f94ea8b6839f1e7c662fa5e64b8c
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-19 21:15:39 +0900

    GREEN) feat(model): 차 경로 방해 여부 구현

--- src/main/java/model/Cha.java
+++ src/main/java/model/Cha.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Map;
+
 public class Cha extends Piece {
 
     public Cha(Team team) {
@@ -7,35 +9,45 @@ public class Cha extends Piece {
     }
 
     @Override
-    public boolean isValidPoint(Point beforePoint, Point afterPoint) {
-        return !((beforePoint.x() == afterPoint.x() && beforePoint.y() == afterPoint.y()) || (beforePoint.x() != afterPoint.x() && beforePoint.y() != afterPoint.y()));
+    public boolean isValidPoint(Point beforePoint, Point targetPoint) {
+        return !((beforePoint.x() == targetPoint.x() && beforePoint.y() == targetPoint.y()) || (
+                beforePoint.x() != targetPoint.x() && beforePoint.y() != targetPoint.y()));
     }
 
     @Override
-    public Path calculatePath(Point beforePoint, Point afterPoint) {
-        int vectorX = afterPoint.x() - beforePoint.x();
-        int vectorY = afterPoint.y() - beforePoint.y();
+    public Path calculatePath(Point beforePoint, Point targetPoint) {
+        int vectorX = targetPoint.x() - beforePoint.x();
+        int vectorY = targetPoint.y() - beforePoint.y();
 
         Path path = new Path();
 
         if (vectorX == 0) {
             int unitVectorY = vectorY / Math.abs(vectorY);
             for (int i = 0; i < Math.abs(vectorY); i++) {
-                path.addPoint(new Point(afterPoint.x(), afterPoint.y() - unitVectorY * i));
+                path.addPoint(new Point(targetPoint.x(), targetPoint.y() - unitVectorY * i));
             }
         }
 
         if (vectorY == 0) {
             int unitVectorX = vectorX / Math.abs(vectorX);
             for (int i = 0; i < Math.abs(vectorX); i++) {
-                path.addPoint(new Point(afterPoint.x() - unitVectorX * i, afterPoint.y()));
+                path.addPoint(new Point(targetPoint.x() - unitVectorX * i, targetPoint.y()));
             }
         }
         return path;
     }
 
     @Override
-    public boolean canMove(int size) {
+    public boolean canMove(Map<Piece, Boolean> piecesOnPathWithTargetOrNot) {
+        if (piecesOnPathWithTargetOrNot.isEmpty()) {
+            return true;
+        }
+        if (piecesOnPathWithTargetOrNot.size() == 1) {
+            if (!piecesOnPathWithTargetOrNot.values().stream().findFirst().get()) {
+                return false;
+            }
+            return piecesOnPathWithTargetOrNot.keySet().stream().findFirst().get().getTeam() != this.team;
+        }
         return false;
     }
 }
