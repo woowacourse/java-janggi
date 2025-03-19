d74b9e77b520695b1cb515d59716e31c3e332a12
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-19 14:05:01 +0900

    test(red): 포가 동일한 위치로 움질일 경우 예외를 발생시킨다

--- src/test/java/domain/piece/CannonTest.java
+++ src/test/java/domain/piece/CannonTest.java
@@ -1,5 +1,7 @@
 package domain.piece;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import domain.Position;
 import domain.Team;
 import java.util.List;
@@ -21,5 +23,15 @@ class CannonTest {
         // then
         List<Move> expected = List.of(Move.BACK, Move.BACK, Move.BACK);
         Assertions.assertThat(moves).isEqualTo(expected);
+    }
+
+    @DisplayName("동일한 위치로 움직일 경우 예외를 발생시킨다")
+    @Test
+    void test2() {
+        Cannon cannon = new Cannon(Team.RED);
+
+        assertThatThrownBy(() -> cannon.calculatePath(new Position(1, 1), new Position(1, 1)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("말을 움직여 주세요");
     }
 }
