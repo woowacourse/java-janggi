8b3f3c8f6446de0c60f30ec969ee64305852eb77
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-20 15:07:23 +0900

    feat: OutputView 구현
    - 보드 출력 기능
    - 잘못된 기물 선택 출력 기능

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,36 @@
+package view;
+
+import board.Board;
+import direction.Point;
+import java.util.Optional;
+import piece.Piece;
+import piece.Pieces;
+
+public class OutputView {
+
+    public static void displayBoard(Board board) {
+        Pieces pieces = board.findAll();
+
+        System.out.println();
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 9; j++) {
+                Point point = new Point(j, i);
+                Optional<Piece> findPiece = pieces.getPieces().stream()
+                        .filter(piece -> piece.getPosition().equals(point))
+                        .findAny();
+                if(findPiece.isEmpty()) {
+                    System.out.print(".");
+                    continue;
+                }
+                System.out.print(findPiece.get().getName());
+            }
+            System.out.println(" " + i);
+        }
+        System.out.println("123456789");
+        System.out.println();
+    }
+
+    public static void displayWrongPoint() {
+        System.out.println("본인의 기물이 아닙니다. 다시 선택해 주세요.");
+    }
+}
