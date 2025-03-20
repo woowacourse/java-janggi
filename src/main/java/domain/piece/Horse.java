fc5d977566ef9075f18efeca2b038e390b9125b5
Jin409 <sualng123@gmail.com>
2025-03-20 11:03:14 +0900

    feat(Soldier): 마의 경로 계산 및 도착점 이동 검증

---
+++ src/main/java/domain/piece/Horse.java
@@ -0,0 +1,56 @@
+package domain.piece;
+
+import domain.PieceMovement;
+import domain.Point;
+import domain.Team;
+import java.util.List;
+
+public class Horse implements Piece {
+
+    private final PieceMovement pieceMovement;
+    private final Team team;
+
+    public Horse(Team team) {
+        List<Route> routes = List.of(
+                new Route(List.of(Direction.NORTH, Direction.NORTHWEST)),
+                new Route(List.of(Direction.NORTH, Direction.NORTHEAST)),
+                new Route(List.of(Direction.EAST, Direction.NORTHEAST)),
+                new Route(List.of(Direction.EAST, Direction.SOUTHEAST)),
+                new Route(List.of(Direction.SOUTH, Direction.SOUTHEAST)),
+                new Route(List.of(Direction.SOUTH, Direction.SOUTHWEST)),
+                new Route(List.of(Direction.WEST, Direction.SOUTHWEST)),
+                new Route(List.of(Direction.WEST, Direction.NORTHWEST))
+        );
+
+        this.pieceMovement = new PieceMovement(routes);
+        this.team = team;
+    }
+
+    @Override
+    public boolean hasEqualTeam(Team team) {
+        return this.team.equals(team);
+    }
+
+    @Override
+    public boolean isAbleToArrive(Point startPoint, Point arrivalPoint) {
+        return pieceMovement.calculateTotalArrivalPoints(startPoint).contains(arrivalPoint);
+    }
+
+    @Override
+    public List<Point> getRoutePoints(Point startPoint, Point arrivalPoint) {
+        return pieceMovement.calculateRoutePoints(startPoint, arrivalPoint);
+    }
+
+    @Override
+    public boolean isMovable(PieceOnRoute pieceOnRoute) {
+        if (pieceOnRoute.hasArrivalPointInMyTeam(team)) {
+            return false;
+        }
+        return pieceOnRoute.hasNotPieceOnRoute();
+    }
+
+    @Override
+    public boolean canJumpOver() {
+        return true;
+    }
+}
---
+++ src/test/java/domain/piece/HorseTest.java
@@ -0,0 +1,92 @@
+package domain.piece;
+
+import domain.Point;
+import domain.Team;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HorseTest {
+    @Test
+    @DisplayName("피스가 이동할 수 있는 지점들을 전부 반환한다")
+    void test_isAbleToArrive() {
+        // given
+        Horse horse = new Horse(Team.CHO);
+        Point startPoint = new Point(0, 0);
+        Point arrivalPoint = new Point(2, 1);
+
+        // when
+        boolean actual = horse.isAbleToArrive(startPoint, arrivalPoint);
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("경로에 있는 모든 지점들을 반환한다")
+    void test_getRoutePoints() {
+        // given
+        Horse horse = new Horse(Team.CHO);
+        Point startPoint = new Point(0, 0);
+        Point arrivalPoint = new Point(2, 1);
+
+        // when
+        List<Point> routePoints = horse.getRoutePoints(startPoint, arrivalPoint);
+
+        // then
+        assertThat(routePoints).containsExactlyInAnyOrder(
+                new Point(1, 0),
+                new Point(2, 1)
+        );
+    }
+
+    @Test
+    @DisplayName("경로 상 기물이 있으면 이동할 수 없다.")
+    void test_isMovableWhenPieceOnRoute() {
+        //given
+        Horse horse = new Horse(Team.CHO);
+        Piece empty = new EmptyPiece();
+        PieceOnRoute pieceOnRoute = new PieceOnRoute(List.of(horse, empty));
+
+        //when&then
+        assertThat(horse.isMovable(pieceOnRoute)).isFalse();
+    }
+
+    @Test
+    @DisplayName("경로 상 기물이 없으면 이동할 수 없다.")
+    void test_isMovable() {
+        //given
+        Horse horse = new Horse(Team.CHO);
+        Piece empty = new EmptyPiece();
+        PieceOnRoute pieceOnRoute = new PieceOnRoute(List.of(empty, empty));
+
+        //when&then
+        assertThat(horse.isMovable(pieceOnRoute)).isTrue();
+    }
+
+    @Test
+    @DisplayName("도착점에 아군 기물이 있으면 이동할 수 없다.")
+    void test_isMovableWhenPieceIsInMyTeam() {
+        //given
+        Horse horse = new Horse(Team.CHO);
+        Piece empty = new EmptyPiece();
+        PieceOnRoute pieceOnRoute = new PieceOnRoute(List.of(empty, horse));
+
+        //when&then
+        assertThat(horse.isMovable(pieceOnRoute)).isFalse();
+    }
+
+    @Test
+    @DisplayName("도착점에 아군 기물이 없으면 이동할 수 있다.")
+    void test_isMovableWhenPieceIsInOtherTeam() {
+        //given
+        Horse horseHan = new Horse(Team.HAN);
+        Horse horseCho = new Horse(Team.CHO);
+        Piece empty = new EmptyPiece();
+        PieceOnRoute pieceOnRoute = new PieceOnRoute(List.of(empty, horseCho));
+
+        //when&then
+        assertThat(horseHan.isMovable(pieceOnRoute)).isTrue();
+    }
+}
