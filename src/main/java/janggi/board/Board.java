892b7d84aee17b8cd6d2023fa1776c7ea7d34b37
BWY <quantumjump10@gmail.com>
2025-03-20 11:19:37 +0900

    feat: Board에서 기물 이동 구현

--- src/main/java/janggi/board/Board.java
+++ src/main/java/janggi/board/Board.java
@@ -14,10 +14,16 @@ public class Board {
     }
 
     public void movePiece(Position start, Position goal) {
-        if (board.get(start) == null) {
+        Piece piece = board.get(start);
+        if (piece == null) {
             throw new IllegalArgumentException("[ERROR] 출발 지점에 기물이 존재하지 않습니다.");
         }
+        piece.validateMovable(board, start, goal);
+        move(start, goal, piece);
+    }
 
-
+    private void move(Position start, Position goal, Piece piece) {
+        board.put(goal, piece);
+        board.remove(start);
     }
 }
