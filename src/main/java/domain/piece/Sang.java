af69cf820c7feeaf4a1f97d187878cfda31e4cf9
Yeongjae Kim <sees77770@gmail.com>
2025-03-20 17:37:39 +0900

    fix(Sang): 상 이동 가능 경로 계산 로직 수정

--- src/main/java/domain/piece/Sang.java
+++ src/main/java/domain/piece/Sang.java
@@ -3,11 +3,11 @@ package domain.piece;
 import domain.JanggiCoordinate;
 import domain.board.JanggiBoard;
 import domain.piece.movement.SangMovement;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sang extends Piece {
-    
+
     public Sang(Country country) {
         super(country, PieceType.SANG);
     }
@@ -15,19 +15,26 @@ public class Sang extends Piece {
     @Override
     public List<JanggiCoordinate> availableMovePositions(JanggiCoordinate currCoordinate,
                                                          JanggiBoard janggiBoard) {
-        return Arrays.stream(SangMovement.values())
-                .filter(sangMovement -> !janggiBoard.hasPiece(
-                        movePosition(currCoordinate, sangMovement.getDirection())))
-                .flatMap(sangMovement -> sangMovement.getLeftDestination().stream()
-                        .filter(subDirection -> !janggiBoard.hasPiece(movePosition(currCoordinate, subDirection)))
-                        .flatMap(subDirection -> sangMovement.getRightDestination().stream()
-                                .map(destination -> movePosition(currCoordinate, destination))
-                                .filter(next -> !janggiBoard.isOutOfBoundary(next))
-                                .filter(next -> !janggiBoard.hasPiece(next) || !janggiBoard.isMyTeam(currCoordinate,
-                                        next))
-                        )
-                )
-                .toList();
+        List<JanggiCoordinate> availablePositions = new ArrayList<>();
+        for (SangMovement sangMovement : SangMovement.values()) {
+            JanggiCoordinate next = movePosition(currCoordinate, sangMovement.getDirection());
+            if (!janggiBoard.hasPiece(next)) {
+                List<JanggiCoordinate> left = sangMovement.getLeftDestination();
+                if (!janggiBoard.hasPiece(movePosition(currCoordinate, left.get(0))) &&
+                        (!janggiBoard.hasPiece(movePosition(currCoordinate, left.get(1))) || !janggiBoard.isMyTeam(
+                                currCoordinate, movePosition(currCoordinate, left.get(1))))) {
+                    availablePositions.add(movePosition(currCoordinate, left.get(1)));
+                }
+                
+                List<JanggiCoordinate> right = sangMovement.getRightDestination();
+                if (!janggiBoard.hasPiece(movePosition(currCoordinate, right.get(0))) &&
+                        (!janggiBoard.hasPiece(movePosition(currCoordinate, right.get(1))) || !janggiBoard.isMyTeam(
+                                currCoordinate, movePosition(currCoordinate, right.get(1))))) {
+                    availablePositions.add(movePosition(currCoordinate, right.get(1)));
+                }
+            }
+        }
+        return availablePositions;
     }
 
     public static JanggiCoordinate movePosition(JanggiCoordinate currCoordinate, JanggiCoordinate moveOffset) {
