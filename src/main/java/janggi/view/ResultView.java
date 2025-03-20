9d66fec2eba63bf4cea0366596cad99d8b2c684f
egaeng09 <egaeng09@gmail.com>
2025-03-20 17:49:42 +0900

    refactor: 불필요한 주석 제거

--- src/main/java/janggi/view/ResultView.java
+++ src/main/java/janggi/view/ResultView.java
@@ -15,7 +15,6 @@ public class ResultView {
     public void printBoard(final Map<Position, Piece> pieces) {
         System.out.printf(HEADER);
         for (int y = 1; y <= 10; y++) {
-            // TODO: 리스트로 스트링 넣고 팀에 따라 색깔 조합 + 한자 받아오기
             StringBuilder sb = new StringBuilder(String.format("%2d ", y));
             for (int x = 1; x <= 9; x++) {
                 Position currentPosition = Position.valueOf(y, x);
