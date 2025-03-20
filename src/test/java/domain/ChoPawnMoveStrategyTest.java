a3ea9520134487e4f50d5c041cb92d8a48aea8b8
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-03-20 13:18:04 +0900

    feat : 졸,병 목표 위치까지 이동경로 반환 기능 구현

--- src/test/java/domain/ChoPawnMoveStrategyTest.java
+++ src/test/java/domain/ChoPawnMoveStrategyTest.java
@@ -3,15 +3,17 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.piece.strategy.ChoPawnMoveStrategy;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ChoPawnMoveStrategyTest {
+    private ChoPawnMoveStrategy pawn = new ChoPawnMoveStrategy();
+
     @DisplayName("졸의 경우 현재 위치에서 출력 기준 상,좌,우 방향으로 한 칸만 이동하여 목적지에 도착할 수 있다면 true를 반환한다")
     @Test
     void test() {
         // given
-        ChoPawnMoveStrategy pawn = new ChoPawnMoveStrategy();
         BoardLocation current = new BoardLocation(1, 2);
         BoardLocation destination = new BoardLocation(1, 1);
 
@@ -26,7 +28,6 @@ public class ChoPawnMoveStrategyTest {
     @Test
     void test2() {
         // given
-        ChoPawnMoveStrategy pawn = new ChoPawnMoveStrategy();
         BoardLocation current = new BoardLocation(1, 3);
         BoardLocation destination = new BoardLocation(1, 1);
 
@@ -41,7 +42,6 @@ public class ChoPawnMoveStrategyTest {
     @Test
     void test3() {
         // given
-        ChoPawnMoveStrategy pawn = new ChoPawnMoveStrategy();
         BoardLocation current = new BoardLocation(3, 2);
         BoardLocation destination = new BoardLocation(3, 3);
 
@@ -51,5 +51,20 @@ public class ChoPawnMoveStrategyTest {
         // then
         assertThat(isMovable).isFalse();
     }
+
+    @DisplayName("졸 현재 위치에서 목표 좌표까지 이동하는 모든 경로를 반환한다")
+    @Test
+    void test4() {
+        // given
+        BoardLocation current = new BoardLocation(1, 1);
+        BoardLocation destination = new BoardLocation(2, 1);
+
+        // when
+        List<BoardLocation> allPath = pawn.createAllPath(current, destination);
+
+        // then
+        assertThat(allPath).containsAll(List.of(destination));
+    }
+
 
 }
--- src/test/java/domain/HanPawnMoveStrategyTest.java
+++ src/test/java/domain/HanPawnMoveStrategyTest.java
@@ -3,15 +3,17 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.piece.strategy.HanPawnMoveStrategy;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class HanPawnMoveStrategyTest {
+    private HanPawnMoveStrategy pawn = new HanPawnMoveStrategy();
+
     @DisplayName("병의 경우 현재 위치에서 출력 기준 하,좌,우 방향으로 한 칸만 이동하여 목적지에 도착할 수 있다면 true를 반환한다")
     @Test
     void test() {
         // given
-        HanPawnMoveStrategy pawn = new HanPawnMoveStrategy();
         BoardLocation current = new BoardLocation(1, 1);
         BoardLocation destination = new BoardLocation(1, 2);
 
@@ -26,7 +28,6 @@ public class HanPawnMoveStrategyTest {
     @Test
     void test2() {
         // given
-        HanPawnMoveStrategy pawn = new HanPawnMoveStrategy();
         BoardLocation current = new BoardLocation(1, 1);
         BoardLocation destination = new BoardLocation(1, 3);
 
@@ -41,7 +42,6 @@ public class HanPawnMoveStrategyTest {
     @Test
     void test3() {
         // given
-        HanPawnMoveStrategy pawn = new HanPawnMoveStrategy();
         BoardLocation current = new BoardLocation(3, 3);
         BoardLocation destination = new BoardLocation(3, 2);
 
@@ -50,6 +50,20 @@ public class HanPawnMoveStrategyTest {
 
         // then
         assertThat(isMovable).isFalse();
+    }
+
+    @DisplayName("병 현재 위치에서 목표 좌표까지 이동하는 모든 경로를 반환한다")
+    @Test
+    void test4() {
+        // given
+        BoardLocation current = new BoardLocation(1, 1);
+        BoardLocation destination = new BoardLocation(2, 1);
+
+        // when
+        List<BoardLocation> allPath = pawn.createAllPath(current, destination);
+
+        // then
+        assertThat(allPath).containsAll(List.of(destination));
     }
 
 }
