e1d7e501655d49f252c5101ed6108e7afa55beef
jhan0121 <jhan0121@gmail.com>
2025-03-20 13:19:24 +0900

    refactor: Route 코드 스타일 개선

--- src/main/java/janggi/Route.java
+++ src/main/java/janggi/Route.java
@@ -6,16 +6,19 @@ import java.util.List;
 public class Route {
 
     public static List<Position> of(final Position departure, final Position destination) {
-        int diffRow = destination.subtractRow(departure);
-        int diffColumn = destination.subtractColumn(departure);
         List<Position> route = new ArrayList<>();
         route.add(departure);
-        int moveCount = Math.max(Math.abs(diffRow), Math.abs(diffColumn));
+        int moveCount = calculateMoveCount(departure, destination);
         for (int i = 0; i < moveCount; i++) {
             decideDirection(route, route.getLast(), destination);
         }
         excludeDepartureAndDestination(route);
+
         return route;
+    }
+
+    private static int calculateMoveCount(final Position departure, final Position destination) {
+        return Math.max(Math.abs(destination.subtractRow(departure)), Math.abs(destination.subtractColumn(departure)));
     }
 
     private static void excludeDepartureAndDestination(final List<Position> route) {
@@ -23,19 +26,20 @@ public class Route {
         route.removeLast();
     }
 
-    private static void decideDirection(final List<Position> route, final Position current,
+    private static void decideDirection(final List<Position> route,
+                                        final Position current,
                                         final Position destination) {
-        int diffRow = destination.subtractRow(current);
-        int diffColumn = destination.subtractColumn(current);
-        int rowDirection = calculateDirection(diffRow);
-        int columnDirection = calculateDirection(diffColumn);
-        if (Math.abs(diffRow) > Math.abs(diffColumn)) {
+        int differenceOfRow = destination.subtractRow(current);
+        int differenceOfColumn = destination.subtractColumn(current);
+        int rowDirection = calculateDirection(differenceOfRow);
+        int columnDirection = calculateDirection(differenceOfColumn);
+        if (Math.abs(differenceOfRow) > Math.abs(differenceOfColumn)) {
             route.add(current.adjust(rowDirection, 0));
         }
-        if (Math.abs(diffRow) < Math.abs(diffColumn)) {
+        if (Math.abs(differenceOfRow) < Math.abs(differenceOfColumn)) {
             route.add(current.adjust(0, columnDirection));
         }
-        if (Math.abs(diffRow) == Math.abs(diffColumn)) {
+        if (Math.abs(differenceOfRow) == Math.abs(differenceOfColumn)) {
             route.add(current.adjust(rowDirection, columnDirection));
         }
     }
