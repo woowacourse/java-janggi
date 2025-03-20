66caee36e3108e3ac2179b46ae8fe8f1d8357a15
wogus <ind07152@naver.com>
2025-03-20 17:18:20 +0900

    refactor: Cannon 메서드 추출

--- src/main/java/domain/piece/Cannon.java
+++ src/main/java/domain/piece/Cannon.java
@@ -24,49 +24,65 @@ public class Cannon extends Piece {
 
     @Override
     public boolean canMove(Position expectedPosition, List<Piece> pieces) {
-        Direction direction = null;
-        for (Direction nextDirection : directions) {
-            Position current = this.position;
-            while (current.canMovePosition(nextDirection.getDeltaRow(), nextDirection.getDeltaColumn())) {
-                current = current.movePosition(nextDirection.getDeltaRow(), nextDirection.getDeltaColumn());
-                if (current.equals(expectedPosition)) {
-                    direction = nextDirection;
-                    break;
-                }
-            }
-        }
+        Direction direction = findDirectionToReachAt(expectedPosition);
         if (direction == null) {
             return false;
         }
+        if (hasCannonPieceAtIntermediatePositions(expectedPosition, pieces, direction)) {
+            return false;
+        }
+        if (hasOnlyOnePieceAtIntermediatePositions(expectedPosition, pieces, direction)) {
+            return false;
+        }
+        return hasNotTeamAtPosition(expectedPosition, pieces, (piece -> piece.isSameType(PieceType.CANNON)));
+    }
+
+    private boolean hasCannonPieceAtIntermediatePositions(Position expectedPosition, List<Piece> pieces,
+                                                          Direction direction) {
+        List<Position> intermediatePositions = findIntermediatePositions(direction, this.position, expectedPosition);
+        for (Position intermediatePosition : intermediatePositions) {
+            for (Piece piece : pieces) {
+                if (piece.hasSamePosition(intermediatePosition) && piece.isSameType(PieceType.CANNON)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean hasOnlyOnePieceAtIntermediatePositions(Position expectedPosition, List<Piece> pieces,
+                                                           Direction direction) {
         List<Position> intermediatePositions = findIntermediatePositions(direction, this.position, expectedPosition);
         int count = 0;
         for (Position intermediatePosition : intermediatePositions) {
             for (Piece piece : pieces) {
                 if (piece.hasSamePosition(intermediatePosition)) {
-                    if (piece.isSameType(PieceType.CANNON)) {
-                        return false;
-                    }
                     count++;
                 }
             }
         }
+        return count != 1;
+    }
 
-        if (count != 1) {
-            return false;
+    private Direction findDirectionToReachAt(Position expectedPosition) {
+        for (Direction nextDirection : directions) {
+            Direction findDirection = findDirection(expectedPosition, nextDirection);
+            if (findDirection != null) {
+                return findDirection;
+            }
         }
+        return null;
+    }
 
-        return hasNotTeamAtPosition(expectedPosition,pieces,(piece ->piece.isSameType(PieceType.CANNON)));
-
-//        boolean check = pieces.stream()
-//                .anyMatch(
-//                        piece -> piece.hasSamePosition(expectedPosition) && (piece.isSameTeam(this) || piece.isSameType(
-//                                PieceType.CANNON)));
-//
-//        if (check) {
-//            return false;
-//        }
-//
-//        return true;
+    private Direction findDirection(Position expectedPosition, Direction nextDirection) {
+        Position current = this.position;
+        while (current.canMovePosition(nextDirection.getDeltaRow(), nextDirection.getDeltaColumn())) {
+            current = current.movePosition(nextDirection.getDeltaRow(), nextDirection.getDeltaColumn());
+            if (current.equals(expectedPosition)) {
+                return nextDirection;
+            }
+        }
+        return null;
     }
 
     private List<Position> findIntermediatePositions(Direction direction, Position start, Position end) {
