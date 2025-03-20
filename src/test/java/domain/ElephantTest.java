a2f27814e061d1cba8e9055401474fde0dd4b0c4
minSsan <msp1125@hanyang.ac.kr>
2025-03-20 16:50:37 +0900

    🔴red: 장애물이 있는 경우 상이 모든 목적지를 반환할 수 있다 테스트 작성

--- src/test/java/domain/ElephantTest.java
+++ src/test/java/domain/ElephantTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +29,36 @@ class ElephantTest {
 
         //when
         final List<ChessPosition> destinations = elephant.getDestinations(ChessPiecePositions.empty());
+
+        //then
+        assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @DisplayName("다른 기물이 있는 경우, 모든 목적지를 반환할 수 있다.")
+    @Test
+    void existOtherPieces() {
+        // given
+        final ChessPosition elephantPosition = new ChessPosition(4, 4);
+        final Elephant elephant = new Elephant(elephantPosition, ChessTeam.BLUE);
+
+        final Map<ChessPosition, ChessPiece> chessPositionPawnMap = Map.of(
+                new ChessPosition(2, 5), new Pawn(new ChessPosition(2, 5), ChessTeam.RED),
+                new ChessPosition(1, 2), new Pawn(new ChessPosition(1, 2), ChessTeam.BLUE),
+                new ChessPosition(7, 2), new Pawn(new ChessPosition(7, 2), ChessTeam.RED)
+        );
+        final ChessPiecePositions piecePositions = ChessPiecePositions.from(chessPositionPawnMap);
+
+        final List<ChessPosition> expected = List.of(
+                new ChessPosition(7, 6),
+                new ChessPosition(7, 2),
+                new ChessPosition(6, 7),
+                new ChessPosition(2, 7),
+                new ChessPosition(6, 1),
+                new ChessPosition(2, 1)
+        );
+
+        //when
+        final List<ChessPosition> destinations = elephant.getDestinations(piecePositions);
 
         //then
         assertThat(destinations).containsExactlyInAnyOrderElementsOf(expected);
