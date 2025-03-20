0aaa7ff56949be17bb3f09ab8bd00e834d9f029a
junhaa <2171326@hansung.ac.kr>
2025-03-20 13:40:54 +0900

    feat: 사용자의 좌표 입력 검증 추가

--- src/main/java/util/PositionConvertor.java
+++ src/main/java/util/PositionConvertor.java
@@ -3,7 +3,17 @@ package util;
 import domain.Position;
 
 public class PositionConvertor {
-    public static Position changeInputToPosition(String input){
+    private static final String REGEX = "^[a-z]\\d$";
+
+    public static Position changeInputToPosition(String input) {
+        validatePositionInput(input);
         return Position.of(Character.getNumericValue(input.charAt(1)), input.charAt(0) - 'a');
+    }
+
+    private static void validatePositionInput(String input) {
+        if (input.matches(REGEX)) {
+            return;
+        }
+        throw new IllegalArgumentException("올바른 좌표 형식이 아닙니다.");
     }
 }
