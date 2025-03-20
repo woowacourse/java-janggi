66bec8f591b0de43724d7b3853cb02544320ce01
ljhee92 <weilainib7@gmail.com>
2025-03-20 11:29:41 +0900

    feat: 순서대로 장기 게임을 진행하는 기능 추가

--- src/main/java/FlowManager.java
+++ src/main/java/FlowManager.java
@@ -1,7 +1,7 @@
-import domain.board.Board;
-import domain.board.BoardGenerator;
+import domain.JanggiManager;
 import domain.piece.Team;
 import view.InputView;
+import view.MoveCommand;
 import view.OutputView;
 import view.SangMaOrderCommand;
 
@@ -12,14 +12,38 @@ public class FlowManager {
     public void startGame() {
         OutputView.printStart();
 
-        SangMaOrderCommand hanSangMaOrderCommand = InputView.inputSangMaOrder(Team.HAN.title());
-        SangMaOrderCommand choSangMaOrderCommand = InputView.inputSangMaOrder(Team.CHO.title());
+        JanggiManager janggiManager = createJanggiManager();
+        OutputView.printBoard(janggiManager.board());
 
-        BoardGenerator boardGenerator = new BoardGenerator();
-        Board board = boardGenerator.generateBoard(hanSangMaOrderCommand, choSangMaOrderCommand);
-        OutputView.printBoard(board);
+        startMove(janggiManager);
+    }
 
+    private void startMove(JanggiManager janggiManager) {
+        boolean isRunning = true;
         Team turn = startTeam;
-        InputView.inputMoveCommand(turn.title());
+
+        while (isRunning) {
+            MoveCommand moveCommand = InputView.inputMoveCommand(turn);
+
+            if (!janggiManager.canMove(moveCommand.source(), moveCommand.destination())) {
+                continue;
+            }
+
+            if (janggiManager.isThereWang(moveCommand.destination())) {
+                OutputView.printMatchResult(turn);
+                break;
+            }
+
+            janggiManager.movePiece(moveCommand.source(), moveCommand.destination());
+            OutputView.printBoard(janggiManager.board());
+
+            turn = turn.inverse();
+        }
+    }
+
+    private JanggiManager createJanggiManager() {
+        SangMaOrderCommand hanSangMaOrderCommand = InputView.inputSangMaOrder(Team.HAN);
+        SangMaOrderCommand choSangMaOrderCommand = InputView.inputSangMaOrder(Team.CHO);
+        return new JanggiManager(hanSangMaOrderCommand, choSangMaOrderCommand);
     }
 }
