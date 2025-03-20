a5a64936075dd654df13518da2875e40981f093c
ryan-dia <cjfdnjs158@naver.com>
2025-03-20 14:13:44 +0900

    feat: 팀 별 출력 색깔 구분 기능 추가
    Co-authored-by:saera-yook<yook.saera@gmail.com>

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,16 +10,19 @@ import java.util.ResourceBundle;
 public final class OutputView {
 
     private static final ResourceBundle rb = ResourceBundle.getBundle("score");
+    private static final String RED = "\033[0;31m";
+    private static final String GREEN = "\033[0;32m";
+    private static final String RESET = "\033[0m";
 
     private OutputView() {
     }
 
     public static void printGreenTurn() {
-        System.out.println("\n초나라 차례입니다.");
+        System.out.println("\n" + GREEN + "초나라" + RESET + " 차례입니다.");
     }
 
     public static void printRedTurn() {
-        System.out.println("한나라 차례입니다.");
+        System.out.println("\n" + RED + "한나라" + RESET + " 차례입니다.");
     }
 
     public static void printBoard(final Board board) {
@@ -30,8 +33,12 @@ public final class OutputView {
                     target = board.findPositionBy(Point.of(j, i));
                     final Piece piece = target.getPiece();
                     final Score score = piece.getScore();
-
-                    System.out.printf("%-1s\t", rb.getString(score.name()) + "(" + j + "," + i + ")");
+                    if (piece.isGreenTeam()) {
+                        System.out.printf("%-1s\t",
+                                GREEN + rb.getString(score.name()) + "(" + j + "," + i + ")" + RESET);
+                    } else {
+                        System.out.printf("%-1s\t", RED + rb.getString(score.name()) + "(" + j + "," + i + ")" + RESET);
+                    }
 
                 } catch (final IllegalArgumentException e) {
                     System.out.printf("%-1s\t", "X" + "(" + j + "," + i + ")");
