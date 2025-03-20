20dd60e46768981242f3a7d7a5b34128d2c00ee8
taek2222 <oot309@daum.net>
2025-03-20 13:54:02 +0900

    test: 포 이동 경로 기물 갯수 검증 테스트 작성

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -1,19 +1,20 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 import domain.piece.Pawn;
 import domain.piece.Piece;
 import domain.piece.PieceDirection;
 import domain.piece.PieceInit;
 import domain.piece.Pieces;
 import domain.piece.Position;
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class BoardTest {
 
@@ -66,6 +67,38 @@ class BoardTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> board.move(han, startPosition, targetPosition))
                 .withMessage("[ERROR] 도착 위치에 아군의 기물이 존재해 이동할 수 없습니다.");
+    }
+
+    @Test
+    void 포_이동_경로에_기물이_1개가_아닌_경우_예외가_발생한다() {
+        // given
+        Position startPosition = Position.of(2, 3);
+        Position targetPosition1 = Position.of(4, 3);
+        Position targetPosition2 = Position.of(7, 3);
+
+        Player han = new Player("한", PieceColor.RED);
+        Player cho = new Player("초", PieceColor.BLUE);
+
+        List<Piece> hanPieces = PieceInit.initHanPieces();
+        List<Piece> choPieces = PieceInit.initChoPieces();
+        hanPieces.add(new Pawn(5, 3, PieceDirection.HAN_PAWN.get()));
+        hanPieces.add(new Pawn(6, 3, PieceDirection.HAN_PAWN.get()));
+
+        Map<Player, Pieces> boardElements = new HashMap<>();
+        boardElements.put(han, new Pieces(hanPieces));
+        boardElements.put(cho, new Pieces(choPieces));
+
+        Board board = new Board(boardElements);
+
+        // when & then
+        assertAll(
+                () -> assertThatIllegalArgumentException()
+                        .isThrownBy(() -> board.move(han, startPosition, targetPosition1))
+                        .withMessage("[ERROR] 포는 중간에 기물이 1개여야 합니다."),
+                () -> assertThatIllegalArgumentException()
+                        .isThrownBy(() -> board.move(han, startPosition, targetPosition2))
+                        .withMessage("[ERROR] 포는 중간에 기물이 1개여야 합니다.")
+        );
     }
 
     @Test
