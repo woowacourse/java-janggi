9e746123fab1ed850abfbd8faf2664cb9c268abf
soeun2537 <soeun2537@gmail.com>
2025-03-20 15:36:30 +0900

    feat: Application 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,39 @@
+import domain.Team;
+import domain.board.Board;
+import domain.board.createStrategy.BoardCreateStrategy;
+import domain.board.createStrategy.MaSangMaSang;
+import domain.board.createStrategy.MaSangSangMa;
+import domain.board.createStrategy.SangMaMaSang;
+import domain.board.createStrategy.SangMaSangMa;
+import java.util.Map;
+import view.InputView;
+import view.InputView.CoordinatesDTO;
+import view.OutputView;
+
+public class Application {
+
+    private static final Map<Integer, BoardCreateStrategy> boardCreateStrategy = Map.of(
+            1, new MaSangSangMa(),
+            2, new MaSangMaSang(),
+            3, new SangMaSangMa(),
+            4, new SangMaMaSang()
+    );
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        int hanTableSetting = inputView.readTableSetting(Team.HAN);
+        int choTableSetting = inputView.readTableSetting(Team.CHO);
+        Board board = Board.create(boardCreateStrategy.get(hanTableSetting), boardCreateStrategy.get(choTableSetting));
+        outputView.printBoard(board.getPieces());
+
+        while (true) {
+            for (Team team : Team.values()) {
+                CoordinatesDTO hanCoordinates = inputView.readMoveCoordinate(team);
+                board.move(hanCoordinates.departure(), hanCoordinates.arrival());
+                outputView.printBoard(board.getPieces());
+            }
+        }
+    }
+}
