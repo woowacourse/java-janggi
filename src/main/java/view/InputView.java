7a55e2f8d0ec8b275a7ceeff6c7be34cd8bc15d3
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-20 15:44:33 +0900

    feat(view) : 입출력 뷰 분리

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import model.JanggiBoard;
+import model.Point;
+import model.Team;
+
+public class InputView {
+    public static void choMoveInput(Scanner sc, JanggiBoard janggiBoard, Team team) {
+        System.out.printf("%s 나라의 차례\n", team.getTeam());
+        System.out.println("이동할 기물의 위치를 선택하세요.");
+        List<Integer> beforePointInput = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toList();
+        Point beforePoint = Point.of(beforePointInput.get(0), beforePointInput.get(1));
+        System.out.println("이동될 위치를 선택하세요.");
+        List<Integer> targetPointInput = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toList();
+        Point targetPoint = Point.of(targetPointInput.get(0), targetPointInput.get(1));
+        janggiBoard.move(beforePoint, targetPoint);
+    }
+}
