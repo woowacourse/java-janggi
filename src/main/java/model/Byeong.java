59750404664a9822f72f832925373f8723036373
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-20 10:47:09 +0900

    GREEN) feat(model): 병 경로 방해 여부 구현

--- src/main/java/model/Byeong.java
+++ src/main/java/model/Byeong.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class Byeong extends Piece {
@@ -10,7 +11,7 @@ public class Byeong extends Piece {
     }
 
     @Override
-    public boolean isValidPoint(Point beforePoint, Point afterPoint) {
+    public boolean isValidPoint(Point beforePoint, Point targetPoint) {
         int moveForward = 1;
 
         if (team == Team.RED){
@@ -21,19 +22,22 @@ public class Byeong extends Piece {
         List<Integer> vertical = List.of(moveForward, 0, 0);
 
         return IntStream.range(0, horizontal.size())
-                .anyMatch(i -> horizontal.get(i) + beforePoint.x() == afterPoint.x() && vertical.get(i) + beforePoint.y() == afterPoint.y());
+                .anyMatch(i -> horizontal.get(i) + beforePoint.x() == targetPoint.x() && vertical.get(i) + beforePoint.y() == targetPoint.y());
     }
 
     @Override
-    public Path calculatePath(Point beforePoint, Point afterPoint) {
+    public Path calculatePath(Point beforePoint, Point targetPoint) {
         Path path = new Path();
-        path.addPoint(new Point(afterPoint.x(), afterPoint.y()));
+        path.addPoint(new Point(targetPoint.x(), targetPoint.y()));
         return path;
     }
 
     @Override
-    public boolean canMove(int size) {
-        return false;
+    public boolean canMove(Map<Piece,Boolean> piecesOnPathWithTargetOrNot) {
+        if (piecesOnPathWithTargetOrNot.size() == 1) {
+            return piecesOnPathWithTargetOrNot.keySet().stream().findFirst().get().getTeam() != this.team;
+        }
+        return true;
     }
 
 }
