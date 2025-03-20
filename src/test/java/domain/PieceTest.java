0aa5a926f910a2498cb926d849f2feedcbe49281
민경윤 <ruddbs3308@naver.com>
2025-03-20 17:48:54 +0900

    test: 기물의 이동 경로에 있는 장애물 갯수가 허용되는 갯수인지 검증

--- src/test/java/domain/PieceTest.java
+++ src/test/java/domain/PieceTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -27,6 +28,19 @@ class PieceTest {
 
             // then
             assertThat(movementRule).isEqualTo(List.of(new Offset(1, 0)));
+        }
+
+        @DisplayName("기물의 이동 경로에 있는 장애물 갯수가 허용되는 갯수인지 확인한다.")
+        @Test
+        void isObstacleCountAllowed() {
+            // given
+            Piece piece = new Piece(PieceType.CANNON, Team.RED);
+
+            // when & then
+            assertAll(
+                    () -> assertThat(piece.isObstacleCountAllowed(1)).isTrue(),
+                    () -> assertThat(piece.isObstacleCountAllowed(2)).isFalse()
+            );
         }
     }
 
