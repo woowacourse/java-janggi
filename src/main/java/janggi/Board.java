8a7b6b012ed1206f3caebcf922215e322bf349a1
sung-june27 <kmymy27@gmail.com>
2025-03-20 10:55:23 +0900

    feat(Board): 장기말 이동 구현

--- src/main/java/janggi/Board.java
+++ src/main/java/janggi/Board.java
@@ -10,6 +10,7 @@ import janggi.piece.Soldier;
 import janggi.piece.Tank;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
@@ -23,48 +24,109 @@ public class Board {
     public static Board init() {
         Map<Position, Piece> board = new HashMap<>();
 
-        board.put(new Position(1,1), new Tank(Side.BLUE));
-        board.put(new Position(2,1), new Elephant(Side.BLUE));
-        board.put(new Position(3,1), new Horse(Side.BLUE));
-        board.put(new Position(4,1), new Guard(Side.BLUE));
+        board.put(new Position(1, 1), new Tank(Side.BLUE));
+        board.put(new Position(2, 1), new Elephant(Side.BLUE));
+        board.put(new Position(3, 1), new Horse(Side.BLUE));
+        board.put(new Position(4, 1), new Guard(Side.BLUE));
 
-        board.put(new Position(6,1), new Guard(Side.BLUE));
-        board.put(new Position(7,1), new Horse(Side.BLUE));
-        board.put(new Position(8,1), new Elephant(Side.BLUE));
-        board.put(new Position(9,1), new Tank(Side.BLUE));
+        board.put(new Position(6, 1), new Guard(Side.BLUE));
+        board.put(new Position(7, 1), new Horse(Side.BLUE));
+        board.put(new Position(8, 1), new Elephant(Side.BLUE));
+        board.put(new Position(9, 1), new Tank(Side.BLUE));
 
-        board.put(new Position(5,2), new King(Side.BLUE));
+        board.put(new Position(5, 2), new King(Side.BLUE));
 
-        board.put(new Position(2,3), new Cannon(Side.BLUE));
-        board.put(new Position(8,3), new Cannon(Side.BLUE));
+        board.put(new Position(2, 3), new Cannon(Side.BLUE));
+        board.put(new Position(8, 3), new Cannon(Side.BLUE));
 
-        board.put(new Position(1,4), new Soldier(Side.BLUE));
-        board.put(new Position(3,4), new Soldier(Side.BLUE));
-        board.put(new Position(5,4), new Soldier(Side.BLUE));
-        board.put(new Position(7,4), new Soldier(Side.BLUE));
-        board.put(new Position(9,4), new Soldier(Side.BLUE));
+        board.put(new Position(1, 4), new Soldier(Side.BLUE));
+        board.put(new Position(3, 4), new Soldier(Side.BLUE));
+        board.put(new Position(5, 4), new Soldier(Side.BLUE));
+        board.put(new Position(7, 4), new Soldier(Side.BLUE));
+        board.put(new Position(9, 4), new Soldier(Side.BLUE));
 
-        board.put(new Position(1,10), new Tank(Side.RED));
-        board.put(new Position(2,10), new Elephant(Side.RED));
-        board.put(new Position(3,10), new Horse(Side.RED));
-        board.put(new Position(4,10), new Guard(Side.RED));
+        board.put(new Position(1, 10), new Tank(Side.RED));
+        board.put(new Position(2, 10), new Elephant(Side.RED));
+        board.put(new Position(3, 10), new Horse(Side.RED));
+        board.put(new Position(4, 10), new Guard(Side.RED));
 
-        board.put(new Position(6,10), new Guard(Side.RED));
-        board.put(new Position(7,10), new Horse(Side.RED));
-        board.put(new Position(8,10), new Elephant(Side.RED));
-        board.put(new Position(9,10), new Tank(Side.RED));
+        board.put(new Position(6, 10), new Guard(Side.RED));
+        board.put(new Position(7, 10), new Horse(Side.RED));
+        board.put(new Position(8, 10), new Elephant(Side.RED));
+        board.put(new Position(9, 10), new Tank(Side.RED));
 
-        board.put(new Position(5,9), new King(Side.RED));
+        board.put(new Position(5, 9), new King(Side.RED));
 
-        board.put(new Position(2,8), new Cannon(Side.RED));
-        board.put(new Position(8,8), new Cannon(Side.RED));
+        board.put(new Position(2, 8), new Cannon(Side.RED));
+        board.put(new Position(8, 8), new Cannon(Side.RED));
 
-        board.put(new Position(1,7), new Soldier(Side.RED));
-        board.put(new Position(3,7), new Soldier(Side.RED));
-        board.put(new Position(5,7), new Soldier(Side.RED));
-        board.put(new Position(7,7), new Soldier(Side.RED));
-        board.put(new Position(9,7), new Soldier(Side.RED));
+        board.put(new Position(1, 7), new Soldier(Side.RED));
+        board.put(new Position(3, 7), new Soldier(Side.RED));
+        board.put(new Position(5, 7), new Soldier(Side.RED));
+        board.put(new Position(7, 7), new Soldier(Side.RED));
+        board.put(new Position(9, 7), new Soldier(Side.RED));
         return new Board(board);
+    }
+
+    public void move(Position start, Position end) {
+        validatePieceExistsByPosition(start);
+        Piece pickedPiece = board.get(start);
+        if (board.containsKey(end)) {
+            Piece targetPiece = board.get(end);
+            validateTargetPiece(pickedPiece, targetPiece);
+        }
+
+        validatePieceOnPath(start, end, pickedPiece);
+
+        board.remove(start);
+        board.put(end, pickedPiece);
+    }
+
+    private void validatePieceOnPath(Position start, Position end, Piece pickedPiece) {
+        List<Position> path = pickedPiece.calculatePath(start, end);
+        if (pickedPiece instanceof Cannon) {
+            validateNotExistsCannonOnPath(path);
+            validateOnePieceOnCannonPath(path);
+        } else {
+            boolean containsPieceOnPath = path.stream()
+                    .anyMatch(board::containsKey);
+            if (containsPieceOnPath) {
+                throw new IllegalArgumentException("경로 상에 말이 존재합니다.");
+            }
+        }
+    }
+
+    private void validateOnePieceOnCannonPath(List<Position> path) {
+        long countPieceOnPath = path.stream()
+                .filter(board::containsKey)
+                .count();
+        if (countPieceOnPath != 1) {
+            throw new IllegalArgumentException("포가 이동할 수 없는 조건입니다.");
+        }
+    }
+
+    private void validateNotExistsCannonOnPath(List<Position> path) {
+        boolean containsCannonOnPath = path.stream()
+                .anyMatch(position -> board.containsKey(position) && board.get(position) instanceof Cannon);
+        if (containsCannonOnPath) {
+            throw new IllegalArgumentException("포의 이동 경로 상에 포가 존재합니다.");
+        }
+    }
+
+    private static void validateTargetPiece(Piece pickedPiece, Piece targetPiece) {
+        if (pickedPiece.isSameSide(targetPiece)) {
+            throw new IllegalArgumentException("같은 팀이 있는 위치로는 이동할 수 없습니다.");
+        }
+        if (targetPiece instanceof Cannon && pickedPiece instanceof Cannon) {
+            throw new IllegalArgumentException("포는 포를 잡을 수 없습니다.");
+        }
+    }
+
+    private void validatePieceExistsByPosition(Position position) {
+        if (board.containsKey(position)) {
+            return;
+        }
+        throw new IllegalArgumentException("선택된 좌표에 말이 없습니다.");
     }
 
     public Map<Position, Piece> getBoard() {
--- src/test/java/janggi/BoardTest.java
+++ src/test/java/janggi/BoardTest.java
@@ -1,9 +1,16 @@
 package janggi;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import janggi.piece.Cannon;
 import janggi.piece.Piece;
+import janggi.piece.Soldier;
+import janggi.piece.Tank;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -44,6 +51,199 @@ public class BoardTest {
                     .toList();
             int expected = 16;
             assertThat(piecesOfSide).hasSize(expected);
+        }
+    }
+
+    @Nested
+    @DisplayName("기물 이동 테스트")
+    class MoveTest {
+
+        @Test
+        @DisplayName("현재 위치의 말과 목적지의 말이 같은 팀이라면 예외를 발생시킨다.")
+        void sholudThrowExceptionWhenCurrentPieceAndDestinationPieceIsSameSide() {
+            // given
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), new Tank(Side.RED),
+                            new Position(2, 1), new Soldier(Side.RED)
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(2, 1);
+
+            // then
+            assertThatThrownBy(() -> board.move(start, end))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("시작점과 도착점이 주어졌을 때, 말을 이동시킨다.")
+        void movePieceWithStartEndPosition() {
+            // given
+            Piece piece = new Tank(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(2, 1);
+
+            // when
+            board.move(start, end);
+
+            // then
+            assertAll(
+                    () -> assertThat(board.getBoard().get(end)).isEqualTo(piece),
+                    () -> assertThat(board.getBoard().get(start)).isNull()
+            );
+        }
+
+        @Test
+        @DisplayName("말의 이동 규칙과 맞지 않는다면 예외를 던진다.")
+        void shouldThrowExceptionWhenUnfollowingRule() {
+            // given
+            Piece piece = new Tank(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(2, 2);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> board.move(start, end))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("말의 경로에 다른 말이 존재한다면 예외를 던진다.")
+        void shouldThrowExceptionWhenExistsPieceOnPath() {
+            // given
+            Piece piece = new Tank(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece,
+                            new Position(3, 1), new Soldier(Side.RED)
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(5, 1);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> board.move(start, end))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("포의 경로 상 말이 2개 이상인 경우 예외를 던진다.")
+        void shouldThrowExceptionWhenExistsOverTwoPieceOnCannonPath() {
+            // given
+            Piece piece = new Cannon(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece,
+                            new Position(3, 1), new Soldier(Side.RED),
+                            new Position(4, 1), new Soldier(Side.RED)
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(6, 1);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> board.move(start, end))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("포의 경로 상 말이 1개인 경우 예외를 던지지 않는다.")
+        void shouldThrowExceptionWhenExistsOnePieceOnCannonPath() {
+            // given
+            Piece piece = new Cannon(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece,
+                            new Position(3, 1), new Soldier(Side.RED)
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(6, 1);
+
+            // when
+
+            // then
+            assertThatCode(() -> board.move(start, end))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("포의 경로 상 포가 존재하는 경우 예외를 던진다.")
+        void shouldThrowExceptionWhenExistsCannonOnCannonPath() {
+            // given
+            Piece piece = new Cannon(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece,
+                            new Position(3, 1), new Cannon(Side.RED)
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(6, 1);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> board.move(start, end))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("포의 경로 상에 말이 존재하지 않는 경우 예외를 던진다.")
+        void shouldThrowExceptionWhenEmptyOnCannonPath() {
+            // given
+            Piece piece = new Cannon(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(6, 1);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> board.move(start, end))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("포의 도착지의 말이 포인 경우 예외를 던진다.")
+        void shouldThrowExceptionWhenExistsCannonOnCannonEndPosition() {
+            // given
+            Piece piece = new Cannon(Side.RED);
+            Board board = new Board(
+                    Map.of(
+                            new Position(1, 1), piece,
+                            new Position(3, 1), new Soldier(Side.RED),
+                            new Position(6, 1), new Cannon(Side.BLUE)
+                    )
+            );
+            Position start = new Position(1, 1);
+            Position end = new Position(6, 1);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> board.move(start, end))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
