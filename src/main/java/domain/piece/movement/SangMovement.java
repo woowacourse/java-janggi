787c95d7a81e907a11b9e476d1abb047ec81ca66
Yeongjae Kim <sees77770@gmail.com>
2025-03-20 16:04:06 +0900

    refactor(SangMovement): 상 이동 방향 네이밍 수정

--- src/main/java/domain/piece/movement/SangMovement.java
+++ src/main/java/domain/piece/movement/SangMovement.java
@@ -1,7 +1,6 @@
 package domain.piece.movement;
 
 import domain.JanggiCoordinate;
-
 import java.util.List;
 
 public enum SangMovement {
@@ -11,37 +10,37 @@ public enum SangMovement {
             List.of(new JanggiCoordinate(1, -2), new JanggiCoordinate(2, -3))),
 
     DOWN(new JanggiCoordinate(0, 1),
-            List.of(new JanggiCoordinate(1, 2), new JanggiCoordinate(2, 3)),
-            List.of(new JanggiCoordinate(-1, 2), new JanggiCoordinate(-2, 3))),
+            List.of(new JanggiCoordinate(-1, 2), new JanggiCoordinate(-2, 3)),
+            List.of(new JanggiCoordinate(1, 2), new JanggiCoordinate(2, 3))),
 
     RIGHT(new JanggiCoordinate(1, 0),
             List.of(new JanggiCoordinate(2, -1), new JanggiCoordinate(3, -2)),
             List.of(new JanggiCoordinate(2, 1), new JanggiCoordinate(3, 2))),
 
     LEFT(new JanggiCoordinate(-1, 0),
-            List.of(new JanggiCoordinate(-2, -1), new JanggiCoordinate(-3, -2)),
-            List.of(new JanggiCoordinate(-2, 1), new JanggiCoordinate(-3, 2)));
+            List.of(new JanggiCoordinate(-2, 1), new JanggiCoordinate(-3, 2)),
+            List.of(new JanggiCoordinate(-2, -1), new JanggiCoordinate(-3, -2)));
 
     private final JanggiCoordinate direction;
-    private final List<JanggiCoordinate> subDirection;
-    private final List<JanggiCoordinate> destination;
+    private final List<JanggiCoordinate> leftDestination;
+    private final List<JanggiCoordinate> rightDestination;
 
-    SangMovement(JanggiCoordinate direction, List<JanggiCoordinate> subDirection,
-                 List<JanggiCoordinate> destination) {
+    SangMovement(JanggiCoordinate direction, List<JanggiCoordinate> leftDestination,
+                 List<JanggiCoordinate> rightDestination) {
         this.direction = direction;
-        this.subDirection = subDirection;
-        this.destination = destination;
+        this.leftDestination = leftDestination;
+        this.rightDestination = rightDestination;
     }
 
     public JanggiCoordinate getDirection() {
         return direction;
     }
 
-    public List<JanggiCoordinate> getSubDirection() {
-        return subDirection;
+    public List<JanggiCoordinate> getLeftDestination() {
+        return leftDestination;
     }
 
-    public List<JanggiCoordinate> getDestination() {
-        return destination;
+    public List<JanggiCoordinate> getRightDestination() {
+        return rightDestination;
     }
 }
