66b98ac342b98ce26820226249e4024cc2f6a00a
ryan-dia <cjfdnjs158@naver.com>
2025-03-20 15:16:14 +0900

    fix: 이동 경로 좌표 수정
    Co-authored-by:saera-yook<yook.saera@gmail.com>

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -78,11 +78,11 @@ public class Point {
     }
 
     public Point left() {
-        return new Point(x, y - 1);
+        return new Point(x - 1, y);
     }
 
     public Point right() {
-        return new Point(x, y + 1);
+        return new Point(x + 1, y);
     }
 
     public Point rightUp() {
