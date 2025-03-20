411acfe01da1b9dec02337d8fd559ba8b5c40368
rladmstn <oz33177351@gmail.com>
2025-03-20 10:57:59 +0900

    test: 이동한 위치에 존재하는 상대 기물 삭제 테스트 작성

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -1,6 +1,8 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import domain.piece.Pawn;
 import domain.piece.Piece;
@@ -64,5 +66,35 @@ class BoardTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> board.move(han, startPosition, targetPosition))
                 .withMessage("[ERROR] 도착 위치에 아군의 기물이 존재해 이동할 수 없습니다.");
+    }
+
+    @Test
+    void 이동한_위치에_존재하는_상대_기물을_삭제한다() {
+        // given
+        Position startPosition = Position.of(1, 1);
+        Position targetPosition = Position.of(1, 7);
+
+        Player han = new Player("한", PieceColor.RED);
+        Player cho = new Player("초", PieceColor.BLUE);
+
+        List<Piece> hanPieces = PieceInit.initHanPieces();
+        List<Piece> choPieces = PieceInit.initChoPieces();
+
+        Map<Player, Pieces> boardElements = new HashMap<>();
+        boardElements.put(han, new Pieces(hanPieces));
+        boardElements.put(cho, new Pieces(choPieces));
+
+        Board board = new Board(boardElements);
+
+        board.move(han, Position.of(1, 4), Position.of(2, 4));
+
+        // when
+        board.move(han, startPosition, targetPosition);
+
+        // then
+        assertAll(() -> {
+            assertThat(choPieces).hasSize(15);
+            assertThat(choPieces).doesNotContain(new Pawn(1, 7, PieceDirection.CHO_PAWN.get()));
+        });
     }
 }
