5be3c4016a85baab4164b3ff846065b37f84dab8
Han <jumdo12@gmail.com>
2025-03-20 13:08:59 +0900

    refactor(Country): country 네임 필드 추가

--- src/main/java/domain/piece/Country.java
+++ src/main/java/domain/piece/Country.java
@@ -1,13 +1,23 @@
 package domain.piece;
 
 public enum Country {
-    CHO,
-    HAN;
+    CHO("초"),
+    HAN("한");
+
+    private final String countryName;
+
+    Country(String countryName) {
+        this.countryName = countryName;
+    }
 
     public static Country convertTurn(Country country){
         if(country == Country.HAN){
             return Country.CHO;
         }
         return Country.HAN;
+    }
+
+    public String getCountryName() {
+        return countryName;
     }
 }
