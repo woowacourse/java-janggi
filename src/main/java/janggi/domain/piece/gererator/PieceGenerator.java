6e5196a0e7a9b9a1aeec79abec1c9bd81350af49
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-20 19:31:46 +0900

    remove: 사용하지 않는 PieceGenerator 제거

--- src/main/java/janggi/domain/piece/gererator/PieceGenerator.java
+++
@@ -1,9 +0,0 @@
-package janggi.domain.piece.gererator;
-
-import janggi.domain.piece.Piece;
-import java.util.List;
-
-public interface PieceGenerator {
-
-    List<Piece> generate(KnightElephantSetting knightElephantSetting);
-}
