4bc5cfbbf6637df233b7be2a139b95f109fca91c
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-20 15:05:08 +0900

    feat: Piece 닉네임 필드 추가

--- src/main/java/piece/Piece.java
+++ src/main/java/piece/Piece.java
@@ -1,24 +1,31 @@
 package piece;
 
-import move.MoveStrategy;
-import move.Position;
 import direction.Point;
 import java.util.Objects;
 import move.MovementRule;
 
 public class Piece {
 
+    private final String nickname;
     private final MovementRule movementRule;
     private Point point;
 
+    public Piece(String nickname, Point point, MovementRule movementRule) {
+        this.nickname = nickname;
+        this.movementRule = movementRule;
+        this.point = point;
+    }
 
-public abstract class Piece {
-    private final MoveStrategy moveStrategy;
-    private Position position;
+    public String getName() {
+        return nickname;
+    }
 
-    public Piece(MoveStrategy moveStrategy, Position position) {
-        this.moveStrategy = moveStrategy;
-        this.position = position;
+    public Point getPosition() {
+        return point;
+    }
+
+    public void move(Pieces allPieces, Point to) {
+        point = movementRule.move(allPieces, point, to);
     }
 
     @Override
