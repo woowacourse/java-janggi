b318161d51db1472619d3b15fc79c4cf49c19ecf
BWY <quantumjump10@gmail.com>
2025-03-20 16:46:25 +0900

    test: Elephant 움직임 테스트 추가

--- src/test/java/janggi/piece/ElephantTest.java
+++ src/test/java/janggi/piece/ElephantTest.java
@@ -3,6 +3,7 @@ package janggi.piece;
 import janggi.Team;
 import janggi.board.Board;
 import janggi.board.Position;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import static janggi.fixture.PositionFixture.createPosition;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
 
 class ElephantTest {
@@ -35,4 +37,23 @@ class ElephantTest {
                 .asInstanceOf(MAP)
                 .containsEntry(goal, piece);
     }
+
+    @Test
+    void 이동_경로에_다른_기물이_있을_경우_예외를_발생한다() {
+        // given
+        Map<Position, Piece> initialBoard = new HashMap<>();
+        Position start = createPosition(4, 4);
+        Position goal = createPosition(6, 7);
+        Elephant piece = new Elephant(Team.GREEN);
+
+        initialBoard.put(start, piece);
+        initialBoard.put(createPosition(4,5), new Soldier(Team.GREEN));
+        Board board = new Board(initialBoard);
+
+        // then
+        assertThatThrownBy(() -> board.movePiece(start, goal, Team.GREEN))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 해당 경로에 다른 기물이 있어 이동할 수 없습니다.");
+    }
+
 }
