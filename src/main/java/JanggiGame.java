4b635ce0f6086de009db2e10114f9ead7b02d407
RaZel713 <razel7130@gmail.com>
2025-03-20 11:11:34 +0900

    feat: 왕조와 출발점과 목적지를 기준으로 기물을 이동시키는 기능 구현

--- src/main/java/JanggiGame.java
+++ src/main/java/JanggiGame.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JanggiGame {
@@ -21,5 +22,31 @@ public class JanggiGame {
 
     public Map<Dot, Piece> getPieces() {
         return pieces; //todo 방어적 복사로 바꾸기
+    }
+
+    public void movePiece(Dynasty dynasty, Dot origin, Dot destination) {
+        if (!pieces.containsKey(origin)) {
+            throw new IllegalArgumentException("[ERROR] 입력 받은 위치에 기물이 존재하지 않습니다.");
+        }
+        if (pieces.get(origin).getDynasty() != dynasty) {
+            throw new IllegalArgumentException("[ERROR] 입력 받은 위치의 기물이 현 사용자 소유의 기물이 아닙니다.");
+        }
+
+        Piece originPiece = pieces.get(origin);
+
+        List<Dot> route = originPiece.getRoute(origin, destination);
+
+        Map<Dot, Piece> routeWithPiece = new HashMap<>();
+
+        for (Dot dot : route) {
+            routeWithPiece.put(dot, pieces.getOrDefault(dot, null));
+        }
+
+        boolean canMove = originPiece.canMove(routeWithPiece, pieces.getOrDefault(destination, null));
+
+        if (canMove) {
+            pieces.remove(origin);
+            pieces.put(destination, originPiece);
+        }
     }
 }
