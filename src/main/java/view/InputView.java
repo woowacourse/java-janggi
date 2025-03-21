c74d64149fa3f43df02c1f72bee9284481a7f83c
DongYeop <s01054830455@gmail.com>
2025-03-21 17:14:50 +0900

    refactor: 입력 뷰 리팩터링
    - 메서드 분리
    - 매직넘버 & 매직 스트링 상수화
    - 입력 좌표 갯수 검증 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,33 +1,55 @@
 package view;
 
-import chessPiece.BoardPosition;
 import java.util.Scanner;
+import position.BoardPosition;
 
 public class InputView {
 
+    private static final int DX = 0;
+    private static final int DY = 1;
+    private static final int POSITION_SIZE = 2;
+    private static final String POSITION_DELIMITER = ",";
+
     private final Scanner scanner = new Scanner(System.in);
 
-    public BoardPosition readPresentPick() {
-        System.out.println("초나라 턴 입니다. 움직이고 싶은 말의 위치를 입력해주세요.");
+    public BoardPosition readPresentPosition() {
+        System.out.println("초나라 턴 입니다. 움직이고 싶은 말의 위치를 입력해주세요.(좌표는 쉼표(,) 기준으로 구분)");
 
         String inputPosition = readLine().trim();
 
-        String[] split = inputPosition.split(",");
-        return new BoardPosition(parseInt(split[0]), parseInt(split[1]));
+        String[] splitPosition = inputSplit(inputPosition);
+        validatePositionSize(splitPosition);
+
+        return new BoardPosition(parseInt(splitPosition[DX]), parseInt(splitPosition[DY]));
     }
 
-    public BoardPosition readFuturePick() {
-        System.out.println("해당 말을 이동시킬 위치를 입려해 주세요.");
+    public BoardPosition readFuturePosition() {
+        System.out.println("해당 말을 이동시킬 위치를 입려해 주세요.(좌표는 쉼표(,) 기준으로 구분)");
 
         String inputPosition = readLine().trim();
 
-        String[] split = inputPosition.split(",");
-        return new BoardPosition(parseInt(split[0]), parseInt(split[1]));
+        String[] splitPosition = inputSplit(inputPosition);
+        validatePositionSize(splitPosition);
+
+        return new BoardPosition(parseInt(splitPosition[DX]), parseInt(splitPosition[DY]));
+    }
+
+    private void validatePositionSize(final String[] split) {
+        if (split.length != POSITION_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 좌표는 쉼표로 구분된 두개를 입력해야 합니다.");
+        }
+    }
+
+    private String[] inputSplit(final String inputPosition) {
+        if (!inputPosition.contains(POSITION_DELIMITER)) {
+            throw new IllegalArgumentException("[ERROR] 좌표는 쉼표(,)로 구분해야 합니다.");
+        }
+        return inputPosition.split(POSITION_DELIMITER);
     }
 
     private int parseInt(final String s) {
         try {
-            return Integer.parseInt(s);
+            return Integer.parseInt(s.trim());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 좌표는 숫자만 입력 할 수 있습니다.");
         }
