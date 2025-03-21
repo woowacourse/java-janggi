bbb26262215a3ca7761b85291f532a224e279adf
Han <jumdo12@gmail.com>
2025-03-21 23:48:29 +0900

    feat: 구현을 다시하기 위해 파일 삭제

--- src/main/java/Application.java
+++
@@ -1,9 +0,0 @@
-import domain.JanggiGame;
-
-public class Application {
-
-    public static void main(String[] args) {
-        JanggiGame janggiGame = new JanggiGame();
-        janggiGame.start();
-    }
-}
