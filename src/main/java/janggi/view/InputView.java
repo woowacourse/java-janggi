b43c1062563f22997ed5bd3a89d16cdcb429449a
jhan0121 <jhan0121@gmail.com>
2025-03-21 18:32:46 +0900

    feat: 플레이어 이름 입력 기능 추가

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -1,6 +1,8 @@
 package janggi.view;
 
+import janggi.domain.Player;
 import janggi.domain.Position;
+import janggi.domain.Team;
 import java.util.Scanner;
 
 public class InputView {
@@ -34,12 +36,30 @@ public class InputView {
         try {
             String input = scanner.nextLine();
             validateEmptyInput(input);
-            String[] splitInput = input.split(" ");
-            int row = Integer.parseInt(splitInput[0]);
-            int column = Integer.parseInt(splitInput[1]);
-            return Position.of(row, column);
+            return parsePosition(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
+    }
+
+    private Position parsePosition(final String input) {
+        String[] splitInput = input.split(" ");
+        validateSplitSize(splitInput);
+        int row = Integer.parseInt(splitInput[0]);
+        int column = Integer.parseInt(splitInput[1]);
+        return Position.of(row, column);
+    }
+
+    private void validateSplitSize(final String[] splitInput) {
+        if (splitInput.length != 2) {
+            throw new IllegalArgumentException("행 열 숫자 2개를 입력해주세요.");
+        }
+    }
+
+    public Player readPlayer(Team team) {
+        System.out.println(team.getCountry() + " 팀에 참가할 플레이어 이름을 입력해주세요.");
+        String input = scanner.nextLine();
+        validateEmptyInput(input);
+        return new Player(input, team);
     }
 }
