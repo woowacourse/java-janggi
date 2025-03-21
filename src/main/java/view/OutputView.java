f5d73350e7ebad6ee5bf2ae980323562cd390cfc
DongYeop <s01054830455@gmail.com>
2025-03-21 20:58:57 +0900

    fix: 장기말 이동 후, 이동 전 말들이 사라지지 않는 문제 해결

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,63 +1,78 @@
 package view;
 
-import chessPiece.BoardPosition;
 import chessPiece.ChessPiece;
-import chessPiece.Nation;
 import java.util.Map;
+import position.BoardPosition;
 
 public class OutputView {
 
     private static final String RED_COLOR_CODE = "\u001B[31m";
     private static final String GREEN_COLOR_CODE = "\u001B[32m";
     private static final String EXIT_CODE = "\u001B[0m";
+
     private static final String EMPTY_SPACE = "ㅤ";
-    private static final String[][] JANGGIPAN = new String[11][10];
 
-    public void printJanggipan(Map<BoardPosition, ChessPiece> board) {
-        String[][] janggipan = JANGGIPAN;
-        createJanggipan(board, janggipan);
-        formatJanggipan(janggipan);
+    private static final int BOARD_WIDTH = 9;
+    private static final int BOARD_HEIGHT = 10;
+    private static final String[][] JANGGI_BOARD_ARR = new String[BOARD_HEIGHT + 1][BOARD_WIDTH + 1];
+
+    public void printJanggiBoard(Map<BoardPosition, ChessPiece> janggiBoard) {
+        initializeJanggiBoard(janggiBoard);
+        printFormattedJanggiBoard();
     }
 
-    private void createJanggipan(final Map<BoardPosition, ChessPiece> board, final String[][] janggipan) {
-        for (BoardPosition boardPosition : board.keySet()) {
-            int x = boardPosition.getRow() + 1;
-            int y = boardPosition.getCol() + 1;
+    private void initializeJanggiBoard(final Map<BoardPosition, ChessPiece> janggiBoard) {
+        setBoardWithEmptySpaces();
+        placeChessPieces(janggiBoard);
+        setBoardLabels();
+    }
 
-            ChessPiece chessPiece = board.get(boardPosition);
-            String name = chessPiece.getName();
-            if (chessPiece.getPieceProfile().getNation().equals(Nation.HAN)) {
-                name = RED_COLOR_CODE + chessPiece.getName() + EXIT_CODE;
+    private void setBoardWithEmptySpaces() {
+        for (int i = 1; i <= BOARD_HEIGHT; i++) {
+            for (int j = 1; j <= BOARD_WIDTH; j++) {
+                JANGGI_BOARD_ARR[i][j] = " | " + EMPTY_SPACE;
             }
-
-            if (chessPiece.getPieceProfile().getNation().equals(Nation.CHO)) {
-                name = GREEN_COLOR_CODE + chessPiece.getName() + EXIT_CODE;
-            }
-            janggipan[x][y] = " | " + name;
         }
     }
 
-    private void formatJanggipan(final String[][] janggipan) {
-        for (int i = 1; i < janggipan.length; i++) {
-            for (int j = 1; j < janggipan[i].length; j++) {
-                if (janggipan[i][j] == null) {
-                    janggipan[i][j] = " | " + EMPTY_SPACE;
-                }
-            }
+    private void placeChessPieces(final Map<BoardPosition, ChessPiece> janggiBoard) {
+        for (BoardPosition boardPosition : janggiBoard.keySet()) {
+            int row = boardPosition.getRow() + 1;
+            int col = boardPosition.getCol() + 1;
+
+            String pieceName = getColoredPieceName(janggiBoard.get(boardPosition));
+            JANGGI_BOARD_ARR[row][col] = " | " + pieceName;
+        }
+    }
+
+    private String getColoredPieceName(final ChessPiece chessPiece) {
+        String name = chessPiece.getName();
+        if (chessPiece.isChoNation()) {
+            return GREEN_COLOR_CODE + name + EXIT_CODE;
+        }
+        if (chessPiece.isHanNation()) {
+            return RED_COLOR_CODE + name + EXIT_CODE;
+        }
+        return name;
+    }
+
+    private void setBoardLabels() {
+        JANGGI_BOARD_ARR[0][0] = EMPTY_SPACE + EMPTY_SPACE;
+
+        for (int row = 1; row <= BOARD_HEIGHT; row++) {
+            JANGGI_BOARD_ARR[row][0] = (row - 1) + EMPTY_SPACE;
         }
 
-        for (int i = 1; i < 10; i++) {
-            janggipan[0][0] = EMPTY_SPACE + EMPTY_SPACE;
-            janggipan[0][i] = EMPTY_SPACE + " " + (i - 1) + EMPTY_SPACE;
+        for (int col = 1; col <= BOARD_WIDTH; col++) {
+            JANGGI_BOARD_ARR[0][col] = EMPTY_SPACE + " " + (col - 1) + EMPTY_SPACE;
         }
+    }
 
-        for (int i = 1; i <= 10; i++) {
-            janggipan[i][0] = i - 1 + EMPTY_SPACE;
-        }
-
-        for (int i = 0; i < janggipan.length; i++) {
-            for (int j = 0; j < janggipan[i].length; j++) {
-                System.out.print(janggipan[i][j]);
+    private void printFormattedJanggiBoard() {
+        System.out.println();
+        for (final String[] row : OutputView.JANGGI_BOARD_ARR) {
+            for (final String cell : row) {
+                System.out.print(cell);
             }
             System.out.println();
         }
