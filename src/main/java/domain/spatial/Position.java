4e5e2b22448c40941eb57edceb481a0129924824
taek2222 <oot309@daum.net>
2025-03-22 16:17:13 +0900

    refactor: record 형태 변경 및 움직임 및 검증 로직 변경
    - 움직임을 더 명확한 네이밍으로 변경하고, Vector 클래스를 추가해 이동 파라미터를 받는 형태로 변경했습니다.
    
    - 유효한 좌표인지는 생성자에서 검증하도록 변경하고, 이동한 후 올바른 좌표가 맞는지의 역할로 변경했습니다.

--- src/main/java/domain/spatial/Position.java
+++ src/main/java/domain/spatial/Position.java
@@ -1,64 +1,35 @@
 package domain.spatial;
 
-import java.util.Objects;
-
-public class Position {
+public record Position(
+        int row,
+        int column
+) {
 
     public static final int MIN_ROW = 1;
     public static final int MIN_COLUMN = 1;
     public static final int MAX_ROW = 9;
     public static final int MAX_COLUMN = 10;
 
-    private final int row;
-    private final int column;
-
-    private Position(final int row, final int column) {
-        this.row = row;
-        this.column = column;
-    }
-
-    public static Position of(final int row, final int column) {
+    public Position {
         validateRange(row, column);
-        return new Position(row, column);
     }
 
-    public static Position ofDirection(final int row, final int column) {
-        return new Position(row, column);
+    public Position moveBy(final Vector vector) {
+        return vector.applyTo(this);
     }
 
-    private static void validateRange(final int row, final int column) {
+    public boolean isMoveValid(final Vector vector) {
+        try {
+            vector.applyTo(this);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private void validateRange(final int row, final int column) {
         if (row < MIN_ROW || column < MIN_COLUMN || row > MAX_ROW || column > MAX_COLUMN) {
             throw new IllegalArgumentException("[ERROR] 좌표 입력은 9X10 보드 이내만 가능합니다.");
         }
-    }
-
-    public Position merge(final Position other) {
-        return new Position(this.row + other.row, this.column + other.column);
-    }
-
-    public boolean isValid() {
-        return !(row < MIN_ROW || column < MIN_COLUMN || row > MAX_ROW || column > MAX_COLUMN);
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        Position position = (Position) object;
-        return row == position.row && column == position.column;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(row, column);
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
     }
 }
