1ed4a4d579b70b12293228adf5da3c627fb2226c
DongYeop <s01054830455@gmail.com>
2025-03-22 19:16:15 +0900

    feat: 장기게임 실행 클래스 추가
    - 장기 게임을 실행한다
    - 잘못된 입력을 하면 예외를 던지고 재입력 한다.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,28 +1,17 @@
-import chessPiece.Board;
-import chessPiece.BoardPosition;
-import chessPiece.ChessPieceInitializer;
-import view.InputView;
-import view.OutputView;
+import janggi.JanggiGame;
+import janggi.view.InputView;
+import janggi.view.OutputView;
 
 public class Application {
+
     public static void main(String[] args) {
 
-        ChessPieceInitializer chessPieceInitializer = new ChessPieceInitializer();
-        Board board = new Board(chessPieceInitializer.hanInit(), chessPieceInitializer.choInit());
         OutputView outputView = new OutputView();
         InputView inputView = new InputView();
 
-        while (true) {
-            try {
-                outputView.printJanggipan(board.getJanggiPan());
-                BoardPosition presentPosition = inputView.readPresentPick();
-                BoardPosition futurePosition = inputView.readFuturePick();
-
-                board.updateBoard(presentPosition, futurePosition);
-            } catch (IllegalArgumentException e) {
-                outputView.printErrorMessage(e.getMessage());
-            }
-        }
+        JanggiGame janggiGame = new JanggiGame(outputView, inputView);
+        janggiGame.startGame();
     }
+
 }
 
---
+++ src/main/java/janggi/JanggiGame.java
@@ -0,0 +1,59 @@
+package janggi;
+
+import janggi.board.Board;
+import janggi.board.ChessPieceInitializer;
+import janggi.position.BoardPosition;
+import janggi.view.InputView;
+import janggi.view.OutputView;
+
+public class JanggiGame {
+
+    private final OutputView outputView;
+    private final InputView inputView;
+
+    public JanggiGame(final OutputView outputView, final InputView inputView) {
+        this.outputView = outputView;
+        this.inputView = inputView;
+    }
+
+    public void startGame() {
+        Board board = setJanggiBoard();
+        while (true) {
+            outputView.printJanggiBoard(board.getJanggiPan());
+
+            try {
+                BoardPosition presentPosition = readPresentPosition();
+                BoardPosition futurePosition = readFuturePosition();
+                board.updateBoard(presentPosition, futurePosition);
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+                continue;
+            }
+        }
+    }
+
+    private Board setJanggiBoard() {
+        ChessPieceInitializer chessPieceInitializer = new ChessPieceInitializer();
+        return new Board(chessPieceInitializer.hanInit(), chessPieceInitializer.choInit());
+    }
+
+    private BoardPosition readPresentPosition() {
+        while (true) {
+            try {
+                return inputView.readPresentPosition();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private BoardPosition readFuturePosition() {
+        while (true) {
+            try {
+                return inputView.readFuturePosition();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+}
