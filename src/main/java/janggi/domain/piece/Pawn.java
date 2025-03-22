b240556b65ac1a7a9e5d2d7d4d43a0013945eb26
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-22 16:47:17 +0900

    refactor: 폰 이동 로직 최적화

--- src/main/java/janggi/domain/piece/Pawn.java
+++ src/main/java/janggi/domain/piece/Pawn.java
@@ -8,44 +8,69 @@ import java.util.Set;
 
 public class Pawn extends Piece {
 
+    private static final int MOVABLE_DISTANCE = 1;
+
     public Pawn(Side side, int x, int y) {
         super(side, x, y);
     }
 
     @Override
     protected boolean isMoveablePosition(Position destination) {
-        return getMovablePositions().contains(destination);
+        if (!isVerticalMove(destination)) {
+            return destination.getXDistance(getPosition()) == MOVABLE_DISTANCE;
+        }
+        if (getSide() == Side.HAN) {
+            return destination.getY() == getYPosition() + MOVABLE_DISTANCE;
+        }
+        return destination.getY() == getYPosition() - MOVABLE_DISTANCE;
     }
 
     @Override
     protected boolean isMoveablePath(List<Piece> existingPieces, Position destination) {
-        Set<Position> moveablePositions = getMovablePositions();
+        List<Piece> onPathPieces = findAllPiecesOnPath(existingPieces, destination);
 
-        List<Piece> pieces = existingPieces.stream()
-                .filter(existingPiece -> existingPiece.isSamePosition(destination))
-                .filter(existingPiece -> moveablePositions.contains(existingPiece.getPosition()))
-                .toList();
-
-        if (pieces.isEmpty()) {
-            return true;
+        if (!onPathPieces.isEmpty()) {
+            return onPathPieces.stream()
+                    .filter(onPathPiece -> onPathPiece.isSamePosition(destination))
+                    .anyMatch(piece -> piece.getSide() != getSide());
         }
-        return pieces.getFirst().getSide() != getSide();
+        return true;
     }
 
-    private Set<Position> getMovablePositions() {
-        Set<Position> moveablePositions = new HashSet<>();
-        if (getXPosition() > 0) {
-            moveablePositions.add(new Position(getXPosition() - 1, getYPosition()));
-        }
-        if (getXPosition() < 9) {
-            moveablePositions.add(new Position(getXPosition() + 1, getYPosition()));
-        }
+    private List<Piece> findAllPiecesOnPath(List<Piece> existingPieces, Position destination) {
+        Set<Position> path = new HashSet<>(findPaths(destination));
 
-        if (getSide() == Side.HAN) {
-            moveablePositions.add(new Position(getXPosition(), getYPosition() + 1));
-            return moveablePositions;
+        return existingPieces.stream()
+                .filter(existingPiece -> path.contains(existingPiece.getPosition()))
+                .toList();
+    }
+
+    private List<Position> findPaths(Position destination) {
+        if (isVerticalMove(destination)) {
+            return findAllVerticalMovablePositions();
         }
-        moveablePositions.add(new Position(getXPosition(), getYPosition() - 1));
-        return moveablePositions;
+        return findAllHorizontalMovablePositions(destination);
+    }
+
+    private List<Position> findAllVerticalMovablePositions() {
+        int x = getXPosition();
+        int y = getYPosition();
+        if (getSide() == Side.HAN) {
+            return List.of(new Position(x, y + MOVABLE_DISTANCE));
+        }
+        return List.of(new Position(x, y - MOVABLE_DISTANCE));
+    }
+
+    private List<Position> findAllHorizontalMovablePositions(Position destination) {
+        int x = getXPosition();
+        int y = getYPosition();
+        if (destination.getX() > getXPosition()) {
+            return List.of(new Position(x + MOVABLE_DISTANCE, y));
+        }
+        return List.of(new Position(x - MOVABLE_DISTANCE, y));
+    }
+
+    private boolean isVerticalMove(Position destination) {
+        return destination.hasSameX(getPosition());
     }
 }
