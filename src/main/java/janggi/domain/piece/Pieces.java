288d84f1d4367198ef9ec8846e22f7e50be68fe4
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-23 03:01:18 +0900

    feat: Piece 일급 컬렉션 구현

---
+++ src/main/java/janggi/domain/piece/Pieces.java
@@ -0,0 +1,44 @@
+package janggi.domain.piece;
+
+import janggi.domain.Position;
+import janggi.domain.Side;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Pieces {
+
+    private final Map<Position, Piece> values;
+
+    public Pieces(Map<Position, Piece> values) {
+        this.values = values;
+    }
+
+    public static Pieces from(List<Piece> pieces) {
+        return new Pieces(pieces.stream().collect(Collectors.toMap(Piece::getPosition, Function.identity())));
+    }
+
+    public boolean isEnemyOnDestination(Side side, Position destination) {
+        return values.values().stream()
+                .filter(onPathPiece -> onPathPiece.isSamePosition(destination))
+                .anyMatch(piece -> piece.getSide() != side);
+    }
+
+    public Pieces getPiecesOnPath(Set<Position> pathsToDestination) {
+        return from(
+                values.values().stream()
+                        .filter(piece -> pathsToDestination.contains(piece.getPosition()))
+                        .toList()
+        );
+    }
+
+    public boolean isEmpty() {
+        return values.isEmpty();
+    }
+
+    public Map<Position, Piece> getValues() {
+        return values;
+    }
+}
