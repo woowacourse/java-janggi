75760c1f81dbecd90a445c5f304195ed640a4e9a
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-23 03:03:24 +0900

    feat: 고정된 움직임 전략 인터페이스 추가

---
+++ src/main/java/janggi/domain/piece/movement/fixed/FixedMovementStrategy.java
@@ -0,0 +1,27 @@
+package janggi.domain.piece.movement.fixed;
+
+import janggi.domain.Position;
+import janggi.domain.Side;
+import janggi.domain.piece.Pieces;
+import janggi.domain.piece.movement.MovementStrategy;
+
+public interface FixedMovementStrategy extends MovementStrategy {
+
+    @Override
+    default boolean isMoveable(Pieces map, Position origin, Side side, Position destination) {
+        return isLegalDestination(origin, destination) && isPathClear(map, side, origin, destination);
+    }
+
+    default boolean isPathClear(Pieces map, Side side, Position origin, Position destination) {
+        Pieces onPathPieces = getAllPiecesOnPath(map, origin, destination);
+
+        if (!onPathPieces.isEmpty()) {
+            return onPathPieces.isEnemyOnDestination(side, destination);
+        }
+        return true;
+    }
+
+    boolean isLegalDestination(Position origin, Position destination);
+
+    Pieces getAllPiecesOnPath(Pieces map, Position origin, Position destination);
+}
---
+++ src/test/java/janggi/domain/piece/movement/fixed/FixedMovementStrategyTest.java
@@ -0,0 +1,180 @@
+package janggi.domain.piece.movement.fixed;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import janggi.domain.Position;
+import janggi.domain.ReplaceUnderBar;
+import janggi.domain.Side;
+import janggi.domain.piece.Elephant;
+import janggi.domain.piece.Piece;
+import janggi.domain.piece.Pieces;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@ReplaceUnderBar
+class FixedMovementStrategyTest {
+
+    private static final Side ALLEY_SIDE = Side.HAN;
+    private static final Side ENEMY_SIDE = Side.CHO;
+    private final RawFixedMovementStrategy rawFixedMovementStrategy = new RawFixedMovementStrategy();
+
+    private static Stream<Arguments> 목적지까지의_경로에_기물이_없는지_확인한다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        ALLEY_SIDE,
+                        new Position(5, 5),
+                        new Position(7, 8),
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        false
+                ),
+                Arguments.of(
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        ALLEY_SIDE,
+                        new Position(5, 5),
+                        new Position(8, 7),
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        false
+                ),
+                Arguments.of(
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        ALLEY_SIDE,
+                        new Position(5, 5),
+                        new Position(8, 7),
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        false
+                )
+        );
+    }
+
+    private static Stream<Arguments> 목적지에_아군이_있다면_움직일_수_없다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        ALLEY_SIDE,
+                        new Position(5, 5),
+                        new Position(7, 8),
+                        createPieces(new Elephant(ALLEY_SIDE, 7, 8))
+                ),
+                Arguments.of(
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        ALLEY_SIDE,
+                        new Position(5, 5),
+                        new Position(8, 7),
+                        createPieces(new Elephant(ALLEY_SIDE, 8, 7))
+                )
+        );
+    }
+
+    private static Stream<Arguments> 목적지에_적군이_있다면_움직일_수_없다_테스트_케이스() {
+        return Stream.of(
+                Arguments.of(
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        ALLEY_SIDE,
+                        new Position(5, 5),
+                        new Position(7, 8),
+                        createPieces(new Elephant(ENEMY_SIDE, 7, 8))
+                ),
+                Arguments.of(
+                        createPieces(new Elephant(ALLEY_SIDE, 5, 6)),
+                        ALLEY_SIDE,
+                        new Position(5, 5),
+                        new Position(8, 7),
+                        createPieces(new Elephant(ENEMY_SIDE, 8, 7))
+                )
+        );
+    }
+
+    private static Pieces createPieces(Piece... pieces) {
+        return new Pieces(Arrays.stream(pieces).collect(Collectors.toMap(Piece::getPosition, Function.identity())));
+    }
+
+    @ParameterizedTest
+    @MethodSource("목적지까지의_경로에_기물이_없는지_확인한다_테스트_케이스")
+    void 목적지까지의_경로에_기물이_없는지_확인한다(
+            Pieces map,
+            Side side,
+            Position origin,
+            Position destination,
+            Pieces onPathPieces,
+            boolean expected
+    ) {
+
+        rawFixedMovementStrategy.setIsLegalDestination(true);
+        rawFixedMovementStrategy.setAllPiecesOnPath(onPathPieces);
+
+        assertThat(rawFixedMovementStrategy.isPathClear(map, side, origin, destination)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("목적지에_아군이_있다면_움직일_수_없다_테스트_케이스")
+    void 목적지에_아군이_있다면_움직일_수_없다(
+            Pieces map,
+            Side side,
+            Position origin,
+            Position destination,
+            Pieces onPathPieces
+    ) {
+
+        rawFixedMovementStrategy.setIsLegalDestination(true);
+        rawFixedMovementStrategy.setAllPiecesOnPath(onPathPieces);
+
+        assertThat(rawFixedMovementStrategy.isPathClear(map, side, origin, destination)).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("목적지에_적군이_있다면_움직일_수_없다_테스트_케이스")
+    void 목적지에_적군이_있다면_움직일_수_없다(
+            Pieces map,
+            Side side,
+            Position origin,
+            Position destination,
+            Pieces onPathPieces
+    ) {
+
+        rawFixedMovementStrategy.setIsLegalDestination(true);
+        rawFixedMovementStrategy.setAllPiecesOnPath(onPathPieces);
+
+        assertThat(rawFixedMovementStrategy.isPathClear(map, side, origin, destination)).isTrue();
+    }
+
+    @Test
+    void 움직일_수_있는_위치이고_경로에_아무도_없다면_움직일_수_있다() {
+        rawFixedMovementStrategy.setIsLegalDestination(true);
+        rawFixedMovementStrategy.setAllPiecesOnPath(new Pieces(Map.of()));
+
+        assertThat(rawFixedMovementStrategy.isMoveable(createPieces(), new Position(0, 0), ALLEY_SIDE,
+                new Position(0, 0))).isTrue();
+    }
+
+    private static final class RawFixedMovementStrategy implements FixedMovementStrategy {
+
+        private boolean isLegalDestination = true;
+        private Pieces allPiecesOnPath = new Pieces(Map.of());
+
+        public void setIsLegalDestination(boolean legalDestination) {
+            isLegalDestination = legalDestination;
+        }
+
+        public void setAllPiecesOnPath(Pieces allPiecesOnPath) {
+            this.allPiecesOnPath = allPiecesOnPath;
+        }
+
+        @Override
+        public boolean isLegalDestination(Position origin, Position destination) {
+            return isLegalDestination;
+        }
+
+        @Override
+        public Pieces getAllPiecesOnPath(Pieces map, Position origin, Position destination) {
+            return allPiecesOnPath;
+        }
+    }
+}
