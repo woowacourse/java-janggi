4034acbdb7e926abc25c6228486c4e8fe1f9c597
seaniiio <cnc4750@naver.com>
2025-03-23 01:27:41 +0900

    refactor: 사용하지 않는 메서드와 필드 제거

--- src/main/java/domain/piece/JanggiSide.java
+++ src/main/java/domain/piece/JanggiSide.java
@@ -7,12 +7,6 @@ public enum JanggiSide {
     HAN,
     NONE;
 
-    private static final List<JanggiSide> VALID_SIDES = List.of(CHO, HAN);
-
-    public static List<JanggiSide> getValidSides() {
-        return VALID_SIDES;
-    }
-
     public JanggiSide getOppositeSide() {
         if (this == CHO) {
             return HAN;
