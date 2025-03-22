cd5ef8dc52fd6c7f9ec1f5b4a5d6885392c0093f
ChoiDongJoon <adong0808@naver.com>
2025-03-22 15:33:11 +0900

    refactor(domain): 인스턴스 변수에서 JanggiBoard 제거

--- src/main/java/janggi/JanggiGame.java
+++ src/main/java/janggi/JanggiGame.java
@@ -10,26 +10,25 @@ import java.util.List;
 
 public class JanggiGame {
 
-    private final JanggiBoard board;
     private final InputView inputView;
     private final OutputView outputView;
 
     public JanggiGame(final InputView inputView, final OutputView outputView) {
-        this.board = JanggiBoard.initializeWithPieces();
         this.inputView = inputView;
         this.outputView = outputView;
     }
 
     public void play() {
+        JanggiBoard board = JanggiBoard.initializeWithPieces();
         while (true) {
             try {
                 outputView.printBoard(board);
 
                 Position selectedPiecePosition = inputView.selectPiece();
 
-                List<Position> reachablePositions = computeReachableDestinations(selectedPiecePosition);
+                List<Position> reachablePositions = computeReachableDestinations(board, selectedPiecePosition);
 
-                Piece catchedPiece = processMove(selectedPiecePosition, reachablePositions);
+                Piece catchedPiece = processMove(board, selectedPiecePosition, reachablePositions);
 
                 if (board.checkGameIsOver(catchedPiece)) {
                     break;
@@ -40,13 +39,13 @@ public class JanggiGame {
         }
     }
 
-    private List<Position> computeReachableDestinations(final Position selectedPiecePosition) {
+    private List<Position> computeReachableDestinations(final JanggiBoard board, final Position selectedPiecePosition) {
         List<Position> reachablePositions = board.computeReachableDestination(selectedPiecePosition);
         outputView.printReachableDestinations(reachablePositions);
         return reachablePositions;
     }
 
-    private Piece processMove(final Position selectedPiecePosition, final List<Position> reachablePositions) {
+    private Piece processMove(final JanggiBoard board, final Position selectedPiecePosition, final List<Position> reachablePositions) {
         Position destination = inputView.askMovableDestination();
         Piece catchedPiece = board.moveOrCatchPiece(selectedPiecePosition, destination, reachablePositions);
         outputView.printMoveResult(catchedPiece);
