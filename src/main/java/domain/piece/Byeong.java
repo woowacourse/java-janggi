07218d9df532c848d77e41ca2e18ce15a7e28de0
Han <jumdo12@gmail.com>
2025-03-22 22:54:33 +0900

    feat(Byeong): 병 움직임 기능 구현

---
+++ src/main/java/domain/piece/Byeong.java
@@ -0,0 +1,53 @@
+package domain.piece;
+
+import domain.Country;
+import domain.JanggiCoordinate;
+import domain.PieceType;
+import domain.board.JanggiBoard;
+
+public class Byeong extends Piece {
+    public Byeong(Country country) {
+        super(country, PieceType.BYEONG);
+    }
+
+    private void validateMoveCoordinate(JanggiBoard board, JanggiCoordinate from, JanggiCoordinate to) {
+        if (board.isOutOfBoundary(from) || board.isOutOfBoundary(to)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 좌표위치 입니다.");
+        }
+    }
+
+    private void validateByeongMove(JanggiCoordinate from, JanggiCoordinate to) {
+        if (super.getCountry() == Country.HAN) {
+            validateHanMove(from, to);
+            return;
+        }
+        validateChoMove(from, to);
+    }
+
+    private void validateHanMove(JanggiCoordinate from, JanggiCoordinate to) {
+        if (from.moveDown().equals(to) || from.moveRight().equals(to) || from.moveLeft().equals(to)) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 해당 위치로 기물을 이동할 수 없습니다.");
+    }
+
+    private void validateChoMove(JanggiCoordinate from, JanggiCoordinate to) {
+        if (from.moveUp().equals(to) || from.moveRight().equals(to) || from.moveLeft().equals(to)) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 해당 위치로 기물을 이동할 수 없습니다.");
+    }
+
+    private void validateTarget(JanggiBoard board, JanggiCoordinate to) {
+        if (board.isOccupied(to) && isSameCountry(board.findPieceByCoordinate(to))) {
+            throw new IllegalArgumentException("[ERROR] 나의 기물이 이미 해당 위치에 있습니다.");
+        }
+    }
+
+    @Override
+    public void validateMove(JanggiBoard board, JanggiCoordinate from, JanggiCoordinate to) {
+        validateMoveCoordinate(board, from, to);
+        validateByeongMove(from, to);
+        validateTarget(board, to);
+    }
+}
---
+++ src/test/java/domain/piece/ByeongTest.java
@@ -0,0 +1,101 @@
+package domain.piece;
+
+import domain.Country;
+import domain.JanggiCoordinate;
+import domain.board.JanggiBoard;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class ByeongTest {
+
+    @Nested
+    class ByeongCoordinateTest {
+
+        @DisplayName("움직이려는 기물의 시작 위치가 보드의 밖이면 에러를 반환한다.")
+        @Test
+        void validateMaFromCoordinate() {
+            Piece piece = new Byeong(Country.HAN);
+            JanggiBoard board = new JanggiBoard(new HashMap<>());
+
+            JanggiCoordinate outOfBoard = new JanggiCoordinate(0, 0);
+            JanggiCoordinate inBoard = new JanggiCoordinate(5, 5);
+
+            assertThatThrownBy(() -> piece.validateMove(board, outOfBoard, inBoard))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("움직이려는 기물의 도착 위치가 보드의 밖이면 에러를 반환한다.")
+        @Test
+        void validateMaToCoordinate() {
+            Piece piece = new Byeong(Country.HAN);
+            JanggiBoard board = new JanggiBoard(new HashMap<>());
+
+            JanggiCoordinate inBoard = new JanggiCoordinate(-1, -1);
+            JanggiCoordinate outOfBoard = new JanggiCoordinate(5, 5);
+
+            assertThatThrownBy(() -> piece.validateMove(board, inBoard, outOfBoard))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("내 기물은 내 기물을 잡으려고 할 수 없다.")
+        @Test
+        void validateTarget() {
+            Piece piece = new Byeong(Country.HAN);
+            Piece maEnemy = new Ma(Country.CHO);
+            Piece maOurTeam = new Ma(Country.HAN);
+            Map<JanggiCoordinate, Piece> map = new HashMap<>();
+
+            JanggiCoordinate myPiece = new JanggiCoordinate(5, 5);
+            JanggiCoordinate enemyMa = new JanggiCoordinate(5, 4);
+            JanggiCoordinate ourMa = new JanggiCoordinate(6, 5);
+
+            map.put(myPiece, piece);
+            map.put(enemyMa, maEnemy);
+            map.put(ourMa, maOurTeam);
+
+            JanggiBoard board = new JanggiBoard(map);
+
+            assertAll(
+                    () -> assertDoesNotThrow(() -> piece.validateMove(board, myPiece, enemyMa)),
+                    () -> assertThatThrownBy(() -> piece.validateMove(board, myPiece, ourMa))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @DisplayName("기물이 현재 위치에서 도달 가능한 위치를 검사한다")
+        @Test
+        void validateMoveToCoordinate() {
+            Piece piece = new Byeong(Country.HAN);
+            Map<JanggiCoordinate, Piece> map = new HashMap<>();
+
+            JanggiCoordinate pieceCoordinate = new JanggiCoordinate(5, 5);
+            JanggiCoordinate reachable1 = new JanggiCoordinate(5, 4);
+            JanggiCoordinate reachable2 = new JanggiCoordinate(6, 5);
+            JanggiCoordinate reachable3 = new JanggiCoordinate(5, 6);
+            JanggiCoordinate unReachable1 = new JanggiCoordinate(7, 4);
+            JanggiCoordinate unReachable2 = new JanggiCoordinate(2, 2);
+            JanggiCoordinate unReachable3 = new JanggiCoordinate(3, 2);
+
+            map.put(pieceCoordinate, piece);
+
+            JanggiBoard board = new JanggiBoard(map);
+
+            assertAll(
+                    () -> assertDoesNotThrow(() -> piece.validateMove(board, pieceCoordinate, reachable1)),
+                    () -> assertDoesNotThrow(() -> piece.validateMove(board, pieceCoordinate, reachable2)),
+                    () -> assertDoesNotThrow(() -> piece.validateMove(board, pieceCoordinate, reachable3)),
+                    () -> assertThatThrownBy(() -> piece.validateMove(board, pieceCoordinate, unReachable1)).isInstanceOf(IllegalArgumentException.class),
+                    () -> assertThatThrownBy(() -> piece.validateMove(board, pieceCoordinate, unReachable2)).isInstanceOf(IllegalArgumentException.class),
+                    () -> assertThatThrownBy(() -> piece.validateMove(board, pieceCoordinate, unReachable3)).isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+    }
+}
