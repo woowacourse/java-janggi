5da1e42190f987f140652716b6cd2063b3dbe56c
(minjae8563) <(minjae8563@naver.com)>
2025-03-23 01:53:56 +0900

    chore: 사용하지 않는 import 문 삭제

--- src/test/java/player/PiecesTest.java
+++ src/test/java/player/PiecesTest.java
@@ -1,20 +1,19 @@
+package player;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import piece.Jol;
-import piece.Nation;
-import piece.PieceProfile;
-import piece.Position;
+import pieceProperty.Position;
 
 class PiecesTest {
 
     @Test
     @DisplayName("Pieces 기물 삭제 테스트")
     void removeTest() {
-        Jol jol = new Jol(new PieceProfile("졸", Nation.HAN), new Position(5, 5));
+        Jol jol = new Jol(new Position(5, 5));
         Pieces pieces = new Pieces(List.of(jol));
 
         pieces.removePiece(new Position(5, 5));
