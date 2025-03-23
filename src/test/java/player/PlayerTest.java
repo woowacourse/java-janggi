dc96a0446d34126e610af69158e7f282f54a257b
(minjae8563) <(minjae8563@naver.com)>
2025-03-23 19:36:07 +0900

    test: 플레이어의 기물 중 왕 생존 여부를 판단하는 테스트 추가

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -1,11 +1,16 @@
 package player;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static player.Nation.CHO;
+import static player.Nation.HAN;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import piece.Janggun;
+import piece.Jol;
+import pieceProperty.Position;
 
 class PlayerTest {
     @Test
@@ -14,6 +19,28 @@ class PlayerTest {
         Pieces pieces = new Pieces(List.of());
 
         assertDoesNotThrow(() -> new Player(pieces, CHO));
+    }
+
+    @Test
+    @DisplayName("왕이 죽었는지 판단 테스트")
+    void isKingDieTest() {
+        //given
+        Pieces pieces = new Pieces(List.of(new Jol(new Position(5, 5))));
+        Player player = new Player(pieces, HAN);
+
+        //when - then
+        assertThat(player.isKingDie()).isTrue();
+    }
+
+    @Test
+    @DisplayName("왕이 살았는지 판단 테스트")
+    void isKingNotDieTest() {
+        //given
+        Pieces pieces = new Pieces(List.of(new Janggun(new Position(5, 5))));
+        Player player = new Player(pieces, HAN);
+
+        //when - then
+        assertThat(player.isKingDie()).isFalse();
     }
 
 }
