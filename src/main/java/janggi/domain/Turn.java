fced94781c18369c8c2c7868935c4b246a72c228
CheChe903 <cheche903@naver.com>
2025-03-23 14:55:05 +0900

    refactor(Turn): 턴 구현 방식 변경
    - 기존에는 List<Team>를 인스턴스 변수로 갖고 있었지만 이제는 team 변수 하나만 갖고 final을 제거하여 team을 변경하는 방식으로 진행합니다.

--- src/main/java/janggi/domain/Turn.java
+++ src/main/java/janggi/domain/Turn.java
@@ -3,37 +3,27 @@ package janggi.domain;
 import static janggi.domain.Team.BLUE;
 import static janggi.domain.Team.RED;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Turn {
-    private final List<Team> teams;
+    private Team team;
 
-    private Turn(final List<Team> teams) {
-        validateTeamSize(teams);
-        this.teams = teams;
+    public Turn(final Team team) {
+        this.team = team;
     }
 
     public static Turn initialize() {
-        return new Turn(new ArrayList<>(List.of(BLUE, RED)));
+        return new Turn(BLUE);
     }
 
-    private void validateTeamSize(final List<Team> teams) {
-        if (isInvalidSize(teams)) {
-            throw new IllegalStateException("올바른 팀 사이즈가 아닙니다.");
-        }
-    }
-
-    private boolean isInvalidSize(final List<Team> teams) {
-        return teams.size() != 2;
-    }
 
     public Team getCurrentTurn() {
-        return teams.getFirst();
+        return team;
     }
 
     public void changeTurn() {
-        Collections.rotate(teams, 1);
+        if (team == BLUE) {
+            team = RED;
+            return;
+        }
+        team = BLUE;
     }
 }
