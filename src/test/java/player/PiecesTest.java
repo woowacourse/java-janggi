8a8c5218101ceaa8313520e4706ce95b17a993e8
(minjae8563) <(minjae8563@naver.com)>
2025-03-23 18:02:56 +0900

    test: 왕 생존 여부 테스트 추가

--- src/test/java/player/PiecesTest.java
+++ src/test/java/player/PiecesTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import piece.Janggun;
 import piece.Jol;
 import pieceProperty.Position;
 
@@ -20,5 +21,27 @@ class PiecesTest {
         assertThat(pieces.getPieces().contains(jol)).isFalse();
     }
 
+    @Test
+    @DisplayName("왕이 죽었는지 확인하는 테스트")
+    void isKingDie() {
+        //given
+        Jol jol = new Jol(new Position(5, 5));
+        Pieces pieces = new Pieces(List.of(jol));
+
+        //when - then
+        assertThat(pieces.isKingDie()).isTrue();
+    }
+
+    @Test
+    @DisplayName("왕이 죽었는지 확인하는 테스트 (왕 생존)")
+    void isKingDieTest() {
+        //given
+        Jol jol = new Jol(new Position(5, 5));
+        Janggun janggun = new Janggun(new Position(6, 4));
+        Pieces pieces = new Pieces(List.of(jol, janggun));
+
+        //when - then
+        assertThat(pieces.isKingDie()).isFalse();
+    }
 
 }
