473cb33e3f34672c67140cd6ef69fa0a5901fd8f
(minjae8563) <(minjae8563@naver.com)>
2025-03-23 19:40:03 +0900

    test: 게임의 종료 여부를 판단하는 테스트 추가

--- src/test/java/JanggiGameStateTest.java
+++ src/test/java/JanggiGameStateTest.java
@@ -1,3 +1,4 @@
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static player.Nation.CHO;
 import static player.Nation.HAN;
@@ -5,6 +6,8 @@ import static player.Nation.HAN;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import piece.Janggun;
+import pieceProperty.Position;
 import player.Pieces;
 import player.Player;
 
@@ -19,6 +22,42 @@ class JanggiGameStateTest {
 
         //when - then
         assertDoesNotThrow(() -> new JanggiGameState(hanPlayer, choPlayer));
+    }
+
+    @Test
+    @DisplayName("게임 종료 판단 테스트")
+    void isGameOverTest() {
+        //given
+        Player hanPlayer = new Player(new Pieces(List.of(new Janggun(new Position(5, 5)))), HAN);
+        Player choPlayer = new Player(new Pieces(List.of()), CHO);
+        JanggiGameState janggiGameState = new JanggiGameState(hanPlayer, choPlayer);
+
+        //when - then
+        assertThat(janggiGameState.isGameOver()).isTrue();
+    }
+
+    @Test
+    @DisplayName("게임 종료 판단 테스트")
+    void isNotGameOverTest() {
+        //given
+        Player hanPlayer = new Player(new Pieces(List.of(new Janggun(new Position(5, 5)))), HAN);
+        Player choPlayer = new Player(new Pieces(List.of(new Janggun(new Position(6, 5)))), CHO);
+        JanggiGameState janggiGameState = new JanggiGameState(hanPlayer, choPlayer);
+
+        //when - then
+        assertThat(janggiGameState.isGameOver()).isFalse();
+    }
+
+    @Test
+    @DisplayName("게임 종료 판단 테스트")
+    void isGameOverTest2() {
+        //given
+        Player hanPlayer = new Player(new Pieces(List.of()), HAN);
+        Player choPlayer = new Player(new Pieces(List.of(new Janggun(new Position(6, 5)))), CHO);
+        JanggiGameState janggiGameState = new JanggiGameState(hanPlayer, choPlayer);
+
+        //when - then
+        assertThat(janggiGameState.isGameOver()).isTrue();
     }
 
 
