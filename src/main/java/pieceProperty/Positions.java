9a8bdf551497730abab1b39181eacc302e07b85d
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 00:29:15 +0900

    feat: List<Position> 을 필드로 가지는 객체 생성자 구현

---
+++ src/main/java/pieceProperty/Positions.java
@@ -0,0 +1,15 @@
+package pieceProperty;
+
+import java.util.List;
+
+public class Positions {
+    private final List<Position> positions;
+
+    public Positions(List<Position> positions) {
+        this.positions = positions;
+    }
+
+    public List<Position> getPositions() {
+        return positions;
+    }
+}
