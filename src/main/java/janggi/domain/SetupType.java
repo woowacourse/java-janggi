e892eee549afa110d845882f5a7a0d10bc8f19e1
jhan0121 <jhan0121@gmail.com>
2025-03-23 17:21:57 +0900

    feat: SetupType 추가

---
+++ src/main/java/janggi/domain/SetupType.java
@@ -0,0 +1,54 @@
+package janggi.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum SetupType {
+    LEFT_ELEPHANT(1, List.of(2, 7), List.of(3, 8)),
+    RIGHT_ELEPHANT(2, List.of(3, 8), List.of(2, 7)),
+    INNER_ELEPHANT(3, List.of(3, 7), List.of(2, 8)),
+    OUTER_ELEPHANT(4, List.of(2, 8), List.of(3, 7)),
+    ;
+
+    private final int option;
+    private final List<Integer> elephantColumnNumbers;
+    private final List<Integer> horseColumnNumbers;
+
+    SetupType(final int option, final List<Integer> elephantColumnNumbers, final List<Integer> horseColumnNumbers) {
+        this.option = option;
+        this.elephantColumnNumbers = elephantColumnNumbers;
+        this.horseColumnNumbers = horseColumnNumbers;
+    }
+
+    public static SetupType findSetupType(int option) {
+        return Arrays.stream(SetupType.values())
+                .filter(setupType -> setupType.option == option)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 옵션입니다"));
+    }
+
+    public List<Integer> getElephantColumnNumbers(Team team) {
+        return selectRowByTeam(team, elephantColumnNumbers);
+    }
+
+    public List<Integer> getHorseColumnNumbers(Team team) {
+        return selectRowByTeam(team, horseColumnNumbers);
+    }
+
+    private List<Integer> selectRowByTeam(Team team, List<Integer> columns) {
+        List<Integer> targetColumns = columns;
+        if (this == LEFT_ELEPHANT || this == RIGHT_ELEPHANT) {
+            targetColumns = reverseTeamColumn(team, columns);
+        }
+        return targetColumns;
+    }
+
+    private static List<Integer> reverseTeamColumn(final Team team, final List<Integer> columns) {
+        if (team.isRed()) {
+            return columns.stream()
+                    .map(row -> 10 - row)
+                    .toList();
+        }
+        return columns;
+    }
+}
