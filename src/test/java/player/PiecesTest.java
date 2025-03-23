87991c50a5d949832bffee1337c1955cd72156ab
(minjae8563) <(minjae8563@naver.com)>
2025-03-23 22:46:36 +0900

    test: 해당 위치에 같은 팀이 있는지 확인하는 테스트 추가

--- src/test/java/player/PiecesTest.java
+++ src/test/java/player/PiecesTest.java
@@ -1,6 +1,8 @@
 package player;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -42,6 +44,22 @@ class PiecesTest {
 
         //when - then
         assertThat(pieces.isKingDie()).isFalse();
+    }
+
+    @Test
+    @DisplayName("아군 판별 테스트")
+    void validateOurTeam() {
+        //given
+        Jol jol = new Jol(new Position(5, 5));
+        Janggun janggun = new Janggun(new Position(6, 4));
+        Pieces pieces = new Pieces(List.of(jol, janggun));
+
+        //when - then
+        assertThatThrownBy(() -> pieces.validateOurTeam(new Position(5, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 해당 위치에 움직일 수 있는 기물이 위치하지 않습니다.");
+        ;
+        assertDoesNotThrow(() -> pieces.validateOurTeam(new Position(5, 5)));
     }
 
 }
