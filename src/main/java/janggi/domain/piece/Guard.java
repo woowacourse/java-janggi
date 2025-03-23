2598adb0a0b877765a1809aacdda17ce4d4bd545
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-23 21:37:52 +0900

    refactor: 가드의 움직임을 인터페이스로 분리

--- src/main/java/janggi/domain/piece/Guard.java
+++ src/main/java/janggi/domain/piece/Guard.java
@@ -2,9 +2,12 @@ package janggi.domain.piece;
 
 import janggi.domain.Position;
 import janggi.domain.Side;
+import janggi.domain.piece.movement.fixed.GuardMovementStrategy;
 import java.util.List;
 
 public class Guard extends Piece {
+
+    private final GuardMovementStrategy movement = new GuardMovementStrategy();
 
     public Guard(Side side, int x, int y) {
         super(side, x, y);
@@ -12,11 +15,11 @@ public class Guard extends Piece {
 
     @Override
     protected boolean isMoveablePosition(Position destination) {
-        return false;
+        return movement.isLegalDestination(getPosition(), destination);
     }
 
     @Override
     protected boolean isMoveablePath(List<Piece> existingPieces, Position destination) {
-        return false;
+        return movement.isPathClear(Pieces.from(existingPieces), getSide(), getPosition(), destination);
     }
 }
---
+++ src/main/java/janggi/domain/piece/movement/fixed/GuardMovementStrategy.java
@@ -0,0 +1,18 @@
+package janggi.domain.piece.movement.fixed;
+
+import janggi.domain.Position;
+import janggi.domain.piece.Pieces;
+import java.util.Map;
+
+public class GuardMovementStrategy implements FixedMovementStrategy {
+
+    @Override
+    public boolean isLegalDestination(Position origin, Position destination) {
+        return false;
+    }
+
+    @Override
+    public Pieces getAllPiecesOnPath(Pieces map, Position origin, Position destination) {
+        return new Pieces(Map.of());
+    }
+}
