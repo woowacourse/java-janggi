23380a4db053f51c53455a4bf4a06fe1d3f710ff
soeun2537 <soeun2537@gmail.com>
2025-03-23 17:01:07 +0900

    refactor: 졸 움직일 수 있는 좌표 후보 찾는 알고리즘 리팩터링

--- src/main/java/piece/Jol.java
+++ src/main/java/piece/Jol.java
@@ -1,8 +1,14 @@
 package piece;
 
+import static coordinate.Direction.LEFT;
+import static coordinate.Direction.RIGHT;
+import static coordinate.Direction.UP;
+
 import board.Board;
 import coordinate.Coordinate;
-import java.util.Optional;
+import coordinate.MoveVector;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,13 +22,13 @@ public class Jol extends Piece {
 
     @Override
     protected Set<Coordinate> findMovableCandidates(Coordinate departure) {
-        return Stream.of(
-                        departure.pickChangedCoordinate(1, 0),
-                        departure.pickChangedCoordinate(-1, 0),
-                        departure.pickChangedCoordinate(0, team.getDirection())
+        return Stream.<List<MoveVector>>of(
+                        List.of(UP),
+                        List.of(RIGHT),
+                        List.of(LEFT)
                 )
-                .filter(Optional::isPresent)
-                .map(Optional::get)
+                .map(departure::pickChangedCoordinate)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toSet());
     }
 
