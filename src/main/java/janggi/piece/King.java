1b4b737ba893e63295b4bca547d8c840ab5ced81
sung-june27 <kmymy27@gmail.com>
2025-03-23 16:46:34 +0900

    feat(King): King 이동 가능 여부 구현

--- src/main/java/janggi/piece/King.java
+++ src/main/java/janggi/piece/King.java
@@ -2,6 +2,7 @@ package janggi.piece;
 
 import janggi.board.Position;
 import java.util.List;
+import java.util.Map;
 
 public class King extends Piece {
 
@@ -10,19 +11,22 @@ public class King extends Piece {
     }
 
     @Override
-    public List<Position> calculatePath(final Position start, final Position end) {
-        int differenceX = end.x() - start.x();
-        int differenceY = end.y() - start.y();
-        validateMovingRule(differenceX, differenceY);
-        return List.of();
+    public boolean canMove(Position start, Position end, Map<Position, Piece> board) {
+        return isValidMovingRule(start, end);
     }
 
-    private void validateMovingRule(final int differenceX, final int differenceY) {
+    private boolean isValidMovingRule(Position start, Position end) {
+        int differenceX = end.x() - start.x();
+        int differenceY = end.y() - start.y();
         int absDifferenceX = Math.abs(differenceX);
         int absDifferenceY = Math.abs(differenceY);
-        if ((absDifferenceX == 1 && absDifferenceY == 0)
-                || (absDifferenceX == 0 && absDifferenceY == 1)) {
-            return;
+        return (absDifferenceX == 1 && absDifferenceY == 0) || (absDifferenceX == 0 && absDifferenceY == 1);
+    }
+
+    @Override
+    public List<Position> calculatePath(final Position start, final Position end) {
+        if (isValidMovingRule(start, end)) {
+            return List.of();
         }
         throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
     }
--- src/test/java/janggi/piece/KingTest.java
+++ src/test/java/janggi/piece/KingTest.java
@@ -5,11 +5,58 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import janggi.board.Position;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class KingTest {
+
+    @ParameterizedTest
+    @DisplayName("이동이 가능하면 true를 반환한다.")
+    @CsvSource(value = {
+            "5, 6",
+            "5, 4",
+            "4, 5",
+            "6, 5"
+    })
+    void shouldReturnTrueWhenCanMove(int destX, int destY) {
+        // given
+        King king = new King(Side.RED);
+        Position start = new Position(5, 5);
+        Position end = new Position(destX, destY);
+
+        // when
+        boolean canMove = king.canMove(start, end, Map.of());
+
+        // then
+        assertThat(canMove).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("말의 이동 규칙이 어긋나면 예외를 발생한다.")
+    @CsvSource(value = {
+            "3, 5",
+            "7, 5",
+            "5, 7",
+            "5, 3",
+            "4, 6",
+            "6, 4",
+            "4, 4",
+            "6, 6"
+    })
+    void shouldReturnFalseWhenUnfollowMovingRule(int destX, int destY) {
+        // given
+        King king = new King(Side.RED);
+        Position start = new Position(5, 5);
+        Position end = new Position(destX, destY);
+
+        // when
+        boolean canMove = king.canMove(start, end, Map.of());
+
+        // then
+        assertThat(canMove).isFalse();
+    }
 
     @ParameterizedTest
     @DisplayName("시작점과 끝점이 주어졌을 때, 이동 경로를 반환한다.")
@@ -21,12 +68,12 @@ class KingTest {
     })
     void shouldReturnTrueWhenValidateMovingRule(int destX, int destY) {
         // given
-        Guard horse = new Guard(Side.RED);
+        King king = new King(Side.RED);
         Position start = new Position(5, 5);
         Position end = new Position(destX, destY);
 
         // when
-        List<Position> path = horse.calculatePath(start, end);
+        List<Position> path = king.calculatePath(start, end);
 
         // then
         assertThat(path).isEmpty();
@@ -46,12 +93,12 @@ class KingTest {
     })
     void shouldReturnTrueWhenUnfollowMovingRule(int destX, int destY) {
         // given
-        Guard guard = new Guard(Side.RED);
+        King king = new King(Side.RED);
         Position start = new Position(5, 5);
         Position end = new Position(destX, destY);
 
         // when
-        assertThatThrownBy(() -> guard.calculatePath(start, end))
+        assertThatThrownBy(() -> king.calculatePath(start, end))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
