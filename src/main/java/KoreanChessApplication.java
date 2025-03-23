ac863122678219b4c6d70172e856d7b5913c9387
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-23 14:13:08 +0900

    refactor: 장기 초기화 파일방식에서 초기 기물을 상태로서 가지고 있게 수정

--- src/main/java/KoreanChessApplication.java
+++ src/main/java/KoreanChessApplication.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import piece.Board;
+import piece.JanggiTeamPiecesCreateFactory;
 import piece.Position;
 import piece.Team;
 
@@ -15,7 +16,7 @@ public class KoreanChessApplication {
 
     public static void main(String[] args) {
         GameView gameView = new GameView();
-        Board board = new Board(INITIATE_JANGGI_FILE);
+        Board board = new Board(new JanggiTeamPiecesCreateFactory());
         gameView.printChangePieceNotImplement();
 
         playKoreanChess(board, gameView);
