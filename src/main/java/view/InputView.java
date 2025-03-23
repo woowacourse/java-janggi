63ac38fd5b6c13ac7ebcede7efc7947daa5a9489
GiSeok Kang <ime.rokz@gmail.com>
2025-03-23 17:37:22 +0900

    refactor: InputView에서 Scanner static 변수로 포함하도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 import direction.Point;
 
 public class InputView {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
     public static Point requestMoveStartPosition() {
         System.out.println("움직일 말을 알려주세요.");
-        Scanner scanner = new Scanner(System.in);
         String from = scanner.nextLine();
-        String[] s = from.split(" ");
+
+        String[] rawFromPoint = from.split(" ");
         return new Point(
-                Integer.parseInt(s[0]),
-                Integer.parseInt(s[1])
+                Integer.parseInt(rawFromPoint[0]),
+                Integer.parseInt(rawFromPoint[1])
         );
     }
 
     public static Point requestMoveEndPosition() {
         System.out.println("도착지를 알려주세요.");
-        Scanner scanner = new Scanner(System.in);
         String to = scanner.nextLine();
-        String[] s = to.split(" ");
+
+        String[] rawToPoint = to.split(" ");
         return new Point(
-                Integer.parseInt(s[0]),
-                Integer.parseInt(s[1])
+                Integer.parseInt(rawToPoint[0]),
+                Integer.parseInt(rawToPoint[1])
         );
     }
 }
