829a3c222750690303a80547130df0d355b629d2
RaZel713 <razel7130@gmail.com>
2025-03-23 17:01:26 +0900

    refactor: enum과 properties를 활용하여 출력을 위한 나라 명을 관리

--- src/main/java/janggiGame/piece/Dynasty.java
+++ src/main/java/janggiGame/piece/Dynasty.java
@@ -1,16 +1,6 @@
 package janggiGame.piece;
 
 public enum Dynasty {
-    HAN("한나라"),
-    CHO("초나라");
-
-    private final String name;
-
-    Dynasty(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    HAN,
+    CHO;
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,22 +4,24 @@ import janggiGame.board.Board;
 import janggiGame.board.Dot;
 import janggiGame.piece.Dynasty;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class InputView {
-
     public static final String ARRANGE_PROMPT = """            
             1. 내상 배치
             2. 외상 배치
             3. 좌상 배치
             4. 우상 배치""";
+    private static final ResourceBundle dynastyBundle = ResourceBundle.getBundle("dynasty");
+    
     private final Scanner scanner = new Scanner(System.in);
 
     public List<Dot> readPieceMovement(Dynasty dynasty) {
         System.out.printf("""
                 %n%s의 차례입니다. 말을 움직여주세요
                 ex) 기물의 x좌표, 기물의 y좌표 > 이동할 x좌표, 이동할 y좌표
-                """, dynasty.getName());
+                """, dynastyBundle.getString(dynasty.name()));
 
         String movementInput = scanner.nextLine();
 
