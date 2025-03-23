6f1040db8514e10a0f3e0c57366f4f32b04d91bc
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 00:32:57 +0900

    test: Position 추가 테스트 추가

--- src/test/java/pieceProperty/PositionsTest.java
+++ src/test/java/pieceProperty/PositionsTest.java
@@ -1,5 +1,6 @@
 package pieceProperty;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -16,6 +17,21 @@ class PositionsTest {
 
         //when-then
         assertDoesNotThrow(() -> new Positions(positionsList));
+    }
+
+    @Test
+    @DisplayName("Position 추가 테스트")
+    void addPositionTest() {
+        //given
+        List<Position> positionsList = List.of(new Position(5, 5));
+        Positions positions = new Positions(positionsList);
+        Position position = new Position(6, 5);
+
+        //when
+        positions.addPosition(position);
+
+        //then
+        assertThat(positions.getPositions().contains(position)).isTrue();
     }
 
 
