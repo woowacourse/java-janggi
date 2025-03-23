eda723fa1bdf26e16d3ac26bcde5efe19bfddc06
soeun2537 <soeun2537@gmail.com>
2025-03-23 16:24:11 +0900

    refactor: 상 이동 경로 찾는 알고리즘 일반화

--- src/main/java/piece/Sang.java
+++ src/main/java/piece/Sang.java
@@ -1,8 +1,19 @@
 package piece;
 
+import static coordinate.DiagonalDirection.LEFT_DOWN;
+import static coordinate.DiagonalDirection.LEFT_UP;
+import static coordinate.DiagonalDirection.RIGHT_DOWN;
+import static coordinate.DiagonalDirection.RIGHT_UP;
+import static coordinate.Direction.DOWN;
+import static coordinate.Direction.LEFT;
+import static coordinate.Direction.RIGHT;
+import static coordinate.Direction.UP;
+
 import board.Board;
 import coordinate.Coordinate;
-import java.util.Optional;
+import coordinate.MoveVector;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,18 +27,18 @@ public class Sang extends Piece {
 
     @Override
     protected Set<Coordinate> findMovableCandidates(Coordinate departure) {
-        return Stream.of(
-                        departure.pickChangedCoordinate(2, -3), // 상우
-                        departure.pickChangedCoordinate(2, 3), // 하우
-                        departure.pickChangedCoordinate(-2, -3), //상좌
-                        departure.pickChangedCoordinate(-2, 3), //하좌
-                        departure.pickChangedCoordinate(3, -2), // 우상
-                        departure.pickChangedCoordinate(3, 2), //우하
-                        departure.pickChangedCoordinate(-3, -2), //좌상
-                        departure.pickChangedCoordinate(-3, 2) //좌하
+        return Stream.<List<MoveVector>>of(
+                        List.of(UP, RIGHT_UP, RIGHT_UP),
+                        List.of(UP, LEFT_UP, LEFT_UP),
+                        List.of(DOWN, RIGHT_DOWN, RIGHT_DOWN),
+                        List.of(DOWN, LEFT_DOWN, LEFT_DOWN),
+                        List.of(RIGHT, RIGHT_UP, RIGHT_UP),
+                        List.of(RIGHT, RIGHT_DOWN, RIGHT_DOWN),
+                        List.of(LEFT, LEFT_UP, LEFT_UP),
+                        List.of(LEFT, LEFT_DOWN, LEFT_DOWN)
                 )
-                .filter(Optional::isPresent)
-                .map(Optional::get)
+                .map(departure::pickChangedCoordinate)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toSet());
     }
 
@@ -40,49 +51,44 @@ public class Sang extends Piece {
 
     @Override
     protected Set<Coordinate> findPaths(Coordinate departure, Coordinate arrival) {
-        int dx = arrival.getX() - departure.getX();
-        int dy = arrival.getY() - departure.getY();
+        return Stream.<List<MoveVector>>of(
+                        List.of(LEFT, LEFT_UP),
+                        List.of(LEFT, LEFT_DOWN),
+                        List.of(RIGHT, RIGHT_UP),
+                        List.of(RIGHT, RIGHT_DOWN),
+                        List.of(UP, LEFT_UP),
+                        List.of(DOWN, LEFT_DOWN),
+                        List.of(UP, RIGHT_UP),
+                        List.of(DOWN, RIGHT_DOWN)
+                )
+                .filter(path -> isMatchingDirection(path, departure, arrival))
+                .findFirst()
+                .map(moveVectors -> toCoordinates(moveVectors, departure))
+                .orElseThrow(() -> new IllegalStateException("유효하지 않은 좌표입니다."));
+    }
 
-        if (Math.abs(dx) > Math.abs(dy)) {
-            //좌우시작
-            if (dx < 0 && dy < 0) {
-                //좌상
-                return Set.of(departure.pickChangedCoordinate(-1, 0).get(),
-                        departure.pickChangedCoordinate(-2, -1).get());
-            } else if (dx < 0 && dy > 0) {
-                //좌하
-                return Set.of(departure.pickChangedCoordinate(-1, 0).get(),
-                        departure.pickChangedCoordinate(-2, 1).get());
-            } else if (dx > 0 && dy < 0) {
-                //우상
-                return Set.of(departure.pickChangedCoordinate(1, 0).get(),
-                        departure.pickChangedCoordinate(2, -1).get());
-            } else if (dx > 0 && dy > 0) {
-                //우하
-                return Set.of(departure.pickChangedCoordinate(1, 0).get(),
-                        departure.pickChangedCoordinate(2, 1).get());
-            }
-        } else {
-            //상하시작
-            if (dx < 0 && dy < 0) {
-                //상좌
-                return Set.of(departure.pickChangedCoordinate(0, -1).get(),
-                        departure.pickChangedCoordinate(-1, -2).get());
-            } else if (dx < 0 && dy > 0) {
-                //하좌
-                return Set.of(departure.pickChangedCoordinate(0, 1).get(),
-                        departure.pickChangedCoordinate(-1, 2).get());
-            } else if (dx > 0 && dy < 0) {
-                //상우
-                return Set.of(departure.pickChangedCoordinate(0, -1).get(),
-                        departure.pickChangedCoordinate(1, -2).get());
-            } else if (dx > 0 && dy > 0) {
-                //하우
-                return Set.of(departure.pickChangedCoordinate(0, 1).get(),
-                        departure.pickChangedCoordinate(1, 2).get());
-            }
-        }
-        throw new IllegalStateException("유효하지 않은 좌표입니다.");
+    private boolean isMatchingDirection(List<MoveVector> path, Coordinate departure, Coordinate arrival) {
+        List<MoveVector> pathIncludeArrival = Stream.concat(
+                path.stream(),
+                Stream.of(path.getLast())
+        ).toList();
+
+        int totalDeltaX = pathIncludeArrival.stream()
+                .mapToInt(MoveVector::deltaX)
+                .sum();
+        int totalDeltaY = pathIncludeArrival.stream()
+                .mapToInt(MoveVector::deltaY)
+                .sum();
+
+        return departure.getX() + totalDeltaX == arrival.getX()
+                && departure.getY() + totalDeltaY == arrival.getY();
+    }
+
+    private Set<Coordinate> toCoordinates(List<MoveVector> moveVectors, Coordinate departure) {
+        return Set.of(
+                departure.pickChangedCoordinate(List.of(moveVectors.getFirst())),
+                departure.pickChangedCoordinate(List.of(moveVectors.getFirst(), moveVectors.getLast()))
+        );
     }
 
     @Override
