cb4edd1f08abdd6b0db6bf5d730b3b9fcb29d7be
CheChe903 <cheche903@naver.com>
2025-03-23 17:00:21 +0900

    refactor: 테스트 패키지 구조 변경

--- src/test/java/janggi/domain/piece/direction/PositionTest.java
+++ src/test/java/janggi/domain/piece/direction/PositionTest.java
@@ -1,9 +1,8 @@
-package janggi.domain.piece;
+package janggi.domain.piece.direction;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import janggi.domain.piece.direction.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
