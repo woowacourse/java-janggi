2c316b0e6eaba6806195108d6a801e2f9f10fdfb
gabean13 <choegabin171@gmail.com>
2025-03-23 21:11:41 +0900

    refactor: BoardSetup 한 클래스로 관리하도록 변경

--- src/main/java/janggi/domain/board/BoardSetUp.java
+++ src/main/java/janggi/domain/board/BoardSetUp.java
@@ -1,8 +1,57 @@
 package janggi.domain.board;
 
+import janggi.domain.Dynasty;
 import janggi.domain.piece.BoardPiece;
-import java.util.Set;
+import janggi.domain.piece.Elephant;
+import janggi.domain.piece.Horse;
+import janggi.domain.piece.Piece;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public interface BoardSetUp {
-    Set<BoardPiece> getPiecePositions();
+public enum BoardSetUp {
+    INNER_ELEPHANT(List.of(
+            new Horse(),
+            new Elephant(),
+            new Elephant(),
+            new Horse()
+    )),
+    OUTER_ELEPHANT(List.of(
+            new Elephant(),
+            new Horse(),
+            new Horse(),
+            new Elephant()
+    )),
+    RIGHT_ELEPHANT(List.of(
+            new Horse(),
+            new Elephant(),
+            new Horse(),
+            new Elephant()
+    )),
+    LEFT_ELEPHANT(List.of(
+            new Elephant(),
+            new Horse(),
+            new Elephant(),
+            new Horse()
+    ));
+
+    private final List<Piece> pieceSetUpOrder;
+
+    BoardSetUp(List<Piece> pieceSetUpOrder) {
+        this.pieceSetUpOrder = pieceSetUpOrder;
+    }
+
+    public Map<Position, BoardPiece> getDynastySetUp(Dynasty dynasty, BoardSetUp boardSetUp) {
+        int xPoint = 10;
+        List<Integer> yPoints = List.of(2, 3, 7, 8);
+        if (dynasty == Dynasty.HAN) {
+            xPoint = 1;
+        }
+        Map<Position, BoardPiece> setup = new HashMap<>();
+        for (int i = 0; i < 4; i++) {
+            Piece piece = boardSetUp.pieceSetUpOrder.get(i);
+            setup.put(new Position(xPoint, yPoints.get(i)), new BoardPiece(piece, dynasty));
+        }
+        return setup;
+    }
 }
