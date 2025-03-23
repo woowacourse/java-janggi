4db6c4af2e5726da43a673dcfcd53dd642ecebc5
RaZel713 <razel7130@gmail.com>
2025-03-24 02:25:33 +0900

    test: 추상 클래스 Piece의 테스트 추가

---
+++ src/test/java/janggiGame/piece/PieceTest.java
@@ -0,0 +1,62 @@
+package janggiGame.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import janggiGame.board.Dot;
+import janggiGame.testhelper.FakePiece;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PieceTest {
+    @DisplayName("기물을 기물이 어떤 나라의 것인지 알릴 수 있다.")
+    @Test
+    void pieceGetDynasty_Test() {
+        // given
+        FakePiece fakePiece = new FakePiece(Dynasty.HAN);
+
+        // when // then
+        assertThat(fakePiece.getDynasty()).isEqualTo(Dynasty.HAN);
+    }
+
+    @DisplayName("기물은 자신의 타입을 알릴 수 있다.")
+    @Test
+    void pieceGetType_Test() {
+        // given
+        FakePiece fakePiece = new FakePiece(Dynasty.HAN);
+
+        // when // then
+        assertThat(fakePiece.getType()).isEqualTo(PieceType.PAWN);
+    }
+
+    @DisplayName("기물은 출발 점과 도착 점을 입력 받아 이동 하는 경로의 중간 지점을 반환할 수 있다.")
+    @Test
+    void pieceGetIntermediatePoints_Test() {
+        // given
+        FakePiece fakePiece = new FakePiece(Dynasty.HAN);
+
+        // when // then
+        assertThat(fakePiece.getIntermediatePoints(Dot.of(0, 9), Dot.of(0, 7))
+                .contains(Dot.of(0, 8))).isTrue();
+    }
+
+    @DisplayName("기물은 이동 조건을 판별할 수 있다.")
+    @Test
+    void pieceValidateMove_Test() {
+        // given
+        FakePiece fakePiece = new FakePiece(Dynasty.HAN);
+
+        // when // then
+        Map<Dot, Piece> intermediatePointsWithPiece = Map.of(
+                Dot.of(0, 8), new Pawn(Dynasty.HAN)
+        );
+
+        Piece destinationPiece = new Pawn(Dynasty.HAN);
+
+        assertThatCode(() -> fakePiece.validateMove(intermediatePointsWithPiece, destinationPiece))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessageStartingWith("[ERROR] ");
+    }
+
+}
---
+++ src/test/java/janggiGame/testhelper/FakePiece.java
@@ -0,0 +1,28 @@
+package janggiGame.testhelper;
+
+import janggiGame.board.Dot;
+import janggiGame.piece.Dynasty;
+import janggiGame.piece.Piece;
+import janggiGame.piece.PieceType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakePiece extends Piece {
+    // 해당 구현체는 추상 클래스인 Piece 테스트를 위한 구현체이다.
+
+    public FakePiece(Dynasty dynasty) {
+        super(dynasty);
+    }
+
+    @Override
+    public List<Dot> getIntermediatePoints(Dot origin, Dot destination) {
+        List<Dot> intermediatePoints = new ArrayList<>();
+        intermediatePoints.add(Dot.of(0, 8));
+        return intermediatePoints;
+    }
+
+    @Override
+    public PieceType getType() {
+        return PieceType.PAWN;
+    }
+}
