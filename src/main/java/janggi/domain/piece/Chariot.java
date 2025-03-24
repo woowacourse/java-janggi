f7afd10a559c9273c9f6e3b50380cae3703aa38b
LeeJaeHoon <dlwogns3413@naver.com>
2025-03-24 20:04:27 +0900

    test: Chariot 단위 테스트 작성

--- src/main/java/janggi/domain/piece/Chariot.java
+++ src/main/java/janggi/domain/piece/Chariot.java
@@ -15,7 +15,7 @@ public class Chariot extends Piece {
     @Override
     public boolean canMove(PiecesOnPath piecesOnPath) {
         if (piecesOnPath.isDestinationOfDynasty(dynasty)) {
-            throw new IllegalArgumentException("목적지에 같은 나라의 기물이 있어 갈 수 없습니다.");
+            return false;
         }
         return piecesOnPath.isAllEmptyWithoutDestination();
     }
