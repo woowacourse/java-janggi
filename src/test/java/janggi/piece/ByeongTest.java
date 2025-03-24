b7d3a10f549a87e10e963b0aea8b0968e61cf3f9
DongYeop <s01054830455@gmail.com>
2025-03-25 00:12:38 +0900

    test: 병 이동 테스트 추가

--- src/test/java/janggi/piece/ByeongTest.java
+++ src/test/java/janggi/piece/ByeongTest.java
@@ -1,10 +1,12 @@
 package janggi.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import janggi.position.Position;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class ByeongTest {
+
     @DisplayName("병은 이름과 위치를 가진다.")
     @Test
     void byenogBoardPosition() {
@@ -27,7 +30,7 @@ class ByeongTest {
         assertThat(byeong.getBoardPosition().getRow()).isEqualTo(0);
     }
 
-    @DisplayName("자신의 위치를 기준으로 이동할 수 없다면 예외를 던진다.")
+    @DisplayName("자신의 위치를 기준으로 이동할 수 없다면(가로,세로 한칸을 제외한 경로) 예외를 던진다.")
     @ParameterizedTest
     @MethodSource("byeongNonIsMovePositionProvider")
     void nonIsMove(Position position) {
@@ -38,6 +41,14 @@ class ByeongTest {
         assertThatThrownBy(() -> byeong.isMove(position))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
+    }
+
+    private static Stream<Arguments> byeongNonIsMovePositionProvider() {
+        return Stream.of(
+                Arguments.of(new Position(4, 5)),
+                Arguments.of(new Position(6, 3)),
+                Arguments.of(new Position(6, 6))
+        );
     }
 
     @DisplayName("자신의 위치를 기준으로 뒤를 제외한 가로,세로 한칸 이동을 할 수 있다면 true를 반환한다.")
@@ -54,6 +65,14 @@ class ByeongTest {
         assertThat(actual).isTrue();
     }
 
+    private static Stream<Arguments> byeongIsMovePositionProvider() {
+        return Stream.of(
+                Arguments.of(new Position(6, 5)),
+                Arguments.of(new Position(5, 6)),
+                Arguments.of(new Position(5, 4))
+        );
+    }
+
     @DisplayName("병은 자신의 위치에서 목적지까지의 경로를 계산하여 반환한다.")
     @Test
     void makeRoute() {
@@ -68,19 +87,38 @@ class ByeongTest {
         assertThat(actual.contains(futurePosition)).isTrue();
     }
 
-    private static Stream<Arguments> byeongNonIsMovePositionProvider() {
-        return Stream.of(
-                Arguments.of(new Position(4, 5)),
-                Arguments.of(new Position(6, 3)),
-                Arguments.of(new Position(6, 6))
+    @DisplayName("병의 이동 경로에 장애물이 있다면 예외를 던진다.")
+    @Test
+    void hasObstacle() {
+        //given
+        Byeong byeong = new Byeong(new PieceProfile("병", Nation.HAN), new Position(5, 5));
+
+        Map<Position, Piece> board = Map.of(
+                new Position(6, 5), new Cha(new PieceProfile("차", Nation.HAN), new Position(6, 5))
         );
+
+        Position futurePosition = new Position(6, 5);
+
+        //when //then
+        assertThatThrownBy(() -> byeong.checkObstacle(futurePosition, board))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
     }
 
-    private static Stream<Arguments> byeongIsMovePositionProvider() {
-        return Stream.of(
-                Arguments.of(new Position(6, 5)),
-                Arguments.of(new Position(5, 6)),
-                Arguments.of(new Position(5, 4))
+    @DisplayName("병의 이동 경로에 장애물이 없다면 예외를 던지지 않는다.")
+    @Test
+    void nonObstacle() {
+        //given
+        Byeong byeong = new Byeong(new PieceProfile("병", Nation.HAN), new Position(5, 5));
+
+        Map<Position, Piece> board = Map.of(
+                new Position(7, 5), new Cha(new PieceProfile("차", Nation.HAN), new Position(7, 5))
         );
+
+        Position futurePosition = new Position(6, 5);
+
+        //when //then
+        assertThatCode(() -> byeong.checkObstacle(futurePosition, board))
+                .doesNotThrowAnyException();
     }
 }
