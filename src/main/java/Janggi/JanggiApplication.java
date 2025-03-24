edee8ff3d0dcfada47d2dd67c3fe7ba5075123ad
changuii <rhljh7410@gmail.com>
2025-03-24 12:55:39 +0900

    refactor: 장기를 두는 흐름을 메서드로 묶어내어 읽기 쉽게 수정
    - 매직 넘버도 상수화하였다.

--- src/main/java/Janggi/JanggiApplication.java
+++ src/main/java/Janggi/JanggiApplication.java
@@ -9,17 +9,23 @@ import Janggi.piece.Country;
 import Janggi.view.OutputView;
 
 public class JanggiApplication {
+    private static final int MOVE_SOURCE = 0;
+    private static final int MOVE_DESTINATION = 1;
 
     public static void main(String[] args) {
         OutputView.printIntroduce();
         final BoardFactory boardFactory = new BoardFactory();
         final Board board = boardFactory.generateBoard();
+        startJanggi(board);
+    }
+
+    private static void startJanggi(final Board board) {
         Country type = Country.getFirstTurnCountry();
 
         while (true) {
             OutputView.printBoard(board, type);
             final List<Position> positions = InputView.readPositions();
-            board.updatePosition(positions.get(0), positions.get(1), type);
+            board.updatePosition(positions.get(MOVE_SOURCE), positions.get(MOVE_DESTINATION), type);
             type = type.toggleCountry();
         }
     }
