ef5037102e29feac35dc3505292ba45692690653
taek2222 <oot309@daum.net>
2025-03-24 23:58:47 +0900

    refactor: 중간 기물 검증은 Piece 책임으로 변경

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -44,32 +44,21 @@ public record Board(
 
     private void validatePieceMovingPath(final Player player, final Position targetPosition, final Piece piece) {
         List<Position> path = piece.getPath(targetPosition);
+        int pathPieceCount = getContainedPiecesCount(path);
+
+        piece.validateMoveByPathPieceCount(pathPieceCount);
         if (piece.isCannon()) {
             validateCannonMoving(player, targetPosition, path);
-            return;
-        }
-
-        if (existsOtherPieceInPath(path)) {
-            throw new IllegalArgumentException("[ERROR] 다른 기물을 지나칠 수 없습니다.");
         }
     }
 
     private void validateCannonMoving(final Player player, final Position targetPosition, final List<Position> path) {
-        int count = getContainedPiecesCount(path);
-        if (count != 1) {
-            throw new IllegalArgumentException("[ERROR] 포는 중간에 기물이 1개여야 합니다.");
-        }
         if (getOppositePieces(player).isCannonByPosition(targetPosition)) {
             throw new IllegalArgumentException("[ERROR] 포는 상대 포를 잡을 수 없습니다.");
         }
         if (path.stream().anyMatch(this::existsCannon)) {
             throw new IllegalArgumentException("[ERROR] 포는 다른 포를 지나칠 수 없습니다.");
         }
-    }
-
-    private boolean existsOtherPieceInPath(final List<Position> path) {
-        int containedPiecesCount = getContainedPiecesCount(path);
-        return containedPiecesCount != 0;
     }
 
     private int getContainedPiecesCount(final List<Position> path) {
