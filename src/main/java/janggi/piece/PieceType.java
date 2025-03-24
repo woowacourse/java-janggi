4c234bc72766bc9d86ff9afe4998ce8379cdbd88
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-24 21:41:19 +0900

    feat: PieceType 설명 추가

--- src/main/java/janggi/piece/PieceType.java
+++ src/main/java/janggi/piece/PieceType.java
@@ -1,16 +1,26 @@
 package janggi.piece;
 
 public enum PieceType {
-    GENERAL,
-    GUARD,
-    SOLDIER,
-    HORSE,
-    ELEPHANT,
-    CHARIOT,
-    CANNON,
+    GENERAL("장"),
+    GUARD("사"),
+    SOLDIER("병/졸"),
+    HORSE("마"),
+    ELEPHANT("상"),
+    CHARIOT("차"),
+    CANNON("포"),
     ;
+
+    private final String description;
+
+    PieceType(final String description) {
+        this.description = description;
+    }
 
     public boolean isCannon() {
         return this == CANNON;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
