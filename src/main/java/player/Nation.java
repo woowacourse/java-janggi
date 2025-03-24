e74efa122156b682dae191e3a32b55c179f05647
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 14:17:32 +0900

    test: 공격 팀 기준으로 수비 팀 선정 기능 구현

--- src/main/java/player/Nation.java
+++ src/main/java/player/Nation.java
@@ -1,6 +1,15 @@
 package player;
 
+import piece.Cha;
+
 public enum Nation {
     HAN,
-    CHO
+    CHO;
+
+    public Nation getDefenseNation() {
+        if (this.equals(HAN)) {
+            return CHO;
+        }
+        return HAN;
+    }
 }
