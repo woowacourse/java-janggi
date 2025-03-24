1d859f55650ac6affb3a349dff881e1b9616ebdf
Yehyeok Bang <qkddpgur318@gmail.com>
2025-03-24 22:30:53 +0900

    refactor: assertAll 적용
    - 순서의 의존된 단언문 실행 결과를 방지합니다.
    - 불필요한 개행을 제거합니다.

--- src/test/java/janggi/board/BoardGeneratorTest.java
+++ src/test/java/janggi/board/BoardGeneratorTest.java
@@ -1,7 +1,7 @@
 package janggi.board;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import janggi.board.point.Point;
 import janggi.piece.Cannon;
@@ -25,25 +25,27 @@ class BoardGeneratorTest {
         Board board = BoardGenerator.generate();
 
         // then
-        assertThat(board.getPiece(new Point(0, 3))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(2, 3))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(4, 3))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(6, 3))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(8, 3))).isInstanceOf(Soldier.class);
+        assertAll(
+                () -> assertThat(board.getPiece(new Point(0, 3))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(2, 3))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(4, 3))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(6, 3))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(8, 3))).isInstanceOf(Soldier.class),
 
-        assertThat(board.getPiece(new Point(1, 2))).isInstanceOf(Cannon.class);
-        assertThat(board.getPiece(new Point(7, 2))).isInstanceOf(Cannon.class);
+                () -> assertThat(board.getPiece(new Point(1, 2))).isInstanceOf(Cannon.class),
+                () -> assertThat(board.getPiece(new Point(7, 2))).isInstanceOf(Cannon.class),
 
-        assertThat(board.getPiece(new Point(4, 1))).isInstanceOf(General.class);
+                () -> assertThat(board.getPiece(new Point(4, 1))).isInstanceOf(General.class),
 
-        assertThat(board.getPiece(new Point(0, 0))).isInstanceOf(Chariot.class);
-        assertThat(board.getPiece(new Point(1, 0))).isInstanceOf(Elephant.class);
-        assertThat(board.getPiece(new Point(2, 0))).isInstanceOf(Horse.class);
-        assertThat(board.getPiece(new Point(3, 0))).isInstanceOf(Guard.class);
-        assertThat(board.getPiece(new Point(5, 0))).isInstanceOf(Guard.class);
-        assertThat(board.getPiece(new Point(6, 0))).isInstanceOf(Elephant.class);
-        assertThat(board.getPiece(new Point(7, 0))).isInstanceOf(Horse.class);
-        assertThat(board.getPiece(new Point(8, 0))).isInstanceOf(Chariot.class);
+                () -> assertThat(board.getPiece(new Point(0, 0))).isInstanceOf(Chariot.class),
+                () -> assertThat(board.getPiece(new Point(1, 0))).isInstanceOf(Elephant.class),
+                () -> assertThat(board.getPiece(new Point(2, 0))).isInstanceOf(Horse.class),
+                () -> assertThat(board.getPiece(new Point(3, 0))).isInstanceOf(Guard.class),
+                () -> assertThat(board.getPiece(new Point(5, 0))).isInstanceOf(Guard.class),
+                () -> assertThat(board.getPiece(new Point(6, 0))).isInstanceOf(Elephant.class),
+                () -> assertThat(board.getPiece(new Point(7, 0))).isInstanceOf(Horse.class),
+                () -> assertThat(board.getPiece(new Point(8, 0))).isInstanceOf(Chariot.class)
+        );
     }
 
     @DisplayName("한나라 초기 세팅이 올바른지 확인한다.")
@@ -53,25 +55,27 @@ class BoardGeneratorTest {
         Board board = BoardGenerator.generate();
 
         // then
-        assertThat(board.getPiece(new Point(0, 6))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(2, 6))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(4, 6))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(6, 6))).isInstanceOf(Soldier.class);
-        assertThat(board.getPiece(new Point(8, 6))).isInstanceOf(Soldier.class);
+        assertAll(
+                () -> assertThat(board.getPiece(new Point(0, 6))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(2, 6))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(4, 6))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(6, 6))).isInstanceOf(Soldier.class),
+                () -> assertThat(board.getPiece(new Point(8, 6))).isInstanceOf(Soldier.class),
 
-        assertThat(board.getPiece(new Point(1, 7))).isInstanceOf(Cannon.class);
-        assertThat(board.getPiece(new Point(7, 7))).isInstanceOf(Cannon.class);
+                () -> assertThat(board.getPiece(new Point(1, 7))).isInstanceOf(Cannon.class),
+                () -> assertThat(board.getPiece(new Point(7, 7))).isInstanceOf(Cannon.class),
 
-        assertThat(board.getPiece(new Point(4, 8))).isInstanceOf(General.class);
+                () -> assertThat(board.getPiece(new Point(4, 8))).isInstanceOf(General.class),
 
-        assertThat(board.getPiece(new Point(0, 9))).isInstanceOf(Chariot.class);
-        assertThat(board.getPiece(new Point(1, 9))).isInstanceOf(Elephant.class);
-        assertThat(board.getPiece(new Point(2, 9))).isInstanceOf(Horse.class);
-        assertThat(board.getPiece(new Point(3, 9))).isInstanceOf(Guard.class);
-        assertThat(board.getPiece(new Point(5, 9))).isInstanceOf(Guard.class);
-        assertThat(board.getPiece(new Point(6, 9))).isInstanceOf(Elephant.class);
-        assertThat(board.getPiece(new Point(7, 9))).isInstanceOf(Horse.class);
-        assertThat(board.getPiece(new Point(8, 9))).isInstanceOf(Chariot.class);
+                () -> assertThat(board.getPiece(new Point(0, 9))).isInstanceOf(Chariot.class),
+                () -> assertThat(board.getPiece(new Point(1, 9))).isInstanceOf(Elephant.class),
+                () -> assertThat(board.getPiece(new Point(2, 9))).isInstanceOf(Horse.class),
+                () -> assertThat(board.getPiece(new Point(3, 9))).isInstanceOf(Guard.class),
+                () -> assertThat(board.getPiece(new Point(5, 9))).isInstanceOf(Guard.class),
+                () -> assertThat(board.getPiece(new Point(6, 9))).isInstanceOf(Elephant.class),
+                () -> assertThat(board.getPiece(new Point(7, 9))).isInstanceOf(Horse.class),
+                () -> assertThat(board.getPiece(new Point(8, 9))).isInstanceOf(Chariot.class)
+        );
     }
 
     @DisplayName("총 기물은 32개여야 한다.")
