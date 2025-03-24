e13f327f080429e58d2cda1e9ecf9ffef7bccf2f
changuii <rhljh7410@gmail.com>
2025-03-24 15:20:05 +0900

    refactor: 장기판 초기화의 3중 반복문을 읽기 쉽게 리팩터링하라

--- src/main/java/Janggi/board/BoardFactory.java
+++ src/main/java/Janggi/board/BoardFactory.java
@@ -1,25 +1,29 @@
 package Janggi.board;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import Janggi.piece.Piece;
 import Janggi.piece.Country;
+import Janggi.piece.Piece;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class BoardFactory {
 
     public Board generateBoard() {
         final Map<Position, Piece> initMap = new HashMap<>();
 
-        for (final Country country : Country.values()) {
-            for (final PieceInitialPosition pieceType : PieceInitialPosition.values()) {
-                final List<Position> initPositions = pieceType.getInitPositions(country);
-                for (final Position position : initPositions) {
-                    initMap.put(position, pieceType.createPiece(country));
-                }
-            }
+        for (final PieceInitialPosition pieceType : PieceInitialPosition.values()) {
+            initMap.putAll(initPosition(pieceType, Country.CHO));
+            initMap.putAll(initPosition(pieceType, Country.HAN));
         }
 
         return new Board(initMap);
+    }
+
+    private static Map<Position, Piece> initPosition(final PieceInitialPosition pieceType, final Country country) {
+        return pieceType.getInitPositions(country).stream()
+                .collect(Collectors.toMap(
+                        Function.identity(), position -> pieceType.createPiece(country)
+                ));
     }
 }
