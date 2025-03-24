82e95925fcbfdf81c6c4b4fd197e3d4d27afe5dc
Yeongjae Kim <sees77770@gmail.com>
2025-03-24 15:56:45 +0900

    refactor: Ma 움직임 enum 제거

--- src/main/java/domain/piece/Ma.java
+++ src/main/java/domain/piece/Ma.java
@@ -3,26 +3,21 @@ package domain.piece;
 import domain.Coordinate;
 import domain.board.Board;
 import domain.piece.movement.Movement;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Ma extends Piece {
 
-    private enum MaMovement {
+    private static final List<Movement> MIDDLE_MOVEMENTS = List.of(
+            Movement.UP, Movement.DOWN, Movement.LEFT, Movement.RIGHT
+    );
 
-        UP(Movement.UP, List.of(Movement.UP_UP_LEFT, Movement.UP_UP_RIGHT)),
-        DOWN(Movement.DOWN, List.of(Movement.DOWN_DOWN_RIGHT, Movement.DOWN_DOWN_LEFT)),
-        RIGHT(Movement.RIGHT, List.of(Movement.UP_RIGHT_RIGHT, Movement.DOWN_RIGHT_RIGHT)),
-        LEFT(Movement.LEFT, List.of(Movement.UP_LEFT_LEFT, Movement.DOWN_LEFT_LEFT));
-
-        private final Movement direction;
-        private final List<Movement> destination;
-
-        MaMovement(Movement direction, List<Movement> destination) {
-            this.direction = direction;
-            this.destination = destination;
-        }
-    }
+    private static final Map<Movement, List<Movement>> DESTINATIONS = Map.of(
+            Movement.UP, List.of(Movement.UP_UP_LEFT, Movement.UP_UP_RIGHT),
+            Movement.DOWN, List.of(Movement.DOWN_DOWN_LEFT, Movement.DOWN_DOWN_RIGHT),
+            Movement.LEFT, List.of(Movement.UP_LEFT_LEFT, Movement.DOWN_LEFT_LEFT),
+            Movement.RIGHT, List.of(Movement.UP_RIGHT_RIGHT, Movement.DOWN_RIGHT_RIGHT)
+    );
 
     public Ma(Country country) {
         super(country, PieceType.MA);
@@ -30,13 +25,12 @@ public class Ma extends Piece {
 
     @Override
     public List<Coordinate> availableMovePositions(Coordinate from, Board board) {
-        return Arrays.stream(MaMovement.values())
-                .filter(maMovement -> !board.hasPiece(from.move(maMovement.direction)))
-                .flatMap(maMovement -> maMovement.destination.stream()
+        return MIDDLE_MOVEMENTS.stream()
+                .filter(middle -> !board.hasPiece(from.move(middle)))
+                .flatMap(middle -> DESTINATIONS.get(middle).stream()
                         .map(from::move)
                         .filter(next -> !next.isOutOfBoundary())
                         .filter(next -> !board.hasPiece(next) || !board.isMyTeam(country, next))
                 ).toList();
     }
-
 }
