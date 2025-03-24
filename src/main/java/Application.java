f5e15c3ea26b1feca114f5168c353c44fd172f1a
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 15:37:55 +0900

    feat: 사용자에게 입력 받고, 결과를 출력하는 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,12 @@
-import piece.Board;
-import piece.Position;
+import static player.Nation.CHO;
+import static player.Nation.HAN;
+
+import java.util.List;
+import pieceProperty.Position;
 import piece.JanggiPieceInitializer;
+import player.Pieces;
+import player.Player;
+import player.Players;
 import view.InputView;
 import view.OutputView;
 
@@ -8,21 +14,29 @@ public class Application {
     public static void main(String[] args) {
 
         JanggiPieceInitializer janggiPieceInitializer = new JanggiPieceInitializer();
-        Board board = new Board(janggiPieceInitializer.hanInit(), janggiPieceInitializer.choInit());
-        OutputView outputView = new OutputView();
+        Pieces choPieces = janggiPieceInitializer.choInit();
+        Pieces hanPieces = janggiPieceInitializer.hanInit();
+        Player hanPlayer = new Player(hanPieces, HAN);
+        Player choPlayer = new Player(choPieces, CHO);
+        Players players = new Players(List.of(choPlayer, hanPlayer));
+        JanggiGameState janggiGameState = new JanggiGameState(players);
+
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
 
-        while (true) {
-            try {
-                outputView.printJanggipan(board.getJanggiPan());
-                Position presentPosition = inputView.readPresentPick();
-                Position futurePosition = inputView.readFuturePick();
+        while (!janggiGameState.isGameOver()) {
+            try{
+                outputView.printJanggiPan(hanPieces, choPieces);
+                Position presentPosition = inputView.getPresentPosition(janggiGameState.getAttackNation());
+                Position destination = inputView.getDestination();
 
-                board.updateBoard(presentPosition, futurePosition);
+                janggiGameState.movePiece(presentPosition, destination);
             } catch (IllegalArgumentException e) {
-                outputView.printErrorMessage(e.getMessage());
+                System.out.println(e.getMessage());
             }
+
         }
+
     }
 }
 
