2c27b8c76372aeabb052561ee2f4235ebf104be8
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-24 21:38:09 +0900

    fix: 테스트 위한 상수 깨진 문제 수정

--- src/test/java/move/SangMoveBehaviorTest.java
+++ src/test/java/move/SangMoveBehaviorTest.java
@@ -22,7 +22,7 @@ class SangMoveBehaviorTest {
 
         org.assertj.core.api.Assertions.assertThatCode(() -> {
             org.junit.jupiter.api.Assertions.assertAll(
-                    () -> Assertions.assertThat(route).hasSize(2),
+                    () -> Assertions.assertThat(route).hasSize(3),
                     () -> Assertions.assertThatIterable(route).containsExactlyElementsOf(expectPositions)
             );
         }).doesNotThrowAnyException();
