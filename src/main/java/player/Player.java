957b9af06cec4a613435f98f06a7845b550805c1
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 11:48:40 +0900

    feat: Player 에 있는 메서드 기능 구현

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,5 +1,8 @@
 package player;
 
+import pieceProperty.Position;
+import pieceProperty.Positions;
+
 public class Player {
     private final Pieces pieces;
     private final Nation nation;
@@ -9,7 +12,37 @@ public class Player {
         this.nation = nation;
     }
 
+    public void movePiece(Position presentPosition, Position destination) {
+    }
+
+    public void validateAllyPieceAtStart(Position presentPosition) {
+        pieces.validateAllyPieceAtStart(presentPosition);
+    }
+
+    public void validateAllyPieceAtDestination(Position destination) {
+        pieces.validateAllyPieceAtDestination(destination);
+    }
+
+    public void canPieceMoveTo(Position presentPosition, Position destination) {
+        pieces.canPieceMoveTo(presentPosition, destination);
+    }
+
+    public Positions makeRoute(Position presentPosition, Position destination) {
+        return pieces.makeRoute(presentPosition, destination);
+    }
+
+    public void removePiece(Position destination) {
+    }
+
+    public boolean isSameNation(Nation nation) {
+        return this.nation.equals(nation);
+    }
+
     public boolean isKingDie() {
         return pieces.isKingDie();
+    }
+
+    public Pieces getPieces() {
+        return pieces;
     }
 }
