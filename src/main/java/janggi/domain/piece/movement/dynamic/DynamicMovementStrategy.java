b950a89f1930347f68926dd983faf5531270e3c1
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-24 11:27:44 +0900

    feat: 동적 움직임 전략 내부 구현

---
+++ src/main/java/janggi/domain/piece/movement/dynamic/DynamicMovementStrategy.java
@@ -0,0 +1,18 @@
+package janggi.domain.piece.movement.dynamic;
+
+import janggi.domain.Position;
+import janggi.domain.Side;
+import janggi.domain.piece.Pieces;
+import janggi.domain.piece.movement.MovementStrategy;
+
+public interface DynamicMovementStrategy extends MovementStrategy {
+
+    @Override
+    default boolean isMoveable(Pieces map, Position origin, Side side, Position destination) {
+        return isLegalDestination(side, origin, destination) && isLegalPath(map, side, origin, destination);
+    }
+
+    boolean isLegalDestination(Side side, Position origin, Position destination);
+
+    boolean isLegalPath(Pieces existingPieces, Side side, Position origin, Position destination);
+}
