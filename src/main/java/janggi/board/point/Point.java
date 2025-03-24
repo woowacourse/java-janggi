50a85ed1b85525223de444ae178c0d1653123a6a
Yehyeok Bang <qkddpgur318@gmail.com>
2025-03-25 01:53:59 +0900

    feat: 위치 비교 및 1칸 이동 여부 확인 기능 추가
    - 좌표계를 기준으로 뒤에 있는지, 맨해튼 거리가 1인지 확인하는 기능을 추가합니다.

--- src/main/java/janggi/board/point/Point.java
+++ src/main/java/janggi/board/point/Point.java
@@ -20,4 +20,16 @@ public record Point(
     public int calculateYDistance(Point otherPoint) {
         return Math.abs(this.y - otherPoint.y);
     }
+
+    public boolean isBehind(Point other) {
+        return this.y < other.y;
+    }
+
+    public boolean isOneStepAway(Point other) {
+        return manhattanDistance(other) == 1;
+    }
+
+    private int manhattanDistance(Point other) {
+        return calculateXDistance(other) + calculateYDistance(other);
+    }
 }
--- src/test/java/janggi/board/point/PointTest.java
+++ src/test/java/janggi/board/point/PointTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PointTest {
 
@@ -54,5 +56,38 @@ class PointTest {
         // when & then
         assertThat(point.equals(otherPoint))
                 .isTrue();
+    }
+
+    @DisplayName("뒤에 있는지 확인한다. (y가 더 작은지 확인)")
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, 0, 1, true",
+            "0, 1, 0, 0, false",
+    })
+    void isBehindTest(int x, int y, int otherX, int otherY, boolean expected) {
+        // given
+        Point point = new Point(x, y);
+        Point otherPoint = new Point(otherX, otherY);
+
+        // when & then
+        assertThat(point.isBehind(otherPoint))
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("한 칸만 떨어져 있는지 확인한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, 0, 1, true",
+            "0, 0, 1, 0, true",
+            "0, 0, 1, 1, false",
+    })
+    void isOneStepAwayTest(int fromX, int fromY, int toX, int toY, boolean expected) {
+        // given
+        Point point = new Point(fromX, fromY);
+        Point otherPoint = new Point(toX, toY);
+
+        // when & then
+        assertThat(point.isOneStepAway(otherPoint))
+                .isEqualTo(expected);
     }
 }
