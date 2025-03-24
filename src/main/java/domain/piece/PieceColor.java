9eb65994e614c2c5183149e0b1e4d570631fa526
geonwoo <kysub99@naver.com>
2025-03-24 21:48:26 +0900

    refactor: PieceColor에서 name 필드 삭제

--- src/main/java/domain/piece/PieceColor.java
+++ src/main/java/domain/piece/PieceColor.java
@@ -1,18 +1,8 @@
 package domain.piece;
 
 public enum PieceColor {
-    RED("한나라"),
-    BLUE("초나라"),
-    NONE(""),
+    RED,
+    BLUE,
+    NONE,
     ;
-
-    private final String teamName;
-
-    PieceColor(String teamName) {
-        this.teamName = teamName;
-    }
-
-    public String getTeamName() {
-        return teamName;
-    }
 }
