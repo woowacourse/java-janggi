2a7b27a61f73dff66adeff35fa90719837884285
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-03-24 15:10:54 +0900

    refactor: Movement를 이용한 Chariot 로직 수정

--- src/main/java/model/Chariot.java
+++ src/main/java/model/Chariot.java
@@ -1,9 +1,16 @@
 package model;
 
+import static model.Movement.DOWN;
+import static model.Movement.LEFT;
+import static model.Movement.RIGHT;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Chariot extends Piece {
+
+    private final List<Movement> movements = List.of(Movement.UP, DOWN, LEFT, RIGHT);
 
     public Chariot(Team team) {
         super(team);
@@ -16,17 +23,22 @@ public class Chariot extends Piece {
 
     @Override
     public List<Position> calculateAllDirection(Position departure, Position arrival) {
-        List<List<Position>> allDirection = List.of(
-            departure.findUpDirectionUntilEnd(arrival),
-            departure.findDownDirectionUntilEnd(arrival),
-            departure.findLeftDirectionUntilEnd(arrival),
-            departure.findRightDirectionUntilEnd(arrival)
-        );
+        return calculatePositionOfMovement(departure, arrival);
+    }
 
-        return allDirection.stream()
-            .filter(direction -> !direction.isEmpty())
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("해당 위치로는 이동할 수 없습니다."));
+    private List<Position> calculatePositionOfMovement(Position departure, Position arrival) {
+        for (Movement movement : movements) {
+            List<Position> temporaryPosition = new ArrayList<>();
+            Position movedPosition = departure.copyOf();
+            while (movedPosition.canMove(movement)) {
+                temporaryPosition.add(movedPosition.move(movement));
+                movedPosition = movedPosition.move(movement);
+                if (movedPosition.equals(arrival)) {
+                    return temporaryPosition;
+                }
+            }
+        }
+        throw new IllegalArgumentException("해당 위치로 이동할 수 없습니다.");
     }
 
     @Override
