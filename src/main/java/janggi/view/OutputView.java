6b1b12950fcc0f7795f5c2af73b033dab029927e
DongYeop <s01054830455@gmail.com>
2025-03-25 03:25:40 +0900

    refactor: 기물 이동 성공 메세지 출력 개행 추가

--- src/main/java/janggi/view/OutputView.java
+++ src/main/java/janggi/view/OutputView.java
@@ -79,7 +79,7 @@ public class OutputView {
     }
 
     public void printSuccessMove() {
-        System.out.printf("성공적으로 기물을 이동했습니다.");
+        System.out.println("성공적으로 기물을 이동했습니다.");
     }
 
     public void printErrorMessage(final String message) {
