6b1cc7e50d11f23bd837e9dd1f8884fff52a3b12
taek2222 <oot309@daum.net>
2025-03-24 18:29:40 +0900

    refactor: 이동 전략 추상 클래스로 변경 및 중복 로직 제거

--- src/main/java/domain/strategy/InnerElephantInitializer.java
+++ src/main/java/domain/strategy/InnerElephantInitializer.java
@@ -5,18 +5,12 @@ import domain.Team;
 import domain.direction.PieceDirection;
 import domain.piece.Piece;
 import domain.piece.Pieces;
-import domain.piece.category.Cannon;
-import domain.piece.category.Chariot;
 import domain.piece.category.Elephant;
-import domain.piece.category.Guard;
 import domain.piece.category.Horse;
-import domain.piece.category.King;
-import domain.piece.category.Soldier;
 import domain.spatial.Position;
-import java.util.ArrayList;
 import java.util.List;
 
-public class InnerElephantInitializer implements SettingUpInitializer {
+public class InnerElephantInitializer extends SettingUpInitializer {
 
     @Override
     public Pieces initPieces(final Player player) {
@@ -27,57 +21,26 @@ public class InnerElephantInitializer implements SettingUpInitializer {
     }
 
     private Pieces initHanPieces() {
-        List<Piece> hanPieces = new ArrayList<>();
-        hanPieces.add(new King(new Position(5, 2), PieceDirection.KING.get()));
+        List<Piece> pieces = initHanBasicPieces();
 
-        hanPieces.add(new Chariot(new Position(1, 1), PieceDirection.CHARIOT.get()));
-        hanPieces.add(new Chariot(new Position(9, 1), PieceDirection.CHARIOT.get()));
+        pieces.add(new Horse(new Position(2, 1), PieceDirection.HORSE.get()));
+        pieces.add(new Horse(new Position(8, 1), PieceDirection.HORSE.get()));
 
-        hanPieces.add(new Cannon(new Position(2, 3), PieceDirection.CANNON.get()));
-        hanPieces.add(new Cannon(new Position(8, 3), PieceDirection.CANNON.get()));
+        pieces.add(new Elephant(new Position(3, 1), PieceDirection.ELEPHANT.get()));
+        pieces.add(new Elephant(new Position(7, 1), PieceDirection.ELEPHANT.get()));
 
-        hanPieces.add(new Horse(new Position(2, 1), PieceDirection.HORSE.get()));
-        hanPieces.add(new Horse(new Position(8, 1), PieceDirection.HORSE.get()));
-
-        hanPieces.add(new Elephant(new Position(3, 1), PieceDirection.ELEPHANT.get()));
-        hanPieces.add(new Elephant(new Position(7, 1), PieceDirection.ELEPHANT.get()));
-
-        hanPieces.add(new Guard(new Position(4, 1), PieceDirection.GUARD.get()));
-        hanPieces.add(new Guard(new Position(6, 1), PieceDirection.GUARD.get()));
-
-        hanPieces.add(new Soldier(new Position(1, 4), PieceDirection.HAN_SOLDIER.get()));
-        hanPieces.add(new Soldier(new Position(3, 4), PieceDirection.HAN_SOLDIER.get()));
-        hanPieces.add(new Soldier(new Position(5, 4), PieceDirection.HAN_SOLDIER.get()));
-        hanPieces.add(new Soldier(new Position(7, 4), PieceDirection.HAN_SOLDIER.get()));
-        hanPieces.add(new Soldier(new Position(9, 4), PieceDirection.HAN_SOLDIER.get()));
-        return new Pieces(hanPieces);
+        return new Pieces(pieces);
     }
 
     private Pieces initChoPieces() {
-        List<Piece> choPieces = new ArrayList<>();
-        choPieces.add(new King(new Position(5, 9), PieceDirection.KING.get()));
+        List<Piece> pieces = initChoBasicPieces();
 
-        choPieces.add(new Chariot(new Position(1, 10), PieceDirection.CHARIOT.get()));
-        choPieces.add(new Chariot(new Position(9, 10), PieceDirection.CHARIOT.get()));
+        pieces.add(new Horse(new Position(2, 10), PieceDirection.HORSE.get()));
+        pieces.add(new Horse(new Position(8, 10), PieceDirection.HORSE.get()));
 
-        choPieces.add(new Cannon(new Position(2, 8), PieceDirection.CANNON.get()));
-        choPieces.add(new Cannon(new Position(8, 8), PieceDirection.CANNON.get()));
+        pieces.add(new Elephant(new Position(3, 10), PieceDirection.ELEPHANT.get()));
+        pieces.add(new Elephant(new Position(7, 10), PieceDirection.ELEPHANT.get()));
 
-        choPieces.add(new Horse(new Position(2, 10), PieceDirection.HORSE.get()));
-        choPieces.add(new Horse(new Position(8, 10), PieceDirection.HORSE.get()));
-
-        choPieces.add(new Elephant(new Position(3, 10), PieceDirection.ELEPHANT.get()));
-        choPieces.add(new Elephant(new Position(7, 10), PieceDirection.ELEPHANT.get()));
-
-        choPieces.add(new Guard(new Position(4, 10), PieceDirection.GUARD.get()));
-        choPieces.add(new Guard(new Position(6, 10), PieceDirection.GUARD.get()));
-
-        choPieces.add(new Soldier(new Position(1, 7), PieceDirection.CHO_SOLDIER.get()));
-        choPieces.add(new Soldier(new Position(3, 7), PieceDirection.CHO_SOLDIER.get()));
-        choPieces.add(new Soldier(new Position(5, 7), PieceDirection.CHO_SOLDIER.get()));
-        choPieces.add(new Soldier(new Position(7, 7), PieceDirection.CHO_SOLDIER.get()));
-        choPieces.add(new Soldier(new Position(9, 7), PieceDirection.CHO_SOLDIER.get()));
-
-        return new Pieces(choPieces);
+        return new Pieces(pieces);
     }
 }
--- src/main/java/domain/strategy/SettingUpInitializer.java
+++ src/main/java/domain/strategy/SettingUpInitializer.java
@@ -1,8 +1,65 @@
 package domain.strategy;
 
 import domain.Player;
+import domain.direction.PieceDirection;
+import domain.piece.Piece;
 import domain.piece.Pieces;
+import domain.piece.category.Cannon;
+import domain.piece.category.Chariot;
+import domain.piece.category.Guard;
+import domain.piece.category.King;
+import domain.piece.category.Soldier;
+import domain.spatial.Position;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface SettingUpInitializer {
-    Pieces initPieces(final Player player);
+public abstract class SettingUpInitializer {
+
+    public abstract Pieces initPieces(final Player player);
+
+    List<Piece> initHanBasicPieces() {
+        List<Piece> pieces = new ArrayList<>();
+
+        pieces.add(new King(new Position(5, 2), PieceDirection.KING.get()));
+
+        pieces.add(new Chariot(new Position(1, 1), PieceDirection.CHARIOT.get()));
+        pieces.add(new Chariot(new Position(9, 1), PieceDirection.CHARIOT.get()));
+
+        pieces.add(new Cannon(new Position(2, 3), PieceDirection.CANNON.get()));
+        pieces.add(new Cannon(new Position(8, 3), PieceDirection.CANNON.get()));
+
+        pieces.add(new Guard(new Position(4, 1), PieceDirection.GUARD.get()));
+        pieces.add(new Guard(new Position(6, 1), PieceDirection.GUARD.get()));
+
+        pieces.add(new Soldier(new Position(1, 4), PieceDirection.HAN_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(3, 4), PieceDirection.HAN_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(5, 4), PieceDirection.HAN_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(7, 4), PieceDirection.HAN_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(9, 4), PieceDirection.HAN_SOLDIER.get()));
+
+        return pieces;
+    }
+
+    List<Piece> initChoBasicPieces() {
+        List<Piece> pieces = new ArrayList<>();
+
+        pieces.add(new King(new Position(5, 9), PieceDirection.KING.get()));
+
+        pieces.add(new Chariot(new Position(1, 10), PieceDirection.CHARIOT.get()));
+        pieces.add(new Chariot(new Position(9, 10), PieceDirection.CHARIOT.get()));
+
+        pieces.add(new Cannon(new Position(2, 8), PieceDirection.CANNON.get()));
+        pieces.add(new Cannon(new Position(8, 8), PieceDirection.CANNON.get()));
+
+        pieces.add(new Guard(new Position(4, 10), PieceDirection.GUARD.get()));
+        pieces.add(new Guard(new Position(6, 10), PieceDirection.GUARD.get()));
+
+        pieces.add(new Soldier(new Position(1, 7), PieceDirection.CHO_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(3, 7), PieceDirection.CHO_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(5, 7), PieceDirection.CHO_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(7, 7), PieceDirection.CHO_SOLDIER.get()));
+        pieces.add(new Soldier(new Position(9, 7), PieceDirection.CHO_SOLDIER.get()));
+
+        return pieces;
+    }
 }
