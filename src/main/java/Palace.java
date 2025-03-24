5652b31a3d18d7db8e27525366781efa2287318a
sonjh919 <kingjh1125@gmail.com>
2025-03-24 11:24:43 +0900

    feat: 궁 생성

---
+++ src/main/java/Palace.java
@@ -0,0 +1,4 @@
+public class Palace extends Piece {
+
+
+}
---
+++ src/test/java/PalaceTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PalaceTest {
+
+    @Test
+    @DisplayName("장기 말의 종류에는 궁이 있다.")
+    void createPalaceTest(){
+        Piece palace = new Palace();
+    }
+}
