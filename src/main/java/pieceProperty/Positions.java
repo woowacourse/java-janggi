826060c626d669d8f4ef874daa1fef10d7be0c86
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 12:19:35 +0900

    feat: 경로 상에 같은 위치 기물 확인 기능 구현

--- src/main/java/pieceProperty/Positions.java
+++ src/main/java/pieceProperty/Positions.java
@@ -2,6 +2,7 @@ package pieceProperty;
 
 import java.util.ArrayList;
 import java.util.List;
+import piece.Piece;
 
 public class Positions {
     private final List<Position> positions;
@@ -13,7 +14,12 @@ public class Positions {
     public void addPosition(Position position) {
         positions.add(position);
     }
+
     public List<Position> getPositions() {
         return positions;
+    }
+
+    public boolean containsPosition(Piece piece) {
+        return positions.stream().anyMatch(piece::isSamePosition);
     }
 }
