c4b9719224818e1823724ceb6d07affe6e432164
KJungW <sagakkb0707@naver.com>
2025-03-24 10:38:19 +0900

    test(piece): 마 테스트 정리

--- src/test/java/janggi/piece/MaTest.java
+++ src/test/java/janggi/piece/MaTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import janggi.value.Position;
 import java.util.List;
 import java.util.stream.Stream;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,72 +15,67 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class MaTest {
 
-    static final Position STANDARD = new Position(4, 4);
+    static final Position START_POSITION = new Position(4, 4);
+    static final Position PATH_POSITION = new Position(5, 4);
+    static final Position FIRST_DESTINATION = new Position(6, 3);
+    static final Position SECOND_DESTINATION = new Position(6, 3);
+
 
     @DisplayName("장기말을 이동시킬 수 있다.")
     @ParameterizedTest
     @MethodSource()
-    void test1(Position destination) {
-        //given
-        Ma ma = new Ma(STANDARD);
+    void canMove(Position destination) {
+        Ma ma = new Ma(START_POSITION);
 
-        //when
         Ma movedMa = ma.move(destination, List.of(), List.of());
 
-        //then
         assertThat(movedMa.getPosition()).isEqualTo(destination);
     }
 
-    static Stream<Arguments> test1() {
+    static Stream<Arguments> canMove() {
         return Stream.of(
-                Arguments.of(new Position(STANDARD.x() + 2, STANDARD.y() + 1)),
-                Arguments.of(new Position(STANDARD.x() + 2, STANDARD.y() - 1)),
-                Arguments.of(new Position(STANDARD.x() + 1, STANDARD.y() - 2)),
-                Arguments.of(new Position(STANDARD.x() + 1, STANDARD.y() + 2)),
-                Arguments.of(new Position(STANDARD.x() - 1, STANDARD.y() - 2)),
-                Arguments.of(new Position(STANDARD.x() - 1, STANDARD.y() + 2)),
-                Arguments.of(new Position(STANDARD.x() - 2, STANDARD.y() - 1)),
-                Arguments.of(new Position(STANDARD.x() - 2, STANDARD.y() + 1))
+                Arguments.of(new Position(START_POSITION.x() + 2, START_POSITION.y() + 1)),
+                Arguments.of(new Position(START_POSITION.x() + 2, START_POSITION.y() - 1)),
+                Arguments.of(new Position(START_POSITION.x() + 1, START_POSITION.y() - 2)),
+                Arguments.of(new Position(START_POSITION.x() + 1, START_POSITION.y() + 2)),
+                Arguments.of(new Position(START_POSITION.x() - 1, START_POSITION.y() - 2)),
+                Arguments.of(new Position(START_POSITION.x() - 1, START_POSITION.y() + 2)),
+                Arguments.of(new Position(START_POSITION.x() - 2, START_POSITION.y() - 1)),
+                Arguments.of(new Position(START_POSITION.x() - 2, START_POSITION.y() + 1))
         );
     }
-
 
     @DisplayName("장기말의 이동 규칙에 어긋난 경우 이동이 불가능합니다.")
     @ParameterizedTest
     @MethodSource()
-    void test2(Position destination) {
-        //given
-        Ma ma = new Ma(STANDARD);
+    void canNotMoveBecauseRuleOfMove(Position destination) {
+        Ma ma = new Ma(START_POSITION);
 
-        //when & then
         assertThatThrownBy(() -> ma.move(destination, List.of(), List.of()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 이동이 불가능합니다.");
     }
 
-    static Stream<Arguments> test2() {
+    static Stream<Arguments> canNotMoveBecauseRuleOfMove() {
         return Stream.of(
-                Arguments.of(new Position(STANDARD.x() + 1, STANDARD.y() + 1)),
-                Arguments.of(new Position(STANDARD.x() + 1, STANDARD.y() - 1)),
-                Arguments.of(new Position(STANDARD.x() - 1, STANDARD.y() + 1)),
-                Arguments.of(new Position(STANDARD.x() - 1, STANDARD.y() - 1))
+                Arguments.of(new Position(START_POSITION.x() + 1, START_POSITION.y() + 1)),
+                Arguments.of(new Position(START_POSITION.x() + 1, START_POSITION.y() - 1)),
+                Arguments.of(new Position(START_POSITION.x() - 1, START_POSITION.y() + 1)),
+                Arguments.of(new Position(START_POSITION.x() - 1, START_POSITION.y() - 1))
         );
     }
 
-    @DisplayName("아군 장기말이 직진 경로안에 장애물로 있을 경우 이동이 불가능하다.")
+    @DisplayName("아군 장기말이 경로상에 장애물로 있을 경우 이동이 불가능하다.")
     @Test
-    void test4() {
-        //given
-        Ma ma = new Ma(STANDARD);
-        Position hurdlePosition = new Position(5, 4);
-        Ma hurdle = new Ma(hurdlePosition);
+    void canNotMoveBecauseAlliesInPath() {
+        Ma ma = new Ma(START_POSITION);
+        Ma alliesPieceInPath = new Ma(PATH_POSITION);
 
-        //when & then
         assertAll(
-                () -> assertThatThrownBy(() -> ma.move(new Position(6, 3), List.of(), List.of(hurdle)))
+                () -> assertThatThrownBy(() -> ma.move(FIRST_DESTINATION, List.of(), List.of(alliesPieceInPath)))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("[ERROR] 이동이 불가능합니다."),
-                () -> assertThatThrownBy(() -> ma.move(new Position(6, 5), List.of(), List.of(hurdle)))
+                () -> assertThatThrownBy(() -> ma.move(SECOND_DESTINATION, List.of(), List.of(alliesPieceInPath)))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("[ERROR] 이동이 불가능합니다.")
         );
@@ -89,31 +83,26 @@ public class MaTest {
 
     @DisplayName("아군 장기말이 목적지에 장애물로 있을 경우 이동이 불가능하다.")
     @Test
-    void test5() {
-        //given
-        Ma ma = new Ma(STANDARD);
-        Position hurdle = new Position(6, 3);
+    void canNotMoveBecauseAlliesInDestination() {
+        Ma ma = new Ma(START_POSITION);
+        Ma alliesPieceInDestination = new Ma(FIRST_DESTINATION);
 
-        //when & then
-        assertThatThrownBy(() -> ma.move(hurdle, List.of(), List.of(new Ma(hurdle))))
+        assertThatThrownBy(() -> ma.move(FIRST_DESTINATION, List.of(), List.of(alliesPieceInDestination)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 이동이 불가능합니다.");
     }
 
     @DisplayName("상대 장기말이 경로에 장애물로 있을 경우 이동이 불가능하다.")
     @Test
-    void test6() {
-        //given
-        Ma ma = new Ma(STANDARD);
-        Position hurdlePosition = new Position(5, 4);
-        Ma hurdle = new Ma(hurdlePosition);
+    void canNotMoveBecauseEnemyInPath() {
+        Ma ma = new Ma(START_POSITION);
+        Ma enemyPiece = new Ma(PATH_POSITION);
 
-        //when & then
         assertAll(
-                () -> assertThatThrownBy(() -> ma.move(new Position(6, 3), List.of(hurdle), List.of()))
+                () -> assertThatThrownBy(() -> ma.move(FIRST_DESTINATION, List.of(enemyPiece), List.of()))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("[ERROR] 이동이 불가능합니다."),
-                () -> assertThatThrownBy(() -> ma.move(new Position(6, 5), List.of(hurdle), List.of()))
+                () -> assertThatThrownBy(() -> ma.move(SECOND_DESTINATION, List.of(enemyPiece), List.of()))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("[ERROR] 이동이 불가능합니다.")
         );
@@ -121,16 +110,12 @@ public class MaTest {
 
     @DisplayName("상대 장기말이 목적지에 있을 경우 이동이 가능하다.")
     @Test
-    void test7() {
-        //given
-        Ma ma = new Ma(STANDARD);
-        Position hurdle = new Position(6, 3);
+    void canMoveWithEnemyInDestination() {
+        Ma ma = new Ma(START_POSITION);
+        Ma enemyPiece = new Ma(FIRST_DESTINATION);
 
-        //when
-        Ma movedMa = ma.move(hurdle, List.of(new Ma(hurdle)), List.of());
+        Ma movedMa = ma.move(FIRST_DESTINATION, List.of(enemyPiece), List.of());
 
-        //then
-        Assertions.assertThat(movedMa.getPosition()).isEqualTo(hurdle);
+        assertThat(movedMa.getPosition()).isEqualTo(FIRST_DESTINATION);
     }
-
 }
