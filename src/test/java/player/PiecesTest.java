9437fc776595342dfd46f894f9111f4252be45c9
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 16:13:42 +0900

    test: 경로 상에 포 존재 유무 확인 테스트

--- src/test/java/player/PiecesTest.java
+++ src/test/java/player/PiecesTest.java
@@ -159,4 +159,25 @@ class PiecesTest {
         assertThat(pieces.isPoAt(new Position(4, 3))).isTrue();
     }
 
+    @Test
+    @DisplayName("경로에 포 존재 확인 테스트")
+    void isExistPoInRoute() {
+        //given
+        Jol jol = new Jol(new Position(5, 5));
+        Janggun janggun = new Janggun(new Position(6, 5));
+        Po po = new Po(new Position(4, 3));
+        Pieces pieces = new Pieces(List.of(jol, janggun, po));
+        Positions route1 = new Positions(
+                List.of(new Position(4, 3))
+        );
+
+        Positions route2 = new Positions(
+                List.of(new Position(0, 0))
+        );
+
+        //when-then
+        assertThat(pieces.isExistPoInRoute(route1)).isTrue();
+        assertThat(pieces.isExistPoInRoute(route2)).isFalse();
+    }
+
 }
