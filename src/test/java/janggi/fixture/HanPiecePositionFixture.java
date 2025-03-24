0fad4c1068bf4621ad0ed328e80ec9eb12a7ce22
KJungW <sagakkb0707@naver.com>
2025-03-24 15:55:14 +0900

    test(piece): 한의 초기배치 테스트 추가

--- src/test/java/janggi/fixture/HanPiecePositionFixture.java
+++ src/test/java/janggi/fixture/HanPiecePositionFixture.java
@@ -5,11 +5,31 @@ import java.util.List;
 
 public class HanPiecePositionFixture {
 
-    public final static List<Position> GUNG_POSITIONS_IN_HAN = List.of(new Position(4, 1));
-    public final static List<Position> CHA_POSITIONS_IN_HAN = List.of(new Position(0, 0), new Position(8, 0));
-    public final static List<Position> SA_POSITIONS_IN_HAN = List.of(new Position(3, 0), new Position(5, 0));
-    public final static List<Position> PO_POSITIONS_IN_HAN = List.of(new Position(1, 2), new Position(7, 2));
-    public final static List<Position> JOL_POSITIONS_IN_HAN = List.of(
+    public final static List<Position> GUNG_POSITIONS = List.of(
+            new Position(4, 1));
+    public final static List<Position> CHA_POSITIONS = List.of(
+            new Position(0, 0), new Position(8, 0));
+    public final static List<Position> SA_POSITIONS = List.of(
+            new Position(3, 0), new Position(5, 0));
+    public final static List<Position> PO_POSITIONS = List.of(
+            new Position(1, 2), new Position(7, 2));
+    public final static List<Position> JOL_POSITIONS = List.of(
             new Position(0, 3), new Position(2, 3), new Position(4, 3),
             new Position(6, 3), new Position(8, 3));
+    public final static List<Position> MA_POSITIONS_WITH_LEFT_SANG = List.of(
+            new Position(2, 0), new Position(7, 0));
+    public final static List<Position> MA_POSITIONS_WITH_RIGHT_SANG = List.of(
+            new Position(1, 0), new Position(6, 0));
+    public final static List<Position> MA_POSITIONS_WITH_IN_SANG = List.of(
+            new Position(1, 0), new Position(7, 0));
+    public final static List<Position> MA_POSITIONS_WITH_OUT_SANG = List.of(
+            new Position(2, 0), new Position(6, 0));
+    public final static List<Position> SANG_POSITIONS_WITH_LEFT_SANG = List.of(
+            new Position(1, 0), new Position(6, 0));
+    public final static List<Position> SANG_POSITIONS_WITH_RIGHT_SANG = List.of(
+            new Position(2, 0), new Position(7, 0));
+    public final static List<Position> SANG_POSITIONS_WITH_IN_SANG = List.of(
+            new Position(2, 0), new Position(6, 0));
+    public final static List<Position> SANG_POSITIONS_WITH_OUT_SANG = List.of(
+            new Position(1, 0), new Position(7, 0));
 }
--- src/test/java/janggi/setting/PieceAssignTypeTest.java
+++ src/test/java/janggi/setting/PieceAssignTypeTest.java
@@ -3,6 +3,7 @@ package janggi.setting;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import janggi.fixture.ChoPiecePositionFixture;
+import janggi.fixture.HanPiecePositionFixture;
 import janggi.piece.Piece;
 import janggi.piece.PieceType;
 import janggi.value.Position;
@@ -96,28 +97,78 @@ class PieceAssignTypeTest {
         );
     }
 
-//    @Test
-//    @DisplayName("한의 초기 장기말 배치를 할 수 있다.")
-//    void canAssignPieceInHanCamp() {
-//        List<Piece> pieces = PieceAssignType.LEFT_SANG.makeAssign(CampType.HAN);
-//
-//        List<Piece> gung = pieces.stream().filter(piece -> piece.checkPieceType(PieceType.GUNG)).toList();
-//        List<Piece> cha = pieces.stream().filter(piece -> piece.checkPieceType(PieceType.GUNG)).toList();
-//        List<Piece> sa = pieces.stream().filter(piece -> piece.checkPieceType(PieceType.GUNG)).toList();
-//        List<Piece> po = pieces.stream().filter(piece -> piece.checkPieceType(PieceType.GUNG)).toList();
-//        List<Piece> jol = pieces.stream().filter(piece -> piece.checkPieceType(PieceType.GUNG)).toList();
-//
-//        assertAll(
-//                () -> assertThat(gung).extracting(Piece::getPosition)
-//                        .containsExactlyInAnyOrderElementsOf(HanPiecePositionFixture.GUNG_POSITIONS_IN_HAN),
-//                () -> assertThat(cha).extracting(Piece::getPosition)
-//                        .containsExactlyInAnyOrderElementsOf(HanPiecePositionFixture.CHA_POSITIONS_IN_HAN),
-//                () -> assertThat(sa).extracting(Piece::getPosition)
-//                        .containsExactlyInAnyOrderElementsOf(HanPiecePositionFixture.SA_POSITIONS_IN_HAN),
-//                () -> assertThat(po).extracting(Piece::getPosition)
-//                        .containsExactlyInAnyOrderElementsOf(HanPiecePositionFixture.PO_POSITIONS_IN_HAN),
-//                () -> assertThat(jol).extracting(Piece::getPosition)
-//                        .containsExactlyInAnyOrderElementsOf(HanPiecePositionFixture.JOL_POSITIONS_IN_HAN)
-//        );
-//    }
+    @ParameterizedTest
+    @DisplayName("한의 초기 장기말 배치를 할 수 있다.")
+    @MethodSource
+    void canAssignPieceInHan(PieceType pieceType, List<Position> piecePositions) {
+        List<Piece> pieces = PieceAssignType.LEFT_SANG.makeAssign(CampType.HAN);
+        List<Piece> pieceInPieceType = pieces.stream().filter(piece -> piece.checkPieceType(pieceType)).toList();
+
+        assertThat(pieceInPieceType).extracting(Piece::getPosition)
+                .containsExactlyInAnyOrderElementsOf(piecePositions);
+    }
+
+    static Stream<Arguments> canAssignPieceInHan() {
+        return Stream.of(
+                Arguments.of(PieceType.GUNG, HanPiecePositionFixture.GUNG_POSITIONS),
+                Arguments.of(PieceType.CHA, HanPiecePositionFixture.CHA_POSITIONS),
+                Arguments.of(PieceType.SA, HanPiecePositionFixture.SA_POSITIONS),
+                Arguments.of(PieceType.PO, HanPiecePositionFixture.PO_POSITIONS),
+                Arguments.of(PieceType.JOL, HanPiecePositionFixture.JOL_POSITIONS)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("초기 배치 타입에 따라 한의 마 장기말의 초기 위치를 결정할 수 있다.")
+    @MethodSource
+    void canAssignMaByAssignTypeInHan(PieceAssignType assignType, List<Position> expectedMaPositions) {
+        List<Piece> pieces = assignType.makeAssign(CampType.HAN);
+
+        List<Piece> actualMaPositions = pieces.stream()
+                .filter(piece -> piece.checkPieceType(PieceType.MA))
+                .toList();
+
+        assertThat(actualMaPositions).extracting(Piece::getPosition)
+                .containsExactlyInAnyOrderElementsOf(expectedMaPositions);
+    }
+
+    static Stream<Arguments> canAssignMaByAssignTypeInHan() {
+        return Stream.of(
+                Arguments.of(PieceAssignType.LEFT_SANG,
+                        HanPiecePositionFixture.MA_POSITIONS_WITH_LEFT_SANG),
+                Arguments.of(PieceAssignType.RIGHT_SANG,
+                        HanPiecePositionFixture.MA_POSITIONS_WITH_RIGHT_SANG),
+                Arguments.of(PieceAssignType.IN_SANG,
+                        HanPiecePositionFixture.MA_POSITIONS_WITH_IN_SANG),
+                Arguments.of(PieceAssignType.OUT_SANG,
+                        HanPiecePositionFixture.MA_POSITIONS_WITH_OUT_SANG)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("초기 배치 타입에 따라 한의 상 장기말의 초기 위치를 결정할 수 있다.")
+    @MethodSource
+    void canAssignSangByAssignTypeInHan(PieceAssignType assignType, List<Position> expectedSangPositions) {
+        List<Piece> pieces = assignType.makeAssign(CampType.HAN);
+
+        List<Piece> actualMaPositions = pieces.stream()
+                .filter(piece -> piece.checkPieceType(PieceType.SANG))
+                .toList();
+
+        assertThat(actualMaPositions).extracting(Piece::getPosition)
+                .containsExactlyInAnyOrderElementsOf(expectedSangPositions);
+    }
+
+    static Stream<Arguments> canAssignSangByAssignTypeInHan() {
+        return Stream.of(
+                Arguments.of(PieceAssignType.LEFT_SANG,
+                        HanPiecePositionFixture.SANG_POSITIONS_WITH_LEFT_SANG),
+                Arguments.of(PieceAssignType.RIGHT_SANG,
+                        HanPiecePositionFixture.SANG_POSITIONS_WITH_RIGHT_SANG),
+                Arguments.of(PieceAssignType.IN_SANG,
+                        HanPiecePositionFixture.SANG_POSITIONS_WITH_IN_SANG),
+                Arguments.of(PieceAssignType.OUT_SANG,
+                        HanPiecePositionFixture.SANG_POSITIONS_WITH_OUT_SANG)
+        );
+    }
 }
