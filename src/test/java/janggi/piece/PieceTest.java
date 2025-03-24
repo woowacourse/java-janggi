dd464958c1cacd9c44588cb9a75f037b65e3c232
changuii <rhljh7410@gmail.com>
2025-03-24 15:20:24 +0900

    test: isCannon에 대한 단위 테스트 추가
    - 커밋 누락

---
+++ src/test/java/janggi/piece/PieceTest.java
@@ -0,0 +1,50 @@
+package janggi.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import Janggi.piece.Cannon;
+import Janggi.piece.Chariot;
+import Janggi.piece.Country;
+import Janggi.piece.Elephant;
+import Janggi.piece.General;
+import Janggi.piece.Guard;
+import Janggi.piece.Horse;
+import Janggi.piece.Piece;
+import Janggi.piece.Soldier;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class PieceTest {
+
+    @Nested
+    @DisplayName("캐넌인지 검증하는 로직")
+    class IsCannon {
+
+        @DisplayName("Cannon이라면, true를 반환한다.")
+        @ParameterizedTest
+        @MethodSource
+        void isCannon(final Piece piece, final boolean expected) {
+            // given & when
+            final boolean actual = piece.isCannon();
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        static Stream<Arguments> isCannon(){
+            return Stream.of(
+                    Arguments.of(new Cannon(Country.CHO), true),
+                    Arguments.of(new Chariot(Country.CHO), false),
+                    Arguments.of(new Elephant(Country.CHO), false),
+                    Arguments.of(new General(Country.CHO), false),
+                    Arguments.of(new Guard(Country.CHO), false),
+                    Arguments.of(new Horse(Country.CHO), false),
+                    Arguments.of(new Soldier(Country.CHO), false)
+            );
+        }
+    }
+}
