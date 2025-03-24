6f10e5a5c425c0064d746efddcc4f7738913a075
eunseongu <rndmstjs75@gmail.com>
2025-03-24 15:42:21 +0900

    refactor: 좌표 입력 값 처리 방식 변경

--- src/main/java/janggi/Application.java
+++ src/main/java/janggi/Application.java
@@ -2,15 +2,20 @@ package janggi;
 
 import janggi.board.Board;
 import janggi.board.BoardGenerator;
+import janggi.board.Point;
+import janggi.camp.Camp;
 import janggi.piece.Piece;
 import janggi.view.View;
+import java.util.List;
 
 public class Application {
 
+    private static final String ERROR_MESSAGE_FORMAT = "%n[ERROR] %s";
     private static final Camp FIRST_TURN_CAMP = Camp.CHU;
     private static final int FROM_POINT_INDEX = 0;
     private static final int TO_POINT_INDEX = 1;
-    private static final String ERROR_MESSAGE_FORMAT = "%n[ERROR] %s";
+    public static final int X_COORDINATE = 0;
+    public static final int Y_COORDINATE = 1;
 
     public static void main(String[] args) {
         View view = new View();
@@ -40,11 +45,16 @@ public class Application {
         }
     }
 
-    private static void playTurn(String[] input, Camp baseCamp, Board board) {
-        Point from = new Point(input[FROM_POINT_INDEX]);
-        Point to = new Point(input[TO_POINT_INDEX]);
+    private static void playTurn(List<List<Integer>> moveInputs, Camp baseCamp, Board board) {
+        Point from = createPointFromInput(moveInputs, FROM_POINT_INDEX);
+        Point to = createPointFromInput(moveInputs, TO_POINT_INDEX);
         validateSelectedPiece(board, from, baseCamp);
         board.move(from, to);
+    }
+
+    private static Point createPointFromInput(List<List<Integer>> moveInputs, int index) {
+        List<Integer> coordinates = moveInputs.get(index);
+        return new Point(coordinates.get(X_COORDINATE), coordinates.get(Y_COORDINATE));
     }
 
     private static void validateSelectedPiece(Board board, Point from, Camp baseCamp) {
--- src/main/java/janggi/view/View.java
+++ src/main/java/janggi/view/View.java
@@ -1,9 +1,10 @@
 package janggi.view;
 
-import janggi.Camp;
-import janggi.PieceSymbol;
-import janggi.Point;
+import janggi.board.Point;
+import janggi.camp.Camp;
 import janggi.piece.Piece;
+import janggi.piece.PieceSymbol;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -11,8 +12,14 @@ public class View {
 
     private static final int ROW = 10;
     private static final int COLUMN = 9;
+    public static final int COORDINATES_COUNT = 2;
+    public static final int X_COORDINATE_INDEX = 0;
+    public static final int Y_COORDINATE_INDEX = 1;
+    public static final int FROM_POINT_INDEX = 0;
+    public static final int TO_POINT_INDEX = 1;
     private static final String EMPTY_SPACE = "ㅤ";
     private static final String BOARD_DELIMITER_LINE = " | ";
+    public static final String POINT_INPUT_DELIMITER = ",";
 
     private final Scanner scanner = new Scanner(System.in);
 
@@ -46,11 +53,28 @@ public class View {
         throw new IllegalArgumentException("y 또는 n을 입력해야 합니다.");
     }
 
-    public String[] readMove(Camp camp) {
+    public List<List<Integer>> readMove(Camp camp) {
         System.out.printf("%n%n[%s의 차례입니다.]%n", camp.getName());
         System.out.println("이동시킬 기물의 좌표와 도착 지점의 좌표를 입력해 주세요. 예) 03,13");
-        return scanner.nextLine()
-                .split(",", -1);
+        String[] pointInput = scanner.nextLine()
+                .split(POINT_INPUT_DELIMITER, -1);
+        List<Integer> fromPoint = parseCoordinates(pointInput[FROM_POINT_INDEX]);
+        List<Integer> toPoint = parseCoordinates(pointInput[TO_POINT_INDEX]);
+        return List.of(fromPoint, toPoint);
+    }
+
+    private List<Integer> parseCoordinates(String input) {
+        if (input.length() != COORDINATES_COUNT) {
+            throw new IllegalArgumentException("잘못된 좌표 입력입니다.");
+        }
+        try {
+            String[] split = input.split("", -1);
+            int x = Integer.parseInt(split[X_COORDINATE_INDEX]);
+            int y = Integer.parseInt(split[Y_COORDINATE_INDEX]);
+            return List.of(x, y);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
     }
 
     public void displayBoard(Map<Point, Piece> placedPieces) {
