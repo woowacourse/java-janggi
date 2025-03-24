ebadb104c5aa68348f44c013bacd7fc945b7e1c0
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 16:25:16 +0900

    test: 포가 움직일 수 있는 조건 테스트 추가

--- src/test/java/player/PlayersTest.java
+++ src/test/java/player/PlayersTest.java
@@ -14,6 +14,7 @@ import piece.Cha;
 import piece.Janggun;
 import piece.Jol;
 import piece.Ma;
+import piece.Po;
 import pieceProperty.Position;
 
 class PlayersTest {
@@ -224,5 +225,45 @@ class PlayersTest {
         assertThat(pieces1.getPieces().contains(janggun)).isFalse();
     }
 
+    @Test
+    @DisplayName("경로에 포 존재 확인 테스트")
+    void isPoExistInRoute() {
+        //given
+        Po po = new Po(new Position(5, 5));
+        Jol jol = new Jol(new Position(6, 5));
 
+        Pieces pieces = new Pieces(List.of(po, jol));
+
+        Player player = new Player(pieces, CHO);
+        Player player1 = new Player(new Pieces(List.of(new Po(new Position(8, 5)))), HAN);
+
+        Players players = new Players(List.of(player1, player));
+
+        //when - then
+        assertThatThrownBy(() ->
+                players.validateMovement(CHO, new Position(5, 5), new Position(9, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 포가 가는 경로에 장애물이 2개 이상 존재하거나 포가 존재하여 이동할 수 없습니다.");
+        }
+
+    @Test
+    @DisplayName("경로에 장애물 2개 이상 존재 확인 테스트")
+    void isExistObstacleOverTwoInRoute() {
+        //given
+        Po po = new Po(new Position(5, 5));
+        Jol jol = new Jol(new Position(6, 5));
+
+        Pieces pieces = new Pieces(List.of(po, jol));
+
+        Player player = new Player(pieces, CHO);
+        Player player1 = new Player(new Pieces(List.of(new Jol(new Position(8, 5)))), HAN);
+
+        Players players = new Players(List.of(player1, player));
+
+        //when - then
+        assertThatThrownBy(() ->
+                players.validateMovement(CHO, new Position(5, 5), new Position(9, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 포가 가는 경로에 장애물이 2개 이상 존재하거나 포가 존재하여 이동할 수 없습니다.");
+    }
 }
