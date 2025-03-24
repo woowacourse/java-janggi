68bc6d1b36ff828c72f30c0bfae64ff3ac11bbd6
(minjae8563) <(minjae8563@naver.com)>
2025-03-24 12:19:24 +0900

    test: 경로 상에 같은 위치 기물 테스트 추가

--- src/test/java/pieceProperty/PositionsTest.java
+++ src/test/java/pieceProperty/PositionsTest.java
@@ -3,9 +3,12 @@ package pieceProperty;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import piece.Byeong;
+import piece.Jol;
 
 class PositionsTest {
 
@@ -32,6 +35,20 @@ class PositionsTest {
 
         //then
         assertThat(positions.getPositions().contains(position)).isTrue();
+    }
+
+    @Test
+    @DisplayName("경로 상에 같은 위치 기물 확인 테스트")
+    void containsPositionTest() {
+        //given
+        List<Position> positionsList = new ArrayList<>(List.of(new Position(5, 5)));
+        Positions positions = new Positions(positionsList);
+        Jol jol = new Jol(new Position(5, 5));
+        Byeong byeong = new Byeong(new Position(5, 6));
+
+        //when-then
+        assertThat(positions.containsPosition(jol)).isTrue();
+        assertThat(positions.containsPosition(byeong)).isFalse();
     }
 
 
