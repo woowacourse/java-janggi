93d3393953eaad4feabbd524ca7afa19ac2a4148
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-24 22:05:31 +0900

    feat: 뷰 개선

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -1,13 +1,16 @@
 package janggi.view;
 
+import janggi.Player;
+import janggi.Team;
+
 import java.util.Scanner;
 
 public class InputView {
 
     private final Scanner scanner = new Scanner(System.in);
 
-    public MoveCommand inputMoveCommand() {
-        System.out.println("자신의 기물을 움직이세요. (예시) 1,1 2,1");
+    public MoveCommand inputMoveCommand(final Player player) {
+        System.out.println(processColor(player.getTeam(), player.getTeam().getDescription() + "나라") + ", " + "자신의 기물을 움직이세요. (예시) 1,1 2,1");
 
         String move = scanner.nextLine();
 
@@ -20,5 +23,20 @@ public class InputView {
                 departurePosition[1],
                 destinationPosition[0],
                 destinationPosition[1]);
+    }
+
+    private String processColor(final Team team, final String string) {
+        if (team.isCho()) {
+            return processGreenColorString(string);
+        }
+        return processRedColorString(string);
+    }
+
+    private String processRedColorString(final String string) {
+        return "\u001B[31m" + string + "\u001B[0m";
+    }
+
+    private String processGreenColorString(final String string) {
+        return "\u001B[32m" + string + "\u001B[0m";
     }
 }
