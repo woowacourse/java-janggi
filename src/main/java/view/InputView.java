a2bd3170408c05aecb8b04fc74cacc3789999385
ryan-dia <cjfdnjs158@naver.com>
2025-03-25 21:52:49 +0900

    feat: 입력값 검증 기능 추가
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,16 +12,26 @@ public final class InputView {
 
     public static List<String> readToPoint() {
         final String input = prompt("이동할 곳의 좌표를 입력해주세요.(예: 1,1)");
-        return Arrays.asList(input.split(","));
+        final List<String> numbers = Arrays.asList(input.strip().split(","));
+        validateSize(numbers);
+        return numbers;
     }
 
     public static List<String> readFromPoint() {
         final String input = prompt("움직일 말의 좌표를 입력해주세요.(예: 1,1)");
-        return Arrays.asList(input.split(","));
+        final List<String> numbers = Arrays.asList(input.strip().split(","));
+        validateSize(numbers);
+        return numbers;
     }
 
     private static String prompt(final String message) {
         System.out.println(message);
         return scanner.nextLine();
+    }
+
+    private static void validateSize(final List<String> numbers) {
+        if (numbers.size() != 2) {
+            throw new IllegalArgumentException("좌표는 (숫자,숫자) 형식으로 입력해주세요.");
+        }
     }
 }
