beb7152df090d1742d1e36e9dce76fe10f77cd93
mimi <110226866+hongkikii@users.noreply.github.com>
2025-03-25 16:56:57 +0900

    refactor: Direction 필드 변경

--- src/main/java/direction/Direction.java
+++ src/main/java/direction/Direction.java
@@ -3,35 +3,40 @@ package direction;
 import java.util.Arrays;
 
 public enum Direction {
-    LEFT(new Point(-1, 0)),
-    RIGHT(new Point(1, 0)),
-    UP(new Point(0, -1)),
-    DOWN(new Point(0, 1)),
-    UP_LEFT_DIAGONAL(new Point(-1, -1)),
-    DOWN_LEFT_DIAGONAL(new Point(-1, 1)),
-    UP_RIGHT_DIAGONAL(new Point(1, -1)),
-    DOWN_RIGHT_DIAGONAL(new Point(1, 1));
+    LEFT(-1, 0),
+    RIGHT(1, 0),
+    UP(0, -1),
+    DOWN(0, 1),
+    UP_LEFT_DIAGONAL(-1, -1),
+    DOWN_LEFT_DIAGONAL(-1, 1),
+    UP_RIGHT_DIAGONAL(1, -1),
+    DOWN_RIGHT_DIAGONAL(1, 1);
 
-    private final Point direction;
+    private final int row;
+    private final int column;
 
-    Direction(Point direction) {
-        this.direction = direction;
+    Direction(int row, int column) {
+        this.row = row;
+        this.column = column;
     }
 
     public static Direction find(Point from, Point to) {
         Point distance = to.minus(from);
-        Point compareDirection = new Point(Integer.signum(distance.x()), Integer.signum(distance.y()));
-        return Arrays.stream(Direction.values())
-                .filter(value -> value.direction.equals(compareDirection))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 방향이 존재하지 않습니다."));
+        return Direction.findBy(Integer.signum(distance.x()), Integer.signum(distance.y()));
     }
 
     public int getX() {
-        return direction.x();
+        return row;
     }
 
     public int getY() {
-        return direction.y();
+        return column;
+    }
+    
+    private static Direction findBy(int compareRow, int compareColumn) {
+        return Arrays.stream(Direction.values())
+                .filter(direction -> direction.row == compareRow && direction.column == compareColumn)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 방향이 존재하지 않습니다."));
     }
 }
