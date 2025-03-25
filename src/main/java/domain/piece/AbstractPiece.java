829fd1650f3f01261c5c66475582f0c6347b8758
ryan-dia <cjfdnjs158@naver.com>
2025-03-25 21:55:14 +0900

    refactor: 메시지를 던지는 방식으로 변경
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/main/java/domain/piece/AbstractPiece.java
+++ src/main/java/domain/piece/AbstractPiece.java
@@ -14,6 +14,6 @@ public abstract class AbstractPiece implements Piece {
     }
 
     public final boolean isGreenTeam() {
-        return Team.GREEN == team;
+        return Team.isGreenTeam(team);
     }
 }
