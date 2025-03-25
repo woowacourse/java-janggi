3f370792dfd51191d2360c0d3d50cac620bfa6ec
RaZel713 <razel7130@gmail.com>
2025-03-25 15:50:42 +0900

    test: 차의 테스트 추가

--- src/test/java/janggiGame/piece/ChariotTest.java
+++ src/test/java/janggiGame/piece/ChariotTest.java
@@ -7,10 +7,38 @@ import janggiGame.Position;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ChariotTest {
+    private static Stream<Arguments> provideRowAndColumn() {
+        return Stream.of(
+                Arguments.of(3, 3),
+                Arguments.of(1, 1),
+                Arguments.of(1, 3),
+                Arguments.of(3, 1)
+        );
+    }
+
+    @DisplayName("차가 목적지로 갈 수 없다면 예외를 발생 시킨다")
+    @ParameterizedTest
+    @MethodSource("provideRowAndColumn")
+    void chariotCanValidateDestination(int row, int column) {
+        // given
+        Position origin = Position.of(2, 2);
+        Position destination = Position.of(row, column);
+        Chariot chariot = new Chariot(Dynasty.HAN);
+
+        // when // then
+        assertThatCode(() -> chariot.getIntermediatePoints(origin, destination))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessageStartingWith("[ERROR] ");
+    }
+
     @DisplayName("차는 목적지로 가는 경로를 구할 수 있다.")
     @Test
     void chariotCanGetIntermediatePoints() {
@@ -28,65 +56,50 @@ class ChariotTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("차가 목적지로 갈 수 없다면 예외를 발생시킨다")
+    @DisplayName("차는 이동 경로에 기물이 존재 한다면 이동할 수 없다")
     @Test
-    void chariotCannotGetIntermediatePoints() {
+    void chariotCanNotMoveIfIntermediatePointsHasPiece() {
         // given
-        Position origin = Position.of(1, 1);
-        Position destination = Position.of(2, 3);
+        Map<Position, Piece> intermediatePointsWithPiece = new LinkedHashMap<>();
         Chariot chariot = new Chariot(Dynasty.HAN);
 
-        // when // then
-        assertThatCode(() -> chariot.getIntermediatePoints(origin, destination))
-                .isInstanceOf(UnsupportedOperationException.class)
-                .hasMessageStartingWith("[ERROR]");
-    }
-
-
-    @DisplayName("차는 이동 경로에 어떤 말도 없다면 이동 가능하다")
-    @Test
-    void chariotJudgeMovable() {
-        // given
-        Map<Position, Piece> routesWithPiece = new LinkedHashMap<>();
-        Chariot chariot = new Chariot(Dynasty.HAN);
-
-        routesWithPiece.put(Position.of(1, 2), null);
-        routesWithPiece.put(Position.of(1, 3), null);
+        intermediatePointsWithPiece.put(Position.of(1, 2), null);
+        intermediatePointsWithPiece.put(Position.of(1, 3), new Chariot(Dynasty.HAN));
 
         // when // then
-        assertThatCode(() -> chariot.validateMove(routesWithPiece, null))
-                .doesNotThrowAnyException();
-    }
-
-    @DisplayName("차는 이동 경로에 기물이 존재한다면 이동할 수 없다")
-    @Test
-    void chariotJudgeMovable2() {
-        // given
-        Map<Position, Piece> routesWithPiece = new LinkedHashMap<>();
-        Chariot chariot = new Chariot(Dynasty.HAN);
-
-        routesWithPiece.put(Position.of(1, 2), null);
-        routesWithPiece.put(Position.of(1, 3), new Chariot(Dynasty.HAN));
-
-        // when // then
-        assertThatCode(() -> chariot.validateMove(routesWithPiece, null))
-                .isInstanceOf(UnsupportedOperationException.class)
-                .hasMessageStartingWith("[ERROR]");
-    }
-
-    @DisplayName("차는 목적지에 같은 나라의 기물이 존재한다면 이동할 수 없다")
-    @Test
-    void chariotJudgeMovable3() {
-        // given
-        Map<Position, Piece> routesWithPiece = new LinkedHashMap<>();
-        Chariot chariot = new Chariot(Dynasty.HAN);
-
-        routesWithPiece.put(Position.of(1, 2), null);
-        routesWithPiece.put(Position.of(1, 3), null);
-
-        // when // then
-        assertThatCode(() -> chariot.validateMove(routesWithPiece, new Chariot(Dynasty.HAN)))
+        assertThatCode(() -> chariot.validateMove(intermediatePointsWithPiece, null))
                 .isInstanceOf(UnsupportedOperationException.class)
                 .hasMessageStartingWith("[ERROR] ");
+    }
+
+    @DisplayName("차는 목적지에 같은 나라의 기물이 존재 한다면 이동할 수 없다")
+    @Test
+    void chariotCanNotMoveToSameDynastyPiece() {
+        // given
+        Map<Position, Piece> intermediatePointsWithPiece = new LinkedHashMap<>();
+        Chariot chariot = new Chariot(Dynasty.HAN);
+
+        intermediatePointsWithPiece.put(Position.of(1, 2), null);
+        intermediatePointsWithPiece.put(Position.of(1, 3), null);
+
+        // when // then
+        assertThatCode(() -> chariot.validateMove(intermediatePointsWithPiece, new Chariot(Dynasty.HAN)))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessageStartingWith("[ERROR] ");
+    }
+
+    @DisplayName("차는 이동 경로에 어떤 말도 없다면 이동 가능 하다")
+    @Test
+    void chariotCanMove() {
+        // given
+        Map<Position, Piece> intermediatePointsWithPiece = new LinkedHashMap<>();
+        Chariot chariot = new Chariot(Dynasty.HAN);
+
+        intermediatePointsWithPiece.put(Position.of(1, 2), null);
+        intermediatePointsWithPiece.put(Position.of(1, 3), null);
+
+        // when // then
+        assertThatCode(() -> chariot.validateMove(intermediatePointsWithPiece, null))
+                .doesNotThrowAnyException();
     }
 }
