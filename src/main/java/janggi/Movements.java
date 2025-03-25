b310795ff824aef5e570c08300bb15a54e9b444c
kjyyjk <rlawnsdud920@gmail.com>
2025-03-25 14:26:02 +0900

    feat: Movements가 향하는 방향을 판단하는 기능 구현
    - 장기는 하나의 움직임 내에서 특정 방향의 반대로 향하지 않는다.
    예를 들어 위로 향하던 중에 다시 아래로 향하지를 않는다.
    따라서 Movements는 상하좌우 어떤 방향을 향해 움직이는지 계산하고 반환한다.

--- src/main/java/janggi/Movements.java
+++ src/main/java/janggi/Movements.java
@@ -1,5 +1,14 @@
 package janggi;
 
+import static janggi.Movement.DOWN;
+import static janggi.Movement.LEFT;
+import static janggi.Movement.LEFT_DOWN;
+import static janggi.Movement.LEFT_UP;
+import static janggi.Movement.RIGHT;
+import static janggi.Movement.RIGHT_DOWN;
+import static janggi.Movement.RIGHT_UP;
+import static janggi.Movement.UP;
+
 import janggi.board.position.Position;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +33,29 @@ public class Movements {
             path.add(position);
         }
         return new Path(path);
+    }
+
+    public boolean isUpward() {
+        return movements.contains(UP) ||
+                movements.contains(LEFT_UP) ||
+                movements.contains(RIGHT_UP);
+    }
+
+    public boolean isDownward() {
+        return movements.contains(DOWN) ||
+                movements.contains(LEFT_DOWN) ||
+                movements.contains(RIGHT_DOWN);
+    }
+
+    public boolean isLeftward() {
+        return movements.contains(LEFT) ||
+                movements.contains(LEFT_UP) ||
+                movements.contains(LEFT_DOWN);
+    }
+
+    public boolean isRightward() {
+        return movements.contains(RIGHT) ||
+                movements.contains(RIGHT_UP) ||
+                movements.contains(RIGHT_DOWN);
     }
 }
--- src/test/java/janggi/MovementsTest.java
+++ src/test/java/janggi/MovementsTest.java
@@ -10,6 +10,8 @@ import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 import janggi.board.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class MovementsTest {
     @DisplayName("주어진_시작_지점부터_내부_움직임에_따라_경로를_만들고_반환한다")
@@ -25,5 +27,61 @@ class MovementsTest {
         // then
         assertThat(path).extracting("path", as(LIST))
                 .containsExactly(start, start.up(), start.up().up(), start.up().up().right());
+    }
+
+    @DisplayName("내부_움직임이_위를_향하는지_여부를_반환한다")
+    @CsvSource(value = {"UP:true", "LEFT_UP:true", "RIGHT_UP:true", "DOWN:false"}, delimiterString = ":")
+    @ParameterizedTest
+    void isUpward(Movement movement, boolean expected) {
+        // given
+        Movements movements = new Movements(movement);
+
+        // when
+        boolean result = movements.isUpward();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("내부_움직임이_아래를_향하는지_여부를_반환한다")
+    @CsvSource(value = {"DOWN:true", "LEFT_DOWN:true", "RIGHT_DOWN:true", "UP:false"}, delimiterString = ":")
+    @ParameterizedTest
+    void isDownward(Movement movement, boolean expected) {
+        // given
+        Movements movements = new Movements(movement);
+
+        // when
+        boolean result = movements.isDownward();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("내부_움직임이_왼쪽을_향하는지_여부를_반환한다")
+    @CsvSource(value = {"LEFT:true", "LEFT_UP:true", "LEFT_DOWN:true", "UP:false"}, delimiterString = ":")
+    @ParameterizedTest
+    void isLeftward(Movement movement, boolean expected) {
+        // given
+        Movements movements = new Movements(movement);
+
+        // when
+        boolean result = movements.isLeftward();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("내부_움직임이_오른쪽을_향하는지_여부를_반환한다")
+    @CsvSource(value = {"RIGHT:true", "RIGHT_UP:true", "RIGHT_DOWN:true", "UP:false"}, delimiterString = ":")
+    @ParameterizedTest
+    void isRightward(Movement movement, boolean expected) {
+        // given
+        Movements movements = new Movements(movement);
+
+        // when
+        boolean result = movements.isRightward();
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
