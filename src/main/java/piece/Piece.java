f71b43bee5ec5bc6a15989a65a8b7e8fdee85f1d
GiSeok Kang <ime.rokz@gmail.com>
2025-03-25 22:52:03 +0900

    refactor: Piece 인터페이스로 변경

--- src/main/java/piece/Piece.java
+++ src/main/java/piece/Piece.java
@@ -1,37 +1,8 @@
 package piece;
 
 import direction.Point;
-import move.MovementRule;
 
-public class Piece {
+public interface Piece {
 
-    private final String nickname;
-    private final MovementRule movementRule;
-    private Point point;
-
-    public Piece(String nickname, Point point, MovementRule movementRule) {
-        this.nickname = nickname;
-        this.movementRule = movementRule;
-        this.point = point;
-    }
-
-    public void move(Pieces allPieces, Point to) {
-        point = movementRule.move(allPieces, point, to);
-    }
-
-    public boolean isSameType(String nickname) {
-        return this.nickname.equalsIgnoreCase(nickname);
-    }
-
-    public boolean isSamePoint(Point point) {
-        return this.point.equals(point);
-    }
-
-    public String getName() {
-        return nickname;
-    }
-
-    public Point getPosition() {
-        return point;
-    }
+    void move(Pieces pieces, Point destination);
 }
