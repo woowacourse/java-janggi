43fe253aeb397994dbef59170fd86e99be669d81
YuyoungRhee <bbclara@naver.com>
2025-03-25 13:50:50 +0900

    refactor: 보드 생성 책임을 Application으로 이동

--- src/main/java/janggi/Application.java
+++ src/main/java/janggi/Application.java
@@ -1,15 +1,45 @@
 package janggi;
 
 import janggi.controller.JanggiController;
+import janggi.domain.board.BoardSetup;
+import janggi.domain.board.InitialBoard;
+import janggi.domain.board.PlayingBoard;
+import janggi.domain.piece.PieceColor;
 import janggi.view.InputView;
 import janggi.view.OutputView;
+import java.util.function.Supplier;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
-        JanggiController controller = new JanggiController(inputView, outputView);
+        InitialBoard initialBoard = getWithRetry(() -> setupBoard(inputView));
+        PlayingBoard playingBoard = new PlayingBoard(initialBoard.getInitialBoard());
+        outputView.printBoard(playingBoard);
+
+        JanggiController controller = new JanggiController(inputView, outputView, playingBoard);
         controller.run();
+    }
+
+    private static InitialBoard setupBoard(InputView inputView) {
+        BoardSetup redSetup = getBoardSetup(inputView, PieceColor.RED);
+        BoardSetup blueSetup = getBoardSetup(inputView, PieceColor.BLUE);
+        return InitialBoard.createBoard(redSetup, blueSetup);
+    }
+
+    private static BoardSetup getBoardSetup(InputView inputView, PieceColor teamColor) {
+        int setNumber = inputView.readBoardSetup(teamColor);
+        return BoardSetup.from(setNumber);
+    }
+
+    public static <T> T getWithRetry(Supplier<T> task) {
+        while (true) {
+            try {
+                return task.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/janggi/controller/JanggiController.java
+++ src/main/java/janggi/controller/JanggiController.java
@@ -1,91 +1,62 @@
 package janggi.controller;
 
 import janggi.domain.JanggiGame;
-import janggi.domain.board.BoardSetup;
-import janggi.domain.board.PlayingBoard;
-import janggi.domain.board.InitialBoard;
 import janggi.domain.board.Column;
+import janggi.domain.board.PlayingBoard;
 import janggi.domain.board.Position;
 import janggi.domain.board.Row;
 import janggi.domain.gameState.BlueTurn;
-import janggi.domain.piece.PieceColor;
 import janggi.domain.piece.PieceType;
 import janggi.dto.MoveCommandDto;
 import janggi.view.InputView;
 import janggi.view.OutputView;
-
 import janggi.view.PieceTypeName;
 import java.util.Map;
 
 public class JanggiController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final PlayingBoard playingBoard;
 
-    public JanggiController(InputView inputView, OutputView outputView) {
+    public JanggiController(InputView inputView, OutputView outputView, PlayingBoard playingBoard) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.playingBoard = playingBoard;
     }
 
     public void run() {
-        InitialBoard initialBoard = setupBoard();
-        PlayingBoard playingBoard = new PlayingBoard(initialBoard.getInitialBoard());
-        outputView.printBoard(playingBoard);
-
         JanggiGame janggiGame = new JanggiGame(new BlueTurn(playingBoard));
 
         while (!janggiGame.isFinished()) {
-            String input = inputView.readCommand();
-            GameCommand command = GameCommand.from(input);
-
-            Map<GameCommand, Runnable> commands = Map.of(
-                    GameCommand.MOVE, () -> playMove(MoveCommandDto.from(input), janggiGame, playingBoard),
-                    GameCommand.QUIT, this::gameQuit
-            );
-
-            Runnable action = commands.get(command);
-            action.run();
+            processWithRetry(() -> playSingleCommand(janggiGame));
         }
 
         outputView.printWinner(janggiGame.getTurnColor());
     }
 
-    private void playMove(MoveCommandDto commandDto, JanggiGame janggiGame, PlayingBoard playingBoard) {
-        processWithRetry(() -> playTurn(janggiGame, commandDto));
-        outputView.printBoard(playingBoard);
+    private void playSingleCommand(JanggiGame janggiGame) {
+        String input = inputView.readCommand();
+        GameCommand command = GameCommand.from(input);
+
+        Map<GameCommand, Runnable> commands = Map.of(
+                GameCommand.MOVE, () -> playTurn(MoveCommandDto.from(input), janggiGame),
+                GameCommand.QUIT, this::gameQuit
+        );
+        Runnable action = commands.get(command);
+        action.run();
     }
 
-    private void playTurn(JanggiGame janggiGame, MoveCommandDto commandDto) {
-        //MoveCommandDto commandDto = readMoveCommand(janggiGame.getTurnColor());
-
+    private void playTurn(MoveCommandDto commandDto, JanggiGame janggiGame) {
         Position source = createPosition(commandDto.sourceRow(), commandDto.sourceCol());
         Position destination = createPosition(commandDto.destinationRow(), commandDto.destinationCol());
         PieceType pieceType = PieceTypeName.getTypeFrom(commandDto.pieceName());
 
         janggiGame.move(pieceType, source, destination);
+
+        outputView.printBoard(playingBoard);
     }
 
-    private void gameQuit() {
-        System.out.println("게임 종료");
-        System.exit(0);
-    }
-
-    private InitialBoard setupBoard() {
-        BoardSetup redSetup = getBoardSetup(PieceColor.RED);
-        BoardSetup blueSetup = getBoardSetup(PieceColor.BLUE);
-        return InitialBoard.createBoard(redSetup, blueSetup);
-    }
-
-    private BoardSetup getBoardSetup(PieceColor teamColor) {
-        int setNumber = inputView.readBoardSetup(teamColor);
-        return BoardSetup.from(setNumber);
-    }
-
-    private MoveCommandDto readMoveCommand(PieceColor turnColor) {
-        outputView.printTurnNotice(turnColor);
-        return inputView.readMoveCommand();
-    }
-
-    private static Position createPosition(char rowInput, char colInput) {
+    private Position createPosition(char rowInput, char colInput) {
         int rowInt = Character.getNumericValue(rowInput);
         Row row = Row.from(rowInt);
 
@@ -93,6 +64,11 @@ public class JanggiController {
         Column column = Column.from(colInt);
 
         return new Position(row, column);
+    }
+
+    private void gameQuit() {
+        System.out.println("게임 종료");
+        System.exit(0);
     }
 
     private void processWithRetry(Runnable runnable) {
