e4cbd98d046074b8518f319727fac3d8f3ff1f47
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-25 17:39:51 +0900

    feat: 사 궁성 이동 구현

--- src/main/java/move/SaMoveBehavior.java
+++ src/main/java/move/SaMoveBehavior.java
@@ -1,24 +1,38 @@
 package move;
 
 import java.util.List;
+import move.direction.Direction;
+import move.direction.Directions;
 import piece.PieceType;
-import piece.Pieces;
 import piece.Team;
 import piece.position.JanggiPosition;
 
 public class SaMoveBehavior extends JanggiMoveBehavior {
 
-    private static final String NOT_IMPLEMENTED_FEATURE = "아직 구현되지 않은 기능입니다.";
+    private final List<Directions> canMoveDirections = List.of(
+            new Directions(List.of(Direction.RIGHT)),
+            new Directions(List.of(Direction.LEFT)),
+            new Directions(List.of(Direction.DOWN)),
+            new Directions(List.of(Direction.UP))
+    );
 
-    @Override
-    public JanggiPosition moveOnRoute(JanggiPosition destination, Pieces onRoutePieces, Team moveTeam) {
-        throw new IllegalStateException(NOT_IMPLEMENTED_FEATURE);
-    }
+    private final List<Directions> diagonalCanMoveDirections = List.of(
+            new Directions(List.of(Direction.UP_RIGHT)),
+            new Directions(List.of(Direction.UP_LEFT)),
+            new Directions(List.of(Direction.DOWN_LEFT)),
+            new Directions(List.of(Direction.DOWN_RIGHT))
+    );
 
     @Override
     public List<JanggiPosition> calculateLegalRoute(JanggiPosition startPosition, JanggiPosition endPosition,
                                                     Team team) {
-        throw new IllegalStateException(NOT_IMPLEMENTED_FEATURE);
+        if (!isInsideGungsungCase(startPosition, endPosition)) {
+            throw new InvalidMovePosition();
+        }
+        if (isDiagonalGungsungCase(startPosition, endPosition)) {
+            return calculateLegalRoute(startPosition, endPosition, diagonalCanMoveDirections);
+        }
+        return calculateLegalRoute(startPosition, endPosition, canMoveDirections);
     }
 
     @Override
--- src/test/java/move/SaMoveBehaviorTest.java
+++ src/test/java/move/SaMoveBehaviorTest.java
@@ -1,18 +1,69 @@
 package move;
 
-import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import piece.Piece;
 import piece.Pieces;
 import piece.Team;
 import piece.position.JanggiPosition;
 
 class SaMoveBehaviorTest {
+
     @Test
-    void 아직_구현되지않음() {
+    void 사는_가능한_경로를_반환한다() {
+        JanggiPosition startPosition = new JanggiPosition(1, 4);
+        JanggiPosition endPosition = new JanggiPosition(1, 5);
         JanggiMoveBehavior moveBehavior = new SaMoveBehavior();
+
+        List<JanggiPosition> route = moveBehavior.calculateLegalRoute(startPosition, endPosition, Team.BLUE);
+
+        Assertions.assertThat(route).containsExactlyElementsOf(List.of(endPosition));
+    }
+
+    @Test
+    void 사는_궁성_밖으로_갈_수_없다() {
+        JanggiPosition startPosition = new JanggiPosition(0, 0);
+        JanggiPosition endPosition = new JanggiPosition(0, 1);
+        JanggiMoveBehavior moveBehavior = new SaMoveBehavior();
+
+        Assertions.assertThatThrownBy(() -> moveBehavior.calculateLegalRoute(startPosition, endPosition, Team.BLUE))
+                .isInstanceOf(InvalidMovePosition.class);
+    }
+
+    @Test
+    void 사는_대각선_궁성에서_대각선으로_이동할_수_있다() {
+        JanggiPosition startPosition = new JanggiPosition(1, 4);
+        JanggiPosition endPosition = new JanggiPosition(2, 5);
+        Pieces pieces = new Pieces(List.of(new Piece(startPosition, new SaMoveBehavior(), Team.BLUE)));
+        Piece placePiece = pieces.move(startPosition, endPosition, pieces);
+        Piece expectedPiece = new Piece(endPosition, new SaMoveBehavior(), Team.BLUE);
+        Assertions.assertThat(placePiece).isEqualTo(expectedPiece);
+    }
+
+    @Test
+    void 사는_같은_팀을_먹을수_없다() {
+        JanggiMoveBehavior moveBehavior = new SaMoveBehavior();
+        JanggiPosition otherPiecePosition = new JanggiPosition(1, 4);
+        JanggiPosition destinationPiecePosition = new JanggiPosition(2, 4);
+        Pieces onRoutePieces = new Pieces(
+                List.of(new Piece(otherPiecePosition, new JolMoveBehavior(), Team.BLUE),
+                        new Piece(destinationPiecePosition, new JolMoveBehavior(), Team.BLUE))
+        );
         Assertions.assertThatThrownBy(
-                        () -> moveBehavior.moveOnRoute(new JanggiPosition(0, 5), new Pieces(new ArrayList<>()), Team.BLUE))
-                .isInstanceOf(IllegalStateException.class);
+                        () -> moveBehavior.moveOnRoute(new JanggiPosition(0, 5), onRoutePieces, Team.BLUE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 사는_적팀이면_먹을수_있다() {
+        JanggiMoveBehavior moveBehavior = new SaMoveBehavior();
+        JanggiPosition destinationPiecePosition = new JanggiPosition(2, 4);
+        Pieces onRoutePieces = new Pieces(
+                List.of(new Piece(destinationPiecePosition, new JolMoveBehavior(), Team.RED))
+        );
+
+        JanggiPosition move = moveBehavior.moveOnRoute(new JanggiPosition(2, 4), onRoutePieces, Team.BLUE);
+        Assertions.assertThat(move).isEqualTo(destinationPiecePosition);
     }
 }
