b8a2477b2c198f6043dbdf7ca61c12eaa12040ce
RaZel713 <razel7130@gmail.com>
2025-03-25 16:01:51 +0900

    test: 장의 테스트 추가

--- src/test/java/janggiGame/piece/KingTest.java
+++ src/test/java/janggiGame/piece/KingTest.java
@@ -7,17 +7,47 @@ import janggiGame.Position;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class KingTest {
-    @DisplayName("장의 목적지로 가는 경로는 항상 비어있다.")
+    private static Stream<Arguments> provideRowAndColumn() {
+        return Stream.of(
+                Arguments.of(1, 1),
+                Arguments.of(5, 5),
+                Arguments.of(1, 3),
+                Arguments.of(3, 1),
+                Arguments.of(2, 5),
+                Arguments.of(5, 2)
+        );
+    }
+
+    @DisplayName("장이 목적지로 갈 수 없다면 예외를 발생 시킨다")
+    @ParameterizedTest
+    @MethodSource("provideRowAndColumn")
+    void kingCanValidateDestination(int row, int column) {
+        // given
+        Position origin = Position.of(3, 3);
+        Position destination = Position.of(row, column);
+        King king = new King(Dynasty.HAN);
+
+        // when // then
+        assertThatCode(() -> king.getIntermediatePoints(origin, destination))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessageStartingWith("[ERROR] ");
+    }
+
+    @DisplayName("장의 목적지로 가는 경로는 항상 비어 있다.")
     @Test
     void kingCanGetIntermediatePoints() {
         // given
         Position origin = Position.of(1, 1);
         Position destination = Position.of(1, 0);
-        Advisor king = new Advisor(Dynasty.HAN);
+        King king = new King(Dynasty.HAN);
 
         // when
         List<Position> actual = king.getIntermediatePoints(origin, destination);
@@ -26,16 +56,28 @@ class KingTest {
         assertThat(actual).isEmpty();
     }
 
-    @DisplayName("장은 목적지에 같은 나라의 기물이 존재한다면 이동할 수 없다")
+    @DisplayName("장은 목적지에 같은 나라의 기물이 존재 한다면 이동할 수 없다.")
     @Test
-    void kingJudgeMovable3() {
+    void kingCanNotMoveToSameDynastyPiece() {
         // given
         Map<Position, Piece> routesWithPiece = new LinkedHashMap<>();
-        Advisor king = new Advisor(Dynasty.HAN);
+        King king = new King(Dynasty.HAN);
 
         // when // then
         assertThatCode(() -> king.validateMove(routesWithPiece, new Chariot(Dynasty.HAN)))
                 .isInstanceOf(UnsupportedOperationException.class)
                 .hasMessageStartingWith("[ERROR] ");
+    }
+
+    @DisplayName("장은 목적지에 같은 나라의 기물이 존재하지 않는다면 이동할 수 있다.")
+    @Test
+    void kingCanMove() {
+        // given
+        Map<Position, Piece> intermediatePointsWithPiece = new LinkedHashMap<>();
+        King king = new King(Dynasty.HAN);
+
+        // when // then
+        assertThatCode(() -> king.validateMove(intermediatePointsWithPiece, new Advisor(Dynasty.CHO)))
+                .doesNotThrowAnyException();
     }
 }
