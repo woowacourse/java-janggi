e74032bd9adad5d501d9596754f4fbbcbd5efe58
jhpark1227 <junhyeok0246@naver.com>
2025-03-25 16:43:32 +0900

    test: Gung 단위테스트 변경

--- src/test/java/domain/piece/GungTest.java
+++ src/test/java/domain/piece/GungTest.java
@@ -1,48 +1,46 @@
 package domain.piece;
 
-import domain.Move;
 import domain.Position;
 import domain.Team;
 import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class GungTest {
 
-    @DisplayName("사는 상하좌우로 이동할 수 있다.")
     @ParameterizedTest
     @CsvSource({
-            "5,4,BACK",
-            "3,4,FRONT",
-            "4,3,LEFT",
-            "4,5,RIGHT"
+            "1, 0",
+            "0, 1",
+            "-1, 0",
+            "0, -1"
     })
-    void test(int row, int column, Move move) {
-
+    void 궁은_상하좌우로_이동할_수_있다(int movedRow, int movedColumn) {
         //given
         Gung gung = new Gung(Team.CHO);
-        Position startPosition = new Position(4, 4);
-        Position targetPosition = new Position(row, column);
+        int row = 4;
+        int column = 4;
+        Position startPosition = new Position(row, column);
+        Position targetPosition = new Position(row + movedRow, column + movedColumn);
 
         //when
-        List<Position> resultMove = gung.calculatePath(startPosition, targetPosition);
+        List<Position> actual = gung.calculatePath(startPosition, targetPosition);
 
         // then
-        Assertions.assertThat(resultMove).isEqualTo(List.of());
+        Assertions.assertThat(actual).isEqualTo(List.of());
     }
 
-    @DisplayName("킹이 이동할 수 없는 위치라면 예외가 발생한다.")
     @Test
-    void test1() {
-
-        //given
+    void 궁은_두_칸_이동할_수_없다() {
+        // given
         Gung gung = new Gung(Team.HAN);
+        Position src = new Position(4, 1);
+        Position dest = new Position(4, 3);
 
         // when & then
-        Assertions.assertThatThrownBy(() -> gung.calculatePath(new Position(4, 1), new Position(4, 3)))
+        Assertions.assertThatThrownBy(() -> gung.calculatePath(src, dest))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이 위치로 이동할 수 없습니다.");
     }
