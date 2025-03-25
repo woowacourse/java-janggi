9352aaae8d9995d88060f9d0fa5ed055e6d83258
DongYeop <s01054830455@gmail.com>
2025-03-25 13:42:26 +0900

    test: 다른 팀의 기물이라면 예외발생 테스트 추가

--- src/test/java/janggi/piece/PieceTest.java
+++ src/test/java/janggi/piece/PieceTest.java
@@ -1,5 +1,6 @@
 package janggi.piece;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import janggi.position.Position;
@@ -14,13 +15,41 @@ class PieceTest {
     @Test
     void sameNation() {
         //given
-        final Piece piece = new StubPiece(new PieceProfile("테스트 말", Team.HAN), new Position(0, 0));
-        final Piece other = new StubPiece(new PieceProfile("테스트 말", Team.HAN), new Position(1, 0));
+        final Piece piece = new StubPiece(new PieceProfile(" testPiece", Team.HAN), new Position(0, 0));
+        final Piece other = new StubPiece(new PieceProfile(" testPiece", Team.HAN), new Position(1, 0));
 
         //when //then
         assertThatThrownBy(() -> piece.validateTeam(other))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("다른 팀의 기물을 선택하면 예외가 발생한다.")
+    @Test
+    void nonSameTeam() {
+        //given
+        final Piece piece = new StubPiece(new PieceProfile(" testPiece", Team.HAN), new Position(0, 0));
+        final Piece other = new StubPiece(new PieceProfile(" testPiece", Team.CHO), new Position(1, 0));
+
+        //when //then
+        assertThatThrownBy(() -> piece.validateTeam(other.getPieceProfile().getNation()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    @DisplayName("위치를 변경할 수 있다.")
+    @Test
+    void updatePosition() {
+        //given
+        final Piece piece = new StubPiece(new PieceProfile(" testPiece", Team.HAN), new Position(0, 0));
+
+        final Position position = new Position(1, 1);
+
+        //when
+        piece.updatePiecePositionBy(position);
+
+        //then
+        assertThat(piece).isEqualTo(new StubPiece(new PieceProfile(" testPiece", Team.HAN), new Position(1, 1)));
     }
 
     static class StubPiece extends Piece {
