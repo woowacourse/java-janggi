438cbea1d9913a7fa714a8ff33f8615140b03405
ryan-dia <cjfdnjs158@naver.com>
2025-03-25 15:03:53 +0900

    refactor: 말의 초기 좌표 enum으로 분리
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/main/java/domain/BoardFactory.java
+++ src/main/java/domain/BoardFactory.java
@@ -25,55 +25,43 @@ public final class BoardFactory {
         positions.addAll(generatePositions(
                 General::new,
                 Score.GENERAL,
-                createGreenPoints(Point.of(4, 1)),
-                createRedPoints(Point.of(4, 8))
+                InitialPoint.GENERAL.greenPoints,
+                InitialPoint.GENERAL.redPoints
         ));
         positions.addAll(generatePositions(
                 Guard::new,
                 Score.GUARD,
-                createGreenPoints(Point.of(3, 0), Point.of(5, 0)),
-                createRedPoints(Point.of(3, 9), Point.of(5, 9))
+                InitialPoint.GUARD.greenPoints,
+                InitialPoint.GUARD.redPoints
         ));
         positions.addAll(generatePositions(
                 Horse::new, Score.HORSE,
-                createGreenPoints(Point.of(2, 0), Point.of(7, 0)),
-                createRedPoints(Point.of(2, 9), Point.of(7, 9))
+                InitialPoint.HORSE.greenPoints,
+                InitialPoint.HORSE.redPoints
         ));
         positions.addAll(generatePositions(
                 Elephant::new,
                 Score.ELEPHANT,
-                createGreenPoints(Point.of(1, 0), Point.of(6, 0)),
-                createRedPoints(Point.of(1, 9), Point.of(6, 9))
+                InitialPoint.ELEPHANT.greenPoints,
+                InitialPoint.ELEPHANT.redPoints
         ));
         positions.addAll(generatePositions(
                 Chariot::new,
                 Score.CHARIOT,
-                createGreenPoints(Point.of(0, 0), Point.of(8, 0)),
-                createRedPoints(Point.of(0, 9), Point.of(8, 9))
+                InitialPoint.CHARIOT.greenPoints,
+                InitialPoint.CHARIOT.redPoints
         ));
         positions.addAll(generatePositions(
                 Cannon::new,
                 Score.CANNON,
-                createGreenPoints(Point.of(1, 2), Point.of(7, 2)),
-                createRedPoints(Point.of(1, 7), Point.of(7, 7))
+                InitialPoint.CANNON.greenPoints,
+                InitialPoint.CANNON.redPoints
         ));
         positions.addAll(generatePositions(
                 Soldier::new,
                 Score.SOLDIER,
-                createGreenPoints(
-                        Point.of(0, 3),
-                        Point.of(2, 3),
-                        Point.of(4, 3),
-                        Point.of(6, 3),
-                        Point.of(8, 3)
-                ),
-                createRedPoints(
-                        Point.of(0, 6),
-                        Point.of(2, 6),
-                        Point.of(4, 6),
-                        Point.of(6, 6),
-                        Point.of(8, 6)
-                )
+                InitialPoint.SOLDIER.greenPoints,
+                InitialPoint.SOLDIER.redPoints
         ));
 
         return new Board(positions);
@@ -109,11 +97,22 @@ public final class BoardFactory {
         return positions;
     }
 
-    private static List<Point> createRedPoints(final Point... args) {
-        return List.of(args);
-    }
+    private enum InitialPoint {
+        GENERAL(List.of(Point.of(4, 1)), List.of(Point.of(4, 8))),
+        SOLDIER(List.of(Point.of(0, 3), Point.of(2, 3), Point.of(4, 3), Point.of(6, 3), Point.of(8, 3)),
+                List.of(Point.of(0, 6), Point.of(2, 6), Point.of(4, 6), Point.of(6, 6), Point.of(8, 6))),
+        GUARD(List.of(Point.of(3, 0), Point.of(5, 0)), List.of(Point.of(3, 9), Point.of(5, 9))),
+        ELEPHANT(List.of(Point.of(1, 0), Point.of(6, 0)), List.of(Point.of(1, 9), Point.of(6, 9))),
+        HORSE(List.of(Point.of(2, 0), Point.of(7, 0)), List.of(Point.of(2, 9), Point.of(7, 9))),
+        CANNON(List.of(Point.of(1, 2), Point.of(7, 2)), List.of(Point.of(1, 7), Point.of(7, 7))),
+        CHARIOT(List.of(Point.of(0, 0), Point.of(8, 0)), List.of(Point.of(0, 9), Point.of(8, 9)));
 
-    private static List<Point> createGreenPoints(final Point... args) {
-        return List.of(args);
+        private final List<Point> greenPoints;
+        private final List<Point> redPoints;
+
+        InitialPoint(final List<Point> greenPoints, final List<Point> redPoints) {
+            this.greenPoints = greenPoints;
+            this.redPoints = redPoints;
+        }
     }
 }
