26a74abcf272597fa29994f7d9c5a4b218c772bc
jhpark1227 <junhyeok0246@naver.com>
2025-03-25 17:29:24 +0900

    test: Sa 단위테스트 추가

--- src/test/java/domain/piece/SaTest.java
+++ src/test/java/domain/piece/SaTest.java
@@ -1,36 +1,48 @@
 package domain.piece;
 
-import domain.Move;
 import domain.Position;
 import domain.Team;
 import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class SaTest {
 
 
-    @DisplayName("사는 상하좌우로 이동할 수 있다.")
     @ParameterizedTest
     @CsvSource({
-            "5,4,BACK",
-            "3,4,FRONT",
-            "4,3,LEFT",
-            "4,5,RIGHT"
+            "1, 0",
+            "0, 1",
+            "-1, 0",
+            "0, -1"
     })
-    void test(int row, int column, Move move) {
-
+    void 궁은_상하좌우로_이동할_수_있다(int movedRow, int movedColumn) {
         //given
         Sa sa = new Sa(Team.CHO);
-        Position startPosition = new Position(4, 4);
-        Position targetPosition = new Position(row, column);
+        int row = 4;
+        int column = 4;
+        Position startPosition = new Position(row, column);
+        Position targetPosition = new Position(row + movedRow, column + movedColumn);
 
         //when
-        List<Position> resultMove = sa.calculatePath(startPosition, targetPosition);
+        List<Position> actual = sa.calculatePath(startPosition, targetPosition);
 
         // then
-        Assertions.assertThat(resultMove).isEqualTo(List.of());
+        Assertions.assertThat(actual).isEqualTo(List.of());
+    }
+
+    @Test
+    void 궁은_두_칸_이동할_수_없다() {
+        // given
+        Sa sa = new Sa(Team.HAN);
+        Position src = new Position(4, 1);
+        Position dest = new Position(4, 3);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> sa.calculatePath(src, dest))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이 위치로 이동할 수 없습니다.");
     }
 }
