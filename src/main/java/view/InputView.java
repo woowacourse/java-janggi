cd3a9aadfd5d01b0f523336436fa227ab592e7b0
Jin409 <sualng123@gmail.com>
2025-03-25 17:31:39 +0900

    refactor(InputView): 매직 넘버 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,12 +10,15 @@ public final class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final int START_POINT_INDEX = 0;
     private static final int ARRIVAL_POINT_INDEX = 1;
+    private static final int VALID_POINT_SIZE = 2;
+    public static final String SPLITTER = ",";
 
     public List<Point> readMovementRequest() {
         System.out.println("출발점과 도착점의 위치를 알려주세요 ex.2,1 3,1");
         final String input = scanner.nextLine();
+
+        validateInput(input);
         final String[] splitInput = input.split(" ");
-        validateInput(splitInput);
 
         final List<Integer> rawStartPoint = formatToIntegerList(splitInput, START_POINT_INDEX);
         final Point startPoint = new Point(rawStartPoint.getFirst(), rawStartPoint.getLast());
@@ -26,19 +29,21 @@ public final class InputView {
         return List.of(startPoint, arrivalPoint);
     }
 
-    private void validateInput(final String[] input) {
-        validateSize(input);
-        validateFormat(input);
+    private void validateInput(final String input) {
+        final String[] splitInput = input.split(" ");
+
+        validateSize(splitInput);
+        validateFormat(splitInput);
     }
 
     private static void validateFormat(String[] input) {
-        if (!input[0].contains(",") || !input[1].contains(",")) {
+        if (!input[START_POINT_INDEX].contains(SPLITTER) || !input[ARRIVAL_POINT_INDEX].contains(SPLITTER)) {
             throw new JanggiArgumentException("2,1 3,1 와 같이 쉼표(,) 를 통해 구분되어야 합니다.");
         }
     }
 
     private static void validateSize(String[] input) {
-        if (input.length != 2) {
+        if (input.length != VALID_POINT_SIZE) {
             throw new JanggiArgumentException("2,1 3,1 와 같이 출발점과 도착점을 모두 명시해야 합니다.");
         }
     }
