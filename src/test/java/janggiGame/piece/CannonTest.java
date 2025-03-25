13dd0dd4beb937a24a5cebe15b0300463c222b9b
RaZel713 <razel7130@gmail.com>
2025-03-25 15:38:35 +0900

    test: 포의 테스트 추가

--- src/test/java/janggiGame/piece/CannonTest.java
+++ src/test/java/janggiGame/piece/CannonTest.java
@@ -7,10 +7,38 @@ import janggiGame.Position;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CannonTest {
+    private static Stream<Arguments> provideRowAndColumn() {
+        return Stream.of(
+                Arguments.of(3, 3),
+                Arguments.of(1, 1),
+                Arguments.of(1, 3),
+                Arguments.of(3, 1)
+        );
+    }
+
+    @DisplayName("포가 목적지로 갈 수 없다면 예외를 발생 시킨다.")
+    @ParameterizedTest
+    @MethodSource("provideRowAndColumn")
+    void cannonCanValidateDestination(int row, int column) {
+        // given
+        Position origin = Position.of(2, 2);
+        Position destination = Position.of(row, column);
+        Cannon cannon = new Cannon(Dynasty.HAN);
+
+        // when // then
+        assertThatCode(() -> cannon.getIntermediatePoints(origin, destination))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessageStartingWith("[ERROR] ");
+    }
+
     @DisplayName("포는 목적지로 가는 경로를 구할 수 있다.")
     @Test
     void cannonCanGetIntermediatePoints() {
@@ -27,64 +55,50 @@ class CannonTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("포가 목적지로 갈 수 없다면 예외를 발생시킨다")
-    @Test
-    void cannonCannotGetIntermediatePoints() {
-        // given
-        Position origin = Position.of(1, 1);
-        Position destination = Position.of(2, 3);
-        Cannon cannon = new Cannon(Dynasty.HAN);
-
-        // when // then
-        assertThatCode(() -> cannon.getIntermediatePoints(origin, destination))
-                .isInstanceOf(UnsupportedOperationException.class)
-                .hasMessageStartingWith("[ERROR]");
-    }
-
-    @DisplayName("포는 이동 경로에 단 하나의 말만 존재한다면 이동 가능하다")
-    @Test
-    void cannonJudgeMovable1() {
-        // given
-        Map<Position, Piece> routesWithPiece = new LinkedHashMap<>();
-        Cannon cannon = new Cannon(Dynasty.HAN);
-
-        routesWithPiece.put(Position.of(1, 2), null);
-        routesWithPiece.put(Position.of(1, 3), new Chariot(Dynasty.HAN));
-
-        // when // then
-        assertThatCode(() -> cannon.validateMove(routesWithPiece, null))
-                .doesNotThrowAnyException();
-    }
-
     @DisplayName("포가 자신을 제외한 다른 포를 넘으려고 할 때 예외를 발생 시킨다.")
     @Test
-    void cannonJudgeMovable2() {
+    void cannonCanNotJumpCannon() {
         // given
-        Map<Position, Piece> routesWithPiece = new LinkedHashMap<>();
+        Map<Position, Piece> intermediatePointsWithPiece = new LinkedHashMap<>();
         Cannon cannon = new Cannon(Dynasty.HAN);
 
-        routesWithPiece.put(Position.of(1, 2), null);
-        routesWithPiece.put(Position.of(1, 3), new Cannon(Dynasty.HAN));
+        intermediatePointsWithPiece.put(Position.of(1, 2), null);
+        intermediatePointsWithPiece.put(Position.of(1, 3), new Cannon(Dynasty.HAN));
 
         // when // then
-        assertThatCode(() -> cannon.validateMove(routesWithPiece, null))
+        assertThatCode(() -> cannon.validateMove(intermediatePointsWithPiece, null))
                 .isInstanceOf(UnsupportedOperationException.class)
-                .hasMessageStartingWith("[ERROR]");
+                .hasMessageStartingWith("[ERROR] ");
     }
 
-    @DisplayName("포는 포를 공격할 수 없다.")
+    @DisplayName("포가 다른 포를 공격 하려고 할 때 예외를 발생 시킨다.")
     @Test
-    void cannonJudgeMovable3() {
+    void cannonCanNotAttackCannon() {
         // given
-        Map<Position, Piece> routesWithPiece = new LinkedHashMap<>();
+        Map<Position, Piece> intermediatePointsWithPiece = new LinkedHashMap<>();
         Cannon cannon = new Cannon(Dynasty.HAN);
 
-        routesWithPiece.put(Position.of(1, 2), null);
-        routesWithPiece.put(Position.of(1, 3), new Chariot(Dynasty.HAN));
+        intermediatePointsWithPiece.put(Position.of(1, 2), null);
+        intermediatePointsWithPiece.put(Position.of(1, 3), new Chariot(Dynasty.HAN));
 
         // when // then
-        assertThatCode(() -> cannon.validateMove(routesWithPiece, new Cannon(Dynasty.CHO))).isInstanceOf(
+        assertThatCode(() -> cannon.validateMove(intermediatePointsWithPiece, new Cannon(Dynasty.CHO))).isInstanceOf(
                         UnsupportedOperationException.class)
-                .hasMessageStartingWith("[ERROR]");
+                .hasMessageStartingWith("[ERROR] ");
+    }
+
+    @DisplayName("포는 이동 경로에 단 하나의 말만 존재 한다면 이동 가능 하다.")
+    @Test
+    void cannonCanMoveIfIntermediatePointsHasOnePiece() {
+        // given
+        Map<Position, Piece> intermediatePointsWithPiece = new LinkedHashMap<>();
+        Cannon cannon = new Cannon(Dynasty.HAN);
+
+        intermediatePointsWithPiece.put(Position.of(1, 2), null);
+        intermediatePointsWithPiece.put(Position.of(1, 3), new Chariot(Dynasty.HAN));
+
+        // when // then
+        assertThatCode(() -> cannon.validateMove(intermediatePointsWithPiece, null))
+                .doesNotThrowAnyException();
     }
 }
