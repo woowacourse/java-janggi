6573041a451f68e4440df9a32539bac90fddbbf8
rladmstn <oz33177351@gmail.com>
2025-03-25 15:00:05 +0900

    feat: 상차림 기능 추가

--- src/main/java/domain/SetUp.java
+++ src/main/java/domain/SetUp.java
@@ -1,25 +1,32 @@
 package domain;
 
+import domain.direction.PieceDirection;
+import domain.piece.Piece;
 import domain.piece.Position;
+import domain.piece.category.Elephant;
+import domain.piece.category.Horse;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
 public enum SetUp {
 
     INNER_ELEPHANT("1",
-            new Position(3, 1),
-            new Position(2, 1)
+            new Position(2, 1),
+            new Position(3, 1)
     ),
     OUTER_ELEPHANT("2",
-            new Position(2, 1),
-            new Position(3, 1)
-    ),
-    RIGHT_ELEPHANT("3",
             new Position(3, 1),
             new Position(2, 1)
     ),
-    LEFT_ELEPHANT("4",
+    RIGHT_ELEPHANT("3",
             new Position(2, 1),
             new Position(3, 1)
+    ),
+    LEFT_ELEPHANT("4",
+            new Position(3, 1),
+            new Position(2, 1)
     ),
     ;
 
@@ -38,5 +45,43 @@ public enum SetUp {
                 .filter(setUp -> setUp.command.equals(input))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 잘못된 선택입니다."));
+    }
+
+    public List<Piece> initializeHorseAndElephant(Function<Position, Position> teamSide) {
+        if (this == INNER_ELEPHANT || this == OUTER_ELEPHANT) {
+            return innerOuterPieces(teamSide);
+        }
+        if (this == RIGHT_ELEPHANT) {
+            return rightElephant(teamSide);
+        }
+        if (this == LEFT_ELEPHANT) {
+            return leftElephant(teamSide);
+        }
+        throw new IllegalArgumentException("[ERROR] 잘못된 선택입니다.");
+    }
+
+    public List<Piece> innerOuterPieces(Function<Position, Position> teamSide) {
+        return getPieces(teamSide, hanHorsePosition.flipLeftRight(), hanElephantPosition.flipLeftRight());
+    }
+
+    public List<Piece> rightElephant(Function<Position, Position> teamSide) {
+        return getPieces(teamSide, hanHorsePosition.flipLeftRight().moveRow(-1),
+                hanElephantPosition.flipLeftRight().moveRow(1));
+    }
+
+    public List<Piece> leftElephant(Function<Position, Position> teamSide) {
+        return getPieces(teamSide, hanHorsePosition.flipLeftRight().moveRow(1),
+                hanElephantPosition.flipLeftRight().moveRow(-1));
+    }
+
+    private List<Piece> getPieces(Function<Position, Position> teamSide, Position hanHorsePosition,
+                                  Position hanElephantPosition) {
+        List<Piece> pieces = new ArrayList<>();
+        pieces.add(new Horse(teamSide.apply(this.hanHorsePosition), PieceDirection.HORSE.get()));
+        pieces.add(new Horse(teamSide.apply(hanHorsePosition), PieceDirection.HORSE.get()));
+        pieces.add(new Elephant(teamSide.apply(this.hanElephantPosition), PieceDirection.ELEPHANT.get()));
+        pieces.add(new Elephant(teamSide.apply(hanElephantPosition),
+                PieceDirection.ELEPHANT.get()));
+        return pieces;
     }
 }
