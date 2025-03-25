25b36e5ea8bcbb38ff303444dd0cf3faa84a1619
민경윤 <ruddbs3308@naver.com>
2025-03-26 00:15:39 +0900

    refactor: 기물의 이동 검증을 Movement를 통해 하도록 변경

--- src/main/java/domain/piece/Horse.java
+++ src/main/java/domain/piece/Horse.java
@@ -11,6 +11,7 @@ import static domain.board.Offset.UP;
 
 import domain.Team;
 import domain.board.Offset;
+import domain.board.movement.Movement;
 import java.util.List;
 import java.util.Map;
 
@@ -32,7 +33,8 @@ public class Horse extends Piece {
     }
 
     @Override
-    protected void validateOffset(final Offset offset) {
+    protected void validateMovement(final Movement movement) {
+        final Offset offset = movement.calcaulteOffset();
         if (!MOVEMENT_RULES.containsKey(offset)) {
             throw new IllegalArgumentException("해당 말은 해당 위치로 이동할 수 없습니다.");
         }
--- src/main/java/domain/piece/Piece.java
+++ src/main/java/domain/piece/Piece.java
@@ -3,6 +3,7 @@ package domain.piece;
 import domain.Team;
 import domain.board.BoardPosition;
 import domain.board.Offset;
+import domain.board.movement.Movement;
 import java.util.List;
 import java.util.Objects;
 
@@ -20,13 +21,13 @@ public abstract class Piece {
             final BoardPosition before,
             final BoardPosition after
     ) {
-        final Offset offset = after.calculateOffset(before);
-        validateOffset(offset);
+        final Movement movement = Movement.of(before, after);
+        validateMovement(movement);
 
-        return createMovementRule(offset);
+        return createMovementRule(movement.calcaulteOffset());
     }
 
-    protected abstract void validateOffset(final Offset offset);
+    protected abstract void validateMovement(final Movement movement);
 
     protected abstract List<Offset> createMovementRule(final Offset offset);
 
