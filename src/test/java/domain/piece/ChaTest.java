0c7e289cd43eafa7c3f2d7d214ae60c71aa2bc89
jhpark1227 <junhyeok0246@naver.com>
2025-03-25 16:43:44 +0900

    test: Cha 단위테스트 추가

--- src/test/java/domain/piece/ChaTest.java
+++ src/test/java/domain/piece/ChaTest.java
@@ -1,42 +1,93 @@
 package domain.piece;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.Position;
 import domain.Team;
 import java.util.List;
-import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ChaTest {
 
-    @DisplayName("차의 이동 경로를 계산할 수 있다")
     @Test
-    void test1() {
+    void 차가_위로_이동하는_경로를_계산할_수_있다() {
+        // given
         Cha cha = new Cha(Team.HAN);
+        Position src = new Position(4, 1);
+        Position dest = new Position(1, 1);
 
-        List<Position> path1 = cha.calculatePath(new Position(1, 1), new Position(1, 4));
-        List<Position> path2 = cha.calculatePath(new Position(1, 1), new Position(4, 1));
-        List<Position> path3 = cha.calculatePath(new Position(6, 1), new Position(3, 1));
-        List<Position> expected1 = List.of(new Position(1, 2), new Position(1, 3));
-        List<Position> expected2 = List.of(new Position(2, 1), new Position(3, 1));
-        List<Position> expected3 = List.of(new Position(5, 1), new Position(4, 1));
+        // when
+        List<Position> path = cha.calculatePath(src, dest);
 
-        SoftAssertions.assertSoftly(softAssertions -> {
-            softAssertions.assertThat(path1).isEqualTo(expected1);
-            softAssertions.assertThat(path2).isEqualTo(expected2);
-            softAssertions.assertThat(path3).isEqualTo(expected3);
-
-        });
+        // then
+        List<Position> expected = List.of(new Position(3, 1), new Position(2, 1));
+        assertThat(path).isEqualTo(expected);
     }
 
-    @DisplayName("차로 이동할 수 없는 위치인 경우 예외를 발생시킨다")
     @Test
-    void test3() {
+    void 차가_아래로_이동하는_경로를_계산할_수_있다() {
+        // given
         Cha cha = new Cha(Team.HAN);
+        Position src = new Position(1, 1);
+        Position dest = new Position(4, 1);
 
-        assertThatThrownBy(() -> cha.calculatePath(new Position(1, 1), new Position(2, 2)))
+        // when
+        List<Position> path = cha.calculatePath(src, dest);
+
+        // then
+        List<Position> expected = List.of(new Position(2, 1), new Position(3, 1));
+        assertThat(path).isEqualTo(expected);
+    }
+
+    @Test
+    void 차가_왼쪽으로_이동하는_경로를_계산할_수_있다() {
+        // given
+        Cha cha = new Cha(Team.HAN);
+        Position src = new Position(4, 4);
+        Position dest = new Position(1, 4);
+
+        // when
+        List<Position> path = cha.calculatePath(src, dest);
+
+        // then
+        List<Position> expected = List.of(new Position(3, 4), new Position(2, 4));
+        assertThat(path).isEqualTo(expected);
+    }
+
+    @Test
+    void 차가_오른쪽으로_이동하는_경로를_계산할_수_있다() {
+        // given
+        Cha cha = new Cha(Team.HAN);
+        Position src = new Position(1, 1);
+        Position dest = new Position(1, 4);
+
+        // when
+        List<Position> path1 = cha.calculatePath(src, dest);
+
+        // then
+        List<Position> expected1 = List.of(new Position(1, 2), new Position(1, 3));
+        assertThat(path1).isEqualTo(expected1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "1, -1",
+            "-1, 1",
+            "-1, -1"
+    })
+    void 차로_이동할_수_없는_위치인_경우_예외를_발생시킨다(int movedRow, int movedColumn) {
+        // given
+        Cha cha = new Cha(Team.HAN);
+        int row = 5;
+        int column = 5;
+        Position src = new Position(row, column);
+        Position dest = new Position(row + movedRow, column + movedColumn);
+        // when & then
+        assertThatThrownBy(() -> cha.calculatePath(src, dest))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이 위치로는 움직일 수 없습니다.");
     }
