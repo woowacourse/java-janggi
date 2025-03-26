3a70617aa5b3d6652481558f1c942f4143ce87c2
taek2222 <oot309@daum.net>
2025-03-26 17:43:14 +0900

    test: 가독성 향상을 위한MethodSource 테스트 CsvSource 형태로 변경

--- src/test/java/domain/MoveInfosTest.java
+++ src/test/java/domain/MoveInfosTest.java
@@ -4,17 +4,18 @@ import domain.piece.category.PieceCategory;
 import domain.spatial.Position;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MoveInfosTest {
 
     @ParameterizedTest
-    @MethodSource
+    @CsvSource({
+            "NONE, 0",
+            "CANNON, 1"
+    })
     void 경로에_기물의_갯수를_반환한다(PieceCategory pathPieceCategory, int excepted) {
         // given
         List<MoveInfo> moveInfoElements = new ArrayList<>();
@@ -29,15 +30,11 @@ class MoveInfosTest {
         assertThat(result).isEqualTo(excepted);
     }
 
-    private static Stream<Arguments> 경로에_기물의_갯수를_반환한다() {
-        return Stream.of(
-                Arguments.of(PieceCategory.NONE, 0),
-                Arguments.of(PieceCategory.CANNON, 1)
-        );
-    }
-
     @ParameterizedTest
-    @MethodSource
+    @CsvSource({
+            "KING, KING, true",
+            "KING, SOLDIER, false"
+    })
     void 이동_기물과_도착지_기물이_같은지_판단한다(PieceCategory startPiece, PieceCategory targetPiece, boolean excepted) {
         // given
         List<MoveInfo> moveInfoElements = new ArrayList<>();
@@ -51,13 +48,5 @@ class MoveInfosTest {
 
         // then
         assertThat(result).isEqualTo(excepted);
-    }
-
-    private static Stream<Arguments> 이동_기물과_도착지_기물이_같은지_판단한다() {
-        return Stream.of(
-                Arguments.of(PieceCategory.KING, PieceCategory.KING, true),
-                Arguments.of(PieceCategory.KING, PieceCategory.SOLDIER, false)
-
-        );
     }
 }
