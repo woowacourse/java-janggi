7fdb2e322002bd009243dfec7749850163871d71
BWY <quantumjump10@gmail.com>
2025-03-26 16:02:18 +0900

    fix: 방향 검증 과정 부호 수정

--- src/main/java/janggi/piece/Soldier.java
+++ src/main/java/janggi/piece/Soldier.java
@@ -13,27 +13,27 @@ public class Soldier extends Piece {
 
     @Override
     public List<Position> findPositionsInPath(Position start, Position goal) {
-        int rowDifference = start.calculatesRowDifference(goal);
         int columnDifference = start.calculatesColumnDifference(goal);
+        int rowDifference = start.calculatesRowDifference(goal);
 
-        if (!isValidMoveRule(rowDifference, columnDifference)) {
+        if (!isValidMoveRule(columnDifference, rowDifference)) {
             throw new IllegalArgumentException("[ERROR] 병의 이동 규칙에 어긋나는 움직임입니다.");
         }
 
         return List.of(goal);
     }
 
-    private boolean isValidMoveRule(int rowDifference, int columnDifference) {
+    private boolean isValidMoveRule(int columnDifference, int rowDifference) {
         return isValidDirection(rowDifference) && isValidDistance(rowDifference, columnDifference);
     }
 
     private boolean isValidDirection(int rowDifference) {
-        return (getTeam() == Team.GREEN && rowDifference > 0) ||
-                (getTeam() == Team.RED && rowDifference < 0);
+        return (getTeam() == Team.GREEN && rowDifference >= 0) ||
+                (getTeam() == Team.RED && rowDifference <= 0);
     }
 
-    private boolean isValidDistance(int rowDifference, int columnDifference) {
-        return Math.abs(rowDifference) + Math.abs(columnDifference) == 1;
+    private boolean isValidDistance(int columnDifference, int rowDifference) {
+        return Math.abs(columnDifference) + Math.abs(rowDifference) == 1;
     }
 
     @Override
