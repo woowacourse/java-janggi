673d817281c201cf398168f62038214241a12389
eoehd1ek <eoehd2ek@gmail.com>
2025-03-26 20:13:34 +0900

    fix(Direction): 왼쪽 오른쪽 방향값이 잘못된 오류 수정

--- src/main/java/janggi/position/Direction.java
+++ src/main/java/janggi/position/Direction.java
@@ -3,10 +3,9 @@ package janggi.position;
 public record Direction(int x, int y) {
     public static final Direction UP = new Direction(0, -1);
     public static final Direction DOWN = new Direction(0, 1);
-    public static final Direction LEFT = new Direction(1, 0);
-    public static final Direction RIGHT = new Direction(-1, 0);
-
-
+    public static final Direction LEFT = new Direction(-1, 0);
+    public static final Direction RIGHT = new Direction(1, 0);
+    
     public boolean isReverseFrontVerticalDirection(Direction direction) {
         int reverseVerticalDirection = -y;
         return direction.y == reverseVerticalDirection;
