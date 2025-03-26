bd6ad949bb708a7eee291c7fad104b2ef73d5100
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-03-26 15:22:16 +0900

    refactor: 장기판 초기화를 담당하는 Started 상태 구현

---
+++ src/main/java/janggiGame/state/Started/Started.java
@@ -0,0 +1,68 @@
+package janggiGame.state.Started;
+
+import janggiGame.arrangement.ArrangementStrategy;
+import janggiGame.board.Dot;
+import janggiGame.piece.Dynasty;
+import janggiGame.piece.Piece;
+import janggiGame.state.GameResult;
+import janggiGame.state.GameScore;
+import janggiGame.state.Running.ChoTurn;
+import janggiGame.state.State;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Started implements State {
+
+    public static final String ERROR_MESSAGE = "[ERROR] 아직 장기판이 초기화되지 않았습니다.";
+
+    @Override
+    public State arrangePieces(ArrangementStrategy hanStrategy, ArrangementStrategy choStrategy) {
+        Map<Dot, Piece> pieces = new HashMap<>();
+        pieces.putAll(arrangeHanPieces(hanStrategy));
+        pieces.putAll(arrangeChoPieces(choStrategy));
+        return new ChoTurn(pieces, false);
+    }
+
+    private Map<Dot, Piece> arrangeHanPieces(ArrangementStrategy strategy) {
+        return strategy.arrangeHan(Dynasty.HAN);
+    }
+
+    private Map<Dot, Piece> arrangeChoPieces(ArrangementStrategy strategy) {
+        return strategy.arrangeCho(Dynasty.CHO);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public State takeTurn(Dot origin, Dot destination) {
+        throw new IllegalStateException(ERROR_MESSAGE);
+    }
+
+    @Override
+    public State skipTurn() {
+        throw new IllegalStateException(ERROR_MESSAGE);
+    }
+
+    @Override
+    public GameResult getGameResult() {
+        throw new IllegalStateException(ERROR_MESSAGE);
+    }
+
+    @Override
+    public GameScore getGameScore() {
+        throw new IllegalStateException(ERROR_MESSAGE);
+    }
+
+    @Override
+    public Map<Dot, Piece> getPieces() {
+        throw new IllegalStateException(ERROR_MESSAGE);
+    }
+
+    @Override
+    public Dynasty getCurrentDynasty() {
+        throw new IllegalStateException(ERROR_MESSAGE);
+    }
+}
---
+++ src/test/java/janggiGame/state/Started/StartedTest.java
@@ -0,0 +1,73 @@
+package janggiGame.state.Started;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import janggiGame.arrangement.ArrangementStrategy;
+import janggiGame.arrangement.InnerElephantStrategy;
+import janggiGame.board.Board;
+import janggiGame.state.Running.ChoTurn;
+import janggiGame.state.State;
+import java.util.stream.Stream;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class StartedTest {
+
+    @DisplayName("시작 상태에서 호출할 수 없는 메서드를 호출 시 예외가 발생한다")
+    @ParameterizedTest
+    @MethodSource("provideInvalidMethod")
+    void test(ThrowingCallable throwingCallable) {
+        // when // then
+        assertThatCode(throwingCallable)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+    public static Stream<Arguments> provideInvalidMethod() {
+        State started = new Started();
+
+        return Stream.of(
+                Arguments.of((ThrowingCallable) () -> started.takeTurn(Board.findBy(1, 1), Board.findBy(2, 2))),
+                Arguments.of((ThrowingCallable) started::skipTurn),
+                Arguments.of((ThrowingCallable) started::getGameResult),
+                Arguments.of((ThrowingCallable) started::getGameScore),
+                Arguments.of((ThrowingCallable) started::getPieces),
+                Arguments.of((ThrowingCallable) started::getCurrentDynasty)
+        );
+    }
+
+    @DisplayName("시작상태는 끝나지 않은 상태이다")
+    @Test
+    void isFinished() {
+        // given
+        State started = new Started();
+
+        // when
+        boolean actual = started.isFinished();
+
+        // then
+        assertThat(actual).isFalse();
+    }
+
+    @DisplayName("시작상태에서 기물을 배치하면 초나라의 턴으로 넘어간다")
+    @Test
+    void startedToChoTurn() {
+        // given
+        State started = new Started();
+        ArrangementStrategy strategy = new InnerElephantStrategy();
+
+        // when
+        State actual = started.arrangePieces(strategy, strategy);
+
+        // then
+        assertThat(actual).isInstanceOf(ChoTurn.class);
+    }
+
+
+}
