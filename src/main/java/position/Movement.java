63ddeb49cf8a88ec46a556b0385a083e1247062d
byesol <bsj5967@naver.com>
2025-03-26 19:47:22 +0900

    feat: 움직임 경우의 수 추가

--- src/main/java/position/Movement.java
+++ src/main/java/position/Movement.java
@@ -5,11 +5,33 @@ public enum Movement {
     DOWN(0, -1),
     LEFT(-1, 0),
     RIGHT(1, 0),
+
     LEFT_UP(-1, 1),
     RIGHT_UP(1, 1),
     LEFT_DOWN(-1, -1),
     RIGHT_DOWN(1, -1),
-    ;
+    UP_RIGHT(-1, 1),
+    DOWN_RIGHT(1, 1),
+    DOWN_LEFT(1, -1),
+    UP_LEFT(-1, -1),
+
+    UP_UP_RIGHT(-2, 1),
+    UP_RIGHT_RIGHT(-1, 2),
+    DOWN_RIGHT_RIGHT(1, 2),
+    DOWN_DOWN_RIGHT(2, 1),
+    DOWN_DOWN_LEFT(2, -1),
+    DOWN_LEFT_LEFT(1, -2),
+    UP_LEFT_LEFT(-1, -2),
+    UP_UP_LEFT(-2, -1),
+
+    UP_UP_UP_RIGHT_RIGHT(-3, 2),
+    UP_UP_RIGHT_RIGHT_RIGHT(-2, 3),
+    DOWN_DOWN_RIGHT_RIGHT_RIGHT(2, 3),
+    DOWN_DOWN_DOWN_RIGHT_RIGHT(3, 2),
+    DOWN_DOWN_DOWN_LEFT_LEFT(3, -2),
+    DOWN_DOWN_LEFT_LEFT_LEFT(2, -3),
+    UP_UP_LEFT_LEFT_LEFT(-2, -3),
+    UP_UP_UP_LEFT_LEFT(-3, -2);;
 
     private final int x;
 
