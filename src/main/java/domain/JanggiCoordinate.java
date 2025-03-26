b862245ffc9df3a1a07a64916ed52d5f36f81218
Han <jumdo12@gmail.com>
2025-03-26 11:18:25 +0900

    style(Coordinate): 메서드 선언 순서 개선

--- src/main/java/domain/JanggiCoordinate.java
+++ src/main/java/domain/JanggiCoordinate.java
@@ -2,17 +2,6 @@ package domain;
 
 public record JanggiCoordinate(int row, int col) {
 
-    public int distanceTo(JanggiCoordinate to) {
-        int rowDst = Math.abs(this.row() - to.row());
-        int colDst = Math.abs(this.col() - to.col());
-
-        return square(rowDst) + square(colDst);
-    }
-
-    private int square(int n) {
-        return (int) Math.pow(n, 2);
-    }
-
     public JanggiCoordinate moveUp() {
         return new JanggiCoordinate(this.row - 1, this.col);
     }
@@ -56,6 +45,17 @@ public record JanggiCoordinate(int row, int col) {
             return moveLeft();
         }
         return moveDown();
+    }
+
+    public int distanceTo(JanggiCoordinate to) {
+        int rowDst = Math.abs(this.row() - to.row());
+        int colDst = Math.abs(this.col() - to.col());
+
+        return square(rowDst) + square(colDst);
+    }
+
+    private int square(int n) {
+        return (int) Math.pow(n, 2);
     }
 
     @Override
