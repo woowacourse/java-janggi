71e84976112c49e6d00fcfb870cb8434fbdb084d
taek2222 <oot309@daum.net>
2025-03-26 20:11:23 +0900

    refactor: 구조 변경으로, Piece 메서드 정리

--- src/main/java/domain/piece/category/Soldier.java
+++ src/main/java/domain/piece/category/Soldier.java
@@ -7,6 +7,7 @@ import domain.spatial.Position;
 
 public class Soldier extends Piece {
 
+    private static final PieceCategory CATEGORY = PieceCategory.SOLDIER;
     private static final int PIECES_TO_PASS = 0;
 
     public Soldier(final Position position, final Directions directions) {
@@ -14,30 +15,18 @@ public class Soldier extends Piece {
     }
 
     @Override
-    public Soldier updatePosition(final Position position) {
+    public Soldier move(final Position position, final MoveInfos moveInfos) {
+        validateMove(moveInfos);
         return new Soldier(position, directions);
     }
 
     @Override
-    public boolean isKing() {
-        return false;
+    public PieceCategory getCategory() {
+        return CATEGORY;
     }
 
-    @Override
-    public boolean isCannon() {
-        return false;
-    }
-
-    @Override
-    public void validateMove(final MoveInfos moveInfos) {
+    private void validateMove(final MoveInfos moveInfos) {
         if (moveInfos.countPiecesInPath() != PIECES_TO_PASS) {
-            throw new IllegalArgumentException("[ERROR] 병/졸은 중간에 기물이 " + PIECES_TO_PASS + "개여야 합니다.");
-        }
-    }
-
-    @Override
-    public void validateMoveByPathPieceCount(final int pathPieceCount) {
-        if (pathPieceCount != PIECES_TO_PASS) {
             throw new IllegalArgumentException("[ERROR] 병/졸은 중간에 기물이 " + PIECES_TO_PASS + "개여야 합니다.");
         }
     }
