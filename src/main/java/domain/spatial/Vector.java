766bf5bba974f294baff9b924c174b8b5c053278
taek2222 <oot309@daum.net>
2025-03-26 22:05:16 +0900

    refactor: 움직임 객체 검증 로직 가독성 개선

--- src/main/java/domain/spatial/Vector.java
+++ src/main/java/domain/spatial/Vector.java
@@ -5,19 +5,17 @@ public record Vector(
         int moveColumn
 ) {
 
-    private static final int MAX_MOVE_ROW = 1;
-    private static final int MIN_MOVE_ROW = -1;
-    private static final int MAX_MOVE_COLUMN = 1;
-    private static final int MIN_MOVE_COLUMN = -1;
-
     public Vector {
         validateRange(moveRow, moveColumn);
     }
 
     private void validateRange(final int moveRow, final int moveColumn) {
-        if (moveRow > MAX_MOVE_ROW || moveColumn > MAX_MOVE_COLUMN || moveRow < MIN_MOVE_ROW
-                || moveColumn < MIN_MOVE_COLUMN) {
+        if (isInvalidMove(moveRow) || isInvalidMove(moveColumn)) {
             throw new IllegalArgumentException("이동 방향의 수치는 -1, 0, 1만 가능합니다.");
         }
+    }
+
+    private boolean isInvalidMove(final int move) {
+        return move != -1 && move != 0 && move != 1;
     }
 }
