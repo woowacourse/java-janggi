da90621467e4f7586e3cbe10b473338d1b0b874a
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-26 20:43:31 +0900

    test: Position 테스트 파라미터 수정

--- src/test/java/janggi/position/PositionTest.java
+++ src/test/java/janggi/position/PositionTest.java
@@ -3,6 +3,11 @@ package janggi.position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -23,27 +28,39 @@ public class PositionTest {
         );
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("makeOutOfBoardTrueTestData")
     @DisplayName("Position이 board 이용 가능한 위치가 아니면 True")
-    void outOfBoardTrueTest() {
-        //given
-        //when
-        //then
-        assertAll(
-                () -> Assertions.assertThat(new Position(new Row(11), new Column(10)).isOutOfBoards()).isTrue(),
-                () -> Assertions.assertThat(new Position(new Row(0), new Column(0)).isOutOfBoards()).isTrue()
+    void outOfBoardTrueTest(Position position) {
+        Assertions.assertThat(position.isOutOfBoards()).isTrue();
+    }
+
+    static Stream<Arguments> makeOutOfBoardTrueTestData() {
+        return Stream.of(
+        Arguments.arguments(
+        new Position(new Row(11), new Column(10))
+                ),
+        Arguments.arguments(
+        new Position(new Row(0), new Column(0))
+                )
         );
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("makeOutOfBoardFalseTestData")
     @DisplayName("Position이 board 이용 가능한 위치에 있으면 False")
-    void outOfBoardFalseTest() {
-        //given
-        //when
-        //then
-        assertAll(
-                () -> Assertions.assertThat(new Position(new Row(10), new Column(9)).isOutOfBoards()).isFalse(),
-                () -> Assertions.assertThat(new Position(new Row(1), new Column(1)).isOutOfBoards()).isFalse()
+    void outOfBoardFalseTest(Position position) {
+        Assertions.assertThat(position.isOutOfBoards()).isFalse();
+    }
+
+    static Stream<Arguments> makeOutOfBoardFalseTestData() {
+        return Stream.of(
+                Arguments.arguments(
+                        new Position(new Row(10), new Column(9))
+                ),
+                Arguments.arguments(
+                        new Position(new Row(1), new Column(1))
+                )
         );
     }
 }
