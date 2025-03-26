ab72d0b4bbc89c616271bd04f55f1e539e821496
bowook <mbosx316@naver.com>
2025-03-26 19:43:48 +0900

    feat(Sa) : 적 장기말 잡기 기능 추가

--- src/main/java/janggi/piece/Sa.java
+++ src/main/java/janggi/piece/Sa.java
@@ -28,6 +28,7 @@ public class Sa extends Piece {
         if (!ableToMove(destination, enemyPieces, allyPieces)) {
             throw new IllegalArgumentException("[ERROR] 이동이 불가능합니다.");
         }
+        enemyPieces.beAttackedAt(destination);
         return new Sa(destination);
     }
 
