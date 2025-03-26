6127e963c9e658f16a902ec90406249d89505a70
taek2222 <oot309@daum.net>
2025-03-26 15:40:38 +0900

    test: 경로의 배치된 기물이 없는 지 판단 테스트 케이스 추가

--- src/test/java/domain/MoveInfoTest.java
+++ src/test/java/domain/MoveInfoTest.java
@@ -1,30 +1,40 @@
 package domain;
 
+import domain.direction.PieceDirection;
 import domain.piece.Piece;
+import domain.piece.category.King;
 import domain.spatial.Position;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MoveInfoTest {
 
-    @Test
-    void 경로에_배치된_기물이_없는지_판단한다() {
+    @ParameterizedTest
+    @MethodSource
+    void 경로에_배치된_기물이_없는지_판단한다(Piece pathPiece, boolean excepted) {
         // given
-        Map<Position, Piece> paths = new HashMap<>();
-        paths.put(new Position(1, 2), null);
-        paths.put(new Position(2, 2), null);
-        paths.put(new Position(3, 2), null);
+        Map<Position, Piece> pathPieces = new LinkedHashMap<>();
+        pathPieces.put(new Position(1, 2), pathPiece);
 
-        MoveInfo moveInfo = new MoveInfo(paths);
+        MoveInfo moveInfo = new MoveInfo(pathPieces);
 
         // when
         boolean result = moveInfo.isPathInPiece();
 
         // then
-        assertThat(result)
-                .isTrue();
+        assertThat(result).isEqualTo(excepted);
+    }
+
+    private static Stream<Arguments> 경로에_배치된_기물이_없는지_판단한다() {
+        return Stream.of(
+                Arguments.of(null, true),
+                Arguments.of(new King(new Position(1, 2), PieceDirection.KING.get()), false)
+        );
     }
 }
