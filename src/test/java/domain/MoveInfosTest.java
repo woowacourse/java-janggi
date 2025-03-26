5fd4f0e20044187a19a50853a95d859e92793503
taek2222 <oot309@daum.net>
2025-03-26 17:35:04 +0900

    test: 이동 기물과 도착지 기물이 같은지 판단 테스트 작성

--- src/test/java/domain/MoveInfosTest.java
+++ src/test/java/domain/MoveInfosTest.java
@@ -36,28 +36,28 @@ class MoveInfosTest {
         );
     }
 
-//    @ParameterizedTest
-//    @MethodSource
-//    void 이동_기물과_도착지_기물이_같은지_판단한다(PieceCategory startPiece, PieceCategory targetPiece, boolean excepted) {
-//        // given
-//        List<MoveInfo> moveInfoElements = new ArrayList<>();
-//        moveInfoElements.add(new MoveInfo(new Position(1, 2), PieceCategory.NONE));
-//        moveInfoElements.add(new MoveInfo(new Position(2, 2), targetPiece));
-//
-//        MoveInfos moveInfos = new MoveInfos(pathPieces);
-//
-//        // when
-//        boolean result = moveInfos.isSameTargetPiece(startPiece);
-//
-//        // then
-//        assertThat(result).isEqualTo(excepted);
-//    }
-//
-//    private static Stream<Arguments> 이동_기물과_도착지_기물이_같은지_판단한다() {
-//        return Stream.of(
-//                Arguments.of(PieceCategory.KING, PieceCategory.KING, true),
-//                Arguments.of(PieceCategory.KING, PieceCategory.SOLDIER, false)
-//
-//        );
-//    }
+    @ParameterizedTest
+    @MethodSource
+    void 이동_기물과_도착지_기물이_같은지_판단한다(PieceCategory startPiece, PieceCategory targetPiece, boolean excepted) {
+        // given
+        List<MoveInfo> moveInfoElements = new ArrayList<>();
+        moveInfoElements.add(new MoveInfo(new Position(1, 2), PieceCategory.NONE));
+        moveInfoElements.add(new MoveInfo(new Position(2, 2), targetPiece));
+
+        MoveInfos moveInfos = new MoveInfos(moveInfoElements);
+
+        // when
+        boolean result = moveInfos.isSameTargetPiece(startPiece);
+
+        // then
+        assertThat(result).isEqualTo(excepted);
+    }
+
+    private static Stream<Arguments> 이동_기물과_도착지_기물이_같은지_판단한다() {
+        return Stream.of(
+                Arguments.of(PieceCategory.KING, PieceCategory.KING, true),
+                Arguments.of(PieceCategory.KING, PieceCategory.SOLDIER, false)
+
+        );
+    }
 }
