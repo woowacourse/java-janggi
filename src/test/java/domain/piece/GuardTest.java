1c2efd19214e1fa8587d1fbfb35670561c5937ac
dltmddus <tmddus4671@gmail.com>
2025-03-26 15:46:28 +0900

    feat: 사가 궁성 안에서 이동하는 테스트 추가

---
+++ src/test/java/domain/piece/GuardTest.java
@@ -0,0 +1,66 @@
+package domain.piece;
+
+import static domain.Fixtures.EIGHT_FIVE;
+import static domain.Fixtures.EIGHT_FOUR;
+import static domain.Fixtures.EIGHT_SIX;
+import static domain.Fixtures.NINE_FIVE;
+import static domain.Fixtures.NINE_FOUR;
+import static domain.Fixtures.NINE_SIX;
+import static domain.Fixtures.SEVEN_FIVE;
+import static domain.Fixtures.ZERO_FIVE;
+import static domain.Fixtures.ZERO_FOUR;
+import static domain.Fixtures.ZERO_SIX;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import domain.JanggiPosition;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class GuardTest {
+    @ParameterizedTest
+    @MethodSource("provideGuardAfterPositions")
+    void 사를_궁성안에서_이동시킬_수_있다(JanggiPosition afterPosition) {
+        // given
+        Piece piece = new Guard(Side.CHO);
+
+        // when & then
+        assertDoesNotThrow(() -> piece.findMovablePath(NINE_FIVE, afterPosition));
+    }
+
+    static Stream<Arguments> provideGuardAfterPositions() {
+        return Stream.of(
+                Arguments.of(ZERO_FOUR),
+                Arguments.of(ZERO_FIVE),
+                Arguments.of(ZERO_SIX),
+                Arguments.of(NINE_FOUR),
+                Arguments.of(NINE_SIX),
+                Arguments.of(EIGHT_FOUR),
+                Arguments.of(EIGHT_FIVE),
+                Arguments.of(EIGHT_SIX)
+        );
+    }
+
+    @Test
+    void 사를_궁성밖에서_이동시킬_수_없다() {
+        // given
+        Piece piece = new Guard(Side.CHO);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> piece.findMovablePath(EIGHT_FIVE, SEVEN_FIVE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 잘못된_대각선_방향으로_이동시킬_수_없다() {
+        // given
+        Piece piece = new Guard(Side.CHO);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> piece.findMovablePath(EIGHT_FIVE, NINE_FOUR))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
