974a7732b5b11b54c3c8be18b1a6fd4cf20765fd
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-26 13:50:45 +0900

    feat: 포의 경로 구하기 테스트 추가

--- src/test/java/domain/piece/CannonTest.java
+++ src/test/java/domain/piece/CannonTest.java
@@ -5,33 +5,65 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import domain.Position;
 import domain.Team;
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CannonTest {
 
-    @DisplayName("포의 이동 경로를 계산할 수 있다")
-    @Test
-    void test() {
+    @DisplayName("포의 상하좌우 경로를 계산할 수 있다")
+    @ParameterizedTest
+    @MethodSource("providePositions")
+    void test1(Position startPosition, Position targetPosition, List<Position> expected) {
         // given
         Cannon cannon = new Cannon(Team.RED);
-        Position startPosition = new Position(3, 2);
-        Position targetPosition = new Position(6, 2);
+
         // when
         List<Position> moves = cannon.calculatePath(startPosition, targetPosition);
+
         // then
-        List<Position> expected = List.of(new Position(4, 2), new Position(5, 2));
         Assertions.assertThat(moves).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> providePositions() {
+        return Stream.of(
+                Arguments.of(
+                        new Position(3, 2),
+                        new Position(6, 2),
+                        List.of(new Position(4, 2), new Position(5, 2))
+                ),
+                Arguments.of(
+                        new Position(6, 2),
+                        new Position(3, 2),
+                        List.of(new Position(5, 2), new Position(4, 2))
+                ),
+                Arguments.of(
+                        new Position(3, 2),
+                        new Position(3, 5),
+                        List.of(new Position(3, 3), new Position(3, 4))
+                ),
+                Arguments.of(
+                        new Position(3, 8),
+                        new Position(3, 5),
+                        List.of(new Position(3, 7), new Position(3, 6))
+                )
+        );
     }
 
     @DisplayName("포로 이동할 수 없는 위치인 경우 예외를 발생시킨다")
     @Test
-    void test3() {
+    void test2() {
+        //given
         Cannon cannon = new Cannon(Team.RED);
 
+        // when & then
         assertThatThrownBy(() -> cannon.calculatePath(new Position(1, 1), new Position(2, 2)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이 위치로는 움직일 수 없습니다.");
     }
+
 }
