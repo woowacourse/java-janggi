0423575d160dc7b870a23233c4931e29fe7a1b0c
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-03-26 15:20:41 +0900

    refactor: 게임 진행 상태를 나타낼 State 인터페이스 추가

---
+++ src/main/java/janggiGame/state/State.java
@@ -0,0 +1,18 @@
+package janggiGame.state;
+
+import janggiGame.arrangement.ArrangementStrategy;
+import janggiGame.board.Dot;
+import janggiGame.piece.Dynasty;
+import janggiGame.piece.Piece;
+import java.util.Map;
+
+public interface State {
+    State arrangePieces(ArrangementStrategy hanStrategy, ArrangementStrategy choStrategy);
+    State takeTurn(Dot origin, Dot destination);
+    State skipTurn();
+    boolean isFinished();
+    GameResult getGameResult();
+    GameScore getGameScore();
+    Map<Dot, Piece> getPieces();
+    Dynasty getCurrentDynasty();
+}
