862ea2106c99711789acbea11d060e6ef18def4d
GiSeok Kang <ime.rokz@gmail.com>
2025-03-26 15:47:28 +0900

    refactor: Elephant 움직임 로직 변경

--- src/main/java/direction/Movement.java
+++ src/main/java/direction/Movement.java
@@ -12,7 +12,15 @@ public enum Movement {
     DOWN_DOWN_LEFT(-1, 2),
     DOWN_DOWN_RIGHT(1, 2),
     LEFT_LEFT_UP(-2, -1),
-    LEFT_LEFT_DOWN(-2, 1)
+    LEFT_LEFT_DOWN(-2, 1),
+    LEFT_LEFT_LEFT_UP_UP(-3, -2),
+    LEFT_LEFT_LEFT_DOWN_DOWN(-3, 2),
+    DOWN_DOWN_DOWN_LEFT_LEFT(-2, 3),
+    DOWN_DOWN_DOWN_RIGHT_RIGHT(2, 3),
+    RIGHT_RIGHT_RIGHT_UP_UP(3, -2),
+    RIGHT_RIGHT_RIGHT_DOWN_DOWN(3, 2),
+    UP_UP_UP_LEFT_LEFT(-2, -3),
+    UP_UP_UP_RIGHT_RIGHT(2, -3)
     ;
 
     private final int column;
---
+++ src/main/java/piece/Elephant.java
@@ -0,0 +1,59 @@
+package piece;
+
+import direction.Movement;
+import direction.Point;
+import java.util.List;
+import java.util.Map;
+
+public class Elephant implements Piece {
+
+    private static final Map<Movement, List<Movement>> MOVEMENT_PATH = Map.of(
+            Movement.LEFT_LEFT_LEFT_UP_UP, List.of(Movement.LEFT, Movement.LEFT_LEFT_UP),
+            Movement.LEFT_LEFT_LEFT_DOWN_DOWN, List.of(Movement.LEFT, Movement.LEFT_LEFT_DOWN),
+            Movement.DOWN_DOWN_DOWN_LEFT_LEFT, List.of(Movement.DOWN, Movement.DOWN_DOWN_LEFT),
+            Movement.DOWN_DOWN_DOWN_RIGHT_RIGHT, List.of(Movement.DOWN, Movement.DOWN_DOWN_RIGHT),
+            Movement.RIGHT_RIGHT_RIGHT_UP_UP, List.of(Movement.RIGHT, Movement.RIGHT_RIGHT_UP),
+            Movement.RIGHT_RIGHT_RIGHT_DOWN_DOWN, List.of(Movement.RIGHT, Movement.RIGHT_RIGHT_DOWN),
+            Movement.UP_UP_UP_LEFT_LEFT, List.of(Movement.UP, Movement.UP_UP_LEFT),
+            Movement.UP_UP_UP_RIGHT_RIGHT, List.of(Movement.UP, Movement.UP_UP_RIGHT)
+    );
+
+    private final String nickname;
+    private Point current;
+
+    public Elephant(String nickname, Point current) {
+        this.nickname = nickname;
+        this.current = current;
+    }
+
+    @Override
+    public void move(Pieces pieces, Point destination) {
+        Movement destinationMovement = getDestinationMovement(destination);
+        List<Movement> movements = MOVEMENT_PATH.get(destinationMovement);
+
+        Point nextPoint = new Point(current.column(), current.row());
+        for (Movement pathMovement : movements) {
+            nextPoint = nextPoint.move(pathMovement);
+            validateIsExistPieceInPoint(pieces, nextPoint);
+        }
+
+        current = new Point(destination.column(), destination.row());
+    }
+
+    private Movement getDestinationMovement(Point destination) {
+        for (Movement destinationMovement : MOVEMENT_PATH.keySet()) {
+            Point predictDestination = current.move(destinationMovement);
+            if (predictDestination.equals(destination)) {
+                return destinationMovement;
+            }
+        }
+
+        throw new IllegalArgumentException("[ERROR] 선택할 수 없는 목적지입니다.");
+    }
+
+    private static void validateIsExistPieceInPoint(Pieces pieces, Point nextPoint) {
+        if (pieces.isExistPieceIn(nextPoint)) {
+            throw new IllegalArgumentException("[ERROR] 경로에 기물이 존재합니다.");
+        }
+    }
+}
