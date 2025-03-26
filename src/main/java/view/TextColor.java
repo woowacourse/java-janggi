796323de1a2f0b62735092410ae76ed8960fb0cf
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-26 15:06:55 +0900

    refactor: 유틸 메서드 정의

--- src/main/java/view/TextColor.java
+++ src/main/java/view/TextColor.java
@@ -1,8 +1,18 @@
 package view;
 
+import domain.Team;
+import domain.player.Player;
+
 public class TextColor {
     public static final String red = "\u001B[31m";
     public static final String blue = "\u001B[34m";
     public static final String white = "\u001B[37m";
     public static final String exit = "\u001B[0m";
+
+    public static String specifyTeamColor(Player player) {
+        if (player.getTeam() == Team.BLUE) {
+            return TextColor.blue;
+        }
+        return TextColor.red;
+    }
 }
