4f0e89189f4021ccecc6e8d8a5f9f2c2eb2f49bf
민경윤 <ruddbs3308@naver.com>
2025-03-26 17:02:30 +0900

    feat: Team의 name에 해당하는 Team을 찾는 기능 구현

--- src/main/java/domain/Team.java
+++ src/main/java/domain/Team.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Team {
     GREEN("초나라"),
     RED("한나라");
@@ -16,6 +18,13 @@ public enum Team {
         }
 
         return GREEN;
+    }
+
+    public static Team from(final String name) {
+        return Arrays.stream(Team.values())
+                .filter(team -> team.name().equals(name.toUpperCase()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 팀이 없습니다."));
     }
 
     public String getTitle() {
--- src/test/java/domain/TeamTest.java
+++ src/test/java/domain/TeamTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +20,27 @@ class TeamTest {
                     () -> assertThat(Team.GREEN.opposite()).isEqualTo(Team.RED),
                     () -> assertThat(Team.RED.opposite()).isEqualTo(Team.GREEN)
             );
+        }
+
+        @DisplayName("Team의 name에 해당하는 Team을 반환한다.")
+        @Test
+        void from() {
+            assertAll(
+                    () -> assertThat(Team.from("green")).isEqualTo(Team.GREEN),
+                    () -> assertThat(Team.from("red")).isEqualTo(Team.RED)
+            );
+        }
+    }
+
+    @Nested
+    class InvalidCases {
+
+        @DisplayName("Team의 name에 해당하는 Team이 없다면 예외가 발생한다.")
+        @Test
+        void from() {
+            assertThatThrownBy(() -> Team.from("yello"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("해당하는 팀이 없습니다.");
         }
     }
 }
