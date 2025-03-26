b82f5420f63b8f874ab6836e4707dbe61b6b5327
dltmddus <tmddus4671@gmail.com>
2025-03-26 13:48:50 +0900

    refactor: 메서드 네이밍 isCorrectPosition -> canReachTargetPosition 수정

--- src/main/java/domain/piece/state/NonContinuousPiece.java
+++ src/main/java/domain/piece/state/NonContinuousPiece.java
@@ -10,14 +10,14 @@ import java.util.Map.Entry;
 public abstract class NonContinuousPiece extends Moved {
     public List<Pattern> findMovablePath(Path path, JanggiPosition beforePosition, JanggiPosition afterPosition) {
         return path.getPaths().entrySet().stream()
-                .filter(entry -> isCorrectPosition(beforePosition, afterPosition, entry))
+                .filter(entry -> canReachTargetPosition(beforePosition, afterPosition, entry))
                 .findFirst()
                 .map(Entry::getValue)
                 .orElseThrow(() -> new IllegalArgumentException("해당 말은 해당 경로로 이동할 수 없습니다."));
     }
 
-    private static boolean isCorrectPosition(JanggiPosition beforePosition, JanggiPosition afterPosition,
-                                             Entry<Direction, List<Pattern>> entry) {
+    private static boolean canReachTargetPosition(JanggiPosition beforePosition, JanggiPosition afterPosition,
+                                                  Entry<Direction, List<Pattern>> entry) {
         List<Pattern> patterns = entry.getValue();
         JanggiPosition newPosition = beforePosition.move(patterns);
         return newPosition.equals(afterPosition);
