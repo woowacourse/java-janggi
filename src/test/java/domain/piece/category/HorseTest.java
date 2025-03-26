841afea3f235027e16715054c15ff6e3c1aac4da
taek2222 <oot309@daum.net>
2025-03-26 18:51:39 +0900

    test: 각 기물의 움직임 검증 테스트 작성

---
+++ src/test/java/domain/piece/category/HorseTest.java
@@ -0,0 +1,27 @@
+package domain.piece.category;
+
+import domain.MoveInfo;
+import domain.MoveInfos;
+import domain.direction.PieceDirection;
+import domain.piece.Piece;
+import domain.spatial.Position;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class HorseTest {
+
+    @Test
+    void 마는_경로에_기물이_있는_경우_예외가_발생한다() {
+        // given
+        MoveInfos moveInfos = new MoveInfos(List.of(new MoveInfo(PieceCategory.GUARD)));
+
+        Piece piece = new Horse(new Position(1, 2), PieceDirection.HORSE.get());
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.validateMove(moveInfos))
+                .withMessage("[ERROR] 마는 중간에 기물이 0개여야 합니다.");
+    }
+}
---
+++ src/test/java/domain/piece/category/KingTest.java
@@ -0,0 +1,27 @@
+package domain.piece.category;
+
+import domain.MoveInfo;
+import domain.MoveInfos;
+import domain.direction.PieceDirection;
+import domain.piece.Piece;
+import domain.spatial.Position;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class KingTest {
+
+    @Test
+    void 왕은_경로에_기물이_있는_경우_예외가_발생한다() {
+        // given
+        MoveInfos moveInfos = new MoveInfos(List.of(new MoveInfo(PieceCategory.GUARD)));
+
+        Piece piece = new King(new Position(1, 2), PieceDirection.KING.get());
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.validateMove(moveInfos))
+                .withMessage("[ERROR] 왕은 중간에 기물이 0개여야 합니다.");
+    }
+}
