af54b950c804ec6e059325b3c9a6296bf994913c
eoehd1ek <eoehd2ek@gmail.com>
2025-03-26 17:15:16 +0900

    fix: 변경된 장기판 좌표에 맞게 방향값 수정

--- src/main/java/janggi/position/Direction.java
+++ src/main/java/janggi/position/Direction.java
@@ -1,8 +1,8 @@
 package janggi.position;
 
 public record Direction(int x, int y) {
-    public static final Direction UP = new Direction(0, 1);
-    public static final Direction DOWN = new Direction(0, -1);
-    public static final Direction LEFT = new Direction(-1, 0);
-    public static final Direction RIGHT = new Direction(1, 0);
+    public static final Direction UP = new Direction(0, -1);
+    public static final Direction DOWN = new Direction(0, 1);
+    public static final Direction LEFT = new Direction(1, 0);
+    public static final Direction RIGHT = new Direction(-1, 0);
 }
