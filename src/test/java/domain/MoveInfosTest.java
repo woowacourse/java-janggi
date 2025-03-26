867cb5ca6e61fb95581e98ce631f2a5955f1367b
taek2222 <oot309@daum.net>
2025-03-26 17:24:11 +0900

    test: 경로에 기물의 갯수 반환 테스트로 변경

--- src/test/java/domain/MoveInfosTest.java
+++ src/test/java/domain/MoveInfosTest.java
@@ -15,49 +15,49 @@ class MoveInfosTest {
 
     @ParameterizedTest
     @MethodSource
-    void 경로에_배치된_기물이_없는지_판단한다(PieceCategory pathPieceCategory, boolean excepted) {
+    void 경로에_기물의_갯수를_반환한다(PieceCategory pathPieceCategory, int excepted) {
         // given
         List<MoveInfo> moveInfoElements = new ArrayList<>();
-        moveInfos.add(new MoveInfo(new Position(1, 2), pathPieceCategory))
+        moveInfoElements.add(new MoveInfo(new Position(1, 2), pathPieceCategory));
 
         MoveInfos moveInfos = new MoveInfos(moveInfoElements);
 
         // when
-        boolean result = moveInfos.isPathInPiece();
+        int result = moveInfos.countPiecesInPath();
 
         // then
         assertThat(result).isEqualTo(excepted);
     }
 
-    private static Stream<Arguments> 경로에_배치된_기물이_없는지_판단한다() {
+    private static Stream<Arguments> 경로에_기물의_갯수를_반환한다() {
         return Stream.of(
-                Arguments.of(PieceCategory.NONE, true),
-                Arguments.of(PieceCategory.CANNON, false)
+                Arguments.of(PieceCategory.NONE, 0),
+                Arguments.of(PieceCategory.CANNON, 1)
         );
     }
 
-    @ParameterizedTest
-    @MethodSource
-    void 이동_기물과_도착지_기물이_같은지_판단한다(PieceCategory startPiece, PieceCategory targetPiece, boolean excepted) {
-        // given
-        List<MoveInfo> moveInfoElements = new ArrayList<>();
-        moveInfoElements.add(new MoveInfo(new Position(1, 2), PieceCategory.NONE));
-        moveInfoElements.add(new MoveInfo(new Position(2, 2), targetPiece));
-
-        MoveInfos moveInfos = new MoveInfos(pathPieces);
-
-        // when
-        boolean result = moveInfos.isSameTargetPiece(startPiece);
-
-        // then
-        assertThat(result).isEqualTo(excepted);
-    }
-
-    private static Stream<Arguments> 이동_기물과_도착지_기물이_같은지_판단한다() {
-        return Stream.of(
-                Arguments.of(PieceCategory.KING, PieceCategory.KING, true),
-                Arguments.of(PieceCategory.KING, PieceCategory.SOLDIER, false)
-
-        );
-    }
+//    @ParameterizedTest
+//    @MethodSource
+//    void 이동_기물과_도착지_기물이_같은지_판단한다(PieceCategory startPiece, PieceCategory targetPiece, boolean excepted) {
+//        // given
+//        List<MoveInfo> moveInfoElements = new ArrayList<>();
+//        moveInfoElements.add(new MoveInfo(new Position(1, 2), PieceCategory.NONE));
+//        moveInfoElements.add(new MoveInfo(new Position(2, 2), targetPiece));
+//
+//        MoveInfos moveInfos = new MoveInfos(pathPieces);
+//
+//        // when
+//        boolean result = moveInfos.isSameTargetPiece(startPiece);
+//
+//        // then
+//        assertThat(result).isEqualTo(excepted);
+//    }
+//
+//    private static Stream<Arguments> 이동_기물과_도착지_기물이_같은지_판단한다() {
+//        return Stream.of(
+//                Arguments.of(PieceCategory.KING, PieceCategory.KING, true),
+//                Arguments.of(PieceCategory.KING, PieceCategory.SOLDIER, false)
+//
+//        );
+//    }
 }
