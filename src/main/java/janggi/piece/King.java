2ea1e8303e844ee9319636770aacaa2ad87745d2
sung-june27 <kmymy27@gmail.com>
2025-03-26 16:34:04 +0900

    feat(King): 궁성 내 움직임 구현

--- src/main/java/janggi/piece/King.java
+++ src/main/java/janggi/piece/King.java
@@ -10,8 +10,8 @@ public class King extends Piece {
     }
 
     @Override
-    public boolean canMove(Position start, Position end, Map<Position, Piece> board) {
-        return isValidMovingRule(start, end);
+    public boolean canMove(final Position start, final Position end, final Map<Position, Piece> board) {
+        return isValidMovingRule(start, end) && start.isOneStep(end) && start.isMoveInPalace(end);
     }
 
     @Override
@@ -19,9 +19,7 @@ public class King extends Piece {
         return false;
     }
 
-    private boolean isValidMovingRule(Position start, Position end) {
-        int absDeltaX = start.absDeltaX(end);
-        int absDeltaY = start.absDeltaY(end);
-        return (absDeltaX == 1 && absDeltaY == 0) || (absDeltaX == 0 && absDeltaY == 1);
+    private boolean isValidMovingRule(final Position start, final Position end) {
+        return start.isVerticalMove(end) || start.isHorizontalMove(end) || start.isMoveDiagonalInPalace(end);
     }
 }
