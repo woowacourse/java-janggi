e353344d1fde9e880ff427df7929c2bdf147fb7a
KimDaehyeon <daehyeon3351@gmail.com>
2025-03-26 16:22:04 +0900

    feat: Pieces 에 접근만 가능한 View 인터페이스 추가

---
+++ src/main/java/janggi/domain/piece/pieces/PiecesView.java
@@ -0,0 +1,32 @@
+package janggi.domain.piece.pieces;
+
+import janggi.domain.piece.PieceType;
+import janggi.domain.piece.PieceView;
+import janggi.domain.piece.Position;
+import janggi.domain.piece.Side;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface PiecesView {
+
+    boolean isEnemyOnDestination(Side side, Position destination);
+
+    PiecesView getPiecesOnPath(Collection<Position> pathsToDestination);
+
+    boolean hasOnlyOnePiece();
+
+    PiecesView getMapWithoutPosition(int x, int y);
+
+    Optional<? extends PieceView> findByPosition(Position position);
+
+    int size();
+
+    boolean isEmpty();
+
+    int countByPieceType(PieceType pieceType);
+
+    List<Side> findAllByPieceType(PieceType pieceType);
+
+    boolean containsPieceType(PieceType pieceType);
+}
