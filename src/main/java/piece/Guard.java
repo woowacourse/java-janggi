c81472c187398f4998f34079e78282dfafb0fa07
GiSeok Kang <ime.rokz@gmail.com>
2025-03-26 16:02:42 +0900

    refactor: Guard 움직임 로직 변경

---
+++ src/main/java/piece/Guard.java
@@ -0,0 +1,50 @@
+package piece;
+
+import direction.Movement;
+import direction.Point;
+import java.util.List;
+import move.MovementRule;
+
+public class Guard implements Piece {
+
+    private static final List<Movement> PATH = List.of(Movement.LEFT, Movement.RIGHT, Movement.UP, Movement.DOWN);
+    public static final int MAXIMUM_MOVE_RANGE = 1;
+
+    private final String nickname;
+    private Point current;
+
+    public Guard(String nickname, Point current) {
+        this.nickname = nickname;
+        this.current = current;
+    }
+
+    @Override
+    public void move(Pieces pieces, Point destination) {
+        Movement destinationMovement = getDestinationMovement(destination);
+        validateIsExistPieceInPoint(pieces, current.move(destinationMovement));
+
+        current = current.move(destinationMovement);
+    }
+
+    @Override
+    public boolean isSamePoint(Point point) {
+        return false;
+    }
+
+    private Movement getDestinationMovement(Point destination) {
+        for (Movement destinationMovement : PATH) {
+            Point predictDestination = current.move(destinationMovement);
+            if (predictDestination.equals(destination)) {
+                return destinationMovement;
+            }
+        }
+
+        throw new IllegalArgumentException("[ERROR] 선택할 수 없는 목적지입니다.");
+    }
+
+    private static void validateIsExistPieceInPoint(Pieces pieces, Point nextPoint) {
+        if (pieces.isExistPieceIn(nextPoint)) {
+            throw new IllegalArgumentException("[ERROR] 경로에 기물이 존재합니다.");
+        }
+    }
+}
