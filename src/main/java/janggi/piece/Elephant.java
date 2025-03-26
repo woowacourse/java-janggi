c11f5359515a651b895f213448ae44643791ad00
BWY <quantumjump10@gmail.com>
2025-03-26 22:38:41 +0900

    refactor: 상의 이동 규칙 검증 추가

--- src/main/java/janggi/piece/Elephant.java
+++ src/main/java/janggi/piece/Elephant.java
@@ -1,10 +1,32 @@
 package janggi.piece;
 
+import janggi.board.Position;
+
+import java.util.Map;
+
 public class Elephant extends Piece {
     private static final PieceType TYPE = PieceType.ELEPHANT;
+    private static final int ELEPHANT_MOVE_LONG = 3;
+    private static final int ELEPHANT_MOVE_SHORT = 2;
 
     public Elephant(Team team) {
         super(team);
+    }
+
+    @Override
+    public void validateMovable(Map<Position, Piece> board, Position start, Position goal) {
+        validateElephantMove(start, goal);
+        Piece attacker = board.get(start);
+        validatePath(board, start, goal);
+        validateNonOurArmyAtGoal(board, goal, attacker.getTeam());
+    }
+
+    private void validateElephantMove(Position start, Position goal) {
+        int dColumn = Math.abs(start.calculatesColumnDifference(goal));
+        int dRow = Math.abs(start.calculatesRowDifference(goal));
+        if ((dColumn != ELEPHANT_MOVE_SHORT || dRow != ELEPHANT_MOVE_LONG) && (dColumn != ELEPHANT_MOVE_LONG || dRow != ELEPHANT_MOVE_SHORT)) {
+            throw new IllegalArgumentException("[ERROR] 상의 이동 규칙에 어긋나는 움직임입니다.");
+        }
     }
 
     @Override
