ea79f9163f7896ca1786c9da5f8836b20feb9661
(minjae8563) <(minjae8563@naver.com)>
2025-03-26 15:52:06 +0900

    test: 경로를 바탕으로 새로운 Position 반환 테스트 추가

--- src/test/java/piece/PositionTest.java
+++ src/test/java/piece/PositionTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import pieceProperty.Movement;
 import pieceProperty.Position;
 
 class PositionTest {
@@ -64,6 +65,318 @@ class PositionTest {
 
         assertThat(position.calculateDRow(position1)).isEqualTo(1);
         assertThat(position.calculateDCol(position1)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("위 움직임 계산 테스트")
+    void calculateUpMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(4, 5));
+    }
+
+    @Test
+    @DisplayName("아래 움직임 계산 테스트")
+    void calculateDownMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(6, 5));
+    }
+
+    @Test
+    @DisplayName("왼쪽 움직임 계산 테스트")
+    void calculateLeftMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateLeftMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(5, 4));
+    }
+
+    @Test
+    @DisplayName("오른쪽 움직임 계산 테스트")
+    void calculateRightMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateRightMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(5, 6));
+    }
+
+    @Test
+    @DisplayName("오른쪽 위 대각선 움직임 계산 테스트")
+    void upCalculateRightMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateRightUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(4, 6));
+    }
+
+    @Test
+    @DisplayName("왼쪽 위 대각선 움직임 계산 테스트")
+    void upCalculateLeftMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateLeftUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(4, 4));
+    }
+
+    @Test
+    @DisplayName("오른쪽 아래 대각선 움직임 계산 테스트")
+    void downCalculateRightMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateRightDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(6, 6));
+    }
+
+    @Test
+    @DisplayName("왼쪽 아래 대각선 움직임 계산 테스트")
+    void downCalculateLeftMovementTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateLeftDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(6, 4));
+    }
+
+    @Test
+    @DisplayName("위_오른쪽 대각선 움직임 계산 테스트")
+    void upRightUpTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateUpRightUPMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(3, 6));
+    }
+
+    @Test
+    @DisplayName("위_왼쪽 위 대각선 움직임 계산 테스트")
+    void upLeftUpTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateUpLeftUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(3, 4));
+    }
+
+    @Test
+    @DisplayName("오른쪽_오른쪽 위 대각선 움직임 계산 테스트")
+    void rightRightUpTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateRightRightUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(4, 7));
+    }
+
+    @Test
+    @DisplayName("오른쪽_오른쪽 아래 대각선 움직임 계산 테스트")
+    void rightRightDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateRightRightDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(6, 7));
+    }
+
+    @Test
+    @DisplayName("아래_오른쪽 아래 대각선 움직임 계산 테스트")
+    void downRightDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateDownRightDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(7, 6));
+    }
+
+    @Test
+    @DisplayName("아래_왼쪽 아래 대각선 움직임 계산 테스트")
+    void downLeftDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateDownLeftDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(7, 4));
+    }
+
+    @Test
+    @DisplayName("왼쪽_왼쪽 위 대각선 움직임 계산 테스트")
+    void leftLeftUpTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateLeftLeftUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(4, 3));
+    }
+
+    @Test
+    @DisplayName("왼쪽_왼쪽 아래 대각선 움직임 계산 테스트")
+    void leftLeftDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateLeftLeftDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(6, 3));
+    }
+
+    @Test
+    @DisplayName("위_오른쪽 대각선_오른쪽 대각선 움직인 계산 테스트")
+    void upRightUpRightUpTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateUpRightUpRightUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(2, 7));
+    }
+
+    @Test
+    @DisplayName("위_왼쪽 대각선_왼쪽 대각선 움직인 계산 테스트")
+    void upLeftUpLeftUpTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateUpLeftUpLeftUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(2, 3));
+    }
+
+    @Test
+    @DisplayName("오른쪽_오른쪽 대각선_오른쪽 대각선 움직임 계산 테스트")
+    void rightRightUpRightUpTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateRightRightUpRightUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(3, 8));
+    }
+
+    @Test
+    @DisplayName("오른쪽_오른쪽 아래 대각선_오른쪽 아래 대각선 움직임 계산 테스트")
+    void rightRightDownRightDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateRightRightDownRightDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(7, 8));
+    }
+
+    @Test
+    @DisplayName("아래_오른쪽 아래 대각선_오른쪽 아래 대각선 움직임 계산 테스트")
+    void downRightDownRightDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateDownRightDownRightDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(8, 7));
+    }
+
+    @Test
+    @DisplayName("아래_왼쪽 아래 대각선_왼쪽 아래 대각선 움직임 계산 테스트")
+    void downLeftDownLeftDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateDownLeftDownLeftDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(8, 3));
+    }
+
+    @Test
+    @DisplayName("왼쪽_왼쪽 위 대각선_왼쪽 위 대각선 움직임 계산 테스트")
+    void leftLeftUpLeftUPTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateLeftLeftUpLeftUpMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(3, 2));
+    }
+
+    @Test
+    @DisplayName("왼쪽_왼쪽 아래 대각선_왼쪽 아래 대각선 움직임 계산 테스트")
+    void leftLeftDownLeftDownTest() {
+        // given
+        Position present = new Position(5, 5);
+
+        //when
+        Position future = present.calculateLeftLeftDownLeftDownMovement();
+
+        //then
+        assertThat(future).isEqualTo(new Position(7, 2));
     }
 
 }
