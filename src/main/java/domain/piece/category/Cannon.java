6ec10a42a5ee38e6e138cfd75b4b8ff02cf7f914
taek2222 <oot309@daum.net>
2025-03-26 19:01:31 +0900

    feat: 포의 이동 경로 검증하는 기능 추가

--- src/main/java/domain/piece/category/Cannon.java
+++ src/main/java/domain/piece/category/Cannon.java
@@ -1,11 +1,13 @@
 package domain.piece.category;
 
+import domain.MoveInfos;
 import domain.direction.Directions;
 import domain.piece.Piece;
 import domain.spatial.Position;
 
 public class Cannon extends Piece {
 
+    private static final PieceCategory CATEGORY = PieceCategory.CANNON;
     private static final int PIECES_TO_PASS = 1;
 
     public Cannon(final Position position, final Directions directions) {
@@ -25,6 +27,21 @@ public class Cannon extends Piece {
     @Override
     public boolean isCannon() {
         return true;
+    }
+
+    @Override
+    public void validateMove(final MoveInfos moveInfos) {
+        if (moveInfos.countPiecesInPath() != PIECES_TO_PASS) {
+            throw new IllegalArgumentException("[ERROR] 포는 중간에 기물이 " + PIECES_TO_PASS + "개여야 합니다.");
+        }
+
+        if (moveInfos.isSameAsTargetPiece(CATEGORY)) {
+            throw new IllegalArgumentException("[ERROR] 포는 상대 포를 잡을 수 없습니다.");
+        }
+
+        if (moveInfos.hasSamePieceCategoryInPath(CATEGORY)) {
+            throw new IllegalArgumentException("[ERROR] 포는 다른 포를 지나칠 수 없습니다.");
+        }
     }
 
     @Override
