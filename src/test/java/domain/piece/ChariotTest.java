290a32f3b146ff8cbb8729b326694dc7a2ee5c21
ryan-dia <cjfdnjs158@naver.com>
2025-03-26 21:24:03 +0900

    test: 차 궁성 내 이동 관련 테스트 추가
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/test/java/domain/piece/ChariotTest.java
+++ src/test/java/domain/piece/ChariotTest.java
@@ -2,7 +2,6 @@ package domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import domain.position.Distance;
 import domain.position.Point;
 import java.util.List;
 import org.assertj.core.api.SoftAssertions;
@@ -14,28 +13,31 @@ class ChariotTest {
 
     @ParameterizedTest
     @CsvSource({
-            "0,1,true", "1,0,true", "-1,0,true", "0,-1,true", "0,0,false", "9,9,false",
+            "1,1,1,2,true", "1,1,2,1,true", "1,1,0,1,true", "1,1,1,0,true", "1,1,1,1,false", "1,1,8,8,false",
     })
-    void 말이_움직일_수_있으면_true_아니면_false를_반환한다(final int x, final int y, boolean expected) {
+    void 말이_움직일_수_있으면_true_아니면_false를_반환한다(final int x1, final int y1, final int x2, final int y2,
+                                           final boolean expected) {
 
         // given
-        Chariot chariot = PieceFactory.createRedTeam(Chariot::new);
+        final Chariot chariot = PieceFactory.createRedTeam(Chariot::new);
 
         // when
-        Distance distance = new Distance(x, y);
+        final Point point1 = Point.newInstance(x1, y1);
+        final Point point2 = Point.newInstance(x2, y2);
 
         // then
-        assertThat(chariot.isMovable(distance)).isEqualTo(expected);
+        assertThat(chariot.isMovable(point1, point2)).isEqualTo(expected);
     }
 
     @Test
     void 차의_이동_가능_경로_모두_반환() {
 
         // given
-        Chariot chariot = PieceFactory.createGreenTeam(Chariot::new);
+        final Chariot chariot = PieceFactory.createGreenTeam(Chariot::new);
 
         // when
-        List<Point> possiblePoint = chariot.calculatePossiblePoint(Point.newInstance(0, 0), Point.newInstance(0, 9));
+        final List<Point> possiblePoint = chariot.calculatePossiblePoint(Point.newInstance(0, 0),
+                Point.newInstance(0, 9));
 
         // then
         SoftAssertions.assertSoftly(softly -> {
@@ -44,4 +46,39 @@ class ChariotTest {
                     .contains(Point.newInstance(0, 1), Point.newInstance(0, 5), Point.newInstance(0, 8));
         });
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,0,4,1", "3,0,5,2", "4,1,3,0", "4,1,5,2"
+    })
+    void 차의_궁성_내_이동_가능하면_true(final int x1, final int y1, final int x2, final int y2) {
+
+        // given
+        final Chariot chariot = PieceFactory.createGreenTeam(Chariot::new);
+
+        // when
+        final Point firstPoint = Point.newInstance(x1, y1);
+        final Point secondPoint = Point.newInstance(x2, y2);
+
+        // then
+        assertThat(chariot.isMovable(firstPoint, secondPoint)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,0,6,3", "4,1,6,3", "3,2,1,4", "4,1,2,3"
+    })
+    void 차의_궁성_내_이동_불가능하면_false(final int x1, final int y1, final int x2, final int y2) {
+
+        // given
+        final Chariot chariot = PieceFactory.createGreenTeam(Chariot::new);
+
+        // when
+        final Point firstPoint = Point.newInstance(x1, y1);
+        final Point secondPoint = Point.newInstance(x2, y2);
+
+        // then
+        assertThat(chariot.isMovable(firstPoint, secondPoint)).isFalse();
+    }
+
 }
