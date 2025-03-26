b511ffea683eaf34de0864288a062ffe1193d5e3
민경윤 <ruddbs3308@naver.com>
2025-03-26 16:30:57 +0900

    feat: 데이터베이스 설정

---
+++ src/main/java/dao/DatabaseConfig.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class DatabaseConfig {
+
+    public static final String SERVER = "localhost:13306";
+    public static final String DATABASE = "janggi";
+    public static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+}
