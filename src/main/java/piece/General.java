030383c23d400e923bfd8e52d8a4f84b10e35380
byesol <bsj5967@naver.com>
2025-03-27 21:16:45 +0900

    feat: 장 움직임 중간 블록 체크 기능 작성

--- src/main/java/piece/General.java
+++ src/main/java/piece/General.java
@@ -1,5 +1,6 @@
 package piece;
 
+import game.Board;
 import java.util.List;
 import java.util.Set;
 import position.Movement;
@@ -22,6 +23,14 @@ public class General extends Piece {
                 .filter(path -> !path.isEmpty() && path.getLast().equals(toPosition))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("해당 위치로 이동할 수 없습니다."));
+    }
+
+    @Override
+    public void validateRoute(final List<Position> positions, Board board) {
+        if (positions.subList(0, positions.size() - 1).stream()
+                .anyMatch(position -> board.getBoard().containsKey(position))) {
+            throw new IllegalArgumentException("중간에 기물이 있어 갈 수 없습니다.");
+        }
     }
 
 }
--- src/test/java/piece/GeneralTest.java
+++ src/test/java/piece/GeneralTest.java
@@ -6,9 +6,15 @@ import static testutil.StaticTest.A1;
 import static testutil.StaticTest.A2;
 import static testutil.StaticTest.A3;
 import static testutil.StaticTest.B1;
-import static testutil.StaticTest.C1;
 import static testutil.StaticTest.D1;
+import static testutil.StaticTest.D5;
+import static testutil.StaticTest.E4;
+import static testutil.StaticTest.E5;
+import static testutil.StaticTest.E6;
+import static testutil.StaticTest.F5;
 
+import game.Board;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class GeneralTest {
@@ -17,37 +23,34 @@ public class GeneralTest {
     void 장군은_위_아래_왼쪽_오른쪽_으로_움직일_수_있다() {
         // given
         General general = new General(Country.Cho);
+        Board board = new Board(Map.of());
 
         // then
-        assertThatCode(() -> general.getPathForMoving(A1, A2))
-                .doesNotThrowAnyException();
-
-        assertThatCode(() -> general.getPathForMoving(A2, A1))
-                .doesNotThrowAnyException();
-
-        assertThatCode(() -> general.getPathForMoving(B1, A1))
-                .doesNotThrowAnyException();
-
-        assertThatCode(() -> general.getPathForMoving(B1, C1))
-                .doesNotThrowAnyException();
+        assertThatCode(() -> general.canMove(E5, E4, board)).doesNotThrowAnyException();
+        assertThatCode(() -> general.canMove(E5, E6, board)).doesNotThrowAnyException();
+        assertThatCode(() -> general.canMove(E5, D5, board)).doesNotThrowAnyException();
+        assertThatCode(() -> general.canMove(E5, F5, board)).doesNotThrowAnyException();
     }
 
     @Test
     void 장군은_위_아래_왼쪽_오른쪽_제외하고_움직일_수_없다() {
+        Board board = new Board(Map.of());
+
         // given
         General general = new General(Country.Cho);
 
         // then
-        assertThatThrownBy(() -> general.getPathForMoving(A1, A3))
+        assertThatThrownBy(() -> general.canMove(A1, A3, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
 
-        assertThatThrownBy(() -> general.getPathForMoving(A2, B1))
+        assertThatThrownBy(() -> general.canMove(A2, B1, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
 
-        assertThatThrownBy(() -> general.getPathForMoving(B1, D1))
+        assertThatThrownBy(() -> general.canMove(B1, D1, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
     }
+
 }
