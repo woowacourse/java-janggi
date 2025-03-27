e74bee910c330b66e16a08d60a1e0deb6ef0fdbe
eoehd1ek <eoehd2ek@gmail.com>
2025-03-27 11:43:11 +0900

    refector(Guard, King): 궁전 기물의 중복 코드 제거

--- src/main/java/janggi/piece/Guard.java
+++ src/main/java/janggi/piece/Guard.java
@@ -1,27 +1,22 @@
 package janggi.piece;
 
+import janggi.piece.movement.MovementRule;
+import janggi.piece.movement.PalaceMovementRule;
 import janggi.position.Position;
 import java.util.List;
 
 public class Guard extends Piece {
 
+    private final MovementRule movementRule;
+
     public Guard(final Color color) {
         super(color);
+        this.movementRule = new PalaceMovementRule();
     }
 
     @Override
     public List<Position> calculatePath(final Position start, final Position end) {
-        validateMovingRule(start, end);
-        return List.of();
-    }
-
-    private void validateMovingRule(final Position start, final Position end) {
-        int absDifferenceX = start.calculateAbsoluteDifferenceX(end);
-        int absDifferenceY = start.calculateAbsoluteDifferenceY(end);
-        if ((absDifferenceX == 1 && absDifferenceY == 0)
-                || (absDifferenceX == 0 && absDifferenceY == 1)) {
-            return;
-        }
-        throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
+        movementRule.validateMovementRule(start, end);
+        return movementRule.calculatePath(start, end);
     }
 }
