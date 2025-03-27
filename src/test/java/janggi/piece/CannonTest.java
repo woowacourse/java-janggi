9d15f32467858ba646b4c67bcdf54b0f1589221d
eoehd1ek <eoehd2ek@gmail.com>
2025-03-27 19:00:37 +0900

    refector(CannonTest): 부정확한 네이밍 수정

--- src/test/java/janggi/piece/CannonTest.java
+++ src/test/java/janggi/piece/CannonTest.java
@@ -15,9 +15,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 class CannonTest {
 
     @ParameterizedTest
-    @DisplayName("시작점과 끝점이 주어졌을 때, 이동 경로를 반환한다.")
+    @DisplayName("시작점과 끝점이 주어졌을 때, 포의 이동 경로를 반환한다.")
     @MethodSource("pathArguments")
-    void shouldReturnFalseWhenRouteExists(Position end, List<Position> expectedPath) {
+    void should_return_path_by_start_and_end_position(Position end, List<Position> expectedPath) {
         // given
         Cannon cannon = new Cannon(Color.RED);
         Position start = new Position(5, 5);
@@ -29,15 +29,52 @@ class CannonTest {
         assertThat(path).containsExactlyElementsOf(expectedPath);
     }
 
+    private static Stream<Arguments> pathArguments() {
+        return Stream.of(
+                // 상상상
+                Arguments.of(
+                        new Position(5, 2),
+                        List.of(
+                                new Position(5, 4),
+                                new Position(5, 3)
+                        )
+                ),
+                // 하하하
+                Arguments.of(
+                        new Position(5, 8),
+                        List.of(
+                                new Position(5, 6),
+                                new Position(5, 7)
+                        )
+                ),
+                // 좌좌좌
+                Arguments.of(
+                        new Position(2, 5),
+                        List.of(
+                                new Position(4, 5),
+                                new Position(3, 5)
+                        )
+                ),
+                // 우우우
+                Arguments.of(
+                        new Position(8, 5),
+                        List.of(
+                                new Position(6, 5),
+                                new Position(7, 5)
+                        )
+                )
+        );
+    }
+
     @ParameterizedTest
-    @DisplayName("차의 이동 규칙이 어긋나면 예외를 발생한다.")
+    @DisplayName("도착점이 차의 이동 규칙에 어긋나면 예외가 발생한다")
     @CsvSource(value = {
-            "6, 6",
-            "4, 6",
-            "6, 4",
-            "4, 4"
+            "4, 4",  // 좌상
+            "4, 6",  // 좌하
+            "6, 4",  // 우상
+            "6, 6"   // 우하
     })
-    void shouldReturnTrueWhenUnfollowMovingRule(int destX, int destY) {
+    void should_throw_exception_when_unfollow_cannon_moving_rule(int destX, int destY) {
         // given
         Cannon cannon = new Cannon(Color.RED);
         Position start = new Position(5, 5);
@@ -46,38 +83,5 @@ class CannonTest {
         // when
         assertThatThrownBy(() -> cannon.calculatePath(start, end))
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    private static Stream<Arguments> pathArguments() {
-        return Stream.of(
-                Arguments.of(
-                        new Position(8, 5),
-                        List.of(
-                                new Position(6, 5),
-                                new Position(7, 5)
-                        )
-                ),
-                Arguments.of(
-                        new Position(2, 5),
-                        List.of(
-                                new Position(4, 5),
-                                new Position(3, 5)
-                        )
-                ),
-                Arguments.of(
-                        new Position(5, 8),
-                        List.of(
-                                new Position(5, 6),
-                                new Position(5, 7)
-                        )
-                ),
-                Arguments.of(
-                        new Position(5, 2),
-                        List.of(
-                                new Position(5, 4),
-                                new Position(5, 3)
-                        )
-                )
-        );
     }
 }
