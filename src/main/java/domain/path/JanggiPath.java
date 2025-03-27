a05d35b44f99bc2c8ddba9260e3c64e5dad362c4
minSsan <msp1125@hanyang.ac.kr>
2025-03-27 16:29:57 +0900

    feat(JanggiPath): 장기 경로 equals 재정의

--- src/main/java/domain/path/JanggiPath.java
+++ src/main/java/domain/path/JanggiPath.java
@@ -4,6 +4,7 @@ import domain.position.JanggiPosition;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class JanggiPath {
     private final List<JanggiPosition> path;
@@ -18,5 +19,18 @@ public class JanggiPath {
 
     public List<JanggiPosition> getPath() {
         return Collections.unmodifiableList(path);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        JanggiPath that = (JanggiPath) object;
+        return Objects.equals(path, that.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path);
     }
 }
