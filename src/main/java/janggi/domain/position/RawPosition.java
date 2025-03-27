6d0caad93d773b2c378150ea25d714a375781904
yush <yush2202@gmail.com>
2025-03-27 20:10:59 +0900

    feat: 이동 기능 추가

--- src/main/java/janggi/domain/position/RawPosition.java
+++ src/main/java/janggi/domain/position/RawPosition.java
@@ -2,4 +2,35 @@ package janggi.domain.position;
 
 public record RawPosition(int x, int y) {
 
+    public RawPosition up() {
+        return new RawPosition(x, y + 1);
+    }
+
+    public RawPosition down() {
+        return new RawPosition(x, y - 1);
+    }
+
+    public RawPosition left() {
+        return new RawPosition(x - 1, y);
+    }
+
+    public RawPosition right() {
+        return new RawPosition(x + 1, y);
+    }
+
+    public RawPosition upRightDiagonal() {
+        return new RawPosition(x + 1, y + 1);
+    }
+
+    public RawPosition downRightDiagonal() {
+        return new RawPosition(x + 1, y - 1);
+    }
+
+    public RawPosition upLeftDiagonal() {
+        return new RawPosition(x - 1, y + 1);
+    }
+
+    public RawPosition downLeftDiagonal() {
+        return new RawPosition(x - 1, y - 1);
+    }
 }
