8abcf17cab44a25ddce81fdaa0dbcde947927d53
rladmstn <oz33177351@gmail.com>
2025-03-27 19:47:40 +0900

    feat: 플레이어 점수 정보 추가

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,8 +1,29 @@
 package domain.player;
 
-public record Player(
-        String name,
-        Team team
-) {
+public class Player {
 
+    private final String name;
+    private final Team team;
+    private int score = 0;
+
+    public Player(final String name, final Team team) {
+        this.name = name;
+        this.team = team;
+    }
+
+    public void addScore(final int score) {
+        this.score += score;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Team team() {
+        return team;
+    }
+
+    public int score() {
+        return score;
+    }
 }
