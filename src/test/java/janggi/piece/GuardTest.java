02f2ca84cbb8e9e669b7b41623c8f6eb1262cafd
BWY <quantumjump10@gmail.com>
2025-03-27 13:13:25 +0900

    refactor: 이동 목적지에 같은 진영의 기물이 있을 경우 예외를 발생하는 테스트 추가

--- src/test/java/janggi/piece/GuardTest.java
+++ src/test/java/janggi/piece/GuardTest.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static janggi.fixture.PositionFixture.createPosition;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
 
@@ -33,5 +34,24 @@ class GuardTest {
         assertThat(board).extracting("board")
                 .asInstanceOf(MAP)
                 .containsEntry(goal, piece);
+    }
+
+    @CsvSource(value = {"4:5", "3:4", "5:4", "4:3"}, delimiterString = ":")
+    @ParameterizedTest
+    void 목적지에_같은_진영의_기물이_있을_경우_예외를_발생한다(int column, int row) {
+        // given
+        Map<Position, Piece> initialBoard = new HashMap<>();
+        Position start = createPosition(4, 4);
+        Position goal = createPosition(column, row);
+        Guard piece = new Guard(Team.GREEN);
+
+        initialBoard.put(start, piece);
+        initialBoard.put(createPosition(column,row), new Soldier(Team.GREEN));
+        Board board = new Board(() -> initialBoard);
+
+        // then
+        assertThatThrownBy(() -> board.movePiece(start, goal, Team.GREEN))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 목적지에 같은 진영의 기물이 있어 이동할 수 없습니다.");
     }
 }
--- src/test/java/janggi/piece/SoldierTest.java
+++ src/test/java/janggi/piece/SoldierTest.java
@@ -52,6 +52,24 @@ class SoldierTest {
         assertThatThrownBy(() -> board.movePiece(start, goal, team))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 병의 이동 규칙에 어긋나는 움직임입니다.");
+    }
 
+    @CsvSource(value = {"4:5", "3:4", "5:4", "4:3"}, delimiterString = ":")
+    @ParameterizedTest
+    void 목적지에_같은_진영의_기물이_있을_경우_예외를_발생한다(int column, int row) {
+        // given
+        Map<Position, Piece> initialBoard = new HashMap<>();
+        Position start = createPosition(4, 4);
+        Position goal = createPosition(column, row);
+        Guard piece = new Guard(Team.GREEN);
+
+        initialBoard.put(start, piece);
+        initialBoard.put(createPosition(column,row), new Soldier(Team.GREEN));
+        Board board = new Board(() -> initialBoard);
+
+        // then
+        assertThatThrownBy(() -> board.movePiece(start, goal, Team.GREEN))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 목적지에 같은 진영의 기물이 있어 이동할 수 없습니다.");
     }
 }
