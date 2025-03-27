6db11919ad85f18ecc04a5f51e48abbf08e7d21d
wogus <ind07152@naver.com>
2025-03-27 17:48:43 +0900

    feat: 궁 안 이동 기능 추가

--- src/main/java/domain/piece/path/FixedSingleMovePathFinder.java
+++ src/main/java/domain/piece/path/FixedSingleMovePathFinder.java
@@ -2,9 +2,27 @@ package domain.piece.path;
 
 import domain.position.Direction;
 import domain.position.Position;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class FixedSingleMovePathFinder implements PathFinder{
+    private static final Map<Position,List<Direction>> PALACE_MOVEMENT;
+
+    static {
+        PALACE_MOVEMENT = Map.of(
+                Position.of(0,3), List.of(Direction.RIGHT_UP),
+                Position.of(0,5), List.of(Direction.LEFT_UP),
+                Position.of(2,3), List.of(Direction.RIGHT_DOWN),
+                Position.of(2,5), List.of(Direction.LEFT_DOWN),
+                Position.of(1,4), List.of(Direction.RIGHT_UP,Direction.LEFT_UP,Direction.RIGHT_DOWN,Direction.LEFT_DOWN),
+                Position.of(7,3), List.of(Direction.RIGHT_UP),
+                Position.of(7,5), List.of(Direction.LEFT_UP),
+                Position.of(9,3), List.of(Direction.RIGHT_DOWN),
+                Position.of(9,5), List.of(Direction.LEFT_DOWN),
+                Position.of(8,4), List.of(Direction.RIGHT_UP,Direction.LEFT_UP,Direction.RIGHT_DOWN,Direction.LEFT_DOWN)
+        );
+    }
 
     private final List<Direction> directions;
 
@@ -14,12 +32,16 @@ public class FixedSingleMovePathFinder implements PathFinder{
 
     @Override
     public List<Position> findIntermediatePositions(Position from, Position to) {
-        validateDestination(from, to);
+        List<Direction> possibleDirections = new ArrayList<>(directions);
+        if(PALACE_MOVEMENT.containsKey(from)){
+            possibleDirections.addAll(PALACE_MOVEMENT.get(from));
+        }
+        validateDestination(from, to, possibleDirections);
         return List.of();
     }
 
-    private void validateDestination(Position from,Position to){
-        boolean canNotMove = directions.stream()
+    private void validateDestination(Position from,Position to, List<Direction> possibleDirections){
+        boolean canNotMove = possibleDirections.stream()
                 .filter(direction -> from.canMovePosition(direction.getDeltaRow(), direction.getDeltaColumn()))
                 .map(direction -> from.movePosition(direction.getDeltaRow(), direction.getDeltaColumn()))
                 .noneMatch(position -> position.equals(to));
