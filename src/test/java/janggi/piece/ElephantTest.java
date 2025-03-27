9d9235492e69b2b653ef93a1876418e8662014c9
eoehd1ek <eoehd2ek@gmail.com>
2025-03-27 19:01:03 +0900

    refector(ElephantTest): 부정확한 네이밍 수정

--- src/test/java/janggi/piece/ElephantTest.java
+++ src/test/java/janggi/piece/ElephantTest.java
@@ -15,9 +15,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 class ElephantTest {
 
     @ParameterizedTest
-    @DisplayName("시작점과 끝점이 주어졌을 때, 이동 경로를 반환한다.")
+    @DisplayName("시작점과 끝점이 주어졌을 때, 상의 이동 경로를 반환한다.")
     @MethodSource("pathArguments")
-    void shouldReturnTrueWhenValidateMovingRule(Position end, List<Position> expected) {
+    void should_return_path_by_start_and_end_position(Position end, List<Position> expected) {
         // given
         Elephant elephant = new Elephant(Color.RED);
         Position start = new Position(5, 5);
@@ -31,48 +31,7 @@ class ElephantTest {
 
     private static Stream<Arguments> pathArguments() {
         return Stream.of(
-                Arguments.of(
-                        new Position(3, 8),
-                        List.of(
-                                new Position(5, 6),
-                                new Position(4, 7)
-                        )
-                ),
-                Arguments.of(
-                        new Position(7, 8),
-                        List.of(
-                                new Position(5, 6),
-                                new Position(6, 7)
-                        )
-                ),
-                Arguments.of(
-                        new Position(8, 7),
-                        List.of(
-                                new Position(6, 5),
-                                new Position(7, 6)
-                        )
-                ),
-                Arguments.of(
-                        new Position(8, 3),
-                        List.of(
-                                new Position(6, 5),
-                                new Position(7, 4)
-                        )
-                ),
-                Arguments.of(
-                        new Position(7, 2),
-                        List.of(
-                                new Position(5, 4),
-                                new Position(6, 3)
-                        )
-                ),
-                Arguments.of(
-                        new Position(3, 2),
-                        List.of(
-                                new Position(5, 4),
-                                new Position(4, 3)
-                        )
-                ),
+                // 5,5 -> 4,5좌 -> 3,4좌상 -> 2,3좌상
                 Arguments.of(
                         new Position(2, 3),
                         List.of(
@@ -80,37 +39,85 @@ class ElephantTest {
                                 new Position(3, 4)
                         )
                 ),
+                // 5,5 -> 4,5좌 -> 3,6좌하 -> 2,7좌하
                 Arguments.of(
                         new Position(2, 7),
                         List.of(
                                 new Position(4, 5),
                                 new Position(3, 6)
                         )
+                ),
+                // 5,5 -> 6,5우 -> 7,4우상 -> 8,3우상
+                Arguments.of(
+                        new Position(8, 3),
+                        List.of(
+                                new Position(6, 5),
+                                new Position(7, 4)
+                        )
+                ),
+                // 5,5 -> 6,5우 -> 7,6우하 -> 8,7우하
+                Arguments.of(
+                        new Position(8, 7),
+                        List.of(
+                                new Position(6, 5),
+                                new Position(7, 6)
+                        )
+                ),
+                // 5,5 -> 5,4상 -> 4,3좌상 -> 3,2좌상
+                Arguments.of(
+                        new Position(3, 2),
+                        List.of(
+                                new Position(5, 4),
+                                new Position(4, 3)
+                        )
+                ),
+                // 5,5 -> 5,4상 -> 6,3우상 -> 7,2우상
+                Arguments.of(
+                        new Position(7, 2),
+                        List.of(
+                                new Position(5, 4),
+                                new Position(6, 3)
+                        )
+                ),
+                // 5,5 -> 5,6하 -> 4,7좌하 -> 3,8좌하
+                Arguments.of(
+                        new Position(3, 8),
+                        List.of(
+                                new Position(5, 6),
+                                new Position(4, 7)
+                        )
+                ),
+                // 5,5 -> 5,6하 -> 6,7우하 -> 7,8우하
+                Arguments.of(
+                        new Position(7, 8),
+                        List.of(
+                                new Position(5, 6),
+                                new Position(6, 7)
+                        )
                 )
         );
     }
 
-
     @ParameterizedTest
-    @DisplayName("상의 이동 규칙이 어긋나면 예외를 발생한다.")
+    @DisplayName("도착점이 상의 이동 규칙에 어긋나면 예외가 발생한다")
     @CsvSource(value = {
-            "3, 5",
-            "7, 5",
-            "5, 7",
-            "5, 3",
-            "4, 6",
-            "6, 4",
-            "4, 4",
-            "6, 6"
+            "5, 3",  // 상상
+            "5, 7",  // 하하
+            "3, 5",  // 좌좌
+            "7, 5",  // 우우
+            "4, 4",  // 좌상
+            "4, 6",  // 좌하
+            "6, 4",  // 우상
+            "6, 6"   // 우하
     })
-    void shouldReturnTrueWhenUnfollowMovingRule(int destX, int destY) {
+    void should_throw_exception_when_unfollow_elephant_moving_rule(int destX, int destY) {
         // given
-        Guard guard = new Guard(Color.RED);
+        Elephant elephant = new Elephant(Color.RED);
         Position start = new Position(5, 5);
         Position end = new Position(destX, destY);
 
         // when
-        assertThatThrownBy(() -> guard.calculatePath(start, end))
+        assertThatThrownBy(() -> elephant.calculatePath(start, end))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
