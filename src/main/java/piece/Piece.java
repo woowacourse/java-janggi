726556b489e59561a61635066d481d9f60e03998
byesol <bsj5967@naver.com>
2025-03-27 21:06:38 +0900

    feat: 상 움직임 중간 블록 체크 기능 작성

--- src/main/java/piece/Piece.java
+++ src/main/java/piece/Piece.java
@@ -1,5 +1,9 @@
 package piece;
 
+import game.Board;
+import java.util.List;
+import position.Position;
+
 public abstract class Piece {
     private final PieceType pieceType;
     private final Country country;
@@ -9,4 +13,14 @@ public abstract class Piece {
         this.pieceType = pieceType;
         this.country = country;
     }
+
+    public void canMove(final Position fromPosition, final Position toPosition, Board board) {
+        List<Position> route = getPathForMoving(fromPosition, toPosition);
+        validateRoute(route, board);
+    }
+
+    public abstract List<Position> getPathForMoving(Position fromPosition, Position toPosition);
+
+    public abstract void validateRoute(List<Position> positions, Board board);
+
 }
--- src/test/java/piece/ElephantTest.java
+++ src/test/java/piece/ElephantTest.java
@@ -2,12 +2,17 @@ package piece;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static piece.Country.Cho;
 import static testutil.StaticTest.B3;
 import static testutil.StaticTest.B7;
 import static testutil.StaticTest.C2;
 import static testutil.StaticTest.C8;
 import static testutil.StaticTest.D5;
+import static testutil.StaticTest.E4;
 import static testutil.StaticTest.E5;
+import static testutil.StaticTest.E6;
+import static testutil.StaticTest.F4;
 import static testutil.StaticTest.F6;
 import static testutil.StaticTest.G2;
 import static testutil.StaticTest.G6;
@@ -15,6 +20,8 @@ import static testutil.StaticTest.G8;
 import static testutil.StaticTest.H3;
 import static testutil.StaticTest.H7;
 
+import game.Board;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class ElephantTest {
@@ -22,36 +29,66 @@ public class ElephantTest {
     @Test
     void 상은_시작지와_목적지에_따른_이동경로를_반환한다() {
         // given
-        Elephant elephant = new Elephant(Country.Cho);
+        Elephant elephant = new Elephant(Cho);
+        Board board = new Board(Map.of());
 
         // then
-        assertThatCode(() -> elephant.getPathForMoving(E5, C8)).doesNotThrowAnyException();
-        assertThatCode(() -> elephant.getPathForMoving(E5, B7)).doesNotThrowAnyException();
-        assertThatCode(() -> elephant.getPathForMoving(E5, C2)).doesNotThrowAnyException();
-        assertThatCode(() -> elephant.getPathForMoving(E5, B3)).doesNotThrowAnyException();
-        assertThatCode(() -> elephant.getPathForMoving(E5, G8)).doesNotThrowAnyException();
-        assertThatCode(() -> elephant.getPathForMoving(E5, G2)).doesNotThrowAnyException();
-        assertThatCode(() -> elephant.getPathForMoving(E5, H7)).doesNotThrowAnyException();
-        assertThatCode(() -> elephant.getPathForMoving(E5, H3)).doesNotThrowAnyException();
-
+        assertAll(
+                () -> assertThatCode(() -> elephant.canMove(E5, C8, board)).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> elephant.canMove(E5, B7, board)).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> elephant.canMove(E5, C2, board)).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> elephant.canMove(E5, B3, board)).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> elephant.canMove(E5, G8, board)).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> elephant.canMove(E5, G2, board)).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> elephant.canMove(E5, H7, board)).doesNotThrowAnyException(),
+                () -> assertThatCode(() -> elephant.canMove(E5, H3, board)).doesNotThrowAnyException()
+        );
     }
 
     @Test
     void 상은_정해진_루트가_아니면_이동할_수_없다() {
         // given
-        Elephant elephant = new Elephant(Country.Cho);
+        Elephant elephant = new Elephant(Cho);
+        Board board = new Board(Map.of());
 
         // then
-        assertThatThrownBy(() -> elephant.getPathForMoving(E5, D5))
+        assertThatThrownBy(() -> elephant.canMove(E5, D5, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
 
-        assertThatThrownBy(() -> elephant.getPathForMoving(E5, F6))
+        assertThatThrownBy(() -> elephant.canMove(E5, F6, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
 
-        assertThatThrownBy(() -> elephant.getPathForMoving(E5, G6))
+        assertThatThrownBy(() -> elephant.canMove(E5, G6, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
     }
+
+    @Test
+    void 상은_중간에_기물이_있으면_이동할_수_없다() {
+        // given
+        Elephant elephant = new Elephant(Cho);
+        Board board = new Board(Map.of(
+                D5, new Elephant(Cho),
+                F4, new Elephant(Cho),
+                E4, new Elephant(Cho),
+                E6, new Elephant(Cho)
+        ));
+
+        // then
+        assertThatThrownBy(() -> elephant.canMove(E5, C8, board))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중간에 기물이 있어 갈 수 없습니다.");
+
+        assertThatThrownBy(() -> elephant.canMove(E5, B7, board))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중간에 기물이 있어 갈 수 없습니다.");
+
+        assertThatThrownBy(() -> elephant.canMove(E5, C2, board))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중간에 기물이 있어 갈 수 없습니다.");
+    }
+
+
 }
