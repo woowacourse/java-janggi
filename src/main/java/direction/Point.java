0a2be961599582dd595b0e4e39b9cd0d3114df77
GiSeok Kang <ime.rokz@gmail.com>
2025-03-27 11:18:29 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/direction/Point.java
+++ src/main/java/direction/Point.java
@@ -10,10 +10,6 @@ public record Point(int column, int row) {
         return new Point(column + other.column, row + other.row);
     }
 
-    public Point multiply(int dir) {
-        return new Point(column * dir, row * dir);
-    }
-
     public boolean isDifferentColumn(Point point) {
         return point.column != this.column;
     }
