d3b421221347094595e766ac7b12970e4e2a83c1
eoehd1ek <eoehd2ek@gmail.com>
2025-03-27 19:10:16 +0900

    refector(SoldierTest): 부정확한 네이밍 수정

--- src/test/java/janggi/piece/SoldierTest.java
+++ src/test/java/janggi/piece/SoldierTest.java
@@ -12,13 +12,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 class SoldierTest {
 
     @ParameterizedTest
-    @DisplayName("BLUE 병사의 시작점과 끝점이 주어졌을 때, 이동 경로를 반환한다.")
+    @DisplayName("시작점과 끝점이 주어졌을 때, BLUE 병사의 이동 경로를 반환한다")
     @CsvSource(value = {
             "5, 4", // 상
             "4, 5", // 좌
             "6, 5"  // 우
     })
-    void shouldReturnPathWhenBlueSide(int destX, int destY) {
+    void should_return_path_by_start_and_end_position_of_blue_color_piece(int destX, int destY) {
         // given
         Soldier soldier = new Soldier(Color.BLUE);
         Position start = new Position(5, 5);
@@ -32,13 +32,13 @@ class SoldierTest {
     }
 
     @ParameterizedTest
-    @DisplayName("RED 병사의 시작점과 끝점이 주어졌을 때, 이동 경로를 반환한다.")
+    @DisplayName("시작점과 끝점이 주어졌을 때, RED 병사의 이동 경로를 반환한다")
     @CsvSource(value = {
             "5, 6", // 하
             "4, 5", // 좌
             "6, 5"  // 우
     })
-    void shouldReturnPathWhenRedSide(int destX, int destY) {
+    void should_return_path_by_start_and_end_position_of_red_color_piece(int destX, int destY) {
         // given
         Soldier soldier = new Soldier(Color.RED);
         Position start = new Position(5, 5);
@@ -52,15 +52,15 @@ class SoldierTest {
     }
 
     @ParameterizedTest
-    @DisplayName("BLUE 병사의 이동 경로가 벗어나면 예외를 던진다.")
+    @DisplayName("도착점이 BLUE 병사의 이동 규칙에 어긋나면 예외가 발생한다")
     @CsvSource(value = {
-            "5, 6", // 하
-            "6, 6", // 우하
-            "4, 6", // 좌하
             "3, 5", // 좌좌
-            "7, 5"  // 우우
+            "7, 5", // 우우
+            "5, 6", // 하
+            "4, 6", // 좌하
+            "6, 6"  // 우하
     })
-    void shouldThrowExceptionWhenBlueSide(int destX, int destY) {
+    void should_throw_exception_when_unfollow_blue_color_soldier_moving_rule(int destX, int destY) {
         // given
         Soldier soldier = new Soldier(Color.BLUE);
         Position start = new Position(5, 5);
@@ -72,15 +72,15 @@ class SoldierTest {
     }
 
     @ParameterizedTest
-    @DisplayName("RED 병사의 이동 경로가 벗어나면 예외를 던진다.")
+    @DisplayName("도착점이 RED 병사의 이동 규칙에 어긋나면 예외가 발생한다")
     @CsvSource(value = {
-            "5, 4", // 상
-            "6, 4", // 우상
-            "4, 4", // 좌상
             "3, 5", // 좌좌
-            "7, 5"  // 우우
+            "7, 5", // 우우
+            "5, 4", // 상
+            "4, 4", // 좌상
+            "6, 4"  // 우상
     })
-    void shouldThrowExceptionWhenRedSide(int destX, int destY) {
+    void should_throw_exception_when_unfollow_red_color_soldier_moving_rule(int destX, int destY) {
         // given
         Soldier soldier = new Soldier(Color.RED);
         Position start = new Position(5, 5);
