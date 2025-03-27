d5068b15387eda6feb125cfa267ce557561a4cbd
eoehd1ek <eoehd2ek@gmail.com>
2025-03-27 12:01:10 +0900

    refector(Soldier): 한칸 이동 기물의 경로 구하기 인터페이스로 구현

--- src/main/java/janggi/piece/Soldier.java
+++ src/main/java/janggi/piece/Soldier.java
@@ -1,30 +1,25 @@
 package janggi.piece;
 
+import janggi.piece.movement.MovementRule;
+import janggi.piece.movement.OneBlockMovementRule;
 import janggi.position.Direction;
 import janggi.position.Position;
 import java.util.List;
 
 public class Soldier extends Piece {
 
+    private final MovementRule movementRule;
+
     public Soldier(final Color color) {
         super(color);
+        this.movementRule = new OneBlockMovementRule();
     }
 
     @Override
     public List<Position> calculatePath(final Position start, final Position end) {
-        validateMovingRule(start, end);
+        movementRule.validateMovementRule(start, end);
         validateDirection(start, end);
-        return List.of();
-    }
-
-    private void validateMovingRule(final Position start, final Position end) {
-        int absDifferenceX = start.calculateAbsoluteDifferenceX(end);
-        int absDifferenceY = start.calculateAbsoluteDifferenceY(end);
-        if ((absDifferenceX == 1 && absDifferenceY == 0)
-                || (absDifferenceX == 0 && absDifferenceY == 1)) {
-            return;
-        }
-        throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
+        return movementRule.calculatePath(start, end);
     }
 
     private void validateDirection(final Position start, final Position end) {
@@ -33,5 +28,4 @@ public class Soldier extends Piece {
             throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
         }
     }
-
 }
---
+++ src/main/java/janggi/piece/movement/OneBlockMovementRule.java
@@ -0,0 +1,23 @@
+package janggi.piece.movement;
+
+import janggi.position.Position;
+import java.util.List;
+
+public class OneBlockMovementRule implements MovementRule {
+
+    @Override
+    public List<Position> calculatePath(final Position start, final Position end) {
+        return List.of();
+    }
+
+    @Override
+    public void validateMovementRule(final Position start, final Position end) {
+        final int absDifferenceX = start.calculateAbsoluteDifferenceX(end);
+        final int absDifferenceY = start.calculateAbsoluteDifferenceY(end);
+        if ((absDifferenceX == 1 && absDifferenceY == 0)
+                || (absDifferenceX == 0 && absDifferenceY == 1)) {
+            return;
+        }
+        throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
+    }
+}
