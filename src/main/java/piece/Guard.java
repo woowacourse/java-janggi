baa870f2c12cbb84c930d7a627b366b15b077a4e
byesol <bsj5967@naver.com>
2025-03-27 15:54:23 +0900

    feat: 선비 움직임 기능 구현

---
+++ src/main/java/piece/Guard.java
@@ -0,0 +1,22 @@
+package piece;
+
+import java.util.List;
+import java.util.Set;
+import position.Movement;
+import position.Position;
+
+public class Guard {
+    private static final Set<List<Movement>> pieceMovements = Set.of(
+            List.of(Movement.UP),
+            List.of(Movement.DOWN),
+            List.of(Movement.LEFT),
+            List.of(Movement.RIGHT));
+
+    public List<Position> getPathForMoving(Position fromPosition, Position toPosition) {
+        return pieceMovements.stream()
+                .map(route -> fromPosition.findMovablePositions(route))
+                .filter(path -> !path.isEmpty() && path.getLast().equals(toPosition))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 위치로 이동할 수 없습니다."));
+    }
+}
---
+++ src/test/java/piece/GuardTest.java
@@ -0,0 +1,53 @@
+package piece;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static testutil.StaticTest.A1;
+import static testutil.StaticTest.A2;
+import static testutil.StaticTest.A3;
+import static testutil.StaticTest.B1;
+import static testutil.StaticTest.C1;
+import static testutil.StaticTest.D1;
+
+import org.junit.jupiter.api.Test;
+
+public class GuardTest {
+
+    @Test
+    void 사는_위_아래_왼쪽_오른쪽_으로_움직일_수_있다() {
+        // given
+        Guard guard = new Guard();
+
+        // then
+        assertThatCode(() -> guard.getPathForMoving(A1, A2))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> guard.getPathForMoving(A2, A1))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> guard.getPathForMoving(B1, A1))
+                .doesNotThrowAnyException();
+
+        assertThatCode(() -> guard.getPathForMoving(B1, C1))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 사는_위_아래_왼쪽_오른쪽_제외하고_움직일_수_없다() {
+        // given
+        Guard guard = new Guard();
+
+        // then
+        assertThatThrownBy(() -> guard.getPathForMoving(A1, A3))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 위치로 이동할 수 없습니다.");
+
+        assertThatThrownBy(() -> guard.getPathForMoving(A2, B1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 위치로 이동할 수 없습니다.");
+
+        assertThatThrownBy(() -> guard.getPathForMoving(B1, D1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 위치로 이동할 수 없습니다.");
+    }
+}
