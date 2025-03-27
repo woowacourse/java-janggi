975d39854113d47efa3e1fbceda91cea339b5a91
dltmddus <tmddus4671@gmail.com>
2025-03-27 13:53:37 +0900

    test: 궁, 사 궁성 관련 이동 테스트 추가

--- src/test/java/domain/JanggiPositionTest.java
+++ src/test/java/domain/JanggiPositionTest.java
@@ -1,18 +1,28 @@
 package domain;
 
+import static domain.Fixtures.FIVE_NINE;
+import static domain.Fixtures.FOUR_EIGHT;
+import static domain.Fixtures.FOUR_ZERO;
 import static domain.Fixtures.ONE_NINE;
 import static domain.Fixtures.ONE_THREE;
 import static domain.Fixtures.ONE_ZERO;
+import static domain.Fixtures.SIX_EIGHT;
+import static domain.Fixtures.SIX_ZERO;
 import static domain.Fixtures.THREE_SEVEN;
 import static domain.Fixtures.TWO_ZERO;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.pattern.Pattern;
+import domain.piece.Side;
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class JanggiPositionTest {
 
@@ -95,5 +105,27 @@ public class JanggiPositionTest {
 
         // then
         Assertions.assertThat(gap).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDiagonalPositions")
+    void 이전_좌표와_이후_좌표가_대각선_방향임을_알_수_있다(JanggiPosition afterPosition) {
+        // when & then
+        assertThat(FIVE_NINE.isDiagonalTo(afterPosition)).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDiagonalPositions")
+    void 궁성의_중앙을_지나는_이동임을_알_수_있다(JanggiPosition afterPosition) {
+        // when & then
+        assertThat(FIVE_NINE.isPassThroughCenter(Side.CHO, afterPosition)).isTrue();
+    }
+
+    static Stream<Arguments> provideDiagonalPositions() {
+        return Stream.of(
+                Arguments.of(SIX_EIGHT),
+                Arguments.of(FOUR_EIGHT),
+                Arguments.of(FOUR_ZERO),
+                Arguments.of(SIX_ZERO));
     }
 }
