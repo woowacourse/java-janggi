6247cab5ff8b48dcc16f0676cc625bc93e631e67
byesol <bsj5967@naver.com>
2025-03-27 16:46:53 +0900

    feat: 졸병 움직임 기능 작성

---
+++ src/main/java/piece/Soldier.java
@@ -0,0 +1,25 @@
+package piece;
+
+import java.util.List;
+import java.util.Set;
+import position.Movement;
+import position.Position;
+
+public class Soldier extends Piece {
+    private static final Set<List<Movement>> pieceMovements = Set.of(
+            List.of(Movement.UP),
+            List.of(Movement.LEFT),
+            List.of(Movement.RIGHT));
+
+    public Soldier(final Country country) {
+        super(PieceType.SOLDIER, country);
+    }
+
+    public List<Position> getPathForMoving(Position fromPosition, Position toPosition) {
+        return pieceMovements.stream()
+                .map(route -> fromPosition.findMovablePositions(route))
+                .filter(path -> !path.isEmpty() && path.getLast().equals(toPosition))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 위치로 이동할 수 없습니다."));
+    }
+}
