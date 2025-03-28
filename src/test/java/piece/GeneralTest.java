7f7beae13d9e33aa3ddc7f1675e699e9422c57f7
byesol <bsj5967@naver.com>
2025-03-28 09:21:26 +0900

    refactor(GeneralTest, GuardTest): 테스트 네이밍, parameter값 리팩토링

--- src/test/java/piece/GeneralTest.java
+++ src/test/java/piece/GeneralTest.java
@@ -15,12 +15,29 @@ import static testutil.TestConstant.F5;
 
 import game.Board;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import position.Position;
 
 public class GeneralTest {
 
-    @Test
-    void 장군은_위_아래_왼쪽_오른쪽_으로_움직일_수_있다() {
+
+    public static Stream<Arguments> VALID_MOVE_POSITIONS() {
+        return Stream.of(
+                Arguments.of(E5, E4),
+                Arguments.of(E5, E6),
+                Arguments.of(E5, D5),
+                Arguments.of(E5, F5)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("VALID_MOVE_POSITIONS")
+    void 장군은_위_아래_왼쪽_오른쪽_으로_움직일_수_있다(Position fromPosition, Position toPosition) {
         // given
         General general = new General(Country.Cho);
         Board board = new Board(Map.of());
@@ -52,5 +69,6 @@ public class GeneralTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
     }
+
 
 }
--- src/test/java/piece/GuardTest.java
+++ src/test/java/piece/GuardTest.java
@@ -6,26 +6,42 @@ import static testutil.TestConstant.A1;
 import static testutil.TestConstant.A2;
 import static testutil.TestConstant.A3;
 import static testutil.TestConstant.B1;
-import static testutil.TestConstant.C1;
 import static testutil.TestConstant.D1;
+import static testutil.TestConstant.D5;
+import static testutil.TestConstant.E4;
+import static testutil.TestConstant.E5;
+import static testutil.TestConstant.E6;
+import static testutil.TestConstant.F5;
 
 import game.Board;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import position.Position;
 
 public class GuardTest {
 
-    @Test
-    void 사는_위_아래_왼쪽_오른쪽_으로_움직일_수_있다() {
+    public static Stream<Arguments> VALID_MOVE_POSITIONS() {
+        return Stream.of(
+                Arguments.of(E5, E4),
+                Arguments.of(E5, E6),
+                Arguments.of(E5, D5),
+                Arguments.of(E5, F5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("VALID_MOVE_POSITIONS")
+    void 사는_위_아래_왼쪽_오른쪽_으로_움직일_수_있다(Position fromPosition, Position toPosition) {
         // given
         Guard guard = new Guard(Country.Cho);
         Board board = new Board(Map.of());
 
         // then
-        assertThatCode(() -> guard.canMove(A1, A2, board)).doesNotThrowAnyException();
-        assertThatCode(() -> guard.canMove(A2, A1, board)).doesNotThrowAnyException();
-        assertThatCode(() -> guard.canMove(B1, A1, board)).doesNotThrowAnyException();
-        assertThatCode(() -> guard.canMove(B1, C1, board)).doesNotThrowAnyException();
+        assertThatCode(() -> guard.canMove(fromPosition, toPosition, board)).doesNotThrowAnyException();
     }
 
     @Test
