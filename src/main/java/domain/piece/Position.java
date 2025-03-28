6a77be58e7084d861ed26dec3387945d4a011734
rladmstn <oz33177351@gmail.com>
2025-03-29 02:10:12 +0900

    feat: 궁성 내 위치인지 판단 기능 추가

--- src/main/java/domain/piece/Position.java
+++ src/main/java/domain/piece/Position.java
@@ -10,14 +10,6 @@ public class Position {
     public static final int MAX_ROW = 9;
     public static final int MAX_COLUMN = 10;
 
-    private static final int PALACE_MIN_ROW = 4;
-    private static final int PALACE_MAX_ROW = 6;
-
-    private static final int HAN_PALACE_MIN_COLUMN = 1;
-    private static final int HAN_PALACE_MAX_COLUMN = 3;
-    private static final int CHO_PALACE_MIN_COLUMN = 8;
-    private static final int CHO_PALACE_MAX_COLUMN = 10;
-
     private final int row;
     private final int column;
 
@@ -50,9 +42,9 @@ public class Position {
     }
 
     public boolean isInPalace() {
-        return (PALACE_MIN_ROW <= row && row <= PALACE_MAX_ROW) &&
-                ((HAN_PALACE_MIN_COLUMN <= column && column <= HAN_PALACE_MAX_COLUMN) ||
-                        (CHO_PALACE_MIN_COLUMN <= column && column <= CHO_PALACE_MAX_COLUMN));
+        return (Palace.MIN_ROW <= row && row <= Palace.MAX_ROW) &&
+                ((Palace.HAN_MIN_COLUMN <= column && column <= Palace.HAN_MAX_COLUMN) ||
+                        (Palace.CHO_MIN_COLUMN <= column && column <= Palace.CHO_MAX_COLUMN));
     }
 
     public Position flipLeftRight() {
