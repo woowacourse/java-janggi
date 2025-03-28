f4e748dfda0bcfd399999d6ad2464d49e89b158c
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-28 19:49:27 +0900

    refactor: Cannon 기물, 추상메서드를 오버라이딩 방식으로 수정

--- src/main/java/piece/Cannon.java
+++ src/main/java/piece/Cannon.java
@@ -1,47 +1,47 @@
 package piece;
 
 import board.Board;
-import board.Position;
+import position.Position;
+import validator.DirectionCheckable;
+import validator.ObstructionCheckable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 
-public class Cannon extends Piece {
+public class Cannon extends Piece implements DirectionCheckable, ObstructionCheckable {
 
-    public Cannon(final TeamType teamType) {
-        super(teamType);
+    public Cannon(final Position position, final Country country) {
+        super(position, country);
     }
 
     @Override
-    protected boolean withInDirection(Position src, Position destination) {
-        return src.isSameLine(destination);
+    public void validateMoveCondition(Position src, Position dest, Board board) {
+        validateDirection(src, dest);
+        List<Position> internalPositions = getInternalPositions(dest);
+        validateObstruction(board, internalPositions, 1);
+
+        List<Position> existPositions = board.findExistPositions(internalPositions);
+        Piece findPiece = board.getPieceBy(existPositions.getFirst());
+        if (this.equalsType(findPiece)) {
+            throw new IllegalArgumentException("포는 포를 죽일 수 없습니다.");
+        }
     }
 
     @Override
-    protected boolean withInRangeByMovement(double distanceByPositions) {
-        return true;
+    public BiPredicate<Position, Position> directionRule() {
+        return Position::isSameLine;
     }
 
-    @Override
-    protected boolean passFilter(Position src, Position destination, Board board) {
-        int count = 0;
-        final List<Position> positions = src.calculateBetweenPositions(destination);
-        for (final Position position : positions) {
-            if (board.existPieceByPosition(position)) {
-                count++;
-            }
-            if (isSamePieceType(board, position)) {
-                return false;
+    private List<Position> getInternalPositions(Position destination) {
+        List<Position> positions = new ArrayList<>();
+        for (int x = Math.min(position.x(), destination.x()); x <= Math.max(position.x(), destination.x()); x++) {
+            for (int y = Math.min(position.y(), destination.y()); y <= Math.max(position.y(), destination.y()); y++) {
+                positions.add(new Position(x, y));
             }
         }
-        return count == 1;
-    }
-
-    private boolean isSamePieceType(Board board, Position position) {
-        Piece piece = board.getPieceByPosition(position);
-        if (equalsType(piece)) {
-            return true;
-        }
-        return false;
+        positions.removeLast();
+        return positions;
     }
 
     @Override
--- src/test/java/piece/CannonTest.java
+++ src/test/java/piece/CannonTest.java
@@ -1,116 +1,101 @@
 package piece;
 
 import board.Board;
-import board.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import position.LineDirection;
+import position.Position;
 
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CannonTest {
 
-    @DisplayName("Cannon은 목적지가 같은 라인이 아니라면 false를 반환한다.")
+    @DisplayName("Cannon은 동서남북 방향 내에 dest가 존재해야 한다")
     @Test
-    void withInDirectionFailureByNonSameLine() {
+    void validateMove() {
         // given
-        final Piece cannonPiece = new Cannon(TeamType.BLUE);
-        final Position now = new Position(1, 1);
-        final Position ableDest = new Position(1, 3);
+        Country dumyCountry = Country.HAN;
+        Country.assignDirection(dumyCountry, LineDirection.UP);
+        Position dumyPosition = new Position(1, 1);
+        Position src = dumyPosition;
+        final Piece cannon = new Cannon(src, dumyCountry);
+
+        Position obstructionPosition = new Position(1, 2);
+        Piece obstructionPiece = new General(obstructionPosition, dumyCountry);
+        final Board board = new Board(Map.of(obstructionPosition, obstructionPiece));
+
+        // when & then: 1 : success
+        final Position ableDest = new Position(1, 4);
+        assertThatCode(
+                () -> cannon.validateMove(src, ableDest, board)
+        ).doesNotThrowAnyException();
+
+        // when & then : 2 : failure
         final Position notAbleDest = new Position(2, 2);
-
-        // when
-        final boolean actual1 = cannonPiece.withInDirection(now, ableDest);
-        final boolean actual2 = cannonPiece.withInDirection(now, notAbleDest);
-
-        // then
-        org.junit.jupiter.api.Assertions.assertAll(
-                () -> assertThat(actual1).isTrue(),
-                () -> assertThat(actual2).isFalse()
-        );
+        assertThatThrownBy(
+                () -> cannon.validateMove(src, notAbleDest, board)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("Cannon은 목적지까지 가는 중 아무런 기물이 없다면 false를 반환한다.")
+    @DisplayName("Cannon은 목적지까지 기물이 하나만 존재해야 한다.")
     @Test
-    void passFilterFailureByNonPieceBetween() {
+    void validateMoveWithObstruction() {
         // given
-        final Piece cannonPiece = new Cannon(TeamType.BLUE);
-        final Position now = new Position(1, 1);
-        final Position ableDest = new Position(1, 3);
-        final Position notAbleDest = new Position(2, 1);
-        final TeamType teamType = TeamType.RED;
-        final Map<Position, Piece> map = Map.of(new Position(1, 2),
-                new Chariot(teamType));
-        final Board board = new Board(map);
+        Country turnCountry = Country.HAN;
+        Country.assignDirection(turnCountry, LineDirection.UP);
+        Position dumyPosition = new Position(2, 2);
+        final Position src = dumyPosition;
+        final Piece cannon = new Cannon(src, turnCountry);
 
-        // when
-        final boolean actual1 = cannonPiece.passFilter(now, ableDest, board);
-        final boolean actual2 = cannonPiece.passFilter(now, notAbleDest, board);
+        final Position obstructionPosition1 = new Position(2, 3);
+        final Piece obstructionPiece1 = new General(obstructionPosition1, turnCountry);
+        final Position dest = new Position(2, 5);
 
-        // then
-        org.junit.jupiter.api.Assertions.assertAll(
-                () -> assertThat(actual1).isTrue(),
-                () -> assertThat(actual2).isFalse()
-        );
+        // when & then : 1 : success
+        final Board ableBoard = new Board(Map.of(
+                obstructionPosition1, obstructionPiece1
+        ));
+        assertThatCode(
+                () -> cannon.validateMove(src, dest, ableBoard)
+        ).doesNotThrowAnyException();
+
+        // when & then : 2 : failure
+        final Position obstructionPosition2 = new Position(2, 4);
+        final Piece obstructionPiece2 = new Cannon(obstructionPosition2, turnCountry);
+        final Board notAbleBoard = new Board(Map.of(
+                obstructionPosition2, obstructionPiece2
+        ));
+        assertThatThrownBy(
+                () -> cannon.validateMove(src, dest, notAbleBoard)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("Cannon은 목적지까지 가던 중 포를 만나면 false를 반환한다.")
-    @Test
-    void isAbleToMoveFailureByMeetCannon() {
-        // given
-        final Piece cannonPiece = new Cannon(TeamType.BLUE);
-        final Position now = new Position(1, 1);
-        final Position destPosition = new Position(1, 3);
-        final TeamType teamType = TeamType.RED;
-        final Map<Position, Piece> map = Map.of(new Position(1, 2),
-                new Cannon(teamType));
-        final Board board = new Board(map);
-
-        // when
-        final boolean actual2 = cannonPiece.isAbleToMove(now, destPosition, board);
-
-        // then
-        assertThat(actual2).isFalse();
-    }
-
-    @DisplayName("Cannon은 목적지까지 가던 중 2개 이상의 기물을 만나면 false를 반환한다.")
-    @Test
-    void isAbleToMoveFailureByOneMorePieces() {
-        // given
-        final Piece cannonPiece = new Cannon(TeamType.BLUE);
-        final Position now = new Position(1, 1);
-        final Position notAbleDest = new Position(1, 4);
-        final TeamType teamType = TeamType.RED;
-        final Map<Position, Piece> map = Map.of(new Position(1, 2),
-                new Chariot(teamType), new Position(1, 3),
-                new Chariot(teamType));
-        final Board board = new Board(map);
-
-        // when
-        final boolean actual2 = cannonPiece.isAbleToMove(now, notAbleDest, board);
-
-        // then
-        assertThat(actual2).isFalse();
-    }
 
     @DisplayName("포는 포를 죽일 수 없다.")
     @Test
-    void isAbleToMoveFailureByDestinationIsCannon() {
+    void canMoveFailureThatDestinationCantCannon() {
         // given
-        final Piece cannonPiece = new Cannon(TeamType.BLUE);
-        final Position now = new Position(1, 1);
-        final Position notAbleDest = new Position(1, 4);
-        final TeamType teamType = TeamType.RED;
-        final Map<Position, Piece> map = Map.of(new Position(1, 4),
-                new Cannon(TeamType.BLUE), new Position(1, 3),
-                new Chariot(teamType));
-        final Board board = new Board(map);
+        Country turnCountry = Country.HAN;
+        Country.assignDirection(turnCountry, LineDirection.UP);
+        Position dumyPosition = new Position(2, 2);
+        final Position src = dumyPosition;
+        final Piece cannon = new Cannon(src, turnCountry);
 
-        // when
-        final boolean actual2 = cannonPiece.isAbleToMove(now, notAbleDest, board);
+        final Position obstructionPosition1 = new Position(2, 3);
+        final Piece obstructionPiece1 = new General(obstructionPosition1, turnCountry);
+        final Position destPosition = new Position(2, 5);
+        final Piece dest = new Cannon(destPosition, turnCountry);
 
-        // then
-        assertThat(actual2).isFalse();
+        // when & then : 1 : success
+        final Board ableBoard = new Board(Map.of(
+                obstructionPosition1, obstructionPiece1,
+                destPosition, dest
+        ));
+        assertThatThrownBy(
+                () -> cannon.validateMove(src, destPosition, ableBoard)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
