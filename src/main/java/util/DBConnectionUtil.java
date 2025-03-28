1e71771b84dbe7b4900d71586441f08e87da6d3c
junhaa <2171326@hansung.ac.kr>
2025-03-29 02:30:25 +0900

    feat: mysql 커넥션 연결 기능 구현

---
+++ src/main/java/util/DBConnectionUtil.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectionUtil {
+    private static final String SERVER = "127.0.0.1:13306";
+    private static final String DATABASE = "janggi";
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("데이터베이스 커넥션 획득에 실패했습니다.");
+        }
+    }
+}
---
+++ src/test/java/util/DBConnectionUtilTest.java
@@ -0,0 +1,17 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DBConnectionUtilTest {
+
+    @Test
+    @DisplayName("커넥션 획득에 성공하면 예외가 발생하지 않는다")
+    void getConnectionTest() {
+        // when & then
+        assertThatCode(DBConnectionUtil::getConnection)
+                .doesNotThrowAnyException();
+    }
+}
