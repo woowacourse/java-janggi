2f1c0fc9e93ce6e6d804fd8a6a4fa73c5a0a66dd
changuii <rhljh7410@gmail.com>
2025-03-28 21:43:24 +0900

    refactor: Guard 코드 리팩터링

--- src/main/java/janggi/piece/Guard.java
+++ src/main/java/janggi/piece/Guard.java
@@ -4,7 +4,7 @@ import janggi.board.JanggiScore;
 import janggi.board.VisibleBoard;
 import janggi.coordinate.Position;
 
-public class Guard extends Piece{
+public class Guard extends Piece {
 
     private static final int GUARD_DISTANCE = 1;
     private static final JanggiScore KILL_JANGGI_SCORE = new JanggiScore(3);
@@ -20,16 +20,23 @@ public class Guard extends Piece{
 
     @Override
     protected boolean canMove(final Position now, final Position destination, final VisibleBoard visibleBoard) {
-        if(!destination.isInsidePalace(country)){
+        if (!destination.isInsidePalace(country)) {
             return false;
         }
 
-        if ((now.isCornerInPalace(country) && destination.isCenterInPalace(country))
-                || (now.isCenterInPalace(country) && destination.isCornerInPalace(country))) {
+        if (isCenterToCorner(now, destination) || isCornerToCenter(now, destination)) {
             return true;
         }
 
         return now.calculateDistance(destination) == GUARD_DISTANCE;
+    }
+
+    private boolean isCenterToCorner(final Position now, final Position destination) {
+        return now.isCenterInPalace() && destination.isCornerInPalace();
+    }
+
+    private boolean isCornerToCenter(final Position now, final Position destination) {
+        return now.isCornerInPalace() && destination.isCenterInPalace();
     }
 
 }
