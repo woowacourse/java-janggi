343cc40b19bb5b5644241f8c760cf6a3a3aa5eb6
egaeng09 <egaeng09@gmail.com>
2025-03-28 22:02:32 +0900

    feat: 졸과 병의 궁성 움직임 기능 추가

--- src/test/java/janggi/piece/ByeongTest.java
+++ src/test/java/janggi/piece/ByeongTest.java
@@ -52,7 +52,7 @@ class ByeongTest {
         // When & Then
         assertThatThrownBy(() -> byeong.makePath(currentPosition, arrivalPosition))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 병은 앞, 좌, 우로 한 칸 씩만 이동할 수 있습니다.");
+                .hasMessageContaining("[ERROR] 병은 앞 방향으로 이어진 선을 따라 한 칸 씩만 이동할 수 있습니다.");
     }
 
     @Test
@@ -69,6 +69,24 @@ class ByeongTest {
         // When & Then
         assertThatThrownBy(() -> byeong.makePath(currentPosition, arrivalPosition))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 병은 앞, 좌, 우로 한 칸 씩만 이동할 수 있습니다.");
+                .hasMessageContaining("[ERROR] 병은 앞 방향으로 이어진 선을 따라 한 칸 씩만 이동할 수 있습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "9, 5, 10, 4",
+            "9, 5, 10, 6",
+    })
+    void 병은_궁성_안에서_간선을_따라_대각선으로_1칸_이동한다(final int currentY, final int currentX, final int arrivalY,
+                                        final int arrivalX) {
+        // Given
+        Position currentPosition = new Position(currentY, currentX);
+        Position arrivalPosition = new Position(arrivalY, arrivalX);
+
+        // When
+        Path path = byeong.makePath(currentPosition, arrivalPosition);
+
+        // Then
+        assertThat(path).isEqualTo(new Path(List.of(arrivalPosition)));
     }
 }
--- src/test/java/janggi/piece/JolTest.java
+++ src/test/java/janggi/piece/JolTest.java
@@ -52,7 +52,7 @@ class JolTest {
         // When & Then
         assertThatThrownBy(() -> jol.makePath(currentPosition, arrivalPosition))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 졸은 앞, 좌, 우로 한 칸 씩만 이동할 수 있습니다.");
+                .hasMessageContaining("[ERROR] 졸은 앞 방향으로 이어진 선을 따라 한 칸 씩만 이동할 수 있습니다.");
     }
 
     @Test
@@ -69,6 +69,24 @@ class JolTest {
         // When & Then
         assertThatThrownBy(() -> jol.makePath(currentPosition, arrivalPosition))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 졸은 앞, 좌, 우로 한 칸 씩만 이동할 수 있습니다.");
+                .hasMessageContaining("[ERROR] 졸은 앞 방향으로 이어진 선을 따라 한 칸 씩만 이동할 수 있습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "9, 5, 8, 4",
+            "9, 5, 8, 6",
+    })
+    void 졸은_궁성_안에서_간선을_따라_대각선으로_1칸_이동한다(final int currentY, final int currentX, final int arrivalY,
+                                        final int arrivalX) {
+        // Given
+        Position currentPosition = new Position(currentY, currentX);
+        Position arrivalPosition = new Position(arrivalY, arrivalX);
+
+        // When
+        Path path = jol.makePath(currentPosition, arrivalPosition);
+
+        // Then
+        assertThat(path).isEqualTo(new Path(List.of(arrivalPosition)));
     }
 }
