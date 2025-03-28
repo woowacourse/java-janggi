98e7ce9d188aae0d1dc4181300f8e89fe42f70aa
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-28 14:02:06 +0900

    refactor: Elephant 기물, 추상메서드를 오버라이딩 방식으로 수정

--- src/main/java/piece/Elephant.java
+++ src/main/java/piece/Elephant.java
@@ -1,34 +1,73 @@
 package piece;
 
 import board.Board;
-import board.Position;
+import movement.MovePath;
+import movement.MovePaths;
+import movement.Movement;
+import position.Position;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class Elephant extends Piece{
+public class Elephant extends Piece {
 
-    public Elephant(final TeamType teamType) {
-        super(teamType);
+    private static final MovePaths movePaths;
+    private static final double DISTANCE;
+
+    static {
+        movePaths = new MovePaths(List.of(
+                new MovePath(Movement.UP, Movement.LEFT_UP, Movement.LEFT_UP),
+                new MovePath(Movement.UP, Movement.RIGHT_UP, Movement.RIGHT_UP),
+                new MovePath(Movement.DOWN, Movement.LEFT_DOWN, Movement.LEFT_DOWN),
+                new MovePath(Movement.DOWN, Movement.RIGHT_DOWN, Movement.RIGHT_DOWN),
+                new MovePath(Movement.LEFT, Movement.LEFT_UP, Movement.LEFT_UP),
+                new MovePath(Movement.LEFT, Movement.LEFT_DOWN, Movement.LEFT_DOWN),
+                new MovePath(Movement.RIGHT, Movement.RIGHT_UP, Movement.RIGHT_UP),
+                new MovePath(Movement.RIGHT, Movement.RIGHT_DOWN, Movement.RIGHT_DOWN)
+        ));
+
+        DISTANCE = movePaths.calculateDistance();
     }
 
-    @Override
-    protected boolean withInDirection(Position src, Position destination) {
-        return true;
+    public Elephant(final Position position, final Country country) {
+        super(position, country);
     }
 
+    // 수정원하는 메서드
+    // todo: 현재, board 인자 대신 함수형 인터페이스로 가능한지 여부 검토 중
+    // todo: 하나의 인터페이스만 사용하면 가능한데, 3개 정도면 불가능 판단할 것
+    // todo: depth 처리 잘하기
     @Override
-    protected boolean withInRangeByMovement(double distanceByPositions) {
-        return distanceByPositions == Math.sqrt(13);
-    }
-
-    @Override
-    protected boolean passFilter(Position src, Position destination, Board board) {
-        final List<Position> positions = src.calculateElephantMiddlePositions(destination);
-        for (final Position position : positions) {
-            if (board.existPieceByPosition(position)) {
-                return false;
-            }
+    protected void validateFilter(Position src, Position destination, Board board) {
+        List<Position> internalPositions = getInternalPositions(getInternalMovePaths(findCorrectMovePath(destination)));
+        if (!board.isCorrectExistPositionCount(internalPositions, 0)) {
+            throw new IllegalArgumentException("목적지까지의 경로 내부에 기물이 존재해서는 안됩니다.");
         }
-        return true;
+    }
+
+    private List<Position> getInternalPositions(MovePaths internalMoverPaths) {
+        List<Position> positions = new ArrayList<>();
+        Position buffer = new Position(position.x(), position.y());
+        for (MovePath movePath : internalMoverPaths.getMovePaths()) {
+            for (Movement movement : movePath.getMovements()) {
+                buffer = buffer.move(movement);
+            }
+            positions.add(buffer);
+        }
+        return positions;
+    }
+
+    private MovePaths getInternalMovePaths(MovePath movePath) {
+        return  movePath.getInternalMovements();
+    }
+
+    private MovePath findCorrectMovePath(Position destination) {
+        return movePaths.findCorrectMovePath(position, destination);
+    }
+
+    @Override
+    protected double getDistance() {
+        return DISTANCE;
     }
 
     @Override
--- src/test/java/piece/ElephantTest.java
+++ src/test/java/piece/ElephantTest.java
@@ -1,61 +1,69 @@
 package piece;
 
 import board.Board;
-import board.Position;
+import position.LineDirection;
+import position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class ElephantTest {
 
     @DisplayName("Elephant는 직-대-대 방향으로 이동할 수 있다.")
     @Test
-    void isAbleToMove() {
+    void validateMove() {
         // given
-        final Piece elephantPiece = new Elephant(TeamType.BLUE);
-        final Position now = new Position(1, 1);
-        final Position ableDest = new Position(4, 3);
-        final Position notAbleDest = new Position(1, 2);
+        Country dumyCountry = Country.HAN;
+        Country.assignDirection(dumyCountry, LineDirection.UP);
+        Position dumyPosition = new Position(1, 1);
+        final Piece horse = new Elephant(dumyPosition, dumyCountry);
+
+        final Position src = dumyPosition;
         final Board board = new Board(new HashMap<>());
-        final TeamType teamType = TeamType.RED;
 
-        // when
-        final boolean actual1 = elephantPiece.isAbleToMove(now, ableDest, board);
-        final boolean actual2 = elephantPiece.isAbleToMove(now, notAbleDest, board);
+        // when & then: 1 : success
+        final Position ableDest = new Position(3, 4);
+        assertThatCode(
+                () -> horse.validateMove(src, ableDest, board)
+        ).doesNotThrowAnyException();
 
-        // then
-        org.junit.jupiter.api.Assertions.assertAll(
-                () -> assertThat(actual1).isTrue(),
-                () -> assertThat(actual2).isFalse()
-        );
+        // when & then : 2 : failure
+        final Position notAbleDest = new Position(1, 2);
+        assertThatThrownBy(
+                () -> horse.validateMove(src, notAbleDest, board)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("Elephant는 직 또는 직-대 방향에 기물이 존재하면 이동할 수 없다.")
     @Test
-    void isAbleToMoveByAnyPiece() {
+    void validateMoveWithObstruction() {
         // given
-        final Piece elephantPiece = new Elephant(TeamType.BLUE);
-        final Position now = new Position(2, 2);
-        final Position dest1 = new Position(4, 5);
-        final Position dest2 = new Position(5, 4);
+        Country turnCountry = Country.HAN;
+        Country.assignDirection(turnCountry, LineDirection.UP);
+        Position dumyPosition = new Position(2, 2);
+        final Piece horse = new Elephant(dumyPosition, turnCountry);
+        final Position src = dumyPosition;
 
-
+        final Position obstructionPosition = new Position(3, 2);
+        final Piece obstructionPiece = new Cannon(obstructionPosition, turnCountry);
         final Board board = new Board(Map.of(
-                new Position(2, 3), new Cannon(TeamType.RED),
-                new Position(4, 3), new Cannon(TeamType.RED)
+                obstructionPosition, obstructionPiece
         ));
-        final TeamType teamType = TeamType.RED;
 
-        // when
-        final boolean actual1 = elephantPiece.isAbleToMove(now, dest1, board);
-        final boolean actual2 = elephantPiece.isAbleToMove(now, dest2, board);
+        // when & then : 1 : success
+        final Position ableDest = new Position(4, 5);
+        assertThatCode(
+                () -> horse.validateMove(src, ableDest, board)
+        ).doesNotThrowAnyException();
 
-        // then
-        assertThat(actual1).isFalse();
-        assertThat(actual2).isFalse();
+        // when & then : 2 : failure
+        final Position notAbleDest = new Position(5, 4);
+        assertThatThrownBy(
+                () -> horse.validateMove(src, notAbleDest, board)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
