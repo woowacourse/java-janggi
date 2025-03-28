cd70196ced0f0541b0e1f3c6dee96274912623e9
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-03-28 17:21:33 +0900

    refactor : 함수형 인터페이스 명시

--- src/main/java/domain/board/PieceExtractor.java
+++ src/main/java/domain/board/PieceExtractor.java
@@ -3,6 +3,7 @@ package domain.board;
 import domain.piece.Piece;
 import java.util.List;
 
+@FunctionalInterface
 public interface PieceExtractor {
 
     List<Piece> extract(List<BoardLocation> boardLocations);
--- src/main/java/domain/board/PieceFinder.java
+++ src/main/java/domain/board/PieceFinder.java
@@ -3,6 +3,7 @@ package domain.board;
 import domain.piece.Piece;
 import java.util.Optional;
 
+@FunctionalInterface
 public interface PieceFinder {
 
     Optional<Piece> findByLocation(BoardLocation boardLocation);
