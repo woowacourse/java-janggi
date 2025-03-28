da3495b9b4acaa22564e351adaf80a9a2f0571be
changuii <rhljh7410@gmail.com>
2025-03-28 21:35:55 +0900

    refactor: soldier 코드 리팩터링

--- src/main/java/janggi/piece/Soldier.java
+++ src/main/java/janggi/piece/Soldier.java
@@ -7,7 +7,6 @@ import janggi.coordinate.Position;
 public class Soldier extends Piece {
 
     private static final int SOLDIER_DISTANCE = 1;
-    private static final double SOLDIER_PALACE_DISTANCE = Math.sqrt(2); 
     private static final JanggiScore KILL_JANGGI_SCORE = new JanggiScore(2);
 
     public Soldier(final Country country) {
@@ -21,27 +20,30 @@ public class Soldier extends Piece {
 
     @Override
     protected boolean canMove(final Position now, final Position destination, final VisibleBoard visibleBoard) {
-        if (country == Country.HAN && !now.isXLessThan(destination)) {
-            return false;
-        }
-        if(country == Country.CHO && !now.isXGreaterThan(destination)){
+        if (isDestinationDirectionFront(now, destination, country)) {
             return false;
         }
 
-        if (isInsidePalaceMove(now, destination)) {
-            return now.calculateDistance(destination) == SOLDIER_PALACE_DISTANCE;
+        if (!now.isSameLine(destination) && now.isSamePalace(destination)) {
+            return isCornerToCenter(now, destination) || isCenterToCorner(now, destination);
         }
 
         return now.calculateDistance(destination) == SOLDIER_DISTANCE;
     }
 
-    private boolean isInsidePalaceMove(final Position now, final Position destination) {
-        final Country opponentCountry = country.toggleCountry();
-        if((now.isCornerInPalace(opponentCountry) && destination.isCenterInPalace(opponentCountry))
-        || (now.isCenterInPalace(opponentCountry) && destination.isCornerInPalace(opponentCountry))){
-            return true;
+    private boolean isDestinationDirectionFront(final Position now, final Position destination, final Country country) {
+        if(country == Country.HAN){
+            return now.isXGreaterThan(destination);
         }
-        return false;
+        return now.isXLessThan(destination);
+    }
+
+    private boolean isCenterToCorner(final Position now, final Position destination) {
+        return now.isCenterInPalace() && destination.isCornerInPalace();
+    }
+
+    private boolean isCornerToCenter(final Position now, final Position destination) {
+        return now.isCornerInPalace() && destination.isCenterInPalace();
     }
 
 }
