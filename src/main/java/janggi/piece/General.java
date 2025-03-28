22f420232a33345d8af6901aaff41ddb18561c8a
BWY <quantumjump10@gmail.com>
2025-03-28 22:13:23 +0900

    feat: 장의 궁성 내부 영역에서의 이동 검증 기능 추가

--- src/main/java/janggi/piece/General.java
+++ src/main/java/janggi/piece/General.java
@@ -1,11 +1,77 @@
 package janggi.piece;
 
+import janggi.board.Board;
+import janggi.board.Position;
+
+import java.util.List;
+
 public class General extends Piece {
     private final PieceType pieceType;
 
     public General(Team team) {
         super(team);
         this.pieceType = PieceType.GENERAL;
+    }
+
+    @Override
+    public void validateMovable(Board board, Position start, Position goal) {
+        validateMoveArea(board, start, goal);
+        Piece attacker = board.getPiece(start);
+        validateNonOurArmyAtGoal(board, goal, attacker.getTeam());
+        validatePath(board, start, goal);
+    }
+
+    public void validateMoveArea(Board board, Position start, Position goal) {
+        Piece general = board.getPiece(start);
+        if (general.getTeam() == Team.GREEN) {
+            validateGreenStart(board, start);
+            validateGreenGoal(board, goal);
+        }
+
+        if (general.getTeam() == Team.RED) {
+            validateRedStart(board, start);
+            validateRedGoal(board, goal);
+        }
+    }
+
+    private void validateGreenStart(Board board, Position start) {
+        if (!board.isInnerBottomPalace(start)) {
+            throw new IllegalArgumentException("[ERROR] 초나라 장의 출발 지점은 하단부 궁성 내 좌표여야 합니다.");
+        }
+    }
+
+    private void validateGreenGoal(Board board, Position goal) {
+        if (!board.isInnerBottomPalace(goal)) {
+            throw new IllegalArgumentException("[ERROR] 초나라 장의 목적 지점은 하단부 궁성 내 좌표여야 합니다.");
+        }
+    }
+
+    private void validateRedStart(Board board, Position start) {
+        if (!board.isInnerUpperPalace(start)) {
+            throw new IllegalArgumentException("[ERROR] 한나라 장의 출발 지점은 상단부 궁성 내 좌표여야 합니다.");
+        }
+    }
+
+    private void validateRedGoal(Board board, Position goal) {
+        if (!board.isInnerUpperPalace(goal)) {
+            throw new IllegalArgumentException("[ERROR] 한나라 장의 목적 지점은 상단부 궁성 내 좌표여야 합니다.");
+        }
+    }
+
+    @Override
+    public List<Position> findPositionsInPath(Position start, Position goal) {
+        int columnDifference = start.calculatesColumnDifference(goal);
+        int rowDifference = start.calculatesRowDifference(goal);
+
+        if (!isValidDistance(columnDifference, rowDifference)) {
+            throw new IllegalArgumentException("[ERROR] 장의 이동 규칙에 어긋나는 움직임입니다.");
+        }
+
+        return List.of(goal);
+    }
+
+    private boolean isValidDistance(int columnDifference, int rowDifference) {
+        return Math.abs(columnDifference) + Math.abs(rowDifference) == 1;
     }
 
     @Override
