22b5889ea2d5d14140744333d8610cc959f96c4d
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-28 16:40:20 +0900

    feat(test) : 테스트 세팅 및 테스트 후 롤백 기능 추가
    - Dao connection 필드로 추출
      -  rollback 메서드 추가
    - Dao 각 테스트 진행 후 롤백

--- src/main/java/model/dao/JanggiBoardDao.java
+++ src/main/java/model/dao/JanggiBoardDao.java
@@ -15,80 +15,89 @@ public final class JanggiBoardDao {
     private static final String USERNAME = "user"; //  MySQL 서버 아이디
     private static final String PASSWORD = "password"; // MySQL 서버 비밀번호
 
+    private final Connection connection;
+
+    public JanggiBoardDao() {
+        this.connection = getConnection();
+
+    }
+
+
     public Connection getConnection() {
         // 드라이버 연결
         try {
             System.out.println("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION);
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+            Connection janggiBoardDBConnection = DriverManager.getConnection(
+                    "jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+            janggiBoardDBConnection.setAutoCommit(false);
+            return janggiBoardDBConnection;
         } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
+            System.err.println("[ERROR] DB 연결 오류:" + e.getMessage());
             e.printStackTrace();
             return null;
         }
+    }
+
+    public void rollback() throws SQLException {
+        connection.rollback();
     }
 
     public void updateJanggiGame(int gameId, List<PieceDto> pieces) {
         final var deleteGameQuery = "DELETE FROM JanggiBoard WHERE game_id = ?";
         final var insertGameQuery = "INSERT INTO JanggiBoard (game_id, x_pos, y_pos, team_name, piece_type)VALUES(?, ?, ?, ?, ?)";
 
-        try (final var connection = getConnection()) {
-            try (final var deleteStatement = connection.prepareStatement(deleteGameQuery)) {
-                deleteStatement.setInt(1, gameId);
-                deleteStatement.executeUpdate();
-            }
-
-            try (final var preparedStatement = connection.prepareStatement(insertGameQuery)) {
-                for (final var piece : pieces) {
-                    preparedStatement.setInt(1, gameId);
-                    preparedStatement.setInt(2, piece.x());
-                    preparedStatement.setInt(3, piece.y());
-                    preparedStatement.setString(4, piece.team());
-                    preparedStatement.setString(5, piece.pieceName());
-
-                    preparedStatement.addBatch();
-                }
-                preparedStatement.executeBatch();
-            }
-
+        try (final var deleteStatement = connection.prepareStatement(deleteGameQuery)) {
+            deleteStatement.setInt(1, gameId);
+            deleteStatement.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("[ERROR] DB 삭제 오류 :" + e.getMessage());
         }
 
+        try (final var preparedStatement = connection.prepareStatement(insertGameQuery)) {
+            for (final var piece : pieces) {
+                preparedStatement.setInt(1, gameId);
+                preparedStatement.setInt(2, piece.x());
+                preparedStatement.setInt(3, piece.y());
+                preparedStatement.setString(4, piece.team());
+                preparedStatement.setString(5, piece.pieceName());
+
+                preparedStatement.addBatch();
+            }
+            preparedStatement.executeBatch();
+        } catch (SQLException e) {
+            System.out.println("[ERROR] DB 추가 오류 :" + e.getMessage());
+        }
     }
 
     public void deleteJanggiGame(int gameId) {
         final var deleteGameQuery = "DELETE FROM JanggiBoard WHERE game_id = ?";
-        try (final var connection = getConnection()) {
-            try (final var deleteStatement = connection.prepareStatement(deleteGameQuery)) {
-                deleteStatement.setInt(1, gameId);
-                deleteStatement.executeUpdate();
-            }
+
+        try (final var deleteStatement = connection.prepareStatement(deleteGameQuery)) {
+            deleteStatement.setInt(1, gameId);
+            deleteStatement.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("[ERROR] DB 삭제 오류 :" + e.getMessage());
         }
     }
 
     public boolean existJanggiGame(int gameId) {
         final var existGameQuery = "SELECT 1 FROM JanggiBoard WHERE game_id = ? LIMIT 1";
-        try (final var connection = getConnection()) {
-            try (final var existStatement = connection.prepareStatement(existGameQuery)) {
-                existStatement.setInt(1, gameId);
-                return existStatement.executeQuery().next();
-            }
+
+        try (final var existStatement = connection.prepareStatement(existGameQuery)) {
+            existStatement.setInt(1, gameId);
+            return existStatement.executeQuery().next();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("[ERROR] DB 데이터 탐색 오류 :" + e.getMessage());
         }
+        return false;
     }
 
     public List<PieceDto> findByGameId(int gameId) {
         final var query = "SELECT x_pos, y_pos, team_name, piece_type FROM JanggiBoard WHERE game_id = ?";
         final List<PieceDto> results = new ArrayList<>();
 
-        try (final var connection = getConnection();
-             final var preparedStatement = connection.prepareStatement(query)) {
-
+        try (final var preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, gameId);
-
             try (final var resultSet = preparedStatement.executeQuery()) {
                 while (resultSet.next()) {
                     int xPos = resultSet.getInt("x_pos");
@@ -101,10 +110,9 @@ public final class JanggiBoardDao {
                 }
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println("[ERROR] DB 데이터 탐색 오류 :" + e.getMessage());
         }
         return results;
     }
-
 
 }
--- src/test/java/model/dao/JanggiBoardDaoTest.java
+++ src/test/java/model/dao/JanggiBoardDaoTest.java
@@ -5,13 +5,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.sql.SQLException;
 import model.janggiboard.JanggiBoard;
 import model.janggiboard.JanggiBoardSetUp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class JanggiBoardDaoTest {
 
     private static final int TEST_GAME_ID = 999;
-    private final JanggiBoardDao janggiboardDao = new JanggiBoardDao();
+    private JanggiBoardDao janggiboardDao;
+
+    @BeforeEach
+    void setUp() {
+        janggiboardDao = new JanggiBoardDao();
+    }
+
+    @AfterEach
+    void rollbackJanggiGame() throws SQLException {
+        janggiboardDao.rollback();
+    }
 
     @Test
     void connection() throws SQLException {
@@ -21,20 +33,24 @@ class JanggiBoardDaoTest {
     }
 
     @Test
-    void updateJanggiGameTest() throws SQLException {
+    void updateJanggiGameTest() {
         JanggiBoard janggiBoard = new JanggiBoard(JanggiBoardSetUp.DEFAULT_SETUP);
 
         janggiboardDao.updateJanggiGame(TEST_GAME_ID, janggiBoard.getAlivePieces());
     }
 
     @Test
-    @DisplayName("게임 정보 존재 여부 테스트 - updateJanggiGameTest 사전 호출 필수")
-    void existJanggiGameTest() throws SQLException {
+    @DisplayName("게임 정보 존재 여부 테스트")
+    void existJanggiGameTest() {
+        assertThat(janggiboardDao.existJanggiGame(TEST_GAME_ID)).isFalse();
+
+        janggiboardDao.updateJanggiGame(TEST_GAME_ID, new JanggiBoard(JanggiBoardSetUp.DEFAULT_SETUP).getAlivePieces());
+
         assertThat(janggiboardDao.existJanggiGame(TEST_GAME_ID)).isTrue();
     }
 
     @Test
-    void deleteJanggiGameTest() throws SQLException {
+    void deleteJanggiGameTest() {
         janggiboardDao.deleteJanggiGame(TEST_GAME_ID);
     }
 
