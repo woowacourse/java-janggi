051a4cfd4068158fb73ad8272f74497784680936
ykmxxi <ykmxxi97@gmail.com>
2025-03-28 21:36:20 +0900

    test: 차례 확인과 포의 위치 확인 테스트 추가

--- src/test/java/board/BoardTest.java
+++ src/test/java/board/BoardTest.java
@@ -2,13 +2,19 @@ package board;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import game.Turn;
+import piece.Cannon;
 import piece.Chariot;
 import piece.Piece;
 import piece.Soldier;
@@ -23,6 +29,46 @@ class BoardTest {
         Board board = new Board(Map.of(position, piece));
 
         assertThat(board.findPieceByPosition(position)).isEqualTo(new Chariot(Team.BLUE));
+    }
+
+    @Test
+    void 장기_게임은_초나라부터_시작한다() {
+        Position position = new Position(4, 4);
+        Board board = new Board(Map.of(position, new Soldier(Team.BLUE)));
+        Turn turn = new Turn();
+
+        assertDoesNotThrow(() -> board.isValidTurn(position, turn));
+    }
+
+    @Test
+    void 초나라_다음_차례는_한나라이다() {
+        Position position = new Position(4, 4);
+        Board board = new Board(Map.of(position, new Soldier(Team.RED)));
+        Turn turn = new Turn();
+        turn.increaseRound();
+
+        assertDoesNotThrow(() -> board.isValidTurn(position, turn));
+    }
+
+    @Test
+    void 초나라_차례에_한나라_기물을_선택하면_시작할_수_없다() {
+        Position position = new Position(4, 4);
+        Board board = new Board(Map.of(position, new Soldier(Team.RED)));
+        Turn turn = new Turn();
+
+        assertThatThrownBy(() -> board.isValidTurn(position, turn))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 한나라_차례에_초나라_기물을_선택하면_시작할_수_없다() {
+        Position position = new Position(4, 4);
+        Board board = new Board(Map.of(position, new Soldier(Team.BLUE)));
+        Turn turn = new Turn();
+        turn.increaseRound();
+
+        assertThatThrownBy(() -> board.isValidTurn(position, turn))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -87,6 +133,23 @@ class BoardTest {
 
         assertThatThrownBy(() -> board.findPieceByPosition(new Position(2, 2)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @MethodSource
+    @ParameterizedTest
+    void 위치가_포의_위치인지_알려준다(Piece piece, boolean expected) {
+        Position position = new Position(4, 4);
+        Board board = new Board(Map.of(position, piece));
+
+        assertThat(board.isCannonPosition(position)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> 위치가_포의_위치인지_알려준다() {
+        return Stream.of(
+                Arguments.of(new Cannon(Team.RED), true),
+                Arguments.of(new Chariot(Team.RED), false)
+        );
     }
 
 }
