b00d08bc398cba3e9434e66fa471d8b23eb9ddc6
ykmxxi <ykmxxi97@gmail.com>
2025-03-28 21:41:03 +0900

    refactor: 테스트 메서드명 구체화

--- src/test/java/piece/DirectionTest.java
+++ src/test/java/piece/DirectionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class DirectionTest {
 
     @Test
-    void 방향은_위_아래_왼쪽_오른쪽이_있다() {
+    void 직진_방향은_위_아래_왼쪽_오른쪽이_있다() {
         assertThat(Direction.getStraightDirection()).contains(
                 Direction.LEFT,
                 Direction.RIGHT,
