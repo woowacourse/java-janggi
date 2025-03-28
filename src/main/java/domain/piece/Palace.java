b82e19ff29f6a25f35002784853b244043b6dae6
rladmstn <oz33177351@gmail.com>
2025-03-29 02:10:55 +0900

    feat: 궁성에 대한 방향 추가 기능 구현

---
+++ src/main/java/domain/piece/Palace.java
@@ -0,0 +1,45 @@
+package domain.piece;
+
+import domain.direction.Direction;
+import domain.direction.Vector;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Palace {
+
+    public static final int MIN_ROW = 4;
+    public static final int MAX_ROW = 6;
+
+    public static final int HAN_MIN_COLUMN = 1;
+    public static final int HAN_MAX_COLUMN = 3;
+    public static final int CHO_MIN_COLUMN = 8;
+    public static final int CHO_MAX_COLUMN = 10;
+
+    public static Set<Direction> getMovableDirectionInPalace(final Position position) {
+        if (position.getRow() == MAX_ROW && (position.getColumn() == HAN_MIN_COLUMN
+                || position.getColumn() == CHO_MIN_COLUMN)) {
+            return Set.of(new Direction(List.of(Vector.DOWN_LEFT)));
+        }
+        if (position.getRow() == MIN_ROW && (position.getColumn() == HAN_MIN_COLUMN
+                || position.getColumn() == CHO_MIN_COLUMN)) {
+            return Set.of(new Direction(List.of(Vector.DOWN_RIGHT)));
+        }
+        if (position.getRow() == MAX_ROW && (position.getColumn() == HAN_MAX_COLUMN
+                || position.getColumn() == CHO_MAX_COLUMN)) {
+            return Set.of(new Direction(List.of(Vector.UP_LEFT)));
+        }
+        if (position.getRow() == MIN_ROW && (position.getColumn() == HAN_MAX_COLUMN
+                || position.getColumn() == CHO_MAX_COLUMN)) {
+            return Set.of(new Direction(List.of(Vector.UP_RIGHT)));
+        }
+        if (position.getRow() == (MAX_ROW - 1) && (position.getColumn() == (HAN_MAX_COLUMN - 1)
+                || position.getColumn() == (CHO_MAX_COLUMN - 1))) {
+            return Vector.getDiagonals().stream()
+                    .map(vector -> new Direction(List.of(vector)))
+                    .collect(Collectors.toSet());
+        }
+        return new HashSet<>(); // 이 중에서도 없으면 아무것도 추가 안해도 됨
+    }
+}
