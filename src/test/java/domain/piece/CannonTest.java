7cb4d9911dd33346f8fc3f12efc69d315995f079
ryan-dia <cjfdnjs158@naver.com>
2025-03-28 16:05:11 +0900

    test: 포의 궁성내 이동 테스트 작성
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/test/java/domain/piece/CannonTest.java
+++ src/test/java/domain/piece/CannonTest.java
@@ -49,4 +49,54 @@ class CannonTest {
                     .contains(Point.newInstance(1, 3), Point.newInstance(1, 5), Point.newInstance(1, 8));
         });
     }
+
+    @ParameterizedTest
+    @CsvSource({"3,0,5,2", "3,2,5,0", "5,2,3,0", "5,0,3,2"})
+    void 포의_궁성_내_이동_가능하면_true(final int x1, final int y1, final int x2, final int y2) {
+
+        // given
+        final Cannon cannon = PieceFactory.createGreenTeam(Cannon::new);
+
+        // when
+        final Point firstPoint = Point.newInstance(x1, y1);
+        final Point secondPoint = Point.newInstance(x2, y2);
+
+        // then
+        assertThat(cannon.isMovable(firstPoint, secondPoint)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3,0,6,3", "4,1,6,3", "5,2,6,3", "3,2,1,4", "4,1,2,3", "3,0,4,1"})
+    void 포의_궁성_내_이동_불가능하면_false(final int x1, final int y1, final int x2, final int y2) {
+
+        // given
+        final Cannon cannon = PieceFactory.createGreenTeam(Cannon::new);
+
+        // when
+        final Point firstPoint = Point.newInstance(x1, y1);
+        final Point secondPoint = Point.newInstance(x2, y2);
+
+        // then
+        assertThat(cannon.isMovable(firstPoint, secondPoint)).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3,0,5,2,4,1", "3,2,5,0,4,1", "5,9,3,7,4,8", "5,7,3,9,4,8"})
+    void 포의_궁성_내_이동_가능_경로_반환(final int x1, final int y1, final int x2, final int y2, final int x3, final int y3) {
+
+        // given
+        final Cannon cannon = PieceFactory.createGreenTeam(Cannon::new);
+
+        // when
+        final Point firstPoint = Point.newInstance(x1, y1);
+        final Point secondPoint = Point.newInstance(x2, y2);
+
+        final List<Point> points = cannon.calculatePossiblePoint(firstPoint, secondPoint);
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(points).hasSize(1);
+            softly.assertThat(points.getFirst()).isEqualTo(Point.newInstance(x3, y3));
+        });
+    }
 }
