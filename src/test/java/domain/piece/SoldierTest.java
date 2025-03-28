b2055c576f8a1c24d790669cb0dbcccf2c636178
ryan-dia <cjfdnjs158@naver.com>
2025-03-28 17:49:20 +0900

    test: 졸의 궁성 내 이동 테스트 수정
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/test/java/domain/piece/SoldierTest.java
+++ src/test/java/domain/piece/SoldierTest.java
@@ -10,9 +10,12 @@ import utils.PieceFactory;
 class SoldierTest {
 
     @ParameterizedTest
-    @CsvSource({"1,1,1,0,true", "1,1,2,1,true", "1,1,0,1,true", "1,1,1,2,false", "1,1,5,6,false"})
-    void 레드팀일_때_말이_움직일_수_있으면_true_아니면_false를_반환한다(final int x1, final int y1, final int x2, final int y2,
-                                                  final boolean expected) {
+    @CsvSource({
+            "1,1,1,0,true", "1,1,2,1,true", "1,1,0,1,true", "1,1,1,2,false", "1,1,5,6,false",
+            "3,2,4,1,true", "5,2,4,1,true", "4,1,3,0,true", "4,1,5,1,true", "3,3,2,2,false", "3,2,2,1,false"
+    })
+    void 한나라팀일_때_말이_움직일_수_있으면_true_아니면_false를_반환한다(final int x1, final int y1, final int x2, final int y2,
+                                                   final boolean expected) {
 
         // given
         final Soldier soldier = PieceFactory.createRedTeam(Soldier::new);
@@ -26,9 +29,12 @@ class SoldierTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"1,1,1,2,true", "1,1,2,1,true", "1,1,0,1,true", "1,1,1,0,false", "1,1,5,6,false"})
-    void 그린팀일_때_말이_움직일_수_있으면_true_아니면_false를_반환한다(final int x1, final int y1, final int x2, final int y2,
-                                                  final boolean expected) {
+    @CsvSource({
+            "1,1,1,2,true", "1,1,2,1,true", "1,1,0,1,true", "1,1,1,0,false", "1,1,5,6,false",
+            "3,7,4,8,true", "5,7,4,8,true", "4,8,3,9,true", "4,8,5,9,true", "3,7,5,9,false", "3,6,4,7,false"
+    })
+    void 초나라팀일_때_말이_움직일_수_있으면_true_아니면_false를_반환한다(final int x1, final int y1, final int x2, final int y2,
+                                                   final boolean expected) {
 
         // given
         final Soldier soldier = PieceFactory.createGreenTeam(Soldier::new);
