f622d41d20d367f027c9f501f06fc2708bef46ca
kjyyjk <rlawnsdud920@gmail.com>
2025-03-28 16:07:40 +0900

    docs: 궁성과 점수 계산에 대한 기능 목록 추가

---
+++ src/test/java/janggi/piece/GeneralTest.java
@@ -0,0 +1,19 @@
+package janggi.piece;
+
+import org.junit.jupiter.api.Test;
+
+class GeneralTest {
+
+    @Test
+    void aa() {
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+}
