a73888f7e3a2cadde5973be1954528aa3634e375
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-28 21:37:50 +0900

    refactor: 위치와 movement 연결

--- src/main/java/position/Position.java
+++ src/main/java/position/Position.java
@@ -20,26 +20,8 @@ public record Position(int x, int y) {
         return Math.sqrt(dx * dx + dy * dy);
     }
 
-    // todo : row, column인 Position 이랑 int인 movement랑 자료형 어떻게 연결해줄 것인지 고민해보기
     public Position move(Movement movement) {
-        if (movement == Movement.UP) {
-            return new Position(x, y - 1);
-        } else if (movement == Movement.DOWN) {
-            return new Position(x, y + 1);
-        } else if (movement == Movement.RIGHT) {
-            return new Position(x + 1, y);
-        } else if (movement == Movement.LEFT) {
-            return new Position(x - 1, y);
-        } else if (movement == Movement.LEFT_UP) {
-            return new Position(x - 1, y - 1);
-        } else if (movement == Movement.LEFT_DOWN) {
-            return new Position(x - 1, y + 1);
-        } else if (movement == Movement.RIGHT_DOWN) {
-            return new Position(x + 1, y + 1);
-        } else if (movement == Movement.RIGHT_UP) {
-            return new Position(x + 1, y - 1);
-        }
-        return new Position(x, y);
+        return new Position(x + movement.x(), y + movement.y());
     }
 
     public boolean isSameLine(final Position dest) {
