8fbbd550dd4eb216a51692715d4ce6aefaceffec
junhaa <2171326@hansung.ac.kr>
2025-03-29 02:38:24 +0900

    feat: 플레이어의 이름과 팀을 데이터베이스에 저장하는 기능 구현

---
+++ src/main/java/dao/PlayerDao.java
@@ -0,0 +1,65 @@
+package dao;
+
+import domain.TeamType;
+import domain.player.Player;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerDao {
+    private final Connection connection;
+
+    public PlayerDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public long savePlayer(Player player, Long gameId) {
+        String query = "INSERT INTO player (game_id, name, team_type) VALUES (?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setLong(1, gameId);
+            preparedStatement.setString(2, player.getName());
+            preparedStatement.setString(3, player.getTeamType().name());
+            preparedStatement.executeUpdate();
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            generatedKeys.next();
+            return generatedKeys.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 접근 도중 예외가 발생했습니다.");
+        }
+    }
+
+    public List<Player> findPlayersByGameId(Long gameId) {
+        String query = "SELECT * FROM player WHERE game_id = ?";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setLong(1, gameId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return mapTeamPlayers(resultSet);
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 접근 도중 예외가 발생했습니다.");
+        }
+    }
+
+    private List<Player> mapTeamPlayers(ResultSet resultSet) {
+        List<Player> players = new ArrayList<>();
+
+        try {
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                TeamType teamType = TeamType.valueOf(resultSet.getString("team_type"));
+                players.add(new Player(name, teamType));
+            }
+            return players;
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 접근 도중 예외가 발생했습니다.");
+        }
+    }
+}
---
+++ src/test/java/dao/PlayerDaoTest.java
@@ -0,0 +1,81 @@
+package dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import dao.fixture.JanggiGameTestFixture;
+import domain.TeamType;
+import domain.player.Player;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.DBConnectionUtil;
+
+class PlayerDaoTest {
+
+    private PlayerDao playerDao;
+    private Connection connection;
+
+    @BeforeEach
+    void setup() throws SQLException {
+        connection = DBConnectionUtil.getConnection();
+        connection.setAutoCommit(false);
+        playerDao = new PlayerDao(connection);
+    }
+
+    @AfterEach
+    void rollback() throws SQLException {
+        connection.rollback();
+        connection.close();
+    }
+
+    @Test
+    @DisplayName("플레이어 데이터를 저장한다")
+    void savePlayerTest() throws SQLException {
+        // given
+        long gameId = JanggiGameTestFixture.saveNewJanggiGame(connection);
+        String name = "루키";
+        TeamType team = TeamType.HAN;
+        Player player = new Player(name, team);
+
+        // when & then
+        assertThatCode(() -> playerDao.savePlayer(player, gameId))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("게임에 해당하는 플레이어들의 데이터를 조회한다")
+    void findPlayersByGameIdTest() throws SQLException {
+        // given
+        long gameId = JanggiGameTestFixture.saveNewJanggiGame(connection);
+
+        String choPlayerName = "루키";
+        TeamType choPlayerTeam = TeamType.CHO;
+        Player choPlayer = new Player(choPlayerName, choPlayerTeam);
+
+        String hanPlayerName = "코기";
+        TeamType hanPlayerTeam = TeamType.HAN;
+        Player hanPlayer = new Player(hanPlayerName, hanPlayerTeam);
+
+        playerDao.savePlayer(choPlayer, gameId);
+        playerDao.savePlayer(hanPlayer, gameId);
+
+        // when
+        List<Player> players = playerDao.findPlayersByGameId(gameId);
+
+        //then
+        Player findChoPlayer = players.getFirst();
+        Player findHanPlayer = players.getLast();
+        assertAll(
+                () -> assertThat(findChoPlayer.getName()).isEqualTo("루키"),
+                () -> assertThat(findHanPlayer.getName()).isEqualTo("코기"),
+                () -> assertThat(findChoPlayer.getTeamType()).isEqualTo(TeamType.CHO),
+                () -> assertThat(findHanPlayer.getTeamType()).isEqualTo(TeamType.HAN)
+        );
+    }
+}
