b29b5fd7c0fbd9c2bcc3cefc0ba6039d318f4780
KJungW <sagakkb0707@naver.com>
2025-03-28 12:03:20 +0900

    refactor(piece): 궁과 방향 열거형의 관계성 제거

--- src/main/java/janggi/piece/Gung.java
+++ src/main/java/janggi/piece/Gung.java
@@ -1,11 +1,34 @@
 package janggi.piece;
 
-import janggi.direction.OneStepDirection;
 import janggi.setting.CampType;
+import janggi.setting.GungSungCoordinate;
+import janggi.value.Path;
 import janggi.value.Position;
+import janggi.value.RelativePath;
+import janggi.value.RelativePosition;
 import java.util.List;
+import java.util.Optional;
 
 public class Gung extends Piece {
+
+    private static final List<RelativePath> RELATIVE_PATH = List.of(
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(-1, 0))),
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(1, 0))),
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(0, 1))),
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(0, -1))),
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(1, 1))),
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(1, -1))),
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(-1, 1))),
+            new RelativePath(
+                    List.of(new RelativePosition(0, 0), new RelativePosition(-1, -1)))
+    );
 
     public Gung(final Position position) {
         super(PieceType.GUNG, position);
@@ -26,19 +49,30 @@ public class Gung extends Piece {
 
     @Override
     public boolean ableToMove(Position destination, List<Piece> enemy, List<Piece> allies) {
-        OneStepDirection direction = OneStepDirection.parse(getPosition(), destination);
-
-        boolean followRuleOfMove = checkRuleOfMove(direction);
-        boolean existAlliesInDestination = existPieceInDestination(destination, allies);
-        return followRuleOfMove && !existAlliesInDestination;
+        Optional<Path> optionalPath = calculatePath(destination);
+        if (optionalPath.isEmpty()) {
+            return false;
+        }
+        Path path = optionalPath.get();
+        boolean isPathInGungSung = isPathInGungSung(path);
+        boolean existAlliesInDestination = existPieceInPosition(destination, allies);
+        return isPathInGungSung && !existAlliesInDestination;
     }
 
-    private boolean checkRuleOfMove(OneStepDirection direction) {
-        return direction != OneStepDirection.NONE;
+    private Optional<Path> calculatePath(Position destination) {
+        return RELATIVE_PATH.stream()
+                .filter(route -> route.getDestination(getPosition()).equals(destination))
+                .map(route -> route.calculatePath(getPosition()))
+                .findFirst();
     }
 
-    private boolean existPieceInDestination(Position destination, List<Piece> allies) {
-        return allies.stream()
-                .anyMatch(piece -> piece.getPosition().equals(destination));
+    private boolean isPathInGungSung(Path path) {
+        boolean isStartInRange = GungSungCoordinate.isInRange(path.getStart());
+        boolean isEndInRange = GungSungCoordinate.isInRange(path.getEnd());
+        return isStartInRange && isEndInRange;
+    }
+
+    private boolean existPieceInPosition(Position position, List<Piece> pieces) {
+        return pieces.stream().anyMatch(piece -> position.equals(piece.getPosition()));
     }
 }
