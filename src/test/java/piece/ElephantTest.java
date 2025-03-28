d567c19d4beecfb49dc1b9fd938f57b7c47a5102
byesol <bsj5967@naver.com>
2025-03-28 09:18:54 +0900

    refactor: 테스트 네이밍, parameter값 리팩토링

--- src/test/java/piece/ElephantTest.java
+++ src/test/java/piece/ElephantTest.java
@@ -3,10 +3,12 @@ package piece;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static piece.Country.Cho;
+import static testutil.TestConstant.A1;
 import static testutil.TestConstant.B3;
 import static testutil.TestConstant.B7;
 import static testutil.TestConstant.C2;
 import static testutil.TestConstant.C8;
+import static testutil.TestConstant.D3;
 import static testutil.TestConstant.D5;
 import static testutil.TestConstant.E4;
 import static testutil.TestConstant.E5;
@@ -31,14 +33,16 @@ public class ElephantTest {
 
     static Stream<Arguments> VALID_MOVE_POSITIONS() {
         return Stream.of(
-                Arguments.of(C8),
-                Arguments.of(B7),
-                Arguments.of(C2),
-                Arguments.of(B3),
-                Arguments.of(G8),
-                Arguments.of(G2),
-                Arguments.of(H7),
-                Arguments.of(H3)
+                Arguments.of(E5, C8),
+                Arguments.of(E5, B7),
+                Arguments.of(E5, C2),
+                Arguments.of(E5, B3),
+                Arguments.of(E5, G8),
+                Arguments.of(E5, G2),
+                Arguments.of(E5, H7),
+                Arguments.of(E5, H3),
+                Arguments.of(E5, H3),
+                Arguments.of(A1, D3)
         );
     }
 
@@ -52,31 +56,31 @@ public class ElephantTest {
 
     @ParameterizedTest
     @MethodSource("VALID_MOVE_POSITIONS")
-    void 상은_시작지와_목적지에_따른_이동경로를_반환한다(Position toPosition) {
+    void 상은_정해진_루트로_이동_가능하다(Position fromPosition, Position toPosition) {
         // given
         Elephant elephant = new Elephant(Cho);
         Board board = new Board(Map.of());
 
         // then
-        assertThatCode(() -> elephant.canMove(E5, toPosition, board)).doesNotThrowAnyException();
+        assertThatCode(() -> elephant.canMove(fromPosition, toPosition, board)).doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @MethodSource("INVALID_MOVE_POSITIONS")
-    void 상은_정해진_루트가_아니면_이동할_수_없다(Position toPosition) {
+    void 상은_정해진_루트가_아니면_이동할_수_없다(Position fromPosition, Position toPosition) {
         // given
         Elephant elephant = new Elephant(Cho);
         Board board = new Board(Map.of());
 
         // then
-        assertThatThrownBy(() -> elephant.canMove(E5, toPosition, board))
+        assertThatThrownBy(() -> elephant.canMove(fromPosition, toPosition, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 위치로 이동할 수 없습니다.");
     }
 
     @ParameterizedTest
     @MethodSource("VALID_MOVE_POSITIONS")
-    void 상은_중간에_기물이_있으면_이동할_수_없다(Position toPosition) {
+    void 상은_중간에_기물이_있으면_이동할_수_없다(Position fromPosition, Position toPosition) {
         // given
         Elephant elephant = new Elephant(Cho);
         Board board = new Board(Map.of(
@@ -87,8 +91,23 @@ public class ElephantTest {
         ));
 
         // then
-        assertThatThrownBy(() -> elephant.canMove(E5, toPosition, board))
+        assertThatThrownBy(() -> elephant.canMove(fromPosition, toPosition, board))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중간에 기물이 있어 갈 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("VALID_MOVE_POSITIONS")
+    void 상은_마지막에_아군이_있으면_이동할_수_없다(Position fromPosition, Position toPosition) {
+        // given
+        Elephant elephant = new Elephant(Cho);
+        Board board = new Board(Map.of(
+                toPosition, new Elephant(Cho)
+        ));
+
+        // then
+        assertThatThrownBy(() -> elephant.canMove(fromPosition, toPosition, board))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("아군 기물이 위치해 있습니다. ");
     }
 }
