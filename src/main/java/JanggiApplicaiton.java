812f75025365b7ee3073ddf8cf68e2e74420588b
[CLOUD4] 백승주 <bsj5967@naver.com>
2025-03-29 22:23:18 +0900

    feat: outputview  구현

---
+++ src/main/java/JanggiApplicaiton.java
@@ -0,0 +1,14 @@
+import game.JanggiGame;
+import view.InputView;
+import view.OutputView;
+
+public class JanggiApplicaiton {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        JanggiGame janggiGame = new JanggiGame(inputView, outputView);
+        janggiGame.start();
+    }
+}
