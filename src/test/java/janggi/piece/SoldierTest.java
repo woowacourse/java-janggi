5697544a172718d0be80fea0a303f3dd01522445
ppparkta <pppppark2@gmail.com>
2025-03-29 16:39:59 +0900

    test: 졸 기물 테스트 보강

--- src/test/java/janggi/piece/SoldierTest.java
+++ src/test/java/janggi/piece/SoldierTest.java
@@ -10,19 +10,38 @@ import org.junit.jupiter.api.Test;
 
 class SoldierTest {
     @Test
-    @DisplayName("졸은 수직/수평 한칸만 움직일 수 있다")
+    @DisplayName("초나라 졸은 앞, 좌우 한칸만 움직일 수 있다")
     void test1() {
         // given
         Soldier soldierUnitRule = new Soldier(Team.CHO);
 
         // when
-        List<Route> routes = soldierUnitRule.calculateRoutes(new Position(0, 0));
+        List<Route> routes = soldierUnitRule.calculateRoutes(new Position(1, 9));
 
         // then
         Assertions.assertThat(routes).isNotEmpty();
         Assertions.assertThat(routes).containsOnly(
-                Route.of(List.of(new Position(1, 0))),
-                Route.of(List.of(new Position(0, 1)))
+                Route.of(List.of(new Position(0, 9))),
+                Route.of(List.of(new Position(1, 8))),
+                Route.of(List.of(new Position(2, 9)))
+        );
+    }
+
+    @Test
+    @DisplayName("한나라 졸은 뒤, 좌우 한칸만 움직일 수 있다")
+    void test2() {
+        // given
+        Soldier soldierUnitRule = new Soldier(Team.HAN);
+
+        // when
+        List<Route> routes = soldierUnitRule.calculateRoutes(new Position(1, 0));
+
+        // then
+        Assertions.assertThat(routes).isNotEmpty();
+        Assertions.assertThat(routes).containsOnly(
+                Route.of(List.of(new Position(2, 0))),
+                Route.of(List.of(new Position(1, 1))),
+                Route.of(List.of(new Position(0, 0)))
         );
     }
 }
