9dc0a2819c38fd75d0b56722583b5653212f4f8c
ppparkta <pppppark2@gmail.com>
2025-03-29 20:20:45 +0900

    feat: 사 기물 추가

---
+++ src/main/java/janggi/piece/pieces/Scholar.java
@@ -0,0 +1,30 @@
+package janggi.piece.pieces;
+
+import janggi.piece.PieceType;
+import janggi.piece.Team;
+import janggi.position.Position;
+import janggi.position.Route;
+import java.util.List;
+
+public class Scholar implements Piece {
+    private final Team team;
+
+    public Scholar(Team team) {
+        this.team = team;
+    }
+
+    @Override
+    public List<Route> calculateRoutes(Position position) {
+        return List.of();
+    }
+
+    @Override
+    public PieceType getType() {
+        return PieceType.SCHOLAR;
+    }
+
+    @Override
+    public Team getTeam() {
+        return team;
+    }
+}
