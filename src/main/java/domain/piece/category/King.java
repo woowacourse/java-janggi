8e0c796b4d2275be91a8905f0e51d39da3d1fdbf
taek2222 <oot309@daum.net>
2025-03-29 22:17:08 +0900

    feat: 궁성 내부 대각선 이동인 경우의 이동 및 예외 기능 추가

--- src/main/java/domain/piece/category/King.java
+++ src/main/java/domain/piece/category/King.java
@@ -2,8 +2,10 @@ package domain.piece.category;
 
 import domain.MoveInfos;
 import domain.direction.Directions;
+import domain.direction.PieceDirection;
 import domain.piece.Piece;
 import domain.spatial.Position;
+import java.util.List;
 
 public class King extends Piece {
 
@@ -11,6 +13,19 @@ public class King extends Piece {
 
     public King(final Position position, final Directions directions) {
         super(position, directions);
+    }
+
+    @Override
+    public List<Position> getPaths(final Position target) {
+        List<Position> paths = directions.getPaths(position, target);
+        if (position.isWithinPalace()) {
+            paths = PieceDirection.DIAGONAL.get().getPaths(position, target);
+        }
+        validatePaths(paths);
+        if (position.isWithinPalace()) {
+            validateDiagonalPaths(paths);
+        }
+        return paths;
     }
 
     @Override
