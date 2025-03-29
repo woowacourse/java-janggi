9fdc38a41c64299f8ecfed14881b1a96b5a9c9a4
changuii <rhljh7410@gmail.com>
2025-03-29 14:08:20 +0900

    test: ElephantTest 리팩터링

--- src/test/java/janggi/piece/ElephantTest.java
+++ src/test/java/janggi/piece/ElephantTest.java
@@ -6,53 +6,79 @@ import janggi.board.Board;
 import janggi.coordinate.JanggiPosition;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ElephantTest {
 
-    @DisplayName("Elephant는 직-대-대 방향으로 이동할 수 있다.")
-    @Test
-    void elephant() {
-        // given
-        final Piece elephantPiece = new Elephant(Country.CHO);
-        final JanggiPosition now = new JanggiPosition(1, 1);
-        final JanggiPosition ableDest = new JanggiPosition(4, 3);
-        final JanggiPosition notAbleDest = new JanggiPosition(1, 2);
-        final Board board = new Board(new HashMap<>());
+    private final Elephant elephant = new Elephant(Country.CHO);
+    private final Map<JanggiPosition, Piece> janggiBoard = new HashMap<>();
 
-        // when
-        final boolean actual1 = elephantPiece.isAbleToMove(now, ableDest, board);
-        final boolean actual2 = elephantPiece.isAbleToMove(now, notAbleDest, board);
-
-        // then
-        org.junit.jupiter.api.Assertions.assertAll(
-                () -> assertThat(actual1).isTrue(),
-                () -> assertThat(actual2).isFalse()
-        );
+    @BeforeEach
+    void initJanggiBoard() {
+        janggiBoard.clear();
     }
 
-    @DisplayName("Elephant는 직 또는 직-대 방향에 기물이 존재하면 이동할 수 없다.")
-    @Test
-    void elephant1() {
-        // given
-        final Piece elephantPiece = new Elephant(Country.CHO);
-        final JanggiPosition now = new JanggiPosition(2, 2);
-        final JanggiPosition dest1 = new JanggiPosition(4, 5);
-        final JanggiPosition dest2 = new JanggiPosition(5, 4);
-
-
-        final Board board = new Board(Map.of(
-                new JanggiPosition(2, 3), new Cannon(Country.HAN),
-                new JanggiPosition(4, 3), new Cannon(Country.HAN)
-        ));
-
-        // when
-        final boolean actual1 = elephantPiece.isAbleToMove(now, dest1, board);
-        final boolean actual2 = elephantPiece.isAbleToMove(now, dest2, board);
-
-        // then
-        assertThat(actual1).isFalse();
-        assertThat(actual2).isFalse();
+    public void placePieceOnJanggiBoard(final JanggiPosition janggiPosition, final Piece piece) {
+        janggiBoard.put(janggiPosition, piece);
     }
+
+    @Nested
+    @DisplayName("이동 로직")
+    class CanMove {
+
+        @DisplayName("Elephant는 직-대-대 방향으로 이동할 수 있다.")
+        @ParameterizedTest
+        @MethodSource
+        void elephant(final JanggiPosition dest, final boolean expected) {
+            // given
+            final JanggiPosition now = new JanggiPosition(1, 1);
+            final Board board = new Board(janggiBoard);
+
+            // when
+            final boolean actual = elephant.canMove(now, dest, board);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        static Stream<Arguments> elephant() {
+            return Stream.of(
+                    Arguments.of(new JanggiPosition(4, 3), true),
+                    Arguments.of(new JanggiPosition(1, 2), false)
+            );
+        }
+
+        @DisplayName("Elephant는 직 또는 직-대 방향에 기물이 존재하면 이동할 수 없다.")
+        @ParameterizedTest
+        @MethodSource
+        void elephant1(final JanggiPosition dest, final JanggiPosition hurdlePosition) {
+            // given
+            final JanggiPosition now = new JanggiPosition(2, 2);
+
+            final Piece hurdle = new Chariot(Country.HAN);
+            placePieceOnJanggiBoard(hurdlePosition, hurdle);
+
+            final Board board = new Board(janggiBoard);
+
+            // when
+            final boolean actual = elephant.canMove(now, dest, board);
+
+            // then
+            assertThat(actual).isFalse();
+        }
+
+        static Stream<Arguments> elephant1() {
+            return Stream.of(
+                    Arguments.of(new JanggiPosition(4, 5), new JanggiPosition(2, 3)),
+                    Arguments.of(new JanggiPosition(5, 4), new JanggiPosition(4, 3))
+            );
+        }
+    }
+
 }
