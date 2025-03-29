4b51259d4a73f574238000da20d64b334b576baf
RaZel713 <razel7130@gmail.com>
2025-03-29 20:52:34 +0900

    feat: DB 접속 로직 구현

---
+++ src/test/java/dao/BoardDaoTest.java
@@ -0,0 +1,17 @@
+package dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class BoardDaoTest {
+    private final BoardDao boardDao = new BoardDao();
+
+    @Test
+    public void connection() throws SQLException {
+        try (final var connection = boardDao.getConnection()) {
+            assertThat(connection).isNotNull();
+        }
+    }
+}
