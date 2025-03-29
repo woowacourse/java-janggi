c935d99c61787a6d50daa5aba30ad3241a074ee6
ppparkta <pppppark2@gmail.com>
2025-03-29 21:54:05 +0900

    feat: 사 기물 움직임 구현

--- src/main/java/janggi/piece/pieces/Scholar.java
+++ src/main/java/janggi/piece/pieces/Scholar.java
@@ -1,30 +1,43 @@
 package janggi.piece.pieces;
 
+import janggi.board.Palace;
 import janggi.piece.PieceType;
 import janggi.piece.Team;
+import janggi.position.Direction;
 import janggi.position.Position;
 import janggi.position.Route;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Scholar implements Piece {
-    private final Team team;
-
-    public Scholar(Team team) {
-        this.team = team;
-    }
-
+public record Scholar(Team team) implements Piece {
     @Override
     public List<Route> calculateRoutes(Position position) {
-        return List.of();
+        List<Route> routes = new ArrayList<>();
+
+        List<Direction> allDirection = Direction.getAllDirection();
+        for (Direction direction : allDirection) {
+            applyGeneralProperty(position, direction, routes);
+        }
+        return routes;
+    }
+
+    private static void applyGeneralProperty(Position position, Direction direction, List<Route> routes) {
+        int column = direction.moveColumn(position.getColumn());
+        int row = direction.moveRow(position.getRow());
+        if (position.isSamePoint(new Position(column, row))) {
+            return;
+        }
+        if (!Palace.isInPalace(column, row)) {
+            return;
+        }
+        if (direction.idDiagonal() && !Palace.canDiagonalInPalace(position.getColumn(), position.getRow())) {
+            return;
+        }
+        routes.add(Route.of(List.of(new Position(column, row))));
     }
 
     @Override
     public PieceType getType() {
         return PieceType.SCHOLAR;
-    }
-
-    @Override
-    public Team getTeam() {
-        return team;
     }
 }
