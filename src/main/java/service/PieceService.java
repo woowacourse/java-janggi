26ee1eb313adaf95126b58d3e5e74cb40dc3eff6
LJM\eueo8 <eueo8259@gmail.com>
2025-03-29 17:49:48 +0900

    feat : PieceService 생성

---
+++ src/main/java/service/PieceService.java
@@ -0,0 +1,26 @@
+package service;
+
+import dao.PieceDao;
+import domain.game.JanggiGame;
+import dto.BoardDto;
+import java.util.Optional;
+
+public class PieceService {
+    private final PieceDao pieceDao;
+
+    public PieceService(PieceDao pieceDao) {
+        this.pieceDao = pieceDao;
+    }
+
+    public void createPieceTable(){
+        pieceDao.createPieceTableIfNotExists();
+    }
+
+    public void insertInitializePieceIfNotExists(JanggiGame janggiGame){
+        pieceDao.initializePieceIfNotExists(janggiGame.getBoard());
+    }
+
+    public Optional<BoardDto> getAlivePieces(){
+        return pieceDao.findByAllAlivePieces();
+    }
+}
