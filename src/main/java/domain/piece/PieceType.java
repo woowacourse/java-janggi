80ba1e7fdf7aea64a6ac8f36eb976794be1fc60b
ryan-dia <cjfdnjs158@naver.com>
2025-03-29 23:07:35 +0900

    feat: pieceType 생성 책임 추가
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/main/java/domain/piece/PieceType.java
+++ src/main/java/domain/piece/PieceType.java
@@ -1,12 +1,25 @@
 package domain.piece;
 
+import domain.Team;
+import java.util.function.Function;
+
 public enum PieceType {
-    GENERAL,
-    SOLDIER,
-    GUARD,
-    ELEPHANT,
-    HORSE,
-    CANNON,
-    CHARIOT;
+    GENERAL(General::new),
+    SOLDIER(Soldier::new),
+    GUARD(Guard::new),
+    ELEPHANT(Elephant::new),
+    HORSE(Horse::new),
+    CANNON(Cannon::new),
+    CHARIOT(Chariot::new);
+
+    private final Function<Team, Piece> function;
+
+    PieceType(final Function<Team, Piece> function) {
+        this.function = function;
+    }
+
+    public static Piece find(final PieceType pieceType, final Team team) {
+        return pieceType.function.apply(team);
+    }
 }
 
