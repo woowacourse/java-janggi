3f8ed27ab7ca6f37dd995e3f24b26862c17631fe
taek2222 <oot309@daum.net>
2025-03-29 23:33:31 +0900

    test: 플레이어 점수 증가 테스트 작성

---
+++ src/test/java/domain/PlayerTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import domain.piece.Score;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    void 플레이어의_점수를_증가한다() {
+        // given
+        Player player = new Player(Team.HAN, new Score(0));
+
+        // when
+        player.increaseScore(new Score(10));
+
+        // then
+        Assertions.assertThat(player.getScore().getValue()).isEqualTo(10);
+    }
+}
