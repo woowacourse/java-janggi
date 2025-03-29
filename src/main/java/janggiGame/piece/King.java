7f1b1f23677e5d9311f59f6ae824ac77b7f21986
RaZel713 <razel7130@gmail.com>
2025-03-29 12:29:14 +0900

    feat: 궁성 안에서 장의 대각선 이동 구현

--- src/main/java/janggiGame/piece/King.java
+++ src/main/java/janggiGame/piece/King.java
@@ -15,16 +15,39 @@ public class King extends Piece {
         int dx = origin.calculateRowChange(destination);
         int dy = origin.calculateColumnChange(destination);
 
+        if (validatePieceInPalaceDiagonal(origin, destination)) {
+            return List.of();
+        }
         validateRoute(dx, dy);
 
         return List.of();
     }
 
-    private void validateRoute(int dx, int dy) {
-        if (dx == 0 && dy == 0) {
+    private boolean validatePieceInPalaceDiagonal(Position origin, Position destination) {
+        if (origin.equals(destination)) {
             throw new IllegalArgumentException("[ERROR] 같은 위치로 이동할 수 없습니다.");
         }
 
+        if (!(origin.isInPalace() && destination.isInPalace())) {
+            throw new IllegalArgumentException("[ERROR] 장은 궁 안에서만 움직일 수 있습니다.");
+        }
+
+        if (origin.isInChoPalaceDiagonal() && destination.isInChoPalaceDiagonal()) {
+            if (origin.isCenterOfChoPalace() || destination.isCenterOfChoPalace()) {
+                return true;
+            }
+        }
+
+        if (origin.isInHanPalaceDiagonal() && destination.isInHanPalaceDiagonal()) {
+            if (origin.isCenterOfHanPalace() || destination.isCenterOfHanPalace()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void validateRoute(int dx, int dy) {
         if (Math.abs(dx) + Math.abs(dy) != 1) {
             throw new UnsupportedOperationException("[ERROR] 장이 이동할 수 있는 목적지가 아닙니다.");
         }
--- src/test/java/janggiGame/piece/KingTest.java
+++ src/test/java/janggiGame/piece/KingTest.java
@@ -18,12 +18,33 @@ import org.junit.jupiter.params.provider.MethodSource;
 class KingTest {
     private static Stream<Arguments> provideRowAndColumn() {
         return Stream.of(
-                Arguments.of(1, 1),
-                Arguments.of(5, 5),
-                Arguments.of(1, 3),
-                Arguments.of(3, 1),
-                Arguments.of(2, 5),
-                Arguments.of(5, 2)
+                Arguments.of(3, 2),
+                Arguments.of(4, 2),
+                Arguments.of(5, 2),
+                Arguments.of(5, 1),
+                Arguments.of(5, 0)
+        );
+    }
+
+    private static Stream<Arguments> providePositionInPalaceDiagonal() {
+        return Stream.of(
+                Arguments.of(Position.of(3, 7), Position.of(4, 8)),
+                Arguments.of(Position.of(3, 9), Position.of(4, 8)),
+                Arguments.of(Position.of(5, 7), Position.of(4, 8)),
+                Arguments.of(Position.of(5, 9), Position.of(4, 8)),
+
+                Arguments.of(Position.of(4, 8), Position.of(3, 7)),
+                Arguments.of(Position.of(4, 8), Position.of(3, 9)),
+                Arguments.of(Position.of(4, 8), Position.of(5, 7)),
+                Arguments.of(Position.of(4, 8), Position.of(5, 9))
+        );
+    }
+
+    private static Stream<Arguments> providePositionOutOfPalace() {
+        return Stream.of(
+                Arguments.of(Position.of(3, 7), Position.of(3, 6)),
+                Arguments.of(Position.of(3, 9), Position.of(2, 9)),
+                Arguments.of(Position.of(5, 7), Position.of(5, 6))
         );
     }
 
@@ -32,7 +53,7 @@ class KingTest {
     @MethodSource("provideRowAndColumn")
     void kingCanValidateDestination(int row, int column) {
         // given
-        Position origin = Position.of(3, 3);
+        Position origin = Position.of(3, 0);
         Position destination = Position.of(row, column);
         King king = new King(Dynasty.HAN);
 
@@ -46,8 +67,8 @@ class KingTest {
     @Test
     void kingCanGetIntermediatePoints() {
         // given
-        Position origin = Position.of(1, 1);
-        Position destination = Position.of(1, 0);
+        Position origin = Position.of(3, 1);
+        Position destination = Position.of(4, 1);
         King king = new King(Dynasty.HAN);
 
         // when
@@ -80,5 +101,44 @@ class KingTest {
         // when // then
         assertThatCode(() -> king.validateMove(intermediatePointsWithPiece, new Advisor(Dynasty.CHO)))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("장의 위치가 궁성 안 대각선의 위치인 경우, 대각선 이동이 가능하다.")
+    @ParameterizedTest
+    @MethodSource("providePositionInPalaceDiagonal")
+    void kingCanMoveDiagonalInPalaceDiagonal(Position origin, Position destination) {
+        // given
+        King king = new King(Dynasty.HAN);
+
+        // when
+        List<Position> actual = king.getIntermediatePoints(origin, destination);
+
+        // then
+        assertThat(actual).isEmpty();
+    }
+
+    @DisplayName("장은 같은 위치로 이동할 수 없다.")
+    @Test
+    void kingCannotMoveToSamePosition_Test() {
+        // given
+        King king = new King(Dynasty.HAN);
+
+        // when // then
+        assertThatCode(() -> king.getIntermediatePoints(Position.of(4, 8), Position.of(4, 8)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR] ");
+    }
+
+    @DisplayName("장은 궁 밖으로 움직일 수 없다.")
+    @ParameterizedTest
+    @MethodSource("providePositionOutOfPalace")
+    void kingCanMoveInPalace_Test(Position origin, Position destination) {
+        // given
+        King king = new King(Dynasty.HAN);
+
+        // when // then
+        assertThatCode(() -> king.getIntermediatePoints(origin, destination))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR] ");
     }
 }
