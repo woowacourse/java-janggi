87df9f1c1c08c964960a05f487550c49d5e68163
ryan-dia <cjfdnjs158@naver.com>
2025-03-29 23:07:51 +0900

    refactor: 반환 타입 변경
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/main/java/domain/BoardFactory.java
+++ src/main/java/domain/BoardFactory.java
@@ -18,7 +18,7 @@ public final class BoardFactory {
     private BoardFactory() {
     }
 
-    public static Board create() {
+    public static List<Position> create() {
         final List<Position> positions = new ArrayList<>();
         positions.addAll(generate(General::new, InitialPoint.GENERAL.greenPoints, InitialPoint.GENERAL.redPoints));
         positions.addAll(generate(Guard::new, InitialPoint.GUARD.greenPoints, InitialPoint.GUARD.redPoints));
@@ -27,7 +27,7 @@ public final class BoardFactory {
         positions.addAll(generate(Chariot::new, InitialPoint.CHARIOT.greenPoints, InitialPoint.CHARIOT.redPoints));
         positions.addAll(generate(Cannon::new, InitialPoint.CANNON.greenPoints, InitialPoint.CANNON.redPoints));
         positions.addAll(generate(Soldier::new, InitialPoint.SOLDIER.greenPoints, InitialPoint.SOLDIER.redPoints));
-        return new Board(positions);
+        return positions;
     }
 
     private static <T extends Piece> List<Position> generate(
