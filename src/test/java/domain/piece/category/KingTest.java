80a5e37252744f29e5d16d1b92b24a2ec10d75e8
taek2222 <oot309@daum.net>
2025-03-29 22:05:41 +0900

    test: 궁성 내부 대각선 이동인 경우의 이동 및 예외 테스트 작성

--- src/test/java/domain/piece/category/KingTest.java
+++ src/test/java/domain/piece/category/KingTest.java
@@ -1,7 +1,9 @@
 package domain.piece.category;
 
 import domain.MoveInfos;
+import domain.direction.Directions;
 import domain.direction.PieceDirection;
+import domain.piece.Piece;
 import domain.spatial.Position;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -21,5 +23,41 @@ class KingTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> king.move(target, new MoveInfos(List.of())))
                 .withMessage("왕은 궁성 밖으로 이동할 수 없습니다.");
+    }
+
+    @Test
+    void 이동_경로가_아닌_경우_예외가_발생한다() {
+        // given
+        Piece piece = new King(new Position(1, 2), PieceDirection.KING.get());
+        Position target = new Position(2, 3);
+
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.getPaths(target))
+                .withMessage("이동할 수 없는 좌표입니다. 다시 확인해주세요.");
+    }
+
+    @Test
+    void 궁성_내부_대각선_이동이_가능하다() {
+        // given
+        Piece piece = new King(new Position(5, 3), new Directions(List.of(), false));
+
+        Position target = new Position(4, 2);
+
+        // when && then
+        piece.getPaths(target);
+    }
+
+    @Test
+    void 궁성_대각선_이동인_경우에_외부로_이동할_경우_예외가_발생한다() {
+        // given
+        Piece piece = new King(new Position(5, 3), new Directions(List.of(), false));
+
+        Position target = new Position(6, 4);
+
+        // when && then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.getPaths(target))
+                .withMessage("궁성 이동의 경우 밖으로 이동할 수 없습니다.");
     }
 }
