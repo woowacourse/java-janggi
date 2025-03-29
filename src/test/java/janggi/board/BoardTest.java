d9b0558f55cda02573da491536d1525531069b30
changuii <rhljh7410@gmail.com>
2025-03-29 15:01:42 +0900

    test: BoardTest 리팩터링

--- src/test/java/janggi/board/BoardTest.java
+++ src/test/java/janggi/board/BoardTest.java
@@ -1,7 +1,6 @@
 package janggi.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import janggi.coordinate.JanggiPosition;
 import janggi.piece.Cannon;
@@ -18,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -27,6 +27,18 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class BoardTest {
 
+    private final Map<JanggiPosition, Piece> janggiBoard = new HashMap<>();
+
+    @BeforeEach
+    void initJanggiBoard() {
+        janggiBoard.clear();
+    }
+
+    public void placePieceOnJanggiBoard(final JanggiPosition janggiPosition, final Piece piece) {
+        janggiBoard.put(janggiPosition, piece);
+    }
+
+
     @Nested
     @DisplayName("보드 생성")
     class ConstructBoard {
@@ -34,9 +46,8 @@ public class BoardTest {
         @DisplayName("보드를 생성했을 때 null이 아니어야 한다.")
         @Test
         void construct1() {
-            //given
-            //when
-            final var board = new Board(new HashMap<>());
+            // given & when
+            final Board board = new Board(janggiBoard);
 
             // then
             assertThat(board.getJanggiBoard()).isNotNull();
@@ -62,95 +73,106 @@ public class BoardTest {
     class ContainsPiece {
 
         @DisplayName("보드의 특정 위치에 피스가 존재한다면 true, 아니라면 false를 반환한다.")
-        @Test
-        void existPieceByPosition() {
+        @ParameterizedTest
+        @MethodSource
+        void existPieceByPosition(final JanggiPosition janggiPosition, final boolean expected) {
             // given
-            final Map<JanggiPosition, Piece> map = Map.of(new JanggiPosition(1, 1), new General(Country.HAN));
-            final Board board = new Board(map);
-            final JanggiPosition existJanggiPosition = new JanggiPosition(1, 1);
-            final JanggiPosition notExistJanggiPosition = new JanggiPosition(1, 2);
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new General(Country.HAN));
+            final Board board = new Board(janggiBoard);
 
             // when
-            final boolean actual1 = board.existPieceByPosition(existJanggiPosition);
-            final boolean actual2 = board.existPieceByPosition(notExistJanggiPosition);
+            final boolean actual = board.existPieceByPosition(janggiPosition);
 
             // then
-            org.junit.jupiter.api.Assertions.assertAll(
-                    () -> Assertions.assertThat(actual1).isTrue(),
-                    () -> Assertions.assertThat(actual2).isFalse()
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        static Stream<Arguments> existPieceByPosition() {
+            return Stream.of(
+                    Arguments.of(new JanggiPosition(1, 1), true),
+                    Arguments.of(new JanggiPosition(1, 2), false)
             );
         }
 
-        @DisplayName("보드의 특정 위치가 포라면 true를 반환한다.")
-        @Test
-        void isCannonByPosition() {
+        @DisplayName("보드의 특정 위치가 포라면 true를 반환한다. 아니라면 false를 반환한다.")
+        @ParameterizedTest
+        @MethodSource
+        void isCannonByPosition(final JanggiPosition janggiPosition, final boolean expected) {
             // given
-            final Map<JanggiPosition, Piece> map = Map.of(
-                    new JanggiPosition(1, 1), new Cannon(Country.HAN),
-                    new JanggiPosition(1, 2), new Soldier(Country.HAN)
-            );
-            final Board board = new Board(map);
-            final JanggiPosition cannonJanggiPosition = new JanggiPosition(1, 1);
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new Cannon(Country.HAN));
+            placePieceOnJanggiBoard(new JanggiPosition(1, 2), new Soldier(Country.HAN));
+            final Board board = new Board(janggiBoard);
 
             // when
-            final boolean actualCannon = board.isCannonByPosition(cannonJanggiPosition);
+            final boolean actual = board.isCannonByPosition(janggiPosition);
 
             // then
-            assertThat(actualCannon).isTrue();
+            assertThat(actual).isEqualTo(expected);
         }
 
-        @DisplayName("Positions 내부에 포가 존재한다면 true를 반환한다.")
-        @Test
-        void containsCannonByPositions() {
+        static Stream<Arguments> isCannonByPosition() {
+            return Stream.of(
+                    Arguments.of(new JanggiPosition(1, 1), true),
+                    Arguments.of(new JanggiPosition(1, 2), false)
+            );
+        }
+
+        @DisplayName("Positions 내부에 포가 존재한다면 true를 반환한다. 존재하지 않는다면 false를 반환한다.")
+        @ParameterizedTest
+        @MethodSource
+        void containsCannonByPositions(final List<JanggiPosition> janggiPositions, final boolean expected) {
             // given
-            final Map<JanggiPosition, Piece> map = Map.of(
-                    new JanggiPosition(1, 1), new Cannon(Country.HAN),
-                    new JanggiPosition(1, 2), new Soldier(Country.HAN)
-            );
-            final Board board = new Board(map);
-            final List<JanggiPosition> janggiPositions = List.of(
-                    new JanggiPosition(1, 1), new JanggiPosition(1, 2)
-            );
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new Cannon(Country.HAN));
+            placePieceOnJanggiBoard(new JanggiPosition(1, 2), new Soldier(Country.HAN));
+            final Board board = new Board(janggiBoard);
 
             // when
             final boolean actual = board.containsCannonByPositions(janggiPositions);
 
             // then
-            assertThat(actual).isTrue();
+            assertThat(actual).isEqualTo(expected);
         }
 
-        @Test
-        @DisplayName("보드의 특정 위치의 기물이 주어진 팀과 같다면 true를 반환한다.")
-        void equalsTeamTypeByPosition() {
-            // given
-            final Map<JanggiPosition, Piece> map = Map.of(
-                    new JanggiPosition(1, 1), new Cannon(Country.HAN)
+        static Stream<Arguments> containsCannonByPositions() {
+            return Stream.of(
+                    Arguments.of(List.of(new JanggiPosition(1, 1), new JanggiPosition(1, 2)), true),
+                    Arguments.of(List.of(new JanggiPosition(2, 2), new JanggiPosition(2, 3)), false)
             );
-            final Board board = new Board(map);
+        }
+
+        @DisplayName("보드의 특정 위치의 기물이 주어진 팀과 같다면 true를 반환한다. 다르다면 false를 반환한다.")
+        @ParameterizedTest
+        @MethodSource
+        void equalsTeamTypeByPosition(final Country country, final boolean expected) {
+            // given
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new Cannon(Country.HAN));
+            final Board board = new Board(janggiBoard);
 
             final JanggiPosition janggiPosition = new JanggiPosition(1, 1);
-            final Country equalsCountry = Country.HAN;
-            final Country notEqualsCountry = Country.CHO;
 
             // when
-            final boolean actualEquals = board.equalsTeamTypeByPosition(janggiPosition, equalsCountry);
-            final boolean actualNotEquals = board.equalsTeamTypeByPosition(janggiPosition, notEqualsCountry);
+            final boolean actual = board.equalsTeamTypeByPosition(janggiPosition, country);
 
             // then
-            assertThat(actualEquals).isTrue();
-            assertThat(actualNotEquals).isFalse();
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        static Stream<Arguments> equalsTeamTypeByPosition() {
+            return Stream.of(
+                    Arguments.of(Country.HAN, true),
+                    Arguments.of(Country.CHO, false)
+            );
         }
 
         @DisplayName("보드에 주어진 범위에 존재하는 기물의 수를 반환")
         @Test
         void calculatePieceCountByPositions() {
             // given
-            final Map<JanggiPosition, Piece> map = Map.of(
-                    new JanggiPosition(1, 1), new Cannon(Country.HAN),
-                    new JanggiPosition(1, 2), new Cannon(Country.HAN),
-                    new JanggiPosition(1, 3), new Cannon(Country.HAN)
-            );
-            final Board board = new Board(map);
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new Cannon(Country.HAN));
+            placePieceOnJanggiBoard(new JanggiPosition(1, 2), new Cannon(Country.HAN));
+            placePieceOnJanggiBoard(new JanggiPosition(1, 3), new Cannon(Country.HAN));
+            final Board board = new Board(janggiBoard);
+
             final List<JanggiPosition> janggiPositions = List.of(
                     new JanggiPosition(1, 1), new JanggiPosition(1, 2)
             );
@@ -167,27 +189,25 @@ public class BoardTest {
     @Nested
     @DisplayName("기물 위치 이동")
     class UpdateJanggiPosition {
+
         @Test
         @DisplayName("src 위치에 있는 기물이 dest로 옮겨질 수 있다면 이동시킨다.")
         void updatePosition3() {
             // given
-            final Map<JanggiPosition, Piece> map = Map.of(
-                    new JanggiPosition(1, 1), new Chariot(Country.HAN)
-            );
-            final Board board = new Board(map);
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new Chariot(Country.HAN));
+            final Board board = new Board(janggiBoard);
 
             final JanggiPosition src = new JanggiPosition(1, 1);
             final JanggiPosition dest = new JanggiPosition(1, 5);
-            final Country country = Country.HAN;
+            final Country currentTurnCountry = Country.HAN;
 
             // when
-            board.updatePosition(src, dest, country);
+            board.updatePosition(src, dest, currentTurnCountry);
 
             // then
             Assertions.assertThat(board.getJanggiBoard())
                     .containsKey(dest)
                     .doesNotContainKey(src);
-
         }
 
 
@@ -195,14 +215,14 @@ public class BoardTest {
         @DisplayName("src 위치에 기물이 존재하지 않으면 예외가 발생한다")
         void updatePosition() {
             // given
-            final Board board = new Board(new HashMap<>());
+            final Board board = new Board(janggiBoard);
 
             final JanggiPosition src = new JanggiPosition(1, 1);
             final JanggiPosition dest = new JanggiPosition(1, 2);
-            final Country country = Country.HAN;
+            final Country currentTurnCountry = Country.HAN;
 
             // when & then
-            Assertions.assertThatThrownBy(() -> board.updatePosition(src, dest, country))
+            Assertions.assertThatThrownBy(() -> board.updatePosition(src, dest, currentTurnCountry))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
@@ -210,17 +230,15 @@ public class BoardTest {
         @DisplayName("src 위치에 기물이 현재 턴의 팀이 아니라면 예외가 발생한다")
         void updatePosition1() {
             // given
-            final Map<JanggiPosition, Piece> map = Map.of(
-                    new JanggiPosition(1, 1), new Cannon(Country.CHO)
-            );
-            final Board board = new Board(map);
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new Cannon(Country.CHO));
+            final Board board = new Board(janggiBoard);
 
             final JanggiPosition src = new JanggiPosition(1, 1);
             final JanggiPosition dest = new JanggiPosition(1, 2);
-            final Country country = Country.HAN;
+            final Country currentTurnCountry = Country.HAN;
 
             // when & then
-            Assertions.assertThatThrownBy(() -> board.updatePosition(src, dest, country))
+            Assertions.assertThatThrownBy(() -> board.updatePosition(src, dest, currentTurnCountry))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
@@ -228,17 +246,15 @@ public class BoardTest {
         @DisplayName("src 위치에 기물이 dest로 갈 수 없다면 예외가 발생한다")
         void updatePosition2() {
             // given
-            final Map<JanggiPosition, Piece> map = Map.of(
-                    new JanggiPosition(1, 1), new Cannon(Country.HAN)
-            );
-            final Board board = new Board(map);
+            placePieceOnJanggiBoard(new JanggiPosition(1, 1), new Cannon(Country.HAN));
+            final Board board = new Board(janggiBoard);
 
             final JanggiPosition src = new JanggiPosition(1, 1);
             final JanggiPosition dest = new JanggiPosition(1, 2);
-            final Country country = Country.HAN;
+            final Country currentTurnCountry = Country.HAN;
 
             // when & then
-            Assertions.assertThatThrownBy(() -> board.updatePosition(src, dest, country))
+            Assertions.assertThatThrownBy(() -> board.updatePosition(src, dest, currentTurnCountry))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
@@ -247,20 +263,37 @@ public class BoardTest {
     @DisplayName("보드의 상태 검증")
     class Is {
 
-        @DisplayName("장군들이 모두 살아있는가?")
-        @Test
-        void isAliveAllGenerals() {
+        @DisplayName("장군들이 모두 살아있다면 true, 아니라면 false를 반환한다.")
+        @ParameterizedTest
+        @MethodSource
+        void isAliveAllGenerals(final Map<JanggiPosition, Piece> janggiBoard, final boolean expected) {
             // given
-            final Map<JanggiPosition, Piece> janggiBoard = Map.of(
-                    new JanggiPosition(1, 1), new General(Country.HAN)
-            );
             final Board board = new Board(janggiBoard);
 
             // when
             final boolean actual = board.isAliveAllGenerals();
 
             // then
-            assertThat(actual).isFalse();
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        static Stream<Arguments> isAliveAllGenerals(){
+            final Map<JanggiPosition, Piece> aliveAll = Map.of(
+                    new JanggiPosition(2, 5), new General(Country.HAN),
+                    new JanggiPosition(9, 5), new General(Country.CHO)
+            );
+            final Map<JanggiPosition, Piece> aliveOnlyChoGeneral = Map.of(
+                    new JanggiPosition(2, 5), new General(Country.HAN)
+            );
+            final Map<JanggiPosition, Piece> aliveOnlyHanGeneral = Map.of(
+                    new JanggiPosition(9, 5), new General(Country.CHO)
+            );
+
+            return Stream.of(
+                    Arguments.of( aliveAll, true),
+                    Arguments.of( aliveOnlyHanGeneral, false),
+                    Arguments.of( aliveOnlyChoGeneral, false)
+            );
         }
     }
 
@@ -271,35 +304,39 @@ public class BoardTest {
         @DisplayName("남은 기물수를 기준으로 점수를 계산한다.")
         @ParameterizedTest
         @MethodSource
-        void kill(final Piece piece, final JanggiScore expectedJanggiScore) {
+        void kill(final Piece piece, final JanggiScore expected, final Country country) {
             // given
-            final JanggiPosition janggiPosition = new JanggiPosition(1, 2);
-            final Map<JanggiPosition, Piece> janggiBoard = Map.of(
-                    janggiPosition, piece
-            );
+            placePieceOnJanggiBoard(new JanggiPosition(1, 2), piece);
             final Board board = new Board(janggiBoard);
 
             // when
-            final JanggiScore actualCho = board.calculateScoreByCountry(Country.CHO);
-            final JanggiScore actualHan = board.calculateScoreByCountry(Country.HAN);
+            final JanggiScore actual = board.calculateScoreByCountry(country);
 
             // then
-            assertSoftly(s -> {
-                s.assertThat(actualCho).isEqualTo(expectedJanggiScore);
-                s.assertThat(actualHan).isEqualTo(new JanggiScore(73.5));
-            });
+            assertThat(actual).isEqualTo(expected);
         }
 
         static Stream<Arguments> kill() {
             final double MAX_SCORE_OF_CHO = 72;
+            final double MAX_SCORE_OF_HAN = 73.5;
+            final Country HAN = Country.HAN;
+            final Country CHO = Country.CHO;
+
             return Stream.of(
-                    Arguments.of(new Cannon(Country.HAN), new JanggiScore(MAX_SCORE_OF_CHO - 7)),
-                    Arguments.of(new Chariot(Country.HAN), new JanggiScore(MAX_SCORE_OF_CHO - 13)),
-                    Arguments.of(new Elephant(Country.HAN), new JanggiScore(MAX_SCORE_OF_CHO - 3)),
-                    Arguments.of(new General(Country.HAN), new JanggiScore(MAX_SCORE_OF_CHO - 0)),
-                    Arguments.of(new Guard(Country.HAN), new JanggiScore(MAX_SCORE_OF_CHO - 3)),
-                    Arguments.of(new Horse(Country.HAN), new JanggiScore(MAX_SCORE_OF_CHO - 5)),
-                    Arguments.of(new Soldier(Country.HAN), new JanggiScore(MAX_SCORE_OF_CHO - 2))
+                    Arguments.of(new Cannon(HAN), new JanggiScore(MAX_SCORE_OF_CHO - 7), CHO),
+                    Arguments.of(new Chariot(HAN), new JanggiScore(MAX_SCORE_OF_CHO - 13), CHO),
+                    Arguments.of(new Elephant(HAN), new JanggiScore(MAX_SCORE_OF_CHO - 3), CHO),
+                    Arguments.of(new General(HAN), new JanggiScore(MAX_SCORE_OF_CHO - 0), CHO),
+                    Arguments.of(new Guard(HAN), new JanggiScore(MAX_SCORE_OF_CHO - 3), CHO),
+                    Arguments.of(new Horse(HAN), new JanggiScore(MAX_SCORE_OF_CHO - 5), CHO),
+                    Arguments.of(new Soldier(HAN), new JanggiScore(MAX_SCORE_OF_CHO - 2), CHO),
+                    Arguments.of(new Cannon(CHO), new JanggiScore(MAX_SCORE_OF_HAN - 7), HAN),
+                    Arguments.of(new Chariot(CHO), new JanggiScore(MAX_SCORE_OF_HAN - 13), HAN),
+                    Arguments.of(new Elephant(CHO), new JanggiScore(MAX_SCORE_OF_HAN - 3), HAN),
+                    Arguments.of(new General(CHO), new JanggiScore(MAX_SCORE_OF_HAN - 0), HAN),
+                    Arguments.of(new Guard(CHO), new JanggiScore(MAX_SCORE_OF_HAN - 3), HAN),
+                    Arguments.of(new Horse(CHO), new JanggiScore(MAX_SCORE_OF_HAN - 5), HAN),
+                    Arguments.of(new Soldier(CHO), new JanggiScore(MAX_SCORE_OF_HAN - 2), HAN)
             );
         }
     }
