3008cb580a3edbdf11644f32a4e860aa23f02374
ryan-dia <cjfdnjs158@naver.com>
2025-03-29 23:12:26 +0900

    feat: 장기게임 db 연결 로직 구현
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,32 @@
+import db.JanggiDao;
+import domain.Board;
 import domain.BoardFactory;
+import domain.GameState;
 import domain.JanggiGame;
+import domain.Team;
+import domain.position.Position;
+import java.util.List;
 
 public class Application {
 
     public static void main(final String[] args) {
-        final JanggiGame janggiGame = new JanggiGame(BoardFactory.create());
+        final JanggiDao janggiDao = new JanggiDao();
+        final GameState gameState = janggiDao.getGameState();
+        if (gameState.isRunning()) {
+            final JanggiGame janggiGame = new JanggiGame(new Board(janggiDao.getPositions()), janggiDao.getTurn());
+            janggiGame.start();
+            janggiDao.updateGameState(GameState.END);
+            janggiDao.changeTurn(Team.GREEN);
+            janggiDao.deleteAllPosition();
+            return;
+        }
+        janggiDao.updateGameState(GameState.RUNNING);
+        final List<Position> positions = BoardFactory.create();
+        positions.forEach(janggiDao::savePosition);
+        final JanggiGame janggiGame = new JanggiGame(new Board(positions), Team.GREEN);
         janggiGame.start();
+        janggiDao.updateGameState(GameState.END);
+        janggiDao.changeTurn(Team.GREEN);
+        janggiDao.deleteAllPosition();
     }
 }
