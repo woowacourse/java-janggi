88dcac843674e124f005d4354e66270aebbacb5c
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-29 22:16:17 +0900

    test: Soldier 기물, 대각선 간선 연결 단위 테스트 추가

--- src/test/java/piece/SoldierTest.java
+++ src/test/java/piece/SoldierTest.java
@@ -16,8 +16,7 @@ class SoldierTest {
 
     @DisplayName("Soldier는 뒷 방향을 제외하고 한 간선을 이동할 수 있다.")
     @Test
-    void isAbleToMoveByDirection() {
-
+    void validateMove() {
         // given
         PositionFactory positionFactory = new PositionFactory();
         positionFactory.basicSettingGraph();
@@ -53,6 +52,42 @@ class SoldierTest {
         final Position ableDest4 = new Position(1, 3);
         assertThatCode(
                 () -> soldier.validateMove(src, ableDest4, board)
+        ).doesNotThrowAnyException();
+    }
+
+    @DisplayName("Soldier는 뒷 방향을 제외하고 한 간선을 이동할 수 있다. : 대각선 간선")
+    @Test
+    void validateMoveWithDiagonalSetting() {
+        // given
+        PositionFactory positionFactory = new PositionFactory();
+        positionFactory.basicSettingGraph();
+        positionFactory.diagonalSettingGraph();
+
+        final Country country = Country.HAN;
+        Country.assignDirection(country, LineDirection.UP);
+        final Position dumyPosition = new Position(5, 2);
+        Position src = dumyPosition;
+        final Piece soldier = new Soldier(src, country);
+        final Board board = new Board(Map.of(
+                src, soldier
+        ));
+
+        // when & then: 1 : failure
+        final Position ableDest1 = new Position(4, 1);
+        assertThatThrownBy(
+                () -> soldier.validateMove(src, ableDest1, board)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+        // when & then : 2 : success
+        final Position ableDest2 = new Position(4, 3);
+        assertThatCode(
+                () -> soldier.validateMove(src, ableDest2, board)
+        ).doesNotThrowAnyException();
+
+        // when & then: 3 : success
+        final Position ableDest3 = new Position(6, 3);
+        assertThatCode(
+                () -> soldier.validateMove(src, ableDest3, board)
         ).doesNotThrowAnyException();
     }
 }
