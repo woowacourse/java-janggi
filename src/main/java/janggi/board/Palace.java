538496bf5bf9fd15e6daf65d344f50ae04bab837
ppparkta <pppppark2@gmail.com>
2025-03-29 21:23:22 +0900

    feat: 궁성 확인 기능 구현

--- src/main/java/janggi/board/Palace.java
+++ src/main/java/janggi/board/Palace.java
@@ -1,4 +1,63 @@
 package janggi.board;
 
-public class Palace {
+import java.util.Arrays;
+
+public enum Palace {
+    HAN(3, 0),
+    CHO(3, 7),
+    ;
+
+    public static final int WIDTH = 3;
+    public static final int HEIGHT = 3;
+
+    private final int startColumn;
+    private final int startRow;
+
+    Palace(int startColumn, int startRow) {
+        this.startColumn = startColumn;
+        this.startRow = startRow;
+    }
+
+    public static boolean canDiagonalInPalace(int pickedColumn, int pickedRow) {
+        if (!isInPalace(pickedColumn, pickedRow)) {
+            return false;
+        }
+        for (Palace team : values()) {
+            if (extracted(pickedColumn, pickedRow, team)) {
+                return true;
+            }
+            if (team.startColumn + WIDTH - 1 == pickedColumn || team.startRow + HEIGHT - 1 == pickedRow) {
+                return true;
+            }
+        }
+        return isCenterInPalace(pickedColumn, pickedRow);
+    }
+
+    private static boolean extracted(int pickedColumn, int pickedRow, Palace team) {
+        if (team.startColumn == pickedColumn || team.startRow == pickedRow) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isCenterInPalace(int pickedColumn, int pickedRow) {
+        return Arrays.stream(values())
+                .anyMatch(team -> team.startColumn + 1 == pickedColumn && team.startRow + 1 == pickedRow);
+    }
+
+    public static boolean isInPalace(int pickedColumn, int pickedRow) {
+        for (Palace team : values()) {
+            if (isInsidePalace(team, pickedColumn, pickedRow)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isInsidePalace(Palace team, int pickedColumn, int pickedRow) {
+        int endRow = team.startRow + HEIGHT;
+        int endColumn = team.startColumn + WIDTH;
+        return pickedRow >= team.startRow && pickedRow < endRow
+                && pickedColumn >= team.startColumn && pickedColumn < endColumn;
+    }
 }
---
+++ src/test/java/janggi/board/PalaceTest.java
@@ -0,0 +1,58 @@
+package janggi.board;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PalaceTest {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "0,3,true", "0,4,true", "0,5,true",
+            "1,3,true", "1,4,true", "1,5,true",
+            "2,3,true", "2,4,true", "2,5,true",
+
+            "7,3,true", "7,4,true", "7,5,true",
+            "8,3,true", "8,4,true", "8,5,true",
+            "9,3,true", "9,4,true", "9,5,true"
+    })
+    @DisplayName("궁성 내에 있는지 확인한다.")
+    void test1(String point) {
+        // given
+        String[] position = point.split(",");
+        int column = Integer.parseInt(position[0]);
+        int row = Integer.parseInt(position[1]);
+        boolean expected = Boolean.getBoolean(position[2]);
+
+        // when
+        boolean inPalace = Palace.isInPalace(column, row);
+
+        // then
+        Assertions.assertThat(inPalace).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "0,3,true", "0,4,false", "0,5,true",
+            "1,3,false", "1,4,true", "1,5,false",
+            "2,3,true", "2,4,false", "2,5,true",
+
+            "7,3,true", "7,4,false", "7,5,true",
+            "8,3,false", "8,4,true", "8,5,false",
+            "9,3,true", "9,4,false", "9,5,true"
+    })
+    @DisplayName("궁성 내 대각선 이동이 가능한 지점에 있는지 확인한다.")
+    void test2(String point) {
+        // given
+        String[] position = point.split(",");
+        int column = Integer.parseInt(position[0]);
+        int row = Integer.parseInt(position[1]);
+        boolean expected = Boolean.getBoolean(position[2]);
+
+        // when
+        boolean inPalace = Palace.canDiagonalInPalace(column, row);
+
+        // then
+        Assertions.assertThat(inPalace).isEqualTo(expected);
+    }
+}
