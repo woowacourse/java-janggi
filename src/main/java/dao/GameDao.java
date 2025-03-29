74509322627b4cd75a47bd156f27c1afa296cfdd
LJM\eueo8 <eueo8259@gmail.com>
2025-03-29 15:10:01 +0900

    refactor : 초기 테이블 생성 및 세팅 로직 분리

--- src/main/java/dao/GameDao.java
+++ src/main/java/dao/GameDao.java
@@ -11,24 +11,28 @@ import java.sql.Statement;
 public class GameDao {
     private final Connection connection = JdbcConnection.getInstance();
 
-    public void initializeGameTable() {
+    public void createGameTableIfNotExists() {
         final var createTableQuery = "CREATE TABLE IF NOT EXISTS game ("
                 + "game_id INT AUTO_INCREMENT PRIMARY KEY, "
                 + "turn VARCHAR(64) NOT NULL)";
 
-        final var insertInitialTurnQuery = "INSERT INTO game (turn) VALUES (?)";
-
         try (Statement stmt = connection.createStatement()) {
             stmt.execute(createTableQuery);
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-            String checkQuery = "SELECT COUNT(*) FROM game";
-            try (PreparedStatement pstmt = connection.prepareStatement(checkQuery);
-                 ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next() && rs.getInt(1) == 0) {
-                    try (PreparedStatement insertStmt = connection.prepareStatement(insertInitialTurnQuery)) {
-                        insertStmt.setString(1, Team.CHO.name());
-                        insertStmt.executeUpdate();
-                    }
+    public void initializeGameIfNotExists() {
+        final var insertInitialTurnQuery = "INSERT INTO game (turn) VALUES (?)";
+
+        String checkQuery = "SELECT COUNT(*) FROM game";
+        try (PreparedStatement pstmt = connection.prepareStatement(checkQuery);
+             ResultSet rs = pstmt.executeQuery()) {
+            if (rs.next() && rs.getInt(1) == 0) {
+                try (PreparedStatement insertStmt = connection.prepareStatement(insertInitialTurnQuery)) {
+                    insertStmt.setString(1, Team.CHO.name());  // 초기 턴을 "CHO"로 설정
+                    insertStmt.executeUpdate();
                 }
             }
         } catch (final SQLException e) {
