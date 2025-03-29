f7b4bd3fc8ccf12b40a94d571870cb1ad3bb1544
ppparkta <pppppark2@gmail.com>
2025-03-29 17:47:23 +0900

    test: route 테스트 추가

--- src/test/java/janggi/position/RouteTest.java
+++ src/test/java/janggi/position/RouteTest.java
@@ -1,5 +1,6 @@
 package janggi.position;
 
+import janggi.game.Pieces;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,13 +17,45 @@ class RouteTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("경로에 포 기물이 있다면 포가 지나갈 수 없다.")
     void test2() {
         // given
+        Route route = Route.of(List.of(new Position(1, 2)));
+        Pieces pieces = new Pieces();
 
         // when
+        boolean canCannonJump = route.canBombJump(pieces);
 
         // then
+        Assertions.assertThat(canCannonJump).isFalse();
+    }
 
+    @Test
+    @DisplayName("도착지를 제외한 경로에 포 외의 다른 기물이 하나 있어야 포가 지나갈 수 있다.")
+    void test3() {
+        // given
+        Route route = Route.of(List.of(new Position(0, 4), new Position(0, 3), new Position(0, 2)));
+        Pieces pieces = new Pieces();
+
+        // when
+        boolean canCannonJump = route.canBombJump(pieces);
+
+        // then
+        Assertions.assertThat(canCannonJump).isTrue();
+    }
+
+    @Test
+    @DisplayName("거리를 이용해서 도착지점을 계산한다")
+    void test4() {
+        // given
+        Position start = new Position(0, 4);
+        Position end = new Position(0, 2);
+        Route route = Route.of(List.of(start, new Position(0, 3), end));
+
+        // when
+        Position position = route.searchEndPoint(start);
+
+        // then
+        Assertions.assertThat(position).isEqualTo(end);
     }
 }
