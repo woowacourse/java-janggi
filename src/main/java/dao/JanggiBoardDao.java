9af2f21426d0b128fd007093639e93da451e11be
minSsan <msp1125@hanyang.ac.kr>
2025-03-29 19:15:13 +0900

    🟢green: 기물 db 추가 기능 작성

--- src/main/java/dao/JanggiBoardDao.java
+++ src/main/java/dao/JanggiBoardDao.java
@@ -2,9 +2,12 @@ package dao;
 
 import domain.janggiPiece.JanggiChessPiece;
 import domain.position.JanggiPosition;
+import domain.type.JanggiPieceType;
+import domain.type.JanggiTeam;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JanggiBoardDao implements BoardDao {
@@ -27,7 +30,66 @@ public class JanggiBoardDao implements BoardDao {
 
     @Override
     public void addPiece(JanggiPosition position, JanggiChessPiece piece) {
+        final String query = "INSERT INTO piece(position_row, position_col, type_id, team_id) VALUES(?, ?, ?, ?)";
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, position.getRow());
+            preparedStatement.setInt(2, position.getCol());
+            preparedStatement.setInt(3, findPieceTypeId(piece.getChessPieceType()));
+            preparedStatement.setInt(4, findTeamId(piece.getTeam()));
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private int findPieceTypeId(JanggiPieceType type) {
+        final String query = "SELECT id FROM pieceType WHERE name = ?";
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, getTypeName(type));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("id");
+            }
+            throw new RuntimeException(type + " 타입이 존재하지 않습니다.");
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getTypeName(JanggiPieceType type) {
+        return switch (type) {
+            case ELEPHANT -> "ELEPHANT";
+            case CHARIOT -> "CHARIOT";
+            case CANNON -> "CANNON";
+            case HORSE -> "HORSE";
+            case GUARD -> "GUARD";
+            case PAWN -> "PAWN";
+            case KING -> "KING";
+        };
+    }
+
+    private int findTeamId(JanggiTeam team) {
+        final String query = "SELECT id FROM team WHERE name = ?";
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, getTeamName(team));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("id");
+            }
+            throw new RuntimeException(team + " 팀이 존재하지 않습니다.");
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getTeamName(JanggiTeam team) {
+        return switch (team) {
+            case BLUE -> "BLUE";
+            case RED -> "RED";
+        };
     }
 
     @Override
