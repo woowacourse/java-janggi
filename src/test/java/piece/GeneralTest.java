c43a15c76b9be1cbaae98d1f091c7a104336e2a0
GiSeok Kang <ime.rokz@gmail.com>
2025-03-29 21:48:59 +0900

    refactor: General 궁성 룰 적용하여 테스트 변경

--- src/test/java/piece/GeneralTest.java
+++ src/test/java/piece/GeneralTest.java
@@ -16,8 +16,8 @@ public class GeneralTest {
     @DisplayName("궁은 위로 한 칸 이동할 수 있다.")
     void can_move_up() {
         // given
-        Point start = new Point(2, 2);
-        Point destination = new Point(2, 1);
+        Point start = new Point(5, 9);
+        Point destination = new Point(5, 8);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -34,8 +34,8 @@ public class GeneralTest {
     @DisplayName("궁은 위로 두 칸 이상 이동할 수 없다.")
     void only_move_up_once() {
         // given
-        Point start = new Point(2, 4);
-        Point destination = new Point(2, 2);
+        Point start = new Point(5, 9);
+        Point destination = new Point(5, 7);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -50,8 +50,8 @@ public class GeneralTest {
     @DisplayName("궁은 아래로 한 칸 이동할 수 있다.")
     void can_move_down() {
         // given
-        Point start = new Point(2, 2);
-        Point destination = new Point(2, 3);
+        Point start = new Point(5, 9);
+        Point destination = new Point(5, 10);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -68,8 +68,8 @@ public class GeneralTest {
     @DisplayName("궁은 아래로 두 칸 이상 이동할 수 없다.")
     void only_move_down_once() {
         // given
-        Point start = new Point(2, 2);
-        Point destination = new Point(2, 4);
+        Point start = new Point(5, 8);
+        Point destination = new Point(5, 10);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -84,8 +84,8 @@ public class GeneralTest {
     @DisplayName("궁은 왼쪽으로 한 칸 이동할 수 있다.")
     void can_move_left() {
         // given
-        Point start = new Point(2, 2);
-        Point destination = new Point(1, 2);
+        Point start = new Point(5, 9);
+        Point destination = new Point(4, 9);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -102,8 +102,8 @@ public class GeneralTest {
     @DisplayName("궁은 왼쪽으로 두 칸 이상 이동할 수 없다.")
     void only_move_left_once() {
         // given
-        Point start = new Point(3, 2);
-        Point destination = new Point(1, 2);
+        Point start = new Point(5, 9);
+        Point destination = new Point(3, 9);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -118,8 +118,8 @@ public class GeneralTest {
     @DisplayName("궁은 오른쪽으로 한 칸 이동할 수 있다.")
     void can_move_right() {
         // given
-        Point start = new Point(2, 2);
-        Point destination = new Point(3, 2);
+        Point start = new Point(5, 9);
+        Point destination = new Point(6, 9);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -136,8 +136,8 @@ public class GeneralTest {
     @DisplayName("궁은 오른쪽으로 두 칸 이상 이동할 수 없다.")
     void only_move_right_once() {
         // given
-        Point start = new Point(2, 2);
-        Point destination = new Point(4, 2);
+        Point start = new Point(5, 9);
+        Point destination = new Point(7, 9);
 
         General general = new General(start);
         Pieces pieces = new Pieces(List.of(general));
@@ -149,11 +149,161 @@ public class GeneralTest {
     }
 
     @Test
+    @DisplayName("궁은 궁성 중앙에 있을 때, 왼쪽 1칸, 위쪽 1칸 대각선 이동이 가능하다.")
+    void castle_center_can_move_left_up() {
+        // given
+        Point destination = new Point(4, 8);
+        General general = new General(new Point(5, 9));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 궁성 중앙에 있을 때, 왼쪽 1칸, 아래쪽 1칸 대각선 이동이 가능하다.")
+    void castle_center_can_move_left_down() {
+        // given
+        Point destination = new Point(4, 10);
+        General general = new General(new Point(5, 9));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 궁성 중앙에 있을 때, 오른쪽 1칸, 위쪽 1칸 대각선 이동이 가능하다.")
+    void castle_center_can_move_right_up() {
+        // given
+        Point destination = new Point(6, 8);
+        General general = new General(new Point(5, 9));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 궁성 중앙에 있을 때, 오른쪽 1칸, 아래쪽 1칸 대각선 이동이 가능하다.")
+    void castle_center_can_move_right_down() {
+        // given
+        Point destination = new Point(6, 10);
+        General general = new General(new Point(5, 9));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 왼쪽 1칸, 위쪽 1칸 궁성 코너에 있을 때, 오른쪽 1칸, 아래쪽 1칸 대각선 이동이 가능하다.")
+    void castle_left_up_corner_can_move_right_down() {
+        // given
+        Point destination = new Point(5, 9);
+        General general = new General(new Point(4, 8));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 왼쪽 1칸, 아래쪽 1칸 궁성 코너에 있을 때, 궁성 중앙으로만 대각선 이동이 가능하다.")
+    void castle_left_down_corner_can_move_right_up() {
+        // given
+        Point destination = new Point(5, 9);
+        General general = new General(new Point(4, 10));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 오른쪽 1칸, 위쪽 1칸 궁성 코너에 있을 때, 궁성 중앙으로만 대각선 이동이 가능하다.")
+    void castle_right_up_corner_can_move_left_down() {
+        // given
+        Point destination = new Point(5, 9);
+        General general = new General(new Point(5, 8));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 오른쪽 1칸, 아래쪽 1칸 궁성 코너에 있을 때, 궁성 중앙으로만 대각선 이동이 가능하다.")
+    void castle_right_down_corner_can_move_left_up() {
+        // given
+        Point destination = new Point(5, 9);
+        General general = new General(new Point(4, 10));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        general.move(pieces, destination);
+
+        // then
+        Assertions.assertThat(general)
+                .extracting("current")
+                .isEqualTo(destination);
+    }
+
+    @Test
+    @DisplayName("궁은 왼쪽 1칸, 위쪽 1칸 궁성 코너에 있을 때, 오른쪽 1칸, 아래쪽 1칸 대각선 이동 외에는 대각선 이동이 불가능하다.")
+    void castle_left_up_corner_dont_move_except_right_down() {
+        // given
+        Point destination = new Point(5, 7);
+        General general = new General(new Point(4, 8));
+        Pieces pieces = new Pieces(List.of(general));
+
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> general.move(pieces, destination))
+                        .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     @DisplayName("경로 상 본인의 기물이 있으면 움직일 수 없다.")
-    void elephant_dont_move_to_exist_piece_in_path() {
+    void dont_move_to_exist_piece_in_path() {
         // given
         General general = new General(new Point(2, 2));
-        Player player = new Player(List.of(general, new Horse(new Point(2, 3))), Team.HAN);
+        Player player = new Player(new Pieces(List.of(general, new Horse(new Point(2, 3)))), 0, Team.HAN);
 
         Point destination = new Point(2, 3);
 
