001798818d087863284c6a755958db50be66cb16
changuii <rhljh7410@gmail.com>
2025-03-29 14:13:20 +0900

    test: GeneralTest 리팩터링

--- src/test/java/janggi/piece/GeneralTest.java
+++ src/test/java/janggi/piece/GeneralTest.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import janggi.board.Board;
 import janggi.coordinate.JanggiPosition;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -15,41 +17,53 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class GeneralTest {
 
+    private final General general = new General(Country.CHO);
+    private final Map<JanggiPosition, Piece> janggiBoard = new HashMap<>();
+
+    @BeforeEach
+    void initJanggiBoard(){
+        janggiBoard.clear();
+    }
 
     @Nested
     @DisplayName("장의 움직임")
     class CanMove {
         @DisplayName("General은 주변 한칸으로 이동할 수 있다.")
-        @Test
-        void general() {
+        @ParameterizedTest
+        @MethodSource
+        void general(final JanggiPosition dest, final boolean expected) {
             // given
-            final Piece generalPiece = new General(Country.CHO);
             final JanggiPosition now = new JanggiPosition(9, 4);
-            final JanggiPosition ableDest = new JanggiPosition(9, 5);
-            final JanggiPosition notAbleDest = new JanggiPosition(8, 5);
-            final Board board = new Board(new HashMap<>());
+            final Board board = new Board(janggiBoard);
+
             // when
-            final boolean actual1 = generalPiece.isAbleToMove(now, ableDest, board);
-            final boolean actual2 = generalPiece.isAbleToMove(now, notAbleDest, board);
+            final boolean actual = general.canMove(now, dest, board);
 
             // then
-            org.junit.jupiter.api.Assertions.assertAll(
-                    () -> assertThat(actual1).isTrue(),
-                    () -> assertThat(actual2).isFalse()
-            );
+            assertThat(actual).isEqualTo(expected);
         }
 
+        static Stream<Arguments> general(){
+            return Stream.of(
+                    Arguments.of(new JanggiPosition(9, 5), true),
+                    Arguments.of(new JanggiPosition(8, 5), false)
+            );
+        }
+    }
+
+    @Nested
+    @DisplayName("궁성 내부 움직임 (장은 반드시 궁성 내)")
+    class IsInPalace {
         @DisplayName("General이 궁성 외부로 나갈 수 없다.")
         @Test
         void general1() {
             // given
-            final Piece generalPiece = new General(Country.CHO);
             final JanggiPosition now = new JanggiPosition(9, 6);
             final JanggiPosition notAbleDest = new JanggiPosition(9, 7);
-            final Board board = new Board(new HashMap<>());
+            final Board board = new Board(janggiBoard);
 
             // when
-            final boolean actual = generalPiece.canMove(now, notAbleDest, board);
+            final boolean actual = general.canMove(now, notAbleDest, board);
 
             // then
             assertThat(actual).isFalse();
@@ -60,11 +74,10 @@ class GeneralTest {
         @MethodSource
         void general2(final JanggiPosition source, final JanggiPosition destination) {
             // given
-            final Piece generalPiece = new General(Country.CHO);
-            final Board board = new Board(new HashMap<>());
+            final Board board = new Board(janggiBoard);
 
             // when
-            final boolean actual = generalPiece.canMove(source, destination, board);
+            final boolean actual = general.canMove(source, destination, board);
 
             // then
             assertThat(actual).isTrue();
@@ -82,11 +95,11 @@ class GeneralTest {
         @MethodSource
         void general3(final JanggiPosition source, final JanggiPosition destination, final Country country) {
             // given
-            final Piece generalPiece = new General(country);
-            final Board board = new Board(new HashMap<>());
+            final General general = new General(country);
+            final Board board = new Board(janggiBoard);
 
             // when
-            final boolean actual = generalPiece.canMove(source, destination, board);
+            final boolean actual = general.canMove(source, destination, board);
 
             // then
             assertThat(actual).isFalse();
@@ -100,5 +113,6 @@ class GeneralTest {
                     Arguments.of(new JanggiPosition(1, 4), new JanggiPosition(9, 5), Country.HAN)
             );
         }
+
     }
 }
