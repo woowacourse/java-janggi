3bd7418bbf98e7af370fd71e64a79b2c860d6f08
LJM\eueo8 <eueo8259@gmail.com>
2025-03-29 17:50:01 +0900

    feat : Team 이름으로 Team 반환하는 메서드 구현

--- src/main/java/domain/piece/Team.java
+++ src/main/java/domain/piece/Team.java
@@ -1,6 +1,7 @@
 package domain.piece;
 
 import domain.board.Palace;
+import java.util.Arrays;
 
 public enum Team {
     HAN(Palace.HAN_PALACE),
@@ -14,5 +15,11 @@ public enum Team {
 
     public Palace getPalace() {
         return palace;
+    }
+
+    public static Team getTeamByName(String name){
+        return Arrays.stream(Team.values())
+                .filter(team -> team.name().equals(name)).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지않는 팀의 이름입니다."));
     }
 }
