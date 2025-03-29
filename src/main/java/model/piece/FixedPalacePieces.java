94bb3c577645a4bc62b221238f81ef73e8cf188d
supernovaMK <minkikim2002@gmail.com>
2025-03-29 22:57:56 +0900

    feat: 궁성 안에만 있는 기물들 추상화

---
+++ src/main/java/model/piece/FixedPalacePieces.java
@@ -0,0 +1,32 @@
+package model.piece;
+
+import model.Point;
+import model.Team;
+
+public abstract class FixedPalacePieces extends Piece {
+
+    private static final int PALACE_MIN_POINT_X = 3;
+    private static final int PALACE_MAX_POINT_X = 5;
+
+    protected FixedPalacePieces(Team team, PieceName pieceName) {
+        super(team, pieceName);
+    }
+
+    @Override
+    public boolean isValidPoint(Point beforePoint, Point targetPoint) {
+        validatePalaceRange(targetPoint);
+        validateGungMove(beforePoint, targetPoint);
+        return true;
+    }
+
+    public void validatePalaceRange(Point targetPoint) {
+        if (targetPoint.x() < PALACE_MIN_POINT_X || targetPoint.x() > PALACE_MAX_POINT_X) {
+            throw new IllegalArgumentException("궁 밖에 나갈 수 없습니다.");
+        }
+        if (targetPoint.y() < team.getyGoongMinimum() || targetPoint.y() > team.getyGoongMaximum()) {
+            throw new IllegalArgumentException("궁 밖에 나갈 수 없습니다.");
+        }
+    }
+
+    protected abstract void validateGungMove(Point beforePoint, Point targetPoint);
+}
