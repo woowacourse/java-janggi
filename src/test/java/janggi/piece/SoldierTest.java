a6f463eb7ea6512a8caccb6d123fa1fef6c1bc2c
kjyyjk <rlawnsdud920@gmail.com>
2025-03-29 19:07:59 +0900

    test(red): 졸병이 궁성 내에서 앞을 향해 대각으로 움직이는 기능

--- src/test/java/janggi/piece/SoldierTest.java
+++ src/test/java/janggi/piece/SoldierTest.java
@@ -53,10 +53,36 @@ class SoldierTest {
     }
 
     @DisplayName("졸병이_궁성_내에서_대각으로_뒤를_향해_움직이면_예외를_발생한다")
+    @CsvSource(value = {"RED:3:0:4:1", "RED:4:1:5:2", "RED:5:0:4:1", "RED:4:1:3:2", "GREEN:3:9:4:8",
+            "GREEN:4:8:5:7", "GREEN:5:9:4:8", "GREEN:4:8:3:7"}, delimiterString = ":")
+    @ParameterizedTest
+    void should_ThrowException_WhenMoveDiagonalBackward(
+                    Team team,
+                    int startColumn,
+                    int startRow,
+                    int goalColumn,
+                    int goalRow
+            ) {
+        // given
+        Map<Position, Piece> initialBoard = new HashMap<>();
+        Position start = createPosition(startColumn, startRow);
+        Position goal = createPosition(goalColumn, goalRow);
+        Soldier piece = new Soldier(team);
+
+        initialBoard.put(start, piece);
+        Board board = new Board(initialBoard);
+
+        // then
+        assertThatThrownBy(() -> piece.validateMovable(board, start, goal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 선택하신 기물은 해당 목적지로 이동할 수 없습니다.");
+    }
+
+    @DisplayName("졸병은_궁성_내에서_앞을_향해_대각으로_움직일_수_있다")
     @CsvSource(value = {"RED:3:2:4:1", "RED:4:1:5:0", "RED:5:2:4:1", "RED:4:1:3:0", "GREEN:3:7:4:8",
             "GREEN:4:8:5:9", "GREEN:5:7:4:8", "GREEN:4:8:3:9"}, delimiterString = ":")
     @ParameterizedTest
-    void should_ThrowException_WhenMoveDiagonalBackward(
+    void should_Not_ThrowException_WhenMoveDiagonalForward(
             Team team,
             int startColumn,
             int startRow,
@@ -73,8 +99,7 @@ class SoldierTest {
         Board board = new Board(initialBoard);
 
         // then
-        assertThatThrownBy(() -> piece.validateMovable(board, start, goal))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 선택하신 기물은 해당 목적지로 이동할 수 없습니다.");
+        assertThatCode(() -> piece.validateMovable(board, start, goal))
+                .doesNotThrowAnyException();
     }
 }
