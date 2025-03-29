8bd66c491906442014669aa28e9c983259fdc7d5
GiSeok Kang <ime.rokz@gmail.com>
2025-03-29 21:29:08 +0900

    feat: 왕 궁성 내 이동에 관한 기능 구현

--- src/main/java/direction/Point.java
+++ src/main/java/direction/Point.java
@@ -1,6 +1,34 @@
 package direction;
 
-public record Point(int column, int row) {
+import java.util.Objects;
+
+public final class Point {
+
+    private static final int HORIZONTAL_START = 1;
+    private static final int HORIZONTAL_END = 9;
+    private static final int VERTICAL_START = 1;
+    private static final int VERTICAL_END = 10;
+
+    private static final int PALACE_START_X = 4;
+    private static final int PALACE_END_X = 6;
+
+    private static final int CHO_PALACE_START_Y = 10;
+    private static final int CHO_PALACE_END_Y = 8;
+    private static final int HAN_PALACE_START_Y = 1;
+    private static final int HAN_PALACE_END_Y = 3;
+
+    private static final int PALACE_CENTER_X = 5;
+
+    private static final int CHO_PALACE_CENTER_Y = 9;
+    private static final int HAN_PALACE_CENTER_Y = 2;
+
+    private final int column;
+    private final int row;
+
+    public Point(int column, int row) {
+        this.column = column;
+        this.row = row;
+    }
 
     public Point minus(Point other) {
         return new Point(column - other.column, row - other.row);
@@ -24,5 +52,47 @@ public record Point(int column, int row) {
 
     public int moveCount(Point point) {
         return Math.abs(point.row) + Math.abs(point.column);
+    }
+
+    public boolean isPalaceCorner() {
+        return (PALACE_START_X == column || PALACE_END_X == column) && (CHO_PALACE_END_Y == row
+                || CHO_PALACE_START_Y == row
+                || HAN_PALACE_START_Y == row || HAN_PALACE_END_Y == row);
+    }
+
+    public boolean isPalace() {
+        return (PALACE_START_X <= column && PALACE_END_X >= column) && (
+                (CHO_PALACE_START_Y >= row && CHO_PALACE_END_Y <= row)
+                        || (HAN_PALACE_START_Y <= row && HAN_PALACE_END_Y >= row));
+    }
+
+    public boolean isPalaceCenter() {
+        return (PALACE_CENTER_X == column && (CHO_PALACE_CENTER_Y == row || HAN_PALACE_CENTER_Y == row));
+    }
+
+    public int column() {
+        return column;
+    }
+
+    public int row() {
+        return row;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Point) obj;
+        return this.column == that.column &&
+                this.row == that.row;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column, row);
     }
 }
--- src/main/java/piece/General.java
+++ src/main/java/piece/General.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class General extends Piece  {
 
-    private static final List<Movement> PATH = List.of(Movement.LEFT, Movement.RIGHT, Movement.UP, Movement.DOWN);
+    private static final List<Movement> PATH = List.of(Movement.LEFT, Movement.RIGHT, Movement.UP, Movement.DOWN,
+            Movement.LEFT_UP, Movement.LEFT_DOWN, Movement.RIGHT_UP, Movement.RIGHT_DOWN);
 
     public General() {
     }
@@ -20,7 +21,17 @@ public class General extends Piece  {
         Movement destinationMovement = getDestinationMovement(destination);
         validateIsExistPieceInPoint(allPieces, current.move(destinationMovement));
 
+        if (destinationMovement.isDiagonalMove()) {
+            validateIsPalace(destination);
+        }
+
         current = current.move(destinationMovement);
+    }
+
+    private void validateIsPalace(Point destination) {
+        if ((!current.isPalaceCenter() && !current.isPalaceCorner()) || !destination.isPalace()) {
+            throw new IllegalArgumentException("[ERROR] 선택할 수 없는 목적지입니다.");
+        }
     }
 
     @Override
