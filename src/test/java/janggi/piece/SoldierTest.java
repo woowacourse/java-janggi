081f9e72a71a3b6e55e1ee70ad40ece884001fd7
changuii <rhljh7410@gmail.com>
2025-03-29 14:38:43 +0900

    test: SoldierTest 리팩터링

--- src/test/java/janggi/piece/SoldierTest.java
+++ src/test/java/janggi/piece/SoldierTest.java
@@ -5,58 +5,75 @@ import static org.assertj.core.api.Assertions.assertThat;
 import janggi.board.Board;
 import janggi.coordinate.JanggiPosition;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class SoldierTest {
 
-    @DisplayName("Soldier는 뒷 방향을 제외하고 한 칸을 이동할 수 있다.")
-    @Test
-    void soldiers() {
+    private final Piece soldierOfHan = new Soldier(Country.HAN);
+    private final Map<JanggiPosition, Piece> janggiBoard = new HashMap<>();
 
-        // given
-        final Piece soldierPiece = new Soldier(Country.HAN);
-        final JanggiPosition now = new JanggiPosition(2, 2);
-        final JanggiPosition ableDest = new JanggiPosition(3, 2);
-        final JanggiPosition notAbleDest = new JanggiPosition(1, 2);
-        final Board board = new Board(new HashMap<>());
-
-        // when
-        final boolean actual1 = soldierPiece.isAbleToMove(now, ableDest, board);
-        final boolean actual2 = soldierPiece.isAbleToMove(now, notAbleDest, board);
-
-        // then
-        org.junit.jupiter.api.Assertions.assertAll(
-                () -> assertThat(actual1).isTrue(),
-                () -> assertThat(actual2).isFalse()
-        );
+    @BeforeEach
+    void initJanggiBoard() {
+        janggiBoard.clear();
     }
 
-    @DisplayName("Soldier은 상대 궁성에서 뒷 방향을 제외하고 대각 -> 중심, 중심 -> 대각으로 이동할 수 있다.")
-    @ParameterizedTest
-    @MethodSource
-    void soldier1(final JanggiPosition source, final JanggiPosition destination, final Country country) {
-        // given
-        final Piece soldierPiece = new Soldier(country);
-        final Board board = new Board(new HashMap<>());
+    @Nested
+    @DisplayName("기물 이동 로직")
+    class CanMove {
 
-        // when
-        final boolean actual = soldierPiece.isAbleToMove(source, destination, board);
+        @DisplayName("Soldier는 뒷 방향을 제외하고 한 칸을 이동할 수 있다.")
+        @ParameterizedTest
+        @MethodSource
+        void soldiers(final JanggiPosition dest, final boolean expected) {
 
-        // then
-        assertThat(actual).isTrue();
+            // given
+            final JanggiPosition now = new JanggiPosition(2, 2);
+            final Board board = new Board(janggiBoard);
+
+            // when
+            final boolean actual = soldierOfHan.canMove(now, dest, board);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        static Stream<Arguments> soldiers() {
+            return Stream.of(
+                    Arguments.of(new JanggiPosition(3, 2), true),
+                    Arguments.of(new JanggiPosition(1, 2), false)
+            );
+        }
+
+        @DisplayName("Soldier은 상대 궁성에서 뒷 방향을 제외하고 대각 -> 중심, 중심 -> 대각으로 이동할 수 있다.")
+        @ParameterizedTest
+        @MethodSource
+        void soldier1(final JanggiPosition source, final JanggiPosition destination, final Country country) {
+            // given
+            final Piece soldier = new Soldier(country);
+            final Board board = new Board(janggiBoard);
+
+            // when
+            final boolean actual = soldier.canMove(source, destination, board);
+
+            // then
+            assertThat(actual).isTrue();
+        }
+
+        static Stream<Arguments> soldier1() {
+            return Stream.of(
+                    Arguments.of(new JanggiPosition(8, 4), new JanggiPosition(9, 5), Country.HAN),
+                    Arguments.of(new JanggiPosition(9, 5), new JanggiPosition(10, 6), Country.HAN),
+                    Arguments.of(new JanggiPosition(3, 4), new JanggiPosition(2, 5), Country.CHO),
+                    Arguments.of(new JanggiPosition(2, 5), new JanggiPosition(1, 6), Country.CHO)
+            );
+        }
     }
 
-    static Stream<Arguments> soldier1(){
-        return Stream.of(
-                Arguments.of(new JanggiPosition(8, 4), new JanggiPosition(9, 5), Country.HAN),
-                Arguments.of(new JanggiPosition(9, 5), new JanggiPosition(10, 6), Country.HAN),
-                Arguments.of(new JanggiPosition(3, 4), new JanggiPosition(2, 5), Country.CHO),
-                Arguments.of(new JanggiPosition(2, 5), new JanggiPosition(1, 6), Country.CHO)
-        );
-    }
 }
