64a830a6adc375f4ac298d01787dad743d149e32
supernovaMK <minkikim2002@gmail.com>
2025-03-29 22:57:37 +0900

    feat: 궁성에 영향을 받는 기물들 추상화

---
+++ src/main/java/model/piece/Palace.java
@@ -0,0 +1,30 @@
+package model.piece;
+
+import java.util.List;
+import model.Point;
+
+public enum Palace {
+    ALL_PALACE(List.of(new Point(3, 0), new Point(4, 1), new Point(3, 2), new Point(5, 0), new Point(5, 2),
+            new Point(3, 9), new Point(4, 8), new Point(3, 7), new Point(5, 9), new Point(5, 7))),
+    BLUE_PALACE(List.of(new Point(3, 0), new Point(4, 1), new Point(3, 2), new Point(5, 0), new Point(5, 2))),
+    RED_PALACE(List.of(new Point(3, 9), new Point(4, 8), new Point(3, 7), new Point(5, 9), new Point(5, 7)));
+
+    private final List<Point> points;
+
+    Palace(List<Point> points) {
+        this.points = points;
+    }
+
+    public static Palace wherePalace(Point point) {
+        for (Palace palace : Palace.values()) {
+            if (palace.getPoints().contains(point)) {
+                return palace;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 탐색입니다.");
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+}
---
+++ src/main/java/model/piece/PalaceMovablePieces.java
@@ -0,0 +1,25 @@
+package model.piece;
+
+import model.Point;
+import model.Team;
+
+public abstract class PalaceMovablePieces extends Piece {
+
+    protected PalaceMovablePieces(Team team, PieceName pieceName) {
+        super(team, pieceName);
+    }
+
+    @Override
+    public boolean isValidPoint(Point beforePoint, Point targetPoint) {
+        if (Palace.ALL_PALACE.getPoints().contains(beforePoint)) {
+            validateGungCross(beforePoint, targetPoint);
+            return true;
+        }
+        validateMovement(beforePoint, targetPoint);
+        return true;
+    }
+
+    public abstract void validateGungCross(Point beforePoint, Point targetPoint);
+
+    public abstract void validateMovement(Point beforePoint, Point targetPoint);
+}
