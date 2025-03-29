b4d4fbb1fe2af4710ed278747b425693e09343d5
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-29 22:05:13 +0900

    feat: Soldier 기물, 인접 노드 방식으로 유효성 검사하도록 기능 구현

--- src/test/java/piece/SoldierTest.java
+++ src/test/java/piece/SoldierTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import position.LineDirection;
 import position.Position;
+import position.PositionFactory;
 
 import java.util.Map;
 
@@ -13,48 +14,42 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SoldierTest {
 
-    @DisplayName("Soldier는 팀에 따라 이동 방향이 바뀐다.")
-    @Test
-    void moveActions() {
-        // given
-
-        // when
-
-        // then
-    }
-
-    @DisplayName("Soldier는 뒷 방향을 제외하고 한 칸을 이동할 수 있다.")
+    @DisplayName("Soldier는 뒷 방향을 제외하고 한 간선을 이동할 수 있다.")
     @Test
     void isAbleToMoveByDirection() {
 
         // given
-        final Position src = new Position(2, 3);
+        PositionFactory positionFactory = new PositionFactory();
+        positionFactory.basicSettingGraph();
+
         final Country country = Country.HAN;
         Country.assignDirection(country, LineDirection.UP);
+        final Position dumyPosition = new Position(2, 3);
+        Position src = dumyPosition;
         final Piece soldier = new Soldier(src, country);
         final Board board = new Board(Map.of(
                 src, soldier
         ));
 
-        // when & then: 1 : success
+        // when & then: 1 : failure
         final Position ableDest1 = new Position(2, 2);
-        assertThatCode(
-                () -> soldier.validateMove(src, ableDest1, board)
-        ).doesNotThrowAnyException();
-
-        // when & then : 2 : failure
-        final Position ableDest2 = new Position(2, 4);
         assertThatThrownBy(
-                () -> soldier.validateMove(src, ableDest2, board)
+                () -> soldier.validateMove(src, ableDest1, board)
         ).isInstanceOf(IllegalArgumentException.class);
 
-        // when & then: 1 : success
+        // when & then : 2 : success
+        final Position ableDest2 = new Position(2, 4);
+        assertThatCode(
+                () -> soldier.validateMove(src, ableDest2, board)
+        ).doesNotThrowAnyException();
+
+        // when & then: 3 : success
         final Position ableDest3 = new Position(3, 3);
         assertThatCode(
                 () -> soldier.validateMove(src, ableDest3, board)
         ).doesNotThrowAnyException();
 
-        // when & then: 1 : success
+        // when & then: 4 : success
         final Position ableDest4 = new Position(1, 3);
         assertThatCode(
                 () -> soldier.validateMove(src, ableDest4, board)
