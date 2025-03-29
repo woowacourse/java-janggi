df337384ad7565e53c069af7907be359fafbc43a
gabean13 <choegabin171@gmail.com>
2025-03-29 20:05:50 +0900

    feat: Path 기능 구현
    - 이동하는 방향을 저장한다.
    - start, end로 좌표상 이동가능하다면 path를 반환한다.
    - start, end로 이동 좌표를 계산한다

---
+++ src/main/java/janggi/domain/piece/Path.java
@@ -0,0 +1,83 @@
+package janggi.domain.piece;
+
+import janggi.domain.board.Direction;
+import janggi.domain.board.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class Path {
+    private final List<Direction> path;
+
+    public Path(List<Direction> path) {
+        this.path = path;
+    }
+
+    public static Path calculatePath(Point start, Point end, Set<List<Direction>> paths) {
+        return paths.stream()
+                .filter(path -> canMoveEndPointByPath(start, end, path))
+                .findFirst()
+                .map(Path::new)
+                .orElseThrow(() -> new IllegalArgumentException("해당 위치로 이동할 수 없습니다."));
+    }
+
+    private static boolean canMoveEndPointByPath(final Point start, Point end, List<Direction> path) {
+        Point current = start;
+        for (Direction direction : path) {
+            if (!direction.canMoveFrom(current)) {
+                break;
+            }
+            current = direction.moveFrom(current);
+        }
+        return current.equals(end);
+    }
+
+    public static Path calculatePath(Point start, Point end, List<Direction> directions) {
+        Direction movedDirection = directions.stream()
+                .filter(direction -> canMoveEndPointByDirection(start, end, direction))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 위치로 이동할 수 없습니다."));
+        return getPathByDirection(start, end, movedDirection);
+    }
+
+    private static boolean canMoveEndPointByDirection(final Point start, Point end,
+                                                      Direction direction) {
+        Point current = start;
+        while (!current.equals(end) && direction.canMoveFrom(current)) {
+            current = direction.moveFrom(current);
+        }
+        return current.equals(end);
+    }
+
+    private static Path getPathByDirection(Point start, Point end, Direction movedDirection) {
+        List<Direction> path = new ArrayList<>();
+        int moveCount = Math.abs(start.x() - end.x()) + Math.abs(start.y() - end.y());
+        if (movedDirection.isDiagonal()) {
+            return getPath(movedDirection, moveCount/2, path);
+        }
+        return getPath(movedDirection, moveCount, path);
+    }
+
+    private static Path getPath(Direction movedDirection, int moveCount, List<Direction> path) {
+        for (int i = 0; i < moveCount; i++) {
+            path.add(movedDirection);
+        }
+        return new Path(path);
+    }
+
+    public List<Point> getMovedPoints(final Point start, Point end) {
+        Point current = start;
+        List<Point> movedPoints = new ArrayList<>();
+        for (Direction direction : path) {
+            movedPoints.add(current);
+            current = direction.moveFrom(current);
+        }
+        movedPoints.add(end);
+        return movedPoints;
+    }
+
+    public List<Direction> getPath() {
+        return Collections.unmodifiableList(path);
+    }
+}
---
+++ src/test/java/janggi/domain/piece/PathTest.java
@@ -0,0 +1,97 @@
+package janggi.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import janggi.domain.board.Direction;
+import janggi.domain.board.Point;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("경로 테스트")
+class PathTest {
+
+    @DisplayName("방향으로 경로를 찾을 수 있다.")
+    @Test
+    void calculatePath_Direction() {
+        // given
+        Point start = new Point(1, 1);
+        Point end = new Point(4, 4);
+        List<Direction> availablePath = List.of(Direction.DOWN_LEFT, Direction.DOWN_RIGHT);
+
+        // when
+        Path path = Path.calculatePath(start, end, availablePath);
+
+        // then
+        assertThat(path.getPath())
+                .isEqualTo(List.of(Direction.DOWN_RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_RIGHT));
+    }
+
+    @DisplayName("방향으로 경로를 찾을 수 없으면 예외가 발생한다.")
+    @Test
+    void calculatePath_DirectionException() {
+        // given
+        Point start = new Point(1, 1);
+        Point end = new Point(4, 3);
+        List<Direction> availablePath = List.of(Direction.DOWN_LEFT, Direction.DOWN_RIGHT);
+
+        // then
+        assertThatThrownBy(() -> Path.calculatePath(start, end, availablePath))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 위치로 이동할 수 없습니다.");
+    }
+
+    @DisplayName("고정된 경로를 찾을 수 있다.")
+    @Test
+    void calculatePath_Path() {
+        // given
+        Point start = new Point(1, 1);
+        Point end = new Point(3, 2);
+        Set<List<Direction>> availablePath = Set.of(
+                List.of(Direction.DOWN_LEFT, Direction.DOWN),
+                List.of(Direction.DOWN_RIGHT, Direction.DOWN)
+        );
+
+        // when
+        Path path = Path.calculatePath(start, end, availablePath);
+
+        // then
+        assertThat(path.getPath())
+                .isEqualTo(List.of(Direction.DOWN_RIGHT, Direction.DOWN));
+    }
+
+    @DisplayName("고정된 경로를 찾을 수 없으면 예외가 발생한다.")
+    @Test
+    void calculatePath_PathException() {
+        // given
+        Point start = new Point(1, 1);
+        Point end = new Point(3, 3);
+        Set<List<Direction>> availablePath = Set.of(
+                List.of(Direction.DOWN_LEFT, Direction.DOWN),
+                List.of(Direction.DOWN_RIGHT, Direction.DOWN)
+        );
+
+        // then
+        assertThatThrownBy(() -> Path.calculatePath(start, end, availablePath))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 위치로 이동할 수 없습니다.");
+    }
+
+    @DisplayName("경로로 이동 좌표를 알 수 있다.")
+    @Test
+    void getMovedPoints() {
+        // given
+        Point start = new Point(4, 2);
+        Point end = new Point(1, 1);
+        Path path = new Path(List.of(Direction.UP, Direction.UP, Direction.UP_LEFT));
+
+        // when
+        List<Point> movedPoints = path.getMovedPoints(start, end);
+
+        // then
+        assertThat(movedPoints)
+                .isEqualTo(List.of(new Point(4, 2), new Point(3, 2), new Point(2, 2), new Point(1, 1)));
+    }
+}
