8126ee1fe46bfb8c8c5a8512479fcc3466d9deea
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-30 12:41:19 +0900

    feat: 궁성 바운더리 처리 기능 추라

--- src/main/java/piece/General.java
+++ src/main/java/piece/General.java
@@ -3,8 +3,9 @@ package piece;
 import board.Board;
 import position.Position;
 import position.PositionFactory;
+import validator.PalaceCheckable;
 
-public class General extends Piece {
+public class General extends Piece implements PalaceCheckable {
 
     public General(final Position position, final Country country) {
         super(position, country);
@@ -13,6 +14,7 @@ public class General extends Piece {
     // TODO 2025. 3. 29. 17:35: PositionFactory에 Position들을 넘기는 게 올바른 책임인가?
     @Override
     public void validateMoveCondition(Position src, Position dest, Board board) {
+        validateBound(dest);
         PositionFactory.validateAdjacentPositionBy(src, dest);
     }
 
