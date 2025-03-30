a095866f1c6166ca78f775502e8d9950aa307b18
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-30 13:21:12 +0900

    feat: Application에 셋팅 연결

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,21 @@
 package view;
 
+import piece.Country;
+import position.LineDirection;
 import position.Position;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final Scanner sc = new Scanner(System.in);
+
+    private static final Map<String, LineDirection> LINE_DIRECTION_FORMAT = Map.of(
+            "위", LineDirection.UP,
+            "아래", LineDirection.DOWN
+    );
 
     public static List<Position> readPositions() {
         System.out.println("move <scrPosition> <destPosition> 형식으로 입력해주세요");
@@ -27,5 +35,20 @@ public class InputView {
             positions.add(new Position(x, y));
         }
         return positions;
+    }
+
+    public static LineSettingDto readLineSettingByCountry() {
+        System.out.println("한 나라가 어느 위치에 배정받을 지를 정해주세요. (위, 아래)");
+        System.out.println("ex) 위");
+        String input = sc.nextLine();
+        LineDirection lineDirection = getLineDirectionByInput(input);
+        return new LineSettingDto(Country.HAN, lineDirection);
+    }
+
+    private static LineDirection getLineDirectionByInput(String input) {
+        if (!LINE_DIRECTION_FORMAT.containsKey(input)) {
+            throw new IllegalArgumentException("잘못된 입력입니다: " + input);
+        }
+        return LINE_DIRECTION_FORMAT.get(input);
     }
 }
