b9bcabd8a222319138e04f1f50e6d60007f14d9e
[CLOUD4] 백승주 <bsj5967@naver.com>
2025-03-31 00:55:52 +0900

    feat(db): DB 연결 테스트

---
+++ src/test/java/dao/UserDaoTest.java
@@ -0,0 +1,21 @@
+package dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import dao.UserDao;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class UserDaoTest {
+
+    private final UserDao userDao = new UserDao();
+
+    @Test
+    public void connection() {
+        try (final var connection = userDao.getConnection()) {
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
