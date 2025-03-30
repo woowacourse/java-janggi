35e4a482258fea9a15feb75c0f60b9af3b9f99ab
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-30 23:41:32 +0900

    feat: jdbc 연결

---
+++ src/main/java/database/DbConnection.java
@@ -0,0 +1,46 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    private static DbConnection dbConnection;
+    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
+    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USERNAME = "root"; //  MySQL 서버 아이디
+    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+
+    private DbConnection() {
+    }
+
+    public static DbConnection getInstance() {
+        if (dbConnection == null) {
+            dbConnection = new DbConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC 드라이버 로드 실패: " + e.getMessage());
+            throw new SQLException("JDBC 드라이버를 찾을 수 없습니다.");
+        }
+        return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("연결 종료 실패: " + e.getMessage());
+            }
+        }
+    }
+}
