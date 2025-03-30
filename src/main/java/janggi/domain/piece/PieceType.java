a71d61cc1430f2b52da41f5bcf400c760f881966
gabean13 <choegabin171@gmail.com>
2025-03-30 22:30:49 +0900

    feat: PieceType으로 HanSoldier, ChuSolider 추가

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -1,19 +1,29 @@
 package janggi.domain.piece;
 
+import janggi.domain.board.Dynasty;
+import java.util.function.Function;
+
 public enum PieceType {
-    GENERAL(0),
-    CHARIOT(13),
-    CANNON(7),
-    HORSE(5),
-    GUARD(3),
-    ELEPHANT(3),
-    SOLIDER(2)
+    GENERAL(0, General::new),
+    CHARIOT(13, Chariot::new),
+    CANNON(7, Cannon::new),
+    HORSE(5, Horse::new),
+    GUARD(3, Guard::new),
+    ELEPHANT(3, Elephant::new),
+    HAN_SOLIDER(2, HanSoldier::new),
+    CHU_SOLIDER(2, ChuSoldier::new)
     ;
 
     private final int score;
+    private final Function<Dynasty, Piece> function;
 
-    PieceType(int score) {
+    PieceType(int score, Function<Dynasty, Piece> function) {
         this.score = score;
+        this.function = function;
+    }
+
+    public Piece from(Dynasty dynasty) {
+        return this.function.apply(dynasty);
     }
 
     public int getScore() {
