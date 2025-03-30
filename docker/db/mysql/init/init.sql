e17dcd1561a9cea71752ffb1888d59f8b97a22a3
ljhee92 <weilainib7@gmail.com>
2025-03-30 17:17:58 +0900

    feat: 도커 테이블 init 추가

---
+++ docker/db/mysql/init/init.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS janggi
+DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE janggi;
+
+CREATE TABLE IF NOT EXISTS `turn` (
+    turn VARCHAR(3) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS `board` (
+	point_row       INT NOT NULL,
+	point_column    INT NOT NULL,
+	team            VARCHAR(3) NOT NULL,
+	piece_type       VARCHAR(6) NOT NULL,
+	PRIMARY KEY (point_row, point_column)
+);
