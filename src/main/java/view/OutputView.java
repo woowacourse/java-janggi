ac64f069a715b56d7afba889a79c6a8c15f69272
Han <jumdo12@gmail.com>
2025-03-30 21:18:44 +0900

    refactor: 출력 형싱 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -83,13 +83,14 @@ public class OutputView {
 
     public void printGameNames(List<GameRoomDTO> allGames) {
         System.out.println("현재 세이브 되어있는 게임 목록입니다.");
+        System.out.println();
         for (GameRoomDTO dto : allGames) {
             DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 m분");
             String creationTime = dto.creationDate().format(dateTimeFormatter);
-            System.out.println(
-                    "게임 이름 : " + dto.gameRoomName() +
-                            " 현재 턴 : " + dto.currTurn() +
-                            " 저장된 시간 " + creationTime);
+            System.out.println("게임 이름 : " + dto.gameRoomName());
+            System.out.println("현재 턴 : " + dto.currTurn());
+            System.out.println("저장된 시간 " + creationTime);
+            System.out.println();
         }
     }
 }
