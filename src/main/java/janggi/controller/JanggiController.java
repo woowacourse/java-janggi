3000e9c890f4458d96ff7b1b82368c57da2002c1
yush <yush2202@gmail.com>
2025-03-30 16:27:56 +0900

    refactor: 메서드 분리

--- src/main/java/janggi/controller/JanggiController.java
+++ src/main/java/janggi/controller/JanggiController.java
@@ -30,15 +30,68 @@ public class JanggiController {
     }
 
     public void run() throws SQLException {
-
         Connection connection = new Connection();
-        // 테이블이 있는지 없는지(턴 테이블과 기물 테이블이 둘 다 존재해야함) 체크
+        initializeDatabase(connection);
+
+        while (true) {
+            Board board = readBoardFromDatabase();
+            showCurrentState(board);
+
+            UserContinueResponse userContinueResponse = UserExceptionHandler.retryUntilSuccess(inputView::continueGame);
+
+            if (userContinueResponse == UserContinueResponse.QUIT) {
+                outputView.printWinnerWithSurrender(board.getTurn());
+                connection.dropTable("piece");
+                connection.dropTable("turn");
+                break;
+            }
+
+            Piece selectedPiece = UserExceptionHandler.retryUntilSuccess(() -> selectPiece(board));
+            Set<Position> possibleDestinations = board.findDestinations(selectedPiece);
+
+            move(possibleDestinations, board, selectedPiece);
+
+            if (board.isGameEnd(board.getTurn())) {
+                Team winner = board.getWinner(board.getTurn());
+                outputView.printWinnerWithGameEnd(winner);
+
+                connection.dropTable("piece");
+                connection.dropTable("turn");
+                break;
+            }
+
+            updateDatabase(board);
+        }
+    }
+
+    private Board readBoardFromDatabase() {
+        Team currentTurn = turnDao.readTeam();
+        Pieces currentPieces = new Pieces(pieceDao.readAllPiece());
+        return new Board(currentPieces, currentTurn);
+    }
+
+    private void showCurrentState(Board board) {
+        outputView.printTeamScore(board.getTeamScore(Team.RED), board.getTeamScore(Team.BLUE));
+        outputView.printBoard(board.getPieces());
+        outputView.printTurn(board.getTurn());
+    }
+
+    private void updateDatabase(Board board) {
+        pieceDao.deleteAllPiece();
+
+        for (Piece piece : board.getPieces()) {
+            pieceDao.addPiece(piece);
+        }
+        turnDao.updateTeam(board.getTurn());
+    }
+
+    private void initializeDatabase(Connection connection) throws SQLException {
         if (!connection.isTableExist("piece") ||
-                !connection.isTableExist("turn")) {// if (테이블이 둘 중 하나라도 없다면 테이블 모두 삭제 후 새로 생성)
-            if (connection.isTableExist("piece")) { // piece가 있다면 그 테이블 삭제
+                !connection.isTableExist("turn")) {
+            if (connection.isTableExist("piece")) {
                 connection.dropTable("piece");
             }
-            if (connection.isTableExist("turn")) { // turn 이 있다면 그 테이블 삭제
+            if (connection.isTableExist("turn")) {
                 connection.dropTable("turn");
             }
 
@@ -56,63 +109,6 @@ public class JanggiController {
 
             // 턴 테이블에 블루팀 저장
             turnDao.addTeam(Team.BLUE);
-        }
-
-        // if (테이블이 있다면? 테이블에서 데이터를 불러와서 게임 진행) // 이건 어차피 반드시 실행되어야 할 프로세스임
-        // 불러올 데이터 -> 기물들, 현재 턴
-        Pieces currentPieces = new Pieces(pieceDao.readAllPiece());
-        Team currentTurn = turnDao.readTeam();
-        Board board = new Board(currentPieces, currentTurn);
-        List<Piece> pieces = board.getPieces();
-
-        // 이하는 게임 진행
-        outputView.printBoard(pieces);
-
-        while (true) {
-            currentTurn = turnDao.readTeam(); // db에서 턴 읽어오기
-            outputView.printTurn(currentTurn);
-            UserContinueResponse userContinueResponse = UserExceptionHandler.retryUntilSuccess(inputView::continueGame);
-
-            if (userContinueResponse == UserContinueResponse.QUIT) { // 게임종료
-                outputView.printWinnerWithSurrender(currentTurn);
-                connection.dropTable("piece");
-                connection.dropTable("turn");
-                break;
-            }
-
-            Board finalBoard = board;
-            Piece selectedPiece = UserExceptionHandler.retryUntilSuccess(() -> selectPiece(finalBoard));
-            Set<Position> possibleDestinations = board.findDestinations(selectedPiece);
-
-            move(possibleDestinations, board, selectedPiece);
-
-            if (board.isGameEnd(currentTurn)) { // 게임 종료
-                Team winner = board.getWinner(currentTurn);
-                outputView.printWinnerWithGameEnd(winner);
-
-                connection.dropTable("piece");
-                connection.dropTable("turn");
-                break;
-            }
-
-            outputView.printTeamScore(board.getTeamScore(Team.RED), board.getTeamScore(Team.BLUE));
-            outputView.printBoard(board.getPieces());
-
-            // -- 한 턴 종료
-
-            // piece db 데이터 전체 삭제
-            pieceDao.deleteAllPiece();
-            // piece db 새로 생성
-            for (Piece piece : board.getPieces()) {
-                pieceDao.addPiece(piece);
-            }
-
-            // piece 읽어오기
-            List<Piece> piecesFromDb = pieceDao.readAllPiece();
-            board = new Board(new Pieces(piecesFromDb), Team.getOtherTeam(currentTurn));
-
-            // 턴 db에 업데이트
-            turnDao.updateTeam(currentTurn);
         }
     }
 
