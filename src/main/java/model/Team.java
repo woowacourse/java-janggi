e2bdca67059b2d85a721e1857aae39c8ab19a479
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-03-30 17:43:14 +0900

    refactor(model) : 필드 이름 변경

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -5,17 +5,20 @@ import java.util.Arrays;
 public enum Team {
     RED("한"),
     BLUE("초");
-    private final String team;
+    private final String teamName;
 
-    Team(String team) {
-        this.team = team;
-    }
-
-    public String getTeamName() {
-        return team;
+    Team(String teamName) {
+        this.teamName = teamName;
     }
 
     public static Team getTeamWithName(String teamName) {
-        return Arrays.stream(values()).filter(team1 -> team1.getTeamName().equals(teamName)).findFirst().orElseThrow(() -> new NullPointerException("해당하는 이름의 팀이 존재하지 않습니다."));
+        return Arrays.stream(values())
+                .filter(team1 -> team1.getTeamName().equals(teamName))
+                .findFirst()
+                .orElseThrow(() -> new NullPointerException("해당하는 이름의 팀이 존재하지 않습니다."));
+    }
+
+    public String getTeamName() {
+        return teamName;
     }
 }
