bc14a6e5d965be842a90626592244971be4b07b8
ppparkta <pppppark2@gmail.com>
2025-03-30 23:01:34 +0900

    fix: boolean 파싱하도록 수정

--- src/test/java/janggi/board/PalaceTest.java
+++ src/test/java/janggi/board/PalaceTest.java
@@ -9,13 +9,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 class PalaceTest {
     @ParameterizedTest
     @ValueSource(strings = {
-            "0,3,true", "0,4,true", "0,5,true",
-            "1,3,true", "1,4,true", "1,5,true",
-            "2,3,true", "2,4,true", "2,5,true",
+            "3,0,true", "4,0,true", "5,0,true",
+            "3,1,true", "4,1,true", "5,1,true",
+            "3,2,true", "4,2,true", "5,2,true",
 
-            "7,3,true", "7,4,true", "7,5,true",
-            "8,3,true", "8,4,true", "8,5,true",
-            "9,3,true", "9,4,true", "9,5,true"
+            "3,7,true", "4,7,true", "5,7,true",
+            "3,8,true", "4,8,true", "5,8,true",
+            "3,9,true", "4,9,true", "5,9,true"
     })
     @DisplayName("궁성 내에 있는지 확인한다.")
     void test1(String point) {
@@ -23,7 +23,7 @@ class PalaceTest {
         String[] position = point.split(",");
         int column = Integer.parseInt(position[0]);
         int row = Integer.parseInt(position[1]);
-        boolean expected = Boolean.getBoolean(position[2]);
+        boolean expected = Boolean.parseBoolean(position[2]);
 
         // when
         boolean inPalace = Palace.isInPalace(new Position(column, row));
@@ -34,13 +34,13 @@ class PalaceTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "0,3,true", "0,4,false", "0,5,true",
-            "1,3,false", "1,4,true", "1,5,false",
-            "2,3,true", "2,4,false", "2,5,true",
+            "3,0,true", "4,0,false", "5,0,true",
+            "3,1,false", "4,1,true", "5,1,false",
+            "3,2,true", "4,2,false", "5,2,true",
 
-            "7,3,true", "7,4,false", "7,5,true",
-            "8,3,false", "8,4,true", "8,5,false",
-            "9,3,true", "9,4,false", "9,5,true"
+            "3,7,true", "4,7,false", "5,7,true",
+            "3,8,false", "4,8,true", "5,8,false",
+            "3,9,true", "4,9,false", "5,9,true"
     })
     @DisplayName("궁성 내 대각선 이동이 가능한 지점에 있는지 확인한다.")
     void test2(String point) {
@@ -48,7 +48,7 @@ class PalaceTest {
         String[] position = point.split(",");
         int column = Integer.parseInt(position[0]);
         int row = Integer.parseInt(position[1]);
-        boolean expected = Boolean.getBoolean(position[2]);
+        boolean expected = Boolean.parseBoolean(position[2]);
 
         // when
         boolean inPalace = Palace.canDiagonalInPalace(new Position(column, row));
