509fadc06037681287c3bed17964c62dd44f2576
2jin2.1031 <2jin2.1031@gmail.com>
2025-03-30 12:41:19 +0900

    feat: 궁성 바운더리 처리 기능 추라

--- src/test/java/piece/GuardTest.java
+++ src/test/java/piece/GuardTest.java
@@ -1,6 +1,7 @@
 package piece;
 
 import board.Board;
+import board.Palace;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import position.LineDirection;
@@ -24,7 +25,7 @@ class GuardTest {
         Country dumyCountry = Country.HAN;
         Country.assignDirection(dumyCountry, LineDirection.UP);
 
-        Position dumyPosition = new Position(2, 3);
+        Position dumyPosition = new Position(5, 2);
         Position src = dumyPosition;
         final Guard guard = new Guard(src, dumyCountry);
         Board board = new Board(Map.of(
@@ -32,13 +33,75 @@ class GuardTest {
         ));
 
         // when & then : 1 : success
-        Position canReachDestPosition = new Position(3, 3);
+        Position canReachDestPosition = new Position(4, 2);
         assertThatCode(
                 () -> guard.validateMove(src, canReachDestPosition, board)
         ).doesNotThrowAnyException();
 
         // when & then : 2 : failure
-        Position canNotReachDestPosition = new Position(4, 3);
+        Position canNotReachDestPosition = new Position(5, 5);
+        assertThatThrownBy(
+                () -> guard.validateMove(src, canNotReachDestPosition, board)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("인접 노드 한칸을 움직인다. : 대각선")
+    @Test
+    void adjacentPositionWithDiagonalSetting() {
+        // given
+        PositionFactory positionFactory = new PositionFactory();
+        positionFactory.basicSettingGraph();
+        positionFactory.diagonalSettingGraph(Palace.getCenterPositions());
+
+        Country dumyCountry = Country.HAN;
+        Country.assignDirection(dumyCountry, LineDirection.UP);
+
+        Position dumyPosition = new Position(4, 3);
+        Position src = dumyPosition;
+        final Guard guard = new Guard(src, dumyCountry);
+        Board board = new Board(Map.of(
+                src, guard
+        ));
+
+        // when & then : 1 : success
+        Position canReachDestPosition = new Position(5, 2);
+        assertThatCode(
+                () -> guard.validateMove(src, canReachDestPosition, board)
+        ).doesNotThrowAnyException();
+
+        // when & then : 2 : failure
+        Position canNotReachDestPosition = new Position(5, 4);
+        assertThatThrownBy(
+                () -> guard.validateMove(src, canNotReachDestPosition, board)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("궁성 밖을 빠져나갈 수 없다.")
+    @Test
+    void validateMoveByPalaceBound() {
+        // given
+        PositionFactory positionFactory = new PositionFactory();
+        positionFactory.basicSettingGraph();
+        positionFactory.diagonalSettingGraph(Palace.getCenterPositions());
+
+        Country dumyCountry = Country.HAN;
+        Country.assignDirection(dumyCountry, LineDirection.UP);
+
+        Position dumyPosition = new Position(4, 3);
+        Position src = dumyPosition;
+        final Guard guard = new Guard(src, dumyCountry);
+        Board board = new Board(Map.of(
+                src, guard
+        ));
+
+        // when & then : 1 : success
+        Position canReachDestPosition = new Position(5, 3);
+        assertThatCode(
+                () -> guard.validateMove(src, canReachDestPosition, board)
+        ).doesNotThrowAnyException();
+
+        // when & then : 2 : failure
+        Position canNotReachDestPosition = new Position(4, 4);
         assertThatThrownBy(
                 () -> guard.validateMove(src, canNotReachDestPosition, board)
         ).isInstanceOf(IllegalArgumentException.class);
