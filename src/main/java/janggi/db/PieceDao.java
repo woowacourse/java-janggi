2bd8a0f2d7d75051d20a2acb2a876f8bcecbd031
yush <yush2202@gmail.com>
2025-03-30 15:52:40 +0900

    feat: piece 테이블에 데이터를 삽입, 읽기, 삭제 기능 구현 및 테스트

---
+++ src/main/java/janggi/db/PieceDao.java
@@ -0,0 +1,96 @@
+package janggi.db;
+
+import janggi.domain.Team;
+import janggi.domain.piece.Cannon;
+import janggi.domain.piece.Chariot;
+import janggi.domain.piece.Elephant;
+import janggi.domain.piece.General;
+import janggi.domain.piece.Guard;
+import janggi.domain.piece.Horse;
+import janggi.domain.piece.Piece;
+import janggi.domain.piece.Soldier;
+import janggi.domain.position.Position;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PieceDao {
+    public void addPiece(final Piece piece) {
+        final var query = "INSERT INTO piece VALUES(NULL, ?, ? ,?, ?)";
+        try (final var connection = new Connection().getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, piece.getPieceType().name());
+            preparedStatement.setInt(2, piece.getPosition().x());
+            preparedStatement.setInt(3, piece.getPosition().y());
+            preparedStatement.setString(4, piece.getTeam().name());
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Piece> readAllPiece() {
+        final var query = "SELECT * FROM piece";
+        try (final var connection = new Connection().getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            List<Piece> pieces = new ArrayList<>();
+            ResultSet resultSet = preparedStatement.executeQuery(query);
+
+            while (resultSet.next()) {
+                switch (resultSet.getString("pieceType")) {
+                    case "SOLIDER":
+                        pieces.add(
+                                new Soldier(new Position(resultSet.getInt("positionX"), resultSet.getInt("positionY")),
+                                        Team.valueOf(resultSet.getString("team"))));
+                        break;
+                    case "GUARD":
+                        pieces.add(
+                                new Guard(new Position(resultSet.getInt("positionX"), resultSet.getInt("positionY")),
+                                        Team.valueOf(resultSet.getString("team"))));
+                        break;
+                    case "CHARIOT":
+                        pieces.add(
+                                new Chariot(new Position(resultSet.getInt("positionX"), resultSet.getInt("positionY")),
+                                        Team.valueOf(resultSet.getString("team"))));
+                        break;
+                    case "CANNON":
+                        pieces.add(
+                                new Cannon(new Position(resultSet.getInt("positionX"), resultSet.getInt("positionY")),
+                                        Team.valueOf(resultSet.getString("team"))));
+                        break;
+                    case "ELEPHANT":
+                        pieces.add(
+                                new Elephant(new Position(resultSet.getInt("positionX"), resultSet.getInt("positionY")),
+                                        Team.valueOf(resultSet.getString("team"))));
+                        break;
+                    case "HORSE":
+                        pieces.add(
+                                new Horse(new Position(resultSet.getInt("positionX"), resultSet.getInt("positionY")),
+                                        Team.valueOf(resultSet.getString("team"))));
+                        break;
+                    case "GENERAL":
+                        pieces.add(
+                                new General(new Position(resultSet.getInt("positionX"), resultSet.getInt("positionY")),
+                                        Team.valueOf(resultSet.getString("team"))));
+                        break;
+                }
+            }
+            return pieces;
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAllPiece() {
+        final var query = "TRUNCATE TABLE piece;";
+        try (final var connection = new Connection().getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ src/test/java/janggi/db/PieceDaoTest.java
@@ -0,0 +1,48 @@
+package janggi.db;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import janggi.domain.InitialElephantSetting;
+import janggi.domain.PiecesInitializer;
+import janggi.domain.piece.Piece;
+import java.util.List;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PieceDaoTest {
+
+    @Disabled
+    @DisplayName("기물이 테이블에 제대로 들어가는지 확인합니다.")
+    @Test
+    void addPieceTest() {
+        List<Piece> initialPieces = PiecesInitializer.initializePieces(InitialElephantSetting.INNER_ELEPHANT)
+                .getPieces();
+
+        for (Piece initialPiece : initialPieces) {
+            PieceDao pieceDao = new PieceDao();
+            pieceDao.addPiece(initialPiece);
+        }
+    }
+
+    @Disabled
+    @DisplayName("기물을 db에서 가져오는지 확인합니다.")
+    @Test
+    void readAllPieceTest() {
+        List<Piece> expected = PiecesInitializer.initializePieces(InitialElephantSetting.INNER_ELEPHANT)
+                .getPieces();
+
+        PieceDao pieceDao = new PieceDao();
+        List<Piece> initialPieces = pieceDao.readAllPiece();
+
+        assertThat(initialPieces).isEqualTo(expected);
+    }
+
+    @Disabled
+    @DisplayName("piece 테이블을 데이터를 전체 삭제하는지 확인합니다.")
+    @Test
+    void deleteAllPieceTest() {
+        PieceDao pieceDao = new PieceDao();
+        pieceDao.deleteAllPiece();
+    }
+}
