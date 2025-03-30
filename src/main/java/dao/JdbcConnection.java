bbfd257f2ecf5783ea16ad38cd94292d977f6313
LJM\eueo8 <eueo8259@gmail.com>
2025-03-30 22:47:42 +0900

    refactor : e.getStackTrace() 사용으로 수정

--- src/main/java/dao/JdbcConnection.java
+++ src/main/java/dao/JdbcConnection.java
@@ -23,7 +23,7 @@ public class JdbcConnection {
             return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
         } catch (final SQLException e) {
             System.err.println("DB 연결 오류:" + e.getMessage());
-            e.printStackTrace();
+            e.getStackTrace();
             return null;
         }
     }
