5387ef7416a7f6701f04888346c2a14536937092
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-03-31 01:08:59 +0900

    refactor: 개행에 의미 담도록 수정

--- src/main/java/model/piece/Castle.java
+++ src/main/java/model/piece/Castle.java
@@ -31,18 +31,12 @@ public class Castle {
     );
 
     private final List<Position> castle = List.of(
-        new Position(Column.ONE, Row.FOUR), new Position(Column.ONE, Row.FIVE),
-        new Position(Column.ONE, Row.SIX),
-        new Position(Column.TWO, Row.FOUR), new Position(Column.TWO, Row.FIVE),
-        new Position(Column.TWO, Row.SIX),
-        new Position(Column.THREE, Row.FOUR), new Position(Column.THREE, Row.FIVE),
-        new Position(Column.THREE, Row.SIX),
-        new Position(Column.EIGHT, Row.FOUR), new Position(Column.EIGHT, Row.FIVE),
-        new Position(Column.EIGHT, Row.SIX),
-        new Position(Column.NINE, Row.FOUR), new Position(Column.NINE, Row.FIVE),
-        new Position(Column.NINE, Row.SIX),
-        new Position(Column.TEN, Row.FOUR), new Position(Column.TEN, Row.FIVE),
-        new Position(Column.TEN, Row.SIX));
+        new Position(Column.ONE, Row.FOUR), new Position(Column.ONE, Row.FIVE), new Position(Column.ONE, Row.SIX),
+        new Position(Column.TWO, Row.FOUR), new Position(Column.TWO, Row.FIVE), new Position(Column.TWO, Row.SIX),
+        new Position(Column.THREE, Row.FOUR), new Position(Column.THREE, Row.FIVE), new Position(Column.THREE, Row.SIX),
+        new Position(Column.EIGHT, Row.FOUR), new Position(Column.EIGHT, Row.FIVE), new Position(Column.EIGHT, Row.SIX),
+        new Position(Column.NINE, Row.FOUR), new Position(Column.NINE, Row.FIVE), new Position(Column.NINE, Row.SIX),
+        new Position(Column.TEN, Row.FOUR), new Position(Column.TEN, Row.FIVE), new Position(Column.TEN, Row.SIX));
 
     public boolean inCastle(Position position) {
         return castle.contains(position);
