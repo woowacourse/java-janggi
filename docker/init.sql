0c455ca052c9adfb6652b7229a0ee62ae513566f
jhpark1227 <junhyeok0246@naver.com>
2025-03-31 01:04:44 +0900

    feat: 테이블 초기화를 위한 sql 및 docker-compose.yml 파일 작성

---
+++ docker/init.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS janggi;
+
+CREATE DATABASE janggi DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE janggi;
+
+create table game
+(
+    game_id int auto_increment
+        primary key,
+    turn    varchar(255) not null,
+    name    varchar(255) not null,
+    constraint name
+        unique (name)
+);
+
+create table piece
+(
+    y       int          not null,
+    x       int          not null,
+    type    varchar(255) not null,
+    team    varchar(255) not null,
+    game_id int          not null,
+    primary key (y, x, game_id)
+);
+
+
+
