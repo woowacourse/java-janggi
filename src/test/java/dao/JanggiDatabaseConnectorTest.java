23cb730527a5715b3edf60aa8c061f8d8d4f03c0
minSsan <msp1125@hanyang.ac.kr>
2025-03-30 23:26:39 +0900

    refactor(JanggiDatabaseConnectorTest): 드라이버 연결 테스트를 try with resources 방식으로 변경

--- src/test/java/dao/JanggiDatabaseConnectorTest.java
+++ src/test/java/dao/JanggiDatabaseConnectorTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 class JanggiDatabaseConnectorTest {
     @DisplayName("드라이버 연결 테스트")
@@ -15,9 +17,11 @@ class JanggiDatabaseConnectorTest {
         DatabaseConnector connector = new JanggiDatabaseConnector();
 
         // when
-        Connection connection = connector.getConnection();
-
-        // then
-        assertThat(connection).isNotNull();
+        try (Connection connection = connector.getConnection()) {
+            // then
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            fail("데이터베이스 연결 실패");
+        }
     }
 }
