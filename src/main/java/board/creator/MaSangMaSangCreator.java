6999de613c3badb7134e819875e7e627db5e2669
soeun2537 <soeun2537@gmail.com>
2025-03-30 15:58:24 +0900

    refactor: 상차림 클래스명 변경

---
+++ src/main/java/board/creator/MaSangMaSangCreator.java
@@ -0,0 +1,25 @@
+package board.creator;
+
+import coordinate.Coordinate;
+import java.util.List;
+import java.util.Map;
+import piece.Piece;
+import piece.PieceType;
+import team.Team;
+
+public class MaSangMaSangCreator extends TableSettingCreator {
+
+    @Override
+    public Map<Coordinate, Piece> create(Team team) {
+        List<Integer> x = team.getMaSangXCoordinates();
+        int y = team.getMaSangYCoordinate();
+
+        Map<Coordinate, Piece> pieces = super.create(team);
+        pieces.put(new Coordinate(x.get(0), y), new Piece(team, PieceType.마));
+        pieces.put(new Coordinate(x.get(1), y), new Piece(team, PieceType.상));
+        pieces.put(new Coordinate(x.get(2), y), new Piece(team, PieceType.마));
+        pieces.put(new Coordinate(x.get(3), y), new Piece(team, PieceType.상));
+
+        return pieces;
+    }
+}
