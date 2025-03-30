52bfa02156cc2ca4e4a8cd7187295c74b9aa223b
Jin409 <sualng123@gmail.com>
2025-03-30 15:02:01 +0900

    feat(Guard): 사 의 움직임 구현

--- src/main/java/domain/pieces/Guard.java
+++ src/main/java/domain/pieces/Guard.java
@@ -1,20 +1,34 @@
 package domain.pieces;
 
-import static domain.pieces.PieceNames.GUARD;
-
 import domain.Team;
-import domain.board.PieceOnRoute;
 import domain.board.BoardPoint;
-import execptions.JanggiArgumentException;
+import domain.board.PieceOnRoute;
+import domain.movements.DefaultMovement;
+import domain.movements.Direction;
+import domain.movements.PieceMovement;
+import domain.movements.Route;
+import static domain.pieces.PieceNames.GUARD;
 import java.util.List;
 
 public final class Guard implements Piece {
 
     private final Team team;
+    private final PieceMovement movement;
 
     public Guard(final Team team) {
         this.team = team;
+        this.movement = new DefaultMovement(List.of(
+                new Route(List.of(Direction.NORTH)),
+                new Route(List.of(Direction.SOUTH)),
+                new Route(List.of(Direction.EAST)),
+                new Route(List.of(Direction.WEST)),
+                new Route(List.of(Direction.SOUTHEAST)),
+                new Route(List.of(Direction.SOUTHWEST)),
+                new Route(List.of(Direction.NORTHEAST)),
+                new Route(List.of(Direction.NORTHWEST))
+        ));
     }
+
 
     @Override
     public boolean hasEqualTeam(final Team team) {
@@ -23,17 +37,20 @@ public final class Guard implements Piece {
 
     @Override
     public boolean isAbleToArrive(final BoardPoint startBoardPoint, final BoardPoint arrivalBoardPoint) {
-        throw new JanggiArgumentException("신하는 이동할 수 없습니다.");
+        if (!arrivalBoardPoint.isInPalace()) {
+            return false;
+        }
+        return movement.calculateTotalArrivalPoints(startBoardPoint).contains(arrivalBoardPoint);
     }
 
     @Override
     public List<BoardPoint> getRoutePoints(final BoardPoint startBoardPoint, final BoardPoint arrivalBoardPoint) {
-        throw new JanggiArgumentException("신하는 이동할 수 없습니다.");
+        return movement.calculateRoutePoints(startBoardPoint, arrivalBoardPoint);
     }
 
     @Override
     public boolean isMovable(final PieceOnRoute pieceOnRoute) {
-        throw new JanggiArgumentException("장군은 이동할 수 없습니다.");
+        return !pieceOnRoute.hasArrivalPointInMyTeam(team);
     }
 
     @Override
