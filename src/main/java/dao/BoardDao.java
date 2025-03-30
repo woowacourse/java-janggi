352b6fe6ac54704619e84db56c5723236b1fba43
geonwoo <kysub99@naver.com>
2025-03-30 17:54:57 +0900

    refactor: DELETE 후 INSERT 대신 UPDATE 사용

--- src/main/java/dao/BoardDao.java
+++ src/main/java/dao/BoardDao.java
@@ -19,7 +19,7 @@ public class BoardDao {
     private static final String INSERT_PIECE = "INSERT INTO board (position_row, position_column, piece_type, piece_color) VALUES (?, ?, ?, ?)";
     private static final String SELECT_BOARD = "SELECT position_row, position_column, piece_type, piece_color FROM board";
     private static final String DELETE_PIECE = "DELETE FROM board WHERE position_row = ? AND position_column = ?";
-    private static final String SELECT_TYPE_COLOR = "SELECT piece_type, piece_color FROM board WHERE position_row = ? AND position_column = ?";
+    public static final String UPDATE_POSITION = "UPDATE board SET position_row = ?, position_column = ? WHERE position_row = ? AND position_column = ?";
 
     private final UserDao userDao;
 
@@ -70,32 +70,21 @@ public class BoardDao {
 
     public void updatePosition(Position source, Position destination) {
         try (Connection connection = userDao.getConnection()) {
-            // 이동할 기물 정보 가져오기
-            String[] pieceInfo = getPieceInfoByPosition(source, connection);
-
             connection.setAutoCommit(false);
-
             try {
-                try (PreparedStatement deleteSource = connection.prepareStatement(DELETE_PIECE)) {
-                    deleteSource.setInt(1, source.rowValue());
-                    deleteSource.setInt(2, source.columnValue());
-                    deleteSource.executeUpdate();
-                }
-
                 try (PreparedStatement deleteDestination = connection.prepareStatement(DELETE_PIECE)) {
                     deleteDestination.setInt(1, destination.rowValue());
                     deleteDestination.setInt(2, destination.columnValue());
                     deleteDestination.executeUpdate();
                 }
 
-                try (PreparedStatement insertStatement = connection.prepareStatement(INSERT_PIECE)) {
-                    insertStatement.setInt(1, destination.rowValue());
-                    insertStatement.setInt(2, destination.columnValue());
-                    insertStatement.setString(3, pieceInfo[0]);
-                    insertStatement.setString(4, pieceInfo[1]);
-                    insertStatement.executeUpdate();
+                try (PreparedStatement updateStatement = connection.prepareStatement(UPDATE_POSITION)) {
+                    updateStatement.setInt(1, destination.rowValue()); // 새로운 위치
+                    updateStatement.setInt(2, destination.columnValue());
+                    updateStatement.setInt(3, source.rowValue()); // 기존 위치
+                    updateStatement.setInt(4, source.columnValue());
+                    updateStatement.executeUpdate();
                 }
-
                 connection.commit();
             } catch (SQLException e) {
                 connection.rollback();
@@ -106,23 +95,5 @@ public class BoardDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    private String[] getPieceInfoByPosition(Position position, Connection connection) throws SQLException {
-        String[] pieceInfo = new String[2];
-        try (PreparedStatement statement = connection.prepareStatement(SELECT_TYPE_COLOR)) {
-            statement.setInt(1, position.rowValue());
-            statement.setInt(2, position.columnValue());
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    pieceInfo[0] = resultSet.getString("piece_type");
-                    pieceInfo[1] = resultSet.getString("piece_color");
-                } else {
-                    throw new SQLException("해당 위치에 기물이 없습니다: " + position);
-                }
-            }
-        }
-        return pieceInfo;
     }
 }
