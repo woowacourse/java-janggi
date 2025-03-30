7c5223b85535e3cd3833fbebebbdeec950fb0840
ppparkta <pppppark2@gmail.com>
2025-03-31 03:38:28 +0900

    feat: 장기판 순서 업데이트 및 테스트

--- src/main/java/janggi/dao/BoardDao.java
+++ src/main/java/janggi/dao/BoardDao.java
@@ -2,6 +2,7 @@ package janggi.dao;
 
 import janggi.board.Board;
 import janggi.dao.connection.MysqlConnection;
+import janggi.piece.Team;
 import java.sql.SQLException;
 
 public class BoardDao {
@@ -19,6 +20,28 @@ public class BoardDao {
             preparedStatement.executeUpdate();
         } catch (final SQLException e) {
             throw new RuntimeException("장기판 생성에 실패했습니다.");
+        }
+    }
+
+    public void updateBoardTurn(final int boardId, Team team) {
+        final var query = "UPDATE board SET turn = (?) WHERE board_id = (?)";
+        try (final var connection = DbConnection.getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, team.toString());
+            preparedStatement.setString(2, String.valueOf(boardId));
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new RuntimeException("순서 변경에 실패했습니다.");
+        }
+    }
+
+    public void deleteAllBoards() {
+        final var query = "DELETE from board";
+        try (final var connection = DbConnection.getConnection();
+             final var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("장기판 삭제에 실패했습니다.");
         }
     }
 }
--- src/test/java/janggi/dao/BoardDaoTest.java
+++ src/test/java/janggi/dao/BoardDaoTest.java
@@ -4,24 +4,39 @@ import janggi.board.Board;
 import janggi.board.Pieces;
 import janggi.dao.connection.MysqlConnection;
 import janggi.piece.Team;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BoardDaoTest {
+    private BoardDao boardDao;
+
+    @BeforeEach
+    void setUp() {
+        MysqlConnection dbConnection = new MysqlConnection();
+        boardDao = new BoardDao(dbConnection);
+        boardDao.deleteAllBoards();
+    }
+
     @Test
     @DisplayName("데이터베이스에 board를 저장한다")
     void test() {
         // given
-        MysqlConnection dbConnection = new MysqlConnection();
-        BoardDao boardDao = new BoardDao(dbConnection);
-
         Board board = new Board(new Pieces(), Team.CHO);
 
         // when
         boardDao.addBoard(board);
+    }
 
-        // then
+    @Test
+    @DisplayName("데이터베이스에 바뀐 턴을 변경한다.")
+    void test2() {
+        // given
+        Board board = new Board(new Pieces(), Team.CHO);
+        boardDao.addBoard(board);
 
+        // when
+        boardDao.updateBoardTurn(1, Team.HAN);
     }
 
 }
