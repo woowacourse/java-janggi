94702e93bf438640decd36c7a690bb8beaf2f731
kjyyjk <rlawnsdud920@gmail.com>
2025-03-30 21:30:19 +0900

    feat: 차가 궁성 내에서 상/하/좌/우/대각으로 최대 2칸 움직이는 기능 구현

--- src/main/java/janggi/piece/Chariot.java
+++ src/main/java/janggi/piece/Chariot.java
@@ -1,24 +1,34 @@
 package janggi.piece;
 
 import static janggi.moving.Movement.DOWN_STRAIGHT;
+import static janggi.moving.Movement.LEFT_DOWN;
 import static janggi.moving.Movement.LEFT_STRAIGHT;
+import static janggi.moving.Movement.LEFT_UP;
+import static janggi.moving.Movement.RIGHT_DOWN;
 import static janggi.moving.Movement.RIGHT_STRAIGHT;
+import static janggi.moving.Movement.RIGHT_UP;
 import static janggi.moving.Movement.UP_STRAIGHT;
 
-import janggi.moving.Movements;
-import janggi.moving.Path;
-import janggi.moving.PossibleMovements;
 import janggi.Team;
 import janggi.board.Board;
 import janggi.board.position.Position;
-
+import janggi.moving.Movements;
+import janggi.moving.Path;
+import janggi.moving.PossibleMovements;
 import java.util.List;
 
 public class Chariot extends Piece {
     protected static final String NAME = "차";
-    private static final PossibleMovements possibleMovements = new PossibleMovements(
+    private static final PossibleMovements POSSIBLE_MOVEMENTS = new PossibleMovements(
             List.of(new Movements(UP_STRAIGHT), new Movements(DOWN_STRAIGHT), new Movements(LEFT_STRAIGHT),
                     new Movements(RIGHT_STRAIGHT)));
+
+    private static final PossibleMovements POSSIBLE_CASTLE_MOVEMENTS = new PossibleMovements(
+            List.of(new Movements(UP_STRAIGHT), new Movements(DOWN_STRAIGHT), new Movements(LEFT_STRAIGHT),
+                    new Movements(RIGHT_STRAIGHT), new Movements(RIGHT_UP), new Movements(RIGHT_UP, RIGHT_UP),
+                    new Movements(LEFT_UP), new Movements(LEFT_UP, LEFT_UP), new Movements(RIGHT_DOWN),
+                    new Movements(RIGHT_DOWN, RIGHT_DOWN), new Movements(LEFT_DOWN),
+                    new Movements(LEFT_DOWN, LEFT_DOWN)));
 
     public Chariot(Team team) {
         super(team);
@@ -36,7 +46,10 @@ public class Chariot extends Piece {
 
     @Override
     protected PossibleMovements getPossibleMovements(Board board, Position start) {
-        return possibleMovements;
+        if (board.isInCastle(start)) {
+            return POSSIBLE_CASTLE_MOVEMENTS;
+        }
+        return POSSIBLE_MOVEMENTS;
     }
 
     @Override
---
+++ src/test/java/janggi/piece/ChariotTest.java
@@ -0,0 +1,39 @@
+package janggi.piece;
+
+import static janggi.fixture.PositionFixture.createPosition;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import janggi.Team;
+import janggi.board.Board;
+import janggi.board.position.Position;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ChariotTest {
+    @DisplayName("차는_궁성_내부에서_대각으로_움직일_수_있다")
+    @CsvSource(value = {"5:0:4:1", "5:0:3:2", "3:0:4:1", "3:0:5:2", "5:2:4:1", "5:2:3:0", "3:2:4:1", "3:2:5:0"},
+            delimiterString = ":")
+    @ParameterizedTest
+    void validateMovable(
+            int startColumn,
+            int startRow,
+            int goalColumn,
+            int goalRow
+    ) {
+        // given
+        Map<Position, Piece> initialBoard = new HashMap<>();
+        Position start = createPosition(startColumn, startRow);
+        Position goal = createPosition(goalColumn, goalRow);
+        Chariot piece = new Chariot(Team.GREEN);
+
+        initialBoard.put(start, piece);
+        Board board = new Board(initialBoard);
+
+        // then
+        assertThatCode(() -> piece.validateMovable(board, start, goal))
+                .doesNotThrowAnyException();
+    }
+}
