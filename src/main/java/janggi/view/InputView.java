85cfe7f470074c76d9a32924cb749cd3fbc8ce10
Dompoo <dlckdrms0517@gmail.com>
2025-03-30 17:47:08 +0900

    refactor: StringBuilder 대신 StringJoiner 사용

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -9,6 +9,7 @@ import janggi.dto.MoveDto;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringJoiner;
 
 public class InputView {
 
@@ -29,10 +30,10 @@ public class InputView {
     }
 
     public GameStartType inputStartType() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("장기 게임을 시작합니다.").append("\n");
-        sb.append("새로운 게임 : 1 / 이어하기 : 2");
-        System.out.println(sb);
+        StringJoiner sj = new StringJoiner("\n");
+        sj.add("장기 게임을 시작합니다.");
+        sj.add("새로운 게임 : 1 / 이어하기 : 2");
+        System.out.println(sj);
 
         final int input = Integer.parseInt(readLine());
         if (input == 1) return GameStartType.NEW_GAME;
@@ -46,10 +47,10 @@ public class InputView {
     }
 
     public StartingPosition inputStartPositionOf(final Country country) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(viewUtil.convertCountry(country)).append("의 시작 형태를 골라주세요.").append("\n");
-        sb.append("마상마상 : 1 / 상마상마 : 2 / 마상상마 : 3 / 상마마상 : 4");
-        System.out.println(sb);
+        StringJoiner sj = new StringJoiner("\n");
+        sj.add(viewUtil.convertCountry(country) + "의 시작 형태를 골라주세요.");
+        sj.add("마상마상 : 1 / 상마상마 : 2 / 마상상마 : 3 / 상마마상 : 4");
+        System.out.println(sj);
 
         final int input = Integer.parseInt(readLine());
         if (input == 1) return StartingPosition.MA_SANG_MA_SANG;
@@ -60,10 +61,10 @@ public class InputView {
     }
 
     public CommandType inputCommand(final Country currentCountry) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(viewUtil.convertCountry(currentCountry)).append("의 차례입니다.").append("\n");
-        sb.append("움직이기 : 1 / 저장하고 그만두기 : 2");
-        System.out.println(sb);
+        StringJoiner sj = new StringJoiner("\n");
+        sj.add(viewUtil.convertCountry(currentCountry) + "의 차례입니다.");
+        sj.add("움직이기 : 1 / 저장하고 그만두기 : 2");
+        System.out.println(sj);
 
         final int input = Integer.parseInt(readLine());
         if (input == 1) return CommandType.MOVE;
@@ -77,10 +78,10 @@ public class InputView {
     }
 
     public MoveDto inputMove() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("움직일 기물의 위치와 움직일 위치를 입력해주세요.").append("\n");
-        sb.append("입력 방식 : [파일],[랭크]-[파일],[랭크] (예시: 1,1-3,2)");
-        System.out.println(sb);
+        StringJoiner sj = new StringJoiner("\n");
+        sj.add("움직일 기물의 위치와 움직일 위치를 입력해주세요.");
+        sj.add("입력 방식 : [파일],[랭크]-[파일],[랭크] (예시: 1,1-3,2)");
+        System.out.println(sj);
 
         final String[] inputs = readLine().split("-");
         return new MoveDto(viewUtil.parseToPosition(inputs[0]), viewUtil.parseToPosition(inputs[1]));
