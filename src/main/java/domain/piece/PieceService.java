2650703eda3b81c829bbedb802b720569fcf5b42
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-31 17:38:45 +0900

    feat: 장기말 삭제, 장기말 업데이트 ,장기말 찾기

--- src/main/java/domain/piece/PieceService.java
+++ src/main/java/domain/piece/PieceService.java
@@ -2,8 +2,11 @@ package domain.piece;
 
 import database.DbConnection;
 import domain.Position;
+import domain.player.Player;
+import domain.player.Players;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PieceService {
@@ -15,16 +18,40 @@ public class PieceService {
         this.pieceDao = new PieceDao(connection);
     }
 
-    public void saveAllPieces(Map<Position, Piece> pieces) {
+    public void saveAllPieces(Map<Position, Piece> pieces, int gameId) {
         for (Map.Entry<Position, Piece> entry : pieces.entrySet()) {
-            // Map에서 가져온 Position과 Piece를 사용하여 savePiece 호출
-            savePiece(entry.getKey(), entry.getValue());
+            savePiece(entry.getKey(), entry.getValue(), gameId);
         }
     }
 
-    public void savePiece(Position position, Piece piece) {
-        // Piece의 정보를 DB에 저장
+    public void savePiece(Position position, Piece piece, int gameId) {
         pieceDao.insertPiece(piece.getPlayer().getId(), piece.getClass().getSimpleName(), position.getColumn(),
-                position.getRow());
+                position.getRow(), piece.getPoint(), gameId);
     }
+
+
+    public void removeTargetPiece(Piece targetPiece, Position targetPosition, int gameId) {
+        pieceDao.deleteTargetPiece(targetPiece, targetPosition, gameId);
+    }
+
+    // 피스 업데이트 (이동한 피스를 데이터베이스에 업데이트)
+    public void updateMovingPiece(Piece startPiece, Position startPosition, Position targetPosition, int gameId) {
+        pieceDao.updatePiecePosition(startPiece, startPosition, targetPosition, gameId);
+    }
+    
+
+    public Map<Position, Piece> getAllPiecesByGameId(int gameId, Players players) {
+        Player player1 = players.getBluePlayer();
+        Player player2 = players.getRedPlayer();
+
+        Map<Position, Piece> piecesMap1 = pieceDao.selectAllPieceById(gameId, player1); // Blue player's pieces
+        Map<Position, Piece> piecesMap2 = pieceDao.selectAllPieceById(gameId, player2); // Red player's pieces
+
+        Map<Position, Piece> allPiecesMap = new HashMap<>();
+        allPiecesMap.putAll(piecesMap1);
+        allPiecesMap.putAll(piecesMap2);
+
+        return allPiecesMap;
+    }
+
 }
