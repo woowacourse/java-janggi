d545f392ebfc9c1c9ff6edfd5e966b7d63893bb0
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-31 15:18:04 +0900

    chore: db 설정 정보

---
+++ db/janggi.sql
@@ -0,0 +1,14 @@
+create table board_piece
+(
+    `board_piece_id` int primary key auto_increment,
+    `piece_type`     ENUM('KING', 'GUARD', 'HORSE', 'ELEPHANT', 'CANNON', 'CHARIOT', 'SOLDIER') not null,
+    `team`           ENUM('CHO', 'HAN') not null,
+    `column_position`         int         not null,
+    `row_position`            int         not null,
+    `live_status`    boolean     not null
+);
+
+create table turn
+(
+    `turn` ENUM('CHO', 'HAN') primary key
+);
