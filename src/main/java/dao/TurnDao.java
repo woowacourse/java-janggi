b72d366c574655b05db196df83b30cc435f7966b
GiSeok Kang <ime.rokz@gmail.com>
2025-03-31 14:42:24 +0900

    feat: 현재 차례에 대한 정보를 DB에 저장하는 DAO 구현

---
+++ src/main/java/dao/TurnDao.java
@@ -0,0 +1,58 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import team.Team;
+
+public class TurnDao {
+
+    public void addTurn(Team turn) {
+        String sql = "INSERT INTO turn (current_turn) VALUES (?)";
+
+        try (Connection connection = JdbcConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, turn.name());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("[ERROR] 턴 정보를 저장하지 못했습니다.");
+            e.printStackTrace();
+        }
+    }
+
+    public void updateTurn(Connection connection, Team turn) {
+        String sql = "UPDATE turn SET current_turn=?";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, turn.name());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("[ERROR] 턴 정보를 저장하지 못했습니다.");
+            e.printStackTrace();
+        }
+    }
+
+    public String getTurn() {
+        String sql = "SELECT current_turn FROM turn";
+
+        try (Connection connection = JdbcConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            ResultSet turn = preparedStatement.executeQuery();
+            while (turn.next()) {
+                return turn.getString("current_turn");
+            }
+        } catch (SQLException e) {
+            System.err.println("[ERROR] 턴 정보를 저장하지 못했습니다.");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
