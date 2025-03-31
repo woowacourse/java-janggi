9adb507d65f0bb56f97ef7c1873ff7c7cd203415
민경윤 <ruddbs3308@naver.com>
2025-03-31 19:14:26 +0900

    style: 가독성 향상을 위한 줄바꿈 추가

--- src/main/java/dao/ConnectionProvider.java
+++ src/main/java/dao/ConnectionProvider.java
@@ -20,8 +20,12 @@ public class ConnectionProvider {
     public static Connection getConnection() {
         if (connection == null) {
             try {
-                connection = DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME,
-                        PASSWORD);
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://"
+                                + SERVER
+                                + "/"
+                                + DATABASE
+                                + OPTION, USERNAME, PASSWORD);
             } catch (final SQLException e) {
                 throw new RuntimeException("DB 커넥션 실패");
             }
