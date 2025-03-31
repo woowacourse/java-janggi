8f7daa3acca851f012fe6e9a643ae34855da1f0c
saera <yook.saera@gmail.com>
2025-03-31 18:21:06 +0900

    fix: 잘못 merge된 파일 수정

--- src/main/java/domain/piece/Soldier.java
+++ src/main/java/domain/piece/Soldier.java
@@ -22,27 +22,41 @@ public class Soldier extends AbstractPiece {
     @Override
     public boolean isMovable(final Point fromPoint, final Point toPoint) {
         final Direction direction = fromPoint.generateDirection(toPoint);
-        return isGreenForward(direction) || isRedForward(direction) || isHorizontalMovement(direction);
+        if (isGreenTeam()) {
+            return isGreenMovement(direction, fromPoint, toPoint);
+        }
+        return isRedMovement(direction, fromPoint, toPoint);
     }
 
-    private boolean isRedForward(final Direction direction) {
-        return Team.RED == team && isDownMovement(direction);
+    private boolean isGreenMovement(final Direction direction, final Point fromPoint, final Point toPoint) {
+        if (isUpMovement(direction) || isHorizontalMovement(direction)) {
+            return true;
+        }
+        return fromPoint.isRedDiagonalPalace() && toPoint.isRedDiagonalPalace() && isDiagonalMovement(direction, 1);
     }
 
-    private boolean isGreenForward(final Direction direction) {
-        return Team.GREEN == team && isUpMovement(direction);
+    private boolean isRedMovement(final Direction direction, final Point fromPoint, final Point toPoint) {
+        if (isDownMovement(direction) || isHorizontalMovement(direction)) {
+            return true;
+        }
+        return fromPoint.isGreenDiagonalPalace() && toPoint.isGreenDiagonalPalace() && isDiagonalMovement(direction,
+                -1);
     }
 
-    private static boolean isDownMovement(final Direction direction) {
-        return direction.isNotHorizontal() && direction.y() == -1;
+    private static boolean isHorizontalMovement(final Direction direction) {
+        return direction.horizontalDistance() == 1 && direction.verticalDistance() == 0;
+    }
+
+    private boolean isDiagonalMovement(final Direction direction, final int toDirection) {
+        return direction.horizontalDistance() == 1 && direction.y() == toDirection;
     }
 
     private static boolean isUpMovement(final Direction direction) {
         return direction.isNotHorizontal() && direction.y() == 1;
     }
 
-    private static boolean isHorizontalMovement(final Direction direction) {
-        return direction.horizontalDistance() == 1 && direction.verticalDistance() == 0;
+    private static boolean isDownMovement(final Direction direction) {
+        return direction.isNotHorizontal() && direction.y() == -1;
     }
 
     @Override
