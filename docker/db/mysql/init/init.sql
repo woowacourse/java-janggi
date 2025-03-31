908640fcfcf6c5b62c97c1e1beb00acbe0d90c15
ykmxxi <ykmxxi97@gmail.com>
2025-03-31 10:50:27 +0900

    feat: 장기판 저장을 위한 데이터베이스 테이블 init sql 추가

---
+++ docker/db/mysql/init/init.sql
@@ -0,0 +1,20 @@
+create user 'user'@'localhost' identified by 'password';
+
+grant all privileges on *.* to 'user'@'localhost';
+
+flush privileges;
+
+CREATE DATABASE janggi DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE janggi;
+
+CREATE TABLE IF NOT EXISTS piece
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    row_value    INT         NOT NULL,
+    column_value INT         NOT NULL,
+    piece_type   VARCHAR(20) NOT NULL,
+    team         VARCHAR(20) NOT NULL,
+
+    UNIQUE (row_value, column_value)
+);
