bdff7336971ba4279658e170c9df78961c95fc8f
AppleMint98 <applemint9821@gmail.com>
2025-03-31 15:22:35 +0900

    refactor(piece): 다중 분기문 메서드 분리

--- src/main/java/janggi/piece/Cannon.java
+++ src/main/java/janggi/piece/Cannon.java
@@ -17,29 +17,43 @@ public class Cannon extends Piece {
         List<Route> candidateRoutes = computeStraightRoutes(selectedPosition, MOVE_LIMIT);
         List<Position> reachablePositions = new ArrayList<>();
         for (Route route : candidateRoutes) {
-            List<Position> positions = route.getPositions();
-            boolean hasJumped = false;
-            for (Position position : positions) {
-                if (board.isOutOfRange(position) || board.isPositionCannon(position)) {
-                    break;
-                }
-                if (!hasJumped && board.isPositionHasPiece(position)) {
-                    hasJumped = true;
-                    continue;
-                }
-
-                if (hasJumped) {
-                    if (board.isPositionHasPiece(position)) {
-                        if (isEnemyWith(board.findPieceBy(position))) {
-                            reachablePositions.add(position);
-                        }
-                        break;
-                    }
-                    reachablePositions.add(position);
-                }
-            }
+            computeReachablePositions(board, route, reachablePositions);
         }
         return reachablePositions;
+    }
+
+    private void computeReachablePositions(final JanggiBoard board, final Route route,
+                                           final List<Position> reachablePositions) {
+        boolean hasJumped = false;
+        for (Position position : route.getPositions()) {
+            if (wouldStop(board, position)) {
+                break;
+            }
+            if (reachMovableCondition(board, position, hasJumped)) {
+                hasJumped = true;
+                continue;
+            }
+
+            if (hasJumped) {
+                if (couldCatchEnemyPiece(board, position)) {
+                    reachablePositions.add(position);
+                    break;
+                }
+                reachablePositions.add(position);
+            }
+        }
+    }
+
+    private boolean wouldStop(final JanggiBoard board, final Position position) {
+        return board.isOutOfRange(position) || board.isPositionCannon(position);
+    }
+
+    private boolean reachMovableCondition(final JanggiBoard board, final Position position, final boolean hasJumped) {
+        return !hasJumped && board.isPositionHasPiece(position);
+    }
+
+    private boolean couldCatchEnemyPiece(final JanggiBoard board, final Position position) {
+        return board.isPositionHasPiece(position) && isEnemyWith(board.findPieceBy(position));
     }
 
 }
--- src/main/java/janggi/piece/Chariot.java
+++ src/main/java/janggi/piece/Chariot.java
@@ -17,19 +17,31 @@ public class Chariot extends Piece {
         List<Route> candidateRoutes = computeStraightRoutes(selectedPosition, MOVE_LIMIT);
         List<Position> reachablePositions = new ArrayList<>();
         for (Route route : candidateRoutes) {
-            List<Position> positions = route.getPositions();
-            for (Position position : positions) {
-                if (board.isOutOfRange(position) || isAllyWith(board.findPieceBy(position))) {
-                    break;
-                }
-                if (board.isPositionHasPiece(position) && isEnemyWith(board.findPieceBy(position))) {
-                    reachablePositions.add(position);
-                    break;
-                }
-                reachablePositions.add(position);
-            }
+            computeReachablePositions(board, route, reachablePositions);
         }
         return reachablePositions;
+    }
+
+    private void computeReachablePositions(final JanggiBoard board, final Route route,
+                                           final List<Position> reachablePositions) {
+        for (Position position : route.getPositions()) {
+            if (wouldStop(board, position)) {
+                break;
+            }
+            if (couldCatchEnemyPiece(board, position)) {
+                reachablePositions.add(position);
+                break;
+            }
+            reachablePositions.add(position);
+        }
+    }
+
+    private boolean wouldStop(final JanggiBoard board, final Position position) {
+        return board.isOutOfRange(position) || isAllyWith(board.findPieceBy(position));
+    }
+
+    private boolean couldCatchEnemyPiece(final JanggiBoard board, final Position position) {
+        return board.isPositionHasPiece(position) && isEnemyWith(board.findPieceBy(position));
     }
 
 }
