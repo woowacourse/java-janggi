0b16a6e13a738b9e529903886c5f6f222d7ffc62
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-03-31 15:07:37 +0900

    feat: main 의존성 주입 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,27 @@
 import janggi.Janggi;
+import janggi.service.JanggiService;
+import janggi.repository.mysql.MysqlConnectionProvider;
+import janggi.repository.mysql.GameMysqlRepository;
+import janggi.repository.mysql.PieceMysqlRepository;
 import janggi.view.InputView;
 import janggi.view.OutputView;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Application {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws SQLException {
+        final Connection connection = new MysqlConnectionProvider().getConnection();
+
         new Janggi(
                 new InputView(),
-                new OutputView()
-        ).play();
+                new OutputView(),
+                new JanggiService(
+                        new GameMysqlRepository(connection),
+                        new PieceMysqlRepository(connection)
+                )
+        ).run();
 
     }
 }
