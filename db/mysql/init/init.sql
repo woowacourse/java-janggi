f957a525c4009971eaa362f022230a9a29772ca8
CheChe903 <cheche903@naver.com>
2025-03-31 20:27:00 +0900

    docs(docker): 도커 및 init 파일 수정

---
+++ db/mysql/init/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Piece (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       piece_type VARCHAR(50) NOT NULL,
+                       team VARCHAR(10) NOT NULL,
+                       x INT NOT NULL,
+                       y INT NOT NULL
+);
+
+CREATE TABLE Turn (
+                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      team VARCHAR(10)
+);
