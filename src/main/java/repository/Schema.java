1eea2d8194e4c71a91744b36d9a17b4c85cb294a
sonjh919 <kingjh1125@gmail.com>
2025-03-31 14:44:52 +0900

    refactor: 스키마에 enum 추가

--- src/main/java/repository/Schema.java
+++ src/main/java/repository/Schema.java
@@ -1,28 +1,40 @@
 package repository;
 
+import static janggi.piece.PieceType.CANNON;
+import static janggi.piece.PieceType.CHARIOT;
+import static janggi.piece.PieceType.CHOPAWN;
+import static janggi.piece.PieceType.ELEPHANT;
+import static janggi.piece.PieceType.HANPAWN;
+import static janggi.piece.PieceType.HORSE;
+import static janggi.piece.PieceType.KING;
+import static janggi.piece.PieceType.SOLDIER;
+import static janggi.piece.Team.BLANK;
+import static janggi.piece.Team.CHO;
+import static janggi.piece.Team.HAN;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import repository.connection.ConnectDatabase;
 import repository.connection.ConnectMysql;
 
 public class Schema {
-    public static final String CREATE_TEAM = """
+    public static final String CREATE_TEAM = String.format("""
             CREATE TABLE IF NOT EXISTS TEAM (
                 name VARCHAR(20) UNIQUE NOT NULL PRIMARY KEY
             );
             
-            INSERT IGNORE INTO TEAM (name) VALUES ('CHO'), ('HAN');
+            INSERT IGNORE INTO TEAM (name) VALUES ('%s'), ('%s');
             
-            """;
+            """, CHO, HAN);
 
-    public static final String CREATE_PIECE_TYPE = """
+    public static final String CREATE_PIECE_TYPE = String.format("""
             CREATE TABLE IF NOT EXISTS PIECE_TYPE (
                 name VARCHAR(20) UNIQUE NOT NULL PRIMARY KEY
             );
             
-            INSERT IGNORE INTO PIECE_TYPE (name) VALUES ('KING'), ('CHARIOT'), ('CANNON'), ('HORSE'), ('ELEPHANT'), ('SOLDIER'), ('HANPAWN'), ('CHOPAWN'), ('BLANK');
+            INSERT IGNORE INTO PIECE_TYPE (name) VALUES ('%s'), ('%s'), ('%s'), ('%s'), ('%s'), ('%s'), ('%s'), ('%s'), ('%s');
             
-            """;
+            """, KING, CHARIOT, CANNON, HORSE, ELEPHANT, SOLDIER, HANPAWN, CHOPAWN, BLANK);
 
     public static final String CREATE_PIECE = """
             CREATE TABLE IF NOT EXISTS PIECE (
