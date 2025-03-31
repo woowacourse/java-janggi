814d6dd8920b3485b9cd5815fd226fc07f669877
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-03-31 17:24:16 +0900

    refactor: 메인 메서드 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
+import config.MySqlConnector;
 import controller.JanggiController;
+import dao.PieceDao;
 import view.InputView;
 import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        final JanggiController janggiController = new JanggiController(new InputView(), new OutputView());
+        final MySqlConnector mySqlConnector = new MySqlConnector();
+        final JanggiController janggiController = new JanggiController(new InputView(), new OutputView(), new PieceDao(mySqlConnector));
         janggiController.run();
     }
 }
