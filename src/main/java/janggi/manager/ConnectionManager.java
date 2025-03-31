5cad7a941d302c044f7faf6c57a41a212fd14f54
egaeng09 <egaeng09@gmail.com>
2025-03-31 13:56:48 +0900

    refactor: 미사용 주석 제거

--- src/main/java/janggi/manager/ConnectionManager.java
+++ src/main/java/janggi/manager/ConnectionManager.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    private static final String BASE_URL = "jdbc:mysql://localhost:13306/"; // MySQL 서버 주소
-    private static final String DATABASE = "janggi"; // MySQL DATABASE 이름
+    private static final String BASE_URL = "jdbc:mysql://localhost:13306/";
+    private static final String DATABASE = "janggi";
     private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
     public Connection getConnection() {
         try {
