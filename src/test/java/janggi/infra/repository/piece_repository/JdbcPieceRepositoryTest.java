5cd8449e4010c1e9e90e272ec6aeaa9b81bf99bd
Dompoo <dlckdrms0517@gmail.com>
2025-03-31 13:00:09 +0900

    test: 테스트에서 람다식으로 테이블 생성/제거 하던 것을 JUnit 기능으로 리팩터링

--- src/test/java/janggi/infra/repository/piece_repository/JdbcPieceRepositoryTest.java
+++ src/test/java/janggi/infra/repository/piece_repository/JdbcPieceRepositoryTest.java
@@ -4,62 +4,64 @@ import janggi.domain.Country;
 import janggi.domain.piece.Piece;
 import janggi.domain.piece.PieceType;
 import janggi.test_util.InMemoryConnector;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 
 import static janggi.test_util.TestConstant.*;
 import static org.assertj.core.api.Assertions.*;
 
 class JdbcPieceRepositoryTest {
 
-    private void runWithPieceRepository(Consumer<PieceRepository> testCode) {
-        final PieceRepository repository = new JdbcPieceRepository(new InMemoryConnector());
+    private final PieceRepository repository = new JdbcPieceRepository(new InMemoryConnector());
+
+    @BeforeEach
+    void setUp() {
         repository.createTable();
-        testCode.accept(repository);
+    }
+
+    @AfterEach
+    void tearDown() {
         repository.deleteTable();
     }
 
     @Test
     void 기물을_저장한다() {
-        runWithPieceRepository((repository) -> {
-            // given
-            final List<Piece> pieces = List.of(
-                    new Piece(PieceType.PO, POSITION_5_5),
-                    new Piece(PieceType.GUNG, POSITION_3_3),
-                    new Piece(PieceType.JOL, POSITION_2_2)
-            );
+        // given
+        final List<Piece> pieces = List.of(
+                new Piece(PieceType.PO, POSITION_5_5),
+                new Piece(PieceType.GUNG, POSITION_3_3),
+                new Piece(PieceType.JOL, POSITION_2_2)
+        );
 
-            // expected
-            assertThatCode(() -> repository.saveAllPieces(5, Country.CHO, pieces))
-                    .doesNotThrowAnyException();
-        });
+        // expected
+        assertThatCode(() -> repository.saveAllPieces(5, Country.CHO, pieces))
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 모든_기물을_찾는다() {
-        runWithPieceRepository((repository) -> {
-            // given
-            repository.saveAllPieces(5, Country.CHO, List.of(
-                    new Piece(PieceType.PO, POSITION_5_5),
-                    new Piece(PieceType.GUNG, POSITION_3_3),
-                    new Piece(PieceType.JOL, POSITION_2_2)
-            ));
-            repository.saveAllPieces(5, Country.HAN, List.of(
-                    new Piece(PieceType.SANG, POSITION_7_7),
-                    new Piece(PieceType.MA, POSITION_8_8)
-            ));
+        // given
+        repository.saveAllPieces(5, Country.CHO, List.of(
+                new Piece(PieceType.PO, POSITION_5_5),
+                new Piece(PieceType.GUNG, POSITION_3_3),
+                new Piece(PieceType.JOL, POSITION_2_2)
+        ));
+        repository.saveAllPieces(5, Country.HAN, List.of(
+                new Piece(PieceType.SANG, POSITION_7_7),
+                new Piece(PieceType.MA, POSITION_8_8)
+        ));
 
-            // when
-            final Map<Country, List<Piece>> result = repository.findAllPieces(5);
+        // when
+        final Map<Country, List<Piece>> result = repository.findAllPieces(5);
 
-            // then
-            assertThat(result.get(Country.CHO)).extracting("position")
-                    .containsExactlyInAnyOrder(POSITION_5_5, POSITION_3_3, POSITION_2_2);
-            assertThat(result.get(Country.HAN)).extracting("position")
-                    .containsExactlyInAnyOrder(POSITION_7_7, POSITION_8_8);
-        });
+        // then
+        assertThat(result.get(Country.CHO)).extracting("position")
+                .containsExactlyInAnyOrder(POSITION_5_5, POSITION_3_3, POSITION_2_2);
+        assertThat(result.get(Country.HAN)).extracting("position")
+                .containsExactlyInAnyOrder(POSITION_7_7, POSITION_8_8);
     }
 }
--- src/test/java/janggi/infra/repository/turn_repository/JdbcTurnRepositoryTest.java
+++ src/test/java/janggi/infra/repository/turn_repository/JdbcTurnRepositoryTest.java
@@ -2,44 +2,45 @@ package janggi.infra.repository.turn_repository;
 
 import janggi.domain.Country;
 import janggi.test_util.InMemoryConnector;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.*;
 
 class JdbcTurnRepositoryTest {
 
-    private void runWithPieceRepository(Consumer<TurnRepository> testCode) {
-        final TurnRepository repository = new JdbcTurnRepository(new InMemoryConnector());
+    private final TurnRepository repository = new JdbcTurnRepository(new InMemoryConnector());
+
+    @BeforeEach
+    void setUp() {
         repository.createTable();
-        testCode.accept(repository);
+    }
+
+    @AfterEach
+    void tearDown() {
         repository.deleteTable();
     }
 
     @Test
     void 다음_차례를_저장한다() {
-        runWithPieceRepository((repository) -> {
-            // given
+        // given
 
-            // expected
-            assertThatCode(() -> repository.saveTurn(1, Country.CHO))
-                    .doesNotThrowAnyException();
-        });
+        // expected
+        assertThatCode(() -> repository.saveTurn(1, Country.CHO))
+                .doesNotThrowAnyException();
     }
 
     @Test
     void 저장한_차례를_불러온다() {
-        runWithPieceRepository((repository) -> {
-            // given
-            repository.saveTurn(5, Country.CHO);
+        // given
+        repository.saveTurn(5, Country.CHO);
 
-            // when
-            final Country result = repository.findNextTurn(5);
+        // when
+        final Country result = repository.findNextTurn(5);
 
-            // then
-            assertThat(result).isEqualTo(Country.CHO);
-        });
+        // then
+        assertThat(result).isEqualTo(Country.CHO);
     }
 
 }
