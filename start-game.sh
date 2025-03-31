198a41e1b94277823d804cc10ad37c0c72bd5bee
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-31 15:39:11 +0900

    fix: 패키지 명이 달라도 실행되도록 수정

--- start-game.sh
+++ start-game.sh
@@ -3,4 +3,4 @@ echo "데이터베이스 서버가 완전히 켜지기까지 3초 대기중.."
 sleep 3
 ./gradlew shadowjar &&
 docker exec -i janggi-mysql mysql -u root -proot janggi < ./init.sql
-java -jar ./build/libs/java-janggi-1.0-SNAPSHOT-all.jar
+java -jar ./build/libs/*.jar
