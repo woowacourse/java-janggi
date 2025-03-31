7238e40926398c91c65fb95d8491a3b68f6675d4
DEV_WOONG <ywcsuwon@gmail.com>
2025-03-31 17:40:33 +0900

    feat: 팀 분류메서드

--- src/main/java/domain/Team.java
+++ src/main/java/domain/Team.java
@@ -3,4 +3,12 @@ package domain;
 public enum Team {
     BLUE,
     RED;
+
+    public static Team fromString(String color) {
+        return switch (color.toUpperCase()) {
+            case "BLUE" -> BLUE;
+            case "RED" -> RED;
+            default -> throw new IllegalArgumentException("유효하지 않은 팀 색상: " + color);
+        };
+    }
 }
