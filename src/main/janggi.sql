cb66afe5ff8c37aea68c43365a532b083de1467f
ChoiDongjun <adong0808@naver.com>
2025-03-31 14:49:59 +0900

    feat(app): sql 파일 추가

---
+++ src/main/janggi.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Piece (
+   piece_id INT AUTO_INCREMENT NOT NULL,
+   type VARCHAR(20) NOT NULL,
+   side VARCHAR(10) NOT NULL,
+   PRIMARY KEY (piece_id)
+);
+
+CREATE TABLE BoardPiece (
+    board_piece_id INT AUTO_INCREMENT NOT NULL,
+    piece_id INT NOT NULL,
+    game_id INT NOT NULL,
+    x INT NOT NULL,
+    y INT NOT NULL,
+    PRIMARY KEY (board_piece_id),
+    UNIQUE(x, y),
+    FOREIGN KEY (piece_id) REFERENCES Piece(piece_id) ON DELETE CASCADE,
+    FOREIGN KEY (game_id) REFERENCES Game(game_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Game (
+    game_id INT AUTO_INCREMENT NOT NULL,
+    state VARCHAR(10) NOT NULL,
+    PRIMARY KEY (game_id)
+);
