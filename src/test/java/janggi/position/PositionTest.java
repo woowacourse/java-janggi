007ad59ac376b34e46c0f7e555feaaa435cfda38
MinJun Choi <mj043000@naver.com>
2025-04-01 17:28:35 +0900

    test: 부족한 테스트 추가

--- src/test/java/janggi/position/PositionTest.java
+++ src/test/java/janggi/position/PositionTest.java
@@ -6,19 +6,29 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import janggi.moveStrategy.rule.MoveVector;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PositionTest {
 
     @DisplayName("두 개의 position의 차이를 계산한다.")
-    @Test
-    void testGetDiff() {
+    @ParameterizedTest
+    @CsvSource({
+            "6, 6, 5, 7, -1, 1",  // start: (6,6), end: (5,7)
+            "3, 2, 1, 1, -2, -1",  // start: (3,2), end: (1,1)
+            "5, 5, 5, 5, 0, 0",    // start: (5,5), end: (5,5)
+            "7, 4, 5, 6, -2, 2"    // start: (7,4), end: (5,6)
+    })
+    void testGetDiff(int startRow, int startCol, int endRow, int endCol, int expectedRowDiff, int expectedColDiff) {
         // given
-        final Position start = new Position(Row.SIX, Column.SIX);
-        final Position end = new Position(Row.FIVE, Column.SEVEN);
+        final Position start = new Position(Row.of(startRow), Column.of(startCol));
+        final Position end = new Position(Row.of(endRow), Column.of(endCol));
+
         // when
         final MoveVector actual = end.calculateVectorDiff(start);
+
         // then
-        assertThat(actual).isEqualTo(new MoveVector(-1, 1));
+        assertThat(actual).isEqualTo(new MoveVector(expectedRowDiff, expectedColDiff));
     }
 
     @DisplayName("궁성인지 판단한다.")
--- src/test/java/janggi/position/RowTest.java
+++ src/test/java/janggi/position/RowTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class RowTest {
@@ -38,12 +39,13 @@ class RowTest {
     }
 
     @DisplayName("범위가 넘어가는 숫자에 대해서는 예외를 발생한다.")
-    @Test
-    void testWrongRow() {
+    @ParameterizedTest
+    @CsvSource({"-1", "-10", "10", "100"})
+    void testWrongRow(int row) {
         // given
         // when
         // then
-        assertThatThrownBy(() -> Row.of(10))
+        assertThatThrownBy(() -> Row.of(row))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("[ERROR] 보드를 벗어난 값입니다.");
     }
