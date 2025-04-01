d0c8a576d465bdb67278b9cb8bcbe38a360b9600
taek2222 <oot309@daum.net>
2025-04-01 17:16:35 +0900

    feat: 대각선 이동인지 판단하는 기능 추가

--- src/main/java/domain/spatial/Vector.java
+++ src/main/java/domain/spatial/Vector.java
@@ -9,6 +9,10 @@ public record Vector(
         validateRange(moveRow, moveColumn);
     }
 
+    public boolean isDiagonal() {
+        return moveRow != 0 && moveColumn != 0;
+    }
+
     private void validateRange(final int moveRow, final int moveColumn) {
         if (isInvalidMove(moveRow) || isInvalidMove(moveColumn)) {
             throw new IllegalArgumentException("이동 방향의 수치는 -1, 0, 1만 가능합니다.");
