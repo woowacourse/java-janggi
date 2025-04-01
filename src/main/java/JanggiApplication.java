49724ade6aace74f54732f954828b056830b644d
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-04-01 17:10:46 +0900

    refactor: 애플리케이션 이름 변경 및 메인 메서드 수정

---
+++ src/main/java/JanggiApplication.java
@@ -0,0 +1,17 @@
+import config.MySqlConnector;
+import controller.JanggiController;
+import dao.JanggiDaoImpl;
+import dao.PieceDaoImpl;
+import service.JanngiService;
+import view.InputView;
+import view.OutputView;
+
+public class JanggiApplication {
+    public static void main(String[] args) {
+        final MySqlConnector mySqlConnector = new MySqlConnector();
+        final JanngiService janngiService = new JanngiService(new PieceDaoImpl(mySqlConnector),
+                new JanggiDaoImpl(mySqlConnector));
+        final JanggiController janggiController = new JanggiController(new InputView(), new OutputView(), janngiService);
+        janggiController.run();
+    }
+}
