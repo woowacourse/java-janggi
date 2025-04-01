c10fffb245799c7a98fd21e5a40e6ff1ac5540ea
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-04-01 17:02:41 +0900

    refactor: 스코어 연산 메서드 추가

--- src/main/java/domain/score/Score.java
+++ src/main/java/domain/score/Score.java
@@ -1,17 +1,35 @@
 package domain.score;
 
-public record Score(int value) {
-    public Score {
-        if (value < 0) {
-            throw new IllegalArgumentException("점수는 음수가 될 수 없습니다.");
+import domain.type.ChessTeam;
+
+public record Score(double value) implements Comparable<Score> {
+
+    public static Score initScore(final ChessTeam team) {
+        if (team == ChessTeam.BLUE) {
+            return zero();
         }
+        return new Score(1.5);
     }
 
-    public static Score zero() {
+    private static Score zero() {
         return new Score(0);
+    }
+
+    public static Score hundred() {
+        return new Score(100.0);
     }
 
     public Score add(Score other) {
         return new Score(value + other.value);
+    }
+
+    public Score subtract(Score other) { return new Score(value - other.value); }
+
+    public boolean isPositiveScoreValue() {
+        return value > 0;
+    }
+    @Override
+    public int compareTo(final Score o) {
+        return Double.compare(this.value, o.value);
     }
 }
