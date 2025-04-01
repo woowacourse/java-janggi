bd5e5c4174dd7d85b8718a67d8ff835fedce55fa
taek2222 <oot309@daum.net>
2025-04-01 17:15:38 +0900

    test: 대각선 이동인지 판단하는 테스트 작성

--- src/test/java/domain/spatial/VectorTest.java
+++ src/test/java/domain/spatial/VectorTest.java
@@ -3,6 +3,7 @@ package domain.spatial;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class VectorTest {
@@ -18,6 +19,29 @@ class VectorTest {
         assertThatCode(() -> new Vector(moveRow, moveColumn))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이동 방향의 수치는 -1, 0, 1만 가능합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, false",
+            "1, 0, false",
+            "-1, 0, false",
+            "0, 1, false",
+            "0, -1, false",
+            "1, 1, true",
+            "1, -1, true",
+            "-1, 1, true",
+            "-1, -1, true"
+    })
+    void 이동_방향이_대각선인지_판단한다(int moveRow, int moveColumn, boolean expected) {
+        // given
+        Vector vector = new Vector(moveRow, moveColumn);
+
+        // when
+        boolean result = vector.isDiagonal();
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
 
