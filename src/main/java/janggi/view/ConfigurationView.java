b3507fc0a4c4022aa4ad57e6a731a0e3787a2716
Minuring <mw9505@gmail.com>
2025-04-01 19:22:23 +0900

    refactor: 출력 뷰 띄워쓰기

--- src/main/java/janggi/view/ConfigurationView.java
+++ src/main/java/janggi/view/ConfigurationView.java
@@ -7,8 +7,13 @@ public class ConfigurationView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public boolean readOnlineOrLocal(int maxSeconds) {
-        System.out.println("게임 서버에 연결하려면 Y를, 연결하지 않으려면 N을 입력해주세요.");
-        System.out.printf("처음 실행의 경우 최대 %s초 가량 소요될 수 있습니다.%n", maxSeconds);
+        System.out.println("실행 옵션을 선택해 주세요.");
+        System.out.println("- 게임 서버에 연결하기 -> \"Y\"");
+        System.out.println("- 연결하지 않고 게임하기 -> \"N\"");
+        System.out.println();
+        System.out.printf("처음 실행의 경우 최대 %s초 가량 소요될 수 있습니다.", maxSeconds);
+        System.out.println();
+
         String input = scanner.nextLine();
         if ("Y".equalsIgnoreCase(input)) {
             return true;
@@ -28,13 +33,14 @@ public class ConfigurationView {
     }
 
     public void printConnectingServer() {
-        System.out.println("로컬 서버에 연결중입니다..");
+        System.out.println("서버에 연결중입니다..");
         System.out.println(ViewTools.applyColor(
-            ViewTools.RED, "! 강제 종료 시 서버 프로세스(docker)가 유지될 수 있습니다 !%n")
+            ViewTools.RED, "! 강제 종료 시 서버 프로세스(docker)가 유지될 수 있습니다 !")
         );
+        System.out.println();
     }
 
     public void printStoppingServer() {
-        System.out.println("로컬 서버를 중지하는 중입니다.. (약 2초 소요)");
+        System.out.println("서버를 중지하는 중입니다.. (약 2초 소요됨)");
     }
 }
