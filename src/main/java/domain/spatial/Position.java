533a65b9a6a147c8a4072540c730a99af6826931
taek2222 <oot309@daum.net>
2025-04-01 17:09:15 +0900

    feat: 이동 방향 계산하는 로직 추가

--- src/main/java/domain/spatial/Position.java
+++ src/main/java/domain/spatial/Position.java
@@ -32,19 +32,26 @@ public record Position(
         return isWithinRange(newRow, newColumn);
     }
 
-    private void validateRange(final int row, final int column) {
-        if (!isWithinRange(row, column)) {
-            throw new IllegalArgumentException("좌표 입력은 9X10 보드 이내만 가능합니다.");
-        }
+    public boolean isWithinPalace() {
+        boolean isWithinPalaceRow = this.row >= PALACE_MIN_ROW && this.row <= PALACE_MAX_ROW;
+        boolean isWithinPalaceColumn = this.column <= PALACE_MAX_COLUMN_LEFT || this.column >= PALACE_MIN_COLUMN_RIGHT;
+        return isWithinPalaceRow && isWithinPalaceColumn;
+    }
+
+    public Vector calculateVector(final Position target) {
+        int moveRow = target.row - this.row;
+        int moveColumn = target.column - this.column;
+
+        return new Vector(moveRow, moveColumn);
     }
 
     private boolean isWithinRange(final int newRow, final int newColumn) {
         return newRow >= MIN_ROW && newRow <= MAX_ROW && newColumn >= MIN_COLUMN && newColumn <= MAX_COLUMN;
     }
 
-    public boolean isWithinPalace() {
-        boolean isWithinPalaceRow = this.row >= PALACE_MIN_ROW && this.row <= PALACE_MAX_ROW;
-        boolean isWithinPalaceColumn = this.column <= PALACE_MAX_COLUMN_LEFT || this.column >= PALACE_MIN_COLUMN_RIGHT;
-        return isWithinPalaceRow && isWithinPalaceColumn;
+    private void validateRange(final int row, final int column) {
+        if (!isWithinRange(row, column)) {
+            throw new IllegalArgumentException("좌표 입력은 9X10 보드 이내만 가능합니다.");
+        }
     }
 }
