d8d557ba063ff0487612c0d60b341e38ebaf6d12
taek2222 <oot309@daum.net>
2025-04-01 17:46:44 +0900

    refactor: 이동 정보가 2개 이상이 아닌 경우 예외 추가 및 로직 변경
    - 시작과 도착 좌표가 포함이 필수적으로 이루어지기 때문에 2개 이상이 필수이므로 검증을 추가했습니다.
    
    - 다른 메서드의 역할에서 시작 좌표가 포함되어 일부분 추출하는 과정을 추가했습니다.

--- src/main/java/domain/MoveInfos.java
+++ src/main/java/domain/MoveInfos.java
@@ -8,15 +8,15 @@ public class MoveInfos {
     private final List<MoveInfo> moveInfos;
 
     public MoveInfos(final List<MoveInfo> moveInfos) {
+        validateMoveInfos(moveInfos);
         this.moveInfos = moveInfos;
     }
 
     public int countPiecesInIntermediatePath() {
-        if (moveInfos.size() <= 1) { // 마지막 경로는 도착 지점
-            return 0;
-        }
-        List<MoveInfo> intermediatePaths = moveInfos.subList(0, moveInfos.size() - 1);
-        return (int) intermediatePaths.stream().filter(MoveInfo::hasPieceInPath).count();
+        List<MoveInfo> intermediatePaths = moveInfos.subList(1, moveInfos.size() - 1);
+        return (int) intermediatePaths.stream()
+                .filter(MoveInfo::hasPieceInPath)
+                .count();
     }
 
     public boolean isSameAsTargetPiece(final PieceCategory start) {
@@ -25,6 +25,14 @@ public class MoveInfos {
     }
 
     public boolean hasSamePieceCategoryInPath(final PieceCategory category) {
-        return moveInfos.stream().anyMatch(moveInfo -> moveInfo.isSamePieceCategory(category));
+        List<MoveInfo> intermediatePaths = moveInfos.subList(1, moveInfos.size() - 1);
+        return intermediatePaths.stream()
+                .anyMatch(moveInfo -> moveInfo.isSamePieceCategory(category));
+    }
+
+    private void validateMoveInfos(final List<MoveInfo> moveInfos) {
+        if (moveInfos.isEmpty() || moveInfos.size() < 2) {
+            throw new IllegalArgumentException("이동할 수 없는 좌표입니다. 다시 확인해주세요.");
+        }
     }
 }
