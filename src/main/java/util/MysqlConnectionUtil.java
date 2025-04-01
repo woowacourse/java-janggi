bd7050830c7ae165dac5eeb5db7581364bfbe52d
junhaa <2171326@hansung.ac.kr>
2025-04-01 11:18:57 +0900

    refactor: 데이터베이스 연결 정보를 properties 파일로 이동
    - .properties 파일을 커밋하면 안되지만, 테스트를 위해 우선 커밋

--- src/main/java/util/MysqlConnectionUtil.java
+++ src/main/java/util/MysqlConnectionUtil.java
@@ -1,21 +1,33 @@
 package util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MysqlConnectionUtil {
-    private static final String SERVER = "localhost:13306";
-    private static final String DATABASE = "janggi";
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "user";
-    private static final String PASSWORD = "password";
+    private static final String FILE_PATH_IN_RESOURCES = "/db.properties";
 
     public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+        Properties properties = new Properties();
+
+        try (InputStream input = MysqlConnectionUtil.class.getResourceAsStream(FILE_PATH_IN_RESOURCES)) {
+            properties.load(input);
+
+            String server = properties.getProperty("mysql.server");
+            String database = properties.getProperty("mysql.database");
+            String option = properties.getProperty("mysql.option");
+            String username = properties.getProperty("mysql.username");
+            String password = properties.getProperty("mysql.password");
+
+            return DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + option, username, password);
         } catch (SQLException e) {
+            e.printStackTrace();
             throw new RuntimeException("데이터베이스 커넥션 획득에 실패했습니다.");
+        } catch (IOException e) {
+            throw new RuntimeException("설정 파일이 존재하지 않습니다.");
         }
     }
 }
--- src/test/java/util/H2ConnectionUtil.java
+++ src/test/java/util/H2ConnectionUtil.java
@@ -2,21 +2,33 @@ package util;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import org.h2.tools.RunScript;
 
 public class H2ConnectionUtil {
-    private static final String URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
-    private static final String USERNAME = "sa";
-    private static final String PASSWORD = "";
+    private static final String FILE_PATH_IN_RESOURCES = "/db-test.properties";
 
     public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Properties properties = new Properties();
+
+        try (InputStream input = H2ConnectionUtil.class.getResourceAsStream(FILE_PATH_IN_RESOURCES)) {
+            properties.load(input);
+
+            String url = properties.getProperty("test.db.url");
+            String username = properties.getProperty("test.db.username");
+            String password = properties.getProperty("test.db.password");
+
+            return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
+            e.printStackTrace();
             throw new RuntimeException("데이터베이스 커넥션 획득에 실패했습니다.");
+        } catch (IOException e) {
+            throw new RuntimeException("설정 파일이 존재하지 않습니다.");
         }
     }
 
