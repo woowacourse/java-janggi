bce3a4f1b58191596a88bca794e7455f04ef8600
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-04-01 17:06:30 +0900

    chore: 테이블 스키마 파일 수정

--- db/mysql/init/V1_init.sql
+++ db/mysql/init/V1_init.sql
@@ -1,20 +1,18 @@
 CREATE DATABASE IF NOT EXISTS chess;
 USE chess;
 
-CREATE TABLE IF NOT EXISTS janggi (
-                                    id VARCHAR(36) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS janggi
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
     turn VARCHAR(10) COMMENT '현재턴'
-    );
+);
 
-CREATE TABLE IF NOT EXISTS piece (
+CREATE TABLE IF NOT EXISTS piece
+(
 
-    id VARCHAR(36) PRIMARY KEY,
-    team VARCHAR(20) COMMENT '팀',
-    y_axis VARCHAR(10) COMMENT '행 위치',
-    x_axis VARCHAR(10) COMMENT '열 위치',
-    piece_type VARCHAR(20) COMMENT '기물타입',
-    janggi_id VARCHAR(36) COMMENT '장기id',
-    FOREIGN KEY (janggi_id) REFERENCES janggi(id)
-    );
-
-CREATE INDEX IF NOT EXISTS idx_piece_janggi_id ON piece(janngi_id);
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    team       VARCHAR(20) COMMENT '팀',
+    y_axis     VARCHAR(10) COMMENT '행 위치',
+    x_axis     VARCHAR(10) COMMENT '열 위치',
+    piece_type VARCHAR(20) COMMENT '기물타입'
+);
