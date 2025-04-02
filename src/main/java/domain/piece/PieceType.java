a1b31c476039f625ae7d9e4965b624700f28275d
LJM\eueo8 <eueo8259@gmail.com>
2025-04-03 00:05:43 +0900

    refactor : createPiece 메서드 생성

--- src/main/java/domain/piece/PieceType.java
+++ src/main/java/domain/piece/PieceType.java
@@ -1,11 +1,51 @@
 package domain.piece;
 
 public enum PieceType {
-    CHARIOT,
-    CANNON,
-    HORSE,
-    ELEPHANT,
-    SCHOLAR,
-    KING,
-    PAWN;
+    CHARIOT {
+        @Override
+        public Piece createPiece(Team team) {
+            return new Chariot(team);
+        }
+    },
+    CANNON {
+        @Override
+        public Piece createPiece(Team team) {
+            return new Cannon(team);
+        }
+    },
+    HORSE {
+        @Override
+        public Piece createPiece(Team team) {
+            return new Horse(team);
+        }
+    },
+    ELEPHANT {
+        @Override
+        public Piece createPiece(Team team) {
+            return new Elephant(team);
+        }
+    },
+    SCHOLAR {
+        @Override
+        public Piece createPiece(Team team) {
+            return new Scholar(team);
+        }
+    },
+    KING {
+        @Override
+        public Piece createPiece(Team team) {
+            if (team == Team.HAN){
+                return new King(team, new Score(1.5));
+            }
+            return new King(team, new Score(0));
+        }
+    },
+    PAWN {
+        @Override
+        public Piece createPiece(Team team) {
+            return new Pawn(team);
+        }
+    };
+
+    public abstract Piece createPiece(Team team);
 }
