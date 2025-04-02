046bbb100503e20d8c248af9ee6df66417e105ff
eoehd1ek <eoehd2ek@gmail.com>
2025-04-03 02:11:13 +0900

    refector(King): 경로 계산 인터페이스를 사용하도록 수정

--- src/main/java/janggi/piece/King.java
+++ src/main/java/janggi/piece/King.java
@@ -2,16 +2,20 @@ package janggi.piece;
 
 import janggi.piece.movement.MovementRule;
 import janggi.piece.movement.PalaceMovementRule;
+import janggi.piece.path.OneBlockMovementPathCalculator;
+import janggi.piece.path.PathCalculator;
 import janggi.position.Position;
 import java.util.List;
 
 public class King extends Piece {
     private static final PieceType PIECE_TYPE = PieceType.KING;
 
+    private final PathCalculator pathCalculator;
     private final MovementRule movementRule;
 
     public King(final Color color) {
         super(color);
+        this.pathCalculator = new OneBlockMovementPathCalculator();
         this.movementRule = new PalaceMovementRule();
     }
 
@@ -23,7 +27,7 @@ public class King extends Piece {
     @Override
     public List<Position> calculatePath(final Position start, final Position end) {
         movementRule.validateMovementRule(start, end);
-        return movementRule.calculatePath(start, end);
+        return pathCalculator.calculatePath(start, end);
     }
 
     @Override
