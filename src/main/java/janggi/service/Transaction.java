273973dec11303fcc5ab5cf4e31502899af557df
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-03 00:16:45 +0900

    feat: Transaction 추가

---
+++ src/main/java/janggi/service/Transaction.java
@@ -0,0 +1,27 @@
+package janggi.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Consumer;
+
+public class Transaction {
+
+    public void execute(final Connection connection, final Consumer<Connection> command) {
+        try {
+            connection.setAutoCommit(false);
+            command.accept(connection);
+            connection.commit();
+        } catch (final Exception e) {
+            tryRollback(connection, e);
+            throw new RuntimeException("트랜잭션 실패, 롤백 진행", e);
+        }
+    }
+
+    private void tryRollback(final Connection connection, final Exception e) {
+        try {
+            connection.rollback();
+        } catch (final SQLException rollbackEx) {
+            e.addSuppressed(rollbackEx);
+        }
+    }
+}
