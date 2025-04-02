7630405277e0dbf9849c4b7b30f7c1b51dd99d4b
KJungW <sagakkb0707@naver.com>
2025-04-02 17:50:39 +0900

    refactor(db): 인덱싱 추가

--- docker/test/db/mysql/init/init.sql
+++ docker/test/db/mysql/init/init.sql
@@ -18,5 +18,6 @@ CREATE TABLE IF NOT EXISTS move_piece_commands (
       destination_x_position INT,
       destination_y_position INT,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY (game_id) REFERENCES games(game_id)
+      FOREIGN KEY (game_id) REFERENCES games(game_id),
+      INDEX index_by_game_id (game_id)
 );
