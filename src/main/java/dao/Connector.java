90c00c14730dff0a46525260d6eee6448e684804
RaZel713 <razel7130@gmail.com>
2025-04-03 01:06:35 +0900

    refactor: 예외 메시지 수정

--- src/main/java/dao/Connector.java
+++ src/main/java/dao/Connector.java
@@ -15,7 +15,7 @@ public class Connector {
         try {
             return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
         } catch (final SQLException e) {
-            throw new RuntimeException("DB 연결 오류: " + e.getMessage(), e);
+            throw new RuntimeException("[ERROR] DB 연결 오류: " + e.getMessage(), e);
         }
     }
 }
--- src/main/java/dao/TurnDao.java
+++ src/main/java/dao/TurnDao.java
@@ -13,7 +13,7 @@ public class TurnDao {
                 if (resultSet.next()) {
                     return resultSet.getInt("turn");
                 }
-                throw new SQLException("Turn 테이블에서 데이터를 찾을 수 없습니다.");
+                throw new SQLException("[ERROR] Turn 테이블에서 데이터를 찾을 수 없습니다.");
             }
         } catch (final SQLException e) {
             throw new RuntimeException(e);
@@ -27,7 +27,7 @@ public class TurnDao {
             try (final var statement = connection.createStatement()) {
                 int rowsAffected = statement.executeUpdate(query);
                 if (rowsAffected == 0) {
-                    throw new SQLException("Turn 테이블에서 값을 리셋할 수 없습니다.");
+                    throw new SQLException("[ERROR] Turn 테이블에서 값을 리셋할 수 없습니다.");
                 }
             }
         } catch (final SQLException e) {
@@ -42,7 +42,7 @@ public class TurnDao {
             try (final var statement = connection.createStatement()) {
                 int rowsAffected = statement.executeUpdate(query);
                 if (rowsAffected == 0) {
-                    throw new SQLException("Turn 테이블에서 값을 증가시킬 수 없습니다.");
+                    throw new SQLException("[ERROR] Turn 테이블에서 값을 증가시킬 수 없습니다.");
                 }
             }
         } catch (final SQLException e) {
