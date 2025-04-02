49e78a06521c41c4e61d06a7cb3d27f7d2d23851
eoehd1ek <eoehd2ek@gmail.com>
2025-04-03 03:01:49 +0900

    refector(OneBlockMovementRule): 하나의 검증만 하도록 수정

--- src/main/java/janggi/piece/movement/OneBlockMovementRule.java
+++ src/main/java/janggi/piece/movement/OneBlockMovementRule.java
@@ -1,22 +1,12 @@
 package janggi.piece.movement;
 
-import janggi.position.Direction;
 import janggi.position.Position;
 
 public class OneBlockMovementRule implements MovementRule {
 
     @Override
     public void validateMovementRule(final Position start, final Position end) {
-        validateDirection(start, end);
         validateMoveDistance(start, end);
-    }
-
-    private void validateDirection(final Position start, final Position end) {
-        final Direction direction = start.calculateDirection(end);
-        if (start.hasDirection(direction)) {
-            return;
-        }
-        throw new IllegalArgumentException("말의 이동 규칙과 어긋납니다.");
     }
 
     private void validateMoveDistance(final Position start, final Position end) {
