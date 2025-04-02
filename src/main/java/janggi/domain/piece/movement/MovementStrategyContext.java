a3b8c51376833a99640acc12fc56d6ec470f2e41
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-02 15:48:23 +0900

    refactor: 전략 컨텍스트를 레코드가 아닌 불변 클래스로 변경
    - 레코드로 만들 필요가 없다고 판단.
    - getter, toString, equals 등이 필요없기 때문.

--- src/main/java/janggi/domain/piece/movement/MovementStrategyContext.java
+++ src/main/java/janggi/domain/piece/movement/MovementStrategyContext.java
@@ -4,10 +4,18 @@ import janggi.domain.Palace;
 import janggi.domain.piece.Position;
 import janggi.domain.piece.movement.palace.PalaceMovementStrategy;
 
-public record MovementStrategyContext(
-    MovementStrategy defaultMovementStrategy,
-    PalaceMovementStrategy palaceMovementStrategy
-) {
+public class MovementStrategyContext {
+
+    private final MovementStrategy defaultMovementStrategy;
+    private final PalaceMovementStrategy palaceMovementStrategy;
+
+    public MovementStrategyContext(
+        MovementStrategy defaultMovementStrategy,
+        PalaceMovementStrategy palaceMovementStrategy
+    ) {
+        this.defaultMovementStrategy = defaultMovementStrategy;
+        this.palaceMovementStrategy = palaceMovementStrategy;
+    }
 
     public MovementStrategy getMovementStrategy(Position position) {
         return Palace.isInPalace(position) ? palaceMovementStrategy : defaultMovementStrategy;
