bbb80658b09935bcc73fecda4fa15ebd2c1e1e4a
eoehd1ek <eoehd2ek@gmail.com>
2025-04-03 02:11:55 +0900

    refector(Tank): 경로 계산 인터페이스를 사용하도록 수정

--- src/main/java/janggi/piece/Tank.java
+++ src/main/java/janggi/piece/Tank.java
@@ -2,16 +2,21 @@ package janggi.piece;
 
 import janggi.piece.movement.MovementRule;
 import janggi.piece.movement.StraightMovementRule;
+import janggi.piece.path.OneBlockMovementPathCalculator;
+import janggi.piece.path.PathCalculator;
+import janggi.piece.path.StraightMovementPathCalculator;
 import janggi.position.Position;
 import java.util.List;
 
 public class Tank extends Piece {
     private static final PieceType PIECE_TYPE = PieceType.TANK;
 
+    private final PathCalculator pathCalculator;
     private final MovementRule movementRule;
 
     public Tank(final Color color) {
         super(color);
+        this.pathCalculator = new StraightMovementPathCalculator();
         this.movementRule = new StraightMovementRule();
     }
 
@@ -23,7 +28,7 @@ public class Tank extends Piece {
     @Override
     public List<Position> calculatePath(final Position start, final Position end) {
         movementRule.validateMovementRule(start, end);
-        return movementRule.calculatePath(start, end);
+        return pathCalculator.calculatePath(start, end);
     }
 
     @Override
