e8ca231c6e5377c49f606808b29804d76cbc76ee
rladmstn <oz33177351@gmail.com>
2025-04-02 19:11:38 +0900

    chore: DB 초기화 SQL 추가 및 불필요한 volume 제거

---
+++ docker/db/mysql/init/001-create-tables.sql
@@ -0,0 +1,17 @@
+USE janggi;
+
+CREATE TABLE piece (
+    `row` INT NOT NULL,
+    `column` INT NOT NULL,
+    team VARCHAR(50) NOT NULL,
+    piece_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE player (
+    name VARCHAR(50) NOT NULL,
+    team VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE board (
+    current_turn VARCHAR(50) NOT NULL
+);
