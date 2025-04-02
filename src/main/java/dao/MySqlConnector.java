39defd023d77ff88902a606356b420f1415380d8
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-04-02 15:02:31 +0900

    refactor : null 제거 후 예외처리

--- src/main/java/dao/MySqlConnector.java
+++ src/main/java/dao/MySqlConnector.java
@@ -6,19 +6,17 @@ import java.sql.SQLException;
 
 public class MySqlConnector implements DataBaseConnector {
 
-    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
-    private static final String DATABASE = "janggi"; // MySQL DATABASE 이름
+    private static final String SERVER = "localhost:13306";
+    private static final String DATABASE = "janggi";
     private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
     public Connection getConnection() {
-        // 드라이버 연결
         try {
             return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
         } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
-            return null;
+            throw new RuntimeException("[ERROR] 커넥션을 가져오는 데 실패하였습니다.");
         }
     }
 }
