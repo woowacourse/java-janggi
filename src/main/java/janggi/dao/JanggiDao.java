998334a78501a25b7a1b53ff5cdc005a5489d38f
julie <7jujubuilds@gmail.com>
2025-04-02 18:33:56 +0900

    refactor(dao): 반복되는 로직을 메소드화하여 중복 제거

--- src/main/java/janggi/dao/JanggiDao.java
+++ src/main/java/janggi/dao/JanggiDao.java
@@ -9,6 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class JanggiDao {
 
@@ -29,49 +31,67 @@ public class JanggiDao {
         }
     }
 
-    public List<PieceDto> getPieces() {
-        String query = "SELECT * FROM pieces";
-
+    private void executeQuery(String query, Consumer<PreparedStatement> queryHandler) {
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-            List<PieceDto> pieceDtos = new ArrayList<>();
-
-            while (resultSet.next()) {
-                pieceDtos.add(new PieceDto(
-                        resultSet.getString("name"),
-                        resultSet.getString("side"),
-                        resultSet.getInt("position_row"),
-                        resultSet.getInt("position_column")
-                ));
-            }
-
-            return pieceDtos;
+            queryHandler.accept(preparedStatement);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public PieceDto getLastMovedPiece() {
-        String query = "SELECT * FROM pieces ORDER BY last_moved DESC LIMIT 1";
-
+    private <T> T executeQuery(String query, Function<PreparedStatement, T> queryHandler) {
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                return new PieceDto(
-                        resultSet.getString("name"),
-                        resultSet.getString("side"),
-                        resultSet.getInt("position_row"),
-                        resultSet.getInt("position_column")
-                );
-            }
+            return queryHandler.apply(preparedStatement);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        return null;
+    public List<PieceDto> getPieces() {
+        String query = "SELECT * FROM pieces";
+        Function<PreparedStatement, List<PieceDto>> execution = (PreparedStatement preparedStatement) -> {
+            try {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                List<PieceDto> pieceDtos = new ArrayList<>();
+
+                while (resultSet.next()) {
+                    pieceDtos.add(getPieceDto(resultSet));
+                }
+
+                return pieceDtos;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        return executeQuery(query, execution);
+    }
+
+    private PieceDto getPieceDto(ResultSet resultSet) throws SQLException {
+        return new PieceDto(
+                resultSet.getString("name"),
+                resultSet.getString("side"),
+                resultSet.getInt("position_row"),
+                resultSet.getInt("position_column"));
+    }
+
+    public PieceDto getLastMovedPiece() {
+        String query = "SELECT * FROM pieces ORDER BY last_moved DESC LIMIT 1";
+        Function<PreparedStatement, PieceDto> execution = (PreparedStatement preparedStatement) -> {
+            try {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    return getPieceDto(resultSet);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        };
+
+        return executeQuery(query, execution);
     }
 
     public void addPieces(List<PieceDto> piece) {
@@ -94,9 +114,7 @@ public class JanggiDao {
             connection.commit();
         } catch (SQLException e) {
             try {
-                if (connection != null) {
-                    connection.rollback();
-                }
+                connection.rollback();
             } catch (SQLException e2) {
                 throw new RuntimeException(e);
             }
@@ -130,9 +148,7 @@ public class JanggiDao {
             connection.commit();
         } catch (SQLException e) {
             try {
-                if (connection != null) {
-                    connection.rollback();
-                }
+                connection.rollback();
             } catch (SQLException e2) {
                 throw new RuntimeException(e);
             }
@@ -141,12 +157,14 @@ public class JanggiDao {
 
     public void deleteAllPieces() {
         String query = "DELETE FROM pieces";
+        Consumer<PreparedStatement> execution = (PreparedStatement preparedStatement) -> {
+            try {
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        };
 
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        executeQuery(query, execution);
     }
 }
