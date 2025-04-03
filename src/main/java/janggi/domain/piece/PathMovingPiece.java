95b863483b26014c6f579144cc08fe78728d8791
juno <wnsgh732@gmail.com>
2025-04-03 16:47:52 +0900

    refactor: 메서드명 수정

--- src/main/java/janggi/domain/piece/PathMovingPiece.java
+++ src/main/java/janggi/domain/piece/PathMovingPiece.java
@@ -2,7 +2,6 @@ package janggi.domain.piece;
 
 import janggi.domain.Team;
 import janggi.domain.piece.movement.Movement;
-
 import java.util.List;
 import java.util.Map;
 
@@ -17,7 +16,7 @@ public abstract class PathMovingPiece extends Piece {
         List<Movement> movements = findMovements(positionToMove);
         Position currentPosition = getPosition();
         for (Movement movement : movements) {
-            if (!checkPieceCondition(pieces.get(currentPosition))) {
+            if (hasPieceOnPath(pieces.get(currentPosition))) {
                 throw new IllegalArgumentException("불가능한 이동입니다");
             }
             currentPosition = currentPosition.plus(movement.getX(), movement.getY());
@@ -27,8 +26,8 @@ public abstract class PathMovingPiece extends Piece {
         }
     }
 
-    private boolean checkPieceCondition(Piece pieceInPositionToMove) {
-        return pieceInPositionToMove.isNone();
+    private boolean hasPieceOnPath(Piece pieceInPositionToMove) {
+        return !pieceInPositionToMove.isNone();
     }
 
     protected abstract List<Movement> findMovements(Position positionToMove);
