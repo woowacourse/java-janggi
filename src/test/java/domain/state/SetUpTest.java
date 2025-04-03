b612b57f6a46b0e6d710e4892996b86db23ff3c8
geonwoo <kysub99@naver.com>
2025-04-04 00:49:59 +0900

    test: SetUp 상태 테스트 추가

---
+++ src/test/java/domain/state/SetUpTest.java
@@ -0,0 +1,42 @@
+package domain.state;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.board.Board;
+import domain.board.BoardFactory;
+import domain.board.Column;
+import domain.board.Position;
+import domain.board.Row;
+import domain.piece.PieceType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetUpTest {
+
+    private State state;
+
+    @BeforeEach
+    void setUp() {
+        state = new SetUp();
+    }
+
+    @Test
+    void 게임_시작_전에는_기물을_움직일_수_없다() {
+        Position source = new Position(Row.ZERO, Column.ONE);
+        Position target = new Position(Row.EIGHT, Column.ONE);
+
+        assertThatThrownBy(() -> state.movePiece(PieceType.CHARIOT, source, target))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void startGame_호출하면_BlueTurn_상태가_된다() {
+        BoardFactory boardFactory = new BoardFactory();
+        Board board = boardFactory.createBoard();
+
+        State newState = state.startGame(board);
+
+        assertThat(newState).isInstanceOf(BlueTurn.class);
+    }
+}
