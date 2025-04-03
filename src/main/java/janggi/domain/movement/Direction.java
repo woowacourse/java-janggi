21e1748c2104a4e1c9e98a38122ed5082ab30674
julie <7jujubuilds@gmail.com>
2025-04-04 01:30:14 +0900

    fix: Direction 내 누락되었던 상수 추가

--- src/main/java/janggi/domain/movement/Direction.java
+++ src/main/java/janggi/domain/movement/Direction.java
@@ -9,13 +9,25 @@ public enum Direction {
     LEFT(new Vector(0, -1)),
     RIGHT(new Vector(0, 1)),
     LEFT_UP(new Vector(-1, -1)),
+    LEFT_LEFT_UP(new Vector(-1, -2)),
+    LEFT_LEFT_LEFT_UP_UP(new Vector(-2, -3)),
     LEFT_DOWN(new Vector(1, -1)),
+    LEFT_LEFT_DOWN(new Vector(1, -2)),
+    LEFT_LEFT_LEFT_DOWN_DOWN(new Vector(2, -3)),
     RIGHT_UP(new Vector(-1, 1)),
+    RIGHT_RIGHT_UP(new Vector(-1, 2)),
+    RIGHT_RIGHT_RIGHT_UP_UP(new Vector(-2, 3)),
     RIGHT_DOWN(new Vector(1, 1)),
+    RIGHT_RIGHT_DOWN(new Vector(1, 2)),
+    RIGHT_RIGHT_RIGHT_DOWN_DOWN(new Vector(2, 3)),
     DOWN_DOWN_LEFT(new Vector(2, -1)),
-    DOWN_DOWN_RIGHT(new Vector(2, 1)),
     DOWN_DOWN_DOWN_LEFT_LEFT(new Vector(3, -2)),
+    DOWN_DOWN_RIGHT(new Vector(2, 1)),
     DOWN_DOWN_DOWN_RIGHT_RIGHT(new Vector(3, 2)),
+    UP_UP_LEFT(new Vector(-2, -1)),
+    UP_UP_UP_LEFT_LEFT(new Vector(-3, -2)),
+    UP_UP_RIGHT(new Vector(-2, 1)),
+    UP_UP_UP_RIGHT_RIGHT(new Vector(-3, 2)),
     ;
 
     private final Vector vector;
@@ -37,7 +49,8 @@ public enum Direction {
 
     public Direction rotate() {
         Vector vector = getVector();
-        return getDirection(vector);
+        Vector rotatedVector = Vector.rotate(vector);
+        return getDirection(rotatedVector);
     }
 
     public static boolean isDiagonal(Direction direction) {
