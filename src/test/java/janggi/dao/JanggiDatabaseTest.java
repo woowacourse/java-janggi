d786eb921999a5c619a9f32a326ac767a5c5a51a
dye0p <s01054830455@gmail.com>
2025-04-04 02:52:25 +0900

    test: db 연결 테스트 수정

--- src/test/java/janggi/dao/JanggiDatabaseTest.java
+++ src/test/java/janggi/dao/JanggiDatabaseTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.Test;
 
 class JanggiDatabaseTest {
 
-    private final JanggiDatabase janggiDatabase = new JanggiDatabase();
+    /*
+    해당 테스트가 실패한다면 데이터베이스 연결을 해야 합니다.
+     */
 
     @DisplayName("데이터베이스 연결을 할 수 있다.")
     @Test
     void connection() {
-        try (final var connection = janggiDatabase.getConnection()) {
+        try (final var connection = JanggiDatabase.getConnection()) {
             assertThat(connection).isNotNull();
         } catch (SQLException e) {
             throw new RuntimeException(e);
