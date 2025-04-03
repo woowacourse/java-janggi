6249e5b3bc53dede6663473fdfcda01b4b8ab268
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-04-03 18:00:31 +0900

    refactor: service 계층 삭제 후 controller에서 DAO 사용

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,14 @@
-import config.JanggiConfig;
+import dao.DaoConfiguration;
+import dao.GameDao;
+import dao.PieceDao;
+import dao.ProdDaoConfiguration;
 import java.util.Optional;
 import java.util.function.Supplier;
+import model.JanggiGame;
 import model.piece.Piece;
 import model.position.Position;
 import model.piece.Team;
 import model.piece.Score;
-import service.JanggiService;
 import view.GameProgressView;
 import view.GameResultView;
 import view.JanggiPiecesView;
@@ -15,47 +18,62 @@ public class Application {
     private static final GameProgressView gameProgressView = new GameProgressView();
     private static final JanggiPiecesView janggiPiecesView = new JanggiPiecesView();
     private static final GameResultView gameResultView = new GameResultView();
-    private static final JanggiService janggiService = JanggiConfig.createJanggiService();
+    private static final DaoConfiguration daoConfiguration = new ProdDaoConfiguration();
+    private static final PieceDao pieceDao = new PieceDao(daoConfiguration);
+    private static final GameDao gameDao = new GameDao(daoConfiguration);
+    private static final JanggiGame janggiGame = JanggiGame.initPiecesFrom(pieceDao.getAllPieces(), gameDao.getTurn());
 
     public static void main(String[] args) {
         janggiPiecesView.printJanggiStart();
         while (true) {
-            janggiPiecesView.showCurrentPositionOfPieces(janggiService.getPieces());
-            Team currentTurn = janggiService.getCurrentTurn();
+            janggiPiecesView.showCurrentPositionOfPieces(janggiGame.getPieces());
+            Team currentTurn = janggiGame.getCurrentTurn();
             gameProgressView.printCurrentTurnOfTeam(currentTurn);
             Optional<Position> departureOfNullable = createDeparture();
             if (departureOfNullable.isEmpty()) {
-                janggiService.removeGameInfo();
+                removeGameInfo();
                 break;
             }
             Position departure = departureOfNullable.get();
             createArrivalAndMove(departure);
-            if (janggiService.isEnd()) {
-                janggiService.removeGameInfo();
+            if (janggiGame.isEnd()) {
+                removeGameInfo();
                 gameResultView.printGeneralDie(currentTurn);
                 break;
             }
         }
     }
 
+    private static void removeGameInfo() {
+        pieceDao.deletePieces();
+        gameDao.deleteTurn();
+    }
+
     private static void createArrivalAndMove(Position departure) {
         retryOnInvalidInput(() -> {
-            Piece pieceOfDeparture = janggiService.findPieceBy(departure);
+            Piece pieceOfDeparture = janggiGame.findPieceBy(departure);
             Position arrival = gameProgressView.choiceArrivalOf(pieceOfDeparture);
-            janggiService.move(departure, arrival);
+            move(departure, arrival);
             return null;
         });
+    }
+
+    private static void move(Position departure, Position arrival) {
+        janggiGame.move(departure, arrival);
+        pieceDao.deletePiece(arrival);
+        pieceDao.updatePiece(departure, arrival);
+        gameDao.updateTurn(janggiGame.getCurrentTurn());
     }
 
     private static Optional<Position> createDeparture() {
         return retryOnInvalidInput(() -> {
             Optional<Position> departure = gameProgressView.choiceDeparture();
             if (departure.isEmpty()) {
-                Score score = janggiService.showGameResult();
+                Score score = janggiGame.showGameResult();
                 gameResultView.printGameResult(score);
                 return departure;
             }
-            janggiService.findPieceBy(departure.get()); // 해당 위치에 기물이 있는지 검증한다.
+            janggiGame.findPieceBy(departure.get()); // 해당 위치에 기물이 있는지 검증한다.
             return departure;
         });
     }
