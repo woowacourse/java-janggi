c216b026d8ff005a19b4753d3d068c4c8c71093a
mimi <110226866+hongkikii@users.noreply.github.com>
2025-04-03 09:40:19 +0900

    chore: .gitignore 수정

--- src/main/java/location/Distance.java
+++
@@ -1,8 +0,0 @@
-package location;
-
-public record Distance(int x, int y) {
-
-    public static Distance createBy(Position from, Position to) {
-        return new Distance(to.x() - from.x(), to.y() - from.y());
-    }
-}
