62b618f772eacbf17f77c46e2f0d4b0f4ed785a7
KJungW <sagakkb0707@naver.com>
2025-04-03 09:42:48 +0900

    test(fixture): 픽스쳐의 일부 데이터 상수화

--- src/test/java/janggi/data/fixture/GungsungPositionFixture.java
+++ src/test/java/janggi/data/fixture/GungsungPositionFixture.java
@@ -4,13 +4,13 @@ import janggi.value.Position;
 
 public class GungsungPositionFixture {
 
-    public static Position CENTER = new Position(4, 1);
-    public static Position UP = new Position(4, 0);
-    public static Position DOWN = new Position(4, 2);
-    public static Position LEFT = new Position(3, 1);
-    public static Position RIGHT = new Position(5, 1);
-    public static Position UP_RIGHT = new Position(5, 0);
-    public static Position UP_LEFT = new Position(3, 0);
-    public static Position DOWN_RIGHT = new Position(5, 2);
-    public static Position DOWN_LEFT = new Position(3, 2);
+    public static final Position CENTER = new Position(4, 1);
+    public static final Position UP = new Position(4, 0);
+    public static final Position DOWN = new Position(4, 2);
+    public static final Position LEFT = new Position(3, 1);
+    public static final Position RIGHT = new Position(5, 1);
+    public static final Position UP_RIGHT = new Position(5, 0);
+    public static final Position UP_LEFT = new Position(3, 0);
+    public static final Position DOWN_RIGHT = new Position(5, 2);
+    public static final Position DOWN_LEFT = new Position(3, 2);
 }
--- src/test/java/janggi/data/fixture/HanPiecePositionFixture.java
+++ src/test/java/janggi/data/fixture/HanPiecePositionFixture.java
@@ -5,31 +5,31 @@ import java.util.List;
 
 public class HanPiecePositionFixture {
 
-    public final static List<Position> GUNG_POSITIONS = List.of(
+    public static final List<Position> GUNG_POSITIONS = List.of(
             new Position(4, 1));
-    public final static List<Position> CHA_POSITIONS = List.of(
+    public static final List<Position> CHA_POSITIONS = List.of(
             new Position(0, 0), new Position(8, 0));
-    public final static List<Position> SA_POSITIONS = List.of(
+    public static final List<Position> SA_POSITIONS = List.of(
             new Position(3, 0), new Position(5, 0));
-    public final static List<Position> PO_POSITIONS = List.of(
+    public static final List<Position> PO_POSITIONS = List.of(
             new Position(1, 2), new Position(7, 2));
-    public final static List<Position> BYUNG_POSITIONS = List.of(
+    public static final List<Position> BYUNG_POSITIONS = List.of(
             new Position(0, 3), new Position(2, 3), new Position(4, 3),
             new Position(6, 3), new Position(8, 3));
-    public final static List<Position> MA_POSITIONS_WITH_LEFT_SANG = List.of(
+    public static final List<Position> MA_POSITIONS_WITH_LEFT_SANG = List.of(
             new Position(2, 0), new Position(7, 0));
-    public final static List<Position> MA_POSITIONS_WITH_RIGHT_SANG = List.of(
+    public static final List<Position> MA_POSITIONS_WITH_RIGHT_SANG = List.of(
             new Position(1, 0), new Position(6, 0));
-    public final static List<Position> MA_POSITIONS_WITH_IN_SANG = List.of(
+    public static final List<Position> MA_POSITIONS_WITH_IN_SANG = List.of(
             new Position(1, 0), new Position(7, 0));
-    public final static List<Position> MA_POSITIONS_WITH_OUT_SANG = List.of(
+    public static final List<Position> MA_POSITIONS_WITH_OUT_SANG = List.of(
             new Position(2, 0), new Position(6, 0));
-    public final static List<Position> SANG_POSITIONS_WITH_LEFT_SANG = List.of(
+    public static final List<Position> SANG_POSITIONS_WITH_LEFT_SANG = List.of(
             new Position(1, 0), new Position(6, 0));
-    public final static List<Position> SANG_POSITIONS_WITH_RIGHT_SANG = List.of(
+    public static final List<Position> SANG_POSITIONS_WITH_RIGHT_SANG = List.of(
             new Position(2, 0), new Position(7, 0));
-    public final static List<Position> SANG_POSITIONS_WITH_IN_SANG = List.of(
+    public static final List<Position> SANG_POSITIONS_WITH_IN_SANG = List.of(
             new Position(2, 0), new Position(6, 0));
-    public final static List<Position> SANG_POSITIONS_WITH_OUT_SANG = List.of(
+    public static final List<Position> SANG_POSITIONS_WITH_OUT_SANG = List.of(
             new Position(1, 0), new Position(7, 0));
 }
