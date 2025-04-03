3a02c5044c10b24322a0aa512b78781fc0141347
minSsan <msp1125@hanyang.ac.kr>
2025-04-03 23:38:18 +0900

    feat: piece_type, team 테이블 삭제

--- db/init.sql
+++ db/init.sql
@@ -1,37 +1,13 @@
 USE janggi;
 
-CREATE TABLE team (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL
-);
-
-CREATE TABLE piece_type (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL
-);
-
 CREATE TABLE piece (
     position_row INT NOT NULL,
     position_col INT NOT NULL,
-    type_id INT NOT NULL,
-    team_id INT NOT NULL,
-    primary key (position_row, position_col),
-    foreign key (type_id) references piece_type(id),
-    foreign key (team_id) references team(id)
+    type VARCHAR(30) NOT NULL,
+    team VARCHAR(30) NOT NULL,
+    primary key (position_row, position_col)
 );
 
 CREATE TABLE turn (
-    team_id INT NOT NULL PRIMARY KEY,
-    foreign key (team_id) references team(id)
+    team VARCHAR(30) NOT NULL
 );
-
-INSERT INTO team (name) VALUES ('BLUE'), ('RED');
-
-INSERT INTO piece_type (name) VALUES
-                                  ('CANNON'),
-                                  ('CHARIOT'),
-                                  ('HORSE'),
-                                  ('ELEPHANT'),
-                                  ('GUARD'),
-                                  ('PAWN'),
-                                  ('KING');
