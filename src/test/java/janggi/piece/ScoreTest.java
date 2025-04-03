361b1bfb26a89de1aa9bec31279cde08c92381fa
eunseongu <rndmstjs75@gmail.com>
2025-04-03 20:38:43 +0900

    refactor: 불필요한 변수 제거 및 주석 추가

--- src/test/java/janggi/piece/ScoreTest.java
+++ src/test/java/janggi/piece/ScoreTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import janggi.board.Board;
 import janggi.board.BoardGenerator;
-import janggi.dao.PieceDao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,9 +12,10 @@ public class ScoreTest {
     @DisplayName("각 기물의 점수를 계산한다.")
     @Test
     void calculateScoreTest() {
-        PieceDao pieceDao = new PieceDao();
-        Board board = BoardGenerator.generate(pieceDao);
+        // given
+        Board board = BoardGenerator.generate();
 
+        // when & then
         assertThat(board.calculateChuScore()).isEqualTo(72.0);
         assertThat(board.calculateHanScore()).isEqualTo(73.5);
     }
