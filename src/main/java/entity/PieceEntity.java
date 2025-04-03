ab3bbd9d56b0bfeec8e722b8975086761286e44f
minSsan <msp1125@hanyang.ac.kr>
2025-04-04 06:39:15 +0900

    feat: PieceEntity 메서드 추가

--- src/main/java/entity/PieceEntity.java
+++ src/main/java/entity/PieceEntity.java
@@ -1,6 +1,8 @@
 package entity;
 
+import domain.janggiPiece.JanggiChessPiece;
 import domain.janggiPiece.Piece;
+import domain.position.JanggiPosition;
 import domain.type.JanggiTeam;
 
 public record PieceEntity(
@@ -9,6 +11,14 @@ public record PieceEntity(
         JanggiTeam team,
         Piece type
 ) {
+    public PieceEntity move(JanggiPosition to) {
+        return new PieceEntity(to.getRow(), to.getCol(), team, type);
+    }
+
+    public JanggiChessPiece createPiece() {
+        return type().create(team);
+    }
+
     public String getTeamName() {
         return team.name;
     }
