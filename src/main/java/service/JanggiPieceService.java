8d2e462383b5da58146081bf2675d13a16a28068
minSsan <msp1125@hanyang.ac.kr>
2025-04-04 06:41:28 +0900

    feat: PieceDao DB 작업 수행하는 JanggiPieceService 정의

---
+++ src/main/java/service/JanggiPieceService.java
@@ -0,0 +1,95 @@
+package service;
+
+import dao.PieceDao;
+import domain.hurdlePolicy.HurdlePolicy;
+import domain.janggiPiece.JanggiChessPiece;
+import domain.path.Path;
+import domain.position.JanggiPosition;
+import domain.position.JanggiPositionFactory;
+import domain.position.JanggiPositions;
+import domain.position.generator.DefaultPositionsGenerator;
+import domain.score.Score;
+import domain.type.JanggiTeam;
+import entity.PieceEntity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JanggiPieceService {
+    private final JanggiPositions janggiPositions;
+    private final PieceDao pieceDao;
+
+    public JanggiPieceService(
+            DefaultPositionsGenerator defaultPositionsGenerator,
+            PieceDao pieceDao
+    ) {
+        this.pieceDao = pieceDao;
+        this.janggiPositions = getInitPositions(defaultPositionsGenerator);
+    }
+
+    private JanggiPositions getInitPositions(DefaultPositionsGenerator defaultPositionsGenerator) {
+        List<PieceEntity> all = pieceDao.findAll();
+        if (all.isEmpty()) {
+            Map<JanggiPosition, JanggiChessPiece> pieces = defaultPositionsGenerator.generate();
+            saveAll(pieces);
+            return new JanggiPositions(pieces);
+        }
+        Map<JanggiPosition, JanggiChessPiece> result = new HashMap<>();
+        for (PieceEntity entity : all) {
+            JanggiChessPiece piece = entity.createPiece();
+            result.put(JanggiPositionFactory.of(entity.row(), entity.col()), piece);
+        }
+        return new JanggiPositions(result);
+    }
+
+    private void saveAll(Map<JanggiPosition, JanggiChessPiece> pieces) {
+        List<PieceEntity> entities = pieces.entrySet().stream()
+                .map(entry -> new PieceEntity(
+                        entry.getKey().getRow(),
+                        entry.getKey().getCol(),
+                        entry.getValue().getTeam(),
+                        entry.getValue().getChessPieceType()
+                ))
+                .toList();
+        for (PieceEntity entity : entities) {
+            pieceDao.save(entity);
+        }
+    }
+
+    public void move(final JanggiPosition from, final JanggiPosition to) {
+        PieceEntity pieceEntity = pieceDao.findByPosition(from).get();
+        janggiPositions.move(from, to);
+        pieceDao.deleteByPosition(from);
+        pieceDao.deleteByPosition(to);
+        pieceDao.save(pieceEntity.move(to));
+    }
+
+    public List<JanggiPosition> getAvailableDestination(final JanggiPosition position) {
+        JanggiChessPiece chessPiece = janggiPositions.getJanggiPieceByPosition(position);
+        List<Path> coordinatePaths = chessPiece.getCoordinatePaths(position);
+        HurdlePolicy hurdlePolicy = chessPiece.getHurdlePolicy();
+        return hurdlePolicy.pickDestinations(chessPiece.getTeam(), coordinatePaths, janggiPositions);
+    }
+
+    public JanggiChessPiece getPieceByPosition(final JanggiPosition position) {
+        return janggiPositions.getJanggiPieceByPosition(position);
+    }
+
+    public boolean isExistAt(final JanggiPosition targetPosition) {
+        return janggiPositions.existChessPieceByPosition(targetPosition);
+    }
+
+    public void reset() {
+        janggiPositions.reset();
+        pieceDao.deleteAll();
+    }
+
+    public Score getScoreWith(final JanggiTeam team) {
+        return janggiPositions.calculateScoreWith(team);
+    }
+
+    public Map<JanggiPosition, JanggiChessPiece> getPieces() {
+        return janggiPositions.getJanggiPieces();
+    }
+}
