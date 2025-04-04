e37f127862fbd20225ad3c9bfe9ebf5edaa3a5e0
dltmddus <tmddus4671@gmail.com>
2025-04-04 09:32:35 +0900

    refactor: 코드 한 줄 띄워쓰기

--- src/main/java/domain/File.java
+++ src/main/java/domain/File.java
@@ -1,7 +1,16 @@
 package domain;
 
 public enum File {
-    ZERO(0), NINE(9), EIGHT(8), SEVEN(7), SIX(6), FIVE(5), FOUR(4), THREE(3), TWO(2), ONE(1);
+    ZERO(0),
+    NINE(9),
+    EIGHT(8),
+    SEVEN(7),
+    SIX(6),
+    FIVE(5),
+    FOUR(4),
+    THREE(3),
+    TWO(2),
+    ONE(1);
 
     private final int file;
 
--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,7 +1,15 @@
 package domain;
 
 public enum Rank {
-    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9);
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9);
 
     final int rank;
 
