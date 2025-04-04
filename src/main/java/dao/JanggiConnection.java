574612ac72e6f8f2f4c04888c51b052227d7bf8d
Jin409 <sualng123@gmail.com>
2025-04-04 15:33:25 +0900

    chore(application.properties): 데이터베이스 커넥션 설정 외부화

--- src/main/java/dao/JanggiConnection.java
+++ src/main/java/dao/JanggiConnection.java
@@ -1,22 +1,40 @@
 package dao;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class JanggiConnection {
-    private static final String SERVER = "localhost:13306";
-    private static final String DATABASE = "janggi";
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private final String server;
+    private final String database;
+    private final String options;
+    private final String username;
+    private final String password;
+
+    public JanggiConnection() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+
+            this.server = prop.getProperty("database.host") + ":" + prop.getProperty("database.port");
+            this.database = prop.getProperty("database.name");
+            this.options = prop.getProperty("database.options");
+            this.username = prop.getProperty("database.username");
+            this.password = prop.getProperty("database.password");
+
+        } catch (Exception e) {
+            throw new RuntimeException("설정 파일 로딩 실패", e);
+        }
+    }
 
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
+            String url = "jdbc:mysql://" + server + "/" + database + "?" + options;
+            return DriverManager.getConnection(url, username, password);
         } catch (final SQLException e) {
             System.err.println("DB 연결 오류:" + e.getMessage());
-            e.getStackTrace();
             return null;
         }
     }
