ee1f461f5e031f3b3d4e3fe32b0a4fc8ec15ef7d
Mint <audwl03071@gmail.com>
2025-04-05 18:57:33 +0900

    refactor: 사용하지 않은 코드 제거

--- src/main/java/janggi/dto/PieceUpdateDto.java
+++
@@ -1,7 +0,0 @@
-package janggi.dto;
-
-import janggi.direction.PieceType;
-import janggi.piece.players.Team;
-
-public record PieceUpdateDto(Team team, PieceType pieceType, int currentY, int currentX, int arrivalY, int arrivalX) {
-}
