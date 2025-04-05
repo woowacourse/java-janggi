f68c2fa7ef5e1b89e67d845a8a8bd07f43688a02
Mint <audwl03071@gmail.com>
2025-04-05 18:57:22 +0900

    fix: Turn 초기화시 기존 Turn 데이터 제거하도록 수정

--- src/main/java/janggi/dao/turn/TurnDaoImpl.java
+++ src/main/java/janggi/dao/turn/TurnDaoImpl.java
@@ -21,22 +21,15 @@ public class TurnDaoImpl implements TurnDao {
 
     @Override
     public void initialize(final TurnDto turnDto) {
-        final var query = "INSERT INTO turn (current_team) VALUES(?)";
-        try (final var connection = dbUtil.getConnection();
-             final var preparedStatement = connection.prepareStatement(query)) {
-            final int teamId = getTeamIdByName(connection, turnDto.team().name());
-            preparedStatement.setInt(1, teamId);
-            preparedStatement.executeUpdate();
-        } catch (final SQLException e) {
-            throw new RuntimeException(e);
-        }
+        deleteAll();
+        insertCurrentTeam(turnDto);
     }
 
     @Override
     public Turn selectCurrentTeam() {
         final var query = "SELECT current_team FROM turn LIMIT 1";
-        try (final var connection = dbUtil.getConnection();
-             final var preparedStatement = connection.prepareStatement(query)) {
+        try (var connection = dbUtil.getConnection();
+             var preparedStatement = connection.prepareStatement(query)) {
             final var resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
                 final String teamName = getTeamNameById(connection, resultSet.getInt("current_team"));
@@ -51,8 +44,8 @@ public class TurnDaoImpl implements TurnDao {
     @Override
     public void updateTurn(final Team team) {
         final var updateQuery = "UPDATE turn SET current_team = ?";
-        try (final var connection = dbUtil.getConnection();
-             final var preparedStatement = connection.prepareStatement(updateQuery)) {
+        try (var connection = dbUtil.getConnection();
+             var preparedStatement = connection.prepareStatement(updateQuery)) {
             preparedStatement.setInt(1, getTeamIdByName(connection, team.name()));
             preparedStatement.executeUpdate();
         } catch (final SQLException e) {
@@ -63,8 +56,20 @@ public class TurnDaoImpl implements TurnDao {
     @Override
     public void deleteAll() {
         final var deleteQuery = "DELETE FROM turn WHERE turn_id > 0";
-        try (final var connection = dbUtil.getConnection();
-             final var preparedStatement = connection.prepareStatement(deleteQuery)) {
+        try (var connection = dbUtil.getConnection();
+             var preparedStatement = connection.prepareStatement(deleteQuery)) {
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insertCurrentTeam(final TurnDto turnDto) {
+        final var query = "INSERT INTO turn (current_team) VALUES(?)";
+        try (var connection = dbUtil.getConnection();
+             var preparedStatement = connection.prepareStatement(query)) {
+            final int teamId = getTeamIdByName(connection, turnDto.team().name());
+            preparedStatement.setInt(1, teamId);
             preparedStatement.executeUpdate();
         } catch (final SQLException e) {
             throw new RuntimeException(e);
@@ -73,7 +78,7 @@ public class TurnDaoImpl implements TurnDao {
 
     private String getTeamNameById(final Connection connection, final int teamId) {
         final var query = "SELECT name FROM team WHERE team_id = ?";
-        try (final var preparedStatement = connection.prepareStatement(query)) {
+        try (var preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, teamId);
 
             final var resultSet = preparedStatement.executeQuery();
@@ -88,7 +93,7 @@ public class TurnDaoImpl implements TurnDao {
 
     private int getTeamIdByName(final Connection connection, final String teamName) {
         final var query = "SELECT * FROM team WHERE name = ?";
-        try (final var preparedStatement = connection.prepareStatement(query)) {
+        try (var preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, teamName);
 
             final var resultSet = preparedStatement.executeQuery();
