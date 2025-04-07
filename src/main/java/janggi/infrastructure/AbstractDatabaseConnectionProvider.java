ce7a599cb9fcb598d9200013acb2b9bf0d61a778
Mint <audwl03071@gmail.com>
2025-04-07 12:40:01 +0900

    feat: AbstractDatabaseConnectionProvider에 abstract 키워드 추가

--- src/main/java/janggi/infrastructure/AbstractDatabaseConnectionProvider.java
+++ src/main/java/janggi/infrastructure/AbstractDatabaseConnectionProvider.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class AbstractDatabaseConnectionProvider implements DatabaseConnectionProvider {
+public abstract class AbstractDatabaseConnectionProvider implements DatabaseConnectionProvider {
 
     private final String configPath;
 
