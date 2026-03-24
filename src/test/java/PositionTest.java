59fcc57199fd641e745f5f72bcb1827ea1e84206
frombunny <rucy011121@gmail.com>
2026-03-24 20:43:42 +0900

    feat(Position): 좌표 기능 구현

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,26 @@
+import static org.assertj.core.api.Assertions.*;
+
+import domain.Position;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+    @Test
+    void 장기판의_좌표의_범위는_가로_9칸_세로_10칸이다() {
+        Position position = new Position(2, 7);
+
+        assertThat(position).isInstanceOf(Position.class);
+    }
+
+    @Test
+    void 장기판의_가로_범위를_벗어날_경우_예외를_뱐환한다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new Position(9, 7));
+    }
+
+    @Test
+    void 장기판의_세로_범위를_벗어날_경우_예외를_뱐환한다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new Position(7, 11));
+    }
+}
