1c6816682754df23c295232100121e6951cc24a6
sinequanonn <dleck288@gmail.com>
2026-03-24 21:39:21 +0900

    feat: 장기판 위치 VO 구현

---
+++ src/main/java/janggi/domain/Position.java
@@ -0,0 +1,11 @@
+package janggi.domain;
+
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
