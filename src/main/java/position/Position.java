d976c86001f5f434af71be07305209614c47e3a9
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-24 22:10:43 +0900

    feat: 장기판 범위에 맞는 좌표 생성 기능 추가

---
+++ src/main/java/position/Position.java
@@ -0,0 +1,17 @@
+package position;
+
+public record Position(
+        int row,
+        int column
+) {
+    public static Position of(int row, int column) {
+        validateRange(row, column);
+        return new Position(row, column);
+    }
+
+    private static void validateRange(int row, int column) {
+        if (row < 0 || row > 10 || column < 0 || column > 9) {
+            throw new IllegalArgumentException("장기판 범위를 벗어났습니다.");
+        }
+    }
+}
---
+++ src/test/java/position/PositionTest.java
@@ -0,0 +1,25 @@
+package position;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PositionTest {
+    @Test
+    @DisplayName("장기판안에 맞는 좌표를 생성할 수 있다.")
+    void of_ReturnsPosition_WhenCoordinateIsWithinBoard() {
+        Position position = Position.of(1, 2);
+
+        assertThat(position.row()).isEqualTo(1);
+        assertThat(position.column()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("장기판안에 맞지 않는 좌표를 생성할 시 예외가 발생한다")
+    void of_ThrowException_WhenCoordinateIsOutsideBoard() {
+        assertThatThrownBy(() -> Position.of(10, 11))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
