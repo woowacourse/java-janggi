b4c98e7ae1bef532542f5c83e172dd6d862a813b
toctoce <toctoce@naver.com>
2026-03-25 16:09:40 +0900

    feat(Cha): 차 이동 구현

---
+++ src/main/java/domain/piece/Cha.java
@@ -0,0 +1,37 @@
+package domain.piece;
+
+import domain.player.Team;
+import domain.position.Position;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cha extends Piece {
+
+    public Cha(Team team) {
+        super(team);
+    }
+
+    @Override
+    protected List<Position> getRawPositions(Position src) {
+        List<Position> rawPositions = new ArrayList<>();
+
+        for (int x = src.getX(); x <= 8; x++) {
+            rawPositions.add(new Position(x, src.getY()));
+        }
+        for (int x = src.getX(); x >= 0; x--) {
+            rawPositions.add(new Position(x, src.getY()));
+        }
+        for (int y = src.getY(); y <= 9; y++) {
+            rawPositions.add(new Position(src.getX(), y));
+        }
+        for (int y = src.getY(); y >= 9; y--) {
+            rawPositions.add(new Position(src.getX(), y));
+        }
+        return rawPositions;
+    }
+
+    @Override
+    protected List<Position> getPaths(Position src, Position dest) {
+        return List.of();
+    }
+}
