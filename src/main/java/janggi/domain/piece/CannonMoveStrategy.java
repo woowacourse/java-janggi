8440b6ac83fe98f3ade5c9ea8b99a6ee6e220d06
kangrae <kangrae0501@gmail.com>
2026-03-25 21:04:43 +0900

    feat: 포 기물의 이동가능한 위치 목록을 반환하는 기능 구현

--- src/main/java/janggi/domain/piece/CannonMoveStrategy.java
+++ src/main/java/janggi/domain/piece/CannonMoveStrategy.java
@@ -1,7 +1,9 @@
 package janggi.domain.piece;
 
 import janggi.domain.dynasty.Dynasty;
+import janggi.domain.position.Direction;
 import janggi.domain.position.Position;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,12 +11,41 @@ public class CannonMoveStrategy implements MoveStrategy {
 
     @Override
     public List<Position> canMovePositions(Map<Position, Piece> board, Position from, Dynasty dynasty) {
-        return List.of();
+        List<Position> canMovePositions = new ArrayList<>();
+        for (Direction dir : Direction.valuesFourDirection()) {
+            List<Position> positions = from.findPositionsByDirection(dir);
+            boolean isFirst = false;
+            for (Position to : positions) {
+                // 어떤 기물을 넘었을 때
+                if (isFirst) {
+                    if (board.containsKey(to)) {
+                        if (!board.get(to).isSameDynasty(dynasty) && !isCannon(board.get(to))) {
+                            canMovePositions.add(to);
+                        }
+                        break;
+                    }
+                    canMovePositions.add(to);
+                }
+                // 다른 기물을 만났을때
+                if (!isFirst && board.containsKey(to)) {
+                    if (isCannon(board.get(to))) {
+                        break;
+                    }
+                    isFirst = true;
+                }
+            }
+        }
+
+        return canMovePositions;
     }
 
     @Override
     public boolean canMove(Map<Position, Piece> board, Position from, Position to) {
         return false;
+    }
+
+    private boolean isCannon(Piece piece) {
+        return piece.moveStrategy() instanceof CannonMoveStrategy;
     }
 
 }
--- src/test/java/janggi/fixture/FakeBoardDesignPolicy.java
+++ src/test/java/janggi/fixture/FakeBoardDesignPolicy.java
@@ -3,13 +3,14 @@ package janggi.fixture;
 import janggi.domain.board.BoardDesignPolicy;
 import janggi.domain.piece.Piece;
 import janggi.domain.position.Position;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class FakeBoardDesignPolicy implements BoardDesignPolicy {
+
     @Override
     public Map<Position, Piece> initBoard() {
         return new HashMap<>(Map.of());
     }
+
 }
