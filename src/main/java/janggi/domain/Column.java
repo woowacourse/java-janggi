2072c6c30fdf5cf2df5adb112be9ff048324257e
kangrae <kangrae0501@gmail.com>
2026-03-25 10:50:12 +0900

    feat: 열 위치를 나타내는 Column 객체 구현

---
+++ src/main/java/janggi/domain/Column.java
@@ -0,0 +1,20 @@
+package janggi.domain;
+
+public record Column(
+        int column
+) {
+
+    private static final int MIN_COLUMN = 1;
+    private static final int MAX_COLUMN = 9;
+
+    public Column {
+        validate(column);
+    }
+
+    private void validate(int column) {
+        if (column < MIN_COLUMN || column > MAX_COLUMN) {
+            throw new IllegalArgumentException(String.format("열은 %d부터 %d사이의 숫자입니다.", MIN_COLUMN, MAX_COLUMN));
+        }
+    }
+
+}
--- src/test/java/janggi/domain/ColumnTest.java
+++ src/test/java/janggi/domain/ColumnTest.java
@@ -6,21 +6,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class RowTest {
+class ColumnTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 10})
-    public void 행의_값은_1부터_10까지의_숫자이다(int row) {
+    @ValueSource(ints = {1, 9})
+    public void 열의_값은_1부터_9까지의_숫자이다(int column) {
         // when & then
-        assertThatCode(() -> new Row(row))
+        assertThatCode(() -> new Column(column))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 11})
-    public void 행의_값이_1부터_10까지_숫자가_아니면_예외가_발생한다(int row) {
+    @ValueSource(ints = {-1, 0, 10})
+    public void 열의_값이_1부터_9까지_숫자가_아니면_예외가_발생한다(int column) {
         // when & then
-        assertThatThrownBy(() -> new Row(row))
+        assertThatThrownBy(() -> new Column(column))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
