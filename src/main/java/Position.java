9a5a435f9c2f3c30a646585118823c9c52994190
snow___shower <wonsb25@gmail.com>
2026-03-25 13:17:32 +0900

    feat: Position 객체 생성 시 Y 좌표 검증
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,10 +1,15 @@
 public record Position(int column, int row) {
 
     public static final String ERROR_INVALID_X_COORDINATE = "[ERROR] x 좌표가 올바르지 않습니다.";
+    public static final String ERROR_INVALID_Y_COORDINATE = "[ERROR] y 좌표가 올바르지 않습니다.";
 
     public Position {
         if (column < 0 || column > 9) {
             throw new IllegalArgumentException(ERROR_INVALID_X_COORDINATE);
+        }
+
+        if (row < 1 || column > 0) {
+            throw new IllegalArgumentException(ERROR_INVALID_Y_COORDINATE);
         }
     }
 }
--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -18,4 +18,18 @@ public class PositionTest {
         assertThatThrownBy(() -> new Position(10, 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("y 좌표가 1 미만일 경우 예외가 발생한다.")
+    void throwException_When_YCoordinateLessThanZero() {
+        assertThatThrownBy(() -> new Position(1, 0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("y 좌표가 9 초과인 경우 예외가 발생한다.")
+    void throwException_When_YCoordinateMoreThanNine() {
+        assertThatThrownBy(() -> new Position(10, 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
