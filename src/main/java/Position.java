e27a0731a756577ca7dcec3e1962a124d1cad86f
snow___shower <wonsb25@gmail.com>
2026-03-25 13:52:34 +0900

    refactor: Position 최대값 및 최소값 상수 처리
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,14 +1,18 @@
 public record Position(int column, int row) {
 
+    private static final int MIN_COLUMN = 0;
+    private static final int MAX_COLUMN = 9;
+    private static final int MIN_ROW = 1;
+    private static final int MAX_ROW = 9;
     public static final String ERROR_INVALID_X_COORDINATE = "[ERROR] x 좌표가 올바르지 않습니다.";
     public static final String ERROR_INVALID_Y_COORDINATE = "[ERROR] y 좌표가 올바르지 않습니다.";
 
     public Position {
-        if (column < 0 || column > 9) {
+        if (column < MIN_COLUMN || column > MAX_COLUMN) {
             throw new IllegalArgumentException(ERROR_INVALID_X_COORDINATE);
         }
 
-        if (row < 1 || row > 9) {
+        if (row < MIN_ROW || row > MAX_ROW) {
             throw new IllegalArgumentException(ERROR_INVALID_Y_COORDINATE);
         }
     }
