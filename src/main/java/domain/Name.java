348b416d20dfd66b5528016461015a30eedfbd12
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-25 13:15:51 +0900

    feat(Name): 이름 표현 VO 추가

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Name {
+
+    public final String value;
+
+    public Name(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
