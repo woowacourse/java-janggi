f879c2fb66dc213184deaaee8182845dbb5cd2e8
jyt6640 <jyt6640@gmail.com>
2026-03-25 11:44:16 +0900

    test: 구현체에서 턴을 넘기능 기능 테스트 추가
    - 한나라에서 초나라로 턴을 넘기는 기능
    - 초나라에서 한나라로 턴을 넘기는 기능

--- src/test/java/janggi/domain/HanTurnTest.java
+++ src/test/java/janggi/domain/HanTurnTest.java
@@ -5,20 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ChoTurnTest {
+public class HanTurnTest {
 
     @Test
-    @DisplayName("초나라에서 한나라로 턴을 넘기는 기능")
+    @DisplayName("한나라에서 초나라로 턴을 넘기는 기능")
     void turn_change() {
         // given
         Point from = Point.of(1, 1);
         Point to = Point.of(2, 3);
 
         // when
-        GameStatus status = new ChoTurn();
+        GameStatus status = new HanTurn();
         GameStatus gameStatus = status.move(from, to);
 
         //then
-        assertInstanceOf(HanTurn.class, gameStatus);
+        assertInstanceOf(ChoTurn.class, gameStatus);
     }
 }
