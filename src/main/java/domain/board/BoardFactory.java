afdd160cc51aa5c2c4b723c17848877a06a38998
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-25 20:47:01 +0900

    fix(BoardFactory): 초 한 장기판배열에 맞게 생성 위치 수정

--- src/main/java/domain/board/BoardFactory.java
+++ src/main/java/domain/board/BoardFactory.java
@@ -37,9 +37,9 @@ public class BoardFactory {
             int x = xPositions.get(i);
 
             if (type == MA) {
-                board.put(new Position(x, 9), new Ma(Team.CHO));
+                board.put(new Position(x, 0), new Ma(Team.CHO));
             } else if (type == SANG) {
-                board.put(new Position(x, 9), new Sang(Team.CHO));
+                board.put(new Position(x, 0), new Sang(Team.CHO));
             }
         }
     }
@@ -53,9 +53,9 @@ public class BoardFactory {
             int x = xPositions.get(i);
 
             if (type == MA) {
-                board.put(new Position(x, 0), new Ma(Team.HAN));
+                board.put(new Position(x, 9), new Ma(Team.HAN));
             } else if (type == SANG) {
-                board.put(new Position(x, 0), new Sang(Team.HAN));
+                board.put(new Position(x, 9), new Sang(Team.HAN));
             }
         }
     }
@@ -69,42 +69,44 @@ public class BoardFactory {
             }
         }
 
-        board.put(new Position(0, 0), new Cha(Team.HAN));
-        board.put(new Position(1, 0), new Sang(Team.HAN));
-        board.put(new Position(2, 0), new Ma(Team.HAN));
-        board.put(new Position(3, 0), new Sa(Team.HAN));
+        // CHO 기물 (위쪽: y=0~3)
+        board.put(new Position(0, 0), new Cha(Team.CHO));
+        board.put(new Position(1, 0), new Sang(Team.CHO));
+        board.put(new Position(2, 0), new Ma(Team.CHO));
+        board.put(new Position(3, 0), new Sa(Team.CHO));
         board.put(new Position(4, 0), new None(Team.NULL));
-        board.put(new Position(5, 0), new Sa(Team.HAN));
-        board.put(new Position(6, 0), new Ma(Team.HAN));
-        board.put(new Position(7, 0), new Sang(Team.HAN));
-        board.put(new Position(8, 0), new Cha(Team.HAN));
+        board.put(new Position(5, 0), new Sa(Team.CHO));
+        board.put(new Position(6, 0), new Ma(Team.CHO));
+        board.put(new Position(7, 0), new Sang(Team.CHO));
+        board.put(new Position(8, 0), new Cha(Team.CHO));
 
-        board.put(new Position(4, 1), new Jang(Team.HAN));
+        board.put(new Position(4, 1), new Jang(Team.CHO));
 
-        board.put(new Position(1, 2), new Po(Team.HAN));
-        board.put(new Position(7, 2), new Po(Team.HAN));
+        board.put(new Position(1, 2), new Po(Team.CHO));
+        board.put(new Position(7, 2), new Po(Team.CHO));
 
         for (int x = 0; x <= 8; x += 2) {
-            board.put(new Position(x, 3), new Jol(Team.HAN));
+            board.put(new Position(x, 3), new Jol(Team.CHO));
         }
 
-        board.put(new Position(0, 9), new Cha(Team.CHO));
-        board.put(new Position(1, 9), new Sang(Team.CHO));
-        board.put(new Position(2, 9), new Ma(Team.CHO));
-        board.put(new Position(3, 9), new Sa(Team.CHO));
+        // HAN 기물 (아래쪽: y=6~9)
+        board.put(new Position(0, 9), new Cha(Team.HAN));
+        board.put(new Position(1, 9), new Sang(Team.HAN));
+        board.put(new Position(2, 9), new Ma(Team.HAN));
+        board.put(new Position(3, 9), new Sa(Team.HAN));
         board.put(new Position(4, 9), new None(Team.NULL));
-        board.put(new Position(5, 9), new Sa(Team.CHO));
-        board.put(new Position(6, 9), new Ma(Team.CHO));
-        board.put(new Position(7, 9), new Sang(Team.CHO));
-        board.put(new Position(8, 9), new Cha(Team.CHO));
+        board.put(new Position(5, 9), new Sa(Team.HAN));
+        board.put(new Position(6, 9), new Ma(Team.HAN));
+        board.put(new Position(7, 9), new Sang(Team.HAN));
+        board.put(new Position(8, 9), new Cha(Team.HAN));
 
-        board.put(new Position(4, 8), new Jang(Team.CHO));
+        board.put(new Position(4, 8), new Jang(Team.HAN));
 
-        board.put(new Position(1, 7), new Po(Team.CHO));
-        board.put(new Position(7, 7), new Po(Team.CHO));
+        board.put(new Position(1, 7), new Po(Team.HAN));
+        board.put(new Position(7, 7), new Po(Team.HAN));
 
         for (int x = 0; x <= 8; x += 2) {
-            board.put(new Position(x, 6), new Jol(Team.CHO));
+            board.put(new Position(x, 6), new Jol(Team.HAN));
         }
 
         return board;
