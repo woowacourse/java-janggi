de340ec3ce754ad202cc7aeb0c242d8d8bb4053d
zakie <realzzaekkii@gmail.com>
2026-03-25 21:03:00 +0900

    feat(elephantSetup): 상차림 선택 요구 프롬프트 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,8 @@
 package view;
 
+import domain.board.ElephantSetup;
+import java.util.List;
+
 public class OutputView {
 
     public void printEnterChoPlayerNamePrompt() {
@@ -10,11 +13,22 @@ public class OutputView {
         System.out.println("한나라 플레이어의 이름을 입력하세요(2~5자, 영어만 사용):");
     }
 
-    public void printEnterChoElephantSetup() {
-        System.out.println("초나라 플레이어가 사용할 상차림 번호를 입력하세요");
+    public void printChoiceChoElephantSetupPrompt() {
+        System.out.println("초나라 플레이어가 사용할 상차림 번호를 입력하세요 ");
+        printElephantSetups();
     }
 
-    public void printEnterHanElephantSetup() {
-        System.out.println("한나라 플레이어가 사용할 상차림 번호를 입력하세요");
+    public void printChoiceHanElephantSetupPrompt() {
+        System.out.println("한나라 플레이어가 사용할 상차림 번호를 입력하세요 ");
+        printElephantSetups();
+    }
+
+    private void printElephantSetups() {
+        StringBuilder promptBuilder = new StringBuilder();
+        List<String> descriptions = ElephantSetup.descriptions();
+        for (int i = 0; i < descriptions.size(); i++) {
+            promptBuilder.append(String.format("%d. %s ", i + 1, descriptions.get(i)));
+        }
+        System.out.println(promptBuilder);
     }
 }
