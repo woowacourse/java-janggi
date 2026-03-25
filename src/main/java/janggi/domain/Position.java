32589f35e1721e6bca06079ab37bbd9ce73a0351
gleaming9 <gleaming9@khu.ac.kr>
2026-03-25 20:54:58 +0900

    feat: Position 클래스에 거리 계산 및 방향 확인 메서드 추가

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -4,4 +4,19 @@ public record Position(int x, int y) {
     public Position move(Movement movement) {
         return new Position(x + movement.getDx(), y + movement.getDy());
     }
+
+    public int calculateDistance(Position position, boolean isVertical) {
+        if(isVertical){
+            return position.x - this.x;
+        }
+        return position.y - this.y;
+    }
+
+    public boolean isHorizon(Position position) {
+        return position.x == this.x;
+    }
+
+    public boolean isVertical(Position position) {
+        return position.y == this.y;
+    }
 }
