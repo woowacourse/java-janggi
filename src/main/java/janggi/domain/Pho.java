cf66156398db4b65aeecc860313a2bce61c514d5
jyt6640 <jyt6640@gmail.com>
2026-03-25 14:39:49 +0900

    feat: Pho 틀 구현

---
+++ src/main/java/janggi/domain/Pho.java
@@ -0,0 +1,26 @@
+package janggi.domain;
+
+import java.util.List;
+
+public class Pho implements Piece {
+
+    private final Team team;
+    public Pho(Team team) {
+        this.team = team;
+    }
+
+    @Override
+    public boolean canMove(List<Piece> route) {
+        return false;
+    }
+
+    @Override
+    public List<Point> getRoute(Point from, Point to) {
+        return List.of();
+    }
+
+    @Override
+    public boolean isSameTeam(Team team) {
+        return false;
+    }
+}
