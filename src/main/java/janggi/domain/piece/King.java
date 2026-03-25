8f63aaa1488cab23813b6df43948fc057ee89346
haeyoon1 <hynam1220@naver.com>
2026-03-25 15:57:35 +0900

    feat: 궁 움직임 검증 메서드 작성

--- src/main/java/janggi/domain/piece/King.java
+++ src/main/java/janggi/domain/piece/King.java
@@ -1,10 +1,24 @@
 package janggi.domain.piece;
 
+import janggi.domain.Position;
 import janggi.domain.Team;
 
 public class King extends Piece {
 
     public King(Team team) {
         super(team);
+    }
+
+    @Override
+    public void validateMove(Position from, Position to) {
+        if (moveStrategy(from, to)) {
+            return;
+        }
+        throw new IllegalArgumentException("해당 위치로 궁이 이동할 수 없습니다.");
+    }
+
+    private boolean moveStrategy(Position from, Position to) {
+        return (Math.abs(from.getX() - to.getX()) == 0 && Math.abs(from.getY() - to.getY()) == 1) ||
+            (Math.abs(from.getX() - to.getX()) == 1 && Math.abs(from.getY() - to.getY()) == 0);
     }
 }
