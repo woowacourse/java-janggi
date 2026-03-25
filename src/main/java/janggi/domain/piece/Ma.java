67d89f7b8402c434845c1d896d2facf56c0e67a8
haeyoon1 <hynam1220@naver.com>
2026-03-25 20:12:19 +0900

    feat: 마 이동 규칙 메서드 작성

--- src/main/java/janggi/domain/piece/Ma.java
+++ src/main/java/janggi/domain/piece/Ma.java
@@ -2,6 +2,7 @@ package janggi.domain.piece;
 
 import janggi.domain.Position;
 import janggi.domain.Team;
+import java.util.List;
 
 public class Ma extends Piece {
 
@@ -11,6 +12,19 @@ public class Ma extends Piece {
 
     @Override
     public void validateMove(Position from, Position to) {
+        if (moveStrategy(from, to)) {
+            return;
+        }
+        throw new IllegalArgumentException("해당 위치로 마가 이동할 수 없습니다.");
+    }
 
+    @Override
+    public List<Position> getRoutes(Position from, Position to) {
+        return List.of();
+    }
+
+    private boolean moveStrategy(Position from, Position to) {
+        return (Math.abs(from.x() - to.x()) == 1 && Math.abs(from.y() - to.y()) == 2) ||
+            (Math.abs(from.x() - to.x()) == 2 && Math.abs(from.y() - to.y()) == 1);
     }
 }
