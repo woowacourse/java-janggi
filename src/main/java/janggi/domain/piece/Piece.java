b3763869a6b2c270bdc2f56c969592fb7b832510
haeyoon1 <hynam1220@naver.com>
2026-03-25 16:54:24 +0900

    feat: Piece 이동 검증 메서드 작성 - 움직임 규칙에 따른 검증 메서드 - 도착지에 같은 팀 말 존재 여부 확인 메서드 - 이동 경로 사이에 다른 말 존재 여부 확인 메서드

--- src/main/java/janggi/domain/piece/Piece.java
+++ src/main/java/janggi/domain/piece/Piece.java
@@ -1,7 +1,9 @@
 package janggi.domain.piece;
 
+import janggi.domain.Position;
 import janggi.domain.Space;
 import janggi.domain.Team;
+import java.util.List;
 
 public abstract class Piece implements Space {
 
@@ -9,6 +11,27 @@ public abstract class Piece implements Space {
 
     public Piece(Team team) {
         this.team = team;
+    }
+
+    public abstract void validateMove(Position from, Position to);
+
+    public void validateArrival(Space space) {
+        if (space.isBlank()) {
+            return;
+        }
+
+        Piece piece = (Piece) space;
+        if (!team.equals(piece.team)) {
+            return;
+        }
+
+        throw new IllegalArgumentException("이동하려는 위치에 같은 팀의 말이 존재합니다.");
+    }
+
+    public void validateRoutes(List<Piece> pieces){
+        if (!pieces.isEmpty()){
+            throw new IllegalArgumentException("이동 경로 사이에 다른 말이 있으면 안됩니다.");
+        }
     }
 
     @Override
