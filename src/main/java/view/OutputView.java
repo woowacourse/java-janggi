a751e70614a748484c9dadd362247d654488d4f2
전원준 <jwjjwj1960@naver.com>
2026-03-25 19:57:46 +0900

    feat: 장기판 포맷 수정
    - 매직넘버 처리 추가 필요
    - 조건문 분기 처리 필요

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,8 +6,22 @@ import model.Position;
 import model.pieces.Piece;
 
 public class OutputView {
+    private static final String LEFT_CROSS_LINE = "├";
+    private static final String UP_CROSS_LINE = "┬";
+    private static final String RIGHT_CROSS_LINE = "┤";
+    private static final String DOWN_CROSS_LINE = "┴";
+    private static final String CENTER_CROSS_LINE = "┼";
+    private static final String LEFT_UP_LINE = "┌";
+    private static final String LEFT_DOWN_LINE = "└";
+    private static final String RIGHT_UP_LINE = "┐";
+    private static final String RIGHT_DOWN_LINE = "┘";
+
     public static void printArrangeCountry(Country country) {
         System.out.printf("%s나라의 진영을 선택해주세요.%n", country.color() + country.title() + Country.RESET);
+    }
+
+    public static void printLine() {
+        System.out.println();
     }
 
     public static void printBoard(Board board) {
@@ -21,20 +35,58 @@ public class OutputView {
         for (int col = 1; col <= 9; col++) {
             Position position = Position.of(row, col);
             Piece piece = board.findPiece(position);
+            if (piece == null) {
+                printBoardLine(row, col);
+                continue;
+            }
             printMark(piece);
         }
     }
 
     private static void printMark(Piece piece) {
-        if (piece == null) {
-            System.out.print(".");
-        }
-        if (piece != null) {
-            System.out.print(piece.mark(piece.country()));
-        }
+        System.out.printf(String.format("%s ", piece.mark(piece.country())));
     }
 
-    public static void printLine() {
-        System.out.println();
+    private static void printBoardLine(int row, int col) {
+        if (col == 1) {
+            System.out.printf(String.format("%-2s",leftLine(row)));
+            return;
+        }
+        if (col == 9) {
+            System.out.printf(String.format("%-2s",rightLine(row)));
+            return;
+        }
+
+        System.out.printf(String.format("%-2s", crossLine(row)));
+    }
+
+    private static String leftLine(int row) {
+        if (row == 1) {
+            return LEFT_UP_LINE;
+        }
+        if (row == 10) {
+            return LEFT_DOWN_LINE;
+        }
+        return LEFT_CROSS_LINE;
+    }
+
+    private static String rightLine(int row) {
+        if (row == 1) {
+            return RIGHT_UP_LINE;
+        }
+        if (row == 10) {
+            return RIGHT_DOWN_LINE;
+        }
+        return RIGHT_CROSS_LINE;
+    }
+
+    private static String crossLine(int row) {
+        if (row == 1) {
+            return UP_CROSS_LINE;
+        }
+        if (row == 10) {
+            return DOWN_CROSS_LINE;
+        }
+        return CENTER_CROSS_LINE;
     }
 }
--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -1,22 +1,27 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import model.pieces.Cannon;
 import model.pieces.Piece;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import strategy.InnerElephant;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
 class BoardTest {
-    @Test
-    void 보드에_맞는_위치에_잘_들어갔는지_테스트(){
-        Board board = new Board();
+
+    private static Board board;
+    @BeforeAll()
+    static void setUp() {
+        board = new Board();
         Army cho = new Army(new InnerElephant());
         Army han = new Army(new InnerElephant());
         cho.deployTo(board, Country.CHO);
         han.deployTo(board, Country.HAN);
+    }
 
+    @Test
+    void 보드에_맞는_위치에_잘_들어갔는지_테스트(){
         Piece expected = new Cannon(Country.CHO);
         Position position = Position.of(8, 2);
         assertThat(board.isPieceAt(position, expected)).isTrue();
