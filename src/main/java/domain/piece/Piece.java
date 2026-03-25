aab2636a8117b25627e7a55da1554bb26d85db8a
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-25 13:58:13 +0900

    feat(Piece): Piece 공통 메서드들 구현

---
+++ src/main/java/domain/piece/Piece.java
@@ -0,0 +1,27 @@
+package domain.piece;
+
+import domain.player.Team;
+import domain.position.Position;
+import java.util.List;
+
+public abstract class Piece {
+
+    private final Team team;
+
+    public Piece(Team team) {
+        this.team = team;
+    }
+
+    //RawMove를 계산해서 dest가 있는지 없는지 판단.
+    public boolean canMove(Position src, Position dest) {
+        return getRawPositions(src).contains(dest);
+    }
+
+    //이동규칙에 따라 포지션 계산
+    protected abstract List<Position> getRawPositions(Position src);
+
+    //경로 계산.
+    protected abstract List<Position> getPaths(Position src, Position dest);
+
+
+}
