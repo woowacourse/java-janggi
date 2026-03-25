342f38184d0d04468966df95d7418f9ab7ffeb6b
snow___shower <wonsb25@gmail.com>
2026-03-25 20:33:09 +0900

    feat: 상차림 객체 작성 및 순서 반환 로직 추가
    Signed-off-by: snow___shower <wonsb25@gmail.com>

---
+++ src/main/java/domain/SetUp.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static domain.PieceType.HORSE;
+import static domain.PieceType.ELEPHANT;
+
+public enum SetUp {
+    LEFT_ELEPHANT(1, List.of(ELEPHANT, HORSE, ELEPHANT, HORSE)),
+    RIGHT_ELEPHANT(2, List.of(HORSE, ELEPHANT, HORSE, ELEPHANT)),
+    INNER_ELEPHANT(3, List.of(HORSE, ELEPHANT, ELEPHANT, HORSE)),
+    OUTER_ELEPHANT(4, List.of(ELEPHANT, HORSE, HORSE, ELEPHANT));
+
+    private final int setUpNumber;
+    private final List<PieceType> placeOrder;
+
+    SetUp(int setUpNumber, List<PieceType> types) {
+        this.setUpNumber = setUpNumber;
+        this.placeOrder = List.copyOf(types);
+    }
+
+    public static List<PieceType> from(int input) {
+        return Arrays.stream(values())
+                .filter(setUp -> setUp.setUpNumber == input)
+                .map(setUp -> setUp.placeOrder)
+                .findFirst()
+                .orElseThrow();
+    }
+}
