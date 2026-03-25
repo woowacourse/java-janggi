81beec78ae65053d02c5a2837e92841a2c5e0bb9
KIM SEON WOO <sunwoo005@naver.com>
2026-03-25 17:00:56 +0900

    feat : create Column and Row feat

---
+++ src/main/java/Column.java
@@ -0,0 +1,19 @@
+public class Column {
+    private static final int COLUMN_MAX_SIZE = 8;
+    private final int column;
+
+    public Column(int number) {
+        validateColumn(number);
+        this.column = number;
+    }
+
+    public int getValue() {
+        return this.column;
+    }
+
+    private void validateColumn(int number) {
+        if (number > COLUMN_MAX_SIZE) {
+            throw new IllegalArgumentException("최대 값은 10입니다.");
+        }
+    }
+}
---
+++ src/main/java/Row.java
@@ -0,0 +1,19 @@
+public class Row {
+    private static final int ROW_MAX_SIZE = 8;
+    private final int row;
+
+    public Row(int number) {
+        validateRow(number);
+        this.row = number;
+    }
+
+    private void validateRow(int number) {
+        if (number > ROW_MAX_SIZE) {
+            throw new IllegalArgumentException("최대 값은 9입니다.");
+        }
+    }
+
+    public int getValue() {
+        return this.row;
+    }
+}
