974e9ae7e10874dd7cfde0edb2f220979543edfb
Sumin <new.sumyang@gmail.com>
2026-03-25 16:33:21 +0900

    feat: 위치 객체 추가

---
+++ src/main/java/janggi/domain/Position.java
@@ -0,0 +1,30 @@
+package janggi.domain;
+
+import java.util.Objects;
+
+public class Position {
+
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Position position)) {
+            return false;
+        }
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
