c8d91fdff2548dc34e9a84535d92e5f3fdfc727c
snow___shower <wonsb25@gmail.com>
2026-03-25 20:11:16 +0900

    fix: 행렬 대신 xy 좌표로 수정
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -1,20 +1,20 @@
 package domain;
 
-public record Position(int column, int row) {
+public record Position(int x, int y) {
 
-    private static final int MIN_COLUMN = 0;
-    private static final int MAX_COLUMN = 9;
-    private static final int MIN_ROW = 1;
-    private static final int MAX_ROW = 9;
+    private static final int MIN_X = 1;
+    private static final int MAX_X = 9;
+    private static final int MIN_Y = 1;
+    private static final int MAX_Y = 10;
     public static final String ERROR_INVALID_X_COORDINATE = "[ERROR] x 좌표가 올바르지 않습니다.";
     public static final String ERROR_INVALID_Y_COORDINATE = "[ERROR] y 좌표가 올바르지 않습니다.";
 
     public Position {
-        if (column < MIN_COLUMN || column > MAX_COLUMN) {
+        if (x < MIN_X || x > MAX_X) {
             throw new IllegalArgumentException(ERROR_INVALID_X_COORDINATE);
         }
 
-        if (row < MIN_ROW || row > MAX_ROW) {
+        if (y < MIN_Y || y > MAX_Y) {
             throw new IllegalArgumentException(ERROR_INVALID_Y_COORDINATE);
         }
     }
--- src/test/java/domain/PositionTest.java
+++ src/test/java/domain/PositionTest.java
@@ -10,69 +10,69 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class PositionTest {
 
     @Nested
-    class columnCoordinate {
+    class XCoordinate {
         @Test
-        @DisplayName("행 좌표가 0 미만일 경우 예외가 발생한다.")
-        void throwException_When_RowCoordinateLessThanZero() {
-            assertThatThrownBy(() -> new Position(-1, 1))
+        @DisplayName("X 좌표가 1 미만일 경우 예외가 발생한다.")
+        void throwException_When_XCoordinateLessThanZero() {
+            assertThatThrownBy(() -> new Position(0, 1))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
-        @DisplayName("행 좌표가 0인 경우 생성된 위치의 행 좌표는 0이다.")
-        void generate_MinRowCoordinate() {
-            Position position = new Position(0, 1);
+        @DisplayName("X 좌표가 1인 경우 생성된 위치의 X 좌표는 1이다.")
+        void generate_MinXCoordinate() {
+            Position position = new Position(1, 1);
 
-            Assertions.assertThat(position.column()).isEqualTo(0);
+            Assertions.assertThat(position.x()).isEqualTo(1);
         }
 
         @Test
-        @DisplayName("행 좌표가 9 초과인 경우 예외가 발생한다.")
-        void throwException_When_RowCoordinateMoreThanNine() {
+        @DisplayName("X 좌표가 9 초과인 경우 예외가 발생한다.")
+        void throwException_When_XCoordinateMoreThanNine() {
             assertThatThrownBy(() -> new Position(10, 1))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
-        @DisplayName("행 좌표가 9인 경우 생성된 위치의 행 좌표는 9이다.")
-        void generate_MaxRowCoordinate() {
+        @DisplayName("X 좌표가 9인 경우 생성된 위치의 X 좌표는 9이다.")
+        void generate_MaxXCoordinate() {
             Position position = new Position(9, 1);
 
-            Assertions.assertThat(position.column()).isEqualTo(9);
+            Assertions.assertThat(position.x()).isEqualTo(9);
         }
     }
 
     @Nested
-    class RowCoordinate {
+    class YCoordinate {
         @Test
-        @DisplayName("열 좌표가 1 미만일 경우 예외가 발생한다.")
-        void throwException_When_Row_CoordinateLessThanZero() {
+        @DisplayName("Y 좌표가 1 미만일 경우 예외가 발생한다.")
+        void throwException_When_Y_CoordinateLessThanZero() {
             assertThatThrownBy(() -> new Position(1, 0))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
-        @DisplayName("열 좌표가 1인 경우 생성된 위치의 열 좌표는 1이다.")
-        void generate_Min_Row_Coordinate() {
+        @DisplayName("Y 좌표가 1인 경우 생성된 위치의 Y 좌표는 1이다.")
+        void generate_Min_Y_Coordinate() {
             Position position = new Position(1, 1);
 
-            Assertions.assertThat(position.row()).isEqualTo(1);
+            Assertions.assertThat(position.y()).isEqualTo(1);
         }
 
 
         @Test
-        @DisplayName("열 좌표가 9 초과인 경우 예외가 발생한다.")
-        void throwException_When_Row_CoordinateMoreThanNine() {
-            assertThatThrownBy(() -> new Position(1, 10))
+        @DisplayName("Y 좌표가 10 초과인 경우 예외가 발생한다.")
+        void throwException_When_Y_CoordinateMoreThanNine() {
+            assertThatThrownBy(() -> new Position(1, 11))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
-        @DisplayName("열 좌표가 9인 경우 생성된 위치의 열 좌표는 9이다.")
-        void generate_Max_Row_Coordinate() {
-            Position position = new Position(1, 9);
+        @DisplayName("Y 좌표가 10인 경우 생성된 위치의 Y 좌표는 10이다.")
+        void generate_Max_Y_Coordinate() {
+            Position position = new Position(1, 10);
 
-            Assertions.assertThat(position.row()).isEqualTo(9);
+            Assertions.assertThat(position.y()).isEqualTo(10);
         }
     }
 }
