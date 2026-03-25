f13437d3701afc7c3e35ac4bf3b2dc8959da981b
Junha Park <poiu7825@pusan.ac.kr>
2026-03-25 15:11:56 +0900

    feat: 기물 타입에 따른 Piece 구현체 생성 로직 구현

---
+++ src/main/java/janggi/domain/piece/PieceFactory.java
@@ -0,0 +1,22 @@
+package janggi.domain.piece;
+
+import janggi.domain.Team;
+import java.util.Map;
+import java.util.function.Function;
+
+public final class PieceFactory {
+
+    private static final Map<PieceType, Function<Team, Piece>> FACTORY = Map.of(
+            PieceType.CHA, Cha::new,
+            PieceType.PHO, Pho::new,
+            PieceType.MA, Ma::new,
+            PieceType.SANG, Sang::new,
+            PieceType.SA, Sa::new,
+            PieceType.KING, King::new,
+            PieceType.BYEONG, Byeong::new
+    );
+
+    public static Piece createPiece(Team team, PieceType pieceType) {
+        return FACTORY.get(pieceType).apply(team);
+    }
+}
