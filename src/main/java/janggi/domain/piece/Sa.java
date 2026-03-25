1ec756ba5ebd06a9facd5a810fce6139ee9b27a1
gleaming9 <gleaming9@khu.ac.kr>
2026-03-25 15:35:45 +0900

    feat: 사 기물 이동 가능 여부 구현

---
+++ src/main/java/janggi/domain/piece/Sa.java
@@ -0,0 +1,49 @@
+package janggi.domain.piece;
+
+import janggi.domain.BoardInterface;
+import janggi.domain.Movement;
+import janggi.domain.Position;
+import janggi.domain.Side;
+import janggi.domain.strategy.DefaultMoveStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sa extends Started {
+    private final List<List<Movement>> MOVE_RANGE = List.of(
+            List.of(Movement.UP),
+            List.of(Movement.DOWN),
+            List.of(Movement.LEFT),
+            List.of(Movement.RIGHT)
+    );
+
+
+    public Sa(Side side) {
+        super(new DefaultMoveStrategy(), side);
+    }
+
+    @Override
+    public boolean isMovable(Position start, Position end, BoardInterface boardInterface) {
+        List<Position> path = findPath(start, end);
+        return moveStrategy.isMovable(path, side, boardInterface);
+    }
+
+    private List<Position> findPath(Position start, Position end) {
+        for(List<Movement> movements : MOVE_RANGE) {
+            List<Position> calculatedPath = calculatePath(start, movements);
+            if(calculatedPath.getLast().equals(end)) {
+                return calculatedPath;
+            }
+        }
+        throw new IllegalArgumentException("올바른 도착 지점이 아닙니다.");
+    }
+
+    private List<Position> calculatePath(Position start, List<Movement> path) {
+        List<Position> calculatedPath = new ArrayList<>(List.of(start));
+        for (Movement movement : path) {
+            Position step = calculatedPath.getLast().move(movement);
+            calculatedPath.add(step);
+        }
+        return calculatedPath;
+    }
+}
