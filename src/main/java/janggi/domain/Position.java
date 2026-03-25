0a5dc9eddf525ce704fe67be9110f621d171c957
Junha Park <poiu7825@pusan.ac.kr>
2026-03-25 17:51:00 +0900

    feat: Position 값 객체 생성

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -1,12 +1,5 @@
 package janggi.domain;
 
-public class Position {
+public record Position(int x, int y) {
 
-    private final int x;
-    private final int y;
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
 }
