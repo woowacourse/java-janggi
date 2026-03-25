64dfcb4e9ad38e11a600afa4a80b47fedc4b4ba9
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-25 19:37:40 +0900

    test: 보드 초기화 수정

--- src/test/java/janggi/domain/status/ChoTurnTest.java
+++ src/test/java/janggi/domain/status/ChoTurnTest.java
@@ -5,22 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import janggi.domain.Board;
 import janggi.domain.Point;
-import janggi.domain.piece.Piece;
 import janggi.dto.PositionInfo;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ChoTurnTest {
 
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        List<PositionInfo> info = new ArrayList<>();
+        info.add(PositionInfo.from(List.of("HAN", "Cha", "1", "1")));
+        info.add(PositionInfo.from(List.of("CHO", "Cha", "2", "3")));
+        board.init(info);
+    }
+
     @Test
     @DisplayName("초나라에서 한나라로 턴을 넘기는 기능")
     void turn_change() {
         // given
-        Board board = new Board();
-        Point from = Point.of(1, 1);
-        Point to = Point.of(2, 3);
+        Point from = Point.of(2, 3);
+        Point to = Point.of(2, 2);
 
         // when
         GameStatus status = new ChoTurn();
@@ -34,10 +44,6 @@ public class ChoTurnTest {
     @DisplayName("한나라의 기물을 움직일 시 예외 발생")
     void unavailable_move() {
         // given
-        Board board = new Board();
-        List<PositionInfo> info = new ArrayList<>();
-        info.add(PositionInfo.from(List.of("HAN", "Cha", "1", "1")));
-        board.init(info);
         Point from = Point.of(1, 1);
         Point to = Point.of(2, 3);
 
--- src/test/java/janggi/domain/status/HanTurnTest.java
+++ src/test/java/janggi/domain/status/HanTurnTest.java
@@ -9,16 +9,27 @@ import janggi.domain.piece.Piece;
 import janggi.dto.PositionInfo;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class HanTurnTest {
 
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        List<PositionInfo> info = new ArrayList<>();
+        info.add(PositionInfo.from(List.of("HAN", "Cha", "1", "1")));
+        info.add(PositionInfo.from(List.of("CHO", "Cha", "2", "3")));
+        board.init(info);
+    }
+
     @Test
     @DisplayName("한나라에서 초나라로 턴을 넘기는 기능")
     void turn_change() {
         // given
-        Board board = new Board();
         Point from = Point.of(1, 1);
         Point to = Point.of(2, 3);
 
