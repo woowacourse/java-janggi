389726661c35f2440aacce452bb78bb320c40916
gleaming9 <gleaming9@khu.ac.kr>
2026-03-25 20:53:23 +0900

    feat: 궁, 상, 마의 기물 경로 계산 로직 구현

--- src/main/java/janggi/domain/piece/Ma.java
+++ src/main/java/janggi/domain/piece/Ma.java
@@ -1,6 +1,7 @@
 package janggi.domain.piece;
 
 import janggi.domain.BoardInterface;
+import janggi.domain.Movement;
 import janggi.domain.Position;
 import janggi.domain.Side;
 import janggi.domain.strategy.DefaultMoveStrategy;
@@ -9,17 +10,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ma extends Started {
+    private final List<List<Movement>> MOVE_RANGE = List.of(
+            List.of(Movement.UP, Movement.UP_LEFT),
+            List.of(Movement.UP, Movement.UP_RIGHT),
+            List.of(Movement.RIGHT, Movement.UP_RIGHT),
+            List.of(Movement.RIGHT, Movement.DOWN_RIGHT),
+            List.of(Movement.DOWN, Movement.DOWN_RIGHT),
+            List.of(Movement.DOWN, Movement.DOWN_LEFT),
+            List.of(Movement.LEFT, Movement.UP_LEFT),
+            List.of(Movement.LEFT, Movement.DOWN_LEFT)
+    );
+
     public Ma(Side side) {
         super(new DefaultMoveStrategy(), side);
     }
 
     @Override
-    public List<Position> calculatePath(Position start, Position end){
-        return new ArrayList<>();
+    public List<Position> findRoute(Position start, Position end) {
+        for (List<Movement> movements : MOVE_RANGE) {
+            List<Position> calculatedPath = calculatePath(start, movements);
+            if (calculatedPath.getLast().equals(end)) {
+                return calculatedPath;
+            }
+        }
+        throw new IllegalArgumentException("올바른 도착 지점이 아닙니다.");
     }
 
     @Override
     public boolean isMovable(List<Position> path, BoardInterface boardInterface) {
-        return false;
+        return moveStrategy.isMovable(path, side, boardInterface);
+    }
+
+    private List<Position> calculatePath(Position start, List<Movement> path) {
+        List<Position> calculatedPath = new ArrayList<>(List.of(start));
+        for (Movement movement : path) {
+            Position step = calculatedPath.getLast().move(movement);
+            calculatedPath.add(step);
+        }
+        return calculatedPath;
     }
 }
