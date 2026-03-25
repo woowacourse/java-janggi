81fd76f87c6c19b06e9e7881180bb0884c3f26ae
jyt6640 <jyt6640@gmail.com>
2026-03-25 20:03:10 +0900

    feat: Jol 기능 구현
    - 초나라의 졸일 경우 아래로 내려가면 예외 발생
    - 한나라의 졸일 경우 위로 올라가면 예외 발생

---
+++ src/main/java/janggi/domain/piece/Jol.java
@@ -0,0 +1,52 @@
+package janggi.domain.piece;
+
+import static java.lang.Math.abs;
+
+import janggi.domain.Point;
+import janggi.domain.status.Team;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jol implements Piece {
+
+    private final Team team;
+    private final String name;
+
+    public Jol(Team team, String pieceName) {
+        this.team = team;
+        this.name = pieceName;
+    }
+
+    @Override
+    public boolean isSameTeam(Team team) {
+        return this.team.equals(team);
+    }
+
+    @Override
+    public List<Point> getRoute(Point from, Point to) {
+        int pathX = to.getX() - from.getX();
+        int pathY = to.getY() - from.getY();
+
+        if (team.equals(Team.CHO)) {
+            if ((pathY == 1 && pathX == 0) ||
+                    (abs(pathX) == 1 && pathY == 0)
+            ) {
+                return List.of(to);
+            }
+            throw new IllegalArgumentException();
+        }
+
+        if ((pathY == -1 && pathX == 0) ||
+                (abs(pathX) == 1 && pathY == 0)
+        ) {
+            return List.of(to);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    @Override
+    public boolean canMove(List<Piece> route) {
+        return false;
+    }
+}
+
