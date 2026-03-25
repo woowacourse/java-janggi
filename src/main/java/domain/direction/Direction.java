6b4b5992bbfdcd68a62bf2e22899dc0a7683f669
toctoce <toctoce@naver.com>
2026-03-25 13:33:54 +0900

    feat(Direction): 방향 enum 구현

---
+++ src/main/java/domain/direction/Direction.java
@@ -0,0 +1,29 @@
+package domain.direction;
+
+public enum Direction {
+
+    NORTH(0, 1),
+    SOUTH(0, -1),
+    EAST(1, 0),
+    WEST(-1, 0),
+    NORTH_EAST(1, 1),
+    NORTH_WEST(-1, 1),
+    SOUTH_EAST(1, -1),
+    SOUTH_WEST(-1, -1);
+
+    private final int offsetX;
+    private final int offsetY;
+
+    Direction(int offsetX, int offsetY) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+    }
+
+    public int getOffsetX() {
+        return offsetX;
+    }
+
+    public int getOffsetY() {
+        return offsetY;
+    }
+}
