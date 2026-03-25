9773a99391c41c306ee645f6a11f94fb5a0c5060
woohyeon <woohyeon6885@gmail.com>
2026-03-25 13:24:58 +0900

    feat: 경로 조회 기능 구현

---
+++ src/main/java/janggi/Path.java
@@ -0,0 +1,30 @@
+package janggi;
+
+import java.util.List;
+
+public class Path {
+
+    private final List<Position> path;
+
+    public Path(List<Position> path) {
+        validate(path);
+        this.path = path;
+    }
+
+    private void validate(List<Position> path) {
+        if (path.isEmpty()) {
+            throw new IllegalArgumentException("빈 경로입니다.");
+        }
+    }
+
+    public Position getDestination() {
+        return path.getLast();
+    }
+
+    public List<Position> getCourse() {
+        int startIdx = 0;
+        int toIdx = path.size() - 1;
+
+        return path.subList(startIdx, toIdx);
+    }
+}
---
+++ src/test/java/janggi/PathTest.java
@@ -0,0 +1,52 @@
+package janggi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PathTest {
+
+    @DisplayName("빈 경로는 예외가 발생한다.")
+    @Test
+    void emptyPath() {
+        assertThatThrownBy(() -> new Path(List.of()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 경로입니다.");
+    }
+
+    @DisplayName("가장 마지막에 있는 위치를 반환한다.")
+    @Test
+    void getDestination() {
+        //given
+        Path path = new Path(List.of(
+                new Position(Row.ZERO, Column.EIGHT),
+                new Position(Row.ONE, Column.EIGHT),
+                new Position(Row.ONE, Column.SEVEN)
+        ));
+
+        //when & then
+        assertThat(path.getDestination())
+                .isEqualTo(new Position(Row.ONE, Column.SEVEN));
+    }
+
+    @DisplayName("가장 마지막 위치를 제한 나머지 위치들을 반환한다.")
+    @Test
+    void getCourse() {
+        //given
+        Path path = new Path(List.of(
+                new Position(Row.ZERO, Column.EIGHT),
+                new Position(Row.ONE, Column.EIGHT),
+                new Position(Row.ONE, Column.SEVEN)
+        ));
+
+        //when & then
+        assertThat(path.getCourse())
+                .containsExactly(
+                        new Position(Row.ZERO, Column.EIGHT),
+                        new Position(Row.ONE, Column.EIGHT)
+                );
+    }
+}
