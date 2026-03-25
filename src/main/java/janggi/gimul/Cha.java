44043e7fe59d207e8b8ed575eb961115fd4cb75a
woohyeon <woohyeon6885@gmail.com>
2026-03-25 19:19:27 +0900

    feat: 마 이동 기능 구현

--- src/main/java/janggi/gimul/Cha.java
+++ src/main/java/janggi/gimul/Cha.java
@@ -18,10 +18,10 @@ public class Cha extends Gimul {
         }
 
         if (from.isSameRow(to)) {
-            return from.moveHorizontal(from.getColumnDistance(to));
+            return from.moveHorizontal(to.getColumnDistance(from));
         }
 
-        return from.moveVertical(from.getRowDistance(to));
+        return from.moveVertical(to.getRowDistance(from));
     }
 
     @Override
