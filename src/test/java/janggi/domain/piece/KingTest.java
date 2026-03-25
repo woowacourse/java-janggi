d45e5ca95a3bab72052dbbe78b5ef15f9bb59538
haeyoon1 <hynam1220@naver.com>
2026-03-25 15:57:35 +0900

    feat: 궁 움직임 검증 메서드 작성

--- src/test/java/janggi/domain/piece/KingTest.java
+++ src/test/java/janggi/domain/piece/KingTest.java
@@ -11,23 +11,23 @@ class KingTest {
 
     @Test
     void 궁_움직임_정상_처리_테스트() {
-        King king = new King(Team.CHO);
+        Piece piece = new King(Team.CHO);
 
         Position from = new Position(5, 1);
         Position to = new Position(6, 1);
 
-        assertDoesNotThrow(() -> king.validateMove(from, to));
+        assertDoesNotThrow(() -> piece.validateMove(from, to));
     }
 
     @Test
     void 궁_움직임_예외_처리_테스트() {
-        King king = new King(Team.CHO);
+        Piece piece = new King(Team.CHO);
 
         Position from = new Position(5, 1);
         Position to = new Position(8, 1);
 
         assertThatIllegalArgumentException()
-            .isThrownBy(() -> king.validateMove(from, to))
+            .isThrownBy(() -> piece.validateMove(from, to))
             .withMessage("해당 위치로 궁이 이동할 수 없습니다.");
     }
 
