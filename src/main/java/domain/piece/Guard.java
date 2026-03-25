f23c242da2265d6ce5d48279c8ca1998e5f88ad1
HyoYoonNam <dev.rudevico@gmail.com>
2026-03-25 14:40:28 +0900

    feat: 사의 초기 위치 구현

---
+++ src/main/java/domain/piece/Guard.java
@@ -0,0 +1,25 @@
+package domain.piece;
+
+import domain.board.Intersection;
+import domain.game.Side;
+import java.util.List;
+
+public class Guard extends Piece implements StaticPositioned {
+
+    private static final int FAR_FROM_BASE_ROW = 0;
+    private static final List<Integer> INITAL_FILES = List.of(4, 6);
+
+    public Guard(Side side) {
+        super(side);
+    }
+
+    @Override
+    public List<Intersection> initAt() {
+        int baseRow = side.getBaseRow();
+        int initialRow = side.getForwardedRow(baseRow, FAR_FROM_BASE_ROW);
+
+        return INITAL_FILES.stream()
+                .map(file -> new Intersection(initialRow, file))
+                .toList();
+    }
+}
