33330d2114b18f95e2763bef45738bbc5bdc3cbd
Junha Park <poiu7825@pusan.ac.kr>
2026-03-25 10:58:05 +0900

    feat: Board 객체 생성

---
+++ src/main/java/janggi/domain/Board.java
@@ -0,0 +1,21 @@
+package janggi.domain;
+
+import java.util.Map;
+
+public class Board {
+
+    private final Map<Position, Space> piecesInfo;
+
+    public Board() {
+        piecesInfo = initializeBoard();
+    }
+
+    private Map<Position, Space> initializeBoard() {
+        return null;
+    }
+
+    public int campSize(Team team) {
+
+        return 0;
+    }
+}
