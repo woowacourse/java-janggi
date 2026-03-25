f6381cac7987c00bb2544a3ba8f55dde2efdbeb0
minzzun99 <skg06123@naver.com>
2026-03-25 15:07:21 +0900

    feat: 진영 출력 및 턴 시작 메시지 출력 로직 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.Country;
+
 public class OutputView {
     private final OutputViewFormatter formatter;
 
@@ -7,14 +9,25 @@ public class OutputView {
         this.formatter = formatter;
     }
 
-    public void printStartMessage() {
+    public void printGameStartMessage() {
         System.out.println("우테코 장기 게임입니다.");
     }
 
     public void printEndMessage() {
         System.out.println("게임을 종료합니다.");
     }
+
     public void printErrorMessage(String message){
         System.out.println(message);
     }
+
+    public void printCountry(Country country) {
+        System.out.println(formatter.formatCountry(country.getName()));
+    }
+
+    public void printTurnStartMessage() {
+        System.out.println("\n초나라가 먼저 시작합니다.");
+    }
+
+
 }
--- src/main/java/view/OutputViewFormatter.java
+++ src/main/java/view/OutputViewFormatter.java
@@ -1,7 +1,11 @@
 package view;
 
 public class OutputViewFormatter {
-    public String printInvalidInput(String message) {
-        return String.format("[ERROR] 올바르지 않은 입력입니다. %s%n");
+    public String formatInvalidInput(String message) {
+        return String.format("[ERROR] 올바르지 않은 입력입니다. %s%n", message);
+    }
+
+    public String formatCountry(String country) {
+        return String.format("%n---%n%n차례 : %s", country);
     }
 }
