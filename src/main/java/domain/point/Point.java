f36482ffbe50e21121745e6f98dff0922bd907ca
koreaioi <koreaioi@gachon.ac.kr>
2026-03-25 15:08:45 +0900

    refactor(domain): Point 패키지 이동

--- src/main/java/domain/point/Point.java
+++ src/main/java/domain/point/Point.java
@@ -1,4 +1,4 @@
-package domain;
+package domain.point;
 
 import java.util.Objects;
 
--- src/test/java/domain/PointTest.java
+++ src/test/java/domain/PointTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import domain.point.Point;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
