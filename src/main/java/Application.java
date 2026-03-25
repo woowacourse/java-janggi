5e79c984e6629f5f050528fa8b99a3396398ee7f
toctoce <toctoce@naver.com>
2026-03-25 20:27:07 +0900

    feat(Application): 프로그램 어플리케이션 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import domain.GameManager;
+
+public class Application {
+    public static void main(String[] args) {
+        GameManager gameManager = new GameManager();
+        gameManager.run();
+    }
+}
