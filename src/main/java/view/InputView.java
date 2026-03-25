c96ea59beba3c8a92ab6bec0ef589f59bd466a23
toctoce <toctoce@naver.com>
2026-03-25 13:06:51 +0900

    fix(InputView) : 입력 뷰 수정

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,49 @@
+package view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public String askChoPlayerName() {
+        System.out.println("선공 닉네임을 입력하세요.");
+        return sc.nextLine();
+    }
+
+    public String askHanPlayerName() {
+        System.out.println("후공 닉네임을 입력하세요.");
+        return sc.nextLine();
+    }
+
+    public int ascChoPositionInput(String input) {
+        System.out.println("선공 배치 선택 (1 - 상마상마, 2 - 마상마상, 3 - 마상상마, 4 - 상마마상)");
+        try {
+            return Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+    public int askHanPositionInput(String input) {
+        System.out.println("후공 배치 선택 (1 - 상마상마, 2 - 마상마상, 3 - 마상상마, 4 - 상마마상)");
+        try {
+            return Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+    public List<Integer> printPieceSelectInput() {
+        System.out.println("기물 선택");
+        try {
+            List<String> splitString = List.of(sc.nextLine().split(" "));
+            return splitString.stream()
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자 두 개를 공백으로 구분하여 입력하세요.");
+        }
+    }
+}
