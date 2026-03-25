cdb58eda01fe8c7c080e1299820f8e0fb0636329
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-25 21:30:06 +0900

    refactor: 포 기능 구현을 위한 타입 메서드 생성

--- src/main/java/janggi/domain/piece/Piece.java
+++ src/main/java/janggi/domain/piece/Piece.java
@@ -8,4 +8,5 @@ public interface Piece {
     boolean isSameTeam(Team team);
     List<Point> getRoute(Point from, Point to);
     boolean canMove(List<Piece> route);
+    boolean isSameType(PieceType type);
 }
--- src/main/java/janggi/domain/piece/PieceFactory.java
+++ src/main/java/janggi/domain/piece/PieceFactory.java
@@ -5,17 +5,27 @@ import janggi.domain.status.Team;
 public class PieceFactory {
 
     public static Piece of (Team team, String pieceName) {
-        if (pieceName.equals("Pho")) {
-            return new Pho(team, pieceName);
+        PieceType pieceType = PieceType.valueOf(pieceName);
+        if (pieceType.equals(PieceType.PHO)) {
+            return new Pho(team);
         }
-        if (pieceName.equals("Ma")) {
-            return new Ma(team, pieceName);
+        if (pieceType.equals(PieceType.MA)) {
+            return new Ma(team);
         }
-        if (pieceName.equals("Sang")) {
+        if (pieceType.equals(PieceType.SANG)) {
             return new Sang(team, pieceName);
         }
-        if (pieceName.equals("Cha")) {
-            return new Cha(team, pieceName);
+        if (pieceType.equals(PieceType.CHA)) {
+            return new Cha(team);
+        }
+        if (pieceType.equals(PieceType.JOL)) {
+            return new Jol(team);
+        }
+        if (pieceType.equals(PieceType.SA)) {
+            return new Sa(team);
+        }
+        if (pieceType.equals(PieceType.JANG)) {
+            return new Jang(team);
         }
         return null;
     }
