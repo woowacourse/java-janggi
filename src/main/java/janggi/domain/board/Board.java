0d1b45eaa0cd09736f29419ce55c4f631429853e
kangrae <kangrae0501@gmail.com>
2026-03-25 15:30:15 +0900

    pair(밀란): 마상 패턴 적용 및 초기 배치 작업

--- src/main/java/janggi/domain/board/Board.java
+++ src/main/java/janggi/domain/board/Board.java
@@ -2,15 +2,14 @@ package janggi.domain.board;
 
 import janggi.domain.piece.Piece;
 import janggi.domain.position.Position;
-
 import java.util.Map;
 
 public class Board {
 
     private final Map<Position, Piece> board;
 
-    public Board(Map<Position, Piece> board) {
-        this.board = board;
+    public Board(BoardDesignPolicy policy) {
+        this.board = policy.dj();
     }
 
 }
---
+++ src/main/java/janggi/domain/board/HorseElephantPosition.java
@@ -0,0 +1,23 @@
+package janggi.domain.board;
+
+public enum HorseElephantPosition {
+
+    HEHE(2, 3, 7, 8),
+    HEEH(2, 3, 8, 7),
+    EHEH(3, 2, 8, 7),
+    EHHE(3, 2, 7, 8);
+
+    private final int firstHorseColumn;
+    private final int firstElephantColumn;
+    private final int secondHorseColumn;
+    private final int secondElephantColumn;
+
+    HorseElephantPosition(int firstHorseColumn, int firstElephantColumn,
+                          int secondHorseColumn, int secondElephantColumn) {
+        this.firstHorseColumn = firstHorseColumn;
+        this.firstElephantColumn = firstElephantColumn;
+        this.secondHorseColumn = secondHorseColumn;
+        this.secondElephantColumn = secondElephantColumn;
+    }
+    
+}
