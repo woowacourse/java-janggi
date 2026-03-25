712d8b1762c9f10fcdab33f13d6333efa46de725
haeseon lee <haeseon4816@gmail.com>
2026-03-25 20:37:01 +0900

    feat: 장기판과 기물 위치 초기화

--- src/main/java/domain/JanggiBoard.java
+++ src/main/java/domain/JanggiBoard.java
@@ -1,8 +1,6 @@
 package domain;
 
-
-import domain.piece.Blank;
-import domain.piece.Piece;
+import domain.piece.*;
 
 import java.util.Map;
 
@@ -15,6 +13,7 @@ public class JanggiBoard {
     public JanggiBoard(Map<Position, Piece> janggiBoard) {
         this.janggiBoard = janggiBoard;
         initializeBoard();
+        setupInitialPieces();
     }
 
     private void initializeBoard() {
@@ -22,6 +21,36 @@ public class JanggiBoard {
             for (int column = 0; column < BOARD_COLUMNS; column++) {
                 janggiBoard.put(new Position(row, column), new Blank());
             }
+        }
+    }
+
+    private void setupInitialPieces() {
+        setupTeamPieces(Team.HAN, 0, 1,2, 3); // 한나라 기물 배치 (0~3행 위주)
+        setupTeamPieces(Team.CHO, 9, 8,7,6); // 초나라 기물 배치 (9~6행 위주)
+    }
+
+    private void setupTeamPieces(Team team, int baseRow, int kingRow, int cannonRow, int pawnRow) {
+        // 예시: 차(車) 배치
+        janggiBoard.put(new Position(baseRow, 0), new Car(team));
+        janggiBoard.put(new Position(baseRow, 8), new Car(team));
+        // 마
+        janggiBoard.put(new Position(baseRow, 1), new Horse(team));
+        janggiBoard.put(new Position(baseRow, 6), new Horse(team));
+        // 상
+        janggiBoard.put(new Position(baseRow, 2), new Elephant(team));
+        janggiBoard.put(new Position(baseRow, 7), new Elephant(team));
+        // 사
+        janggiBoard.put(new Position(baseRow, 3), new Guard(team));
+        janggiBoard.put(new Position(baseRow, 5), new Guard(team));
+        // 궁
+        janggiBoard.put(new Position(kingRow, 4), new King(team));
+        // 예시: 포(包) 배치
+        janggiBoard.put(new Position(cannonRow, 1), new Cannon(team));
+        janggiBoard.put(new Position(cannonRow, 7), new Cannon(team));
+
+        // 예시: 졸/병 배치
+        for (int col = 0; col < 9; col += 2) {
+            janggiBoard.put(new Position(pawnRow, col), new Pawn(team));
         }
     }
 }
