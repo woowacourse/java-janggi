21bae9491a781da58acaed1ef2fecf63238a0c02
Junha Park <poiu7825@pusan.ac.kr>
2026-03-25 20:21:58 +0900

    test: 상 테스트 작성

---
+++ src/test/java/janggi/domain/piece/SangTest.java
@@ -0,0 +1,56 @@
+package janggi.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import janggi.domain.Position;
+import janggi.domain.Space;
+import janggi.domain.Team;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class SangTest {
+
+    @Test
+    void 상_움직임_정상_처리_테스트() {
+        Piece piece = new Sang(Team.CHO);
+
+        Position from = new Position(6, 0);
+        Position to = new Position(4, 3);
+
+        assertDoesNotThrow(() -> piece.validateMove(from, to));
+    }
+
+    @Test
+    void 상_움직임_예외_처리_테스트() {
+        Piece piece = new Sang(Team.CHO);
+
+        Position from = new Position(0, 0);
+        Position to = new Position(1, 1);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.validateMove(from, to))
+                .withMessage("해당 위치로 상이 이동할 수 없습니다.");
+    }
+
+    @Test
+    void 도착지에_같은_팀의_말이_있을_경우_예외_처리_테스트() {
+        Piece piece = new Sang(Team.CHO);
+        Space space = new Cha(Team.CHO);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.validateArrival(space))
+                .withMessage("이동하려는 위치에 같은 팀의 말이 존재합니다.");
+    }
+
+    @Test
+    void 이동_경로_사이에_말이_있을_경우_예외_처리_테스트() {
+        Piece piece = new Sang(Team.CHO);
+        List<Piece> pieces = List.of(new Cha(Team.CHO));
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.validateRoutes(pieces))
+                .withMessage("이동 경로 사이에 다른 말이 있으면 안됩니다.");
+    }
+}
