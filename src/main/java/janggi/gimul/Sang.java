666219fc6aa5daecdf176a5f9e4bcf35e646cf7d
dahxxn <dahyeonhan4529@gmail.com>
2026-03-25 19:30:24 +0900

    feat: 상 이동 기능 구현

---
+++ src/main/java/janggi/gimul/Sang.java
@@ -0,0 +1,89 @@
+package janggi.gimul;
+
+import janggi.Path;
+import janggi.Position;
+import janggi.Team;
+import java.util.List;
+
+public class Sang extends Gimul {
+
+    protected Sang(Team team) {
+        super(team);
+    }
+
+    @Override
+    public Path getLegalPath(Position from, Position to) {
+        int rowDistance = to.getRowDistance(from);
+        int columnDistance = to.getColumnDistance(from);
+
+        int absRowDistance = Math.abs(rowDistance);
+        int absColumnDistance = Math.abs(columnDistance);
+
+        if ((absRowDistance != 2 || absColumnDistance != 3)
+                && (absRowDistance != 3 || absColumnDistance != 2)) {
+            throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+        }
+
+        Path first = from.moveVertical(-1);
+        Path second = first.getDestination().moveNorthAndWest();
+
+        Path third = Path.concatenate(first, second);
+        Path four = third.getDestination().moveNorthAndWest();
+
+        if (rowDistance == -2 && columnDistance == 1) {
+            first = from.moveVertical(-1);
+            second = first.getDestination().moveNorthAndEast();
+            third = Path.concatenate(first, second);
+            four = third.getDestination().moveNorthAndEast();
+        }
+
+        if (rowDistance == -1 && columnDistance == 2) {
+            first = from.moveHorizontal(1);
+            second = first.getDestination().moveNorthAndEast();
+            third = Path.concatenate(first, second);
+            four = third.getDestination().moveNorthAndEast();
+        }
+
+        if (rowDistance == 1 && columnDistance == 2) {
+            first = from.moveHorizontal(1);
+            second = first.getDestination().moveSouthAndEast();
+            third = Path.concatenate(first, second);
+            four = third.getDestination().moveSouthAndEast();
+        }
+
+        if (rowDistance == 2 && columnDistance == 1) {
+            first = from.moveVertical(1);
+            second = first.getDestination().moveSouthAndEast();
+            third = Path.concatenate(first, second);
+            four = third.getDestination().moveSouthAndEast();
+        }
+
+        if (rowDistance == 2 && columnDistance == -1) {
+            first = from.moveVertical(1);
+            second = first.getDestination().moveSouthAndWest();
+            third = Path.concatenate(first, second);
+            four = third.getDestination().moveSouthAndWest();
+        }
+
+        if (rowDistance == 1 && columnDistance == -2) {
+            first = from.moveHorizontal(-1);
+            second = first.getDestination().moveSouthAndWest();
+            third = Path.concatenate(first, second);
+            four = third.getDestination().moveSouthAndWest();
+        }
+
+        if (rowDistance == -1 && columnDistance == -2) {
+            first = from.moveHorizontal(-1);
+            second = first.getDestination().moveNorthAndWest();
+            third = Path.concatenate(first, second);
+            four = third.getDestination().moveNorthAndWest();
+        }
+
+        return Path.concatenate(third, four);
+    }
+
+    @Override
+    public boolean canPassThrough(List<Gimul> gimuls) {
+        return false;
+    }
+}
---
+++ src/test/java/janggi/gimul/SangTest.java
@@ -0,0 +1,63 @@
+package janggi.gimul;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import janggi.Column;
+import janggi.Path;
+import janggi.Position;
+import janggi.Row;
+import janggi.Team;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SangTest {
+    @DisplayName("상하 또는 좌우로 한칸을 간 후에 같은 방향의 대각선으로 두칸 이동한다.")
+    @Test
+    void getLegalPath() {
+        //given
+        Position from = new Position(Row.SEVEN, Column.FIVE);
+        Position to = new Position(Row.FOUR, Column.THREE);
+        Sang sang = new Sang(Team.CHO);
+
+        //when
+        Path path = sang.getLegalPath(from, to);
+
+        //then
+        assertThat(path.getDestination())
+                .isEqualTo(new Position(Row.FOUR, Column.THREE));
+    }
+
+    @DisplayName("행과 열의 거리가 각각 (1,3) 혹은 (3,1)이 아니면 예외가 발생한다.")
+    @Test
+    void getLegalPath_invalid() {
+        //given
+        Position from = new Position(Row.SEVEN, Column.FIVE);
+        Position to = new Position(Row.FOUR, Column.SIX);
+        Sang sang = new Sang(Team.CHO);
+
+        //when & then
+        assertThatThrownBy(() -> sang.getLegalPath(from, to))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이동할 수 없는 위치입니다.");
+    }
+
+
+    @DisplayName("경로 상에 다른 기물이 존재하면 false를 반환한다.")
+    @Test
+    void canPassThrough() {
+        //given
+        List<Gimul> gimuls = List.of(
+                new Cha(Team.CHO),
+                new Cha(Team.HAN)
+        );
+        Sang sang = new Sang(Team.CHO);
+
+        //when & then
+        assertThat(sang.canPassThrough(gimuls))
+                .isFalse();
+    }
+
+
+}
