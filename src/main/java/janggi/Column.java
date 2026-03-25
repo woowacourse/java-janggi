c726fa284f20ace214dba7dc3d261bf5fd714b2f
dahxxn <dahyeonhan4529@gmail.com>
2026-03-25 15:30:06 +0900

    feat: 수평 방향 행 이동 구현

--- src/main/java/janggi/Column.java
+++ src/main/java/janggi/Column.java
@@ -1,5 +1,8 @@
 package janggi;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Column {
     OUT,
     ONE,
@@ -29,4 +32,27 @@ public enum Column {
         }
         return CACHE_VALUES[(this.ordinal() - 1)];
     }
+
+    public List<Column> to(Column other) {
+        if (this == OUT || other == OUT) {
+            throw new IllegalArgumentException("보드의 바깥 위치가 포함돼 있습니다.");
+        }
+
+        if (this.ordinal() > other.ordinal()) {
+            List<Column> result = Arrays.asList(Arrays.copyOfRange(
+                    CACHE_VALUES,
+                    other.ordinal(),
+                    this.ordinal() + 1
+            ));
+
+            return result.reversed();
+        }
+
+        return Arrays.asList(Arrays.copyOfRange(
+                CACHE_VALUES,
+                this.ordinal(),
+                other.ordinal() + 1)
+        );
+    }
+
 }
--- src/test/java/janggi/ColumnTest.java
+++ src/test/java/janggi/ColumnTest.java
@@ -1,7 +1,7 @@
 package janggi;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -54,5 +54,50 @@ class ColumnTest {
         Column column = Column.OUT;
         assertThat(column.previous())
                 .isEqualTo(Column.OUT);
+    }
+
+    @DisplayName("자신과 other 사이에 있는 column들을 반환한다.")
+    @Test
+    void to() {
+        //given
+        Column seven = Column.SEVEN;
+        Column nine = Column.NINE;
+
+        //when & then
+        assertThat(seven.to(nine))
+                .containsExactly(
+                        Column.SEVEN,
+                        Column.EIGHT,
+                        Column.NINE
+                );
+    }
+
+    @DisplayName("자신의 순서가 other보다 큰 경우에도 자신과 other 사이에 있는 column들을 반환한다.")
+    @Test
+    void to_smaller() {
+        //given
+        Column nine = Column.NINE;
+        Column seven = Column.SEVEN;
+
+        //when & then
+        assertThat(nine.to(seven))
+                .containsExactly(
+                        Column.NINE,
+                        Column.EIGHT,
+                        Column.SEVEN
+                );
+    }
+
+    @DisplayName("자신 또는 other이 out이면 예외가 발생한다.")
+    @Test
+    void to_out() {
+        //given
+        Column seven = Column.SEVEN;
+        Column out = Column.OUT;
+
+        //when & then
+        assertThatThrownBy(() -> seven.to(out))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("보드의 바깥 위치가 포함돼 있습니다.");
     }
 }
