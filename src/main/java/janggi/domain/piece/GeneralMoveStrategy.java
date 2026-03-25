bcf8c3e0e5ac6954db7f3d590d046ea1a1ab4332
kangrae <kangrae0501@gmail.com>
2026-03-25 15:29:52 +0900

    feat: 모든 기물의 이동 규칙 빈 객체 추가

---
+++ src/main/java/janggi/domain/piece/GeneralMoveStrategy.java
@@ -0,0 +1,19 @@
+package janggi.domain.piece;
+
+import janggi.domain.position.Position;
+import java.util.List;
+import java.util.Map;
+
+public class GeneralMoveStrategy implements MoveStrategy {
+
+    @Override
+    public List<Position> canMovePositions(Map<Position, Piece> board, Position from) {
+        return List.of();
+    }
+
+    @Override
+    public boolean canMove(Map<Position, Piece> board, Position from, Position to) {
+        return false;
+    }
+
+}
