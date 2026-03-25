9dab5565d6d25fc953bc374425a08cc4942966ee
Sumin <new.sumyang@gmail.com>
2026-03-25 16:23:26 +0900

    feat: Delta의 집합 도메인 추가

---
+++ src/main/java/janggi/domain/MovePath.java
@@ -0,0 +1,12 @@
+package janggi.domain;
+
+import java.util.List;
+
+public class MovePath {
+
+    private final List<Delta> path;
+
+    public MovePath(List<Delta> path) {
+        this.path = path;
+    }
+}
