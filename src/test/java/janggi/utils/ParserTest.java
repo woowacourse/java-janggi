455379e4f42c20189bf8e92866ffc3ec44b36f40
mvg01 <dkms625@naver.com>
2026-03-25 17:33:05 +0900

    test(parser): 정수 파싱 테스트 추가

---
+++ src/test/java/janggi/utils/ParserTest.java
@@ -0,0 +1,38 @@
+package janggi.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+
+    @Nested
+    @DisplayName("정수 파싱 테스트")
+    class ParseInteger {
+
+        @Test
+        @DisplayName("정상 테스트")
+        void success1() {
+            // given
+            String input = "10";
+            int expected = Integer.valueOf(input);
+
+            // when
+            int actual = Parser.parseInteger(input);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("정수가 아닌 값을 변환하는 경우 테스트")
+        void failure1() {
+            String input = "test";
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Parser.parseInteger(input));
+        }
+    }
+}
