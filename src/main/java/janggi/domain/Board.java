240d932f87423d371d0e7c80f318a20f58702641
Junha Park <poiu7825@pusan.ac.kr>
2026-03-25 15:14:24 +0900

    feat: 보드 초기화 로직 작성

--- src/main/java/janggi/domain/Board.java
+++ src/main/java/janggi/domain/Board.java
@@ -1,21 +1,27 @@
 package janggi.domain;
 
+import janggi.domain.strategy.InitializeStrategy;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Board {
 
     private final Map<Position, Space> piecesInfo;
 
-    public Board() {
-        piecesInfo = initializeBoard();
+    public Board(InitializeStrategy initializeStrategy) {
+        piecesInfo = generateBlankBoard();
+        initializeStrategy.basicSetting(piecesInfo);
     }
 
-    private Map<Position, Space> initializeBoard() {
-        return null;
-    }
+    private Map<Position, Space> generateBlankBoard() {
+        Map<Position, Space> blankBoard = new HashMap<>();
 
-    public int campSize(Team team) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 9; j++) {
+                blankBoard.put(new Position(j, i), new Blank());
+            }
+        }
 
-        return 0;
+        return blankBoard;
     }
 }
