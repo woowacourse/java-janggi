28b5b285266c6a6c05d3bca1fb8dfbaddec574bc
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-25 17:42:57 +0900

    feat(Config): 의존성 주입 세팅
    장기게임, 플레이어들, 플레이어, 장기판의 의존성을 한 곳에서 관리하도록 구현하였습니다.

---
+++ src/main/java/config/DiConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import domain.board.Board;
+import domain.janggigame.JanggiGame;
+import domain.piece.Side;
+import domain.player.Player;
+import domain.players.Players;
+
+public class DiConfig {
+
+    private final Board board = new Board();
+
+    public JanggiGame janggiGame() {
+        return new JanggiGame(players());
+    }
+
+    private Players players() {
+        return new Players(choPlayer(), hanPlayer());
+    }
+
+    private Player choPlayer() {
+        return new Player(Side.CHO, board);
+    }
+
+    private Player hanPlayer() {
+        return new Player(Side.HAN, board);
+    }
+}
