7fb51b5aa218c51a10cbfa58ef86d4fdb3cd8371
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-03-25 22:11:24 +0900

    test: 장기 게임 테스트 임시 수정

--- src/test/java/domain/JanggiGameTest.java
+++ src/test/java/domain/JanggiGameTest.java
@@ -8,6 +8,7 @@ class JanggiGameTest {
 
     @Test
     void 턴_변경_정상_테스트() {
+//        JanggiGame janggiGame = new JanggiGame(new Board(MaSangPosition.MA_SANG_MA_SANG, MaSangPosition.MA_SANG_MA_SANG));
         JanggiGame janggiGame = new JanggiGame(new Board());
         janggiGame.play();
 
