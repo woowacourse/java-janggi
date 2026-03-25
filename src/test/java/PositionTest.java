d1fa8949f55647afcc2a4c4bb62b496d1642aa85
Minjae Kang <rkdalswoals@gmail.com>
2026-03-25 14:59:59 +0900

    test(PositionTest): 테스트 작성

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,43 @@
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PositionTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    void x의_값은_0_이상_8_이하여야_한다(int value) {
+        int y = 0;
+        assertThatCode(() -> new Position(value, y))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    void y의_값은_0_이상_9_이하여야_한다(int value) {
+        int x = 0;
+        assertThatCode(() -> new Position(x, value))
+                .doesNotThrowAnyException();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, 9, 10, 100})
+    void x의_값이_0_이상_8_이하가_아니면_예외가_발생한다(int value) {
+        int y = 0;
+        assertThatThrownBy(() -> new Position(value, y))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, 10, 11, 100})
+    void y의_값이_0_이상_9_이하가_아니면_예외가_발생한다(int value) {
+        int x = 0;
+        assertThatThrownBy(() -> new Position(x, value))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+}
