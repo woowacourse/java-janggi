26ad054a82fbaaa3fa601a4f32ca3c9b4c6fdc5b
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-03-25 16:02:48 +0900

    test: Position 테스트 케이스 추가

---
+++ src/test/java/domain/PositionTest.java
@@ -0,0 +1,22 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    @Test
+    void 범위_벗어난_좌표_예외_테스트() {
+        assertThatThrownBy(() -> new Position(11, 9))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void 범위_안의_좌표_정상_테스트() {
+        assertThatCode(() -> new Position(10, 9))
+                .doesNotThrowAnyException();
+    }
+
+}
