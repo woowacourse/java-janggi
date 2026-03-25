02d30c2284a1661105c15c23034ac8921ada23e0
frombunny <rucy011121@gmail.com>
2026-03-25 13:34:49 +0900

    fix(InitBoard): 장기판 원점 좌표 수정
    - 원점을 좌하단으로 수정
    - 누락된 포 초기화 로직 추가

--- src/main/java/domain/board/BoardFactory.java
+++ src/main/java/domain/board/BoardFactory.java
@@ -11,6 +11,7 @@ public class BoardFactory {
     private static final List<Integer> PAWN_COLUMNS = List.of(0, 2, 4, 6, 8);
     private static final List<Integer> ROOK_COLUMNS = List.of(0, 8);
     private static final List<Integer> ADVISOR_COLUMNS = List.of(3, 5);
+    private static final List<Integer> CANNON_COLUMNS = List.of(1, 7);
 
     private BoardFactory() {
     }
@@ -20,23 +21,27 @@ public class BoardFactory {
 
         List<PieceType> initialSetting = initializeSetting.getInitialSetting();
         for (Integer pawnColumn : PAWN_COLUMNS) {
-            pieces.put(new Position(pawnColumn, 6), new Piece(PieceType.PAWN));
+            pieces.put(new Position(pawnColumn, 3), new Piece(PieceType.PAWN));
         }
 
         for (Integer rookColumn : ROOK_COLUMNS) {
-            pieces.put(new Position(rookColumn, 9), new Piece(PieceType.ROOK));
+            pieces.put(new Position(rookColumn, 0), new Piece(PieceType.ROOK));
         }
 
         for (Integer advisorColumn : ADVISOR_COLUMNS) {
-            pieces.put(new Position(advisorColumn, 9), new Piece(PieceType.ADVISOR));
+            pieces.put(new Position(advisorColumn, 0), new Piece(PieceType.ADVISOR));
         }
 
-        pieces.put(new Position(4, 8), new Piece(PieceType.KING));
+        for (Integer cannonColumn : CANNON_COLUMNS) {
+            pieces.put(new Position(cannonColumn, 2), new Piece(PieceType.CANNON));
+        }
 
-        pieces.put(new Position(1, 9), new Piece(initialSetting.get(0)));
-        pieces.put(new Position(2, 9), new Piece(initialSetting.get(1)));
-        pieces.put(new Position(6, 9), new Piece(initialSetting.get(2)));
-        pieces.put(new Position(7, 9), new Piece(initialSetting.get(3)));
+        pieces.put(new Position(4, 1), new Piece(PieceType.KING));
+
+        pieces.put(new Position(1, 0), new Piece(initialSetting.get(0)));
+        pieces.put(new Position(2, 0), new Piece(initialSetting.get(1)));
+        pieces.put(new Position(6, 0), new Piece(initialSetting.get(2)));
+        pieces.put(new Position(7, 0), new Piece(initialSetting.get(3)));
 
         return new Board(pieces);
     }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -5,6 +5,8 @@ import domain.board.BoardFactory;
 import domain.board.InitializeSetting;
 import domain.board.Position;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,24 +14,90 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 public class BoardTest {
-    @ParameterizedTest
-    @MethodSource("pawnProvider")
-    void 졸을_올바른_위치에_초기화한다 (Position position) {
-        Board board = BoardFactory.createBoard(InitializeSetting.RIGHT_ELEPHANT_SETTING);
-        Piece piece= board.getPiece(position);
-        Assertions.assertThat(piece.pieceType()).isEqualTo(PieceType.PAWN);
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = BoardFactory.createBoard(InitializeSetting.RIGHT_ELEPHANT_SETTING);
     }
 
-    static Stream<Arguments> pawnProvider() {
-        return Stream.of(
-                Arguments.of(new Position(0, 6)),
-                Arguments.of(new Position(2, 6)),
-                Arguments.of(new Position(4, 6)),
-                Arguments.of(new Position(6, 6)),
-                Arguments.of(new Position(8, 6))
-        );
-    }
+    @Nested
+    class 기물_위치_테스트 {
 
+        @ParameterizedTest
+        @MethodSource("pawnProvider")
+        void 졸을_올바른_위치에_초기화한다(Position position) {
+            Piece piece = board.getPiece(position);
+            Assertions.assertThat(piece.pieceType()).isEqualTo(PieceType.PAWN);
+        }
+
+        static Stream<Arguments> pawnProvider() {
+            return Stream.of(
+                    Arguments.of(new Position(0, 3)),
+                    Arguments.of(new Position(2, 3)),
+                    Arguments.of(new Position(4, 3)),
+                    Arguments.of(new Position(6, 3)),
+                    Arguments.of(new Position(8, 3))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("rookProvider")
+        void 차를_올바른_위치에_초기화한다(Position position) {
+            Piece piece = board.getPiece(position);
+            Assertions.assertThat(piece.pieceType()).isEqualTo(PieceType.ROOK);
+        }
+
+        static Stream<Arguments> rookProvider() {
+            return Stream.of(
+                    Arguments.of(new Position(0, 0)),
+                    Arguments.of(new Position(8, 0))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("advisorProvider")
+        void 사를_올바른_위치에_초기화한다(Position position) {
+            Piece piece = board.getPiece(position);
+            Assertions.assertThat(piece.pieceType()).isEqualTo(PieceType.ADVISOR);
+        }
+
+        static Stream<Arguments> advisorProvider() {
+            return Stream.of(
+                    Arguments.of(new Position(3, 0)),
+                    Arguments.of(new Position(5, 0))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("kingProvider")
+        void 궁을_올바른_위치에_초기화한다(Position position) {
+            Piece piece = board.getPiece(position);
+            Assertions.assertThat(piece.pieceType()).isEqualTo(PieceType.KING);
+        }
+
+        static Stream<Arguments> kingProvider() {
+            return Stream.of(
+                    Arguments.of(new Position(4, 1))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("cannonProvider")
+        void 포를_올바른_위치에_초기화한다(Position position) {
+            Piece piece = board.getPiece(position);
+            Assertions.assertThat(piece.pieceType()).isEqualTo(PieceType.CANNON);
+        }
+
+        static Stream<Arguments> cannonProvider() {
+            return Stream.of(
+                    Arguments.of(new Position(1, 2)),
+                    Arguments.of(new Position(7, 2))
+            );
+        }
+
+
+    }
 
 
 }
