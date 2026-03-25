3268300ba5225be6d2080f99fc286f0417591ff9
gleaming9 <gleaming9@khu.ac.kr>
2026-03-25 20:54:26 +0900

    refactor: calculatePath 메서드명을 findRoute로 변경

--- src/main/java/janggi/domain/piece/Pawn.java
+++ src/main/java/janggi/domain/piece/Pawn.java
@@ -1,6 +1,7 @@
 package janggi.domain.piece;
 
 import janggi.domain.BoardInterface;
+import janggi.domain.Movement;
 import janggi.domain.Position;
 import janggi.domain.Side;
 import janggi.domain.strategy.DefaultMoveStrategy;
@@ -9,17 +10,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Started {
-    public Pawn(Side side) {
+    private final List<List<Movement>> MOVE_RANGE;
+
+    public Pawn(Side side, List<List<Movement>> moveRange) {
         super(new DefaultMoveStrategy(), side);
+        MOVE_RANGE = moveRange;
+    }
+
+    public Pawn from(Side side) {
+        List<List<Movement>> moveRange = new ArrayList<>(List.of(List.of(Movement.LEFT), List.of(Movement.RIGHT)));
+        moveRange.add(calculateForwardMovement(side));
+        return new Pawn(side, moveRange);
+    }
+
+    private List<Movement> calculateForwardMovement(Side side){
+        if (side.equals(Side.CHO)) {
+            return List.of(Movement.UP);
+        }
+        return List.of(Movement.DOWN);
     }
 
     @Override
-    public List<Position> calculatePath(Position start, Position end){
-        return new ArrayList<>();
+    public List<Position> findRoute(Position start, Position end) {
+        for (List<Movement> movements : MOVE_RANGE) {
+            List<Position> calculatedPath = calculatePath(start, movements);
+            if (calculatedPath.getLast().equals(end)) {
+                return calculatedPath;
+            }
+        }
+        throw new IllegalArgumentException("올바른 도착 지점이 아닙니다.");
     }
 
     @Override
     public boolean isMovable(List<Position> path, BoardInterface boardInterface) {
-        return false;
+        return moveStrategy.isMovable(path, side, boardInterface);
+    }
+
+    private List<Position> calculatePath(Position start, List<Movement> path) {
+        List<Position> calculatedPath = new ArrayList<>(List.of(start));
+        for (Movement movement : path) {
+            Position step = calculatedPath.getLast().move(movement);
+            calculatedPath.add(step);
+        }
+        return calculatedPath;
     }
 }
