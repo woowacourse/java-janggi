711297dd632e0f336c4d6d0b1cbfc4ead6437172
ghkdtjfxkd <syt1128@gmail.com>
2026-03-25 13:52:57 +0900

    feat: 사 이동 가능 여부 확인 기능 추가

---
+++ src/main/java/domain/GuardMoveStrategy.java
@@ -0,0 +1,43 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuardMoveStrategy extends MoveStrategy {
+
+    private final static int[] DR = {0, 1, 0, -1};
+    private final static int[] DC = {1, 0, -1, 0};
+
+    private final List<Position> destinations;
+
+    private GuardMoveStrategy(Position position) {
+        super(position);
+        this.destinations = setupDestinations();
+    }
+
+    public static GuardMoveStrategy of(Position position) {
+        return new GuardMoveStrategy(position);
+    }
+
+    private List<Position> setupDestinations() {
+        List<Position> positions = new ArrayList<>();
+
+        for (int i = 0; i < DR.length; i++) {
+            int dr = position.row() + DR[i];
+            int dc = position.col() + DC[i];
+            positions.add(Position.of(dr, dc));
+        }
+
+        return positions;
+    }
+
+    @Override
+    public boolean isMoveAble(Position targetPosition) {
+        return destinations.contains(targetPosition);
+    }
+
+    @Override
+    public boolean isRouteBlockedBy(List<Position> piecePositions) {
+        return false;
+    }
+}
--- src/test/java/domain/GeneralMoveStrategyTest.java
+++ src/test/java/domain/GeneralMoveStrategyTest.java
@@ -1,21 +1,48 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class GeneralMoveStrategyTest {
 
-    @Test
-    @DisplayName("장은 상하좌우로 한 칸 이동할 수 있다.")
-    void general_move_test() {
-        Position position = Position.of(1, 4);
-        Position targetPosition  = Position.of(1,5);
-        GeneralMoveStrategy generalMoveStrategy = GeneralMoveStrategy.of(position);
+    @ParameterizedTest
+    @MethodSource("moveablePositions")
+    @DisplayName("장군은 현재 위치 기준 상하좌우 한 칸 이동할 수 있다.")
+    void general_move_test(Position guardPosition, Position expectedTarget) {
+        GeneralMoveStrategy moveStrategy = GeneralMoveStrategy.of(guardPosition);
 
-        boolean isMoveable = generalMoveStrategy.isMoveAble(targetPosition);
-
-        Assertions.assertThat(isMoveable).isTrue();
+        assertThat(moveStrategy.isMoveAble(expectedTarget)).isTrue();
     }
 
+    @ParameterizedTest
+    @MethodSource("nonMovablePositions")
+    @DisplayName("장군은 현재 위치 기준 상하좌우 한 칸을 벗어난 곳으로 이동할 수 없다.")
+    void general_move_test_negative(Position guardPosition, Position wrongTarget) {
+        GeneralMoveStrategy moveStrategy = GeneralMoveStrategy.of(guardPosition);
+
+        assertThat(moveStrategy.isMoveAble(wrongTarget)).isFalse();
+    }
+
+    private static Stream<Arguments> moveablePositions() {
+        return Stream.of(
+                Arguments.arguments(Position.of(1, 4), Position.of(1,5)),
+                Arguments.arguments(Position.of(1, 4), Position.of(1,3)),
+                Arguments.arguments(Position.of(1, 4), Position.of(2,4)),
+                Arguments.arguments(Position.of(1, 4), Position.of(0,4))
+        );
+    }
+
+    private static Stream<Arguments> nonMovablePositions() {
+        return Stream.of(
+                Arguments.arguments(Position.of(1, 4), Position.of(1,2)),
+                Arguments.arguments(Position.of(1, 4), Position.of(3,3)),
+                Arguments.arguments(Position.of(1, 4), Position.of(5,5)),
+                Arguments.arguments(Position.of(1, 4), Position.of(7,7))
+        );
+    }
 }
