df2af61937a7f8c28d14586564c4170033209398
toctoce <toctoce@naver.com>
2026-03-25 20:26:31 +0900

    feat(Formation): 인풋 번호 추가

--- src/main/java/domain/board/Formation.java
+++ src/main/java/domain/board/Formation.java
@@ -7,15 +7,26 @@ import domain.piece.PieceType;
 import java.util.List;
 
 public enum Formation {
-    MA_SANG_MA_SANG(List.of(MA, SANG, MA, SANG)),
-    MA_SANG_SANG_MA(List.of(MA, SANG, SANG, MA)),
-    SANG_MA_SANG_MA(List.of(SANG, MA, SANG, MA)),
-    SANG_MA_MA_SANG(List.of(SANG, MA, MA, SANG));
+    MA_SANG_MA_SANG(List.of(MA, SANG, MA, SANG), 1),
+    MA_SANG_SANG_MA(List.of(MA, SANG, SANG, MA), 2),
+    SANG_MA_SANG_MA(List.of(SANG, MA, SANG, MA), 3),
+    SANG_MA_MA_SANG(List.of(SANG, MA, MA, SANG), 4);
 
     private final List<PieceType> formation;
+    private final int inputNumber;
 
-    Formation(List<PieceType> formation) {
+    Formation(List<PieceType> formation, int inputNumber) {
         this.formation = formation;
+        this.inputNumber = inputNumber;
+    }
+
+    public static Formation from(int inputNumber) {
+        for (Formation formation : values()) {
+            if (formation.inputNumber == inputNumber) {
+                return formation;
+            }
+        }
+        throw new IllegalArgumentException("1에서 4까지 숫자만 입력해주세요");
     }
 
     public List<PieceType> getFormation() {
