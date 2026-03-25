451ac789d0dc19e180210a825c0ba861eef8d1d7
gleaming9 <gleaming9@khu.ac.kr>
2026-03-25 21:50:06 +0900

    fix: 상하 위치 변화량 오류 수정

--- src/main/java/janggi/domain/Movement.java
+++ src/main/java/janggi/domain/Movement.java
@@ -1,14 +1,14 @@
 package janggi.domain;
 
 public enum Movement {
-    UP(1, 0),
-    DOWN(-1, 0),
+    UP(-1, 0),
+    DOWN(1, 0),
     RIGHT(0, 1),
     LEFT(0, -1),
-    UP_RIGHT(1, 1),
-    UP_LEFT(1, -1),
-    DOWN_RIGHT(-1, 1),
-    DOWN_LEFT(-1, -1);
+    UP_RIGHT(-1, 1),
+    UP_LEFT(-1, -1),
+    DOWN_RIGHT(1, 1),
+    DOWN_LEFT(1, -1);
 
     private final int dx;
     private final int dy;
