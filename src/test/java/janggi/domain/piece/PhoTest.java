85604447688894d7038ff70f2549bf3f3025b7a7
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-25 21:30:20 +0900

    test: 포 이동 검증 추가

--- src/test/java/janggi/domain/piece/PhoTest.java
+++ src/test/java/janggi/domain/piece/PhoTest.java
@@ -3,9 +3,13 @@ package janggi.domain.piece;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import janggi.domain.Board;
 import janggi.domain.Point;
 import janggi.domain.status.Team;
+import janggi.dto.PositionInfo;
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,27 +17,41 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class PhoTest {
 
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        List<PositionInfo> info = new ArrayList<>();
+        info.add(PositionInfo.from(List.of("HAN", "PHO", "1", "1")));
+        info.add(PositionInfo.from(List.of("HAN", "CHA", "1", "2")));
+        info.add(PositionInfo.from(List.of("HAN", "CHA", "1", "3")));
+        info.add(PositionInfo.from(List.of("CHO", "PHO", "1", "5")));
+        info.add(PositionInfo.from(List.of("CHO", "PHO", "1", "6")));
+        board.init(info);
+    }
+
     @ParameterizedTest
-    @CsvSource(value = {"0:5", "8:5", "5:0", "5:9"}, delimiter = ':')
+    @CsvSource(value = {"0:5:4", "8:5:2", "5:0:4", "5:9:3"}, delimiter = ':')
     @DisplayName("포가 움직일 때, 경유지는 두 곳이다.")
-    void straight_back_route(int column, int row) {
+    void straight_back_route(int x, int y, int result) {
         // given
-        Piece pho = new Pho(Team.CHO, "Pho");
+        Piece pho = new Pho(Team.CHO);
         Point from = Point.of(5,5);
-        Point to = Point.of(column, row);
+        Point to = Point.of(x, y);
 
         // when
         List<Point> route = pho.getRoute(from, to);
 
         // then
-        assertThat(route.size()).isEqualTo(2);
+        assertThat(route.size()).isEqualTo(result);
     }
 
     @Test
     @DisplayName("이동할 수 없는 위치를 목적지로 할 경우 예외 발생")
     void destination_exception() {
         // given
-        Piece pho = new Pho(Team.CHO, "포");
+        Piece pho = new Pho(Team.CHO);
         Point from = Point.of(0, 0);
         Point to = Point.of(1, 1);
         // when & then
@@ -44,18 +62,39 @@ public class PhoTest {
     @Test
     @DisplayName("경유지에 뛰어넘을 기물이 1개만 있으면 움직일 수 있다.")
     void can_pho_move() {
+        Piece pho = new Pho(Team.CHO);
+        Point from = Point.of(1, 1);
+        Point to = Point.of(1, 3);
 
+        List<Point> route = pho.getRoute(from, to);
+        List<Piece> pieces = route.stream().map(point -> board.getPointAt(point)).toList();
+
+        assertThat(pho.canMove(pieces)).isTrue();
     }
 
     @Test
     @DisplayName("경유지에 뛰어넘을 기물이 2개 이상 있으면 움직일 수 없다.")
     void can_not_pho_move() {
+        Piece pho = new Pho(Team.CHO);
+        Point from = Point.of(1, 1);
+        Point to = Point.of(1, 4);
 
+        List<Point> route = pho.getRoute(from, to);
+        List<Piece> pieces = route.stream().map(point -> board.getPointAt(point)).toList();
+
+        assertThat(pho.canMove(pieces)).isFalse();
     }
 
     @Test
     @DisplayName("경유지에 뛰어넘을 기물이 포일 경우 움직일 수 없다.")
     void huddle_is_pho_can_not_move() {
+        Piece pho = new Pho(Team.CHO);
+        Point from = Point.of(1, 5);
+        Point to = Point.of(1, 7);
 
+        List<Point> route = pho.getRoute(from, to);
+        List<Piece> pieces = route.stream().map(point -> board.getPointAt(point)).toList();
+
+        assertThat(pho.canMove(pieces)).isFalse();
     }
 }
