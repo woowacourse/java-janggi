bc724ca82c915438b6031cd8c2fced93b326ec1a
Junha Park <poiu7825@pusan.ac.kr>
2026-03-25 14:12:14 +0900

    feat: 진영 이름 필드 추가 및 from 메서드 구현

--- src/main/java/janggi/domain/Team.java
+++ src/main/java/janggi/domain/Team.java
@@ -1,7 +1,25 @@
 package janggi.domain;
 
 public enum Team {
-    Han,
-    Cho,
+    HAN("한"),
+    CHO("초"),
     ;
+
+    private final String name;
+
+    Team(String name) {
+        this.name = name;
+    }
+
+    public static Team from(String name) {
+        if (name.equals(HAN.name)) {
+            return HAN;
+        }
+
+        if (name.equals(CHO.name)) {
+            return CHO;
+        }
+
+        throw new IllegalArgumentException("적절하지 않은 진영입니다.");
+    }
 }
