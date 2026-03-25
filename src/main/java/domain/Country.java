2b2ee65c74771e2b43db62691ac912a682d2288c
minzzun99 <skg06123@naver.com>
2026-03-25 15:08:39 +0900

    feat: 진영을 관리하는 Country Enum 추가

---
+++ src/main/java/domain/Country.java
@@ -0,0 +1,22 @@
+package domain;
+
+public enum Country {
+    CHO("초나라", "\u001B[32m"),
+    HAN("한나라", "\u001B[31m");
+
+    private final String name;
+    private final String color;
+
+    Country(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
