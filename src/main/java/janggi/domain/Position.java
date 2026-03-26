a2744b0fbcbf5c5ee351d0049919e1d47ef1cc95
Junha Park <poiu7825@pusan.ac.kr>
2026-03-26 11:30:41 +0900

    feat: 두 위치 차이 반환 메서드 구현 및 오브젝트 메서드 오버라이딩

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -1,5 +1,31 @@
 package janggi.domain;
 
-public record Position(int x, int y) {
+import java.util.Objects;
 
+public record Position(
+        int x,
+        int y
+) {
+
+    public int diffX(Position other) {
+        return x - other.x;
+    }
+
+    public int diffY(Position other) {
+        return y - other.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
