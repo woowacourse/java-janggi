eea264c46f8625b17edcadf3ba5f15981a30691e
Junha Park <poiu7825@pusan.ac.kr>
2026-03-26 11:28:39 +0900

    feat: 포 이동 규칙 메서드 작성

--- src/main/java/janggi/domain/piece/Pho.java
+++ src/main/java/janggi/domain/piece/Pho.java
@@ -1,6 +1,7 @@
 package janggi.domain.piece;
 
 import janggi.domain.Position;
+import janggi.domain.Space;
 import janggi.domain.Team;
 import java.util.List;
 
@@ -12,7 +13,36 @@ public class Pho extends Piece {
 
     @Override
     public void validateMove(Position from, Position to) {
+        if (moveStrategy(from, to)) {
+            return;
+        }
+        throw new IllegalArgumentException("해당 위치로 포가 이동할 수 없습니다.");
+    }
 
+    @Override
+    public void validateArrival(Space space) {
+        super.validateArrival(space);
+
+        Piece piece = (Piece) space;
+        if (isSameType(piece)) {
+            throw new IllegalArgumentException("이동하려는 위치에 상대팀의 포가 존재합니다.");
+        }
+    }
+
+    @Override
+    public void validateRoutes(List<Piece> pieces) {
+        if (pieces.size() != 1) {
+            throw new IllegalArgumentException("포는 이동 경로 사이에 포를 제외한 하나의 말이 있어야 합니다.");
+        }
+
+        if (isContainsPho(pieces)) {
+            throw new IllegalArgumentException("포는 이동 경로 사이에 포를 제외한 하나의 말이 있어야 합니다.");
+        }
+    }
+
+    private boolean isContainsPho(List<Piece> pieces) {
+        return pieces.stream()
+                .anyMatch(piece -> piece.isSameType(this));
     }
 
     @Override
