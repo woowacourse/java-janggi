6489109945fd663dd1fde5541d9fabdf6fd4cc30
smiinii <sm020320@naver.com>
2026-03-26 21:34:38 +0900

    feat: 사용자 입력 처리를 위한 InputView 클래스 추가

---
+++ src/main/java/janggi/view/InputView.java
@@ -0,0 +1,13 @@
+package janggi.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner sc;
+
+    public InputView() {
+        this.sc = new Scanner(System.in);
+    }
+
+
+}
