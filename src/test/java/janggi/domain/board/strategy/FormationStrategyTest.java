5d309768075a37326d1c5d2929da9efee42429dd
SooHyeon Lee <dalsu4594@naver.com>
2026-03-26 15:26:39 +0900

    test: 마상 배치 전략 테스트 추가
    - 마상마상, 마상상마, 상마마상, 상마상마 생성 테스트 추가

---
+++ src/test/java/janggi/domain/board/strategy/FormationStrategyTest.java
@@ -0,0 +1,83 @@
+package janggi.domain.board.strategy;
+
+import janggi.domain.Camp;
+import janggi.domain.Position;
+import janggi.domain.piece.Elephant;
+import janggi.domain.piece.Horse;
+import janggi.domain.piece.Piece;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FormationStrategyTest {
+
+    @Nested
+    class 상마상마_전략{
+
+        @ParameterizedTest
+        @EnumSource(Camp.class)
+        void 상마상마를_선택하면_정상_포지션을_갖는다(Camp camp){
+            FormationStrategy formationStrategy = new ElephantHorseElephantHorse();
+
+            Map<Position, Piece> pieces = formationStrategy.createPieces(camp);
+
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 1))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 2))).isInstanceOf(Horse.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 6))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 7))).isInstanceOf(Horse.class);
+        }
+    }
+
+
+    @Nested
+    class 상마마상_전략{
+        @ParameterizedTest
+        @EnumSource(Camp.class)
+        void 상마마상을_선택하면_정상_포지션을_갖는다(Camp camp){
+            FormationStrategy formationStrategy = new ElephantHorseHorseElephant();
+
+            Map<Position, Piece> pieces = formationStrategy.createPieces(camp);
+
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 1))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 2))).isInstanceOf(Horse.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 7))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 6))).isInstanceOf(Horse.class);
+        }
+    }
+
+    @Nested
+    class 마상상마_전략{
+        @ParameterizedTest
+        @EnumSource(Camp.class)
+        void 마상상마를_선택하면_정상_포지션을_갖는다(Camp camp){
+            FormationStrategy formationStrategy = new HorseElephantElephantHorse();
+
+            Map<Position, Piece> pieces = formationStrategy.createPieces(camp);
+
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 2))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 1))).isInstanceOf(Horse.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 6))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 7))).isInstanceOf(Horse.class);
+        }
+    }
+
+    @Nested
+    class 마상마상_전략{
+        @ParameterizedTest
+        @EnumSource(Camp.class)
+        void 마상마상을_선택하면_정상_포지션을_갖는다(Camp camp){
+            FormationStrategy formationStrategy = new HorseElephantHorseElephant();
+
+            Map<Position, Piece> pieces = formationStrategy.createPieces(camp);
+
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 2))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 1))).isInstanceOf(Horse.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 7))).isInstanceOf(Elephant.class);
+            assertThat(pieces.get(Position.of(camp.initRowPosition(), 6))).isInstanceOf(Horse.class);
+        }
+    }
+}
