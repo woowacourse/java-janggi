de86e2493ca199c66b980c19e6e62cb48615b124
구민서 <minseo12379@gmail.com>
2026-03-26 11:19:36 +0900

    test: Position 테스트 구현

--- src/test/java/janggi/domain/PositionTest.java
+++ src/test/java/janggi/domain/PositionTest.java
@@ -1,12 +1,8 @@
 package janggi.domain;
 
-import janggi.exception.BoardOutOfRangeException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class PositionTest {
@@ -14,11 +10,11 @@ class PositionTest {
     @Test
     void 장기판에_모든_좌표가_생성된다() {
         // when & then
-        for (int x = 0; x <= 8; x++) {
-            for(int y = 0; y <= 9; y++) {
+        for (int x = 1; x <= 9; x++) {
+            for(int y = 1; y <= 10; y++) {
 
-                Position position1 = Position.of(x, y);
-                Position position2 = Position.of(x, y);
+                Position position1 = Position.of(Row.of(x), Column.of(y));
+                Position position2 = Position.of(Row.of(x), Column.of(y));
 
                 assertThat(position1)
                         .as("x=%d, y=%d 에서 캐싱 실패", x, y)
@@ -26,16 +22,4 @@ class PositionTest {
             }
         }
     }
-
-    @ParameterizedTest
-    @CsvSource({
-            "-1, 0", "9, 0",
-            "0, -1", "0, 10",
-            "-1, -1", "9, 10"
-    })
-    void 장기판_범위를_벗어나면_예외가_발생한다(int x, int y) {
-        // when & then
-        assertThatThrownBy(() -> Position.of(x, y)).isInstanceOf(BoardOutOfRangeException.class);
-    }
-
 }
