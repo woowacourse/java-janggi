50596f0fc1e24fce1c2eecfd225e9827e6b547df
sinequanonn <dleck288@gmail.com>
2026-03-26 14:57:19 +0900

    feat: 화면 출력을 위한 콘솔 색상 구현 및 Position getter 추가

---
+++ src/main/java/janggi/constants/Color.java
@@ -0,0 +1,7 @@
+package janggi.constants;
+
+public class Color {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+}
