4f80103e9212f8fe9aa9fd9db955c67fdf805eaa
이동훈 <higgs95@naver.com>
2026-03-26 16:26:37 +0900

    feat: 위치 좌표 범위 검증 기능 구현

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -5,6 +5,7 @@ public class Position {
     private final int y;
 
     public Position(int x, int y) {
+        validatePositionRange(x, y);
         this.x = x;
         this.y = y;
     }
@@ -15,5 +16,22 @@ public class Position {
 
     public int getY() {
         return y;
+    }
+
+    private void validatePositionRange(int x, int y) {
+        validateXRange(x);
+        validateYRange(y);
+    }
+
+    private void validateXRange(int x) {
+        if (1 > x || x > 9) {
+            throw new IllegalArgumentException("X 좌표의 범위는 1~9 사이여야 합니다.");
+        }
+    }
+
+    private void validateYRange(int y) {
+        if (1 > y || y > 10) {
+            throw new IllegalArgumentException("Y 좌표의 범위는 1~10 사이여야 합니다.");
+        }
     }
 }
