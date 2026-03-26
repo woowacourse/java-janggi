8821e5f134bdf942e099d6d60cc9ab246500b013
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-26 21:08:58 +0900

    refactor: 매직 넘버 제거

--- src/main/java/janggi/domain/Point.java
+++ src/main/java/janggi/domain/Point.java
@@ -7,13 +7,15 @@ import java.util.List;
 public class Point {
 
     private static final List<List<Point>> CACHE;
+    private static final int COLUMN_RANGE = 10;
+    private static final int ROW_RANGE = 9;
 
     private final int x;
     private final int y;
 
     static {
         List<List<Point>> temp = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < COLUMN_RANGE; i++) {
             List<Point> row = new ArrayList<>();
             addX(row, i);
             temp.add(row);
@@ -27,6 +29,9 @@ public class Point {
     }
 
     public static Point of(int x, int y) {
+        if (x < 0 || x >= ROW_RANGE || y < 0 || y >= COLUMN_RANGE) {
+            throw new IllegalArgumentException("장기판 범위를 벗어났습니다.");
+        }
         return CACHE.get(y).get(x);
     }
 
@@ -43,7 +48,7 @@ public class Point {
     }
 
     private static void addX(List<Point> row, int y) {
-        for(int i = 0; i < 9; i++) {
+        for(int i = 0; i < ROW_RANGE; i++) {
             row.add(new Point(i, y));
         }
     }
