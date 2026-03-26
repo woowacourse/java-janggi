1f2b1f1ae553c698fb8188bcdb11acc63a948269
Junha Park <poiu7825@pusan.ac.kr>
2026-03-26 15:38:46 +0900

    test: 차 경로 반환 테스트 작성

--- src/test/java/janggi/domain/piece/ChaTest.java
+++ src/test/java/janggi/domain/piece/ChaTest.java
@@ -1,8 +1,10 @@
 package janggi.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import janggi.domain.Path;
 import janggi.domain.Position;
 import janggi.domain.Space;
 import janggi.domain.Team;
@@ -51,5 +53,22 @@ class ChaTest {
         assertThatIllegalArgumentException()
             .isThrownBy(() -> piece.validateRoutes(pieces))
             .withMessage("이동 경로 사이에 다른 말이 있으면 안됩니다.");
+    }
+
+    @Test
+    void 차_경로_반환_테스트() {
+        Piece piece = new Cha(Team.CHO);
+
+        Position from = new Position(0, 0);
+        Position to = new Position(5, 0);
+        Path actual = piece.getPath(from, to);
+
+        Path expect = new Path(List.of(
+                new Position(1, 0),
+                new Position(2, 0),
+                new Position(3, 0),
+                new Position(4, 0)
+        ));
+        assertThat(actual).isEqualTo(expect);
     }
 }
