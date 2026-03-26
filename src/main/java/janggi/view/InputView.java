0bc7c4c27053c584cb3f0be73737abf1c44c0836
이동훈 <higgs95@naver.com>
2026-03-26 16:04:57 +0900

    feat: 기물 이동 좌표 입력 기능 구현

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class InputView {
     private static final String CHOICE_REGEX = "[^1234]";
     private static final int CHOICE_MIN_SIZE = 2;
+    private static final int POSITION_SIZE = 2;
+
     private final Scanner sc;
 
     public InputView() {
@@ -29,6 +31,25 @@ public class InputView {
         return choices;
     }
 
+    public List<Integer> readEndPiecePosition() {
+        System.out.println("도착할 기물의 좌표를 입력하세요.(쉼표로 구분)");
+        return readPiecePosition();
+    }
+
+    public List<Integer> readStartPiecePosition() {
+        System.out.println("이동할 기물의 시작 좌표를 입력하세요.(쉼표로 구분)");
+        return readPiecePosition();
+    }
+
+    private List<Integer> readPiecePosition() {
+        List<Integer> position = Arrays.stream(sc.nextLine().split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+        validatePosition(position);
+        return position;
+    }
+
     private void validateChoiceNumber(String input) {
         if (input.matches(CHOICE_REGEX)) {
             throw new IllegalArgumentException("차림 선택은 1~4번까지 가능합니다.");
@@ -38,6 +59,12 @@ public class InputView {
     private void validateChoicesSize(List<Integer> choices) {
         if (choices.size() != CHOICE_MIN_SIZE) {
             throw new IllegalArgumentException("올바르지 않은 차림 선택입니다.");
+        }
+    }
+
+    private void validatePosition(List<Integer> choices) {
+        if (choices.size() != POSITION_SIZE) {
+            throw new IllegalArgumentException("기물의 좌표를 올바르게 입력하세요.");
         }
     }
 }
