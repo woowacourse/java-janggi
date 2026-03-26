d6b6956e31feb0d23453791f1998dadd8fb4aaf4
조상준 <juncho12011201@gmail.com>
2026-03-26 20:35:02 +0900

    feat: 상과 마의 상차림 모든 케이스 테스트 작성 및 구현
     Co-authored-by: sangjun121 <juncho12011201@gmail.com>
    
    Co-authored-by: Soojin6943 <sjhan0711@gmail.com>

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import strategy.InitializeStrategy;
+import strategy.InnerElephantFormationStrategy;
 import strategy.LeftElephantFormationStrategy;
+import strategy.OuterElephantFormationStrategy;
+import strategy.RightElephantFormationStrategy;
 
 class BoardTest {
     /**
@@ -26,37 +29,94 @@ class BoardTest {
     /**
      * 1. 초나라가 상마상마를 선택한 경우, 보드판에 올바르게 배치된다.
      */
-//    @Test
-//    void 초나라의_상마상마_상차림을_올바르게_배치한다() {
-//        // given
-//        InitializeStrategy strategy = new LeftElephantFormationStrategy();
-//
-//        // when
-//        Board board = new Board(strategy, strategy);
-//
-//        // then
-//        Position choFirstElephant = Position.from(1, 2);
-//        Position choSecondElephant = Position.from(1, 7);
-//        Position choFirstHorse = Position.from(1, 3);
-//        Position choSecondHorse = Position.from(1, 8);
-//
-//        assertThat(board.isExist(hanFirstElephant, new Piece(PieceType.ELEPHANT))).isEqualTo(true);
-//        assertThat(board.isExist(hanSecondElephant, new Piece(PieceType.ELEPHANT))).isEqualTo(true);
-//        assertThat(board.isExist(hanFirstHorse, new Piece(PieceType.HORSE))).isEqualTo(true);
-//        assertThat(board.isExist(hanSecondHorse, new Piece(PieceType.HORSE))).isEqualTo(true);
-//    }
+    @Test
+    void 초나라의_상마상마_상차림을_올바르게_배치한다() {
+        // given
+        InitializeStrategy strategy = new LeftElephantFormationStrategy();
+
+        // when
+        Board board = new Board(strategy, strategy);
+
+        // then
+        Position choFirstElephant = Position.from(10, 2);
+        Position choSecondElephant = Position.from(10, 7);
+        Position choFirstHorse = Position.from(10, 3);
+        Position choSecondHorse = Position.from(10, 8);
+
+        assertThat(board.isExistSameType(choFirstElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choFirstHorse, new Horse(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondHorse, new Horse(Team.CHO))).isEqualTo(true);
+    }
 
     /**
      * 2. 초나라가 상마마상을 선택한 경우, 보드판에 올바르게 배치된다.
      */
+    @Test
+    void 초나라의_상마마상_상차림을_올바르게_배치한다() {
+        // given
+        InitializeStrategy strategy = new OuterElephantFormationStrategy();
+
+        // when
+        Board board = new Board(strategy, strategy);
+
+        // then
+        Position choFirstElephant = Position.from(10, 2);
+        Position choSecondElephant = Position.from(10, 8);
+        Position choFirstHorse = Position.from(10, 3);
+        Position choSecondHorse = Position.from(10, 7);
+
+        assertThat(board.isExistSameType(choFirstElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choFirstHorse, new Horse(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondHorse, new Horse(Team.CHO))).isEqualTo(true);
+    }
 
     /**
      * 3. 초나라가 마상마상을 선택한 경우, 보드판에 올바르게 배치된다.
      */
+    @Test
+    void 초나라의_마상마상_상차림을_올바르게_배치한다() {
+        // given
+        InitializeStrategy strategy = new RightElephantFormationStrategy();
+
+        // when
+        Board board = new Board(strategy, strategy);
+
+        // then
+        Position choFirstElephant = Position.from(10, 3);
+        Position choSecondElephant = Position.from(10, 8);
+        Position choFirstHorse = Position.from(10, 2);
+        Position choSecondHorse = Position.from(10, 7);
+
+        assertThat(board.isExistSameType(choFirstElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choFirstHorse, new Horse(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondHorse, new Horse(Team.CHO))).isEqualTo(true);
+    }
 
     /**
      * 4. 초나라가 마상상마를 선택한 경우, 보드판에 올바르게 배치된다.
      */
+    @Test
+    void 초나라의_마상상마_상차림을_올바르게_배치한다() {
+        // given
+        InitializeStrategy strategy = new InnerElephantFormationStrategy();
+
+        // when
+        Board board = new Board(strategy, strategy);
+
+        // then
+        Position choFirstElephant = Position.from(10, 3);
+        Position choSecondElephant = Position.from(10, 7);
+        Position choFirstHorse = Position.from(10, 2);
+        Position choSecondHorse = Position.from(10, 8);
+
+        assertThat(board.isExistSameType(choFirstElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondElephant, new Elephant(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choFirstHorse, new Horse(Team.CHO))).isEqualTo(true);
+        assertThat(board.isExistSameType(choSecondHorse, new Horse(Team.CHO))).isEqualTo(true);
+    }
 
     /**
      * 5. 한나라가 상마상마를 선택한 경우, 보드판에 올바르게 배치된다.
@@ -84,12 +144,69 @@ class BoardTest {
     /**
      * 6. 한나라가 상마마상을 선택한 경우, 보드판에 올바르게 배치된다.
      */
+    @Test
+    void 한나라의_상마마상_상차림을_올바르게_배치한다() {
+        // given
+        InitializeStrategy strategy = new OuterElephantFormationStrategy();
+
+        // when
+        Board board = new Board(strategy, strategy);
+
+        // then
+        Position hanFirstElephant = Position.from(1, 2);
+        Position hanSecondElephant = Position.from(1, 8);
+        Position hanFirstHorse = Position.from(1, 3);
+        Position hanSecondHorse = Position.from(1, 7);
+
+        assertThat(board.isExistSameType(hanFirstElephant, new Elephant(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanSecondElephant, new Elephant(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanFirstHorse, new Horse(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanSecondHorse, new Horse(Team.HAN))).isEqualTo(true);
+    }
 
     /**
      * 7. 한나라가 마상마상을 선택한 경우, 보드판에 올바르게 배치된다.
      */
+    @Test
+    void 한나라의_마상마상_상차림을_올바르게_배치한다() {
+        // given
+        InitializeStrategy strategy = new RightElephantFormationStrategy();
+
+        // when
+        Board board = new Board(strategy, strategy);
+
+        // then
+        Position hanFirstElephant = Position.from(1, 2);
+        Position hanSecondElephant = Position.from(1, 7);
+        Position hanFirstHorse = Position.from(1, 3);
+        Position hanSecondHorse = Position.from(1, 8);
+
+        assertThat(board.isExistSameType(hanFirstElephant, new Elephant(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanSecondElephant, new Elephant(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanFirstHorse, new Horse(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanSecondHorse, new Horse(Team.HAN))).isEqualTo(true);
+    }
 
     /**
      * 8. 한나라가 마상상마를 선택한 경우, 보드판에 올바르게 배치된다.
      */
+    @Test
+    void 한나라의_마상상마_상차림을_올바르게_배치한다() {
+        // given
+        InitializeStrategy strategy = new InnerElephantFormationStrategy();
+
+        // when
+        Board board = new Board(strategy, strategy);
+
+        // then
+        Position hanFirstElephant = Position.from(1, 3);
+        Position hanSecondElephant = Position.from(1, 7);
+        Position hanFirstHorse = Position.from(1, 2);
+        Position hanSecondHorse = Position.from(1, 8);
+
+        assertThat(board.isExistSameType(hanFirstElephant, new Elephant(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanSecondElephant, new Elephant(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanFirstHorse, new Horse(Team.HAN))).isEqualTo(true);
+        assertThat(board.isExistSameType(hanSecondHorse, new Horse(Team.HAN))).isEqualTo(true);
+    }
 }
