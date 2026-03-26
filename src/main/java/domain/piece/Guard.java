7557952db6fc64e321682cc6c50effa3b39402db
Jinriro <riro12.hanyang.ac.kr>
2026-03-26 11:24:24 +0900

    chore: Guard 커밋 오류 해결

--- src/main/java/domain/piece/Guard.java
+++ src/main/java/domain/piece/Guard.java
@@ -1,9 +1,9 @@
 package domain.piece;
 
-import domain.board.Board;
 import domain.Position;
 import domain.Side;
-
+import domain.board.Board;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Guard extends Piece {
@@ -14,6 +14,21 @@ public class Guard extends Piece {
 
     @Override
     public List<Position> getPossibleMoves(Board board, Position start) {
-        return List.of();
+        List<Position> possiblePositions = new ArrayList<>();
+        int[] dx = {0, -1, 1, 0};
+        int[] dy = {-1, 0, 0, 1};
+
+        for (int i = 0; i < 4; i++) {
+            int nRow = start.row() + dx[i];
+            int nCol = start.col() + dy[i];
+            Position destination = new Position(nCol, nRow);
+
+            if (board.isAvailableDestination(destination)) {
+                possiblePositions.add(destination);
+            }
+        }
+        
+        return possiblePositions;
     }
+
 }
