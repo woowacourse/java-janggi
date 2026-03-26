d32648d730068a72afbc6ac50b4920e91d2d3ccd
smiinii <sm020320@naver.com>
2026-03-26 21:34:15 +0900

    feat: Board 객체 변환을 위한 BoardDto 클래스 추가

---
+++ src/main/java/janggi/dto/BoardDto.java
@@ -0,0 +1,31 @@
+package janggi.dto;
+
+import janggi.domain.Board;
+import janggi.domain.Piece;
+import janggi.domain.Position;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BoardDto {
+    private final Map<List<Integer>, String> pieces;
+
+    private BoardDto(Map<List<Integer>, String> pieces) {
+        this.pieces = pieces;
+    }
+
+    public static BoardDto from(Board board) {
+        Map<List<Integer>, String> result = new HashMap<>();
+        Map<Position, Piece> pieces = board.getBoard();
+
+        for (Position position : pieces.keySet()) {
+            result.put(position.getPosition(), pieces.get(position).getName());
+        }
+        return new BoardDto(result);
+    }
+
+    public Map<List<Integer>, String> getPieces() {
+        return pieces;
+    }
+}
