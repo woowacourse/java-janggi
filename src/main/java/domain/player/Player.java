fc54fdee3ba1f5c71b699055d66587ba02ed3ba3
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-26 20:39:58 +0900

    feat(Player): 잡은기물 저장하는 기능 추가

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,16 +1,35 @@
 package domain.player;
 
+import domain.piece.Piece;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Player {
 
-    public final Name name;
-    public final Team team;
+    private final Name name;
+    private final Team team;
+    private final List<Piece> catchedPiece;
+
 
     public Player(Name name, Team team) {
         this.name = name;
         this.team = team;
+        catchedPiece = new ArrayList<>();
     }
 
     public String getName() {
         return name.getValue();
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public List<Piece> getCatchedPiece() {
+        return List.copyOf(catchedPiece);
+    }
+
+    public void addCatchedPiece(Piece piece) {
+        catchedPiece.add(piece);
     }
 }
