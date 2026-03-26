4b6478f224c21d3fbd382a06270ebc0ef38ccdb2
Huiyeongkim <huiyeong9619@naver.com>
2026-03-26 15:58:28 +0900

    feat: 포의 이동 전략 구현 및 테스트 추가

--- src/main/java/domain/strategy/CannonMoveStrategy.java
+++ src/main/java/domain/strategy/CannonMoveStrategy.java
@@ -1,14 +1,70 @@
 package domain.strategy;
 
 import domain.Board;
+import domain.Type;
 import domain.vo.Position;
 
 public class CannonMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean canMove(Position from, Position to, Board board) {
+        if (isNotCorrectPath(from, to)) 
+            return false;
 
+        int nx = 0, ny = 0;
+        if (from.getRow() < to.getRow()) {
+            nx = 1;
+        }
+        if (from.getRow() > to.getRow()) {
+            nx = -1;
+        }
+        if (from.getCol() < to.getCol()) {
+            ny = 1;
+        }
+        if (from.getCol() > to.getCol()) {
+            ny = -1;
+        }
 
+        int pieceCount = 0;
+        int row = from.getRow();
+        int col = from.getCol();
+        while (true) {
+            row += nx;
+            col += ny;
+
+            if (row == to.getRow() && col == to.getCol()) {
+                if (!isCannon(board, to.getRow(), to.getCol())
+                        && board.isAnotherTeam(from, to)
+                        && pieceCount == 1) {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (board.isExistPosition(Position.of(row, col))) {
+                if (isCannon(board, row, col)) {
+                    return false;
+                }
+
+                pieceCount += 1;
+            }
+
+            if (pieceCount > 1) {
+                return false;
+            }
+        }
+    }
+
+    private boolean isCannon(Board board, int row, int col) {
+        if (board.findPieceByPosition(Position.of(row, col)).isEmpty()) return false;
+        return board.findPieceByPosition(Position.of(row, col)).get().getType() == Type.CANNON;
+    }
+
+    private boolean isNotCorrectPath(Position from, Position to) {
+        if (from.getRow() != to.getRow() && from.getCol() != to.getCol()) {
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/domain/strategy/CannonMoveStrategyTest.java
+++ src/test/java/domain/strategy/CannonMoveStrategyTest.java
@@ -1,54 +1,68 @@
 package domain.strategy;
 
-import domain.Board;
-import domain.BoardFactory;
+import domain.*;
 import domain.vo.Position;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class CannonMoveStrategyTest {
 
     @Test
-    @DisplayName("초나라 포의 이동 경로에 다른 기물이 없으면 정상 이동한다.")
+    @DisplayName("초나라 포의 이동 경로에 포를 제외한 기물이 1개 있으면 정상 이동한다.")
     void 초나라_포_정상_이동() {
         // given
         MoveStrategy strategy = new CannonMoveStrategy();
-        Board board = BoardFactory.setUp();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(2, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+        boardMapper.put(Position.of(3, 1), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+
+        Board board = BoardFactory.of(boardMapper);
 
         // when
         Position from = Position.of(2, 1);
-        Position to = Position.of(3, 3);
+        Position to = Position.of(4, 1);
 
         // then
         Assertions.assertTrue(strategy.canMove(from, to, board));
     }
 
     @Test
-    @DisplayName("한나라 포의 이동 경로에 다른 기물이 없으면 정상 이동한다.")
-    void 한나라_포_정상_이동() {
+    @DisplayName("한나라 포의 이동 경로에 다른 기물이 없으면 이동하지 못한다.")
+    void 포의_이동_경로에_다른_기물이_없으면_이동하지_못한다() {
         // given
-        MoveStrategy strategy = new ElephantMoveStrategy();
-        Board board = BoardFactory.setUp();
+        MoveStrategy strategy = new CannonMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(2, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+
+        Board board = BoardFactory.of(boardMapper);
 
         // when
-        Position from = Position.of(9, 1);
-        Position to = Position.of(6, 3);
+        Position from = Position.of(2, 1);
+        Position to = Position.of(5, 1);
 
         // then
-        Assertions.assertTrue(strategy.canMove(from, to, board));
+        Assertions.assertFalse(strategy.canMove(from, to, board));
     }
 
     @Test
-    @DisplayName("초나라 포의 이동 경로에 기물이 있으면 이동하지 않는다.")
-    void 초나라_포_이동_경로에_기물이_있으면_이동_불가() {
+    @DisplayName("초나라 포의 이동 경로에 기물이 둘 이상 있으면 이동하지 않는다.")
+    void 초나라_포_이동_경로에_기물이_둘_이상_있으면_이동_불가() {
         // given
-        MoveStrategy strategy = new ElephantMoveStrategy();
-        Board board = BoardFactory.setUp();
+        MoveStrategy strategy = new CannonMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(2, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+        boardMapper.put(Position.of(3, 1), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+        boardMapper.put(Position.of(4, 1), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+
+        Board board = BoardFactory.of(boardMapper);
 
         // when
-        Position from = Position.of(0, 1);
-        Position to = Position.of(2, 4);
+        Position from = Position.of(2, 1);
+        Position to = Position.of(5, 1);
 
         // then
         Assertions.assertFalse(strategy.canMove(from, to, board));
@@ -58,12 +72,37 @@ class CannonMoveStrategyTest {
     @DisplayName("목적지에 같은 팀 기물이 있으면 이동하지 않는다.")
     void 포_목적지에_같은_팀_기물이_있으면_이동_불가() {
         // given
-        MoveStrategy strategy = new ElephantMoveStrategy();
-        Board board = BoardFactory.setUp();
+        MoveStrategy strategy = new CannonMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(2, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+        boardMapper.put(Position.of(3, 1), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+        boardMapper.put(Position.of(5, 1), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+
+        Board board = BoardFactory.of(boardMapper);
 
         // when
-        Position from = Position.of(6, 1);
-        Position to = Position.of(3, 4);
+        Position from = Position.of(2, 1);
+        Position to = Position.of(5, 1);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(from, to, board));
+    }
+
+    @Test
+    @DisplayName("목적지에 포가 있으면 이동하지 않는다.")
+    void 목적지에_포가_있으면_이동하지_않는다() {
+        // given
+        MoveStrategy strategy = new CannonMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(2, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+        boardMapper.put(Position.of(3, 1), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+        boardMapper.put(Position.of(5, 1), Piece.of(Team.HAN, Type.CANNON, strategy));
+
+        Board board = BoardFactory.of(boardMapper);
+
+        // when
+        Position from = Position.of(2, 1);
+        Position to = Position.of(5, 1);
 
         // then
         Assertions.assertFalse(strategy.canMove(from, to, board));
@@ -73,14 +112,39 @@ class CannonMoveStrategyTest {
     @DisplayName("목적지에 다른 팀 기물이 있으면 이동한다.")
     void 포_목적지에_다른_팀_기물이_있으면_이동_가능() {
         // given
-        MoveStrategy strategy = new ElephantMoveStrategy();
-        Board board = BoardFactory.setUp();
+        MoveStrategy strategy = new CannonMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(2, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+        boardMapper.put(Position.of(3, 1), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+        boardMapper.put(Position.of(5, 1), Piece.of(Team.HAN, Type.SOLIDER, new FixedMoveStrategy()));
+
+        Board board = BoardFactory.of(boardMapper);
 
         // when
-        Position from = Position.of(3, 4);
-        Position to = Position.of(6, 2);
+        Position from = Position.of(2, 1);
+        Position to = Position.of(5, 1);
 
         // then
         Assertions.assertTrue(strategy.canMove(from, to, board));
+    }
+
+    @Test
+    @DisplayName("포의 이동 경로에 포가 존재하면 이동하지 못한다.")
+    void 포의_이동_경로에_포가_있으면_이동_불가() {
+        // given
+        MoveStrategy strategy = new CannonMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(2, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+        boardMapper.put(Position.of(3, 1), Piece.of(Team.CHU, Type.CANNON, strategy));
+        boardMapper.put(Position.of(5, 1), Piece.of(Team.HAN, Type.SOLIDER, new FixedMoveStrategy()));
+
+        Board board = BoardFactory.of(boardMapper);
+
+        // when
+        Position from = Position.of(2, 1);
+        Position to = Position.of(5, 1);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(from, to, board));
     }
 }
