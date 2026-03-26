10ccac1a14340d4efbbf8014bbbbd05c0b3a43f4
HyoYoonNam <dev.rudevico@gmail.com>
2026-03-26 20:28:53 +0900

    상의 행마법 구현

--- src/main/java/domain/piece/Elephant.java
+++ src/main/java/domain/piece/Elephant.java
@@ -1,10 +1,99 @@
 package domain.piece;
 
+import domain.board.Intersection;
+import domain.direction.Direction;
+import domain.direction.MoveAmount;
 import domain.game.Side;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Elephant extends Piece {
 
+    private static final MoveAmount MOVE_UNIT = new MoveAmount(1);
+
     public Elephant(Side side) {
         super(side);
+    }
+
+    public boolean canMove(
+            Intersection from,
+            Intersection to,
+            AlivePieces alivePieces
+    ) {
+        return movableIntersections(from, alivePieces)
+                .contains(to);
+    }
+
+    public List<Intersection> movableIntersections(
+            Intersection from,
+            AlivePieces alivePieces
+    ) {
+        List<Intersection> movableIntersections = new ArrayList<>();
+        for (Direction direction : side.getAllDirections()) {
+            addIfMovable(from, direction, alivePieces, movableIntersections);
+        }
+
+        return List.copyOf(movableIntersections);
+    }
+
+    private void addIfMovable(
+            Intersection from,
+            Direction direction,
+            AlivePieces alivePieces,
+            List<Intersection> movableIntersections
+    ) {
+        addLeftDestinationIfReachable(from, direction, alivePieces, movableIntersections);
+        addRightDestinationIfReachable(from, direction, alivePieces, movableIntersections);
+    }
+
+    private void addLeftDestinationIfReachable(
+            Intersection from,
+            Direction direction,
+            AlivePieces alivePieces,
+            List<Intersection> movableIntersections
+    ) {
+        List<Intersection> path = pathToLeftDestination(from, direction);
+        Intersection destination = direction.moveForwardLeft(path.getLast(), MOVE_UNIT);
+
+        if (isAvailablePath(alivePieces, path) && isAvailableDestination(destination, alivePieces)) {
+            movableIntersections.add(destination);
+        }
+    }
+
+    private void addRightDestinationIfReachable(
+            Intersection from,
+            Direction direction,
+            AlivePieces alivePieces,
+            List<Intersection> movableIntersections
+    ) {
+        List<Intersection> path = pathToRightDestination(from, direction);
+        Intersection destination = direction.moveForwardRight(path.getLast(), MOVE_UNIT);
+
+        if (isAvailablePath(alivePieces, path) && isAvailableDestination(destination, alivePieces)) {
+            movableIntersections.add(destination);
+        }
+    }
+
+    private List<Intersection> pathToLeftDestination(Intersection from, Direction direction) {
+        Intersection firstNode = direction.moveForward(from, MOVE_UNIT);
+        Intersection secondNode = direction.moveForwardLeft(firstNode, MOVE_UNIT);
+
+        return List.of(firstNode, secondNode);
+    }
+
+    private List<Intersection> pathToRightDestination(Intersection from, Direction direction) {
+        Intersection firstNode = direction.moveForward(from, MOVE_UNIT);
+        Intersection secondNode = direction.moveForwardRight(firstNode, MOVE_UNIT);
+
+        return List.of(firstNode, secondNode);
+    }
+
+    private static boolean isAvailablePath(AlivePieces alivePieces, List<Intersection> path) {
+        return path.stream()
+                .allMatch(node -> node.isInBoard() && alivePieces.isEmpty(node));
+    }
+
+    private boolean isAvailableDestination(Intersection destination, AlivePieces alivePieces) {
+        return destination.isInBoard() && alivePieces.placedNotSameSide(destination, side);
     }
 }
---
+++ src/test/java/domain/piece/ElephantTest.java
@@ -0,0 +1,176 @@
+package domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.board.Intersection;
+import domain.game.Side;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ElephantTest {
+
+    private static final int DEFAULT_ROW = 5;
+    private static final int DEFAULT_FILE = 5;
+    private static final Side SIDE = Side.HAN;
+    private static final Side OPPOSITE_SIDE = Side.CHO;
+    private static final Soldier SAME_SIDE_PEICE = new Soldier(SIDE);
+    private static final Soldier OPPOSITE_SIDE_PEICE = new Soldier(OPPOSITE_SIDE);
+    private static final Intersection CURRENT_INTERSECTION = new Intersection(DEFAULT_ROW, DEFAULT_FILE);
+
+    private static final Intersection LEFT_PATH_NODE = new Intersection(
+            CURRENT_INTERSECTION.row() - 2,
+            CURRENT_INTERSECTION.file() - 1
+    );
+    private static final Intersection RIGHT_PATH_NODE = new Intersection(
+            CURRENT_INTERSECTION.row() - 2,
+            CURRENT_INTERSECTION.file() + 1
+    );
+    private static final Intersection BOTH_PATH_NODE = new Intersection(
+            CURRENT_INTERSECTION.row() - 1,
+            CURRENT_INTERSECTION.file()
+    );
+
+    private static final Intersection LEFT_DESTINATION = new Intersection(
+            CURRENT_INTERSECTION.row() - 3,
+            CURRENT_INTERSECTION.file() - 2
+    );
+    private static final Intersection RIGHT_DESTINATION = new Intersection(
+            CURRENT_INTERSECTION.row() - 3,
+            CURRENT_INTERSECTION.file() + 2
+    );
+
+    @Nested
+    class 이동할_경로에_기물이_있으면_이동할_수_없다 {
+
+        @Test
+        void 왼쪽_경로에_기물이_있으면_왼쪽_목적지로_이동할_수_없다() {
+            // given
+            Elephant elephant = new Elephant(SIDE);
+            AlivePieces alivePieces = new AlivePieces(Map.of(LEFT_PATH_NODE, OPPOSITE_SIDE_PEICE));
+
+            // when
+            boolean canMove = elephant.canMove(CURRENT_INTERSECTION, LEFT_DESTINATION, alivePieces);
+
+            // then
+            assertThat(canMove).isFalse();
+        }
+
+        @Test
+        void 오른쪽_경로에_기물이_있으면_오른쪽_목적지로_이동할_수_없다() {
+            // given
+            Elephant elephant = new Elephant(SIDE);
+            AlivePieces alivePieces = new AlivePieces(Map.of(RIGHT_PATH_NODE, OPPOSITE_SIDE_PEICE));
+
+            // when
+            boolean canMove = elephant.canMove(CURRENT_INTERSECTION, RIGHT_DESTINATION, alivePieces);
+
+            // then
+            assertThat(canMove).isFalse();
+        }
+
+        @Test
+        void 양쪽_경로에_기물이_있으면_양쪽_목적지로_이동할_수_없다() {
+            // given
+            Elephant elephant = new Elephant(SIDE);
+            AlivePieces alivePieces = new AlivePieces(Map.of(BOTH_PATH_NODE, OPPOSITE_SIDE_PEICE));
+
+            // when
+            List<Intersection> movableIntersections = elephant.movableIntersections(CURRENT_INTERSECTION, alivePieces);
+
+            // then
+            assertThat(movableIntersections).doesNotContain(LEFT_DESTINATION, RIGHT_DESTINATION);
+        }
+    }
+
+    @Test
+    void 이동할_경로가_보드_범위_밖이라면_이동할_수_없다() {
+        // given
+        Elephant elephant = new Elephant(SIDE);
+
+        Intersection boarderlineIntersection = new Intersection(1, 1);
+        AlivePieces emptyAlivePlaces = new AlivePieces(Map.of());
+
+        // when
+        List<Intersection> movableIntersection = elephant.movableIntersections(boarderlineIntersection, emptyAlivePlaces);
+
+        // then
+        boolean movableOutOfBoard = movableIntersection.stream()
+                .anyMatch(Intersection::isOutOfBoard);
+
+        assertThat(movableOutOfBoard).isFalse();
+    }
+
+    @Test
+    void 종착지가_보드_범위_밖이라면_이동할_수_없다() {
+        // given
+        Elephant elephant = new Elephant(SIDE);
+
+        Intersection destinationOutOfBoard = new Intersection(3, 3);
+        AlivePieces emptyAlivePlaces = new AlivePieces(Map.of());
+
+        // when
+        List<Intersection> movableIntersection = elephant.movableIntersections(destinationOutOfBoard, emptyAlivePlaces);
+
+        // then
+        boolean movableOutOfBoard = movableIntersection.stream()
+                .anyMatch(Intersection::isOutOfBoard);
+
+        assertThat(movableOutOfBoard).isFalse();
+    }
+
+    @Test
+    void 종착지에_아군_기물이_있다면_이동할_수_없다() {
+        // given
+        Elephant elephant = new Elephant(SIDE);
+
+        AlivePieces alivePieces = new AlivePieces(Map.of(
+                LEFT_DESTINATION, SAME_SIDE_PEICE,
+                RIGHT_DESTINATION, SAME_SIDE_PEICE
+        ));
+
+        // when
+        List<Intersection> movableIntersections = elephant.movableIntersections(CURRENT_INTERSECTION, alivePieces);
+
+        // then
+        assertThat(movableIntersections).doesNotContain(LEFT_DESTINATION, RIGHT_DESTINATION);
+    }
+
+    @Test
+    void 종착지에_상대_기물이_있다면_이동할_수_있다() {
+        // given
+        Elephant elephant = new Elephant(SIDE);
+
+        AlivePieces alivePieces = new AlivePieces(Map.of(
+                LEFT_DESTINATION, OPPOSITE_SIDE_PEICE,
+                RIGHT_DESTINATION, OPPOSITE_SIDE_PEICE
+        ));
+
+        // when
+        List<Intersection> movableIntersections = elephant.movableIntersections(CURRENT_INTERSECTION, alivePieces);
+
+        // then
+        assertThat(movableIntersections).contains(LEFT_DESTINATION, RIGHT_DESTINATION);
+    }
+
+    @Test
+    void 종착지에_기물이_없다면_이동할_수_있다() {
+        // given
+        Elephant elephant = new Elephant(SIDE);
+
+        AlivePieces emptyAlivePieces = new AlivePieces(Map.of());
+
+        // when
+        List<Intersection> movableIntersections = elephant.movableIntersections(CURRENT_INTERSECTION, emptyAlivePieces);
+
+        // then
+        assertThat(movableIntersections).contains(LEFT_DESTINATION, RIGHT_DESTINATION);
+    }
+}
