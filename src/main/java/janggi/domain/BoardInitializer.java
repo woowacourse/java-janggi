f0c82640367689159c1e48d4ccede35e052424e6
sinequanonn <dleck288@gmail.com>
2026-03-26 15:30:56 +0900

    feat: 보드판 초기화 구현

--- src/main/java/janggi/domain/BoardInitializer.java
+++ src/main/java/janggi/domain/BoardInitializer.java
@@ -3,10 +3,12 @@ package janggi.domain;
 import janggi.domain.piece.*;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BoardInitializer {
-    public static Board initializeBoard() {
+
+    public static Board initializeBoard(int hanOpeningFormationChoice, int choOpeningFormationChoice) {
         Map<Position, Piece> board = new LinkedHashMap<>();
         initializeHan(board, hanOpeningFormationChoice);
         initializeCho(board, choOpeningFormationChoice);
@@ -14,7 +16,75 @@ public class BoardInitializer {
         return new Board(board);
     }
 
-    private static void initializeHan(Map<Position, Piece> board) {
+    private static void initializeHanAttire(Map<Position, Piece> board, int choice) {
+        if (choice == 1) {
+            board.put(new Position(2, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+            board.put(new Position(7, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+
+            board.put(new Position(3, 1), new HorsePiece(Name.HORSE, Team.HAN));
+            board.put(new Position(8, 1), new HorsePiece(Name.HORSE, Team.HAN));
+        }
+
+        if (choice == 2) {
+            board.put(new Position(3, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+            board.put(new Position(8, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+
+            board.put(new Position(2, 1), new HorsePiece(Name.HORSE, Team.HAN));
+            board.put(new Position(7, 1), new HorsePiece(Name.HORSE, Team.HAN));
+        }
+        if (choice == 3) {
+            board.put(new Position(3, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+            board.put(new Position(7, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+
+            board.put(new Position(2, 1), new HorsePiece(Name.HORSE, Team.HAN));
+            board.put(new Position(8, 1), new HorsePiece(Name.HORSE, Team.HAN));
+        }
+
+        if (choice == 4) {
+            board.put(new Position(2, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+            board.put(new Position(8, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
+
+            board.put(new Position(3, 1), new HorsePiece(Name.HORSE, Team.HAN));
+            board.put(new Position(7, 1), new HorsePiece(Name.HORSE, Team.HAN));
+        }
+    }
+
+    private static void initializeChoAttire(Map<Position, Piece> board, int choice) {
+        if (choice == 1) { //상마상마
+            board.put(new Position(3, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+            board.put(new Position(8, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+
+            board.put(new Position(2, 10), new HorsePiece(Name.HORSE, Team.CHO));
+            board.put(new Position(7, 10), new HorsePiece(Name.HORSE, Team.CHO));
+        }
+
+        if (choice == 2) { // 마상마상
+            board.put(new Position(2, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+            board.put(new Position(7, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+
+            board.put(new Position(3, 10), new HorsePiece(Name.HORSE, Team.CHO));
+            board.put(new Position(8, 10), new HorsePiece(Name.HORSE, Team.CHO));
+        }
+
+        if (choice == 3) { // 마상상마
+            board.put(new Position(3, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+            board.put(new Position(7, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+
+            board.put(new Position(2, 10), new HorsePiece(Name.HORSE, Team.CHO));
+            board.put(new Position(8, 10), new HorsePiece(Name.HORSE, Team.CHO));
+        }
+
+        if (choice == 4) { // 상마마상
+            board.put(new Position(2, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+            board.put(new Position(8, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
+
+            board.put(new Position(3, 10), new HorsePiece(Name.HORSE, Team.CHO));
+            board.put(new Position(7, 10), new HorsePiece(Name.HORSE, Team.CHO));
+        }
+    }
+
+
+    private static void initializeHan(Map<Position, Piece> board, int openingFormationChoice) {
         board.put(new Position(1, 4), new SoliderPiece(Name.SOLDIER, Team.HAN));
         board.put(new Position(3, 4), new SoliderPiece(Name.SOLDIER, Team.HAN));
         board.put(new Position(5, 4), new SoliderPiece(Name.SOLDIER, Team.HAN));
@@ -29,17 +99,13 @@ public class BoardInitializer {
         board.put(new Position(4, 1), new GuardPiece(Name.GUARD, Team.HAN));
         board.put(new Position(6, 1), new GuardPiece(Name.GUARD, Team.HAN));
 
-        board.put(new Position(3, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
-        board.put(new Position(7, 1), new ElephantPiece(Name.ELEPHANT, Team.HAN));
-
-        board.put(new Position(2, 1), new HorsePiece(Name.HORSE, Team.HAN));
-        board.put(new Position(8, 1), new HorsePiece(Name.HORSE, Team.HAN));
-
         board.put(new Position(1, 1), new ChariotPiece(Name.CHARIOT, Team.HAN));
         board.put(new Position(9, 1), new ChariotPiece(Name.CHARIOT, Team.HAN));
+
+        initializeHanAttire(board, openingFormationChoice);
     }
 
-    private static void initializeCho(Map<Position, Piece> board) {
+    private static void initializeCho(Map<Position, Piece> board, int openingFormationChoice) {
         board.put(new Position(1, 7), new SoliderPiece(Name.SOLDIER, Team.CHO));
         board.put(new Position(3, 7), new SoliderPiece(Name.SOLDIER, Team.CHO));
         board.put(new Position(5, 7), new SoliderPiece(Name.SOLDIER, Team.CHO));
@@ -54,13 +120,9 @@ public class BoardInitializer {
         board.put(new Position(4, 10), new GuardPiece(Name.GUARD, Team.CHO));
         board.put(new Position(6, 10), new GuardPiece(Name.GUARD, Team.CHO));
 
-        board.put(new Position(3, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
-        board.put(new Position(7, 10), new ElephantPiece(Name.ELEPHANT, Team.CHO));
-
-        board.put(new Position(2, 10), new HorsePiece(Name.HORSE, Team.CHO));
-        board.put(new Position(8, 10), new HorsePiece(Name.HORSE, Team.CHO));
-
         board.put(new Position(1, 10), new ChariotPiece(Name.CHARIOT, Team.CHO));
         board.put(new Position(9, 10), new ChariotPiece(Name.CHARIOT, Team.CHO));
+
+        initializeChoAttire(board, openingFormationChoice);
     }
 }
