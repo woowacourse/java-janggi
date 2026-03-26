02914c179c64ac115443a355638fa30fb503b12f
smiinii <sm020320@naver.com>
2026-03-26 21:33:28 +0900

    feat: 각 기물의 MoveStorage 클래스 추가

---
+++ src/main/java/janggi/domain/movestorage/JolMoveStorage.java
@@ -0,0 +1,12 @@
+package janggi.domain.movestorage;
+
+import janggi.domain.BoardState;
+import janggi.domain.Position;
+
+public class JolMoveStorage implements MoveStorage{
+    // TODO: 이동 규칙 구현 (2026. 3. 26.)
+    @Override
+    public boolean canMove(Position from, Position to, BoardState boardState) {
+        return true;
+    }
+}
