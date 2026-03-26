2d71c6fe890fc8b3d6c4742b267af8a2d2e08b34
woohyeon <woohyeon6885@gmail.com>
2026-03-26 18:04:01 +0900

    feat: 장기 플레이 기능 구현

--- src/main/java/janggi/turn/HanTurn.java
+++ src/main/java/janggi/turn/HanTurn.java
@@ -1,10 +1,19 @@
 package janggi.turn;
 
+import janggi.Board;
+import janggi.position.Position;
+
 public class HanTurn implements Turn{
 
+    private final Board board;
+
+    public HanTurn(Board board) {
+        this.board = board;
+    }
+
     @Override
-    public Turn play() {
-        return new ChoTurn();
+    public Turn play(Position from, Position to) {
+        return new ChoTurn(board);
     }
 
     @Override
--- src/test/java/janggi/turn/ChoTurnTest.java
+++ src/test/java/janggi/turn/ChoTurnTest.java
@@ -2,22 +2,63 @@ package janggi.turn;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import janggi.Board;
+import janggi.Team;
+import janggi.gimul.Gimul;
+import janggi.gimul.Ma;
+import janggi.position.Column;
+import janggi.position.Position;
+import janggi.position.Row;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ChoTurnTest {
 
+    Board board;
+
+    @BeforeEach
+    void beforeEach() {
+        Map<Position, Gimul> board = new HashMap<>();
+
+        board.put(
+                new Position(Row.SEVEN, Column.FIVE),
+                new Ma(Team.CHO)
+        );
+        board.put(
+                new Position(Row.EIGHT, Column.SEVEN),
+                new Ma(Team.HAN)
+        );
+        board.put(
+                new Position(Row.SIX, Column.SEVEN),
+                new Ma(Team.CHO)
+        );
+        board.put(
+                new Position(Row.SIX, Column.FIVE),
+                new Ma(Team.CHO)
+        );
+
+        this.board = new Board(board);
+    }
+
     @DisplayName("한나라 턴을 반환한다.")
     @Test
     void play() {
-        assertThat(new ChoTurn().play())
+        //given
+        Position from = new Position(Row.SEVEN, Column.FIVE);
+        Position to = new Position(Row.EIGHT, Column.SEVEN);
+
+        //when & then
+        assertThat(new ChoTurn(board).play(from, to))
                 .isInstanceOf(HanTurn.class);
     }
 
     @DisplayName("게임이 아직 끝나지 않았다.")
     @Test
     void isGameOver() {
-        assertThat(new ChoTurn().isGameOver())
+        assertThat(new ChoTurn(new Board(new HashMap<>())).isGameOver())
                 .isFalse();
     }
 }
