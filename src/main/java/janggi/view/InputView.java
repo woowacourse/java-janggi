74b9d352b66beb0216b4dc287ba64e68475f5381
이동훈 <higgs95@naver.com>
2026-03-26 15:52:32 +0900

    feat: 차림 입력 예외 처리 추가

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String CHOICE_REGEX = "[^1234]";
+    private static final int CHOICE_MIN_SIZE = 2;
     private final Scanner sc;
 
     public InputView() {
@@ -17,9 +19,25 @@ public class InputView {
                 "2. 오른상 차림 (마상마상)\n" +
                 "3. 안상 차림 (마상상마)\n" +
                 "4. 바깥상 차림 (상마마상)");
-        return Arrays.stream(sc.nextLine().split(","))
+        List<Integer> choices = Arrays.stream(sc.nextLine().split(","))
                 .map(String::trim)
+                .peek(this::validateChoiceNumber)
                 .map(Integer::parseInt)
                 .toList();
+
+        validateChoicesSize(choices);
+        return choices;
+    }
+
+    private void validateChoiceNumber(String input) {
+        if (input.matches(CHOICE_REGEX)) {
+            throw new IllegalArgumentException("차림 선택은 1~4번까지 가능합니다.");
+        }
+    }
+
+    private void validateChoicesSize(List<Integer> choices) {
+        if (choices.size() != CHOICE_MIN_SIZE) {
+            throw new IllegalArgumentException("올바르지 않은 차림 선택입니다.");
+        }
     }
 }
