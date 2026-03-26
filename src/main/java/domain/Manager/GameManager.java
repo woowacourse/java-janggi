90aa0d5aa6fc0051d500b6ea668c30ae0924dfc3
toctoce <toctoce@naver.com>
2026-03-26 21:07:48 +0900

    feat(Manager): 포지션 생성 메서드 구현

--- src/main/java/domain/Manager/GameManager.java
+++ src/main/java/domain/Manager/GameManager.java
@@ -9,24 +9,53 @@ import domain.board.Formation;
 import domain.player.Name;
 import domain.player.Player;
 import domain.player.Team;
+import domain.position.Position;
+import java.util.List;
+import java.util.function.Supplier;
 import view.InputView;
 import view.OutputView;
 
 public class GameManager {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+    private Player choPlayer;
+    private Player hanPlayer;
 
     public void run() {
         Board board = initialize();
         outputView.printBoard(board.createDTO().board());
+        //for문 (턴마다 반복)
+        Position src = createPosition();
+        Position dest = createPosition();
+        // 이동 - 잡았으면 플레이어에 추가
+        outputView.printBoard(board.createDTO().board());
+        // 턴 넘기기
+
+    }
+
+    private <T> T retryOnInvalidInput(Supplier<T> function) {
+        while (true) {
+            try {
+                return function.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private Position createPosition() {
+        return retryOnInvalidInput(() -> {
+            List<Integer> numbers = inputView.askSourcePosition();
+            return new Position(numbers.getFirst(), numbers.getLast());
+        });
     }
 
     private Board initialize() {
         String choName = inputView.askChoPlayerName();
-        Player choPlayer = createPlayer(choName, CHO);
+        choPlayer = createPlayer(choName, CHO);
 
         String hanName = inputView.askHanPlayerName();
-        Player hanPlayer = createPlayer(hanName, HAN);
+        hanPlayer = createPlayer(hanName, HAN);
 
         int choPositionInput = inputView.askChoPositionInput();
         Formation choFormation = createFormation(choPositionInput);
@@ -36,6 +65,10 @@ public class GameManager {
 
         return BoardFactory.createWithFormation(choFormation, hanFormation);
     }
+
+//    private Position getSourcePosition() {
+//        inputView.
+//    }
 
     private Player createPlayer(String name, Team team) {
         return new Player(new Name(name), team);
