1e5cf0f408f07682f93e885aed64844482c7d037
toctoce <toctoce@naver.com>
2026-03-26 19:46:09 +0900

    test(Board): 기물 이동 테스트 추가로 작성

--- src/test/java/domain/board/BoardTest.java
+++ src/test/java/domain/board/BoardTest.java
@@ -7,11 +7,13 @@ import domain.piece.Cha;
 import domain.piece.Jol;
 import domain.piece.None;
 import domain.piece.Piece;
+import domain.piece.Po;
 import domain.piece.Sang;
 import domain.player.Team;
 import domain.position.Position;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
@@ -27,22 +29,59 @@ class BoardTest {
         return boardMap;
     }
 
-    @Test
-    void 이동할_수_없는_경우는_canMove가_false_반환() {
-        Map<Position, Piece> boardMap = createEmptyBoard();
-        boardMap.put(new Position(0, 0), new Cha(Team.CHO));
-        boardMap.put(new Position(1, 0), new Sang(Team.CHO));
+    @Nested
+    class 포_이외의_기물_이동_테스트 {
+        @Test
+        void 이동할_수_없는_경우는_canMove가_false_반환() {
+            Map<Position, Piece> boardMap = createEmptyBoard();
+            boardMap.put(new Position(0, 0), new Cha(Team.CHO));
+            boardMap.put(new Position(1, 0), new Sang(Team.CHO));
 
-        Board board = new Board(boardMap);
-        assertFalse(board.canMove(new Position(0, 0), new Position(8, 0)));
+            Board board = new Board(boardMap);
+            assertFalse(board.canMove(new Position(0, 0), new Position(8, 0)));
+        }
+
+        @Test
+        void 이동할_수_있는_경우는_canMove가_true() {
+            Map<Position, Piece> boardMap = createEmptyBoard();
+            boardMap.put(new Position(0, 3), new Jol(Team.CHO));
+
+            Board board = new Board(boardMap);
+            assertTrue(board.canMove(new Position(0, 3), new Position(0, 4)));
+        }
     }
 
-    @Test
-    void 이동할_수_있는_경우는_canMove가_true() {
-        Map<Position, Piece> boardMap = createEmptyBoard();
-        boardMap.put(new Position(0, 3), new Jol(Team.CHO));
+    @Nested
+    class 포_기물_이동_테스트 {
+        @Test
+        void 포가_이동할_수_없는_경우는_canMove가_false_반환() {
+            Map<Position, Piece> boardMap = createEmptyBoard();
+            boardMap.put(new Position(0, 0), new Po(Team.CHO));
 
-        Board board = new Board(boardMap);
-        assertTrue(board.canMove(new Position(0, 3), new Position(0, 4)));
+            Board board = new Board(boardMap);
+            assertFalse(board.canMove(new Position(0, 0), new Position(0, 3)));
+        }
+
+        @Test
+        void 포가_이동할_수_있는_경우는_canMove가_true() {
+            Map<Position, Piece> boardMap = createEmptyBoard();
+            boardMap.put(new Position(0, 0), new Po(Team.CHO));
+            boardMap.put(new Position(0, 1), new Cha(Team.HAN));
+            boardMap.put(new Position(0, 2), new None());
+
+            Board board = new Board(boardMap);
+            assertTrue(board.canMove(new Position(0, 0), new Position(0, 2)));
+        }
+
+        @Test
+        void 포가_도착지에_상대편_포가_있으면_canMove가_false() {
+            Map<Position, Piece> boardMap = createEmptyBoard();
+            boardMap.put(new Position(0, 0), new Po(Team.CHO));
+            boardMap.put(new Position(0, 1), new Cha(Team.HAN));
+            boardMap.put(new Position(0, 2), new Po(Team.HAN));
+
+            Board board = new Board(boardMap);
+            assertFalse(board.canMove(new Position(0, 0), new Position(0, 2)));
+        }
     }
 }
