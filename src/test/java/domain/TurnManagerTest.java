01759d809d0b5234772bf6bf8fa41eea5fe1f052
KIM SEON WOO <sunwoo005@naver.com>
2026-03-26 21:20:50 +0900

    test: add TurnManager test code

---
+++ src/test/java/domain/TurnManagerTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TurnManagerTest {
+
+    @BeforeEach
+    public void setUp(){
+        TurnManager turnManager = new TurnManager();
+    }
+
+    @Test
+    public void 현재_턴이_누구인지_반환한다() {
+        assertThat(turnManager.getCurrentTurn()).isEqualTo(TeamColor.CHO);
+
+    }
+
+    @Test
+    public void 턴이_바뀌면_반대팀이_현재_턴이_된다() {
+        turnManager.progressTurn();
+        assertThat(turnManager.getCurrentTurn()).isEqualTo(TeamColor.HAN);
+
+    }
+}
