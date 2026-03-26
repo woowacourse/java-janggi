b4fdbdc3796b8d3656d64240a021da8b2e12bbf2
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-26 11:27:15 +0900

    feat(Cha): 경로 계산 메서드 개선

--- src/main/java/domain/piece/Cha.java
+++ src/main/java/domain/piece/Cha.java
@@ -1,5 +1,6 @@
 package domain.piece;
 
+import domain.direction.Direction;
 import domain.player.Team;
 import domain.position.Path;
 import domain.position.Position;
@@ -15,29 +16,48 @@ public class Cha extends Piece {
 
     @Override
     public Path calculatePath(Position src, Position dest) {
-        List<Position> path = new ArrayList<>();
-        if (src.getX() == dest.getX()) {
-            if (src.getY() > dest.getY()) {
-                for (int i = src.getY() - 1; i >= dest.getY(); i--) {
-                    path.add(new Position(src.getX(), i));
-                }
-                return new Path(src, dest, path);
-            }
-            for (int i = src.getY() + 1; i < dest.getY(); i++) {
-                path.add(new Position(src.getX(), i));
-            }
-            return new Path(src, dest, path);
-        }
-        if (src.getX() > dest.getX()) {
-            for (int i = src.getX() - 1; i >= dest.getX(); i--) {
-                path.add(new Position(i, src.getY()));
-            }
-            return new Path(src, dest, path);
+        if(!validateMove(src, dest)) {
+            throw new IllegalArgumentException("이동 할 수 있는 경로가 아닙니다.");
         }
 
-        for (int i = src.getX() + 1; i <= dest.getX(); i++) {
-            path.add(new Position(i, src.getY()));
+        Direction direction = determineDirection(src, dest);
+
+        return bulidPath(src, dest, direction);
+    }
+
+    private boolean validateMove(Position src,Position dest) {
+        if(src.equals(dest)) return false;
+
+        return src.getX() == dest.getX() || src.getY() == dest.getY();
+    }
+
+    private Direction determineDirection(Position src, Position dest) {
+        if(src.getX() == dest.getX()) {
+            if(src.getY() > dest.getY()) {
+                return Direction.SOUTH;
+            }
+            return Direction.NORTH;
         }
+
+        if(src.getY() == dest.getY()) {
+            if(src.getX() > dest.getX()) {
+                return Direction.WEST;
+            }
+            return Direction.EAST;
+        }
+
+        throw new IllegalArgumentException("갈 수 있는 경로가 없습니다.");
+    }
+
+    private Path bulidPath(Position src, Position dest, Direction direction) {
+        List<Position> path = new ArrayList<>();
+
+        Position tmp = direction.move(src);
+        path.add(tmp);
+        while (!tmp.equals(dest)) {
+            path.add(direction.move(tmp));
+        }
+
         return new Path(src, dest, path);
     }
 }
