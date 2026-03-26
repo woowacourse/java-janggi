2da80414fadcd28d3080d63901af9fac705b925e
SooHyeon Lee <dalsu4594@naver.com>
2026-03-26 15:30:40 +0900

    feat: 마상 배치 전략 추가
    - 마상상마, 마상마상, 상마상마, 상마마상 전략 패턴 추가

---
+++ src/main/java/janggi/domain/board/strategy/HorseElephantHorseElephant.java
@@ -0,0 +1,22 @@
+package janggi.domain.board.strategy;
+
+import janggi.domain.Camp;
+import janggi.domain.Position;
+import janggi.domain.piece.Elephant;
+import janggi.domain.piece.Horse;
+import janggi.domain.piece.Piece;
+import janggi.domain.piece.strategy.DummyStrategy;
+
+import java.util.Map;
+
+public class HorseElephantHorseElephant implements FormationStrategy{
+    @Override
+    public Map<Position, Piece> createPieces(Camp camp) {
+        return Map.of(
+                Position.of(camp.initRowPosition(), 2), new Elephant(camp, new DummyStrategy()),
+                Position.of(camp.initRowPosition(), 1), new Horse(camp, new DummyStrategy()),
+                Position.of(camp.initRowPosition(), 7), new Elephant(camp, new DummyStrategy()),
+                Position.of(camp.initRowPosition(), 6), new Horse(camp, new DummyStrategy())
+        );
+    }
+}
