6b9c46c5bcf729aeea2ac3699dfdbe93b471ce9c
SooHyeon Lee <dalsu4594@naver.com>
2026-03-26 22:41:49 +0900

    feat: 병사 이동 전략 추가
    - 병사는 앞과 양옆으로 이동
    - 앞은 진형에 따라 다르게 이동

--- src/main/java/janggi/domain/piece/strategy/SoldierStrategy.java
+++ src/main/java/janggi/domain/piece/strategy/SoldierStrategy.java
@@ -6,9 +6,18 @@ import janggi.domain.Position;
 import java.util.List;
 
 public class SoldierStrategy implements MoveStrategy {
+    private final int direction;
+
+    public SoldierStrategy(int direction) {
+        this.direction = direction;
+    }
 
     @Override
     public List<Path> findMovablePaths(Position current) {
-        return List.of();
+        return List.of(
+                new Path(List.of(), Position.of(current.row() + direction, current.column())),
+                new Path(List.of(), Position.of(current.row(), current.column() + 1)),
+                new Path(List.of(), Position.of(current.row(), current.column() - 1))
+        );
     }
 }
