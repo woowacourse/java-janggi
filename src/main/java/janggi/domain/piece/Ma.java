d5822776b2c8d5e54932e369a7c58b42d0918495
haeyoon1 <hynam1220@naver.com>
2026-03-26 14:03:39 +0900

    feat: 마 경로 반환 로직

--- src/main/java/janggi/domain/piece/Ma.java
+++ src/main/java/janggi/domain/piece/Ma.java
@@ -3,6 +3,8 @@ package janggi.domain.piece;
 import janggi.domain.Path;
 import janggi.domain.Position;
 import janggi.domain.Team;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ma extends Piece {
 
@@ -20,14 +22,19 @@ public class Ma extends Piece {
 
     @Override
     public Path getPath(Position from, Position to) {
-        return null;
+        int dx = from.deltaX(to);
+        int dy = from.deltaY(to);
+
+        List<Position> positions = new ArrayList<>();
+        positions.add(new Position(from.x() + dx / 2, from.y() + dy / 2));
+        return new Path(positions);
     }
 
     private boolean moveStrategy(Position from, Position to) {
-        int dx = from.diffX(to);
-        int dy = from.diffY(to);
+        int dx = from.deltaX(to);
+        int dy = from.deltaY(to);
 
         return (Math.abs(dx) == 1 && Math.abs(dy) == 2) ||
-                (Math.abs(dx) == 2 && Math.abs(dy) == 1);
+            (Math.abs(dx) == 2 && Math.abs(dy) == 1);
     }
 }
