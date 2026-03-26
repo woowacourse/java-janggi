d0e4f554e561411b556db38a25167d92bab2bd55
이동훈 <higgs95@naver.com>
2026-03-26 20:20:17 +0900

    feat: 사 이동 가능 여부 판단 및 경로 반환 메서드 구현

--- src/main/java/janggi/domain/piece/GuardPiece.java
+++ src/main/java/janggi/domain/piece/GuardPiece.java
@@ -1,5 +1,7 @@
 package janggi.domain.piece;
 
+import static java.lang.Math.abs;
+
 import janggi.domain.board.Position;
 import java.util.List;
 
@@ -10,11 +12,24 @@ public class GuardPiece extends Piece {
 
     @Override
     public boolean canMove(Position from, Position to) {
-        return false;
+        int preX = from.getX();
+        int preY = from.getY();
+
+        int nextX = to.getX();
+        int nextY = to.getY();
+
+        if (abs(preX - nextX) > 1) {
+            return false;
+        }
+        if (abs(preY - nextY) > 1) {
+            return false;
+        }
+
+        return abs(preX - nextX) + abs(preY - nextY) <= 1;
     }
 
     @Override
     public List<Position> findPath(Position from, Position to) {
-        return List.of();
+        return List.of(to);
     }
 }
