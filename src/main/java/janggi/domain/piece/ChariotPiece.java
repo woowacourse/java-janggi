7676b3c4718d126059edd0d2e3154555b1b3a905
sinequanonn <dleck288@gmail.com>
2026-03-26 22:20:29 +0900

    feat: 차 이동 가능 여부 판단 및 경로 반환 메서드 구현

--- src/main/java/janggi/domain/piece/ChariotPiece.java
+++ src/main/java/janggi/domain/piece/ChariotPiece.java
@@ -1,6 +1,8 @@
 package janggi.domain.piece;
 
 import janggi.domain.board.Position;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChariotPiece extends Piece {
@@ -10,11 +12,46 @@ public class ChariotPiece extends Piece {
 
     @Override
     public boolean canMove(Position from, Position to) {
-        return false;
+        int preX = from.getX();
+        int preY = from.getY();
+
+        int nextX = to.getX();
+        int nextY = to.getY();
+
+        return (preX == nextX && preY != nextY) || (preX != nextX && preY == nextY);
     }
 
     @Override
     public List<Position> findPath(Position from, Position to) {
-        return List.of();
+        List<Position> path = new ArrayList<>();
+        int preX = from.getX();
+        int preY = from.getY();
+
+        int nextX = to.getX();
+        int nextY = to.getY();
+
+        if (preX == nextX) {
+            if (nextY > preY) {
+                for (int y = preY + 1; y <= nextY; y++) {
+                    path.add(new Position(preX, y));
+                }
+                return path;
+            }
+            for (int y = nextY; y < preY; y++) {
+                path.add(new Position(preX, y));
+            }
+            return path;
+        }
+
+        if (nextX > preX) {
+            for (int x = preX + 1; x <= nextX; x++) {
+                path.add(new Position(x, preY));
+            }
+            return path;
+        }
+        for (int x = nextX; x <= preX; x++) {
+            path.add(new Position(x, preY));
+        }
+        return path;
     }
 }
