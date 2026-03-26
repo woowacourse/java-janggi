c23a1f7c4f5f042a03ae70556210ceecb9dd8131
xxbeann <yjb4945@gmail.com>
2026-03-26 16:58:49 +0900

    feat: 졸병의 이동을 구현한다
    - 졸병이 전진과 좌우 한 칸 이동하도록 구현한다
    - 후진과 대각선 이동을 제한한다
    - 졸병의 이동 경로와 도착지 규칙을 설정한다
    - 졸병의 이동 가능 조건과 이동 컨텍스트 테스트를 추가한다

---
+++ src/test/java/pieces/JolByeongTest.java
@@ -0,0 +1,125 @@
+package pieces;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import movepolicy.MoveContext;
+import movepolicy.destination.BasicDestinationRule;
+import movepolicy.path.EmptyPathRule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import position.Position;
+
+class JolByeongTest {
+    private static final Position DEFAULT = new Position(1, 1);
+
+    @Nested
+    @DisplayName("졸병의 행마법 기준으로 도착지에 이동 가능한지 검증한다")
+    class CanMove {
+        @Test
+        void 위쪽으로_한_칸_이동할_수_있다() {
+            // given
+            Piece jolByeong = new JolByeong(Side.HAN);
+            Position departure = DEFAULT;
+            Position destination = departure.moveUp();
+            // when & then
+            assertThatCode(() -> jolByeong.askMoveContext(departure, destination))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 왼쪽으로_한_칸_이동할_수_있다() {
+            // given
+            Piece jolByeong = new JolByeong(Side.HAN);
+            Position departure = DEFAULT;
+            Position destination = departure.moveLeft();
+            // when & then
+            assertThatCode(() -> jolByeong.askMoveContext(departure, destination))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 오른쪽으로_한_칸_이동할_수_있다() {
+            // given
+            Piece jolByeong = new JolByeong(Side.HAN);
+            Position departure = DEFAULT;
+            Position destination = departure.moveRight();
+            // when & then
+            assertThatCode(() -> jolByeong.askMoveContext(departure, destination))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void 두_칸_위쪽이_도착지인_경우_예외를_던진다() {
+            // given
+            Piece jolByeong = new JolByeong(Side.HAN);
+            Position departure = DEFAULT;
+            Position destination = departure.moveUp().moveUp();
+            // when & then
+            assertThatThrownBy(() -> jolByeong.askMoveContext(departure, destination))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 대각선이_도착지인_경우_예외를_던진다() {
+            // given
+            Piece jolByeong = new JolByeong(Side.HAN);
+            Position departure = DEFAULT;
+            Position destination = departure.moveUp().moveRight();
+            // when & then
+            assertThatThrownBy(() -> jolByeong.askMoveContext(departure, destination))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 한_칸_아래쪽이_도착지인_경우_예외를_던진다() {
+            // given
+            Piece jolByeong = new JolByeong(Side.HAN);
+            Position departure = DEFAULT;
+            Position destination = departure.moveDown();
+            // when & then
+            assertThatThrownBy(() -> jolByeong.askMoveContext(departure, destination))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void 출발지와_도착지_사이에는_이동경로가_존재하지_않는다() {
+        // given
+        Piece jolByeong = new JolByeong(Side.HAN);
+        Position departure = DEFAULT;
+        Position destination = departure.moveUp();
+        // when
+        MoveContext moveContext = jolByeong.askMoveContext(departure, destination);
+        // then
+        assertThat(moveContext.pathPositions()).hasSize(0);
+    }
+
+    @Test
+    void 졸병은_다른_진영의_기물만_공격할_수_있다() {
+        // given
+        Piece jolByeong = new JolByeong(Side.HAN);
+        Position departure = DEFAULT;
+        Position destination = departure.moveUp();
+        // when
+        MoveContext moveContext = jolByeong.askMoveContext(departure, destination);
+        // then
+        assertThat(moveContext.destinationRule())
+                .isInstanceOf(BasicDestinationRule.class);
+    }
+
+    @Test
+    void 졸병은_이동_경로에_기물이_없을_때_이동할_수_있다() {
+        // given
+        Piece jolByeong = new JolByeong(Side.HAN);
+        Position departure = DEFAULT;
+        Position destination = departure.moveUp();
+        // when
+        MoveContext moveContext = jolByeong.askMoveContext(departure, destination);
+        // then
+        assertThat(moveContext.pathRule())
+                .isInstanceOf(EmptyPathRule.class);
+    }
+}
