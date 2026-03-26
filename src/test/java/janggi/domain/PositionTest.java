1b6ada6098c43662c27d124016447333f06ae4a5
sinequanonn <dleck288@gmail.com>
2026-03-26 17:12:51 +0900

    test: Position 동등성 테스트 작성

--- src/test/java/janggi/domain/PositionTest.java
+++ src/test/java/janggi/domain/PositionTest.java
@@ -1,11 +1,12 @@
 package janggi.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
+import janggi.domain.board.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PositionTest {
 
@@ -23,7 +24,7 @@ class PositionTest {
     }
 
     @ParameterizedTest
-    @DisplayName("기물의 X 좌표가 1~9 사이가 아니면 예외가 발생한다..")
+    @DisplayName("기물의 X 좌표가 1~9 사이가 아니면 예외가 발생한다.")
     @CsvSource({
             "0,1",
             "10,5",
@@ -35,7 +36,7 @@ class PositionTest {
     }
 
     @ParameterizedTest
-    @DisplayName("기물의 Y 좌표가 1~10 사이가 아니면 예외가 발생한다..")
+    @DisplayName("기물의 Y 좌표가 1~10 사이가 아니면 예외가 발생한다.")
     @CsvSource({
             "1,0",
             "4,11",
@@ -44,5 +45,19 @@ class PositionTest {
     void testPositionWithinValidYRange(int x, int y) {
         assertThatThrownBy(() -> new Position(x, y))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("같은 X,Y 좌표를 가진 Position은 서로 동등하다.")
+    @CsvSource({
+            "1, 1, 1, 1",
+            "1, 10, 1, 10",
+            "9, 10, 9, 10"
+    })
+    void testPositionEquality(int x1, int y1, int x2, int y2) {
+        Position p1 = new Position(x1, y1);
+        Position p2 = new Position(x2, y2);
+
+        assertThat(p1).isEqualTo(p2);
     }
 }
