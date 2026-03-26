7af5f9531c99103109eb850760285aadca68b34e
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-26 20:04:46 +0900

    test(Sang): 이동계산 테스트 구현

---
+++ src/test/java/domain/piece/SangTest.java
@@ -0,0 +1,42 @@
+package domain.piece;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import domain.player.Team;
+import domain.position.Path;
+import domain.position.Position;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
+
+class SangTest {
+    @ParameterizedTest
+    @MethodSource("provideSangPaths")
+    void 상은_앞으로_한칸_대각선으로_두칸_이동한다(int startX, int startY, int destX, int destY) {
+        Piece sang = new Sang(Team.HAN);
+        Position src = new Position(startX, startY);
+        Position dest = new Position(destX, destY);
+
+        Path path = sang.calculatePath(src, dest);
+
+        assertNotNull(path);
+        assertEquals(src, path.getSrc());
+        assertEquals(dest, path.getDest());
+        assertFalse(path.getWaypoints().isEmpty());
+    }
+
+    private static Stream<Arguments> provideSangPaths() {
+        return Stream.of(
+                Arguments.of(4, 4, 6, 7),
+                Arguments.of(4, 4, 2, 7),
+                Arguments.of(4, 4, 6, 1),
+                Arguments.of(4, 4, 2, 1),
+                Arguments.of(4, 4, 7, 6),
+                Arguments.of(4, 4, 7, 2),
+                Arguments.of(4, 4, 1, 6),
+                Arguments.of(4, 4, 1, 2)
+        );
+    }
+}
