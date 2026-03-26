25f5c80e26cde7413a3b74ec6aa7a5bd63b04661
EveryPine <kenton.lim@gmail.com>
2026-03-26 17:22:52 +0900

    test(rule): 이동 가능한 자취 경로 계산 테스트 수정

--- src/test/java/janggi/domain/movement/RuleWithTracesTest.java
+++ src/test/java/janggi/domain/movement/RuleWithTracesTest.java
@@ -7,7 +7,9 @@ import janggi.domain.Position;
 import janggi.domain.board.Board;
 import janggi.domain.board.BoardMediator;
 import janggi.domain.board.BoardMediatorImpl;
+import janggi.domain.piece.Chariot;
 import janggi.domain.piece.Elephant;
+import janggi.domain.piece.Piece;
 import janggi.domain.team.TeamType;
 import java.util.List;
 import java.util.Map;
@@ -22,13 +24,16 @@ public class RuleWithTracesTest {
     @Test
     @DisplayName("이동 가능한 자취 경로 계산 테스트")
     public void execute() {
-        board = new Board(Map.of(Position.valueOf(8, 3), new Elephant(TeamType.BLUE)));
-        boardMediator = new BoardMediatorImpl(board);
+        Map<Position, Piece> positionPieceMap = Map.of(
+            Position.valueOf(5, 3), new Chariot(TeamType.RED),
+            Position.valueOf(8, 3), new Elephant(TeamType.BLUE));
+        Board board = new Board(positionPieceMap);
+        BoardMediator boardMediator = new BoardMediatorImpl(board);
         Direction direction = Direction.valueOf(1, 0);
-        Rule ruleWithTraces = new RuleWithTraces();
+        Rule ruleWithTraces = new RuleWithTraces(List.of(new Movement(MAXIMUM_ROW, direction, boardMediator)));
         List<Position> expected = List.of(Position.valueOf(6, 3), Position.valueOf(7, 3), Position.valueOf(8, 3));
 
-        List<Position> actual = ruleWithTraces.execute(List.of(new Movement(MAXIMUM_ROW, direction, boardMediator)));
+        List<Position> actual = ruleWithTraces.execute(Position.valueOf(5, 3));
 
         assertThat(actual).hasSameElementsAs(expected);
     }
