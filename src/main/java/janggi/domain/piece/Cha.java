6015280a00c03c55d23f96aff0c3d1507b40a992
Junha Park <poiu7825@pusan.ac.kr>
2026-03-26 15:53:36 +0900

    feat: 차 경로 반환 로직 작성

--- src/main/java/janggi/domain/piece/Cha.java
+++ src/main/java/janggi/domain/piece/Cha.java
@@ -3,6 +3,8 @@ package janggi.domain.piece;
 import janggi.domain.Path;
 import janggi.domain.Position;
 import janggi.domain.Team;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cha extends Piece {
 
@@ -20,7 +22,32 @@ public class Cha extends Piece {
 
     @Override
     public Path getPath(Position from, Position to) {
-        return null;
+        int dx = from.deltaX(to);
+        int dy = from.deltaY(to);
+
+        if (dx != 0) {
+            List<Position> positions = getHorizontalPath(from, dx);
+            return new Path(positions);
+        }
+
+        List<Position> positions = getVerticalPath(from, dy);
+        return new Path(positions);
+    }
+
+    private List<Position> getHorizontalPath(Position from, int dx) {
+        List<Position> positions = new ArrayList<>();
+        for (int i = 1; i < dx; i++) {
+            positions.add(new Position(from.x() + i, from.y()));
+        }
+        return positions;
+    }
+
+    private List<Position> getVerticalPath(Position from, int dy) {
+        List<Position> positions = new ArrayList<>();
+        for (int i = 1; i < dy; i++) {
+            positions.add(new Position(from.x(), from.y() + i));
+        }
+        return positions;
     }
 
     private boolean moveStrategy(Position from, Position to) {
