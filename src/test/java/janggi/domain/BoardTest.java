64f7afcad44b9f64551f85ef6e56b920d88d04ba
Junha Park <poiu7825@pusan.ac.kr>
2026-03-26 16:37:46 +0900

    test: 보드 초기화 정상 작동 테스트 작성

--- src/test/java/janggi/domain/BoardTest.java
+++ src/test/java/janggi/domain/BoardTest.java
@@ -1,5 +1,20 @@
 package janggi.domain;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import janggi.domain.strategy.BasicPlacementStrategy;
+import janggi.domain.strategy.InitializeStrategy;
+import org.junit.jupiter.api.Test;
+
 class BoardTest {
+
+
+    @Test
+    void 보드_초기화_정상_테스트() {
+
+        InitializeStrategy strategy = new BasicPlacementStrategy();
+
+        assertDoesNotThrow(() -> new Board(strategy));
+    }
 
 }
