4d638e9b6010866676288b5787142ba3d38b36a8
Huiyeongkim <huiyeong9619@naver.com>
2026-03-26 16:52:18 +0900

    feat: 졸의 이동 규칙 구현 및 테스트 추가

--- src/main/java/domain/strategy/SoldierMoveStrategy.java
+++ src/main/java/domain/strategy/SoldierMoveStrategy.java
@@ -1,12 +1,53 @@
 package domain.strategy;
 
 import domain.Board;
+import domain.Team;
 import domain.vo.Position;
 
 public class SoldierMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean canMove(Position from, Position to, Board board) {
+        if (isNotCorrectPath(from, to))
+            return false;
+
+        if (isWithdraw(from, to, board))
+            return false;
+
+        if (board.isAnotherTeam(from, to)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isWithdraw(Position from, Position to, Board board) {
+        if (board.findPieceByPosition(from).get().getTeam() == Team.CHU) {
+            if (from.getRow() - to.getRow() == 1) {
+                return true;
+            }
+        }
+
+        if (board.findPieceByPosition(from).get().getTeam() == Team.HAN) {
+            if (from.getRow() - to.getRow() == -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNotCorrectPath(Position from, Position to) {
+        if (from.getRow() == to.getRow()) {
+            if (Math.abs(from.getCol() - to.getCol()) != 1) {
+                return true;
+            }
+        }
+
+        if (from.getCol() == to.getCol()) {
+            if (Math.abs(from.getRow() - to.getRow()) != 1) {
+                return true;
+            }
+        }
         return false;
     }
 }
---
+++ src/test/java/domain/strategy/SoldierMoveStrategyTest.java
@@ -0,0 +1,106 @@
+package domain.strategy;
+
+import domain.*;
+import domain.vo.Position;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class SoldierMoveStrategyTest {
+
+    @Test
+    @DisplayName("졸의 목적지에 기물이 없으면 이동한다.")
+    void 졸_정상_이동() {
+        // given
+        MoveStrategy strategy = new SoldierMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(3, 4), Piece.of(Team.CHU, Type.SOLIDER, strategy));
+
+        Board board = BoardFactory.of(boardMapper);
+
+        // when
+        Position position = Position.of(3, 4);
+        Position targetPosition = Position.of(4, 4);
+
+        // then
+        Assertions.assertTrue(strategy.canMove(position, targetPosition, board));
+    }
+
+    @Test
+    @DisplayName("졸의 목적지에 같은 팀 기물이 있으면 이동하지 않는다.")
+    void 졸_목적지에_같은_팀_기물이_있으면_이동_불가() {
+        // given
+        MoveStrategy strategy = new SoldierMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(3, 4), Piece.of(Team.CHU, Type.SOLIDER, strategy));
+        boardMapper.put(Position.of(4, 4), Piece.of(Team.CHU, Type.SOLIDER, strategy));
+
+        Board board = BoardFactory.of(boardMapper);
+
+        // when
+        Position position = Position.of(3, 4);
+        Position targetPosition = Position.of(4, 4);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(position, targetPosition, board));
+    }
+
+
+    @Test
+    @DisplayName("졸의 목적지에 다른 팀 기물이 있으면 이동한다.")
+    void 졸_목적지에_다른_팀_기물이_있으면_정상_이동() {
+        // given
+        MoveStrategy strategy = new SoldierMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(3, 4), Piece.of(Team.CHU, Type.SOLIDER, strategy));
+        boardMapper.put(Position.of(4, 4), Piece.of(Team.HAN, Type.SOLIDER, strategy));
+
+        Board board = BoardFactory.of(boardMapper);
+
+        // when
+        Position position = Position.of(3, 4);
+        Position targetPosition = Position.of(4, 4);
+
+        // then
+        Assertions.assertTrue(strategy.canMove(position, targetPosition, board));
+    }
+
+    @Test
+    @DisplayName("초나라 졸의 이동 경로가 후퇴이면 이동할 수 없다.")
+    void 초나라_졸의_이동_경로가_후퇴이면_이동_불가() {
+        // given
+        MoveStrategy strategy = new SoldierMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(3, 4), Piece.of(Team.CHU, Type.SOLIDER, strategy));
+
+        Board board = BoardFactory.of(boardMapper);
+
+        // when
+        Position position = Position.of(3, 4);
+        Position targetPosition = Position.of(2, 4);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(position, targetPosition, board));
+    }
+
+    @Test
+    @DisplayName("한나라 졸의 이동 경로가 후퇴이면 이동할 수 없다.")
+    void 한나라_졸의_이동_경로가_후퇴이면_이동_불가() {
+        // given
+        MoveStrategy strategy = new SoldierMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(6, 4), Piece.of(Team.HAN, Type.SOLIDER, strategy));
+
+        Board board = BoardFactory.of(boardMapper);
+
+        // when
+        Position position = Position.of(6, 4);
+        Position targetPosition = Position.of(7, 4);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(position, targetPosition, board));
+    }
+}
