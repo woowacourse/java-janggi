0eabf5551961ef798afaade0278a66cbea561469
MODUGGAGI <kiroro0814@naver.com>
2026-03-26 19:52:16 +0900

    feat: MoveCondition 추가

--- src/main/java/janggi/domain/piece/PieceRule.java
+++ src/main/java/janggi/domain/piece/PieceRule.java
@@ -1,31 +1,30 @@
 package janggi.domain.piece;
 
-import janggi.domain.Position;
+import janggi.domain.piece.condition.EmptyCondition;
+import janggi.domain.piece.condition.MoveCondition;
+import janggi.domain.piece.condition.OnePieceExistsCondition;
 import janggi.domain.piece.strategy.ElephantStrategy;
 import janggi.domain.piece.strategy.HorseStrategy;
 import janggi.domain.piece.strategy.MoveStrategy;
 import janggi.domain.piece.strategy.MultiStepStraightStrategy;
 import janggi.domain.piece.strategy.SingleStepStraightStrategy;
 import janggi.domain.piece.strategy.SoldierStrategy;
-import java.util.List;
 
 public enum PieceRule {
 
-    GENERAL(new SingleStepStraightStrategy()),
-    CHARIOT(new MultiStepStraightStrategy()),
-    HORSE(new HorseStrategy()),
-    CANNON(new MultiStepStraightStrategy()),
-    GUARD(new SingleStepStraightStrategy()),
-    ELEPHANT(new ElephantStrategy()),
-    SOLDIER(new SoldierStrategy());
+    GENERAL(new SingleStepStraightStrategy(), new EmptyCondition()),
+    CHARIOT(new MultiStepStraightStrategy(), new EmptyCondition()),
+    HORSE(new HorseStrategy(), new EmptyCondition()),
+    CANNON(new MultiStepStraightStrategy(), new OnePieceExistsCondition()),
+    GUARD(new SingleStepStraightStrategy(), new EmptyCondition()),
+    ELEPHANT(new ElephantStrategy(), new EmptyCondition()),
+    SOLDIER(new SoldierStrategy(), new EmptyCondition());
 
     private final MoveStrategy moveStrategy;
+    private final MoveCondition moveCondition;
 
-    PieceRule(MoveStrategy moveStrategy) {
+    PieceRule(MoveStrategy moveStrategy, MoveCondition moveCondition) {
         this.moveStrategy = moveStrategy;
-    }
-
-    public List<Position> findPath(Position from, Position to, Camp camp) {
-        return moveStrategy.findPath(from, to, camp);
+        this.moveCondition = moveCondition;
     }
 }
