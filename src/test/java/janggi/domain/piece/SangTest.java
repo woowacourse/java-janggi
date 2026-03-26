221557d11665dfe9ccb6cdaa3241474ece873992
Junha Park <poiu7825@pusan.ac.kr>
2026-03-26 15:34:28 +0900

    test: 상 경로 반환 테스트 작성

--- src/test/java/janggi/domain/piece/SangTest.java
+++ src/test/java/janggi/domain/piece/SangTest.java
@@ -1,9 +1,11 @@
 package janggi.domain.piece;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+import janggi.domain.Path;
 import janggi.domain.Position;
 import janggi.domain.Space;
 import janggi.domain.Team;
@@ -52,5 +54,20 @@ class SangTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> piece.validateRoutes(pieces))
                 .withMessage("이동 경로 사이에 다른 말이 있으면 안됩니다.");
+    }
+
+    @Test
+    void 상_경로_반환_테스트() {
+        Piece piece = new Sang(Team.CHO);
+
+        Position from = new Position(1, 0);
+        Position to = new Position(4, 2);
+        Path actual = piece.getPath(from, to);
+
+        Path expect = new Path(List.of(
+                new Position(2, 0),
+                new Position(3, 1)
+        ));
+        assertThat(actual).isEqualTo(expect);
     }
 }
