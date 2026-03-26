7c51f117f35056919b45d20ff04c0144a9ccfe56
sun <sun007021@gmail.com>
2026-03-26 10:57:08 +0900

    feat: 사 이동 전략 구현 및 테스트 케이스 작성

--- src/main/java/domain/piece/Guard.java
+++ src/main/java/domain/piece/Guard.java
@@ -1,10 +1,34 @@
 package domain.piece;
 
+import domain.Position;
 import domain.Side;
+import domain.board.Board;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Guard extends Piece {
 
     public Guard(Side side) {
         super(side);
     }
+
+    @Override
+    public List<Position> getPossibleMoves(Board board, Position start) {
+        List<Position> possiblePositions = new ArrayList<>();
+        int[] dx = {0, -1, 1, 0};
+        int[] dy = {-1, 0, 0, 1};
+
+        for (int i = 0; i < 4; i++) {
+            int nRow = start.row() + dx[i];
+            int nCol = start.col() + dy[i];
+            Position destination = new Position(nCol, nRow);
+
+            if (board.isAvailableDestination(destination)) {
+                possiblePositions.add(destination);
+            }
+        }
+        
+        return possiblePositions;
+    }
+
 }
---
+++ src/test/java/domain/piece/GuardTest.java
@@ -0,0 +1,124 @@
+package domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.Position;
+import domain.Side;
+import domain.board.Board;
+import domain.board.BoardInitializer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GuardTest {
+    static class HanSideGuardInitializer implements BoardInitializer {
+
+        @Override
+        public Map<Position, Piece> initialize() {
+            Map<Position, Piece> piecesPosition = new HashMap<>();
+
+            piecesPosition.put(new Position(3, 0), new Guard(Side.HAN));
+            piecesPosition.put(new Position(3, 2), new Guard(Side.HAN));
+            piecesPosition.put(new Position(3, 4), new Guard(Side.HAN));
+            piecesPosition.put(new Position(3, 5), new Guard(Side.HAN));
+            piecesPosition.put(new Position(3, 6), new Guard(Side.HAN));
+            piecesPosition.put(new Position(3, 7), new Guard(Side.HAN));
+            piecesPosition.put(new Position(4, 6), new Guard(Side.HAN));
+            piecesPosition.put(new Position(2, 6), new Guard(Side.HAN));
+
+            piecesPosition.put(new Position(4, 7), new Guard(Side.CHU));
+
+            return piecesPosition;
+        }
+
+        @Override
+        public Side getFirstTurnSide() {
+            return Side.HAN;
+        }
+    }
+
+    static class ChuSideGuardInitializer implements BoardInitializer {
+
+        @Override
+        public Map<Position, Piece> initialize() {
+            Map<Position, Piece> piecesPosition = new HashMap<>();
+
+            piecesPosition.put(new Position(6, 0), new Guard(Side.CHU));
+            piecesPosition.put(new Position(6, 2), new Guard(Side.CHU));
+            piecesPosition.put(new Position(6, 4), new Guard(Side.CHU));
+            piecesPosition.put(new Position(6, 6), new Guard(Side.CHU));
+            piecesPosition.put(new Position(6, 7), new Guard(Side.CHU));
+
+            return piecesPosition;
+        }
+
+        @Override
+        public Side getFirstTurnSide() {
+            return Side.CHU;
+        }
+    }
+
+
+    @Test
+    @DisplayName("한나라 진영에서 사는 상/하/좌/우 4가지 방향으로 1 칸 이동 가능하다.")
+    void getHanPossibleMovesTest() {
+        // given
+        Board board = new Board(new HanSideGuardInitializer());
+        Position start = new Position(3, 0);
+        Piece guard = board.getPieceBy(start);
+
+        // when
+        List<Position> possibleMoves = guard.getPossibleMoves(board, start);
+
+        // then
+        assertThat(possibleMoves).contains(new Position(4, 0), new Position(2, 0), new Position(3, 1));
+    }
+
+    @Test
+    @DisplayName("초나라 진영에서 사는 상/하/좌/우 4가지 방향으로 1 칸 이동 가능하다.")
+    void getChuPossibleMovesTest() {
+        // given
+        Board board = new Board(new ChuSideGuardInitializer());
+        Position start = new Position(6, 0);
+        Piece guard = board.getPieceBy(start);
+
+        // when
+        List<Position> possibleMoves = guard.getPossibleMoves(board, start);
+
+        // then
+        assertThat(possibleMoves).contains(new Position(5, 0), new Position(7, 0), new Position(6, 1));
+    }
+
+    @Test
+    @DisplayName("사는 아군 기물이 있는 위치로 이동할 수 없다.")
+    void doesNotMoveTest() {
+        // given
+        Board board = new Board(new HanSideGuardInitializer());
+        Position start = new Position(3, 6);
+        Piece guard = board.getPieceBy(start);
+
+        // when
+        List<Position> possibleMoves = guard.getPossibleMoves(board, start);
+
+        // then
+        Assertions.assertThat(possibleMoves.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("사는 상대 기물이 있는 위치로 이동할 수 있다.")
+    void getChuPossibleMoves__Test() {
+        // given
+        Board board = new Board(new HanSideGuardInitializer());
+        Position start = new Position(3, 7);
+        Piece guard = board.getPieceBy(start);
+
+        // when
+        List<Position> possibleMoves = guard.getPossibleMoves(board, start);
+
+        // then
+        assertThat(possibleMoves).contains(new Position(4, 7));
+    }
+}
