d8238dfaa82aa3f90706920f749926bca16112ce
Huiyeongkim <huiyeong9619@naver.com>
2026-03-26 14:29:16 +0900

    feat: 마의 이동 규칙 구현 및 테스트 추가

--- src/main/java/domain/strategy/HorseMoveStrategy.java
+++ src/main/java/domain/strategy/HorseMoveStrategy.java
@@ -7,6 +7,32 @@ public class HorseMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean canMove(Position from, Position to, Board board) {
+        if (isNotCorrectPath(from, to))
+            return false;
+
+        int nx = to.getRow() - from.getRow();
+        int ny = to.getCol() - from.getCol();
+
+        if (board.isExistPosition(Position.of((from.getRow() + nx / 2), (from.getCol() + ny / 2)))) {
+            return false;
+        }
+        if (board.isAnotherTeam(from, to)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isNotCorrectPath(Position from, Position to) {
+        if (Math.abs(from.getRow() - to.getRow()) == 1) {
+            if (Math.abs(from.getCol() - to.getCol()) != 2) {
+                return true;
+            }
+        }
+        if (Math.abs(from.getRow() - to.getRow()) == 2) {
+            if (Math.abs(from.getCol() - to.getCol()) != 1)
+                return true;
+        }
         return false;
     }
 }
---
+++ src/test/java/domain/strategy/HorseMoveStrategyTest.java
@@ -0,0 +1,71 @@
+package domain.strategy;
+
+import domain.Board;
+import domain.vo.Position;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class HorseMoveStrategyTest {
+
+    @Test
+    @DisplayName("초나라 마의 이동 경로에 다른 기물이 없으면 정마 이동한다.")
+    void 초나라_마_정마_이동() {
+        // given
+        MoveStrategy strategy = new HorseMoveStrategy();
+        Board board = Board.of();
+
+        // when
+        Position from = Position.of(0, 2);
+        Position to = Position.of(2, 3);
+
+        // then
+        Assertions.assertTrue(strategy.canMove(from, to, board));
+    }
+
+    @Test
+    @DisplayName("한나라 마의 이동 경로에 다른 기물이 없으면 정마 이동한다.")
+    void 한나라_마_정마_이동() {
+        // given
+        MoveStrategy strategy = new HorseMoveStrategy();
+        Board board = Board.of();
+
+        // when
+        Position from = Position.of(9, 2);
+        Position to = Position.of(7, 3);
+
+        // then
+        Assertions.assertTrue(strategy.canMove(from, to, board));
+    }
+
+    @Test
+    @DisplayName("초나라 마의 이동 경로에 기물이 있으면 이동하지 않는다.")
+    void 초나라_마_이동_경로에_기물이_있으면_이동_불가() {
+        // given
+        MoveStrategy strategy = new ElephantMoveStrategy();
+        Board board = Board.of();
+
+        // when
+        Position from = Position.of(0, 2);
+        Position to = Position.of(1, 4);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(from, to, board));
+    }
+
+    @Test
+    @DisplayName("목적지에 같은 팀 기물이 있으면 이동하지 않는다.")
+    void 마_목적지에_같은_팀_기물이_있으면_이동_불가() {
+        // given
+        MoveStrategy strategy = new HorseMoveStrategy();
+        Board board = Board.of();
+
+        // when
+        Position from = Position.of(0, 2);
+        Position to = Position.of(2, 1);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(from, to, board));
+    }
+}
