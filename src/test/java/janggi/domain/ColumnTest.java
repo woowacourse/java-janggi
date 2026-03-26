ddde159c262df68c394baf61be30b4a17f5bf9d9
smiinii <sm020320@naver.com>
2026-03-26 17:43:19 +0900

    test: Row 및 Column 클래스 테스트 케이스 수정

--- src/test/java/janggi/domain/ColumnTest.java
+++ src/test/java/janggi/domain/ColumnTest.java
@@ -8,17 +8,17 @@ import org.junit.jupiter.api.Test;
 class ColumnTest {
     @Test
     void 장기판의_y좌표_범위를_벗어나면_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Column.of(11)).isInstanceOf(ColumnOutOfRangeException.class);
+        Assertions.assertThatThrownBy(() -> Column.of(10)).isInstanceOf(ColumnOutOfRangeException.class);
         Assertions.assertThatThrownBy(() -> Column.of(-1)).isInstanceOf(ColumnOutOfRangeException.class);
     }
 
     @Test
     void 장기판의_y좌표_범위_내라면_예외가_발생하지_않는다() {
-        Column column1 = Column.of(1);
-        Column column2 = Column.of(10);
+        Column column1 = Column.of(0);
+        Column column2 = Column.of(9);
 
-        Assertions.assertThat(column1.getColumn()).isEqualTo(1);
-        Assertions.assertThat(column2.getColumn()).isEqualTo(10);
+        Assertions.assertThat(column1.getColumn()).isEqualTo(0);
+        Assertions.assertThat(column2.getColumn()).isEqualTo(9);
     }
 
     @Test
--- src/test/java/janggi/domain/RowTest.java
+++ src/test/java/janggi/domain/RowTest.java
@@ -7,17 +7,17 @@ import org.junit.jupiter.api.Test;
 class RowTest {
     @Test
     void 장기판의_x좌표_범위를_벗어나면_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Row.of(10)).isInstanceOf(RowOutOfRangeException.class);
-        Assertions.assertThatThrownBy(() -> Row.of(0)).isInstanceOf(RowOutOfRangeException.class);
+        Assertions.assertThatThrownBy(() -> Row.of(9)).isInstanceOf(RowOutOfRangeException.class);
+        Assertions.assertThatThrownBy(() -> Row.of(-1)).isInstanceOf(RowOutOfRangeException.class);
     }
 
     @Test
     void 장기판의_x좌표_범위_내라면_예외가_발생하지_않는다() {
-        Row row1 = Row.of(1);
-        Row row2 = Row.of(9);
+        Row row1 = Row.of(0);
+        Row row2 = Row.of(8);
 
-        Assertions.assertThat(row1.getRow()).isEqualTo(1);
-        Assertions.assertThat(row2.getRow()).isEqualTo(9);
+        Assertions.assertThat(row1.getRow()).isEqualTo(0);
+        Assertions.assertThat(row2.getRow()).isEqualTo(8);
     }
 
     @Test
