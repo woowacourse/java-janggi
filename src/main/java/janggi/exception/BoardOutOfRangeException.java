5fcf6beac52fcffc799467142efa2469af6c9d99
구민서 <minseo12379@gmail.com>
2026-03-26 11:19:00 +0900

    feat: Row, Column 범위 예외 구현

--- src/main/java/janggi/exception/BoardOutOfRangeException.java
+++
@@ -1,7 +0,0 @@
-package janggi.exception;
-
-public class BoardOutOfRangeException extends BusinessException {
-    public BoardOutOfRangeException() {
-        super("장기판 범위를 벗어난 좌표입니다.");
-    }
-}
