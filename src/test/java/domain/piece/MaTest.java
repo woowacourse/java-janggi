2c5fcb7fa68ba009403ccad6f87bd876a4bf677a
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-26 20:04:53 +0900

    test(Ma): 이동계산 테스트 구현

---
+++ src/test/java/domain/piece/MaTest.java
@@ -0,0 +1,42 @@
+package domain.piece;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import domain.player.Team;
+import domain.position.Path;
+import domain.position.Position;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MaTest {
+    @ParameterizedTest
+    @MethodSource("provideMaPaths")
+    void 마는_앞으로_한칸_대각선으로_한칸_이동한다(int startX, int startY, int destX, int destY) {
+        Piece ma = new Ma(Team.HAN);
+        Position src = new Position(startX, startY);
+        Position dest = new Position(destX, destY);
+
+        Path path = ma.calculatePath(src, dest);
+
+        assertNotNull(path);
+        assertEquals(src, path.getSrc());
+        assertEquals(dest, path.getDest());
+        assertFalse(path.getWaypoints().isEmpty());
+    }
+
+    private static Stream<Arguments> provideMaPaths() {
+        return Stream.of(
+                Arguments.of(4, 4, 6, 6),
+                Arguments.of(4, 4, 2, 6),
+                Arguments.of(4, 4, 6, 2),
+                Arguments.of(4, 4, 2, 2),
+                Arguments.of(4, 4, 6, 5),
+                Arguments.of(4, 4, 6, 3),
+                Arguments.of(4, 4, 2, 5),
+                Arguments.of(4, 4, 2, 3)
+        );
+    }
+
+}
