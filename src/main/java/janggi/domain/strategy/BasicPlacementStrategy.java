9356cbc80a21755b6c2de4f2fd22c5cd537ebd78
Junha Park <poiu7825@pusan.ac.kr>
2026-03-26 16:38:51 +0900

    refactor: ResourceReader 추가로 인한 수정

--- src/main/java/janggi/domain/strategy/BasicPlacementStrategy.java
+++ src/main/java/janggi/domain/strategy/BasicPlacementStrategy.java
@@ -5,31 +5,36 @@ import janggi.domain.piece.PieceType;
 import janggi.domain.Position;
 import janggi.domain.Space;
 import janggi.domain.Team;
+import janggi.util.ResourceReader;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class BasicPlacementStrategy implements InitializeStrategy {
 
+    private static final String DEFAULT_PATH = "data/BasicPlacement.csv";
+
     @Override
     public void basicSetting(Map<Position, Space> blankBoard) {
         try {
-            File file = new File("resources\\data\\BasicPlacement.csv");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line = br.readLine(); // header skip
-            while((line = br.readLine()) != null) {
-                List<String> parts = List.of(line.split(","));
-                int x = Integer.parseInt(parts.get(0));
-                int y = Integer.parseInt(parts.get(1));
-                Team team = Team.from(parts.get(2));
-                PieceType pieceType = PieceType.from(parts.get(3));
-
-                blankBoard.put(new Position(x,y), PieceFactory.createPiece(team, pieceType));
-            }
+            BufferedReader br = ResourceReader.getBufferedReader(DEFAULT_PATH);
+            parseData(blankBoard, br);
         } catch (Exception e) {
             throw new IllegalArgumentException("기물 초기화 실패");
+        }
+    }
+
+    private void parseData(Map<Position, Space> blankBoard, BufferedReader br) throws IOException {
+        String line = br.readLine(); // header skip
+        while ((line = br.readLine()) != null) {
+            List<String> parts = List.of(line.split(","));
+            int x = Integer.parseInt(parts.get(0));
+            int y = Integer.parseInt(parts.get(1));
+            Team team = Team.from(parts.get(2));
+            PieceType pieceType = PieceType.from(parts.get(3));
+
+            blankBoard.put(new Position(x, y), PieceFactory.createPiece(team, pieceType));
         }
     }
 }
