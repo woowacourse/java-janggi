2c961a64c559fdcaf7ee5eb62a1ce502869a5125
dahxxn <dahyeonhan4529@gmail.com>
2026-03-26 17:32:12 +0900

    feat: 게임 오버 턴 상태 구현

---
+++ src/test/java/janggi/turn/GameOverTest.java
@@ -0,0 +1,26 @@
+package janggi.turn;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameOverTest {
+    @DisplayName("게임 종료 시 턴을 수행하고자 하면 예외 발생한다.")
+    @Test
+    void play() {
+        assertThatThrownBy(() -> new GameOver().play())
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("게임 종료 후 턴을 수행할 수 없습니다.");
+    }
+
+    @DisplayName("게임이 아직 끝나지 않았다.")
+    @Test
+    void isGameOver() {
+        assertThat(new GameOver().isGameOver())
+                .isTrue();
+    }
+
+
+}
