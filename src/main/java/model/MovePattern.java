9acc1070af6ccd4dfcb64e3a183c1706d331e976
심호경 <ghrudtla@naver.com>
2026-03-26 15:52:48 +0900

    feat: MoveRule 객체 생성

---
+++ src/main/java/model/MovePattern.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovePattern {
+    private List<Step> steps;
+
+    public MovePattern(){
+        this.steps = new ArrayList<>();
+    }
+
+}
---
+++ src/main/java/model/Step.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Step {
+    private final Direction direction;
+    private final boolean mustBeEmpty;
+
+    Step(Direction direction, boolean mustBeEmpty) {
+        this.direction = direction;
+        this.mustBeEmpty = mustBeEmpty;
+    }
+
+
+
+
+}
