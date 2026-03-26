3c9ee98a6814f0e8914ad336fc1e852215b7906a
KIM SEON WOO <sunwoo005@naver.com>
2026-03-26 16:53:32 +0900

    feat: implement right formation strategy

--- src/main/java/strategy/formation/RightFomrationStrategy.java
+++ src/main/java/strategy/formation/RightFomrationStrategy.java
@@ -1,4 +1,23 @@
 package strategy.formation;
 
-public class RightFomrationStrategy {
+import domain.Piece;
+import domain.PieceType;
+import domain.Position;
+import domain.TeamColor;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RightFomrationStrategy implements InitialFormationStrategy {
+
+    @Override
+    public Map<Position, Piece> setupFormation(TeamColor teamColor) {
+        Map<Position, Piece> formation = new HashMap<>();
+        int y = teamColor == TeamColor.CHO ? 9 : 0;
+
+        formation.put(Position.of(1, y), Piece.of(teamColor, PieceType.ELEPHANT));
+        formation.put(Position.of(2, y), Piece.of(teamColor, PieceType.HORSE));
+        formation.put(Position.of(6, y), Piece.of(teamColor, PieceType.ELEPHANT));
+        formation.put(Position.of(7, y), Piece.of(teamColor, PieceType.HORSE));
+        return formation;
+    }
 }
