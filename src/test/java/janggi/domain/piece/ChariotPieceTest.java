84ab71a4f70a5ffc164f1a89901c7f5c03d0d585
sinequanonn <dleck288@gmail.com>
2026-03-26 22:20:20 +0900

    test: 차 이동 가능 여부 판단 및 경로 테스트 작성

---
+++ src/test/java/janggi/domain/piece/ChariotPieceTest.java
@@ -0,0 +1,55 @@
+package janggi.domain.piece;
+
+import janggi.domain.board.Position;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChariotPieceTest {
+    @ParameterizedTest
+    @DisplayName("차는 상하좌우 직전이면 칸 수에 상관없이 이동 가능하다.")
+    @CsvSource({
+            "2, 3, 6, 4",
+            "8, 3, 9, 1",
+            "2, 3, 3, 6",
+            "8, 3, 2, 4",
+    })
+    void testNotMovableChariot(int preX, int preY, int nextX, int nextY) {
+        ChariotPiece chariotPiece = new ChariotPiece(Team.HAN);
+
+        Assertions.assertThat(chariotPiece.canMove(new Position(preX, preY), new Position(nextX, nextY)))
+                .isFalse();
+    }
+
+    @ParameterizedTest
+    @DisplayName("차는 상하좌우 직전으로 칸 수 상관없이 이동 가능하다.")
+    @CsvSource({
+            "2, 3, 5, 3",
+            "8, 3, 5, 3",
+            "2, 3, 2, 8",
+            "8, 3, 8, 1",
+    })
+    void testMoveChariot(int preX, int preY, int nextX, int nextY) {
+        ChariotPiece chariotPiece = new ChariotPiece(Team.HAN);
+
+        Assertions.assertThat(chariotPiece.canMove(new Position(preX, preY), new Position(nextX, nextY)))
+                .isTrue();
+    }
+
+    @Test
+    @DisplayName("차는 상하좌우 직전이면 칸 수에 상관없이 이동 가능하다.")
+    void testFindDestinationPath() {
+        Position from = new Position(2, 3);
+        Position to = new Position(5, 3);
+        ChariotPiece chariotPiece = new ChariotPiece(Team.HAN);
+
+        List<Position> result = chariotPiece.findPath(from, to);
+        assertThat(result).containsExactly(new Position(3, 3), new Position(4, 3), new Position(5, 3));
+    }
+}
