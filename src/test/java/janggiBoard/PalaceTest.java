096de15be041ae35dc48c4cc54eb753e0f0a3a4e
haeseon lee <haeseon4816@gmail.com>
2026-03-27 20:28:39 +0900

    test: 사 이동 가능 여부 테스트

--- src/test/java/janggiBoard/PalaceTest.java
+++ src/test/java/janggiBoard/PalaceTest.java
@@ -2,6 +2,7 @@ package janggiBoard;
 
 import domain.Position;
 import domain.Team;
+import domain.piece.Guard;
 import domain.piece.King;
 import domain.piece.PieceProvider;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,10 +17,12 @@ public class PalaceTest {
 
     private TestPieceProvider testBoard;
     private King king;
+    private Guard guard;
 
     @BeforeEach
     void setUp() {
         king = new King(Team.CHO);
+        guard = new Guard(Team.CHO);
         testBoard = new TestPieceProvider();
     }
 
@@ -44,6 +47,28 @@ public class PalaceTest {
         boolean isKingMove = king.canMove(currentPosition, targetPosition, testBoard);
         assertThat(isKingMove).isFalse();
     }
+
+    @Test
+    void 사가_목적지에_갈_수_있다() {
+        Position currentPosition = new Position(5, 5);
+        Position targetPosition = new Position(4, 5);
+
+        testBoard.setAllBlank();
+        boolean isGuardMove = guard.canMove(currentPosition, targetPosition, testBoard);
+        assertThat(isGuardMove).isTrue();
+    }
+
+    @Test
+    void 사가_목적지에_갈_수_없다() {
+        Position currentPosition = new Position(5, 5);
+        Position targetPosition = new Position(4, 5);
+
+        testBoard.setAllBlank();
+        testBoard.setBlank(new Position(4, 5));
+        boolean isGuardMove = guard.canMove(currentPosition, targetPosition, testBoard);
+        assertThat(isGuardMove).isFalse();
+    }
+
 
     private static class TestPieceProvider implements PieceProvider {
         private final Map<Position, Boolean> boardState = new HashMap<>();
