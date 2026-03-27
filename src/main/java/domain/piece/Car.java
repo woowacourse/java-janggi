10fb620006549ea2bd82e0c01c4685510dd2a557
haeseon lee <haeseon4816@gmail.com>
2026-03-27 22:17:16 +0900

    test: 차 이동 가능 여부 구현

--- src/main/java/domain/piece/Car.java
+++ src/main/java/domain/piece/Car.java
@@ -1,9 +1,28 @@
 package domain.piece;
 
+import domain.Position;
 import domain.Team;
+import domain.strategy.CarStrategy;
+import domain.strategy.MoveStrategy;
+
+import java.util.List;
 
 public class Car extends Piece {
+    private final MoveStrategy moveStrategy;
+
     public Car(Team team) {
         super(team);
+        this.moveStrategy = new CarStrategy();
+    }
+
+    public boolean canMove(Position currentPosition, Position targetPosition, PieceProvider pieceProvider) {
+        List<Position> moveCandidates = moveStrategy.getMoveCandidates(currentPosition, pieceProvider);
+
+        for (Position candidatePosition : moveCandidates) {
+            if (candidatePosition.equals(targetPosition)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
