d3fbf6a6cf56b28c82f67bf527a832e2ce584b1e
KIM SEON WOO <sunwoo005@naver.com>
2026-03-27 14:36:04 +0900

    feat : implements KingMoveStrategy

---
+++ src/main/java/strategy/move/KingMoveStrategy.java
@@ -0,0 +1,25 @@
+package strategy.move;
+
+import domain.Direction;
+import domain.MovePath;
+import domain.TeamColor;
+import java.util.List;
+
+public class KingMoveStrategy implements MoveStrategy {
+
+    private static final List<MovePath> PATHS = List.of(
+            new MovePath(List.of(Direction.NORTH)),
+            new MovePath(List.of(Direction.SOUTH)),
+            new MovePath(List.of(Direction.EAST)),
+            new MovePath(List.of(Direction.WEST)),
+            new MovePath(List.of(Direction.NORTH_EAST)),
+            new MovePath(List.of(Direction.NORTH_WEST)),
+            new MovePath(List.of(Direction.SOUTH_EAST)),
+            new MovePath(List.of(Direction.SOUTH_WEST))
+    );
+
+    @Override
+    public List<MovePath> getPaths(TeamColor teamColor) {
+        return PATHS;
+    }
+}
