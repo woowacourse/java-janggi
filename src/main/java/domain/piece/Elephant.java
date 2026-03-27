dcf8b75a4daccb3d962db77ae08c76eeb1a795fe
haeseon lee <haeseon4816@gmail.com>
2026-03-27 21:16:57 +0900

    feat: 상 이동 가능 여부 구현

--- src/main/java/domain/piece/Elephant.java
+++ src/main/java/domain/piece/Elephant.java
@@ -1,9 +1,28 @@
 package domain.piece;
 
+import domain.Position;
 import domain.Team;
+import domain.strategy.ElephantStrategy;
+import domain.strategy.MoveStrategy;
+
+import java.util.List;
 
 public class Elephant extends Piece {
+    private final MoveStrategy moveStrategy;
+
     public Elephant(Team team) {
         super(team);
+        this.moveStrategy = new ElephantStrategy();
+    }
+
+    public boolean canMove(Position currentPosition, Position targetPosition, PieceProvider pieceProvider) {
+        List<Position> moveCandidates = moveStrategy.getMoveCandidates(currentPosition, pieceProvider);
+
+        for (Position candidatePosition : moveCandidates) {
+            if (candidatePosition.equals(targetPosition)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
