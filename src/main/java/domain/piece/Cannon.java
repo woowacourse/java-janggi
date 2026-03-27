d4392b72472ef8959a4f45bb0031dd6861a87330
haeseon lee <haeseon4816@gmail.com>
2026-03-28 00:57:58 +0900

    feat: 포 이동 가능 여부 구현

--- src/main/java/domain/piece/Cannon.java
+++ src/main/java/domain/piece/Cannon.java
@@ -1,9 +1,27 @@
 package domain.piece;
 
+import domain.Position;
 import domain.Team;
+import domain.strategy.CannonStrategy;
+
+import java.util.List;
 
 public class Cannon extends Piece {
+
+    private final CannonStrategy cannonStrategy;
+
     public Cannon(Team team) {
         super(team);
+        cannonStrategy = new CannonStrategy();
+    }
+
+    public boolean canMove(Position currentPosition, Position targetPosition, PieceProvider pieceProvider) {
+        List<Position> moveCandidates = cannonStrategy.getMoveCandidates(currentPosition, pieceProvider);
+        for (Position candidatePosition : moveCandidates) {
+            if (candidatePosition.equals(targetPosition)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
