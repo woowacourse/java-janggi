918e0747c3a4395571b02dd9df4b15a1a6c1d017
Sumin <new.sumyang@gmail.com>
2026-03-27 17:27:29 +0900

    test: 기물 궁 테스트 추가

--- src/test/java/janggi/domain/piece/GungTest.java
+++ src/test/java/janggi/domain/piece/GungTest.java
@@ -3,63 +3,64 @@ package janggi.domain.piece;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import janggi.domain.side.TeamType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GungTest {
 
     @Test
-    @DisplayName("시작점에서 상하좌우 한 칸 이동할 수 있다.")
-    void canMoveStraightOneStep() {
+    @DisplayName("궁은 상하좌우 한 칸 이동할 수 있다.")
+    void isValidMovePatternStraightOneStep() {
         // given
-        Gung gung = new Gung();
+        Gung gung = new Gung(TeamType.CHU);
 
         // when & then
         assertAll(
-            () -> assertThat(gung.canMove(4, 4, 4, 5)).isTrue(),
-            () -> assertThat(gung.canMove(4, 4, 4, 3)).isTrue(),
-            () -> assertThat(gung.canMove(4, 4, 5, 4)).isTrue(),
-            () -> assertThat(gung.canMove(4, 4, 3, 4)).isTrue()
+            () -> assertThat(gung.isValidMovePattern(4, 4, 4, 5)).isTrue(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 4, 3)).isTrue(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 5, 4)).isTrue(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 3, 4)).isTrue()
         );
     }
 
     @Test
-    @DisplayName("시작점에서 대각선 한 칸 이동할 수 있다.")
-    void canMoveDiagonalOneStep() {
+    @DisplayName("궁은 대각선 한 칸 이동할 수 있다.")
+    void isValidMovePatternDiagonalOneStep() {
         // given
-        Gung gung = new Gung();
+        Gung gung = new Gung(TeamType.CHU);
 
         // when & then
         assertAll(
-            () -> assertThat(gung.canMove(4, 4, 5, 5)).isTrue(),
-            () -> assertThat(gung.canMove(4, 4, 5, 3)).isTrue(),
-            () -> assertThat(gung.canMove(4, 4, 3, 5)).isTrue(),
-            () -> assertThat(gung.canMove(4, 4, 3, 3)).isTrue()
+            () -> assertThat(gung.isValidMovePattern(4, 4, 5, 5)).isTrue(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 5, 3)).isTrue(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 3, 5)).isTrue(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 3, 3)).isTrue()
         );
     }
 
     @Test
-    @DisplayName("시작점에서 두 칸 이상 이동할 수 없다.")
+    @DisplayName("궁은 두 칸 이상 이동할 수 없다.")
     void cannotMoveOverOneStep() {
         // given
-        Gung gung = new Gung();
+        Gung gung = new Gung(TeamType.CHU);
 
         // when & then
         assertAll(
-            () -> assertThat(gung.canMove(4, 4, 6, 4)).isFalse(),
-            () -> assertThat(gung.canMove(4, 4, 6, 6)).isFalse(),
-            () -> assertThat(gung.canMove(4, 4, 4, 6)).isFalse()
+            () -> assertThat(gung.isValidMovePattern(4, 4, 6, 4)).isFalse(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 6, 6)).isFalse(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 4, 6)).isFalse(),
+            () -> assertThat(gung.isValidMovePattern(4, 4, 2, 4)).isFalse()
         );
-
     }
 
     @Test
-    @DisplayName("제자리로는 이동할 수 없다.")
+    @DisplayName("궁은 제자리로 이동할 수 없다.")
     void cannotMoveSamePosition() {
         // given
-        Gung gung = new Gung();
+        Gung gung = new Gung(TeamType.CHU);
 
         // when & then
-        assertThat(gung.canMove(4, 4, 4, 4)).isFalse();
+        assertThat(gung.isValidMovePattern(4, 4, 4, 4)).isFalse();
     }
 }
