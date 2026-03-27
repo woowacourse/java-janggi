f67b4c8b5d25da9c2f5c49eb846b012faadff46a
haeseon lee <haeseon4816@gmail.com>
2026-03-27 22:17:25 +0900

    test: 차 이동 가능 여부 테스트

---
+++ src/test/java/janggiBoard/CarTest.java
@@ -0,0 +1,67 @@
+package janggiBoard;
+
+import domain.Position;
+import domain.Team;
+import domain.piece.Car;
+import domain.piece.PieceProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarTest {
+
+    private Car car;
+    private TestPieceProvider testBoard;
+
+    @BeforeEach
+    public void setUp() {
+        car = new Car(Team.CHO);
+        testBoard = new TestPieceProvider();
+    }
+
+    @Test
+    void 차는_목적지에_갈_수_있다() {
+        Position currentPosition = new Position(5, 5);
+        Position targetPosition = new Position(7, 5);
+
+        testBoard.setAllBlank();
+        boolean isCarMove = car.canMove(currentPosition, targetPosition, testBoard);
+        assertThat(isCarMove).isTrue();
+    }
+
+    @Test
+    void 차는_목적지에_갈_수_없다() {
+        Position currentPosition = new Position(5, 5);
+        Position targetPosition = new Position(7, 5);
+
+        testBoard.setAllBlank();
+        testBoard.setBlank(new Position(6, 5));
+
+        boolean isCarMove = car.canMove(currentPosition, targetPosition, testBoard);
+
+        assertThat(isCarMove).isFalse();
+    }
+
+
+    private static class TestPieceProvider implements PieceProvider {
+        private final Map<Position, Boolean> boardState = new HashMap<>();
+        private boolean defaultState = true;
+
+        void setBlank(Position pos) {
+            boardState.put(pos, false);
+        }
+
+        void setAllBlank() {
+            this.defaultState = true;
+        }
+
+        @Override
+        public boolean isBlank(Position position) {
+            return boardState.getOrDefault(position, defaultState);
+        }
+    }
+}
