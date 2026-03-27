2d1168224351b7b1a98e566cca1bbfa80d47a593
JYL35 <dlwodbs1223@naver.com>
2026-03-27 15:21:02 +0900

    test(General): 장 이동 전략 테스트 추가

--- src/test/java/janggi/domain/piece/strategy/MoveStrategyTest.java
+++ src/test/java/janggi/domain/piece/strategy/MoveStrategyTest.java
@@ -45,6 +45,21 @@ class MoveStrategyTest {
     }
 
     @Nested
+    class 장_이동_테스트 {
+
+        @DisplayName("장은는 현재 위치에서 앞, 뒤, 양 옆을 1칸씩의 좌표를 도착지점 후보로 반환한다")
+        @Test
+        void 장는_앞뒤_양옆으로_움직인다() {
+            MoveStrategy strategy = new GeneralStrategy();
+            List<Path> paths = strategy.findMovablePaths(Position.of(4, 4));
+            assertThat(paths.get(0).destination()).isEqualTo(Position.of(5, 4));
+            assertThat(paths.get(1).destination()).isEqualTo(Position.of(3, 4));
+            assertThat(paths.get(2).destination()).isEqualTo(Position.of(4, 5));
+            assertThat(paths.get(3).destination()).isEqualTo(Position.of(4, 3));
+        }
+    }
+
+    @Nested
     class 포_이동_테스트 {
 
         @Test
