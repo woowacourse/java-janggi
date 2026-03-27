f3f8b11ab36dbf60c0b34a17ae6996de5f07e214
EveryPine <starcraft819@gmail.com>
2026-03-28 00:54:50 +0900

    refactor(position): 매직 넘버를 상수로 교체

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -41,13 +41,15 @@ public final class Position {
 
     private static void validateRowRange(final int row) {
         if (row < MINIMUM_ROW || row > MAXIMUM_ROW) {
-            throw new IllegalArgumentException("행 입력은 1~10을 입력해야 합니다.");
+            throw new IllegalArgumentException(
+                String.format("행 값은 %d ~ %d사이의 정수여야 합니다.", MINIMUM_ROW, MAXIMUM_ROW));
         }
     }
 
     private static void validateColumnRange(final int column) {
         if (column < MINIMUM_COLUMN || column > MAXIMUM_COLUMN) {
-            throw new IllegalArgumentException("열 입력은 1~9을 입력해야 합니다.");
+            throw new IllegalArgumentException(
+                String.format("열 값은 %d ~ %d사이의 정수여야 합니다.", MINIMUM_COLUMN, MAXIMUM_COLUMN));
         }
     }
 
