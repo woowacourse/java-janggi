2e8a69d9d6bd31a31319250e3642a1ebade2d1f0
Jinriro <riro12.hanyang.ac.kr>
2026-03-27 15:31:42 +0900

    refactor: 사 이동 전략 최적화

--- src/main/java/domain/piece/Guard.java
+++ src/main/java/domain/piece/Guard.java
@@ -1,5 +1,6 @@
 package domain.piece;
 
+import domain.Direction;
 import domain.Position;
 import domain.Side;
 import domain.board.Board;
@@ -15,20 +16,24 @@ public class Guard extends Piece {
     @Override
     public List<Position> getPossibleMoves(Board board, Position start) {
         List<Position> possiblePositions = new ArrayList<>();
-        int[] dx = {0, -1, 1, 0};
-        int[] dy = {-1, 0, 0, 1};
 
-        for (int i = 0; i < 4; i++) {
-            int nRow = start.row() + dx[i];
-            int nCol = start.col() + dy[i];
-            Position destination = new Position(nCol, nRow);
+        List<Direction> directions = List.of(
+                Direction.UP,
+                Direction.DOWN,
+                Direction.LEFT,
+                Direction.RIGHT
+        );
 
-            if (board.isAvailableDestination(destination)) {
-                possiblePositions.add(destination);
+        for (Direction direction : directions) {
+            Position destination = start.nextPosition(direction);
+
+            if (!board.isAvailableDestination(destination)) {
+                continue;
             }
+
+            possiblePositions.add(destination);
         }
-        
+
         return possiblePositions;
     }
-
 }
