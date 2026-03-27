ca342732b319914932620047040ba258c9a5c4a0
ghkdtjfxkd <syt1128@gmail.com>
2026-03-27 18:01:34 +0900

    chore: 컨트롤러 스켈레톤 추가

--- src/main/java/controller/JanggiController.java
+++ src/main/java/controller/JanggiController.java
@@ -1,4 +1,54 @@
 package controller;
 
+import domain.JanggiGame;
+import domain.Position;
+import view.InputView;
+import view.OutputView;
+
 public class JanggiController {
+
+    private final JanggiGame janggiGame;
+
+    public JanggiController(JanggiGame janggiGame) {
+        this.janggiGame = janggiGame;
+    }
+
+    public void run() {
+        while (!janggiGame.isGameFinished()) {
+            playGame();
+        }
+
+        String s = janggiGame.gameStatus();
+        System.out.println(s);
+    }
+
+    private void playGame() {
+        OutputView.printBoard(janggiGame.allFactors());
+        OutputView.printCurrentPlayerTurn(janggiGame.currnetPlayerTurn());
+        execute(this::playerPhase);
+    }
+
+    private void playerPhase() {
+        String input = InputView.selectPiecePosition();
+        String[] split = input.split(", ");
+        Position selected = Position.of(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+
+        input = InputView.selectTargerPosition();
+        split = input.split(", ");
+        Position target = Position.of(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+
+        janggiGame.move(selected, target);
+    }
+
+
+    private void execute(ExecutableTask task) {
+        while (true) {
+            try {
+                task.execute();
+                return;
+            } catch (IllegalArgumentException e) {
+                e.getMessage();
+            }
+        }
+    }
 }
