f1ee429945939d3cc0b6063cc5b73c954bb3315c
picetea44 <picetea44@gmail.com>
2026-03-27 19:22:46 +0900

    test: 궁,사 테스트 케이스 작성

--- src/test/java/domain/activePiece/GeneralTest.java
+++ src/test/java/domain/activePiece/GeneralTest.java
@@ -3,9 +3,13 @@ package domain.activePiece;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import domain.Column;
 import domain.Position;
+import domain.Row;
 import domain.piece.Piece;
 import domain.piece.Team;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class GeneralTest {
@@ -20,6 +24,31 @@ class GeneralTest {
         Piece general = new General(Team.HAN);
         assertThat(general.canMove(new Position(5,5), new Position(6,6))).isFalse();
     }
+
+    @Test
+    void 궁_정상_경로_출력_한다() {
+        ActivePiece general = new General(Team.HAN);
+
+        Position src = new Position(new Row(1), new Column(3));
+        Position dest = new Position(new Row(2), new Column(3));
+        List<Position> routes = new ArrayList<>(
+                List.of(new Position(new Row(1), new Column(3)), new Position(new Row(2), new Column(3))));
+
+        assertThat(general.searchRoute(src,dest)).isEqualTo(routes);
+    }
+
+    @Test
+    void 궁_비정상_경로_출력_한다() {
+        ActivePiece general = new General(Team.HAN);
+
+        Position src = new Position(new Row(1), new Column(3));
+        Position dest = new Position(new Row(2), new Column(3));
+        List<Position> routes = new ArrayList<>(
+                List.of(new Position(new Row(1), new Column(3)), new Position(new Row(3), new Column(3))));
+
+        assertThat(general.searchRoute(src,dest)).isNotEqualTo(routes);
+    }
+
 
 
 }
--- src/test/java/domain/activePiece/GuardTest.java
+++ src/test/java/domain/activePiece/GuardTest.java
@@ -3,9 +3,13 @@ package domain.activePiece;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import domain.Column;
 import domain.Position;
+import domain.Row;
 import domain.piece.Piece;
 import domain.piece.Team;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class GuardTest {
@@ -19,6 +23,29 @@ class GuardTest {
     void 정상_범위가_아니면_거짓() {
         Piece guard = new Guard(Team.HAN);
         assertThat(guard.canMove(new Position(5,5), new Position(8,8))).isFalse();
+    }
+    @Test
+    void 사_정상_경로_출력_한다() {
+        ActivePiece guard = new Guard(Team.HAN);
+
+        Position src = new Position(new Row(1), new Column(3));
+        Position dest = new Position(new Row(2), new Column(3));
+        List<Position> routes = new ArrayList<>(
+                List.of(new Position(new Row(1), new Column(3)), new Position(new Row(2), new Column(3))));
+
+        assertThat(guard.searchRoute(src,dest)).isEqualTo(routes);
+    }
+
+    @Test
+    void 사_비정상_경로_출력_한다() {
+        ActivePiece guard = new Guard(Team.HAN);
+
+        Position src = new Position(new Row(1), new Column(3));
+        Position dest = new Position(new Row(2), new Column(3));
+        List<Position> routes = new ArrayList<>(
+                List.of(new Position(new Row(1), new Column(3)), new Position(new Row(3), new Column(3))));
+
+        assertThat(guard.searchRoute(src,dest)).isNotEqualTo(routes);
     }
 
 
