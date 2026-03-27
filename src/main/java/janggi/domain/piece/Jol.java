a3d72c4e378d76d33ed5e102ac4cc5f4a12e51ec
jyt6640 <jyt6640@gmail.com>
2026-03-27 15:56:12 +0900

    refactor: 이동 검증 기능 구분화

--- src/main/java/janggi/domain/piece/Jol.java
+++ src/main/java/janggi/domain/piece/Jol.java
@@ -16,19 +16,15 @@ public class Jol extends BasePiece {
 
     @Override
     public List<Point> getRoute(Point from, Point to) {
-        int pathX = to.getX() - from.getX();
-        int pathY = to.getY() - from.getY();
-
-        int signY = Integer.compare(pathY, 0);
+        int pathX = Point.getPathX(from, to);
+        int pathY = Point.getPathY(from, to);
         int distanceX = abs(pathX);
         int distanceY = abs(pathY);
 
-        if (distanceX > MAX_DISTANCE || distanceY > MAX_DISTANCE || (distanceX + distanceY > MAX_DISTANCE)) {
-            throw new IllegalArgumentException();
-        }
-        if ((team.equals(Team.CHO) && signY < 0) || (team.equals(Team.HAN) && signY > 0)) {
-            throw new IllegalArgumentException();
-        }
+        validateNoMove(distanceX, distanceY);
+        validateOverMove(distanceX, distanceY);
+        validateBackMove(pathY);
+
         return List.of(to);
     }
 
@@ -41,6 +37,24 @@ public class Jol extends BasePiece {
     @Override
     public PieceType getType() {
         return PieceType.JOL;
+    }
+
+    private void validateNoMove(int distanceX, int distanceY) {
+        if (distanceX == 0 && distanceY == 0) {
+            throw new IllegalArgumentException("제자리 이동은 불가능합니다.");
+        }
+    }
+
+    private void validateOverMove(int distanceX, int distanceY) {
+        if (distanceX + distanceY != 1) {
+            throw new IllegalArgumentException("한 칸만 이동할 수 있습니다.");
+        }
+    }
+
+    private void validateBackMove(int pathY) {
+        if ((team == Team.CHO && pathY < 0) || (team == Team.HAN && pathY > 0)) {
+            throw new IllegalArgumentException("뒤로 이동할 수 없습니다.");
+        }
     }
 }
 
