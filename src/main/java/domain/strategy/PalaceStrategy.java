9b9a2cda94be5fad18c785e99d9669f05f347926
haeseon lee <haeseon4816@gmail.com>
2026-03-27 19:57:15 +0900

    feat: 궁, 사가 규칙대로 이동 구현

--- src/main/java/domain/strategy/PalaceStrategy.java
+++ src/main/java/domain/strategy/PalaceStrategy.java
@@ -3,12 +3,25 @@ package domain.strategy;
 import domain.Position;
 import domain.piece.PieceProvider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PalaceStrategy implements MoveStrategy {
 
     @Override
-    public List<Position> getMoveCandidates(Position source, PieceProvider board) {
-        return List.of();
+    public List<Position> getMoveCandidates(Position currentPosition, PieceProvider board) {
+        List<Position> candidates = new ArrayList<>();
+
+        Direction[] directions = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST,
+                Direction.NORTH_EAST, Direction.NORTH_WEST, Direction.SOUTH_EAST, Direction.SOUTH_WEST};
+
+        for (Direction direction : directions) {
+            int targetRow = currentPosition.getRows() + direction.getRowOffset();
+            int targetColumns = currentPosition.getColumns() + direction.getColOffset();
+
+            Position targetPosition = new Position(targetRow, targetColumns);
+            candidates.add(targetPosition);
+        }
+        return candidates;
     }
 }
