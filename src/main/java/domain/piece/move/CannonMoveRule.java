87c8986f3c87bb9e979b45c147c39b50693f5981
koreaioi <koreaioi@gachon.ac.kr>
2026-03-27 18:28:03 +0900

    feat(domain): 포 이동 가능 여부를 구현

---
+++ src/main/java/domain/piece/move/CannonMoveRule.java
@@ -0,0 +1,111 @@
+package domain.piece.move;
+
+import domain.intersection.Intersection;
+import domain.piece.PieceType;
+import domain.point.Point;
+
+import java.util.List;
+
+import static domain.piece.move.Vector.*;
+import static domain.piece.move.Vector.LEFT;
+import static domain.piece.move.Vector.RIGHT;
+
+public class CannonMoveRule extends MoveRule{
+
+    public CannonMoveRule() {
+        super(PieceType.CANNON, initializeDirections());
+    }
+
+    public boolean support(PieceType pieceType) {
+        return this.pieceType == pieceType;
+    }
+
+    public List<Point> findPossiblePoints(Intersection from, Intersection to) {
+        return directions.findPoints(from.getPoint(), to.getPoint());
+    }
+
+    public boolean checkMoveRule(Intersection from, List<Intersection> path) {
+        Intersection to = path.getLast();
+        validateIsSameTeam(from, to); // 도착지가 같은 팀인지 확인
+        validateObstacleCondition(from, path); // 장애물 검사
+        validateDestinationIsNotCannon(from, to); // 도착지가 포인지 확인한다.
+        return true;
+    }
+
+    // 같은 팀 인지 확인
+    private void validateIsSameTeam(Intersection from, Intersection to) {
+        if (from.isSameTeam(to)) {
+            throw new IllegalArgumentException("같은 팀의 위치로 이동할 수 없습니다.");
+        }
+    }
+
+    private void validateObstacleCondition(Intersection from, List<Intersection> path) {
+        List<Intersection> obstacles = path.subList(0, path.size() - 1).stream()
+                .filter(Intersection::hasPiece)
+                .toList();
+
+        validateObstacleIsOnly(obstacles);
+        validateObstacleIsNotCannon(from, obstacles);
+    }
+
+    private void validateObstacleIsOnly(List<Intersection> list) {
+        if (list.size() != 1) {
+            throw new IllegalArgumentException("포는 반드시 기물 하나를 넘어야 합니다.");
+        }
+    }
+
+    private static void validateObstacleIsNotCannon(Intersection from, List<Intersection> list) {
+        if (list.getFirst().isSamePiece(from)) {
+            throw new IllegalArgumentException("포는 포를 넘어갈 수 없습니다.");
+        }
+    }
+
+    private void validateDestinationIsNotCannon(Intersection from, Intersection to) {
+        if (from.isSamePiece(to)) {
+            throw new IllegalArgumentException("포는 포를 공격할 수 없습니다.");
+        }
+    }
+
+    public static Directions initializeDirections() {
+        return new Directions(List.of(
+                new Direction(List.of(UP)),
+                new Direction(List.of(UP, UP)),
+                new Direction(List.of(UP, UP, UP)),
+                new Direction(List.of(UP, UP, UP, UP)),
+                new Direction(List.of(UP, UP, UP, UP, UP)),
+                new Direction(List.of(UP, UP, UP, UP, UP, UP)),
+                new Direction(List.of(UP, UP, UP, UP, UP, UP, UP)),
+                new Direction(List.of(UP, UP, UP, UP, UP, UP, UP, UP)),
+                new Direction(List.of(UP, UP, UP, UP, UP, UP, UP, UP, UP)),
+
+                new Direction(List.of(DOWN)),
+                new Direction(List.of(DOWN, DOWN)),
+                new Direction(List.of(DOWN, DOWN, DOWN)),
+                new Direction(List.of(DOWN, DOWN, DOWN, DOWN)),
+                new Direction(List.of(DOWN, DOWN, DOWN, DOWN, DOWN)),
+                new Direction(List.of(DOWN, DOWN, DOWN, DOWN, DOWN, DOWN)),
+                new Direction(List.of(DOWN, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN)),
+                new Direction(List.of(DOWN, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN)),
+                new Direction(List.of(DOWN, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN)),
+
+                new Direction(List.of(RIGHT)),
+                new Direction(List.of(RIGHT, RIGHT)),
+                new Direction(List.of(RIGHT, RIGHT, RIGHT)),
+                new Direction(List.of(RIGHT, RIGHT, RIGHT, RIGHT)),
+                new Direction(List.of(RIGHT, RIGHT, RIGHT, RIGHT, RIGHT)),
+                new Direction(List.of(RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT)),
+                new Direction(List.of(RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT)),
+                new Direction(List.of(RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT)),
+
+                new Direction(List.of(LEFT)),
+                new Direction(List.of(LEFT, LEFT)),
+                new Direction(List.of(LEFT, LEFT, LEFT)),
+                new Direction(List.of(LEFT, LEFT, LEFT, LEFT)),
+                new Direction(List.of(LEFT, LEFT, LEFT, LEFT, LEFT)),
+                new Direction(List.of(LEFT, LEFT, LEFT, LEFT, LEFT, LEFT)),
+                new Direction(List.of(LEFT, LEFT, LEFT, LEFT, LEFT, LEFT, LEFT)),
+                new Direction(List.of(LEFT, LEFT, LEFT, LEFT, LEFT, LEFT, LEFT, LEFT))
+        ));
+    }
+
+}
---
+++ src/test/java/domain/rule/CannonMoveRuleTest.java
@@ -0,0 +1,166 @@
+package domain.rule;
+
+import domain.intersection.Intersection;
+import domain.piece.Cannon;
+import domain.piece.Chariot;
+import domain.piece.Team;
+import domain.piece.move.CannonMoveRule;
+import domain.point.Point;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class CannonMoveRuleTest {
+
+    final Point start = new Point(0, 0);
+    final Point middlePoint1 = new Point(1, 0);
+    final Point middlePoint2 = new Point(2, 0);
+    final Point middlePoint3 = new Point(3, 0);
+    final Point middlePoint4 = new Point(4, 0);
+    final Point middlePoint5 = new Point(5, 0);
+    final Point middlePoint6 = new Point(6, 0);
+    final Point middlePoint7 = new Point(7, 0);
+    final Point middlePoint8 = new Point(8, 0);
+    final Point end = new Point(9, 0);
+
+    final Intersection intersection1 = Intersection.empty(middlePoint1);
+    final Intersection intersection2 = Intersection.empty(middlePoint2);
+    final Intersection intersection3 = Intersection.empty(middlePoint3);
+    final Intersection intersection4 = Intersection.empty(middlePoint4);
+    final Intersection intersection5 = Intersection.empty(middlePoint5);
+    final Intersection intersection6 = Intersection.empty(middlePoint6);
+    final Intersection intersection7 = Intersection.empty(middlePoint7);
+    final Intersection intersection8 = Intersection.empty(middlePoint8);
+
+    @Test
+    @DisplayName("도착지에 같은 팀이 있는 경우 예외가 발생한다.")
+    void shouldThrowExceptionWhenDestinationIsSameTeam() {
+        Team sameTeam = Team.CHO;
+
+        Intersection from = new Intersection(start, new Cannon(sameTeam));
+        Intersection onlyObstacleIntersection = new Intersection(middlePoint5, new Cannon(sameTeam));
+        Intersection to = new Intersection(end, new Chariot(sameTeam));
+
+        CannonMoveRule cannonMoveRule = new CannonMoveRule();
+
+        Assertions.assertThatThrownBy(() -> {
+                    cannonMoveRule.checkMoveRule(from, List.of(intersection1,
+                            intersection2,
+                            intersection3,
+                            intersection4,
+                            onlyObstacleIntersection,
+                            intersection6,
+                            intersection7,
+                            intersection8,
+                            to));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("같은 팀의 위치로 이동할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("포의 이동경로 장애물이 포이면 예외가 발생한다.")
+    void shouldThrowExceptionWhenObstacleIsCannon() {
+        Team sameTeam = Team.CHO;
+        Team anotherTeam = Team.HAN;
+
+        Intersection from = new Intersection(start, new Cannon(sameTeam));
+        Intersection cannonObstacle = new Intersection(middlePoint7, new Cannon(anotherTeam));
+        Intersection to = Intersection.empty(end);
+
+        CannonMoveRule cannonMoveRule = new CannonMoveRule();
+
+        Assertions.assertThatThrownBy(() -> {
+                    cannonMoveRule.checkMoveRule(from, List.of(intersection1,
+                            intersection2,
+                            intersection3,
+                            intersection4,
+                            intersection5,
+                            intersection6,
+                            cannonObstacle,
+                            intersection8,
+                            to));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("포는 포를 넘어갈 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("포는 두개의 장애물을 넘어갈 때 예외가 발생한다.")
+    void shouldThrowExceptionWhenCannonJumpTwoObstacle() {
+        Team sameTeam = Team.CHO;
+        Team anotherTeam = Team.HAN;
+
+        Intersection from = new Intersection(start, new Cannon(sameTeam));
+        Intersection obstacle1 = new Intersection(middlePoint5, new Chariot(anotherTeam));
+        Intersection obstacle2 = new Intersection(middlePoint6, new Chariot(anotherTeam));
+        Intersection to = Intersection.empty(end);
+
+        CannonMoveRule cannonMoveRule = new CannonMoveRule();
+
+        Assertions.assertThatThrownBy(() -> {
+                    cannonMoveRule.checkMoveRule(from, List.of(intersection1,
+                            intersection2,
+                            intersection3,
+                            intersection4,
+                            obstacle1,
+                            obstacle2,
+                            intersection7,
+                            intersection8,
+                            to));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("포는 반드시 기물 하나를 넘어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("포의 경로에 장애물이 없을 때 예외가 발생한다.")
+    void shouldThrowExceptionWhenCannonPathDoesntObstacle() {
+        Team sameTeam = Team.CHO;
+
+        Intersection from = new Intersection(start, new Cannon(sameTeam));
+        Intersection to = Intersection.empty(end);
+
+        CannonMoveRule cannonMoveRule = new CannonMoveRule();
+
+        Assertions.assertThatThrownBy(() -> {
+                    cannonMoveRule.checkMoveRule(from, List.of(intersection1,
+                            intersection2,
+                            intersection3,
+                            intersection4,
+                            intersection5,
+                            intersection6,
+                            intersection7,
+                            intersection8,
+                            to));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("포는 반드시 기물 하나를 넘어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("포가 포를 공격할때 예외가 발생한다.")
+    void shouldThrowExceptionWhenCannonAttackCannon() {
+        Team sameTeam = Team.CHO;
+        Team anotherTeam = Team.HAN;
+
+        Intersection from = new Intersection(start, new Cannon(sameTeam));
+        Intersection obstacle = new Intersection(middlePoint6, new Chariot(sameTeam));
+        Intersection to = new Intersection(end, new Cannon(anotherTeam));
+
+
+        CannonMoveRule cannonMoveRule = new CannonMoveRule();
+
+        Assertions.assertThatThrownBy(() -> {
+                    cannonMoveRule.checkMoveRule(from, List.of(intersection1,
+                            intersection2,
+                            intersection3,
+                            intersection4,
+                            intersection5,
+                            obstacle,
+                            intersection7,
+                            intersection8,
+                            to));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("포는 포를 공격할 수 없습니다.");
+    }
+
+}
