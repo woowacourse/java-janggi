ce053d5896d468ebfa126c5d5acec31195c88c81
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-27 23:25:19 +0900

    refactor(GameStatusInfo): UI 책임 분리

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -5,13 +5,13 @@ import java.util.Map;
 import java.util.function.Function;
 
 public enum PieceType {
-    CHA("차"),
-    PHO("포"),
-    MA("마"),
-    SANG("상"),
-    JANG("장"),
-    SA("사"),
-    JOL("졸");
+    CHA,
+    PHO,
+    MA,
+    SANG,
+    JANG,
+    SA,
+    JOL;
 
     private static final Map<PieceType, Function<Team, Piece>> FACTORY = Map.of(
             PieceType.CHA, Cha::new,
@@ -23,17 +23,7 @@ public enum PieceType {
             PieceType.JOL, Jol::new
     );
 
-    private final String name;
-
-    PieceType(String name) {
-        this.name = name;
-    }
-
     public static Piece createPiece(Team team, PieceType pieceType) {
         return FACTORY.get(pieceType).apply(team);
-    }
-
-    public String getName() {
-        return name;
     }
 }
--- src/main/java/janggi/presentation/dto/GameStatusInfo.java
+++ src/main/java/janggi/presentation/dto/GameStatusInfo.java
@@ -1,12 +1,24 @@
 package janggi.presentation.dto;
 
 import janggi.domain.piece.Piece;
+import janggi.domain.piece.PieceType;
 import janggi.domain.status.Team;
 import java.util.List;
+import java.util.Map;
 
 public record GameStatusInfo(
         List<List<String>> pieces
 ) {
+    private static final Map<PieceType, String> DISPLAY_NAMES = Map.of(
+            PieceType.CHA, "차",
+            PieceType.PHO, "포",
+            PieceType.MA, "마",
+            PieceType.SANG, "상",
+            PieceType.JOL, "졸",
+            PieceType.SA, "사",
+            PieceType.JANG, "장"
+    );
+
     public static GameStatusInfo from(List<List<Piece>> board) {
         return new GameStatusInfo(
                 board.stream()
@@ -23,9 +35,9 @@ public record GameStatusInfo(
                         return "\u001B[0m+";
                     }
                     if (point.isSameTeam(Team.CHO)) {
-                        return "\u001B[32m" + point.getType().getName() + "\u001B[0m";
+                        return "\u001B[32m" + DISPLAY_NAMES.get(point.getType()) + "\u001B[0m";
                     }
-                    return "\u001B[31m" + point.getType().getName() + "\u001B[0m";
+                    return "\u001B[31m" + DISPLAY_NAMES.get(point.getType()) + "\u001B[0m";
                 }).toList();
     }
 }
