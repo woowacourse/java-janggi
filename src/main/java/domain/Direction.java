d9013886872c1ce4ed0dd5fac5e9eb717d665114
sun <sun007021@gmail.com>
2026-03-27 13:06:40 +0900

    refactor: 이동 방향 Direction Enum 생성

---
+++ src/main/java/domain/Direction.java
@@ -0,0 +1,22 @@
+package domain;
+
+public enum Direction {
+
+    UP(-1, 0), DOWN(1, 0), LEFT(0, -1), RIGHT(0, 1),
+    UP_LEFT(-1, -1), UP_RIGHT(-1, 1), DOWN_LEFT(1, -1), DOWN_RIGHT(1, 1);
+
+    private final int col, row;
+
+    Direction(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
