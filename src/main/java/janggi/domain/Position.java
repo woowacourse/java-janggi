5777b550d9510a67aecc87bbfec5415629319468
haeyoon1 <hynam1220@naver.com>
2026-03-27 17:42:27 +0900

    feat: 중복 메서드 제거

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -14,18 +14,4 @@ public record Position(
     public int deltaY(Position other) {
         return other.y - y;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Position position = (Position) o;
-        return x == position.x && y == position.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
 }
