3ac905c3c9f8f04080602822d86e01ba4bc47901
bee9827 <ehfrhfo9494@naver.com>
2026-03-27 21:06:26 +0900

    feat: Path 유틸리티 메서드 추가

--- src/main/java/janggi/domain/board/coordinate/Path.java
+++ src/main/java/janggi/domain/board/coordinate/Path.java
@@ -1,12 +1,13 @@
 package janggi.domain.board.coordinate;
 
 import janggi.domain.piece.Pattern;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Path {
     private final List<Point> path;
 
-    private Path(List<Point> path) {
+    public Path(List<Point> path) {
         this.path = path;
     }
 
@@ -16,5 +17,20 @@ public class Path {
 
     public List<Point> getPath() {
         return path;
+    }
+
+    public boolean isEmpty(){
+        return path.isEmpty();
+    }
+
+    public Path cutUntil(Point point) {
+        List<Point> curPath = new ArrayList<>();
+        for (Point pathPoint : path) {
+            curPath.add(pathPoint);
+            if (pathPoint.equals(point)) {
+                break;
+            }
+        }
+        return new Path(curPath);
     }
 }
