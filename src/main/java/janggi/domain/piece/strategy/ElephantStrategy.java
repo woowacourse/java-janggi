69fbc211323e9b2b5826c9a950ebd0c45a94d196
SooHyeon Lee <dalsu4594@naver.com>
2026-03-27 12:40:55 +0900

    feat: 마, 상 이동 전략 추가
    - 마는 한칸 대각으로 8방향으로 이동
    - 상을 한칸 대각 대각으로 8방향 이동

--- src/main/java/janggi/domain/piece/strategy/ElephantStrategy.java
+++ src/main/java/janggi/domain/piece/strategy/ElephantStrategy.java
@@ -8,6 +8,39 @@ import java.util.List;
 public class ElephantStrategy implements MoveStrategy{
     @Override
     public List<Path> findMovablePaths(Position current) {
-        return List.of();
+        return List.of(
+                new Path(
+                        List.of(Position.of(current.row() + 1, current.column()),
+                                Position.of(current.row() + 2, current.column() + 1)),
+                        Position.of(current.row() + 3, current.column() + 2)),
+                new Path(
+                        List.of(Position.of(current.row(), current.column() + 1),
+                                Position.of(current.row() + 1, current.column() + 2)),
+                        Position.of(current.row() + 2, current.column() + 3)),
+                new Path(
+                        List.of(Position.of(current.row(), current.column() + 1),
+                                Position.of(current.row() - 1, current.column() + 2)),
+                        Position.of(current.row() - 2, current.column() + 3)),
+                new Path(
+                        List.of(Position.of(current.row() - 1, current.column()),
+                                Position.of(current.row() - 2, current.column() + 1)),
+                        Position.of(current.row() - 3, current.column() + 2)),
+                new Path(
+                        List.of(Position.of(current.row() + 1, current.column()),
+                                Position.of(current.row() + 2, current.column() - 1)),
+                        Position.of(current.row() + 3, current.column() - 2)),
+                new Path(
+                        List.of(
+                                Position.of(current.row(), current.column() - 1),
+                                Position.of(current.row() + 1, current.column() - 2)),
+                        Position.of(current.row() + 2, current.column() - 3)),
+                new Path(
+                        List.of(Position.of(current.row(), current.column() - 1),
+                                Position.of(current.row() - 1, current.column() - 2)),
+                        Position.of(current.row() - 2, current.column() - 3)),
+                new Path(
+                        List.of(Position.of(current.row() - 1, current.column()),
+                                Position.of(current.row() - 2, current.column() - 1)),
+                        Position.of(current.row() - 3, current.column() - 2)));
     }
 }
--- src/main/java/janggi/domain/piece/strategy/HorseStrategy.java
+++ src/main/java/janggi/domain/piece/strategy/HorseStrategy.java
@@ -8,6 +8,22 @@ import java.util.List;
 public class HorseStrategy implements MoveStrategy{
     @Override
     public List<Path> findMovablePaths(Position current) {
-        return List.of();
+        return List.of(
+                new Path(List.of(Position.of(current.row() + 1, current.column())),
+                        Position.of(current.row() + 2, current.column() + 1)),
+                new Path(List.of(Position.of(current.row(), current.column() + 1)),
+                        Position.of(current.row() + 1, current.column() + 2)),
+                new Path(List.of(Position.of(current.row(), current.column() + 1)),
+                        Position.of(current.row() - 1, current.column() + 2)),
+                new Path(List.of(Position.of(current.row() - 1, current.column())),
+                        Position.of(current.row() - 2, current.column() + 1)),
+                new Path(List.of(Position.of(current.row() + 1, current.column())),
+                        Position.of(current.row() + 2, current.column() - 1)),
+                new Path(List.of(Position.of(current.row(), current.column() - 1)),
+                        Position.of(current.row() + 1, current.column() - 2)),
+                new Path(List.of(Position.of(current.row(), current.column() - 1)),
+                        Position.of(current.row() - 1, current.column() - 2)),
+                new Path(List.of(Position.of(current.row() - 1, current.column())),
+                        Position.of(current.row() - 2, current.column() - 1)));
     }
 }
