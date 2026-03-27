bb9ec07794930c605be4afdeb78518855229270a
JYL35 <dlwodbs1223@naver.com>
2026-03-27 13:10:07 +0900

    feat(Chariot): 차 이동 전략 구현

--- src/main/java/janggi/domain/piece/strategy/ChariotStrategy.java
+++ src/main/java/janggi/domain/piece/strategy/ChariotStrategy.java
@@ -3,11 +3,52 @@ package janggi.domain.piece.strategy;
 import janggi.domain.Path;
 import janggi.domain.Position;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ChariotStrategy implements MoveStrategy{
     @Override
     public List<Path> findMovablePaths(Position current) {
-        return List.of();
+        int currentRow = current.row();
+        int currentCol = current.column();
+        List<Path> paths = new ArrayList<>();
+
+        //row
+        for (int i = current.row() + 1; i <= 9; i++) {
+            List<Position> route = new ArrayList<>();
+            for (int row = current.row() + 1; row <= i; row++) {
+                route.add(Position.of(row, currentCol));
+            }
+            Position destination = route.removeLast();
+            paths.add(new Path(List.copyOf(route), destination));
+        }
+        for (int i = current.row() - 1; i >= 0; i--) {
+            List<Position> route = new ArrayList<>();
+            for (int row = current.row() - 1; row >= i; row--) {
+                route.add(Position.of(row, currentCol));
+            }
+            Position destination = route.removeLast();
+            paths.add(new Path(List.copyOf(route), destination));
+        }
+
+        //column
+        for (int i = current.column() + 1; i < 9; i++) {
+            List<Position> route = new ArrayList<>();
+            for (int col = current.column() + 1; col <= i; col++) {
+                route.add(Position.of(currentRow, col));
+            }
+            Position destination = route.removeLast();
+            paths.add(new Path(List.copyOf(route), destination));
+        }
+        for (int i = current.column() - 1; i >= 0; i--) {
+            List<Position> route = new ArrayList<>();
+            for (int col = current.column() - 1; col >= i; col--) {
+                route.add(Position.of(currentRow, col));
+            }
+            Position destination = route.removeLast();
+            paths.add(new Path(List.copyOf(route), destination));
+        }
+        return Collections.unmodifiableList(paths);
     }
 }
