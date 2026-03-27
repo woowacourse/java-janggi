a4ab7b09238ca250fa21f4db7b730f74dcb620cb
bee9827 <ehfrhfo9494@naver.com>
2026-03-27 21:08:11 +0900

    fix: Side 예외처리 수정

--- src/main/java/janggi/domain/side/Side.java
+++ src/main/java/janggi/domain/side/Side.java
@@ -6,9 +6,6 @@ public enum Side {
     NONE,
     ;
     public static boolean isSameSide(Side firstSide, Side secondSide){
-        if(NONE.equals(firstSide) || NONE.equals(secondSide)){
-            throw new IllegalStateException("NONE Side 상태입니다.");
-        }
         return firstSide.equals(secondSide);
     }
     
