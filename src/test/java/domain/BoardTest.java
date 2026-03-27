3490407fc87c33ded9c65c82c75ff499a1108b80
Huiyeongkim <huiyeong9619@naver.com>
2026-03-27 19:23:40 +0900

    test: Board 생성 테스트 변경

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -1,51 +1,71 @@
 package domain;
 
 import domain.vo.Position;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoardTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("providePiece")
     @DisplayName("보드를 생성하면 기물들 초기화된다.")
-    void 보드_생성() {
+    void 보드_생성(int row, int col, Type expected) {
         // given
         // when
+        Board board = BoardFactory.setUp();
+
         // then
-        assertDoesNotThrow(() -> BoardFactory.setUp());
+        Piece piece = board.findPieceByPosition(Position.of(row, col)).get();
+        assertEquals(expected, piece.getType());
     }
 
     @Test
-    @DisplayName("기물의 직선 이동 경로에 다른 기물이 없으면 이동한다.")
-    void 기물_직선_이동() {
+    @DisplayName("기물의 이동 경로에 다른 기물이 없으면 이동한다.")
+    void 기물_이동() {
         // given
-        Board board = BoardFactory.setUp();
+        Map<Position, Piece> tempBoard = new HashMap<>();
+        tempBoard.put(Position.of(0, 0), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+        tempBoard.put(Position.of(0, 3), Piece.of(Team.CHU, Type.SOLIDER, new FixedMoveStrategy()));
+
+        Board board = Board.of(tempBoard);
 
         // when
         Position from = Position.of(0, 0);
-        Position to = Position.of(1, 0);
+        Position to = Position.of(3, 0);
         board.move(from, to);
 
         // then
         Piece findPiece = board.findPieceByPosition(to).get();
-        Assertions.assertEquals(Type.CHARIOT, findPiece.getType());
+        assertEquals(Type.SOLIDER, findPiece.getType());
     }
 
-    @Test
-    @DisplayName("기물의 대각선 이동 경로에 다른 기물이 없으면 이동한다.")
-    void 기물_대각선_이동() {
-        // given
-        Board board = BoardFactory.setUp();
-        Position to = Position.of(3, 3);
-
-        // when
-        board.move(Position.of(0, 1), Position.of(1, 1));
-        // board.diagonalMove(Position.of(1, 1), to);
-
-        // then
-        // assertEquals(Type.ELEPHANT, board.findPieceByPosition(to).getType());
+    private static Stream<Arguments> providePiece() {
+        return Stream.of(
+                Arguments.of(0, 0, Type.CHARIOT),
+                Arguments.of(0, 1, Type.ELEPHANT),
+                Arguments.of(0, 2, Type.HORSE),
+                Arguments.of(0, 3, Type.GUARD),
+                Arguments.of(0, 8, Type.CHARIOT),
+                Arguments.of(1, 4, Type.GENERAL),
+                Arguments.of(2, 1, Type.CANNON),
+                Arguments.of(3, 0, Type.SOLIDER),
+                Arguments.of(3, 4, Type.SOLIDER),
+                Arguments.of(6, 2, Type.SOLIDER),
+                Arguments.of(7, 1, Type.CANNON),
+                Arguments.of(8, 4, Type.GENERAL),
+                Arguments.of(9, 0, Type.CHARIOT),
+                Arguments.of(9, 3, Type.GUARD),
+                Arguments.of(9, 7, Type.HORSE),
+                Arguments.of(9, 8, Type.CHARIOT)
+        );
     }
 }
