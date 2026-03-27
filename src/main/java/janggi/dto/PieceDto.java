b518d4cf8debc5de1aecae2509885fd94ebd77a2
sinequanonn <dleck288@gmail.com>
2026-03-27 21:30:19 +0900

    remove: 사용하지 않는 DTO 제거

--- src/main/java/janggi/dto/PieceDto.java
+++
@@ -1,9 +0,0 @@
-package janggi.dto;
-
-import janggi.domain.piece.Piece;
-
-public record PieceDto(String pieceName, String teamName) {
-    public static PieceDto from(Piece piece) {
-        return new PieceDto(piece.getPieceName(), piece.getTeamName());
-    }
-}
--- src/main/java/janggi/dto/PositionDto.java
+++
@@ -1,9 +0,0 @@
-package janggi.dto;
-
-import janggi.domain.board.Position;
-
-public record PositionDto(int x, int y) {
-    public static PositionDto from(Position position) {
-        return new PositionDto(position.getX(), position.getY());
-    }
-}
