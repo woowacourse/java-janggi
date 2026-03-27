0e76ae1f713051bfc23c7f9897c16cbf6f817903
haeseon lee <haeseon4816@gmail.com>
2026-03-27 20:38:47 +0900

    feat: 졸 이동 전략 기능 구현

--- src/main/java/domain/strategy/PawnStrategy.java
+++ src/main/java/domain/strategy/PawnStrategy.java
@@ -3,12 +3,24 @@ package domain.strategy;
 import domain.Position;
 import domain.piece.PieceProvider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PawnStrategy implements MoveStrategy {
 
     @Override
-    public List<Position> getMoveCandidates(Position source, PieceProvider board) {
-        return List.of();
+    public List<Position> getMoveCandidates(Position currentPosition, PieceProvider board) {
+        List<Position> candidates = new ArrayList<>();
+
+        Direction[] directions = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
+
+        for (Direction direction : directions) {
+            int targetRow = currentPosition.getRows() + direction.getRowOffset();
+            int targetColumns = currentPosition.getColumns() + direction.getColOffset();
+
+            Position targetPosition = new Position(targetRow, targetColumns);
+            candidates.add(targetPosition);
+        }
+        return candidates;
     }
 }
