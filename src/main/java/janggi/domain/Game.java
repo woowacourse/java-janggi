49fb551245a0b22523987ec886a16cd4ce0888c9
gleaming9 <gleaming9@khu.ac.kr>
2026-03-27 14:14:29 +0900

    feat: 턴 관리 책임을 갖는 게임 클래스를 구현

---
+++ src/main/java/janggi/domain/Game.java
@@ -0,0 +1,22 @@
+package janggi.domain;
+
+import janggi.domain.board.Board;
+import janggi.domain.turn.ChoTurn;
+import janggi.domain.turn.PlayerTurn;
+import janggi.initializer.BoardInitializer;
+
+public class Game {
+    private PlayerTurn playerTurn;
+
+    public Game(Arrangement choArrangement, Arrangement hanArrangement) {
+        this.playerTurn = new ChoTurn(new Board(BoardInitializer.createBoard(choArrangement, hanArrangement)));
+    }
+
+    public void move(Position start, Position end){
+        playerTurn = playerTurn.move(start, end);
+    }
+
+    public boolean isFinished(){
+        return playerTurn.isFinished();
+    }
+}
