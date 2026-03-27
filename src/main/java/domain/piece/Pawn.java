896a27dfc2bcc0a1b17c43988a9462a4954e7516
haeseon lee <haeseon4816@gmail.com>
2026-03-27 20:39:03 +0900

    feat: 졸 이동 가능 여부 구현

--- src/main/java/domain/piece/Pawn.java
+++ src/main/java/domain/piece/Pawn.java
@@ -1,9 +1,29 @@
 package domain.piece;
 
+import domain.Position;
 import domain.Team;
+import domain.strategy.MoveStrategy;
+import domain.strategy.PawnStrategy;
+
+import java.util.List;
 
 public class Pawn extends Piece{
+    private final MoveStrategy moveStrategy;
+
     public Pawn(Team team) {
         super(team);
+        this.moveStrategy = new PawnStrategy();
+    }
+
+    public boolean canMove(Position currentPosition, Position targetPosition, PieceProvider pieceProvider) {
+        List<Position> moveCandidates = moveStrategy.getMoveCandidates(currentPosition, pieceProvider);
+        boolean isTargetPositionBlank = pieceProvider.isBlank(targetPosition);
+
+        for (Position candidatePosition : moveCandidates) {
+            if (candidatePosition.equals(targetPosition) && isTargetPositionBlank) {
+                return true;
+            }
+        }
+        return false;
     }
 }
