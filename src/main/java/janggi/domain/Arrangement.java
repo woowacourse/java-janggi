3ca7371cae75dd4ca111e4edff4071f32727214f
gleaming9 <gleaming9@khu.ac.kr>
2026-03-27 21:01:10 +0900

    fix: 오타 수정 - openningSetup을 openingSetup으로 변경

--- src/main/java/janggi/domain/Arrangement.java
+++ src/main/java/janggi/domain/Arrangement.java
@@ -8,17 +8,17 @@ public enum Arrangement {
     상마마상("상마마상"),
     상마상마("상마상마");
 
-    private final String openningSetup;
+    private final String openingSetup;
 
     private static final String INVALID_OPENING_TEXT = "존재하지 않는 배치입니다.";
 
-    Arrangement(String openningSetup) {
-        this.openningSetup = openningSetup;
+    Arrangement(String openingSetup) {
+        this.openingSetup = openingSetup;
     }
 
-    public static Arrangement from(String openning) {
+    public static Arrangement from(String opening) {
         return Arrays.stream(Arrangement.values())
-                .filter(arrangement -> arrangement.openningSetup.equals(openning))
+                .filter(arrangement -> arrangement.openingSetup.equals(opening))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_OPENING_TEXT));
     }
