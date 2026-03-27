e16f42b1ef0e45b11ecd542fab00a7bb8f59c548
haeseon lee <haeseon4816@gmail.com>
2026-03-27 21:43:50 +0900

    test: 차 이동 기능 구현

--- src/main/java/domain/strategy/CarStrategy.java
+++ src/main/java/domain/strategy/CarStrategy.java
@@ -3,12 +3,41 @@ package domain.strategy;
 import domain.Position;
 import domain.piece.PieceProvider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarStrategy implements MoveStrategy {
 
     @Override
-    public List<Position> getMoveCandidates(Position source, PieceProvider board) {
-        return List.of();
+    public List<Position> getMoveCandidates(Position currentPosition, PieceProvider board) {
+        List<Position> candidates = new ArrayList<>();
+        Direction[] straightDirections = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
+
+        for (Direction direction : straightDirections) {
+            addPathCandidates(currentPosition, direction, board, candidates);
+        }
+
+        return candidates;
+    }
+
+    private void addPathCandidates(Position currentPosition, Direction direction, PieceProvider board, List<Position> candidatePositions) {
+        Position next = currentPosition;
+
+        while (true) {
+            int nextRows = next.getRows() + direction.getRowOffset();
+            int nextColumns = next.getColumns() + direction.getColOffset();
+
+            if (nextRows < 0 || nextRows >= 10 || nextColumns < 0 || nextColumns >= 9) {
+                break;
+            }
+
+            next = new Position(nextRows, nextColumns);
+            if (board.isBlank(next)) {
+                candidatePositions.add(next);
+                continue;
+            }
+            candidatePositions.add(next);
+            break;
+        }
     }
 }
