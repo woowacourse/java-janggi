42226d82f61e7af410ace10532bba937a855cdc1
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-27 15:33:13 +0900

    refactor(main): 안내 멘트 출력 이동

--- src/main/java/janggi/JanggiApplication.java
+++ src/main/java/janggi/JanggiApplication.java
@@ -24,8 +24,8 @@ public class JanggiApplication {
         JanggiGame game = new JanggiGame(board);
 
         OutputView.printGameStatus(GameStatusInfo.from(game.getBoardStatus()));
+        OutputView.printStartGame();
         while (!game.isFinished()) {
-            OutputView.printStartGame();
             List<Point> points = InputView.readPoints();
             game.play(points.get(0), points.get(1));
             OutputView.printGameStatus(GameStatusInfo.from(game.getBoardStatus()));
