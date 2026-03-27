65233694a0fbfda9a3c24ae83cc7265529a402f0
gleaming9 <gleaming9@khu.ac.kr>
2026-03-27 15:29:27 +0900

    fix: 보드 초기화 테스트에서 잘못된 보드 크기 상수를 수정

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -1,29 +1,46 @@
 package janggi.domain;
 
+import java.util.List;
+
 public record Position(int x, int y) {
     public static final int BOARD_START_ROWS = 1;
     public static final int BOARD_START_COLS = 1;
     public static final int BOARD_END_ROWS = 10;
     public static final int BOARD_END_COLS = 9;
 
-    public Position(int x, int y) {
-        validate();
-        this.x = x;
-        this.y = y;
+    public static Position from(List<String> inputs) {
+        try {
+            List<Integer> parsedInputs = getParsedInputs(inputs);
+            int r = parsedInputs.getFirst();
+            int c = parsedInputs.getLast();
+
+            validate(r, c);
+            return new Position(r, c);
+        } catch(Exception e) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
     }
 
-    private void validate() {
-        validateRow();
-        validateCol();
+    private static List<Integer> getParsedInputs(List<String> inputs) {
+        List<Integer> parsedInputs = inputs.stream().map(Integer::parseInt).toList();
+        if (parsedInputs.size() != 2) {
+            throw new IllegalArgumentException("행과 열 두 개의 값이 필요합니다.");
+        }
+        return parsedInputs;
     }
 
-    private void validateRow() {
+    private static void validate(int x, int y) {
+        validateRow(x);
+        validateCol(y);
+    }
+
+    private static void validateRow(int x) {
         if(x < BOARD_START_ROWS || x > BOARD_END_ROWS){
             throw new IllegalArgumentException("유효하지 않은 위치입니다. 행은 1부터 10까지 가능합니다.");
         }
     }
 
-    private void validateCol() {
+    private static void validateCol(int y) {
         if(y < BOARD_START_COLS || y > BOARD_END_COLS) {
             throw new IllegalArgumentException("유효하지 않은 위치입니다. 열은 1부터 9까지 가능합니다.");
         }
