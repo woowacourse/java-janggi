10d776e8e782789a8f281e82dcdc58669eaee0c9
sun <sun007021@gmail.com>
2026-03-27 13:07:05 +0900

    refactor: 다음 이동 위치 계산 책임 분리

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -1,4 +1,8 @@
 package domain;
 
 public record Position(int col, int row) {
+
+    public Position nextPosition(Direction direction) {
+        return new Position(col + direction.getCol(), row + direction.getRow());
+    }
 }
