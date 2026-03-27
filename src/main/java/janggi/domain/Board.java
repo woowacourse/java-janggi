6308a29231d75048db2fe065b8d87b5f8037b258
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-27 09:37:29 +0900

    refactor(Board): 필드 변수명 수정

--- src/main/java/janggi/domain/Board.java
+++ src/main/java/janggi/domain/Board.java
@@ -14,21 +14,21 @@ public class Board {
 
     private static final int BOARD_HEIGHT = 10;
 
-    private final Map<Point, Piece> state;
+    private final Map<Point, Piece> pieces;
 
     public Board() {
-        this.state = new LinkedHashMap<>();
+        this.pieces = new LinkedHashMap<>();
     }
 
     public void init(List<PositionInfo> positionInfos) {
-        positionInfos.forEach(info -> state.put(info.point(), info.piece()));
+        positionInfos.forEach(info -> pieces.put(info.point(), info.piece()));
     }
 
     public void move(Point from, Point to, Team team) {
         validateFromPoint(from, team);
         validateToPoint(to, team);
-        Piece piece = state.get(from);
-        Piece targetPiece = state.get(to);
+        Piece piece = pieces.get(from);
+        Piece targetPiece = pieces.get(to);
         if (targetPiece != null && !piece.canCapture(targetPiece)) {
             throw new IllegalArgumentException("[ERROR] 이 기물은 해당 타겟을 잡을 수 없습니다.");
         }
@@ -36,12 +36,12 @@ public class Board {
         if (!piece.canMove(getPieces(route))) {
             throw new IllegalArgumentException("[ERROR] 해당 기물의 이동 경로에 장애물이 있거나 규칙에 어긋납니다.");
         }
-        state.remove(from);
-        state.put(to, piece);
+        pieces.remove(from);
+        pieces.put(to, piece);
     }
 
     public boolean isKingDie(Team team) {
-        return state.values().stream()
+        return pieces.values().stream()
                 .noneMatch(piece -> piece.isSameType(PieceType.JANG) &&
                         piece.isSameTeam(team));
     }
@@ -51,7 +51,7 @@ public class Board {
         for (int i = 0; i < BOARD_HEIGHT; i++) {
             pieces.add(
                     Point.getRow(i).stream()
-                            .map(state::get)
+                            .map(this.pieces::get)
                             .toList()
             );
         }
@@ -60,7 +60,7 @@ public class Board {
 
     public List<Piece> getPieces(List<Point> point) {
         return point.stream()
-                .map(state::get)
+                .map(pieces::get)
                 .filter(Objects::nonNull)
                 .toList();
     }
@@ -81,10 +81,10 @@ public class Board {
     }
 
     private boolean isSameTeam(Point point, Team team) {
-        return state.get(point).isSameTeam(team);
+        return pieces.get(point).isSameTeam(team);
     }
 
     private boolean isEmptyPoint(Point point) {
-        return state.get(point) == null;
+        return pieces.get(point) == null;
     }
 }
