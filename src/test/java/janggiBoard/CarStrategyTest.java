5dde166a702255e220af9c453c869e81f638210f
haeseon lee <haeseon4816@gmail.com>
2026-03-27 21:43:59 +0900

    test: 차 이동 기능 테스트

---
+++ src/test/java/janggiBoard/CarStrategyTest.java
@@ -0,0 +1,73 @@
+package janggiBoard;
+
+import domain.Position;
+import domain.piece.PieceProvider;
+import domain.strategy.CarStrategy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarStrategyTest {
+
+    private CarStrategy carStrategy;
+    private TestPieceProvider testBoard;
+
+    @BeforeEach
+    void setUp() {
+        carStrategy = new CarStrategy();
+        testBoard = new TestPieceProvider();
+    }
+
+    @Test
+    void 차는_상하좌우_직선_모든칸_후보로_반환한다() {
+        Position position = new Position(5, 4);
+        testBoard.setAllBlank();
+
+        List<Position> candidates = carStrategy.getMoveCandidates(position, testBoard);
+
+        assertThat(candidates).hasSize(17);
+        assertThat(candidates).contains(new Position(0, 4),
+                new Position(9, 4),
+                new Position(5, 0),
+                new Position(5, 8));
+    }
+
+    @Test
+    void 이동경로에_장애물_있으면_그_지점까지_이동후_전진하지_않는다() {
+        Position position = new Position(5, 4);
+        testBoard.setAllBlank();
+
+        Position obstacle = new Position(3, 4);
+        testBoard.setBlank(obstacle);
+
+        List<Position> candidates = carStrategy.getMoveCandidates(position, testBoard);
+
+        assertThat(candidates).contains(new Position(4, 4), new Position(3, 4));
+        assertThat(candidates).doesNotContain(new Position(2, 4),
+                new Position(1, 4),
+                new Position(0, 4));
+    }
+
+    private static class TestPieceProvider implements PieceProvider {
+        private final Map<Position, Boolean> boardState = new HashMap<>();
+        private boolean defaultState = true;
+
+        void setBlank(Position position) {
+            boardState.put(position, false);
+        }
+
+        void setAllBlank() {
+            this.defaultState = true;
+        }
+
+        @Override
+        public boolean isBlank(Position position) {
+            return boardState.getOrDefault(position, defaultState);
+        }
+    }
+}
