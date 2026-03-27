9be1b8efe2149482190979e2425e79bab38204b7
SooHyeon Lee <dalsu4594@naver.com>
2026-03-27 17:49:23 +0900

    test: Path 단위 테스트 추가
    - 경유지에 포지션이 있는지 확인 테스트 추가
    - 도착지에 포지션이 있는지 확인 테스트 추가

---
+++ src/test/java/janggi/domain/PathTest.java
@@ -0,0 +1,60 @@
+package janggi.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PathTest {
+    @Test
+    void 포지션이_도착지면_true를_반환한다(){
+        Path path = new Path(List.of(Position.of(1, 2), Position.of(2, 2)),
+                Position.of(3, 3));
+
+        boolean hasDestination = path.hasDestination(Position.of(3, 3));
+
+        assertThat(hasDestination).isEqualTo(true);
+    }
+
+    @Test
+    void 포지션이_도착지가_아니면_false를_반환한다(){
+        Path path = new Path(List.of(Position.of(1, 2), Position.of(2, 2)),
+                Position.of(3, 3));
+
+        boolean hasDestination = path.hasDestination(Position.of(2, 3));
+
+        assertThat(hasDestination).isEqualTo(false);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2",
+            "2, 2"
+    })
+    void 포지션이_경유지면_true_반환한다(int row, int column){
+        Path path = new Path(List.of(Position.of(1, 2), Position.of(2, 2)),
+                Position.of(3, 3));
+
+        boolean hasRoute = path.hasRoute(Position.of(row, column));
+
+        assertThat(hasRoute).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 4",
+            "3, 5"
+    })
+    void 포지션이_경유지가_아니면_false_반환한다(int row, int column){
+        Path path = new Path(List.of(Position.of(1, 2), Position.of(2, 2)),
+                Position.of(3, 3));
+
+        boolean hasRoute = path.hasRoute(Position.of(row, column));
+
+        assertThat(hasRoute).isEqualTo(false);
+    }
+}
