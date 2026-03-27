247ebbf8610fbb99ec5b4ed8c4af5d6aeff18df8
sun <sun007021@gmail.com>
2026-03-27 13:07:58 +0900

    refactor: 마 이동 전략 최적화

--- src/main/java/domain/piece/Horse.java
+++ src/main/java/domain/piece/Horse.java
@@ -1,5 +1,6 @@
 package domain.piece;
 
+import domain.Direction;
 import domain.board.Board;
 import domain.Position;
 import domain.Side;
@@ -15,39 +16,29 @@ public class Horse extends Piece {
 
     @Override
     public List<Position> getPossibleMoves(Board board, Position start) {
+
+        List<List<Direction>> paths = List.of(
+                List.of(Direction.UP, Direction.UP_LEFT),
+                List.of(Direction.UP, Direction.UP_RIGHT),
+                List.of(Direction.DOWN, Direction.DOWN_LEFT),
+                List.of(Direction.DOWN, Direction.DOWN_RIGHT),
+                List.of(Direction.LEFT, Direction.UP_LEFT),
+                List.of(Direction.LEFT, Direction.DOWN_LEFT),
+                List.of(Direction.RIGHT, Direction.UP_RIGHT),
+                List.of(Direction.RIGHT, Direction.DOWN_RIGHT));
+
         List<Position> possiblePositions = new ArrayList<>();
-        int[] dx = {0, -1, 1, 0};
-        int[] dy = {-1, 0, 0, 1};
 
-        int[] secondMoveDirections = {-1, 1};
-
-        for (int i = 0; i < 4; i++) {
-            int nRow = start.row() + dx[i];
-            int nCol = start.col() + dy[i];
-            Position firstMovePosition = new Position(nCol, nRow);
-            if (!board.isAvailableDestination(firstMovePosition)) {
+        for (List<Direction> path : paths) {
+            Position firstMovePosition = start.nextPosition(path.getFirst());
+            if (board.isInvalidRange(firstMovePosition) || !board.isEmpty(firstMovePosition)) {
                 continue;
             }
-            if (!board.isEmpty(firstMovePosition)) { // 2차
-                continue;
-            }
-            for (int secondMoveDirection : secondMoveDirections) {
-                if (dx[i] == 0) {
-                    int nRow2 = nRow + secondMoveDirection;
-                    int nCol2 = nCol + dy[i];
-                    Position destination = new Position(nCol2, nRow2);
-                    if (board.isAvailableDestination(destination)) {
-                        possiblePositions.add(destination);
-                    }
-                }
-                if (dy[i] == 0) {
-                    int nRow2 = nRow + dx[i];
-                    int nCol2 = nCol + secondMoveDirection;
-                    Position destination = new Position(nCol2, nRow2);
-                    if (board.isAvailableDestination(destination)) {
-                        possiblePositions.add(destination);
-                    }
-                }
+
+            Position destination = firstMovePosition.nextPosition(path.get(1));
+
+            if (board.isAvailableDestination(destination)) {
+                possiblePositions.add(destination);
             }
         }
 
