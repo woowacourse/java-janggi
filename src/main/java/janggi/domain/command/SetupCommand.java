4e2caf35935b294e889c233e22e2977344c18ea2
EveryPine <starcraft819@gmail.com>
2026-03-28 01:00:57 +0900

    refactor: 클래스 인스턴스 변수 개수를 2개 이내로 제한

--- src/main/java/janggi/domain/command/SetupCommand.java
+++ src/main/java/janggi/domain/command/SetupCommand.java
@@ -10,28 +10,26 @@ import java.util.function.Supplier;
 
 public enum SetupCommand {
 
-    INNER_ELEPHANT(1, "안상 차림", InnerElephantSetupPolicy::new),
-    OUTER_ELEPHANT(2, "바깥상 차림", OuterElephantSetupPolicy::new),
-    LEFT_ELEPHANT(3, "왼상 차림", LeftElephantSetupPolicy::new),
-    RIGHT_ELEPHANT(4, "오른상 차림", RightElephantSetupPolicy::new);
+    INNER_ELEPHANT("안상 차림", InnerElephantSetupPolicy::new),
+    OUTER_ELEPHANT("바깥상 차림", OuterElephantSetupPolicy::new),
+    LEFT_ELEPHANT("왼상 차림", LeftElephantSetupPolicy::new),
+    RIGHT_ELEPHANT("오른상 차림", RightElephantSetupPolicy::new);
 
     private static final int MINUMUM_NUMBER = 1;
     private static final int MAXIMUM_NUMBER = 4;
 
-    private final int number;
     private final String description;
     private final Supplier<SetupPolicy> policySupplier;
 
-    SetupCommand(final int number, final String description,
+    SetupCommand(final String description,
         final Supplier<SetupPolicy> setupPolicySupplier) {
-        this.number = number;
         this.description = description;
         this.policySupplier = setupPolicySupplier;
     }
 
     public static SetupCommand pick(final int number) {
         return Arrays.stream(values())
-            .filter(setupCommand -> setupCommand.number == number)
+            .filter(setupCommand -> setupCommand.ordinal() == number)
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException(String.format(
                 "명령 번호는 %d에서 %d까지의 정수 값이어야 합니다.", MINUMUM_NUMBER, MAXIMUM_NUMBER)));
@@ -39,10 +37,6 @@ public enum SetupCommand {
 
     public SetupPolicy toPolicy() {
         return policySupplier.get();
-    }
-
-    public int getNumber() {
-        return number;
     }
 
     public String getDescription() {
