8cb576379ff77202b1ec0e0eb5b496332d438bdb
alstj2384 <ass91160@naver.com>
2026-03-27 16:21:00 +0900

    feat: SingleStepMoveStrategy 구현

---
+++ src/main/java/domain/piece/strategy/SingleStepMoveStrategy.java
@@ -0,0 +1,11 @@
+package domain.piece.strategy;
+
+import domain.position.Position;
+import java.util.List;
+
+public class SingleStepMoveStrategy implements MoveStrategy {
+    @Override
+    public List<Position> findMovablePath(Position start, Position destination) {
+        return null;
+    }
+}
