1e7c80f686be6f6fad3c0cf49e5d7b94e6fecec7
snow___shower <wonsb25@gmail.com>
2026-03-28 11:24:23 +0900

    feat: 상 행마법 구현
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/main/java/domain/piece/strategy/ElephantStrategy.java
+++ src/main/java/domain/piece/strategy/ElephantStrategy.java
@@ -2,10 +2,25 @@ package domain.piece.strategy;
 
 import domain.board.PathChecker;
 import domain.board.Position;
+import domain.piece.Piece;
 
-public class ElephantStrategy implements MoveStrategy{
+import java.util.List;
+
+public class ElephantStrategy implements MoveStrategy {
+
+    private static final String ELEPHANT_CANNOT_MOVE_ERROR_MESSAGE = "[ERROR] 멱이 있어 이동할 수 없습니다.";
+
     @Override
     public void move(Position from, Position to, PathChecker checker) {
+        int dx = from.calculateDx(to);
+        int dy = from.calculateDy(to);
 
+        ElephantDirection direction = ElephantDirection.from(dx, dy);
+        List<Position> path = direction.findPath(from);
+        List<Piece> piecesInPath = checker.findPiecesInPath(path);
+
+        if (!piecesInPath.isEmpty()) {
+            throw new IllegalArgumentException(ELEPHANT_CANNOT_MOVE_ERROR_MESSAGE);
+        }
     }
 }
