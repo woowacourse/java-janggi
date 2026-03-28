f78ce571c4e9e6715408f8445c3e79315b77bebe
최용준 <yj9107vv@gmail.com>
2026-03-28 16:59:32 +0900

    refactor: 장기판 보드 초기화 테스트 이동

--- src/test/java/domain/JanggiGameTest.java
+++ src/test/java/domain/JanggiGameTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import domain.piece.Piece;
 import domain.strategy.HorseMoveStrategy;
-import factory.JanggiBoardFactory;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
@@ -15,26 +14,19 @@ import org.junit.jupiter.api.Test;
 class JanggiGameTest {
 
     @Test
-    @DisplayName("JanggiGame은 생성 시 보드를 초기화하여 생성한다")
-    void initial_Board_test() {
-        JanggiBoardFactory janggiBoardFactory = new JanggiBoardFactory();
-        JanggiGame janggiGame = new JanggiGame(Board.of(janggiBoardFactory.initialBoard()));
-
-        assertThat(janggiGame.allFactors()).hasSize(90);
-    }
-
-    @Test
     @DisplayName("플레이어가 선택한 기물 위치에 플레이어 소유의 기물이 존재하는지")
     void player_select_piece_exist() {
         Map<Position, Piece> testBoard = new HashMap<>();
-        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.GREEN), HorseMoveStrategy.of(Position.of(3, 3)));
-        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN), HorseMoveStrategy.of(Position.of(5, 2)));
+        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.GREEN),
+                HorseMoveStrategy.of(Position.of(3, 3)));
+        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN),
+                HorseMoveStrategy.of(Position.of(5, 2)));
         testBoard.put(horsePiece.position(), horsePiece);
         testBoard.put(soldierPiece.position(), soldierPiece);
         Board board = Board.of(testBoard);
 
         JanggiGame janggiGame = new JanggiGame(board);
-        Position selectPosition = Position.of(3,3);
+        Position selectPosition = Position.of(3, 3);
 
         assertDoesNotThrow(() -> janggiGame.validatePieceSelection(selectPosition));
 
@@ -44,14 +36,16 @@ class JanggiGameTest {
     @DisplayName("플레이어가 선택한 기물 위치가 장기판 범위를 벗어나면 예외를 던진다.")
     void player_select_position_out_of_range_throw_exception() {
         Map<Position, Piece> testBoard = new HashMap<>();
-        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.GREEN), HorseMoveStrategy.of(Position.of(3, 3)));
-        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN), HorseMoveStrategy.of(Position.of(5, 2)));
+        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.GREEN),
+                HorseMoveStrategy.of(Position.of(3, 3)));
+        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN),
+                HorseMoveStrategy.of(Position.of(5, 2)));
         testBoard.put(horsePiece.position(), horsePiece);
         testBoard.put(soldierPiece.position(), soldierPiece);
         Board board = Board.of(testBoard);
 
         JanggiGame janggiGame = new JanggiGame(board);
-        Position selectPosition = Position.of(10,3);
+        Position selectPosition = Position.of(10, 3);
 
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> janggiGame.validatePieceSelection(selectPosition));
@@ -61,14 +55,16 @@ class JanggiGameTest {
     @DisplayName("플레이어가 선택한 기물 위치에 아군 기물이 존재하지 않으면 예외를 던진다.")
     void player_select_position_piece_not_exist_throw_exception() {
         Map<Position, Piece> testBoard = new HashMap<>();
-        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.RED), HorseMoveStrategy.of(Position.of(3, 3)));
-        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN), HorseMoveStrategy.of(Position.of(5, 2)));
+        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.RED),
+                HorseMoveStrategy.of(Position.of(3, 3)));
+        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN),
+                HorseMoveStrategy.of(Position.of(5, 2)));
         testBoard.put(horsePiece.position(), horsePiece);
         testBoard.put(soldierPiece.position(), soldierPiece);
         Board board = Board.of(testBoard);
 
         JanggiGame janggiGame = new JanggiGame(board);
-        Position selectPosition = Position.of(3,3);
+        Position selectPosition = Position.of(3, 3);
 
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> janggiGame.validatePieceSelection(selectPosition));
@@ -78,8 +74,10 @@ class JanggiGameTest {
     @DisplayName("장기 게임은 게임 종료 알 수 있다.")
     void game_finished() {
         Map<Position, Piece> testBoard = new HashMap<>();
-        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.RED), HorseMoveStrategy.of(Position.of(3, 3)));
-        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN), HorseMoveStrategy.of(Position.of(5, 2)));
+        Piece horsePiece = new Piece(PieceProperty.of(PieceType.HORSE, Team.RED),
+                HorseMoveStrategy.of(Position.of(3, 3)));
+        Piece soldierPiece = new Piece(PieceProperty.of(PieceType.SOLDIER, Team.GREEN),
+                HorseMoveStrategy.of(Position.of(5, 2)));
         testBoard.put(horsePiece.position(), horsePiece);
         testBoard.put(soldierPiece.position(), soldierPiece);
         Board board = Board.of(testBoard);
