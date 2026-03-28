de02ec9b5d0231f9b0fcd05fb3b6be013f8cb17e
smiinii <sm020320@naver.com>
2026-03-28 10:35:14 +0900

    refactor: 차(Cha) 이동 가능 여부 로직 리팩토링

--- src/main/java/janggi/domain/movestorage/ChaMoveStorage.java
+++ src/main/java/janggi/domain/movestorage/ChaMoveStorage.java
@@ -7,8 +7,8 @@ import janggi.domain.Row;
 
 import java.util.List;
 
-public class ChaMoveStorage implements MoveStorage{
-    // TODO: if문을 어떻게 줄일까? (2026. 3. 27.)
+public class ChaMoveStorage implements MoveStorage {
+
     @Override
     public boolean canMove(Position from, Position to, BoardState boardState) {
         List<Integer> fromPosition = from.getPosition();
@@ -16,52 +16,33 @@ public class ChaMoveStorage implements MoveStorage{
 
         int fromX = fromPosition.getFirst();
         int fromY = fromPosition.getLast();
-
         int toX = toPosition.getFirst();
         int toY = toPosition.getLast();
 
+        if (fromX != toX && fromY != toY) {
+            return false;
+        }
+
         if (fromX == toX) {
-            if (fromY < toY) {
-                int min = fromY;
-                int max = toY;
-                for (int i = min + 1; i < max; i++) {
-                    Position position = Position.of(Row.of(fromX), Column.of(i));
-                    if (boardState.hasPieceAt(position)) {
-                        return false;
-                    }
-                }
-            }
-            if (fromY > toY) {
-                int min = toY;
-                int max = fromY;
-                for (int i = min + 1; i < max; i++) {
-                    Position position = Position.of(Row.of(fromX), Column.of(i));
-                    if (boardState.hasPieceAt(position)) {
-                        return false;
-                    }
+            int start = Math.min(fromY, toY) + 1;
+            int end = Math.max(fromY, toY);
+
+            for (int i = start; i < end; i++) {
+                Position position = Position.of(Row.of(fromX), Column.of(i));
+                if (boardState.hasPieceAt(position)) {
+                    return false;
                 }
             }
         }
 
         if (fromY == toY) {
-            if (fromX < toX) {
-                int min = fromX;
-                int max = toX;
-                for (int i = min + 1; i < max; i++) {
-                    Position position = Position.of(Row.of(i), Column.of(fromY));
-                    if (boardState.hasPieceAt(position)) {
-                        return false;
-                    }
-                }
-            }
-            if (fromX > toX) {
-                int min = toX;
-                int max = fromX;
-                for (int i = min + 1; i < max; i++) {
-                    Position position = Position.of(Row.of(i), Column.of(fromY));
-                    if (boardState.hasPieceAt(position)) {
-                        return false;
-                    }
+            int start = Math.min(fromX, toX) + 1;
+            int end = Math.max(fromX, toX);
+
+            for (int i = start; i < end; i++) {
+                Position position = Position.of(Row.of(i), Column.of(fromY));
+                if (boardState.hasPieceAt(position)) {
+                    return false;
                 }
             }
         }
