aff9ca395f36926da57529d5f6236426ba9fe2ce
koreaioi <koreaioi@gachon.ac.kr>
2026-03-29 03:30:00 +0900

    refactor(domain): 불필요한 주석 삭제

--- src/main/java/domain/move/rule/MoveRule.java
+++ src/main/java/domain/move/rule/MoveRule.java
@@ -10,10 +10,6 @@ import java.util.List;
 
 public abstract class MoveRule {
 
-    /**
-     * 어떤 피스의 이동 전략인지.
-     * */
-
     protected final PieceType pieceType;
     protected final Directions directions;
 
