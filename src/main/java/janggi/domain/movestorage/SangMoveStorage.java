562a9ce23b3bb2f5fa6c63ec222818a99c8d6326
smiinii <sm020320@naver.com>
2026-03-28 16:09:10 +0900

    feat: 상(Sang) 이동 가능 여부 판단 로직 추가

--- src/main/java/janggi/domain/movestorage/SangMoveStorage.java
+++ src/main/java/janggi/domain/movestorage/SangMoveStorage.java
@@ -1,12 +1,71 @@
 package janggi.domain.movestorage;
 
 import janggi.domain.BoardState;
+import janggi.domain.Column;
 import janggi.domain.Position;
+import janggi.domain.Row;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SangMoveStorage implements MoveStorage{
-    // TODO: 이동 규칙 구현 (2026. 3. 26.)
+    private static final int FORWARD = 3;
+    private static final int DIAGONAL = 2;
+
+    private static final int PATH_STEP_2 = 2;
+    private static final int PATH_STEP_1 = 1;
+    private static final int PATH_STEP_0 = 0;
+
     @Override
     public boolean canMove(Position from, Position to, BoardState boardState) {
+        List<Integer> fromPosition = from.getPosition();
+        List<Integer> toPosition = to.getPosition();
+
+        int fromX = fromPosition.getFirst();
+        int fromY = fromPosition.getLast();
+        int toX = toPosition.getFirst();
+        int toY = toPosition.getLast();
+
+        int diffX = toX - fromX;
+        int diffY = toY - fromY;
+
+        boolean isSangMove = (Math.abs(diffX) == FORWARD && Math.abs(diffY) == DIAGONAL) ||
+                (Math.abs(diffX) == DIAGONAL && Math.abs(diffY) == FORWARD);
+
+        if (!isSangMove) {
+            return false;
+        }
+
+        List<Position> movementPathPositions = new ArrayList<>();
+
+        int signX = Integer.signum(diffX);
+        int signY = Integer.signum(diffY);
+
+        if (Math.abs(diffX) == FORWARD) {
+            int step1X = fromX + (signX * PATH_STEP_1);
+            int step1Y = fromY + (signY * PATH_STEP_0);
+            movementPathPositions.add(Position.of(Row.of(step1X), Column.of(step1Y)));
+
+            int step2X = fromX + (signX * PATH_STEP_2);
+            int step2Y = fromY + (signY * PATH_STEP_1);
+            movementPathPositions.add(Position.of(Row.of(step2X), Column.of(step2Y)));
+        }
+
+        if (Math.abs(diffY) == FORWARD) {
+            int step1X = fromX + (signX * PATH_STEP_0);
+            int step1Y = fromY + (signY * PATH_STEP_1);
+            movementPathPositions.add(Position.of(Row.of(step1X), Column.of(step1Y)));
+
+            int step2X = fromX + (signX * PATH_STEP_1);
+            int step2Y = fromY + (signY * PATH_STEP_2);
+            movementPathPositions.add(Position.of(Row.of(step2X), Column.of(step2Y)));
+        }
+
+        for (Position position : movementPathPositions) {
+            if (boardState.hasPieceAt(position)) {
+                return false;
+            }
+        }
         return true;
     }
 }
