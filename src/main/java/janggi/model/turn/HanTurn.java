074fca230cecf214989f464bd07c7d39a456fa2e
dahxxn <dahyeonhan4529@gmail.com>
2026-03-28 18:57:53 +0900

    fix: Team enum displayName 오류 수정
    - 기존 Team.HAN("초") -> Team.HAN("한"), Team.CHO("한") -> Team.CHO("초")로 변경
    - HanTurn.accept()에서 Team.CHO.getDisplayName() → Team.HAN.getDisplayName()으로 수정

--- src/main/java/janggi/model/turn/HanTurn.java
+++ src/main/java/janggi/model/turn/HanTurn.java
@@ -4,7 +4,6 @@ import janggi.model.Board;
 import janggi.model.Team;
 import janggi.model.position.Position;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 public class HanTurn implements Turn {
 
@@ -36,6 +35,6 @@ public class HanTurn implements Turn {
 
     @Override
     public void accept(BiConsumer<Board, String> consumer) {
-        consumer.accept(board, Team.CHO.getDisplayName());
+        consumer.accept(board, Team.HAN.getDisplayName());
     }
 }
