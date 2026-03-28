b346f135569b261d717912210123dbe7eb9fca1d
JYL35 <dlwodbs1223@naver.com>
2026-03-28 14:45:06 +0900

    test(Advisor): 사 단위 테스트 추가
    - 경로 확인 테스트 추가
    - 도착 지점 기물 확인 테스트 추가

--- src/test/java/janggi/domain/piece/AdvisorTest.java
+++ src/test/java/janggi/domain/piece/AdvisorTest.java
@@ -2,8 +2,12 @@ package janggi.domain.piece;
 
 import janggi.domain.Camp;
 import janggi.domain.piece.strategy.AdvisorStrategy;
+import janggi.domain.piece.strategy.ChariotStrategy;
+import janggi.domain.piece.strategy.GeneralStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,5 +18,28 @@ public class AdvisorTest {
     void isCannon_Always_ReturnFalse() {
         Piece piece = new Advisor(Camp.CHO, new AdvisorStrategy());
         assertThat(piece.isCannon()).isFalse();
+    }
+
+    @DisplayName("사의 경로 확인은 항상 True를 반환한다")
+    @Test
+    void canPassRoute_Always_ReturnTrue() {
+        Piece piece = new Advisor(Camp.CHO, new AdvisorStrategy());
+        assertThat(piece.canPassRoute(new HashMap<>())).isTrue();
+    }
+
+    @DisplayName("도착 지점에 있는 기물이 같은 진영이면 False를 반환한다")
+    @Test
+    void canCatch_DestinationPieceIsSameCamp_ReturnFalse() {
+        Piece piece = new Advisor(Camp.CHO, new AdvisorStrategy());
+        Piece desinationPiece = new Chariot(Camp.CHO, new ChariotStrategy());
+        assertThat(piece.canCatch(desinationPiece)).isFalse();
+    }
+
+    @DisplayName("도착 지점에 있는 기물이 다른 진영이면 False를 반환한다")
+    @Test
+    void canCatch_DestinationPieceIsNotSameCamp_ReturnTrue() {
+        Piece piece = new Advisor(Camp.CHO, new AdvisorStrategy());
+        Piece desinationPiece = new Chariot(Camp.HAN, new ChariotStrategy());
+        assertThat(piece.canCatch(desinationPiece)).isTrue();
     }
 }
