51bccba18b746b6a852158e9a990972a05926ccb
armd482 <armd482@naver.com>
2026-03-28 14:45:44 +0900

    refactor: for문 forEach로 변경

--- src/main/java/janggi/domain/piece/StepPiece.java
+++ src/main/java/janggi/domain/piece/StepPiece.java
@@ -35,10 +35,7 @@ public abstract class StepPiece extends ActivePiece {
 
     private List<Position> calculatePath(Position start, List<Movement> path) {
         List<Position> calculatedPath = new ArrayList<>(List.of(start));
-        for (Movement movement : path) {
-            Position step = calculatedPath.getLast().move(movement);
-            calculatedPath.add(step);
-        }
+        path.forEach(movement -> calculatedPath.add(calculatedPath.getLast().move(movement)));
         return calculatedPath;
     }
 }
