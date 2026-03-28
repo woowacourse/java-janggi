49da4fd6b1457e951fb4f1f800bf939a722a6142
SooHyeon Lee <dalsu4594@naver.com>
2026-03-28 16:33:21 +0900

    feat: AppConfig 추가

---
+++ src/main/java/janggi/config/AppConfig.java
@@ -0,0 +1,11 @@
+package janggi.config;
+
+import janggi.controller.JanggiController;
+import janggi.view.InputView;
+import janggi.view.OutputView;
+
+public class AppConfig {
+    public JanggiController janggiController(){
+        return new JanggiController(new InputView(), new OutputView());
+    }
+}
