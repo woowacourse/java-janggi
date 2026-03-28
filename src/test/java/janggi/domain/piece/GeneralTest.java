fa3977897a8c2c83ce2cad06c42d2c3c4feb574f
JYL35 <dlwodbs1223@naver.com>
2026-03-28 14:41:42 +0900

    test(General): 장 단위 테스트 추가
    - 경로 확인 테스트 추가
    - 도착 지점 기물 확인 테스트 추가

--- src/test/java/janggi/domain/piece/GeneralTest.java
+++ src/test/java/janggi/domain/piece/GeneralTest.java
@@ -1,9 +1,12 @@
 package janggi.domain.piece;
 
 import janggi.domain.Camp;
+import janggi.domain.piece.strategy.ChariotStrategy;
 import janggi.domain.piece.strategy.GeneralStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,5 +17,28 @@ public class GeneralTest {
     void isCannon_Always_ReturnFalse() {
         Piece piece = new General(Camp.CHO, new GeneralStrategy());
         assertThat(piece.isCannon()).isFalse();
+    }
+
+    @DisplayName("장의 경로 확인은 항상 True를 반환한다")
+    @Test
+    void canPassRoute_Always_ReturnTrue() {
+        Piece piece = new General(Camp.CHO, new GeneralStrategy());
+        assertThat(piece.canPassRoute(new HashMap<>())).isTrue();
+    }
+
+    @DisplayName("도착 지점에 있는 기물이 같은 진영이면 False를 반환한다")
+    @Test
+    void canCatch_DestinationPieceIsSameCamp_ReturnFalse() {
+        Piece piece = new General(Camp.CHO, new GeneralStrategy());
+        Piece desinationPiece = new Chariot(Camp.CHO, new ChariotStrategy());
+        assertThat(piece.canCatch(desinationPiece)).isFalse();
+    }
+
+    @DisplayName("도착 지점에 있는 기물이 다른 진영이면 False를 반환한다")
+    @Test
+    void canCatch_DestinationPieceIsNotSameCamp_ReturnTrue() {
+        Piece piece = new General(Camp.CHO, new GeneralStrategy());
+        Piece desinationPiece = new Chariot(Camp.HAN, new ChariotStrategy());
+        assertThat(piece.canCatch(desinationPiece)).isTrue();
     }
 }
