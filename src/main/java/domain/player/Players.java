e12a06383b0887f1a36a4ff1fe9784177b5d9c96
quddaz <quddnddl35@naver.com>
2026-03-28 23:07:49 +0900

    refactor : 변수명 재네이밍

--- src/main/java/domain/player/Players.java
+++ src/main/java/domain/player/Players.java
@@ -29,9 +29,9 @@ public class Players {
     }
 
     private static void validateNonDuplicate(List<String> names) {
-        Set<String> s = new HashSet<>(names);
+        Set<String> nameSet = new HashSet<>(names);
 
-        if (s.size() != names.size()) {
+        if (nameSet.size() != names.size()) {
             throw new IllegalArgumentException("[ERROR] 플레이어 이름은 중복될 수 없습니다.");
         }
     }
