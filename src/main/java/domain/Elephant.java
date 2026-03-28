50f2eb928cf91d6345d612d76ab5df9c23558266
miniminjae92 <rkdalswoals@gmail.com>
2026-03-28 17:54:42 +0900

    feat(Elephant): 기물 이동 규칙 구현

--- src/main/java/domain/Elephant.java
+++ src/main/java/domain/Elephant.java
@@ -1,8 +1,174 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Elephant extends Piece {
+    private final String name = "상";
 
     public Elephant(Side side) {
         super(side);
+    }
+
+    @Override
+    public List<Position> getAllPosition(Position position) {
+        List<Position> positions = new ArrayList<>();
+
+        // 상
+        if (position.upPossible()) {
+            Position curPosition = position.up(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            positions.add(curPosition);
+            if (curPosition.rightUpPossible()) {
+                Position rightUpPosition = curPosition.rightUp();
+                positions.add(rightUpPosition);
+                if (rightUpPosition.rightUpPossible()) {
+                    positions.add(rightUpPosition.rightUp());
+                }
+            }
+            if (curPosition.leftUpPossible()) {
+                Position leftUPosition = curPosition.leftUp();
+                positions.add(leftUPosition);
+                if (leftUPosition.leftUpPossible()) {
+                    positions.add(leftUPosition.leftUp());
+                }
+            }
+        }
+        // 하
+        if (position.downPossible()) {
+            Position curPosition = position.down(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            positions.add(curPosition);
+            if (curPosition.rightDownPossible()) {
+                Position rightDownPosition = curPosition.rightDown();
+                positions.add(rightDownPosition);
+                if (rightDownPosition.rightDownPossible()) {
+                    positions.add(rightDownPosition.rightDown());
+                }
+            }
+            if (curPosition.leftDownPossible()) {
+                Position leftDownPosition = curPosition.leftDown();
+                positions.add(leftDownPosition);
+                if (leftDownPosition.leftDownPossible()) {
+                    positions.add(leftDownPosition.leftDown());
+                }
+            }
+        }
+        // 좌
+        if (position.leftPossible()) {
+            Position curPosition = position.left(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            positions.add(curPosition);
+            if (curPosition.leftUpPossible()) {
+                Position leftUpPosition = curPosition.leftUp();
+                positions.add(leftUpPosition);
+                if (leftUpPosition.leftUpPossible()) {
+                    positions.add(leftUpPosition.leftUp());
+                }
+            }
+            if (curPosition.leftDownPossible()) {
+                Position leftDownPosition = curPosition.leftDown();
+                positions.add(leftDownPosition);
+                if (leftDownPosition.leftDownPossible()) {
+                    positions.add(leftDownPosition.leftDown());
+                }
+            }
+        }
+        // 우
+        if (position.rightPossible()) {
+            Position curPosition = position.right(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            positions.add(curPosition);
+            if (curPosition.rightUpPossible()) {
+                Position rightUpPosition = curPosition.rightUp();
+                positions.add(rightUpPosition);
+                if (rightUpPosition.rightUpPossible()) {
+                    positions.add(rightUpPosition.rightUp());
+                }
+            }
+            if (curPosition.rightDownPossible()) {
+                Position rightDownPosition = curPosition.rightDown();
+                positions.add(rightDownPosition);
+                if (rightDownPosition.rightDownPossible()) {
+                    positions.add(rightDownPosition.rightDown());
+                }
+            }
+        }
+
+        return positions;
+    }
+
+    @Override
+    public List<Position> getPossibleDestinations(Position position, Map<Position, Piece> map) {
+        List<Position> destinations = new ArrayList<>();
+
+        // 상
+        if (position.upPossible() && !map.containsKey(position.up())) {
+            Position curPosition = position.up(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            if (curPosition.rightUpPossible() && !map.containsKey(curPosition.rightUp())) {
+                Position rightUpPosition = curPosition.rightUp();
+                if (rightUpPosition.rightUpPossible() && (!map.containsKey(rightUpPosition.rightUp()) || !map.get(rightUpPosition.rightUp()).isSameSideAs(side))) {
+                    destinations.add(rightUpPosition.rightUp());
+                }
+            }
+            if (curPosition.leftUpPossible() && !map.containsKey(curPosition.leftUp())) {
+                Position leftUpPosition = curPosition.leftUp();
+                if (leftUpPosition.leftUpPossible() && (!map.containsKey(leftUpPosition.rightUp()) || !map.get(leftUpPosition.leftUp()).isSameSideAs(side))) {
+                    destinations.add(leftUpPosition.leftUp());
+                }
+            }
+        }
+        // 하
+        if (position.downPossible() && !map.containsKey(position.down())) {
+            Position curPosition = position.down(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            if (curPosition.rightDownPossible() && !map.containsKey(curPosition.rightDown())) {
+                Position rightDownPosition = curPosition.rightDown();
+                if (rightDownPosition.rightDownPossible() && (!map.containsKey(rightDownPosition.rightDown()) || !map.get(rightDownPosition.rightDown()).isSameSideAs(side))) {
+                    destinations.add(rightDownPosition.rightDown());
+                }
+            }
+            if (curPosition.leftDownPossible() && !map.containsKey(curPosition.leftDown())) {
+                Position leftDownPosition = curPosition.leftDown();
+                if (leftDownPosition.leftDownPossible() && (!map.containsKey(leftDownPosition.leftDown()) || !map.get(leftDownPosition.leftDown()).isSameSideAs(side))) {
+                    destinations.add(leftDownPosition.leftDown());
+                }
+            }
+        }
+        // 좌
+        if (position.leftPossible() && !map.containsKey(position.left())) {
+            Position curPosition = position.left(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            if (curPosition.leftUpPossible() && !map.containsKey(curPosition.leftUp())) {
+                Position leftUpPosition = curPosition.leftUp();
+                if (leftUpPosition.leftUpPossible() && (!map.containsKey(leftUpPosition.leftUp()) || !map.get(leftUpPosition.leftUp()).isSameSideAs(side))) {
+                    destinations.add(leftUpPosition.leftUp());
+                }
+            }
+            if (curPosition.leftDownPossible() && !map.containsKey(curPosition.leftDown())) {
+                Position leftDownPosition = curPosition.leftDown();
+                if (leftDownPosition.leftDownPossible() && (!map.containsKey(leftDownPosition.leftDown()) || !map.get(leftDownPosition.leftDown()).isSameSideAs(side))) {
+                    destinations.add(leftDownPosition.leftDown());
+                }
+            }
+        }
+        // 우
+        if (position.rightPossible() && !map.containsKey(position.right())) {
+            Position curPosition = position.right(); // 애초에 넘길때 가능한지 체크하고 만들면 조건문 하나 줄일 수 있지!
+            if (curPosition.rightUpPossible() && !map.containsKey(curPosition.rightUp())) {
+                Position rightUpPosition = curPosition.rightUp();
+                if (rightUpPosition.rightUpPossible() && (!map.containsKey(rightUpPosition.rightUp()) || !map.get(rightUpPosition.rightUp()).isSameSideAs(side))) {
+                    destinations.add(rightUpPosition.rightUp());
+                }
+            }
+            if (curPosition.rightDownPossible() && !map.containsKey(curPosition.rightDown())) {
+                Position rightDownPosition = curPosition.rightDown();
+                if (rightDownPosition.rightDownPossible() && (!map.containsKey(rightDownPosition.rightDown()) || !map.get(rightDownPosition.rightDown()).isSameSideAs(side))) {
+                    destinations.add(rightDownPosition.rightDown());
+                }
+            }
+        }
+
+        return destinations;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
