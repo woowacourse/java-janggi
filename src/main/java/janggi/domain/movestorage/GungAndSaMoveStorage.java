633b898c5cadbdecded21c1d523ca6d180ea6522
smiinii <sm020320@naver.com>
2026-03-28 20:16:17 +0900

    feat: 궁(Gung)과 사(Sa) 이동 가능 여부 판단 로직 추가

--- src/main/java/janggi/domain/movestorage/GungAndSaMoveStorage.java
+++ src/main/java/janggi/domain/movestorage/GungAndSaMoveStorage.java
@@ -2,11 +2,49 @@ package janggi.domain.movestorage;
 
 import janggi.domain.BoardState;
 import janggi.domain.Position;
+import janggi.domain.Team;
+
+import java.util.List;
 
 public class GungAndSaMoveStorage implements MoveStorage{
-    // TODO: 이동 규칙 구현 (2026. 3. 26.)
     @Override
     public boolean canMove(Position from, Position to, BoardState boardState) {
+        List<Integer> fromPosition = from.getPosition();
+        List<Integer> toPosition = to.getPosition();
+
+        int fromX = fromPosition.getFirst();
+        int fromY = fromPosition.getLast();
+        int toX = toPosition.getFirst();
+        int toY = toPosition.getLast();
+
+        if (!(3 <= fromX && fromX <= 5) || !(3 <= toX && toX <= 5)) {
+            return false;
+        }
+
+        if (boardState.getPieceAt(from).getTeam() == Team.HAN) {
+            if (!(0 <= fromY && fromY <= 2) || !(0 <= toY && toY <= 2)) {
+                return false;
+            }
+        }
+
+        if (boardState.getPieceAt(from).getTeam() == Team.CHO) {
+            if (!(7 <= fromY && fromY <= 9) || !(7 <= toY && toY <= 9)) {
+                return false;
+            }
+        }
+
+        if (fromX != toX && fromY != toY) {
+            return false;
+        }
+
+        if (fromY == toY && Math.abs(fromX - toX) != 1) {
+            return false;
+        }
+
+        if (fromX == toX && Math.abs(fromY - toY) != 1) {
+            return false;
+        }
+
         return true;
     }
 }
