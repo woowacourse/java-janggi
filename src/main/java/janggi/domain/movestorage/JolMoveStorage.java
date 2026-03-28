aca2d4299c3df7e671d1060e45213867f747f320
smiinii <sm020320@naver.com>
2026-03-28 18:15:51 +0900

    feat: 졸(Jol) 이동 가능 여부 판단 로직 추가

--- src/main/java/janggi/domain/movestorage/JolMoveStorage.java
+++ src/main/java/janggi/domain/movestorage/JolMoveStorage.java
@@ -7,9 +7,36 @@ import janggi.domain.Team;
 import java.util.List;
 
 public class JolMoveStorage implements MoveStorage{
-    // TODO: 이동 규칙 구현 (2026. 3. 26.)
+    private static final int HAN_FORWARD = 1;
+    private static final int CHO_FORWARD = -1;
+    public static final int NEXT_TO = 1;
+
     @Override
     public boolean canMove(Position from, Position to, BoardState boardState) {
-        return true;
+        List<Integer> fromPosition = from.getPosition();
+        List<Integer> toPosition = to.getPosition();
+
+        int fromX = fromPosition.getFirst();
+        int fromY = fromPosition.getLast();
+        int toX = toPosition.getFirst();
+        int toY = toPosition.getLast();
+
+        if (Math.abs(fromX - toX) == NEXT_TO && fromY == toY) {
+            return true;
+        }
+
+        if (boardState.getPieceAt(from).getTeam() == Team.HAN) {
+            if (toY - fromY == HAN_FORWARD && fromX == toX) {
+                return true;
+            }
+        }
+
+        if (boardState.getPieceAt(from).getTeam() == Team.CHO) {
+            if (toY - fromY == CHO_FORWARD && fromX == toX) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
