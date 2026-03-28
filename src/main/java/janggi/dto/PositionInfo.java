9756b2f899a9dd0ef719799328c86c675fafed92
jyt6640 <jyt6640@gmail.com>
2026-03-28 10:56:12 +0900

    fix: PositionInfo의 리스트 기반 생성 메서드 복구

--- src/main/java/janggi/dto/PositionInfo.java
+++ src/main/java/janggi/dto/PositionInfo.java
@@ -5,11 +5,21 @@ import janggi.domain.piece.Piece;
 import janggi.domain.piece.PieceFactory;
 import janggi.domain.piece.PieceType;
 import janggi.domain.status.Team;
+import java.util.List;
 
 public record PositionInfo(
         Piece piece,
         Point point
 ) {
+    public static PositionInfo from(List<String> data) {
+        return from(
+                Team.valueOf(data.get(0)),
+                data.get(1),
+                Integer.parseInt(data.get(2)),
+                Integer.parseInt(data.get(3))
+        );
+    }
+
     public static PositionInfo from(Team team, String pieceName, int x, int y) {
         return new PositionInfo(
                 PieceFactory.initPiece(team, PieceType.valueOf(pieceName)),
