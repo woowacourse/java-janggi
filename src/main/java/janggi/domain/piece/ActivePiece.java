7a6db77fbdc98562159574f7e41334311d87404b
armd482 <armd482@naver.com>
2026-03-28 16:20:44 +0900

    refactor: 개행 정리

--- src/main/java/janggi/domain/piece/ActivePiece.java
+++ src/main/java/janggi/domain/piece/ActivePiece.java
@@ -6,6 +6,7 @@ import janggi.domain.policy.RoutePolicy;
 public abstract class ActivePiece extends BasePiece {
     public static final String INVALID_DESTINATION_MESSAGE = "올바른 도착 지점이 아닙니다.";
     public static final String UNMOVABLE_ROUTE_MESSAGE = "이동할 수 없는 경로입니다.";
+
     protected RoutePolicy routePolicy;
 
     public ActivePiece(RoutePolicy routePolicy, Side side, PieceType pieceType) {
