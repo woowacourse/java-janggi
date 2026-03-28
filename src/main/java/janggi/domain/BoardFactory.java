64ecbd9c68cd3562adb9f5ab003ef78a4c45ec95
smiinii <sm020320@naver.com>
2026-03-28 23:12:12 +0900

    fix: 초 기물 배치 오류 수정

--- src/main/java/janggi/domain/BoardFactory.java
+++ src/main/java/janggi/domain/BoardFactory.java
@@ -64,42 +64,42 @@ public class BoardFactory {
     private static void SetChoPieces(Map<Position, Piece> board) {
         // 차
         board.put(Position.of(Row.of(0), Column.of(9)),
-                new Piece(new ChaMoveStorage(), Team.HAN, 13, "車"));
+                new Piece(new ChaMoveStorage(), Team.CHO, 13, "車"));
         board.put(Position.of(Row.of(8), Column.of(9)),
-                new Piece(new ChaMoveStorage(), Team.HAN, 13, "車"));
+                new Piece(new ChaMoveStorage(), Team.CHO, 13, "車"));
         // 상
         board.put(Position.of(Row.of(1), Column.of(9)),
-                new Piece(new SangMoveStorage(), Team.HAN, 3, "象"));
+                new Piece(new SangMoveStorage(), Team.CHO, 3, "象"));
         board.put(Position.of(Row.of(6), Column.of(9)),
-                new Piece(new SangMoveStorage(), Team.HAN, 3, "象"));
+                new Piece(new SangMoveStorage(), Team.CHO, 3, "象"));
         // 마
         board.put(Position.of(Row.of(2), Column.of(9)),
-                new Piece(new MaMoveStorage(), Team.HAN, 5, "馬"));
+                new Piece(new MaMoveStorage(), Team.CHO, 5, "馬"));
         board.put(Position.of(Row.of(7), Column.of(9)),
-                new Piece(new MaMoveStorage(), Team.HAN, 5, "馬"));
+                new Piece(new MaMoveStorage(), Team.CHO, 5, "馬"));
         // 사
         board.put(Position.of(Row.of(3), Column.of(9)),
-                new Piece(new GungAndSaMoveStorage(), Team.HAN, 3, "士"));
+                new Piece(new GungAndSaMoveStorage(), Team.CHO, 3, "士"));
         board.put(Position.of(Row.of(5), Column.of(9)),
-                new Piece(new GungAndSaMoveStorage(), Team.HAN, 3, "士"));
+                new Piece(new GungAndSaMoveStorage(), Team.CHO, 3, "士"));
         // 궁
         board.put(Position.of(Row.of(4), Column.of(8)),
-                new Piece(new GungAndSaMoveStorage(), Team.HAN, 0, "楚"));
+                new Piece(new GungAndSaMoveStorage(), Team.CHO, 0, "楚"));
         // 포
         board.put(Position.of(Row.of(1), Column.of(7)),
-                new Piece(new SangMoveStorage(), Team.HAN, 7, "包"));
+                new Piece(new SangMoveStorage(), Team.CHO, 7, "包"));
         board.put(Position.of(Row.of(7), Column.of(7)),
-                new Piece(new SangMoveStorage(), Team.HAN, 7, "包"));
+                new Piece(new SangMoveStorage(), Team.CHO, 7, "包"));
         // 졸
         board.put(Position.of(Row.of(0), Column.of(6)),
-                new Piece(new JolMoveStorage(), Team.HAN, 2, "卒"));
+                new Piece(new JolMoveStorage(), Team.CHO, 2, "卒"));
         board.put(Position.of(Row.of(2), Column.of(6)),
-                new Piece(new JolMoveStorage(), Team.HAN, 2, "卒"));
+                new Piece(new JolMoveStorage(), Team.CHO, 2, "卒"));
         board.put(Position.of(Row.of(4), Column.of(6)),
-                new Piece(new JolMoveStorage(), Team.HAN, 2,  "卒"));
+                new Piece(new JolMoveStorage(), Team.CHO, 2,  "卒"));
         board.put(Position.of(Row.of(6), Column.of(6)),
-                new Piece(new JolMoveStorage(), Team.HAN, 2,  "卒"));
+                new Piece(new JolMoveStorage(), Team.CHO, 2,  "卒"));
         board.put(Position.of(Row.of(8), Column.of(6)),
-                new Piece(new JolMoveStorage(), Team.HAN, 2,  "卒"));
+                new Piece(new JolMoveStorage(), Team.CHO, 2,  "卒"));
     }
 }
