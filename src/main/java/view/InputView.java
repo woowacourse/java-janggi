6f4a476ae68127fcd77ac0e174a649ad9f3ac7bc
김가현 <hyeonnni@naver.com>
2026-03-28 21:07:53 +0900

    refactor: 출력 메세지 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -30,7 +30,7 @@ public class InputView {
     }
 
     public List<String> askMovePiecePoisiton(Team team) {
-        System.out.println(team + "의 차례입니다. 움직일 기물의 위치와 이동할 위치를 입력하세요. (예: 2,5,4,3)");
+        System.out.println(team + "의 차례입니다. 움직일 기물의 위치와 이동할 위치를 행과 열 순서대로 입력하세요. ( 예: 2,5,4,3 )");
         List<String> positions = Arrays.stream(scanner.nextLine().split(","))
                 .map(String::trim)
                 .collect(Collectors.toList());
