58baa25596a5aad555beb84130c948952a6d8051
smiinii <sm020320@naver.com>
2026-03-28 11:59:28 +0900

    feat: 포(Po) 이동 가능 여부 판단 로직 추가

--- src/main/java/janggi/domain/movestorage/PoMoveStorage.java
+++ src/main/java/janggi/domain/movestorage/PoMoveStorage.java
@@ -1,12 +1,73 @@
 package janggi.domain.movestorage;
 
 import janggi.domain.BoardState;
+import janggi.domain.Column;
+import janggi.domain.Piece;
 import janggi.domain.Position;
+import janggi.domain.Row;
 
-public class PoMoveStorage implements MoveStorage{
-    // TODO: 이동 규칙 구현 (2026. 3. 26.)
+import java.util.List;
+
+public class PoMoveStorage implements MoveStorage {
+
     @Override
     public boolean canMove(Position from, Position to, BoardState boardState) {
+        List<Integer> fromPosition = from.getPosition();
+        List<Integer> toPosition = to.getPosition();
+
+        int fromX = fromPosition.getFirst();
+        int fromY = fromPosition.getLast();
+        int toX = toPosition.getFirst();
+        int toY = toPosition.getLast();
+
+        if (fromX != toX && fromY != toY) {
+            return false;
+        }
+
+        int jumpCount = 0;
+
+        if (fromX == toX) {
+            int start = Math.min(fromY, toY) + 1;
+            int end = Math.max(fromY, toY);
+
+            for (int i = start; i < end; i++) {
+                Position position = Position.of(Row.of(fromX), Column.of(i));
+                if (boardState.hasPieceAt(position)) {
+                    Piece jumpPiece = boardState.getPieceAt(position);
+                    if (jumpPiece.getName().equals("包")) {
+                        return false;
+                    }
+                    jumpCount++;
+                }
+            }
+        }
+
+        if (fromY == toY) {
+            int start = Math.min(fromX, toX) + 1;
+            int end = Math.max(fromX, toX);
+
+            for (int i = start; i < end; i++) {
+                Position position = Position.of(Row.of(i), Column.of(fromY));
+                if (boardState.hasPieceAt(position)) {
+                    Piece jumpPiece = boardState.getPieceAt(position);
+                    if (jumpPiece.getName().equals("包")) {
+                        return false;
+                    }
+                    jumpCount++;
+                }
+            }
+        }
+
+        if (jumpCount != 1) {
+            return false;
+        }
+
+        if (boardState.hasPieceAt(to)) {
+            Piece targetPiece = boardState.getPieceAt(to);
+            if (targetPiece.getName().equals("包")) {
+                return false;
+            }
+        }
         return true;
     }
 }
