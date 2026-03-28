3b37380279efe3fe38e391e77d451bf4816a5a5a
EveryPine <starcraft819@gmail.com>
2026-03-28 10:55:09 +0900

    test(board): 기물 이동 테스트 추가

--- src/test/java/janggi/domain/board/BoardTest.java
+++ src/test/java/janggi/domain/board/BoardTest.java
@@ -2,13 +2,18 @@ package janggi.domain.board;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import janggi.domain.Position;
 import janggi.domain.piece.Cannon;
+import janggi.domain.piece.Chariot;
 import janggi.domain.piece.Piece;
+import janggi.domain.piece.Soldier;
 import janggi.domain.team.TeamType;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -73,6 +78,55 @@ public class BoardTest {
 
             assertThatIllegalStateException()
                 .isThrownBy(() -> board.findPieceByPosition(position));
+        }
+    }
+
+    @Nested
+    @DisplayName("기물 이동 테스트")
+    class MovePiece {
+
+        @Test
+        @DisplayName("이동 대상 위치에 기물이 존재하지 않는 경우")
+        void success_1() {
+            Position from = Position.valueOf(1, 1);
+            Position to = Position.valueOf(1, 3);
+            Piece me = new Cannon(TeamType.RED);
+            Map<Position, Piece> positionPieceMap = Map.of(
+                from, me);
+            Board board = new Board(positionPieceMap);
+            Optional<Piece> expected = Optional.empty();
+
+            Optional<Piece> actual = board.movePiece(from, to);
+
+            assertAll(
+                () -> assertThat(actual).isEqualTo(expected),
+                () -> assertThat(board).extracting("positionPieceMap")
+                    .asInstanceOf(InstanceOfAssertFactories.MAP)
+                    .containsEntry(to, me)
+            );
+        }
+
+        @Test
+        @DisplayName("이동 대상 위치에 기물이 존재하는 경우")
+        void success_2() {
+            Position from = Position.valueOf(1, 1);
+            Position to = Position.valueOf(1, 3);
+            Piece me = new Chariot(TeamType.RED);
+            Piece target = new Soldier(TeamType.BLUE);
+            Map<Position, Piece> positionPieceMap = Map.of(
+                from, me,
+                to, target);
+            Board board = new Board(positionPieceMap);
+            Optional<Piece> expected = Optional.of(target);
+
+            Optional<Piece> actual = board.movePiece(from, to);
+
+            assertAll(
+                () -> assertThat(actual).isEqualTo(expected),
+                () -> assertThat(board).extracting("positionPieceMap")
+                    .asInstanceOf(InstanceOfAssertFactories.MAP)
+                    .containsEntry(to, me)
+            );
         }
     }
 }
