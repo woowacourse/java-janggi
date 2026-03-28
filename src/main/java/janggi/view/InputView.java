1e86296ef1a344b164c34b9dda0e4f75b5d3fd70
smiinii <sm020320@naver.com>
2026-03-28 23:12:53 +0900

    feat: 플레이 턴 입력 로직 및 좌표 파싱 기능 추가

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -1,5 +1,8 @@
 package janggi.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,5 +12,23 @@ public class InputView {
         this.sc = new Scanner(System.in);
     }
 
+    public List<Integer> playTurn(String team) {
+        System.out.println(team + "나라 턴입니다.");
 
+        List<Integer> inputFrom = readCoordinates("이동할 기물의 좌표를 입력하세요. (예 : 1, 2)");
+        List<Integer> inputTo = readCoordinates("도착할 좌표를 입력하세요. (예 : 1, 3)");
+
+        List<Integer> result = new ArrayList<>(inputFrom);
+        result.addAll(inputTo);
+
+        return result;
+    }
+
+    private List<Integer> readCoordinates(String message) {
+        System.out.println(message);
+        return Arrays.stream(sc.nextLine().split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+    }
 }
