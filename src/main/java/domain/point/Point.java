f1e6a5b2bab69aa582e8337ace3c3df630d93f94
koreaioi <koreaioi@gachon.ac.kr>
2026-03-28 16:07:35 +0900

    refactor(domain): Point 리팩토링

--- src/main/java/domain/point/Point.java
+++ src/main/java/domain/point/Point.java
@@ -7,10 +7,21 @@ public record Point(
         int x
 ) {
 
-    public Point(int y, int x) {
-        validate(y, x);
-        this.y = y;
-        this.x = x;
+    private static final int BASE_POINT = 0;
+    private static final int LENGTH_OF_ROW = 10;
+    private static final int LENGTH_OF_FILE = 9;
+    private static final int MAX_OF_ROW = BASE_POINT + LENGTH_OF_ROW;
+    private static final int MAX_OF_FILE = BASE_POINT + LENGTH_OF_FILE;
+
+    public Point {
+        validatePointRange(y, x);
+    }
+
+    private void validatePointRange(int y, int x) {
+        if (checkPointRange(y, x)) {
+            return;
+        }
+        throw new IllegalArgumentException();
     }
 
     public boolean canMake(int y, int x){
@@ -21,15 +32,8 @@ public record Point(
         return new Point(this.y + y, this.x + x);
     }
 
-    private void validate(int y, int x) {
-        if (checkPointRange(y, x)) {
-            return;
-        }
-        throw new IllegalArgumentException();
-    }
-
     private boolean checkPointRange(int y, int x) {
-        return 0 <= y && y <= 9 && 0 <= x && x <= 8;
+        return BASE_POINT <= y && y < MAX_OF_ROW && BASE_POINT <= x && x < MAX_OF_FILE;
     }
 
     public boolean isSameFile(Point other) {
@@ -43,4 +47,5 @@ public record Point(
     public Point next(Vector vector) {
         return new Point(y + vector.dy(), x + vector.dx());
     }
+
 }
