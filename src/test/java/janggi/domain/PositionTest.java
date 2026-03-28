22d50c90ea0c93ab0aea489ea8a7ae4a50b19f0b
dahye <dahye9011@naver.com>
2026-03-28 18:17:07 +0900

    test: 테스트 케이스 추가

--- src/test/java/janggi/domain/PositionTest.java
+++ src/test/java/janggi/domain/PositionTest.java
@@ -32,4 +32,40 @@ public class PositionTest {
         assertThatThrownBy(() -> new Position(row, column))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("보드 범위 내로 이동하면, 이동된 좌표를 가진 새로운 Position 객체를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "4, 4, 1, 0, 5, 4",   // 아래로 1칸
+            "4, 4, -1, 0, 3, 4",  // 위로 1칸
+            "4, 4, 0, 1, 4, 5",   // 오른쪽으로 1칸
+            "4, 4, 0, -1, 4, 3",  // 왼쪽으로 1칸
+            "0, 0, 9, 8, 9, 8"    // 끝에서 끝으로 이동
+    })
+    void 이동_정상_테스트(int row, int column, int moveRow, int moveColumn, int expectedRow, int expectedColumn) {
+        // given
+        Position position = new Position(row, column);
+
+        // when
+        Position movedPosition = position.move(moveRow, moveColumn);
+
+        // then
+        assertThat(movedPosition.row()).isEqualTo(expectedRow);
+        assertThat(movedPosition.column()).isEqualTo(expectedColumn);
+    }
+
+    @DisplayName("이동 결과가 보드 범위를 벗어나는 경우, IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0, 0, -1, 0",
+            "9, 8, 1, 0",
+            "0, 0, 0, -1",
+            "9, 8, 0, 1"
+    })
+    void 이동_범위_예외_테스트(int row, int column, int moveRow, int moveColumn) {
+        Position position = new Position(row, column);
+
+        assertThatThrownBy(() -> position.move(moveRow, moveColumn))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
