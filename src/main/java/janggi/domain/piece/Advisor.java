e66cad8433a98a108ecdf10fbaf6a01632a835b0
JYL35 <dlwodbs1223@naver.com>
2026-03-28 14:45:40 +0900

    feat(Advisor): 사 기능 구현
    - 경로 확인 기능 구현
    - 도착 지점 기물 확인 기능 구현

--- src/main/java/janggi/domain/piece/Advisor.java
+++ src/main/java/janggi/domain/piece/Advisor.java
@@ -13,12 +13,12 @@ public class Advisor extends Piece{
 
     @Override
     public boolean canPassRoute(Map<Position, Piece> piecesInPath) {
-        return false;
+        return true;
     }
 
     @Override
     public boolean canCatch(Piece piece) {
-        return false;
+        return !isSameCamp(piece);
     }
 
     @Override
