f21ec4699271673a1750d3e73e965b9c6c2a0549
koreaioi <koreaioi@gachon.ac.kr>
2026-03-28 23:53:12 +0900

    refactor(domain): 매직 리터럴 상수처리

--- src/main/java/view/InputReader.java
+++ src/main/java/view/InputReader.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 
 public class InputReader {
 
-    private static final String COMMAND_REQUEST_MESSAGE = "의 차례: 출발 기물의 좌표를 입력해주세요 ex 0,0 0,2";
+    private static final String COMMAND_REQUEST_MESSAGE = "%s의 차례: 출발 기물의 좌표를 입력해주세요 ex 0,0 0,2";
+    private static final String FORMATION_REQUEST_MESSAGE = "%s팀의 마,상 포진을 입력해주세요.";
+    private static final String FORMATION_INFORMATION = "1 - 상마상마\n2 - 상마마상\n3 - 마상상마\n4 - 마상마상";
 
     public final Scanner scanner;
 
@@ -17,16 +19,14 @@ public class InputReader {
     }
 
     public Formation requestFormation(Team team) {
-        System.out.println(team + "팀의 마,상 포메이션을 입력해주세요.");
-        System.out.println("1 - 상마상마");
-        System.out.println("2 - 상마마상");
-        System.out.println("3 - 마상상마");
-        System.out.println("4 - 마상마상");
+        System.out.println(FORMATION_INFORMATION);
+        System.out.printf(FORMATION_REQUEST_MESSAGE, team);
         return Formation.from(scanner.nextLine());
     }
 
     public Command requestCommand(Team team) {
-        System.out.println(team + COMMAND_REQUEST_MESSAGE);
+        System.out.printf(COMMAND_REQUEST_MESSAGE, team);
+        System.out.println();
         return Command.from(scanner.nextLine());
     }
 
