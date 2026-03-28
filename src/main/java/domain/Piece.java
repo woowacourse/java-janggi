80c4f315a3f7371f90c5afebc411afcdbf7834f0
miniminjae92 <rkdalswoals@gmail.com>
2026-03-28 17:18:49 +0900

    feat(Piece): 기물 인터페이스 구현

--- src/main/java/domain/Piece.java
+++ src/main/java/domain/Piece.java
@@ -1,14 +1,20 @@
 package domain;
 
+import java.util.List;
+import java.util.Map;
+
 public abstract class Piece {
-    private final Side side;
+    protected final Side side;
 
     public Piece(Side side) {
         this.side = side;
     }
 
-
     public boolean isSameSideAs(Side other) {
         return side.isSameAs(other);
     }
+
+    public abstract List<Position> getAllPosition(Position position);
+
+    public abstract List<Position> getPossibleDestinations(Position position, Map<Position, Piece> map);
 }
