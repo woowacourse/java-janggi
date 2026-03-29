5d64486e8d2aa671933611a53e40f21826914d0d
Huiyeongkim <huiyeong9619@naver.com>
2026-03-29 10:59:41 +0900

    feat: 한나라인 경우 졸 대신 병 출력하도록 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,9 +6,15 @@ import domain.vo.Position;
 import java.util.Map;
 
 public class OutputView {
+
     private static final String RESET_COLOR = "\u001B[0m";
     private static final String CHU_COLOR = "\u001B[32m";
     private static final String HAN_COLOR = "\u001B[31m";
+
+    private static final String CHU_SOLDIER = "졸";
+    private static final String HAN_SOLDIER = "병";
+    private static final String CHU_NATION_NAME = "초";
+    private static final String HAN_NATION_NAME = "한";
 
     private static final int MAX_COL = 8;
     private static final int MAX_ROW = 9;
@@ -20,8 +26,12 @@ public class OutputView {
                 Position position = Position.of(row, col);
                 if (board.containsKey(position)) {
                     Piece piece = board.get(position);
+
                     String color = getNationColor(piece);
-                    System.out.print(color + piece.getTypeName() + RESET_COLOR + "  ");
+                    String type = piece.getTypeName();
+                    type = matchSoldierName(type, piece);
+
+                    System.out.print(color + type + RESET_COLOR + "  ");
                 } else {
                     System.out.print("x   ");
                 }
@@ -34,6 +44,15 @@ public class OutputView {
     }
 
     private static String getNationColor(Piece piece) {
-        return piece.getTeam().name().equals("CHU") ? CHU_COLOR : HAN_COLOR;
+        if(piece.getTeamName().equals(CHU_NATION_NAME))
+            return CHU_COLOR;
+        return HAN_COLOR;
+    }
+
+    private String matchSoldierName(String type, Piece piece) {
+        if (type.equals(CHU_SOLDIER) && piece.getTeamName().equals(HAN_NATION_NAME)) {
+            type = HAN_SOLDIER;
+        }
+        return type;
     }
 }
