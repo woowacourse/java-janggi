0b71593f03e13fd3ab06647b4021e6fda8a65e79
kangrae <kangrae0501@gmail.com>
2026-03-29 16:50:57 +0900

    test: 장과 사 기물에 대한 테스트 추가

---
+++ src/test/java/janggi/domain/piece/GuardMoveStrategyTest.java
@@ -0,0 +1,36 @@
+package janggi.domain.piece;
+
+import janggi.domain.dynasty.Dynasty;
+import janggi.domain.position.Position;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GuardMoveStrategyTest {
+
+    @Test
+    public void 사_기물의_이동가능한_위치_목록을_반환한다() {
+        // given
+        Map<Position, Piece> board = new HashMap<>();
+        PieceType pieceType = PieceType.GUARD;
+        Dynasty dynasty = Dynasty.CHO;
+        Position from = Position.from(5, 5);
+        board.put(from, new Piece(dynasty, pieceType));
+        board.put(Position.from(6, 5), new Piece(dynasty, pieceType));
+        board.put(Position.from(5, 6), new Piece(Dynasty.HAN, pieceType));
+
+        // when
+        List<Position> positions = pieceType.moveStrategy().canMovePositions(board, from, dynasty);
+
+        // then
+        Assertions.assertThat(positions)
+                .containsExactlyInAnyOrder(
+                        Position.from(5, 6),
+                        Position.from(4, 5),
+                        Position.from(5, 4)
+                );
+    }
+
+}
