b8b5f9d2f8efabdfab1be96f8b4aa21a8ee8458f
poketopa <lhs5427ll@gmail.com>
2026-03-29 18:50:43 +0900

    feat(controller): 장기 컨트롤러 생성

--- src/main/java/controller/JanggiController.java
+++ src/main/java/controller/JanggiController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import domain.board.Formation;
+import domain.board.JanggiBoard;
+import domain.board.JanggiGenerator;
+import domain.game.Game;
+import domain.point.Point;
+import domain.team.Team;
+import dto.MoveDTO;
+import view.InputView;
+import view.OutputView;
+
 public class JanggiController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public JanggiController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+        Formation hanFormation = Formation.valueOf(inputView.inputHanWingSetup());
+        Formation choFormation = Formation.valueOf(inputView.inputChoWingSetup());
+        JanggiGenerator janggiGenerator = new JanggiGenerator(hanFormation, choFormation);
+
+        Game game = new Game(new JanggiBoard(janggiGenerator));
+
+        while(true){
+            try {
+                outputView.printCurrentBoardStatus(game.boardStatus());
+                final Team turn = game.currentTurn();
+                outputView.printCurrentTurn(turn);
+                MoveDTO move = new MoveDTO(inputView.inputMovePiecePoint(), inputView.inputDestinationPoint());
+                game.processTurn(move);
+            } catch (IllegalArgumentException e){
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 }
