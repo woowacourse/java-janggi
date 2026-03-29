68ccf14e12909af469a25d2a78687714eef6dfd9
poketopa <lhs5427ll@gmail.com>
2026-03-29 18:50:30 +0900

    feat(Application): 프로그램 시작 도입부 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import controller.JanggiController;
+import domain.board.JanggiBoard;
+import domain.board.JanggiGenerator;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        JanggiController janggiController = new JanggiController(inputView, outputView);
+        janggiController.run();
+
+    }
+
 }
