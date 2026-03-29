6358f66f42147e3f23a2c0c97efbf13b7b5b0c47
mvg01 <dkms625@naver.com>
2026-03-29 13:50:31 +0900

    refactor: SlidingMoveRule 단순화
    - 항상 1개의 Movement를 활용함

--- src/main/java/janggi/domain/movement/SlidingMoveRule.java
+++ src/main/java/janggi/domain/movement/SlidingMoveRule.java
@@ -3,7 +3,6 @@ package janggi.domain.movement;
 import janggi.domain.Position;
 import janggi.domain.board.BoardMediator;
 import janggi.domain.piece.Piece;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SlidingMoveRule implements MoveRule {
@@ -16,22 +15,8 @@ public class SlidingMoveRule implements MoveRule {
 
     @Override
     public List<Position> execute(Position from, final BoardMediator boardMediator) {
-        final List<Position> traces = new ArrayList<>();
         final Piece piece = boardMediator.getPieceInPosition(from);
-        for (int index = 0; index < movementOrder.size() - 1; index++) {
-            final Movement movement = movementOrder.get(index);
-            traces.addAll(movement.calculateTraces(from, piece, boardMediator));
-            final Position destination = movement.calculateDestination(from, piece, boardMediator);
-            traces.add(destination);
-            from = destination;
-        }
-        return findLastPosition(from, piece, boardMediator, traces);
-    }
-
-    private List<Position> findLastPosition(final Position from, final Piece piece, final BoardMediator boardMediator,
-                                            final List<Position> traces) {
-        final Movement lastMovement = movementOrder.getLast();
-        traces.addAll(lastMovement.calculateTraces(from, piece, boardMediator));
-        return traces;
+        final Movement movement = movementOrder.getFirst();
+        return movement.calculateTraces(from, piece, boardMediator);
     }
 }
