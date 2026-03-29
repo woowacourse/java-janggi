1b6d38e84c1ae687c880de4058031b32cf1c8870
김병웅 <quddnddl35@naver.com>
2026-03-29 22:22:00 +0900

    refactor : PositionParser 리스트로 변경

--- src/main/java/parser/PositionParser.java
+++ src/main/java/parser/PositionParser.java
@@ -2,24 +2,28 @@ package parser;
 
 import domain.position.Position;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PositionParser {
 
     private static final String SEPARATOR = ",";
     private static final int COORDINATE_SIZE = 2;
+    private static final int ROW_INDEX = 0;
+    private static final int COLUMN_INDEX = 1;
 
     private PositionParser() {
     }
 
     public static Position parsePosition(String input) {
-
-        int[] coordinates = Arrays.stream(input.split(SEPARATOR))
+        List<Integer> points = Arrays.stream(input.split(SEPARATOR))
                 .map(String::trim)
-                .mapToInt(PositionParser::parseInt)
-                .toArray();
+                .map(PositionParser::parseInt)
+                .toList();
 
-        validateSize(coordinates);
-        return new Position(coordinates[0], coordinates[1]);
+        validateSize(points);
+
+        return new Position(points.get(ROW_INDEX), points.get(COLUMN_INDEX));
     }
 
     private static int parseInt(String input) {
@@ -30,8 +34,8 @@ public class PositionParser {
         }
     }
 
-    private static void validateSize(int[] coordinates) {
-        if (coordinates.length != COORDINATE_SIZE) {
+    private static void validateSize(List<Integer> points) {
+        if (points.size() != COORDINATE_SIZE) {
             throw new IllegalArgumentException(
                     "[ERROR] 2의 숫자를 ','로 구분하여 입력해야 합니다."
             );
