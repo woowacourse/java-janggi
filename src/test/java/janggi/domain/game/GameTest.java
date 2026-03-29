4571d9bea373a217845eace7a8c0cb56b9025197
jhk01007 <jhk001007@naver.com>
2026-03-29 23:20:39 +0900

    test: movePiece()가 턴을 잘 바꾸는지 테스트

--- src/test/java/janggi/domain/game/GameTest.java
+++ src/test/java/janggi/domain/game/GameTest.java
@@ -1,21 +1,34 @@
 package janggi.domain.game;
 
-import static janggi.domain.board.HorseElephantPosition.HEHE;
 import static janggi.domain.dynasty.Dynasty.CHO;
-import static janggi.domain.dynasty.Dynasty.HAN;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import janggi.domain.board.BoardDesignPolicy;
+import janggi.domain.dynasty.Dynasty;
+import janggi.domain.piece.ChariotMoveStrategy;
+import janggi.domain.piece.HorseMoveStrategy;
+import janggi.domain.piece.Piece;
+import janggi.domain.piece.SoldierMoveStrategy;
 import janggi.domain.position.Position;
 import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
 
     @Test
-    public void 선택된_기물이_움직일_수_있는_위치가_없다면_오류를_일으킨다() {
+    @DisplayName("선택된 기물이 움직일 수 있는 위치가 없다면 오류를 일으킨다")
+    public void canMovePositions_fail_no_position() {
         // given
-        Game game = Game.initGame(Map.of(CHO, HEHE, HAN, HEHE));
-        Position from = Position.from(3, 2);
+        Position from = Position.from(1, 1);
+        BoardDesignPolicy boardDesignPolicy = () -> Map.of(
+                from, new Piece(CHO, new ChariotMoveStrategy()),
+                Position.from(1, 2), new Piece(CHO, new HorseMoveStrategy()),
+                Position.from(2, 1), new Piece(CHO, new SoldierMoveStrategy())
+        );
+        Game game = Game.initGame(boardDesignPolicy);
 
         // when & then
         assertThatThrownBy(() -> game.canMovePosition(from))
@@ -23,4 +36,21 @@ class GameTest {
                 .hasMessageContaining("선택된 기물이 이동할 수 있는 위치가 없습니다.");
     }
 
+    @Test
+    @DisplayName("기물을 움직인 후에는 턴이 변경되어야 한다.")
+    public void movePiece_success_change_turn() throws Exception {
+        // given
+        Position from = Position.from(1, 1);
+        BoardDesignPolicy boardDesignPolicy = () -> Map.of(
+                from, new Piece(CHO, new ChariotMoveStrategy())
+        );
+        Game game = Game.initGame(boardDesignPolicy);
+        Dynasty currentTurn = game.currentTurn();
+
+        // when
+        game.movePiece(from, Position.from(1, 2));
+        
+        // then
+        assertThat(game.currentTurn()).isEqualTo(currentTurn.next());
+    }
 }
