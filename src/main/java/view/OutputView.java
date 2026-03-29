f099fbad80b10fe4a2682fef3c8250db117bf18a
LeeJaeHyeok97 <hazardous10@naver.com>
2026-03-29 10:30:26 +0900

    feat: 콘솔 출력 국가별 색 구분 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,22 +6,34 @@ import domain.vo.Position;
 import java.util.Map;
 
 public class OutputView {
+    private static final String RESET_COLOR = "\u001B[0m";
+    private static final String CHU_COLOR = "\u001B[32m";
+    private static final String HAN_COLOR = "\u001B[31m";
+
+    private static final int MAX_COL = 8;
+    private static final int MAX_ROW = 9;
 
     public void printBoard(Map<Position, Piece> board) {
         System.out.println();
-        for (int row = 9; row >= 0; row--) {
-            for (int col = 0; col <= 8; col++) {
+        for (int row = MAX_ROW; row >= 0; row--) {
+            for (int col = 0; col <= MAX_COL; col++) {
                 Position position = Position.of(row, col);
                 if (board.containsKey(position)) {
                     Piece piece = board.get(position);
-                    System.out.print(piece.getTypeName() + "/" + piece.getTeamName() + " ");
+                    String color = getNationColor(piece);
+                    System.out.print(color + piece.getTypeName() + RESET_COLOR + "  ");
                 } else {
-                    System.out.print("x ");
+                    System.out.print("x   ");
                 }
             }
+            System.out.println();
             System.out.println();
         }
 
         System.out.println();
+    }
+
+    private static String getNationColor(Piece piece) {
+        return piece.getTeam().name().equals("CHU") ? CHU_COLOR : HAN_COLOR;
     }
 }
