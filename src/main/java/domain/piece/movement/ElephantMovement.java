fa87834117301d8aa1d08abb6e9474cf3f06061f
HyoYoonNam <dev.rudevico@gmail.com>
2026-03-29 16:17:20 +0900

    feat: 상의 행마법 전략 구현

---
+++ src/main/java/domain/piece/movement/ElephantMovement.java
@@ -0,0 +1,71 @@
+package domain.piece.movement;
+
+import domain.board.Intersection;
+import domain.direction.Direction;
+import domain.direction.MoveAmount;
+import domain.game.Side;
+import java.util.List;
+import java.util.stream.Stream;
+
+public final class ElephantMovement extends PieceMovement {
+
+    private static final MoveAmount MOVE_AMOUNT = new MoveAmount(1);
+
+    @Override
+    protected List<Intersection> candidateIntersections(Intersection from, Side side) {
+        return Stream.of(forwardDirectionIntersections(from, side),
+                        backWardDirectionIntersections(from, side),
+                        leftDirectionIntersections(from, side),
+                        rightDirectionIntersections(from, side))
+                .flatMap(List::stream)
+                .toList();
+    }
+
+    private static List<Intersection> forwardDirectionIntersections(Intersection from, Side side) {
+        Direction forwardDirection = side.getForwardDirection();
+        Intersection forward = forwardDirection.moveForward(from, MOVE_AMOUNT);
+        Intersection forwardLeft = forwardDirection.moveForwardLeft(forward, MOVE_AMOUNT);
+        Intersection forwardLeft2Times = forwardDirection.moveForwardLeft(forwardLeft, MOVE_AMOUNT);
+
+        Intersection forwardRight = forwardDirection.moveForwardRight(forward, MOVE_AMOUNT);
+        Intersection forwardRight2Times = forwardDirection.moveForwardRight(forwardRight, MOVE_AMOUNT);
+
+        return List.of(forwardLeft2Times, forwardRight2Times);
+    }
+
+    private static List<Intersection> backWardDirectionIntersections(Intersection from, Side side) {
+        Direction backwardDirection = side.getBackwardDirection();
+        Intersection forward = backwardDirection.moveForward(from, MOVE_AMOUNT);
+        Intersection forwardLeft = backwardDirection.moveForwardLeft(forward, MOVE_AMOUNT);
+        Intersection forwardLeft2Times = backwardDirection.moveForwardLeft(forwardLeft, MOVE_AMOUNT);
+
+        Intersection forwardRight = backwardDirection.moveForwardRight(forward, MOVE_AMOUNT);
+        Intersection forwardRight2Times = backwardDirection.moveForwardRight(forwardRight, MOVE_AMOUNT);
+
+        return List.of(forwardLeft2Times, forwardRight2Times);
+    }
+
+    private static List<Intersection> leftDirectionIntersections(Intersection from, Side side) {
+        Direction leftDirection = side.getLeftDirection();
+        Intersection forward = leftDirection.moveForward(from, MOVE_AMOUNT);
+        Intersection forwardLeft = leftDirection.moveForwardLeft(forward, MOVE_AMOUNT);
+        Intersection forwardLeft2Times = leftDirection.moveForwardLeft(forwardLeft, MOVE_AMOUNT);
+
+        Intersection forwardRight = leftDirection.moveForwardRight(forward, MOVE_AMOUNT);
+        Intersection forwardRight2Times = leftDirection.moveForwardRight(forwardRight, MOVE_AMOUNT);
+
+        return List.of(forwardLeft2Times, forwardRight2Times);
+    }
+
+    private static List<Intersection> rightDirectionIntersections(Intersection from, Side side) {
+        Direction rightDirection = side.getRightDirection();
+        Intersection forward = rightDirection.moveForward(from, MOVE_AMOUNT);
+        Intersection forwardLeft = rightDirection.moveForwardLeft(forward, MOVE_AMOUNT);
+        Intersection forwardLeft2Times = rightDirection.moveForwardLeft(forwardLeft, MOVE_AMOUNT);
+
+        Intersection forwardRight = rightDirection.moveForwardRight(forward, MOVE_AMOUNT);
+        Intersection forwardRight2Times = rightDirection.moveForwardRight(forwardRight, MOVE_AMOUNT);
+
+        return List.of(forwardLeft2Times, forwardRight2Times);
+    }
+}
