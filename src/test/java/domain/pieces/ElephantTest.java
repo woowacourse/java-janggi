627b90615b66335a214d155ea1e478a65a31bf6f
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-29 19:49:13 +0900

    test: 기물 이동 테스트 작성

---
+++ src/test/java/domain/pieces/ElephantTest.java
@@ -0,0 +1,61 @@
+package domain.pieces;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.Board;
+import domain.Camp;
+import domain.Position;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ElephantTest {
+
+    @Test
+    void 위로_오른대각선_이동_가능() {
+        Board board = new Board();
+        Elephant elephant = new Elephant(Camp.HAN);
+        Position fromPosition = new Position(2, 9);
+        Position toPosition = new Position(4, 6);
+
+        Assertions.assertTrue(elephant.canMove(fromPosition, toPosition, board));
+    }
+
+    @Test
+    void 위로_오른대각선_이동() {
+        Board board = new Board();
+        Elephant elephant = new Elephant(Camp.HAN);
+        Position fromPosition = new Position(2, 9);
+        Position toPosition = new Position(4, 6);
+
+        board.locatePiece(fromPosition, elephant);
+        board.move(fromPosition, toPosition);
+
+        Assertions.assertFalse(board.isExist(fromPosition));
+        Assertions.assertEquals(board.getPieceFrom(toPosition), elephant);
+    }
+
+    @Test
+    void 위로_오른대각선_경로_막힘() {
+        Board board = new Board();
+        Elephant elephant = new Elephant(Camp.HAN);
+        Piece piece = new Piece(Camp.HAN);
+        Position fromPosition = new Position(2, 9);
+        Position toPosition = new Position(4, 6);
+
+        board.locatePiece(fromPosition, elephant);
+        board.locatePiece(new Position(2, 8), piece);
+
+        assertThatThrownBy(() -> board.move(fromPosition, toPosition)).isInstanceOf(
+                IllegalArgumentException.class).hasMessageContaining("[ERROR]", "이동할");
+    }
+
+    @Test
+    void 이동_불가_좌표_이동_불가() {
+        Board board = new Board();
+        Elephant elephant = new Elephant(Camp.HAN);
+        Position fromPosition = new Position(2, 9);
+        Position toPosition = new Position(0, 0);
+
+        Assertions.assertFalse(elephant.canMove(fromPosition, toPosition, board));
+    }
+}
