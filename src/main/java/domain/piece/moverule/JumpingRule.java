09d3660d3886fddb82b432ba5fbfdc3df7948835
HyoYoonNam <dev.rudevico@gmail.com>
2026-03-30 02:36:53 +0900

    feat: 뛰어 넘을 기물이 필요한 이동 규칙 구현
    경로에서 처음 만난 기물을 뛰어 넘을 수 있다.
    
    뛰어 넘을 기물이 존재해야만 하며, 포는 뛰어 넘을 수 없다.
    
    이동할 경로에 다른 기물이 존재하면 이동이 불가능하다.
    
    목적지에 아군 기물이 존재하면 이동이 불가능하다.
    
    목적지에 포가 존재하면 이동이 불가능하다.

---
+++ src/main/java/domain/piece/moverule/JumpingRule.java
@@ -0,0 +1,37 @@
+package domain.piece.moverule;
+
+import domain.board.Intersection;
+import domain.game.Side;
+import domain.piece.AlivePieces;
+import domain.piece.Cannon;
+import domain.piece.Piece;
+import domain.piece.movement.Path;
+import java.util.List;
+
+public final class JumpingRule {
+
+    // TODO 필터링 메서드를 유틸로 분리해서 가독성 좋게 네이밍 고민
+    public List<Intersection> movableDestinations(Side side, List<Path> candidatePaths, AlivePieces alivePieces) {
+        return candidatePaths.stream()
+                .filter(path -> {
+                    List<Piece> screens = path.passingIntersections()
+                            .stream()
+                            .filter(alivePieces::isNotEmpty)
+                            .map(alivePieces::placedAt)
+                            .toList();
+
+                    return screens.size() == 1 && !(screens.getFirst() instanceof Cannon);
+                })
+                .map(Path::destination)
+                .filter(destination -> {
+                    if (alivePieces.isEmpty(destination)) {
+                        return true;
+                    }
+
+                    Piece placedAt = alivePieces.placedAt(destination);
+
+                    return placedAt.hasDifferentSide(side) && !(placedAt instanceof Cannon);
+                })
+                .toList();
+    }
+}
