ad716224edc8ca7025ba6d718d1c952457b3efbf
choubung <choubung@sookmyung.ac.kr>
2026-03-30 12:38:07 +0900

    refactor(advisorMoveRule) : position의 캡슐화를 지키도록 리팩토링

--- src/main/java/janggi/domain/mouveRule/AdvisorMoveRule.java
+++ src/main/java/janggi/domain/mouveRule/AdvisorMoveRule.java
@@ -1,13 +1,29 @@
 package janggi.domain.mouveRule;
 
-import janggi.domain.BoardView;
+import janggi.domain.Direction;
+import janggi.domain.board.BoardView;
 import janggi.domain.vo.Position;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvisorMoveRule implements MoveRule {
+    private static final List<Direction> ADVISOR_PATHS = List.of(Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST);
+
     @Override
     public boolean canMove(Position from, Position to, BoardView board) {
-        int rowDis = Math.abs(to.getRow() - from.getRow());
-        int colDis = Math.abs(to.getCol() - from.getCol());
-        return rowDis <= 1 && colDis <= 1;
+        return candidatePositions(from).contains(to);
+    }
+
+    private List<Position> candidatePositions(Position from) {
+        List<Position> positions =  new ArrayList<>();
+
+        for (Direction direction : ADVISOR_PATHS) {
+            if (from.hasNext(direction)) {
+                positions.add(from.nextPosition(direction));
+            }
+        }
+
+        return positions;
     }
 }
