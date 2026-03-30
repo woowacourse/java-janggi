338f5227ba65c1a9dfc33a5111b672b9dc6d2162
armd482 <armd482@naver.com>
2026-03-31 08:15:02 +0900

    refactor: pawn StepPiece을 상속하여 중복코드 제거

--- src/main/java/janggi/domain/piece/Pawn.java
+++ src/main/java/janggi/domain/piece/Pawn.java
@@ -2,21 +2,14 @@ package janggi.domain.piece;
 
 import janggi.domain.Movement;
 import janggi.domain.Movements;
-import janggi.domain.Position;
-import janggi.domain.Route;
 import janggi.domain.Side;
-import janggi.domain.board.BoardInterface;
 import janggi.domain.policy.ClearPathPolicy;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-public class Pawn extends ActivePiece {
-    private final List<Movements> moveRange;
-
+public class Pawn extends StepPiece {
     public Pawn(Side side, List<Movements> moveRange) {
-        super(new ClearPathPolicy(), side, PieceType.PAWN);
-        this.moveRange = moveRange;
+        super(moveRange, new ClearPathPolicy(), side, PieceType.PAWN);
     }
 
     public static Pawn from(Side side) {
@@ -30,21 +23,5 @@ public class Pawn extends ActivePiece {
             return new Movements(List.of(Movement.UP));
         }
         return new Movements(List.of(Movement.DOWN));
-    }
-
-    @Override
-    public Route findRoute(Position start, Position end) {
-        return moveRange.stream()
-                .map(movements -> movements.calculatePath(start))
-                .flatMap(Optional::stream)
-                .filter(route -> route.isArrivalPoint(end))
-                .findFirst().orElseThrow(() -> new IllegalArgumentException(INVALID_DESTINATION_MESSAGE));
-    }
-
-    @Override
-    public void validateRoute(Route route, BoardInterface boardInterface) {
-        if (!routePolicy.isMovable(route, side, boardInterface)) {
-            throw new IllegalArgumentException(UNMOVABLE_ROUTE_MESSAGE);
-        }
     }
 }
