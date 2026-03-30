d58c14f122e9df997218a9be444069b79f9586fb
choubung <choubung@sookmyung.ac.kr>
2026-03-30 14:57:57 +0900

    test : 마 이동 케이스 추가

--- src/test/java/janggi/domain/mouveRule/HorseMoveRuleTest.java
+++ src/test/java/janggi/domain/mouveRule/HorseMoveRuleTest.java
@@ -5,7 +5,6 @@ import janggi.domain.board.BoardView;
 import janggi.domain.piece.Soldier;
 import janggi.domain.piece.Team;
 import janggi.domain.vo.Position;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,13 +30,30 @@ class HorseMoveRuleTest {
 
     private static Stream<Arguments> provideNormalMove() {
         return Stream.of(
-                Arguments.of("빈 칸으로 정상 이동 (북->북동)",
+                Arguments.of("남쪽 방향 정상 이동 (남->남동)",
                         new Position(0, 0), new Position(2, 1), new FakeBoard()),
+                Arguments.of("남쪽 방향 정상 이동 (남->남서)",
+                        new Position(0, 4), new Position(2, 3), new FakeBoard()),
 
-                Arguments.of("적군을 잡으며 이동",
-                        new Position(0, 0), new Position(2, 1),
-                        FakeBoard.createBoardWith(new Position(2, 1), new Soldier(Team.HAN)))
-        );
+                Arguments.of("북쪽 방향 정상 이동 (북->북동)",
+                        new Position(9, 4), new Position(7, 5), new FakeBoard()),
+                Arguments.of("북쪽 방향 정상 이동 (북->북서)",
+                        new Position(9, 8), new Position(7, 7), new FakeBoard()),
+
+                Arguments.of("동쪽 방향 정상 이동 (동->동북)",
+                        new Position(4, 4), new Position(3, 6), new FakeBoard()),
+                Arguments.of("동쪽 방향 정상 이동 (동->동남)",
+                        new Position(4, 4), new Position(5, 6), new FakeBoard()),
+
+                Arguments.of("서쪽 방향 정상 이동 (서->서북)",
+                        new Position(4, 4), new Position(3, 2), new FakeBoard()),
+                Arguments.of("서쪽 방향 정상 이동 (서->서남)",
+                        new Position(4, 4), new Position(5, 2), new FakeBoard()),
+
+                Arguments.of("적군을 잡으며 이동 (서->서북)",
+                        new Position(4, 4), new Position(3, 2),
+                        FakeBoard.createBoardWith(new Position(3, 2), new Soldier(Team.HAN)))
+                );
     }
 
     private static Stream<Arguments> provideInvalidMove() {
