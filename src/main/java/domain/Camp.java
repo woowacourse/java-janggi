8d5d4e885fe88c521a6cbfd2e856408cc037b9c5
originalchaltteokcookie <wndrnrdk@naver.com>
2026-03-30 11:09:12 +0900

    feat: Camp enum 한글 이름 매핑

--- src/main/java/domain/Camp.java
+++ src/main/java/domain/Camp.java
@@ -1,6 +1,17 @@
 package domain;
 
 public enum Camp {
-    HAN,
-    CHO
+    HAN("한"),
+    CHO("초");
+
+    private final String campName;
+
+    Camp(String campName) {
+        this.campName = campName;
+    }
+
+    public String getCampName() {
+        return campName;
+    }
 }
+
