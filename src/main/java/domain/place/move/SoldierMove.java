46ed7a1456d7eef35a5fa616857c1299318a97ce
quddaz <quddnddl35@naver.com>
2026-03-30 18:12:29 +0900

    feat : 패스 방식 구현 중

---
+++ src/main/java/domain/place/move/SoldierMove.java
@@ -0,0 +1,40 @@
+package domain.place.move;
+
+import domain.place.Place;
+import domain.place.moveStrategy.Direction;
+import domain.place.piece.Side;
+import domain.position.Position;
+import java.util.List;
+import java.util.Map;
+
+public class SoldierMove implements Move {
+
+    private final List<Direction> directions;
+
+    public SoldierMove(Side side) {
+        this.directions = initDirections(side);
+    }
+
+    private List<Direction> initDirections(Side side) {
+        if (side == Side.CHO) {
+            return List.of(Direction.DOWN, Direction.LEFT, Direction.RIGHT);
+        }
+        return List.of(Direction.TOP, Direction.LEFT, Direction.RIGHT);
+    }
+
+    @Override
+    public List<Position> getPath(Position from) {
+        return directions.stream()
+                .flatMap(d -> from.moveIfInBounds(d).stream())
+                .toList();
+    }
+
+    @Override
+    public boolean canMove(Map<Position, Place> path, Position from, Position to) {
+        return directions.stream()
+                .flatMap(d -> from.moveIfInBounds(d).stream())
+                .filter(p -> !path.containsKey(p))
+                .anyMatch(to::equals);
+    }
+
+}
