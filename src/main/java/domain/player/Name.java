f56f68281b7889064f27c4f233c279d20595cdf4
toctoce <toctoce@naver.com>
2026-03-30 13:32:12 +0900

    feat: 이름 검증 메서드 구현

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -1,4 +1,48 @@
 package domain.player;
 
+
 public record Name(String value) {
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 8;
+    private static final String INVALID_NAME_MESSAGE =
+            "닉네임은 %d~%d글자여야 합니다.".formatted(MIN_NAME_LENGTH, MAX_NAME_LENGTH);
+
+    public Name(String value) {
+        String trimmed = value.trim();
+        validateName(trimmed);
+        this.value = trimmed;
+    }
+
+    private void validateName(String value) {
+        if (isInvalidLength(value) || hasInvalidCharacter(value)) {
+            throw new IllegalArgumentException(INVALID_NAME_MESSAGE);
+        }
+    }
+
+    private boolean isInvalidLength(String value) {
+        return value.length() < MIN_NAME_LENGTH || value.length() > MAX_NAME_LENGTH;
+    }
+
+    private boolean hasInvalidCharacter(String value) {
+        return value.chars()
+                .filter(this::isInvalidCharacter)
+                .findAny()
+                .isPresent();
+    }
+
+    private boolean isInvalidCharacter(int character) {
+        return !isEnglish(character) && !isNumber(character) && !isKorean(character);
+    }
+
+    private boolean isEnglish(int character) {
+        return character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z';
+    }
+
+    private boolean isNumber(int character) {
+        return character >= '0' && character <= '9';
+    }
+
+    private boolean isKorean(int character) {
+        return character >= '가' && character <= '힣';
+    }
 }
