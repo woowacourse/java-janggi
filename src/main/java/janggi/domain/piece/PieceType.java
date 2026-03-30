3a5c315f27d2f40905359951ad261d2ad5b6a470
Chocoding1 <galmeagi2@naver.com>
2026-03-30 11:11:36 +0900

    refactor: 미사용 필드 제거

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -1,28 +1,22 @@
 package janggi.domain.piece;
 
 public enum PieceType {
-    CHA("차", 13),
-    GUNG("궁", 0),
-    JOL("졸", 2),
-    MA("마", 5),
-    PO("포", 7),
-    SA("사", 3),
-    SANG("상", 3),
+    CHA("차"),
+    GUNG("궁"),
+    JOL("졸"),
+    MA("마"),
+    PO("포"),
+    SA("사"),
+    SANG("상"),
     ;
 
-    PieceType(String nickname, int score) {
+    PieceType(String nickname) {
         this.nickname = nickname;
-        this.score = score;
     }
 
     private final String nickname;
-    private final int score;
 
     public String getNickname() {
         return nickname;
-    }
-
-    public int getScore() {
-        return score;
     }
 }
