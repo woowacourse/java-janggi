4fd5b514d61c47aefd1eb90eda3dde7b27fadfe2
choubung <choubung@sookmyung.ac.kr>
2026-03-30 13:13:49 +0900

    test : 포 이동 테스트를 FakeBoard를 사용하도록 리팩토링

--- src/test/java/janggi/domain/mouveRule/CannonMoveRuleTest.java
+++ src/test/java/janggi/domain/mouveRule/CannonMoveRuleTest.java
@@ -1,64 +1,93 @@
 package janggi.domain.mouveRule;
 
-import static janggi.domain.BoardFixture.put;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import janggi.domain.Board;
+import janggi.domain.FakeBoard;
+import janggi.domain.piece.Advisor;
 import janggi.domain.piece.Cannon;
 import janggi.domain.piece.Soldier;
 import janggi.domain.piece.Team;
 import janggi.domain.vo.Position;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class CannonMoveRuleTest {
-
-    private Board board = Board.empty();
     private final MoveRule moveRule = new CannonMoveRule();
+    private FakeBoard board;
 
     @Test
-    void 정확히_하나의_기물을_넘어이동할수_있다() {
+    void 하나의_기물을_넘는_정상_이동_테스트() {
+        // given
+        Position from = new Position(0, 0);
+        Position to = new Position(0, 4);
 
-        Position other = new Position(0, 1);
-        put(board, other, new Soldier(Team.HAN));
-        assertThat(moveRule.canMove(new Position(0, 0), new Position(0, 6), board)).isTrue();
+        board = FakeBoard.createBoardWith(new Position(0, 2), new Soldier(Team.CHO));
+
+        // when, then
+        assertThat(moveRule.canMove(from, to, board)).isTrue();
     }
 
     @Test
-    void 넘을기물이_없으면_이동할수없다() {
-        assertThat(moveRule.canMove(new Position(0, 0), new Position(0, 5), board)).isFalse();
+    void 하나의_기물을_넘어_상대_기물_잡기_테스트() {
+        // given
+        Position from = new Position(0, 0);
+        Position to = new Position(0, 4);
+
+        board = FakeBoard.createBoardWith(new Position(0, 2), new Soldier(Team.CHO),
+                new Position(0, 4), new Advisor(Team.HAN));
+
+        // when, then
+        assertThat(moveRule.canMove(from, to, board)).isTrue();
     }
 
     @Test
-    void 경로에_기물이_2개_이상이면_이동할수없다() {
+    void 넘을_기물이_없으면_이동_불가하다() {
+        // given
+        Position from = new Position(0, 0);
+        Position to = new Position(0, 4);
 
-        Position other1 = new Position(0, 2);
-        Position other2 = new Position(0, 3);
-        put(board, other1, new Soldier(Team.HAN));
-        put(board, other2, new Soldier(Team.HAN));
+        board = new FakeBoard();
 
-        assertThat(moveRule.canMove(new Position(0, 0), new Position(0, 6), board)).isFalse();
+        // when, then
+        assertThat(moveRule.canMove(from, to, board)).isFalse();
+    }
+
+
+    @Test
+    void 경로에_기물이_2개_이상이면_이동_불가하다() {
+        // given
+        Position from = new Position(0, 0);
+        Position to = new Position(0, 5);
+
+        board = FakeBoard.createBoardWith(new Position(0, 2), new Soldier(Team.CHO),
+                new Position(0, 4), new Advisor(Team.HAN));
+
+        // when, then
+        assertThat(moveRule.canMove(from, to, board)).isFalse();
     }
 
     @Test
-    void 포를_포다리로_사용할수없다() {
-        Position other = new Position(0, 2);
-        put(board, other, new Cannon(Team.HAN));
+    void 포는_다리로_사용할_수_없다() {
+        // given
+        Position from = new Position(0, 0);
+        Position to = new Position(0, 4);
 
-        assertThat(moveRule.canMove(new Position(5, 0), new Position(5, 6), board)).isFalse();
+        board = FakeBoard.createBoardWith(new Position(0, 2), new Cannon(Team.CHO));
+
+        // when, then
+        assertThat(moveRule.canMove(from, to, board)).isFalse();
     }
 
     @Test
-    void 포를잡을_수_없다() {
+    void 포는_잡을_수_없다() {
+        // given
+        Position from = new Position(0, 0);
+        Position to = new Position(0, 4);
 
-        Position other1 = new Position(0, 2);
-        put(board, other1, new Cannon(Team.HAN));
+        board = FakeBoard.createBoardWith(new Position(0, 2), new Soldier(Team.CHO),
+                new Position(0, 4), new Cannon(Team.HAN));
 
-        Position other2 = new Position(0, 6);
-        put(board, other2, new Cannon(Team.HAN));
-
-        assertThat(moveRule.canMove(new Position(0, 0), new Position(0, 6), board)).isFalse();
+        // when, then
+        assertThat(moveRule.canMove(from, to, board)).isFalse();
     }
-
-
 }
