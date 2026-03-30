2080151a75e84827dac186809ff246a9ba855edf
haeyoon1 <hynam1220@naver.com>
2026-03-31 00:33:53 +0900

    feat: application 파일에 controller 추가

--- src/main/java/janggi/Application.java
+++ src/main/java/janggi/Application.java
@@ -1,8 +1,11 @@
 package janggi;
 
+import janggi.controller.JanggiController;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        JanggiController janggiController = new JanggiController();
+        janggiController.run();
     }
 }
