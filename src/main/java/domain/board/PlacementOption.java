b9e1638ef38d831d6a2e62e7d747213ffe49d78a
JohnPrk <uo3641493@gmail.com>
2026-03-30 19:44:44 +0900

    refactor: 배치 전략 추상 클래스 제거 및 PlacementOption Team 필드 추가
    - PlacementStrategy를 추상 클래스에서 인터페이스로 변경
    - ChoPlacementStrategy, HanPlacementStrategy 삭제
    - 8개 배치 클래스가 PlacementStrategy를 직접 구현하도록 변경
    - PlacementOption에 Team 필드 추가, startsWith 대신 Team 비교로 대체

--- src/main/java/domain/board/PlacementOption.java
+++ src/main/java/domain/board/PlacementOption.java
@@ -8,33 +8,36 @@ import domain.board.han.DefaultHanPlacement;
 import domain.board.han.InnerHorseHanPlacement;
 import domain.board.han.LeftInnerHorseHanPlacement;
 import domain.board.han.RightInnerHorseHanPlacement;
+import domain.piece.Team;
 import util.ErrorMessage;
 
 import java.util.Arrays;
 
 public enum PlacementOption {
 
-    HAN_DEFAULT("1", new DefaultHanPlacement()),
-    HAN_INNER_HORSE("2", new InnerHorseHanPlacement()),
-    HAN_LEFT_INNER_HORSE("3", new LeftInnerHorseHanPlacement()),
-    HAN_RIGHT_INNER_HORSE("4", new RightInnerHorseHanPlacement()),
+    HAN_DEFAULT(Team.HAN, "1", new DefaultHanPlacement()),
+    HAN_INNER_HORSE(Team.HAN, "2", new InnerHorseHanPlacement()),
+    HAN_LEFT_INNER_HORSE(Team.HAN, "3", new LeftInnerHorseHanPlacement()),
+    HAN_RIGHT_INNER_HORSE(Team.HAN, "4", new RightInnerHorseHanPlacement()),
 
-    CHO_DEFAULT("1", new DefaultChoPlacement()),
-    CHO_INNER_HORSE("2", new InnerHorseChoPlacement()),
-    CHO_LEFT_INNER_HORSE("3", new LeftInnerHorseChoPlacement()),
-    CHO_RIGHT_INNER_HORSE("4", new RightInnerHorseChoPlacement());
+    CHO_DEFAULT(Team.CHO, "1", new DefaultChoPlacement()),
+    CHO_INNER_HORSE(Team.CHO, "2", new InnerHorseChoPlacement()),
+    CHO_LEFT_INNER_HORSE(Team.CHO, "3", new LeftInnerHorseChoPlacement()),
+    CHO_RIGHT_INNER_HORSE(Team.CHO, "4", new RightInnerHorseChoPlacement());
 
+    private final Team team;
     private final String input;
     private final PlacementStrategy strategy;
 
-    PlacementOption(String input, PlacementStrategy strategy) {
+    PlacementOption(Team team, String input, PlacementStrategy strategy) {
+        this.team = team;
         this.input = input;
         this.strategy = strategy;
     }
 
     public static PlacementStrategy hanFrom(String input) {
         return Arrays.stream(values())
-                .filter(option -> option.name().startsWith("HAN"))
+                .filter(option -> option.team == Team.HAN)
                 .filter(option -> option.input.equals(input))
                 .map(option -> option.strategy)
                 .findFirst()
@@ -43,7 +46,7 @@ public enum PlacementOption {
 
     public static PlacementStrategy choFrom(String input) {
         return Arrays.stream(values())
-                .filter(option -> option.name().startsWith("CHO"))
+                .filter(option -> option.team == Team.CHO)
                 .filter(option -> option.input.equals(input))
                 .map(option -> option.strategy)
                 .findFirst()
--- src/main/java/domain/board/cho/LeftInnerHorseChoPlacement.java
+++ src/main/java/domain/board/cho/LeftInnerHorseChoPlacement.java
@@ -1,20 +1,33 @@
 package domain.board.cho;
 
+import domain.board.PlacementStrategy;
 import domain.coordination.Coordination;
-import domain.piece.Elephant;
-import domain.piece.Horse;
-import domain.piece.Piece;
-import domain.piece.Team;
+import domain.piece.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class LeftInnerHorseChoPlacement extends ChoPlacementStrategy {
+public class LeftInnerHorseChoPlacement implements PlacementStrategy {
 
     @Override
-    protected void placeHorseAndElephant(Map<Coordination, Piece> map) {
+    public Map<Coordination, Piece> place() {
+        Map<Coordination, Piece> map = new HashMap<>();
+        map.put(Coordination.of(1, 10), new Chariot(Team.CHO));
+        map.put(Coordination.of(9, 10), new Chariot(Team.CHO));
+        map.put(Coordination.of(4, 10), new Guard(Team.CHO));
+        map.put(Coordination.of(6, 10), new Guard(Team.CHO));
+        map.put(Coordination.of(5, 9), new General(Team.CHO));
+        map.put(Coordination.of(2, 8), new Cannon(Team.CHO));
+        map.put(Coordination.of(8, 8), new Cannon(Team.CHO));
+        map.put(Coordination.of(1, 7), new Soldier(Team.CHO));
+        map.put(Coordination.of(3, 7), new Soldier(Team.CHO));
+        map.put(Coordination.of(5, 7), new Soldier(Team.CHO));
+        map.put(Coordination.of(7, 7), new Soldier(Team.CHO));
+        map.put(Coordination.of(9, 7), new Soldier(Team.CHO));
         map.put(Coordination.of(2, 10), new Elephant(Team.CHO));
         map.put(Coordination.of(3, 10), new Horse(Team.CHO));
         map.put(Coordination.of(7, 10), new Elephant(Team.CHO));
         map.put(Coordination.of(8, 10), new Horse(Team.CHO));
+        return map;
     }
 }
