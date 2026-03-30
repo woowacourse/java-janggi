f4bae5ca40808b873d514771715ea976a8a9597d
Soojin6943 <sjhan0711@gmail.com>
2026-03-30 23:07:41 +0900

    test: 상차림 전략패턴에 대한 단위 테스트 구현

---
+++ src/test/java/domain/strategy/LeftElephantFormationStrategyTest.java
@@ -0,0 +1,47 @@
+package domain.strategy;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.PieceType;
+import domain.Position;
+import domain.Team;
+import domain.piece.Elephant;
+import domain.piece.Horse;
+import domain.piece.Piece;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import strategy.InitializeStrategy;
+import strategy.LeftElephantFormationStrategy;
+
+public class LeftElephantFormationStrategyTest {
+    private final InitializeStrategy strategy = new LeftElephantFormationStrategy();
+
+    @Test
+    void 초나라_상마상마_배치가_올바르게_생성된다() {
+        Map<Position, Piece> result = strategy.initialize(Team.CHO);
+
+        assertThat(result.get(Position.from(10, 2)))
+                .isInstanceOf(Elephant.class);
+        assertThat(result.get(Position.from(10, 3)))
+                .isInstanceOf(Horse.class);
+        assertThat(result.get(Position.from(10, 7)))
+                .isInstanceOf(Elephant.class);
+        assertThat(result.get(Position.from(10, 8)))
+                .isInstanceOf(Horse.class);
+    }
+
+    @Test
+    void 한나라_상마상마_배치가_올바르게_생성된다() {
+        Map<Position, Piece> result = strategy.initialize(Team.HAN);
+
+        assertThat(result.get(Position.from(1, 3)))
+                .isInstanceOf(Elephant.class);
+        assertThat(result.get(Position.from(1, 2)))
+                .isInstanceOf(Horse.class);
+        assertThat(result.get(Position.from(1, 8)))
+                .isInstanceOf(Elephant.class);
+        assertThat(result.get(Position.from(1, 7)))
+                .isInstanceOf(Horse.class);
+    }
+}
