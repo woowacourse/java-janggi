8484e7b786a773293834a258605d2cfae3a1c294
김가현 <hyeonnni@naver.com>
2026-03-30 22:27:33 +0900

    refactor(view): Board 메서드 변경에 따른 출력 로직 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,9 +1,11 @@
 package view;
 
 import domain.board.Board;
+import domain.piece.EmptyPiece;
 import domain.piece.Piece;
 import domain.piece.PieceAppearance;
 import domain.position.Position;
+import java.util.Map;
 
 public class OutputView {
     private static final int MAX_ROW = 10;
@@ -16,9 +18,10 @@ public class OutputView {
 
     public void printBoard(Board board) {
         StringBuilder sb = new StringBuilder();
+        Map<Position, Piece> boardState = board.currentPieces();
         for (int row = MAX_ROW; row >= 1; row--) {
             sb.append(row).append("\t");
-            appendRow(sb, board, row);
+            appendRow(sb, boardState, row);
             sb.append(System.lineSeparator());
         }
         appendColumnHeader(sb);
@@ -33,9 +36,9 @@ public class OutputView {
         sb.append(System.lineSeparator());
     }
 
-    private void appendRow(StringBuilder sb, Board board, int row) {
+    private void appendRow(StringBuilder sb, Map<Position, Piece> boardState, int row) {
         for (int col = 1; col <= MAX_COLUMN; col++) {
-            Piece piece = board.pieceAt(new Position(row, col));
+            Piece piece = boardState.getOrDefault(new Position(row, col), new EmptyPiece());
             sb.append(piece.display(appearance)).append("\t");
         }
     }
