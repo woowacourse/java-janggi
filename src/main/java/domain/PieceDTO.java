97661c686574b59ee6368b27969743cf7b1bc6a1
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-03-30 21:04:32 +0900

    feat: 기물 객체 출력용 dto 생성

---
+++ src/main/java/domain/PieceDTO.java
@@ -0,0 +1,10 @@
+package domain;
+
+import domain.piece.Piece;
+import domain.position.Position;
+
+public record PieceDTO(int row, int col, Team team) {
+    public static PieceDTO from(Position position, Piece piece) {
+        return new PieceDTO(position.getRows(), position.getColumns(), piece.getTeam());
+    }
+}
