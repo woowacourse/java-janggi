a32dd2b833de902564e7f24147931f7f012b485c
김병웅 <quddnddl35@naver.com>
2026-03-30 23:35:45 +0900

    refactor : 패스 방식으로 변경

---
+++ src/main/java/domain/place/moveStrategy/Direction.java
@@ -0,0 +1,29 @@
+package domain.place.moveStrategy;
+
+public enum Direction {
+    TOP(1, 0),
+    DOWN(-1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    LEFT_TOP(1, -1),
+    RIGHT_TOP(1, 1),
+    LEFT_DOWN(-1, -1),
+    RIGHT_DOWN(-1, 1);
+
+    private final int row;
+    private final int column;
+
+    Direction(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+}
