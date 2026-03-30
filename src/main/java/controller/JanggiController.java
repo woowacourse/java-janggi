41fea0f79f77135031a94c37b7ac2c99bd653da0
HyoYoonNam <dev.rudevico@gmail.com>
2026-03-30 17:19:50 +0900

    feat: 초기화된 장기판 출력 기능 구현

--- src/main/java/controller/JanggiController.java
+++ src/main/java/controller/JanggiController.java
@@ -1,7 +1,12 @@
 package controller;
 
+import domain.board.Board;
+import domain.board.InitialPieces;
 import domain.board.wing.ChoWings;
 import domain.board.wing.HanWings;
+import domain.game.JanggiGame;
+import domain.piece.AlivePieces;
+import dto.BoardDto;
 import view.InputView;
 import view.OutputView;
 
@@ -20,5 +25,12 @@ public final class JanggiController {
 
         ChoWings choWings = inputView.readChoWings();
         HanWings hanWings = inputView.readHanWings();
+
+        InitialPieces initialPieces = new InitialPieces(hanWings, choWings);
+        AlivePieces alivePieces = new AlivePieces(initialPieces.get());
+        Board board = new Board(alivePieces);
+        JanggiGame janggiGame = new JanggiGame(board);
+
+        outputView.printBoard(board);
     }
 }
