b200d507211db534b138d2e1db3b0ed07e871ea4
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-30 16:18:48 +0900

    test(jol): 궁성에서 한나라 졸은 위로 못가는 검증 테스트 작성

--- src/test/java/janggi/domain/piece/JolTest.java
+++ src/test/java/janggi/domain/piece/JolTest.java
@@ -1,11 +1,14 @@
 package janggi.domain.piece;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import janggi.domain.point.Point;
 import janggi.domain.status.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class JolTest {
 
@@ -46,5 +49,35 @@ public class JolTest {
         // when & then
         assertThatThrownBy(() -> jol.getRoutePoints(from, to))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("궁성 안에서는 대각선으로 이동 할 수 있다.")
+    void can_move_diagonal() {
+        Piece jol = new Jol(Team.HAN);
+        Point from = Point.of(5, 2);
+        Point to = Point.of(4, 1);
+        assertThat(jol.getRoutePoints(from, to)
+                .getPoints()
+                .getFirst()
+                .getColumn()
+        ).isEqualTo(4);
+        assertThat(jol.getRoutePoints(from, to)
+                .getPoints()
+                .getFirst()
+                .getRow()
+        ).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("궁성 안에서 한나라는 위로 대각선은 못간다.")
+    @CsvSource(value = {"4:1:5:2", "4:1:3:2"}, delimiter = ':')
+    void can_not_move_to_up_diagonal(int fromColumn, int fromRow, int toColumn, int toRow) {
+        Piece jol = new Jol(Team.HAN);
+        Point from = Point.of(fromColumn, fromRow);
+        Point to = Point.of(toColumn, toRow);
+        assertThatThrownBy(() -> jol.getRoutePoints(from, to))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 해당 기물의 이동 규칙에 어긋납니다.");
     }
 }
