6f79f8520fa01b69d74422efb05755dc05103a39
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-30 15:55:31 +0900

    refactor(Pho): 궁성 안에서 움직임 구현

--- src/main/java/janggi/domain/piece/Pho.java
+++ src/main/java/janggi/domain/piece/Pho.java
@@ -2,6 +2,7 @@ package janggi.domain.piece;
 
 import static java.lang.Math.abs;
 
+import janggi.domain.piece.direction.CastleDirection;
 import janggi.domain.point.Point;
 import janggi.domain.point.Points;
 import janggi.domain.point.Route;
@@ -19,24 +20,28 @@ public class Pho extends AbstractPiece {
     public Points getRoutePoints(Point from, Point to) {
         int pathCol = to.calculatePathColumn(from);
         int pathRow = to.calculatePathRow(from);
-
+        int signCol = Integer.compare(pathCol, 0);
+        int signRow = Integer.compare(pathRow, 0);
+        List<Point> points = new ArrayList<>();
+        if (from.inSameCastle(to)) {
+            CastleDirection castleDirection = CastleDirection.find(from, signCol, signRow);
+            points.add(Point.of(
+                    from.getColumn() + castleDirection.getTargetCol(),
+                    from.getRow() + castleDirection.getTargetRow()
+                    )
+            );
+            return new Points(points);
+        }
         if (pathRow != 0 && pathCol != 0) {
             throw new IllegalArgumentException("[ERROR] 해당 기물의 이동 규칙에 어긋납니다.");
         }
-
-        int signCol = Integer.compare(pathCol, 0);
-        int signRow = Integer.compare(pathRow, 0);
-
         int distance = Math.max(abs(pathCol), abs(pathRow));
-
-        List<Point> route = new ArrayList<>();
-
         for (int i = 1; i < distance; i++) {
             int nextCol = from.getColumn() + (signCol * i);
             int nextRow = from.getRow() + (signRow * i);
-            route.add(Point.of(nextCol, nextRow));
+            points.add(Point.of(nextCol, nextRow));
         }
-        return new Points(route);
+        return new Points(points);
     }
 
     @Override
