ab64a26eb1c6389bd46f26f6b37daf5cde0a4c0c
haeseon lee <haeseon4816@gmail.com>
2026-03-30 11:03:00 +0900

    feat: Position 행, 열 범위 검증 메서드 추가

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -4,10 +4,14 @@ import java.util.Objects;
 
 public class Position {
 
-    private final int rows; // 행 10
+    private static final int MAX_ROWS = 10;
+    private static final int MAX_COLUMNS = 9;
+
+    private final int rows; //행 10
     private final int columns; //열 9
 
     public Position(int rows, int columns) {
+        validatePosition(rows, columns);
         this.rows = rows;
         this.columns = columns;
     }
@@ -18,6 +22,12 @@ public class Position {
 
     public int getColumns() {
         return columns;
+    }
+
+    private static void validatePosition(int rows, int columns) {
+        if (rows < 0 || rows >= MAX_ROWS || columns < 0 || columns >= MAX_COLUMNS) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않은 위치입니다." + rows + ", " + columns);
+        }
     }
 
     @Override
