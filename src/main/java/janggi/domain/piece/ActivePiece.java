dc2f8d5f500f2db1e5a4ca35bdf9a74dc051e239
armd482 <armd482@naver.com>
2026-03-30 09:46:32 +0900

    refactor: 에러메세지 접근 제한자 변경

--- src/main/java/janggi/domain/piece/ActivePiece.java
+++ src/main/java/janggi/domain/piece/ActivePiece.java
@@ -4,8 +4,8 @@ import janggi.domain.Side;
 import janggi.domain.policy.RoutePolicy;
 
 public abstract class ActivePiece extends BasePiece {
-    public static final String INVALID_DESTINATION_MESSAGE = "올바른 도착 지점이 아닙니다.";
-    public static final String UNMOVABLE_ROUTE_MESSAGE = "이동할 수 없는 경로입니다.";
+    protected static final String INVALID_DESTINATION_MESSAGE = "올바른 도착 지점이 아닙니다.";
+    protected static final String UNMOVABLE_ROUTE_MESSAGE = "이동할 수 없는 경로입니다.";
 
     protected RoutePolicy routePolicy;
 
