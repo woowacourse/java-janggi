eddaf6894f424100bfc83a432c0bb549aecb4121
박병훈 <leafloveu@naver.com>
2026-03-30 11:41:15 +0900

    refactor(controller): run() 메서드 추출

--- src/main/java/controller/JanggiController.java
+++ src/main/java/controller/JanggiController.java
@@ -10,6 +10,7 @@ import dto.PieceInfosDto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.function.Supplier;
 import view.InputView;
 import view.OutputView;
 
@@ -24,63 +25,112 @@ public class JanggiController {
     }
 
     public void run() {
-        outputView.printEnterChoPlayerNamePrompt();
-        String choPlayerName = inputView.readPlayerName();
-        Player choPlayer = Player.cho(choPlayerName);
+        Player choPlayer = setupChoPlayer();
+        Player hanPlayer = setupHanPlayer();
 
+        Board board = initBoard();
+
+        outputView.printBoardWithPieces(board.getAllPieceInfos());
+
+        // TODO: 게임 종료 구현 (사이클 2)
+        while (true) {
+            processTurn(board, Team.CHO);
+            processTurn(board, Team.HAN);
+        }
+    }
+
+    private Player setupHanPlayer() {
         outputView.printEnterHanPlayerNamePrompt();
         String hanPlayerName = inputView.readPlayerName();
-        Player hanPlayer = Player.han(hanPlayerName);
+        return Player.han(hanPlayerName);
+    }
 
+    private Player setupChoPlayer() {
+        outputView.printEnterChoPlayerNamePrompt();
+        String choPlayerName = inputView.readPlayerName();
+        return Player.cho(choPlayerName);
+    }
+
+    private Board initBoard() {
+        ElephantSetup choElephantSetup = retry(this::askChoElephantSetup);
+        ElephantSetup hanElephantSetup = retry(this::askHanElephantSetup);
+
+        return Board.init(choElephantSetup, hanElephantSetup);
+    }
+
+    private ElephantSetup askChoElephantSetup() {
         outputView.printChooseChoElephantSetupPrompt();
-        ElephantSetup choElephantSetup = inputView.readElephantSetup();
+        return inputView.readElephantSetup();
+    }
 
+    private ElephantSetup askHanElephantSetup() {
         outputView.printChooseHanElephantSetupPrompt();
-        ElephantSetup hanElephantSetup = inputView.readElephantSetup();
-
-        Board board = Board.init(choElephantSetup, hanElephantSetup);
-
-        PieceInfosDto pieceInfos = board.getAllPieceInfos();
-        outputView.printBoardWithPieces(pieceInfos);
-
-        processTurn(board, Team.CHO);
-        processTurn(board, Team.HAN);
+        return inputView.readElephantSetup();
     }
 
     private void processTurn(Board board, Team team) {
-        while (true) {
-            PieceInfosDto teamPieces = board.getPieceInfosBy(team);
-            List<Entry<Position, PieceInfoDto>> entryList = new ArrayList<>(teamPieces.pieceInfos().entrySet());
-
-            outputView.printChoosePieceToMovePrompt(entryList);
-            int pieceIndex = inputView.readPieceNumber() - 1;
-
-            if (pieceIndex < 0 || pieceIndex >= entryList.size()) {
-                outputView.printInvalidNumberInput();
-                continue;
-            }
-
-            Position selectedPiecePos = entryList.get(pieceIndex).getKey();
-            List<Position> movablePositions = board.getMovablePositions(selectedPiecePos);
-
-            if (movablePositions.isEmpty()) {
-                outputView.printNoMovablePositionMessage();
-                continue;
-            }
-
-            outputView.printChoosePositionToMovePrompt(movablePositions);
-            int positionIndex = inputView.readPositionNumber() - 1;
-
-            if (positionIndex < 0 || positionIndex >= movablePositions.size()) {
-                outputView.printInvalidNumberInput();
-                continue;
-            }
-
-            Position targetPos = movablePositions.get(positionIndex);
-
-            board.move(selectedPiecePos, targetPos, team);
+        retry(() -> {
+            movePiece(board, team);
             outputView.printBoardWithPieces(board.getAllPieceInfos());
-            break;
+        });
+    }
+
+    private void movePiece(Board board, Team team) {
+        Position from = selectPieceToMove(board, team).getKey();
+        Position to = selectPositionToMove(board, from);
+
+        board.move(from, to, team);
+    }
+
+    private Entry<Position, PieceInfoDto> selectPieceToMove(Board board, Team team) {
+        PieceInfosDto teamPieces = board.getPieceInfosBy(team);
+        List<Entry<Position, PieceInfoDto>> entryList = new ArrayList<>(teamPieces.pieceInfos().entrySet());
+
+        outputView.printChoosePieceToMovePrompt(entryList);
+        int pieceIndex = inputView.readPieceNumber() - 1;
+
+        if (pieceIndex < 0 || pieceIndex >= entryList.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        return entryList.get(pieceIndex);
+    }
+
+    private Position selectPositionToMove(Board board, Position from) {
+        List<Position> movablePositions = board.getMovablePositions(from);
+
+        if (movablePositions.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        outputView.printChoosePositionToMovePrompt(movablePositions);
+        int positionIndex = inputView.readPositionNumber() - 1;
+
+        if (positionIndex < 0 || positionIndex >= movablePositions.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        return movablePositions.get(positionIndex);
+    }
+
+    private void retry(Runnable callback) {
+        while (true) {
+            try {
+                callback.run();
+                return;
+            } catch (IllegalArgumentException exception) {
+                outputView.printInvalidNumberInput();
+            }
+        }
+    }
+
+    private <T> T retry(Supplier<T> callback) {
+        while (true) {
+            try {
+                return callback.get();
+            } catch (IllegalArgumentException exception) {
+                outputView.printInvalidNumberInput();
+            }
         }
     }
 }
