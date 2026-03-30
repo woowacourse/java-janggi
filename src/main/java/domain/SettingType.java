31d7ab6ace5a32e4c99bd8c7d54929469decfbe2
kdongsu5509 <kod66170@gmail.com>
2026-03-30 14:06:24 +0900

    fix: reduce duplication logic

--- src/main/java/domain/SettingType.java
+++ src/main/java/domain/SettingType.java
@@ -7,39 +7,59 @@ public enum SettingType {
     LEFT {
         @Override
         public SettingInfo generate(Team team) {
-            if (team == Team.CHO) {
-                return new SettingInfo(Position.of(1, 3), Position.of(1, 8), Position.of(1, 2), Position.of(1, 7));
-            }
-            return new SettingInfo(Position.of(10, 2), Position.of(10, 7), Position.of(10, 3), Position.of(10, 8));
+            int row = selectProperRow(team);
+            return new SettingInfo(
+                    Position.of(row, 2),
+                    Position.of(row, 7),
+                    Position.of(row, 3),
+                    Position.of(row, 8)
+            );
         }
     },
     RIGHT {
         @Override
         public SettingInfo generate(Team team) {
-            if (team == Team.CHO) {
-                return new SettingInfo(Position.of(1, 2), Position.of(1, 7), Position.of(1, 3), Position.of(1, 8));
-            }
-            return new SettingInfo(Position.of(10, 3), Position.of(10, 8), Position.of(10, 2), Position.of(10, 7));
+            int row = selectProperRow(team);
+            return new SettingInfo(
+                    Position.of(row, 3),
+                    Position.of(row, 8),
+                    Position.of(row, 2),
+                    Position.of(row, 7)
+            );
         }
     },
     INNER {
         @Override
         public SettingInfo generate(Team team) {
-            if (team == Team.CHO) {
-                return new SettingInfo(Position.of(1, 3), Position.of(1, 7), Position.of(1, 2), Position.of(1, 8));
-            }
-            return new SettingInfo(Position.of(10, 3), Position.of(10, 7), Position.of(10, 2), Position.of(10, 8));
+            int row = selectProperRow(team);
+            return new SettingInfo(
+                    Position.of(row, 3),
+                    Position.of(row, 7),
+                    Position.of(row, 2),
+                    Position.of(row, 8)
+            );
         }
     },
     OUTER {
         @Override
         public SettingInfo generate(Team team) {
-            if (team == Team.CHO) {
-                return new SettingInfo(Position.of(1, 2), Position.of(1, 8), Position.of(1, 3), Position.of(1, 7));
-            }
-            return new SettingInfo(Position.of(10, 2), Position.of(10, 8), Position.of(10, 3), Position.of(10, 7));
+            int row = selectProperRow(team);
+            return new SettingInfo(
+                    Position.of(row, 2),
+                    Position.of(row, 8),
+                    Position.of(row, 3),
+                    Position.of(row, 7)
+            );
         }
     };
+
+    protected int selectProperRow(Team team) {
+        if (team == Team.CHO) {
+            return 1;
+        }
+
+        return 10;
+    }
 
     public abstract SettingInfo generate(Team team);
 }
