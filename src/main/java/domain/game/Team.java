f43b409701645084d634fe44ce92244cb87944f6
김가현 <hyeonnni@naver.com>
2026-03-30 20:43:01 +0900

    refactor(domain): Team enum 구조 개선

--- src/main/java/domain/game/Team.java
+++ src/main/java/domain/game/Team.java
@@ -1,37 +1,13 @@
 package domain.game;
 
 public enum Team {
-    CHO("초", 1, 2, 3, 4),
-    HAN("한", 10, 9, 8, 7);
+    CHO("초"),
+    HAN("한");
 
     private final String teamName;
-    private final int backRow;
-    private final int generalRow;
-    private final int cannonRow;
-    private final int soldierRow;
 
-    Team(String teamName, int backRow, int generalRow, int cannonRow, int soldierRow) {
+    Team(String teamName) {
         this.teamName = teamName;
-        this.backRow = backRow;
-        this.generalRow = generalRow;
-        this.cannonRow = cannonRow;
-        this.soldierRow = soldierRow;
-    }
-
-    public int getBackRow() {
-        return backRow;
-    }
-
-    public int getGeneralRow() {
-        return generalRow;
-    }
-
-    public int getCannonRow() {
-        return cannonRow;
-    }
-
-    public int getSoldierRow() {
-        return soldierRow;
     }
 
     public int forwardRowDirection() {
