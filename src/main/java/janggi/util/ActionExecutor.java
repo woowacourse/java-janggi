a9a79a5bda681ad13e3e12820ae005a8707cecbf
Sumin <new.sumyang@gmail.com>
2026-03-30 20:54:49 +0900

    refactor: ActionExecutor depth 규약 적용

--- src/main/java/janggi/util/ActionExecutor.java
+++ src/main/java/janggi/util/ActionExecutor.java
@@ -6,12 +6,11 @@ import java.util.function.Supplier;
 public class ActionExecutor {
 
     public static <T> T retryUntilSuccess(Supplier<T> supplier) {
-        while (true) {
-            try {
-                return supplier.get();
-            } catch (IllegalArgumentException e) {
-                OutputView.printMessage(e.getMessage());
-            }
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return retryUntilSuccess(supplier);
         }
     }
 }
