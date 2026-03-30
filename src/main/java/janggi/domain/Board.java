27ffdfc07fdf73e60ef81a3c2881d7a10f0a668a
haechanmoon <moonhc.dev@gmail.com>
2026-03-30 12:54:49 +0900

    fix(board): 생성자에서 보드초기화 제거

--- src/main/java/janggi/domain/Board.java
+++ src/main/java/janggi/domain/Board.java
@@ -7,58 +7,50 @@ import java.util.Map;
 
 public class Board {
 
-    private final Map<Position, Piece> board;
+    private final Map<Position, Piece> board = new HashMap<>();
 
-    public Board() {
-        this.board = initialize();
-    }
+    public void initialize() {
+        board.put(new Position(1, 7), new Piece(Team.CHO, PieceType.ZOL));
+        board.put(new Position(3, 7), new Piece(Team.CHO, PieceType.ZOL));
+        board.put(new Position(5, 7), new Piece(Team.CHO, PieceType.ZOL));
+        board.put(new Position(7, 7), new Piece(Team.CHO, PieceType.ZOL));
+        board.put(new Position(9, 7), new Piece(Team.CHO, PieceType.ZOL));
 
-    public Map<Position, Piece> initialize() {
-        Map<Position, Piece> initBoard = new HashMap<>();
+        board.put(new Position(2, 8), new Piece(Team.CHO, PieceType.PO));
+        board.put(new Position(8, 8), new Piece(Team.CHO, PieceType.PO));
 
-        initBoard.put(new Position(1, 7), new Piece(Team.CHO, PieceType.ZOL));
-        initBoard.put(new Position(3, 7), new Piece(Team.CHO, PieceType.ZOL));
-        initBoard.put(new Position(5, 7), new Piece(Team.CHO, PieceType.ZOL));
-        initBoard.put(new Position(7, 7), new Piece(Team.CHO, PieceType.ZOL));
-        initBoard.put(new Position(9, 7), new Piece(Team.CHO, PieceType.ZOL));
+        board.put(new Position(1, 10), new Piece(Team.CHO, PieceType.CHA));
+        board.put(new Position(9, 10), new Piece(Team.CHO, PieceType.CHA));
 
-        initBoard.put(new Position(2, 8), new Piece(Team.CHO, PieceType.PO));
-        initBoard.put(new Position(8, 8), new Piece(Team.CHO, PieceType.PO));
+        board.put(new Position(2, 10), new Piece(Team.CHO, PieceType.MA));
+        board.put(new Position(3, 10), new Piece(Team.CHO, PieceType.SANG));
+        board.put(new Position(7, 10), new Piece(Team.CHO, PieceType.MA));
+        board.put(new Position(8, 10), new Piece(Team.CHO, PieceType.SANG));
 
-        initBoard.put(new Position(1, 10), new Piece(Team.CHO, PieceType.CHA));
-        initBoard.put(new Position(9, 10), new Piece(Team.CHO, PieceType.CHA));
+        board.put(new Position(4, 10), new Piece(Team.CHO, PieceType.SA));
+        board.put(new Position(6, 10), new Piece(Team.CHO, PieceType.SA));
+        board.put(new Position(5, 9), new Piece(Team.CHO, PieceType.KING));
 
-        initBoard.put(new Position(2, 10), new Piece(Team.CHO, PieceType.MA));
-        initBoard.put(new Position(3, 10), new Piece(Team.CHO, PieceType.SANG));
-        initBoard.put(new Position(7, 10), new Piece(Team.CHO, PieceType.MA));
-        initBoard.put(new Position(8, 10), new Piece(Team.CHO, PieceType.SANG));
+        board.put(new Position(1, 4), new Piece(Team.HAN, PieceType.ZOL));
+        board.put(new Position(3, 4), new Piece(Team.HAN, PieceType.ZOL));
+        board.put(new Position(5, 4), new Piece(Team.HAN, PieceType.ZOL));
+        board.put(new Position(7, 4), new Piece(Team.HAN, PieceType.ZOL));
+        board.put(new Position(9, 4), new Piece(Team.HAN, PieceType.ZOL));
 
-        initBoard.put(new Position(4, 10), new Piece(Team.CHO, PieceType.SA));
-        initBoard.put(new Position(6, 10), new Piece(Team.CHO, PieceType.SA));
-        initBoard.put(new Position(5, 9), new Piece(Team.CHO, PieceType.KING));
+        board.put(new Position(2, 3), new Piece(Team.HAN, PieceType.PO));
+        board.put(new Position(8, 3), new Piece(Team.HAN, PieceType.PO));
 
-        initBoard.put(new Position(1, 4), new Piece(Team.HAN, PieceType.ZOL));
-        initBoard.put(new Position(3, 4), new Piece(Team.HAN, PieceType.ZOL));
-        initBoard.put(new Position(5, 4), new Piece(Team.HAN, PieceType.ZOL));
-        initBoard.put(new Position(7, 4), new Piece(Team.HAN, PieceType.ZOL));
-        initBoard.put(new Position(9, 4), new Piece(Team.HAN, PieceType.ZOL));
+        board.put(new Position(1, 1), new Piece(Team.HAN, PieceType.CHA));
+        board.put(new Position(9, 1), new Piece(Team.HAN, PieceType.CHA));
 
-        initBoard.put(new Position(2, 3), new Piece(Team.HAN, PieceType.PO));
-        initBoard.put(new Position(8, 3), new Piece(Team.HAN, PieceType.PO));
+        board.put(new Position(2, 1), new Piece(Team.HAN, PieceType.MA));
+        board.put(new Position(3, 1), new Piece(Team.HAN, PieceType.SANG));
+        board.put(new Position(7, 1), new Piece(Team.HAN, PieceType.MA));
+        board.put(new Position(8, 1), new Piece(Team.HAN, PieceType.SANG));
 
-        initBoard.put(new Position(1, 1), new Piece(Team.HAN, PieceType.CHA));
-        initBoard.put(new Position(9, 1), new Piece(Team.HAN, PieceType.CHA));
-
-        initBoard.put(new Position(2, 1), new Piece(Team.HAN, PieceType.MA));
-        initBoard.put(new Position(3, 1), new Piece(Team.HAN, PieceType.SANG));
-        initBoard.put(new Position(7, 1), new Piece(Team.HAN, PieceType.MA));
-        initBoard.put(new Position(8, 1), new Piece(Team.HAN, PieceType.SANG));
-
-        initBoard.put(new Position(4, 1), new Piece(Team.HAN, PieceType.SA));
-        initBoard.put(new Position(6, 1), new Piece(Team.HAN, PieceType.SA));
-        initBoard.put(new Position(5, 2), new Piece(Team.HAN, PieceType.KING));
-
-        return initBoard;
+        board.put(new Position(4, 1), new Piece(Team.HAN, PieceType.SA));
+        board.put(new Position(6, 1), new Piece(Team.HAN, PieceType.SA));
+        board.put(new Position(5, 2), new Piece(Team.HAN, PieceType.KING));
     }
 
     public Map<Position, Piece> getBoard() {
