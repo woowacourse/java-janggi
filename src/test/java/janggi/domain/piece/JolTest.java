31678c37a28790732273741134026c2f948959f0
Sumin <new.sumyang@gmail.com>
2026-03-30 10:58:30 +0900

    test: Jol 테스트 추가

---
+++ src/test/java/janggi/domain/piece/JolTest.java
@@ -0,0 +1,86 @@
+package janggi.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import janggi.domain.side.TeamType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JolTest {
+
+    @Test
+    @DisplayName("초나라 졸은 앞으로 한 칸 이동할 수 있다.")
+    void chuIsValidMovePatternForward() {
+        // given
+        Jol jol = new Jol(TeamType.CHU);
+
+        // when & then
+        assertThat(jol.isValidMovePattern(4, 4, 4, 5)).isTrue();
+    }
+
+    @Test
+    @DisplayName("한나라 졸은 앞으로 한 칸 이동할 수 있다.")
+    void hanIsValidMovePatternForward() {
+        // given
+        Jol jol = new Jol(TeamType.HAN);
+
+        // when & then
+        assertThat(jol.isValidMovePattern(4, 4, 4, 3)).isTrue();
+    }
+
+    @Test
+    @DisplayName("졸은 좌우로 한 칸 이동할 수 있다.")
+    void isValidMovePatternHorizontally() {
+        // given
+        Jol jolOfChu = new Jol(TeamType.CHU);
+        Jol jolOfHan = new Jol(TeamType.HAN);
+
+        // when & then
+        assertAll(
+            () -> assertThat(jolOfChu.isValidMovePattern(4, 4, 5, 4)).isTrue(),
+            () -> assertThat(jolOfChu.isValidMovePattern(4, 4, 3, 4)).isTrue(),
+            () -> assertThat(jolOfHan.isValidMovePattern(4, 4, 5, 4)).isTrue(),
+            () -> assertThat(jolOfHan.isValidMovePattern(4, 4, 3, 4)).isTrue()
+        );
+    }
+
+    @Test
+    @DisplayName("졸은 뒤로 이동할 수 없다.")
+    void cannotMoveBackward() {
+        // given
+        Jol jolOfChu = new Jol(TeamType.CHU);
+        Jol hanJol = new Jol(TeamType.HAN);
+
+        // when & then
+        assertAll(
+            () -> assertThat(jolOfChu.isValidMovePattern(4, 4, 4, 3)).isFalse(),
+            () -> assertThat(hanJol.isValidMovePattern(4, 4, 4, 5)).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("졸은 두 칸 이상 이동하거나 대각선으로 이동할 수 없다.")
+    void cannotMoveInvalidPath() {
+        // given
+        Jol jol = new Jol(TeamType.CHU);
+
+        // when & then
+        assertAll(
+            () -> assertThat(jol.isValidMovePattern(4, 4, 4, 6)).isFalse(),
+            () -> assertThat(jol.isValidMovePattern(4, 4, 2, 4)).isFalse(),
+            () -> assertThat(jol.isValidMovePattern(4, 4, 5, 5)).isFalse(),
+            () -> assertThat(jol.isValidMovePattern(4, 4, 3, 3)).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("졸은 제자리로 이동할 수 없다.")
+    void cannotMoveSamePosition() {
+        // given
+        Jol jol = new Jol(TeamType.CHU);
+
+        // when & then
+        assertThat(jol.isValidMovePattern(4, 4, 4, 4)).isFalse();
+    }
+}
