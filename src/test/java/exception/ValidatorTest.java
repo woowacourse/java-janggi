ba9333db44066cd8fba7caf94dc3191b149a0d87
minzzun99 <skg06123@naver.com>
2026-03-30 17:11:16 +0900

    test: Validator 빈값 입력 테스트 추가

--- src/test/java/exception/ValidatorTest.java
+++ src/test/java/exception/ValidatorTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidatorTest {
@@ -12,13 +13,23 @@ public class ValidatorTest {
     @ParameterizedTest
     @ValueSource(strings = {
             "kang",
-            " ",
             "\t",
+            " ",
+            "-2200000000",
             "2200000000"
     })
     void 정수_변환_예외_테스트(String input) {
         assertThatThrownBy(() -> Validator.validateNumber(input))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자가 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 정수_변환_빈값_예외_테스트(String input) {
+        assertThatThrownBy(() -> Validator.validateNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자가 아닙니다.");
     }
 
     @ParameterizedTest
@@ -40,7 +51,8 @@ public class ValidatorTest {
     })
     void 정수_범위_검증_예외_테스트(int min, int max, int input) {
         assertThatThrownBy(() -> Validator.validateNumberInRange(min, max, input))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("범위 내의 숫자가 아닙니다.");
     }
 
     @ParameterizedTest
