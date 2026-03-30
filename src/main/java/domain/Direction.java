50be0bd74aad9a8f12d1978e44ee55d54ad748fa
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-30 17:01:40 +0900

    refactor: 불필요한 if문 제거

--- src/main/java/domain/Direction.java
+++ src/main/java/domain/Direction.java
@@ -28,12 +28,9 @@ public enum Direction implements Comparator<Direction> {
     public static List<Direction> findDirections(int x, int y) {
         List<Direction> directions = new ArrayList<>();
         while (!(x == 0 && y == 0)) {
-            Direction direction = null;
+            Direction direction = findDiagonal(x, y);
             if (x == 0 || y == 0) {
                 direction = findStraight(x, y);
-            }
-            if (x != 0 && y != 0) {
-                direction = findDiagonal(x, y);
             }
             directions.add(direction);
             x += -direction.x;
