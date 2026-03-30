1d3c1bf632a9b1dc8b52066e3127db9e58fdae4b
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-30 15:54:46 +0900

    test(Cha): 궁성 안에서 움직임 테스트 작성

--- src/test/java/janggi/domain/piece/ChaTest.java
+++ src/test/java/janggi/domain/piece/ChaTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import janggi.domain.point.Point;
 import janggi.domain.point.Points;
-import janggi.domain.point.Route;
 import janggi.domain.status.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,5 +40,17 @@ public class ChaTest {
         // when & then
         assertThatThrownBy(() -> cha.getRoutePoints(from, to))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("궁성 안에서 이동 할 경우 대각선으로 이동 할 수 있다.")
+    @CsvSource(value = {"3:0:5:2:4:1","3:7:5:9:4:8","3:9:5:7:4:8"}, delimiter = ':')
+    void can_move_diagonal(int fromColumn, int fromRow, int toColumn, int toRow, int routeColumn, int routeRow) {
+        Piece cha = new Cha(Team.CHO);
+        Point from = Point.of(fromColumn, fromRow);
+        Point to = Point.of(toColumn, toRow);
+        Points routePoints = cha.getRoutePoints(from, to);
+        assertThat(routePoints.getPoints().getFirst().getColumn()).isEqualTo(routeColumn);
+        assertThat(routePoints.getPoints().getFirst().getRow()).isEqualTo(routeRow);
     }
 }
