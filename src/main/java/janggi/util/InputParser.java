c047c5825f0bda2284f66a17d15c431fd7ee7910
haeyoon1 <hynam1220@naver.com>
2026-03-31 00:38:38 +0900

    feat: 입력 문자열 파싱 메서드 생성

---
+++ src/main/java/janggi/util/InputParser.java
@@ -0,0 +1,33 @@
+package janggi.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputParser {
+
+    private static final String DELIMITER = ",";
+
+    public static List<String> splitByDelimiter(String input) {
+        validateInput(input);
+        List<String> inputs = Arrays.stream(input.split(DELIMITER))
+            .map(String::trim)
+            .filter(name -> !name.isBlank())
+            .toList();
+
+        validateInputs(inputs);
+        return inputs;
+    }
+
+    private static void validateInput(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("좌표값을 입력해주세요.");
+        }
+    }
+
+    private static void validateInputs(List<String> inputs) {
+        if (inputs.isEmpty()) {
+            throw new IllegalArgumentException("좌표는 x,y 형태로 입력해야합니다.");
+        }
+    }
+
+}
