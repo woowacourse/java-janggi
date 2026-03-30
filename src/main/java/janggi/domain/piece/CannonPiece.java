6e91c1cfa3b654431547a4b4e0d212b45308dff9
sinequanonn <dleck288@gmail.com>
2026-03-30 20:16:03 +0900

    remove: 추상 클래스 구현체 제거

--- src/main/java/janggi/domain/piece/CannonPiece.java
+++
@@ -1,9 +0,0 @@
-package janggi.domain.piece;
-
-import janggi.domain.movestrategy.MoveStrategy;
-
-public class CannonPiece extends Piece {
-    public CannonPiece(Team team, MoveStrategy moveStrategy) {
-        super(team, Name.CANNON, moveStrategy);
-    }
-}
--- src/main/java/janggi/domain/piece/SoliderPiece.java
+++
@@ -1,9 +0,0 @@
-package janggi.domain.piece;
-
-import janggi.domain.movestrategy.MoveStrategy;
-
-public class SoliderPiece extends Piece {
-    public SoliderPiece(Team team, MoveStrategy moveStrategy) {
-        super(team, Name.SOLDIER, moveStrategy);
-    }
-}
