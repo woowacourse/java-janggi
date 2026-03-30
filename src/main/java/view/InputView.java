7aaadb1ffcea553a486fe9c946eba31a4461a510
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-03-30 19:54:32 +0900

    refactor: 입력 메시지 구체화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,12 +6,12 @@ public class InputView {
     private final Scanner sc = new Scanner(System.in);
 
     public String inputMovePiece() {
-        System.out.println("어떤 기물을 옮기시겠습니까?");
+        System.out.println("어떤 기물을 옮기시겠습니까?(예시 : 0,0)");
         return sc.nextLine();
     }
 
     public String inputTargetPosition() {
-        System.out.println("어디로 옮기시겠습니까?");
+        System.out.println("어디로 옮기시겠습니까?(예시 : 1,0)");
         return sc.nextLine();
     }
 }
