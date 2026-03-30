bb17ae018213375873b0faa72adf541761b8240a
choiyoung69 <choiyoung609@gmail.com>
2026-03-30 10:47:29 +0900

    refactor: 사용자에게 에러 메세지 노출 방지를 위한 log 출력

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,15 @@
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        try{
+        try {
             Janggi janggi = new Janggi();
             janggi.run();
-        }catch (Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            logger.error("에러 발생", e);
         }
     }
 }
