ebcae3b1302a70ce9b26d08089317e96e2f9d7a9
tjdakf <ksc73450056@gmail.com>
2026-03-30 13:18:57 +0900

    docs: 미션 중 기록 작성 추가

--- src/main/java/janggi/domain/piece/Piece.java
+++ src/main/java/janggi/domain/piece/Piece.java
@@ -5,11 +5,18 @@ import janggi.domain.position.Position;
 import java.util.List;
 
 public interface Piece {
+
     boolean isEmptyPiece();
+
     boolean isSamePiece(Piece other);
+
     boolean isSameTeam(Piece piece);
+
     boolean isSame(Team team);
+
     String getDisplayName();
+
     List<Position> getPath(Position from, Position to);
+
     boolean canMove(List<Piece> piecesOnPath, Piece endPiece);
 }
