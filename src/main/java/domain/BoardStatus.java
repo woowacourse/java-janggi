9d3645d9220f4d7e5ce48dda34905511544eff26
kdongsu5509 <kod66170@gmail.com>
2026-03-30 13:59:25 +0900

    fix : change type of BoardStatus

--- src/main/java/domain/BoardStatus.java
+++ src/main/java/domain/BoardStatus.java
@@ -4,14 +4,9 @@ import domain.piece.Piece;
 import domain.position.Position;
 import java.util.Map;
 
-public class BoardStatus {
-    private final Map<Position, Piece> boardStatus;
-
-
-    private BoardStatus(Map<Position, Piece> boardStatus) {
-        this.boardStatus = boardStatus;
-    }
-
+public record BoardStatus(
+        Map<Position, Piece> boardStatus
+) {
     public static BoardStatus from(Map<Position, Piece> boardStatus) {
         return new BoardStatus(boardStatus);
     }
