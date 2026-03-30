92d1e8a8cec700de03740bee6684e5b3ef4eb18a
Minjae Kang <rkdalswoals@gmail.com>
2026-03-30 16:01:47 +0900

    test(Board): 테스트 추가 및 코드 수정

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -1,33 +1,31 @@
 package domain;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Board {
     private final Map<Position, Piece> board;
-
     public Board(Map<Position, Piece> board) {
         this.board = board;
     }
 
+    public Map<Position, Piece> getBoard() {
+        return board;
+    }
+
     public List<Position> getPossibleDestinations(Position position) {
+        if (!board.containsKey(position)) {
+            throw new IllegalArgumentException("기물이 존재하지 않는 위치입니다.");
+        }
         Piece piece = board.get(position);
+        // TODO: 상대 기물인 경우 에외발생 - 현재 누구 차례인지 정보 필요
         List<Position> positions = piece.getAllPosition(position);
-        Map<Position, Piece> map = makePiecesFromPositions(positions);
+        Map<Position, Piece> map = func(positions);
         return piece.getPossibleDestinations(position, map);
     }
 
-    private Map<Position, Piece> makePiecesFromPositions(List<Position> positions) {
-        Map<Position, Piece> pieces = new HashMap<>();
-
-        for (Position position : positions) {
-            if (board.containsKey(position)) {
-                pieces.put(position, board.get(position));
-            }
-        }
-
-        return pieces;
+    private Map<Position, Piece> func(List<Position> positions) {
+        return Map.of();
     }
 
     public void movePiece(Position from, Position to) {
@@ -38,9 +36,5 @@ public class Board {
         return board.values().stream()
                 .filter(piece -> piece instanceof General)
                 .count() < 2;
-    }
-
-    public Map<Position, Piece> getBoard() {
-        return board;
     }
 }
---
+++ src/test/java/domain/BoardTest.java
@@ -0,0 +1,69 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+    @Test
+    void 장기판은_올바른_위치에_기물을_초기화한다() {
+        Board board = new InitialBoardFactory().create(Formation.LEFT_ELEPHANT, Formation.RIGHT_ELEPHANT);
+        Map<Position, Piece> actual = board.getBoard();
+
+        assertThat(actual).hasSize(32);
+        assertThat(actual.get(new Position(4, 1))).isInstanceOf(General.class);
+        assertThat(actual.get(new Position(4, 8))).isInstanceOf(General.class);
+
+        assertThat(actual.get(new Position(0, 0))).isInstanceOf(Chariot.class);
+        assertThat(actual.get(new Position(8, 0))).isInstanceOf(Chariot.class);
+        assertThat(actual.get(new Position(0, 9))).isInstanceOf(Chariot.class);
+        assertThat(actual.get(new Position(8, 9))).isInstanceOf(Chariot.class);
+
+        assertThat(actual.get(new Position(1, 0))).isInstanceOf(domain.Elephant.class);
+        assertThat(actual.get(new Position(2, 0))).isInstanceOf(domain.Horse.class);
+        assertThat(actual.get(new Position(6, 0))).isInstanceOf(domain.Elephant.class);
+        assertThat(actual.get(new Position(7, 0))).isInstanceOf(domain.Horse.class);
+
+        assertThat(actual.get(new Position(7, 9))).isInstanceOf(domain.Horse.class);
+        assertThat(actual.get(new Position(6, 9))).isInstanceOf(domain.Elephant.class);
+        assertThat(actual.get(new Position(2, 9))).isInstanceOf(domain.Horse.class);
+        assertThat(actual.get(new Position(1, 9))).isInstanceOf(domain.Elephant.class);
+    }
+
+    @Test
+    void 기물을_이동하면_출발_위치는_비고_도착_위치로_옮겨진다() {
+        Map<Position, Piece> pieces = new HashMap<>();
+        Position from = new Position(0, 3);
+        Position to = new Position(0, 4);
+        Piece soldier = new Soldier(Side.CHO);
+        pieces.put(from, soldier);
+        Board board = new Board(pieces);
+
+        board.movePiece(from, to);
+
+        assertThat(board.getBoard()).doesNotContainKey(from);
+        assertThat(board.getBoard().get(to)).isSameAs(soldier);
+    }
+
+    @Test
+    void 양쪽_장군이_모두_있으면_게임은_종료되지_않는다() {
+        Map<Position, Piece> pieces = new HashMap<>();
+        pieces.put(new Position(4, 1), new General(Side.CHO));
+        pieces.put(new Position(4, 8), new General(Side.HAN));
+        Board board = new Board(pieces);
+
+        assertThat(board.isGameOver()).isFalse();
+    }
+
+    @Test
+    void 장군이_하나만_남으면_게임이_종료된다() {
+        Map<Position, Piece> pieces = new HashMap<>();
+        pieces.put(new Position(4, 1), new General(Side.CHO));
+        Board board = new Board(pieces);
+
+        assertThat(board.isGameOver()).isTrue();
+    }
+}
