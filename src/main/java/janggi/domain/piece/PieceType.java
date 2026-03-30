eba6c93ed312e394ce28d4bb2b951ffacf876305
nn98 <jkllhgb@gmail.com>
2026-03-30 15:57:44 +0900

    refactor(pieceType): feedback03.2 - 메서드 참조를 활용한 졸/병 통합

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -2,6 +2,7 @@ package janggi.domain.piece;
 
 import janggi.domain.board.Direction;
 import janggi.domain.board.Position;
+import janggi.domain.game.Side;
 import janggi.domain.route.Paths;
 import janggi.domain.strategy.CannonMoveStrategy;
 import janggi.domain.strategy.ElephantMoveStrategy;
@@ -12,32 +13,32 @@ import janggi.domain.strategy.StepMoveStrategy;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public enum PieceType {
 
-//    사이클 2 에서 궁성 구현 시
+    //    사이클 2 에서 궁성 구현 시
 //    PALACE(EnumSet.allOf(Direction.class), new StepMoveStrategy()),
 //    GUARD(EnumSet.allOf(Direction.class), new StepMoveStrategy()),
-    PALACE(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new StepMoveStrategy()),
-    GUARD(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new StepMoveStrategy()),
-    CHARIOT(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new SlideMoveStrategy()),
-    CANNON(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new CannonMoveStrategy()),
-    HORSE(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new HorseMoveStrategy()),
-    ELEPHANT(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new ElephantMoveStrategy()),
-    CHO_SOLDIER(EnumSet.of(Direction.N, Direction.E, Direction.W), new StepMoveStrategy()),
-    HAN_SOLDIER(EnumSet.of(Direction.S, Direction.E, Direction.W), new StepMoveStrategy()),
+    PALACE(side -> Direction.cardinalDirections(), new StepMoveStrategy()),
+    GUARD(side -> Direction.cardinalDirections(), new StepMoveStrategy()),
+    CHARIOT(side -> Direction.cardinalDirections(), new SlideMoveStrategy()),
+    CANNON(side -> Direction.cardinalDirections(), new CannonMoveStrategy()),
+    HORSE(side -> Direction.cardinalDirections(), new HorseMoveStrategy()),
+    ELEPHANT(side -> Direction.cardinalDirections(), new ElephantMoveStrategy()),
+    SOLDIER(side -> EnumSet.of(side.forwardDirection(), Direction.E, Direction.W), new StepMoveStrategy()),
     ;
 
-    private final EnumSet<Direction> directions;
+    private final Function<Side, EnumSet<Direction>> directionProvider;
     private final MoveStrategy moveStrategy;
 
-    PieceType(EnumSet<Direction> directions, MoveStrategy moveStrategy) {
-        this.directions = directions;
+    PieceType(Function<Side, EnumSet<Direction>> directionProvider, MoveStrategy moveStrategy) {
+        this.directionProvider = directionProvider;
         this.moveStrategy = moveStrategy;
     }
 
-    public Paths calculatePaths(Position current) {
-        return moveStrategy.findMovablePaths(current, directions);
+    public Paths calculatePaths(Position current, Side side) {
+        return moveStrategy.findMovablePaths(current, directionProvider.apply(side));
     }
 
     public List<Position> determineDestinations(Paths paths, Map<Position, Piece> boardState, Piece movingPiece) {
