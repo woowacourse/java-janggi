eeeb79ab96be49490d3d385bbd91a2de671be8d7
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-30 16:44:04 +0900

    feat: 상차림 입력값이 왼상차림/오른상차림/안상차림/바깥상차림도 가능하도록 추가

--- src/main/java/domain/TableSetting.java
+++ src/main/java/domain/TableSetting.java
@@ -6,25 +6,27 @@ import java.util.Collections;
 import java.util.List;
 
 public enum TableSetting {
-    LEFT_TABLE(List.of(PieceType.ELEPHANT, PieceType.HORSE, PieceType.ELEPHANT, PieceType.HORSE), "상마상마"),
-    RIGHT_TABLE(List.of(PieceType.HORSE, PieceType.ELEPHANT, PieceType.HORSE, PieceType.ELEPHANT), "마상마상"),
-    INSIDE_TABLE(List.of(PieceType.HORSE, PieceType.ELEPHANT, PieceType.ELEPHANT, PieceType.HORSE), "마상상마"),
-    OUTSIDE_TABLE(List.of(PieceType.ELEPHANT, PieceType.HORSE, PieceType.HORSE, PieceType.ELEPHANT), "상마마상"),
+    LEFT_TABLE(List.of(PieceType.ELEPHANT, PieceType.HORSE, PieceType.ELEPHANT, PieceType.HORSE), "상마상마", "왼상차림"),
+    RIGHT_TABLE(List.of(PieceType.HORSE, PieceType.ELEPHANT, PieceType.HORSE, PieceType.ELEPHANT), "마상마상", "오른상차림"),
+    INSIDE_TABLE(List.of(PieceType.HORSE, PieceType.ELEPHANT, PieceType.ELEPHANT, PieceType.HORSE), "마상상마", "안상차림"),
+    OUTSIDE_TABLE(List.of(PieceType.ELEPHANT, PieceType.HORSE, PieceType.HORSE, PieceType.ELEPHANT), "상마마상", "바깥상차림"),
     ;
 
     private static final String NOT_FOUND_TABLE_SETTING = "[ERROR] 존재하지 않는 상차림입니다.";
 
     private final List<PieceType> formation;
-    private final String name;
+    private final String formationName;
+    private final String displayName;
 
-    TableSetting(List<PieceType> formation, String name) {
+    TableSetting(List<PieceType> formation, String formationName, String displayName) {
         this.formation = formation;
-        this.name = name;
+        this.formationName = formationName;
+        this.displayName = displayName;
     }
 
     public static TableSetting from(String name) {
         for (TableSetting tableSetting : TableSetting.values()) {
-            if (tableSetting.name.equals(name)) {
+            if (tableSetting.formationName.equals(name) || tableSetting.displayName.equals(name)) {
                 return tableSetting;
             }
         }
--- src/test/java/domain/TableSettingTest.java
+++ src/test/java/domain/TableSettingTest.java
@@ -1,15 +1,25 @@
 package domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TableSettingTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"상마상마", "안상차림"})
+    @DisplayName("상차림을 정상적으로 입력받는다.")
+    void tableSettingTest(String input) {
+        assertDoesNotThrow(() -> TableSetting.from(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"상상상상", "안바깥상차림"})
     @DisplayName("해당하지 않는 상차림을 입력한 경우, 예외가 발생한다.")
-    void tableSettingFormatExceptionTest() {
-        assertThatThrownBy(() -> TableSetting.from("상상상상"))
+    void tableSettingFormatExceptionTest(String input) {
+        assertThatThrownBy(() -> TableSetting.from(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 존재하지 않는 상차림입니다.");
     }
