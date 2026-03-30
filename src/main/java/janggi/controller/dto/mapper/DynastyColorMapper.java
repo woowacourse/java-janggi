dbf5593b0c0e24558c906826a56910fd91c42052
jhk01007 <jhk001007@naver.com>
2026-03-30 14:09:06 +0900

    refactor: ANSI 값 상수 처리

--- src/main/java/janggi/controller/dto/mapper/DynastyColorMapper.java
+++ src/main/java/janggi/controller/dto/mapper/DynastyColorMapper.java
@@ -1,5 +1,6 @@
 package janggi.controller.dto.mapper;
 
+import janggi.controller.dto.DynastyDto;
 import janggi.domain.dynasty.Dynasty;
 import java.util.EnumMap;
 import java.util.Map;
@@ -7,10 +8,13 @@ import java.util.Map;
 public class DynastyColorMapper {
 
     private static final Map<Dynasty, String> dynastyColorMap = new EnumMap<>(Dynasty.class);
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_RESET = "\u001B[0m";
 
     static {
-        dynastyColorMap.put(Dynasty.HAN, "\u001B[31m");
-        dynastyColorMap.put(Dynasty.CHO, "\u001B[34m");
+        dynastyColorMap.put(Dynasty.HAN, ANSI_RED);
+        dynastyColorMap.put(Dynasty.CHO, ANSI_BLUE);
     }
 
     private DynastyColorMapper() {
@@ -18,6 +22,12 @@ public class DynastyColorMapper {
 
     public static String from(Dynasty dynasty) {
         return dynastyColorMap.get(dynasty);
+    }
+
+    public static String getColorInfoByDynasty(Dynasty dynasty) {
+        return dynastyColorMap.get(dynasty) +
+                DynastyDto.from(dynasty).dynastyName() +
+                " " + ANSI_RESET;
     }
 
 }
