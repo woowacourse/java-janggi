27f874dffdb63d4a78ed072ee5c05730e4f254f8
sinequanonn <dleck288@gmail.com>
2026-03-30 20:18:17 +0900

    feat: PieceType 식별하는 getIdentity 규칙 추가

--- src/main/java/janggi/domain/movestrategy/MoveStrategy.java
+++ src/main/java/janggi/domain/movestrategy/MoveStrategy.java
@@ -2,6 +2,7 @@ package janggi.domain.movestrategy;
 
 import janggi.domain.board.Position;
 import janggi.domain.piece.Piece;
+import janggi.domain.piece.PieceType;
 
 import java.util.List;
 import java.util.Map;
@@ -12,5 +13,7 @@ public interface MoveStrategy {
     List<Position> findPath(Position from, Position to);
 
     boolean determineMovingRule(Piece sourcePiece, Map<Position, Piece> positionPieces, Position to);
+
+    PieceType getIdentity();
 }
 
