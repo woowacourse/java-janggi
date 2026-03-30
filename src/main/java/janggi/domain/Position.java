d80b56c6a8d3cba8f3fd14e9f514dea36cd2a5c2
gleaming9 <gleaming9@khu.ac.kr>
2026-03-30 20:24:50 +0900

    refactor: PositionParser로 입력값을 파싱하도록 변경

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -9,34 +9,27 @@ import static janggi.domain.board.Board.BOARD_START_ROWS;
 
 public record Position(int x, int y) {
 
-    private static final String INVALID_POSITION_TYPE = "숫자만 입력 가능합니다.";
     private static final String INVALID_POSITION_SIZE = "행과 열 두 개의 값만 입력하세요.";
     private static final String INVALID_ROW_RANGE = "유효하지 않은 위치입니다. 행은 1부터 10까지 가능합니다.";
     private static final String INVALID_COL_RANGE = "유효하지 않은 위치입니다. 열은 1부터 9까지 가능합니다.";
 
-    public static Position from(List<String> inputs) {
-        try {
-            validateSize(inputs);
-            List<Integer> parsedInputs = inputs.stream().map(Integer::parseInt).toList();
-            int r = parsedInputs.getFirst();
-            int c = parsedInputs.getLast();
-
-            validate(r, c);
-            return new Position(r, c);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INVALID_POSITION_TYPE);
-        }
+    public static Position from(List<Integer> inputs) {
+        int r = inputs.getFirst();
+        int c = inputs.getLast();
+        validate(r, c, inputs);
+        return new Position(r, c);
     }
 
-    private static void validateSize(List<String> inputs) {
+    private static void validate(int x, int y, List<Integer> inputs) {
+        validateSize(inputs);
+        validateRow(x);
+        validateCol(y);
+    }
+
+    private static void validateSize(List<Integer> inputs) {
         if (inputs.size() != 2) {
             throw new IllegalArgumentException(INVALID_POSITION_SIZE);
         }
-    }
-
-    private static void validate(int x, int y) {
-        validateRow(x);
-        validateCol(y);
     }
 
     private static void validateRow(int x) {
--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -1,6 +1,5 @@
 package janggi.view;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,7 +8,6 @@ public class InputView {
     private static final String CHO_ARRANGEMENT_MESSAGE = "초 진영의 배치를 입력해주세요. (예: 마상마상, 마상상마, 상마상마, 상마마상)";
     private static final String START_POSITION_MESSAGE = "움직일 기물의 시작 좌표를 입력해주세요. (예: 3,4)";
     private static final String END_POSITION_MESSAGE = "움직일 기물의 도착 좌표를 입력해주세요. (예: 4,4)";
-    private static final String BASE_DELIMITER = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String askHanArrangement() {
@@ -22,15 +20,15 @@ public class InputView {
         return scanner.nextLine();
     }
 
-    public static List<String> askStartPosition() {
+    public static List<Integer> askStartPosition() {
         System.out.println(START_POSITION_MESSAGE);
         String input = scanner.nextLine();
-        return Arrays.stream(input.split(BASE_DELIMITER)).map(String::trim).toList();
+        return PositionParser.parsePositionInput(input);
     }
 
-    public static List<String> askEndPosition() {
+    public static List<Integer> askEndPosition() {
         System.out.println(END_POSITION_MESSAGE);
         String input = scanner.nextLine();
-        return Arrays.stream(input.split(BASE_DELIMITER)).map(String::trim).toList();
+        return PositionParser.parsePositionInput(input);
     }
 }
