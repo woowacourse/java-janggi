c14fc908bbc83c43cc5bfe9c65d71580000bb7a3
Jeshin Kim <jjason0904@cau.ac.kr>
2026-03-30 15:26:18 +0900

    test(Player): 플레이어 정상 입력, 빈값 예외 테스트 추가

---
+++ src/test/java/domain/player/PlayerTest.java
@@ -0,0 +1,28 @@
+package domain.player;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import common.exception.JanggiException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PlayerTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"janggiPlayer"})
+    void 이름이_비어있지_않으면_플레이어가_정상_생성된다(String name) {
+        Player player = new Player(new Name(name), Team.CHO);
+
+        assertEquals(name, player.getName());
+        assertEquals(Team.CHO, player.getTeam());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   "})
+    void 이름이_빈값이면_예외가_발생한다(String name) {
+        assertThrows(JanggiException.class, () -> new Player(new Name(name), Team.HAN));
+    }
+}
