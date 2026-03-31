3149a986454def7191f66efa37a3d588f3195a9c
toctoce <toctoce@naver.com>
2026-03-31 20:21:40 +0900

    feat(Game): getBoardMap, isRunning 메서드 구현

--- src/main/java/domain/game/Game.java
+++ src/main/java/domain/game/Game.java
@@ -4,10 +4,13 @@ import domain.board.Board;
 import domain.game.state.ChoTurn;
 import domain.game.state.GameState;
 import domain.piece.Piece;
+import domain.player.Player;
 import domain.player.Players;
+import domain.player.Team;
 import domain.position.Position;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
     private Players players;
@@ -48,5 +51,23 @@ public class Game {
 
     public boolean isHan(Position position) {
         return board.isHan(position);
+    }
+
+    public Map<Position, Piece> getBoardMap() {
+        return board.getBoardMap();
+    }
+
+    public boolean isRunning() {
+        return gameState.isRunning();
+    }
+
+    public String getCurrentPlayerName() {
+        Team currentTeam = gameState.getCurrentTeam();
+        Player player = players.getByTeam(currentTeam);
+        return player.getName();
+    }
+
+    public Team getCurrentTeam() {
+        return gameState.getCurrentTeam();
     }
 }
