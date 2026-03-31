1a122f7485bf2401622d02f9fdf1c511492bf1b7
jyt6640 <jyt6640@gmail.com>
2026-03-31 23:03:06 +0900

    feat: 상차림 기능 구현

---
+++ src/main/java/janggi/application/CsvInitialBoardProvider.java
@@ -0,0 +1,13 @@
+package janggi.application;
+
+import janggi.dto.PositionInfo;
+import janggi.util.FileParser;
+import java.util.List;
+
+public class CsvInitialBoardProvider implements InitialBoardProvider {
+
+    @Override
+    public List<PositionInfo> load() {
+        return FileParser.readCsvFile("/janggi.csv");
+    }
+}
