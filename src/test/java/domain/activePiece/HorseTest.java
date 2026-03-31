9a809dcd9e6687446c899c3936b8e46c1fe74413
김가현 <hyeonnni@naver.com>
2026-03-31 22:07:41 +0900

    test: 마 이동 경로 부정 테스트 제거

--- src/test/java/domain/activePiece/HorseTest.java
+++ src/test/java/domain/activePiece/HorseTest.java
@@ -7,26 +7,25 @@ import domain.piece.ActivePiece;
 import domain.piece.Horse;
 import domain.piece.Piece;
 import domain.position.Position;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class HorseTest {
 
     @Test
-    void 정상_범위_입력() {
+    void 마는_직선1칸_대각선_1칸으로_이동할_수_있다() {
         Piece horse = new Horse(Team.HAN);
         assertThat(horse.canMove(new Position(5, 5), new Position(6, 7))).isTrue();
     }
 
     @Test
-    void 정상_범위가_아니면_거짓() {
+    void 마의_이동_범위가_직선1칸_대각선_1칸이_아니면_거짓이다() {
         Piece horse = new Horse(Team.HAN);
         assertThat(horse.canMove(new Position(5, 5), new Position(8, 8))).isFalse();
     }
 
     @Test
-    void 마_정상_경로_출력() {
+    void 마_직선1칸_대각선_1칸_경로_출력() {
         ActivePiece horse = new Horse(Team.HAN);
         Position src = new Position(3, 3);
         Position mid = new Position(4, 3);
@@ -34,16 +33,5 @@ class HorseTest {
         List<Position> routes = List.of(mid);
 
         assertThat(horse.searchRoute(src, dest)).isEqualTo(routes);
-    }
-
-    @Test
-    void 마_비정상_경로_출력() {
-        ActivePiece horse = new Horse(Team.HAN);
-        Position src = new Position(3, 3);
-        Position mid = new Position(6, 3);
-        Position dest = new Position(7, 3);
-        List<Position> routes = new ArrayList<>(List.of(mid, dest));
-
-        assertThat(horse.searchRoute(src, dest)).isNotEqualTo(routes);
     }
 }
