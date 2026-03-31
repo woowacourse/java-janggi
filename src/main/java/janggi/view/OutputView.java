bc1f826774300583d9139b7adf83973ed93c1ec0
haeyoon1 <hynam1220@naver.com>
2026-03-31 21:59:28 +0900

    refactor: 출력 형식 수정

--- src/main/java/janggi/view/OutputView.java
+++ src/main/java/janggi/view/OutputView.java
@@ -9,7 +9,6 @@ public class OutputView {
     public static void printBoard(Board board) {
         for (int y = 0; y < board.getBoardWidth(); y++) {
             printRow(board, y);
-            System.out.println();
         }
     }
 
