4cdb3e6bf4e1a4364681f178e3ec99b21e91ed12
Giyoul <cla12093@gmail.com>
2026-03-31 16:10:57 +0900

    refactor: remove unnecessary exception handling in Col

--- src/main/java/domain/board/Col.java
+++ src/main/java/domain/board/Col.java
@@ -21,15 +21,7 @@ public enum Col {
     }
 
     public static Col toCol(char character) {
-        String convertedCharacter = checkExistingColumn(character);
+        String convertedCharacter = String.valueOf(character);
         return Col.valueOf(convertedCharacter.toUpperCase());
-    }
-
-    private static String checkExistingColumn(char character) {
-        try {
-            return String.valueOf(character);
-        } catch (IllegalArgumentException exception) {
-            throw new IllegalArgumentException("[ERROR] 좌표 형식이 틀렸습니다.");
-        }
     }
 }
