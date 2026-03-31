318a5fe516a9b4a4fbdf55c92f24ba5f9ebaece4
JunHyung1206 <pjh120610@naver.com>
2026-03-31 20:20:02 +0900

    refactor(InitBoard): BoardFactory 수정
    - Map으로 기물마다 생성을 할 수 있도록 변경

--- src/main/java/domain/board/BoardFactory.java
+++ src/main/java/domain/board/BoardFactory.java
@@ -14,8 +14,20 @@ import domain.piece.Team;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class BoardFactory {
+
+    private static final Map<PieceType, Function<Team, Piece>> PIECE_GENERATORS = Map.of(
+            PieceType.ELEPHANT, Elephant::new,
+            PieceType.HORSE, Horse::new,
+            PieceType.SOLDIER, Soldier::new,
+            PieceType.CHARIOT, Chariot::new,
+            PieceType.CANNON, Cannon::new,
+            PieceType.GUARD, Guard::new,
+            PieceType.GENERAL, General::new
+    );
+
     private static final List<Integer> SOLDIER_COLUMNS = List.of(0, 2, 4, 6, 8);
     private static final List<Integer> CHARIOT_COLUMNS = List.of(0, 8);
     private static final List<Integer> GUARD_COLUMNS = List.of(3, 5);
@@ -26,53 +38,46 @@ public class BoardFactory {
 
     public static Board createBoard(InitializeSetting choInitialSetting, InitializeSetting hanInitialSetting) {
         Map<Position, Piece> pieces = new HashMap<>();
-
+        List<PieceType> choSetting = choInitialSetting.getInitialSetting();
+        List<PieceType> hanSetting = hanInitialSetting.getInitialSetting();
 
         for (Integer soliderColumns : SOLDIER_COLUMNS) {
-            pieces.put(new Position(soliderColumns, 3), new Soldier(Team.CHO));
-            pieces.put(new Position(soliderColumns, 6), new Soldier(Team.HAN));
+            pieces.put(new Position(soliderColumns, 3), createPiece(PieceType.SOLDIER, Team.CHO));
+            pieces.put(new Position(soliderColumns, 6), createPiece(PieceType.SOLDIER, Team.HAN));
         }
 
         for (Integer chariotColumn : CHARIOT_COLUMNS) {
-            pieces.put(new Position(chariotColumn, 0), new Chariot(Team.CHO));
-            pieces.put(new Position(chariotColumn, 9), new Chariot(Team.HAN));
-
+            pieces.put(new Position(chariotColumn, 0), createPiece(PieceType.CHARIOT, Team.CHO));
+            pieces.put(new Position(chariotColumn, 9), createPiece(PieceType.CHARIOT, Team.HAN));
         }
 
         for (Integer guardColumn : GUARD_COLUMNS) {
-            pieces.put(new Position(guardColumn, 0), new Guard(Team.CHO));
-            pieces.put(new Position(guardColumn, 9), new Guard(Team.HAN));
-
+            pieces.put(new Position(guardColumn, 0), createPiece(PieceType.GUARD, Team.CHO));
+            pieces.put(new Position(guardColumn, 9), createPiece(PieceType.GUARD, Team.HAN));
         }
 
         for (Integer cannonColumn : CANNON_COLUMNS) {
-            pieces.put(new Position(cannonColumn, 2), new Cannon(Team.CHO));
-            pieces.put(new Position(cannonColumn, 7), new Cannon(Team.HAN));
-
+            pieces.put(new Position(cannonColumn, 2), createPiece(PieceType.CANNON, Team.CHO));
+            pieces.put(new Position(cannonColumn, 7), createPiece(PieceType.CANNON, Team.HAN));
         }
 
         pieces.put(new Position(4, 1), new General(Team.CHO));
         pieces.put(new Position(4, 8), new General(Team.HAN));
 
-        List<PieceType> choSetting = choInitialSetting.getInitialSetting();
         pieces.put(new Position(1, 0), createPiece(choSetting.get(0), Team.CHO));
         pieces.put(new Position(2, 0), createPiece(choSetting.get(1), Team.CHO));
         pieces.put(new Position(6, 0), createPiece(choSetting.get(2), Team.CHO));
         pieces.put(new Position(7, 0), createPiece(choSetting.get(3), Team.CHO));
 
-        List<PieceType> hanSetting = hanInitialSetting.getInitialSetting();
-        pieces.put(new Position(1, 9), createPiece(hanSetting.get(0), Team.HAN));
-        pieces.put(new Position(2, 9), createPiece(hanSetting.get(1), Team.HAN));
-        pieces.put(new Position(6, 9), createPiece(hanSetting.get(2), Team.HAN));
-        pieces.put(new Position(7, 9), createPiece(hanSetting.get(3), Team.HAN));
+        pieces.put(new Position(7, 9), createPiece(hanSetting.get(0), Team.HAN));
+        pieces.put(new Position(6, 9), createPiece(hanSetting.get(1), Team.HAN));
+        pieces.put(new Position(2, 9), createPiece(hanSetting.get(2), Team.HAN));
+        pieces.put(new Position(1, 9), createPiece(hanSetting.get(3), Team.HAN));
 
         return new Board(pieces);
     }
 
     private static Piece createPiece(PieceType pieceType, Team team) {
-        if (pieceType == PieceType.ELEPHANT) {
-            return new Elephant(team);
-        }
-        return new Horse(team);
+        return PIECE_GENERATORS.get(pieceType).apply(team);
     }
 }
