bd969c03667787003e3774e3fdfc17297d0bfe35
minzzun99 <skg06123@naver.com>
2026-03-31 22:19:17 +0900

    feat: 장기판 출력 시 좌표 및 테두리 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,8 +9,6 @@ import service.dto.PieceDto;
 import service.dto.PositionDto;
 
 public class OutputView {
-    public static final String PREFIX_ERROR_MESSAGE = "[ERROR]";
-    public static final String EMPTY_PIECE = "＋";
 
     private final OutputViewFormatter formatter;
 
@@ -23,7 +21,7 @@ public class OutputView {
     }
 
     public void printErrorMessage(String message) {
-        System.out.println(PREFIX_ERROR_MESSAGE + message);
+        System.out.println(formatter.formatErrorMessage(message));
     }
 
     public void printCountry(Country country) {
@@ -35,39 +33,44 @@ public class OutputView {
     }
 
     public void printChangeTurnMessage(String countryName) {
-        System.out.printf("\n차례 : %s\n", countryName);
+        System.out.println(formatter.formatChangeTurn(countryName));
     }
 
     public void printBoard(BoardDto boardDto) {
         StringBuilder boardResult = new StringBuilder();
         Map<PositionDto, PieceDto> pieces = boardDto.pieces();
+        boardResult.append(formatter.formatColNumbers());
+        boardResult.append(formatter.formatHorizontalLine());
 
         for (int x = 1; x <= 10; x++) {
+            boardResult.append(formatter.formatRowNumber(x));
+
             for (int y = 1; y <= 9; y++) {
                 PositionDto nowPosition = new PositionDto(x, y);
                 if (!pieces.containsKey(nowPosition)) {
-                    boardResult.append(String.format("%-3s", EMPTY_PIECE));
+                    boardResult.append(formatter.formatEmptyPiece());
                     continue;
                 }
 
                 PieceDto pieceDto = pieces.get(nowPosition);
                 boardResult.append(
-                        String.format("%s%-3s%s", PieceColor.getColorCode(pieceDto.countryName()),
-                                pieceDto.pieceName()
-                                , PieceColor.getColorCode(PieceColor.NONE.name())));
+                        formatter.formatPiece(PieceColor.getColorCode(pieceDto.countryName()), pieceDto.pieceName()));
             }
-            boardResult.append('\n');
+
+            boardResult.append(formatter.formatRightVerticalLine());
         }
 
+        boardResult.append(formatter.formatHorizontalLine());
         System.out.print(boardResult);
     }
 
-    public void printPiecePossiblePosition(PieceType pieceType,List<PositionDto> positionDtos) {
-        System.out.printf("\n['%s' 기물의 현재 좌표 목록]\n",pieceType.getName());
+    public void printPiecePossiblePosition(PieceType pieceType, List<PositionDto> positionDtos) {
+        StringBuilder result = new StringBuilder(formatter.formatPossiblePositionHeader(pieceType.getName()));
         int num = 1;
         for (PositionDto dto : positionDtos) {
-            System.out.printf("%d. [%d,%d]\n", num++, dto.x(), dto.y());
+            result.append(formatter.formatPossiblePosition(num++, dto.x(), dto.y()));
         }
-    }
 
+        System.out.print(result);
+    }
 }
