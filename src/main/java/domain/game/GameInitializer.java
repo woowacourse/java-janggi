3112dc0b2e23d6e28191fac49c6ca8ecd3869ef2
toctoce <toctoce@naver.com>
2026-03-31 20:40:26 +0900

    refactor(GameInitializer): 게임 생성시 이름 문자열 대신 Players 객체를 받도록 변경

--- src/main/java/domain/game/GameInitializer.java
+++ src/main/java/domain/game/GameInitializer.java
@@ -1,29 +1,15 @@
 package domain.game;
 
-import static domain.player.Team.CHO;
-import static domain.player.Team.HAN;
-
 import domain.board.Board;
 import domain.board.BoardFactory;
 import domain.board.Formation;
-import domain.player.Name;
-import domain.player.Player;
 import domain.player.Players;
-import domain.player.Team;
-import java.util.List;
 
 public class GameInitializer {
 
-    public static Game initialize(String choName, String hanName, int choPositionInput, int hanPositionInput) {
-        Players players = createPlayers(choName, hanName);
+    public static Game initialize(Players players, int choPositionInput, int hanPositionInput) {
         Board board = createBoard(choPositionInput, hanPositionInput);
         return new Game(players, board);
-    }
-
-    private static Players createPlayers(String choName, String hanName) {
-        Player choPlayer = createPlayer(choName, CHO);
-        Player hanPlayer = createPlayer(hanName, HAN);
-        return new Players(List.of(choPlayer, hanPlayer));
     }
 
     private static Board createBoard(int choPositionInput, int hanPositionInput) {
@@ -31,10 +17,6 @@ public class GameInitializer {
         Formation hanFormation = createFormation(hanPositionInput);
 
         return BoardFactory.createWithFormation(choFormation, hanFormation);
-    }
-
-    private static Player createPlayer(String name, Team team) {
-        return new Player(new Name(name), team);
     }
 
     private static Formation createFormation(int positionInput) {
