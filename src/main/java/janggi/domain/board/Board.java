807eefd64d081ff6a747ba6deb3203397508fc42
kkb <gimgyubin1225@gmail.com>
2026-03-31 19:38:27 +0900

    chore: 메서드 정의 순서를 중요도에 따라 재배치

--- src/main/java/janggi/domain/board/Board.java
+++ src/main/java/janggi/domain/board/Board.java
@@ -60,11 +60,6 @@ public class Board {
         }
     }
 
-    public boolean isNotEmpty() {
-        return !boardState.values().stream()
-                .allMatch(Piece::isEmpty);
-    }
-
     public void validateLocationToMove(Side currentSide, Location locationToMove) {
         validateLocation(locationToMove);
         Piece target = boardState.get(locationToMove);
@@ -73,21 +68,9 @@ public class Board {
         }
     }
 
-    private void validateMove(Location from, Location to) {
-        validateLocation(from);
-        validateLocation(to);
-        validatePieceExist(from);
-    }
-
-    private List<Piece> getPiecesOnRoute(Piece piece, Location from, Location to) {
-        return piece.calculateRoute(from, to).stream()
-                .map(boardState::get)
-                .toList();
-    }
-
-    private void executeMove(Location from, Location to, Piece piece) {
-        boardState.put(to, piece);
-        boardState.put(from, EmptyPiece.getInstance());
+    public boolean isNotEmpty() {
+        return !boardState.values().stream()
+                .allMatch(Piece::isEmpty);
     }
 
     public List<List<Piece>> to2DArray() {
@@ -103,6 +86,27 @@ public class Board {
         return List.copyOf(pieces);
     }
 
+    private void executeMove(Location from, Location to, Piece piece) {
+        boardState.put(to, piece);
+        boardState.put(from, EmptyPiece.getInstance());
+    }
+
+    private boolean isNotSameSide(Piece piece, Side side) {
+        return !piece.isSameSide(side);
+    }
+
+    private List<Piece> getPiecesOnRoute(Piece piece, Location from, Location to) {
+        return piece.calculateRoute(from, to).stream()
+                .map(boardState::get)
+                .toList();
+    }
+
+    private void validateMove(Location from, Location to) {
+        validateLocation(from);
+        validateLocation(to);
+        validatePieceExist(from);
+    }
+
     private void validateLocation(Location location) {
         if (!boardState.containsKey(location)) {
             throw new IllegalArgumentException("해당 좌표는 보드판에 존재하지 않습니다.");
@@ -113,9 +117,5 @@ public class Board {
         if (boardState.get(location).isEmpty()) {
             throw new IllegalArgumentException("해당 좌표에 기물이 존재하지 않습니다.");
         }
-    }
-
-    private boolean isNotSameSide(Piece piece, Side side) {
-        return !piece.isSameSide(side);
     }
 }
