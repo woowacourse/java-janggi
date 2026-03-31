2949e373b3131cce43096e3fe370f542d246e433
김가현 <hyeonnni@naver.com>
2026-03-31 22:07:48 +0900

    test: 사 이동 경로 부정 테스트 제거

--- src/test/java/domain/activePiece/GuardTest.java
+++ src/test/java/domain/activePiece/GuardTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.game.Team;
 import domain.piece.ActivePiece;
-import domain.piece.Guard;
+import domain.piece.PalacePiece;
 import domain.piece.Piece;
 import domain.position.Column;
 import domain.position.Position;
@@ -16,37 +16,25 @@ import org.junit.jupiter.api.Test;
 class GuardTest {
 
     @Test
-    void 정상_범위_입력() {
-        Piece guard = new Guard(Team.HAN);
+    void 사_이동_범위는_직선이어야한다() {
+        Piece guard = PalacePiece.guard(Team.HAN);
         assertThat(guard.canMove(new Position(5, 5), new Position(6, 5))).isTrue();
     }
 
     @Test
-    void 정상_범위가_아니면_거짓() {
-        Piece guard = new Guard(Team.HAN);
+    void 사_이동_범위가_직선이_아니면_거짓() {
+        Piece guard = PalacePiece.guard(Team.HAN);
         assertThat(guard.canMove(new Position(5, 5), new Position(8, 8))).isFalse();
     }
 
     @Test
-    void 사_정상_경로_출력_한다() {
-        ActivePiece guard = new Guard(Team.HAN);
+    void 사는_인접_이동_시_중간_경로_위치가_없다() {
+        ActivePiece guard = PalacePiece.guard(Team.HAN);
 
         Position src = new Position(new Row(1), new Column(3));
         Position dest = new Position(new Row(2), new Column(3));
         List<Position> routes = new ArrayList<>();
 
         assertThat(guard.searchRoute(src, dest)).isEqualTo(routes);
-    }
-
-    @Test
-    void 사_비정상_경로_출력_한다() {
-        ActivePiece guard = new Guard(Team.HAN);
-
-        Position src = new Position(new Row(1), new Column(3));
-        Position dest = new Position(new Row(2), new Column(3));
-        List<Position> routes = new ArrayList<>(
-                List.of(new Position(new Row(3), new Column(3))));
-
-        assertThat(guard.searchRoute(src, dest)).isNotEqualTo(routes);
     }
 }
