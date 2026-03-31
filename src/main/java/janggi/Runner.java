0c4010a30f6dc8a0f0f781ebd8ebcea46b25cfe9
armd482 <armd482@naver.com>
2026-03-31 20:36:18 +0900

    refactor: 상수 static으로 변경

--- src/main/java/janggi/Runner.java
+++ src/main/java/janggi/Runner.java
@@ -10,8 +10,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Runner {
-    private final String UNEXPECTED_SERVER_ERROR_LOG_MESSAGE = "예측하지 못한 시스템 오류 발생";
-    private final String SYSTEM_ERROR_MESSAGE = "시스템 오류가 발생하여 게임을 종료합니다.";
+    private static final String UNEXPECTED_SERVER_ERROR_LOG_MESSAGE = "예측하지 못한 시스템 오류 발생";
+    private static final String SYSTEM_ERROR_MESSAGE = "시스템 오류가 발생하여 게임을 종료합니다.";
 
     private static final Logger logger = Logger.getLogger(Runner.class.getName());
     private Game game;
