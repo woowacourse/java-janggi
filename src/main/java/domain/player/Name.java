fdaeed726bfb5faa25cceb9e6ff51bd473d94af3
박병훈 <leafloveu@naver.com>
2026-03-31 15:07:15 +0900

    chore(name):상수, 메서드 네이밍 적절하게 수정 및 에러 메시지 구체화

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -4,24 +4,24 @@ public record Name(
         String name
 ) {
 
-    private static final int MINIMUM_LENGTH = 2;
-    private static final int MAXIMUM_LENGTH = 5;
-    private static final String NAME_PATTERN = String.format("^[A-Za-z]{%d,%d}", MINIMUM_LENGTH, MAXIMUM_LENGTH);
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String VALID_NAME_REGEX = String.format("^[A-Za-z]{%d,%d}$", MIN_NAME_LENGTH, MAX_NAME_LENGTH);
 
     public Name {
-        validateEmpty(name);
-        validatePattern(name);
+        validateNotBlank(name);
+        validateNameFormat(name);
     }
 
-    private void validateEmpty(final String name) {
+    private void validateNotBlank(final String name) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("빈 칸 입력 불가능");
+            throw new IllegalArgumentException("이름을 입력하지 않았거나 이름이 공백입니다. 이름은 2~5자의 영문자로 이루어진 문자열만 가능합니다.");
         }
     }
 
-    private void validatePattern(final String name) {
-        if (!name.matches(NAME_PATTERN)) {
-            throw new IllegalArgumentException("이름 규칙에 맞지 않음");
+    private void validateNameFormat(final String name) {
+        if (!name.matches(VALID_NAME_REGEX)) {
+            throw new IllegalArgumentException("이름이 규칙에 맞지 않습니다. 이름은 2~5자의 영문자로 이루어진 문자열만 가능합니다. name: " + name);
         }
     }
 }
