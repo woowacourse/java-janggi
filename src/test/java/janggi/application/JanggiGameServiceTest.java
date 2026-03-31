53fe18d23ff486829785b36c6a7592d2827136ed
jyt6640 <jyt6640@gmail.com>
2026-03-31 21:32:23 +0900

    test: 저장된 게임이 없을 시 새 게임 시작 테스트 추가

--- src/test/java/janggi/application/JanggiGameServiceTest.java
+++ src/test/java/janggi/application/JanggiGameServiceTest.java
@@ -3,6 +3,9 @@ package janggi.application;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import janggi.application.dto.GameSummary;
+import janggi.domain.JanggiGame;
+import janggi.domain.status.Team;
+import janggi.dto.PositionInfo;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,5 +31,26 @@ public class JanggiGameServiceTest {
         assertThat(gameSummaries).hasSize(2);
         assertThat(gameSummaries.get(0).id()).isEqualTo(1L);
         assertThat(gameSummaries.get(1).id()).isEqualTo(2L);
+    }
+
+    @Test
+    @DisplayName("저장된 게임이 없을 시 새 게임 시작")
+    void no_save_game_new_game_start() {
+        // given
+        GameRepository gameRepository = new FakeGameRepository(List.of());
+        InitialBoardProvider initialBoardProvider = new FakeInitialBoardProvider(
+                List.of(
+                        PositionInfo.from(Team.CHO, "JANG", 4, 1),
+                        PositionInfo.from(Team.HAN, "JANG", 4, 8)
+                )
+        )
+        JanggiGameService janggiGameService = new JanggiGameService(gameRepository, initialBoardProvider);
+
+        // when
+        JanggiGame janggiGame = janggiGameService.startNewGame();
+
+        // then
+        assertThat(janggiGame.currentTurn()).isEqualTo(Team.CHO);
+        assertThat(janggiGame.boardStatus()).hasSize(2);
     }
 }
