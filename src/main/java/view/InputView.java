ade11e96bbf25af213106bedec7c032f2e7631cc
wontop02 <wontop@wontop-MacBookAir.local>
2026-03-31 15:39:15 +0900

    refactor: 상차림 입력 예시 문구 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
-    private static final String INPUT_TABLE_SETTING = "%s의 상차림을 입력하세요. (ex - 상마상마(왼상차림), 마상마상(오른상차림), 마상상마(안상차림), 상마마상(바깥상차림)";
+    private static final String INPUT_TABLE_SETTING = "%s의 상차림을 입력하세요. (ex - 상마상마, 마상마상, 마상상마, 상마마상)";
     private static final String INPUT_FROM_POSITION = "움직일 기물의 좌표를 입력하세요.";
     private static final String INPUT_TO_POSITION = "움직이고 싶은 좌표를 입력하세요.";
 
