fcfd62a5d3af986e7b2f8fb04c32caad76623b63
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-31 22:06:39 +0900

    feat(Application): 의존성 주입

--- src/main/java/janggi/JanggiApplication.java
+++ src/main/java/janggi/JanggiApplication.java
@@ -1,37 +1,19 @@
 package janggi;
 
-import janggi.domain.Board;
-import janggi.domain.JanggiGame;
-import janggi.domain.point.Point;
-import janggi.domain.piece.Piece;
-import janggi.presentation.dto.GameStatusInfo;
-import janggi.presentation.dto.MoveCommand;
-import janggi.presentation.dto.PositionInfo;
-import janggi.presentation.ui.InputView;
-import janggi.presentation.ui.OutputView;
-import janggi.util.Console;
-import janggi.util.FileParser;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import janggi.application.JanggiGameService;
+import janggi.domain.board.BoardRepository;
+import janggi.infra.JdbcBoardRepository;
+import janggi.presentation.JanggiGameController;
+import java.sql.SQLException;
+import org.h2.tools.Server;
 
 public class JanggiApplication {
-    public static void main(String[] args) {
-        List<PositionInfo> positionInfos = FileParser.readCsvFile("/janggi.csv");
-        Map<Point, Piece> pieces = new LinkedHashMap<>();
-        positionInfos.forEach(info -> pieces.put(info.point(), info.piece()));
-        Board board = new Board();
-        board.init(pieces);
-        JanggiGame game = new JanggiGame(board);
-
-        OutputView.printGameStatus(GameStatusInfo.from(game.getBoardStatus()));
-        OutputView.printStartGame();
-        while (!game.isFinished()) {
-            MoveCommand points = InputView.readPoints();
-            game.play(points.from(), points.to());
-            OutputView.printGameStatus(GameStatusInfo.from(game.getBoardStatus()));
-        }
-        OutputView.printWinner(game.getWinner());
-        Console.close();
+    public static void main(String[] args) throws SQLException {
+        Server server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
+        BoardRepository repository = new JdbcBoardRepository();
+        JanggiGameService service = new JanggiGameService(repository);
+        JanggiGameController controller = new JanggiGameController(service);
+        controller.run();
+        server.stop();
     }
 }
