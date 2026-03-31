4b385a17fba016c6a490ba85ce9ff1e95db1fe13
이동훈 <higgs95@naver.com>
2026-03-31 17:21:23 +0900

    refactor: board 이동 검증 메서드 분리

--- src/main/java/janggi/domain/board/Board.java
+++ src/main/java/janggi/domain/board/Board.java
@@ -20,22 +20,7 @@ public class Board {
     }
 
     public void move(Position from, Position to) {
-        if (!hasPieceAt(from)) {
-            throw new IllegalArgumentException("해당 출발 위치에는 기물이 존재하지 않습니다");
-        }
-
-        if (!isCurrentTeamPiece(from)) {
-            throw new IllegalArgumentException("해당 기물은 현재 턴의 진영 기물이 아닙니다.");
-        }
-
-        if (!canMoveByBasicMovingRule(from, to)) {
-            throw new IllegalArgumentException("해당 기물은 그 위치로 이동할 수 없습니다.");
-        }
-
-        if (!canMoveBySpecialMovingRule(from, to)) {
-            throw new IllegalArgumentException("해당 기물의 이동 규칙에 맞지 않습니다.");
-        }
-
+        validateMove(from, to);
         movePiece(from, to);
         changeTurn();
     }
@@ -44,29 +29,60 @@ public class Board {
         return board.containsKey(position);
     }
 
-    public boolean isCurrentTeamPiece(Position from) {
+    private boolean isCurrentTeamPiece(Position from) {
         Piece piece = board.get(from);
         return turn.isCurrentTeam(piece.getTeam());
     }
 
-    public boolean canMoveByBasicMovingRule(Position from, Position to) {
+    private boolean canMoveByBasicMovingRule(Position from, Position to) {
         Piece piece = board.get(from);
         return piece.canMoveByBasicMovingRule(from, to);
     }
 
-    public boolean canMoveBySpecialMovingRule(Position from, Position to) {
+    private boolean canMoveBySpecialMovingRule(Position from, Position to) {
         Map<Position, Piece> paths = getPositionPiecesFromPath(from, to);
         Piece piece = board.get(from);
         return piece.canMoveBySpecialMovingRule(paths, to);
     }
 
-    public void movePiece(Position from, Position to) {
+    private void movePiece(Position from, Position to) {
         board.put(to, board.get(from));
         board.remove(from);
     }
 
-    public void changeTurn() {
+    private void changeTurn() {
         this.turn = turn.changeTurn();
+    }
+
+    private void validateMove(Position from, Position to) {
+        validatePieceExistsAt(from);
+        validateCurrentTurn(from);
+        validateBasicMovingRule(from, to);
+        validateSpecialMovingRule(from, to);
+    }
+
+    private void validatePieceExistsAt(Position from) {
+        if (!hasPieceAt(from)) {
+            throw new IllegalArgumentException("해당 출발 위치에는 기물이 존재하지 않습니다");
+        }
+    }
+
+    private void validateCurrentTurn(Position from) {
+        if (!isCurrentTeamPiece(from)) {
+            throw new IllegalArgumentException("해당 기물은 현재 턴의 진영 기물이 아닙니다.");
+        }
+    }
+
+    private void validateBasicMovingRule(Position from, Position to) {
+        if (!canMoveByBasicMovingRule(from, to)) {
+            throw new IllegalArgumentException("해당 기물은 그 위치로 이동할 수 없습니다.");
+        }
+    }
+
+    private void validateSpecialMovingRule(Position from, Position to) {
+        if (!canMoveBySpecialMovingRule(from, to)) {
+            throw new IllegalArgumentException("해당 기물의 이동 규칙에 맞지 않습니다.");
+        }
     }
 
     private Map<Position, Piece> getPositionPiecesFromPath(Position from, Position to) {
