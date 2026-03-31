e8a296a92160817a38d7cc659b68af22d5194fc1
jyt6640 <jyt6640@gmail.com>
2026-03-31 14:58:22 +0900

    test: 데이터베이스 init 호출 시 테이블 생성 테스트 추가

---
+++ src/test/java/janggi/persistence/DatabaseInitializerTest.java
@@ -0,0 +1,36 @@
+package janggi.persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DatabaseInitializerTest {
+
+    @Test
+    @DisplayName("init 호출 시 테이블 생성")
+    void init() throws SQLException {
+        // given
+        JdbcConnectionManager connectionManager = new JdbcConnectionManager();
+        DatabaseInitializer databaseInitializer = new DatabaseInitializer(connectionManager);
+
+        // when
+        databaseInitializer.init();
+
+        //then
+        assertThat(existsTable(connectionManager, "GAMES")).isTrue();
+        assertThat(existsTable(connectionManager, "GAME_PIECES")).isTrue();
+    }
+
+    private boolean existsTable(JdbcConnectionManager connectionManager, String tableName) throws SQLException {
+        try (Connection connection = connectionManager.getConnection()){
+            ResultSet resultSet = connection.getMetaData()
+                    .getTables(null, null, tableName, null);
+            return resultSet.next();
+        }
+    }
+}
