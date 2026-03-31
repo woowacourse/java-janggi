f31ba13201d839e188e7d816f59fefe4caa8dc19
choubung <choubung@sookmyung.ac.kr>
2026-03-31 14:25:13 +0900

    test : Direction 테스트 작성

--- src/test/java/janggi/domain/DirectionTest.java
+++ src/test/java/janggi/domain/DirectionTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package janggi.domain;
+
+import janggi.domain.vo.Position;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class DirectionTest {
-  
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1, 0, 1, NORTH",
+            "1, 1, 2, 1, SOUTH",
+            "1, 1, 1, 2, EAST",
+            "1, 1, 1, 0, WEST",
+    })
+    void 동서남북_방향_찾기_테스트(int fromRow, int fromCol, int toRow, int toCol, Direction direction) {
+        Position from = new Position(fromRow, fromCol);
+        Position to = new Position(toRow, toCol);
+
+        assertThat(Direction.findFourDirection(from, to)).isEqualTo(direction);
+    }
 }
