b895e2b0eddf6dc744aaec1fe147fd0cb9cf1c76
armd482 <armd482@naver.com>
2026-03-31 20:39:28 +0900

    refactor: routePolicy final로 변경

--- src/main/java/janggi/domain/piece/ActivePiece.java
+++ src/main/java/janggi/domain/piece/ActivePiece.java
@@ -7,7 +7,7 @@ public abstract class ActivePiece extends BasePiece {
     protected static final String INVALID_DESTINATION_MESSAGE = "올바른 도착 지점이 아닙니다.";
     protected static final String UNMOVABLE_ROUTE_MESSAGE = "이동할 수 없는 경로입니다.";
 
-    protected RoutePolicy routePolicy;
+    protected final RoutePolicy routePolicy;
 
     public ActivePiece(RoutePolicy routePolicy, Side side, PieceType pieceType) {
         super(side, pieceType);
