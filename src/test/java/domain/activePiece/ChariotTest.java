3fd8267731fe8d5ab831120839815e19052af1b2
김가현 <hyeonnni@naver.com>
2026-03-31 22:07:20 +0900

    test: 차 이동 경로 부정 테스트 제거

--- src/test/java/domain/activePiece/ChariotTest.java
+++ src/test/java/domain/activePiece/ChariotTest.java
@@ -3,9 +3,8 @@ package domain.activePiece;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.game.Team;
-import domain.piece.ActivePiece;
-import domain.piece.Chariot;
 import domain.piece.Piece;
+import domain.piece.StraightMovePiece;
 import domain.position.Column;
 import domain.position.Position;
 import domain.position.Row;
@@ -17,21 +16,21 @@ class ChariotTest {
 
     @Test
     void 차는_직선_운동한다() {
-        Piece cha = new Chariot(Team.HAN);
+        Piece cha = StraightMovePiece.chariot(Team.HAN);
         assertThat(cha.canMove(new Position(1, 4), new Position(1, 8))).isTrue();
 
     }
 
     @Test
     void 차는_직선이_아닌_방향으로_가지_못한다() {
-        Piece cha = new Chariot(Team.HAN);
+        Piece cha = StraightMovePiece.chariot(Team.HAN);
         assertThat(cha.canMove(new Position(5, 5), new Position(3, 3))).isFalse();
 
     }
 
     @Test
     void 직선_방향_경로를_출력_한다() {
-        ActivePiece chariot = new Chariot(Team.HAN);
+        StraightMovePiece chariot = StraightMovePiece.chariot(Team.HAN);
         Position src = new Position(new Row(1), new Column(3));
         Position dest = new Position(new Row(4), new Column(3));
         List<Position> routes = new ArrayList<>(
@@ -39,18 +38,5 @@ class ChariotTest {
                         new Position(new Row(3), new Column(3))));
 
         assertThat(chariot.searchRoute(src, dest)).isEqualTo(routes);
-    }
-
-    @Test
-    void 잘못된_직선_방향_경로를_출력_한다() {
-        ActivePiece chariot = new Chariot(Team.HAN);
-        Position src = new Position(new Row(1), new Column(3));
-        Position dest = new Position(new Row(4), new Column(3));
-        List<Position> routes = new ArrayList<>(
-                List.of(new Position(new Row(3), new Column(4)),
-                        new Position(new Row(5), new Column(4)),
-                        new Position(new Row(7), new Column(4))));
-
-        assertThat(chariot.searchRoute(src, dest)).isNotEqualTo(routes);
     }
 }
