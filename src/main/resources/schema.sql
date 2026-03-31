3c0d4937edc91f87214b3be5c63b504e28a33e19
jyt6640 <jyt6640@gmail.com>
2026-03-31 15:25:28 +0900

    refactor: 테이블이 있을 시 다시 생성 되지 않도록 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE games (
+CREATE TABLE IF NOT EXISTS games (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     current_turn VARCHAR(10) NOT NULL,
     finished BOOLEAN NOT NULL,
     winner VARCHAR(10)
 );
 
-CREATE TABLE game_pieces (
+CREATE TABLE IF NOT EXISTS game_pieces (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     game_id BIGINT NOT NULL,
     team VARCHAR(10) NOT NULL,
