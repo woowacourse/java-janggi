ba5fe372b2ab26f992ad10628c834126e9c15569
armd482 <armd482@naver.com>
2026-03-31 09:41:30 +0900

    test: board 기물 정보 메서드 테스트 코드 작성

--- src/test/java/janggi/domain/board/BoardTest.java
+++ src/test/java/janggi/domain/board/BoardTest.java
@@ -8,12 +8,38 @@ import janggi.domain.Position;
 import janggi.domain.Side;
 import janggi.domain.piece.Piece;
 import janggi.domain.piece.PieceType;
+import janggi.domain.piece.Po;
 import janggi.initializer.BoardInitializer;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BoardTest {
+    private Board board = new Board(BoardInitializer.createBoard(Arrangement.MA_SANG_MA_SANG, Arrangement.MA_SANG_MA_SANG));
+
+    @Test
+    void 빈_칸인지_여부를_제대로_반영한다() {
+        assertThat(board.isEmpty(new Position(4,4))).isTrue();
+        assertThat(board.isEmpty(new Position(1,1))).isFalse();
+    }
+
+    @Test
+    void 해당_타입의_기물인지_여부를_제대로_반영한다() {
+        assertThat(board.isEqualPieceType(new Position(4,4), PieceType.NONE)).isTrue();
+        assertThat(board.isEqualPieceType(new Position(1,1), PieceType.CHA)).isTrue();
+
+        assertThat(board.isEqualPieceType(new Position(1,2), PieceType.SANG)).isFalse();
+        assertThat(board.isEqualPieceType(new Position(1,3), PieceType.MA)).isFalse();
+    }
+
+    @Test
+    void 해당_기물이_같은_진영인지_여부를_제대로_반영한다() {
+        assertThat(board.isAlly(Side.HAN, new Position(1,1))).isTrue();
+        assertThat(board.isAlly(Side.HAN, new Position(9,1))).isFalse();
+    }
+
     @Test
     void 자기_진영의_기물을_움직이면_정상_작동한다() {
         Board board = new Board(BoardInitializer.createBoard(Arrangement.MA_SANG_MA_SANG, Arrangement.MA_SANG_MA_SANG));
