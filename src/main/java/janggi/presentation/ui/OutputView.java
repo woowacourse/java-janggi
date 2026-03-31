a6b33cf04843bf5fbb92163a02271bf31465ef8f
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-03-31 22:08:05 +0900

    feat(OutputView): 점수 및 에러 메시지 출력 기능 구현

--- src/main/java/janggi/presentation/ui/OutputView.java
+++ src/main/java/janggi/presentation/ui/OutputView.java
@@ -20,7 +20,15 @@ public class OutputView {
                 .forEach(System.out::println);
     }
 
+    public static void printCurrentScore(double hanScore, double choScore) {
+        System.out.printf("한 : %.1f, 초 : %.1f\n", hanScore, choScore);
+    }
+
     public static void printStartGame() {
         System.out.println("장기 게임을 시작합니다. 초나라부터 시작하고, 왼쪽 아래를 (0,0)으로 합니다.");
+    }
+
+    public static void printError(String message) {
+        System.out.println(message);
     }
 }
