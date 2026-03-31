8e94c9a110f60d4b9ad834913c999b22d3971076
김가현 <hyeonnni@naver.com>
2026-03-31 22:07:58 +0900

    test: 궁 이동 경로 부정 테스트 제거

--- src/test/java/domain/activePiece/GeneralTest.java
+++ src/test/java/domain/activePiece/GeneralTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.game.Team;
 import domain.piece.ActivePiece;
-import domain.piece.General;
+import domain.piece.PalacePiece;
 import domain.piece.Piece;
 import domain.position.Column;
 import domain.position.Position;
@@ -16,37 +16,25 @@ import org.junit.jupiter.api.Test;
 class GeneralTest {
 
     @Test
-    void 정상_범위_입력() {
-        Piece general = new General(Team.HAN);
+    void 궁은_직선_범위로_이동_한다() {
+        Piece general = PalacePiece.general(Team.HAN);
         assertThat(general.canMove(new Position(5, 5), new Position(5, 6))).isTrue();
     }
 
     @Test
-    void 정상_범위가_아니면_거짓() {
-        Piece general = new General(Team.HAN);
+    void 궁_이동_범위가_직선이_아니면_거짓() {
+        Piece general = PalacePiece.general(Team.HAN);
         assertThat(general.canMove(new Position(5, 5), new Position(6, 6))).isFalse();
     }
 
     @Test
-    void 궁_정상_경로_출력_한다() {
-        ActivePiece general = new General(Team.HAN);
+    void 궁은_인접_이동_시_중간_경로_위치가_없다() {
+        ActivePiece general = PalacePiece.general(Team.HAN);
 
         Position src = new Position(new Row(1), new Column(3));
         Position dest = new Position(new Row(2), new Column(3));
         List<Position> routes = new ArrayList<>();
 
         assertThat(general.searchRoute(src, dest)).isEqualTo(routes);
-    }
-
-    @Test
-    void 궁_비정상_경로_출력_한다() {
-        ActivePiece general = new General(Team.HAN);
-
-        Position src = new Position(new Row(1), new Column(3));
-        Position dest = new Position(new Row(2), new Column(3));
-        List<Position> routes = new ArrayList<>(
-                List.of(new Position(new Row(3), new Column(3))));
-
-        assertThat(general.searchRoute(src, dest)).isNotEqualTo(routes);
     }
 }
