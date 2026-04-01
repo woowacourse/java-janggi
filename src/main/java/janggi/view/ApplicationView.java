3948d4b530c8ca700ccdb4df97ec13659e46cacd
kkb <gimgyubin1225@gmail.com>
2026-04-01 23:44:01 +0900

    refactor: 전략 옵션 출력시 StrategyViewResolver 에서 decisionNumber를 포함한 형식을 반환하도록 변경
    - ApplicationView 에서 decisionNumber를 직접 조회하고 조합하던 방식에서 변경

--- src/main/java/janggi/view/ApplicationView.java
+++ src/main/java/janggi/view/ApplicationView.java
@@ -28,7 +28,7 @@ public class ApplicationView {
         outputWriter.printPromptMessage(SideViewResolver.toDisplayName(side) + "팀의 초기화 전략 번호를 입력해주세요.");
 
         for (StrategyLabel strategy : strategies) {
-            String strategyDecisionOption = String.format("%d. %s", strategy.getDecisionNumber(), StrategyViewResolver.toDisplayName(strategy));
+            String strategyDecisionOption = StrategyViewResolver.toDisplayName(strategy);
             outputWriter.printPromptMessage(strategyDecisionOption);
         }
 
--- src/main/java/janggi/view/resolver/StrategyViewResolver.java
+++ src/main/java/janggi/view/resolver/StrategyViewResolver.java
@@ -8,11 +8,13 @@ public class StrategyViewResolver {
     }
 
     public static String toDisplayName(StrategyLabel strategy) {
-        return switch (strategy) {
+        String name = switch (strategy) {
             case HEEH -> "마상상마";
             case HEHE -> "마상마상";
             case EHEH -> "상마상마";
             case EHHE -> "상마마상";
         };
+
+        return String.format("%d.%s", strategy.getDecisionNumber(), name);
     }
 }
