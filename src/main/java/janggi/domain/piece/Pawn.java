492f158a866242d2430b02b5b48c5b5bed90ffa6
gleaming9 <gleaming9@khu.ac.kr>
2026-04-01 15:44:14 +0900

    refactor: Pawn이 StepPiece를 상속받도록 변경

--- src/main/java/janggi/domain/piece/Pawn.java
+++ src/main/java/janggi/domain/piece/Pawn.java
@@ -1,19 +1,15 @@
 package janggi.domain.piece;
 
 import janggi.domain.Movement;
-import janggi.domain.Position;
 import janggi.domain.Side;
 import janggi.domain.policy.ClearPathPolicy;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Pawn extends ActivePiece {
-    private final List<List<Movement>> MOVE_RANGE;
-
+public class Pawn extends StepPiece {
     public Pawn(Side side, List<List<Movement>> moveRange) {
-        super(new ClearPathPolicy(), side, PieceType.PAWN);
-        MOVE_RANGE = moveRange;
+        super(moveRange, new ClearPathPolicy(), side, PieceType.PAWN);
     }
 
     public static Pawn from(Side side) {
@@ -27,25 +23,5 @@ public class Pawn extends ActivePiece {
             return List.of(Movement.UP);
         }
         return List.of(Movement.DOWN);
-    }
-
-    @Override
-    public List<Position> findRoute(Position start, Position end) {
-        for (List<Movement> movements : MOVE_RANGE) {
-            List<Position> calculatedPath = calculatePath(start, movements);
-            if (calculatedPath.getLast().equals(end)) {
-                return calculatedPath;
-            }
-        }
-        throw new IllegalArgumentException(INVALID_DESTINATION_MESSAGE);
-    }
-
-    private List<Position> calculatePath(Position start, List<Movement> path) {
-        List<Position> calculatedPath = new ArrayList<>(List.of(start));
-        for (Movement movement : path) {
-            Position step = calculatedPath.getLast().move(movement);
-            calculatedPath.add(step);
-        }
-        return calculatedPath;
     }
 }
