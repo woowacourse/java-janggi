17ecfab0d53dd0f2b4dc9b6e765d7dddf08c4ec8
quddaz <quddnddl35@naver.com>
2026-04-01 10:04:57 +0900

    feat : 트랜잭션 커넥션 설정

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -32,7 +32,7 @@ public class GameService {
     }
 
     public void saveGame(Map<Position, Place> board, String name, Side side) {
-        try (Connection conn = H2ConnectionManager.getConnection()) {
+        try (Connection conn = connectionManager.getConnection()) {
             conn.setAutoCommit(false);
 
             try {
