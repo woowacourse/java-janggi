9081e7d5d88793e6791fce98fa8f753734762d2a
wontop02 <wontop@wontop-MacBookAir.local>
2026-04-01 13:25:42 +0900

    refactor: MoveOneSpacePiece 클래스명을 직관적으로 MoveOneStepPiece로 변경

--- src/main/java/domain/piece/Guard.java
+++ src/main/java/domain/piece/Guard.java
@@ -2,7 +2,7 @@ package domain.piece;
 
 import domain.Country;
 
-public class Guard extends MoveOneSpacePiece {
+public class Guard extends MoveOneStepPiece {
     public Guard(Country country) {
         super(new PieceInfo(PieceType.GUARD, country));
     }
