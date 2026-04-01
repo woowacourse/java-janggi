3e8cc8e4445e913b8b0f710e70c80f8f76aee8f0
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-01 15:58:02 +0900

    refactor(Piece): 추상 골격 클래스 분류

--- src/main/java/janggi/domain/piece/Piece.java
+++ src/main/java/janggi/domain/piece/Piece.java
@@ -5,45 +5,15 @@ import janggi.domain.point.Points;
 import janggi.domain.point.Route;
 import janggi.domain.status.Team;
 
-public abstract class Piece {
+public interface Piece {
+    boolean canMove(Route route);
+    boolean isSameTeam(Team team);
+    boolean isSameType(PieceType type);
+    Points getRoutePoints(Point from, Point to);
+    PieceType getType();
+    int getScore();
 
-    private final Team team;
-    private final PieceType type;
-    private final int score;
-
-    public Piece(int score, Team team, PieceType type) {
-        this.score = score;
-        this.team = team;
-        this.type = type;
-    }
-
-    public abstract Points getRoutePoints(Point from, Point to);
-
-    public boolean canCapture(Piece target) {
+    default boolean canCapture(Piece piece) {
         return true;
-    }
-
-    public boolean canMove(Route route) {
-        return route.isEmpty();
-    }
-
-    public boolean isSameTeam(Team team) {
-        return this.team.equals(team);
-    }
-
-    public boolean isSameType(PieceType type) {
-        return this.type.equals(type);
-    }
-
-    public PieceType getType() {
-        return type;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    protected Team getTeam() {
-        return this.team;
     }
 }
