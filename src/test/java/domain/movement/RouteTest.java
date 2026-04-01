b33909ae0353622734422134227941dad05ee3bd
Kim, Joon <kjoon418@naver.com>
2026-04-01 18:43:43 +0900

    test: Route에 대한 테스트 작성

---
+++ src/test/java/domain/movement/RouteTest.java
@@ -0,0 +1,197 @@
+package domain.movement;
+
+import static domain.util.AssertUtils.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.board.Intersection;
+import domain.game.Side;
+import domain.piece.AlivePieces;
+import domain.piece.Piece;
+import domain.piece.Soldier;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class RouteTest {
+
+    private static final Side SIDE = Side.HAN;
+    private static final Side OPPOSITE_SIDE = Side.CHO;
+    private static final Piece SAME_SIDE_PIECE = new Soldier(SIDE);
+    private static final Piece OPPOSITE_SIDE_PIECE = new Soldier(OPPOSITE_SIDE);
+
+    private static final String EMPTY_ROUTE_MESSAGE = "경로에는 하나 이상의 좌표 정보가 필요합니다.";
+
+    @Nested
+    class 본인의_상태를_검증한다 {
+
+        @Test
+        void 경로가_없다면_예외를_던진다() {
+            assertThatThrownBy(() -> new Route(List.of()))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(EMPTY_ROUTE_MESSAGE);
+        }
+
+        @Test
+        void 경로가_있다면_정상적으로_생성된다() {
+            Intersection intersection = new Intersection(5, 5);
+
+            assertThatNoException(() -> new Route(List.of(intersection)));
+        }
+    }
+
+    @Nested
+    class 목적지가_이용_가능한_상태인지를_판단한다 {
+
+        private final Intersection destination = new Intersection(5, 5);
+
+        @Test
+        void 목적지에_아군_기물이_있다면_이용할_수_없다() {
+            // given
+            Route route = new Route(List.of(destination));
+
+            AlivePieces alivePieces = new AlivePieces(Map.of(
+                    destination, SAME_SIDE_PIECE
+            ));
+
+            // when
+            boolean available = route.isDestinationAvailable(alivePieces, SIDE);
+
+            // then
+            assertThat(available).isFalse();
+        }
+
+        @Test
+        void 목적지에_상대_기물이_있다면_이용할_수_있다() {
+            // given
+            Route route = new Route(List.of(destination));
+
+            AlivePieces alivePieces = new AlivePieces(Map.of(
+                    destination, OPPOSITE_SIDE_PIECE
+            ));
+
+            // when
+            boolean available = route.isDestinationAvailable(alivePieces, SIDE);
+
+            // then
+            assertThat(available).isTrue();
+        }
+
+        @Test
+        void 목적지가_비어_있다면_이용할_수_있다() {
+            // given
+            Route route = new Route(List.of(destination));
+
+            AlivePieces emptyAlivePieces = new AlivePieces(Map.of());
+
+            // when
+            boolean available = route.isDestinationAvailable(emptyAlivePieces, SIDE);
+
+            // then
+            assertThat(available).isTrue();
+        }
+    }
+
+    @Nested
+    class 경로를_통해_목적지로_이동할_수_있는지를_판단한다 {
+
+        @Test
+        void 경로에_기물이_있다면_이동할_수_없다() {
+            // given
+            Intersection path = new Intersection(5, 6);
+            Intersection destination = new Intersection(5, 7);
+            Route route = new Route(List.of(path, destination));
+
+            Map<Intersection, Piece> pieces = Map.of(
+                    path, OPPOSITE_SIDE_PIECE
+            );
+            AlivePieces alivePieces = new AlivePieces(pieces);
+
+            // when
+            boolean canReach = route.canReachDestinationThroughPath(alivePieces, SIDE);
+
+            // then
+            assertThat(canReach).isFalse();
+        }
+
+        @Test
+        void 경로에_기물이_없다면_이동할_수_있다() {
+            // given
+            Intersection path = new Intersection(5, 6);
+            Intersection destination = new Intersection(5, 7);
+            Route route = new Route(List.of(path, destination));
+
+            AlivePieces emptyAlivePieces = new AlivePieces(Map.of());
+
+            // when
+            boolean canReach = route.canReachDestinationThroughPath(emptyAlivePieces, SIDE);
+
+            // then
+            assertThat(canReach).isTrue();
+        }
+
+        @Test
+        void 이용할_수_없는_목적지라면_이동할_수_없다() {
+            // given
+            Intersection path = new Intersection(5, 6);
+            Intersection destination = new Intersection(5, 7);
+            Route route = new Route(List.of(path, destination));
+
+            Map<Intersection, Piece> pieces = Map.of(
+                    destination, SAME_SIDE_PIECE
+            );
+            AlivePieces alivePieces = new AlivePieces(pieces);
+
+            // when
+            boolean canReach = route.canReachDestinationThroughPath(alivePieces, SIDE);
+
+            // then
+            assertThat(canReach).isFalse();
+        }
+    }
+
+    @Nested
+    class 목적지까지의_경로를_반환한다 {
+
+        @Test
+        void 목적지만_있다면_빈_컬렉션을_반환한다() {
+            // given
+            Intersection destination = new Intersection(3, 3);
+            Route route = new Route(List.of(destination));
+
+            // when
+            List<Intersection> path = route.getPath();
+
+            // then
+            assertThat(path).isEmpty();
+        }
+
+        @Test
+        void 경로가_있다면_경로에_대한_좌표_컬렉션을_반환한다() {
+            // given
+            Intersection firstNode = new Intersection(2, 2);
+            Intersection secondNode = new Intersection(2, 3);
+            Intersection destination = new Intersection(2, 4);
+
+            Route route = new Route(List.of(firstNode, secondNode, destination));
+
+            List<Intersection> path = route.getPath();
+
+            assertThat(path).containsExactly(firstNode, secondNode);
+        }
+    }
+
+    @Test
+    void 목적지를_반환한다() {
+        Intersection firstNode = new Intersection(4, 4);
+        Intersection secondNode = new Intersection(4, 5);
+        Intersection destination = new Intersection(4, 6);
+
+        Route route = new Route(List.of(firstNode, secondNode, destination));
+
+        Intersection actualDestination = route.getDestination();
+
+        assertThat(actualDestination).isEqualTo(destination);
+    }
+}
