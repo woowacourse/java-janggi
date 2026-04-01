a6789f05b76afdc39dc3b3adddd118cfe8a77752
LeeJaeHyeok97 <hazardous10@naver.com>
2026-04-01 17:19:58 +0900

    fix: 기물을 움직일 위치를 입력하다 오류 시 다시 그 위치부터 입력받도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -38,7 +38,7 @@ public class InputView {
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println();
-            return readPosition();
+            return readTargetPosition();
         }
     }
 
