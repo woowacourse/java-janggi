89b3f7abd0eb77f8ffc7bdfdc6f07d4ffc24e7b4
armd482 <armd482@naver.com>
2026-04-01 13:35:41 +0900

    feat: 궁성 기능 추가

--- src/main/java/janggi/domain/Position.java
+++ src/main/java/janggi/domain/Position.java
@@ -2,14 +2,27 @@ package janggi.domain;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class Position {
-    public static final int POSITION_COMPONENTS_SIZE = 2;
-
     public static final int BOARD_START_ROWS = 1;
     public static final int BOARD_START_COLS = 1;
     public static final int BOARD_END_ROWS = 10;
     public static final int BOARD_END_COLS = 9;
+
+    public static final int POSITION_COMPONENTS_SIZE = 2;
+
+    private static final Integer GUNG_SUNG_COL_START = 4;
+    private static final Integer GUNG_SUNG_COL_END = 6;
+    private static final Integer HAN_GUNG_SUNG_ROW_START = 1;
+    private static final Integer HAN_GUNG_SUNG_ROW_END = 3;
+    private static final Integer CHO_GUNG_SUNG_ROW_START = 8;
+    private static final Integer CHO_GUNG_SUNG_ROW_END = 10;
+
+    private static final Set<Position> GungSungM = Set.of(
+            new Position(1, 5), new Position(2, 4), new Position(2, 6), new Position(3,5),
+            new Position(8, 5), new Position(9, 4), new Position(9, 6), new Position(10,5)
+    );
 
     private static final String INVALID_POSITION_SIZE = "행과 열 두 개의 값만 입력하세요.";
     private static final String INVALID_ROW_RANGE = "유효하지 않은 위치입니다. 행은 1부터 10까지 가능합니다.";
@@ -101,9 +114,26 @@ public class Position {
     }
 
     private boolean isLinear(Position position) {
+        if(position.x == x && position.y == y) {
+            return false;
+        }
+
         int dx = Math.abs(position.x - x);
         int dy = Math.abs(position.y - y);
 
-        return (dx == 0 && dy != 0) || (dy == 0 && dx != 0);
+        return dx == 0 || dy == 0 || isGungSungDiagonal(position);
+    }
+
+    private boolean isGungSungDiagonal(Position position) {
+        return isGungSung(this) && isGungSung(position) && (!GungSungM.contains(this) && !GungSungM.contains(position));
+    }
+
+    private boolean isGungSung(Position position) {
+        if(position.y < GUNG_SUNG_COL_START || position.y > GUNG_SUNG_COL_END) {
+            return false;
+        }
+
+        return (position.x >= HAN_GUNG_SUNG_ROW_START && position.x <= HAN_GUNG_SUNG_ROW_END)
+                || (position.x >= CHO_GUNG_SUNG_ROW_START && position.x <= CHO_GUNG_SUNG_ROW_END);
     }
 }
