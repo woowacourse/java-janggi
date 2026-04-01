fb89db72394c615f4d7898026f3907e016fdd96a
Jinriro <riro12.hanyang.ac.kr>
2026-04-01 14:58:01 +0900

    refactor: DTO 레코드 변경

--- src/main/java/dto/BoardDto.java
+++ src/main/java/dto/BoardDto.java
@@ -5,15 +5,5 @@ import domain.piece.Piece;
 
 import java.util.Map;
 
-public class BoardDto {
-
-    private final Map<Position, Piece> board;
-
-    public BoardDto(Map<Position, Piece> board) {
-        this.board = board;
-    }
-
-    public Map<Position, Piece> getBoard() {
-        return board;
-    }
+public record BoardDto(Map<Position, Piece> board) {
 }
