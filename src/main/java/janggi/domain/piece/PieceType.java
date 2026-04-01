7b15c690aa12f23afaf0db974c3e50ed66e02bf6
dahye <dahye9011@naver.com>
2026-04-01 16:00:13 +0900

    refactor: 방향 결정 로직 Function 활용하여 변경

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -13,43 +13,35 @@ import janggi.domain.strategy.StepMoveStrategy;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public enum PieceType {
 
 //    TODO: 사이클 2 에서 궁성 구현 시
 //    PALACE(EnumSet.allOf(Direction.class), new StepMoveStrategy()),
 //    GUARD(EnumSet.allOf(Direction.class), new StepMoveStrategy()),
-    PALACE(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new StepMoveStrategy()),
-    GUARD(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new StepMoveStrategy()),
-    CHARIOT(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new SlideMoveStrategy()),
-    CANNON(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new CannonMoveStrategy()),
-    HORSE(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new HorseMoveStrategy()),
-    ELEPHANT(EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new ElephantMoveStrategy()),
-    SOLDIER(EnumSet.noneOf(Direction.class), new StepMoveStrategy()) {
-        @Override
-        public Paths calculatePaths(Position current, Side side) {
-            return getMoveStrategy().findMovablePaths(current, side.getSoldierDirections());
-        }
-    },
+    PALACE(side -> EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new StepMoveStrategy()),
+    GUARD(side -> EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new StepMoveStrategy()),
+    CHARIOT(side -> EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new SlideMoveStrategy()),
+    CANNON(side -> EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new CannonMoveStrategy()),
+    HORSE(side -> EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new HorseMoveStrategy()),
+    ELEPHANT(side -> EnumSet.of(Direction.N, Direction.S, Direction.E, Direction.W), new ElephantMoveStrategy()),
+    SOLDIER(Side::getSoldierDirections, new StepMoveStrategy()),
     ;
 
-    private final EnumSet<Direction> directions;
+    private final Function<Side, EnumSet<Direction>> directionProvider;
     private final MoveStrategy moveStrategy;
 
-    PieceType(EnumSet<Direction> directions, MoveStrategy moveStrategy) {
-        this.directions = directions;
+    PieceType(Function<Side, EnumSet<Direction>> directionProvider, MoveStrategy moveStrategy) {
         this.moveStrategy = moveStrategy;
+        this.directionProvider = directionProvider;
     }
 
     public Paths calculatePaths(Position current, Side side) {
-        return moveStrategy.findMovablePaths(current, directions);
+        return moveStrategy.findMovablePaths(current, directionProvider.apply(side));
     }
 
     public List<Position> determineDestinations(Paths paths, Map<Position, Piece> boardState, Piece movingPiece) {
         return moveStrategy.determineDestinations(paths, boardState, movingPiece);
-    }
-
-    protected MoveStrategy getMoveStrategy() {
-        return moveStrategy;
     }
 }
