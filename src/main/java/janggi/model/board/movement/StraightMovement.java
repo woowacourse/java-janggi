e4ddf7889dbfb04c8e33eb8bafa1c62aae7ca1f2
woohyeon <woohyeon6885@gmail.com>
2026-04-01 10:45:12 +0900

    refactor: 여러 칸수를 이동하는 규칙을 StraightMovement에서 분리

--- src/main/java/janggi/model/board/movement/StraightMovement.java
+++ src/main/java/janggi/model/board/movement/StraightMovement.java
@@ -1,13 +1,9 @@
 package janggi.model.board.movement;
 
-import janggi.model.board.position.Column;
 import janggi.model.board.position.Position;
 import janggi.model.board.PositionPath;
-import janggi.model.board.position.Row;
-import java.util.ArrayList;
-import java.util.List;
 
-public class StraightMovement implements Movement {
+public class StraightMovement extends MultiStepMovement {
 
     @Override
     public PositionPath move(Position from, Position to) {
@@ -20,53 +16,5 @@ public class StraightMovement implements Movement {
          }
 
         return moveVertically(from, to);
-    }
-
-    protected PositionPath moveHorizontally(Position from, Position to) {
-        List<Position> between = new ArrayList<>();
-
-        int fromValue = from.column().getValue();
-        int toValue = to.column().getValue();
-
-        int step = 1;
-        if (fromValue > toValue) {
-            step = -1;
-        }
-
-        int current = fromValue + step;
-
-        while (current != toValue) {
-            between.add(new Position(
-                    from.row(),
-                    Column.of(current++)
-            ));
-        }
-
-        return new PositionPath(between);
-    }
-
-    protected PositionPath moveVertically(Position from, Position to) {
-        List<Position> between = new ArrayList<>();
-
-        int fromValue = from.row().getValue();
-        int toValue = to.row().getValue();
-
-        int step = 1;
-        if (fromValue > toValue) {
-            step = -1;
-        }
-
-        int current = fromValue + step;
-
-        while (current != toValue) {
-            between.add(new Position(
-                    Row.of(current),
-                    from.column()
-            ));
-
-            current += step;
-        }
-
-        return new PositionPath(between);
     }
 }
