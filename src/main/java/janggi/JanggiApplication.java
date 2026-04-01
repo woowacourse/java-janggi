4927a1914c69d18ba0d7f380447759ada9b6adf9
MODUGGAGI <kiroro0814@naver.com>
2026-04-01 15:51:28 +0900

    refactor: 의존성 주입 전용 클래스 제거

--- src/main/java/janggi/JanggiApplication.java
+++ src/main/java/janggi/JanggiApplication.java
@@ -1,9 +1,12 @@
 package janggi;
 
+import janggi.view.InputView;
+import janggi.view.OutputView;
+import java.util.Scanner;
+
 public class JanggiApplication {
     public static void main(String[] args) {
-        AppConfig app = new AppConfig();
-        JanggiGame janggi = app.janggi();
+        JanggiGame janggi = new JanggiGame(new InputView(new Scanner(System.in)), new OutputView());
         janggi.run();
     }
 }
