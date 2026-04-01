5fcf7c36a060a160ab549cf71745ba495c17a23f
LeeJaeHyeok97 <hazardous10@naver.com>
2026-04-01 15:22:45 +0900

    test: 사의 이동 테스트 이동 좌표값 변경

--- src/test/java/domain/strategy/GuardMoveStrategyTest.java
+++ src/test/java/domain/strategy/GuardMoveStrategyTest.java
@@ -2,7 +2,12 @@ package domain.strategy;
 
 import domain.Board;
 import domain.BoardFactory;
+import domain.Piece;
+import domain.Team;
+import domain.Type;
 import domain.vo.Position;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,15 +33,18 @@ class GuardMoveStrategyTest {
     @DisplayName("사의 목적지에 같은 팀 기물이 있으면 이동하지 않는다.")
     void 사_목적지에_같은_팀_기물이_있으면_이동_불가() {
         // given
-        MoveStrategy strategy = new GuardMoveStrategy();
-        Board board = BoardFactory.setUp();
+        MoveStrategy guardMoveStrategy = new GuardMoveStrategy();
+        Map<Position, Piece> PositionPiecePair = new LinkedHashMap<>();
+        PositionPiecePair.put(Position.of(0, 3), Piece.of(Team.CHU, Type.GUARD, guardMoveStrategy));
+        PositionPiecePair.put(Position.of(0, 2), Piece.of(Team.CHU, Type.GUARD, guardMoveStrategy));
+        Board board = Board.of(PositionPiecePair);
 
         // when
         Position position = Position.of(0, 3);
         Position targetPosition = Position.of(0, 2);
 
         // then
-        Assertions.assertFalse(strategy.canMove(position, targetPosition, board));
+        Assertions.assertFalse(guardMoveStrategy.canMove(position, targetPosition, board));
     }
 
 
@@ -44,14 +52,18 @@ class GuardMoveStrategyTest {
     @DisplayName("사의 목적지에 다른 팀 기물이 있으면 이동한다.")
     void 사_목적지에_다른_팀_기물이_있으면_정상_이동() {
         // given
-        MoveStrategy strategy = new GuardMoveStrategy();
-        Board board = BoardFactory.setUp();
+        MoveStrategy guardMoveStrategy = new GuardMoveStrategy();
+        MoveStrategy soldierMoveStrategy = new SoldierMoveStrategy();
+        Map<Position, Piece> PositionPiecePair = new LinkedHashMap<>();
+        PositionPiecePair.put(Position.of(0, 3), Piece.of(Team.CHU, Type.GUARD, guardMoveStrategy));
+        PositionPiecePair.put(Position.of(1, 3), Piece.of(Team.HAN, Type.SOLDIER, soldierMoveStrategy));
+        Board board = Board.of(PositionPiecePair);
 
         // when
         Position position = Position.of(0, 3);
-        Position targetPosition = Position.of(6, 0);
+        Position targetPosition = Position.of(1, 3);
 
         // then
-        Assertions.assertTrue(strategy.canMove(position, targetPosition, board));
+        Assertions.assertTrue(guardMoveStrategy.canMove(position, targetPosition, board));
     }
 }
