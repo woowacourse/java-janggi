649c3dfe67a34ae352233d48de53934922466aab
woohyeon <woohyeon6885@gmail.com>
2026-04-01 11:53:13 +0900

    refactor: PositionTest에서 사용하지 않는 테스트 제거

--- src/test/java/janggi/model/board/position/PositionTest.java
+++ src/test/java/janggi/model/board/position/PositionTest.java
@@ -1,7 +1,6 @@
 package janggi.model.board.position;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,50 +41,4 @@ class PositionTest {
         assertThat(position.getDistanceTo(other))
                 .isEqualTo(3);
     }
-//
-//    @DisplayName("입력받은 거리만큼 수평 이동하는 경로를 반환한다.")
-//    @Test
-//    void moveHorizontal() {
-//        //given
-//        Position from = new Position(Row.SIX, Column.THREE);
-//
-//        //when & then
-//        MoveResult moveResult = from.moveHorizontal(3);
-//        assertThat(moveResult.getTo()).isEqualTo(new Position(Row.SIX, Column.SIX));
-//    }
-//
-//    @DisplayName("수평 이동 결과로 보드 밖으로 나가면 예외가 발생한다.")
-//    @Test
-//    void moveHorizontal_out() {
-//        //given
-//        Position from = new Position(Row.SIX, Column.THREE);
-//
-//        //when & then
-//        assertThatThrownBy(() -> from.moveHorizontal(9))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("보드 밖으로는 이동할 수 없습니다.");
-//    }
-//
-//    @DisplayName("다른 위치가 들어올때 수직 이동 경로를 반환한다.")
-//    @Test
-//    void moveVertical() {
-//        //given
-//        Position from = new Position(Row.SIX, Column.THREE);
-//
-//        //when & then
-//        MoveResult moveResult = from.moveVertical(3);
-//        assertThat(moveResult.getTo()).isEqualTo(new Position(Row.NINE, Column.THREE));
-//    }
-//
-//    @DisplayName("수직 이동 결과로 보드 밖으로 나가면 예외가 발생한다.")
-//    @Test
-//    void moveVertical_out() {
-//        //given
-//        Position from = new Position(Row.SIX, Column.THREE);
-//
-//        //when & then
-//        assertThatThrownBy(() -> from.moveVertical(10))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("보드 밖으로는 이동할 수 없습니다.");
-//    }
 }
