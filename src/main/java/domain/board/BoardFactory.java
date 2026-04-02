0d1452cea9536a67fecd6c1586c47afab4b070e1
JunHyung1206 <pjh120610@naver.com>
2026-04-02 13:56:21 +0900

    refactor(InitBoard): 기물 초기 위치 메서드 리팩토링
    - 각각의 기물마다, 기물의 위치 초기화 메서드 추출

--- src/main/java/domain/board/BoardFactory.java
+++ src/main/java/domain/board/BoardFactory.java
@@ -28,53 +28,81 @@ public class BoardFactory {
             PieceType.GENERAL, General::new
     );
 
+    private static final int GENERAL_COLUMN = 4;
     private static final List<Integer> SOLDIER_COLUMNS = List.of(0, 2, 4, 6, 8);
     private static final List<Integer> CHARIOT_COLUMNS = List.of(0, 8);
     private static final List<Integer> GUARD_COLUMNS = List.of(3, 5);
     private static final List<Integer> CANNON_COLUMNS = List.of(1, 7);
+    private static final List<Integer> CHO_DYNAMIC_COLUMNS = List.of(1, 2, 6, 7);
+    private static final List<Integer> HAN_DYNAMIC_COLUMNS = List.of(7, 6, 2, 1);
 
     private BoardFactory() {
     }
 
     public static Board createBoard(InitializeSetting choInitialSetting, InitializeSetting hanInitialSetting) {
         Map<Position, Piece> pieces = new HashMap<>();
-        List<PieceType> choSetting = choInitialSetting.getInitialSetting();
-        List<PieceType> hanSetting = hanInitialSetting.getInitialSetting();
 
-        for (Integer soliderColumns : SOLDIER_COLUMNS) {
-            pieces.put(new Position(soliderColumns, 3), createPiece(PieceType.SOLDIER, Team.CHO));
-            pieces.put(new Position(soliderColumns, 6), createPiece(PieceType.SOLDIER, Team.HAN));
-        }
-
-        for (Integer chariotColumn : CHARIOT_COLUMNS) {
-            pieces.put(new Position(chariotColumn, 0), createPiece(PieceType.CHARIOT, Team.CHO));
-            pieces.put(new Position(chariotColumn, 9), createPiece(PieceType.CHARIOT, Team.HAN));
-        }
-
-        for (Integer guardColumn : GUARD_COLUMNS) {
-            pieces.put(new Position(guardColumn, 0), createPiece(PieceType.GUARD, Team.CHO));
-            pieces.put(new Position(guardColumn, 9), createPiece(PieceType.GUARD, Team.HAN));
-        }
-
-        for (Integer cannonColumn : CANNON_COLUMNS) {
-            pieces.put(new Position(cannonColumn, 2), createPiece(PieceType.CANNON, Team.CHO));
-            pieces.put(new Position(cannonColumn, 7), createPiece(PieceType.CANNON, Team.HAN));
-        }
-
-        pieces.put(new Position(4, 1), new General(Team.CHO));
-        pieces.put(new Position(4, 8), new General(Team.HAN));
-
-        pieces.put(new Position(1, 0), createPiece(choSetting.get(0), Team.CHO));
-        pieces.put(new Position(2, 0), createPiece(choSetting.get(1), Team.CHO));
-        pieces.put(new Position(6, 0), createPiece(choSetting.get(2), Team.CHO));
-        pieces.put(new Position(7, 0), createPiece(choSetting.get(3), Team.CHO));
-
-        pieces.put(new Position(7, 9), createPiece(hanSetting.get(0), Team.HAN));
-        pieces.put(new Position(6, 9), createPiece(hanSetting.get(1), Team.HAN));
-        pieces.put(new Position(2, 9), createPiece(hanSetting.get(2), Team.HAN));
-        pieces.put(new Position(1, 9), createPiece(hanSetting.get(3), Team.HAN));
+        placeSoldiers(pieces);
+        placeChariots(pieces);
+        placeGuards(pieces);
+        placeCannons(pieces);
+        placeGeneral(pieces);
+        placeDynamicPieces(pieces, choInitialSetting, hanInitialSetting);
 
         return new Board(pieces);
+    }
+
+    private static void placeGeneral(Map<Position, Piece> pieces) {
+        int choGeneralRow = 1;
+        int hanGeneralRow = 8;
+        pieces.put(new Position(GENERAL_COLUMN, choGeneralRow), createPiece(PieceType.GENERAL, Team.CHO));
+        pieces.put(new Position(GENERAL_COLUMN, hanGeneralRow), createPiece(PieceType.GENERAL, Team.HAN)); // 🌟 꼬여있던 X, Y 버그 해결
+    }
+
+    private static void placeSoldiers(Map<Position, Piece> pieces) {
+        int choSoldiersRow = 3;
+        int hanSoldiersRow = 6;
+        for (Integer column : SOLDIER_COLUMNS) {
+            pieces.put(new Position(column, choSoldiersRow), createPiece(PieceType.SOLDIER, Team.CHO));
+            pieces.put(new Position(column, hanSoldiersRow), createPiece(PieceType.SOLDIER, Team.HAN));
+        }
+    }
+
+    private static void placeCannons(Map<Position, Piece> pieces) {
+        int choCannonsRow = 2;
+        int hanCannonsRow = 7;
+        for (Integer column : CANNON_COLUMNS) {
+            pieces.put(new Position(column, choCannonsRow), createPiece(PieceType.CANNON, Team.CHO));
+            pieces.put(new Position(column, hanCannonsRow), createPiece(PieceType.CANNON, Team.HAN));
+        }
+    }
+
+    private static void placeGuards(Map<Position, Piece> pieces) {
+        int choGuardsRow = 0;
+        int hanGuardsRow = 9;
+        for (Integer column : GUARD_COLUMNS) {
+            pieces.put(new Position(column, choGuardsRow), createPiece(PieceType.GUARD, Team.CHO));
+            pieces.put(new Position(column, hanGuardsRow), createPiece(PieceType.GUARD, Team.HAN));
+        }
+    }
+
+    private static void placeChariots(Map<Position, Piece> pieces) {
+        int choChariotsRow = 0;
+        int hanChariotsRow = 9;
+        for (Integer column : CHARIOT_COLUMNS) {
+            pieces.put(new Position(column, choChariotsRow), createPiece(PieceType.CHARIOT, Team.CHO));
+            pieces.put(new Position(column, hanChariotsRow), createPiece(PieceType.CHARIOT, Team.HAN));
+        }
+    }
+
+    private static void placeDynamicPieces(Map<Position, Piece> pieces, InitializeSetting choSetting, InitializeSetting hanSetting) {
+        List<PieceType> choTypes = choSetting.getInitialSetting();
+        List<PieceType> hanTypes = hanSetting.getInitialSetting();
+
+        for (int i = 0; i < 4; i++) {
+            pieces.put(new Position(CHO_DYNAMIC_COLUMNS.get(i), 0), createPiece(choTypes.get(i), Team.CHO));
+            pieces.put(new Position(HAN_DYNAMIC_COLUMNS.get(i), 9), createPiece(hanTypes.get(i), Team.HAN));
+        }
     }
 
     private static Piece createPiece(PieceType pieceType, Team team) {
