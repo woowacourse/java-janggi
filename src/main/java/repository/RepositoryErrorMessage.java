83892f681d85de9434554cb0ac2656c2b999c385
kdongsu5509 <kod66170@gmail.com>
2026-04-02 19:47:41 +0900

    feat : add missing changed file

--- src/main/java/repository/RepositoryErrorMessage.java
+++ src/main/java/repository/RepositoryErrorMessage.java
@@ -2,7 +2,8 @@ package repository;
 
 public enum RepositoryErrorMessage {
     CONFIG_FILE_NOT_FOUND("클래스패스에서 설정 파일을 찾을 수 없습니다"),
-    ERROR_ON_CONFIG_FILE_READING("DB 정보를 읽어오는데 실패하였습니다");
+    ERROR_ON_CONFIG_FILE_READING("DB 정보를 읽어오는데 실패하였습니다"),
+    BATCH_ERROR("배치 쿼리 실행 중 오류가 발생했습니다.");
 
     private final String message;
 
