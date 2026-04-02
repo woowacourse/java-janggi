3c4a09dfa4cffd59b149429492aba3cc18a4819f
poketopa <lhs5427ll@gmail.com>
2026-04-02 23:27:15 +0900

    refactor(domain): MoveRule의 생성자의 접근 지정자 변경

--- src/main/java/domain/piece/move/MoveRule.java
+++ src/main/java/domain/piece/move/MoveRule.java
@@ -10,7 +10,7 @@ public abstract class MoveRule {
     protected final PieceType pieceType;
     protected final Directions directions;
 
-    public MoveRule(PieceType pieceType, Directions directions) {
+    protected MoveRule(PieceType pieceType, Directions directions) {
         this.pieceType = pieceType;
         this.directions = directions;
     }
