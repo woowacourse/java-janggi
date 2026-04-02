6940dcd6d8b1c5e41a89ac53387cf362a2d6539c
smiinii <sm020320@naver.com>
2026-04-02 15:22:29 +0900

    refactor: OutputView의 체스판 출력 로직 리팩토링

--- src/main/java/janggi/view/OutputView.java
+++ src/main/java/janggi/view/OutputView.java
@@ -3,8 +3,10 @@ package janggi.view;
 import janggi.dto.BoardDto;
 import janggi.dto.PieceDto;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
@@ -19,6 +21,8 @@ public class OutputView {
     private static final String ANSI_RESET = "\u001B[0m";
 
     private static final String TEAM_HAN = "HAN";
+
+    private static final List<Integer> DIAGONAL_OFFSETS = List.of(2, 5, 8);
 
     public void printStartMessage() {
         System.out.println("게임을 시작하겠습니다.");
@@ -40,12 +44,12 @@ public class OutputView {
     }
 
     public void printBoard(BoardDto boardDto) {
-        String[][] boardView = generateBoardView(boardDto);
-
         printXCoordinates();
 
         for (int row = 0; row < BOARD_HEIGHT; row++) {
-            printNodeRow(boardView, row);
+            List<String> rowView = generateRowView(row, boardDto.getPieces());
+            printNodeRow(rowView, row);
+
             if (row < BOARD_HEIGHT - 1) {
                 printSpacerRows(row);
             }
@@ -53,26 +57,18 @@ public class OutputView {
         System.out.println();
     }
 
-    private String[][] generateBoardView(BoardDto boardDto) {
-        String[][] boardView = new String[BOARD_HEIGHT][BOARD_WIDTH];
+    private List<String> generateRowView(int row, List<PieceDto> allPieces) {
+        List<String> rowNodes = new ArrayList<>(Collections.nCopies(BOARD_WIDTH, EMPTY_NODE));
 
-        for (int row = 0; row < BOARD_HEIGHT; row++) {
-            Arrays.fill(boardView[row], EMPTY_NODE);
-        }
+        allPieces.stream()
+                .filter(piece -> piece.getRow() == row)
+                .forEach(piece -> {
+                    String colorCode = determineFactionColor(piece);
+                    String formattedPiece = String.format("[%s%s%s]", colorCode, piece.getName(), ANSI_RESET);
+                    rowNodes.set(piece.getColumn(), formattedPiece);
+                });
 
-        List<PieceDto> pieces = boardDto.getPieces();
-        for (PieceDto piece : pieces) {
-            int row = piece.getRow();
-            int col = piece.getColumn();
-
-            String colorCode = determineFactionColor(piece);
-            String pieceName = piece.getName();
-
-            String formattedPiece = String.format("[%s%s%s]", colorCode, pieceName, ANSI_RESET);
-            boardView[row][col] = formattedPiece;
-        }
-
-        return boardView;
+        return rowNodes;
     }
 
     private String determineFactionColor(PieceDto piece) {
@@ -92,49 +88,38 @@ public class OutputView {
         System.out.println();
     }
 
-    private void printNodeRow(String[][] boardView, int row) {
-        StringBuilder rowBuilder = new StringBuilder();
-        rowBuilder.append(row).append("  ");
-
-        for (int col = 0; col < BOARD_WIDTH; col++) {
-            rowBuilder.append(boardView[row][col]);
-            if (col < BOARD_WIDTH - 1) {
-                rowBuilder.append("-------");
-            }
-        }
-        System.out.println(rowBuilder.toString());
+    private void printNodeRow(List<String> rowView, int rowIndex) {
+        String joinedRow = String.join("-------", rowView);
+        System.out.println(rowIndex + "  " + joinedRow);
     }
 
     private void printSpacerRows(int row) {
-        int[] diagonalOffsets = {2, 5, 8};
-
         for (int spacerIndex = 0; spacerIndex < SPACER_LINES; spacerIndex++) {
-            char[] spacer = new char[BOARD_WIDTH * NODE_CHARS];
-            Arrays.fill(spacer, ' ');
+            List<String> spacerRow = new ArrayList<>(Collections.nCopies(BOARD_WIDTH * NODE_CHARS, " "));
 
             for (int col = 0; col < BOARD_WIDTH; col++) {
-                spacer[col * NODE_CHARS + 1] = 'ㅣ';
+                spacerRow.set(col * NODE_CHARS + 1, "ㅣ");
             }
 
-            int offset = diagonalOffsets[spacerIndex];
-            addPalaceDiagonals(spacer, row, offset);
+            int offset = DIAGONAL_OFFSETS.get(spacerIndex);
+            addPalaceDiagonals(spacerRow, row, offset);
 
             System.out.print("   ");
-            System.out.println(new String(spacer));
+            System.out.println(String.join("", spacerRow));
         }
     }
 
-    private void addPalaceDiagonals(char[] spacer, int row, int offset) {
+    private void addPalaceDiagonals(List<String> spacerRow, int row, int offset) {
         int x3Center = 3 * NODE_CHARS + 1;
         int x4Center = 4 * NODE_CHARS + 1;
         int x5Center = 5 * NODE_CHARS + 1;
 
         if (row == 0 || row == 7) {
-            spacer[x3Center + offset] = '\\';
-            spacer[x5Center - offset] = '/';
+            spacerRow.set(x3Center + offset, "\\");
+            spacerRow.set(x5Center - offset, "/");
         } else if (row == 1 || row == 8) {
-            spacer[x4Center - offset] = '/';
-            spacer[x4Center + offset] = '\\';
+            spacerRow.set(x4Center - offset, "/");
+            spacerRow.set(x4Center + offset, "\\");
         }
     }
 }
