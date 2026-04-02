c0f941b7ef62f9391af5272acfb32de3b8cb4e46
khcho96 <khcho1492@gmail.com>
2026-04-02 13:47:22 +0900

    test(InputParser): 패키지 변경

--- src/test/java/parser/InputParserTest.java
+++ src/test/java/parser/InputParserTest.java
@@ -1,4 +1,4 @@
-package domain;
+package parser;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import parser.InputParser;
 
 class InputParserTest {
 
