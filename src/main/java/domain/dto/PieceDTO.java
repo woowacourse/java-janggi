1131fdccef7b82f233788b505d5afe50df61bbc3
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-03 02:50:33 +0900

    refactor: 기물의 좌표를 가져오는 메서드 변경

--- src/main/java/domain/dto/PieceDTO.java
+++ src/main/java/domain/dto/PieceDTO.java
@@ -6,6 +6,6 @@ import domain.position.Position;
 
 public record PieceDTO(int row, int col, Team team) {
     public static PieceDTO from(Position position, Piece piece) {
-        return new PieceDTO(position.getRow(), position.getColumn(), piece.getTeam());
+        return new PieceDTO(position.row(), position.col(), piece.getTeam());
     }
 }
