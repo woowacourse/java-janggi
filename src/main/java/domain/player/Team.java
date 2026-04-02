7285c76e221cfdc2578949ba7daaef6e37b3b462
Jeshin Kim <jjason0904@cau.ac.kr>
2026-04-02 18:38:10 +0900

    feat(Team): 무승부 점수 계산 대비 Team의 한의 후수 추가점수 추가

--- src/main/java/domain/player/Team.java
+++ src/main/java/domain/player/Team.java
@@ -1,13 +1,15 @@
 package domain.player;
 
 public enum Team {
-    CHO(9),
-    HAN(0);
+    CHO(9, 0),
+    HAN(0, 1.5);
 
     private final int column;
+    private final double score;
 
-    Team(int column) {
+    Team(int column, double score) {
         this.column = column;
+        this.score = score;
     }
 
     public boolean isHan() {
@@ -20,5 +22,9 @@ public enum Team {
 
     public int getColumn() {
         return column;
+    }
+
+    public double getScore() {
+        return score;
     }
 }
