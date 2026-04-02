98153c0b202ed55595ee45181f2dd0c1659dc252
koreaioi <koreaioi@gachon.ac.kr>
2026-04-02 14:11:17 +0900

    refactor(domain): Directions를 record 타입으로 변경
    방어적 복사를 위해 생성자 단계에서 copyOf 사용

--- src/main/java/domain/move/directions/Directions.java
+++ src/main/java/domain/move/directions/Directions.java
@@ -6,15 +6,14 @@ import domain.point.Point;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.IntStream;
 
-public class Directions {
-
-    private final List<Direction> directions;
+public record Directions(
+        List<Direction> directions
+) {
 
     public Directions(List<Direction> directions) {
-        this.directions = directions;
+        this.directions = List.copyOf(directions);
     }
 
     public static Directions empty() {
@@ -44,15 +43,4 @@ public class Directions {
                 .orElseThrow(IllegalArgumentException::new);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Directions that = (Directions) o;
-        return Objects.equals(directions, that.directions);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(directions);
-    }
 }
