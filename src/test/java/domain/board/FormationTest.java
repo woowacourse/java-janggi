2a6d8845b95374b007981b31bb533a2d2cd51c90
khcho96 <khcho1492@gmail.com>
2026-04-02 17:26:04 +0900

    test(FormationTest): 불필요한 테스트 코드 삭제

--- src/test/java/domain/board/FormationTest.java
+++ src/test/java/domain/board/FormationTest.java
@@ -2,13 +2,6 @@ package domain.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import domain.Position;
-import domain.Side;
-import domain.piece.Elephant;
-import domain.piece.Horse;
-import domain.piece.Piece;
-import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import parser.Command;
 
@@ -20,17 +13,5 @@ class FormationTest {
         assertThat(Formation.from(Command.SECOND)).isEqualTo(Formation.RIGHT_ELEPHANT);
         assertThat(Formation.from(Command.THIRD)).isEqualTo(Formation.OUTER_ELEPHANT);
         assertThat(Formation.from(Command.FOURTH)).isEqualTo(Formation.INNER_ELEPHANT);
-    }
-
-    @Test
-    void LEFT_ELEPHANT은_상마상마로_배치한다() {
-        Map<Position, Piece> pieces = new HashMap<>();
-
-        Formation.LEFT_ELEPHANT.placeElephant(pieces, Side.CHO);
-
-        assertThat(pieces.get(Position.of(1, 0))).isInstanceOf(Elephant.class);
-        assertThat(pieces.get(Position.of(2, 0))).isInstanceOf(Horse.class);
-        assertThat(pieces.get(Position.of(6, 0))).isInstanceOf(Elephant.class);
-        assertThat(pieces.get(Position.of(7, 0))).isInstanceOf(Horse.class);
     }
 }
