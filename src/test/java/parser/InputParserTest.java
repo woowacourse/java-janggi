25b8dff4ca7881e77d0b74d3245967e2f2218b44
khcho96 <khcho1492@gmail.com>
2026-04-02 17:26:16 +0900

    test(InputParserTest): 테스트 코드 수정

--- src/test/java/parser/InputParserTest.java
+++ src/test/java/parser/InputParserTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 class InputParserTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"(0,3)", "(3,  9)"})
+    @ValueSource(strings = {"0,3", "3,  9", "  8 ,0 "})
     void 올바른_좌표_형식이_입력되는_경우_정상_동작한다(String input) {
         assertThatCode(() -> InputParser.parsePosition(input))
                 .doesNotThrowAnyException();
@@ -18,7 +18,7 @@ class InputParserTest {
 
     @ParameterizedTest
     @NullAndEmptySource
-    @ValueSource(strings = {" ", "0,3", "a,b", "(a,b)", "()", "(,)"})
+    @ValueSource(strings = {" ", "a,b", "(0,3)", "(a,b)", "()", "(,)", "1", "1,2,3"})
     void 위치_입력_포맷이_올바른_형태가_아니면_예외가_발생한다(String input) {
         assertThatThrownBy(() -> InputParser.parsePosition(input))
                 .isInstanceOf(IllegalArgumentException.class);
