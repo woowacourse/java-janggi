2382e2863bac38fea29a391786053986d6d5365a
Jinriro <riro12.hanyang.ac.kr>
2026-04-02 13:18:12 +0900

    refactor: Board 응답을 DTO로 분리하고 PieceDto, PositionDto 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,29 +1,18 @@
 package view;
 
-import domain.coordinate.Position;
 import domain.board.Side;
-import domain.piece.Piece;
 import domain.piece.PieceType;
 import dto.BoardDto;
 
+import dto.PieceDto;
+import dto.PositionDto;
+import view.message.PieceFormatter;
 import view.message.SideView;
-
-import java.util.Map;
 
 public class OutputView {
 
     private static final int COL_SIZE = 10;
     private static final int ROW_SIZE = 9;
-    private static final Map<PieceType, String> PIECE_TYPE = Map.of(
-            PieceType.PAWN, "卒",
-            PieceType.HORSE, "馬",
-            PieceType.ELEPHANT, "象",
-            PieceType.CHARIOT, "車",
-            PieceType.CANNON, "包",
-            PieceType.GUARD, "士",
-            PieceType.KING, "將",
-            PieceType.EMPTY, "ㅁ"
-    );
 
     public void printBoard(BoardDto boardDto) {
         System.out.println("\n   0  1   2  3   4   5  6   7  8");
@@ -31,26 +20,22 @@ public class OutputView {
         for (int i = 0; i < COL_SIZE; i++) {
             System.out.print(i + " ");
             for (int j = 0; j < ROW_SIZE; j++) {
-                printPieceBySide(boardDto.board().get(new Position(i, j)));
+                printPieceBySide(boardDto.board().get(new PositionDto(i, j)));
             }
 
             System.out.println();
         }
     }
 
-    private void printPieceBySide(Piece piece) {
-        Side side = piece.getSide();
+    private void printPieceBySide(PieceDto pieceDto) {
+        Side side = pieceDto.getSide();
 
-        if (piece.isNeutral()) {
-            System.out.print(" " + formatPiece(piece) + " ");
+        if (pieceDto.getPieceType() == PieceType.EMPTY) {
+            System.out.print(" " + PieceFormatter.from(pieceDto.getPieceType()) + " ");
             return;
         }
 
-        System.out.print(" " + SideView.getSideColor(side) + formatPiece(piece) + SideView.getResetColor() + " ");
-    }
-
-    private String formatPiece(Piece piece) {
-        return PIECE_TYPE.get(piece.getType());
+        System.out.print(" " + SideView.getSideColor(side) + PieceFormatter.from(pieceDto.getPieceType()) + SideView.getResetColor() + " ");
     }
 
     public static void printErrorMessage(String message) {
