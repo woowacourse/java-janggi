2783f1a26e7a8452523e8deccf9841aa97a09264
JYL35 <dlwodbs1223@naver.com>
2026-04-02 14:00:21 +0900

    feat(PieceDisplayName): 진영별 기물의 표시 이름 반환 기능 구현

---
+++ src/main/java/janggi/domain/piece/PieceDisplayName.java
@@ -0,0 +1,29 @@
+package janggi.domain.piece;
+
+import janggi.domain.Camp;
+
+public enum PieceDisplayName {
+    ADVISOR("士", "士"),
+    CANNON("包", "包"),
+    CHARIOT("車", "車"),
+    ELEPHANT("象", "象"),
+    GENERAL("楚", "漢"),
+    HORSE("馬", "馬"),
+    SOLIDER("卒", "兵");
+
+
+    private final String choName;
+    private final String hanName;
+
+    PieceDisplayName(String choName, String hanName) {
+        this.choName = choName;
+        this.hanName = hanName;
+    }
+
+    public String findDisplayName(Camp camp) {
+        if (camp.isCho()) {
+            return choName;
+        }
+        return hanName;
+    }
+}
