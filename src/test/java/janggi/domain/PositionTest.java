e1138ec5697fce1d763e786e554a2f42eb3e29aa
Jiihyun <cwlgusc@gmail.com>
2026-04-02 20:27:48 +0900

    refactor: Position format 테스트 추가

--- src/test/java/janggi/domain/PositionTest.java
+++ src/test/java/janggi/domain/PositionTest.java
@@ -3,16 +3,35 @@ package janggi.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import janggi.exception.ExceptionMessage;
+import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class PositionTest {
 
     private static final int MIN_POSITION_INDEX = 0;
     private static final int MAX_ROW_INDEX = 9;
     private static final int MAX_COLUMN_INDEX = 8;
+
+    static Stream<List<Integer>> createPositionFormat() {
+        return Stream.of(
+                List.of(),
+                List.of(1),
+                List.of(1, 2, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createPositionFormat")
+    void 포지션에_행과_열이_존재하지_않을_경우_예외가_발생한다(List<Integer> position) {
+        Assertions.assertThatThrownBy(() -> Position.from(position))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.INVALID_INPUT_FORMAT.getMessage());
+    }
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -24,6 +43,7 @@ public class PositionTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.ROW_OUT_OF_RANGE.getMessage(MIN_POSITION_INDEX, MAX_ROW_INDEX));
     }
+
 
     @ParameterizedTest
     @CsvSource(value = {
