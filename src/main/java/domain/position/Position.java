68c23b613cdf43a6646c628b4f62466d42444e20
Jeshin Kim <jjason0904@cau.ac.kr>
2026-04-02 12:44:49 +0900

    feat(Position): 궁성 판단 기능 추가

--- src/main/java/domain/position/Position.java
+++ src/main/java/domain/position/Position.java
@@ -8,6 +8,13 @@ import static common.Constants.MIN_ROW;
 import common.exception.JanggiException;
 
 public record Position(int row, int column) {
+    private static final int PALACE_MIN_COLUMN = 3;
+    private static final int PALACE_MAX_COLUMN = 5;
+    private static final int TOP_PALACE_MIN_ROW = 0;
+    private static final int TOP_PALACE_MAX_ROW = 2;
+    private static final int BOTTOM_PALACE_MIN_ROW = 7;
+    private static final int BOTTOM_PALACE_MAX_ROW = 9;
+
     public Position {
         validate(row, column);
     }
@@ -21,5 +28,25 @@ public record Position(int row, int column) {
                     "열값은 %s이상 %s이하여야 합니다. 입력값: %s".formatted(MIN_COLUMN, MAX_COLUMN, column)
             );
         }
+    }
+
+    public boolean isInPalace() {
+        return isInTopPalace() || isInBottomPalace();
+    }
+
+    private boolean isInTopPalace() {
+        return isInPalaceColumnRange() && isInPalaceRowRange(TOP_PALACE_MIN_ROW, TOP_PALACE_MAX_ROW);
+    }
+
+    private boolean isInBottomPalace() {
+        return isInPalaceColumnRange() && isInPalaceRowRange(BOTTOM_PALACE_MIN_ROW, BOTTOM_PALACE_MAX_ROW);
+    }
+
+    private boolean isInPalaceRowRange(int minRow, int maxRow) {
+        return row >= minRow && row <= maxRow;
+    }
+
+    private boolean isInPalaceColumnRange() {
+        return column >= PALACE_MIN_COLUMN && column <= PALACE_MAX_COLUMN;
     }
 }
--- src/test/java/domain/position/PositionTest.java
+++ src/test/java/domain/position/PositionTest.java
@@ -1,7 +1,9 @@
 package domain.position;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import common.exception.JanggiException;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -38,5 +40,35 @@ class PositionTest {
     })
     void Row가_0미만_9초과_Column이_0미만_8초과이면_예외가_발생한다(int row, int column) {
         assertThrows(JanggiException.class, () -> new Position(row, column));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,3",
+            "1,4",
+            "2,5",
+            "7,3",
+            "8,4",
+            "9,5"
+    })
+    void 궁성_좌표이면_true를_반환한다(int row, int column) {
+        Position position = new Position(row, column);
+
+        assertTrue(position.isInPalace());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,2",
+            "3,4",
+            "6,4",
+            "7,2",
+            "9,6",
+            "5,4"
+    })
+    void 궁성_좌표가_아니면_false를_반환한다(int row, int column) {
+        Position position = new Position(row, column);
+
+        assertFalse(position.isInPalace());
     }
 }
