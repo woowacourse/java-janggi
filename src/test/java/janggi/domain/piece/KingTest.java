c5a0d292a2d56cd118486ac54d41ab394c4315a8
yItWorks <poiu7825@pusan.ac.kr>
2026-04-02 11:32:59 +0900

    test: 왕 궁성 움직임 테스트 케이스 추가

--- src/test/java/janggi/domain/piece/KingTest.java
+++ src/test/java/janggi/domain/piece/KingTest.java
@@ -7,6 +7,7 @@ import janggi.domain.position.Position;
 import janggi.domain.space.Space;
 import janggi.domain.space.piece.King;
 import janggi.domain.space.piece.Piece;
+import janggi.domain.space.piece.Sa;
 import janggi.domain.space.piece.Sang;
 import janggi.domain.space.piece.Team;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,8 @@ class KingTest {
     void 궁_움직임_정상_처리_테스트() {
         Piece piece = new King(Team.CHO);
 
-        Position from = new Position(5, 1);
-        Position to = new Position(6, 1);
+        Position from = new Position(4, 1);
+        Position to = new Position(5, 1);
 
         assertDoesNotThrow(() -> piece.validateMove(from, to));
     }
@@ -44,5 +45,39 @@ class KingTest {
         assertThatIllegalArgumentException()
             .isThrownBy(() -> piece.validateArrival(space))
             .withMessage("이동하려는 위치에 같은 팀의 말이 존재합니다.");
+    }
+
+    @Test
+    void 궁_궁성_정상_이동_테스트() {
+        Piece piece = new King(Team.CHO);
+
+        Position from = new Position(3, 0);
+        Position to = new Position(4, 1);
+
+        assertDoesNotThrow(() -> piece.validateMove(from, to));
+    }
+
+    @Test
+    void 궁_궁성_예외_이동_테스트() {
+        Piece piece = new King(Team.CHO);
+
+        Position from = new Position(3, 0);
+        Position to = new Position(5, 1);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.validateMove(from, to))
+                .withMessage("해당 위치로 궁이 이동할 수 없습니다.");
+    }
+
+    @Test
+    void 궁_궁성_바깥_이동_예외_테스트() {
+        Piece piece = new King(Team.CHO);
+
+        Position from = new Position(3, 0);
+        Position to = new Position(2, 0);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> piece.validateMove(from, to))
+                .withMessage("해당 위치로 궁이 이동할 수 없습니다.");
     }
 }
