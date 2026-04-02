ecc300048f840fc67d89ef3741dcf063d0ff64ae
kangrae <kangrae0501@gmail.com>
2026-04-02 16:03:40 +0900

    Revert "refactor: piece를 레코드에서 class로 변경"
    This reverts commit d3e10cadf3d54c2285f6c39897db2a59c188ffcd.

--- src/main/java/janggi/domain/piece/Piece.java
+++ src/main/java/janggi/domain/piece/Piece.java
@@ -5,15 +5,10 @@ import janggi.domain.dynasty.Dynasty;
 import janggi.domain.position.Position;
 import java.util.List;
 
-public class Piece {
-
-    private final Dynasty dynasty;
-    private final PieceType pieceType;
-
-    public Piece(Dynasty dynasty, PieceType pieceType) {
-        this.dynasty = dynasty;
-        this.pieceType = pieceType;
-    }
+public record Piece(
+        Dynasty dynasty,
+        PieceType pieceType
+) {
 
     public List<Position> canMovePosition(BoardSnapshot board, Position from) {
         return pieceType.moveStrategy().findPlaceablePositions(board, from, dynasty);
@@ -27,11 +22,4 @@ public class Piece {
         return this.pieceType.equals(pieceType);
     }
 
-    public Dynasty dynasty() {
-        return dynasty;
-    }
-
-    public PieceType pieceType() {
-        return pieceType;
-    }
 }
