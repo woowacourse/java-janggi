ebbfc8f0cc476e411f54ecaf21be6df71f3c1d2c
softmoca <acyc0909@gmail.com>
2026-04-02 13:53:40 +0900

    test : 궁성 이동 규칙 실패 테스트 추가

--- src/test/java/janggi/domain/mouveRule/CannonMoveRuleTest.java
+++ src/test/java/janggi/domain/mouveRule/CannonMoveRuleTest.java
@@ -57,4 +57,57 @@ class CannonMoveRuleTest {
 
         assertThat(moveRule.canMove(new Position(0, 0), new Position(0, 6), board)).isFalse();
     }
+
+    // 궁성 대각선
+    @Test
+    void 궁성_대각선_2칸_이동시_중간에_기물_1개_넘으면_이동_가능() {
+        Board board = Board.of(Map.of(
+                new Position(1, 4), new Soldier(Team.HAN)
+        ));
+        assertThat(moveRule.canMove(
+                new Position(0, 3), new Position(2, 5), board)).isTrue();
+    }
+
+    @Test
+    void 초_궁성에서도_대각선_2칸_이동_가능() {
+        Board board = Board.of(Map.of(
+                new Position(8, 4), new Soldier(Team.CHO)
+        ));
+        assertThat(moveRule.canMove(
+                new Position(7, 3), new Position(9, 5), board)).isTrue();
+    }
+
+    @Test
+        // 아래 테스트들 PR
+    void 궁성_대각선_2칸_이동시_중간에_기물_없으면_이동_불가() {
+        assertThat(moveRule.canMove(
+                new Position(0, 3), new Position(2, 5), board)).isFalse();
+    }
+
+    @Test
+    void 궁성_대각선_2칸_이동시_다리가_포이면_이동_불가() {
+        Board board = Board.of(Map.of(
+                new Position(1, 4), new Cannon(Team.CHO)
+        ));
+        assertThat(moveRule.canMove(
+                new Position(0, 3), new Position(2, 5), board)).isFalse();
+    }
+
+    @Test
+    void 궁성_대각선_2칸_이동시_도착칸에_포가_있으면_이동_불가() {
+        Board board = Board.of(Map.of(
+                new Position(1, 4), new Soldier(Team.HAN),
+                new Position(2, 5), new Cannon(Team.CHO)
+        ));
+        assertThat(moveRule.canMove(
+                new Position(0, 3), new Position(2, 5), board)).isFalse();
+    }
+
+    @Test
+    void 궁성_대각선_1칸은_넘을_기물이_없으므로_이동_불가() {
+        assertThat(moveRule.canMove(
+                new Position(1, 4), new Position(0, 3), board)).isFalse();
+    }
+
+
 }
