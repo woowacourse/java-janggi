382fe5e65cdb9a84333f3b504dbe7a6c43fb7559
jyt6640 <jyt6640@gmail.com>
2026-04-02 16:08:28 +0900

    feat: 궁성 안에서 대각선으로 이동하는 기능 구현

--- src/main/java/janggi/domain/piece/Cha.java
+++ src/main/java/janggi/domain/piece/Cha.java
@@ -15,6 +15,13 @@ public class Cha extends BasePiece {
 
     @Override
     public List<Point> getRoute(Point from, Point to) {
+        if (from.isInSamePalace(to)) {
+            return getPalaceRoute(from, to);
+        }
+        return getNormalRoute(from, to);
+    }
+
+    private List<Point> getNormalRoute(Point from, Point to) {
         List<Point> route = new ArrayList<>();
         int pathX = to.getPathX(from);
         int pathY = to.getPathY(from);
@@ -30,6 +37,19 @@ public class Cha extends BasePiece {
             route.add(Point.of(nextX, nextY));
         }
         return route;
+    }
+
+    private List<Point> getPalaceRoute(Point from, Point to) {
+        int pathX = abs(to.getPathX(from));
+        int pathY = abs(to.getPathY(from));
+
+        if (pathX != 2 || pathY != 2) {
+            throw new IllegalArgumentException("궁성 경로가 아닙니다.");
+        }
+
+        int middleX = (from.getX() + to.getX()) / 2;
+        int middleY = (from.getY() + to.getY()) / 2;
+        return List.of((Point.of(middleX, middleY)));
     }
 
     private void validateDiagonalMove(int pathX, int pathY) {
