4c1353967734af6c893f86bfca759197dc438a0b
yItWorks <poiu7825@pusan.ac.kr>
2026-04-02 11:33:11 +0900

    feat: 왕 궁성 움직임 로직 추가

--- src/main/java/janggi/domain/space/piece/King.java
+++ src/main/java/janggi/domain/space/piece/King.java
@@ -11,10 +11,9 @@ public class King extends Piece {
 
     @Override
     public void validateMove(Position from, Position to) {
-        if (isMovable(from, to)) {
-            return;
+        if (!isMovable(from, to)) {
+            throw new IllegalArgumentException("해당 위치로 궁이 이동할 수 없습니다.");
         }
-        throw new IllegalArgumentException("해당 위치로 궁이 이동할 수 없습니다.");
     }
 
     @Override
@@ -23,10 +22,26 @@ public class King extends Piece {
     }
 
     private boolean isMovable(Position from, Position to) {
+        if (!isInsideCastle(from, to)) {
+            return false;
+        }
+
+        return isStraightMove(from, to) || isDigonalMove(from, to);
+    }
+
+    private boolean isStraightMove(Position from, Position to) {
         int dx = from.deltaX(to);
         int dy = from.deltaY(to);
 
         return (Math.abs(dx) == 0 && Math.abs(dy) == 1) ||
                 (Math.abs(dx) == 1 && Math.abs(dy) == 0);
+    }
+
+    private boolean isDigonalMove(Position from, Position to) {
+        return from.isDiagonalMoveInCastle(to);
+    }
+
+    private boolean isInsideCastle(Position from, Position to) {
+        return from.isInsideCastle() && to.isInsideCastle();
     }
 }
