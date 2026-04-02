faa43c43b467a53b5d8eb1b4877040e4c48fa89e
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-03 02:49:52 +0900

    fix: 불필요한 검증 메서드 제거

--- src/main/java/domain/strategy/Strategy.java
+++ src/main/java/domain/strategy/Strategy.java
@@ -9,10 +9,4 @@ import java.util.List;
 
 public interface Strategy {
     List<Position> getMoveCandidates(Position from, PieceProvider board);
-
-    private void validateBlank(Piece piece) {
-        if (piece instanceof Blank) {
-            throw new IllegalArgumentException("해당 위치에 기물이 존재하지 않습니다.");
-        }
-    }
 }
