de84fcc7513511ff7bdeb906ec759e0ed1bd6124
haechanmoon <moonhc.dev@gmail.com>
2026-04-02 17:15:03 +0900

    refactor(PieceType): 미사용 필드 제거

--- src/main/java/janggi/domain/PieceType.java
+++ src/main/java/janggi/domain/PieceType.java
@@ -7,39 +7,26 @@ import janggi.domain.moveRules.PoMoveRule;
 import janggi.domain.moveRules.SaMoveRule;
 import janggi.domain.moveRules.SangMoveRule;
 import janggi.domain.moveRules.ZolMoveRule;
-import java.util.List;
 
 public enum PieceType {
-    KING("왕", List.of(5), 9, new KingMoveRule()),
-    SA("사", List.of(4, 6), 10, new SaMoveRule()),
-    SANG("상", List.of(3, 7), 10, new SangMoveRule()),
-    MA("마", List.of(2, 8), 10, new MaMoveRule()),
-    CHA("차", List.of(1, 9), 10, new ChaMoveRule()),
-    PO("포", List.of(2, 8), 8, new PoMoveRule()),
-    ZOL("졸", List.of(1, 3, 5, 7, 9), 7, new ZolMoveRule());
+    KING("왕", new KingMoveRule()),
+    SA("사", new SaMoveRule()),
+    SANG("상", new SangMoveRule()),
+    MA("마", new MaMoveRule()),
+    CHA("차", new ChaMoveRule()),
+    PO("포", new PoMoveRule()),
+    ZOL("졸", new ZolMoveRule());
 
     private final String name;
-    private final List<Integer> xPositions;
-    private final int yPosition;
     private final MoveRule moveRule;
 
-    PieceType(String name, List<Integer> xPositions, int yPosition, MoveRule moveRule) {
+    PieceType(String name, MoveRule moveRule) {
         this.name = name;
-        this.xPositions = xPositions;
-        this.yPosition = yPosition;
         this.moveRule = moveRule;
     }
 
     public String getName() {
         return name;
-    }
-
-    public List<Integer> getXPositions() {
-        return xPositions;
-    }
-
-    public int getYPosition() {
-        return yPosition;
     }
 
     public MoveRule getMoveRule() {
