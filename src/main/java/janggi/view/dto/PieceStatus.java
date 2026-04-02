99141af2cdbfc0184995a55fd70410250161ab98
JYL35 <dlwodbs1223@naver.com>
2026-04-02 14:07:40 +0900

    feat(PieceStatus): 기물의 상태를 담는 DTO 구현

---
+++ src/main/java/janggi/view/dto/PieceStatus.java
@@ -0,0 +1,35 @@
+package janggi.view.dto;
+
+import janggi.domain.Camp;
+import janggi.domain.Position;
+
+public record PieceStatus(
+        PositionInfo positionInfo,
+        PieceInfo pieceInfo
+) {
+    public static PieceStatus from(Position position, Camp camp, String displayName) {
+        int row = position.getRow();
+        int col = position.getColumn();
+        return new PieceStatus(new PositionInfo(row, col), new PieceInfo(camp.isCho(), displayName));
+    }
+
+    private record PositionInfo(int row, int col) {}
+
+    private record PieceInfo(boolean isCho, String displayName) {}
+
+    public int getRow() {
+        return positionInfo.row();
+    }
+
+    public int getColumn() {
+        return positionInfo.col();
+    }
+
+    public boolean isCho() {
+        return pieceInfo.isCho();
+    }
+
+    public String getDisplayName() {
+        return pieceInfo.displayName();
+    }
+}
