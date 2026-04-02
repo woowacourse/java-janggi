9780a9a27043645b32e7aa9e28dca9b68c8ec3e1
yItWorks <poiu7825@pusan.ac.kr>
2026-04-02 11:06:54 +0900

    feat: 포 궁성 움직임 로직 추가

--- src/main/java/janggi/domain/space/piece/Pho.java
+++ src/main/java/janggi/domain/space/piece/Pho.java
@@ -76,10 +76,18 @@ public class Pho extends Piece {
     }
 
     private boolean isMovable(Position from, Position to) {
+        return isStraightMove(from, to) || isDigonalMove(from, to);
+    }
+
+    private boolean isStraightMove(Position from, Position to) {
         int dx = from.deltaX(to);
         int dy = from.deltaY(to);
 
         return (Math.abs(dx) == 0 && Math.abs(dy) > 1) ||
                 (Math.abs(dx) > 1 && Math.abs(dy) == 0);
+    }
+
+    private boolean isDigonalMove(Position from, Position to) {
+        return from.isDiagonalMoveInCastle(to);
     }
 }
