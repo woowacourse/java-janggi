7b85362279378b99937376c343abe607bd226531
jyt6640 <jyt6640@gmail.com>
2026-04-02 20:58:36 +0900

    feat: 기물 타입별 점수 추가

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -1,21 +1,27 @@
 package janggi.domain.piece;
 
 public enum PieceType {
-    CHA("차"),
-    PHO("포"),
-    MA("마"),
-    SANG("상"),
-    JANG("장"),
-    SA("사"),
-    JOL("졸");
+    CHA("차", 3),
+    PHO("포", 3),
+    MA("마", 3),
+    SANG("상", 3),
+    JANG("장", 5),
+    SA("사", 1),
+    JOL("졸", 1);
 
     private final String name;
+    private final int score;
 
-    PieceType(String name) {
+    PieceType(String name, int score) {
         this.name = name;
+        this.score = score;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
