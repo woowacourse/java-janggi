3f1f4bcac4214a2abafba2115e990efd2192510d
kdongsu5509 <kod66170@gmail.com>
2026-04-02 19:33:00 +0900

    feat:
    feat : add new batch feature
    - batch 기능 추가
    - 메서드 이름 excuteXXX 로 통일
    - read 반환값 추가

--- src/main/java/repository/jdbc/JdbcTemplate.java
+++ src/main/java/repository/jdbc/JdbcTemplate.java
@@ -4,34 +4,98 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
     private final JdbcConnectionGenerator jdbcConnectionGenerator;
+    private ResultSet resultSet;
 
     public JdbcTemplate(JdbcConnectionGenerator jdbcConnectionGenerator) {
         this.jdbcConnectionGenerator = jdbcConnectionGenerator;
     }
 
-    public ResultSet read(String sql, Object... parameters) throws SQLException {
+    public <T> List<T> executeRead(String sql, RowMapper<T> rowMapper, Object... parameters) throws SQLException {
         Connection connection = jdbcConnectionGenerator.getDBConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-        for (int i = 1; i <= parameters.length; i++) {
-            preparedStatement.setObject(i, parameters[i - 1]);
-        }
+        try {
+            for (int i = 1; i <= parameters.length; i++) {
+                preparedStatement.setObject(i, parameters[i - 1]);
+            }
 
-        return preparedStatement.executeQuery();
+            resultSet = preparedStatement.executeQuery();
+            List<T> results = new ArrayList<>();
+
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet));
+            }
+
+            return results;
+        } finally {
+            close(preparedStatement, connection, null);
+        }
     }
 
-    public int command(String sql, Object... parameters) throws SQLException {
+    public int executeCommand(String sql, Object... parameters) throws SQLException {
         Connection connection = jdbcConnectionGenerator.getDBConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-        for (int i = 1; i <= parameters.length; i++) {
-            preparedStatement.setObject(i, parameters[i - 1]);
+        try {
+            for (int i = 1; i <= parameters.length; i++) {
+                preparedStatement.setObject(i, parameters[i - 1]);
+            }
+
+            return preparedStatement.executeUpdate();
+        } finally {
+            close(preparedStatement, connection, null);
+        }
+    }
+
+    public void executeBatchCommand(String sql, List<List<Object>> parameterBatch) throws SQLException {
+        Connection connection = jdbcConnectionGenerator.getDBConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        try {
+            for (List<Object> params : parameterBatch) {
+                for (int i = 0; i < params.size(); i++) {
+                    preparedStatement.setObject(i + 1, params.get(i));
+                }
+                preparedStatement.addBatch();
+            }
+            preparedStatement.executeBatch();
+        } finally {
+            close(preparedStatement, connection, null);
+        }
+    }
+
+    private static void close(PreparedStatement stmt, Connection con, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                System.out.println("rs close error" + e);
+            }
         }
 
-        return preparedStatement.executeUpdate();
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                System.out.println("stmt close error" + e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println("con close error" + e);
+            }
+        }
+    }
+
+    public interface RowMapper<T> {
+        T mapRow(ResultSet rs) throws SQLException;
     }
 }
--- src/test/java/repository/jdbc/JdbcTemplateTest.java
+++ src/test/java/repository/jdbc/JdbcTemplateTest.java
@@ -1,6 +1,8 @@
 package repository.jdbc;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +12,12 @@ import org.junit.jupiter.api.Test;
 class JdbcTemplateTest {
 
     private static final String CONFIG_FILE_NAME = "database.properties";
+    private static final String CREATE_TEST_PIECE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS piece (" +
+            "piece_id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+            "piece_row INT NOT NULL, " +
+            "piece_col INT NOT NULL, " +
+            "team VARCHAR(10) NOT NULL, " +
+            "type VARCHAR(10) NOT NULL)";
 
     private JdbcConnectionGenerator generator;
     private JdbcTemplate template;
@@ -22,33 +30,45 @@ class JdbcTemplateTest {
 
     @AfterEach
     void clearAll() throws SQLException {
-        String sql = "DROP TABLE piece";
-        template.command(sql);
+        String sql = "DROP ALL OBJECTS";
+        template.executeCommand(sql);
     }
 
     @Test
     @DisplayName("read을 잘 수행한다")
-    void read_good() {
+    void executeRead_good() {
         //given
         String testSql = "SELECT *";
 
         Assertions.assertDoesNotThrow(
-                () -> template.read(testSql)
+                () -> template.executeRead(testSql, (resultSet) -> null)
         );
     }
 
     @Test
     @DisplayName("command 잘 수행한다")
-    void command_good() {
-        String sql = "CREATE TABLE IF NOT EXISTS piece (" +
-                "piece_id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
-                "piece_row INT NOT NULL, " +
-                "piece_col INT NOT NULL, " +
-                "team VARCHAR(10) NOT NULL, " +
-                "type VARCHAR(10) NOT NULL)";
+    void executeCommand_good() {
 
         Assertions.assertDoesNotThrow(
-                () -> template.command(sql)
+                () -> template.executeCommand(CREATE_TEST_PIECE_TABLE_SQL)
         );
+    }
+
+    @Test
+    @DisplayName("batch 잘 수행한다")
+    void executeBatch_good() throws SQLException {
+        int expectedResultSize = 3;
+        template.executeCommand(CREATE_TEST_PIECE_TABLE_SQL);
+
+        String sql = "INSERT INTO piece(piece_row, piece_col, team, type) VALUES (?, ?, ?, ?)";
+        List<List<Object>> tempPieces = new ArrayList<>();
+        tempPieces.add(List.of(1, 1, "CHO", "CHA"));
+        tempPieces.add(List.of(2, 1, "HAN", "JOL"));
+        tempPieces.add(List.of(3, 2, "CHO", "JANG"));
+
+        template.executeBatchCommand(sql, tempPieces);
+
+        int count = template.executeRead("SELECT COUNT(*) AS cnt FROM piece", rs -> rs.getInt("cnt")).get(0);
+        Assertions.assertEquals(expectedResultSize, count);
     }
 }
