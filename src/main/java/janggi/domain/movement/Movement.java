33b2605435a31c15a8549ff7e3f54ae68bcddebf
mvg01 <dkms625@naver.com>
2026-04-02 23:28:15 +0900

    test: CannonMoveRuleTest 테스트 케이스 추가

--- src/main/java/janggi/domain/movement/Movement.java
+++ src/main/java/janggi/domain/movement/Movement.java
@@ -79,10 +79,15 @@ public class Movement {
     // 경로에 장애물을 만나면 그때까지의 리스트를 반환하고, 적을 만난다면 적의 좌표를 포함하여 반환한다.
     public List<Position> calculateTraces(final Position from, final Piece piece, final BoardMediator boardMediator) {
         final List<Position> traces = new ArrayList<>();
+        Position prev = from;
         for (int distance = 1; distance <= maxDistance; distance++) {
             Position to = calculateNextPosition(from, distance);
+            if (prev.equals(to)) {
+                return traces;
+            }
             if (!hasPieceAt(to, boardMediator)) {
                 traces.add(to);
+                prev = to;
                 continue;
             }
             final Piece target = findPieceAt(to, boardMediator);
