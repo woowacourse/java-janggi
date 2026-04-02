604ee9a03cbade26c5e81d216dccbe3335900e92
choubung <choubung@sookmyung.ac.kr>
2026-04-02 09:26:27 +0900

    feat(game) : turn 전환 메서드 추가

--- src/main/java/janggi/domain/JanggiGame.java
+++ src/main/java/janggi/domain/JanggiGame.java
@@ -6,15 +6,20 @@ public class JanggiGame {
     public Team currentTurn = Team.CHO;
     private boolean isFinished = false;
 
-
     public boolean isFinished() {
         return isFinished;
     }
-
 
     public Team findCurrentTeam() {
         return currentTurn;
     }
 
+    public void changeTurn() {
+        if (currentTurn == Team.CHO) {
+            currentTurn = Team.HAN;
+            return;
+        }
 
+        currentTurn = Team.CHO;
+    }
 }
