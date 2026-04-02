a7b65010d5c34b8cb7fcb3292c28ffe270a50bb1
Huiyeongkim <huiyeong9619@naver.com>
2026-04-02 11:06:04 +0900

    refactor: 안 쓰는 코드 제거

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -84,27 +84,6 @@ public class InputView {
         }
     }
 
-    public Boolean readRetryCommand() {
-        System.out.println("계속 하시겠습니까?(y/n)");
-
-        String input = scanner.nextLine();
-        try {
-            validateRetryCommand(input);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println();
-            return readRetryCommand();
-        }
-
-        return input.equals("y");
-    }
-
-    private void validateRetryCommand(final String input) {
-        if (!input.equals("y") && !input.equals("n")) {
-            throw new IllegalArgumentException("[ERROR] y 또는 n만 입력 가능합니다.");
-        }
-    }
-
     private void validatePositionFormat(final String input) {
         if (!input.matches(POSITION_PATTERN)) {
             throw new IllegalArgumentException("[ERROR] 입력 형식이 잘못되었습니다. '숫자 공백 숫자' 형식이어야 합니다.");
