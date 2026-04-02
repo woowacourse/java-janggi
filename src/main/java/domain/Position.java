0cf6e2ebef5dd5d3cb31394ed1a56cc965ac967f
yeo-li <parkseongyeol2110@gmail.com>
2026-04-02 23:35:57 +0900

    refactor: Position CACHE key-value 구조 변경

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -9,7 +9,7 @@ public class Position {
     private static final String INVALID_POSITION_RANGE = String.format("x 좌표는 %d~%d, y 좌표는 %d~%d, 사이여야 합니다.",
         BoardSpec.MIN_X, BoardSpec.MAX_X, BoardSpec.MIN_Y, BoardSpec.MAX_Y);
 
-    private static final Map<Integer, Position> CACHE = new HashMap<>();
+    private static final Map<Integer, Map<Integer, Position>> CACHE = new HashMap<>();
     private final int x;
     private final int y;
 
@@ -20,7 +20,9 @@ public class Position {
 
     public static Position of(int x, int y) {
         validateOutOfRange(x, y);
-        return CACHE.computeIfAbsent(x * 100 + y, k -> new Position(x, y));
+        return CACHE
+            .computeIfAbsent(x, k -> new HashMap<>())
+            .computeIfAbsent(y, k -> new Position(x, y));
     }
 
     public boolean canMove(int dx, int dy) {
