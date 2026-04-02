c585398a5f410e2c19a1cd573cff3b32213f072b
JYL35 <dlwodbs1223@naver.com>
2026-04-02 14:00:09 +0900

    test(PieceDisplayName): 진영별 기물의 표시 이름 반환 테스트 작성

---
+++ src/test/java/janggi/domain/piece/PieceDisplayNameTest.java
@@ -0,0 +1,35 @@
+package janggi.domain.piece;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import janggi.domain.Camp;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PieceDisplayNameTest {
+
+    @ParameterizedTest
+    @DisplayName("진영(Camp)에 따라 기물의 올바른 표시 이름을 반환한다")
+    @CsvSource({
+            "ADVISOR, CHO, 士",
+            "ADVISOR, HAN, 士",
+            "CANNON, CHO, 包",
+            "CANNON, HAN, 包",
+            "CHARIOT, CHO, 車",
+            "CHARIOT, HAN, 車",
+            "ELEPHANT, CHO, 象",
+            "ELEPHANT, HAN, 象",
+            "GENERAL, CHO, 楚",
+            "GENERAL, HAN, 漢",
+            "HORSE, CHO, 馬",
+            "HORSE, HAN, 馬",
+            "SOLIDER, CHO, 卒",
+            "SOLIDER, HAN, 兵"
+    })
+    void findDisplayName_giveCamp_ReturnDisplayNameByCamp(PieceDisplayName pieceDisplayName, Camp camp, String expectedName) {
+        String displayName = pieceDisplayName.findDisplayName(camp);
+
+        assertThat(displayName).isEqualTo(expectedName);
+    }
+}
