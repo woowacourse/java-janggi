906399c52264431bbde88da0ee1800e03c5bb8db
frombunny <rucy011121@gmail.com>
2026-04-03 00:10:45 +0900

    test(PathGenerator): PathGenerator 누락 테스트 추가

---
+++ src/test/java/domain/path/PathGeneratorTest.java
@@ -0,0 +1,38 @@
+package domain.path;
+
+import domain.board.Position;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PathGeneratorTest {
+    @Test
+    void 직선_경로를_정상적으로_생성한다() {
+        Position departure = new Position(0, 0);
+        Position destination = new Position(0, 2);
+        Direction direction = Direction.UP;
+
+        List<Position> path = PathGenerator.generateStraightPath(departure, destination, direction);
+
+        assertThat(path).containsExactly(
+                new Position(0, 1),
+                new Position(0, 2)
+        );
+    }
+
+    @Test
+    void 복합_경로를_정상적으로_생성한다() {
+        Position departure = new Position(8, 0);
+        Position destination = new Position(7, 2);
+        List<Direction> directions = List.of(Direction.UP, Direction.NORTHWEST);
+
+        List<Position> path = PathGenerator.generateComplexPath(departure, destination, directions);
+
+        assertThat(path).containsExactly(
+                new Position(8, 1),
+                new Position(7, 2)
+        );
+    }
+}
