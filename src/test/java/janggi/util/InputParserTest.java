48ae7453e837496e02bee89ceb0710ad98d5a2c8
kangrae <kangrae0501@gmail.com>
2026-04-02 11:23:39 +0900

    test: parse 메서드 테스트 수정
    - 오류를 일으키지 않는다는 것만 판단하지 않고, 정확한 값을 반환하는지 테스트하도록 변경

--- src/test/java/janggi/util/InputParserTest.java
+++ src/test/java/janggi/util/InputParserTest.java
@@ -2,23 +2,27 @@ package janggi.util;
 
 import static janggi.util.InputParser.parseHorseElephantPositionOrdinal;
 import static janggi.util.InputParser.parsePosition;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputParserTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}은 올바른 마와 상의 상차림 법 입력이다.")
     @ValueSource(strings = {"1", "2", "3", "4"})
     public void 마와_상의_차림법을_1에서_4까지의_숫자로_입력받는다(String input) {
-        // when & then
-        assertThatCode(() -> parseHorseElephantPositionOrdinal(input))
-                .doesNotThrowAnyException();
+        // when
+        int ordinal = parseHorseElephantPositionOrdinal(input);
+
+        // then
+        assertThat(ordinal).isEqualTo(Integer.parseInt(input));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자가 아닌 {0}은 올바르지않은 마와 상의 상차림 법 입력이다.")
     @ValueSource(strings = {"a", " ", "-", "one"})
     public void 마와_상의_차림법이_숫자가_아니면_오류를_일으킨다(String input) {
         // when & then
@@ -27,7 +31,7 @@ class InputParserTest {
                 .hasMessageContaining("숫자로 입력해주세요.");
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "1부터 4의 정수가 아닌 {0}은 올바르지않은 마와 상의 상차림 법 입력이다.")
     @ValueSource(strings = {"-1", "0", "5"})
     public void 마와_상의_차림법이_1에서_4까지의_수가_아니면_오류를_일으킨다(String input) {
         // when & then
@@ -36,15 +40,21 @@ class InputParserTest {
                 .hasMessageContaining("1, 2, 3, 4 중 하나의 숫자를 입력해주세요.");
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}은 콤마로 구분되는 올바른 위치 정보이다.")
     @ValueSource(strings = {"1,1", "10,9", "1, 3", " 2, 4"})
     public void 위치_정보를_콤마로_구분된_두_숫자로_입력받는다(String input) {
-        // when & then
-        assertThatCode(() -> parsePosition(input))
-                .doesNotThrowAnyException();
+        // when
+        List<Integer> position = parsePosition(input);
+        List<Integer> expectPosition = Arrays.stream(input.split(","))
+                .map(str -> Integer.parseInt(str.strip()))
+                .toList();
+
+        // then
+        assertThat(position.size()).isEqualTo(2);
+        assertThat(position).containsAnyElementsOf(expectPosition);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "행과 열, 2개가 아닌 {0}은 올바르지않은 위치 정보이다.")
     @ValueSource(strings = {"1.3", "2'1", "31", "4 3"})
     public void 위치_정보가_콤마로_구분했을때_두개가_아니면_오류를_일으킨다(String input) {
         // when & then
@@ -53,7 +63,7 @@ class InputParserTest {
                 .hasMessageContaining("콤마로 구분된 두 개의 숫자를 올바르게 입력해주세요.");
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "숫자가 아닌 문자를 포함한 {0}은 올바르지않은 위치 정보이다.")
     @ValueSource(strings = {"a,3", "2, ", "3,q", "4,-"})
     public void 위치_정보를_콤마로_구분했을때_숫자가_아니면_오류를_일으킨다(String input) {
         // when & then
