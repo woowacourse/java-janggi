fe48f45322745b5a0fcdc42c35a3d0467224d19c
choubung <choubung@sookmyung.ac.kr>
2026-04-02 09:26:57 +0900

    view : 장기판 출력 뷰 생성 및 턴 출력 추가

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -1,14 +1,20 @@
 package janggi.view;
 
 import janggi.domain.dto.MoveCommand;
+import janggi.domain.piece.Team;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
-    public MoveCommand readMovePositions() {
-
+    public MoveCommand readMovePositions(Team team) {
+        String teamName = "";
+        if (team == Team.CHO) teamName = "초나라";
+        if (team == Team.HAN) teamName = "한나라";
+        
+        System.out.println(teamName + "의 턴입니다.");
         System.out.println("장기를 이동시킬 좌표들을 이동할행 이동할열 도착할행 도착할열 형태로 입력해 주세요.");
         System.out.println("ex 1 1 2 2");
 
--- src/main/java/janggi/view/OutputView.java
+++ src/main/java/janggi/view/OutputView.java
@@ -1,4 +1,62 @@
 package janggi.view;
 
+import janggi.domain.piece.Piece;
+import janggi.domain.piece.PieceType;
+import janggi.domain.piece.Team;
+import janggi.domain.vo.position.Position;
+
+import java.util.Map;
+
 public class OutputView {
+    private static final int ROW_SIZE = 10;
+    private static final int COL_SIZE = 9;
+
+    public void printIntroduce() {
+        System.out.println("장기 게임에 오신 걸 환영합니다.");
+    }
+
+    public void printBoard(Map<Position, Piece> pieces) {
+        printHeader();
+
+        for (int r = 0; r < ROW_SIZE; r++) {
+            System.out.print(r + " ");
+            for (int c = 0; c < COL_SIZE; c++) {
+                Piece piece = pieces.getOrDefault(new Position(r, c), null); // 혹은 EmptyPiece
+                System.out.print(formatCell(piece));
+            }
+            System.out.println();
+        }
+    }
+
+    private void printHeader() {
+        System.out.print("    ");
+
+        for (int i = 0; i < COL_SIZE; i++) {
+            System.out.print(i + "     ");
+        }
+        System.out.println("(Col)");
+    }
+
+    private String formatCell(Piece piece) {
+        if (piece == null || piece.isEmpty()) {
+            return "[ . ] ";
+        }
+        String team = piece.isSameTeam(Team.CHO) ? "초" : "한";
+        String symbol = getSymbol(piece.pieceType());
+
+        return String.format("[%s%s] ", team, symbol);
+    }
+
+    private String getSymbol(PieceType type) {
+        return switch (type) {
+            case KING -> "將";
+            case ADVISOR -> "士";
+            case TANK -> "車";
+            case HORSE -> "馬";
+            case ELEPHANT -> "象";
+            case CANNON -> "包";
+            case SOLDIER -> "卒";
+            default -> "？";
+        };
+    }
 }
