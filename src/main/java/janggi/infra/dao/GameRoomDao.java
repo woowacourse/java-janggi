8f1c77a5d5445fe714d73e294499ddcf9747d98e
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-03 15:07:05 +0900

    refactor(GameRoom): 예외처리 추가

--- src/main/java/janggi/infra/dao/GameRoomDao.java
+++ src/main/java/janggi/infra/dao/GameRoomDao.java
@@ -6,49 +6,58 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Optional;
 
 public class GameRoomDao {
 
-    public Long save(GameRoomData data, Connection connection) throws SQLException {
+    public Long save(GameRoomData data, Connection connection) {
         String sql = "INSERT INTO game_room (current_turn, winner, cha_score, han_score) VALUES (?, ?, ?, ?)";
-        PreparedStatement roomStatement = connection.prepareStatement(sql,
-                Statement.RETURN_GENERATED_KEYS);
-        roomStatement.setString(1, data.currentTurn());
-        roomStatement.setString(2, data.winner());
-        roomStatement.setDouble(3, data.choScore());
-        roomStatement.setDouble(4, data.hanScore());
-        roomStatement.executeUpdate();
-        ResultSet resultSet = roomStatement.getGeneratedKeys();
-        if (!resultSet.next()) {
-            throw new SQLException("[ERROR] 게임 보드 생성 실패");
+        try(PreparedStatement roomStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            roomStatement.setString(1, data.currentTurn());
+            roomStatement.setString(2, data.winner());
+            roomStatement.setDouble(3, data.choScore());
+            roomStatement.setDouble(4, data.hanScore());
+            roomStatement.executeUpdate();
+            ResultSet resultSet = roomStatement.getGeneratedKeys();
+            if (!resultSet.next()) {
+                throw new SQLException("[ERROR] 게임 보드 생성 실패");
+            }
+            return resultSet.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] 게임 방 데이터를 DB에 저장하는 중 오류가 발생했습니다.", e);
         }
-        return resultSet.getLong(1);
     }
 
-    public void update(Long roomId, GameRoomData data, Connection connection) throws SQLException {
+    public void update(Long roomId, GameRoomData data, Connection connection) {
         String sql = "UPDATE game_room SET current_turn = ?, winner = ?, cha_score = ?, han_score = ? WHERE id = ?";
-        PreparedStatement roomStatement = connection.prepareStatement(sql);
-        roomStatement.setString(1, data.currentTurn());
-        roomStatement.setString(2, data.winner());
-        roomStatement.setDouble(3, data.choScore());
-        roomStatement.setDouble(4, data.hanScore());
-        roomStatement.setLong(5, roomId);
-        roomStatement.executeUpdate();
+        try (PreparedStatement roomStatement = connection.prepareStatement(sql)) {
+            roomStatement.setString(1, data.currentTurn());
+            roomStatement.setString(2, data.winner());
+            roomStatement.setDouble(3, data.choScore());
+            roomStatement.setDouble(4, data.hanScore());
+            roomStatement.setLong(5, roomId);
+            roomStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] 기물 이동 위치를 DB에 저장하는 중 오류가 발생했습니다.", e);
+        }
     }
 
-    public GameRoomData findRoomById(Long roomId, Connection connection) throws SQLException {
+    public Optional<GameRoomData> findRoomById(Long roomId, Connection connection) {
         String sql = "SELECT current_turn, winner, cha_score, han_score FROM game_room WHERE id=?";
-        PreparedStatement roomStatement = connection.prepareStatement(sql);
-        roomStatement.setLong(1, roomId);
-        ResultSet resultSet = roomStatement.executeQuery();
-        if (resultSet.next()) {
-            return new GameRoomData(
-                    resultSet.getString("current_turn"),
-                    resultSet.getString("winner"),
-                    resultSet.getDouble("cha_score"),
-                    resultSet.getDouble("han_score")
-            );
+        try (PreparedStatement roomStatement = connection.prepareStatement(sql)) {
+            roomStatement.setLong(1, roomId);
+            ResultSet resultSet = roomStatement.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(new GameRoomData(
+                        resultSet.getString("current_turn"),
+                        resultSet.getString("winner"),
+                        resultSet.getDouble("cha_score"),
+                        resultSet.getDouble("han_score")
+                ));
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] 해당 방을 DB에서 조회하는 중 오류가 발생했습니다.", e);
         }
-        throw new IllegalArgumentException("[ERROR] 해당 방이 존재하지 않습니다.");
     }
 }
