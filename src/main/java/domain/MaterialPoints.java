f9b89fd269d6e071b0d92c794cd50a9d98fbdf12
Yoonhojoon <dbsghwns1209@gmail.com>
2026-04-03 15:13:22 +0900

    refactor(MaterialPoints): record로 수정

--- src/main/java/domain/MaterialPoints.java
+++ src/main/java/domain/MaterialPoints.java
@@ -1,12 +1,6 @@
 package domain;
 
-public final class MaterialPoints {
-
-    private final double value;
-
-    private MaterialPoints(double value) {
-        this.value = value;
-    }
+public record MaterialPoints(double value) {
 
     public static MaterialPoints zero() {
         return new MaterialPoints(0);
@@ -18,22 +12,5 @@ public final class MaterialPoints {
 
     public MaterialPoints plus(MaterialPoints other) {
         return new MaterialPoints(this.value + other.value);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        MaterialPoints that = (MaterialPoints) object;
-        return Double.compare(that.value, value) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Double.hashCode(value);
     }
 }
