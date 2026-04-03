c404444f3dc7cbf3b47f45365395971bb86ac990
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-03 09:22:07 +0900

    refactor(Repository): nullable하지 않은 메서드 수정

--- src/main/java/janggi/domain/board/BoardRepository.java
+++ src/main/java/janggi/domain/board/BoardRepository.java
@@ -4,7 +4,7 @@ import janggi.domain.JanggiGame;
 import janggi.domain.point.Point;
 
 public interface BoardRepository {
-    Long save(JanggiGame game);
+    long save(JanggiGame game);
     void update(Long roomId, Point from, Point to, JanggiGame game);
     JanggiGame loadGame(Long gameRoomId);
 }
