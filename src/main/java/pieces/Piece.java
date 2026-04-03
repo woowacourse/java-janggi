2722e2df456266c68947a46e1298861d23186268
JinGoo <zzgg3040@naver.com>
2026-04-04 08:37:55 +0900

    feat: piece 규칙과 점수는 type 에서 호출

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -2,8 +2,6 @@ package pieces;
 
 import java.util.List;
 import java.util.Objects;
-import movepolicy.move.Movement;
-import movepolicy.rule.MoveRule;
 import movepolicy.rule.MoveTrace;
 import participant.Score;
 import position.Position;
@@ -12,18 +10,10 @@ public class Piece {
 
     private final Side side;
     private final PieceType type;
-    private final MoveRule moveRule;
-    private final Movement movement;
-
-    private Piece(Side side, PieceType type, MoveRule moveRule, Movement movement) {
-        this.side = side;
-        this.type = type;
-        this.moveRule = moveRule;
-        this.movement = movement;
-    }
 
     public Piece(Side side, PieceType type) {
-        this(side, type, type.getMoveRule(), type.getMovement());
+        this.side = side;
+        this.type = type;
     }
 
     public final boolean isSameSide(Side side) {
@@ -35,17 +25,21 @@ public class Piece {
     }
 
     public final void validateDestination(Position departure, Position destination) {
-        if (!movement.canReach(departure, destination, side)) {
+        if (!type.getMovement().canReach(departure, destination, side)) {
             throw new IllegalArgumentException("행마법으로는 해당 위치로 이동할 수 없습니다.");
         }
     }
 
     public final List<Position> findPathPositions(Position departure, Position destination) {
-        return movement.findPathPositions(departure, destination, side);
+        return type.getMovement().findPathPositions(departure, destination, side);
     }
 
     public final void validate(MoveTrace moveTrace) {
-        moveRule.validate(moveTrace);
+        type.getMoveRule().validate(moveTrace);
+    }
+
+    public final PieceType getType() {
+        return type;
     }
 
     public final boolean isPo() {
@@ -53,11 +47,7 @@ public class Piece {
     }
 
     public final boolean isGung() {
-        return type == PieceType.GUNG;
-    }
-
-    public final PieceType getType() {
-        return type;
+        return type.isGung();
     }
 
     public Score getScore() {
