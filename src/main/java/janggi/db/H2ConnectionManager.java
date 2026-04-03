77fa7be06b64048f43deb48cc50f8f4362b4494f
MODUGGAGI <kiroro0814@naver.com>
2026-04-03 09:09:42 +0900

    feat: DB 연결 및 초기 설정 추가

---
+++ src/main/java/janggi/db/H2ConnectionManager.java
@@ -0,0 +1,25 @@
+package janggi.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class H2ConnectionManager {
+
+    private static final String URL = "jdbc:h2:file:./data/janggi";
+    private static final String USER = "stark";
+    private static final String PASSWORD = "stark123!";
+
+    private static final String UNABLE_TO_ACCESS_DATABASE = "[ERROR] H2 데이터베이스에 연결할 수 없습니다.";
+
+    private H2ConnectionManager() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new IllegalStateException(UNABLE_TO_ACCESS_DATABASE);
+        }
+    }
+}
