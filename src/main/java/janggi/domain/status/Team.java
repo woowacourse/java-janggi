db8cf19befb4da413b60b9ea9746dc81b4c06c3e
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-03 15:46:11 +0900

    refactor(Team): 방향 판단 team에 위임

--- src/main/java/janggi/domain/status/Team.java
+++ src/main/java/janggi/domain/status/Team.java
@@ -1,6 +1,16 @@
 package janggi.domain.status;
 
 public enum Team {
-    CHO,
-    HAN;
+    CHO {
+        public boolean isBackward(int signRow) {
+            return signRow < 0;
+        }
+    },
+    HAN {
+        public boolean isBackward(int signRow) {
+            return signRow > 0;
+        }
+    };
+
+    public abstract boolean isBackward(int signRow);
 }
