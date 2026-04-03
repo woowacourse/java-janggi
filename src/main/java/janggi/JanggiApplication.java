23d2c4f5293cfc0263aa78bfffad4cd79dd57772
woohyeon <woohyeon6885@gmail.com>
2026-04-03 15:06:37 +0900

    feat: h2 db 커넥션 생성 기능 구현

--- src/main/java/janggi/JanggiApplication.java
+++ src/main/java/janggi/JanggiApplication.java
@@ -8,7 +8,6 @@ import janggi.view.OutputView;
 public class JanggiApplication {
 
     public static void main(String[] args) {
-
         JanggiController controller = new JanggiController(
                 new OutputView(),
                 new InputView(),
