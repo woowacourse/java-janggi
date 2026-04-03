071422eda232db1740cbe0d25ecc2c449a232fd3
haeyoon1 <hynam1220@naver.com>
2026-04-03 17:44:34 +0900

    feat: 출력 방식 수정

--- src/main/java/janggi/view/OutputView.java
+++ src/main/java/janggi/view/OutputView.java
@@ -7,12 +7,23 @@ import janggi.domain.Space;
 public class OutputView {
 
     public static void printBoard(Board board) {
+        printHeader(board);
         for (int y = 0; y < board.getBoardWidth(); y++) {
             printRow(board, y);
         }
+        System.out.println();
+    }
+
+    private static void printHeader(Board board) {
+        System.out.print("   ");
+        for (int x = 0; x < board.getBoardHeight(); x++) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
     }
 
     private static void printRow(Board board, int y) {
+        System.out.print(y + ": ");
         for (int x = 0; x < board.getBoardHeight(); x++) {
             Space space = board.getSpace(new Position(x, y));
             System.out.print(formatSpace(space) + " ");
