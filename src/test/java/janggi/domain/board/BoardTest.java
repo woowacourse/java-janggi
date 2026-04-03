40e5f49cba6405721090f60e9ea4db77c7d87a89
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-03 15:45:44 +0900

    test(HanScore): 한나라 보너스 점수 계산 책임 위임에 따른 테스트 수정

--- src/test/java/janggi/domain/board/BoardTest.java
+++ src/test/java/janggi/domain/board/BoardTest.java
@@ -89,16 +89,6 @@ class BoardTest {
     }
 
     @Test
-    @DisplayName("한나라의 기물이 차와 포만 있으면 총합은 1.5점 가산점을 더해서 21.5이다.")
-    void han_total_score() {
-        Map<Point, Piece> pieces = new LinkedHashMap<>();
-        pieces.put(Point.of(0, 0), new Cha(Team.HAN));
-        pieces.put(Point.of(1, 1), new Pho(Team.HAN));
-        board = new Board(pieces);
-        Assertions.assertThat(board.calculateScore(Team.HAN)).isEqualTo(21.5);
-    }
-
-    @Test
     @DisplayName("초나라의 기물이 차와 포만 있으면 총합은 20이다.")
     void cho_total_score() {
         Map<Point, Piece> pieces = new LinkedHashMap<>();
