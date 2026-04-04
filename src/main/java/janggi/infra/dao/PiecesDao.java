47f689c5a1ce92f8dfc6c7d0207cc16816183fbd
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-04 09:05:00 +0900

    refactor(DAO): 에외처리 수정

--- src/main/java/janggi/infra/dao/PiecesDao.java
+++ src/main/java/janggi/infra/dao/PiecesDao.java
@@ -10,43 +10,55 @@ import java.util.List;
 
 public class PiecesDao {
 
-    public void save(Long roomId, List<PieceData> data, Connection connection) throws SQLException {
+    public void save(Long roomId, List<PieceData> data, Connection connection) {
         String sql = "INSERT INTO piece (game_room_id, piece_name, team, row_pos, col_pos) VALUES (?, ?, ?, ?, ?)";
-        PreparedStatement pieceStatement = connection.prepareStatement(sql);
-        for (PieceData piece : data) {
-            pieceStatement.setLong(1, roomId);
-            pieceStatement.setString(2, piece.pieceName());
-            pieceStatement.setString(3, piece.teamName());
-            pieceStatement.setInt(4, piece.row());
-            pieceStatement.setInt(5, piece.column());
-            pieceStatement.executeUpdate();
+        try {
+            PreparedStatement pieceStatement = connection.prepareStatement(sql);
+            for (PieceData piece : data) {
+                pieceStatement.setLong(1, roomId);
+                pieceStatement.setString(2, piece.pieceName());
+                pieceStatement.setString(3, piece.teamName());
+                pieceStatement.setInt(4, piece.row());
+                pieceStatement.setInt(5, piece.column());
+                pieceStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] 기물을 저장하던 중 오류가 발생했습니다.", e);
         }
     }
 
-    public void delete(Long roomId, int row, int col, Connection connection) throws SQLException {
+    public void delete(Long roomId, int row, int col, Connection connection) {
         String sql = "DELETE FROM piece WHERE game_room_id = ? AND row_pos = ? AND col_pos = ?";
-        PreparedStatement killStatement = connection.prepareStatement(sql);
-        killStatement.setLong(1, roomId);
-        killStatement.setInt(2, row);
-        killStatement.setInt(3, col);
-        killStatement.executeUpdate();
+        try {
+            PreparedStatement killStatement = connection.prepareStatement(sql);
+            killStatement.setLong(1, roomId);
+            killStatement.setInt(2, row);
+            killStatement.setInt(3, col);
+            killStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] 잡힌 기물을 제거하던 중 오류가 발생했습니다.", e);
+        }
     }
 
-    public void update(Long roomId, int fromRow, int fromCol, int toRow, int toCol, Connection connection) throws SQLException {
+    public void update(Long roomId, int fromRow, int fromCol, int toRow, int toCol, Connection connection) {
         String movePieceSql = "UPDATE piece SET row_pos = ?, col_pos = ? WHERE game_room_id = ? AND row_pos = ? AND col_pos = ?";
-        PreparedStatement moveStatement = connection.prepareStatement(movePieceSql);
-        moveStatement.setInt(1, toRow);
-        moveStatement.setInt(2, toCol);
-        moveStatement.setLong(3, roomId);
-        moveStatement.setInt(4, fromRow);
-        moveStatement.setInt(5, fromCol);
-        moveStatement.executeUpdate();
+        try {
+            PreparedStatement moveStatement = connection.prepareStatement(movePieceSql);
+            moveStatement.setInt(1, toRow);
+            moveStatement.setInt(2, toCol);
+            moveStatement.setLong(3, roomId);
+            moveStatement.setInt(4, fromRow);
+            moveStatement.setInt(5, fromCol);
+            moveStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] 기물을 이동하던 종 오류가 발생했습니다.", e);
+        }
     }
 
     public List<PieceData> findAllByRoomId(Long roomId, Connection connection) {
         String sql = "SELECT piece_name, team, row_pos, col_pos FROM piece WHERE game_room_id = ?";
-        List<PieceData> piecesData = new ArrayList<>();
         try {
+            List<PieceData> piecesData = new ArrayList<>();
             PreparedStatement pieceStatement = connection.prepareStatement(sql);
             pieceStatement.setLong(1, roomId);
             ResultSet resultSet = pieceStatement.executeQuery();
@@ -60,7 +72,7 @@ public class PiecesDao {
             }
             return piecesData;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("[ERROR] 해당 방의 기물을 DB에서 조회하는 중 오류가 발생했습니다.", e);
         }
     }
 }
