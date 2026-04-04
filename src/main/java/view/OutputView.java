4ce9755cd7cd2eb3107dbb641e5922989281a561
picetea44 <picetea44@gmail.com>
2026-04-04 17:47:57 +0900

    refactor: OutputView 매직넘버 상수화

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,7 +6,9 @@ import domain.piece.PieceAppearance;
 import domain.position.Position;
 
 public class OutputView {
+    private static final int MIN_ROW = 1;
     private static final int MAX_ROW = 10;
+    private static final int MIN_COLUMN = 1;
     private static final int MAX_COLUMN = 9;
     private final PieceAppearance appearance;
 
@@ -16,7 +18,7 @@ public class OutputView {
 
     public void printBoard(Board board) {
         StringBuilder sb = new StringBuilder();
-        for (int row = MAX_ROW; row >= 1; row--) {
+        for (int row = MAX_ROW; row >= MIN_ROW; row--) {
             sb.append(row).append("\t");
             appendRow(sb, board, row);
             sb.append(System.lineSeparator());
@@ -31,14 +33,14 @@ public class OutputView {
 
     private void appendColumnHeader(StringBuilder sb) {
         sb.append(" \t");
-        for (int column = 1; column <= MAX_COLUMN; column++) {
+        for (int column = MIN_COLUMN; column <= MAX_COLUMN; column++) {
             sb.append(column).append("\t");
         }
         sb.append(System.lineSeparator());
     }
 
     private void appendRow(StringBuilder sb, Board board, int row) {
-        for (int column = 1; column <= MAX_COLUMN; column++) {
+        for (int column = MIN_COLUMN; column <= MAX_COLUMN; column++) {
             Piece piece = board.pieceAt(new Position(row, column));
             sb.append(piece.display(appearance)).append("\t");
         }
