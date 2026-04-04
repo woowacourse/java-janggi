f6c3a00fed3d9cdad4fdbd52422bf9969fc9d46d
JaeheonLee <jaeheon0826@jbnu.ac.kr>
2026-04-04 11:39:39 +0900

    feat: 궁 및 사 기물의 궁성 내 이동 가능 여부 확인
    Signed-off-by: JaeheonLee <jaeheon0826@jbnu.ac.kr>

--- src/test/java/domain/piece/strategy/GeneralAndGuardStrategyTest.java
+++ src/test/java/domain/piece/strategy/GeneralAndGuardStrategyTest.java
@@ -1,20 +1,19 @@
 package domain.piece.strategy;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
 import domain.board.Board;
 import domain.board.BoardChecker;
 import domain.board.Position;
 import domain.piece.Camp;
 import domain.piece.Piece;
 import domain.piece.PieceType;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class GeneralAndGuardStrategyTest {
 
@@ -43,7 +42,7 @@ public class GeneralAndGuardStrategyTest {
 
         assertThatThrownBy(() -> generalAndGuard.move(from, to, boardChecker))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 한 칸만 움직일 수 있습니다.");
+                .hasMessage("[ERROR] 장군/사는 한 칸 직선 또는 궁 내부 대각선 이동만 가능합니다.");
     }
 
     @Test
@@ -56,11 +55,11 @@ public class GeneralAndGuardStrategyTest {
 
         assertThatThrownBy(() -> generalAndGuard.move(from, to, boardChecker))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 한 칸만 움직일 수 있습니다.");
+                .hasMessage("[ERROR] 장군/사는 한 칸 직선 또는 궁 내부 대각선 이동만 가능합니다.");
     }
 
     @Test
-    @DisplayName("대각선으로 이동하는 경우 예외가 발생한다.")
+    @DisplayName("궁성 외 영역에서 대각선으로 이동하는 경우 예외가 발생한다.")
     void throwException_When_Diagonal_Forward() {
         Position from = new Position(1, 1);
         Position to = new Position(2, 2);
@@ -69,12 +68,12 @@ public class GeneralAndGuardStrategyTest {
 
         assertThatThrownBy(() -> generalAndGuard.move(from, to, boardChecker))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 한 칸만 움직일 수 있습니다.");
+                .hasMessage("[ERROR] 장군/사는 한 칸 직선 또는 궁 내부 대각선 이동만 가능합니다.");
     }
 
     @Test
     @DisplayName("경로상의 기물 리스트가 비어있는 경우 정상 이동한다.")
-    void moveSuccessfully_When_PiecesIsEmpty() {
+    void moveSuccess_When_PiecesIsEmpty() {
         Position from = new Position(1, 1);
         Position to = new Position(1, 2);
 
@@ -82,5 +81,37 @@ public class GeneralAndGuardStrategyTest {
 
         assertThatCode(() -> generalAndGuard.move(from, to, boardChecker))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("장군과 사는 궁성 내에서 대각선 한 칸 이동할 수 있다.")
+    void moveSuccess_When_DiagonalInsidePalace() {
+        Position from = new Position(4, 1);
+        Position to = new Position(5, 2);
+
+        dummyBoard.clear();
+        dummyBoard.put(from, new Piece(Camp.HAN, PieceType.GENERAL));
+        boardChecker = new Board(dummyBoard);
+
+        Piece general = dummyBoard.get(from);
+
+        assertThatCode(() -> general.move(from, to, boardChecker))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("장군과 사는 궁성 내에서 두 칸 이상 움직일 수 없다.")
+    void moveFail_When_DiagonalMoveTwoStep() {
+        Position from = new Position(4, 1);
+        Position to = new Position(6, 3);
+
+        dummyBoard.clear();
+        dummyBoard.put(from, new Piece(Camp.HAN, PieceType.GENERAL));
+        boardChecker = new Board(dummyBoard);
+
+        Piece general = dummyBoard.get(from);
+
+        assertThatThrownBy(() -> general.move(from, to, boardChecker))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
