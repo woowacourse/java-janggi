c42fbd2a798868cce7e94fbb7a157fe30d4bdde5
haeseon lee <haeseon4816@gmail.com>
2026-04-04 20:07:09 +0900

    test: 기물이 궁성 내에 존재하는지 테스트

---
+++ src/test/java/janggiBoard/palaceTest/PalacePositionTest.java
@@ -0,0 +1,32 @@
+package janggiBoard.palaceTest;
+
+import domain.Position;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PalacePositionTest {
+
+    @Test
+    void 한나라_현재_좌표가_궁성내에_존재하면_참을_반환한다() {
+        Position currentPosition = new Position(0, 3);
+        Assertions.assertThat(currentPosition.isInsidePalace()).isTrue();
+    }
+
+    @Test
+    void 한나라_현재_좌표가_궁성내에_존재하지_않으면_거짓을_반환한다() {
+        Position currentPosition = new Position(3, 3);
+        Assertions.assertThat(currentPosition.isInsidePalace()).isFalse();
+    }
+
+    @Test
+    void 초나라_현재_좌표가_궁성내에_존재하면_참을_반환한다() {
+        Position currentPosition = new Position(7, 3);
+        Assertions.assertThat(currentPosition.isInsidePalace()).isTrue();
+    }
+
+    @Test
+    void 초나라_현재_좌표가_궁성내에_존재하지_않으면_거짓을_반환한다() {
+        Position currentPosition = new Position(8, 6);
+        Assertions.assertThat(currentPosition.isInsidePalace()).isFalse();
+    }
+}
