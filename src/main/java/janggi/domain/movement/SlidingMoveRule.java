70e1036f3cea509b8bf27d520edd0cf7a8483907
mvg01 <dkms625@naver.com>
2026-04-04 22:07:19 +0900

    feat: SlidingMoveRule 클래스 추가

---
+++ src/main/java/janggi/domain/movement/SlidingMoveRule.java
@@ -0,0 +1,20 @@
+package janggi.domain.movement;
+
+import janggi.domain.Position;
+import janggi.domain.board.BoardMediator;
+import janggi.domain.team.TeamType;
+import java.util.List;
+
+public class SlidingMoveRule implements MoveRule {
+
+    private final Movement movementOrder;
+
+    public SlidingMoveRule(Movement movementOrder) {
+        this.movementOrder = movementOrder;
+    }
+
+    @Override
+    public List<Position> execute(Position from, final TeamType teamType, final BoardMediator boardMediator) {
+        return movementOrder.calculateTraces(from, teamType, boardMediator);
+    }
+}
