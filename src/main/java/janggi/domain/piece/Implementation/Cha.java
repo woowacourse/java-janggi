4233e24d585ff0b65a0109dc8fb9dd53556d0e7e
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-04 10:48:18 +0900

    refactor(PieceType): 점수 추가 및 Piece별 Score 제거

--- src/main/java/janggi/domain/piece/Implementation/Cha.java
+++ src/main/java/janggi/domain/piece/Implementation/Cha.java
@@ -7,10 +7,8 @@ import janggi.domain.status.Team;
 
 public class Cha extends AbstractStraightPiece {
 
-    private static final int SCORE = 13;
-
     public Cha(Team team) {
-        super(SCORE, team, PieceType.CHA);
+        super(team, PieceType.CHA);
     }
 
     public boolean canMove(Route route) {
