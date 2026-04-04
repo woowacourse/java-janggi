75f713717caee6874e1df0bd8923d273c26c032f
EveryPine <starcraft819@gmail.com>
2026-04-04 17:36:06 +0900

    fix(schema): 모든 테이블에 created_at, updated_at 컬럼 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS games
     name        varchar(255) NOT NULL,
     turns_taken numeric,
     team_queue  varchar(255) NOT NULL,
+    created_at  timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at  timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CHECK (team_queue in ('RED,BLUE', 'BLUE,RED'))
 );
 
@@ -15,6 +17,8 @@ CREATE TABLE IF NOT EXISTS board_cells
     piece_type varchar(30) NOT NULL,
     team       varchar(30) NOT NULL,
     game_id    bigint,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (game_id) REFERENCES games (id) ON DELETE CASCADE,
     CHECK (row_pos >= 1 AND row_pos <= 10 AND column_pos >= 1 AND column_pos <= 9)
         AND (piece_type in
