5ab2d1571a855bfa87df60c70ea2bcc2874ac72a
jhk01007 <jhk001007@naver.com>
2026-04-04 23:50:21 +0900

    rename: init -> schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
--- DROP TABLE IF EXISTS piece_position;
--- DROP TABLE IF EXISTS game_room;
+-- 콘솔로 테스트할 때 계속 데이터가 쌓이는 것을 방지
+DROP TABLE IF EXISTS piece_position;
+DROP TABLE IF EXISTS game;
 
 CREATE TABLE IF NOT EXISTS game
 (
