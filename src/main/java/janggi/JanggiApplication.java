084e61828c4a77ec2f8d5e66fe04984beae1cb66
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-04 10:24:25 +0900

    refactor(DataConnectionManager): 추상화 도입

--- src/main/java/janggi/JanggiApplication.java
+++ src/main/java/janggi/JanggiApplication.java
@@ -3,6 +3,7 @@ package janggi;
 import janggi.application.JanggiGameService;
 import janggi.domain.board.BoardRepository;
 import janggi.infra.DataConnectionManager;
+import janggi.infra.H2DataSourceFactory;
 import janggi.infra.JdbcBoardRepository;
 import janggi.infra.TransactionTemplate;
 import janggi.infra.dao.GameRoomDao;
@@ -16,7 +17,7 @@ import org.h2.tools.Server;
 public class JanggiApplication {
     public static void main(String[] args) throws SQLException {
         Server server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
-        DataConnectionManager manager = new DataConnectionManager();
+        DataConnectionManager manager = new DataConnectionManager(H2DataSourceFactory.create());
         GameRoomDao roomDao = new GameRoomDao();
         PiecesDao piecesDao = new PiecesDao();
         BoardRepository repository = new JdbcBoardRepository(roomDao, piecesDao);
