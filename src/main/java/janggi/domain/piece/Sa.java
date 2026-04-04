b3d5260f615a0c60768222d42f429c9de78d218b
kkb <gimgyubin1225@gmail.com>
2026-04-04 13:41:59 +0900

    feat: 궁성 기물이 궁성을 벗어나는 경우 예외를 발생시키는 기능 추가

--- src/main/java/janggi/domain/piece/Sa.java
+++ src/main/java/janggi/domain/piece/Sa.java
@@ -25,6 +25,9 @@ public class Sa extends ActivePiece {
 
     @Override
     public List<Location> calculateRoute(Intersection from, Intersection to) {
+        if(!to.isPalace()) {
+            throw new RouteResolveException(ErrorCode.PALACE_OUT_OF_RANGE);
+        }
         List<Location> moveRoutes;
 
         try {
