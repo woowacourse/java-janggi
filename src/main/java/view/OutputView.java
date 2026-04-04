c81da207bf4626a57374e3a42d65c9008a37ac05
Jinriro <riro12.hanyang.ac.kr>
2026-04-04 15:30:20 +0900

    style: 콘솔 보드 레이아웃 업데이트 및 궁성 대각선 영역 가독성 향상

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,11 +1,12 @@
 package view;
 
-import domain.board.Side;
+import domain.state.Side;
 import domain.piece.PieceType;
 import dto.BoardDto;
 
 import dto.PieceDto;
 import dto.PositionDto;
+import dto.ScoreDto;
 import view.message.PieceFormatter;
 import view.message.SideView;
 
@@ -15,16 +16,84 @@ public class OutputView {
     private static final int ROW_SIZE = 9;
 
     public void printBoard(BoardDto boardDto) {
-        System.out.println("\n   0  1   2  3   4   5  6   7  8");
-
+        System.out.println("\n  0ㅤㅤㅤ1ㅤㅤ 2ㅤㅤ 3ㅤ ㅤ4ㅤ ㅤ5ㅤㅤㅤ6ㅤ ㅤ7ㅤㅤ 8");
         for (int i = 0; i < COL_SIZE; i++) {
-            System.out.print(i + " ");
+            System.out.print(i);
             for (int j = 0; j < ROW_SIZE; j++) {
                 printPieceBySide(boardDto.board().get(new PositionDto(i, j)));
-            }
 
+                if (j < 8) {
+                    System.out.print("ㅡ");
+                }
+            }
             System.out.println();
+
+            if (i < 9) {
+                for (int c = 0; c < 9; c++) {
+                if (c == 0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("ㅤ");
+                    }
+
+                    String line = " ㅣ ";
+
+                    if (i == 0 || i == 7) {
+                        if (c == 3) {
+                            line = " ㅣ \\";
+                        } else if (c == 4) {
+                            line = "ㅣ /";
+                        } else if (c == 5) {
+                            line = "ㅣ ";
+                        }
+                    }
+
+                    if (i == 1 || i == 8) {
+                        if (c == 3) {
+                            line = " ㅣ /";
+                        } else if (c == 4) {
+                            line = "ㅣ \\";
+                        } else if (c == 5) {
+                            line = "ㅣ ";
+                        }
+                    }
+
+                    System.out.print(line);
+                }
+
+                System.out.println();
+            }
         }
+    }
+
+    public void printScore(ScoreDto scoreDto) {
+        System.out.printf(
+                "%s 점수: %s %s 점수: %s\n",
+                SideView.from(Side.CHU),
+                formatScore(scoreDto.getChuSideScore()),
+                SideView.from(Side.HAN),
+                formatScore(scoreDto.getHanSideScore())
+        );
+    }
+
+    public void printTurnPassMessage(Side side) {
+        System.out.println("\n" + SideView.from(side) + "가 한 수를 쉽니다.");
+    }
+
+    public void printSurrenderMessage(Side side) {
+        System.out.println("\n" + SideView.from(side) + "가 기권했습니다.");
+    }
+
+    public void printVictoryMessage(Side side) {
+        System.out.println("\n" + SideView.from(side) + "가 승리했습니다 !!!");
+    }
+
+    public void printCheckMessage() {
+        System.out.println("\n!!! 장군 !!!");
+    }
+
+    public void printCheckMateMessage() {
+        System.out.println("\n!!! 외통수 !!!");
     }
 
     private void printPieceBySide(PieceDto pieceDto) {
@@ -36,6 +105,13 @@ public class OutputView {
         }
 
         System.out.print(" " + SideView.getSideColor(side) + PieceFormatter.from(pieceDto.getPieceType()) + SideView.getResetColor() + " ");
+    }
+
+    private String formatScore(double score) {
+        if (score == (long) score) {
+            return String.valueOf((long) score);
+        }
+        return String.valueOf(score);
     }
 
     public static void printErrorMessage(String message) {
