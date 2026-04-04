92e43d7cb07df7af7e3409701b71454d9ac256f0
LeeJaeHyeok97 <hazardous10@naver.com>
2026-04-04 17:15:34 +0900

    refactor: 사의 이동 규칙에 불필요한 중복 코드 제거

--- src/main/java/domain/strategy/GuardMoveStrategy.java
+++ src/main/java/domain/strategy/GuardMoveStrategy.java
@@ -30,18 +30,6 @@ public class GuardMoveStrategy implements MoveStrategy {
     }
 
     private boolean isNotCorrectPath(final Position from, final Position to) {
-        if (Math.abs(from.getRow() - to.getRow()) + Math.abs(from.getCol() - to.getCol()) != 1) {
-            return true;
-        }
-
-        if (from.getRow() == to.getRow() && Math.abs(from.getCol() - to.getCol()) != 1) {
-            return true;
-        }
-
-        if (from.getCol() == to.getCol() && Math.abs(from.getRow() - to.getRow()) != 1) {
-            return true;
-        }
-
-        return false;
+        return Math.abs(from.getRow() - to.getRow()) + Math.abs(from.getCol() - to.getCol()) != 1;
     }
 }
