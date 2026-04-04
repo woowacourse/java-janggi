1e3cdce0a0a8f5c1d7b5602af1d0e7376e90bd9c
Jinriro <riro12.hanyang.ac.kr>
2026-04-04 15:16:33 +0900

    feat: 궁성 영역 범위 제한 정책 구현

---
+++ src/test/java/domain/policy/PalaceBoundaryPolicyTest.java
@@ -0,0 +1,58 @@
+package domain.policy;
+
+import domain.board.Board;
+import domain.board.BoardInitializer;
+import domain.coordinate.Direction;
+import domain.coordinate.Position;
+import domain.piece.EmptyPiece;
+import domain.piece.Guard;
+import domain.piece.Piece;
+import domain.state.Side;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class PalaceBoundaryPolicyTest {
+
+    static class PalaceTest implements BoardInitializer {
+
+        @Override
+        public Map<Position, Piece> initialize() {
+            Map<Position, Piece> piecesPosition = new HashMap<>();
+
+            piecesPosition.put(new Position(1, 3), new Guard(Side.HAN));
+
+            initializeEmptyPiece(piecesPosition);
+            return piecesPosition;
+        }
+
+        private void initializeEmptyPiece(Map<Position, Piece> pieceInitPlacements) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 9; j++) {
+                    pieceInitPlacements.putIfAbsent(new Position(i, j), EmptyPiece.getInstance());
+                }
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("")
+    void applyTest() {
+        // given
+        Board board = new Board(new PalaceTest().initialize());
+        MovePolicy movePolicy = new PalaceBoundaryPolicy();
+        Position start = new Position(1, 3);
+        List<Direction> directions = List.of(Direction.LEFT, Direction.LEFT);
+
+        // when
+        List<Position> result = movePolicy.apply(board, start, directions);
+
+        // then
+        assertThat(result.size()).isEqualTo(0);
+    }
+}
