3e65e5363b6bd62d8345ab18e0ac20675fc01b91
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-04-05 11:29:58 +0900

    refactor(MaDirection): Sang과 통일성 유지

--- src/main/java/janggi/domain/piece/direction/MaDirection.java
+++ src/main/java/janggi/domain/piece/direction/MaDirection.java
@@ -1,77 +1,58 @@
 package janggi.domain.piece.direction;
 
 import java.util.Arrays;
-import java.util.List;
 
 public enum MaDirection {
     UP_LEFT(
-        List.of(
-            new Offset(-1, -2),
-            new Offset(0, -1)
-        )
+        new Offset(-1, -2),
+        new Offset(0, -1)
     ),
     UP_RIGHT(
-        List.of(
-            new Offset(1, -2),
-            new Offset(0, -1)
-        )
+        new Offset(1, -2),
+        new Offset(0, -1)
     ),
     DOWN_LEFT(
-        List.of(
-            new Offset(-1, 2),
-            new Offset(0, 1)
-        )
+        new Offset(-1, 2),
+        new Offset(0, 1)
     ),
     DOWN_RIGHT(
-        List.of(
-            new Offset(1, 2),
-            new Offset(0, 1)
-        )
+        new Offset(1, 2),
+        new Offset(0, 1)
     ),
     LEFT_UP(
-        List.of(
-            new Offset(-2, -1),
-            new Offset(-1, 0)
-        )
+        new Offset(-2, -1),
+        new Offset(-1, 0)
     ),
     LEFT_DOWN(
-        List.of(
-            new Offset(-2, 1),
-            new Offset(-1, 0)
-        )
+        new Offset(-2, 1),
+        new Offset(-1, 0)
     ),
     RIGHT_UP(
-        List.of(
-            new Offset(2, -1),
-            new Offset(1, 0)
-        )
+        new Offset(2, -1),
+        new Offset(1, 0)
     ),
     RIGHT_DOWN(
-        List.of(
-            new Offset(2, 1),
-            new Offset(1, 0)
-        )
+        new Offset(2, 1),
+        new Offset(1, 0)
     ),
     ;
 
-    private final List<Offset> routes;
+    private final Offset target;
+    private final Offset route;
 
-    MaDirection(List<Offset> routes) {
-        this.routes = routes;
+    MaDirection(Offset target, Offset route) {
+        this.target = target;
+        this.route = route;
     }
 
     public static MaDirection find(int directionCol, int directionRow) {
         return Arrays.stream(values())
-                .filter(dir -> dir.getTarget().directionColumn() == directionCol && dir.getTarget().directionRow() == directionRow)
+                .filter(dir -> dir.target.directionColumn() == directionCol && dir.target.directionRow() == directionRow)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 마가 이동할 수 없는 방향입니다."));
     }
 
     public Offset getWaypoint() {
-        return routes.getLast();
-    }
-
-    private Offset getTarget() {
-        return routes.getFirst();
+        return route;
     }
 }
