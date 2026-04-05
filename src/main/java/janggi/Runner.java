e63eb27ed0f3b0fec38c81c391aec7df809c8570
armd482 <armd482@naver.com>
2026-04-05 16:35:59 +0900

    refactor: 에러 출력 outputview로 변경

--- src/main/java/janggi/Runner.java
+++ src/main/java/janggi/Runner.java
@@ -164,7 +164,7 @@ public class Runner {
             try {
                 return supplier.get();
             } catch (Exception e) {
-                System.out.println(e.getMessage());
+                OutputView.printErrorMessage(e.getMessage());
             }
         }
     }
