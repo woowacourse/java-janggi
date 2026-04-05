cfadd7327b7908b5f3c7f411e5d2383b8994f891
haeseon lee <haeseon4816@gmail.com>
2026-04-05 16:47:47 +0900

    test: 장기보드에 존재하는 기물의 점수 합을 반환하는지 테스트

---
+++ src/test/java/janggiBoard/scoreTest/GameScoreTest.java
@@ -0,0 +1,20 @@
+package janggiBoard.scoreTest;
+
+import domain.Team;
+import domain.board.JanggiBoard;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+public class GameScoreTest {
+
+    @Test
+    void 현재_장기보드에_존재하는_기물의_점수합을_반환한다() {
+        JanggiBoard janggiBoard = new JanggiBoard(new HashMap<>());
+
+        double score = janggiBoard.calculateScore(Team.CHO);
+
+        Assertions.assertThat(score).isEqualTo(72.0);
+    }
+}
