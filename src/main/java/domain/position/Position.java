aa39e4b7cf851d33ed230187c757b4907ce030ff
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-06 01:28:07 +0900

    refactor: 위치 도메인에 있는 음수 좌표 검증 로직 제거

--- src/main/java/domain/position/Position.java
+++ src/main/java/domain/position/Position.java
@@ -3,8 +3,5 @@ package domain.position;
 public record Position(int row, int col) {
 
     public Position {
-        if (row <= 0 || col <= 0) {
-            throw new IllegalArgumentException("좌표는 음수일 수 없습니다.");
-        }
     }
 }
