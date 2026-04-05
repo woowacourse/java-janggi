ecffaf82537f46f493c649725ff873394c580bf1
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-06 01:11:36 +0900

    fix: 이동 전략이 팀 정보를 사용하도록 변경

--- src/main/java/domain/strategy/Strategy.java
+++ src/main/java/domain/strategy/Strategy.java
@@ -1,12 +1,11 @@
 package domain.strategy;
 
-import domain.piece.Blank;
-import domain.piece.Piece;
+import domain.Team;
 import domain.position.Position;
 import domain.PieceProvider;
 
 import java.util.List;
 
 public interface Strategy {
-    List<Position> getMoveCandidates(Position from, PieceProvider board);
+    List<Position> getMoveCandidates(Position from, Team team, PieceProvider board);
 }
