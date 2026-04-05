09ffe4d8439126ff32ef4761d5295b02a9cbe84d
HyoYoonNam <dev.rudevico@gmail.com>
2026-04-06 02:09:41 +0900

    refactor: PalaceMovement.toPalaceCenter private 변경
    해당 메서드는 같은 interface의 default method인
    palaceDiagonalPaths에서만 호출되기 때문에 불필요한 외부 노출을 없애고자
    private으로 변경한다.

--- src/main/java/domain/move/strategy/PalaceMovement.java
+++ src/main/java/domain/move/strategy/PalaceMovement.java
@@ -6,18 +6,6 @@ import java.util.List;
 
 public interface PalaceMovement {
 
-    default Intersection toPalaceCenter(Intersection from) {
-        if (!from.isPalaceCorner()) {
-            throw new IllegalStateException("궁성의 코너에서만 중앙으로 이동할 수 있습니다.");
-        }
-
-        if (from.row() == 1 || from.row() == 3) {
-            return new Intersection(2, 5);
-        }
-
-        return new Intersection(9, 5);
-    }
-
     default List<Path> palaceDiagonalPaths(Intersection from) {
         if (from.isPalaceCenter()) {
             final int row = from.row();
@@ -38,5 +26,17 @@ public interface PalaceMovement {
         }
 
         return List.of();
+    }
+
+    private Intersection toPalaceCenter(Intersection from) {
+        if (!from.isPalaceCorner()) {
+            throw new IllegalStateException("궁성의 코너에서만 중앙으로 이동할 수 있습니다.");
+        }
+
+        if (from.row() == 1 || from.row() == 3) {
+            return new Intersection(2, 5);
+        }
+
+        return new Intersection(9, 5);
     }
 }
--- src/test/java/domain/move/strategy/PalaceMovementTest.java
+++ src/test/java/domain/move/strategy/PalaceMovementTest.java
@@ -1,7 +1,6 @@
 package domain.move.strategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.board.Intersection;
 import domain.move.Path;
@@ -19,46 +18,6 @@ class PalaceMovementTest {
 
     private final PalaceMovement palaceMovementImplements = new PalaceMovement() {
     };
-
-    @DisplayName("궁성의 귀(모서리, 코너) 부분에서만 궁성 중앙으로 이동할 수 있다")
-    @Nested
-    class 궁성_중앙_이동 {
-
-        @DisplayName("궁성의 귀가 아닌 곳에서 이동을 시도하면 예외를 던진다")
-        @Test
-        void 궁성의_귀가_아닌_곳이면_예외를_던진다() {
-            Intersection nonPalaceCornerIntersection = new Intersection(3, 5);
-            assertThatThrownBy(() -> palaceMovementImplements.toPalaceCenter(nonPalaceCornerIntersection))
-                    .isInstanceOf(IllegalStateException.class);
-        }
-
-        @DisplayName("궁성의 귀에서 이동을 시도하면 성공한다")
-        @ParameterizedTest(name = "귀({0})에서 중앙으로 이동")
-        @MethodSource("allPalaceCornerAndCenter")
-        void 궁성의_귀면_성공(Intersection corner, Intersection expected) {
-            Intersection palaceCenter = palaceMovementImplements.toPalaceCenter(corner);
-
-            assertThat(palaceCenter).isEqualTo(expected);
-        }
-
-        private static Stream<Arguments> allPalaceCornerAndCenter() {
-            final Intersection hanCenter = position(2, 5);
-            final Intersection choCenter = position(9, 5);
-
-            return Stream.of(
-                    // HAN 진영
-                    Arguments.of(position(1, 4), hanCenter),
-                    Arguments.of(position(1, 6), hanCenter),
-                    Arguments.of(position(3, 4), hanCenter),
-                    Arguments.of(position(3, 6), hanCenter),
-                    // CHO 진영
-                    Arguments.of(position(8, 4), choCenter),
-                    Arguments.of(position(8, 6), choCenter),
-                    Arguments.of(position(10, 4), choCenter),
-                    Arguments.of(position(10, 6), choCenter)
-            );
-        }
-    }
 
     @DisplayName("연결된 대각선을 찾는다")
     @Nested
