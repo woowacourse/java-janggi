611990a6fee087c17222ce464aa91ccf2a0a61cc
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-06 01:13:19 +0900

    fix: 차를 팀에 따라 이동 좌표를 계산하도록 변경

--- src/main/java/domain/strategy/CarStrategy.java
+++ src/main/java/domain/strategy/CarStrategy.java
@@ -3,6 +3,7 @@ package domain.strategy;
 import static domain.Index.BOARD_COLUMNS;
 import static domain.Index.BOARD_ROWS;
 
+import domain.Team;
 import domain.position.Position;
 import domain.PieceProvider;
 
@@ -14,22 +15,22 @@ import java.util.stream.Collectors;
 public class CarStrategy implements Strategy {
 
     @Override
-    public List<Position> getMoveCandidates(Position from, PieceProvider board) {
+    public List<Position> getMoveCandidates(Position from, Team team, PieceProvider board) {
 
         Direction[] straightDirections = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
 
         return Arrays.stream(Direction.values()) // 모든 방향 혹은 특정 방향 배열
-                .flatMap(direction -> addPathCandidates(from, direction, board).stream())
+                .flatMap(direction -> addPathCandidates(from, team, direction, board).stream())
                 .collect(Collectors.toList());
     }
 
-    private List<Position> addPathCandidates(Position currentPosition, Direction direction, PieceProvider board) {
+    private List<Position> addPathCandidates(Position from, Team team, Direction direction, PieceProvider board) {
         List<Position> candidates = new ArrayList<>();
-        Position next = currentPosition;
+        Position next = from;
 
         while (true) {
-            int nextRows = next.row() + direction.getRowOffset();
-            int nextColumns = next.col() + direction.getColOffset();
+            int nextRows = next.row() + direction.getRowOffset(team);
+            int nextColumns = next.col() + direction.getColOffset(team);
 
             if (nextRows < 0 || nextRows >= BOARD_ROWS.getIndex() || nextColumns < 0
                     || nextColumns >= BOARD_COLUMNS.getIndex()) {
