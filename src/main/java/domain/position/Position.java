e00eb7e6e15db7decffa75070d07107f3b84b068
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-06 01:26:26 +0900

    fix: 0번 행,열이 입력되지 않는 오류 수정

--- src/main/java/domain/position/Position.java
+++ src/main/java/domain/position/Position.java
@@ -3,7 +3,7 @@ package domain.position;
 public record Position(int row, int col) {
 
     public Position {
-        if (row < 0 || col < 0) {
+        if (row <= 0 || col <= 0) {
             throw new IllegalArgumentException("좌표는 음수일 수 없습니다.");
         }
     }
