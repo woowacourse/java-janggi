df5a14eb058b16954b1d9440e7942b519e371702
alstj2384 <ass91160@naver.com>
2026-04-05 16:25:48 +0900

    chore: 테스트용 DB 환경 분리 및 db.properties 추가

--- src/main/java/repository/ConnectionManager.java
+++ src/main/java/repository/ConnectionManager.java
@@ -1,13 +1,29 @@
 package repository;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String URL = "jdbc:mysql://localhost:3306/janggi";
-    private static final String USER = "janggi-user";
-    private static final String PASSWORD = "password";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try (InputStream is = ConnectionManager.class.getClassLoader().getResourceAsStream("db.properties")) {
+            Properties properties = new Properties();
+            properties.load(is);
+
+            URL = properties.getProperty("db.url");
+            USER = properties.getProperty("db.user");
+            PASSWORD = properties.getProperty("db.password");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private ConnectionManager() {
     }
