ebfffd23ceb8d80159d2c0d2136657f19474a8d2
mvg01 <dkms625@naver.com>
2026-04-05 11:35:20 +0900

    refactor: Direction을 Enum Type으로 변경

--- src/main/java/janggi/domain/movement/Direction.java
+++ src/main/java/janggi/domain/movement/Direction.java
@@ -1,19 +1,19 @@
 package janggi.domain.movement;
 
-public final class Direction {
+public enum Direction {
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1),
+    UP_RIGHT(-1, 1),
+    UP_LEFT(-1, -1),
+    DOWN_RIGHT(1, 1),
+    DOWN_LEFT(1, -1);
 
-    public static final Direction UP = new Direction(-1, 0);
-    public static final Direction DOWN = new Direction(1, 0);
-    public static final Direction LEFT = new Direction(0, -1);
-    public static final Direction RIGHT = new Direction(0, 1);
-    public static final Direction UP_RIGHT = new Direction(-1, 1);
-    public static final Direction UP_LEFT = new Direction(-1, -1);
-    public static final Direction DOWN_RIGHT = new Direction(1, 1);
-    public static final Direction DOWN_LEFT = new Direction(1, -1);
     private final int rowDirection;
     private final int columnDirection;
 
-    private Direction(final int rowDirection, final int columnDirection) {
+    Direction(final int rowDirection, final int columnDirection) {
         this.rowDirection = rowDirection;
         this.columnDirection = columnDirection;
     }
