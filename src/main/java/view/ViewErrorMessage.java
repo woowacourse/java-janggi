80a25c2bf4c484db22a98e9a4db542d334b3ac5f
kdongsu5509 <kod66170@gmail.com>
2026-04-05 15:08:26 +0900

    feat: feat : add asking about continue feature

--- src/main/java/view/ViewErrorMessage.java
+++ src/main/java/view/ViewErrorMessage.java
@@ -6,7 +6,8 @@ public enum ViewErrorMessage {
     INVALID_ACTION_INPUT("1, 2 만 입력해주세요"),
     INVALID_SETTING_TYPE_INPUT("1 ~ 4 사이의 숫자만 입력해주세요"),
     NOT_MATCH_PIECE("일치하는 기물 정보가 없습니다"),
-    NOT_DEFINED_TEAM("정의되지 않는 팀입니다.");
+    NOT_DEFINED_TEAM("정의되지 않는 팀입니다."),
+    ONLY_YES_OR_NO("y 혹은 n만 입력해주세요");
 
 
     private final String message;
