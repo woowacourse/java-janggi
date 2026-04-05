4a2b23e992161207bcbca3eaf7433e48a6ce5c7a
armd482 <armd482@naver.com>
2026-04-05 12:47:02 +0900

    refactor: 내부 값 가져오는 메서드 추가

--- src/main/java/janggi/domain/turn/TurnState.java
+++ src/main/java/janggi/domain/turn/TurnState.java
@@ -1,6 +1,14 @@
 package janggi.domain.turn;
 
+import janggi.domain.Side;
 import janggi.domain.piece.PieceAttribute;
 
 public record TurnState(PlayerTurn playerTurn, TurnAttribute turnAttribute, PieceAttribute movedPiece) {
+    public Side getNextTurnSide() {
+        return turnAttribute.side();
+    }
+
+    public int getNextTurn() {
+        return turnAttribute.turn();
+    }
 }
