27dd3be51337b50c5b14bfd55a1c50cae5b88d55
armd482 <armd482@naver.com>
2026-04-05 15:20:57 +0900

    refactor: final로 변경

--- src/main/java/janggi/domain/piece/Pawn.java
+++ src/main/java/janggi/domain/piece/Pawn.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class Pawn extends SingleLinearPiece {
-    private Map<Side, Set<Movement>> isBackward = Map.of(
+    private static final Map<Side, Set<Movement>> isBackward = Map.of(
             Side.HAN, Set.of(Movement.UP, Movement.UP_LEFT, Movement.UP_RIGHT),
             Side.CHO, Set.of(Movement.DOWN, Movement.DOWN_LEFT, Movement.DOWN_RIGHT)
     );
