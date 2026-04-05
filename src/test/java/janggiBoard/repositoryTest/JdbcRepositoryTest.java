e167f370b3b59a1552f49275e98c433d5e841ae2
haeseon lee <haeseon4816@gmail.com>
2026-04-05 23:41:39 +0900

    test: JdbcGameRepository 게임 상태 저장 기능 테스트

--- src/test/java/janggiBoard/repositoryTest/JdbcRepositoryTest.java
+++ src/test/java/janggiBoard/repositoryTest/JdbcRepositoryTest.java
@@ -9,6 +9,7 @@ import domain.piece.PieceType;
 import domain.strategy.PalaceStrategy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import repository.JdbcGameRepository;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +17,7 @@ import java.util.Map;
 public class JdbcRepositoryTest {
 
     private final JdbcBoardRepository jdbcBoardRepository = new JdbcBoardRepository();
+    private final JdbcGameRepository jdbcGameRepository = new JdbcGameRepository();
 
     @Test
     void 보드를_저장하고_다시_불러왔을때_정보가_일치해야한다() {
@@ -27,5 +29,19 @@ public class JdbcRepositoryTest {
         Map<Position, Piece> result = jdbcBoardRepository.findAll();
 
         Assertions.assertThat(result.get(position).getPieceType()).isEqualTo(PieceType.KING);
+    }
+
+    @Test
+    void 게임의_상태를_저장하고_다시_불러왔을떄_정보가_일치해야한다() {
+        Team currentTurn = Team.HAN;
+        boolean isFinished = false;
+        double choScore = 10.0;
+        double hanScore = 20.0;
+
+        jdbcGameRepository.save(currentTurn, isFinished, choScore, hanScore);
+
+        Team turn = jdbcGameRepository.findCurrentTurn();
+
+        Assertions.assertThat(turn).isEqualTo(currentTurn);
     }
 }
