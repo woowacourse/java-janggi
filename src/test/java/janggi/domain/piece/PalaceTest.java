5d3327974deb181b1634e7f497fd9a93a08f7331
tjdakf <ksc73450056@gmail.com>
2026-04-05 20:43:21 +0900

    feat: 궁성 길로의 이동인지 판정 기능 추가

--- src/test/java/janggi/domain/piece/PalaceTest.java
+++ src/test/java/janggi/domain/piece/PalaceTest.java
@@ -19,10 +19,35 @@ class PalaceTest {
             "25, 95, false"
     })
     void 궁성_좌표_내에서_이동인지_판정한다(String from, String to, boolean expected) {
+        // given
         Palace palace = new Palace();
 
+        // when
         boolean result = palace.isPalaceMove(Position.from(from), Position.from(to));
 
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "14, 25, true",
+            "14, 36, true",
+            "14, 24, false",
+            "15, 24, false",
+            "06, 95, true",
+            "06, 84, true",
+            "06, 05, false",
+            "05, 94, false"
+    })
+    void 궁성_길_이동인지_판정한다(String from, String to, boolean expected) {
+        // given
+        Palace palace = new Palace();
+
+        // when
+        boolean result = palace.hasRoute(Position.from(from), Position.from(to));
+
+        // then
         assertThat(result).isEqualTo(expected);
     }
 }
