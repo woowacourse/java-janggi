ab99c3eb97c52d169281149903cd612ae2023ce5
snow___shower <wonsb25@gmail.com>
2026-04-05 14:39:38 +0900

    feat: 차의 궁성 이동 규칙 구현
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/main/java/domain/piece/strategy/ChariotStrategy.java
+++ src/main/java/domain/piece/strategy/ChariotStrategy.java
@@ -4,19 +4,57 @@ import domain.board.PathChecker;
 import domain.board.Position;
 import domain.piece.Piece;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ChariotStrategy implements MoveStrategy {
 
-    public static final String CHARIOT_CANNOT_MOVE_ERROR_MESSAGE = "[ERROR] 차 이동 경로 상에 기물이 존재하여 움직일 수 없습니다.";
+    private static final String CHARIOT_CANNOT_MOVE_ERROR_MESSAGE = "[ERROR] 차 이동 경로 상에 기물이 존재하여 움직일 수 없습니다.";
+    private static final Position HAN_GENERAL = new Position(5, 2);
+    private static final Position CHO_GENERAL = new Position(5, 9);
 
     @Override
     public void move(Position from, Position to, PathChecker checker) {
-        List<Position> path = from.findPath(to);
+        List<Position> path = determinePath(from, to, checker);
+
         List<Piece> piecesInPath = checker.findPiecesInPath(path);
 
         if (!piecesInPath.isEmpty()) {
             throw new IllegalArgumentException(CHARIOT_CANNOT_MOVE_ERROR_MESSAGE);
         }
+    }
+
+    private List<Position> determinePath(Position from, Position to, PathChecker pathChecker) {
+        if (isDiagonalMoveInPalace(from, to, pathChecker)) {
+            return diagonalPath(from, to);
+        }
+        return from.findPath(to);
+    }
+
+    private boolean isDiagonalMoveInPalace(Position from, Position to, PathChecker pathChecker) {
+        int dx = Math.abs(from.x() - to.x());
+        int dy = Math.abs(from.y() - to.y());
+        boolean isInPalace = pathChecker.isInPalace(from) && pathChecker.isInPalace(to);
+        boolean isIncludePalaceCenter = pathChecker.isOnPalaceCenter(from) || pathChecker.isOnPalaceCenter(to);
+
+        if (!(isInPalace && dx == dy)) {
+            return false;
+        }
+
+        return dx == 2 || isIncludePalaceCenter;
+    }
+
+    private List<Position> diagonalPath(Position from, Position to) {
+        if (Math.abs(from.x() - to.x()) == 1) {
+            return Collections.emptyList();
+        }
+        return List.of(findPalaceCenter(from));
+    }
+
+    private Position findPalaceCenter(Position from) {
+        if (from.y() < 4) {
+            return HAN_GENERAL;
+        }
+        return CHO_GENERAL;
     }
 }
--- src/test/java/domain/piece/strategy/ChariotStrategyTest.java
+++ src/test/java/domain/piece/strategy/ChariotStrategyTest.java
@@ -28,6 +28,10 @@ public class ChariotStrategyTest {
         dummyBoard.put(new Position(1, 1), new Piece(Camp.HAN, PieceType.CHARIOT));
         dummyBoard.put(new Position(1, 2), new Piece(Camp.HAN, PieceType.SOLDIER));
         dummyBoard.put(new Position(1, 5), new Piece(Camp.CHO, PieceType.SOLDIER));
+        dummyBoard.put(new Position(6, 1), new Piece(Camp.CHO, PieceType.CHARIOT));
+        dummyBoard.put(new Position(6, 2), new Piece(Camp.CHO, PieceType.CHARIOT));
+        dummyBoard.put(new Position(4, 8), new Piece(Camp.CHO, PieceType.CHARIOT));
+        dummyBoard.put(new Position(5, 9), new Piece(Camp.CHO, PieceType.CHARIOT));
 
         pathChecker = new Board(dummyBoard);
     }
@@ -48,11 +52,72 @@ public class ChariotStrategyTest {
     @DisplayName("경로상의 기물 리스트가 비어있는 경우 정상 이동한다.")
     void moveSuccessfully_When_PiecesIsEmpty() {
         Position from = new Position(1, 1);
-        Position to = new Position(7, 1);
+        Position to = new Position(3, 1);
 
         Piece chariot = dummyBoard.get(from);
 
         assertThatCode(() -> chariot.move(from, to, pathChecker))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("출발 지점과 도착 지점이 궁성 내부이고 중앙을 포함한 대각선 1칸 이동이 가능하다")
+    void moveSuccess_When_ChariotIsInPalaceAndDiagonalOneSpaceMove() {
+        Position from = new Position(6, 1);
+        Position to = new Position(5, 2);
+
+        Piece chariot = dummyBoard.get(from);
+
+        assertThatCode(() -> chariot.move(from, to, pathChecker))
+                .doesNotThrowAnyException();
+    }
+
+
+    @Test
+    @DisplayName("출발 지점과 도착 지점이 궁성 내부이고 중앙을 포함하지 않은 대각선 1칸 이동은 예외가 발생한다")
+    void throwException_When_ChariotIsInPalaceButNoIncludeCenterDiagonalOneSpaceMove() {
+        Position from = new Position(6, 2);
+        Position to = new Position(5, 3);
+
+        Piece chariot = dummyBoard.get(from);
+
+        assertThatThrownBy(() -> chariot.move(from, to, pathChecker))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("출발 지점과 도착 지점이 궁성 내부이고 중앙이 비어있는 대각선 2칸 이동이 가능하다")
+    void moveSuccess_When_ChariotIsInPalaceAndDiagonalTwoSpaceMove() {
+        Position from = new Position(6, 1);
+        Position to = new Position(4, 3);
+
+        Piece chariot = dummyBoard.get(from);
+
+        assertThatCode(() -> chariot.move(from, to, pathChecker))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("출발 지점과 도착 지점이 궁성 내부이고 중앙이 비어있지 않은 대각선 2칸 이동은 예외가 발생한다")
+    void throwException_When_ChariotIsInPalaceButNoIncludeCenterDiagonalTwoSpaceMove() {
+        Position from = new Position(4, 8);
+        Position to = new Position(6, 10);
+
+        Piece chariot = dummyBoard.get(from);
+
+        assertThatThrownBy(() -> chariot.move(from, to, pathChecker))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("궁성 밖에서의 대각선 이동은 예외가 발생한다")
+    void throwException_When_DiagonalMoveOutOfPalace() {
+        Position from = new Position(1, 2);
+        Position to = new Position(2, 3);
+
+        Piece chariot = dummyBoard.get(from);
+
+        assertThatThrownBy(() -> chariot.move(from, to, pathChecker))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
