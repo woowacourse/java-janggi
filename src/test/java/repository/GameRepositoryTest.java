862e615215575431bf2619771c8df7c7d62b5d02
alstj2384 <ass91160@naver.com>
2026-04-05 17:23:39 +0900

    test: Repository 테스트 추가

---
+++ src/test/java/repository/GameRepositoryTest.java
@@ -0,0 +1,91 @@
+package repository;
+
+import domain.piece.Team;
+import domain.settingType.SettingType;
+import domain.state.JanggiGame;
+import domain.state.Playing;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameRepositoryTest {
+    private final GameRepository gameRepository = new GameRepository();
+
+    @BeforeEach
+    void init() {
+        String sql = "DELETE FROM GAME_ROOM";
+
+        try (Connection conn = ConnectionManager.getConnection();
+             PreparedStatement psmt = conn.prepareStatement(sql)) {
+            psmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void save() {
+        String title = "테스트용 장기방";
+        Team team = Team.CHO;
+        long savedId = gameRepository.save(team, title);
+
+        List<GameRoomInfo> gameInfo = gameRepository.getAll();
+
+        Assertions.assertThat(gameInfo).hasSize(1);
+        Assertions.assertThat(gameInfo.getFirst().id()).isEqualTo(savedId);
+        Assertions.assertThat(gameInfo.getFirst().title()).isEqualTo(title);
+    }
+
+    @Test
+    void 턴이_변경되면_반영되어야_한다() {
+        String title = "테스트용 장기방";
+        Team team = Team.CHO;
+        long savedId = gameRepository.save(team, title);
+
+        // when
+        JanggiGame game = Playing.init(savedId, SettingType.LEFT, SettingType.LEFT);
+        game = game.pass();
+
+        gameRepository.updateTurn(game);
+        Team updatedTurn = gameRepository.getInfo(savedId);
+
+        // then
+        Assertions.assertThat(updatedTurn).isEqualTo(Team.HAN);
+    }
+
+    @Test
+    void 턴이_변경되어야_한다() {
+        String title = "테스트용 장기방";
+        Team team = Team.HAN;
+        long savedId = gameRepository.save(team, title);
+
+        // when
+        JanggiGame game = Playing.init(savedId, SettingType.LEFT, SettingType.LEFT);
+
+        gameRepository.updateResult(game);
+
+        String sql = "SELECT IS_FINISHED FROM GAME_ROOM WHERE ID = ?";
+
+        boolean isFinished = true;
+        try (
+                Connection connection = ConnectionManager.getConnection();
+                PreparedStatement psmt = connection.prepareStatement(sql)
+        ) {
+            psmt.setLong(1, savedId);
+            ResultSet resultSet = psmt.executeQuery();
+            while (resultSet.next()) {
+                isFinished = resultSet.getBoolean(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // then
+        Assertions.assertThat(isFinished).isFalse();
+    }
+}
