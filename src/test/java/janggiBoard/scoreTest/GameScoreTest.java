2f5a045d4964b92232c40c1548ea91f8b2e26d45
haeseon lee <haeseon4816@gmail.com>
2026-04-05 17:00:03 +0900

    test: 한나라의 경우 총 점수에 1.5점을 추가하는 기능 테스트

--- src/test/java/janggiBoard/scoreTest/GameScoreTest.java
+++ src/test/java/janggiBoard/scoreTest/GameScoreTest.java
@@ -17,4 +17,13 @@ public class GameScoreTest {
 
         Assertions.assertThat(score).isEqualTo(72.0);
     }
+
+    @Test
+    void 현재_장기보드에_존재하는_한나라_기물의_점수합을_반환한다() {
+        JanggiBoard janggiBoard = new JanggiBoard(new HashMap<>());
+
+        double score = janggiBoard.calculateScore(Team.HAN);
+
+        Assertions.assertThat(score).isEqualTo(73.5);
+    }
 }
