263815f41ba6c3e2215adb2c14c3c4bdbcf9bb68
전원준 <jwjjwj1960@naver.com>
2026-04-05 17:50:27 +0900

    docs: init.sql 수정

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -5,22 +5,6 @@ CREATE TABLE IF NOT EXISTS janggi_game (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS janggi_game_piece (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    janggi_game_id BIGINT NOT NULL,
-    piece_id BIGINT NOT NULL,
-
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS janggi_game_movement (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    janggi_game_id BIGINT NOT NULL,
-    movement_id BIGINT NOT NULL,
-
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS piece (
     id BIGINT NOT NULL AUTO_INCREMENT,
     row_pos INT NOT NULL,
@@ -30,13 +14,4 @@ CREATE TABLE IF NOT EXISTS piece (
 
     PRIMARY KEY (id),
     UNIQUE (row_pos, col_pos, team, type)
-);
-
-CREATE TABLE IF NOT EXISTS movement (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    source VARCHAR(8) NOT NULL,
-    destination VARCHAR(8) NOT NULL,
-
-    PRIMARY KEY (id),
-    UNIQUE (source, destination)
 );
