101a00f6a29afaa9181e41e01f97e002bab66d38
woohyeon <woohyeon6885@gmail.com>
2026-04-05 16:58:21 +0900

    fix: 병졸/궁/사의 궁성 이동 기능 복구

--- src/main/java/janggi/model/piece/Byeong.java
+++ src/main/java/janggi/model/piece/Byeong.java
@@ -2,6 +2,7 @@ package janggi.model.piece;
 
 import janggi.model.Team;
 import janggi.model.movement.Movement;
+import janggi.model.movement.palace.PalaceAdjacentMovement;
 import janggi.model.movement.patternBasedMovement.ByeongMovement;
 import janggi.model.position.absolute.Position;
 import janggi.model.position.absolute.PositionPath;
@@ -9,19 +10,24 @@ import java.util.List;
 
 public class Byeong extends Piece {
 
+    private final Movement palaceMovement;
+
     private Byeong(
             Team team,
             PieceType pieceType,
-            Movement defaultMovement
+            Movement defaultMovement,
+            Movement palaceMovement
     ) {
         super(team, pieceType, defaultMovement);
+        this.palaceMovement = palaceMovement;
     }
 
     public Byeong(Team team) {
         this(
                 team,
                 PieceType.BYEONG,
-                new ByeongMovement()
+                new ByeongMovement(),
+                new PalaceAdjacentMovement()
         );
     }
 
@@ -30,6 +36,10 @@ public class Byeong extends Piece {
         if ((team == Team.CHO && isMovingSouth(from, to))
                 ||(team == Team.HAN && !isMovingSouth(from, to))) {
             throw new IllegalArgumentException("이동할 수 없는 위치입니다.");
+        }
+
+        if (from.isInSamePalaceWith(to)) {
+            return palaceMovement.move(from, to);
         }
 
         return defaultMovement.move(from, to);
