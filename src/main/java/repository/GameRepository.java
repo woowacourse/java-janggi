dd8a8179b6c9fab6e2520973ed6aebf107f6ad7f
haeseon lee <haeseon4816@gmail.com>
2026-04-05 23:41:19 +0900

    feat: JdbcGameRepository 게임 상태 저장 기능 구현

---
+++ src/main/java/repository/GameRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import domain.Team;
+
+public interface GameRepository {
+    void save(Team currentTurn, boolean isFinished, double choScore, double hanScore);
+
+    Team findCurrentTurn();
+
+    void deleteAll();
+}
