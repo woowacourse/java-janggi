75469183a4015deb7705c95a8dd0a792c4c1c067
nn98 <jkllhgb@gmail.com>
2026-04-06 16:47:32 +0900

    refactor(DB): createdAt DTO 에 포함했으니 불용 메서드 제거

--- src/main/java/janggi/persistence/GameRepository.java
+++ src/main/java/janggi/persistence/GameRepository.java
@@ -4,7 +4,6 @@ import janggi.domain.game.GameManager;
 import janggi.dto.GameSessionDTO;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface GameRepository {
@@ -14,8 +13,6 @@ public interface GameRepository {
     long insertGame(Connection connection, GameManager gameManager) throws SQLException;
 
     GameManager findByGameId(Connection connection, long gameId) throws SQLException;
-
-    LocalDateTime findCreatedAtById(Connection connection, long gameId) throws SQLException;
 
     void updateTurn(Connection connection, long gameId, GameManager gameManager) throws SQLException;
 }
--- src/main/java/janggi/persistence/JanggiGameRepository.java
+++ src/main/java/janggi/persistence/JanggiGameRepository.java
@@ -120,29 +120,6 @@ public class JanggiGameRepository implements GameRepository {
     }
 
     @Override
-    public LocalDateTime findCreatedAtById(Connection connection, long gameId) throws SQLException {
-        String sql = "select created_at from game where game_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            return executeFindCreatedAtById(statement, gameId);
-        }
-
-    }
-
-    private LocalDateTime executeFindCreatedAtById(PreparedStatement statement, long gameId) throws SQLException {
-        statement.setLong(1, gameId);
-        try (ResultSet resultSet = statement.executeQuery()) {
-            return mapToLocalDateTime(resultSet);
-        }
-    }
-
-    private LocalDateTime mapToLocalDateTime(ResultSet resultSet) throws SQLException {
-        if (!resultSet.next()) {
-            throw new SQLException("해당 ID의 게임이 조회되지 않습니다.");
-        }
-        return resultSet.getObject("created_at", LocalDateTime.class);
-    }
-
-    @Override
     public void updateTurn(Connection connection, long gameId, GameManager gameManager) throws SQLException {
         String sql = "update game set current_turn = ? where game_id = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
