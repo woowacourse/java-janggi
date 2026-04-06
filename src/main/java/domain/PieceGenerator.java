7b810a88d3a5b7d5b94ea1e57b89d98252a52ed2
CHOSEONGHYEON <jcd007055@gmail.com>
2026-04-06 09:36:47 +0900

    refactor(PieceGenerator): 반복되는 코드 개선

--- src/main/java/domain/PieceGenerator.java
+++ src/main/java/domain/PieceGenerator.java
@@ -24,77 +24,56 @@ public class PieceGenerator {
 
     private static Map<Position, Piece> generatePiece(Camp camp) {
         Map<Position, Piece> board = new HashMap<>();
-        board.putAll(generateGeneral(camp));
-        board.putAll(generateSoldier(camp));
-        board.putAll(generateGuard(camp));
-        board.putAll(generateCannon(camp));
-        board.putAll(generateChariot(camp));
+        board.putAll(generatePieces(camp, InitialPieceLocation.GENERAL, PieceType.GENERAL));
+        board.putAll(generatePieces(camp, InitialPieceLocation.SOLDIER, PieceType.SOLDIER));
+        board.putAll(generatePieces(camp, InitialPieceLocation.GUARD, PieceType.GUARD));
+        board.putAll(generatePieces(camp, InitialPieceLocation.CANNON, PieceType.CANNON));
+        board.putAll(generatePieces(camp, InitialPieceLocation.CHARIOT, PieceType.CHARIOT));
 
         return board;
     }
 
     private static Map<Position, Piece> generatePiece(Camp camp, ElephantFormation elephantFormation) {
         Map<Position, Piece> board = new HashMap<>();
-        board.putAll(generateHorse(camp, elephantFormation));
-        board.putAll(generateElephant(camp, elephantFormation));
+        board.putAll(generatePieces(camp, InitialPieceLocation.HORSE, elephantFormation, PieceType.HORSE));
+        board.putAll(generatePieces(camp, InitialPieceLocation.ELEPHANT, elephantFormation, PieceType.ELEPHANT));
 
         return board;
     }
 
-    private static Map<Position, Piece> generateGeneral(Camp camp) {
-        Map<Position, Piece> generalPosition = new HashMap<>();
-
-        List<Position> positions = InitialPieceLocation.GENERAL.getPositions(camp);
-        positions.forEach(position -> generalPosition.put(position, new General(camp)));
-
-        return generalPosition;
+    private static Map<Position, Piece> generatePieces(Camp camp,
+                                                       InitialPieceLocation location,
+                                                       PieceType pieceType) {
+        Map<Position, Piece> pieces = new HashMap<>();
+        List<Position> positions = location.getPositions(camp);
+        for (Position position : positions) {
+            pieces.put(position, createPiece(camp, pieceType));
+        }
+        return pieces;
     }
 
-    private static Map<Position, Piece> generateSoldier(Camp camp) {
-        Map<Position, Piece> soldierPosition = new HashMap<>();
-        List<Position> positions = InitialPieceLocation.SOLDIER.getPositions(camp);
-        positions.forEach(position -> soldierPosition.put(position, new Soldier(camp)));
-
-        return soldierPosition;
+    private static Map<Position, Piece> generatePieces(Camp camp,
+                                                       InitialPieceLocation location,
+                                                       ElephantFormation elephantFormation,
+                                                       PieceType pieceType) {
+        Map<Position, Piece> pieces = new HashMap<>();
+        List<Position> positions = location.getPositions(camp, elephantFormation);
+        for (Position position : positions) {
+            pieces.put(position, createPiece(camp, pieceType));
+        }
+        return pieces;
     }
 
-    private static Map<Position, Piece> generateGuard(Camp camp) {
-        Map<Position, Piece> guardPosition = new HashMap<>();
-        List<Position> positions = InitialPieceLocation.GUARD.getPositions(camp);
-        positions.forEach(position -> guardPosition.put(position, new Guard(camp)));
-
-        return guardPosition;
-    }
-
-    private static Map<Position, Piece> generateHorse(Camp camp, ElephantFormation elephantFormation) {
-        Map<Position, Piece> horsePosition = new HashMap<>();
-        List<Position> positions = InitialPieceLocation.HORSE.getPositions(camp, elephantFormation);
-        positions.forEach(position -> horsePosition.put(position, new Horse(camp)));
-
-        return horsePosition;
-    }
-
-    private static Map<Position, Piece> generateCannon(Camp camp) {
-        Map<Position, Piece> cannonPosition = new HashMap<>();
-        List<Position> positions = InitialPieceLocation.CANNON.getPositions(camp);
-        positions.forEach(position -> cannonPosition.put(position, new Cannon(camp)));
-
-        return cannonPosition;
-    }
-
-    private static Map<Position, Piece> generateElephant(Camp camp, ElephantFormation elephantFormation) {
-        Map<Position, Piece> elephantPosition = new HashMap<>();
-        List<Position> positions = InitialPieceLocation.ELEPHANT.getPositions(camp, elephantFormation);
-        positions.forEach(position -> elephantPosition.put(position, new Elephant(camp)));
-
-        return elephantPosition;
-    }
-
-    private static Map<Position, Piece> generateChariot(Camp camp) {
-        Map<Position, Piece> chariotPosition = new HashMap<>();
-        List<Position> positions = InitialPieceLocation.CHARIOT.getPositions(camp);
-        positions.forEach(position -> chariotPosition.put(position, new Chariot(camp)));
-
-        return chariotPosition;
+    private static Piece createPiece(Camp camp, PieceType pieceType) {
+        return switch (pieceType) {
+            case GENERAL -> new General(camp);
+            case GUARD -> new Guard(camp);
+            case HORSE -> new Horse(camp);
+            case ELEPHANT -> new Elephant(camp);
+            case CANNON -> new Cannon(camp);
+            case CHARIOT -> new Chariot(camp);
+            case SOLDIER -> new Soldier(camp);
+            default -> throw new IllegalArgumentException("[ERROR] 알 수 없는 기물 타입: " + pieceType);
+        };
     }
 }
