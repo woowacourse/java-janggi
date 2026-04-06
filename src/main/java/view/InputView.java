441996e30717a2f96eb5fff20d97077019d4bbe7
haeseon lee <haeseon4816@gmail.com>
2026-04-06 10:35:42 +0900

    feat: InputView에 중단된 게임 재도전 여부 묻는 메서드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,13 +6,18 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public String readMovePiecePosition(String teamName) {
+    public static String readMovePiecePosition(String teamName) {
         System.out.print(teamName + "나라의 턴입니다. 이동할 기물의 좌표를 입력해주세요.(e.g. 0,0)\n");
         return scanner.nextLine();
     }
 
-    public String readTargetPiecePosition() {
+    public static String readTargetPiecePosition() {
         System.out.print("어떤 좌표로 이동하시겠습니까?\n");
+        return scanner.nextLine();
+    }
+
+    public static String askResumeGame() {
+        System.out.println("중단된 게임이 있습니다. 이어서 진행하시겠습니까?");
         return scanner.nextLine();
     }
 }
