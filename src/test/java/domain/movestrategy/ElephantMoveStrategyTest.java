086e9eb3ec7d76d845fcffb06811e417f80770cf
박병훈 <leafloveu@naver.com>
2026-04-06 16:22:37 +0900

    fix(elephant): 테스트에서 상이 아니라 차를 배치한 문제 수정

--- src/test/java/domain/movestrategy/ElephantMoveStrategyTest.java
+++ src/test/java/domain/movestrategy/ElephantMoveStrategyTest.java
@@ -29,7 +29,7 @@ class ElephantMoveStrategyTest {
         Position from = Position.of(5, 5);
 
         Map<Position, Piece> pieces = new HashMap<>();
-        pieces.put(from, Piece.choPieceOf(PieceType.CHARIOT));
+        pieces.put(from, Piece.choPieceOf(PieceType.ELEPHANT));
 
         Board board = Board.init(pieces);
 
