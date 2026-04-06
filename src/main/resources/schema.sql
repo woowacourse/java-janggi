c6b7e9ede269c8b76a245d6ea62a506e94389950
smiinii <sm020320@naver.com>
2026-04-06 17:37:29 +0900

    feat: DB 초기화 유틸 클래스 및 테스트 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS Game (
+    game_id INT AUTO_INCREMENT PRIMARY KEY,
+    state VARCHAR(50),
+    turn VARCHAR(50),
+    start_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS Piece (
+    game_id INT,
+    type VARCHAR(50),
+    `row` INT,
+    col INT
+);
