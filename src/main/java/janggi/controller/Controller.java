674561c1f388753b20bb47272bbb3bd677d266a7
구민서 <minseo12379@gmail.com>
2026-04-06 14:23:07 +0900

    chore: 사용하지 않는 패키지 삭제

--- src/main/java/janggi/controller/Controller.java
+++ src/main/java/janggi/controller/Controller.java
@@ -2,9 +2,6 @@ package janggi.controller;
 
 import janggi.domain.Board;
 import janggi.domain.BoardFactory;
-import janggi.domain.Column;
-import janggi.domain.Position;
-import janggi.domain.Row;
 import janggi.domain.Team;
 import janggi.dto.BoardDto;
 import janggi.exception.BusinessException;
