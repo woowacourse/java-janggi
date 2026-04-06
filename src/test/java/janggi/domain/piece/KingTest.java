417793e67d47fcafc9b8cfea707e68bc62ace738
choubung <choubung@sookmyung.ac.kr>
2026-04-06 10:40:34 +0900

    test : 궁성 안 이동 테스트 케이스 추가

--- src/test/java/janggi/domain/piece/KingTest.java
+++ src/test/java/janggi/domain/piece/KingTest.java
@@ -15,10 +15,14 @@ class KingTest {
 
     @ParameterizedTest
     @CsvSource({
-            "1, 4, 0, 4",
-            "1, 4, 2, 4",
-            "1, 4, 1, 3",
-            "1, 4, 1, 5"
+            "1, 4, 0, 4", // 북
+            "1, 4, 2, 4", // 남
+            "1, 4, 1, 3", // 서
+            "1, 4, 1, 5", // 동
+            "2, 3, 1, 4", // 북동
+            "2, 5, 1, 4", // 북서
+            "0, 3, 1, 4", // 남동
+            "0, 5, 1, 4" // 남서
     })
     void 정상_이동_true_반환_테스트(int fromRow, int fromCol, int toRow, int toCol) {
         // given
@@ -40,7 +44,7 @@ class KingTest {
         Position from = new Position(fromRow, fromCol);
         Position to = new Position(toRow, toCol);
 
-        board = new Board(Map.of(from, new King(Team.HAN)));
+        board = new Board(Map.of(from, new Advisor(Team.HAN)));
 
         // when, then
         assertThat(king.canMove(from, to, board)).isTrue();
@@ -50,7 +54,6 @@ class KingTest {
     @CsvSource({
             "0, 4, 2, 4", // 남쪽 두 칸
             "0, 4, 0, 7", // 동쪽 세 칸
-            "0, 4, 1, 5", // 대각선 한 칸 (사이클 1 비허용)
             "0, 0, 2, 0"  // 경계에서 두 칸 이동
     })
     void 한_칸_초과_이동시_false_반환_테스트(int fromRow, int fromCol, int toRow, int toCol) {
@@ -58,7 +61,34 @@ class KingTest {
         Position from = new Position(fromRow, fromCol);
         Position to = new Position(toRow, toCol);
 
-        board = new Board(Map.of(from, new King(Team.HAN)));
+        board = new Board(Map.of(from, new Advisor(Team.HAN)));
+
+        // when, then
+        assertThat(king.canMove(from, to, board)).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 4, 1, 3", // 잘못된 대각선
+            "0, 4, 1, 5",
+            "2, 4, 1, 3",
+            "2, 4, 1, 5",
+            "0, 3, 0, 2", // 궁성 바깥
+            "1, 3, 1, 2",
+            "2, 3, 2, 2",
+            "2, 3, 3, 3",
+            "2, 4, 3, 4",
+            "2, 5, 3, 5",
+            "2, 5, 2, 6",
+            "1, 5, 1, 6",
+            "0, 5, 0, 6"
+    })
+    void 허용되지_않은_이동시_예외가_발생한다(int fromRow, int fromCol, int toRow, int toCol) {
+        // given
+        Position from = new Position(fromRow, fromCol);
+        Position to = new Position(toRow, toCol);
+
+        board = new Board(Map.of(from, new Advisor(Team.HAN)));
 
         // when, then
         assertThat(king.canMove(from, to, board)).isFalse();
