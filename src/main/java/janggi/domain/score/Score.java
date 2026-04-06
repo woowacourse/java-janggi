016a857b49289336d21f8b2f9ab891b3f8142ec5
tjdakf <ksc73450056@gmail.com>
2026-04-06 13:49:17 +0900

    refactor: 점수 계산 로직 private 메서드 분리

--- src/main/java/janggi/domain/score/Score.java
+++ src/main/java/janggi/domain/score/Score.java
@@ -7,21 +7,14 @@ import janggi.domain.team.Team;
 import java.util.Map;
 
 public class Score {
+    public static final double SECOND_TURN_BONUS = 1.5;
 
     private final double hanScore;
     private final double choScore;
 
     public Score(Map<Position, Piece> board) {
-        hanScore = 1.5 + board.values().stream()
-                .filter(piece -> piece.isSameTeam(Team.HAN))
-                .mapToDouble(piece -> piece.getType().getScore())
-                .sum();
-
-
-        choScore = 0 + board.values().stream()
-                .filter(piece -> piece.isSameTeam(Team.CHO))
-                .mapToDouble(piece -> piece.getType().getScore())
-                .sum();
+        hanScore = calculateScore(board, SECOND_TURN_BONUS, Team.HAN);
+        choScore = calculateScore(board, 0.0, Team.CHO);
     }
 
     public double getHanScore() {
@@ -30,5 +23,12 @@ public class Score {
 
     public double getChoScore() {
         return choScore;
+    }
+
+    private double calculateScore(Map<Position, Piece> board, double bonus, Team team) {
+        return bonus + board.values().stream()
+                .filter(piece -> piece.isSameTeam(team))
+                .mapToDouble(piece -> piece.getType().getScore())
+                .sum();
     }
 }
