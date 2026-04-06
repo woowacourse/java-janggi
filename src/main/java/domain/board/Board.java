e809342eaa70542a55472f483ab85b6a552d3baf
sun <sun007021@gmail.com>
2026-04-06 20:29:00 +0900

    refactor: 보드 배열에서 Map으로 변경

--- src/main/java/domain/board/Board.java
+++ src/main/java/domain/board/Board.java
@@ -6,6 +6,7 @@ import domain.piece.EmptyPiece;
 import domain.piece.Piece;
 import domain.piece.Pieces;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,22 +14,23 @@ public class Board implements Pieces {
 
     private static final BoardBounds BOUNDS = BoardBounds.JANGGI;
 
-    private final Piece[][] board = new Piece[BOUNDS.colsize()][BOUNDS.rowSize()];
+    private final Map<Position, Piece> board;
 
     public Board(Map<Position, Piece> initialize) {
+        board = new HashMap<>();
         for (Position position : BOUNDS.allPositions()) {
-            board[position.col()][position.row()] = initialize.getOrDefault(position, EmptyPiece.getInstance());
+            board.put(position, initialize.getOrDefault(position, EmptyPiece.getInstance()));
         }
     }
 
     public void move(Position start, Position destination) {
-        board[destination.col()][destination.row()] = board[start.col()][start.row()];
-        board[start.col()][start.row()] = EmptyPiece.getInstance();
+        board.put(destination, board.get(start));
+        board.put(start, EmptyPiece.getInstance());
     }
 
     @Override
     public Piece getPiece(Position position) {
-        return board[position.col()][position.row()];
+        return board.get(position);
     }
 
     public List<Position> getPossibleMoves(Position start) {
@@ -40,7 +42,7 @@ public class Board implements Pieces {
         CellSnapshot[][] snapshot = new CellSnapshot[BOUNDS.colsize()][BOUNDS.rowSize()];
         for (int i = 0; i < BOUNDS.colsize(); i++) {
             for (int j = 0; j < BOUNDS.rowSize(); j++) {
-                Piece piece = board[i][j];
+                Piece piece = board.get(new Position(i, j));
                 snapshot[i][j] = new CellSnapshot(piece.getType(), piece.getSide());
             }
         }
