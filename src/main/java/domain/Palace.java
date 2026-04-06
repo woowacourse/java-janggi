a402f34f18490c39b1aee541518c4b3c612023d8
최용준 <yj9107vv@gmail.com>
2026-04-06 15:29:06 +0900

    feat: 궁성 영역 규칙에 따라 이동 가능한 위치 반환
    - reachablePositionInPalace() 메서드 추가
    1. 궁성 영역의 각 모서리인 경우 궁성 중앙으로 이동 위치 반환
    2. 궁성 영역의 중앙인 경우 각 모서리로 이동 위치들 반환
    3. 궁성 영역에서 대각 이동 불가능 시 빈 리스트를 반환

--- src/main/java/domain/Palace.java
+++ src/main/java/domain/Palace.java
@@ -1,9 +1,34 @@
 package domain;
 
+import java.util.List;
+
 public class Palace {
 
     private static final Position palaceRedCenter = new Position(1, 4);
     private static final Position palaceGreenCenter = new Position(8, 4);
+
+    // TODO: 궁성 영역인 경우 궁성 내부에서만 움직일 수 있는 곳을 알려줌. (대각 이동이 가능한 모서리 4곳과, 중앙에서 4곳 모서리로 이동할 수 있다.)
+    public List<Position> reachablePositionsInPalace(Position currentPosition) {
+        if (isPalaceGreenArea(currentPosition)) {
+            return diagonalPositionsInPalace(palaceGreenCenter, currentPosition);
+        }
+        if (isPalaceRedArea(currentPosition)) {
+            return diagonalPositionsInPalace(palaceRedCenter, currentPosition);
+        }
+
+        return List.of();
+    }
+
+    private List<Position> diagonalPositionsInPalace(Position palaceCenter, Position currentPosition) {
+        if (currentPosition.equals(palaceCenter)) {
+            return List.of(currentPosition.upCrossRight(), currentPosition.upCrossLeft(),
+                    currentPosition.downCrossLeft(), currentPosition.downCrossRight());
+        }
+        if (Math.abs(palaceCenter.row() - currentPosition.row()) == 1 && Math.abs(palaceCenter.col() - currentPosition.col()) == 1) {
+            return List.of(palaceCenter);
+        }
+        return List.of();
+    }
 
     public boolean isPalaceRedArea(Position currentPosition) {
         return isPalaceArea(currentPosition, palaceRedCenter);
