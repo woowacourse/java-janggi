42837772ed2297eac295c210b34c30ec13a6e497
Huiyeongkim <huiyeong9619@naver.com>
2026-04-06 13:57:22 +0900

    feat: 차는 대각선 포인트인 경우만 대각선 이동할 수 있도록 수정

--- src/main/java/domain/strategy/ChariotMoveStrategy.java
+++ src/main/java/domain/strategy/ChariotMoveStrategy.java
@@ -1,16 +1,13 @@
 package domain.strategy;
 
 import domain.Board;
-import domain.Piece;
-import domain.Team;
 import domain.vo.Position;
 
 public class ChariotMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean canMove(final Position from, final Position to, final Board board) {
-        if (isNotStraightPath(from, to) &&
-                (isOutsideOwnPalace(from, to, board) || isNotDiagonalPath(from, to)))
+        if (isNotStraightPath(from, to) && isNotDiagonalPath(from, to, board))
             return false;
 
         int dx = Integer.compare(to.getRow(), from.getRow());
@@ -34,23 +31,9 @@ public class ChariotMoveStrategy implements MoveStrategy {
         return board.canOccupy(from, to);
     }
 
-    private boolean isOutsideOwnPalace(Position from, Position to, Board board) {
-        Piece piece = board.findPieceByPosition(from)
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 이동할 기물이 없습니다."));
-        Team team = piece.getTeam();
-
-        if (!board.isInOwnPalace(from, team)) {
-            return true;
-        }
-
-        if (!board.isInOwnPalace(to, team)) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isNotDiagonalPath(Position from, Position to) {
-        if (Math.abs(from.getRow() - to.getRow()) == Math.abs(from.getCol() - to.getCol())) {
+    private boolean isNotDiagonalPath(Position from, Position to, Board board) {
+        if (board.canMoveDiagonallyInPalace(from, to)
+                && Math.abs(from.getRow() - to.getRow()) == Math.abs(from.getCol() - to.getCol())) {
             return false;
         }
         return true;
--- src/test/java/domain/strategy/ChariotMoveStrategyTest.java
+++ src/test/java/domain/strategy/ChariotMoveStrategyTest.java
@@ -51,6 +51,23 @@ class ChariotMoveStrategyTest {
     }
 
     @Test
+    @DisplayName("차는 대각선 포인트가 아닌 곳으로 대각선 이동할 수 없다")
+    void chariotShouldNotMoveWhenDiagonallyToNonDiagonalPoint() {
+        // given
+        MoveStrategy strategy = new ChariotMoveStrategy();
+        Map<Position, Piece> boardMapper = new HashMap<>();
+        boardMapper.put(Position.of(1, 3), Piece.of(Team.CHU, Type.CHARIOT, strategy));
+        Board board = Board.of(boardMapper);
+
+        // when
+        Position position = Position.of(1, 3);
+        Position targetPosition = Position.of(2, 4);
+
+        // then
+        Assertions.assertFalse(strategy.canMove(position, targetPosition, board));
+    }
+
+    @Test
     @DisplayName("차의 직선 이동 경로에 기물이 있으면 직선 이동하지 않는다.")
     void chariotShouldNotMoveWhenPieceInPath() {
         // given
