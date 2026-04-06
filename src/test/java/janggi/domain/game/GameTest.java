31006333ff5051271e2f981428f3caa1c606ed9a
bee9827 <ehfrhfo9494@naver.com>
2026-04-06 17:06:22 +0900

    test: Game.move 테스트 추가

--- src/test/java/janggi/domain/game/GameTest.java
+++ src/test/java/janggi/domain/game/GameTest.java
@@ -1,29 +1,69 @@
 package janggi.domain.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import janggi.domain.board.setup.BoardSetUp;
 import janggi.domain.board.setup.InElephantSetUp;
 import janggi.domain.piece.unit.Soldier;
 import janggi.domain.point.Point;
+import janggi.domain.side.Side;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
 
+    private Game game;
 
-    @Test
-    @DisplayName("제자리 이동을 하면 턴을 넘긴다.")
-    void move() {
-        Point choPoint = new Point(0, 0);
-        BoardSetUp boardSetUp = InElephantSetUp.INSTANCE;
-        Game game = Game.createGame(boardSetUp, boardSetUp);
+    @BeforeEach
+    void setUp() {
+        game = Game.createGame(
+                InElephantSetUp.INSTANCE,
+                InElephantSetUp.INSTANCE
+        );
+    }
 
-        game.move(choPoint, choPoint);
+    @Nested
+    class Move {
+        @Test
+        @DisplayName("제자리 이동을 하면 턴을 넘긴다.")
+        void move() {
+            Point choPoint = new Point(0, 0);
+            BoardSetUp boardSetUp = InElephantSetUp.INSTANCE;
+            Game game = Game.createGame(boardSetUp, boardSetUp);
 
-        assertThat(game.isTurnPiece(choPoint)).isFalse();
+            game.move(choPoint, choPoint);
+
+            assertThat(game.isTurnPiece(choPoint)).isFalse();
+        }
+
+        @Test
+        @DisplayName("이동에 성공하면 턴을 바꾼다.")
+        void switchTurnWhenMove() {
+            // given
+            Point point = new Point(3, 0);
+
+            // when
+            game.move(point, point);
+
+            // then
+            assertThat(game.getTurn()).isEqualTo(Side.HAN);
+        }
+
+        @Test
+        @DisplayName("이동할 수 없는 위치라면 예외를 던진다.")
+        void throwExceptionWhenNotDestination() {
+            // given
+            Point from = new Point(3, 0);
+            Point invalidTo = new Point(5, 5);
+
+            // when & then
+            assertThatThrownBy(() -> game.move(from, invalidTo))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
     @Nested
