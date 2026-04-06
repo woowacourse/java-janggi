10c39c0df4dc0d5bbf29f5df62440d3bf625514d
khcho96 <khcho1492@gmail.com>
2026-04-06 19:25:52 +0900

    refactor(Position): getter 삭제

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -55,9 +55,6 @@ public class Position {
         return x * 31 + y;
     }
 
-    public int getX() { return x; }
-    public int getY() { return y; }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
