bac5d4b501f662d093977b5fe4451f3790c9c4fb
haeyoon1 <hynam1220@naver.com>
2026-04-06 18:20:27 +0900

    feat: 게임 시작 모드 관련 enum 생성 및 입력 로직 생성

--- src/main/java/janggi/view/InputView.java
+++ src/main/java/janggi/view/InputView.java
@@ -6,6 +6,16 @@ public class InputView {
 
     public static final Scanner scanner = new Scanner(System.in);
 
+    public static String askGameMode() {
+        System.out.println("새로운 게임을 시작할 시에는 new, 이전 게임을 불러오기 위해서는 load를 입력해주세요.");
+        return scanner.nextLine();
+    }
+
+    public static String askGameId() {
+        System.out.println("불러올 게임의 id를 입력해주세요.");
+        return scanner.nextLine();
+    }
+
     public static String askFromPosition() {
         System.out.println("옮길 기물의 현재 좌표를 x,y 형식으로 입력해주세요.");
         return scanner.nextLine();
