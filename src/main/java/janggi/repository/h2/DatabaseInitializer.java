ebc0fc78cacc05c6aac41d9acc2e09d328a201e9
bee9827 <ehfrhfo9494@naver.com>
2026-04-06 14:30:00 +0900

    refactor: DBInitilizer NullException 발생하지 않도록 수정

--- src/main/java/janggi/repository/h2/DatabaseInitializer.java
+++ src/main/java/janggi/repository/h2/DatabaseInitializer.java
@@ -1,10 +1,10 @@
 package janggi.repository.h2;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.Statement;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class DatabaseInitializer {
@@ -17,20 +17,27 @@ public class DatabaseInitializer {
         try (Connection conn = DataSource.getConnection();
              Statement stmt = conn.createStatement()) {
 
-            String sql = new BufferedReader(
-                    new InputStreamReader(
-                            Objects.requireNonNull(DatabaseInitializer.class
-                                    .getClassLoader()
-                                    .getResourceAsStream(DIR))
-                    )
-            ).lines().collect(Collectors.joining("\n"));
+            InputStream resourceAsStream = readFile();
 
-            stmt.execute(sql);
+            String sql = new BufferedReader(new InputStreamReader(resourceAsStream))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
 
-        } catch (NullPointerException e) {
-            throw new IllegalStateException("파일 찾기 실패~");
+            stmt.executeUpdate(sql);
+
         } catch (Exception e) {
             throw new IllegalStateException("DB 초기화 실패", e);
         }
+    }
+
+    private static InputStream readFile() {
+        InputStream resourceAsStream = DatabaseInitializer.class
+                .getClassLoader()
+                .getResourceAsStream(DIR);
+
+        if (resourceAsStream == null) {
+            throw new IllegalStateException("파일 찾기 실패~");
+        }
+        return resourceAsStream;
     }
 }
