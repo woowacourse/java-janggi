8cfa6fb225fc78bcfbf0cb9a41cde2e678400628
최용준 <yj9107vv@gmail.com>
2026-04-06 14:06:49 +0900

    feat: 장군/사 기물은 궁성 외부로 움직일 수 없게 예외 추가
    - GeneralMoveStrategy, GuardMoveStrategy 두 클래스 isPalace() 메서드 추가
    - isPalace() 목적지가 궁성 외부일 시 예외를 던짐
    - canMoveTo()에서 isPalace() 범위 검증 후 목적지 이동 가능한지로 변경

--- src/main/java/domain/strategy/GeneralMoveStrategy.java
+++ src/main/java/domain/strategy/GeneralMoveStrategy.java
@@ -5,6 +5,28 @@ import java.util.List;
 
 public class GeneralMoveStrategy extends MoveStrategy {
 
+    private static final String ERROR_GENERAL_CAN_MOVE_ONLY_WITHIN_PALACE = "[ERROR] 장군은 궁성 내부에서만 움직일 수 있다.";
+
+    @Override
+    public boolean canMoveTo(Position currentPosition, Position destination) {
+        if (isPalace(destination)) {
+            return createDestinations(currentPosition).contains(destination);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasValidPathTo(Position currentPosition, Position destination, List<Position> occupiedPositions) {
+        return true;
+    }
+
+    private boolean isPalace(Position destination) {
+        if (!palace.isPalaceRedArea(destination) && !palace.isPalaceGreenArea(destination)) {
+            throw new IllegalArgumentException(ERROR_GENERAL_CAN_MOVE_ONLY_WITHIN_PALACE);
+        }
+        return true;
+    }
+
     private List<Position> createDestinations(Position currentPosition) {
         return List.of(
                 currentPosition.right(),
@@ -12,15 +34,5 @@ public class GeneralMoveStrategy extends MoveStrategy {
                 currentPosition.up(),
                 currentPosition.left()
         );
-    }
-
-    @Override
-    public boolean canMoveTo(Position currentPosition, Position destination) {
-        return createDestinations(currentPosition).contains(destination);
-    }
-
-    @Override
-    public boolean hasValidPathTo(Position currentPosition, Position destination, List<Position> occupiedPositions) {
-        return true;
     }
 }
--- src/main/java/domain/strategy/GuardMoveStrategy.java
+++ src/main/java/domain/strategy/GuardMoveStrategy.java
@@ -5,6 +5,28 @@ import java.util.List;
 
 public class GuardMoveStrategy extends MoveStrategy {
 
+    private static final String ERROR_GUARD_CAN_MOVE_ONLY_WITHIN_PALACE = "[ERROR] 사는 궁성 내부에서만 움직일 수 있다.";
+
+    @Override
+    public boolean canMoveTo(Position currentPosition, Position destination) {
+        if (isPalace(destination)) {
+            return createDestinations(currentPosition).contains(destination);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasValidPathTo(Position currentPosition, Position destination, List<Position> occupiedPositions) {
+        return true;
+    }
+
+    private boolean isPalace(Position destination) {
+        if (!palace.isPalaceRedArea(destination) && !palace.isPalaceGreenArea(destination)) {
+            throw new IllegalArgumentException(ERROR_GUARD_CAN_MOVE_ONLY_WITHIN_PALACE);
+        }
+        return true;
+    }
+
     private List<Position> createDestinations(Position currentPosition) {
         return List.of(
                 currentPosition.right(),
@@ -12,15 +34,5 @@ public class GuardMoveStrategy extends MoveStrategy {
                 currentPosition.up(),
                 currentPosition.left()
         );
-    }
-
-    @Override
-    public boolean canMoveTo(Position currentPosition, Position destination) {
-        return createDestinations(currentPosition).contains(destination);
-    }
-
-    @Override
-    public boolean hasValidPathTo(Position currentPosition, Position destination, List<Position> occupiedPositions) {
-        return true;
     }
 }
