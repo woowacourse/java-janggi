9889553a5b99707567a2ffd4a671043d47c019d6
박병훈 <leafloveu@naver.com>
2026-04-06 16:20:40 +0900

    test(cannon): 포는 포를 잡을 수 없음 테스트 추가

--- src/test/java/domain/movestrategy/CannonMoveStrategyTest.java
+++ src/test/java/domain/movestrategy/CannonMoveStrategyTest.java
@@ -105,4 +105,25 @@ class CannonMoveStrategyTest {
         // then
         assertThat(result).isEmpty();
     }
+
+    @Test
+    @DisplayName("포는 포를 잡을 수 없다")
+    void cannonCantCatchOtherCannon() {
+        // given
+        Position from = Position.of(1, 5);
+        Position otherCannonPosition = Position.of(7, 5);
+
+        Map<Position, Piece> pieces = new HashMap<>();
+        pieces.put(from, Piece.choPieceOf(PieceType.CANNON));
+        pieces.put(Position.of(3, 5), Piece.choPieceOf(PieceType.SOLDIER));
+        pieces.put(otherCannonPosition, Piece.hanPieceOf(PieceType.CANNON));
+
+        Board board = Board.init(pieces);
+
+        // when
+        List<Position> result = strategy.getMovablePositions(board, from);
+
+        // then
+        assertThat(result).doesNotContain(otherCannonPosition);
+    }
 }
