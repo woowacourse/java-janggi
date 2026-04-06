0e07374e3bc84abc5a0bcd4dfab948f3b30c65c1
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-04-07 00:51:55 +0900

    fix: 포 이동 검증 추가 구현

--- src/main/java/domain/pieces/Po.java
+++ src/main/java/domain/pieces/Po.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Optional;
 
 import domain.PieceFinder;
+import domain.Position;
 import domain.enums.Country;
 import domain.enums.Direction;
 import domain.enums.PieceType;
-import domain.Position;
 
 public class Po extends Piece {
 
@@ -23,9 +23,9 @@ public class Po extends Piece {
 
     @Override
     public List<Position> getAvailableRoute(Position start, PieceFinder finder) {
-        // TODO: 차의 규칙 더하기 필요
         List<Position> availableRoute = new ArrayList<>();
-        for (Direction direction : Direction.getCardinalDirections()){
+        for (Direction direction : Direction.getCardinalDirections()) {
+            List<Piece> pieces = new ArrayList<>();
             int i = Position.MAX_ROW;
             Position now = start;
             while (i-- > 0) {
@@ -33,45 +33,39 @@ public class Po extends Piece {
                 if (position.isEmpty()) {
                     break;
                 }
+                now = position.get();
                 Piece endPiece = finder.find(position.get());
-                if (endPiece==None.INSTANCE) {
+                if (isNotPo(endPiece) && isDifferentCountry(endPiece.getCountry()) && hasNotPoAmongPath(pieces) && hasOnePieceAmongPath(pieces)) {
                     availableRoute.add(position.get());
-                    now = position.get();
-                    continue;
                 }
-                if (isDifferentCountry(endPiece.getCountry())) {
-                    availableRoute.add(position.get());
-                    break;
-                }
+                pieces.add(endPiece);
             }
-
         }
         return availableRoute;
     }
 
-//    @Override
-//    public boolean isAvailableRoute(List<Piece> pieces, PieceType endPieceType) {
-//        // 중간에 기물 하나인지 && 넘는게 포인지
-//        if (countSameLine(pieces) != 1) {
-//            return false;
-//        }
-//
-//        // 도착 기물이 포인지
-//        return !endPieceType.equals(PieceType.PO);
-//    }
-//
-//    private int countSameLine(List<Piece> pieces) {
-//        int pieceCount = 0;
-//        for (Piece piece : pieces) {
-//            if (piece.getPieceType().equals(PieceType.PO)) {
-//                throw new IllegalArgumentException("포는 포를 넘을 수 없습니다.");
-//            }
-//
-//            if (!piece.equals(None.INSTANCE)) {
-//                pieceCount++;
-//            }
-//        }
-//
-//        return pieceCount;
-//    }
+
+    private boolean isNotPo(Piece piece) {
+        return !(piece.getPieceType() == PieceType.PO);
+    }
+
+    private boolean hasOnePieceAmongPath(List<Piece> pieces) {
+        int count = 0;
+        for (Piece piece : pieces) {
+            if (piece.getPieceType() != PieceType.NONE) {
+                count++;
+            }
+        }
+        return count == 1;
+    }
+
+    private boolean hasNotPoAmongPath(List<Piece> pieces) {
+        for (Piece piece : pieces) {
+            if (piece.getPieceType() == PieceType.PO) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
