c62cb1e64bdca0921e7678550903a59fb3da3cc7
SooHyeon Lee <dalsu4594@naver.com>
2026-04-06 21:30:29 +0900

    test: 궁성(Palace, Palaces) 단위 테스트 추가
    - 포지션이 궁성에 존재하는지 테스트 추가
    - 궁성 위치에 따른 반환값 테스트추가

--- src/test/java/janggi/domain/ScoreTest.java
+++ src/test/java/janggi/domain/ScoreTest.java
@@ -1,19 +1,21 @@
 package janggi.domain;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ScoreTest {
 
     @Test
-    void 점수는_음수가_될_수_없다(){
+    void 점수는_음수가_될_수_없다() {
         assertThatThrownBy(() -> new Score(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("점수는 음수가 될 수 없습니다.");
     }
 
     @Test
-    void 점수끼리_더하면_합산된_점수를_반환한다(){
+    void 점수끼리_더하면_합산된_점수를_반환한다() {
         Score score1 = new Score(1);
         Score score2 = new Score(1);
 
@@ -23,7 +25,7 @@ class ScoreTest {
     }
 
     @Test
-    void 점수가_더_크면_true를_반환한다(){
+    void 점수가_더_크면_true를_반환한다() {
         Score higher = new Score(2);
         Score lower = new Score(1);
 
@@ -33,7 +35,7 @@ class ScoreTest {
     }
 
     @Test
-    void 점수가_더_작으면_false를_반환한다(){
+    void 점수가_더_작으면_false를_반환한다() {
         Score lower = new Score(1);
         Score higher = new Score(2);
 
