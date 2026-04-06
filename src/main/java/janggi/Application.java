fec594858fd7028f985f4898e97417099929e082
haeyoon1 <hynam1220@naver.com>
2026-04-07 00:29:08 +0900

    feat: application 객체 조립 로직 수정

--- src/main/java/janggi/Application.java
+++ src/main/java/janggi/Application.java
@@ -1,11 +1,21 @@
 package janggi;
 
 import janggi.controller.JanggiController;
+import janggi.dao.GameDao;
+import janggi.dao.PieceDao;
+import janggi.service.JanggiService;
+import javax.sql.DataSource;
 
 public class Application {
 
     public static void main(String[] args) {
-        JanggiController janggiController = new JanggiController();
+        DataSource dataSource = DataSourceFactory.createDataSource();
+
+        GameDao gameDao = new GameDao(dataSource);
+        PieceDao pieceDao = new PieceDao(dataSource);
+        JanggiService janggiService = new JanggiService(gameDao, pieceDao);
+        JanggiController janggiController = new JanggiController(janggiService);
+
         janggiController.run();
     }
 }
