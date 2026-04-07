791e8a121da71ea4047c94821ab053c2179de7c6
khcho96 <khcho1492@gmail.com>
2026-04-07 09:45:43 +0900

    test(ChariotTest): 테스트 코드 구현

--- src/test/java/domain/piece/ChariotTest.java
+++ src/test/java/domain/piece/ChariotTest.java
@@ -67,4 +67,30 @@ class ChariotTest {
         assertThat(movable.getPositions()).contains(Position.of(1, 0), Position.of(4, 0), Position.of(5, 0));
         assertThat(movable.getPositions()).doesNotContain(Position.of(6, 0));
     }
+
+    @Test
+    @DisplayName("차는 궁성 대각선으로 연속 이동할 수 있다")
+    void moveDiagonalInPalace() {
+        Position current = Position.of(3, 0);
+        Board board = new Board(Map.of(current, PieceFactory.createChariot(Side.CHO)));
+
+        Destinations movable = board.findDestinations(current);
+
+        assertThat(movable.getPositions()).contains(Position.of(4, 1), Position.of(5, 2));
+    }
+
+    @Test
+    @DisplayName("차는 궁성 대각선 경로에서 아군을 만나면 더 이동할 수 없다")
+    void allyObstacleInPalaceDiagonal() {
+        Position current = Position.of(3, 0);
+        Position ally = Position.of(4, 1);
+        Board board = new Board(Map.of(
+                current, PieceFactory.createChariot(Side.CHO),
+                ally, PieceFactory.createSoldier(Side.CHO)
+        ));
+
+        Destinations movable = board.findDestinations(current);
+
+        assertThat(movable.getPositions()).doesNotContain(Position.of(4, 1), Position.of(5, 2));
+    }
 }
