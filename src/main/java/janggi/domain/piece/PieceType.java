626f0ff5c86f3881bd9eb98a104f5485f8e39143
Chocoding1 <galmeagi2@naver.com>
2026-04-07 19:49:28 +0900

    refactor: 기물 점수 추가

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -1,22 +1,28 @@
 package janggi.domain.piece;
 
 public enum PieceType {
-    CHA("차"),
-    GUNG("궁"),
-    JOL("졸"),
-    MA("마"),
-    PO("포"),
-    SA("사"),
-    SANG("상"),
+    CHA("차", 13),
+    GUNG("궁", 0),
+    JOL("졸", 2),
+    MA("마", 5),
+    PO("포", 7),
+    SA("사", 3),
+    SANG("상", 3),
     ;
 
-    PieceType(String name) {
+    PieceType(String name, int score) {
         this.name = name;
+        this.score = score;
     }
 
     private final String name;
+    private final int score;
 
     public String getName() {
         return name;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
