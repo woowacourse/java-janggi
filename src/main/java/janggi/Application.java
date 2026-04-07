b35018b35ba5e3f7724d3d7a023ac9e14db5e06b
mvg01 <dkms625@naver.com>
2026-04-07 23:11:54 +0900

    feat: 애플리케이션 시작 시 DB 테이블을 초기화하는 클래스 추가

--- src/main/java/janggi/Application.java
+++ src/main/java/janggi/Application.java
@@ -1,10 +1,13 @@
 package janggi;
 
+import static janggi.db.InitDatabaseTable.initDatabaseTable;
+
 import janggi.controller.JanggiController;
 
 public class Application {
 
     public static void main(String[] args) {
+        initDatabaseTable();
         final JanggiController janggiController = new JanggiController();
         janggiController.run();
     }
---
+++ src/main/java/janggi/db/InitDatabaseTable.java
@@ -0,0 +1,21 @@
+package janggi.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InitDatabaseTable {
+    private static final String sql1 = "CREATE TABLE IF NOT EXISTS game (id INT AUTO_INCREMENT PRIMARY KEY, current_turn VARCHAR(10));";
+    private static final String sql2 = "CREATE TABLE IF NOT EXISTS piece (id INT AUTO_INCREMENT PRIMARY KEY, game_id INT, position_row INT, position_column INT, piece_type VARCHAR(50), team_type VARCHAR(50), FOREIGN KEY (game_id) REFERENCES game(id));";
+
+    public static void initDatabaseTable() {
+        try (Connection connection = DatabaseConnector.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.execute(sql1);
+            statement.execute(sql2);
+        } catch (SQLException e) {
+            throw new RuntimeException("데이터베이스 오류");
+        }
+    }
+}
