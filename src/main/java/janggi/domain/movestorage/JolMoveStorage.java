6068f36ae46a3f3296629a1fba253bf67c2ba272
구민서 <minseo12379@gmail.com>
2026-04-07 20:49:48 +0900

    refactor: JolMoveStorage 내 메서드 depth 감소

--- src/main/java/janggi/domain/movestorage/JolMoveStorage.java
+++ src/main/java/janggi/domain/movestorage/JolMoveStorage.java
@@ -4,38 +4,38 @@ import janggi.domain.BoardView;
 import janggi.domain.Position;
 import janggi.domain.Team;
 
-import java.util.List;
-
 public class JolMoveStorage implements MoveStorage{
     private static final int HAN_FORWARD = 1;
     private static final int CHO_FORWARD = -1;
     public static final int NEXT_TO = 1;
 
     @Override
-    public boolean canMove(Position from, Position to, BoardView boardState) {
-        int fromRow = from.getRowValue();
-        int fromColumn = from.getColumnValue();
-        int toRow = to.getRowValue();
-        int toColumn = to.getColumnValue();
+    public boolean canMove(Position from, Position to, BoardView boardView) {
+        Team team = boardView.getPieceAt(from).getTeam();
 
-        if (Math.abs(fromRow - toRow) == NEXT_TO && fromColumn == toColumn) {
+        if (isSidewaysMove(from, to)) {
             return true;
         }
 
-        Team currentTeam = boardState.getPieceAt(from).getTeam();
-
-        if (currentTeam == Team.HAN) {
-            if (toColumn - fromColumn == HAN_FORWARD && fromRow == toRow) {
-                return true;
-            }
+        if (isForwardMove(from, to, team)) {
+            return true;
         }
 
-        if (currentTeam == Team.CHO) {
-            if (toColumn - fromColumn == CHO_FORWARD && fromRow == toRow) {
-                return true;
-            }
-        }
-        
         return false;
+    }
+
+    private boolean isSidewaysMove(Position from, Position to) {
+        return from.getColumnValue() == to.getColumnValue() && Math.abs(from.getRowValue() - to.getRowValue()) == NEXT_TO;
+    }
+
+    private boolean isForwardMove(Position from, Position to, Team team) {
+        int columnDiff = to.getColumnValue() - from.getColumnValue();
+        boolean isSameRow = from.getRowValue() == to.getRowValue();
+
+        if (team == Team.HAN) {
+            return isSameRow && columnDiff == HAN_FORWARD;
+        }
+
+        return isSameRow && columnDiff == CHO_FORWARD;
     }
 }
