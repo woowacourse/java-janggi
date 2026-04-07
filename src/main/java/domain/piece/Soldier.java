335c73cebd4f78cb3c0f3ad1f2e0183913ec4456
khcho96 <khcho1492@gmail.com>
2026-04-07 14:15:38 +0900

    feat(Soldier): 기물 점수 계산 기능 구현

--- src/main/java/domain/piece/Soldier.java
+++ src/main/java/domain/piece/Soldier.java
@@ -23,4 +23,9 @@ public class Soldier extends Piece {
     public String getName() {
         return pieceType.getName();
     }
+
+    @Override
+    public int getScore() {
+        return pieceType.getScore();
+    }
 }
