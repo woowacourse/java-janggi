7a29f9d7cb968d11c2856b466b275cbbed7e59ad
snow___shower <wonsb25@gmail.com>
2026-04-07 16:24:49 +0900

    feat: 게임 시작 흐름 제어 및 DB 연동
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,10 +1,9 @@
 package application;
 
+import dao.GameDAO;
 import domain.game.Game;
 import domain.piece.Camp;
-import view.GameCommand;
-import view.InputView;
-import view.OutputView;
+import view.*;
 
 import java.util.NoSuchElementException;
 
@@ -17,18 +16,49 @@ public class Application {
     }
 
     public void run() {
-        outputView.printSetUpOptions();
-        int hanSetUp = readSetUp(Camp.HAN);
-        int choSetUp = readSetUp(Camp.CHO);
 
-        Game game = new Game(choSetUp, hanSetUp);
-        outputView.printBoard(game.board());
+        GameDAO gameDAO = new GameDAO();
+        gameDAO.initTables();
 
-        playJanggi(game);
+        String menu = readValidMainMenu();
+
+        if ("1".equals(menu)) {
+            startNewGame(gameDAO);
+            return;
+        }
+        loadPreviousGame(gameDAO);
     }
 
-    private void playJanggi(Game game) {
-        progressJanggi(game);
+    private void startJanggi(Game game, int gameId, GameDAO gameDAO) {
+        outputView.printBoard(game.board());
+        playJanggi(game, gameId, gameDAO);
+    }
+
+    private void startNewGame(GameDAO gameDAO) {
+        outputView.printSetUpOptions();
+        int hanSetup = readSetUp(Camp.HAN);
+        int choSetup = readSetUp(Camp.CHO);
+
+        Game game = new Game(choSetup, hanSetup);
+        int gameId = gameDAO.save(game);
+
+        outputView.printNewGameStart(gameId);
+
+        startJanggi(game, gameId, gameDAO);
+    }
+
+    private void loadPreviousGame(GameDAO gameDAO) {
+        int gameId = readValidGameId();
+
+        Game game = gameDAO.findBy(gameId);
+
+        outputView.printLoadPreviousGame(gameId);
+
+        startJanggi(game, gameId, gameDAO);
+    }
+
+    private void playJanggi(Game game, int gameId, GameDAO gameDAO) {
+        progressJanggi(game, gameId, gameDAO);
 
         if (game.isScoreWin()) {
             outputView.printScore(game.choScore(), game.hanScore());
@@ -36,13 +66,13 @@ public class Application {
         outputView.printWinner(game.winner());
     }
 
-    private void progressJanggi(Game game) {
+    private void progressJanggi(Game game, int gameId, GameDAO gameDAO) {
         while (!game.isFinished()) {
             outputView.printTurnPrompt(game.currentTurn());
 
             try {
                 GameCommand command = inputView.readCommand();
-                execute(game, command);
+                execute(game, command, gameId, gameDAO);
                 outputView.printBoard(game.board());
             } catch (IllegalArgumentException | NoSuchElementException exception) {
                 outputView.printError(exception.getMessage());
@@ -64,7 +94,38 @@ public class Application {
         }
     }
 
-    private void execute(Game game, GameCommand command) {
+    private void execute(Game game, GameCommand command, int gameId, GameDAO gameDAO) {
         command.execute(game);
+
+        if (command instanceof MoveCommand moveCommand) {
+            gameDAO.updateMove(gameId, game, moveCommand.from(), moveCommand.to());
+            return;
+        }
+
+        if (command instanceof PassCommand) {
+            gameDAO.updateGameStatus(gameId, game);
+        }
+    }
+
+    private String readValidMainMenu() {
+        while (true) {
+            outputView.printMainMenu();
+            try {
+                return inputView.readMainMenu();
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private int readValidGameId() {
+        while (true) {
+            outputView.printLoadGameMenu();
+            try {
+                return inputView.readGameId();
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
     }
 }
