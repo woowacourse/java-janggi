e900664beff969754e20022c3461abefdb840a34
khcho96 <khcho1492@gmail.com>
2026-04-07 14:16:06 +0900

    feat(Elephant): 기물 점수 계산 기능 구현

--- src/main/java/domain/piece/Elephant.java
+++ src/main/java/domain/piece/Elephant.java
@@ -23,4 +23,9 @@ public class Elephant extends Piece {
     public String getName() {
         return pieceType.getName();
     }
+
+    @Override
+    public int getScore() {
+        return pieceType.getScore();
+    }
 }
