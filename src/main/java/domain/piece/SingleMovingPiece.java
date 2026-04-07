d3a2da29e516ae7db5fe63fc62c8e7bc2d30b324
Jihyun3478 <leejihyun3478@gmail.com>
2026-04-07 11:30:07 +0900

    fix: 출발지와 동일하게 목적지도 궁성 내부인지 확인하도록 수정

--- src/main/java/domain/piece/SingleMovingPiece.java
+++ src/main/java/domain/piece/SingleMovingPiece.java
@@ -20,7 +20,7 @@ public abstract class SingleMovingPiece extends Piece {
         Distance distance = from.calculateDistance(to);
         List<Direction> directions = Direction.findDirections(distance.x(), distance.y());
 
-        if (from.isInPalace(pieceInfo.country())) {
+        if (from.isInPalace(pieceInfo.country()) && to.isInPalace(pieceInfo.country())) {
             validateDirectionsInPalace(directions);
             return directions;
         }
