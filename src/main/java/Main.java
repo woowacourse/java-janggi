90421ef47762cae8901ba55e38b1842678330e9d
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-04-07 21:51:11 +0900

    feat: H2 DB를 붙이기 위한 테이블 생성 쿼리문 및 설정 클래스 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import config.DataSourceConfig;
+import domain.piece.PieceRepository;
 import janggigame.JanggiGame;
+import janggigame.JanggiGameRepository;
+import util.SchemaInitializer;
+
+import javax.sql.DataSource;
 
 public class Main {
     public static void main(String[] args) {
-        JanggiGame janggiGame = new JanggiGame();
+        DataSource dataSource = DataSourceConfig.dataSource();
+        SchemaInitializer.initialize(dataSource);
+        JanggiGameRepository janggiGameRepository = new JanggiGameRepository(dataSource);
+        PieceRepository pieceRepository = new PieceRepository(dataSource);
+        JanggiGame janggiGame = new JanggiGame(janggiGameRepository, pieceRepository);
         janggiGame.run();
     }
 }
