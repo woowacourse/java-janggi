6fcb6204b3b1f5f6d4ffcaeb20dfc738423039fa
khcho96 <khcho1492@gmail.com>
2026-04-07 14:15:00 +0900

    feat(PieceType): 기물 점수 구현

--- src/main/java/domain/piece/PieceType.java
+++ src/main/java/domain/piece/PieceType.java
@@ -1,22 +1,28 @@
 package domain.piece;
 
 public enum PieceType {
-    GENERAL("궁"),
-    CHARIOT("차"),
-    CANNON("포"),
-    HORSE("마"),
-    ELEPHANT("상"),
-    GUARD("사"),
-    SOLDIER("졸"),
+    GENERAL("궁", 0),
+    CHARIOT("차", 13),
+    CANNON("포", 7),
+    HORSE("마", 5),
+    ELEPHANT("상", 3),
+    GUARD("사", 3),
+    SOLDIER("졸", 2),
     ;
 
     private final String name;
+    private final int score;
 
-    PieceType(String name) {
+    PieceType(String name, int score) {
         this.name = name;
+        this.score = score;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
