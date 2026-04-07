8b1377ff3e12ee69b3d4e647be83ab1db7afe17f
khcho96 <khcho1492@gmail.com>
2026-04-07 09:42:44 +0900

    feat(CompositeMovementStrategy):다중 기물 이동 전략 클래스 구현

---
+++ src/main/java/domain/strategy/CompositeMovementStrategy.java
@@ -0,0 +1,19 @@
+package domain.strategy;
+
+import domain.Position;
+import java.util.List;
+
+public class CompositeMovementStrategy implements MovementStrategy {
+    private final List<MovementStrategy> strategies;
+
+    public CompositeMovementStrategy(List<MovementStrategy> strategies) {
+        this.strategies = List.copyOf(strategies);
+    }
+
+    @Override
+    public List<Path> generatePaths(Position current) {
+        return strategies.stream()
+                .flatMap(strategy -> strategy.generatePaths(current).stream())
+                .toList();
+    }
+}
