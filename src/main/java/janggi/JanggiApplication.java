7b91c09c7de89e89e97e1a6e2b326c8ea555e68a
MODUGGAGI <kiroro0814@naver.com>
2026-04-07 09:33:47 +0900

    feat: AppConfig 도입
    - 의존관계를 주입해주는 설정 클래스를 생성했습니다.

--- src/main/java/janggi/JanggiApplication.java
+++ src/main/java/janggi/JanggiApplication.java
@@ -1,26 +1,18 @@
 package janggi;
 
+import janggi.config.AppConfig;
+import janggi.db.ConnectionManager;
 import janggi.db.DatabaseInitializer;
-import janggi.db.H2ConnectionManager;
-import janggi.db.TransactionManager;
-import janggi.repository.GamePieceRepository;
-import janggi.repository.GameStateRepository;
-import janggi.service.GameService;
-import janggi.view.InputView;
-import janggi.view.OutputView;
-import java.util.Scanner;
 
 public class JanggiApplication {
-    public static void main(String[] args) {
-        H2ConnectionManager connectionManager = new H2ConnectionManager();
-        new DatabaseInitializer(connectionManager).initialize();
-        TransactionManager transactionManager = new TransactionManager(connectionManager);
 
-        GameRunner janggi = new GameRunner(
-                new InputView(new Scanner(System.in)),
-                new OutputView(),
-                new GameService(transactionManager, new GameStateRepository(), new GamePieceRepository())
-        );
-        janggi.run();
+    public static void main(String[] args) {
+        AppConfig config = new AppConfig();
+
+        ConnectionManager connectionManager = config.connectionManager();
+        new DatabaseInitializer(connectionManager).initialize();
+
+        GameRunner gameRunner = config.gameRunner();
+        gameRunner.run();
     }
 }
---
+++ src/main/java/janggi/config/AppConfig.java
@@ -0,0 +1,38 @@
+package janggi.config;
+
+import janggi.GameRunner;
+import janggi.db.ConnectionManager;
+import janggi.db.TransactionManager;
+import janggi.repository.GamePieceDaoImpl;
+import janggi.repository.GameRepository;
+import janggi.repository.GameStateDaoImpl;
+import janggi.view.InputView;
+import janggi.view.OutputView;
+import java.util.Scanner;
+
+public class AppConfig {
+
+    private static final String URL = "jdbc:h2:file:./data/janggi";
+    private static final String USER = "stark";
+    private static final String PASSWORD = "stark123!";
+
+    public GameRunner gameRunner() {
+        return new GameRunner(
+                new InputView(new Scanner(System.in)),
+                new OutputView(),
+                gameRepository()
+        );
+    }
+
+    private GameRepository gameRepository() {
+        return new GameRepository(transactionManager(), new GameStateDaoImpl(), new GamePieceDaoImpl());
+    }
+
+    private TransactionManager transactionManager() {
+        return new TransactionManager(connectionManager());
+    }
+
+    public ConnectionManager connectionManager() {
+        return new ConnectionManager(URL, USER, PASSWORD);
+    }
+}
