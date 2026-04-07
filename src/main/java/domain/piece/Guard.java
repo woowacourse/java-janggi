5beecefc08915fc0f8135c6fef5f9aaec4b42aa5
khcho96 <khcho1492@gmail.com>
2026-04-07 14:16:22 +0900

    feat(Guard): 기물 점수 계산 기능 구현

--- src/main/java/domain/piece/Guard.java
+++ src/main/java/domain/piece/Guard.java
@@ -23,4 +23,9 @@ public class Guard extends Piece {
     public String getName() {
         return pieceType.getName();
     }
+
+    @Override
+    public int getScore() {
+        return pieceType.getScore();
+    }
 }
