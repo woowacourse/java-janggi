56ea303a37ba50a17adda863d7e468fa2d63417a
khcho96 <khcho1492@gmail.com>
2026-04-07 09:43:18 +0900

    feat(PalaceDiagonalContinuousStrategy): 차,포 궁성 대각선 이동 전략 클래스 구현

---
+++ src/main/java/domain/strategy/PalaceDiagonalContinuousStrategy.java
@@ -0,0 +1,32 @@
+package domain.strategy;
+
+import domain.Position;
+import domain.Palace;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PalaceDiagonalContinuousStrategy implements MovementStrategy {
+    @Override
+    public List<Path> generatePaths(Position current) {
+        return Palace.diagonals(current).stream()
+                .map(direction -> createPath(current, direction))
+                .flatMap(Optional::stream)
+                .toList();
+    }
+
+    private Optional<Path> createPath(Position current, Direction direction) {
+        List<Position> positions = new ArrayList<>();
+        Position cursor = current;
+
+        while (Palace.canMoveDiagonal(cursor, direction)) {
+            cursor = cursor.move(direction);
+            positions.add(cursor);
+        }
+
+        if (positions.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(new Path(positions));
+    }
+}
