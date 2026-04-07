52133ce9b128b345fef635d14be045264c046181
choubung <choubung@sookmyung.ac.kr>
2026-04-07 12:44:39 +0900

    test : 왕 배치를 위한 헬퍼 메서드 작성

--- src/test/java/janggi/domain/JanggiGameTest.java
+++ src/test/java/janggi/domain/JanggiGameTest.java
@@ -1,9 +1,9 @@
 package janggi.domain;
 
 import janggi.domain.board.Board;
-import janggi.domain.board.BoardInitializer;
 import janggi.domain.piece.King;
-import janggi.domain.piece.Tank;
+import janggi.domain.piece.Piece;
+import janggi.domain.piece.Soldier;
 import janggi.domain.piece.Team;
 import janggi.domain.vo.position.Position;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,15 +38,13 @@ class JanggiGameTest {
 
     @Test
     void 연속_턴넘김_종료_테스트() {
-        // given: 보드에 기물을 적절히 배치 (초: 차(12점), 한: 빈 보드 + 덤(1.5점))
-        board = Board.createBoardWith(
-                new Position(1, 4), new King(Team.HAN),
-                new Position(8, 4), new King(Team.CHO),
-                new Position(9, 0), new Tank(Team.CHO)
+        // given (초: 졸(2점), 한: 빈 보드 + 덤(1.5점))
+        board = createBoardWithKings(
+                new Position(9, 0), new Soldier(Team.CHO)
         );
         janggiGame = new JanggiGame(board, scoreBoard);
 
-        // when: 양 팀 연속 skip
+        // when
         janggiGame.skipTurn(); // 초(CHO) 턴 쉼
         janggiGame.changeTurn();
         janggiGame.skipTurn(); // 한(HAN) 턴 쉼
@@ -55,13 +53,13 @@ class JanggiGameTest {
         assertThat(janggiGame.isFinished()).isTrue();
 
         janggiGame.decideWinner();
-        assertThat(janggiGame.getWinner()).isEqualTo(Team.CHO); // 12.0 vs 1.5
+        assertThat(janggiGame.getWinner()).isEqualTo(Team.CHO); // 2.0 vs 1.5
     }
 
     @Test
     void 연속_기권_아닌_경우_게임_정상_진행_테스트() {
         // given
-        board = new Board(BoardInitializer.createBoard());
+        board = createBoardWithKings();
         janggiGame = new JanggiGame(board, scoreBoard);
 
         // when
@@ -76,7 +74,7 @@ class JanggiGameTest {
     @Test
     void 기권패_테스트() {
         // given
-        janggiGame = new JanggiGame(new Board(), scoreBoard);
+        janggiGame = new JanggiGame(createBoardWithKings(), scoreBoard);
 
         // when
         janggiGame.resign();
@@ -84,5 +82,18 @@ class JanggiGameTest {
         // then
         assertThat(janggiGame.isFinished()).isTrue();
         assertThat(janggiGame.getWinner()).isEqualTo(Team.HAN);
+    }
+
+    private Board createBoardWithKings(Object... extraPieces) {
+        Board board = Board.createBoardWith(
+                new Position(1, 4), new King(Team.HAN),
+                new Position(8, 4), new King(Team.CHO)
+        );
+
+        for (int i = 0; i < extraPieces.length; i += 2) {
+            board.place((Position) extraPieces[i], (Piece) extraPieces[i + 1]);
+        }
+
+        return board;
     }
 }
