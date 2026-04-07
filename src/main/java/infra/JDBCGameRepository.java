29635768e1193f3e5ce1a5d4a6546c43b9f0b1d4
나의찬 <cab07036@naver.com>
2026-04-07 14:52:19 +0900

    fix(GameRepository): Game에서 조회시에 ResultSet 오류 수정

--- src/main/java/infra/JDBCGameRepository.java
+++ src/main/java/infra/JDBCGameRepository.java
@@ -56,10 +56,11 @@ public class JDBCGameRepository implements GameRepository {
     public boolean isFinished(Long gameId) {
         String sql = "SELECT is_finished FROM game WHERE id = ?";
 
-        try(Connection connection = JDBCContext.getConnection();
-            PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (Connection connection = JDBCContext.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, gameId);
             ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
             return resultSet.getBoolean("is_finished");
         } catch (SQLException e) {
             throw new RuntimeException("조회 실패", e);
@@ -70,10 +71,11 @@ public class JDBCGameRepository implements GameRepository {
     public Optional<String> findCurrentTurnById(Long gameId) {
         String sql = "SELECT current_turn FROM game WHERE id = ?";
 
-        try(Connection connection = JDBCContext.getConnection();
-            PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (Connection connection = JDBCContext.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, gameId);
             ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
             return Optional.of(resultSet.getString("current_turn"));
         } catch (SQLException e) {
             throw new RuntimeException("조회 실패", e);
