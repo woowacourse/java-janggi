e8d6e59e941fdefbc3435cbb3e018edda0ab65b1
minzzun99 <skg06123@naver.com>
2026-04-07 13:02:51 +0900

    feat: game_state 테이블 내부 날짜, 종료 상태, 각 나라별 스코어 추가

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,8 +1,12 @@
 
 CREATE TABLE IF NOT EXISTS game_state (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    turn VARCHAR(10)
-    );
+    turn VARCHAR(10),
+    is_finished BOOLEAN DEFAULT FALSE,
+    cho_score DOUBLE DEFAULT 0.0,
+    han_score DOUBLE DEFAULT 0.0,
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 CREATE TABLE IF NOT EXISTS piece_state (
     id INT AUTO_INCREMENT PRIMARY KEY,game_id INT,
@@ -11,4 +15,4 @@ CREATE TABLE IF NOT EXISTS piece_state (
     row_pos INT,
     col_pos INT,
     FOREIGN KEY (game_id) REFERENCES game_state(id) ON DELETE CASCADE
-    );
+);
