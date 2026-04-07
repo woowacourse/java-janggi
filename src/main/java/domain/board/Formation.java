b161714cd4680403dab3c675ed739586761e5562
poketopa <lhs5427ll@gmail.com>
2026-04-07 14:51:47 +0900

    refactor (domain): 예외 발생 시 에러메시지 추가

--- src/main/java/domain/board/Formation.java
+++ src/main/java/domain/board/Formation.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Formation {
-
     HORSE_ELEPHANT_HORSE_ELEPHANT(1, List.of(2, 7), List.of(1, 6)),
     HORSE_ELEPHANT_ELEPHANT_HORSE(2, List.of(2, 6), List.of(1, 7)),
     ELEPHANT_HORSE_ELEPHANT_HORSE(3, List.of(1, 6), List.of(2, 7)),
@@ -25,7 +24,7 @@ public enum Formation {
         return Arrays.stream(Formation.values())
                 .filter(formation -> formation.formatNumber == formatNumber)
                 .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상차림 번호입니다."));
     }
 
     public List<Integer> elephantFormations() {
@@ -35,5 +34,4 @@ public enum Formation {
     public List<Integer> horseFormations() {
         return horseFormations;
     }
-
 }
--- src/main/java/domain/move/Directions.java
+++ src/main/java/domain/move/Directions.java
@@ -1,10 +1,9 @@
-package domain.piece.move;
+package domain.move;
 
 import domain.point.Point;
 import java.util.List;
 
 public class Directions {
-
     private final List<Direction> directions;
 
     public Directions(List<Direction> directions) {
@@ -12,11 +11,10 @@ public class Directions {
     }
 
     public List<Point> findPoints(Point from, Point to) {
-        return directions.stream()
+        return List.copyOf(directions.stream()
                 .filter(direction -> direction.canReach(from, to))
                 .findFirst()
                 .map(direction -> direction.getPoints(from))
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new IllegalArgumentException("해당 기물이 이동할 수 없는 위치/방향입니다.")));
     }
-
 }
