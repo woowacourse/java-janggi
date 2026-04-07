192dec09d9505ad946b0024537c07f952b42343c
choubung <choubung@sookmyung.ac.kr>
2026-04-07 12:45:10 +0900

    feat : 무승부 점수 계산을 위한 ScoreBoard 작성

---
+++ src/main/java/janggi/domain/ScoreBoard.java
@@ -0,0 +1,47 @@
+package janggi.domain;
+
+import janggi.domain.piece.Piece;
+import janggi.domain.piece.PieceType;
+import janggi.domain.piece.Team;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ScoreBoard {
+    private final Map<PieceType, Double> POINTS = new HashMap<>() {
+        {
+            put(PieceType.TANK, 12.0);
+            put(PieceType.CANNON, 7.0);
+            put(PieceType.HORSE, 5.0);
+            put(PieceType.ELEPHANT, 3.0);
+            put(PieceType.ADVISOR, 3.0);
+            put(PieceType.SOLDIER, 2.0);
+        }
+    };
+    private double hanScore = 0;
+    private double choScore = 0;
+
+    public void saveHanScore(List<Piece> pieces) {
+        hanScore = calculateScore(pieces) + 1.5;
+    }
+
+    public void saveChoScore(List<Piece> pieces) {
+        choScore = calculateScore(pieces);
+    }
+
+    public Team winner() {
+        if (hanScore > choScore) {
+            return Team.HAN;
+        }
+
+        return Team.CHO;
+    }
+
+    private double calculateScore(List<Piece> pieces) {
+        return pieces.stream()
+                .map(Piece::pieceType)
+                .mapToDouble(type -> POINTS.getOrDefault(type, 0.0))
+                .sum();
+    }
+}
