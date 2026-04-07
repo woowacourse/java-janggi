d65cf86fa9e9cbd87c55e4203ce2b7578841fb85
Jihyun3478 <leejihyun3478@gmail.com>
2026-04-07 13:54:46 +0900

    refactor: movePiece()가 MoveResult DTO를 반환하도록 수정

--- src/main/java/controller/JanggiController.java
+++ src/main/java/controller/JanggiController.java
@@ -6,7 +6,7 @@ import domain.board.Board;
 import domain.board.Country;
 import domain.board.Position;
 import domain.board.TableSetting;
-import java.util.ArrayList;
+import dto.MoveResult;
 import java.util.List;
 import java.util.Optional;
 import service.GameService;
@@ -68,13 +68,10 @@ public class JanggiController {
             outputView.printCurrentScores(board.calculateScore());
             outputView.printBoard(board.getPieceInfos());
 
-            List<Position> positions = new ArrayList<>();
-            boolean isGeneralCaught = movePiece(board, country, positions);
-            Position from = positions.get(0);
-            Position to = positions.get(1);
-            gameService.updateBoard(gameId, from, to, board.getPieceInfos());
+            MoveResult moveResult = movePiece(board, country);
+            gameService.updateBoard(gameId, moveResult.from(), moveResult.to(), board.getPieceInfos());
 
-            if (isGeneralCaught) {
+            if (moveResult.isGeneralCaught()) {
                 gameService.finishGame(gameId);
                 outputView.printWinner(CountryFormatter.from(country), CountryFormatter.from(otherSide));
                 return;
@@ -95,7 +92,7 @@ public class JanggiController {
         }
     }
 
-    private boolean movePiece(Board board, Country country, List<Position> positions) {
+    private MoveResult movePiece(Board board, Country country) {
         while (true) {
             try {
                 Position from = makeFromPosition();
@@ -105,9 +102,7 @@ public class JanggiController {
                 from.validatePositions(to);
 
                 boolean isGeneralCaught = board.move(from, to);
-                positions.add(from);
-                positions.add(to);
-                return isGeneralCaught;
+                return MoveResult.of(from, to, isGeneralCaught);
             } catch (IllegalArgumentException exception) {
                 outputView.printErrorMessage(exception.getMessage());
             }
