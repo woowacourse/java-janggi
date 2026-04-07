3f1098786fc6ea022350d2ef1774421724993ca3
tjdakf <ksc73450056@gmail.com>
2026-04-07 11:17:12 +0900

    refactor: GameRepository try문 위치 수정 및 에러 메시지 구체화

--- src/main/java/janggi/repository/GameRepository.java
+++ src/main/java/janggi/repository/GameRepository.java
@@ -18,20 +18,14 @@ public class GameRepository {
     }
 
     public int createGame(Map<Position, Piece> board) {
-        try {
-            int gameId = insertGame();
-            savePieces(gameId, board);
-            return gameId;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        int gameId = insertGame();
+        savePieces(gameId, board);
+        return gameId;
     }
 
     public Optional<GameInfo> findGame() {
-        try {
-            String sql = "SELECT * FROM game";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
+        String sql = "SELECT * FROM game";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             ResultSet rs = pstmt.executeQuery();
             if (!rs.next()) {
                 return Optional.empty();
@@ -41,14 +35,13 @@ public class GameRepository {
                     rs.getString("current_turn"),
                     rs.getString("winner")));
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("[ERROR] DB 게임 조회 실패", e);
         }
     }
 
     public List<PieceInfo> findPieces(int gameId) {
-        try {
-            String sql = "SELECT type, team, piece_row, piece_col FROM piece WHERE game_id = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+        String sql = "SELECT type, team, piece_row, piece_col FROM piece WHERE game_id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, gameId);
 
             ResultSet rs = pstmt.executeQuery();
@@ -62,89 +55,102 @@ public class GameRepository {
             }
             return pieces;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("[ERROR] DB 기물 정보 조회 실패", e);
         }
     }
 
     public void movePiece(int gameId, Position from, Position to) {
-        try {
-            String deleteSql = "DELETE FROM piece WHERE game_id = ? AND piece_row = ? AND piece_col = ?";
-            PreparedStatement deletePstmt = conn.prepareStatement(deleteSql);
-            deletePstmt.setInt(1, gameId);
-            deletePstmt.setInt(2, to.getRowValue());
-            deletePstmt.setInt(3, to.getColumnValue());
-            deletePstmt.executeUpdate();
-
-            String updateSql = "UPDATE piece SET piece_row = ?, piece_col = ? WHERE game_id = ? AND piece_row = ? AND piece_col = ?";
-            PreparedStatement updatePstmt = conn.prepareStatement(updateSql);
-            updatePstmt.setInt(1, to.getRowValue());
-            updatePstmt.setInt(2, to.getColumnValue());
-            updatePstmt.setInt(3, gameId);
-            updatePstmt.setInt(4, from.getRowValue());
-            updatePstmt.setInt(5, from.getColumnValue());
-            updatePstmt.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        deletePieceAt(gameId, to);
+        updatePiecePosition(gameId, from, to);
     }
 
     public void updateTurn(int gameId, Team nextTurn) {
-        try {
-            String sql = "UPDATE game SET current_turn = ? WHERE id = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+        String sql = "UPDATE game SET current_turn = ? WHERE id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, nextTurn.name());
             pstmt.setInt(2, gameId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("[ERROR] DB 현재 차례 업데이트 실패", e);
         }
     }
 
     public void updateWinner(int gameId, Team winner) {
-        try {
-            String sql = "UPDATE game SET winner = ? WHERE id = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+        String sql = "UPDATE game SET winner = ? WHERE id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, winner.name());
             pstmt.setInt(2, gameId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("[ERROR] DB 승자 업데이트 실패", e);
         }
     }
 
     public void deleteGame(int gameId) {
-        try {
-            String sql = "DELETE FROM game WHERE id = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+        String sql = "DELETE FROM game WHERE id = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, gameId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("[ERROR] DB 게임 삭제 실패", e);
         }
     }
 
 
-    private int insertGame() throws SQLException {
+    private int insertGame() {
         String sql = "INSERT INTO game (current_turn) VALUES(?)";
-        PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        pstmt.setString(1, Team.FIRST_TURN.name());
-        pstmt.executeUpdate();
+        try (PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstmt.setString(1, Team.FIRST_TURN.name());
+            pstmt.executeUpdate();
 
-        ResultSet rs = pstmt.getGeneratedKeys();
-        rs.next();
-        return rs.getInt(1);
-    }
-
-    private void savePieces(int gameId, Map<Position, Piece> board) throws SQLException {
-        String sql = "INSERT INTO piece (game_id, type, team, piece_row, piece_col) VALUES (?, ?, ?, ?, ?)";
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        for (Map.Entry<Position, Piece> entry : board.entrySet()) {
-            addPieceBatch(pstmt, gameId, entry.getKey(), entry.getValue());
+            ResultSet rs = pstmt.getGeneratedKeys();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] DB 새로운 게임 추가 실패", e);
         }
-        pstmt.executeBatch();
     }
 
-    private void addPieceBatch(PreparedStatement pstmt, int gameId, Position position, Piece piece) throws SQLException {
+    private void savePieces(int gameId, Map<Position, Piece> board) {
+        String sql = "INSERT INTO piece (game_id, type, team, piece_row, piece_col) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            for (Map.Entry<Position, Piece> entry : board.entrySet()) {
+                addPieceBatch(pstmt, gameId, entry.getKey(), entry.getValue());
+            }
+            pstmt.executeBatch();
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] DB 기물 저장 실패", e);
+        }
+    }
+
+    private void deletePieceAt(int gameId, Position to) {
+        String deleteSql = "DELETE FROM piece WHERE game_id = ? AND piece_row = ? AND piece_col = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(deleteSql)) {
+            pstmt.setInt(1, gameId);
+            pstmt.setInt(2, to.getRowValue());
+            pstmt.setInt(3, to.getColumnValue());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] DB 도착 좌표 기물 삭제 실패", e);
+        }
+    }
+
+    private void updatePiecePosition(int gameId, Position from, Position to) {
+        String updateSql = "UPDATE piece SET piece_row = ?, piece_col = ? WHERE game_id = ? AND piece_row = ? AND piece_col = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(updateSql)) {
+            pstmt.setInt(1, to.getRowValue());
+            pstmt.setInt(2, to.getColumnValue());
+            pstmt.setInt(3, gameId);
+            pstmt.setInt(4, from.getRowValue());
+            pstmt.setInt(5, from.getColumnValue());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] DB 기물 이동 업데이트 실패", e);
+        }
+    }
+
+    private void addPieceBatch(PreparedStatement pstmt, int gameId, Position position, Piece piece) throws
+            SQLException {
         if (piece.isEmptyPiece()) {
             return;
         }
