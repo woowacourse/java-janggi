a902621d1e20e6f0d8e0004c2f5d8ffc18f87329
mvg01 <dkms625@naver.com>
2026-04-07 22:49:37 +0900

    feat: DB 연결을 담당하는 DatabaseConnector 추가

---
+++ src/main/java/janggi/db/DatabaseConnector.java
@@ -0,0 +1,20 @@
+package janggi.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    private static final String URL = "jdbc:h2:./janggi";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 접속 에러");
+        }
+    }
+}
