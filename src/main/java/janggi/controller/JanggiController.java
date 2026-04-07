42734748cba033c5b03fa857818a0ba2b93593ab
choubung <choubung@sookmyung.ac.kr>
2026-04-07 12:49:55 +0900

    feat : 장기 게임 종료까지 흐름 진행 구현

--- src/main/java/janggi/controller/JanggiController.java
+++ src/main/java/janggi/controller/JanggiController.java
@@ -1,11 +1,10 @@
 package janggi.controller;
 
-import janggi.domain.board.Board;
 import janggi.domain.JanggiGame;
+import janggi.domain.board.Board;
 import janggi.domain.board.BoardInitializer;
 import janggi.domain.dto.MoveCommand;
 import janggi.domain.piece.Team;
-import janggi.domain.vo.position.Position;
 import janggi.view.InputView;
 import janggi.view.OutputView;
 
@@ -15,19 +14,38 @@ public class JanggiController {
 
     public void run() {
         Board board = new Board(BoardInitializer.createBoard());
-        JanggiGame janggiGame = new JanggiGame();
+        JanggiGame janggiGame = new JanggiGame(board);
 
         outputView.printIntroduce();
         outputView.printBoard(board.getBoard());
 
         while (!janggiGame.isFinished()) {
-            Team currentTeam = janggiGame.findCurrentTeam();
-            MoveCommand moveCommand = inputView.readMovePositions(currentTeam);
+            Team currentTeam = janggiGame.getCurrentTeam();
 
-            board.move(moveCommand.getFrom(), moveCommand.getTo(), currentTeam);
+            int option = inputView.readTurnBehavior(currentTeam);
+
+            if (option == 1) {
+                janggiGame.playTurn();
+                MoveCommand moveCommand = inputView.readMovePositions(currentTeam);
+                board.move(moveCommand.getFrom(), moveCommand.getTo(), currentTeam);
+                outputView.printBoard(board.getBoard());
+            }
+
+            if (option == 2) {
+                janggiGame.skipTurn();
+                outputView.skipTurn();
+            }
+
+            if (option == 3) {
+                janggiGame.resign();
+                outputView.printResign(currentTeam);
+            }
+
             janggiGame.changeTurn();
-            outputView.printBoard(board.getBoard());
         }
+
+        janggiGame.decideWinner();
+        outputView.printWinner(janggiGame.getWinner());
     }
 }
 
--- src/main/java/janggi/domain/JanggiGame.java
+++ src/main/java/janggi/domain/JanggiGame.java
@@ -1,25 +1,78 @@
 package janggi.domain;
 
+import janggi.domain.board.BoardView;
 import janggi.domain.piece.Team;
 
-public class JanggiGame {
-    public Team currentTurn = Team.CHO;
-    private boolean isFinished = false;
+import java.util.HashMap;
+import java.util.Map;
 
-    public boolean isFinished() {
-        return isFinished;
+public class JanggiGame {
+    private final BoardView board;
+    private Map<Team, Boolean> skip = new HashMap<>(){
+        {
+            put(Team.HAN, false);
+            put(Team.CHO, false);
+        }
+    };
+    private Team currentTurn = Team.CHO;
+    private Team winner = Team.NONE;
+
+    public JanggiGame(BoardView board) {
+        this.board = board;
     }
 
-    public Team findCurrentTeam() {
+    public boolean isFinished() {
+        return isKingCaught() || winner != Team.NONE || allTeamSkip();
+    }
+
+    public Team getCurrentTeam() {
         return currentTurn;
     }
 
     public void changeTurn() {
-        if (currentTurn == Team.CHO) {
-            currentTurn = Team.HAN;
+        currentTurn = currentTurn.anotherTeam();
+    }
+
+    public void skipTurn() {
+        skip.put(currentTurn, true);
+    }
+
+    public void playTurn() {
+        skip.put(currentTurn, false);
+    }
+
+
+    public Team getWinner() {
+        return winner;
+    }
+
+    public void resign() {
+        winner = currentTurn.anotherTeam();
+    }
+
+    public void decideWinner() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        if (winner != Team.NONE) {
             return;
         }
 
-        currentTurn = Team.CHO;
+        if (isKingCaught()) {
+            winner = board.kingsOnBoard().get(0).findTeam();
+            return;
+        }
+
+        scoreBoard.saveHanScore(board.piecesOf(Team.HAN));
+        scoreBoard.saveChoScore(board.piecesOf(Team.CHO));
+
+        winner = scoreBoard.winner();
+    }
+
+    private boolean allTeamSkip() {
+        return skip.values().stream().filter(status -> status == true).count() == 2;
+    }
+
+    private boolean isKingCaught() {
+        return board.kingsOnBoard().size() < 2;
     }
 }
