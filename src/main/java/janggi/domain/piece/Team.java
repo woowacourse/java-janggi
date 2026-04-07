9735d61ba49ace68293d5461cc09c6be34a699ee
choubung <choubung@sookmyung.ac.kr>
2026-04-07 12:50:09 +0900

    feat : 상대 팀을 찾는 메서드 추가

--- src/main/java/janggi/domain/piece/Team.java
+++ src/main/java/janggi/domain/piece/Team.java
@@ -4,4 +4,16 @@ public enum Team {
     CHO(),
     HAN(),
     NONE();
+
+    public Team anotherTeam() {
+        if (this == CHO) {
+            return HAN;
+        }
+
+        if (this == HAN) {
+            return CHO;
+        }
+
+        throw new IllegalArgumentException("CHO 또는 HAN의 진영을 입력해야 합니다.");
+    }
 }
