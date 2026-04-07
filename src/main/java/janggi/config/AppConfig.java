174411768e2a9474c341c88b05804cf67bcea4c5
MODUGGAGI <kiroro0814@naver.com>
2026-04-07 14:13:56 +0900

    refactor: DB 설정 properties 파일 분리

--- src/main/java/janggi/config/AppConfig.java
+++ src/main/java/janggi/config/AppConfig.java
@@ -9,21 +9,22 @@ import janggi.repository.GameRepository;
 import janggi.repository.GameStateDaoImpl;
 import janggi.view.InputView;
 import janggi.view.OutputView;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Scanner;
 import org.h2.jdbcx.JdbcConnectionPool;
 
 public class AppConfig implements AutoCloseable {
 
-    private static final String URL = "jdbc:h2:file:./data/janggi";
-    private static final String USER = "stark";
-    private static final String PASSWORD = "stark123!";
+    private static final String INVALID_PROPERTIES = "[ERROR] db.properties 파일을 찾을 수 없습니다.";
 
     private final JdbcConnectionPool connectionPool;
     private final DatabaseInitializer databaseInitializer;
     private final GameRunner gameRunner;
 
     public AppConfig() {
-        this.connectionPool = JdbcConnectionPool.create(URL, USER, PASSWORD);
+        this.connectionPool = createConnectionPool();
         ConnectionManager connectionManager = new ConnectionManager(connectionPool);
         this.databaseInitializer = new DatabaseInitializer(connectionManager);
         this.gameRunner = createGameRunner(connectionManager);
@@ -37,6 +38,28 @@ public class AppConfig implements AutoCloseable {
         return databaseInitializer;
     }
 
+    private JdbcConnectionPool createConnectionPool() {
+        Properties properties = new Properties();
+        try (InputStream input = AppConfig.class.getResourceAsStream("/db.properties")) {
+            validateProperties(input);
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = properties.getProperty("db.url");
+        String user = properties.getProperty("db.user");
+        String password = properties.getProperty("db.password");
+
+        return JdbcConnectionPool.create(url, user, password);
+    }
+
+    private void validateProperties(InputStream input) {
+        if (input == null) {
+            throw new IllegalStateException(INVALID_PROPERTIES);
+        }
+    }
+
     private GameRunner createGameRunner(ConnectionManager connectionManager) {
         TransactionManager transactionManager = new TransactionManager(connectionManager);
         GameRepository gameRepository = new GameRepository(
@@ -44,7 +67,7 @@ public class AppConfig implements AutoCloseable {
                 new GameStateDaoImpl(),
                 new GamePieceDaoImpl()
         );
-        
+
         return new GameRunner(
                 new InputView(new Scanner(System.in)),
                 new OutputView(),
