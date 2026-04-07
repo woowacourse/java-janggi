02b8786bbc684d994d862da7eef5b8cdbc1baf2d
kkb <gimgyubin1225@gmail.com>
2026-04-07 11:03:41 +0900

    test: 커넥션 테스트 완료 후 커넥션을 해제하는 로직 추가

--- src/test/java/janggi/repository/util/DBConnectionProviderTest.java
+++ src/test/java/janggi/repository/util/DBConnectionProviderTest.java
@@ -3,7 +3,9 @@ package janggi.repository.util;
 
 import janggi.support.TestDBConnectionProvider;
 import java.sql.Connection;
+import java.sql.SQLException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,17 +13,31 @@ import org.junit.jupiter.api.Test;
 class DBConnectionProviderTest {
 
     ConnectionProvider connectionProvider;
+    Connection conn;
 
     @BeforeEach
     void setup() {
         connectionProvider = new TestDBConnectionProvider();
     }
 
+    @AfterEach
+    void tearDown() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException("테스트 커넥션 종료 실패", e);
+            }
+        }
+    }
+
     @Test
     @DisplayName("데이터베이스 커넥션을 획득한다.")
     void connectDatabase() {
-        Connection conn = connectionProvider.getConnection();
+        // given
+        conn = connectionProvider.getConnection();
 
+        // when & then
         Assertions.assertThat(conn).isNotNull();
     }
 }
