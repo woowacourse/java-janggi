9f9ed4d6a4af7b0db0f639a8b12bed0d7cc15837
dahye <dahye9011@naver.com>
2026-04-08 23:45:38 +0900

    feat: DBConnection 구현

---
+++ src/main/java/janggi/infrastructure/DBConnection.java
@@ -0,0 +1,19 @@
+package janggi.infrastructure;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("[ERROR] DB 연결 실패. 환경 변수를 확인하세요.");
+        }
+    }
+}
