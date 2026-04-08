4f39aa2bb9c1d6f09a415b78ef2434a2589bbe8b
Jinriro <riro12.hanyang.ac.kr>
2026-04-08 13:44:57 +0900

    refactor: 상속 방지를 위한 클래스 final 설정

--- src/main/java/persistence/DatabaseConnector.java
+++ src/main/java/persistence/DatabaseConnector.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnector {
+public final class DatabaseConnector {
 
     private static final String URL = "jdbc:mysql://localhost:3307/jangi_db";
     private static final String USER = "root";
