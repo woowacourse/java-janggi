be716b399361ee547e97445627cbc93e4c80bc8e
Jihyun3478 <leejihyun3478@gmail.com>
2026-04-08 16:46:37 +0900

    refactor: board 테이블의 x, y 컬럼에 유니크 제약조건 및 game_id 컬럼에 index 걸도록 수정

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,17 +1,21 @@
-CREATE TABLE IF NOT EXISTS game (
-    game_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    created_at TIMESTAMP,
+CREATE TABLE IF NOT EXISTS game
+(
+    game_id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    created_at        TIMESTAMP,
     cho_table_setting VARCHAR(20),
     han_table_setting VARCHAR(20),
-    is_finished BOOLEAN DEFAULT FALSE
+    is_finished       BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS board (
-    board_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    x INTEGER,
-    y INTEGER,
+CREATE TABLE IF NOT EXISTS board
+(
+    board_id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    x          INTEGER UNIQUE,
+    y          INTEGER UNIQUE,
     piece_type VARCHAR(20),
-    country VARCHAR(10),
-    game_id BIGINT,
-    FOREIGN KEY (game_id) REFERENCES game(game_id)
+    country    VARCHAR(10),
+    game_id    BIGINT,
+    FOREIGN KEY (game_id) REFERENCES game (game_id)
 );
+
+CREATE INDEX IF NOT EXISTS board_idx_game_id ON board (game_id);
