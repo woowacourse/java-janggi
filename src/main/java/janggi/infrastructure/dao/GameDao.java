a2fe5dd89ae17a0bef63784a01bcbc52a9fd88a8
yItWorks <poiu7825@pusan.ac.kr>
2026-04-08 16:28:08 +0900

    fix: 불필요한 메서드 호출 제거로 인한 버그 수정

--- src/main/java/janggi/infrastructure/dao/GameDao.java
+++ src/main/java/janggi/infrastructure/dao/GameDao.java
@@ -35,7 +35,7 @@ public class GameDao {
             pstmt.setInt(3, dto.y());
             pstmt.setString(4, dto.pieceType());
             pstmt.setString(5, dto.team());
-            pstmt.executeBatch();
+            pstmt.executeUpdate();
         }
     }
 
