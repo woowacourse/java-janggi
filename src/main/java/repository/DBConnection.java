f8aeedec3b9a70f137977b4c1aa44cccc3e116dc
haeseon lee <haeseon4816@gmail.com>
2026-04-08 17:26:00 +0900

    refactor: DB 접속 정보 외부 설정 파일로 분리

--- src/main/java/repository/DBConnection.java
+++ src/main/java/repository/DBConnection.java
@@ -1,24 +1,28 @@
 package repository;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/janggi_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234";
-
     public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+        Properties properties = new Properties();
 
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("[ERROR] JDBC 드라이버를 찾을 수 없습니다.", e);
+        try {
+            properties.load(new FileReader("src/main/resources/db.properties"));
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.user");
+            String password = properties.getProperty("db.password");
+
+            return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
-            throw new RuntimeException("[ERROR] DB 연결에 실패했습니다.", e);
+            throw new RuntimeException("[ERROR] DB 연결에 실패했습니다. " + e);
+        } catch (IOException e) {
+            throw new RuntimeException("[ERROR] 파일을 불러오는데 실패하였습니다. " + e);
         }
     }
 }
