946df2c1b9e0c86d814cf01b3feffb43b3d15b5a
최용준 <yj9107vv@gmail.com>
2026-04-08 11:30:19 +0900

    feat: 각 기물마다 점수를 추가
    - PieceType enum 클래스에 score 점수 추가

--- src/main/java/domain/PieceType.java
+++ src/main/java/domain/PieceType.java
@@ -2,26 +2,32 @@ package domain;
 
 public enum PieceType {
 
-    GENERAL("궁"),
-    GUARD("사"),
-    SOLDIER("졸"),
+    GENERAL("궁", 0),
+    GUARD("사", 3),
+    SOLDIER("졸", 2),
 
-    HORSE("마"),
-    ELEPHANT("상"),
-    CHARIOT("차"),
+    HORSE("마", 5),
+    ELEPHANT("상", 3),
+    CHARIOT("차", 13),
 
-    CANNON("포"),
+    CANNON("포", 7),
 
-    EMPTY_VALUE("*")
+    EMPTY_VALUE("*", 0)
     ;
 
     private final String description;
+    private final int score;
 
-    PieceType(String description) {
+    PieceType(String description, int score) {
         this.description = description;
+        this.score = score;
     }
 
     public String description() {
         return this.description;
+    }
+
+    public int score() {
+        return score;
     }
 }
