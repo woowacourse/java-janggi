3b30a3575577f18611c1c99bd700e8d6de70306d
Yoonhojoon <dbsghwns1209@gmail.com>
2026-04-08 14:06:59 +0900

    style(GameDatabase): google java style 적용

--- src/main/java/persistence/GameDatabase.java
+++ src/main/java/persistence/GameDatabase.java
@@ -1,11 +1,16 @@
 package persistence;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class GameDatabase {
+
     private static final String FILE_URL = "jdbc:h2:file:./data/janggi;AUTO_SERVER=TRUE";
-    private GameDatabase() {}
+
+    private GameDatabase() {
+    }
+
     public static Connection openFileConnection() throws SQLException {
         return DriverManager.getConnection(FILE_URL);
     }
