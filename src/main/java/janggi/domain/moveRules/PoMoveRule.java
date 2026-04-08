42989a294e042029cd8b731854f90059a30602ab
haechanmoon <moonhc.dev@gmail.com>
2026-04-08 22:31:00 +0900

    refactor: MoveRule에 포 기물 이동 기능 구현

--- src/main/java/janggi/domain/moveRules/PoMoveRule.java
+++ src/main/java/janggi/domain/moveRules/PoMoveRule.java
@@ -3,45 +3,38 @@ package janggi.domain.moveRules;
 import janggi.domain.Direction;
 import janggi.domain.Piece;
 import janggi.domain.Position;
-import janggi.domain.Route;
 import janggi.domain.Team;
-import janggi.domain.board.Board;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class PoMoveRule implements MoveRule {
 
     @Override
-    public List<Route> findRoutes(Team team) {
-        Route route1 = new Route(List.of(Direction.NORTH));
-        Route route2 = new Route(List.of(Direction.EAST));
-        Route route3 = new Route(List.of(Direction.SOUTH));
-        Route route4 = new Route(List.of(Direction.WEST));
-        return List.of(route1, route2, route3, route4);
-    }
-
-    @Override
-    public List<Position> calculateAvailablePositions(Position position, Team team, Board board) {
+    public List<Position> calculateAvailablePositions(Position position, Team team, Map<Position, Piece> state) {
         List<Position> availablePositions = new ArrayList<>();
-        List<Direction> directions = List.of(Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST);
+        List<Direction> directions = getPoDirections();
         for (Direction direction : directions) {
-            Position bridge = findBridge(position, direction, board);
+            Position bridge = findBridge(position, direction, state);
             if (bridge != null) {
-                collectPositions(bridge, direction, team, board, availablePositions);
+                collectPositions(bridge, direction, team, state, availablePositions);
             }
         }
         return availablePositions;
     }
 
-    private Position findBridge(Position currentPosition, Direction direction, Board board) {
-        if (!currentPosition.canMoveTo(direction)) {
+    private Position findBridge(Position currentPosition, Direction direction, Map<Position, Piece> state) {
+        if (currentPosition.cannotMoveTo(direction)) {
             return null;
         }
+
+        // TODO: 보드판 범위를 벗어나는 Position으로 move 시에 오류 해결
         Position next = currentPosition.move(direction);
-        Piece piece = board.getPiece(next);
+        Piece piece = state.get(next);
 
         if (piece == null) {
-            return findBridge(next, direction, board);
+            return findBridge(next, direction, state);
         }
         if (piece.isPo()) {
             return null;
@@ -49,18 +42,18 @@ public class PoMoveRule implements MoveRule {
         return next;
     }
 
-    private void collectPositions(Position currentPosition, Direction direction, Team team, Board board,
+    private void collectPositions(Position currentPosition, Direction direction, Team team, Map<Position, Piece> state,
                                   List<Position> result) {
-        if (!currentPosition.canMoveTo(direction)) {
+        if (currentPosition.cannotMoveTo(direction)) {
             return;
         }
         Position next = currentPosition.move(direction);
-        Piece piece = board.getPiece(next);
+        Piece piece = state.get(next);
 
         addIfValid(piece, next, team, result);
 
         if (piece == null) {
-            collectPositions(next, direction, team, board, result);
+            collectPositions(next, direction, team, state, result);
         }
     }
 
@@ -72,5 +65,11 @@ public class PoMoveRule implements MoveRule {
         if (!piece.isAlly(team) && !piece.isPo()) {
             result.add(next);
         }
+    }
+
+    private static List<Direction> getPoDirections() {
+        return Arrays.stream(Direction.values())
+                .filter(Direction::isStraight)
+                .toList();
     }
 }
