9ae32d957a70243a3a435719a213c9337e4cf03a
dahxxn <dahyeonhan4529@gmail.com>
2026-04-08 13:55:09 +0900

    refactor: Sang의 잘못된 displayName 수정

--- src/test/java/janggi/gimul/SangTest.java
+++ src/test/java/janggi/gimul/SangTest.java
@@ -111,7 +111,7 @@ class SangTest {
         assertThat(sang.getScore()).isEqualTo(new Score(3));
     }
 
-    @DisplayName("상은 점프할 수 없다.")
+    @DisplayName("상은 점프할 수 있다.")
     @Test
     void isJumpable() {
         Sang sang = new Sang(Team.CHO);
