646d5b50f51ee5cb233144f8b343b7898bbab444
김가현 <hyeonnni@naver.com>
2026-04-08 16:05:51 +0900

    refactor(domain): 한나라 초기 점수를 매직 넘버에서 상수로 분리

--- src/main/java/domain/game/Team.java
+++ src/main/java/domain/game/Team.java
@@ -2,8 +2,9 @@ package domain.game;
 
 public enum Team {
     CHO(new Score(0.0)),
-    HAN(new Score(1.5));
+    HAN(new Score(Team.HAN_HANDICAP_SCORE));
 
+    private static final double HAN_HANDICAP_SCORE = 1.5;
     private final Score initialScore;
     private Score teamScore;
 
