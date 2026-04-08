112ea4cc5abf56e1cce4ac09edfa879847f03fb2
최용준 <yj9107vv@gmail.com>
2026-04-08 19:55:01 +0900

    refactor: GameStatus에 현재 턴 정보가 포함되어 GameState의 currentTurn 필드 제거

--- src/main/java/persistence/entity/GameState.java
+++ src/main/java/persistence/entity/GameState.java
@@ -1,17 +1,14 @@
 package persistence.entity;
 
 import domain.GameStatus;
-import domain.Team;
 import java.util.List;
 import java.util.Objects;
 
 public record GameState(
-        Team currentTurn,
         List<PieceState> pieceStates,
         GameStatus gameStatus
 ) {
     public GameState {
-        Objects.requireNonNull(currentTurn);
         Objects.requireNonNull(pieceStates);
         Objects.requireNonNull(gameStatus);
         pieceStates = List.copyOf(pieceStates);
