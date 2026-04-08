f830ca365794bf08429ebc47f4c0e7127c5eaf69
yItWorks <poiu7825@pusan.ac.kr>
2026-04-08 11:18:53 +0900

    refactor: 재귀 구문 반복문으로 교체

--- src/main/java/janggi/util/RetryExecutor.java
+++ src/main/java/janggi/util/RetryExecutor.java
@@ -6,20 +6,23 @@ import java.util.function.Supplier;
 public class RetryExecutor {
 
     public static <T> T retry(Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            return retry(supplier);
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
         }
     }
 
     public static void retry(Runnable runnable) {
-        try {
-            runnable.run();
-        } catch (IllegalStateException e) {
-            OutputView.printErrorMessage(e.getMessage());
-            retry(runnable);
+        while (true) {
+            try {
+                runnable.run();
+                return;
+            } catch (IllegalStateException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
         }
     }
 }
