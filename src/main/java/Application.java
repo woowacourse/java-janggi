b558547d3e5f35a1a6a69acbcfe70f07d5fd7312
sauter001 <lsj457@naver.com>
2026-04-09 08:26:19 +0900

    feat: Application 최상위 예외 처리 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,6 +7,14 @@ import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        try {
+            run();
+        } catch (IllegalStateException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void run() {
         DatabaseConfig.initSchema();
         JanggiService janggiService = new JanggiService(new JanggiRepositoryImpl(DatabaseConfig.getConnection()));
         JanggiController janggiController = new JanggiController(new InputView(), new OutputView(), janggiService);
