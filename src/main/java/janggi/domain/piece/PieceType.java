338fdfbd057c609716faafc1724ac98e0131068b
kkb <gimgyubin1225@gmail.com>
2026-04-08 10:42:10 +0900

    refactor: PieceType에 각 타입에 대한 기물 생성 로직을 추가

--- src/main/java/janggi/domain/piece/PieceType.java
+++ src/main/java/janggi/domain/piece/PieceType.java
@@ -1,21 +1,26 @@
 package janggi.domain.piece;
 
+import janggi.domain.Side;
+import java.util.function.Function;
+
 public enum PieceType {
-    CHA("차", 13),
-    GUNG("궁", 0),
-    JOLBYEOUNG("졸,병", 2),
-    MA("마", 5),
-    PO("포", 7),
-    SA("사", 3),
-    SANG("상", 3),
-    EMPTY("없음", 0);
+    CHA("차", 13, Cha::new),
+    GUNG("궁", 0, Gung::new),
+    JOLBYEOUNG("졸,병", 2, Jolbyeong::new),
+    MA("마", 5, Ma::new),
+    PO("포", 7, Po::new),
+    SA("사", 3, Sa::new),
+    SANG("상", 3, Sang::new),
+    EMPTY("없음", 0, null);
 
     private final String displayName;
     private final double score;
+    private final Function<Side, Piece> pieceFactory;
 
-    PieceType(String displayName, double score) {
+    PieceType(String displayName, double score, Function<Side, Piece> pieceFactory) {
         this.displayName = displayName;
         this.score = score;
+        this.pieceFactory = pieceFactory;
     }
 
     @Override
@@ -25,5 +30,12 @@ public enum PieceType {
 
     public double getScore() {
         return score;
+    }
+
+    public Piece createPiece(Side side) {
+        if(this == EMPTY) {
+            return EmptyPiece.getInstance();
+        }
+        return pieceFactory.apply(side);
     }
 }
--- src/main/java/janggi/domain/strategy/BoardAssembler.java
+++ src/main/java/janggi/domain/strategy/BoardAssembler.java
@@ -2,16 +2,9 @@ package janggi.domain.strategy;
 
 import janggi.domain.Intersection;
 import janggi.domain.Side;
-import janggi.domain.piece.Cha;
-import janggi.domain.piece.Gung;
-import janggi.domain.piece.Jolbyeong;
-import janggi.domain.piece.Ma;
 import janggi.domain.piece.Piece;
-import janggi.domain.piece.Po;
-import janggi.domain.piece.Sa;
-import janggi.domain.piece.Sang;
+import janggi.domain.piece.PieceType;
 import java.util.List;
-import java.util.function.Function;
 
 public class BoardAssembler {
 
@@ -78,26 +71,26 @@ public class BoardAssembler {
     }
 
     private enum DefaultPieceFactory {
-        CHA(0, List.of(0, 8), Cha::new),
-        MA(0, List.of(1, 7), Ma::new),
-        SANG(0, List.of(2, 6), Sang::new),
-        SA(0, List.of(3, 5), Sa::new),
-        GUNG(1, List.of(4), Gung::new),
-        PO(2, List.of(1, 7), Po::new),
-        JOL(3, List.of(0, 2, 4, 6, 8), Jolbyeong::new);
+        CHA(0, List.of(0, 8), PieceType.CHA),
+        MA(0, List.of(1, 7), PieceType.MA),
+        SANG(0, List.of(2, 6), PieceType.SANG),
+        SA(0, List.of(3, 5), PieceType.SA),
+        GUNG(1, List.of(4), PieceType.GUNG),
+        PO(2, List.of(1, 7), PieceType.PO),
+        JOLBYEOUNG(3, List.of(0, 2, 4, 6, 8), PieceType.JOLBYEOUNG);
 
         private final int row;
         private final List<Integer> cols;
-        private final Function<Side, Piece> pieceFactory;
+        private final PieceType pieceType;
 
-        DefaultPieceFactory(int row, List<Integer> cols, Function<Side, Piece> pieceFactory) {
+        DefaultPieceFactory(int row, List<Integer> cols, PieceType pieceType) {
             this.row = row;
             this.cols = cols;
-            this.pieceFactory = pieceFactory;
+            this.pieceType = pieceType;
         }
 
         public Piece createPiece(Side side) {
-            return pieceFactory.apply(side);
+            return pieceType.createPiece(side);
         }
 
         public int getRow(Side side) {
