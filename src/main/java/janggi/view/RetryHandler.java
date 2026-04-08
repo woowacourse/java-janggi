66f1895fb3e4fb288ecae022ba8b7444773eed88
dahxxn <dahyeonhan4529@gmail.com>
2026-04-08 14:45:47 +0900

    refactor: RetryHandler 재귀 호출에서 반복문으로 수정

--- src/main/java/janggi/view/RetryHandler.java
+++ src/main/java/janggi/view/RetryHandler.java
@@ -1,21 +1,38 @@
 package janggi.view;
 
+import java.util.Optional;
+
 public class RetryHandler {
     public static void retryUntilSuccess(InputAction inputAction) {
+        for (boolean success = false; !success; ) {
+            success = tryRun(inputAction);
+        }
+    }
+
+    private static boolean tryRun(InputAction inputAction) {
         try {
             inputAction.run();
-        } catch (IllegalArgumentException | IllegalStateException exception) {
-            System.out.println("[ERROR] " + exception.getMessage());
-            retryUntilSuccess(inputAction);
+            return true;
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return false;
         }
     }
 
     public static <T> T retryUntilSuccess(InputHandler<T> inputHandler) {
+        Optional<T> result = Optional.empty();
+        while (result.isEmpty()) {
+            result = tryRun(inputHandler);
+        }
+        return result.get();
+    }
+
+    private static <T> Optional<T> tryRun(InputHandler<T> inputHandler) {
         try {
-            return inputHandler.run();
-        } catch (IllegalArgumentException | IllegalStateException exception) {
-            System.out.println("[ERROR] " + exception.getMessage());
-            return retryUntilSuccess(inputHandler);
+            return Optional.of(inputHandler.run());
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return Optional.empty();
         }
     }
 }
