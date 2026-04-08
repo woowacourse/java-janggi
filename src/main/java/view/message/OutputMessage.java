79ee2062013260c339a240d21726aade51f5e601
최용준 <yj9107vv@gmail.com>
2026-04-08 12:59:50 +0900

    feat: OutputMessage 기물 점수 출력 메시지 추가

--- src/main/java/view/message/OutputMessage.java
+++ src/main/java/view/message/OutputMessage.java
@@ -3,6 +3,8 @@ package view.message;
 public enum OutputMessage {
 
     PLAYER_TURN_SIGN("%s입니다."),
+    GREEN_PIECES_SCORE("초나라 기물 점수: "),
+    RED_PIECES_SCORE("한나라 기물 점수: ")
     ;
 
     private final String message;
