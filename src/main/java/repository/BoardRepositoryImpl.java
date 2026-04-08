5bb8af1a01c23446232b7a8bd9181f7518ed9432
Jihyun3478 <leejihyun3478@gmail.com>
2026-04-08 23:57:54 +0900

    fix: saveAll()에서 배치를 활용해 쿼리를 DB에 한번만 전송되도록 수정

--- src/main/java/repository/BoardRepositoryImpl.java
+++ src/main/java/repository/BoardRepositoryImpl.java
@@ -14,8 +14,23 @@ import java.util.Map;
 public class BoardRepositoryImpl implements BoardRepository {
     @Override
     public void saveAll(Connection connection, Long gameId, Map<Position, PieceInfo> pieceInfos) {
-        for (Map.Entry<Position, PieceInfo> pieceInfo : pieceInfos.entrySet()) {
-            save(connection, gameId, pieceInfo.getKey(), pieceInfo.getValue());
+        String sql = "INSERT INTO board(x, y, piece_type, country, game_id) VALUES (?, ?, ?, ?, ?)";
+
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            for (Map.Entry<Position, PieceInfo> pieceInfo : pieceInfos.entrySet()) {
+                statement.setInt(1, pieceInfo.getKey().x());
+                statement.setInt(2, pieceInfo.getKey().y());
+                statement.setString(3, pieceInfo.getValue().pieceType().getDbValue());
+                statement.setString(4, pieceInfo.getValue().country().getDbValue());
+                statement.setLong(5, gameId);
+
+                statement.addBatch();
+            }
+            statement.executeBatch();
+        } catch (SQLException exception) {
+            throw new IllegalStateException("[ERROR] DB에 보드 데이터를 저장하는 도중, 오류가 발생했습니다.", exception);
         }
     }
 
@@ -28,8 +43,8 @@ public class BoardRepositoryImpl implements BoardRepository {
         ) {
             statement.setInt(1, to.x());
             statement.setInt(2, to.y());
-            statement.setString(3, pieceInfo.pieceType().name());
-            statement.setString(4, pieceInfo.country().name());
+            statement.setString(3, pieceInfo.pieceType().getDbValue());
+            statement.setString(4, pieceInfo.country().getDbValue());
             statement.setLong(5, gameId);
             statement.executeUpdate();
         } catch (SQLException exception) {
