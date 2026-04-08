d40e3fcd2addc8dd56929f4976ec716775be6d73
zzaekkii <zzaekkii@gmail.com>
2026-04-08 15:28:51 +0900

    refactor(name): Pattern 객체 재사용하도록 변경
    - 로치 & 찰리 피드백

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -1,11 +1,14 @@
 package domain.player;
 
+import java.util.regex.Pattern;
+
 public record Name(
         String name
 ) {
     private static final int MINIMUM_LENGTH = 2;
     private static final int MAXIMUM_LENGTH = 5;
-    private static final String NAME_PATTERN = String.format("^[A-Za-z]{%d,%d}", MINIMUM_LENGTH, MAXIMUM_LENGTH);
+    private static final String NAME_REGEX = String.format("^[A-Za-z]{%d,%d}", MINIMUM_LENGTH, MAXIMUM_LENGTH);
+    private static final Pattern NAME_PATTERN = Pattern.compile(NAME_REGEX);
     private static final String INVALID_PATTERN = "이름은 " + MINIMUM_LENGTH + "-" + MAXIMUM_LENGTH + " 길이의 영문자여야 합니다.";
 
     public Name {
@@ -13,7 +16,7 @@ public record Name(
     }
 
     private void validatePattern(final String name) {
-        if (!name.matches(NAME_PATTERN)) {
+        if (!NAME_PATTERN.matcher(name).matches()) {
             throw new IllegalArgumentException(INVALID_PATTERN);
         }
     }
