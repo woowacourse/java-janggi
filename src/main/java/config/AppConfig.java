358a1ffb12594963e7e48b4399dced0cd70d0fcc
yeo-li <parkseongyeol2110@gmail.com>
2026-04-08 20:18:20 +0900

    refactor: 프로젝트 의존성 분리

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,43 @@
+package config;
+
+import domain.console.GameConsole;
+import repository.connector.Connector;
+import repository.connector.MysqlConnector;
+import repository.game_record.GameRecordRepository;
+import repository.game_record.GameRecordRepositoryImpl;
+import repository.move_record.MoveRecordRepository;
+import repository.move_record.MoveRecordRepositoryImpl;
+import service.GameService;
+import view.InputView;
+import view.OutputView;
+
+public class AppConfig {
+
+    public GameConsole gameConsole() {
+        return new GameConsole(gameService(), inputView(), outputView());
+    }
+
+    private GameService gameService() {
+        return new GameService(moveRecordRepository(), gameRecordRepository());
+    }
+
+    private InputView inputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
+    }
+
+    private GameRecordRepository gameRecordRepository() {
+        return new GameRecordRepositoryImpl(connector());
+    }
+
+    private MoveRecordRepository moveRecordRepository() {
+        return new MoveRecordRepositoryImpl(connector());
+    }
+
+    private Connector connector() {
+        return new MysqlConnector();
+    }
+}
