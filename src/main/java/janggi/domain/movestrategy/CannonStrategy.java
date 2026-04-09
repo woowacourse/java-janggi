982be4fdb8e7a72b95800ad9e33ec1af9d31df29
sinequanonn <dleck288@gmail.com>
2026-04-09 13:36:01 +0900

    refactor: 포 이동 규칙 수정

--- src/main/java/janggi/domain/movestrategy/CannonStrategy.java
+++ src/main/java/janggi/domain/movestrategy/CannonStrategy.java
@@ -1,6 +1,5 @@
 package janggi.domain.movestrategy;
 
-import janggi.domain.board.Position;
 import janggi.domain.movestrategy.rule.MoveRule;
 import janggi.domain.piece.Piece;
 import janggi.domain.piece.PieceType;
@@ -9,25 +8,8 @@ import java.util.List;
 
 public class CannonStrategy extends AbstractMoveStrategy {
 
-    private final List<MoveRule> moveRules;
-
     public CannonStrategy(List<MoveRule> moveRules) {
-        this.moveRules = moveRules;
-    }
-
-    @Override
-    public boolean canMove(Position from, Position to) {
-        return moveRules.stream()
-                .anyMatch(rule -> rule.canMove(from, to));
-    }
-
-    @Override
-    public List<Position> findPath(Position from, Position to) {
-        return moveRules.stream()
-                .filter(rule -> rule.canMove(from, to))
-                .map(rule -> rule.findPath(from, to))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("이동 경로를 찾을 수 없습니다."));
+        super(moveRules);
     }
 
     @Override
