f28a1a5610c8449ad4fbd84fd398789c23ad204d
최용준 <yj9107vv@gmail.com>
2026-04-09 20:44:10 +0900

    test: 인메모리 저장/조회 테스트 구현

---
+++ src/test/java/persistence/repository/InMemoryGameStateRepositoryTest.java
@@ -0,0 +1,41 @@
+package persistence.repository;
+
+import domain.GameStatus;
+import domain.PieceProperty;
+import domain.PieceType;
+import domain.Position;
+import domain.Team;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import persistence.entity.GameState;
+import persistence.entity.PieceState;
+
+class InMemoryGameStateRepositoryTest {
+
+    private final PieceProperty horseGreenProperty = new PieceProperty(PieceType.HORSE, Team.GREEN);
+    private final PieceProperty soldierGreenProperty = new PieceProperty(PieceType.SOLDIER, Team.GREEN);
+    private final PieceProperty generalGreenProperty = new PieceProperty(PieceType.GENERAL, Team.GREEN);
+
+    @Test
+    @DisplayName("게임 상태 저장/조회 검증 테스트")
+    void save_and_load_game_state_test() {
+        GameStateRepository repository = new InMemoryGameStateRepository();
+
+        Position horsePosition = new Position(9, 1);
+        Position soldierPosition = new Position(6, 0);
+        Position generalPosition = new Position(8, 4);
+
+        PieceState horseState = new PieceState(horseGreenProperty, horsePosition);
+        PieceState soldierState = new PieceState(soldierGreenProperty, soldierPosition);
+        PieceState generalState = new PieceState(generalGreenProperty, generalPosition);
+        GameState expected = new GameState(List.of(horseState, soldierState, generalState), GameStatus.GREEN_PLAYER_TURN);
+
+        repository.save(expected);
+        GameState actual = repository.load();
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+}
