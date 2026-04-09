bcb760c15970d79c5bfe33fe9931f05c48f09b4a
최용준 <yj9107vv@gmail.com>
2026-04-09 21:24:51 +0900

    feat: Application에 게임 초기화 및 저장 게임 불러오기 로직 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,17 @@
+import application.GameInitializer;
+import application.GamePersistenceService;
 import controller.JanggiController;
-import domain.GameStatus;
 import domain.JanggiGame;
-import factory.JanggiBoardFactory;
 
 public class Application {
 
     public static void main(String[] args) {
-        JanggiBoardFactory janggiBoardFactory = new JanggiBoardFactory();
-        JanggiGame janggiGame = JanggiGame.of(janggiBoardFactory.initialBoard(), GameStatus.GREEN_PLAYER_TURN);
-        JanggiController janggiController = new JanggiController(janggiGame);
+        GameInitializer gameInitializer = new GameInitializer();
+        gameInitializer.initializeDatabase();
+
+        GamePersistenceService gamePersistenceService = gameInitializer.createGamePersistenceService();
+        JanggiGame janggiGame = gamePersistenceService.loadOrCreate();
+        JanggiController janggiController = new JanggiController(janggiGame, gamePersistenceService);
 
         janggiController.run();
     }
