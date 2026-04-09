fc618fe30ba5a0d450cda3068e4399200282e7e5
SooHyeon Lee <dalsu4594@naver.com>
2026-04-09 23:28:06 +0900

    feat: 엔티티 mapper 추가

---
+++ src/main/java/janggi/persistence/mapper/GameMapper.java
@@ -0,0 +1,43 @@
+package janggi.persistence.mapper;
+
+import janggi.domain.Camp;
+import janggi.domain.Janggi;
+import janggi.domain.board.BoardFactory;
+import janggi.domain.piece.Piece;
+import janggi.domain.position.Position;
+import janggi.persistence.entity.GameEntity;
+import janggi.persistence.entity.vo.Status;
+import janggi.persistence.entity.vo.Turn;
+
+import java.util.Map;
+
+public class GameMapper {
+
+    public GameEntity toGameEntity(String id, String name, Janggi janggi){
+        return new GameEntity(
+                id,
+                name,
+                Status.of(janggi),
+                Turn.of(janggi.currentCamp())
+        );
+    }
+
+    public Janggi toDomain(GameEntity gameEntity, Map<Position, Piece> board) {
+        return Janggi.load(
+                BoardFactory.load(board),
+                toCamp(gameEntity.turn()),
+                isRunning(gameEntity.status())
+        );
+    }
+
+    private Camp toCamp(Turn turn){
+        if(turn.isCho()){
+            return Camp.CHO;
+        }
+        return Camp.HAN;
+    }
+
+    private boolean isRunning(Status status) {
+        return status.isRunning();
+    }
+}
