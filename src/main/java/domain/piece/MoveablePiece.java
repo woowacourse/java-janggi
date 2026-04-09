2d6488b453a5cdb822937a0af9258cda789c86ab
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-09 12:51:10 +0900

    feat: 도착 좌표의 기물을 검증하는 기능 구현

--- src/main/java/domain/piece/MoveablePiece.java
+++ src/main/java/domain/piece/MoveablePiece.java
@@ -19,4 +19,10 @@ public abstract class MoveablePiece extends Piece {
     public List<Position> getMoveCandidates(Position from, PieceProvider board) {
         return moveStrategy.getMoveCandidates(from, team, board);
     }
+
+    public void validateTarget(Piece targetPiece) {
+        if (targetPiece.getTeam() == team) {
+            throw new IllegalArgumentException("[ERROR] 같은 팀의 기물은 잡을 수 없습니다.");
+        }
+    }
 }
