9f14185ae99b34d09b09abf43904536ef0e23f19
최용준 <yj9107vv@gmail.com>
2026-04-09 21:23:34 +0900

    feat: GamePersistenceService를 통한 게임 생성, 저장, 불러오기 구현

---
+++ src/main/java/application/GamePersistenceService.java
@@ -0,0 +1,42 @@
+package application;
+
+import domain.GameStatus;
+import domain.JanggiGame;
+import factory.JanggiBoardFactory;
+import persistence.entity.GameState;
+import persistence.mapper.GameStateMapper;
+import persistence.repository.GameStateRepository;
+
+public final class GamePersistenceService {
+
+    private final GameStateRepository repository;
+    private final GameStateMapper gameStateMapper;
+    private final JanggiBoardFactory boardFactory;
+
+    public GamePersistenceService(GameStateRepository repository, GameStateMapper gameStateMapper,
+                                  JanggiBoardFactory boardFactory) {
+        this.repository = repository;
+        this.gameStateMapper = gameStateMapper;
+        this.boardFactory = boardFactory;
+    }
+
+    public JanggiGame loadOrCreate() {
+        try {
+            GameState gameState = repository.load();
+            return gameStateMapper.mapToJanggiGame(gameState);
+        } catch (IllegalStateException exception) {
+            return JanggiGame.of(boardFactory.initialBoard(), GameStatus.GREEN_PLAYER_TURN);
+        }
+    }
+
+    public JanggiGame createNewGame() {
+        JanggiGame janggiGame = JanggiGame.of(boardFactory.initialBoard(), GameStatus.GREEN_PLAYER_TURN);
+        repository.save(gameStateMapper.mapFrom(janggiGame));
+        return janggiGame;
+    }
+
+    public void save(JanggiGame janggiGame) {
+        GameState gameState = gameStateMapper.mapFrom(janggiGame);
+        repository.save(gameState);
+    }
+}
