9919545cffd6db2773d5de0defb60032895898e3
Chocoding1 <galmeagi2@naver.com>
2026-04-09 19:15:29 +0900

    refactor: 기물 이동 메서드명 수정

--- src/main/java/janggi/JanggiRunner.java
+++ src/main/java/janggi/JanggiRunner.java
@@ -24,7 +24,7 @@ public class JanggiRunner {
             OutputView.printBoard(BoardSpots.from(janggiGame.makeCurrentTurnBoardSnapShot()));
             Position startPosition = ActionExecutor.retryUntilSuccess(() -> readValidStartPosition(janggiGame));
             Position endPosition = ActionExecutor.retryUntilSuccess(() -> readValidEndPosition(janggiGame, startPosition));
-            janggiGame.doGame(startPosition, endPosition);
+            janggiGame.movePiece(startPosition, endPosition);
         }
         OutputView.printGameOver(janggiGame.winTeamName());
     }
