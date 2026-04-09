2286ba5f1df2201105b34dbb020f020300fdea91
poketopa <lhs5427ll@gmail.com>
2026-04-09 13:32:27 +0900

    feat (domain): Directions.merge 메서드 추가 (궁성 경로 추가 로직)

--- src/main/java/domain/move/Directions.java
+++ src/main/java/domain/move/Directions.java
@@ -1,6 +1,7 @@
 package domain.move;
 
 import domain.point.Point;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Directions {
@@ -16,5 +17,12 @@ public class Directions {
                 .findFirst()
                 .map(direction -> direction.getPoints(from))
                 .orElseThrow(() -> new IllegalArgumentException("해당 기물이 이동할 수 없는 위치/방향입니다.")));
+    }
+
+    public Directions merge(Directions other) {
+        List<Direction> merged = new ArrayList<>(this.directions.size() + other.directions.size());
+        merged.addAll(this.directions);
+        merged.addAll(other.directions);
+        return new Directions(List.copyOf(merged));
     }
 }
