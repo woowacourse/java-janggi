ed5b241b182d0572b48bb019b99e6eecfdeac630
JYL35 <dlwodbs1223@naver.com>
2026-04-09 17:36:21 +0900

    feat(HanTurn): 한의 턴에 기물을 움직이는 기능 구현

---
+++ src/main/java/janggi/domain/state/HanTurn.java
@@ -0,0 +1,31 @@
+package janggi.domain.state;
+
+import janggi.domain.Camp;
+import janggi.domain.Position;
+import janggi.domain.board.Board;
+
+public class HanTurn extends Running {
+    private static final Camp camp = Camp.HAN;
+
+    @Override
+    public GameState move(Position from, Position to, Board board) {
+        validateCamp(from, board);
+        board.movePiece(from, to);
+        if (board.isOnlyGeneralOfCampAlive(camp)) {
+            return new Checkmate(camp);
+        }
+        return new ChoTurn();
+    }
+
+    @Override
+    public Camp turn() {
+        return camp;
+    }
+
+    @Override
+    public void validateCamp(Position current, Board board) {
+        if (!board.isSameCamp(current, camp)) {
+            throw new IllegalArgumentException("자신의 기물만 선택할 수 있습니다.");
+        }
+    }
+}
