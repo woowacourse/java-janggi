fa9321589af50790caa0d4ec129fca74991daf2f
sinequanonn <dleck288@gmail.com>
2026-04-09 13:36:47 +0900

    refactor: 각 기물 생성 구현체 변경

--- src/main/java/janggi/domain/piece/PieceFactory.java
+++ src/main/java/janggi/domain/piece/PieceFactory.java
@@ -1,7 +1,9 @@
 package janggi.domain.piece;
 
 import janggi.domain.board.BoardDirection;
-import janggi.domain.movestrategy.*;
+import janggi.domain.movestrategy.CannonStrategy;
+import janggi.domain.movestrategy.DefaultMoveStrategy;
+import janggi.domain.movestrategy.PalaceRestrictMoveStrategy;
 import janggi.domain.movestrategy.rule.*;
 import janggi.domain.palace.PalaceFactory;
 
@@ -10,7 +12,7 @@ import java.util.List;
 public class PieceFactory {
 
     public static Piece createGeneral(Team team) {
-        return new Piece(PieceType.GENERAL, team, new GeneralStrategy(
+        return new Piece(PieceType.GENERAL, team, new PalaceRestrictMoveStrategy(
                 PalaceFactory.createPalace(team),
                 List.of(new StraightOneStepMoveRule(),
                         new PalaceDiagonalOneStepMoveRule(PalaceFactory.createPalace(team)))
@@ -18,7 +20,7 @@ public class PieceFactory {
     }
 
     public static Piece createGuard(Team team) {
-        return new Piece(PieceType.GUARD, team, new GuardStrategy(
+        return new Piece(PieceType.GUARD, team, new PalaceRestrictMoveStrategy(
                 PalaceFactory.createPalace(team),
                 List.of(new StraightOneStepMoveRule(),
                         new PalaceDiagonalOneStepMoveRule(PalaceFactory.createPalace(team)))
@@ -26,7 +28,7 @@ public class PieceFactory {
     }
 
     public static Piece createChariot(Team team) {
-        return new Piece(PieceType.CHARIOT, team, new ChariotStrategy(
+        return new Piece(PieceType.CHARIOT, team, new DefaultMoveStrategy(
                 List.of(new StraightForwardMoveRule(),
                         new PalaceDiagonalForwardMoveRule(List.of(
                                 PalaceFactory.createPalace(Team.HAN),
@@ -42,15 +44,17 @@ public class PieceFactory {
     }
 
     public static Piece createHorse(Team team) {
-        return new Piece(PieceType.HORSE, team, new HorseStrategy());
+        return new Piece(PieceType.HORSE, team, new DefaultMoveStrategy(List.of(new HorseMoveRule())));
     }
 
     public static Piece createElephant(Team team) {
-        return new Piece(PieceType.ELEPHANT, team, new ElephantStrategy());
+        return new Piece(PieceType.ELEPHANT, team, new DefaultMoveStrategy(
+                List.of(new ElephantMoveRule())
+        ));
     }
 
     public static Piece createSolider(Team team, BoardDirection direction) {
-        return new Piece(PieceType.SOLDIER, team, new SoliderStrategy(
+        return new Piece(PieceType.SOLDIER, team, new DefaultMoveStrategy(
                 List.of(new DirectionalOneStepMoveRule(direction),
                         new PalaceDiagonalDirectionalOneStepMoveRule(PalaceFactory.createPalace(team), direction))));
     }
