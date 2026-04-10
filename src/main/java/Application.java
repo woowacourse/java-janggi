6ea62fb617d1982fc68dc1d492fe700a197317d6
frombunny <rucy011121@gmail.com>
2026-04-10 14:11:34 +0900

    feat(Controller): DB 설정 연결

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,56 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import controller.GameController;
+import data.BoardRepository;
+import data.TransactionManager;
 import domain.board.Board;
 import domain.board.BoardInitializer;
 import view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        Board board = new Board(BoardInitializer.init(InputView.readBoardSetting()));
-        GameController gameController = new GameController(board);
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:~/janggi;INIT=RUNSCRIPT FROM 'src/main/resources/create_tables.sql'");
+        hikariConfig.setDriverClassName("org.h2.Driver");
+        hikariConfig.setUsername("sa");
+        hikariConfig.setPassword("");
 
-        while (!board.isGameInProgress()) {
+        HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+        TransactionManager transactionManager = new TransactionManager(hikariDataSource);
+
+        BoardRepository boardRepository = new BoardRepository();
+
+        Board board;
+        int boardId = InputView.readBoardNumber();
+        if (boardId == 0) {
+            board = Board.from(BoardInitializer.init(InputView.readBoardSetting()));
+        } else {
+            board = transactionManager.executeTransaction(connection -> boardRepository.findById(connection, (long) boardId));
+        }
+
+        transactionManager.executeTransaction(connection -> {
+            boardRepository.save(connection, board);
+            return null;
+        });
+
+        GameController gameController = new GameController(board);
+        while (board.isGameInProgress()) {
             gameController.printBoard();
-            gameController.move();
+
+            transactionManager.executeTransaction(connection -> {
+                gameController.move();
+                boardRepository.save(connection, board);
+                return null;
+            });
         }
 
         gameController.printBoard();
         gameController.printWinner();
+
+        transactionManager.executeTransaction(connection -> {
+                    boardRepository.delete(connection, board);
+                    return null;
+                }
+        );
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,8 +7,13 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static Integer readBoardNumber() {
+        System.out.println("불러올 보드 번호를 입력하세요. 새로 생성하려면 0을 입력하세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
     public static BoardSetting readBoardSetting() {
-        System.out.println("장기판 상차림 번호를 선택해주세요.");
+        System.out.println("생성할 장기판 상차림 번호를 선택해주세요.");
         System.out.println("1. 왼상차림  2. 오른상차림  3. 바깥상차림  4. 안상차림");
         return BoardSetting.from(scanner.nextLine());
     }
