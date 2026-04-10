df6668daa91612ccfbd9b220252a1967239118a1
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-04-10 20:19:30 +0900

    remove(Feedback 04): 불용 코드 제거

--- src/main/java/janggi/service/JanggiService.java
+++ src/main/java/janggi/service/JanggiService.java
@@ -3,8 +3,6 @@ package janggi.service;
 import janggi.domain.board.Board;
 import janggi.domain.game.GameManager;
 import janggi.domain.game.Players;
-import janggi.domain.game.Side;
-import janggi.domain.game.Turn;
 import janggi.dto.GameSessionDto;
 import janggi.persistence.repository.GameRepository;
 import java.sql.Connection;
@@ -25,15 +23,6 @@ public class JanggiService {
 
     public GameManager loadGameSession(Connection connection, long gameId) throws SQLException {
         return gameRepository.findByGameId(connection, gameId);
-    }
-
-    private GameManager generateGameManagerByLoadedData(GameSessionDto gameSession, Board board) {
-        long gameId = gameSession.gameId();
-        String choPlayerName = gameSession.choPlayerName();
-        String hanPlayerName = gameSession.hanPlayerName();
-        Turn currentTurn = new Turn(Side.valueOf(gameSession.currentTurn()));
-        Players players = Players.fromCurrentTurn(choPlayerName, hanPlayerName, currentTurn);
-        return GameManager.loadGame(players, board, gameId);
     }
 
     public GameManager createNewSession(Connection connection, String choName, String hanName)
