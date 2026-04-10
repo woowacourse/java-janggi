6242257e6a717126d059d83e52d4ed2c191ef9de
KIM SEON WOO <sunwoo005@naver.com>
2026-04-10 13:39:45 +0900

    feat: jdbc h2 연결 및 테스트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import database.ConnectionManager;
+import database.DatabaseConfig;
+import database.DatabaseInitializer;
+
 public class Main {
 
     public static void main(String[] args) {
+        DatabaseConfig databaseConfig = new DatabaseConfig();
+        DatabaseInitializer databaseInitializer = new DatabaseInitializer(new ConnectionManager(databaseConfig));
+        databaseInitializer.initialize();
+
         new GameRunner().run();
     }
 }
-
 
