58b33b9a89648a7a713d596b23d4b27eb2fe60bb
최용준 <yj9107vv@gmail.com>
2026-04-10 17:59:13 +0900

    feat: 저장된 게임이 있으면 새 게임 또는 이어하기를 선택하도록 구현

--- src/main/java/controller/JanggiController.java
+++ src/main/java/controller/JanggiController.java
@@ -4,18 +4,37 @@ import application.GamePersistenceService;
 import domain.JanggiGame;
 import domain.Position;
 import java.util.List;
+import java.util.function.Supplier;
 import utils.Parser;
+import view.GameStartOption;
 import view.InputView;
 import view.OutputView;
 
 public class JanggiController {
 
-    private final JanggiGame janggiGame;
     private final GamePersistenceService gamePersistenceService;
+    private final JanggiGame janggiGame;
 
-    public JanggiController(JanggiGame janggiGame, GamePersistenceService gamePersistenceService) {
-        this.janggiGame = janggiGame;
+    public JanggiController(GamePersistenceService gamePersistenceService) {
         this.gamePersistenceService = gamePersistenceService;
+        this.janggiGame = createJanggiGame();
+    }
+
+    private JanggiGame createJanggiGame() {
+        if (gamePersistenceService.exist()) {
+            return selectedJanggiGame();
+        }
+
+        return gamePersistenceService.createNewGame();
+    }
+
+    private JanggiGame selectedJanggiGame() {
+        GameStartOption gameStartOption = retryUntilValid(InputView::readGameStartOption);
+        if (gameStartOption.isContinueGame()) {
+            return gamePersistenceService.loadOrCreate();
+        }
+
+        return gamePersistenceService.createNewGame();
     }
 
     public void run() {
@@ -58,6 +77,16 @@ public class JanggiController {
         return new Position(target.getFirst(), target.getLast());
     }
 
+
+    private <T> T retryUntilValid(Supplier<T> supplier) {
+        do {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (true);
+    }
 
     private void execute(ExecutableTask task) {
         while (true) {
