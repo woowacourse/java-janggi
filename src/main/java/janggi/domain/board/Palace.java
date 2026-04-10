e7242c5217a44ee72ce48e923cf0d99620a75139
rin <chaerin0916@gmail.com>
2026-04-11 01:00:38 +0900

    feat: 궁성 내 대각선 이동 경로 찾기 구현

--- src/main/java/janggi/domain/board/Palace.java
+++ src/main/java/janggi/domain/board/Palace.java
@@ -1,22 +1,46 @@
 package janggi.domain.board;
 
 
+import janggi.domain.common.Direction;
 import janggi.domain.common.Position;
+import janggi.domain.route.Route;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public enum Palace {
 
-    CHO(8, 10),
-    HAN(1, 3);
+    CHO(8, 10, Map.of(
+            new Position(4, 8), List.of(new Route(List.of(Direction.DOWN_RIGHT))),
+            new Position(6, 8), List.of(new Route(List.of(Direction.DOWN_LEFT))),
+            new Position(5, 9), List.of(
+                    new Route(List.of(Direction.UP_LEFT)),
+                    new Route(List.of(Direction.UP_RIGHT)),
+                    new Route(List.of(Direction.DOWN_LEFT)),
+                    new Route(List.of(Direction.DOWN_RIGHT))),
+            new Position(4, 10), List.of(new Route(List.of(Direction.UP_RIGHT))),
+            new Position(6, 10), List.of(new Route(List.of(Direction.UP_LEFT))))),
+
+    HAN(1, 3, Map.of(
+            new Position(4, 1), List.of(new Route(List.of(Direction.DOWN_RIGHT))),
+            new Position(6, 1), List.of(new Route(List.of(Direction.DOWN_LEFT))),
+            new Position(5, 2), List.of(
+                    new Route(List.of(Direction.UP_LEFT)),
+                    new Route(List.of(Direction.UP_RIGHT)),
+                    new Route(List.of(Direction.DOWN_LEFT)),
+                    new Route(List.of(Direction.DOWN_RIGHT))),
+            new Position(4, 3), List.of(new Route(List.of(Direction.UP_RIGHT))),
+            new Position(6, 3), List.of(new Route(List.of(Direction.UP_LEFT)))));
 
     private final int START_X = 4;
     private final int END_X = 6;
 
     private final List<Position> palacePositions;
+    private final Map<Position, List<Route>> diagonalInfos;
 
-    Palace(int startY, int endY) {
+    Palace(int startY, int endY, Map<Position, List<Route>> diagonalInfos) {
         this.palacePositions = initializePalacePositions(startY, endY);
+        this.diagonalInfos = diagonalInfos;
     }
 
     public boolean isInPalace(Position position) {
@@ -35,5 +59,9 @@ public enum Palace {
         for (int y = startY; y <= endY; y++) {
             palacePositions.add(new Position(x, y));
         }
+    }
+
+    public List<Route> findDiagonalRoutes(Position position) {
+        return diagonalInfos.getOrDefault(position, List.of());
     }
 }
--- src/test/java/janggi/domain/board/PalaceTest.java
+++ src/test/java/janggi/domain/board/PalaceTest.java
@@ -3,7 +3,10 @@ package janggi.domain.board;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import janggi.domain.common.Position;
+import janggi.domain.route.Route;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -29,5 +32,50 @@ public class PalaceTest {
     @DisplayName("좌표가 초나라 궁성 범위 안에 있는지 확인한다.")
     void 한나라_궁성_좌표_확인(int x, int y) {
         assertThat(Palace.HAN.isInPalace(new Position(x, y))).isTrue();
+    }
+
+    @Test
+    @DisplayName("궁성 중앙에서 갈 수 있는 대각선 경로는 4가지이다.")
+    void 궁성_중앙에서의_대각선_경로_4가지() {
+        // given
+        Position position = new Position(5, 9);
+
+        // when
+        List<Route> palacePositions = Palace.CHO.findDiagonalRoutes(position);
+
+        // then
+        assertThat(palacePositions).hasSize(4);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 8", "6, 8", "4, 10", "6, 10"
+    })
+    @DisplayName("궁성 꼭지점에서 갈 수 있는 대각선 경로는 1가지이다.")
+    void 궁성_꼭지점에서의_대각선_경로_1가지씩_존재(int x, int y) {
+        // given
+        Position position = new Position(x, y);
+
+        // when
+        List<Route> palacePositions = Palace.CHO.findDiagonalRoutes(position);
+
+        // then
+        assertThat(palacePositions).hasSize(1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "5, 1", "4, 2", "6, 2", "5, 3"
+    })
+    @DisplayName("궁성 모서리에서 갈 수 있는 대각선 경로는 없다.")
+    void 궁성_모서리에서의_대각선_경로_없음(int x, int y) {
+        // given
+        Position position = new Position(x, y);
+
+        // when
+        List<Route> palacePositions = Palace.HAN.findDiagonalRoutes(position);
+
+        // then
+        assertThat(palacePositions).isEmpty();
     }
 }
