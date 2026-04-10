1d4c3f02c4bfce33593ad13e2a983145fbd9189e
Jinriro <riro12.hanyang.ac.kr>
2026-04-10 14:35:30 +0900

    refactor: Side 클래스 수정

--- src/main/java/domain/state/Side.java
+++ src/main/java/domain/state/Side.java
@@ -2,21 +2,27 @@ package domain.state;
 
 public enum Side {
 
-    HAN(0),
-    CHU(9),
-    NEUTRAL(-1);
+    HAN(0, 1.5),
+    CHU(9, 0),
+    NEUTRAL(-1, 0);
 
     private final int startingRow;
+    private final double bonusScore;
     private Side opposite;
 
-    Side(int startingRow) {
+    Side(int startingRow, double bonusScore) {
         this.startingRow = startingRow;
+        this.bonusScore = bonusScore;
     }
 
     static {
         HAN.opposite = CHU;
         CHU.opposite = HAN;
         NEUTRAL.opposite = NEUTRAL;
+    }
+
+    public static GameState firstMoveSide() {
+        return new ChuSide();
     }
 
     public boolean isNeutral() {
@@ -29,5 +35,9 @@ public enum Side {
 
     public int getStartingRow() {
         return startingRow;
+    }
+
+    public double getBonusScore() {
+        return bonusScore;
     }
 }
