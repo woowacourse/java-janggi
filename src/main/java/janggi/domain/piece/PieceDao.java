d9bb0b4eafb3ea5df4a5e5dba3c7fd2c0476223a
Chocoding1 <galmeagi2@naver.com>
2026-04-10 20:44:31 +0900

    refactor: 기물 저장 중 오류 발생 시 rollback 기능 추가

--- src/main/java/janggi/domain/piece/PieceDao.java
+++ src/main/java/janggi/domain/piece/PieceDao.java
@@ -15,24 +15,42 @@ public class PieceDao {
     public void saveAll(List<PieceDto> pieceDtos) {
         String sql = "INSERT INTO piece (turn_id, piece_type, team_type, x, y) VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = DatabaseConnector.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
+        Connection conn = null;
+        try {
+            conn = DatabaseConnector.getConnection();
             conn.setAutoCommit(false);
 
-            for (PieceDto pieceDto : pieceDtos) {
-                pstmt.setLong(1, pieceDto.turnId());
-                pstmt.setString(2, pieceDto.pieceType().getName());
-                pstmt.setString(3, pieceDto.teamType().getName());
-                pstmt.setInt(4, pieceDto.x());
-                pstmt.setInt(5, pieceDto.y());
-                pstmt.addBatch();
-            }
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            pstmt.executeBatch();
-            conn.commit();
+                for (PieceDto pieceDto : pieceDtos) {
+                    pstmt.setLong(1, pieceDto.turnId());
+                    pstmt.setString(2, pieceDto.pieceType().getName());
+                    pstmt.setString(3, pieceDto.teamType().getName());
+                    pstmt.setInt(4, pieceDto.x());
+                    pstmt.setInt(5, pieceDto.y());
+                    pstmt.addBatch();
+                }
+
+                pstmt.executeBatch();
+                conn.commit();
+            }
         } catch (SQLException e) {
+            if (conn != null) {
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    e.addSuppressed(ex);
+                }
+            }
             throw new RuntimeException("생존 기물 데이터 저장 중 오류가 발생했습니다.", e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException("DB 컨넥션 해제 중 오류가 발생했습니다.", e);
+                }
+            }
         }
     }
 
