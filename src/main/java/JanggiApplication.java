c3f8dcb18e3f471f0191fb96f7ca7692d258e8bb
picetea44 <picetea44@gmail.com>
2026-04-10 16:53:19 +0900

    refactor: DB ConnectionManger, Dao, Repository 연결

--- src/main/java/JanggiApplication.java
+++ src/main/java/JanggiApplication.java
@@ -1,10 +1,24 @@
 import controller.JanggiController;
+import dao.BoardPieceDao;
+import dao.GameRoomDao;
+import db.ConnectionManager;
+import db.DatabaseInitializer;
+import repository.GameRepository;
+import service.JanggiGameService;
 import view.InputView;
 import view.OutputView;
 
 public class JanggiApplication {
     public static void main(String[] args) {
-        JanggiController janggiController = new JanggiController(new InputView(), new OutputView());
-        janggiController.run();
+        ConnectionManager connectionManager = new ConnectionManager();
+        new DatabaseInitializer(connectionManager).initialize();
+
+        GameRoomDao gameRoomDao = new GameRoomDao(connectionManager);
+        BoardPieceDao boardPieceDao = new BoardPieceDao(connectionManager);
+        GameRepository gameRepository = new GameRepository(gameRoomDao, boardPieceDao);
+        JanggiGameService gameService = new JanggiGameService(gameRepository);
+
+        JanggiController controller = new JanggiController(new InputView(), new OutputView(), gameService);
+        controller.run();
     }
 }
--- src/main/java/controller/JanggiController.java
+++ src/main/java/controller/JanggiController.java
@@ -1,38 +1,77 @@
 package controller;
 
+import dao.GameRoomRawData;
 import domain.board.formation.FormationType;
 import domain.game.JanggiGame;
 import domain.game.Team;
 import domain.position.Position;
 import java.util.List;
+import service.JanggiGameService;
 import view.InputView;
+import view.MainMenu;
 import view.OutputView;
 
 public class JanggiController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final JanggiGameService gameService;
 
-    public JanggiController(InputView inputView, OutputView outputView) {
+    public JanggiController(InputView inputView, OutputView outputView, JanggiGameService gameService) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.gameService = gameService;
     }
 
     public void run() {
-        JanggiGame game = createGame();
-        outputView.printBoard(game.getBoard());
+        while (true) {
+            MainMenu menu = inputView.askMainMenu();
+            if (menu == MainMenu.EXIT) {
+                return;
+            }
+            handleMenu(menu);
+        }
+    }
 
+    private void handleMenu(MainMenu menu) {
+        try {
+            if (menu == MainMenu.NEW_GAME) {
+                createAndPlayGame();
+                return;
+            }
+            if (menu == MainMenu.LOAD_GAME) {
+                listAndEnterRoom();
+            }
+        } catch (RuntimeException e) {
+            outputView.printError(e.getMessage());
+        }
+    }
+
+    private void createAndPlayGame() {
+        String roomName = inputView.askRoomName();
+        FormationType choFormation = FormationConverter.convert(inputView.initialFormation(Team.CHO));
+        FormationType hanFormation = FormationConverter.convert(inputView.initialFormation(Team.HAN));
+        JanggiGame game = gameService.createGame(roomName, choFormation, hanFormation);
+        outputView.printGameCreated(game.getId());
+        playGame(game);
+    }
+
+    private void listAndEnterRoom() {
+        List<GameRoomRawData> rooms = gameService.listRooms();
+        outputView.printRoomList(rooms);
+        if (rooms.isEmpty()) {
+            return;
+        }
+        long roomId = inputView.askRoomId();
+        playGame(gameService.enterGame(roomId));
+    }
+
+    private void playGame(JanggiGame game) {
+        outputView.printBoard(game.getBoard());
         while (game.isRunning()) {
             playTurn(game);
             outputView.printBoard(game.getBoard());
         }
-
         outputView.printResult(game.findWinner(), game.scoreOf(Team.CHO), game.scoreOf(Team.HAN));
-    }
-
-    private JanggiGame createGame() {
-        FormationType choFormation = FormationConverter.convert(inputView.initialFormation(Team.CHO));
-        FormationType hanFormation = FormationConverter.convert(inputView.initialFormation(Team.HAN));
-        return JanggiGame.of(choFormation, hanFormation);
     }
 
     private void playTurn(JanggiGame game) {
@@ -46,11 +85,11 @@ public class JanggiController {
         try {
             String input = inputView.askTurnInput(game.currentTurn());
             if (inputView.isPass(input)) {
-                game.pass();
+                gameService.pass(game);
                 return true;
             }
             List<Position> positions = inputView.parseMoveInput(input);
-            game.move(positions.get(0), positions.get(1));
+            gameService.move(game, positions.get(0), positions.get(1));
             return true;
         } catch (IllegalArgumentException e) {
             outputView.printError(e.getMessage());
