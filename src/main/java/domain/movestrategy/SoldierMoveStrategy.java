89088d82f0f06eb5eeebf480905608d72a0d2cba
zzaekkii <zzaekkii@gmail.com>
2026-04-12 02:37:29 +0900

    refactor(move): 졸병의 궁성 이동 전략 추가

--- src/main/java/domain/movestrategy/SoldierMoveStrategy.java
+++ src/main/java/domain/movestrategy/SoldierMoveStrategy.java
@@ -2,9 +2,10 @@ package domain.movestrategy;
 
 import domain.board.Board;
 import domain.piece.Delta;
-import domain.piece.Piece;
 import domain.piece.Position;
 import domain.player.Team;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SoldierMoveStrategy implements MoveStrategy {
@@ -19,20 +20,51 @@ public class SoldierMoveStrategy implements MoveStrategy {
 
     @Override
     public List<Position> calculateMovablePositions(final Position from, final Board board) {
-        final List<Delta> paths = getPathsByTeam(from, board);
+        final List<Position> movable = new ArrayList<>();
 
-        return paths.stream()
+        movable.addAll(calculateDefaultMoves(from, board));
+        movable.addAll(calculatePalaceMovablePositions(from, board));
+
+        return movable;
+    }
+
+    private List<Position> calculateDefaultMoves(final Position from, final Board board) {
+        final Team team = board.getPiece(from).getTeam();
+
+        return getDefaultPathsByTeam(team).stream()
                 .map(from::move)
                 .filter(board::inBoard)
                 .filter(to -> !isAlly(from, to, board))
                 .toList();
     }
 
+    @Override
+    public List<Position> calculatePalaceMovablePositions(final Position from, final Board board) {
+        if (!board.inPalace(from)) {
+            return List.of();
+        }
 
-    private static List<Delta> getPathsByTeam(final Position from, final Board board) {
-        final Piece fromPiece = board.getPiece(from);
+        final Team team = board.getPiece(from).getTeam();
 
-        if (fromPiece.isSameTeam(Team.HAN)) {
+        return board.getPalaceDeltas(from).stream()
+                .filter(delta -> isForward(delta, team))
+                .map(from::move)
+                .filter(to -> !isAlly(from, to, board))
+                .toList();
+    }
+
+
+    private boolean isForward(final Delta delta, final Team team) {
+        final int dy = delta.row();
+
+        if (team == Team.CHO) {
+            return dy < 0;
+        }
+        return dy > 0;
+    }
+
+    private static List<Delta> getDefaultPathsByTeam(final Team team) {
+        if (team == Team.HAN) {
             return HAN_PATHS;
         }
         return CHO_PATHS;
