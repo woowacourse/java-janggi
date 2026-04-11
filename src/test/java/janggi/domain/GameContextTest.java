940c1b0e0345e1d399a6400a0b42e4272e97e158
mvg01 <dkms625@naver.com>
2026-04-12 01:00:54 +0900

    refactor: 테스트 분리 및 셋업 설정

--- src/test/java/janggi/domain/GameContextTest.java
+++ src/test/java/janggi/domain/GameContextTest.java
@@ -9,6 +9,7 @@ import janggi.domain.team.TeamType;
 import janggi.domain.team.TurnManager;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -32,16 +33,23 @@ class GameContextTest {
     @DisplayName("makeMove 테스트")
     class MakeMove {
 
+        private Map<Position, Piece> pieceMap;
+        private Position from;
+        private Position to;
+        private GameContext gameContext;
+
+        @BeforeEach
+        void setup() {
+            from = Position.valueOf(1, 1);
+            to = Position.valueOf(1, 2);
+            pieceMap = new HashMap<>();
+            pieceMap.put(from, new General(TeamType.RED));
+            gameContext = new GameContext(new TurnManager(), new Board(pieceMap));
+        }
+
         @Test
         @DisplayName("기물을 이동하면 턴이 변경된다")
         void success1() {
-            // given
-            Position from = Position.valueOf(1, 1);
-            Position to = Position.valueOf(1, 2);
-            Map<Position, Piece> pieceMap = new HashMap<>();
-            pieceMap.put(from, new General(TeamType.RED));
-            GameContext gameContext = new GameContext(new TurnManager(), new Board(pieceMap));
-
             // when
             gameContext.makeMove(from, to);
 
@@ -52,19 +60,23 @@ class GameContextTest {
         @Test
         @DisplayName("기물을 이동하면 기물의 위치가 변경된다")
         void success2() {
-            // given
-            Position from = Position.valueOf(1, 1);
-            Position to = Position.valueOf(1, 2);
-            Map<Position, Piece> pieceMap = new HashMap<>();
-            pieceMap.put(from, new General(TeamType.RED));
-            GameContext gameContext = new GameContext(new TurnManager(), new Board(pieceMap));
-
             // when
             gameContext.makeMove(from, to);
 
             // then
             Map<Position, Piece> result = gameContext.getPositionPieceMap();
-            assertThat(result.containsKey(to) && !result.containsKey(from)).isTrue();
+            assertThat(result).containsKey(to);
+        }
+
+        @Test
+        @DisplayName("기물을 이동하면 기존의 위치는 보드에 포함되지 않는다")
+        void success3() {
+            // when
+            gameContext.makeMove(from, to);
+
+            // then
+            Map<Position, Piece> result = gameContext.getPositionPieceMap();
+            assertThat(result).doesNotContainKey(from);
         }
     }
 }
