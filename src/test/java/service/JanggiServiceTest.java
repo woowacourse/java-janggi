0f06c9845b7fe58a6e6982cd13289314c038de80
HyoYoonNam <dev.rudevico@gmail.com>
2026-04-11 23:16:20 +0900

    refactor: 테스트 독립성을 위한 clear 1회만 호출

--- src/test/java/service/JanggiServiceTest.java
+++ src/test/java/service/JanggiServiceTest.java
@@ -27,7 +27,6 @@ class JanggiServiceTest {
     void setUp() throws IOException {
         DataSource dataSource = MemoryDBConnectionUtil.getDataSource();
         janggiService = new JanggiService(new TransactionTemplate(dataSource), new JanggiGameRepository(dataSource));
-        janggiService.clear();
     }
 
     @AfterEach
