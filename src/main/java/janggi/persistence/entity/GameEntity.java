c81ae57c4152035a220819490026aa5fc3bb6f1d
SooHyeon Lee <dalsu4594@naver.com>
2026-04-11 15:19:17 +0900

    feat: 게임 이름 검증 추가

--- src/main/java/janggi/persistence/entity/GameEntity.java
+++ src/main/java/janggi/persistence/entity/GameEntity.java
@@ -10,10 +10,17 @@ public class GameEntity {
     private Turn turn;
 
     public GameEntity(String id, String name, Status status, Turn turn) {
+        validateNameLength(name);
         this.id = id;
         this.name = name;
         this.status = status;
         this.turn = turn;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() < 1 || name.length() > 50) {
+            throw new IllegalArgumentException("게임 이름 길이는 1이상 50 이하여야 합니다.");
+        }
     }
 
     public String id() {
