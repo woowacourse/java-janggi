397444e3502e4fe0057b0708752c8daed79667ec
Jihyun3478 <leejihyun3478@gmail.com>
2026-04-11 15:48:59 +0900

    refactor: 기물 이동 여부 예외를 Piece가 하도록 수정

--- src/main/java/domain/piece/Cannon.java
+++ src/main/java/domain/piece/Cannon.java
@@ -4,7 +4,6 @@ import domain.board.Country;
 import java.util.List;
 
 public class Cannon extends StraightMovingPiece {
-    private static final String CAN_NOT_MOVE_TO_POSITION = "[ERROR] 해당 경로로 기물을 이동시킬 수 없습니다.";
     private static final String CANNON_CAN_NOT_CATCH_CANNON = "[ERROR] 포는 포를 잡을 수 없습니다.";
 
     private static final int REQUIRED_PIECES_ON_PATH = 1;
--- src/main/java/domain/piece/Piece.java
+++ src/main/java/domain/piece/Piece.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Piece {
-    private static final String CAN_NOT_MOVE_TO_POSITION = "[ERROR] 해당 경로로 기물을 이동시킬 수 없습니다.";
+    protected static final String CAN_NOT_MOVE_TO_POSITION = "[ERROR] 해당 경로로 기물을 이동시킬 수 없습니다.";
 
     protected final PieceInfo pieceInfo;
 
