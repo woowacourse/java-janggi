40cd8c24db651aed6766a94744bb0c6bc4a9a3c0
Jiihyun <cwlgusc@gmail.com>
2026-04-11 14:59:44 +0900

    refactor: db 관련 예외메시지 구체적이게 변경

--- src/main/java/janggi/repository/GameRoomRepository.java
+++ src/main/java/janggi/repository/GameRoomRepository.java
@@ -4,10 +4,13 @@ import javax.sql.DataSource;
 import janggi.domain.game.GameRoom;
 import janggi.domain.game.GameStatus;
 import janggi.domain.piece.camp.CampType;
+import janggi.exception.ExceptionMessage;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,90 +23,98 @@ public class GameRoomRepository {
     }
 
     public long save(GameRoom gameRoom) {
-        return insert("INSERT INTO game_rooms(current_turn, game_status) VALUES(?, ?)",
-                gameRoom.getCurrentTurn().name(),
-                gameRoom.getGameStatus().name());
-    }
+        String sql = "INSERT INTO game_rooms(current_turn, game_status)" +
+                " VALUES(?, ?)";
 
-    private long insert(String sql, Object... params) {
-        try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            for (int i = 0; i < params.length; i++) {
-                preparedStatement.setObject(i + 1, params[i]);
-            }
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            preparedStatement.setString(1, gameRoom.getCurrentTurn().name());
+            preparedStatement.setString(2, gameRoom.getGameStatus().name());
+
             preparedStatement.execute();
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet.next()) {
-                return resultSet.getLong(1);
+
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    return resultSet.getLong(1);
+                }
             }
             return -1L;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(ExceptionMessage.GAME_ROOM_SAVE_ERROR.getMessage(), e);
         }
     }
 
     public List<Long> findAllByGameStatus(GameStatus gameStatus) {
-        return executeQuery("SELECT game_room_id FROM game_rooms WHERE game_status = ?",
-                resultSet -> {
-                    List<Long> ids = new ArrayList<>();
-                    while (resultSet.next()) {
-                        ids.add(resultSet.getLong("game_room_id"));
-                    }
-                    return ids;
-                }, gameStatus.name());
-    }
+        String sql = "SELECT game_room_id " +
+                "FROM game_rooms " +
+                "WHERE game_status = ?";
 
-    public GameRoom findById(long gameRoomId) {
-        return executeQuery("SELECT * FROM game_rooms WHERE game_room_id = ?",
-                rs -> {
-                    if (rs.next()) {
-                        return new GameRoom(
-                                rs.getLong("game_room_id"),
-                                CampType.valueOf(rs.getString("current_turn")),
-                                GameStatus.valueOf(rs.getString("game_status")),
-                                rs.getTimestamp("start_at").toLocalDateTime(),
-                                rs.getTimestamp("end_at") != null
-                                        ? rs.getTimestamp("end_at").toLocalDateTime()
-                                        : null,
-                                rs.getTimestamp("last_updated_at").toLocalDateTime()
-                        );
-                    }
-                    throw new RuntimeException("게임방을 찾을 수 없습니다. gameRoomId: " + gameRoomId);
-                }, gameRoomId);
-    }
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-    public void update(GameRoom gameRoom) {
-        update("UPDATE game_rooms SET current_turn = ?, game_status = ?, start_at = ?,  end_at = ?, last_updated_at = CURRENT_TIMESTAMP WHERE game_room_id = ?",
-                gameRoom.getCurrentTurn().name(),
-                gameRoom.getGameStatus().name(),
-                gameRoom.getStartAt(),
-                gameRoom.getEndAt(),
-                gameRoom.getGameRoomId()
-        );
-    }
+            preparedStatement.setString(1, gameStatus.name());
 
-    private void update(String sql, Object... params) {
-        try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(sql);
-            for (int i = 0; i < params.length; i++) {
-                preparedStatement.setObject(i + 1, params[i]);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                List<Long> ids = new ArrayList<>();
+                while (resultSet.next()) {
+                    ids.add(resultSet.getLong("game_room_id"));
+                }
+                return ids;
             }
-            preparedStatement.execute();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(ExceptionMessage.GAME_ROOM_FIND_ERROR.getMessage(), e);
         }
     }
 
-    private <T> T executeQuery(String sql, ResultSetMapper<T> mapper, Object... params) {
-        try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(sql);
-            for (int i = 0; i < params.length; i++) {
-                preparedStatement.setObject(i + 1, params[i]);
+    public GameRoom findById(long gameRoomId) {
+        String sql = "SELECT * FROM game_rooms" +
+                " WHERE game_room_id = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, gameRoomId);
+
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                if (rs.next()) {
+                    return new GameRoom(
+                            rs.getLong("game_room_id"),
+                            CampType.valueOf(rs.getString("current_turn")),
+                            GameStatus.valueOf(rs.getString("game_status")),
+                            rs.getTimestamp("start_at").toLocalDateTime(),
+                            rs.getTimestamp("end_at") != null
+                                    ? rs.getTimestamp("end_at").toLocalDateTime()
+                                    : null,
+                            rs.getTimestamp("last_updated_at").toLocalDateTime()
+                    );
+                }
+                throw new IllegalArgumentException(ExceptionMessage.GAME_ROOM_NOT_FOUND.getMessage(gameRoomId));
             }
-            ResultSet rs = preparedStatement.executeQuery();
-            return mapper.map(rs);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(ExceptionMessage.GAME_ROOM_FIND_ERROR.getMessage(gameRoomId), e);
+        }
+    }
+
+    public void update(GameRoom gameRoom) {
+        String sql = "UPDATE game_rooms " +
+                "SET current_turn = ?, game_status = ?, start_at = ?,  end_at = ?, last_updated_at = CURRENT_TIMESTAMP" +
+                " WHERE game_room_id = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, gameRoom.getCurrentTurn().name());
+            preparedStatement.setString(2, gameRoom.getGameStatus().name());
+            preparedStatement.setTimestamp(3, Timestamp.valueOf(gameRoom.getStartAt()));
+            preparedStatement.setTimestamp(4, gameRoom.getEndAt() != null
+                    ? Timestamp.valueOf(gameRoom.getEndAt())
+                    : null);
+            preparedStatement.setLong(5, gameRoom.getGameRoomId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(ExceptionMessage.GAME_ROOM_UPDATE_ERROR.getMessage(gameRoom.getGameRoomId()), e);
         }
     }
 }
--- src/main/java/janggi/repository/PieceRepository.java
+++ src/main/java/janggi/repository/PieceRepository.java
@@ -6,6 +6,8 @@ import janggi.domain.piece.Piece;
 import janggi.domain.piece.PieceRule;
 import janggi.domain.piece.PlacedPiece;
 import janggi.domain.piece.camp.CampType;
+import janggi.exception.ExceptionMessage;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,98 +24,118 @@ public class PieceRepository {
     }
 
     public long save(PlacedPiece placedPiece) {
-        return insert("INSERT INTO pieces(game_room_id, camp, piece_type, row_position, col_position) VALUES(?, ?, ?, ?, ?)",
-                placedPiece.getGameRoomId(),
-                placedPiece.getCampType().name(),
-                placedPiece.getPieceRule().name(),
-                placedPiece.getRowPosition(),
-                placedPiece.getColPosition());
-    }
+        String sql = "INSERT INTO pieces(game_room_id, camp, piece_type, row_position, col_position)" +
+                " VALUES(?, ?, ?, ?, ?)";
 
-    private long insert(String sql, Object... params) {
-        try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            for (int i = 0; i < params.length; i++) {
-                preparedStatement.setObject(i + 1, params[i]);
-            }
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            preparedStatement.setLong(1, placedPiece.getGameRoomId());
+            preparedStatement.setString(2, placedPiece.getCampType().name());
+            preparedStatement.setString(3, placedPiece.getPieceRule().name());
+            preparedStatement.setInt(4, placedPiece.getRowPosition());
+            preparedStatement.setInt(5, placedPiece.getColPosition());
+
             preparedStatement.execute();
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet.next()) {
-                return resultSet.getLong(1);
+
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    return resultSet.getLong(1);
+                }
             }
             return -1L;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(ExceptionMessage.PIECE_SAVE_ERROR.getMessage(
+                    placedPiece.getGameRoomId(), placedPiece.getRowPosition(), placedPiece.getColPosition()), e);
         }
     }
 
     public Map<Position, Piece> findByGameRoomId(long gameRoomId) {
-        return executeQuery("SELECT * FROM pieces WHERE game_room_id = ?",
-                rs -> {
-                    Map<Position, Piece> pieces = new HashMap<>();
-                    while (rs.next()) {
-                        Position position = new Position(rs.getInt("row_position"), rs.getInt("col_position"));
-                        Piece piece = new Piece(PieceRule.valueOf(rs.getString("piece_type")), CampType.valueOf(rs.getString("camp")));
-                        pieces.put(position, piece);
-                    }
-                    return pieces;
-                },
-                gameRoomId
-        );
-    }
+        String sql = "SELECT * FROM pieces" +
+                " WHERE game_room_id = ?";
 
-    public PlacedPiece findByGameIdAndPosition(long gameRoomId, int row, int column) {
-        return executeQuery("SELECT * FROM pieces WHERE game_room_id = ? AND row_position = ? AND col_position = ?",
-                rs -> {
-                    if (rs.next()) {
-                        return new PlacedPiece(
-                                rs.getLong("piece_id"),
-                                rs.getLong("game_room_id"),
-                                CampType.valueOf(rs.getString("camp")),
-                                PieceRule.valueOf(rs.getString("piece_type")),
-                                rs.getInt("row_position"),
-                                rs.getInt("col_position")
-                        );
-                    }
-                    throw new RuntimeException(String.format("%d번 게임방 내 %행%d열 기물을 찾을 수 없습니다", gameRoomId, row, column));
-                },
-                gameRoomId, row, column
-        );
-    }
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-    public void update(PlacedPiece placedPiece) {
-        update("UPDATE pieces SET game_room_id = ?, camp = ?, piece_type = ?, row_position = ?, col_position = ? WHERE piece_id = ?",
-                placedPiece.getGameRoomId(), placedPiece.getCampType().name(), placedPiece.getPieceRule().name()
-                , placedPiece.getRowPosition(), placedPiece.getColPosition(), placedPiece.getPlacedPieceId());
-    }
+            preparedStatement.setLong(1, gameRoomId);
 
-    public void delete(PlacedPiece placedPiece) {
-        update("DELETE FROM pieces WHERE piece_id = ?",
-                placedPiece.getPlacedPieceId());
-    }
-
-    private void update(String sql, Object... params) {
-        try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(sql);
-            for (int i = 0; i < params.length; i++) {
-                preparedStatement.setObject(i + 1, params[i]);
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                Map<Position, Piece> pieces = new HashMap<>();
+                while (rs.next()) {
+                    Position position = new Position(rs.getInt("row_position"), rs.getInt("col_position"));
+                    Piece piece = new Piece(PieceRule.valueOf(rs.getString("piece_type")), CampType.valueOf(rs.getString("camp")));
+                    pieces.put(position, piece);
+                }
+                return pieces;
             }
-            preparedStatement.execute();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(ExceptionMessage.PIECE_FIND_ERROR.getMessage(gameRoomId), e);
         }
     }
 
-    private <T> T executeQuery(String sql, ResultSetMapper<T> mapper, Object... params) {
-        try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(sql);
-            for (int i = 0; i < params.length; i++) {
-                preparedStatement.setObject(i + 1, params[i]);
+    public PlacedPiece findByGameIdAndPosition(long gameRoomId, int row, int column) {
+        String sql = "SELECT * FROM pieces " +
+                "WHERE game_room_id = ? AND row_position = ? AND col_position = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, gameRoomId);
+            preparedStatement.setInt(2, row);
+            preparedStatement.setInt(3, column);
+
+            try (ResultSet rs = preparedStatement.executeQuery()) {
+                if (rs.next()) {
+                    return new PlacedPiece(
+                            rs.getLong("piece_id"),
+                            rs.getLong("game_room_id"),
+                            CampType.valueOf(rs.getString("camp")),
+                            PieceRule.valueOf(rs.getString("piece_type")),
+                            rs.getInt("row_position"),
+                            rs.getInt("col_position")
+                    );
+                }
+                throw new IllegalArgumentException(ExceptionMessage.PIECE_NOT_FOUND.getMessage(gameRoomId, row, column));
             }
-            ResultSet rs = preparedStatement.executeQuery();
-            return mapper.map(rs);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(ExceptionMessage.PIECE_FIND_ERROR.getMessage(gameRoomId), e);
+        }
+    }
+
+    public void update(PlacedPiece placedPiece) {
+        String sql = "UPDATE pieces" +
+                " SET game_room_id = ?, camp = ?, piece_type = ?, row_position = ?, col_position = ? " +
+                "WHERE piece_id = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, placedPiece.getGameRoomId());
+            preparedStatement.setString(2, placedPiece.getCampType().name());
+            preparedStatement.setString(3, placedPiece.getPieceRule().name());
+            preparedStatement.setInt(4, placedPiece.getRowPosition());
+            preparedStatement.setInt(5, placedPiece.getColPosition());
+            preparedStatement.setLong(6, placedPiece.getPlacedPieceId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(ExceptionMessage.PIECE_UPDATE_ERROR.getMessage(
+                    placedPiece.getGameRoomId(), placedPiece.getRowPosition(), placedPiece.getColPosition()), e);
+        }
+    }
+
+    public void delete(PlacedPiece placedPiece) {
+        String sql = "DELETE FROM pieces " +
+                "WHERE piece_id = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, placedPiece.getPlacedPieceId());
+            
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(ExceptionMessage.PIECE_DELETE_ERROR.getMessage(placedPiece.getPlacedPieceId()), e);
         }
     }
 }
