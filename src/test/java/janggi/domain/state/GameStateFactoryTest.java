1f6f61d659f32b13cb9d3f3fe77ee7e1de38a820
JYL35 <dlwodbs1223@naver.com>
2026-04-11 21:23:06 +0900

    test(GameStateFactory): GameState 생성 테스트 작성

---
+++ src/test/java/janggi/domain/state/GameStateFactoryTest.java
@@ -0,0 +1,42 @@
+package janggi.domain.state;
+
+import janggi.domain.piece.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GameStateFactoryTest {
+
+    static Stream<Arguments> stateProvider() {
+        return Stream.of(
+                Arguments.of("DRAW", "CHO", false, Draw.class),
+                Arguments.of("Checkmate", "CHO", false, Checkmate.class),
+                Arguments.of("GiveUp", "CHO", false, GiveUp.class),
+                Arguments.of("ChoTurn", "CHO", true, ChoTurn.class),
+                Arguments.of("HanTurn", "HAN", true, HanTurn.class)
+        );
+    }
+
+    @DisplayName("올바른 상태 타입이면 게임 상태를 반환한다")
+    @ParameterizedTest
+    @MethodSource("stateProvider")
+    void create_CorrectStateType_ReturnCorrectState(String stateType, String turn, boolean ongoing, Class<? extends Piece> stateClass) {
+        assertThat(GameStateFactory.create(turn, ongoing, stateType))
+                .isInstanceOf(stateClass);
+    }
+
+    @DisplayName("올바르지 않은 종료 상태 타입이면 예외를 발생한다")
+    @Test
+    void create_CorrectStateType_ReturnCorrectState() {
+        assertThatThrownBy(() -> GameStateFactory.create("HAN", false, "INCORRECT"))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("지원되지 않는 게임 종료 상태입니다.");
+    }
+}
