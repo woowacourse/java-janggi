9962b8a118dc5c1bf6ae706bfec5ff4ec7d7b2c7
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-12 23:40:07 +0900

    refactor: instanceof 대신 기물 타입으로 비교하도록 수정

--- src/main/java/domain/board/JanggiBoard.java
+++ src/main/java/domain/board/JanggiBoard.java
@@ -52,7 +52,7 @@ public class JanggiBoard implements PieceProvider {
 
     @Override
     public boolean isBlank(Position position) {
-        return getPiece(position) instanceof Blank;
+        return getPiece(position).getPieceType() == PieceType.BLANK;
     }
 
     @Override
