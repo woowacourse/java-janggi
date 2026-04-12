f38ad2ea535cfc4101b16adf59dd2dd71e97909d
Jihyun3478 <leejihyun3478@gmail.com>
2026-04-12 11:52:12 +0900

    refactor: gameId가 null일 경우 IllegalStateException을 던지도록 수정

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -5,12 +5,15 @@ import dto.GameInfo;
 import java.time.LocalDateTime;
 
 public class Game {
-    private Long gameId;
+    private static final String GAME_IS_NOT_SAVED = "[ERROR] 저장되지 않은 게임입니다.";
+
+    private final Long gameId;
     private final LocalDateTime createdAt;
     private final TableSetting choTableSetting;
     private final TableSetting hanTableSetting;
 
     public Game(TableSetting choTableSetting, TableSetting hanTableSetting) {
+        gameId = null;
         this.createdAt = LocalDateTime.now();
         this.choTableSetting = choTableSetting;
         this.hanTableSetting = hanTableSetting;
@@ -24,6 +27,9 @@ public class Game {
     }
 
     public Long getGameId() {
+        if (gameId == null) {
+            throw new IllegalStateException(GAME_IS_NOT_SAVED);
+        }
         return gameId;
     }
 
