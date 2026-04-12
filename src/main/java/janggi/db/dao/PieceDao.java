32e950c2c7e293b0521ce433193501b37d2746ff
rin <chaerin0916@gmail.com>
2026-04-12 22:17:49 +0900

    refactor(PieceDao): 메서드 추출을 통해 뎁스1로 수정

--- src/main/java/janggi/db/dao/PieceDao.java
+++ src/main/java/janggi/db/dao/PieceDao.java
@@ -18,15 +18,7 @@ public class PieceDao {
 
         try (Connection connection = DbConnector.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
-            for (PieceEntity piece : pieces) {
-                preparedStatement.setLong(1, piece.getGameId());
-                preparedStatement.setInt(2, piece.getX());
-                preparedStatement.setInt(3, piece.getY());
-                preparedStatement.setString(4, piece.getPieceType().name());
-                preparedStatement.setString(5, piece.getTeam().name());
-                preparedStatement.addBatch();
-            }
+            collectPieceBatch(pieces, preparedStatement);
             preparedStatement.executeBatch();
         } catch (SQLException e) {
             throw new RuntimeException("[ERROR] 기물 저장 중 오류가 발생했습니다", e);
@@ -48,26 +40,49 @@ public class PieceDao {
 
     public List<PieceEntity> findAllByGameId(Long gameId) {
         String sql = "SELECT * FROM pieces WHERE game_id = ?";
-        List<PieceEntity> pieces = new ArrayList<>();
 
         try (Connection connection = DbConnector.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setLong(1, gameId);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                while (resultSet.next()) {
-                    pieces.add(new PieceEntity(
-                            resultSet.getLong("game_id"),
-                            resultSet.getInt("x"),
-                            resultSet.getInt("y"),
-                            PieceType.valueOf(resultSet.getString("piece_type")),
-                            Team.valueOf(resultSet.getString("team"))
-                    ));
-                }
-            }
+            return getPieceEntities(preparedStatement);
         } catch (SQLException e) {
             throw new RuntimeException("[ERROR] 기물 조회 중 오류가 발생했습니다", e);
         }
+    }
+
+    private List<PieceEntity> getPieceEntities(PreparedStatement preparedStatement) throws SQLException {
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            return collectPieces(resultSet);
+        }
+    }
+
+    private List<PieceEntity> collectPieces(ResultSet resultSet) throws SQLException {
+        List<PieceEntity> pieces = new ArrayList<>();
+        while (resultSet.next()) {
+            pieces.add(mapToEntity(resultSet));
+        }
         return pieces;
+    }
+
+    private void collectPieceBatch(List<PieceEntity> pieces, PreparedStatement preparedStatement) throws SQLException {
+        for (PieceEntity piece : pieces) {
+            preparedStatement.setLong(1, piece.getGameId());
+            preparedStatement.setInt(2, piece.getX());
+            preparedStatement.setInt(3, piece.getY());
+            preparedStatement.setString(4, piece.getPieceType().name());
+            preparedStatement.setString(5, piece.getTeam().name());
+            preparedStatement.addBatch();
+        }
+    }
+
+    private PieceEntity mapToEntity(ResultSet resultSet) throws SQLException {
+        return new PieceEntity(
+                resultSet.getLong("game_id"),
+                resultSet.getInt("x"),
+                resultSet.getInt("y"),
+                PieceType.valueOf(resultSet.getString("piece_type")),
+                Team.valueOf(resultSet.getString("team"))
+        );
     }
 }
