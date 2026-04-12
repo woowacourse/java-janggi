b0f880754690d812d11d0a2d8f4f34ab53fa07b1
koreaioi <koreaioi@gachon.ac.kr>
2026-04-12 21:30:08 +0900

    refactor: KOMI대신 BONUS로 변경
    - '덤'을 의미하는 KOMI는 바둑 용어
    - KOMI라는 단어 자체를 사람들이 잘 모름.

--- src/main/java/database/dto/GameResult.java
+++ src/main/java/database/dto/GameResult.java
@@ -9,10 +9,10 @@ public record GameResult(
         double choScore
 ) {
 
-    public static final double KOMI_POINT = 1.5;
+    public static final double BONUS_POINT = 1.5;
 
     public static GameResult from(JanggiBoard janggiBoard) {
-        double hanScore = janggiBoard.calculateTeamScore(Team.HAN) + KOMI_POINT;
+        double hanScore = janggiBoard.calculateTeamScore(Team.HAN) + BONUS_POINT;
         double choScore = janggiBoard.calculateTeamScore(Team.CHO);
 
         return new GameResult(janggiBoard.getWinner(), hanScore, choScore);
