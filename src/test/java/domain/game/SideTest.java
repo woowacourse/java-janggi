db2a00655403252a7dfef25669928509dd4db047
HyoYoonNam <dev.rudevico@gmail.com>
2026-04-12 20:41:05 +0900

    refactor: 미사용 import 제거

--- src/test/java/domain/game/SideTest.java
+++ src/test/java/domain/game/SideTest.java
@@ -6,13 +6,9 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 import domain.board.Intersection;
 import domain.direction.MoveAmount;
 import java.util.List;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("Side 테스트")
 class SideTest {
