3bf024ba7845ea418c8fa7313221c34a6d5c8089
choubung <choubung@sookmyung.ac.kr>
2026-04-13 15:39:02 +0900

    docs : 스키마 글자 수 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS game (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     is_finished BOOLEAN DEFAULT FALSE,
-    current_turn VARCHAR(10) NOT NULL
+    current_turn VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS piece (
     game_id BIGINT,
     row_index INT,
     col_index INT,
-    piece_type VARCHAR(10),
-    team VARCHAR(10),
+    piece_type VARCHAR(50),
+    team VARCHAR(50),
     PRIMARY KEY (game_id, row_index, col_index),
     FOREIGN KEY (game_id) REFERENCES game(id)
 );
