c136f5036ffa03fa8ee35665bf3f00a170a78b31
최용준 <yj9107vv@gmail.com>
2026-04-13 11:18:25 +0900

    style: isCannon 메서드 호출 공백 제거

--- src/main/java/domain/PieceProperty.java
+++ src/main/java/domain/PieceProperty.java
@@ -6,7 +6,7 @@ public record PieceProperty(PieceType pieceType, Team team) {
         return pieceType == PieceType.GENERAL;
     }
 
-    public boolean isCannon () {
+    public boolean isCannon() {
         return pieceType == PieceType.CANNON;
     }
 
