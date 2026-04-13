12f062370b4adf6541ff35ef5b9ccfda20172339
zzaekkii <zzaekkii@gmail.com>
2026-04-13 16:14:50 +0900

    feat(jdbc): jdbc 커넥션 연결 설정 추가

---
+++ src/main/resources/janggi_game_initialize_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS game (
+    game_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    status VARCHAR(10) NOT NULL,
+    current_player_id BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS player (
+    player_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    game_id BIGINT NOT NULL,
+    name VARCHAR(10) NOT NULL,
+    team VARCHAR(10) NOT NULL,
+    score INT NOT NULL,
+    CONSTRAINT uq_player_game_team UNIQUE (game_id, team),
+    CONSTRAINT fk_player_game FOREIGN KEY (game_id) REFERENCES game(game_id)
+);
+
+CREATE TABLE IF NOT EXISTS piece (
+    piece_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    game_id BIGINT NOT NULL,
+    team VARCHAR(10) NOT NULL,
+    piece_type VARCHAR(10) NOT NULL,
+    row INT NOT NULL,
+    column INT NOT NULL,
+    CONSTRAINT uq_piece_game_position UNIQUE (game_id, row, column),
+    CONSTRAINT fk_piece_game FOREIGN KEY (game_id) REFERENCES game(game_id)
+);
