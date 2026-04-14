da4616accdd6a8586fa52ca73656988300f97a61
haechanmoon <moonhc.dev@gmail.com>
2026-04-14 21:48:54 +0900

    feat: schema를 파일로 분리

--- src/main/java/janggi/dao/DBConnection.java
+++ src/main/java/janggi/dao/DBConnection.java
@@ -1,9 +1,11 @@
 package janggi.dao;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Objects;
 
 public class DBConnection {
     private static final String URL = "jdbc:h2:./janggi_db";
@@ -19,32 +21,15 @@ public class DBConnection {
     }
 
     public static void initializeDatabase() {
-        String createGameTableQuery = """
-                CREATE TABLE IF NOT EXISTS game (
-                    game_id INT AUTO_INCREMENT PRIMARY KEY,
-                    current_turn VARCHAR(10) NOT NULL
-                );
-                """;
-
-        String createPieceTableQuery = """
-                CREATE TABLE IF NOT EXISTS piece (
-                    piece_id INT AUTO_INCREMENT PRIMARY KEY,
-                    game_id INT,
-                    team VARCHAR(10) NOT NULL,
-                    piece_type VARCHAR(10) NOT NULL,
-                    position_row INT NOT NULL,
-                    position_column INT NOT NULL,
-                    FOREIGN KEY (game_id) REFERENCES game(game_id) ON DELETE CASCADE
-                );
-                """;
-
         try (Connection connection = getConnection();
              Statement statement = connection.createStatement()) {
+            String sql = new String(
+                    Objects.requireNonNull(DBConnection.class.getResourceAsStream("/schema.sql")).readAllBytes()
+            );
+            statement.execute(sql.split(";")[0]);
+            statement.execute(sql.split(";")[1]);
 
-            statement.execute(createGameTableQuery);
-            statement.execute(createPieceTableQuery);
-
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException("DB 테이블 초기화 실패: " + e.getMessage());
         }
     }
