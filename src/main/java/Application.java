970c8172b256255401cc476fceaad8cd470ffe05
poketopa <lhs5427ll@gmail.com>
2026-04-14 10:57:18 +0900

    feat(application): 컨트롤러 dao 의존성 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,24 @@
+import config.ConnectionFactory;
+import config.DatabaseConfig;
 import controller.JanggiController;
+import dao.GameDao;
 import mapper.BoardOutputMapper;
+import transaction.TransactionTemplate;
 import view.InputView;
 import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        ConnectionFactory connectionFactory = DatabaseConfig.createConnectionFactory();
+
+        GameDao gameDao = new GameDao(connectionFactory);
+        TransactionTemplate transactionTemplate = new TransactionTemplate(connectionFactory);
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         BoardOutputMapper boardOutputMapper = new BoardOutputMapper();
 
-        JanggiController janggiController = new JanggiController(inputView, outputView, boardOutputMapper);
+        JanggiController janggiController = new JanggiController(
+                inputView, outputView, boardOutputMapper, gameDao, transactionTemplate);
         janggiController.run();
     }
 }
