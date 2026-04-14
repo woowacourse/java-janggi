79073ce5292de859973bad2dcf9d344d194e272a
poketopa <lhs5427ll@gmail.com>
2026-04-14 11:12:33 +0900

    feat(config): DB 커넥션 팩토리 추가

---
+++ src/main/java/config/ConnectionFactory.java
@@ -0,0 +1,9 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface ConnectionFactory {
+    Connection getConnection() throws SQLException;
+}
