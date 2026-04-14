e16bc527bf06e1371977cc61dd0772f11660102c
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-04-14 10:00:30 +0900

    refactor(PieceFactory): 기물 생성 방법 개선

--- src/main/java/janggi/domain/piece/PieceFactory.java
+++ src/main/java/janggi/domain/piece/PieceFactory.java
@@ -11,23 +11,21 @@ public class PieceFactory {
     private PieceFactory() {
     }
 
-    private static final Map<String, Function<Camp, Piece>> PIECE_CREATORS = Map.of(
-            "GENERAL", camp -> new General(camp, new GeneralStrategy()),
-            "ADVISOR", camp -> new Advisor(camp, new AdvisorStrategy()),
-            "CHARIOT", camp -> new Chariot(camp, new ChariotStrategy()),
-            "CANNON", camp -> new Cannon(camp, new CannonStrategy()),
-            "HORSE", camp -> new Horse(camp, new HorseStrategy()),
-            "ELEPHANT", camp -> new Elephant(camp, new ElephantStrategy()),
-            "SOLDIER", camp -> new Soldier(camp, new SoldierStrategy(camp.forward()))
+    private static final Map<PieceDisplayName, Function<Camp, Piece>> PIECE_CREATORS = Map.of(
+            PieceDisplayName.GENERAL, camp -> new General(camp, new GeneralStrategy()),
+            PieceDisplayName.ADVISOR, camp -> new Advisor(camp, new AdvisorStrategy()),
+            PieceDisplayName.CHARIOT, camp -> new Chariot(camp, new ChariotStrategy()),
+            PieceDisplayName.CANNON, camp -> new Cannon(camp, new CannonStrategy()),
+            PieceDisplayName.HORSE, camp -> new Horse(camp, new HorseStrategy()),
+            PieceDisplayName.ELEPHANT, camp -> new Elephant(camp, new ElephantStrategy()),
+            PieceDisplayName.SOLDIER, camp -> new Soldier(camp, new SoldierStrategy(camp.forward()))
     );
 
     public static Piece create(String pieceType, Camp camp) {
-        validatePieceType(pieceType);
-        return PIECE_CREATORS.get(pieceType).apply(camp);
-    }
-
-    private static void validatePieceType(String pieceType) {
-        if (!PIECE_CREATORS.containsKey(pieceType)) {
+        try {
+            PieceDisplayName typeName = PieceDisplayName.valueOf(pieceType);
+            return PIECE_CREATORS.get(typeName).apply(camp);
+        } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("존재하지 않는 기물 타입입니다: " + pieceType);
         }
     }
