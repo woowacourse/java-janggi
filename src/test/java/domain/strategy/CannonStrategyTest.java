9b8751590614a804abe0345479d2f0cc1fcc8d99
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-16 23:26:36 +0900

    fix: 포 전략의 적 기물을 건너뛰는 기능테스

--- src/test/java/domain/strategy/CannonStrategyTest.java
+++ src/test/java/domain/strategy/CannonStrategyTest.java
@@ -14,32 +14,43 @@ import static org.assertj.core.api.Assertions.*;
 
     private CannonStrategy cannonStrategy;
     private TestFixture testBoard;
+    private final Position currentPosition = new Position(0, 0);
 
     @BeforeEach
     public void setUp() {
         cannonStrategy = new CannonStrategy();
         testBoard = new TestFixture();
+        testBoard.setPiece(currentPosition, new Cannon(Team.CHO));
     }
 
     @Test
     void 포가_기물_한개를_넘어_빈칸으로_이동하는지_확인한다() {
-        Position currentPosition = new Position(0, 0);
-        testBoard.setPiece(new Position(0, 0), new Cannon(Team.CHO));
-        testBoard.setPiece(new Position(2, 0), new Guard(Team.CHO));
+        testBoard.setPiece(new Position(2, 0), new Guard(Team.HAN));
 
         List<Position> candidates = cannonStrategy.getMoveCandidates(currentPosition, Team.CHO, testBoard);
+
         assertThat(candidates).contains(new Position(3, 0), new Position(9, 0));
         assertThat(candidates).doesNotContain(new Position(1, 0), new Position(2, 0));
     }
 
     @Test
     void 포는_다른포를_건너뛸_수_없다() {
-        Position currentPosition = new Position(0, 0);
-        testBoard.setPiece(new Position(2, 0), new Cannon(Team.CHO));
+        testBoard.setPiece(new Position(2, 0), new Cannon(Team.HAN));
 
         List<Position> candidates = cannonStrategy.getMoveCandidates(currentPosition, Team.CHO, testBoard);
-        assertThat(candidates.size()).isEqualTo(0);
+
+        assertThat(candidates).isEmpty();
     }
+
+     @Test
+     void 포는_적_기물을_건너뛸_수_있다() {
+         Position currentPosition = new Position(0, 0);
+         testBoard.setPiece(new Position(4, 0), new Pawn(Team.HAN));
+
+         List<Position> candidates = cannonStrategy.getMoveCandidates(currentPosition, Team.CHO, testBoard);
+         assertThat(candidates).contains(new Position(5, 0));
+         assertThat(candidates).doesNotContain(new Position(3, 0));
+     }
 
     @Test
     void 포는_기물이_없으면_이동할_수_없다() {
